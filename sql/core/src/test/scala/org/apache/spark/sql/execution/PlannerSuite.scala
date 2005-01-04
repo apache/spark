93367a77aa787b67a3bc775a42d3cf820007a0f0
jiake <ke.a.jia@intel.com>
2005-01-05 02:44:55 +0800

    compile error

--- sql/core/src/test/scala/org/apache/spark/sql/execution/PlannerSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/PlannerSuite.scala
@@ -766,7 +766,8 @@ class PlannerSuite extends SharedSparkSession with AdaptiveSparkPlanHelper {
              SortExec(_, _,
                ShuffleExchangeExec(HashPartitioning(leftPartitioningExpressions, _), _, _), _),
              SortExec(_, _,
-               ShuffleExchangeExec(HashPartitioning(rightPartitioningExpressions, _), _, _), _), _) =>
+               ShuffleExchangeExec(HashPartitioning(rightPartitioningExpressions, _),
+               _, _), _), _) =>
         assert(leftKeys === smjExec.leftKeys)
         assert(rightKeys === smjExec.rightKeys)
         assert(leftKeys === leftPartitioningExpressions)
