e93f6226658cc18fd29995e72f73c0d9246682d3
Antonio <antonio@interpals.net>
2012-02-11 00:56:28 -0800

    Added sorting by key for pair RDDs

--- core/src/main/scala/spark/Partitioner.scala
+++ core/src/main/scala/spark/Partitioner.scala
@@ -24,3 +24,40 @@ class HashPartitioner(partitions: Int) extends Partitioner {
       false
   }
 }
+
+class RangePartitioner[K <% Ordered[K],V](partitions: Int, rdd: RDD[(K,V)], ascending: Boolean = true) 
+  extends Partitioner {
+
+  def numPartitions = partitions
+
+  val rddSize = rdd.count()
+  val maxSampleSize = partitions*10.0
+  val frac = 1.0.min(maxSampleSize / rddSize)
+  val rddSample = rdd.sample(true, frac, 1).collect.toList
+    .sortWith((x, y) => if (ascending) x._1 < y._1 else x._1 > y._1)
+    .map(_._1)
+  val bucketSize:Float = rddSample.size / partitions
+  val rangeBounds = rddSample.zipWithIndex.filter(_._2 % bucketSize == 0)
+    .map(_._1).slice(1, partitions)
+
+  def getPartition(key: Any): Int = { 
+    key match {
+      case k:K => {
+        val p = 
+          rangeBounds.zipWithIndex.foldLeft(0) {
+            case (part, (bound, index)) =>  
+              if (k > bound) index + 1 else part
+          }   
+        if (ascending) p else numPartitions-1-p
+      }   
+      case _ => 0
+    }
+  }
+
+  override def equals(other: Any): Boolean = other match {
+    case r: RangePartitioner[K,V] =>
+      r.numPartitions == numPartitions
+    case _ => false
+  }
+}
+
