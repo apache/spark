85cd9979f2ba0ed6c0b3d458ab4d3d4f0a7909b2
Mosharaf Chowdhury <mosharaf@cs.berkeley.edu>
2012-07-13 01:13:14 -0700

    Fix for isLocal

--- core/src/main/scala/spark/SparkContext.scala
+++ core/src/main/scala/spark/SparkContext.scala
@@ -63,7 +63,7 @@ class SparkContext(
     System.setProperty("spark.master.port", "0")
   }
 
-  private val isLocal = (master == "local" || master.startsWith("local["))
+  private val isLocal = (master == "local" || master.startsWith("local[")) && !master.startsWith("localhost")
 
   // Create the Spark execution environment (cache, map output tracker, etc)
   val env = SparkEnv.createFromSystemProperties(
