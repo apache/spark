d1b7f41671feb6e17e98383b1770757b4941cc3b
Tathagata Das <tathagata.das1565@gmail.com>
2012-07-27 09:00:45 -0700

    Fixed bug in BoundedMemoryCache.

--- core/src/main/scala/spark/BoundedMemoryCache.scala
+++ core/src/main/scala/spark/BoundedMemoryCache.scala
@@ -91,7 +91,11 @@ class BoundedMemoryCache(maxBytes: Long) extends Cache with Logging {
   protected def reportEntryDropped(datasetId: Any, partition: Int, entry: Entry) {
     logInfo("Dropping key (%s, %d) of size %d to make space".format(datasetId, partition, entry.size))
     // TODO: remove BoundedMemoryCache
-    SparkEnv.get.cacheTracker.dropEntry(datasetId.asInstanceOf[(Int, Int)]._2, partition)
+    datasetId match {
+      case rddDatasetId: (Int, Int) =>
+        SparkEnv.get.cacheTracker.dropEntry(rddDatasetId._2, partition)
+      case _ =>
+    }    
   }
 }
 
