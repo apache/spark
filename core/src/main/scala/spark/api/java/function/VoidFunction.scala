6ae3c375a9e8a97ff96649b3637e4a011d849990
Matei Zaharia <matei@eecs.berkeley.edu>
2012-08-12 23:10:19 +0200

    Renamed apply() to call() in Java API and allowed it to throw Exceptions

--- core/src/main/scala/spark/api/java/function/VoidFunction.scala
+++ core/src/main/scala/spark/api/java/function/VoidFunction.scala
@@ -2,11 +2,12 @@ package spark.api.java.function
 
 // This allows Java users to write void methods without having to return Unit.
 abstract class VoidFunction[T] extends Serializable {
-  def apply(t: T) : Unit
+  @throws(classOf[Exception])
+  def call(t: T) : Unit
 }
 
 // VoidFunction cannot extend AbstractFunction1 (because that would force users to explicitly
 // return Unit), so it is implicitly converted to a Function1[T, Unit]:
 object VoidFunction {
-  implicit def toFunction[T](f: VoidFunction[T]) : Function1[T, Unit] = ((x : T) => f(x))
+  implicit def toFunction[T](f: VoidFunction[T]) : Function1[T, Unit] = ((x : T) => f.call(x))
 }
