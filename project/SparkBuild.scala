1f539aa473981ec47b253dfe86c84ed2bcf286ba
Matei Zaharia <matei@eecs.berkeley.edu>
2012-09-24 14:12:48 -0700

    Update Scala version dependency to 2.9.2

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -22,7 +22,7 @@ object SparkBuild extends Build {
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.spark-project",
     version := "0.6.0-SNAPSHOT",
-    scalaVersion := "2.9.1",
+    scalaVersion := "2.9.2",
     scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
     unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
     retrieveManaged := true,
