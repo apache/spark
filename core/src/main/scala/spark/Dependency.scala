4f72066a9ab02308f733bf248b1ca003abcc0874
Josh Rosen <joshrosen@eecs.berkeley.edu>
2012-10-06 23:59:55 -0700

    Document the Dependency classes.

--- core/src/main/scala/spark/Dependency.scala
+++ core/src/main/scala/spark/Dependency.scala
@@ -1,11 +1,26 @@
 package spark
 
+/**
+ * Base class for dependencies.
+ */
 abstract class Dependency[T](val rdd: RDD[T]) extends Serializable
 
+/**
+ * Base class for dependencies where each partition of the parent RDD is used by at most one
+ * partition of the child RDD.  Narrow dependencies allow for pipelined execution.
+ */
 abstract class NarrowDependency[T](rdd: RDD[T]) extends Dependency(rdd) {
+  /**
+   * Get the parent partitions for a child partition.
+   * @param outputPartition a partition of the child RDD
+   * @return the partitions of the parent RDD that the child partition depends upon
+   */
   def getParents(outputPartition: Int): Seq[Int]
 }
 
+/**
+ * Represents a dependency on the output of a shuffle stage.
+ */
 class ShuffleDependency[K, V, C](
     val shuffleId: Int,
     @transient rdd: RDD[(K, V)],
@@ -13,10 +28,20 @@ class ShuffleDependency[K, V, C](
     val partitioner: Partitioner)
   extends Dependency(rdd)
 
+/**
+ * Represents a one-to-one dependency between partitions of the parent and child RDDs.
+ */
 class OneToOneDependency[T](rdd: RDD[T]) extends NarrowDependency[T](rdd) {
   override def getParents(partitionId: Int) = List(partitionId)
 }
 
+/**
+ * Represents a one-to-one dependency between ranges of partitions in the parent and child RDDs.
+ * @param rdd the parent RDD
+ * @param inStart the start of the range in the parent RDD
+ * @param outStart the start of the range in the child RDD
+ * @param length the length of the range
+ */
 class RangeDependency[T](rdd: RDD[T], inStart: Int, outStart: Int, length: Int)
   extends NarrowDependency[T](rdd) {
   
