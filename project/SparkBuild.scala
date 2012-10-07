14719b93ff4ea7c3234a9389621be3c97fa278b9
Patrick Wendell <pwendell@gmail.com>
2012-10-06 23:02:36 -0700

    Adding Sonatype releases to SBT.
    This does a few things to get this branch ready for release:
    
    1. Upgrades the sbt and Scala version
    2. Sets the release number to 0.5.1
    3. Adds the Sonatype publishing target
    4. Installs the PGP signing plugin
    5. Removes the Mesos jar dependency

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -2,13 +2,14 @@ import sbt._
 import Keys._
 import sbtassembly.Plugin._
 import AssemblyKeys._
+import com.jsuereth.pgp.sbtplugin.PgpKeys._
 
 object SparkBuild extends Build {
   // Hadoop version to build against. For example, "0.20.2", "0.20.205.0", or
   // "1.0.1" for Apache releases, or "0.20.2-cdh3u3" for Cloudera Hadoop.
   val HADOOP_VERSION = "0.20.205.0"
 
-  lazy val root = Project("root", file("."), settings = sharedSettings) aggregate(core, repl, examples, bagel)
+  lazy val root = Project("root", file("."), settings = rootSettings) aggregate(core, repl, examples, bagel)
 
   lazy val core = Project("core", file("core"), settings = coreSettings)
 
@@ -20,19 +21,62 @@ object SparkBuild extends Build {
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.spark-project",
-    version := "0.5.1-SNAPSHOT",
-    scalaVersion := "2.9.1",
+    version := "0.5.1",
+    scalaVersion := "2.9.2",
     scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
     unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
     retrieveManaged := true,
     transitiveClassifiers in Scope.GlobalScope := Seq("sources"),
     testListeners <<= target.map(t => Seq(new eu.henkelmann.sbt.JUnitXmlTestsListener(t.getAbsolutePath))),
-    publishTo <<= baseDirectory { base => Some(Resolver.file("Local", base / "target" / "maven" asFile)(Patterns(true, Resolver.mavenStyleBasePattern))) },
+
     libraryDependencies ++= Seq(
       "org.eclipse.jetty" % "jetty-server" % "7.5.3.v20111011",
       "org.scalatest" %% "scalatest" % "1.6.1" % "test",
       "org.scalacheck" %% "scalacheck" % "1.9" % "test"
     ),
+
+    parallelExecution := false,
+
+    /* Sonatype publishing settings */
+    resolvers ++= Seq("sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+      "sonatype-staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2/"),
+    publishMavenStyle := true,
+    useGpg in Global := true,
+    pomExtra := (
+      <url>http://spark-project.org/</url>
+      <licenses>
+        <license>
+          <name>BSD License</name>
+          <url>https://github.com/mesos/spark/blob/master/LICENSE</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <connection>scm:git:git@github.com:mesos/spark.git</connection>
+        <url>scm:git:git@github.com:mesos/spark.git</url>
+      </scm>
+      <developers>
+        <developer>
+          <id>matei</id>
+          <name>Matei Zaharia</name>
+          <email>matei.zaharia@gmail.com</email>
+          <url>http://www.cs.berkeley.edu/~matei</url>
+          <organization>U.C. Berkeley Computer Science</organization>
+          <organizationUrl>http://www.cs.berkeley.edu/</organizationUrl>
+        </developer>
+      </developers>
+    ),
+
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("sonatype-snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("sonatype-staging"  at nexus + "service/local/staging/deploy/maven2")
+    },
+
+    credentials += Credentials(Path.userHome / ".sbt" / "sonatype.credentials"),
+
     /* Workaround for issue #206 (fixed after SBT 0.11.0) */
     watchTransitiveSources <<= Defaults.inDependencies[Task[Seq[File]]](watchSources.task,
       const(std.TaskExtra.constant(Nil)), aggregate = true, includeRoot = true) apply { _.join.map(_.flatten) }
@@ -59,9 +103,14 @@ object SparkBuild extends Build {
       "de.javakaffee" % "kryo-serializers" % "0.9",
       "org.jboss.netty" % "netty" % "3.2.6.Final",
       "it.unimi.dsi" % "fastutil" % "6.4.2",
-      "colt" % "colt" % "1.2.0"
+      "colt" % "colt" % "1.2.0",
+      "org.apache.mesos" % "mesos" % "0.9.0-incubating"
     )
   ) ++ assemblySettings ++ Seq(test in assembly := {})
+
+  def rootSettings = sharedSettings ++ Seq(
+    publish := {}
+  )
 
   def replSettings = sharedSettings ++ Seq(
     name := "spark-repl",
