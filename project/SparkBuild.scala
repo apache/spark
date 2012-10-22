902a60818760e6a246d614e3c77dda2ca83a8c51
Matei Zaharia <matei@eecs.berkeley.edu>
2012-10-22 11:43:57 -0700

    Update version to 0.6.1-SNAPSHOT to show this is in development

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -33,7 +33,7 @@ object SparkBuild extends Build {
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.spark-project",
-    version := "0.6.0",
+    version := "0.6.1-SNAPSHOT",
     scalaVersion := "2.9.2",
     scalacOptions := Seq(/*"-deprecation",*/ "-unchecked", "-optimize"), // -deprecation is too noisy due to usage of old Hadoop API, enable it once that's no longer an issue
     unmanagedJars in Compile <<= baseDirectory map { base => (base / "lib" ** "*.jar").classpath },
--- repl/src/main/scala/spark/repl/SparkILoop.scala
+++ repl/src/main/scala/spark/repl/SparkILoop.scala
@@ -200,7 +200,7 @@ class SparkILoop(in0: Option[BufferedReader], val out: PrintWriter, val master: 
       ____              __  
      / __/__  ___ _____/ /__
     _\ \/ _ \/ _ `/ __/  '_/
-   /___/ .__/\_,_/_/ /_/\_\   version 0.6.0
+   /___/ .__/\_,_/_/ /_/\_\   version 0.6.1-SNAPSHOT
       /_/                  
 """)
     import Properties._
