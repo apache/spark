cfd9d4010605470023e184349b0f5412b879be36
Charles Reiss <charles@eecs.berkeley.edu>
2012-11-27 13:55:56 -0800

    Fix NullPointerException when unregistering a map output twice.

--- core/src/main/scala/spark/MapOutputTracker.scala
+++ core/src/main/scala/spark/MapOutputTracker.scala
@@ -110,7 +110,7 @@ private[spark] class MapOutputTracker(actorSystem: ActorSystem, isMaster: Boolea
     var array = mapStatuses.get(shuffleId)
     if (array != null) {
       array.synchronized {
-        if (array(mapId).address == bmAddress) {
+        if (array(mapId) != null && array(mapId).address == bmAddress) {
           array(mapId) = null
         }
       }
