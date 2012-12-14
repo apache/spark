24d7aa2d150ec7e20d4527c4223df183be8bb330
Charles Reiss <charles@eecs.berkeley.edu>
2012-12-13 18:39:23 -0800

    Extra whitespace in ExecutorExitCode

--- core/src/main/scala/spark/executor/ExecutorExitCode.scala
+++ core/src/main/scala/spark/executor/ExecutorExitCode.scala
@@ -12,12 +12,15 @@ private[spark]
 object ExecutorExitCode {
   /** The default uncaught exception handler was reached. */
   val UNCAUGHT_EXCEPTION = 50
+
   /** The default uncaught exception handler was called and an exception was encountered while
       logging the exception. */
   val UNCAUGHT_EXCEPTION_TWICE = 51
+
   /** The default uncaught exception handler was reached, and the uncaught exception was an 
       OutOfMemoryError. */
   val OOM = 52
+
   /** DiskStore failed to create a local temporary directory after many attempts. */
   val DISK_STORE_FAILED_TO_CREATE_DIR = 53
 
