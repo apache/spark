2d98fff0651af4d527f41ba50c01f453fa049464
Josh Rosen <joshrosen@eecs.berkeley.edu>
2012-12-27 10:13:29 -0800

    Add IPython support to pyspark-shell.
    Suggested by / based on code from @MLnick

--- pyspark/pyspark/shell.py
+++ pyspark/pyspark/shell.py
@@ -1,21 +1,30 @@
 """
 An interactive shell.
 """
+import argparse  # argparse is avaiable for Python < 2.7 through easy_install.
 import code
 import sys
 
 from pyspark.context import SparkContext
 
 
-def main(master='local'):
+def main(master='local', ipython=False):
     sc = SparkContext(master, 'PySparkShell')
-    print "Spark context available as sc."
-    code.interact(local={'sc': sc})
+    user_ns = {'sc' : sc}
+    banner = "Spark context avaiable as sc."
+    if ipython:
+        import IPython
+        IPython.embed(user_ns=user_ns, banner2=banner)
+    else:
+        print banner
+        code.interact(local=user_ns)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        master = sys.argv[1]
-    else:
-        master = 'local'
-    main(master)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("master", help="Spark master host (default='local')",
+                        nargs='?', type=str, default="local")
+    parser.add_argument("-i", "--ipython", help="Run IPython shell",
+                        action="store_true")
+    args = parser.parse_args()
+    main(args.master, args.ipython)
