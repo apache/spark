e7b2e16a80908a33946ff95e7327a615180e5e8b
Matei Zaharia <matei@eecs.berkeley.edu>
2013-01-13 19:34:07 -0800

    Make filter preserve partitioner info, since it can
    Conflicts:
    	core/src/main/scala/spark/rdd/FilteredRDD.scala

--- core/src/main/scala/spark/rdd/FilteredRDD.scala
+++ core/src/main/scala/spark/rdd/FilteredRDD.scala
@@ -8,5 +8,6 @@ private[spark]
 class FilteredRDD[T: ClassManifest](prev: RDD[T], f: T => Boolean) extends RDD[T](prev.context) {
   override def splits = prev.splits
   override val dependencies = List(new OneToOneDependency(prev))
+  override val partitioner = prev.partitioner    // Since filter cannot change a partition's keys
   override def compute(split: Split) = prev.iterator(split).filter(f)
 }
