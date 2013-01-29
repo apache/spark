38b83bc66bd440810778b6bc543766e25c084112
Imran Rashid <imran@quantifind.com>
2013-01-28 18:20:57 -0800

    can get task runtime summary from task info

--- core/src/main/scala/spark/scheduler/StageInfo.scala
+++ core/src/main/scala/spark/scheduler/StageInfo.scala
@@ -2,5 +2,10 @@ package spark.scheduler
 
 import cluster.TaskInfo
 import collection._
+import spark.util.Distribution
 
-case class StageInfo(val stage: Stage, val taskInfos: mutable.Buffer[TaskInfo] = mutable.Buffer[TaskInfo]())
+case class StageInfo(val stage: Stage, val taskInfos: mutable.Buffer[TaskInfo] = mutable.Buffer[TaskInfo]()) {
+  def getTaskRuntimeDistribution = {
+    new Distribution(taskInfos.map{_.duration.toDouble})
+  }
+}
