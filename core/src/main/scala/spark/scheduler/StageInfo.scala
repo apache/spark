cec9c768c2eed8d4eefaae0003eeb3971317cef9
Imran Rashid <imran@quantifind.com>
2013-01-28 20:02:53 -0800

    convenient name available in StageInfo

--- core/src/main/scala/spark/scheduler/StageInfo.scala
+++ core/src/main/scala/spark/scheduler/StageInfo.scala
@@ -5,6 +5,9 @@ import collection._
 import spark.util.Distribution
 
 case class StageInfo(val stage: Stage, val taskInfos: mutable.Buffer[TaskInfo] = mutable.Buffer[TaskInfo]()) {
+
+  def name = stage.rdd.name + "(" + stage.origin + ")"
+
   def getTaskRuntimeDistribution = {
     new Distribution(taskInfos.map{_.duration.toDouble})
   }
