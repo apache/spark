6289d9654e32fc92418d41cc6e32fee30f85c833
Reynold Xin <rxin@cs.berkeley.edu>
2013-01-31 17:49:36 -0800

    Removed the TODO comment from PartitionPruningRDD.

--- core/src/main/scala/spark/rdd/PartitionPruningRDD.scala
+++ core/src/main/scala/spark/rdd/PartitionPruningRDD.scala
@@ -8,8 +8,6 @@ import spark.{PruneDependency, RDD, SparkEnv, Split, TaskContext}
  * all partitions. An example use case: If we know the RDD is partitioned by range,
  * and the execution DAG has a filter on the key, we can avoid launching tasks
  * on partitions that don't have the range covering the key.
- *
- * TODO: This currently doesn't give partition IDs properly!
  */
 class PartitionPruningRDD[T: ClassManifest](
     @transient prev: RDD[T],
