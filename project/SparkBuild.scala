718474b9c6ab985833af9cde69f13c4d33498f6d
Nick Pentreath <nick.pentreath@gmail.com>
2013-02-21 12:11:31 +0200

    Bumping Algebird to 0.1.9

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -155,7 +155,7 @@ object SparkBuild extends Build {
 
   def examplesSettings = sharedSettings ++ Seq(
     name := "spark-examples",
-    libraryDependencies ++= Seq("com.twitter" % "algebird-core_2.9.2" % "0.1.8")
+    libraryDependencies ++= Seq("com.twitter" % "algebird-core_2.9.2" % "0.1.9")
   )
 
   def bagelSettings = sharedSettings ++ Seq(name := "spark-bagel")
