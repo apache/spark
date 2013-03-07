f0164e5047f3a023768cdf0515a7a7d1c75ef7c3
kalpit <shahkalpit84@gmail.com>
2013-03-06 17:03:15 -0800

    upgraded sbt version, sbt plugins and some library dependencies to latest stable version

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -92,10 +92,10 @@ object SparkBuild extends Build {
 */
 
     libraryDependencies ++= Seq(
-      "org.eclipse.jetty" % "jetty-server" % "7.5.3.v20111011",
-      "org.scalatest" %% "scalatest" % "1.8" % "test",
-      "org.scalacheck" %% "scalacheck" % "1.9" % "test",
-      "com.novocode" % "junit-interface" % "0.8" % "test",
+      "org.eclipse.jetty" % "jetty-server" % "7.6.8.v20121106",
+      "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+      "org.scalacheck" %% "scalacheck" % "1.10.0" % "test",
+      "com.novocode" % "junit-interface" % "0.9" % "test",
       "org.easymock" % "easymock" % "3.1" % "test"
     ),
     parallelExecution := false,
