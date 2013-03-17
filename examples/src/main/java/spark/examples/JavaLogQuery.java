b990caeb807c7b9891ecaceca8b9d4920fec26c9
Nick Pentreath <nick.pentreath@gmail.com>
2013-03-17 20:03:27 +0200

    Changes to more closely match line length limit style

--- examples/src/main/java/spark/examples/JavaLogQuery.java
+++ examples/src/main/java/spark/examples/JavaLogQuery.java
@@ -22,8 +22,9 @@ public class JavaLogQuery {
 
     public static List<String> exampleApacheLogs = Lists.newArrayList(
             "10.10.10.10 - \"FRED\" [18/Jan/2013:17:56:07 +1100] \"GET http://images.com/2013/Generic.jpg " +
-            "HTTP/1.1\" 304 315 \"http://referall.com/\" \"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; GTB7.4; .NET CLR 2.0.50727; " +
-            ".NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022; .NET CLR 3.0.4506.2152; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR " +
+            "HTTP/1.1\" 304 315 \"http://referall.com/\" \"Mozilla/4.0 (compatible; MSIE 7.0; " +
+            "Windows NT 5.1; GTB7.4; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; .NET CLR 3.0.04506.648; " +
+            ".NET CLR 3.5.21022; .NET CLR 3.0.4506.2152; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR " +
             "3.5.30729; Release=ARP)\" \"UD-1\" - \"image/jpeg\" \"whatever\" 0.350 \"-\" - \"\" 265 923 934 \"\" " +
             "62.24.11.25 images.com 1358492167 - Whatup",
             "10.10.10.10 - \"FRED\" [18/Jan/2013:18:02:37 +1100] \"GET http://images.com/2013/Generic.jpg " +
@@ -34,7 +35,8 @@ public class JavaLogQuery {
             "0 73.23.2.15 images.com 1358492557 - Whatup");
 
 
-    public static Pattern apacheLogRegex = Pattern.compile("^([\\d.]+) (\\S+) (\\S+) \\[([\\w\\d:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) ([\\d\\-]+) \"([^\"]+)\" \"([^\"]+)\".*");
+    public static Pattern apacheLogRegex = Pattern.compile(
+        "^([\\d.]+) (\\S+) (\\S+) \\[([\\w\\d:/]+\\s[+\\-]\\d{4})\\] \"(.+?)\" (\\d{3}) ([\\d\\-]+) \"([^\"]+)\" \"([^\"]+)\".*");
 
     /** Tracks the total query count and number of aggregate bytes for a particular group. */
     public static class Stats implements Serializable {
