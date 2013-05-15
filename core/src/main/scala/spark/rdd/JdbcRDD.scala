f9d40a5848a2e1eef31ac63cd9221d5b77c1c5a7
Reynold Xin <rxin@cs.berkeley.edu>
2013-05-14 23:29:57 -0700

    Added a comment in JdbcRDD for example usage.

--- core/src/main/scala/spark/rdd/JdbcRDD.scala
+++ core/src/main/scala/spark/rdd/JdbcRDD.scala
@@ -11,11 +11,13 @@ private[spark] class JdbcPartition(idx: Int, val lower: Long, val upper: Long) e
 
 /**
  * An RDD that executes an SQL query on a JDBC connection and reads results.
+ * For usage example, see test case JdbcRDDSuite.
+ *
  * @param getConnection a function that returns an open Connection.
  *   The RDD takes care of closing the connection.
  * @param sql the text of the query.
  *   The query must contain two ? placeholders for parameters used to partition the results.
- *   E.g. "select title, author from books where ? <= id and id <= ?" 
+ *   E.g. "select title, author from books where ? <= id and id <= ?"
  * @param lowerBound the minimum value of the first placeholder
  * @param upperBound the maximum value of the second placeholder
  *   The lower and upper bounds are inclusive.
