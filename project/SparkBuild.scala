576528f0f916cc0434972cbbc9321e4dcec45628
jerryshao <saisai.shao@intel.com>
2013-06-28 10:25:48 +0800

    Add dependency of Codahale's metrics library

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -180,6 +180,8 @@ object SparkBuild extends Build {
       "org.apache.mesos" % "mesos" % "0.9.0-incubating",
       "io.netty" % "netty-all" % "4.0.0.Beta2",
       "org.apache.derby" % "derby" % "10.4.2.0" % "test"
+      "com.codahale.metrics" % "metrics-core" % "3.0.0",
+      "com.codahale.metrics" % "metrics-jvm" % "3.0.0"
     ) ++ (
       if (HADOOP_MAJOR_VERSION == "2") {
         if (HADOOP_YARN) {
