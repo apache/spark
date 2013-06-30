2964df7a4e9d39c6206709680b9828e0cb7b1430
Joseph E. Gonzalez <joseph.e.gonzalez@gmail.com>
2013-06-29 21:04:35 -0700

    Commenting out unused test code.

--- graph/src/test/scala/spark/graph/GraphSuite.scala
+++ graph/src/test/scala/spark/graph/GraphSuite.scala
@@ -7,40 +7,40 @@ import spark.SparkContext
 
 class GraphSuite extends FunSuite with LocalSparkContext {
 
-  test("graph partitioner") {
-    sc = new SparkContext("local", "test")
-    val vertices = sc.parallelize(Seq(Vertex(1, "one"), Vertex(2, "two")))
-    val edges = sc.parallelize(Seq(Edge(1, 2, "onlyedge")))
-    var g = new Graph(vertices, edges)
-
-    g = g.withPartitioner(4, 7)
-    assert(g.numVertexPartitions === 4)
-    assert(g.numEdgePartitions === 7)
-
-    g = g.withVertexPartitioner(5)
-    assert(g.numVertexPartitions === 5)
-
-    g = g.withEdgePartitioner(8)
-    assert(g.numEdgePartitions === 8)
-
-    g = g.mapVertices(x => x)
-    assert(g.numVertexPartitions === 5)
-    assert(g.numEdgePartitions === 8)
-
-    g = g.mapEdges(x => x)
-    assert(g.numVertexPartitions === 5)
-    assert(g.numEdgePartitions === 8)
-
-    val updates = sc.parallelize(Seq((1, " more")))
-    g = g.updateVertices(
-      updates,
-      (v, u: Option[String]) => if (u.isDefined) v.data + u.get else v.data)
-    assert(g.numVertexPartitions === 5)
-    assert(g.numEdgePartitions === 8)
-
-    g = g.reverse
-    assert(g.numVertexPartitions === 5)
-    assert(g.numEdgePartitions === 8)
-
-  }
+//  test("graph partitioner") {
+//    sc = new SparkContext("local", "test")
+//    val vertices = sc.parallelize(Seq(Vertex(1, "one"), Vertex(2, "two")))
+//    val edges = sc.parallelize(Seq(Edge(1, 2, "onlyedge")))
+//    var g = Graph(vertices, edges)
+//
+//    g = g.withPartitioner(4, 7)
+//    assert(g.numVertexPartitions === 4)
+//    assert(g.numEdgePartitions === 7)
+//
+//    g = g.withVertexPartitioner(5)
+//    assert(g.numVertexPartitions === 5)
+//
+//    g = g.withEdgePartitioner(8)
+//    assert(g.numEdgePartitions === 8)
+//
+//    g = g.mapVertices(x => x)
+//    assert(g.numVertexPartitions === 5)
+//    assert(g.numEdgePartitions === 8)
+//
+//    g = g.mapEdges(x => x)
+//    assert(g.numVertexPartitions === 5)
+//    assert(g.numEdgePartitions === 8)
+//
+//    val updates = sc.parallelize(Seq((1, " more")))
+//    g = g.updateVertices(
+//      updates,
+//      (v, u: Option[String]) => if (u.isDefined) v.data + u.get else v.data)
+//    assert(g.numVertexPartitions === 5)
+//    assert(g.numEdgePartitions === 8)
+//
+//    g = g.reverse
+//    assert(g.numVertexPartitions === 5)
+//    assert(g.numEdgePartitions === 8)
+//
+//  }
 }
