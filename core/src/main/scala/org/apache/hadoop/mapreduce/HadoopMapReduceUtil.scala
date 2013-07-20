8b1c1520fc26132a21062ebb063dea25e9b36b8b
Jey Kottalam <jey@cs.berkeley.edu>
2013-07-20 11:34:18 -0700

    add comment

--- core/src/main/scala/org/apache/hadoop/mapreduce/HadoopMapReduceUtil.scala
+++ core/src/main/scala/org/apache/hadoop/mapreduce/HadoopMapReduceUtil.scala
@@ -22,16 +22,16 @@ import org.apache.hadoop.conf.Configuration
 trait HadoopMapReduceUtil {
   def newJobContext(conf: Configuration, jobId: JobID): JobContext = {
     val klass = firstAvailableClass(
-        "org.apache.hadoop.mapreduce.task.JobContextImpl",
-        "org.apache.hadoop.mapreduce.JobContext")
+        "org.apache.hadoop.mapreduce.task.JobContextImpl",  // hadoop2, hadoop2-yarn
+        "org.apache.hadoop.mapreduce.JobContext")           // hadoop1
     val ctor = klass.getDeclaredConstructor(classOf[Configuration], classOf[JobID])
     ctor.newInstance(conf, jobId).asInstanceOf[JobContext]
   }
 
   def newTaskAttemptContext(conf: Configuration, attemptId: TaskAttemptID): TaskAttemptContext = {
     val klass = firstAvailableClass(
-        "org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl",
-        "org.apache.hadoop.mapreduce.TaskAttemptContext")
+        "org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl",  // hadoop2, hadoop2-yarn
+        "org.apache.hadoop.mapreduce.TaskAttemptContext")           // hadoop1
     val ctor = klass.getDeclaredConstructor(classOf[Configuration], classOf[TaskAttemptID])
     ctor.newInstance(conf, attemptId).asInstanceOf[TaskAttemptContext]
   }
