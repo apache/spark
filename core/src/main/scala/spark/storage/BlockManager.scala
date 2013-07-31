ad7e9d0d64277f616f90f2ca8bf8a5844641883a
Reynold Xin <reynoldx@gmail.com>
2013-07-30 17:11:54 -0700

    CompressionCodec cleanup. Moved it to spark.io package.

--- core/src/main/scala/spark/storage/BlockManager.scala
+++ core/src/main/scala/spark/storage/BlockManager.scala
@@ -27,11 +27,10 @@ import akka.dispatch.{Await, Future}
 import akka.util.Duration
 import akka.util.duration._
 
-import com.ning.compress.lzf.{LZFInputStream, LZFOutputStream}
-
 import it.unimi.dsi.fastutil.io.FastByteArrayOutputStream
 
 import spark.{Logging, SparkEnv, SparkException, Utils}
+import spark.io.CompressionCodec
 import spark.network._
 import spark.serializer.Serializer
 import spark.util.{ByteBufferInputStream, IdGenerator, MetadataCleaner, TimeStampedHashMap}
@@ -158,7 +157,12 @@ private[spark] class BlockManager(
   val metadataCleaner = new MetadataCleaner("BlockManager", this.dropOldBlocks)
   initialize()
 
-  var compressionCodec: CompressionCodec = null
+  // The compression codec to use. Note that the "lazy" val is necessary because we want to delay
+  // the initialization of the compression codec until it is first used. The reason is that a Spark
+  // program could be using a user-defined codec in a third party jar, which is loaded in
+  // Executor.updateDependencies. When the BlockManager is initialized, user level jars hasn't been
+  // loaded yet.
+  private lazy val compressionCodec: CompressionCodec = CompressionCodec.createCodec()
 
   /**
    * Construct a BlockManager with a memory limit set based on system properties.
@@ -921,32 +925,14 @@ private[spark] class BlockManager(
    * Wrap an output stream for compression if block compression is enabled for its block type
    */
   def wrapForCompression(blockId: String, s: OutputStream): OutputStream = {
-    if (compressionCodec == null) {
-      compressionCodec = Class.forName(System.getProperty("spark.storage.compression.codec",
-        "spark.storage.LZFCompressionCodec"), true, Thread.currentThread.getContextClassLoader)
-        .newInstance().asInstanceOf[CompressionCodec]
-    }
-
-    if (shouldCompress(blockId)) {
-      //(new LZFOutputStream(s)).setFinishBlockOnFlush(true)
-      compressionCodec.compressionOutputStream(s)
-    } else {
-      s
-    }
+    if (shouldCompress(blockId)) compressionCodec.compressionOutputStream(s) else s
   }
 
   /**
    * Wrap an input stream for compression if block compression is enabled for its block type
    */
   def wrapForCompression(blockId: String, s: InputStream): InputStream = {
-    if (compressionCodec == null) {
-      compressionCodec = Class.forName(System.getProperty("spark.storage.compression.codec",
-        "spark.storage.LZFCompressionCodec"), true, Thread.currentThread.getContextClassLoader)
-        .newInstance().asInstanceOf[CompressionCodec]
-    }
-
-    if (shouldCompress(blockId)) /*new LZFInputStream(s) */
-      compressionCodec.compressionInputStream(s) else s
+    if (shouldCompress(blockId)) compressionCodec.compressionInputStream(s) else s
   }
 
   def dataSerialize(
