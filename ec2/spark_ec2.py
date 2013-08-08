7e1b7d8a96bbf2bc4655a528ef3b17a7820e313f
Patrick Wendell <pwendell@gmail.com>
2013-08-08 14:17:11 -0700

    Adding web UI port

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -218,6 +218,7 @@ def launch_cluster(conn, opts, cluster_name):
     master_group.authorize(src_group=slave_group)
     master_group.authorize('tcp', 22, 22, '0.0.0.0/0')
     master_group.authorize('tcp', 8080, 8081, '0.0.0.0/0')
+    master_group.authorize('tcp', 33000, 33000, '0.0.0.0/0')
     master_group.authorize('tcp', 50030, 50030, '0.0.0.0/0')
     master_group.authorize('tcp', 50070, 50070, '0.0.0.0/0')
     master_group.authorize('tcp', 60070, 60070, '0.0.0.0/0')
