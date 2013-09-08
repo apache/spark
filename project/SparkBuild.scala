8de8ee5d3cf01fd225336064d9586380a4fb6ad4
Patrick Wendell <pwendell@gmail.com>
2013-09-08 10:07:29 -0700

    Ganglia sink

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -207,6 +207,7 @@ object SparkBuild extends Build {
       "com.codahale.metrics" % "metrics-core" % "3.0.0",
       "com.codahale.metrics" % "metrics-jvm" % "3.0.0",
       "com.codahale.metrics" % "metrics-json" % "3.0.0",
+      "com.codahale.metrics" % "metrics-ganglia" % "3.0.0",
       "com.twitter" % "chill_2.9.3" % "0.3.1",
       "com.twitter" % "chill-java" % "0.3.1"
     )
