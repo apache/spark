f6de5b26dbd012f38aef5443c8fb36b3f690fa9e
Patrick Wendell <pwendell@gmail.com>
2013-09-12 07:01:59 +0000

    Removing 'rcX' from version and some formatting changes.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -79,7 +79,7 @@ object SparkBuild extends Build {
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization := "org.apache.spark",
-    version := "0.8.0-incubating-rc3-SNAPSHOT",
+    version := "0.8.0-incubating-SNAPSHOT",
     scalaVersion := "2.9.3",
     scalacOptions := Seq("-unchecked", "-optimize", "-deprecation", 
       "-target:" + SCALAC_JVM_VERSION),
