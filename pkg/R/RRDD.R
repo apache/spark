4c2e5164be8ade5f8b58823309a887f500bf539f
Zongheng Yang <zongheng.y@gmail.com>
2013-09-13 10:59:22 -0700

    Add simple prototype of S4 class RRDD. Make TextFile() returns an RRDD.

---
+++ pkg/R/RRDD.R
@@ -0,0 +1,23 @@
+# RRDD (RDD in R) class implemented in S4 OO system.
+
+# TODO: do we really want to mix S3 and S4?
+setOldClass("jobjRef")
+
+setClass("RRDD", slots = list(jrdd = "jobjRef"))
+
+setValidity("RRDD",
+            function(object) {
+              cls <- object@jrdd$getClass()
+              className <- cls$getName()
+              if (grep("spark.api.java.*RDD*", className) == 1) {
+                TRUE
+              } else {
+                paste("Invalid RDD class ", className)
+              }
+            })
+
+# Constructor of the RRDD class.
+RRDD <- function(jrdd) {
+  new("RRDD", jrdd = jrdd)
+}
+
