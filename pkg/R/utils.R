84c1fd2db017c186bafc3c7c7fa2d546cbce69ff
Zongheng Yang <zongheng.y@gmail.com>
2013-09-15 10:40:09 -0700

    Use .jsimplify() to get around generic List's get() type erasure problem

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -2,16 +2,11 @@
 
 "JavaListToRList" <- function(jList) {
   size <- .jcall(jList, "I", "size")
-
-  # FIXME: each call to the lambda gives the error, most likely due to type erasure in List<E>:
-  # Error in .jfield(x, "Ljava/lang/Class;", "TYPE") :
-    # trying to generate an object from a virtual class ("jobjRef")
-  # Perhaps: 
-    # calling toString() on every object and deal with strings?
-    # mimick PySpark: write custom Scala serializers for SparkR
+  # TODO: test with RRDD[T] where T is not String
   lapply(0:(size - 1), 
          function(index) {
-           .jcall(jList, "Ljava/lang/Object;", "get", as.integer(index))
+           jElem <- .jcall(jList, "Ljava/lang/Object;", "get", as.integer(index))
+           .jsimplify(jElem)
          })
 }
           
