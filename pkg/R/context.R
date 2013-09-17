978aa0f8ae837790c1cd9851aceb063d92002f28
Zongheng Yang <zongheng.y@gmail.com>
2013-09-17 12:43:08 -0700

    Add parallelize() skeleton: only return serialized slices now

--- pkg/R/context.R
+++ pkg/R/context.R
@@ -1,7 +1,9 @@
+# context.R: SparkContext driven functions
+
 # Read a text file from HDFS, a local file system (available on all
 # nodes), or any Hadoop-supported file system URI, and return it as an
 # RRDD of Strings.
-"textFile" <- function(jsc, name, minSplits=NULL) {
+textFile <- function(jsc, name, minSplits=NULL) {
   # FIXME: if execute into this if block, errors:
   # Error in .jcall(jsc, "sc", c()) : RcallMethod: invalid method name
   if (is.null(minSplits)) {
@@ -13,3 +15,11 @@
   RRDD(jrdd)
 }
 
+
+# Distribute a local R collection (list/vector) to form an RRDD.
+# FIXME: bound/safeguard numSlices
+parallelize <- function(jsc, coll, numSlices) {
+  sliceLen <- length(coll) / numSlices
+  slices <- split(coll, rep(1:(numSlices + 1), each = sliceLen)[1:length(coll)])
+  serializedSlices <- lapply(slices, serialize, connection = NULL)
+}
