fc7693f97d5fe5bc4540755be82db8957cc91fc7
Zongheng Yang <zongheng.y@gmail.com>
2013-09-24 13:13:42 -0700

    Refactor and enhance the previously added unit test a little bit.

--- pkg/inst/tests/test_parallelize_collect.R
+++ pkg/inst/tests/test_parallelize_collect.R
@@ -1,5 +1,6 @@
 context("parallelize() and collect()")
 
+# Mock data
 numVector <- c(-10:97)
 numList <- list(sqrt(1), sqrt(2), sqrt(3), 4 ** 10)
 strVector <- c("Dexter Morgan: I suppose I should be upset, even feel",
@@ -10,28 +11,35 @@ strList <- list("Dexter Morgan: Blood. Sometimes it sets my teeth on edge, ",
                 "other times it helps me control the chaos.",
                 "Dexter Morgan: Harry and Dorris Morgan did a wonderful job ",
                 "raising me. But they're both dead now. I didn't kill them. Honest.")
+
+# JavaSparkContext handle
 jsc <- sparkR.init()
 
 test_that("parallelize() on simple vectors and lists returns an RRDD", {
   numVectorRRDD <- parallelize(jsc, numVector, 1)
   numVectorRRDD2 <- parallelize(jsc, numVector, 10)
-  expect_that(class(numVectorRRDD), is_equivalent_to("RRDD"))
-  expect_that(class(numVectorRRDD2), is_equivalent_to("RRDD"))
-
   numListRRDD <- parallelize(jsc, numList, 1)
   numListRRDD2 <- parallelize(jsc, numList, 4)
-  expect_that(class(numListRRDD), is_equivalent_to("RRDD"))
-  expect_that(class(numListRRDD2), is_equivalent_to("RRDD"))
-
   strVectorRRDD <- parallelize(jsc, strVector, 2)
   strVectorRRDD2 <- parallelize(jsc, strVector, 3)
-  expect_that(class(strVectorRRDD), is_equivalent_to("RRDD"))
-  expect_that(class(strVectorRRDD2), is_equivalent_to("RRDD"))
-
   strListRRDD <- parallelize(jsc, strList, 4)
   strListRRDD2 <- parallelize(jsc, strList, 1)
-  expect_that(class(strListRRDD), is_equivalent_to("RRDD"))
-  expect_that(class(strListRRDD2), is_equivalent_to("RRDD"))
+
+  rrdds <- c(numVectorRRDD,
+             numVectorRRDD2,
+             numListRRDD,
+             numListRRDD2,
+             strVectorRRDD,
+             strVectorRRDD2,
+             strListRRDD,
+             strListRRDD2)
+
+  for (rrdd in rrdds) {
+    expect_that(class(rrdd), is_equivalent_to("RRDD"))
+    expect_true(.hasSlot(rrdd, "jrdd")
+                && class(rrdd@jrdd) == "jobjRef"
+                && .jinstanceof(rrdd@jrdd, "org/apache/spark/api/java/JavaRDD"))
+  }
 })
 
 test_that("collect(), following a parallelize(), gives back the original collections", {
