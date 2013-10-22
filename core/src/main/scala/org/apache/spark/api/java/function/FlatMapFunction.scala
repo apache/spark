768eb9c9629f0e3c24477acf87ad15938ddd75ce
Josh Rosen <joshrosen@apache.org>
2013-10-22 14:26:34 -0700

    Remove redundant Java Function call() definitions
    This should fix SPARK-902, an issue where some
    Java API Function classes could cause
    AbstractMethodErrors when user code is compiled
    using the Eclipse compiler.
    
    Thanks to @MartinWeindel for diagnosing this
    problem.
    
    (This PR subsumes / closes #30)

--- core/src/main/scala/org/apache/spark/api/java/function/FlatMapFunction.scala
+++ core/src/main/scala/org/apache/spark/api/java/function/FlatMapFunction.scala
@@ -21,8 +21,5 @@ package org.apache.spark.api.java.function
  * A function that returns zero or more output records from each input record.
  */
 abstract class FlatMapFunction[T, R] extends Function[T, java.lang.Iterable[R]] {
-  @throws(classOf[Exception])
-  def call(x: T) : java.lang.Iterable[R]
-
   def elementType() : ClassManifest[R] = ClassManifest.Any.asInstanceOf[ClassManifest[R]]
 }
--- core/src/main/scala/org/apache/spark/api/java/function/Function2.java
+++ core/src/main/scala/org/apache/spark/api/java/function/Function2.java
@@ -19,7 +19,6 @@ package org.apache.spark.api.java.function;
 
 import scala.reflect.ClassManifest;
 import scala.reflect.ClassManifest$;
-import scala.runtime.AbstractFunction2;
 
 import java.io.Serializable;
 
@@ -28,8 +27,6 @@ import java.io.Serializable;
  */
 public abstract class Function2<T1, T2, R> extends WrappedFunction2<T1, T2, R>
   implements Serializable {
-
-  public abstract R call(T1 t1, T2 t2) throws Exception;
 
   public ClassManifest<R> returnType() {
     return (ClassManifest<R>) ClassManifest$.MODULE$.fromClass(Object.class);
