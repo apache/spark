0608a0084e1631778e4c7fa0e311cf18b681da14
Michael Armbrust <michael@databricks.com>
2013-11-07 00:04:45 -0800

    tighten public interface

--- src/main/scala/rules/RuleExecutor.scala
+++ src/main/scala/rules/RuleExecutor.scala
@@ -4,9 +4,9 @@ package rules
 import plans._
 
 abstract class RuleExecutor[PlanType <: QueryPlan[_]] {
-  case class Batch(name: String, rules: Rule[PlanType]*)
+  protected case class Batch(name: String, rules: Rule[PlanType]*)
 
-  val batches: Seq[Batch]
+  protected val batches: Seq[Batch]
 
   def apply(plan: PlanType): PlanType = {
     batches.foldLeft(plan) {
