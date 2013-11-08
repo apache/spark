1c111361acfef22e9ec3a5f9e6f4d06696cb0b48
Michael Armbrust <michael@databricks.com>
2013-11-07 18:16:07 -0800

    first draft of error handling / plans for debugging.

---
+++ src/main/scala/errors/package.scala
@@ -0,0 +1,18 @@
+package catalyst
+
+import trees._
+
+package object errors {
+
+  class OptimizationException[TreeType <: TreeNode[_]]
+    (tree: TreeType, msg: String, cause: Throwable = null) extends Exception(msg, cause) {
+
+  }
+
+  def attachTree[TreeType <: TreeNode[_], A](tree: TreeType, msg: String = "")(f: => A): A = {
+    try f catch {
+      case e: Exception => throw new OptimizationException(tree, msg, e)
+    }
+  }
+
+}
