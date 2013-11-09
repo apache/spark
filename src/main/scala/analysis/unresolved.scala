6f9bafd501b28cbe56eb8e70cbe9a8f4bf9a8b85
Michael Armbrust <michael@databricks.com>
2013-11-09 01:07:11 -0800

    empty output for unresolved relation to avoid exception in resolution.

--- src/main/scala/analysis/unresolved.scala
+++ src/main/scala/analysis/unresolved.scala
@@ -9,7 +9,7 @@ class UnresolvedException[TreeType <: TreeNode[_]](tree: TreeType, function: Str
   errors.OptimizationException(tree, "Invalid call to $function on unresolved object")
 
 case class UnresolvedRelation(name: String, alias: Option[String]) extends LeafNode {
-  def output = throw new UnresolvedException(this, "output")
+  def output = Nil
 }
 
 case class UnresolvedAttribute(name: String) extends Attribute with trees.LeafNode[Expression] {
