1e423ebc12b81275139cea0150973d84077114c5
Michael Armbrust <michael@databricks.com>
2013-11-08 21:26:36 -0800

    placeholders in LogicalPlan, docs

--- src/main/scala/plans/logical/LogicalPlan.scala
+++ src/main/scala/plans/logical/LogicalPlan.scala
@@ -2,16 +2,47 @@ package catalyst
 package plans
 package logical
 
+import expressions.Attribute
 import trees._
 
 abstract class LogicalPlan extends QueryPlan[LogicalPlan] {
   self: Product =>
+
+  /**
+   * Returns the set of attributes that are referenced by this node
+   * during evaluation.
+   */
+  def references: Set[Attribute] = ???
+
+  /**
+   * Returns the set of attributes that are output by this node.
+   */
+  def outputSet: Set[Attribute] = ???
+
+  /**
+   * Returns the set of attributes that this node takes as
+   * input from its children.
+   */
+  def inputSet: Set[Attribute] = ???
 }
 
+/**
+ * A logical plan node with no children.
+ */
 abstract class LeafNode extends LogicalPlan with trees.LeafNode[LogicalPlan] {
   self: Product =>
 }
 
+/**
+ * A logical plan node with single child.
+ */
 abstract class UnaryNode extends LogicalPlan with trees.UnaryNode[LogicalPlan] {
+  self: Product =>
+}
+
+/**
+ * A logical plan node with a left and right child.
+ */
+abstract class BinaryNode extends LogicalPlan with trees.BinaryNode[LogicalPlan] {
   self: Product =>
 }
