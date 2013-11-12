c7cd57f28cf3acb9a268d1630a3f1de8077bdd37
Michael Armbrust <michael@databricks.com>
2013-11-11 16:42:59 -0800

    docs for util function.

--- src/main/scala/util/package.scala
+++ src/main/scala/util/package.scala
@@ -3,6 +3,12 @@ package catalyst
 import java.io.File
 
 package object util {
+  /**
+   * Returns a path to a temporary file that probably does not exist.
+   * Note, there is always the race condition that someone created this
+   * file since the last time we checked.  Thus, this shouldn't be used
+   * for anything security conscious.
+   */
   def getTempFilePath(prefix: String, suffix: String = ""): File = {
     val tempFile = File.createTempFile(prefix, suffix)
     tempFile.delete()
