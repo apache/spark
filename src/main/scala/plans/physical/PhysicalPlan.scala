26f410afaefff80c0cc7fda915c8fd5fdb8a388e
Michael Armbrust <michael@databricks.com>
2013-11-14 21:12:04 -0800

    physical traits should extend PhysicalPlan.

--- src/main/scala/plans/physical/PhysicalPlan.scala
+++ src/main/scala/plans/physical/PhysicalPlan.scala
@@ -15,14 +15,14 @@ abstract class PhysicalPlan extends QueryPlan[PhysicalPlan] {
   def execute(): RDD[_]
 }
 
-abstract trait LeafNode extends trees.LeafNode[PhysicalPlan] {
+abstract trait LeafNode extends PhysicalPlan with trees.LeafNode[PhysicalPlan] {
   self: Product =>
 }
 
-abstract trait UnaryNode extends trees.UnaryNode[PhysicalPlan] {
+abstract trait UnaryNode extends PhysicalPlan with trees.UnaryNode[PhysicalPlan] {
   self: Product =>
 }
 
-abstract trait BinaryNode extends trees.BinaryNode[PhysicalPlan] {
+abstract trait BinaryNode extends PhysicalPlan with trees.BinaryNode[PhysicalPlan] {
   self: Product =>
 }
