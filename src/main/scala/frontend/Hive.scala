e1f7f4c37b495683bba96227f51650a3149c7b1e
Michael Armbrust <michael@databricks.com>
2013-11-18 13:45:22 -0800

    Remove "NativePlaceholder" hack.

--- src/main/scala/frontend/Hive.scala
+++ src/main/scala/frontend/Hive.scala
@@ -39,6 +39,74 @@ case class AddJar(jarPath: String) extends Command
 case class AddFile(filePath: String) extends Command
 
 object Hive {
+  protected val nativeCommands = Seq(
+    "TOK_EXPLAIN",
+
+    "TOK_DESCFUNCTION",
+    "TOK_DESCTABLE",
+    "TOK_DESCDATABASE",
+    "TOK_SHOW_TABLESTATUS",
+    "TOK_SHOWDATABASES",
+    "TOK_SHOWFUNCTIONS",
+    "TOK_SHOWINDEXES",
+    "TOK_SHOWINDEXES",
+    "TOK_SHOWPARTITIONS",
+    "TOK_SHOWTABLES",
+
+    "TOK_LOCKTABLE",
+    "TOK_SHOWLOCKS",
+    "TOK_UNLOCKTABLE",
+
+    "TOK_CREATEROLE",
+    "TOK_DROPROLE",
+    "TOK_GRANT",
+    "TOK_GRANT_ROLE",
+    "TOK_REVOKE",
+    "TOK_SHOW_GRANT",
+    "TOK_SHOW_ROLE_GRANT",
+
+    "TOK_CREATEFUNCTION",
+    "TOK_DROPFUNCTION",
+
+    "TOK_ALTERDATABASE_PROPERTIES",
+    "TOK_ALTERINDEX_PROPERTIES",
+    "TOK_ALTERINDEX_REBUILD",
+    "TOK_ALTERTABLE_ADDCOLS",
+    "TOK_ALTERTABLE_ADDPARTS",
+    "TOK_ALTERTABLE_ARCHIVE",
+    "TOK_ALTERTABLE_CLUSTER_SORT",
+    "TOK_ALTERTABLE_DROPPARTS",
+    "TOK_ALTERTABLE_PARTITION",
+    "TOK_ALTERTABLE_PROPERTIES",
+    "TOK_ALTERTABLE_RENAME",
+    "TOK_ALTERTABLE_RENAMECOL",
+    "TOK_ALTERTABLE_REPLACECOLS",
+    "TOK_ALTERTABLE_TOUCH",
+    "TOK_ALTERTABLE_UNARCHIVE",
+    "TOK_ANALYZE",
+    "TOK_CREATEDATABASE",
+    "TOK_CREATEINDEX",
+    "TOK_CREATETABLE",
+    "TOK_DROPDATABASE",
+    "TOK_DROPINDEX",
+    "TOK_DROPTABLE",
+    "TOK_MSCK",
+
+    // TODO(marmbrus): Figure out how view are expanded by hive, as we might need to handle this.
+    "TOK_ALTERVIEW_ADDPARTS",
+    "TOK_ALTERVIEW_DROPPARTS",
+    "TOK_ALTERVIEW_PROPERTIES",
+    "TOK_ALTERVIEW_RENAME",
+    "TOK_CREATEVIEW",
+    "TOK_DROPVIEW",
+
+    "TOK_EXPORT",
+    "TOK_IMPORT",
+    "TOK_LOAD",
+
+    "TOK_SWITCHDATABASE"
+  )
+
   def parseSql(sql: String): LogicalPlan = {
     if(sql.toLowerCase.startsWith("set"))
       ConfigurationAssignment(sql)
@@ -59,13 +127,13 @@ object Hive {
               (new ParseDriver()).parse(sql, null /* no context required for parsing alone */))
           } catch {
             case pe: org.apache.hadoop.hive.ql.parse.ParseException =>
-              sys.error(s"Failed to parse '$sql'")
+              throw new RuntimeException(s"Failed to parse sql: '$sql'", pe)
           }
 
-      nodeToPlan(tree) match {
-        case NativePlaceholder(_) => NativeCommand(sql)
-        case other => other
-      }
+      if(nativeCommands contains tree.getText)
+        NativeCommand(sql)
+      else
+        nodeToPlan(tree)
     }
   }
 
@@ -78,77 +146,9 @@ object Hive {
     }
   }
 
-  // Kinda gross hack...
-  case class NativePlaceholder(ast: Node) extends Command
-
-  protected def nodeToPlan(node: Node): LogicalPlan = node match {
-    case a @ Token("TOK_EXPLAIN", _) => NativePlaceholder(a)
-
-    case a @ Token("TOK_DESCFUNCTION", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DESCTABLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DESCDATABASE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOW_TABLESTATUS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWDATABASES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWFUNCTIONS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWINDEXES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWINDEXES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWPARTITIONS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWTABLES", _) => NativePlaceholder(a)
-
-    case a @ Token("TOK_LOCKTABLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOWLOCKS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_UNLOCKTABLE", _) => NativePlaceholder(a)
-
-    case a @ Token("TOK_CREATEROLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DROPROLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_GRANT", _) => NativePlaceholder(a)
-    case a @ Token("TOK_GRANT_ROLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_REVOKE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOW_GRANT", _) => NativePlaceholder(a)
-    case a @ Token("TOK_SHOW_ROLE_GRANT", _) => NativePlaceholder(a)
-
-    case a @ Token("TOK_CREATEFUNCTION", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DROPFUNCTION", _) => NativePlaceholder(a)
-
-    case a @ Token("TOK_ALTERDATABASE_PROPERTIES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERINDEX_PROPERTIES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERINDEX_REBUILD", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_ADDCOLS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_ADDPARTS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_ARCHIVE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_CLUSTER_SORT", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_DROPPARTS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_PARTITION", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_PROPERTIES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_RENAME", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_RENAMECOL", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_REPLACECOLS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_TOUCH", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERTABLE_UNARCHIVE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ANALYZE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_CREATEDATABASE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_CREATEINDEX", _) => NativePlaceholder(a)
-    case a @ Token("TOK_CREATETABLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DROPDATABASE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DROPINDEX", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DROPTABLE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_MSCK", _) => NativePlaceholder(a)
-
-    // TODO(marmbrus): Figure out how view are expanded by hive, as we might need to handle this.
-    case a @ Token("TOK_ALTERVIEW_ADDPARTS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERVIEW_DROPPARTS", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERVIEW_PROPERTIES", _) => NativePlaceholder(a)
-    case a @ Token("TOK_ALTERVIEW_RENAME", _) => NativePlaceholder(a)
-    case a @ Token("TOK_CREATEVIEW", _) => NativePlaceholder(a)
-    case a @ Token("TOK_DROPVIEW", _) => NativePlaceholder(a)
 
 
-    case a @ Token("TOK_EXPORT", _) => NativePlaceholder(a)
-    case a @ Token("TOK_IMPORT", _) => NativePlaceholder(a)
-    case a @ Token("TOK_LOAD", _) => NativePlaceholder(a)
 
-    case a @ Token("TOK_SWITCHDATABASE", _) => NativePlaceholder(a)
-    case a @ Token("TOK_EXPLAIN", _) => NativePlaceholder(a)
 
     case Token("TOK_QUERY",
            fromClause ::
