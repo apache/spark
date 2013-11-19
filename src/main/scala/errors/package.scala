f90da36f3229151b4244d4ffad72b4d01ae86f1a
Michael Armbrust <michael@databricks.com>
2013-11-18 23:26:46 -0800

    better printing of optimization exceptions

--- src/main/scala/errors/package.scala
+++ src/main/scala/errors/package.scala
@@ -7,6 +7,9 @@ package object errors {
   class OptimizationException[TreeType <: TreeNode[_]]
     (tree: TreeType, msg: String, cause: Throwable = null) extends Exception(msg, cause) {
 
+    override def getMessage: String = {
+      s"${super.getMessage}, tree:\n$tree"
+    }
   }
 
   def attachTree[TreeType <: TreeNode[_], A](tree: TreeType, msg: String = "")(f: => A): A = {
