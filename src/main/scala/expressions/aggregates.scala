88f2efda2400f522e11ab9938d7d51ee3decdea5
Michael Armbrust <michael@databricks.com>
2013-11-29 00:41:59 -0800

    Add sum / count distinct expressions.

--- src/main/scala/expressions/aggregates.scala
+++ src/main/scala/expressions/aggregates.scala
@@ -14,9 +14,24 @@ case class Count(child: Expression) extends AggregateExpression with trees.Unary
   override def toString = s"COUNT($child)"
 }
 
+case class CountDistinct(expressions: Seq[Expression]) extends AggregateExpression {
+  def children = expressions
+  def references = expressions.flatMap(_.references).toSet
+  def nullable = false
+  def dataType = IntegerType
+  override def toString = s"COUNT(DISTINCT ${expressions.mkString(",")}})"
+}
+
 case class Average(child: Expression) extends AggregateExpression with trees.UnaryNode[Expression] {
   def references = child.references
   def nullable = false
   def dataType = DoubleType
   override def toString = s"AVG($child)"
+}
+
+case class Sum(child: Expression) extends AggregateExpression with trees.UnaryNode[Expression] {
+  def references = child.references
+  def nullable = false
+  def dataType = child.dataType
+  override def toString = s"SUM($child)"
 }
