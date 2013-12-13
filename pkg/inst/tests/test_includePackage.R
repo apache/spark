db56a345b7526a2541e09595efe8d93de4fc6c57
Shivaram Venkataraman <shivaram@eecs.berkeley.edu>
2013-12-12 19:42:04 -0800

    Add a test case for include package

---
+++ pkg/inst/tests/test_includePackage.R
@@ -0,0 +1,35 @@
+context("include R packages")
+
+# JavaSparkContext handle
+sc <- sparkR.init()
+
+# Partitioned data
+nums <- 1:3
+rrdd <- parallelize(sc, nums, 3L)
+
+test_that("include inside function", {
+  # Only run the test if Matrix is installed.
+  if ("Matrix" %in% rownames(installed.packages())) {
+    suppressPackageStartupMessages(library(Matrix))
+    generateSparse <- function(x) {
+      suppressPackageStartupMessages(library(Matrix))
+      sparseMatrix(i=c(1, 2, 3), j=c(1, 2, 3), x=c(1, 2, 3))
+    }
+
+    sparseMat <- lapplyPartition(rrdd, generateSparse)
+    actual <- collect(sparseMat)
+  }
+})
+
+test_that("use include package", {
+  # Only run the test if Matrix is installed.
+  if ("Matrix" %in% rownames(installed.packages())) {
+    suppressPackageStartupMessages(library(Matrix))
+    generateSparse <- function(x) {
+      sparseMatrix(i=c(1, 2, 3), j=c(1, 2, 3), x=c(1, 2, 3))
+    }
+    includePackage(sc, Matrix)
+    sparseMat <- lapplyPartition(rrdd, generateSparse)
+    actual <- collect(sparseMat)
+  }
+})
