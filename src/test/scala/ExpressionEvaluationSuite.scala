7fcf480fd82182ca9a256dda52cf6c9e963f023d
Michael Armbrust <michael@databricks.com>
2013-12-14 16:48:02 -0800

    test for and logic

--- src/test/scala/ExpressionEvaluationSuite.scala
+++ src/test/scala/ExpressionEvaluationSuite.scala
@@ -3,7 +3,7 @@ package expressions
 
 import org.scalatest.FunSuite
 
-import types.IntegerType
+import types._
 import expressions._
 
 import dsl._
@@ -12,5 +12,22 @@ class ExpressionEvaluationSuite extends FunSuite {
 
   test("literals") {
     assert(Evaluate(Literal(1) + Literal(1), Nil) === 2)
+  }
+
+  test("boolean logic") {
+    val andTruthTable =
+      (true, true, true) ::
+      (true, false, false) ::
+      (false, true, false) ::
+      (false, false, false) :: Nil
+
+    andTruthTable.foreach {
+      case (l,r,answer) =>
+        val result = Evaluate(Literal(l, BooleanType) && Literal(r, BooleanType), Nil)
+        if(result != answer)
+          fail(s"$l && $r != $result")
+    }
+
+
   }
 }
