dfad3f504eedbcefb1e968c74df272353c7dc70c
Zongheng Yang <zongheng.y@gmail.com>
2013-12-23 15:25:14 -0800

    Add test_utils.R: a unit test for convertJListToRList().

---
+++ pkg/inst/tests/test_utils.R
@@ -0,0 +1,22 @@
+context("functions in utils.R")
+
+# JavaSparkContext handle
+sc <- sparkR.init()
+
+test_that("convertJListToRList() gives back (deserializes) the original JLists
+          of strings and integers", {
+  # It's hard to manually create a Java List using rJava, since it does not
+  # support generics well. Instead, we rely on collect() returning a
+  # JList.
+  nums <- as.list(1:10)
+  rrdd <- parallelize(sc, nums, 1L)
+  jList <- .jcall(rrdd@jrdd, "Ljava/util/List;", "collect")
+  rList <- convertJListToRList(jList, flatten = TRUE)
+  expect_equal(rList, nums)
+
+  strs <- as.list("hello", "spark")
+  rrdd <- parallelize(sc, strs, 2L)
+  jList <- .jcall(rrdd@jrdd, "Ljava/util/List;", "collect")
+  rList <- convertJListToRList(jList, flatten = TRUE)
+  expect_equal(rList, strs)
+})
