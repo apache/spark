d5c05c6da11cbdcdbe8287348d3304a3c88796a4
Michael Armbrust <michael@databricks.com>
2013-12-24 12:54:07 -0800

    For now, ignore the big data benchmark tests when the data isn't there.

--- src/test/scala/shark2/BigDataBenchmark.scala
+++ src/test/scala/shark2/BigDataBenchmark.scala
@@ -11,7 +11,6 @@ class BigDataBenchmarkTests extends HiveComaparisionTest {
   import TestShark._
 
   val testDataDirectory = new File("target/big-data-benchmark-testdata")
-  require(testDataDirectory.exists())
 
   val testTables = Seq(
     TestTable(
@@ -60,45 +59,50 @@ class BigDataBenchmarkTests extends HiveComaparisionTest {
 
   testTables.foreach(registerTestTable)
 
-  createQueryTest("query1",
-    "SELECT pageURL, pageRank FROM rankings WHERE pageRank > 1")
+  if(!testDataDirectory.exists()) {
+    // TODO: Auto download the files on demand.
+    ignore("No data files found for BigDataBenchmark tests.") {}
+  } else {
+    createQueryTest("query1",
+      "SELECT pageURL, pageRank FROM rankings WHERE pageRank > 1")
 
-  createQueryTest("query2",
-    "SELECT SUBSTR(sourceIP, 1, 10), SUM(adRevenue) FROM uservisits GROUP BY SUBSTR(sourceIP, 1, 10)")
+    createQueryTest("query2",
+      "SELECT SUBSTR(sourceIP, 1, 10), SUM(adRevenue) FROM uservisits GROUP BY SUBSTR(sourceIP, 1, 10)")
 
-  createQueryTest("query3",
-    """
-      |SELECT sourceIP,
-      |       sum(adRevenue) as totalRevenue,
-      |       avg(pageRank) as pageRank
-      |FROM
-      |  rankings R JOIN
-      |  (SELECT sourceIP, destURL, adRevenue
-      |   FROM uservisits UV
-      |   WHERE UV.visitDate > "1980-01-01"
-      |   AND UV.visitDate < "1980-04-01")
-      |   NUV ON (R.pageURL = NUV.destURL)
-      |GROUP BY sourceIP
-      |ORDER BY totalRevenue DESC
-      |LIMIT 1
-    """.stripMargin)
+    createQueryTest("query3",
+      """
+        |SELECT sourceIP,
+        |       sum(adRevenue) as totalRevenue,
+        |       avg(pageRank) as pageRank
+        |FROM
+        |  rankings R JOIN
+        |  (SELECT sourceIP, destURL, adRevenue
+        |   FROM uservisits UV
+        |   WHERE UV.visitDate > "1980-01-01"
+        |   AND UV.visitDate < "1980-04-01")
+        |   NUV ON (R.pageURL = NUV.destURL)
+        |GROUP BY sourceIP
+        |ORDER BY totalRevenue DESC
+        |LIMIT 1
+      """.stripMargin)
 
-  createQueryTest("query4",
-    """
-      |DROP TABLE IF EXISTS url_counts_partial;
-      |CREATE TABLE url_counts_partial AS
-      |  SELECT TRANSFORM (line)
-      |  USING 'python target/url_count.py' as (sourcePage,
-      |    destPage, count) from documents;
-      |DROP TABLE IF EXISTS url_counts_total;
-      |CREATE TABLE url_counts_total AS
-      |  SELECT SUM(count) AS totalCount, destpage
-      |  FROM url_counts_partial GROUP BY destpage
-      |-- The following queries run, but generate different results in HIVE likely because the UDF is not deterministic
-      |-- given different input splits.
-      |-- SELECT CAST(SUM(count) AS INT) FROM url_counts_partial
-      |-- SELECT COUNT(*) FROM url_counts_partial
-      |-- SELECT * FROM url_counts_partial
-      |-- SELECT * FROM url_counts_total
-    """.stripMargin)
+    createQueryTest("query4",
+      """
+        |DROP TABLE IF EXISTS url_counts_partial;
+        |CREATE TABLE url_counts_partial AS
+        |  SELECT TRANSFORM (line)
+        |  USING 'python target/url_count.py' as (sourcePage,
+        |    destPage, count) from documents;
+        |DROP TABLE IF EXISTS url_counts_total;
+        |CREATE TABLE url_counts_total AS
+        |  SELECT SUM(count) AS totalCount, destpage
+        |  FROM url_counts_partial GROUP BY destpage
+        |-- The following queries run, but generate different results in HIVE likely because the UDF is not deterministic
+        |-- given different input splits.
+        |-- SELECT CAST(SUM(count) AS INT) FROM url_counts_partial
+        |-- SELECT COUNT(*) FROM url_counts_partial
+        |-- SELECT * FROM url_counts_partial
+        |-- SELECT * FROM url_counts_total
+      """.stripMargin)
+  }
 }
