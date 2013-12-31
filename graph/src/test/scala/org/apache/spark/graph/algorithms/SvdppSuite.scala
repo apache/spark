6e50df6255dbe25c880dced3d15a77241eba803d
Jianping J Wang <jianping.j.wang@gmail.com>
2013-12-31 22:02:16 +0800

    Update SvdppSuite.scala

--- graph/src/test/scala/org/apache/spark/graph/algorithms/SvdppSuite.scala
+++ graph/src/test/scala/org/apache/spark/graph/algorithms/SvdppSuite.scala
@@ -13,17 +13,17 @@ class SvdppSuite extends FunSuite with LocalSparkContext {
 
   test("Test SVD++ with mean square error on training set") {
     withSpark { sc =>
-      val SvdppErr = 0.2
+      val SvdppErr = 8.0
       val edges = sc.textFile("mllib/data/als/test.data").map { line =>
         val fields = line.split(",")
         Edge(fields(0).toLong * 2, fields(1).toLong * 2 + 1, fields(2).toDouble)
       }
-      val conf = new SvdppConf(10, 2, 0.0, 5.0, 0.007, 0.007, 0.005, 0.015)
-      val graph = Svdpp.run(edges, conf)
+      val conf = new SvdppConf(10, 2, 0.0, 5.0, 0.007, 0.007, 0.005, 0.015) // 2 iterations
+      var (graph, u) = Svdpp.run(edges, conf)
       val err = graph.vertices.collect.map{ case (vid, vd) =>
-        if (vid % 2 == 1) { vd._4 } else { 0.0 }
+        if (vid % 2 == 1) vd._4 else 0.0
       }.reduce(_ + _) / graph.triplets.collect.size
-      assert(err < SvdppErr)
+      assert(err <= SvdppErr)
     }
   }
 
