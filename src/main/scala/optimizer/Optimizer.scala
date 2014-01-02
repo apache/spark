4283400fa63389eb315381bcf40a4f8f510a5838
Timothy Chen <tnachen@gmail.com>
2014-01-02 00:20:05 -0800

    Add limited predicate push down

--- src/main/scala/optimizer/Optimizer.scala
+++ src/main/scala/optimizer/Optimizer.scala
@@ -1,18 +1,212 @@
 package catalyst
 package optimizer
 
+import catalyst.expressions._
+import catalyst.expressions.Alias
+import catalyst.plans.Inner
 import catalyst.plans.logical._
+import catalyst.plans.logical.Filter
+import catalyst.plans.logical.Project
+import catalyst.plans.logical.Subquery
 import catalyst.rules._
+import catalyst.types.DataType
 
 object Optimize extends RuleExecutor[LogicalPlan] {
   val batches =
     Batch("Subqueries", Once,
-      EliminateSubqueries) :: Nil
+      EliminateSubqueries,
+      CombineFilters,
+      PredicatePushDown,
+      PredicatePushDownWithAlias,
+      PushPredicateThroughInnerEqualJoin) :: Nil
 
 }
 
 object EliminateSubqueries extends Rule[LogicalPlan] {
   def apply(plan: LogicalPlan): LogicalPlan = plan transform {
     case Subquery(_, child) => child
+  }
+}
+
+object CombineFilters extends Rule[LogicalPlan] {
+  def apply(plan: LogicalPlan): LogicalPlan = plan transform {
+    case ff@Filter(fc, nf@Filter(nc, grandChild)) => Filter(And(nc, fc), grandChild)
+  }
+}
+
+object PredicatePushDown extends Rule[LogicalPlan] {
+  def apply(plan: LogicalPlan): LogicalPlan = plan transform {
+    case filter@Filter(_, project@Project(_, grandChild))
+      if filter.references subsetOf (grandChild.outputSet) =>
+      project.copy(child = filter.copy(child = grandChild))
+  }
+}
+
+object EmptyExpression extends EmptyExpressionType(null, null)
+
+case class EmptyExpressionType(left: Expression, right: Expression) extends Expression {
+  self: Product =>
+
+  case object NullType extends DataType
+
+  /** Returns a Seq of the children of this node */
+
+  def children: Seq[Expression] = Seq.empty
+
+  def dataType: DataType = NullType
+
+  def nullable: Boolean = true
+
+  def references: Set[Attribute] = Set.empty
+}
+
+object PushPredicateThroughInnerEqualJoin extends Rule[LogicalPlan] {
+  class BinaryConditionStatus {
+    var left = false
+    var right = false
+  }
+
+  object Direction extends Enumeration {
+    type Direction = Value
+    val Left, Right = Value
+  }
+
+  def apply(plan: LogicalPlan): LogicalPlan = plan transform {
+    case filter@Filter(_, join@Join(l, r, joinType@Inner, cond)) => {
+      var joinLeftExpression: Option[Expression] = None
+      var joinRightExpression: Option[Expression] = None
+
+      val newFilterCondition = rewriteCondition (filter.condition transform {
+        case and: And => {
+          val status = new BinaryConditionStatus
+
+          val rightFilter =
+            combineExpression(
+              joinFilter(status, Direction.Left, and.left, r.outputSet),
+              joinFilter(status, Direction.Right, and.right, r.outputSet),
+              And
+            )
+          val leftFilter =
+            combineExpression(
+              joinFilter(status, Direction.Left, and.left, l.outputSet),
+              joinFilter(status, Direction.Right, and.right, l.outputSet),
+              And
+            )
+
+          joinLeftExpression = combineExpression(joinLeftExpression, leftFilter, And)
+          joinRightExpression = combineExpression(joinRightExpression, rightFilter, And)
+
+          (status.left, status.right) match {
+            case (true, true) => EmptyExpression
+            case (true, false) => and.right
+            case (false, true) => and.left
+            case (false, false) => and
+          }
+        }
+
+        case u: BinaryExpression if u.references subsetOf (l.outputSet) => {
+          joinLeftExpression = combineExpression(joinLeftExpression, Option(u), And)
+          EmptyExpression
+        }
+
+        case u: BinaryExpression if u.references subsetOf (r.outputSet) => {
+          joinRightExpression = combineExpression(joinRightExpression, Option(u), And)
+          EmptyExpression
+        }
+      })
+
+      (newFilterCondition, joinLeftExpression, joinRightExpression) match {
+        case (EmptyExpression, _, _) =>
+          copyJoin(join, joinLeftExpression, joinRightExpression)
+        case (_, None, None) => filter
+        case _ => {
+          filter.copy(condition = newFilterCondition,
+            child = copyJoin(
+              join, joinLeftExpression, joinRightExpression
+            )
+          )
+        }
+      }
+    }
+  }
+
+  def rewriteCondition(exp: Expression): Expression = {
+    exp match {
+      case b: And => b match {
+        case And(EmptyExpression, e:Expression) => rewriteCondition(e)
+        case And(e:Expression, EmptyExpression) => rewriteCondition(e)
+        case And(e:Expression, e2:Expression) => And(rewriteCondition(e), rewriteCondition(e2))
+        case And(EmptyExpression, EmptyExpression) => EmptyExpression
+      }
+      case _ => exp
+    }
+  }
+
+  def combineExpression[T1, T2](firstExp: Option[T1],
+                                secondExp: Option[T2],
+                                combineFunc: (T1, T2) => T2): Option[T2] = {
+    (firstExp, secondExp) match {
+      case (o1: Some[T1], o2: Some[T2]) => Option(combineFunc(o1.get, o2.get))
+      case (None, o2: Some[T2]) => o2
+      case (o1: Some[T1], None) => Option(o1.get.asInstanceOf[T2])
+      case (None, None) => None
+    }
+  }
+
+  def copyJoin(join: Join,
+               joinLeftExpression: Option[Expression],
+               joinRightExpression: Option[Expression]): Join = {
+    join.copy(
+      left =
+        combineExpression(
+          joinLeftExpression,
+          Option(join.left),
+          Filter
+        ).get,
+      right =
+        combineExpression(
+          joinRightExpression,
+          Option(join.right),
+          Filter
+        ).get
+    )
+  }
+
+  def joinFilter(status: BinaryConditionStatus,
+                 dir: Direction.Value,
+                 expression: Expression,
+                 outputSet: Set[Attribute]): Option[Expression] = {
+    if (expression.references subsetOf (outputSet)) {
+      dir match {
+        case Direction.Left => status.left = true
+        case Direction.Right => status.right = true
+      }
+      Option(expression)
+    } else None
+  }
+}
+
+object PredicatePushDownWithAlias extends Rule[LogicalPlan] {
+  def apply(plan: LogicalPlan): LogicalPlan = plan transform {
+    case filter@Filter(condition, project@Project(fields, grandChild)) =>
+      val refMissing = filter.references &~ (filter.references intersect grandChild.outputSet)
+      val sourceAliases = fields.filter(_.isInstanceOf[Alias]).map(_.asInstanceOf[Alias]).toSet
+      if (isAliasOf(refMissing, sourceAliases))
+        project.copy(child = filter.copy(
+          replaceAlias(condition, sourceAliases),
+          grandChild))
+      else filter
+  }
+
+  def isAliasOf(refMissing: Set[Attribute], sourceAliases: Set[Alias]): Boolean = {
+    refMissing.forall(r => sourceAliases.exists(_.exprId == r.exprId))
+  }
+
+  // Assuming the expression cost is minimal, we can replace condition with the alias's child to push down predicate
+  def replaceAlias(condition: Expression, sourceAliases: Set[Alias]): Expression = {
+    condition transform {
+      case a: AttributeReference if sourceAliases.exists(_.exprId == a.exprId) =>
+        sourceAliases.find(_.exprId == a.exprId).get.child
+    }
   }
 }
--- src/test/scala/OptimizerSuite.scala
+++ src/test/scala/OptimizerSuite.scala
@@ -34,6 +34,8 @@ class OptimizerSuite extends FunSuite {
   protected def comparePlans(plan1: LogicalPlan, plan2: LogicalPlan) {
     val normalized1 = normalizeExprIds(plan1)
     val normalized2 = normalizeExprIds(plan2)
+    println(normalized1.treeString)
+    println(normalized2.treeString)
     if(normalized1 != normalized2)
       fail(
         s"""
@@ -82,9 +84,32 @@ class OptimizerSuite extends FunSuite {
         .where('e === 1)
         .analyze
 
-    /* Your code here */
-    fail("not implemented")
+    val optimized = Optimize(originalQuery.analyze)
+    val correctAnswer =
+      testRelation
+        .where('a + 'b === 1)
+        .select('a + 'b as 'e)
+        .analyze
+
+    comparePlans(optimized, correctAnswer)
   }
+
+  test("filters: combines filters") {
+    val originalQuery = testRelation
+      .select('a)
+      .where('a === 1)
+      .where('a === 2)
+
+    val optimized = Optimize(originalQuery.analyze)
+    val correctAnswer =
+      testRelation
+        .where('a === 1 && 'a === 2)
+        .select('a).analyze
+
+
+    comparePlans(optimized, correctAnswer)
+  }
+
 
   test("joins: push to either side") {
     val x = testRelation.subquery('x)
@@ -94,10 +119,51 @@ class OptimizerSuite extends FunSuite {
       x.join(y)
         .where("x.b".attr === 1)
         .where("y.b".attr === 2)
-        .analyze
     }
 
-    fail("not implemented")
+    val optimized = Optimize(originalQuery.analyze)
+    val left = testRelation.where('b === 1)
+    val right = testRelation.where('b === 2)
+    val correctAnswer =
+      left.join(right).analyze
+
+    comparePlans(optimized, correctAnswer)
+  }
+
+  test("joins: push to one side") {
+    val x = testRelation.subquery('x)
+    val y = testRelation.subquery('y)
+
+    val originalQuery = {
+      x.join(y)
+        .where("x.b".attr === 1)
+    }
+
+    val optimized = Optimize(originalQuery.analyze)
+    val left = testRelation.where('b === 1)
+    val right = testRelation
+    val correctAnswer =
+      left.join(right).analyze
+
+    comparePlans(optimized, correctAnswer)
+  }
+
+  test("joins: rewrite filter to push to either side") {
+    val x = testRelation.subquery('x)
+    val y = testRelation.subquery('y)
+
+    val originalQuery = {
+      x.join(y)
+        .where("x.b".attr === 1 && "y.b".attr === 2)
+    }
+
+    val optimized = Optimize(originalQuery.analyze)
+    val left = testRelation.where('b === 1)
+    val right = testRelation.where('b === 2)
+    val correctAnswer =
+      left.join(right).analyze
+
+    comparePlans(optimized, correctAnswer)
   }
 
   test("joins: can't push down") {
@@ -107,11 +173,10 @@ class OptimizerSuite extends FunSuite {
     val originalQuery = {
       x.join(y)
         .where("x.b".attr === "y.b".attr)
-        .analyze
     }
     val optimized = Optimize(originalQuery.analyze)
 
-    comparePlans(optimizer.EliminateSubqueries(originalQuery), optimized)
+    comparePlans(optimizer.EliminateSubqueries(originalQuery.analyze), optimized)
   }
 
   test("joins: conjunctive predicates") {
@@ -121,9 +186,63 @@ class OptimizerSuite extends FunSuite {
     val originalQuery = {
       x.join(y)
         .where(("x.b".attr === "y.b".attr) && ("x.a".attr === 1) && ("y.a".attr === 1))
-        .analyze
     }
 
-    fail("not implemented")
+    val optimized = Optimize(originalQuery.analyze)
+    val left = testRelation.where('a === 1).subquery('x)
+    val right = testRelation.where('a === 1).subquery('y)
+    val correctAnswer =
+      left.join(right)
+        .where("x.b".attr === "y.b".attr)
+        .analyze
+
+    comparePlans(optimized, optimizer.EliminateSubqueries(correctAnswer))
+  }
+
+  test("joins: conjunctive predicates #2") {
+    val x = testRelation.subquery('x)
+    val y = testRelation.subquery('y)
+
+    val originalQuery = {
+      x.join(y)
+        .where(("x.b".attr === "y.b".attr) && ("x.a".attr === 1))
+    }
+
+    val optimized = Optimize(originalQuery.analyze)
+    val left = testRelation.where('a === 1).subquery('x)
+    val right = testRelation.subquery('y)
+    val correctAnswer =
+      left.join(right)
+        .where("x.b".attr === "y.b".attr)
+        .analyze
+
+    comparePlans(optimized, optimizer.EliminateSubqueries(correctAnswer))
+  }
+
+  test("joins: conjunctive predicates #3") {
+    val x = testRelation.subquery('x)
+    val y = testRelation.subquery('y)
+    val z = testRelation.subquery('z)
+
+    val originalQuery = {
+      z.join(x.join(y))
+        .where(("x.b".attr === "y.b".attr) && ("x.a".attr === 1) && ("z.a".attr >= 3) && ("z.a".attr === "x.b".attr))
+    }
+
+    val optimized = Optimize(originalQuery.analyze)
+    val lleft = testRelation.where('a >= 3).subquery('z)
+    val left = testRelation.where('a === 1).subquery('x)
+    val right = testRelation.subquery('y)
+    val correctAnswer =
+      lleft
+        .join(
+          left
+            .join(right)
+            .where("x.b".attr === "y.b".attr)
+        )
+        .where("z.a".attr === "x.b".attr)
+        .analyze
+
+    comparePlans(optimized, optimizer.EliminateSubqueries(correctAnswer))
   }
 }
