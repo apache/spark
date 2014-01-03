e36caf5a210578b49ae16353d983bf1ba0e7459c
Reynold Xin <rxin@apache.org>
2014-01-02 23:28:05 -0800

    Renamed Rule.name to Rule.ruleName since name is used too frequently in the code base and is shadowed often by local scope.

--- src/main/scala/catalyst/rules/Rule.scala
+++ src/main/scala/catalyst/rules/Rule.scala
@@ -4,12 +4,11 @@ package rules
 import trees._
 
 abstract class Rule[TreeType <: TreeNode[_]] extends Logging {
-  val name = {
+
+  /** Name for this rule, automatically inferred based on class name. */
+  val ruleName: String = {
     val className = getClass.getName
-    if(className endsWith "$")
-      className.dropRight(1)
-    else
-      className
+    if (className endsWith "$") className.dropRight(1) else className
   }
 
   def apply(plan: TreeType): TreeType
