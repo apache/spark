044c8ad3a47d245198a16a68e36a417e80e8c37e
Matei Zaharia <matei@databricks.com>
2014-01-07 16:12:20 -0500

    Fix unit test compilation

--- core/src/test/scala/org/apache/spark/deploy/JsonProtocolSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/JsonProtocolSuite.scala
@@ -73,7 +73,8 @@ class JsonProtocolSuite extends FunSuite {
     new ApplicationDescription("name", 4, 1234, cmd, "sparkHome", "appUiUrl")
   }
   def createAppInfo() : ApplicationInfo = {
-    new ApplicationInfo(3, "id", createAppDesc(), new Date(123456789), null, "appUriStr")
+    new ApplicationInfo(
+      3, "id", createAppDesc(), new Date(123456789), null, "appUriStr", Int.MaxValue)
   }
   def createWorkerInfo() : WorkerInfo = {
     new WorkerInfo("id", "host", 8080, 4, 1234, null, 80, "publicAddress")
