372a533a6c091361115f0f0712e93ef3af376b30
Andrew Or <andrewor14@gmail.com>
2014-01-09 21:47:49 -0800

    Fix wonky imports from merge

--- streaming/src/test/java/org/apache/spark/streaming/JavaAPISuite.java
+++ streaming/src/test/java/org/apache/spark/streaming/JavaAPISuite.java
@@ -17,14 +17,6 @@
 
 package org.apache.spark.streaming;
 
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
-import com.google.common.io.Files;
-
-import kafka.serializer.StringDecoder;
-
 import scala.Tuple2;
 
 import org.junit.After;
@@ -36,6 +28,7 @@ import java.util.*;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
+import com.google.common.collect.Sets;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.HashPartitioner;
