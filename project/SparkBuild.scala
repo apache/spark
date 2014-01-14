1d9c210dcbc0c63893921fdef591edd9ffe4c48e
Patrick Wendell <pwendell@gmail.com>
2014-01-14 00:45:27 -0800

    Version changes for release 0.9.0.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -120,7 +120,7 @@ object SparkBuild extends Build {
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization       := "org.apache.spark",
-    version            := "0.9.0-incubating-SNAPSHOT",
+    version            := "0.9.0-incubating",
     scalaVersion       := "2.10.3",
     scalacOptions := Seq("-Xmax-classfile-name", "120", "-unchecked", "-deprecation",
       "-target:" + SCALAC_JVM_VERSION),
