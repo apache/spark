4c2c45eaf14197b79cf5949bb370a74c52a38ff0
Harry Brundage <harry.brundage@gmail.com>
2014-01-21 10:34:05 -0500

    Add an applescript to the spark conf file that autoconnects the VPN if it can't find the interface the VPN should create

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -26,12 +26,23 @@ if [[ -z "$MASTER" ]]; then
   export SPARK_JAVA_OPTS="-Dspark.cores.max=10"
 
   vpn_interface=tap0;
+  get_ip_command="ifconfig $vpn_interface 2>&1 | grep "inet" | awk '{print $2}'"
   if ifconfig $vpn_interface > /dev/null 2>&1; then
-    export SPARK_LOCAL_IP=`ifconfig $vpn_interface 2>&1 | grep "inet" | awk '{print $2}'`
+    export SPARK_LOCAL_IP=`$get_ip_command`
   else
-    echo
-    echo "WARNING: could not find an VPN interface to connect to the Shopify Spark Cluster! Is your VPN connected?"
-    echo
+    echo "WARNING: could not find an VPN interface to connect to the Shopify Spark Cluster! Trying to autoconnect..."
+    osascript -e "
+      tell application \"Viscosity\"
+        connect first connection
+        set total to 0
+        repeat while (first connection's state is not equal to \"Connected\") or total is greater than 15
+          delay 0.5
+          set total to total + 0.5
+        end repeat
+        return \"Connected!\"
+      end tell
+    "
+    export SPARK_LOCAL_IP=`$get_ip_command`
   fi
 
   if which ipython > /dev/null; then
