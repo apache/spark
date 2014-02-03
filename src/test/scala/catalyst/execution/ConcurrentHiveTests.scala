784536466cc3fe69ea230f0e63f7c4cd670fdadc
Michael Armbrust <michael@databricks.com>
2014-02-02 21:13:40 -0800

    deactivate concurrent test.

--- src/test/scala/catalyst/execution/ConcurrentHiveTests.scala
+++ src/test/scala/catalyst/execution/ConcurrentHiveTests.scala
@@ -4,13 +4,15 @@ package execution
 import org.scalatest.{FunSuite, BeforeAndAfterAll}
 
 class ConcurrentHiveTests extends FunSuite with BeforeAndAfterAll {
-  test("Multiple Hive Instances") {
-    (1 to 10).map { i =>
-      val ts = new TestSharkInstance
-      ts.runSqlHive("SHOW TABLES")
-      val q = ts.stringToTestQuery("SELECT * FROM src").q
-      q.toRdd.collect()
-      ts.runSqlHive("SHOW TABLES")
+  ignore("multiple instances not supported") {
+    test("Multiple Hive Instances") {
+      (1 to 10).map { i =>
+        val ts = new TestSharkInstance
+        ts.runSqlHive("SHOW TABLES")
+        val q = ts.stringToTestQuery("SELECT * FROM src").q
+        q.toRdd.collect()
+        ts.runSqlHive("SHOW TABLES")
+      }
     }
   }
 }
