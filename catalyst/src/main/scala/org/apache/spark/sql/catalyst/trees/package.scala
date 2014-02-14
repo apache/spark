678341a50b793b09658b823fa1bdc61a9293d770
Mark Hamstra <markhamstra@gmail.com>
2014-02-14 10:21:24 -0800

    Replaced non-ascii text

--- catalyst/src/main/scala/org/apache/spark/sql/catalyst/trees/package.scala
+++ catalyst/src/main/scala/org/apache/spark/sql/catalyst/trees/package.scala
@@ -10,7 +10,7 @@ package catalyst
  *     transform - accepts a partial function that is used to generate a new tree.  When the
  *     partial function can be applied to a given tree segment, that segment is replaced with the
  *     result.  After attempting to apply the partial function to a given node, the transform
- *     function recursively attempts to apply the function to that node’s children.
+ *     function recursively attempts to apply the function to that node's children.
  *   </li>
  *   <li>debugging support - pretty printing, easy splicing of trees, etc.</li>
  * </ul>
