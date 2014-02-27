42907d051923ff815da7e59608d867df6ab859ca
Binh Nguyen <ngbinh@gmail.com>
2014-02-27 00:25:05 -0800

    Shell scripts to copy public key to Vagrant nodes

---
+++ vagrant/vagrant_nodes.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# user
+export VAGRANT_SPARK_USER="spark-user"
+
+# password
+export VAGRANT_SPARK_PASSWORD="spark"
+
+# master ip
+export VAGRANT_SPARK_MASTER="192.168.2.10"
+
+# worker 1 ip
+export VAGRANT_SPARK_WORKER_1="192.168.2.20"
+
+# worker 2 ip
+export VAGRANT_SPARK_WORKER_2="192.168.2.21"
