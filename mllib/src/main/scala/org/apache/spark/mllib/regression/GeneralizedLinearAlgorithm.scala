ba7de4d2daffa084988e4f2a7eb7d8a37f5f015c
schmit <schmit@stanford.edu>
2014-03-16 19:49:03 -0700

    removed comment

--- mllib/src/main/scala/org/apache/spark/mllib/regression/GeneralizedLinearAlgorithm.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/regression/GeneralizedLinearAlgorithm.scala
@@ -35,7 +35,6 @@ abstract class GeneralizedLinearModel(val weights: Array[Double], val intercept:
   extends Serializable {
 
   // Create a column vector that can be used for predictions
-  /// sven: switch back to private if possible
   protected val weightsMatrix = new DoubleMatrix(weights.length, 1, weights:_*)
 
   /**
