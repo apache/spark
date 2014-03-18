36efa627d3f70fc35af8b6ec3898d277deb93fc6
Patrick Wendell <pwendell@gmail.com>
2014-03-17 17:23:25 -0700

    Set project name in pom files and added eclipse/intellij plugins.

--- project/build.scala
+++ project/build.scala
@@ -1,10 +1,18 @@
 import sbt._
 import com.typesafe.sbt.pom.{PomBuild, SbtPomKeys}
+import net.virtualvoid.sbt.graph.Plugin.graphSettings
+
 
 object MyBuild extends PomBuild {
   override def settings = {
     println("Settings accessed") // Temprorary debug statement
-    super.settings ++ Seq(SbtPomKeys.profiles := Seq("yarn"))
+    super.settings ++ Seq(SbtPomKeys.profiles := Seq())
+  }
+
+  override def projectDefinitions(baseDirectory: File): Seq[Project] = {
+    super.projectDefinitions(baseDirectory).map { x => 
+      x.settings(graphSettings: _*)
+    }
   }
 }
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -28,3 +28,8 @@ addSbtPlugin("com.eed3si9n" % "sbt-unidoc" % "0.3.0")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-pom-reader" % "1.0-SNAPSHOT")
 
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.4.0")
+
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
