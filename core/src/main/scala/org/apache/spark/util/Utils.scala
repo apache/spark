aee3569f07262500b4c0e197f079e6f4319eeefb
witgo <witgo@qq.com>
2014-03-19 23:12:28 +0800

    Modify the position of the filter

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -686,7 +686,7 @@ private[spark] object Utils extends Logging {
    */
   def getCallSiteInfo: CallSiteInfo = {
     val trace = Thread.currentThread.getStackTrace().filter( el =>
-      (!el.getMethodName.contains("getStackTrace")))
+      ((!el.getMethodName.contains("getStackTrace")) && (el.getClassName != "scala.Option")))
 
     // Keep crawling up the stack trace until we find the first function not inside of the spark
     // package. We track the last (shallowest) contiguous Spark method. This might be an RDD
@@ -698,7 +698,7 @@ private[spark] object Utils extends Logging {
     var finished = false
     var firstUserClass = "<unknown>"
 
-    for (el <- trace if el.getClassName != "scala.Option") {
+    for (el <- trace) {
       if (!finished) {
         if (SPARK_CLASS_REGEX.findFirstIn(el.getClassName).isDefined) {
           lastSparkMethod = if (el.getMethodName == "<init>") {
