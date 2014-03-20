8a882eff9d4a7377ba2f470a59a3ab4722c66d9d
Tathagata Das <tathagata.das1565@gmail.com>
2014-03-20 16:55:35 -0700

    Bumped versions to Spark 0.9.1
    Self explanatory!
    
    Author: Tathagata Das <tathagata.das1565@gmail.com>
    
    Closes #190 from tdas/branch-0.9-version-bump and squashes the following commits:
    
    34576ee [Tathagata Das] Bumped versions to Spark 0.9.1

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -134,7 +134,7 @@ object SparkBuild extends Build {
 
   def sharedSettings = Defaults.defaultSettings ++ Seq(
     organization       := "org.apache.spark",
-    version            := "0.9.0-incubating",
+    version            := "0.9.1",
     scalaVersion       := "2.10.3",
     scalacOptions := Seq("-Xmax-classfile-name", "120", "-unchecked", "-deprecation",
       "-target:" + SCALAC_JVM_VERSION),
