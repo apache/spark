0dbf2fb46a56d03a458d84794b7335c6a8c5eba4
Cheng Lian <lian@databricks.com>
2014-03-22 10:41:16 +0800

    Make necessary renaming due to rebase
    Signed-off-by: Cheng Lian <lian.cs.zju@gmail.com>

--- sql/core/src/test/scala/org/apache/spark/sql/columnar/ColumnarQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/columnar/ColumnarQuerySuite.scala
@@ -17,14 +17,14 @@
 
 package org.apache.spark.sql.columnar
 
-import org.apache.spark.sql.{TestData, TestSqlContext, DslQueryTest}
+import org.apache.spark.sql.{TestData, TestSQLContext, DslQueryTest}
 import org.apache.spark.sql.execution.SparkLogicalPlan
 
 class ColumnarQuerySuite extends DslQueryTest {
   import TestData._
 
   test("simple columnar query") {
-    val plan = TestSqlContext.executePlan(testData).executedPlan
+    val plan = TestSQLContext.executePlan(testData).executedPlan
     val scan = SparkLogicalPlan(InMemoryColumnarTableScan(plan.output, plan))
 
     checkAnswer(scan, testData.data)
