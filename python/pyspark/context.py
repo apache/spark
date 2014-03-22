1dcadf909bb3e0fe7f57258fb9b4f33923e0bd03
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-03-22 03:31:13 -0700

    typo

--- python/pyspark/context.py
+++ python/pyspark/context.py
@@ -383,7 +383,7 @@ class SparkContext(object):
             raise Exception("storageLevel must be of type pyspark.StorageLevel")
 
         newStorageLevel = self._jvm.org.apache.spark.storage.StorageLevel
-        return newStorageLevel(storageLevel.useDisk, storageLevel.useMemory, storageLevel.userTachyon,
+        return newStorageLevel(storageLevel.useDisk, storageLevel.useMemory, storageLevel.useTachyon,
             storageLevel.deserialized, storageLevel.replication)
 
     def setJobGroup(self, groupId, description):
