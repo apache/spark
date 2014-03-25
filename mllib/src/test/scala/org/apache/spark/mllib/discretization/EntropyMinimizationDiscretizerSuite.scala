0104fe0fa693263d070865641ef847483fb2470d
LIDIAgroup <hmourit@gmail.com>
2014-03-25 13:04:03 +0100

    Fixed EntropyMinimizationDiscretizer.

--- mllib/src/test/scala/org/apache/spark/mllib/discretization/EntropyMinimizationDiscretizerSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/discretization/EntropyMinimizationDiscretizerSuite.scala
@@ -1,22 +1,34 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements. See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.apache.spark.mllib.discretization
 
-import org.scalatest.FunSuite
-import org.apache.spark.mllib.util.LocalSparkContext
-import org.apache.spark.rdd.RDD
-import org.apache.spark.SparkContext._
-import org.apache.spark.mllib.regression.LabeledPoint
 import scala.util.Random
-import org.apache.spark.mllib.util.InfoTheory
+import org.scalatest.FunSuite
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.LocalSparkContext
 
 object EntropyMinimizationDiscretizerSuite {
-    val nFeatures = 5
-    val nDatapoints = 50
-    val nLabels = 3
-    val nPartitions = 3
+  val nFeatures = 5
+  val nDatapoints = 50
+  val nLabels = 3
+  val nPartitions = 3
 	    
-	def generateLabeledData : Array[LabeledPoint] =
-	{
-        
+	def generateLabeledData : Array[LabeledPoint] = {
 	    val rnd = new Random(42)
 	    val labels = Array.fill[Double](nLabels)(rnd.nextDouble)
 	    	    
@@ -29,11 +41,10 @@ object EntropyMinimizationDiscretizerSuite {
 
 class EntropyMinimizationDiscretizerSuite extends FunSuite with LocalSparkContext {
     
-    test("EMD discretization") {
-        val rnd = new Random()
+  test("EMD discretization") {
+    val rnd = new Random(13)
 		
-		val data =
-		for (i <- 1 to 99) yield
+		val data = for (i <- 1 to 99) yield
 			if (i <= 33) {
 			    LabeledPoint(1.0, Array(i.toDouble + rnd.nextDouble*2 - 1))
 			} else if (i <= 66) {
@@ -42,7 +53,7 @@ class EntropyMinimizationDiscretizerSuite extends FunSuite with LocalSparkContex
 			    LabeledPoint(3.0, Array(i.toDouble + rnd.nextDouble*2 - 1))
 			}
 		
-		val shuffledData = data.sortWith((lp1, lp2) => if (rnd.nextDouble < 0.5) true else false)
+		val shuffledData = data.sortWith((lp1, lp2) => rnd.nextDouble < 0.5)
 		
 		val rdd = sc.parallelize(shuffledData, 3)
 		
@@ -55,6 +66,6 @@ class EntropyMinimizationDiscretizerSuite extends FunSuite with LocalSparkContex
 		if (math.abs(thresholdsArray(2) - 66.5) > 1.55) {
 		    fail("Selected thresholds aren't what they should be.")
 		}
-    }
+  }
     
 }
