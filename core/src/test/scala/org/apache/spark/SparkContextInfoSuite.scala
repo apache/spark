cbaad85bcbe2f48b56173fe6243e1cade6152f6f
witgo <witgo@qq.com>
2014-03-25 13:28:13 -0700

    Avoid Option while generating call site
    This is an update on https://github.com/apache/spark/pull/180, which changes the solution from blacklisting "Option.scala" to avoiding the Option code path while generating the call path.
    
    Also includes a unit test to prevent this issue in the future, and some minor refactoring.
    
    Thanks @witgo for reporting this issue and working on the initial solution!
    
    Author: witgo <witgo@qq.com>
    Author: Aaron Davidson <aaron@databricks.com>
    
    Closes #222 from aarondav/180 and squashes the following commits:
    
    f74aad1 [Aaron Davidson] Avoid Option while generating call site & add unit tests
    d2b4980 [witgo] Modify the position of the filter
    1bc22d7 [witgo] Fix Stage.name return "apply at Option.scala:120"

--- core/src/test/scala/org/apache/spark/SparkContextInfoSuite.scala
+++ core/src/test/scala/org/apache/spark/SparkContextInfoSuite.scala
@@ -17,7 +17,7 @@
 
 package org.apache.spark
 
-import org.scalatest.FunSuite
+import org.scalatest.{Assertions, FunSuite}
 
 class SparkContextInfoSuite extends FunSuite with LocalSparkContext {
   test("getPersistentRDDs only returns RDDs that are marked as cached") {
@@ -55,5 +55,39 @@ class SparkContextInfoSuite extends FunSuite with LocalSparkContext {
 
     rdd.collect()
     assert(sc.getRDDStorageInfo.size === 1)
+  }
+
+  test("call sites report correct locations") {
+    sc = new SparkContext("local", "test")
+    testPackage.runCallSiteTest(sc)
+  }
+}
+
+/** Call site must be outside of usual org.apache.spark packages (see Utils#SPARK_CLASS_REGEX). */
+package object testPackage extends Assertions {
+  private val CALL_SITE_REGEX = "(.+) at (.+):([0-9]+)".r
+
+  def runCallSiteTest(sc: SparkContext) {
+    val rdd = sc.makeRDD(Array(1, 2, 3, 4), 2)
+    val rddCreationSite = rdd.getCreationSite
+    val curCallSite = sc.getCallSite() // note: 2 lines after definition of "rdd"
+
+    val rddCreationLine = rddCreationSite match {
+      case CALL_SITE_REGEX(func, file, line) => {
+        assert(func === "makeRDD")
+        assert(file === "SparkContextInfoSuite.scala")
+        line.toInt
+      }
+      case _ => fail("Did not match expected call site format")
+    }
+
+    curCallSite match {
+      case CALL_SITE_REGEX(func, file, line) => {
+        assert(func === "getCallSite") // this is correct because we called it from outside of Spark
+        assert(file === "SparkContextInfoSuite.scala")
+        assert(line.toInt === rddCreationLine.toInt + 2)
+      }
+      case _ => fail("Did not match expected call site format")
+    }
   }
 }
