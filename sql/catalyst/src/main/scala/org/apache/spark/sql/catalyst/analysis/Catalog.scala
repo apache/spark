95d7d2a3fc2adc0bbca90d015c6ca319fffb26aa
jerryshao <saisai.shao@intel.com>
2014-03-30 10:03:58 -0700

    [SPARK-1354][SQL] Add tableName as a qualifier for SimpleCatelogy
    Fix attribute unresolved when query with table name as a qualifier in SQLContext with SimplCatelog, details please see [SPARK-1354](https://issues.apache.org/jira/browse/SPARK-1354?jql=project%20%3D%20SPARK).
    
    Author: jerryshao <saisai.shao@intel.com>
    
    Closes #272 from jerryshao/qualifier-fix and squashes the following commits:
    
    7950170 [jerryshao] Add tableName as a qualifier for SimpleCatelogy

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Catalog.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Catalog.scala
@@ -47,10 +47,11 @@ class SimpleCatalog extends Catalog {
       tableName: String,
       alias: Option[String] = None): LogicalPlan = {
     val table = tables.get(tableName).getOrElse(sys.error(s"Table Not Found: $tableName"))
+    val tableWithQualifiers = Subquery(tableName, table)
 
     // If an alias was specified by the lookup, wrap the plan in a subquery so that attributes are
     // properly qualified with this alias.
-    alias.map(a => Subquery(a.toLowerCase, table)).getOrElse(table)
+    alias.map(a => Subquery(a.toLowerCase, tableWithQualifiers)).getOrElse(tableWithQualifiers)
   }
 }
 
