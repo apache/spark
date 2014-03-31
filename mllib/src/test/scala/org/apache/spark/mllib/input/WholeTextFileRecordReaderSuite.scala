6bdf2c2ecc44390ab7e909fe583f69f24a5a1fa2
Xusen Yin <yinxusen@gmail.com>
2014-04-01 02:10:42 +0800

    test for small local file system block size

--- mllib/src/test/scala/org/apache/spark/mllib/input/WholeTextFileRecordReaderSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/input/WholeTextFileRecordReaderSuite.scala
@@ -43,6 +43,9 @@ class WholeTextFileRecordReaderSuite extends FunSuite with BeforeAndAfterAll {
 
   override def beforeAll() {
     sc = new SparkContext("local", "test")
+    
+    // Set the block size of local file system to test whether files are split right or not.
++   sc.hadoopConfiguration.setLong("fs.local.block.size", 32)
   }
 
   override def afterAll() {
