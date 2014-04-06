7fb13b21864ac030ae3bd09d0a4262bdb5bf66f3
Patrick Wendell <pwendell@gmail.com>
2014-04-06 10:35:57 -0700

    Changes to UnionRDD and EmptyRDD

--- core/src/main/scala/org/apache/spark/rdd/EmptyRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/EmptyRDD.scala
@@ -24,7 +24,7 @@ import org.apache.spark.{Partition, SparkContext, TaskContext}
 /**
  * An RDD that has no partitions and no elements.
  */
-class EmptyRDD[T: ClassTag](sc: SparkContext) extends RDD[T](sc, Nil) {
+private[spark] class EmptyRDD[T: ClassTag](sc: SparkContext) extends RDD[T](sc, Nil) {
 
   override def getPartitions: Array[Partition] = Array.empty
 
