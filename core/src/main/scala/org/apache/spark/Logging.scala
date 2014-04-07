e4c76b98396604cecf5094161f6fbfd03996cae5
Patrick Wendell <pwendell@gmail.com>
2014-04-07 14:32:11 -0700

    Logging

--- core/src/main/scala/org/apache/spark/Logging.scala
+++ core/src/main/scala/org/apache/spark/Logging.scala
@@ -22,9 +22,13 @@ import org.slf4j.{Logger, LoggerFactory}
 import org.slf4j.impl.StaticLoggerBinder
 
 /**
+ * <span class="badge" style="float: right; background-color: #44751E;">DEVELOPER API</span>
  * Utility trait for classes that want to log data. Creates a SLF4J logger for the class and allows
  * logging messages at different levels using methods that only evaluate parameters lazily if the
  * log level is enabled.
+ * 
+ * NOTE: DO NOT USE this class outside of Spark. It is intended as an internal utility.
+ *       This will likely be changed or removed in future releases.
  */
 trait Logging {
   // Make the log field transient so that objects with Logging can
