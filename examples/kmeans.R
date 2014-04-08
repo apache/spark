1f7ffb0a1389d6a4f4bbce2a6364773fcfeacc8c
Antonio Piccolboni <antonio@piccolboni.info>
2014-04-08 15:35:03 -0700

    implemented using matrices and vectorized calls wherever possible

--- examples/kmeans.R
+++ examples/kmeans.R
@@ -1,40 +1,32 @@
-require(SparkR)
+library(SparkR)
 
 # Logistic regression in Spark.
 # Note: unlike the example in Scala, a point here is represented as a vector of
 # doubles.
 
-parseVector <- function(line) {
-  nums = strsplit(line, " ")[[1]]
-  sapply(nums, as.double)
-}
-
 parseVectors <- function(lines) {
-	lines = strsplit(as.character(lines) , " ", fixed = TRUE)
-	split(matrix(as.numeric(unlist(lines)), ncol = length(lines[[1]])), 1:length(lines))
+	lines <- strsplit(as.character(lines) , " ", fixed = TRUE)
+	list(matrix(as.numeric(unlist(lines)), ncol = length(lines[[1]])))
 }
 
+dist.fun <- 
+	function(P, C) {
+		apply(
+			C,
+			1, 
+			function(x) 
+				colSums((t(P) - x)^2))}
 
-closestPoint <- function(p, centers) {
-  bestIndex <- 0
-  closest <- .Machine$double.xmax
-  for (i in 1:length(centers)) {
-    tempDist <- sum((p - centers[[i]]) ** 2)
-    if (tempDist < closest) {
-      closest <- tempDist
-      bestIndex <- i
-    }
-  }
-  bestIndex
-}
-
+closestPoint <- 
+	function(P, C)
+		max.col(-dist.fun(P, C))
 # Main program
 
-args <- commandArgs(trailing = TRUE)
+args <- commandArgs(trailing = TRUE) 
 
 if (length(args) != 4) {
-  print("Usage: kmeans <master> <file> <K> <convergeDist>")
-  q("no")
+	print("Usage: kmeans <master> <file> <K> <convergeDist>")
+	q("no")
 }
 
 sc <- sparkR.init(args[[1]], "RKMeans")
@@ -44,33 +36,36 @@ convergeDist <- as.double(args[[4]])
 lines <- textFile(sc, args[[2]])
 points <- cache(lapplyPartition(lines, parseVectors))
 # kPoints <- take(points, K)
-kPoints <- takeSample(points, FALSE, K, 16189L)
+kPoints <- do.call(rbind, takeSample(points, FALSE, K, 16189L))
 tempDist <- 1.0
 
 while (tempDist > convergeDist) {
-  closest <- lapply(points,
-                    function(p) { list(closestPoint(p, kPoints), list(p, 1)) })
-
-  pointStats <- reduceByKey(closest,
-                            function(p1, p2) {
-                              list(p1[[1]] + p2[[1]], p1[[2]] + p2[[2]])
-                            },
-                            2L)
-
-  newPoints <- collect(lapply(pointStats,
-                              function(tup) {
-                                list(tup[[1]], tup[[2]][[1]] / tup[[2]][[2]])
-                              }))
-
-  tempDist <- sum(sapply(newPoints,
-                         function(tup) {
-                           sum((kPoints[[tup[[1]]]] - tup[[2]]) ** 2)
-                         }))
-
-  for (tup in newPoints)
-    kPoints[[tup[[1]]]] <- tup[[2]]
-
-  cat("Finished iteration (delta = ", tempDist, ")\n")
+	closest <- lapplyPartition(
+		lapply(points,
+					 function(p) {
+					 	cp <- closestPoint(p, kPoints); 
+					 	mapply(list, unique(cp), split.data.frame(cbind(1, p), cp), SIMPLIFY=FALSE)}),
+		function(x) do.call(c, x))
+	
+	pointStats <- reduceByKey(closest,
+														function(p1, p2) {
+															t(colSums(rbind(p1, p2)))
+														},
+														2L)
+	
+	newPoints <- do.call(
+		rbind,
+		collect(lapply(pointStats,
+									 function(tup) {
+									 	point.sum <- tup[[2]][, -1]
+									 	point.count <- tup[[2]][, 1]
+									 	point.sum/point.count
+									 })))
+	
+	D <- dist.fun(kPoints, newPoints)
+	tempDist <- sum(D[cbind(1:3, max.col(-D))])
+  kPoints <- newPoints
+	cat("Finished iteration (delta = ", tempDist, ")\n")
 }
 
 cat("Final centers:\n")
