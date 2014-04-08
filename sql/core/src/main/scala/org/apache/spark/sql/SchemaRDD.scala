d69594dca922f87aa4ac05c4ab0b59a47eb12e5b
Ahir Reddy <ahirreddy@gmail.com>
2014-04-07 22:11:23 -0700

    returning dictionaries works

--- sql/core/src/main/scala/org/apache/spark/sql/SchemaRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SchemaRDD.scala
@@ -27,6 +27,7 @@ import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.plans.{Inner, JoinType}
 import org.apache.spark.sql.catalyst.types.BooleanType
 import org.apache.spark.api.java.JavaRDD
+import java.util.{Map => JMap}
 
 /**
  * <span class="badge" style="float: right; background-color: darkblue;">ALPHA COMPONENT</span>
@@ -312,11 +313,18 @@ class SchemaRDD(
   def analyze = sqlContext.analyzer(logicalPlan)
 
   def javaToPython: JavaRDD[Array[Byte]] = {
+    //val fieldNames: Seq[String] = logicalPlan.references.map(_.name)
     this.mapPartitions { iter =>
-      val unpickle = new Pickler
+      val pickle = new Pickler
       iter.map { row =>
-        val fields: Array[Any] = (for (i <- 0 to row.length - 1) yield row(i)).toArray
-        unpickle.dumps(fields)
+        val fieldNames: Seq[String] = (1 to row.length).map(_.toString + "KEY") //TODO: Temporary
+        val map: JMap[String, Any] = new java.util.HashMap
+        val arr: java.util.ArrayList[Any] = new java.util.ArrayList
+        row.zip(fieldNames).foreach { case (obj, name) =>
+          map.put(name, obj)
+        }
+        arr.add(map)
+        pickle.dumps(arr)
       }
     }
   }
