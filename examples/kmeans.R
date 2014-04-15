948738af15695bb9db22b0a4932398cb09e585f3
Antonio Piccolboni <antonio@piccolboni.info>
2014-04-14 21:52:53 -0700

    converted tabs to spaces per project request

--- examples/kmeans.R
+++ examples/kmeans.R
@@ -5,28 +5,28 @@ library(SparkR)
 # doubles.
 
 parseVectors <- function(lines) {
-	lines <- strsplit(as.character(lines) , " ", fixed = TRUE)
-	list(matrix(as.numeric(unlist(lines)), ncol = length(lines[[1]])))
+  lines <- strsplit(as.character(lines) , " ", fixed = TRUE)
+  list(matrix(as.numeric(unlist(lines)), ncol = length(lines[[1]])))
 }
 
 dist.fun <- 
-	function(P, C) {
-		apply(
-			C,
-			1, 
-			function(x) 
-				colSums((t(P) - x)^2))}
+  function(P, C) {
+    apply(
+      C,
+      1, 
+      function(x) 
+        colSums((t(P) - x)^2))}
 
 closestPoint <- 
-	function(P, C)
-		max.col(-dist.fun(P, C))
+  function(P, C)
+    max.col(-dist.fun(P, C))
 # Main program
 
 args <- commandArgs(trailing = TRUE) 
 
 if (length(args) != 4) {
-	print("Usage: kmeans <master> <file> <K> <convergeDist>")
-	q("no")
+  print("Usage: kmeans <master> <file> <K> <convergeDist>")
+  q("no")
 }
 
 sc <- sparkR.init(args[[1]], "RKMeans")
@@ -40,32 +40,32 @@ kPoints <- do.call(rbind, takeSample(points, FALSE, K, 16189L))
 tempDist <- 1.0
 
 while (tempDist > convergeDist) {
-	closest <- lapplyPartition(
-		lapply(points,
-					 function(p) {
-					 	cp <- closestPoint(p, kPoints); 
-					 	mapply(list, unique(cp), split.data.frame(cbind(1, p), cp), SIMPLIFY=FALSE)}),
-		function(x) do.call(c, x))
-	
-	pointStats <- reduceByKey(closest,
-														function(p1, p2) {
-															t(colSums(rbind(p1, p2)))
-														},
-														2L)
-	
-	newPoints <- do.call(
-		rbind,
-		collect(lapply(pointStats,
-									 function(tup) {
-									 	point.sum <- tup[[2]][, -1]
-									 	point.count <- tup[[2]][, 1]
-									 	point.sum/point.count
-									 })))
-	
-	D <- dist.fun(kPoints, newPoints)
-	tempDist <- sum(D[cbind(1:3, max.col(-D))])
+  closest <- lapplyPartition(
+    lapply(points,
+           function(p) {
+             cp <- closestPoint(p, kPoints); 
+             mapply(list, unique(cp), split.data.frame(cbind(1, p), cp), SIMPLIFY=FALSE)}),
+    function(x) do.call(c, x))
+  
+  pointStats <- reduceByKey(closest,
+                            function(p1, p2) {
+                              t(colSums(rbind(p1, p2)))
+                            },
+                            2L)
+  
+  newPoints <- do.call(
+    rbind,
+    collect(lapply(pointStats,
+                   function(tup) {
+                     point.sum <- tup[[2]][, -1]
+                     point.count <- tup[[2]][, 1]
+                     point.sum/point.count
+                   })))
+  
+  D <- dist.fun(kPoints, newPoints)
+  tempDist <- sum(D[cbind(1:3, max.col(-D))])
   kPoints <- newPoints
-	cat("Finished iteration (delta = ", tempDist, ")\n")
+  cat("Finished iteration (delta = ", tempDist, ")\n")
 }
 
 cat("Final centers:\n")
