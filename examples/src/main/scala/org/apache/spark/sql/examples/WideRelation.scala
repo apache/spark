8d149d6aac88b2a5369dec0b6fc53d780b5ea9d1
wangfei <wangfei1@huawei.com>
2014-04-17 16:05:57 +0800

    Update WideRelation.scala

--- examples/src/main/scala/org/apache/spark/sql/examples/WideRelation.scala
+++ examples/src/main/scala/org/apache/spark/sql/examples/WideRelation.scala
@@ -23,7 +23,7 @@ import org.apache.spark.sql.SQLContext
 object WideRelation {
   def main(args: Array[String]) {
     println("org.apache.spark.sql.examples.WideRelation master")
-    val sc = new SparkContext("local", "WideRelation", "F:\\code\\spark-master", SparkContext.jarOfClass(this.getClass))
+    val sc = new SparkContext("local", "WideRelation")
     val sqlContext = new SQLContext(sc)
     // Importing the SQL context gives access to all the SQL functions and implicit conversions.
     import sqlContext._
