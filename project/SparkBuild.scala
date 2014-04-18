f6aac55ef7a78569e79e9ee5b5582ee1f2ce56d5
Nick Pentreath <nick.pentreath@gmail.com>
2014-04-18 16:19:17 +0200

    Bring back msgpack

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -344,7 +344,8 @@ object SparkBuild extends Build {
         "com.twitter"                % "chill-java"       % chillVersion excludeAll(excludeAsm),
         "org.tachyonproject"         % "tachyon"          % "0.4.1-thrift" excludeAll(excludeHadoop, excludeCurator, excludeEclipseJetty, excludePowermock),
         "com.clearspring.analytics"  % "stream"           % "2.5.1",
-        "org.spark-project"          % "pyrolite"         % "2.0"
+        "org.spark-project"          % "pyrolite"         % "2.0",
+        "org.msgpack"               %% "msgpack-scala"    % "0.6.8"
       ),
     libraryDependencies ++= maybeAvro
   )
