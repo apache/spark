b20ec7e9d78c9c72d04506a43496a370d771c100
Nick Pentreath <nick.pentreath@gmail.com>
2014-04-19 20:56:44 +0200

    Clean up merge duplicate dependencies

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -344,8 +344,6 @@ object SparkBuild extends Build {
         "com.twitter"                % "chill-java"       % chillVersion excludeAll(excludeAsm),
         "org.tachyonproject"         % "tachyon"          % "0.4.1-thrift" excludeAll(excludeHadoop, excludeCurator, excludeEclipseJetty, excludePowermock),
         "com.clearspring.analytics"  % "stream"           % "2.5.1" excludeAll(excludeFastutil),
-        "org.spark-project"          % "pyrolite"         % "2.0"
-        "com.clearspring.analytics"  % "stream"           % "2.5.1",
         "org.spark-project"          % "pyrolite"         % "2.0",
         "org.msgpack"               %% "msgpack-scala"    % "0.6.8"
       ),
