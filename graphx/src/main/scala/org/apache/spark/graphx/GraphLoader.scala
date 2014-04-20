d6d60e21bfc97fa39b25aa875c96ca9fc05a9973
Ankur Dave <ankurdave@gmail.com>
2014-04-19 20:08:57 -0700

    In GraphLoader, coalesce to minEdgePartitions

--- graphx/src/main/scala/org/apache/spark/graphx/GraphLoader.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/GraphLoader.scala
@@ -60,7 +60,7 @@ object GraphLoader extends Logging {
     val startTime = System.currentTimeMillis
 
     // Parse the edge data table directly into edge partitions
-    val edges = sc.textFile(path, minEdgePartitions).mapPartitionsWithIndex { (pid, iter) =>
+    val edges = sc.textFile(path, minEdgePartitions).coalesce(minEdgePartitions).mapPartitionsWithIndex { (pid, iter) =>
       val builder = new EdgePartitionBuilder[Int]
       iter.foreach { line =>
         if (!line.isEmpty && line(0) != '#') {
