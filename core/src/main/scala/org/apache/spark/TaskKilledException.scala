1fdf659d2fdf23c5562e5dc646d05083062281ed
Reynold Xin <rxin@apache.org>
2014-04-24 00:27:45 -0700

    SPARK-1601 & SPARK-1602: two bug fixes related to cancellation
    This should go into 1.0 since it would return wrong data when the bug happens (which is pretty likely if cancellation is used). Test case attached.
    
    1. Do not put partially executed partitions into cache (in task killing).
    
    2. Iterator returned by CacheManager#getOrCompute was not an InterruptibleIterator, and was thus leading to uninterruptible jobs.
    
    Thanks @aarondav and @ahirreddy for reporting and helping debug.
    
    Author: Reynold Xin <rxin@apache.org>
    
    Closes #521 from rxin/kill and squashes the following commits:
    
    401033f [Reynold Xin] Merge branch 'master' of https://git-wip-us.apache.org/repos/asf/spark into kill
    7a7bdd2 [Reynold Xin] Add a new line in the end of JobCancellationSuite.scala.
    35cd9f7 [Reynold Xin] Fixed a bug that partially executed partitions can be put into cache (in task killing).

--- core/src/main/scala/org/apache/spark/TaskKilledException.scala
+++ core/src/main/scala/org/apache/spark/TaskKilledException.scala
@@ -18,23 +18,6 @@
 package org.apache.spark
 
 /**
- * An iterator that wraps around an existing iterator to provide task killing functionality.
- * It works by checking the interrupted flag in [[TaskContext]].
+ * Exception for a task getting killed.
  */
-private[spark] class InterruptibleIterator[+T](val context: TaskContext, val delegate: Iterator[T])
-  extends Iterator[T] {
-
-  def hasNext: Boolean = {
-    // TODO(aarondav/rxin): Check Thread.interrupted instead of context.interrupted if interrupt
-    // is allowed. The assumption is that Thread.interrupted does not have a memory fence in read
-    // (just a volatile field in C), while context.interrupted is a volatile in the JVM, which
-    // introduces an expensive read fence.
-    if (context.interrupted) {
-      throw new TaskKilledException
-    } else {
-      delegate.hasNext
-    }
-  }
-
-  def next(): T = delegate.next()
-}
+private[spark] class TaskKilledException extends RuntimeException
