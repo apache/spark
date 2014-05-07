9ce3746c31f4ad66b5aa0f82d9fd59bb8e92e759
Andrew Ash <andrew@andrewash.com>
2014-05-07 01:46:16 -0700

    Typo fix: fetchting -> fetching

--- make-distribution.sh
+++ make-distribution.sh
@@ -189,7 +189,7 @@ if [ "$SPARK_TACHYON" == "true" ]; then
   TMPD=`mktemp -d 2>/dev/null || mktemp -d -t 'disttmp'`
 
   pushd $TMPD > /dev/null
-  echo "Fetchting tachyon tgz"
+  echo "Fetching tachyon tgz"
   wget "$TACHYON_URL"
 
   tar xf "tachyon-${TACHYON_VERSION}-bin.tar.gz"
