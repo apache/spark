a59d76b28f7a646744e04b9f0ccd6ad99ad84203
wangfei <scnbwf@yeah.net>
2014-05-10 00:48:36 +0800

    Throw exception when memoryFracton is out of range

--- core/src/main/scala/org/apache/spark/util/collection/ExternalAppendOnlyMap.scala
+++ core/src/main/scala/org/apache/spark/util/collection/ExternalAppendOnlyMap.scala
@@ -76,6 +76,16 @@ class ExternalAppendOnlyMap[K, V, C](
   private val maxMemoryThreshold = {
     val memoryFraction = sparkConf.getDouble("spark.shuffle.memoryFraction", 0.3)
     val safetyFraction = sparkConf.getDouble("spark.shuffle.safetyFraction", 0.8)
+
+    if (memoryFraction > 1 && memoryFraction <= 0) {
+      throw new Exception("spark.shuffle.memoryFraction should be between 0 and 1.")
+    }
+
+    if (safetyFraction > 1 && safetyFraction <= 0) {
+      throw new Exception("spark.shuffle.safetyFraction should be between 0 and 1.")
+    }
+
+
     (Runtime.getRuntime.maxMemory * memoryFraction * safetyFraction).toLong
   }
 
