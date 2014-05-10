561510867a1b79beef57acf9df65c9f88481435d
witgo <witgo@qq.com>
2014-05-10 10:15:04 -0700

    [SPARK-1644] The org.datanucleus:* should not be packaged into spark-assembly-*.jar
    Author: witgo <witgo@qq.com>
    
    Closes #688 from witgo/SPARK-1644 and squashes the following commits:
    
    56ad6ac [witgo] review commit
    87c03e4 [witgo] Merge branch 'master' of https://github.com/apache/spark into SPARK-1644
    6ffa7e4 [witgo] review commit
    a597414 [witgo] The org.datanucleus:* should not be packaged into spark-assembly-*.jar

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -579,12 +579,13 @@ object SparkBuild extends Build {
   def extraAssemblySettings() = Seq(
     test in assembly := {},
     mergeStrategy in assembly := {
-      case m if m.toLowerCase.endsWith("manifest.mf") => MergeStrategy.discard
-      case m if m.toLowerCase.matches("meta-inf.*\\.sf$") => MergeStrategy.discard
-      case "log4j.properties" => MergeStrategy.discard
+      case PathList("org", "datanucleus", xs @ _*)             => MergeStrategy.discard
+      case m if m.toLowerCase.endsWith("manifest.mf")          => MergeStrategy.discard
+      case m if m.toLowerCase.matches("meta-inf.*\\.sf$")      => MergeStrategy.discard
+      case "log4j.properties"                                  => MergeStrategy.discard
       case m if m.toLowerCase.startsWith("meta-inf/services/") => MergeStrategy.filterDistinctLines
-      case "reference.conf" => MergeStrategy.concat
-      case _ => MergeStrategy.first
+      case "reference.conf"                                    => MergeStrategy.concat
+      case _                                                   => MergeStrategy.first
     }
   )
 
