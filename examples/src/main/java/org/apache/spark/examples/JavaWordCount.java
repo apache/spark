969053c9343280720ead5ef0ff3ead9d33c4c8f5
Wisely Chen <thegiive@gmail.com>
2014-05-14 23:35:25 +0800

    Change the origin java example version to Java 8 lambda version

--- examples/src/main/java/org/apache/spark/examples/JavaWordCount.java
+++ examples/src/main/java/org/apache/spark/examples/JavaWordCount.java
@@ -17,58 +17,34 @@
 
 package org.apache.spark.examples;
 
-import scala.Tuple2;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.*;
+import org.apache.spark.api.java.function.*;
+import scala.Tuple2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public final class JavaWordCount {
-  private static final Pattern SPACE = Pattern.compile(" ");
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Usage: JavaWordCount <file>");
+            System.exit(1);
+        }
 
-    if (args.length < 1) {
-      System.err.println("Usage: JavaWordCount <file>");
-      System.exit(1);
+        JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("JavaWordCount"));
+
+        JavaRDD<String> lines = sc.textFile(args[0]);
+        JavaPairRDD<String, Integer> counts = lines.flatMap(line -> Arrays.asList(line.split(" ")))
+                                                    .mapToPair(w -> new Tuple2<>(w, 1))
+                                                    .reduceByKey((x, y) -> x + y);
+
+        System.out.println("Word count is " + counts.count() + " .");
+        sc.stop();
+
     }
-
-    SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
-    JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-    JavaRDD<String> lines = ctx.textFile(args[0], 1);
-
-    JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
-      @Override
-      public Iterable<String> call(String s) {
-        return Arrays.asList(SPACE.split(s));
-      }
-    });
-
-    JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
-      @Override
-      public Tuple2<String, Integer> call(String s) {
-        return new Tuple2<String, Integer>(s, 1);
-      }
-    });
-
-    JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
-      @Override
-      public Integer call(Integer i1, Integer i2) {
-        return i1 + i2;
-      }
-    });
-
-    List<Tuple2<String, Integer>> output = counts.collect();
-    for (Tuple2<?,?> tuple : output) {
-      System.out.println(tuple._1() + ": " + tuple._2());
-    }
-    ctx.stop();
-  }
 }
