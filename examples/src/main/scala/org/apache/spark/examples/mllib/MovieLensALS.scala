050f1caf4b58502a72215c5ed77d143cfaf130b8
Xiangrui Meng <meng@databricks.com>
2014-05-19 20:35:12 -0700

    add a sample dataset for MovieLensALS example

--- examples/src/main/scala/org/apache/spark/examples/mllib/MovieLensALS.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/MovieLensALS.scala
@@ -29,6 +29,7 @@ import org.apache.spark.serializer.{KryoSerializer, KryoRegistrator}
 
 /**
  * An example app for ALS on MovieLens data (http://grouplens.org/datasets/movielens/).
+ * A synthetic dataset in MovieLens format can be found at `data/mllib/sample_movielens_data.txt`.
  */
 object MovieLensALS {
 
@@ -70,6 +71,15 @@ object MovieLensALS {
         .required()
         .text("input paths to a MovieLens dataset of ratings")
         .action((x, c) => c.copy(input = x))
+      note(
+        """
+          |For example, the following command runs this app on a synthetic dataset:
+          |
+          | bin/spark-submit --class org.apache.spark.examples.mllib.MovieLensALS \
+          |  examples/target/scala-*/spark-examples-*.jar \
+          |  --rank 5 --numIterations 20 --lambda 1.0 --kryo \
+          |  data/mllib/sample_movielens_data.txt
+        """.stripMargin)
     }
 
     parser.parse(args, defaultParams).map { params =>
