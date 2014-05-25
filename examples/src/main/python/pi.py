2829e840981bd445b06ab61c4b0526897c1616f4
Reynold Xin <rxin@apache.org>
2014-05-25 01:08:47 -0700

    Fix PEP8 violations in examples/src/main/python.

--- examples/src/main/python/pi.py
+++ examples/src/main/python/pi.py
@@ -29,9 +29,11 @@ if __name__ == "__main__":
     sc = SparkContext(appName="PythonPi")
     slices = int(sys.argv[1]) if len(sys.argv) > 1 else 2
     n = 100000 * slices
+
     def f(_):
         x = random() * 2 - 1
         y = random() * 2 - 1
         return 1 if x ** 2 + y ** 2 < 1 else 0
+
     count = sc.parallelize(xrange(1, n+1), slices).map(f).reduce(add)
     print "Pi is roughly %f" % (4.0 * count / n)
