aeffc2004e2ee4b8f060c44575a90e1dbcf901e3
Reynold Xin <rxin@apache.org>
2014-05-25 01:44:49 -0700

    SPARK-1822: Some minor cleanup work on SchemaRDD.count()
    Minor cleanup following #841.
    
    Author: Reynold Xin <rxin@apache.org>
    
    Closes #868 from rxin/schema-count and squashes the following commits:
    
    5442651 [Reynold Xin] SPARK-1822: Some minor cleanup work on SchemaRDD.count()
    
    (cherry picked from commit d66642e3978a76977414c2fdaedebaad35662667)
    Signed-off-by: Reynold Xin <rxin@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/TestData.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/TestData.scala
@@ -47,7 +47,7 @@ object TestData {
       (1, null) ::
       (2, 2) :: Nil)
 
-  val testData4 = logical.LocalRelation('a.int, 'b.int)
+  val emptyTableData = logical.LocalRelation('a.int, 'b.int)
 
   case class UpperCaseData(N: Int, L: String)
   val upperCaseData =
