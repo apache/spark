c2be415da0fc13a547a8b6f505072e3ca1f6a835
Sandy Ryza <sandy@cloudera.com>
2014-05-27 09:48:32 -0700

    Java and Python aggregateByKey

--- python/pyspark/rdd.py
+++ python/pyspark/rdd.py
@@ -1178,6 +1178,17 @@ class RDD(object):
                     combiners[k] = mergeCombiners(combiners[k], v)
             return combiners.iteritems()
         return shuffled.mapPartitions(_mergeCombiners)
+   
+    def aggregateByKey(self, zeroValue, seqFunc, combFunc, numPartitions=None):
+        """
+        Aggregate the values of each key, using given combine functions and a neutral "zero value".
+        This function can return a different result type, U, than the type of the values in this RDD,
+        V. Thus, we need one operation for merging a T into a U and one operation for merging two U's,
+        The former operation is used for merging values within a partition, and the latter is used
+        for merging values between partitions. To avoid memory allocation, both of these functions are
+        allowed to modify and return their first argument instead of creating a new U.
+        """
+        return self.combineByKey(lambda v: func(zeroValue, v), seqFunc, combFunc, numPartitions)
 
     def foldByKey(self, zeroValue, func, numPartitions=None):
         """
