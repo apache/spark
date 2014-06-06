a761f16eeacf909cc954ba993a40d85157b08269
witgo <witgo@qq.com>
2014-06-06 11:45:21 -0700

    [SPARK-1841]: update scalatest to version 2.1.5
    Author: witgo <witgo@qq.com>
    
    Closes #713 from witgo/scalatest and squashes the following commits:
    
    b627a6a [witgo] merge master
    51fb3d6 [witgo] merge master
    3771474 [witgo] fix RDDSuite
    996d6f9 [witgo] fix TimeStampedWeakValueHashMap test
    9dfa4e7 [witgo] merge bug
    1479b22 [witgo] merge master
    29b9194 [witgo] fix code style
    022a7a2 [witgo] fix test dependency
    a52c0fa [witgo] fix test dependency
    cd8f59d [witgo] Merge branch 'master' of https://github.com/apache/spark into scalatest
    046540d [witgo] fix RDDSuite.scala
    2c543b9 [witgo] fix ReplSuite.scala
    c458928 [witgo] update scalatest to version 2.1.5

--- core/src/test/scala/org/apache/spark/ShuffleNettySuite.scala
+++ core/src/test/scala/org/apache/spark/ShuffleNettySuite.scala
@@ -23,11 +23,11 @@ class ShuffleNettySuite extends ShuffleSuite with BeforeAndAfterAll {
 
   // This test suite should run all tests in ShuffleSuite with Netty shuffle mode.
 
-  override def beforeAll(configMap: Map[String, Any]) {
+  override def beforeAll() {
     System.setProperty("spark.shuffle.use.netty", "true")
   }
 
-  override def afterAll(configMap: Map[String, Any]) {
+  override def afterAll() {
     System.setProperty("spark.shuffle.use.netty", "false")
   }
 }
