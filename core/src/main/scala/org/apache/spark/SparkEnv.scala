026641521ecd18a474e9962a708b9fa5ce3e5837
Andrew Ash <andrew@andrewash.com>
2014-06-08 22:47:48 -0700

    SPARK-2077 Log serializer that actually ends up being used

--- core/src/main/scala/org/apache/spark/SparkEnv.scala
+++ core/src/main/scala/org/apache/spark/SparkEnv.scala
@@ -175,6 +175,7 @@ object SparkEnv extends Logging {
 
     val serializer = instantiateClass[Serializer](
       "spark.serializer", "org.apache.spark.serializer.JavaSerializer")
+    logInfo(s"Using serializer: ${serializer.getClass}")
 
     val closureSerializer = instantiateClass[Serializer](
       "spark.closure.serializer", "org.apache.spark.serializer.JavaSerializer")
