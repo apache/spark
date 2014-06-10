4ca441d4612ac4b228b83e94cd4b7e899bd1371d
Anant <anant.asty@gmail.com>
2014-06-10 16:34:31 -0600

    Implmented check_optput withinthe module to work with python 2.6

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -669,13 +669,22 @@ def ssh(host, opts, command):
             tries = tries + 1
 
 
+def _check_output(*popenargs, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = subprocess.Popen(stdout=PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
+
 def ssh_read(host, opts, command):
-    if sys.version_info < (2, 7):
-        return subprocess.Popen(
-            ssh_command(opts) + ['%s@%s' %
-                                 (opts.user, host), stringify_command(command)],
-        stdout=subprocess.PIPE).stdout
-    return subprocess.check_output(
+    return _check_output(
         ssh_command(opts) + ['%s@%s' % (opts.user, host), stringify_command(command)])
 
 
