cc5bf45308825c95a044027697369f5a56ad45c8
Kan Zhang <kzhang@apache.org>
2014-06-16 13:51:32 -0700

    [SPARK-2130] End-user friendly String representation for StorageLevel in Python

--- python/pyspark/rdd.py
+++ python/pyspark/rdd.py
@@ -1451,6 +1451,8 @@ class RDD(object):
         >>> rdd1 = sc.parallelize([1,2])
         >>> rdd1.getStorageLevel()
         StorageLevel(False, False, False, False, 1)
+        >>> print(rdd1.getStorageLevel())
+        Serialized 1x Replicated
         """
         java_storage_level = self._jrdd.getStorageLevel()
         storage_level = StorageLevel(java_storage_level.useDisk(),
