d81c08bac9756045865ed6490252fbb3f7591142
Kan Zhang <kzhang@apache.org>
2014-06-16 23:31:31 -0700

    [SPARK-2130] End-user friendly String repr for StorageLevel in Python
    JIRA issue https://issues.apache.org/jira/browse/SPARK-2130
    
    This PR adds an end-user friendly String representation for StorageLevel
    in Python, similar to ```StorageLevel.description``` in Scala.
    ```
    >>> rdd = sc.parallelize([1,2])
    >>> storage_level = rdd.getStorageLevel()
    >>> storage_level
    StorageLevel(False, False, False, False, 1)
    >>> print(storage_level)
    Serialized 1x Replicated
    ```
    
    Author: Kan Zhang <kzhang@apache.org>
    
    Closes #1096 from kanzhang/SPARK-2130 and squashes the following commits:
    
    7c8b98b [Kan Zhang] [SPARK-2130] Prettier epydoc output
    cc5bf45 [Kan Zhang] [SPARK-2130] End-user friendly String representation for StorageLevel in Python

--- python/pyspark/rdd.py
+++ python/pyspark/rdd.py
@@ -1448,9 +1448,12 @@ class RDD(object):
     def getStorageLevel(self):
         """
         Get the RDD's current storage level.
+
         >>> rdd1 = sc.parallelize([1,2])
         >>> rdd1.getStorageLevel()
         StorageLevel(False, False, False, False, 1)
+        >>> print(rdd1.getStorageLevel())
+        Serialized 1x Replicated
         """
         java_storage_level = self._jrdd.getStorageLevel()
         storage_level = StorageLevel(java_storage_level.useDisk(),
