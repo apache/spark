e50a06c487327badc9466fde3831d94d81383ba0
klonikar <klonikar@informatica.com>
2014-06-23 08:52:49 +0530

    adding ability to process sparse vectors saved in comma-colon separated index:value,index:value format

--- examples/src/main/scala/org/apache/spark/examples/SparkLRMultiClass.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkLRMultiClass.scala
@@ -22,13 +22,14 @@ import breeze.linalg._
 import breeze.numerics._
 import org.apache.spark._
 import org.apache.spark.SparkContext._
+//import scala.language.existentials // for feature warning in scala 2.10.x
 
 /**
  * @author: Kiran Lonikar
  * Logistic regression based classification for multiple lables. Uses simple gradient descent algorithm with regularization.
  * Usage: SparkLRMultiClass [dataFileName] [numClasses] [lambda] [alpha] [maxIterations] [costThreshold] [numFeatures]
  * From the spark base dir:
- * Compile standalone using:
+ * Compile standalone using (make sure you have built spark before that):
  * scalac -d target -cp assembly/target/scala-2.10/spark-assembly-1.1.0-SNAPSHOT-hadoop1.0.4.jar examples/src/main/scala/org/apache/spark/examples/SparkLRMultiClass.scala
  * Execute using:
  * java -cp target;assembly/target/scala-2.10/spark-assembly-1.1.0-SNAPSHOT-hadoop1.0.4.jar -Dspark.master=local[4] org.apache.spark.examples.SparkLRMultiClass examples\src\main\resources\ny-weather-preprocessed.csv 9 0.5 10 50 0.001
@@ -45,13 +46,40 @@ object SparkLRMultiClass {
   */
   def csv2featureVectors(sc: SparkContext, dataFile: String, numFeaturesIn: Int) = {
       val data = sc.textFile(dataFile)
-	  val vectors = data.map(line => line.split(",").map(_.toDouble))
-	  val featureVectors = vectors.map {p =>
-				val x = DenseVector.vertcat(DenseVector(1.0), DenseVector(p.slice(0, p.length-1)))
-				val yVal = p(p.length-1)
-				(yVal.toInt, x)
-				}
-	  val numFeatures = if(numFeaturesIn == 0) vectors.take(1)(0).size - 1 else numFeaturesIn
+	  val vectors = data.map { line => 
+		val fields = line.split(",")
+		if(fields(0).contains(":")) {
+			val indVals = (0 to (fields.length-2)).map { i =>
+							val parts = fields(i).split(":")
+							(parts(0).toInt, parts(1).toDouble) // index, value
+						}.unzip
+			// The bias/intercept element is assumed to be part of the index-value encoded vector (for example due to mahout kind of encoding)
+			val vb = new VectorBuilder[Double](indVals._1.toArray, indVals._2.toArray, indVals._1.length, indVals._1.max+1)
+			val yVal = fields(fields.length-1).split(":")(1).toInt
+			(yVal, vb.toSparseVector)
+		}
+		else {
+			val vals = fields.map { _.toDouble }
+			// Bias/intercept of 1.0 added in front
+			val x = DenseVector.vertcat(DenseVector(1.0), DenseVector(vals.slice(0, vals.length-1)))
+			val yVal = vals(vals.length-1).toInt
+			(yVal, x)
+		}
+	  }
+	  val featureVec = vectors.take(1)(0)._2
+	  val numFeatures = if(numFeaturesIn == 0) {
+							featureVec match {
+								case sv: SparseVector[Double] => vectors.map { p => p._2.length }.reduce((acc, curr) => if(acc > curr) acc else curr) // no bias/intercept
+								case dv: DenseVector[Double] => dv.length - 1 // to account for bias/intercept element
+								case _ => 0 // throw exception?
+							}
+						}
+						else numFeaturesIn
+	   val featureVectors = featureVec match {
+								case sv: SparseVector[Double] => vectors.map { p => val sv = p._2.asInstanceOf[SparseVector[Double]]; (p._1, new SparseVector[Double](sv.index, sv.data, sv.activeSize, numFeatures+1)) }
+								case dv: DenseVector[Double] => vectors
+								case _ => vectors // throw exception?
+							}
 	  (numFeatures, featureVectors)
   }
   
@@ -93,7 +121,7 @@ object SparkLRMultiClass {
 				val grad = x*(h - y)
 				val J = -(y*breeze.numerics.log(h) + (1.0-y)*breeze.numerics.log(1.0-h))
 				(grad, J)
-			}.reduce((acc, curr) => ((acc._1 + curr._1), (acc._2 + curr._2)))
+			}.reduce {(acc, curr) => ((acc._1 + curr._1), (acc._2 + curr._2))}
 			var grad = (grad_cost._1 + theta*lambda)/m
 			grad(0) -= theta(0)*lambda/m
 			cost = (grad_cost._2 + lambda*(theta.t*theta - theta(0)*theta(0))/2.0)/m
@@ -112,25 +140,20 @@ object SparkLRMultiClass {
 	println(all_theta.toString(Int.MaxValue, Int.MaxValue))
 	
 	// Prediction
-	val correctPredictions = featureVectors.map {p =>
+	val predictions = featureVectors.map {p =>
 				val (yVal, x) = p
 				val z = all_theta*x
 				val h = sigmoid(z)
 				val c = argmax(h)
-				if(c == yVal) 1 else 0
-			}.reduce (_ + _)
+				(yVal, c)
+			}
 
+	val correctPredictions = predictions.map { p => if(p._1 == p._2) 1 else 0 }.reduce(_ + _)
+	
 	val accuracy = correctPredictions/m
 	println("Total correct predictions:  " + correctPredictions + " out of " + m + ", accuracy: " + accuracy);
 
-	val confusionMatrix = featureVectors.map {p =>
-				val (yVal, x) = p
-				val z = all_theta*x
-				val h = sigmoid(z)
-				val c = argmax(h)
-				((yVal, c), 1)
-			}
-			.reduceByKey {(a,b) => a + b}.collect
+	val confusionMatrix = predictions.map {p => ((p._1, p._2), 1) }.reduceByKey {(a,b) => a + b}.collect
 	println("Distribution of predictions: ")
 	confusionMatrix.map { x => x match { case ((actualVal, predictedVal), count) => println("(("+ actualVal+","+predictedVal+")," + count + ")") }}
 
