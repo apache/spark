0b4983723d39488b8a2ce7f3e13f5bdb1d25ac83
Yanjie Gao <gaoyanjie55@163.com>
2014-06-27 16:56:02 +0800

    delete the annotation

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
@@ -236,8 +236,8 @@ private[sql] abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
         execution.Limit(limit, planLater(child))(sparkContext) :: Nil
       case Unions(unionChildren) =>
         execution.Union(unionChildren.map(planLater))(sparkContext) :: Nil
-      case logical.Intersect(left,right) =>                                        //yanjiegaonew
-        execution.Intersect(planLater(left),planLater(right)) :: Nil   //yanjiegaonew
+      case logical.Intersect(left,right) =>                                        
+        execution.Intersect(planLater(left),planLater(right)) :: Nil   
       case logical.Generate(generator, join, outer, _, child) =>
         execution.Generate(generator, join = join, outer = outer, planLater(child)) :: Nil
       case logical.NoRelation =>
