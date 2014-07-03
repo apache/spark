1d36165796b656bb90409e9e254e27b7e72d36d0
Prashant Sharma <prashant.s@imaginea.com>
2014-07-03 15:06:58 -0700

    [SPARK-2109] Setting SPARK_MEM for bin/pyspark does not work.
    Trivial fix.
    
    Author: Prashant Sharma <prashant.s@imaginea.com>
    
    Closes #1050 from ScrapCodes/SPARK-2109/pyspark-script-bug and squashes the following commits:
    
    77072b9 [Prashant Sharma] Changed echos to redirect to STDERR.
    13f48a0 [Prashant Sharma] [SPARK-2109] Setting SPARK_MEM for bin/pyspark does not work.
    (cherry picked from commit 731f683b1bd8abbb83030b6bae14876658bbf098)
    
    Signed-off-by: Patrick Wendell <pwendell@gmail.com>

--- bin/compute-classpath.sh
+++ bin/compute-classpath.sh
@@ -65,10 +65,10 @@ fi
 # Verify that versions of java used to build the jars and run Spark are compatible
 jar_error_check=$("$JAR_CMD" -tf "$ASSEMBLY_JAR" nonexistent/class/path 2>&1)
 if [[ "$jar_error_check" =~ "invalid CEN header" ]]; then
-  echo "Loading Spark jar with '$JAR_CMD' failed. "
-  echo "This is likely because Spark was compiled with Java 7 and run "
-  echo "with Java 6. (see SPARK-1703). Please use Java 7 to run Spark "
-  echo "or build Spark with Java 6."
+  echo "Loading Spark jar with '$JAR_CMD' failed. " 1>&2
+  echo "This is likely because Spark was compiled with Java 7 and run " 1>&2
+  echo "with Java 6. (see SPARK-1703). Please use Java 7 to run Spark " 1>&2
+  echo "or build Spark with Java 6." 1>&2
   exit 1
 fi
 
