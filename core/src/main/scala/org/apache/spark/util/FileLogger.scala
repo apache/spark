0e20f7121b9e8ecb334d69c36ef3cdcbf326699d
Andrew Or <andrewor14@gmail.com>
2014-07-03 14:51:11 -0700

    Shift responsibility of resolving paths up one level

--- core/src/main/scala/org/apache/spark/util/FileLogger.scala
+++ core/src/main/scala/org/apache/spark/util/FileLogger.scala
@@ -51,8 +51,8 @@ private[spark] class FileLogger(
   private val dateFormat = new ThreadLocal[SimpleDateFormat]() {
     override def initialValue(): SimpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
   }
-  private val resolvedLogDir = Utils.resolveURI(logDir)
-  private val fileSystem = Utils.getHadoopFileSystem(resolvedLogDir)
+
+  private val fileSystem = Utils.getHadoopFileSystem(logDir)
   var fileIndex = 0
 
   // Only used if compression is enabled
@@ -82,18 +82,18 @@ private[spark] class FileLogger(
    * Create a logging directory with the given path.
    */
   private def createLogDir() {
-    val path = new Path(resolvedLogDir)
+    val path = new Path(logDir)
     if (fileSystem.exists(path)) {
       if (overwrite) {
-        logWarning("Log directory %s already exists. Overwriting...".format(resolvedLogDir))
+        logWarning("Log directory %s already exists. Overwriting...".format(logDir))
         // Second parameter is whether to delete recursively
         fileSystem.delete(path, true)
       } else {
-        throw new IOException("Log directory %s already exists!".format(resolvedLogDir))
+        throw new IOException("Log directory %s already exists!".format(logDir))
       }
     }
     if (!fileSystem.mkdirs(path)) {
-      throw new IOException("Error in creating log directory: %s".format(resolvedLogDir))
+      throw new IOException("Error in creating log directory: %s".format(logDir))
     }
     if (dirPermissions.isDefined) {
       val fsStatus = fileSystem.getFileStatus(path)
@@ -109,7 +109,7 @@ private[spark] class FileLogger(
    * (dirPermissions) used when class was instantiated.
    */
   private def createWriter(fileName: String, perms: Option[FsPermission] = None): PrintWriter = {
-    val logPath = resolvedLogDir + "/" + fileName
+    val logPath = logDir + "/" + fileName
     val uri = new URI(logPath)
     val path = new Path(logPath)
     val defaultFs = FileSystem.getDefaultUri(hadoopConf).getScheme
