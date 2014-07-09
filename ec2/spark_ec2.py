a36eed0b5772f90be9f11fceddf1c6a5618450ff
Nicholas Chammas <nicholas.chammas@gmail.com>
2014-07-09 10:50:07 -0400

    name ec2 instances and security groups consistently
    Security groups created by spark-ec2 do not prepend “spark-“ to the
    name.
    
    Since naming the instances themselves is new to spark-ec2, it’s better
    to change that pattern to match the existing naming pattern for the
    security groups, rather than the other way around.

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -428,11 +428,11 @@ def launch_cluster(conn, opts, cluster_name):
     for master in master_nodes:
         master.add_tag(
             key='Name',
-            value='spark-{cn}-master-{iid}'.format(cn=cluster_name, iid=master.id))
+            value='{cn}-master-{iid}'.format(cn=cluster_name, iid=master.id))
     for slave in slave_nodes:
         slave.add_tag(
             key='Name',
-            value='spark-{cn}-slave-{iid}'.format(cn=cluster_name, iid=slave.id))
+            value='{cn}-slave-{iid}'.format(cn=cluster_name, iid=slave.id))
 
     # Return all the instances
     return (master_nodes, slave_nodes)
