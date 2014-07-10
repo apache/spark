61118014169beb53aefcc77ac70bbe0a9d642962
Artjom-Metro <Artjom-Metro@users.noreply.github.com>
2014-07-10 18:17:18 +0200

    Reduce the default number of iterations
    Update SparkPageRank and reduce the number of iteration the page rank algorithm runs by default.

--- examples/src/main/scala/org/apache/spark/examples/SparkPageRank.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPageRank.scala
@@ -36,7 +36,7 @@ object SparkPageRank {
       System.exit(1)
     }
     val sparkConf = new SparkConf().setAppName("PageRank")
-    val iters = if (args.length > 0) args(1).toInt else 100
+    val iters = if (args.length > 0) args(1).toInt else 10
     val ctx = new SparkContext(sparkConf)
     val lines = ctx.textFile(args(0), 1)
     val links = lines.map{ s =>
