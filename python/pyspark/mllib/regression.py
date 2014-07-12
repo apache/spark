f7b58ed25d8228c25cac0fc72616fb6ebb392959
Sandy Ryza <sandy@cloudera.com>
2014-07-12 12:46:35 -0700

    SPARK-2461. Add a toString method to GeneralizedLinearModel

--- python/pyspark/mllib/regression.py
+++ python/pyspark/mllib/regression.py
@@ -66,6 +66,8 @@ class LinearModel(object):
     def intercept(self):
         return self._intercept
 
+    def __str__(self):
+        return "(weights: " + str(self._coeff) + ", intercept: " + str(self._intercept) + ")"
 
 class LinearRegressionModelBase(LinearModel):
 
