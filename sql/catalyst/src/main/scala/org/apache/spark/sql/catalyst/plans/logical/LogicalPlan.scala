8bd2816a90d5eb942f126dd6b77aef3e4d260bcf
Zongheng Yang <zongheng.y@gmail.com>
2014-07-16 11:25:27 -0700

    Add a note on performance of statistics.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/LogicalPlan.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/LogicalPlan.scala
@@ -32,6 +32,10 @@ abstract class LogicalPlan extends QueryPlan[LogicalPlan] {
    * statistic produced by the children.  To override this behavior, override `statistics` and
    * assign it a overriden version of `Statistics`.
    *
+   * '''NOTE''': concrete and/or overriden versions of statistics fields should pay attention to the
+   * performance of the implementations.  The reason is that estimations might get triggered in
+   * performance-critical processes, such as query plan planning.
+   *
    * @param sizeInBytes Physical size in bytes. For leaf operators this defaults to 1, otherwise it
    *                    defaults to the product of children's `sizeInBytes`.
    */
