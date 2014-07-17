97cddc038fd07e317c5aceea8a145aaeb9b33e34
Andrew Or <andrewor14@gmail.com>
2014-07-16 18:42:48 -0700

    Add different severity levels
    With this commit, the HistoryNotFoundPage behaves differently for
    each of the following scenarios:
      (1) User did not enable event logging -> point them to the configs
      (2) Event logs are not found for some reason -> tell them that
      (3) Exception is thrown in replaying -> show them the stack trace

--- core/src/main/scala/org/apache/spark/deploy/master/ui/HistoryNotFoundPage.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/ui/HistoryNotFoundPage.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.deploy.master.ui
 
+import java.net.URLDecoder
 import javax.servlet.http.HttpServletRequest
 
 import scala.xml.Node
@@ -26,17 +27,47 @@ import org.apache.spark.ui.{UIUtils, WebUIPage}
 private[spark] class HistoryNotFoundPage(parent: MasterWebUI)
   extends WebUIPage("history/not-found") {
 
+  /**
+   * Render a page that conveys failure in loading application history.
+   *
+   * This accepts 3 HTTP parameters:
+   *   msg = message to display to the user
+   *   title = title of the page
+   *   exception = detailed description of the exception in loading application history (if any)
+   *
+   * Parameters "msg" and "exception" are assumed to be UTF-8 encoded.
+   */
   def render(request: HttpServletRequest): Seq[Node] = {
-    val content =
+    val titleParam = request.getParameter("title")
+    val msgParam = request.getParameter("msg")
+    val exceptionParam = request.getParameter("exception")
+
+    // If no parameters are specified, assume the user did not enable event logging
+    val defaultTitle = "Event logging is not enabled"
+    val defaultContent =
       <div class="row-fluid">
         <div class="span12" style="font-size:14px">
-          No event logs were found for this application. To
+          No event logs were found for this application! To
           <a href="http://spark.apache.org/docs/latest/monitoring.html">enable event logging</a>,
-          please set <span style="font-style:italic">spark.eventLog.enabled</span> to true and
+          set <span style="font-style:italic">spark.eventLog.enabled</span> to true and
           <span style="font-style:italic">spark.eventLog.dir</span> to the directory to which your
           event logs are written.
         </div>
       </div>
-    UIUtils.basicSparkPage(content, "Application history not found")
+
+    val title = Option(titleParam).getOrElse(defaultTitle)
+    val content = Option(msgParam)
+      .map { msg => URLDecoder.decode(msg, "UTF-8") }
+      .map { msg =>
+        <div class="row-fluid">
+          <div class="span12" style="font-size:14px">{msg}</div>
+        </div> ++
+        Option(exceptionParam)
+          .map { e => URLDecoder.decode(e, "UTF-8") }
+          .map { e => <pre>{e}</pre> }
+          .getOrElse(Seq.empty)
+      }.getOrElse(defaultContent)
+
+    UIUtils.basicSparkPage(content, title)
   }
 }
