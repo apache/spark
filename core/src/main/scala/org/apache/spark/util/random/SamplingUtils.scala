69400105159f64f0672da896c313e2a22525d219
Reynold Xin <rxin@apache.org>
2014-07-17 22:00:13 -0700

    Reservoir sampling implementation.

--- core/src/main/scala/org/apache/spark/util/random/SamplingUtils.scala
+++ core/src/main/scala/org/apache/spark/util/random/SamplingUtils.scala
@@ -17,7 +17,47 @@
 
 package org.apache.spark.util.random
 
+import scala.reflect.ClassTag
+
 private[spark] object SamplingUtils {
+
+  /**
+   * Reservoir Sampling implementation.
+   *
+   * @param input input size
+   * @param k reservoir size
+   * @return (samples, input size)
+   */
+  def reservoirSample[T: ClassTag](input: Iterator[T], k: Int): (Array[T], Int) = {
+    val reservoir = new Array[T](k)
+    // Put the first k elements in the reservoir.
+    var i = 0
+    while (i < k && input.hasNext) {
+      val item = input.next()
+      reservoir(i) = item
+      i += 1
+    }
+
+    // If we have consumed all the elements, return them. Otherwise do the replacement.
+    if (i < k) {
+      // If input size < k, trim the array to return only an array of input size.
+      val trimReservoir = new Array[T](i)
+      System.arraycopy(reservoir, 0, trimReservoir, 0, i)
+      (trimReservoir, i)
+    } else {
+      // If input size > k, continue the sampling process.
+      val rand = new XORShiftRandom
+      while (input.hasNext) {
+        val item = input.next()
+        val replacementIndex = rand.nextInt(i)
+        if (replacementIndex < k) {
+          reservoir(replacementIndex) = item
+        }
+        i += 1
+      }
+      (reservoir, i)
+    }
+  }
 
   /**
    * Returns a sampling rate that guarantees a sample of size >= sampleSizeLowerBound 99.99% of
