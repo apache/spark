a7b5b1ccea79a6dfa2dbe69d4cb433421efe916c
Aaron Davidson <aaron@databricks.com>
2014-07-21 00:46:47 -0700

    fix licenses

--- core/src/main/scala/org/apache/spark/util/collection/Sorter.java
+++ core/src/main/scala/org/apache/spark/util/collection/Sorter.java
@@ -1,6 +1,4 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
