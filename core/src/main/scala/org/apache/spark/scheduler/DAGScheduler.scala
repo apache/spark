94bc6e91a89f62238bbf5ffb99b80007e92a4b68
lianhuiwang <lianhuiwang09@gmail.com>
2014-07-23 11:45:59 +0800

    DAGScheduler donot pass priority to TaskSet

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -778,10 +778,8 @@ class DAGScheduler(
       logInfo("Submitting " + tasks.size + " missing tasks from " + stage + " (" + stage.rdd + ")")
       myPending ++= tasks
       logDebug("New pending tasks: " + myPending)
-      val priority:String = properties.getProperty("spark.scheduler.priority", "0")
       taskScheduler.submitTasks(
-        new TaskSet(tasks.toArray, stage.id, stage.newAttemptId(), stage.jobId, priority.toInt,
-          properties))
+        new TaskSet(tasks.toArray, stage.id, stage.newAttemptId(), stage.jobId, properties))
       stageToInfos(stage).submissionTime = Some(clock.getTime())
     } else {
       logDebug("Stage " + stage + " is actually done; %b %d %d".format(
