87948d5cc83876f428a93c1e6a5dd9ef38dfae12
Reynold Xin <rxin@apache.org>
2014-07-22 22:41:53 -0700

    Use HttpBroadcast.

--- core/src/main/scala/org/apache/spark/broadcast/BroadcastManager.scala
+++ core/src/main/scala/org/apache/spark/broadcast/BroadcastManager.scala
@@ -39,7 +39,7 @@ private[spark] class BroadcastManager(
     synchronized {
       if (!initialized) {
         val broadcastFactoryClass =
-          conf.get("spark.broadcast.factory", "org.apache.spark.broadcast.TorrentBroadcastFactory")
+          conf.get("spark.broadcast.factory", "org.apache.spark.broadcast.HttpBroadcastFactory")
 
         broadcastFactory =
           Class.forName(broadcastFactoryClass).newInstance.asInstanceOf[BroadcastFactory]
