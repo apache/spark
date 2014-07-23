33ef56189e76748920bf44962b9652a1d7338445
Gera Shegalov <gera@twitter.com>
2014-07-22 21:05:12 -0500

    [YARN] SPARK-2577: File upload to viewfs is broken due to mount point re...
    Opting to the option 2 defined in SPARK-2577, i.e., retrieve and pass the correct file system object to addResource.
    
    Author: Gera Shegalov <gera@twitter.com>
    
    Closes #1483 from gerashegalov/master and squashes the following commits:
    
    90c9087 [Gera Shegalov] [YARN] SPARK-2577: File upload to viewfs is broken due to mount point resolution

--- yarn/common/src/main/scala/org/apache/spark/deploy/yarn/ClientBase.scala
+++ yarn/common/src/main/scala/org/apache/spark/deploy/yarn/ClientBase.scala
@@ -232,7 +232,8 @@ trait ClientBase extends Logging {
         if (!ClientBase.LOCAL_SCHEME.equals(localURI.getScheme())) {
           val setPermissions = if (destName.equals(ClientBase.APP_JAR)) true else false
           val destPath = copyRemoteFile(dst, qualifyForLocal(localURI), replication, setPermissions)
-          distCacheMgr.addResource(fs, conf, destPath, localResources, LocalResourceType.FILE,
+          val destFs = FileSystem.get(destPath.toUri(), conf)
+          distCacheMgr.addResource(destFs, conf, destPath, localResources, LocalResourceType.FILE,
             destName, statCache)
         } else if (confKey != null) {
           sparkConf.set(confKey, localPath)
