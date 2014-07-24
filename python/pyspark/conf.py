f192df718b1f2cd05f03fbe0d5ca63b38ad144ce
Prashant Sharma <prashant.s@imaginea.com>
2014-07-24 13:05:45 +0530

    Code Review

--- python/pyspark/conf.py
+++ python/pyspark/conf.py
@@ -101,7 +101,7 @@ class SparkConf(object):
 
     def setIfMissing(self, key, value):
         """Set a configuration property, if not already set."""
-        if self.get(key) == None:
+        if self.get(key) is None:
             self.set(key, value)
         return self
 
