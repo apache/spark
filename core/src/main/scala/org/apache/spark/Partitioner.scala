60be09e9e1e8f9fa7ebb039fa11d925bbce48a08
Xiangrui Meng <meng@databricks.com>
2014-07-23 19:41:33 -0700

    remove importance sampler

--- core/src/main/scala/org/apache/spark/Partitioner.scala
+++ core/src/main/scala/org/apache/spark/Partitioner.scala
@@ -19,8 +19,6 @@ package org.apache.spark
 
 import java.io.{IOException, ObjectInputStream, ObjectOutputStream}
 
-import org.apache.spark.util.random.{XORShiftRandom, SamplingUtils}
-
 import scala.collection.mutable.ArrayBuffer
 import scala.reflect.{ClassTag, classTag}
 import scala.util.hashing.byteswap32
@@ -28,6 +26,7 @@ import scala.util.hashing.byteswap32
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.JavaSerializer
 import org.apache.spark.util.{CollectionsUtils, Utils}
+import org.apache.spark.util.random.{XORShiftRandom, SamplingUtils}
 
 /**
  * An object that defines how the elements in a key-value pair RDD are partitioned by key.
