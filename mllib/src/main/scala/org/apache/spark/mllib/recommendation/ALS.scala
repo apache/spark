ea25033744bef26bbe18cf5674ff01c3d4e8048d
GuoQiang Li <witgo@qq.com>
2014-07-30 13:09:24 +0800

    checkpoint products 3,6,9 ...

--- mllib/src/main/scala/org/apache/spark/mllib/recommendation/ALS.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/recommendation/ALS.scala
@@ -255,7 +255,7 @@ class ALS private (
           rank, lambda, alpha, YtY)
         previousProducts.unpersist()
         logInfo("Re-computing U given I (Iteration %d/%d)".format(iter, iterations))
-        if (sc.checkpointDir.isDefined && (iter % 3 == 1)) {
+        if (sc.checkpointDir.isDefined && (iter % 3 == 0)) {
           products.checkpoint()
         }
         products.setName(s"products-$iter").persist()
@@ -271,7 +271,7 @@ class ALS private (
         logInfo("Re-computing I given U (Iteration %d/%d)".format(iter, iterations))
         products = updateFeatures(numProductBlocks, users, userOutLinks, productInLinks,
           rank, lambda, alpha, YtY = None)
-        if (sc.checkpointDir.isDefined && (iter % 3 == 1)) {
+        if (sc.checkpointDir.isDefined && (iter % 3 == 0)) {
           products.checkpoint()
         }
         products.setName(s"products-$iter")
