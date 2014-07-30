bd841f9e3252404e29d593d7f7da5005e7f0b624
Matei Zaharia <matei@databricks.com>
2014-07-30 12:40:00 -0700

    Various review comments

--- core/src/main/scala/org/apache/spark/shuffle/hash/HashShuffleWriter.scala
+++ core/src/main/scala/org/apache/spark/shuffle/hash/HashShuffleWriter.scala
@@ -33,6 +33,10 @@ private[spark] class HashShuffleWriter[K, V](
   private val dep = handle.dependency
   private val numOutputSplits = dep.partitioner.numPartitions
   private val metrics = context.taskMetrics
+
+  // Are we in the process of stopping? Because map tasks can call stop() with success = true
+  // and then call stop() with success = false if they get an exception, we want to make sure
+  // we don't try deleting files, etc twice.
   private var stopping = false
 
   private val blockManager = SparkEnv.get.blockManager
