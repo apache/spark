7393b4d85cfb623602ea45b3ddcfdd50497157d0
Michael Armbrust <michael@databricks.com>
2014-07-29 22:42:54 -0700

    [SQL] Handle null values in debug()
    Author: Michael Armbrust <michael@databricks.com>
    
    Closes #1646 from marmbrus/nullDebug and squashes the following commits:
    
    49050a8 [Michael Armbrust] Handle null values in debug()

--- sql/core/src/main/scala/org/apache/spark/sql/execution/debug/package.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/debug/package.scala
@@ -105,7 +105,9 @@ package object debug {
             var i = 0
             while (i < numColumns) {
               val value = currentRow(i)
-              columnStats(i).elementTypes += HashSet(value.getClass.getName)
+              if (value != null) {
+                columnStats(i).elementTypes += HashSet(value.getClass.getName)
+              }
               i += 1
             }
             currentRow
