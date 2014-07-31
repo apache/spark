6bb8372b6637d61691a9254ae367a27e41b98e2c
joyyoj <sunshch@gmail.com>
2014-07-31 23:26:15 +0800

    SparkPushSink

---
+++ external/flume-sink/src/main/java/org/apache/spark/flume/sink/SparkRpcClient.java
@@ -0,0 +1,349 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.flume.sink;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.FlumeException;
+import org.apache.flume.api.*;
+import org.apache.spark.flume.sink.utils.LogicalHostRouter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SparkRpcClient extends AbstractRpcClient implements RpcClient {
+    private static final Logger logger = LoggerFactory.getLogger(SparkRpcClient.class);
+    private static final String HOSTNAME_KEY = "hostname";
+    private static final String HOST_ROUTER_PATH = "router.path";
+    private static final String HOST_ROUTER_RETRY_TIMES = "router.retry.times";
+    private static final String HOST_ROUTER_RETRY_INTERVAL = "router.retry.interval";
+    private LogicalHostRouter router;
+    private Integer maxTries = 1;
+    private volatile boolean isActive = false;
+    private Properties configurationProperties;
+    private final ClientPool clientPool = new ClientPool();
+
+    private class ClientHandler {
+        private HostInfo hostInfo = null;
+        private Properties props = null;
+        private RpcClient client = null;
+        private volatile boolean isConnected = false;
+
+        public ClientHandler(HostInfo hostInfo, Properties props) {
+            this.hostInfo = hostInfo;
+            this.props = props;
+        }
+
+        public synchronized void close() {
+            if (isConnected) {
+                client.close();
+                isConnected = false;
+                client = null;
+                logger.info("closed client");
+            }
+        }
+
+        public synchronized RpcClient getClient() {
+            if (client == null) {
+                Properties props = new Properties();
+                props.putAll(configurationProperties);
+                props.put(RpcClientConfigurationConstants.CONFIG_CLIENT_TYPE,
+                        RpcClientFactory.ClientType.DEFAULT.name());
+                props.put(RpcClientConfigurationConstants.CONFIG_HOSTS,
+                        hostInfo.getReferenceName());
+                props.put(
+                        RpcClientConfigurationConstants.CONFIG_HOSTS_PREFIX
+                                + hostInfo.getReferenceName(),
+                        hostInfo.getHostName() + ":" + hostInfo.getPortNumber());
+                props.put(RpcClientConfigurationConstants.CONFIG_CLIENT_TYPE,
+                        RpcClientConfigurationConstants.DEFAULT_CLIENT_TYPE);
+                client = RpcClientFactory.getInstance(props);
+//                    client = new NettyAvroRpcClient();
+//                    ((NettyAvroRpcClient) client).configure(props);
+                isConnected = true;
+                logger.debug("create new RpcClient:" + hostInfo.getHostName() + ":" + hostInfo.getPortNumber());
+            }
+            return client;
+        }
+    }
+
+    private class ClientPool implements LogicalHostRouter.LogicalHostRouterListener {
+        private List<ClientHandler> clientHandlers = new CopyOnWriteArrayList<ClientHandler>();
+        private CountDownLatch emptyLatch = new CountDownLatch(1);
+        private AtomicInteger currentClientIndex = new AtomicInteger(0);
+
+        @Override
+        public void physicalHostAdded(String logicalHost, LogicalHostRouter.PhysicalHost physicalHost) {
+            logger.info("receive host added info " + physicalHost.toString());
+            HostInfo hostInfo = new HostInfo("h1", physicalHost.getIp(), physicalHost.getPort());
+            addHost(hostInfo);
+        }
+
+        @Override
+        public void physicalHostRemoved(String logicalHost, LogicalHostRouter.PhysicalHost physicalHost) {
+            logger.info("receive host removed info " + physicalHost.toString());
+            HostInfo hostInfo = new HostInfo("h1", physicalHost.getIp(), physicalHost.getPort());
+            removeHost(hostInfo);
+        }
+
+        private boolean isSameHost(HostInfo left, HostInfo right) {
+            return left.getHostName().equals(right.getHostName()) && left.getPortNumber() == right.getPortNumber();
+        }
+
+        public void addHost(HostInfo hostInfo) {
+            logger.info("add host " + hostInfo.getHostName() + ":" + hostInfo.getPortNumber());
+            for (ClientHandler handler : clientHandlers) {
+                if (isSameHost(handler.hostInfo, hostInfo)) {
+                    return;
+                }
+            }
+            clientHandlers.add(new ClientHandler(hostInfo, configurationProperties));
+            emptyLatch.countDown();
+            logger.info("host added");
+        }
+
+        public void removeHost(HostInfo hostInfo) {
+            for (ClientHandler handler : clientHandlers) {
+                if (isSameHost(handler.hostInfo, hostInfo)) {
+                    clientHandlers.remove(handler);
+                    return;
+                }
+            }
+        }
+
+        public ClientHandler getClientHandler() {
+            int index = currentClientIndex.getAndIncrement();
+            if (currentClientIndex.get() >= clientHandlers.size()) {
+                currentClientIndex.set(0);
+            }
+            if (index >= clientHandlers.size()) {
+                index = 0;
+            }
+            try {
+                emptyLatch.await();
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+            }
+            Preconditions.checkElementIndex(index, clientHandlers.size());
+            return clientHandlers.get(index);
+        }
+
+        public void close() {
+            FlumeException closeException = null;
+            int nSuccess = clientHandlers.size();
+            for (ClientHandler handler : clientHandlers) {
+                try {
+                    handler.close();
+                    ++nSuccess;
+                } catch (FlumeException e) {
+                    closeException = e;
+                }
+            }
+            clientHandlers.clear();
+            if (closeException != null) {
+                throw new FlumeException("Close Exception total: " + clientHandlers.size()
+                        + " success: " + nSuccess, closeException);
+            }
+        }
+    }
+
+    public SparkRpcClient(Properties props) {
+        this.configurationProperties = props;
+    }
+
+    private int getInteger(Properties prop, String key, int defaultValue) {
+        String value = prop.getProperty(key);
+        if (value != null && value.trim().length() > 0) {
+            try {
+                return Integer.parseInt(value.trim());
+            } catch (NumberFormatException e) {
+                logger.warn("invalid " + key + " is set, value: " + value);
+            }
+        }
+        return defaultValue;
+    }
+
+    //This function has to be synchronized to establish a happens-before
+    //relationship for different threads that access this object
+    //since shared data structures are created here.
+    private synchronized void configureHosts(Properties properties)
+            throws FlumeException {
+        if (isActive) {
+            logger.error("This client was already configured, " +
+                    "cannot reconfigure.");
+            throw new FlumeException("This client was already configured, " +
+                    "cannot reconfigure.");
+        }
+
+        String routerPath = properties.getProperty(HOST_ROUTER_PATH);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(routerPath), HOST_ROUTER_PATH + " is empty");
+        LogicalHostRouter.Conf conf =  LogicalHostRouter.Conf.fromRouterPath(routerPath);
+        String routerRetryTimes = properties.getProperty(HOST_ROUTER_RETRY_TIMES);
+        if (routerRetryTimes != null) {
+            conf.setRetryTimes(Integer.parseInt(routerRetryTimes));
+        }
+        String routerRetryInterval = properties.getProperty(HOST_ROUTER_RETRY_INTERVAL);
+        if (routerRetryInterval != null) {
+            conf.setRetryInterval(Integer.parseInt(routerRetryInterval));
+        }
+        if (this.router != null) {
+            this.router.stop();
+        }
+        try {
+            router = new LogicalHostRouter(conf);
+            router.start();
+            String logicalHost = properties.getProperty(HOSTNAME_KEY);
+            List<String> logicalHosts = new ArrayList<String>();
+            logicalHosts.add(logicalHost);
+            router.registerListener(clientPool, logicalHosts);
+            List<LogicalHostRouter.PhysicalHost> physicalHosts = router.getPhysicalHosts(logicalHost);
+            maxTries = Math.max(1, physicalHosts.size());
+            for (LogicalHostRouter.PhysicalHost host : physicalHosts) {
+                HostInfo hostInfo = new HostInfo("h1", host.getIp(), host.getPort());
+                clientPool.addHost(hostInfo);
+            }
+        } catch (IOException e) {
+            logger.error("failed to read hosts ", e);
+            throw new FlumeException("This client read hosts failed " + e.getMessage());
+        }
+        maxTries = getInteger(properties, RpcClientConfigurationConstants.CONFIG_MAX_ATTEMPTS, maxTries);
+        batchSize = getInteger(properties, RpcClientConfigurationConstants.CONFIG_BATCH_SIZE,
+                RpcClientConfigurationConstants.DEFAULT_BATCH_SIZE);
+        if (batchSize < 1) {
+            logger.warn("A batch-size less than 1 was specified: " + batchSize
+                    + ". Using default instead.");
+            batchSize = RpcClientConfigurationConstants.DEFAULT_BATCH_SIZE;
+        }
+        isActive = true;
+    }
+
+    /**
+     * Tries to append an event to the currently connected client. If it cannot
+     * send the event, it tries to send to next available host
+     *
+     * @param event The event to be appended.
+     * @throws org.apache.flume.EventDeliveryException
+     */
+    @Override
+    public void append(Event event) throws EventDeliveryException {
+        synchronized (this) {
+            if (!isActive) {
+                logger.error("Attempting to append to an already closed client.");
+                throw new EventDeliveryException(
+                        "Attempting to append to an already closed client.");
+            }
+        }
+        // Sit in an finite loop and try to append!
+        ClientHandler clientHandler = null;
+        for (int tries = 0; tries < maxTries; ++tries) {
+            try {
+                clientHandler = clientPool.getClientHandler();
+                clientHandler.getClient().append(event);
+                return;
+            } catch (EventDeliveryException e) {
+                // Could not send event through this client, try to pick another client.
+                logger.warn("Client failed. Exception follows: ", e);
+                clientHandler.close();
+            } catch (Exception e2) {
+                logger.error("Failed to send event: " + e2.getMessage());
+                if (clientHandler != null) {
+                    clientHandler.close();
+                }
+                throw new EventDeliveryException(
+                        "Failed to send event. Exception follows: ", e2);
+            }
+        }
+        logger.error("Tried many times, could not send event.");
+        throw new EventDeliveryException("Failed to send the event!");
+    }
+
+    /**
+     * Tries to append a list of events to the currently connected client. If it
+     * cannot send the event, it tries to send to next available host
+     *
+     * @param events The events to be appended.
+     * @throws EventDeliveryException
+     */
+    @Override
+    public void appendBatch(List<Event> events)
+            throws EventDeliveryException {
+        synchronized (this) {
+            if (!isActive) {
+                logger.error("Attempting to append to an already closed client.");
+                throw new EventDeliveryException(
+                        "Attempting to append to an already closed client!");
+            }
+        }
+        ClientHandler clientHandler = null;
+        for (int tries = 0; tries < maxTries; ++tries) {
+            try {
+                clientHandler = clientPool.getClientHandler();
+                clientHandler.getClient().appendBatch(events);
+                return;
+            } catch (EventDeliveryException e) {
+                // Could not send event through this client, try to pick another client.
+                logger.warn("Client failed. Exception follows: " + e.getMessage());
+                clientHandler.close();
+            } catch (Exception e1) {
+                logger.error("No clients active: " + e1.getMessage());
+                if (clientHandler != null) {
+                    clientHandler.close();
+                }
+                throw new EventDeliveryException("No clients currently active. " +
+                        "Exception follows: ", e1);
+            }
+        }
+        logger.error("Tried many times, could not send event.");
+        throw new EventDeliveryException("Failed to send the event!");
+    }
+
+    // Returns false if and only if this client has been closed explicitly.
+    // Should we check if any clients are active, if none are then return false?
+    // This method has to be lightweight, so not checking if hosts are active.
+    @Override
+    public synchronized boolean isActive() {
+        return isActive;
+    }
+
+    /**
+     * Close the connection. This function is safe to call over and over.
+     */
+    @Override
+    public synchronized void close() throws FlumeException {
+        clientPool.close();
+        this.router.unregisterListener(clientPool);
+        this.router.stop();
+        this.router = null;
+    }
+
+    @Override
+    public void configure(Properties properties) throws FlumeException {
+        configurationProperties = new Properties();
+        configurationProperties.putAll(properties);
+        configureHosts(configurationProperties);
+    }
+}
---
+++ external/flume-sink/src/main/java/org/apache/spark/flume/sink/utils/LogicalHostRouter.java
@@ -0,0 +1,303 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.flume.sink.utils;
+
+import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * LogicalHostRouter supplies a map between logical host and physical host.
+ * A physical host denotes a real socket address, which consists of one host and port. host:port
+ * A logical host consists of several physical hosts. [host1:port1, host2:port2, ...]
+ * user hold a logical host instead of a actual host:port.
+ */
+public final class LogicalHostRouter {
+    private static final Logger LOG = LoggerFactory.getLogger(LogicalHostRouter.class);
+    private final static int LISTENER_EXECUTORS_NUMBER = 2;
+
+    private final Conf conf;
+    private ExecutorService executors = Executors.newFixedThreadPool(LISTENER_EXECUTORS_NUMBER);
+    private List<LogicalHostRouterListener> listeners = new CopyOnWriteArrayList<LogicalHostRouterListener>();
+    private ZkProxy zkProxy = null;
+
+    public LogicalHostRouter(Conf conf) {
+        this.conf = conf;
+        this.zkProxy = ZkProxy.get(new ZkProxy.Conf(conf.zkAddress, conf.zkRetryTimes,
+                conf.zkRetryIntervalInMs));
+    }
+
+    public void start() throws IOException {
+        try {
+            LOG.info("begin to start logical host router");
+            zkProxy.start();
+        } catch (Exception e) {
+            LOG.error("failed to start zkProxy:" + e.getMessage());
+            throw new IOException("failed to start logical host router:" + e.getMessage());
+        }
+    }
+
+    public void stop() {
+        LOG.info("stop logical host router");
+        try {
+            zkProxy.stop();
+            this.executors.shutdown();
+            this.executors.awaitTermination(1000, TimeUnit.MILLISECONDS);
+            this.executors.shutdownNow();
+        } catch (Exception e) {
+            LOG.error("failed to stop " + e.getMessage());
+        }
+    }
+
+    // register a physical host to logical host
+    public void registerPhysicalHost(String logicalHost, PhysicalHost hostInfo) throws IOException {
+        String zkNodePath = getZkNodePath(logicalHost, hostInfo);
+        try {
+            zkProxy.create(zkNodePath, new byte[0], ZkProxy.ZkNodeMode.EPHEMERAL, true);
+        } catch (Exception e) {
+            throw new IOException("failed to register to " + zkNodePath, e);
+        }
+    }
+
+    // remove a physical host from logical host
+    public void unregisterPhysicalHost(String logicalHost, PhysicalHost hostInfo) throws
+            IOException {
+        String zkNodePath = getZkNodePath(logicalHost, hostInfo);
+        try {
+            zkProxy.delete(zkNodePath);
+        } catch (Exception e) {
+            throw new IOException("failed to unregister " + zkNodePath, e);
+        }
+    }
+
+    /*
+     * register a listener to watch physical hosts changes.
+     * @param watchedLogicalHosts logical hosts to be watched
+     *
+     * Notice: Currently, LogicalHostAdded event is not supported
+     */
+    public synchronized void registerListener(LogicalHostRouterListener listener, List<String> watchedLogicalHosts) throws IOException {
+        try {
+            for (String logicalHost : watchedLogicalHosts) {
+                PhysicalHostUpdateEventListener zkListener = new PhysicalHostUpdateEventListener();
+                String zkPath = getZkNodePath(logicalHost);
+                zkProxy.addEventListener(zkPath, zkListener);
+            }
+        } catch (Exception e) {
+            LOG.error("failed to registerListener:" + e.getMessage());
+            throw new IOException(e);
+        }
+        listeners.add(listener);
+    }
+
+    public void unregisterListener(LogicalHostRouterListener listener) {
+        listeners.remove(listener);
+    }
+
+    public List<String> getLogicalHosts() throws IOException {
+        String zkNodePath = conf.zkPath;
+        try {
+            return zkProxy.getChildren(zkNodePath);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    public List<PhysicalHost> getPhysicalHosts(String logicalHost) throws IOException {
+        String zkNodePath = getZkNodePath(logicalHost);
+        List<PhysicalHost> results = new ArrayList<PhysicalHost>();
+        try {
+            if (zkProxy.checkExists(zkNodePath)) {
+                List<String> children = zkProxy.getChildren(zkNodePath);
+                for (String child : children) {
+                    results.add(getPhysicalHost(child));
+                }
+            }
+        } catch (Exception e) {
+            throw new IOException("failed to get physical hosts from " + zkNodePath, e);
+        }
+        return results;
+    }
+
+    private void processPhysicalHostAddedEvent(String logicalHost, PhysicalHost physicalHost) {
+        for (LogicalHostRouterListener listener : listeners) {
+            listener.physicalHostAdded(logicalHost, physicalHost);
+        }
+    }
+
+    private void processPhysicalHostRemovedEvent(String logicalHost, PhysicalHost physicalHost) {
+        for (LogicalHostRouterListener listener : listeners) {
+            listener.physicalHostRemoved(logicalHost, physicalHost);
+        }
+    }
+
+    private PhysicalHost getPhysicalHost(String zkNodeName) {
+        String[] addr = zkNodeName.split(":");
+        Preconditions.checkState(addr.length == 2, addr + " is not in host:port format");
+        PhysicalHost host = new PhysicalHost(addr[0], Integer.parseInt(addr[1]));
+        return host;
+    }
+
+    private String getZkNodeName(PhysicalHost host) {
+        return host.ip + ":" + host.port;
+    }
+
+    private String getZkNodePath(String logicalHost) {
+        return conf.zkPath + "/" + logicalHost;
+    }
+
+    private String getZkNodePath(String logicalHost, PhysicalHost hostInfo) {
+        return getZkNodePath(logicalHost) + "/" + getZkNodeName(hostInfo);
+    }
+
+    /*
+    * Listen such event that a physical host is added or removed from a logical host
+    */
+    public static interface LogicalHostRouterListener {
+        void physicalHostAdded(final String logicalHost, final PhysicalHost hostInfo);
+
+        void physicalHostRemoved(final String logicalHost, final PhysicalHost hostInfo);
+    }
+
+    public static class PhysicalHost {
+        private String ip;
+        private int port;
+
+        public PhysicalHost(String ip, int port) {
+            this.ip = ip;
+            this.port = port;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        @Override
+        public String toString() {
+            return ip + ":" + port;
+        }
+    }
+
+    public static class Conf {
+        private String zkAddress;
+        private String zkPath;
+        private int zkRetryIntervalInMs = 1000;
+        private int zkRetryTimes = 3;
+
+        public Conf setZkAddress(String zkAddress) {
+            this.zkAddress = zkAddress;
+            return this;
+        }
+
+        public Conf setZkPath(String zkPath) {
+            this.zkPath = zkPath;
+            return this;
+        }
+
+        public Conf setRetryTimes(int retryTimes) {
+            this.zkRetryTimes = retryTimes;
+            return this;
+        }
+
+        public Conf setRetryInterval(int retryIntervalInMs) {
+            this.zkRetryIntervalInMs = retryIntervalInMs;
+            return this;
+        }
+
+        public static Conf fromRouterPath(String routerPath) {
+            int index = routerPath.indexOf("/");
+            return new Conf().setZkAddress(routerPath.substring(0,
+                    index)).setZkPath(routerPath.substring(index));
+        }
+    }
+
+    private class PhysicalHostUpdateEventListener implements ZkProxy.ChildrenEventListener {
+        @Override
+        public void childAddedEvent(String nodePath, byte[] data) {
+            final Info info = getInfo(nodePath);
+            if (info != null) {
+                executors.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPhysicalHostAddedEvent(info.logicalHost, info.physicalHost);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void childRemovedEvent(String nodePath, byte[] data) {
+            final Info info = getInfo(nodePath);
+            if (info != null) {
+                executors.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        processPhysicalHostRemovedEvent(info.logicalHost, info.physicalHost);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void childUpdatedEvent(String nodePath, byte[] data) {
+        }
+
+        // extract logicalhost and its physical hosts from zkNodePath
+        // eg. /path/to/logicalhost/host:port => {logicalhost, (host, port)}
+        private Info getInfo(String zkNodePath) {
+            int preIndex = zkNodePath.indexOf(conf.zkPath);
+            if (preIndex < 0) {
+                return null;
+            }
+            String[] parts = zkNodePath.substring(conf.zkPath.length() + 1).split("/");
+            if (parts.length == 2) {
+                Info info = new Info();
+                String[] hostInfo = parts[1].split(":");
+                if (hostInfo.length == 2) {
+                    try {
+                        info.logicalHost = parts[0];
+                        info.physicalHost = new PhysicalHost(hostInfo[0],
+                                Integer.parseInt(hostInfo[1]));
+                        return info;
+                    } catch (NumberFormatException e) {
+                        LOG.error(e.getMessage());
+                    }
+                }
+            }
+            return null;
+        }
+
+        private class Info {
+            String logicalHost;
+            PhysicalHost physicalHost;
+        }
+    }
+
+}
