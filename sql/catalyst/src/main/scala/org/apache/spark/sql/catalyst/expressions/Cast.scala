f319d2f04793b583ae947c26d6e3b542a2c8c90c
ChiaYung Su <chiayung@appier.com>
2014-08-01 23:15:31 +0800

    fixed length array cast to string

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Cast.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Cast.scala
@@ -42,12 +42,18 @@ case class Cast(child: Expression, dataType: DataType) extends UnaryExpression {
   // UDFToString
   private[this] def castToString: Any => Any = child.dataType match {
     case BinaryType => buildCast[Array[Byte]](_, new String(_, "UTF-8"))
+    case FixedLenBinaryType(_) => buildCast[Array[Byte]](_, new String(_, "UTF-8"))
     case TimestampType => buildCast[Timestamp](_, timestampToString)
     case _ => buildCast[Any](_, _.toString)
   }
 
   // BinaryConverter
   private[this] def castToBinary: Any => Any = child.dataType match {
+    case StringType => buildCast[String](_, _.getBytes("UTF-8"))
+  }
+
+  // FixedLenBinaryConverter
+  private[this] def castToFixedLenBinary(x:Int): Any => Any = child.dataType match {
     case StringType => buildCast[String](_, _.getBytes("UTF-8"))
   }
 
@@ -256,6 +262,7 @@ case class Cast(child: Expression, dataType: DataType) extends UnaryExpression {
     case FloatType => castToFloat
     case LongType => castToLong
     case DoubleType => castToDouble
+    case FixedLenBinaryType(_) => castToFixedLenBinary(dataType.asInstanceOf[FixedLenBinaryType].length)
   }
 
   override def eval(input: Row): Any = {
