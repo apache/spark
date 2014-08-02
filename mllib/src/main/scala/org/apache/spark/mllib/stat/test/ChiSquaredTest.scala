4e4e36199aa81d9d1628322c499e40556fbdc6ef
Doris Xin <doris.s.xin@gmail.com>
2014-08-01 19:15:57 -0700

    WIP

--- mllib/src/main/scala/org/apache/spark/mllib/stat/test/ChiSquaredTest.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/stat/test/ChiSquaredTest.scala
@@ -17,8 +17,10 @@
 
 package org.apache.spark.mllib.stat.test
 
+import cern.jet.stat.Probability.chiSquareComplemented
+
 import org.apache.spark.rdd.RDD
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.{DenseVector, Vector}
 
 private[stat] object ChiSquaredTest {
 
@@ -40,9 +42,42 @@ private[stat] object ChiSquaredTest {
     }
   }
 
-  private def chiSquaredPearson(expected: RDD[Double],
-      observed: RDD[Double]): ChiSquaredTestResult = ???
+  private def chiSquaredPearson(x: RDD[Double],
+      y: RDD[Double]): ChiSquaredTestResult = {
+    val mat: RDD[Vector] = x.zip(y).map { case (xi, yi) => new DenseVector(Array(xi, yi)) }
+    chiSquaredPearson(mat)
+  }
 
-  private def chiSquaredPearson(counts: RDD[Vector]): ChiSquaredTestResult = ???
+  private def chiSquaredPearson(counts: RDD[Vector]): ChiSquaredTestResult = {
+    val numCols = counts.first.size
+    val colSums = new Array[Double](numCols)
+    var result = (colSums, 0) // second value is for count of vectors in the RDD
 
+    // Make two passes over the RDD with the first pass for collecting column sums
+    // TODO check that the counts are all non-negative in this pass
+    counts.aggregate(result)(
+      (sums, vector) => ((sums._1, vector.toArray).zipped.map(_ + _), sums._2 + 1),    // seqOp
+      (sums1, sums2) => ((sums1._1, sums2._1).zipped.map(_ + _), sums1._2 + sums2._2)) // combOp
+
+    val total = colSums.sum
+
+    // Second pass to compute chi-squared statistic
+    val statistic = counts.aggregate(0.0)(rowStatistic(colSums, total), _ + _)
+    val df = (numCols - 1) * (result._2 - 1)
+    val pValue = chiSquareComplemented(statistic, df)
+
+    new ChiSquaredTestResult(pValue, Array(df), statistic, PEARSON)
+  }
+
+  // curried function to be used as seqOp in the aggregate operation to collect statistic
+  private def rowStatistic(colSums: Array[Double], total: Double) = {
+    (statistic: Double, vector: Vector) => {
+      val arr = vector.toArray
+      val rowSum = arr.sum
+      (arr, colSums).zipped.foldLeft(statistic) { case (stat, (observed, colSum)) =>
+        val expected = rowSum * colSum / total
+        stat + (observed - expected) * (observed - expected) / expected
+      }
+    }
+  }
 }
