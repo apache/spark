2152e24d64d6a07cf6c550c9f13ab0231596be98
Sarah Gerweck <sarah.a180@gmail.com>
2014-08-03 19:47:05 -0700

    Fix some bugs with spaces in directory name.
    Any time you use the directory name (`FWDIR`) it needs to be surrounded
    in quotes. If you're also using wildcards, you can safely put the quotes
    around just `$FWDIR`.
    
    Author: Sarah Gerweck <sarah.a180@gmail.com>
    
    Closes #1756 from sarahgerweck/folderSpaces and squashes the following commits:
    
    732629d [Sarah Gerweck] Fix some bugs with spaces in directory name.
    (cherry picked from commit 5507dd8e18fbb52d5e0c64a767103b2418cb09c6)
    
    Signed-off-by: Patrick Wendell <pwendell@gmail.com>

--- make-distribution.sh
+++ make-distribution.sh
@@ -168,22 +168,22 @@ mkdir -p "$DISTDIR/lib"
 echo "Spark $VERSION$GITREVSTRING built for Hadoop $SPARK_HADOOP_VERSION" > "$DISTDIR/RELEASE"
 
 # Copy jars
-cp $FWDIR/assembly/target/scala*/*assembly*hadoop*.jar "$DISTDIR/lib/"
-cp $FWDIR/examples/target/scala*/spark-examples*.jar "$DISTDIR/lib/"
+cp "$FWDIR"/assembly/target/scala*/*assembly*hadoop*.jar "$DISTDIR/lib/"
+cp "$FWDIR"/examples/target/scala*/spark-examples*.jar "$DISTDIR/lib/"
 
 # Copy example sources (needed for python and SQL)
 mkdir -p "$DISTDIR/examples/src/main"
-cp -r $FWDIR/examples/src/main "$DISTDIR/examples/src/"
+cp -r "$FWDIR"/examples/src/main "$DISTDIR/examples/src/"
 
 if [ "$SPARK_HIVE" == "true" ]; then
-  cp $FWDIR/lib_managed/jars/datanucleus*.jar "$DISTDIR/lib/"
+  cp "$FWDIR"/lib_managed/jars/datanucleus*.jar "$DISTDIR/lib/"
 fi
 
 # Copy license and ASF files
 cp "$FWDIR/LICENSE" "$DISTDIR"
 cp "$FWDIR/NOTICE" "$DISTDIR"
 
-if [ -e $FWDIR/CHANGES.txt ]; then
+if [ -e "$FWDIR"/CHANGES.txt ]; then
   cp "$FWDIR/CHANGES.txt" "$DISTDIR"
 fi
 
