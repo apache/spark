932372a7876c7ba0ad59e9ebc48a13efda84e80a
giwa <ugw.gi.world@gmail.com>
2014-08-03 23:27:56 -0700

    clean up dstream.py

--- python/pyspark/streaming/dstream.py
+++ python/pyspark/streaming/dstream.py
@@ -140,12 +140,6 @@ class DStream(object):
         def combineLocally(iterator):
             combiners = {}
             for x in iterator:
-
-                #TODO for count operation make sure count implementation
-                # This is different from what pyspark does
-                #if isinstance(x, int):
-                #    x = ("", x)
-
                 (k, v) = x
                 if k not in combiners:
                     combiners[k] = createCombiner(v)
