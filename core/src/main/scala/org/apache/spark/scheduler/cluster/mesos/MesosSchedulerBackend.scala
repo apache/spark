d80e45559db90227ee5d0fdd18eb1a33ce2ff30b
wangfei <wangfei1@huawei.com>
2014-08-07 08:56:41 +0800

    fix import

--- core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosSchedulerBackend.scala
@@ -28,7 +28,7 @@ import org.apache.mesos.{Scheduler => MScheduler}
 import org.apache.mesos._
 import org.apache.mesos.Protos.{TaskInfo => MesosTaskInfo, TaskState => MesosTaskState, _}
 
-import org.apache.spark.{SparkConf, Logging, SparkContext, SparkException}
+import org.apache.spark.{SparkConf, Logging, SparkContext, TaskState, SparkException}
 import org.apache.spark.scheduler.{ExecutorExited, ExecutorLossReason, SchedulerBackend, SlaveLost, TaskDescription, TaskSchedulerImpl, WorkerOffer}
 import org.apache.spark.util.Utils
 import org.apache.spark.deploy.Command
