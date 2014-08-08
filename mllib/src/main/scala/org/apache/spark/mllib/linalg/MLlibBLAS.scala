14e664595ce6adfa21a8085b8fd13a21b88a07d6
Xiangrui Meng <meng@databricks.com>
2014-08-07 19:21:07 -0700

    add ddot

--- mllib/src/main/scala/org/apache/spark/mllib/linalg/MLlibBLAS.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/linalg/MLlibBLAS.scala
@@ -14,6 +14,7 @@ private[mllib] object MLlibBLAS {
    * y += alpha * x
    */
   def daxpy(alpha: Double, x: Vector, y: Vector) {
+    require(x.size == y.size)
     y match {
       case dy: DenseVector =>
         x match {
@@ -34,17 +35,15 @@ private[mllib] object MLlibBLAS {
   /**
    * y += alpha * x
    */
-  def daxpy(alpha: Double, x: DenseVector, y: DenseVector) {
+  private def daxpy(alpha: Double, x: DenseVector, y: DenseVector) {
     val n = x.size
-    require(y.size == x.size)
     f2jBLAS.daxpy(n, alpha, x.values, 1, y.values, 1)
   }
 
   /**
    * y += alpha * x
    */
-  def daxpy(alpha: Double, x: SparseVector, y: DenseVector) {
-    require(x.size == y.size)
+  private def daxpy(alpha: Double, x: SparseVector, y: DenseVector) {
     val nnz = x.indices.size
     if (alpha == 1.0) {
       var k = 0
@@ -59,5 +58,70 @@ private[mllib] object MLlibBLAS {
         k += 1
       }
     }
+  }
+
+  /**
+   * dot(x, y)
+   */
+  def ddot(x: Vector, y: Vector): Double = {
+    require(x.size == y.size)
+    (x, y) match {
+      case (dx: DenseVector, dy: DenseVector) =>
+        ddot(dx, dy)
+      case (dx: DenseVector, sy: SparseVector) =>
+        ddot(sy, dx)
+      case (sx: SparseVector, dy: DenseVector) =>
+        ddot(sx, dy)
+      case (sx: SparseVector, sy: SparseVector) =>
+        ddot(sx, sy)
+      case _ =>
+        throw new IllegalArgumentException(s"ddot doesn't support (${x.getClass}, ${y.getClass}).")
+    }
+  }
+
+  /**
+   * dot(x, y)
+   */
+  private def ddot(x: DenseVector, y: DenseVector): Double = {
+    val n = x.size
+    f2jBLAS.ddot(n, x.values, 1, y.values, 1)
+  }
+
+  /**
+   * dot(x, y)
+   */
+  private def ddot(x: SparseVector, y: DenseVector): Double = {
+    val nnz = x.indices.size
+    var sum = 0.0
+    var k = 0
+    while (k < nnz) {
+      sum += x.values(k) * y.values(x.indices(k))
+      k += 1
+    }
+    sum
+  }
+
+  /**
+   * dot(x, y)
+   */
+  private def ddot(x: SparseVector, y: SparseVector): Double = {
+    var kx = 0
+    val nnzx = x.indices.size
+    var ky = 0
+    val nnzy = y.indices.size
+    var sum = 0.0
+    // y catching x
+    while (kx < nnzx && ky < nnzy) {
+      val ix = x.indices(kx)
+      while (ky < nnzy && y.indices(ky) < ix) {
+        ky += 1
+      }
+      if (ky < nnzy && y.indices(ky) == ix) {
+        sum += x.values(kx) * y.values(ky)
+        ky += 1
+      }
+      kx += 1
+    }
+    sum
   }
 }
--- mllib/src/test/scala/org/apache/spark/mllib/linalg/MLlibBLASSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/linalg/MLlibBLASSuite.scala
@@ -3,6 +3,7 @@ package org.apache.spark.mllib.linalg
 import org.scalatest.FunSuite
 
 import org.apache.spark.mllib.util.TestingUtils._
+import org.apache.spark.mllib.linalg.MLlibBLAS._
 
 class MLlibBLASSuite extends FunSuite {
 
@@ -14,21 +15,46 @@ class MLlibBLASSuite extends FunSuite {
     val expected = Vectors.dense(2.1, 1.0, -0.2)
 
     val dy1 = Vectors.dense(dy.clone())
-    MLlibBLAS.daxpy(alpha, sx, dy1)
+    daxpy(alpha, sx, dy1)
     assert(dy1 ~== expected absTol 1e-15)
 
     val dy2 = Vectors.dense(dy.clone())
-    MLlibBLAS.daxpy(alpha, dx, dy2)
+    daxpy(alpha, dx, dy2)
     assert(dy2 ~== expected absTol 1e-15)
 
     val sy = Vectors.sparse(4, Array(0, 1), Array(2.0, 1.0))
 
     intercept[IllegalArgumentException] {
-      MLlibBLAS.daxpy(alpha, sx, sy)
+      daxpy(alpha, sx, sy)
     }
 
     intercept[IllegalArgumentException] {
-      MLlibBLAS.daxpy(alpha, dx, sy)
+      daxpy(alpha, dx, sy)
     }
+
+    withClue("vector sizes must match") {
+      intercept[Exception] {
+        daxpy(alpha, sx, Vectors.dense(1.0, 2.0))
+      }
+    }
+  }
+
+  test("ddot") {
+    val sx = Vectors.sparse(3, Array(0, 2), Array(1.0, -2.0))
+    val dx = Vectors.dense(1.0, 0.0, -2.0)
+    val sy = Vectors.sparse(3, Array(0, 1), Array(2.0, 1.0))
+    val dy = Vectors.dense(2.0, 1.0, 0.0)
+
+    assert(ddot(sx, sy) ~== 2.0 absTol 1e-15)
+    assert(ddot(sy, sx) ~== 2.0 absTol 1e-15)
+    assert(ddot(sx, dy) ~== 2.0 absTol 1e-15)
+    assert(ddot(dy, sx) ~== 2.0 absTol 1e-15)
+    assert(ddot(dx, dy) ~== 2.0 absTol 1e-15)
+    assert(ddot(dy, dx) ~== 2.0 absTol 1e-15)
+
+    assert(ddot(sx, sx) ~== 5.0 absTol 1e-15)
+    assert(ddot(dx, dx) ~== 5.0 absTol 1e-15)
+    assert(ddot(sx, dx) ~== 5.0 absTol 1e-15)
+    assert(ddot(dx, dx) ~== 5.0 absTol 1e-15)
   }
 }
