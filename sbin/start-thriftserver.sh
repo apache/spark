516554df73b1055ef14f6aebabd794cbf6ccdb90
wangfei <wangfei_hello@126.com>
2014-08-12 10:23:16 +0800

    quote variables to fix this issue

--- sbin/start-thriftserver.sh
+++ sbin/start-thriftserver.sh
@@ -65,19 +65,14 @@ while (($#)); do
   case $1 in
     --hiveconf)
       ensure_arg_number $# 2
-      THRIFT_SERVER_ARGS+=($1); shift
-      THRIFT_SERVER_ARGS+=($1); shift
-      ;;
-      
-    --driver-java-options)
-      shift;
-      export SPARK_SUBMIT_OPTS=$1; shift
+      THRIFT_SERVER_ARGS+=("$1"); shift
+      THRIFT_SERVER_ARGS+=("$1"); shift
       ;;
 
     *)
-      SUBMISSION_ARGS+=($1); shift
+      SUBMISSION_ARGS+=("$1"); shift
       ;;
   esac
 done
 
-eval exec "$FWDIR"/bin/spark-submit --class $CLASS ${SUBMISSION_ARGS[*]} spark-internal ${THRIFT_SERVER_ARGS[*]}
+exec "$FWDIR"/bin/spark-submit --class $CLASS "${SUBMISSION_ARGS[@]}" spark-internal "${THRIFT_SERVER_ARGS[@]}"
