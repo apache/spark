c49b50cceeb95e43c1054d3c2b8eb4b214d6db93
Reynold Xin <rxin@apache.org>
2014-08-15 10:58:29 -0700

    Removed JavaSerializer change.

--- core/src/main/scala/org/apache/spark/serializer/JavaSerializer.scala
+++ core/src/main/scala/org/apache/spark/serializer/JavaSerializer.scala
@@ -63,7 +63,7 @@ extends DeserializationStream {
   def close() { objIn.close() }
 }
 
-private[spark] class JavaSerializerInstance(counterReset: Int, defaultClassLoader: ClassLoader)
+private[spark] class JavaSerializerInstance(counterReset: Int)
   extends SerializerInstance {
 
   override def serialize[T: ClassTag](t: T): ByteBuffer = {
@@ -91,7 +91,7 @@ private[spark] class JavaSerializerInstance(counterReset: Int, defaultClassLoade
   }
 
   override def deserializeStream(s: InputStream): DeserializationStream = {
-    new JavaDeserializationStream(s, defaultClassLoader)
+    new JavaDeserializationStream(s, Utils.getContextOrSparkClassLoader)
   }
 
   def deserializeStream(s: InputStream, loader: ClassLoader): DeserializationStream = {
@@ -111,10 +111,7 @@ private[spark] class JavaSerializerInstance(counterReset: Int, defaultClassLoade
 class JavaSerializer(conf: SparkConf) extends Serializer with Externalizable {
   private var counterReset = conf.getInt("spark.serializer.objectStreamReset", 100)
 
-  override def newInstance(): SerializerInstance = {
-    val classLoader = defaultClassLoader.getOrElse(Thread.currentThread.getContextClassLoader)
-    new JavaSerializerInstance(counterReset, classLoader)
-  }
+  override def newInstance(): SerializerInstance = new JavaSerializerInstance(counterReset)
 
   override def writeExternal(out: ObjectOutput) {
     out.writeInt(counterReset)
