f2c56c976bc6faa83b8357c80caad1f4839eb06d
Hari Shreedharan <hshreedharan@cloudera.com>
2014-08-14 23:14:45 -0700

    Update SparkSinkSuite.scala

--- external/flume-sink/src/test/scala/org/apache/spark/streaming/flume/sink/SparkSinkSuite.scala
+++ external/flume-sink/src/test/scala/org/apache/spark/streaming/flume/sink/SparkSinkSuite.scala
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.spark.streaming.flume.sink
 
 import java.net.InetSocketAddress
@@ -17,23 +33,6 @@ import org.apache.flume.event.EventBuilder
 import org.apache.spark.streaming.TestSuiteBase
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory
 
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 class SparkSinkSuite extends TestSuiteBase {
   val eventsPerBatch = 1000
   val channelCapacity = 5000
@@ -136,8 +135,7 @@ class SparkSinkSuite extends TestSuiteBase {
       future.onComplete {
         case Success(events) => assert(events.getEvents.size() === 1000)
           batchCounter.countDown()
-        case Failure(t) =>
-          batchCounter.countDown()
+        case Failure(t) => batchCounter.countDown()
           throw t
       }
     })
