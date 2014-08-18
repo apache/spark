44856654c81ceb92ef6380691027744d4bf76589
Hari Shreedharan <harishreedharan@gmail.com>
2014-08-17 19:50:31 -0700

    [HOTFIX][STREAMING] Allow the JVM/Netty to decide which port to bind to in Flume Polling Tests.
    Author: Hari Shreedharan <harishreedharan@gmail.com>
    
    Closes #1820 from harishreedharan/use-free-ports and squashes the following commits:
    
    b939067 [Hari Shreedharan] Remove unused import.
    67856a8 [Hari Shreedharan] Remove findFreePort.
    0ea51d1 [Hari Shreedharan] Make some changes to getPort to use map on the serverOpt.
    1fb0283 [Hari Shreedharan] Merge branch 'master' of https://github.com/apache/spark into use-free-ports
    b351651 [Hari Shreedharan] Allow Netty to choose port, and query it to decide the port to bind to. Leaving findFreePort as is, if other tests want to use it at some point.
    e6c9620 [Hari Shreedharan] Making sure the second sink uses the correct port.
    11c340d [Hari Shreedharan] Add info about race condition to scaladoc.
    e89d135 [Hari Shreedharan] Adding Scaladoc.
    6013bb0 [Hari Shreedharan] [STREAMING] Find free ports to use before attempting to create Flume Sink in Flume Polling Suite

--- external/flume-sink/src/main/scala/org/apache/spark/streaming/flume/sink/SparkSink.scala
+++ external/flume-sink/src/main/scala/org/apache/spark/streaming/flume/sink/SparkSink.scala
@@ -130,6 +130,14 @@ class SparkSink extends AbstractSink with Logging with Configurable {
     blockingLatch.await()
     Status.BACKOFF
   }
+
+  private[flume] def getPort(): Int = {
+    serverOpt
+      .map(_.getPort)
+      .getOrElse(
+        throw new RuntimeException("Server was not started!")
+      )
+  }
 }
 
 /**
