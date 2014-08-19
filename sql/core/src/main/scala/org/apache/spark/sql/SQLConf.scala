0cc05dd7f7f91484260345464c083e88d3eb33f9
wangfei <wangfei_hello@126.com>
2014-08-19 12:47:48 +0800

    add note for use synchronizedMap

--- sql/core/src/main/scala/org/apache/spark/sql/SQLConf.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SQLConf.scala
@@ -50,8 +50,9 @@ private[spark] object SQLConf {
 trait SQLConf {
   import SQLConf._
 
-  @transient protected[spark] val settings =
-    new java.util.concurrent.ConcurrentHashMap[String, String]
+  /** Only low degree of contention is expected for conf, thus NOT using ConcurrentHashMap. */
+  @transient protected[spark] val settings = java.util.Collections.synchronizedMap(
+    new java.util.HashMap[String, String]())
 
   /** ************************ Spark SQL Params/Hints ******************* */
   // TODO: refactor so that these hints accessors don't pollute the name space of SQLContext?
