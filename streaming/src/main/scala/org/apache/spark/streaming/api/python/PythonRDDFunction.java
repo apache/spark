93f7637bfde0ff9a611d6b5d975232575eeb8fc0
giwa <ugw.gi.world@gmail.com>
2014-08-20 19:34:20 -0700

    fixed explanaiton

--- streaming/src/main/scala/org/apache/spark/streaming/api/python/PythonRDDFunction.java
+++ streaming/src/main/scala/org/apache/spark/streaming/api/python/PythonRDDFunction.java
@@ -5,7 +5,7 @@ import org.apache.spark.streaming.Time;
 
 /*
  * Interface for py4j callback function.
- * This function is called by pyspark.streaming.dstream.DStream.foreachRDD .
+ * This interface is related to pyspark.streaming.dstream.DStream.foreachRDD .
  */
 public interface PythonRDDFunction {
   JavaRDD<byte[]> call(JavaRDD<byte[]> rdd, long time);
