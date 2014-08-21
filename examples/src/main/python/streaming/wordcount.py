f04882c9ad90b0af9cf0e1f8037e84df0c81f74f
giwa <ugw.gi.world@gmail.com>
2014-08-20 18:54:22 -0700

    clen up examples

--- examples/src/main/python/streaming/wordcount.py
+++ examples/src/main/python/streaming/wordcount.py
@@ -1,7 +1,5 @@
 import sys
-from operator import add
 
-from pyspark.conf import SparkConf
 from pyspark.streaming.context import StreamingContext
 from pyspark.streaming.duration import *
 
@@ -9,16 +7,14 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print >> sys.stderr, "Usage: wordcount <directory>"
         exit(-1)
-    conf = SparkConf()
-    conf.setAppName("PythonStreamingWordCount")
 
-    ssc = StreamingContext(conf=conf, duration=Seconds(1))
+    ssc = StreamingContext(appName="PythonStreamingWordCount", duration=Seconds(1))
 
     lines = ssc.textFileStream(sys.argv[1])
     words = lines.flatMap(lambda line: line.split(" "))
     mapped_words = words.map(lambda x: (x, 1))
-    count = mapped_words.reduceByKey(add)
-    
+    count = mapped_words.reduceByKey(lambda a, b: a+b)
     count.pyprint()
+
     ssc.start()
     ssc.awaitTermination()
