24f95db9766177b557ee42c5aeaad974c5c18d50
giwa <ugw.gi.world@gmail.com>
2014-08-20 18:54:22 -0700

    clen up examples

--- examples/src/main/python/streaming/network_wordcount.py
+++ examples/src/main/python/streaming/network_wordcount.py
@@ -1,7 +1,6 @@
 import sys
 from operator import add
 
-from pyspark.conf import SparkConf
 from pyspark.streaming.context import StreamingContext
 from pyspark.streaming.duration import *
 
@@ -9,15 +8,14 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print >> sys.stderr, "Usage: wordcount <hostname> <port>"
         exit(-1)
-    conf = SparkConf()
-    conf.setAppName("PythonStreamingNetworkWordCount")
-    ssc = StreamingContext(conf=conf, duration=Seconds(1))
+    ssc = StreamingContext(appName="PythonStreamingNetworkWordCount", 
+                           duration=Seconds(1))
 
     lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
     words = lines.flatMap(lambda line: line.split(" "))
     mapped_words = words.map(lambda word: (word, 1))
     count = mapped_words.reduceByKey(add)
-
     count.pyprint()
+
     ssc.start()
     ssc.awaitTermination()
