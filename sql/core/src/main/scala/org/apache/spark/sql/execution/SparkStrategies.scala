7112da8fe8d382a1180118f206db78f8e610d83f
Michael Armbrust <michael@databricks.com>
2014-08-23 16:19:10 -0700

    [SPARK-2554][SQL] CountDistinct partial aggregation and object allocation improvements
    Author: Michael Armbrust <michael@databricks.com>
    Author: Gregory Owen <greowen@gmail.com>
    
    Closes #1935 from marmbrus/countDistinctPartial and squashes the following commits:
    
    5c7848d [Michael Armbrust] turn off caching in the constructor
    8074a80 [Michael Armbrust] fix tests
    32d216f [Michael Armbrust] reynolds comments
    c122cca [Michael Armbrust] Address comments, add tests
    b2e8ef3 [Michael Armbrust] Merge remote-tracking branch 'origin/master' into countDistinctPartial
    fae38f4 [Michael Armbrust] Fix style
    fdca896 [Michael Armbrust] cleanup
    93d0f64 [Michael Armbrust] metastore concurrency fix.
    db44a30 [Michael Armbrust] JIT hax.
    3868f6c [Michael Armbrust] Merge pull request #9 from GregOwen/countDistinctPartial
    c9e67de [Gregory Owen] Made SpecificRow and types serializable by Kryo
    2b46c4b [Michael Armbrust] Merge remote-tracking branch 'origin/master' into countDistinctPartial
    8ff6402 [Michael Armbrust] Add specific row.
    58d15f1 [Michael Armbrust] disable codegen logging
    87d101d [Michael Armbrust] Fix isNullAt bug
    abee26d [Michael Armbrust] WIP
    27984d0 [Michael Armbrust] Merge remote-tracking branch 'origin/master' into countDistinctPartial
    57ae3b1 [Michael Armbrust] Fix order dependent test
    b3d0f64 [Michael Armbrust] Add golden files.
    c1f7114 [Michael Armbrust] Improve tests / fix serialization.
    f31b8ad [Michael Armbrust] more fixes
    38c7449 [Michael Armbrust] comments and style
    9153652 [Michael Armbrust] better toString
    d494598 [Michael Armbrust] Fix tests now that the planner is better
    41fbd1d [Michael Armbrust] Never try and create an empty hash set.
    050bb97 [Michael Armbrust] Skip no-arg constructors for kryo,
    bd08239 [Michael Armbrust] WIP
    213ada8 [Michael Armbrust] First draft of partially aggregated and code generated count distinct / max
    
    (cherry picked from commit 7e191fe29bb09a8560cd75d453c4f7f662dff406)
    Signed-off-by: Michael Armbrust <michael@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
@@ -23,6 +23,7 @@ import org.apache.spark.sql.catalyst.planning._
 import org.apache.spark.sql.catalyst.plans._
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.plans.physical._
+import org.apache.spark.sql.catalyst.types._
 import org.apache.spark.sql.columnar.{InMemoryRelation, InMemoryColumnarTableScan}
 import org.apache.spark.sql.parquet._
 
@@ -148,7 +149,10 @@ private[sql] abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
     }
 
     def canBeCodeGened(aggs: Seq[AggregateExpression]) = !aggs.exists {
-      case _: Sum | _: Count => false
+      case _: Sum | _: Count | _: Max | _: CombineSetsAndCount => false
+      // The generated set implementation is pretty limited ATM.
+      case CollectHashSet(exprs) if exprs.size == 1  &&
+           Seq(IntegerType, LongType).contains(exprs.head.dataType) => false
       case _ => true
     }
 
