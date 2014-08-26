eae017610f92cafb1f41b4c80bdc07247b270655
Davies Liu <davies.liu@gmail.com>
2014-08-25 17:05:37 -0700

    choose different disks from different processes and instances

--- python/pyspark/shuffle.py
+++ python/pyspark/shuffle.py
@@ -22,7 +22,7 @@ import shutil
 import warnings
 import gc
 import itertools
-import operator
+import random
 
 import pyspark.heapq3 as heapq
 from pyspark.serializers import BatchedSerializer, PickleSerializer
@@ -61,6 +61,10 @@ def _get_local_dirs(sub):
     """ Get all the directories """
     path = os.environ.get("SPARK_LOCAL_DIRS", "/tmp")
     dirs = path.split(",")
+    if len(dirs) > 1:
+        # different order in different processes and instances
+        rnd = random.Random(os.getpid() + id(dirs))
+        random.shuffle(dirs, rnd.random)
     return [os.path.join(d, "python", str(os.getpid()), sub) for d in dirs]
 
 
