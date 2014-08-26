0f947f1239831a6ed3b47af65816715999bbe57b
Andrew Or <andrewor14@gmail.com>
2014-08-25 23:36:09 -0700

    [SPARK-2886] Use more specific actor system name than "spark"
    As of #1777 we log the name of the actor system when it binds to a port. The current name "spark" is super general and does not convey any meaning. For instance, the following line is taken from my driver log after setting `spark.driver.port` to 5001.
    ```
    14/08/13 19:33:29 INFO Remoting: Remoting started; listening on addresses:
    [akka.tcp://sparkandrews-mbp:5001]
    14/08/13 19:33:29 INFO Remoting: Remoting now listens on addresses:
    [akka.tcp://sparkandrews-mbp:5001]
    14/08/06 13:40:05 INFO Utils: Successfully started service 'spark' on port 5001.
    ```
    This commit renames this to "sparkDriver" and "sparkExecutor". The goal of this unambitious PR is simply to make the logged information more explicit without introducing any change in functionality.
    
    Author: Andrew Or <andrewor14@gmail.com>
    
    Closes #1810 from andrewor14/service-name and squashes the following commits:
    
    8c459ed [Andrew Or] Use a common variable for driver/executor actor system names
    3a92843 [Andrew Or] Change actor name to sparkDriver and sparkExecutor
    921363e [Andrew Or] Merge branch 'master' of github.com:apache/spark into service-name
    c8c6a62 [Andrew Or] Do not include hyphens in actor name
    1c1b42e [Andrew Or] Avoid spaces in akka system name
    f644b55 [Andrew Or] Use more specific service name
    
    (cherry picked from commit b21ae5bbb9baa966f69303a30659aa8bbb2098da)
    Signed-off-by: Andrew Or <andrewor14@gmail.com>

--- core/src/main/scala/org/apache/spark/util/AkkaUtils.scala
+++ core/src/main/scala/org/apache/spark/util/AkkaUtils.scala
@@ -27,7 +27,7 @@ import akka.pattern.ask
 import com.typesafe.config.ConfigFactory
 import org.apache.log4j.{Level, Logger}
 
-import org.apache.spark.{SparkException, Logging, SecurityManager, SparkConf}
+import org.apache.spark.{Logging, SecurityManager, SparkConf, SparkEnv, SparkException}
 
 /**
  * Various utility classes for working with Akka.
@@ -192,10 +192,11 @@ private[spark] object AkkaUtils extends Logging {
   }
 
   def makeDriverRef(name: String, conf: SparkConf, actorSystem: ActorSystem): ActorRef = {
+    val driverActorSystemName = SparkEnv.driverActorSystemName
     val driverHost: String = conf.get("spark.driver.host", "localhost")
     val driverPort: Int = conf.getInt("spark.driver.port", 7077)
     Utils.checkHost(driverHost, "Expected hostname")
-    val url = s"akka.tcp://spark@$driverHost:$driverPort/user/$name"
+    val url = s"akka.tcp://$driverActorSystemName@$driverHost:$driverPort/user/$name"
     val timeout = AkkaUtils.lookupTimeout(conf)
     logInfo(s"Connecting to $name: $url")
     Await.result(actorSystem.actorSelection(url).resolveOne(timeout), timeout)
