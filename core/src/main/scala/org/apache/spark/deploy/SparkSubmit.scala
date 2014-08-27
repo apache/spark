17ec6fc4923ea1159fc90c2a4356fd438e0bc033
Andrew Or <andrewor14@gmail.com>
2014-08-26 22:39:23 -0700

    Fix tests

--- core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
@@ -173,7 +173,7 @@ object SparkSubmit {
       OptionAssigner(args.master, ALL_CLUSTER_MGRS, ALL_DEPLOY_MODES, sysProp = "spark.master"),
       OptionAssigner(args.name, ALL_CLUSTER_MGRS, ALL_DEPLOY_MODES, sysProp = "spark.app.name"),
       OptionAssigner(args.jars, ALL_CLUSTER_MGRS, ALL_DEPLOY_MODES, sysProp = "spark.jars"),
-      OptionAssigner(args.driverMemory, ALL_CLUSTER_MGRS, ALL_DEPLOY_MODES,
+      OptionAssigner(args.driverMemory, ALL_CLUSTER_MGRS, CLIENT,
         sysProp = "spark.driver.memory"),
       OptionAssigner(args.driverExtraClassPath, ALL_CLUSTER_MGRS, ALL_DEPLOY_MODES,
         sysProp = "spark.driver.extraClassPath"),
