ebc61ec514e441621a5301cfc52a7800aea933c9
wangfei <wangfei_hello@126.com>
2014-08-29 17:37:15 -0700

    [SPARK-3296][mllib] spark-example should be run-example in head notation of DenseKMeans and SparseNaiveBayes
    `./bin/spark-example`  should be `./bin/run-example` in DenseKMeans and SparseNaiveBayes
    
    Author: wangfei <wangfei_hello@126.com>
    
    Closes #2193 from scwf/run-example and squashes the following commits:
    
    207eb3a [wangfei] spark-example should be run-example
    27a8999 [wangfei] ./bin/spark-example should be ./bin/run-example

--- examples/src/main/scala/org/apache/spark/examples/mllib/DenseKMeans.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/DenseKMeans.scala
@@ -27,7 +27,7 @@ import org.apache.spark.mllib.linalg.Vectors
 /**
  * An example k-means app. Run with
  * {{{
- * ./bin/spark-example org.apache.spark.examples.mllib.DenseKMeans [options] <input>
+ * ./bin/run-example org.apache.spark.examples.mllib.DenseKMeans [options] <input>
  * }}}
  * If you use it as a template to create your own app, please use `spark-submit` to submit your app.
  */
