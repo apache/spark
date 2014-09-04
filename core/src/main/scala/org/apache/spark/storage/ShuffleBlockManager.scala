e5f77ae97bf78a2b57ef2e0e2ff8c9ed4bb8c50d
Liang-Chi Hsieh <viirya@gmail.com>
2014-09-03 17:04:53 -0700

    [SPARK-3345] Do correct parameters for ShuffleFileGroup
    In the method `newFileGroup` of class `FileShuffleBlockManager`, the parameters for creating new `ShuffleFileGroup` object is in wrong order.
    
    Because in current codes, the parameters `shuffleId` and `fileId` are not used. So it doesn't cause problem now. However it should be corrected for readability and avoid future problem.
    
    Author: Liang-Chi Hsieh <viirya@gmail.com>
    
    Closes #2235 from viirya/correct_shufflefilegroup_params and squashes the following commits:
    
    fe72567 [Liang-Chi Hsieh] Do correct parameters for ShuffleFileGroup.

--- core/src/main/scala/org/apache/spark/storage/ShuffleBlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/ShuffleBlockManager.scala
@@ -168,7 +168,7 @@ class ShuffleBlockManager(blockManager: BlockManager,
           val filename = physicalFileName(shuffleId, bucketId, fileId)
           blockManager.diskBlockManager.getFile(filename)
         }
-        val fileGroup = new ShuffleFileGroup(fileId, shuffleId, files)
+        val fileGroup = new ShuffleFileGroup(shuffleId, fileId, files)
         shuffleState.allFileGroups.add(fileGroup)
         fileGroup
       }
