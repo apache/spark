1972c695b3963f42615c02422717b0da26f4af1d
Alexander Ulanov <nashb@yandex.ru>
2014-09-05 18:11:21 +0400

    ANN test suite: learning XOR function

---
+++ mllib/src/test/scala/org/apache/spark/mllib/ann/ANNSuite.scala
@@ -0,0 +1,29 @@
+package org.apache.spark.mllib.ann
+
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.util.LocalSparkContext
+import org.scalatest.FunSuite
+
+class ANNSuite extends FunSuite with LocalSparkContext {
+  private val inputs = Array[Array[Double]](
+    Array[Double](0,0),
+    Array[Double](0,1),
+    Array[Double](1,0),
+    Array[Double](1,1)
+  )
+  private val outputs = Array[Double](0, 1, 1, 0)
+  private val inputSize = 2
+  private val hiddenSize = 5
+  private val outputSize = 1
+  test("ANN learns XOR function") {
+    val data = inputs.zip(outputs).map { case(features, label) =>
+      (Vectors.dense(features), Vectors.dense(Array(label)))}
+    val rddData = sc.parallelize(data, 2)
+    val ann = new ParallelANN(Array[Int](inputSize, hiddenSize, outputSize))
+    ann.optimizer.setNumIterations(2000).setStepSize(2.0)
+    val model = ann.train(rddData)
+    val predictionAndLabels = rddData.map { case(input, label) =>
+      (model.predictV(input)(0), label(0)) }.collect()
+    assert(predictionAndLabels.forall { case(p, l) => (math.round(p) - l) == 0 })
+  }
+}
