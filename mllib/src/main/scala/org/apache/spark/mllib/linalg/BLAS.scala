d2d5851d49f30fa7224887b841a0ed8dc3e494ac
Burak <brkyvz@gmail.com>
2014-09-05 13:34:24 -0700

    [SPARK-3418][MLlib] Sparse Matrix support and additional native BLAS operations added

--- mllib/src/main/scala/org/apache/spark/mllib/linalg/BLAS.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/linalg/BLAS.scala
@@ -18,6 +18,9 @@
 package org.apache.spark.mllib.linalg
 
 import com.github.fommil.netlib.{BLAS => NetlibBLAS, F2jBLAS}
+import com.github.fommil.netlib.BLAS.{getInstance => NativeBLAS}
+
+import org.apache.commons.lang.StringEscapeUtils.escapeJava
 
 /**
  * BLAS routines for MLlib's vectors and matrices.
@@ -25,6 +28,7 @@ import com.github.fommil.netlib.{BLAS => NetlibBLAS, F2jBLAS}
 private[mllib] object BLAS extends Serializable {
 
   @transient private var _f2jBLAS: NetlibBLAS = _
+  @transient private var _nativeBLAS: NetlibBLAS = _
 
   // For level-1 routines, we use Java implementation.
   private def f2jBLAS: NetlibBLAS = {
@@ -197,4 +201,442 @@ private[mllib] object BLAS extends Serializable {
         throw new IllegalArgumentException(s"scal doesn't support vector type ${x.getClass}.")
     }
   }
+
+  // For level-3 routines, we use the native BLAS.
+  private def nativeBLAS: NetlibBLAS = {
+    if (_nativeBLAS == null) {
+      _nativeBLAS = NativeBLAS
+    }
+    _nativeBLAS
+  }
+
+  /**
+   * C := alpha * A * B + beta * C
+   * @param transA specify whether to use matrix A, or the transpose of matrix A. Should be "N" or
+   *               "n" to use A, and "T" or "t" to use the transpose of A.
+   * @param transB specify whether to use matrix B, or the transpose of matrix B. Should be "N" or
+   *               "n" to use B, and "T" or "t" to use the transpose of B.
+   * @param alpha a scalar to scale the multiplication A * B.
+   * @param A the matrix A that will be left multiplied to B. Size of m x k.
+   * @param B the matrix B that will be left multiplied by A. Size of k x n.
+   * @param beta a scalar that can be used to scale matrix C.
+   * @param C the resulting matrix C. Size of m x n.
+   */
+  def gemm(
+            transA: String,
+            transB: String,
+            alpha: Double,
+            A: Matrix,
+            B: DenseMatrix,
+            beta: Double,
+            C: DenseMatrix) {
+
+    var mA: Int = A.numRows
+    var nB: Int = B.numCols
+    var kA: Int = A.numCols
+    var kB: Int = B.numRows
+    var transposeA: Boolean = false
+    var transposeB: Boolean = false
+    if (transA == "T" || transA=="t"){
+      mA = A.numCols
+      kA = A.numRows
+      transposeA = true
+    }
+    require(transA == "T" || transA == "t" || transA == "N" || transA == "n",
+      s"Invalid argument used for transA: $transA. " +
+        escapeJava("Must be \"N\", \"n\", \"T\", or \"t\""))
+    if (transB == "T" || transB=="t"){
+      nB = B.numRows
+      kB = B.numCols
+      transposeB = true
+    }
+    require(transB == "T" || transB == "t" || transB == "N" || transB == "n",
+      s"Invalid argument used for transB: $transB. " +
+        escapeJava("Must be \"N\", \"n\", \"T\", or \"t\""))
+
+    require(kA == kB, s"The columns of A don't match the rows of B. A: $kA, B: $kB")
+    require(mA == C.numRows, s"The rows of C don't match the rows of A. C: ${C.numRows}, A: $mA")
+    require(nB == C.numCols,
+      s"The columns of C don't match the columns of B. C: ${C.numCols}, A: $nB")
+
+    A match {
+      case sparse: SparseMatrix =>
+        gemm(transA, transB, alpha, sparse, B, beta, C, transposeA, transposeB, mA, kA, nB)
+      case dense: DenseMatrix =>
+        gemm(transA, transB, alpha, dense, B, beta, C, mA, kA, nB)
+      case _ =>
+        throw new IllegalArgumentException(s"gemm doesn't support matrix type ${A.getClass}.")
+    }
+  }
+
+  /**
+   * C := alpha * A * B + beta * C
+   *
+   * @param alpha a scalar to scale the multiplication A * B.
+   * @param A the matrix A that will be left multiplied to B. Size of m x k.
+   * @param B the matrix B that will be left multiplied by A. Size of k x n.
+   * @param beta a scalar that can be used to scale matrix C.
+   * @param C the resulting matrix C. Size of m x n.
+   */
+  def gemm(
+            alpha: Double,
+            A: Matrix,
+            B: DenseMatrix,
+            beta: Double,
+            C: DenseMatrix) {
+
+    gemm("N", "N", alpha, A, B, beta, C)
+  }
+
+  /**
+   * C := alpha * A * B
+   *
+   * @param transA specify whether to use matrix A, or the transpose of matrix A. Should be "N" or
+   *               "n" to use A, and "T" or "t" to use the transpose of A.
+   * @param transB specify whether to use matrix B, or the transpose of matrix B. Should be "N" or
+   *               "n" to use B, and "T" or "t" to use the transpose of B.
+   * @param alpha a scalar to scale the multiplication A * B.
+   * @param A the matrix A that will be left multiplied to B. Size of m x k.
+   * @param B the matrix B that will be left multiplied by A. Size of k x n.
+   *
+   * @return The resulting matrix C. Size of m x n.
+   */
+  def gemm(transA: String,
+            transB: String,
+            alpha: Double,
+            A: Matrix,
+            B: DenseMatrix) : DenseMatrix = {
+
+    var mA: Int = A.numRows
+    var nB: Int = B.numCols
+    var kA: Int = A.numCols
+    var kB: Int = B.numRows
+
+    if (transA == "T" || transA=="T"){
+      mA = A.numCols
+      kA = A.numRows
+    }
+    if (transB == "T" || transB=="T"){
+      nB = B.numRows
+      kB = B.numCols
+    }
+
+    val C: DenseMatrix = DenseMatrix.zeros(mA, nB)
+
+    gemm(transA, transB, alpha, A, B, 0.0, C)
+
+    C
+  }
+
+  /**
+   * C := alpha * A * B
+   *
+   * @param alpha a scalar to scale the multiplication A * B.
+   * @param A the matrix A that will be left multiplied to B. Size of m x k.
+   * @param B the matrix B that will be left multiplied by A. Size of k x n.
+   *
+   * @return The resulting matrix C. Size of m x n.
+   */
+  def gemm(
+     alpha: Double,
+     A: Matrix,
+     B: DenseMatrix) : DenseMatrix = {
+
+    gemm("N", "N", alpha, A, B)
+  }
+
+  /**
+   * C := alpha * A * B + beta * C
+   * For `DenseMatrix` A.
+   */
+  private def gemm(
+      transA: String,
+      transB: String,
+      alpha: Double,
+      A: DenseMatrix,
+      B: DenseMatrix,
+      beta: Double,
+      C: DenseMatrix,
+      mA: Int,
+      kA: Int,
+      nB: Int) {
+
+    nativeBLAS.dgemm(transA,transB, mA, nB, kA, alpha, A.toArray, A.numRows, B.toArray, B.numRows,
+      beta, C.toArray, C.numRows)
+  }
+
+  /**
+   * C := alpha * A * B + beta * C
+   * For `SparseMatrix` A.
+   */
+  private def gemm(
+            transA: String,
+            transB: String,
+            alpha: Double,
+            A: SparseMatrix,
+            B: DenseMatrix,
+            beta: Double,
+            C: DenseMatrix,
+            transposeA: Boolean,
+            transposeB: Boolean,
+            mA: Int,
+            kA: Int,
+            nB: Int) {
+
+
+    val Avals = A.toArray
+    val Arows = if (!transposeA) A.rowIndices else A.colIndices
+    val Acols = if (!transposeA) A.colIndices else A.rowIndices
+
+    // Slicing is easy in this case. This is the optimal multiplication setting for sparse matrices
+    if (transposeA){
+      var colCounter = 0
+      while (colCounter < nB){ // Tests showed that the outer loop being columns was faster
+        var rowCounter = 0
+        while (rowCounter < mA){
+          val indStart = Arows(rowCounter)
+          val indEnd = Arows(rowCounter + 1)
+          var elementCount = 0 // Loop over non-zero entries in column (actually the row indices,
+                               // since this is a transposed multiplication
+          var sum = 0.0
+          while(indStart + elementCount < indEnd){
+            val AcolIndex = Acols(indStart + elementCount)
+            val Bval = if (!transposeB) B(AcolIndex, colCounter) else B(colCounter, AcolIndex)
+            sum += Avals(indStart + elementCount) * Bval
+            elementCount += 1
+          }
+          C.update(rowCounter, colCounter, beta * C(rowCounter, colCounter) + sum)
+          rowCounter += 1
+        }
+        colCounter += 1
+      }
+    } else {
+
+      // Scale matrix first if `beta` is not equal to 0.0
+      if (beta != 0.0){
+        var i = 0
+        val CLength = C.numCols * C.numRows
+        while ( i < CLength) {
+          C.values(i) *= beta
+          i += 1
+        }
+      }
+
+      // Perform matrix multiplication and add to C. The rows of A are multiplied by the columns of
+      // B, and added to C.
+      var colCounterForA = 0 // The column of A to multiply with the row of B
+      while (colCounterForA < kA){
+        val indStart = Acols(colCounterForA)
+        val indEnd = Acols(colCounterForA + 1)
+
+        var elementCount = 0 // Loop over non-zero entries in column
+        while (indStart + elementCount < indEnd){
+          var colCounterForB = 0 // the column to be updated in C
+          val rowIndex = Arows(indStart + elementCount) // the row to be updated in C
+          while (colCounterForB < nB){
+            val Bval =
+              if (!transposeB){
+                B(colCounterForA, colCounterForB)
+              } else {
+                B(colCounterForB, colCounterForA)
+              }
+
+            C.update(rowIndex, colCounterForB,
+              C(rowIndex, colCounterForB) + Avals(indStart + elementCount) * Bval)
+
+            colCounterForB += 1
+          }
+
+          elementCount += 1
+        }
+        colCounterForA += 1
+      }
+    }
+  }
+
+  /**
+   * y := alpha * A * x + beta * y
+   * @param trans specify whether to use matrix A, or the transpose of matrix A. Should be "N" or
+   *               "n" to use A, and "T" or "t" to use the transpose of A.
+   * @param alpha a scalar to scale the multiplication A * x.
+   * @param A the matrix A that will be left multiplied to x. Size of m x n.
+   * @param x the vector x that will be left multiplied by A. Size of n x 1.
+   * @param beta a scalar that can be used to scale vector y.
+   * @param y the resulting vector y. Size of m x 1.
+   */
+  def gemv(
+      trans: String,
+      alpha: Double,
+      A: Matrix,
+      x: DenseVector,
+      beta: Double,
+      y: DenseVector) {
+
+    var mA: Int = A.numRows
+    val nx: Int = x.size
+    var nA: Int = A.numCols
+    var transposeA: Boolean = false
+    if (trans == "T" || trans=="t"){
+      mA = A.numCols
+      nA = A.numRows
+      transposeA = true
+    }
+    require(trans == "T" || trans == "t" || trans == "N" || trans == "n",
+      s"Invalid argument used for trans: $trans. " +
+        escapeJava("Must be \"N\", \"n\", \"T\", or \"t\""))
+
+    require(nA == nx, s"The columns of A don't match the number of elements of x. A: $nA, x: $nx")
+    require(mA == y.size,
+      s"The rows of A don't match the number of elements of y. A: $mA, y:${y.size}}")
+
+    A match {
+      case sparse: SparseMatrix =>
+        gemv(trans, alpha, sparse, x, beta, y, mA, nA, transposeA)
+      case dense: DenseMatrix =>
+        gemv(trans, alpha, dense, x, beta, y)
+      case _ =>
+        throw new IllegalArgumentException(s"gemv doesn't support matrix type ${A.getClass}.")
+    }
+  }
+
+  /**
+   * y := alpha * A * x + beta * y
+   *
+   * @param alpha a scalar to scale the multiplication A * x.
+   * @param A the matrix A that will be left multiplied to x. Size of m x n.
+   * @param x the vector x that will be left multiplied by A. Size of n x 1.
+   * @param beta a scalar that can be used to scale vector y.
+   * @param y the resulting vector y. Size of m x 1.
+   */
+  def gemv(
+            alpha: Double,
+            A: Matrix,
+            x: DenseVector,
+            beta: Double,
+            y: DenseVector) {
+
+    gemv("N", alpha, A, x, beta, y)
+  }
+
+  /**
+   * y := alpha * A * x
+   *
+   * @param trans specify whether to use matrix A, or the transpose of matrix A. Should be "N" or
+   *               "n" to use A, and "T" or "t" to use the transpose of A.
+   * @param alpha a scalar to scale the multiplication A * x.
+   * @param A the matrix A that will be left multiplied to x. Size of m x n.
+   * @param x the vector x that will be left multiplied by A. Size of n x 1.
+   *
+   * @return `DenseVector` y, the result of the matrix-vector multiplication. Size of m x 1.
+   */
+  def gemv(
+            trans: String,
+            alpha: Double,
+            A: Matrix,
+            x: DenseVector): DenseVector = {
+
+    val m = if(trans == "N" || trans == "n") A.numRows else A.numCols
+
+    val y: DenseVector = new DenseVector(Array.fill(m)(0.0))
+    gemv(trans, alpha, A, x, 0.0, y)
+
+    y
+  }
+
+  /**
+   * y := alpha * A * x
+   *
+   * @param alpha a scalar to scale the multiplication A * x.
+   * @param A the matrix A that will be left multiplied to x. Size of m x n.
+   * @param x the vector x that will be left multiplied by A. Size of n x 1.
+   *
+   * @return `DenseVector` y, the result of the matrix-vector multiplication. Size of m x 1.
+   */
+  def gemv(
+            alpha: Double,
+            A: Matrix,
+            x: DenseVector): DenseVector = {
+
+    gemv("N", alpha, A, x)
+  }
+
+
+  /**
+   * y := alpha * A * x + beta * y
+   * For `DenseMatrix` A.
+   */
+  private def gemv(
+      trans: String,
+      alpha: Double,
+      A: DenseMatrix,
+      x: DenseVector,
+      beta: Double,
+      y: DenseVector) {
+
+    nativeBLAS.dgemv(trans, A.numRows, A.numCols, alpha, A.toArray, A.numRows, x.toArray, 1, beta,
+      y.toArray, 1)
+  }
+
+  /**
+   * y := alpha * A * x + beta * y
+   * For `SparseMatrix` A.
+   */
+  private def gemv(
+      trans: String,
+      alpha: Double,
+      A: SparseMatrix,
+      x: DenseVector,
+      beta: Double,
+      y: DenseVector,
+      mA: Int,
+      nA: Int,
+      transposeA: Boolean) {
+
+    val Avals = A.toArray
+    val Arows = if (!transposeA) A.rowIndices else A.colIndices
+    val Acols = if (!transposeA) A.colIndices else A.rowIndices
+
+    // Slicing is easy in this case. This is the optimal multiplication setting for sparse matrices
+    if (transposeA){
+      var rowCounter = 0
+      while (rowCounter < mA){
+        val indStart = Arows(rowCounter)
+        val indEnd = Arows(rowCounter + 1)
+        var elementCount = 0 // Number of elements already multiplied
+        var sum = 0.0
+        while(indStart + elementCount < indEnd){
+          sum += Avals(indStart + elementCount) * x.values(Acols(indStart + elementCount))
+          elementCount += 1
+        }
+        y.values(rowCounter) =  beta * y.values(rowCounter) + sum
+        rowCounter += 1
+      }
+    } else {
+      // Scale vector first if `beta` is not equal to 0.0
+      if (beta != 0.0){
+        var i = 0
+        val yLength = y.size
+        while (i < yLength) {
+          y.values(i) *= beta
+          i += 1
+        }
+      }
+
+      // Perform matrix-vector multiplication and add to y
+      var colCounterForA = 0
+      while (colCounterForA < nA){
+        val indStart = Acols(colCounterForA)
+        val indEnd = Acols(colCounterForA + 1)
+
+        var elementCount = 0 // Number of non-zero entries in column
+        while (indStart + elementCount < indEnd){
+          val rowIndex = Arows(indStart + elementCount)
+          y.values(rowIndex) += Avals(indStart + elementCount) * x.values(colCounterForA)
+          elementCount += 1
+        }
+        colCounterForA += 1
+      }
+    }
+  }
+
 }
