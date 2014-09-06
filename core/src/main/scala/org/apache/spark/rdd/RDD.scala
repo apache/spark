fd66905483c2295decec87f072bc9f60db3823fb
Andrew Ash <andrew@andrewash.com>
2014-09-05 18:52:05 -0700

    SPARK-3211 .take() is OOM-prone with empty partitions
    Instead of jumping straight from 1 partition to all partitions, do exponential
    growth and double the number of partitions to attempt each time instead.
    
    Fix proposed by Paul Nepywoda
    
    Author: Andrew Ash <andrew@andrewash.com>
    
    Closes #2117 from ash211/SPARK-3211 and squashes the following commits:
    
    8b2299a [Andrew Ash] Quadruple instead of double for a minor speedup
    e5f7e4d [Andrew Ash] Update comment to better reflect what we're doing
    09a27f7 [Andrew Ash] Update PySpark to be less OOM-prone as well
    3a156b8 [Andrew Ash] SPARK-3211 .take() is OOM-prone with empty partitions

--- core/src/main/scala/org/apache/spark/rdd/RDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/RDD.scala
@@ -1064,11 +1064,10 @@ abstract class RDD[T: ClassTag](
       // greater than totalParts because we actually cap it at totalParts in runJob.
       var numPartsToTry = 1
       if (partsScanned > 0) {
-        // If we didn't find any rows after the first iteration, just try all partitions next.
-        // Otherwise, interpolate the number of partitions we need to try, but overestimate it
-        // by 50%.
+        // If we didn't find any rows after the previous iteration, quadruple and retry.  Otherwise,
+        // interpolate the number of partitions we need to try, but overestimate it by 50%.
         if (buf.size == 0) {
-          numPartsToTry = totalParts - 1
+          numPartsToTry = partsScanned * 4
         } else {
           numPartsToTry = (1.5 * num * partsScanned / buf.size).toInt
         }
