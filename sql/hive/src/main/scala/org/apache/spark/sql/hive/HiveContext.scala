701dcd23fdf2b0cd0d959c41258bd4f212d52b50
Aaron Staple <aaron.staple@gmail.com>
2014-09-08 10:55:44 -0700

    [SPARK-2781][SQL] Check resolution of LogicalPlans in Analyzer.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
@@ -262,7 +262,9 @@ class HiveContext(sc: SparkContext) extends SQLContext(sc) {
   /* An analyzer that uses the Hive metastore. */
   @transient
   override protected[sql] lazy val analyzer =
-    new Analyzer(catalog, functionRegistry, caseSensitive = false)
+    new Analyzer(catalog, functionRegistry, caseSensitive = false) {
+      override val extendedRules = catalog.CreateTables :: catalog.PreInsertionCasts :: Nil
+    }
 
   /**
    * Runs the specified SQL query using Hive.
@@ -353,9 +355,8 @@ class HiveContext(sc: SparkContext) extends SQLContext(sc) {
 
   /** Extends QueryExecution with hive specific features. */
   protected[sql] abstract class QueryExecution extends super.QueryExecution {
-    // TODO: Create mixin for the analyzer instead of overriding things here.
-    override lazy val optimizedPlan =
-      optimizer(ExtractPythonUdfs(catalog.PreInsertionCasts(catalog.CreateTables(analyzed))))
+    // TODO: Utilize extendedRules in the analyzer instead of overriding things here.
+    override lazy val optimizedPlan = optimizer(ExtractPythonUdfs(analyzed))
 
     override lazy val toRdd: RDD[Row] = executedPlan.execute().map(_.copy())
 
