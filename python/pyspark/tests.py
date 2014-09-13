2aea0da84c58a179917311290083456dfa043db7
Davies Liu <davies.liu@gmail.com>
2014-09-13 16:22:04 -0700

    [SPARK-3030] [PySpark] Reuse Python worker
    Reuse Python worker to avoid the overhead of fork() Python process for each tasks. It also tracks the broadcasts for each worker, avoid sending repeated broadcasts.
    
    This can reduce the time for dummy task from 22ms to 13ms (-40%). It can help to reduce the latency for Spark Streaming.
    
    For a job with broadcast (43M after compress):
    ```
        b = sc.broadcast(set(range(30000000)))
        print sc.parallelize(range(24000), 100).filter(lambda x: x in b.value).count()
    ```
    It will finish in 281s without reused worker, and it will finish in 65s with reused worker(4 CPUs). After reusing the worker, it can save about 9 seconds for transfer and deserialize the broadcast for each tasks.
    
    It's enabled by default, could be disabled by `spark.python.worker.reuse = false`.
    
    Author: Davies Liu <davies.liu@gmail.com>
    
    Closes #2259 from davies/reuse-worker and squashes the following commits:
    
    f11f617 [Davies Liu] Merge branch 'master' into reuse-worker
    3939f20 [Davies Liu] fix bug in serializer in mllib
    cf1c55e [Davies Liu] address comments
    3133a60 [Davies Liu] fix accumulator with reused worker
    760ab1f [Davies Liu] do not reuse worker if there are any exceptions
    7abb224 [Davies Liu] refactor: sychronized with itself
    ac3206e [Davies Liu] renaming
    8911f44 [Davies Liu] synchronized getWorkerBroadcasts()
    6325fc1 [Davies Liu] bugfix: bid >= 0
    e0131a2 [Davies Liu] fix name of config
    583716e [Davies Liu] only reuse completed and not interrupted worker
    ace2917 [Davies Liu] kill python worker after timeout
    6123d0f [Davies Liu] track broadcasts for each worker
    8d2f08c [Davies Liu] reuse python worker

--- python/pyspark/tests.py
+++ python/pyspark/tests.py
@@ -1222,10 +1222,45 @@ class TestWorker(PySparkTestCase):
         except OSError:
             self.fail("daemon had been killed")
 
+        # run a normal job
+        rdd = self.sc.parallelize(range(100), 1)
+        self.assertEqual(100, rdd.map(str).count())
+
     def test_fd_leak(self):
         N = 1100  # fd limit is 1024 by default
         rdd = self.sc.parallelize(range(N), N)
         self.assertEquals(N, rdd.count())
+
+    def test_after_exception(self):
+        def raise_exception(_):
+            raise Exception()
+        rdd = self.sc.parallelize(range(100), 1)
+        self.assertRaises(Exception, lambda: rdd.foreach(raise_exception))
+        self.assertEqual(100, rdd.map(str).count())
+
+    def test_after_jvm_exception(self):
+        tempFile = tempfile.NamedTemporaryFile(delete=False)
+        tempFile.write("Hello World!")
+        tempFile.close()
+        data = self.sc.textFile(tempFile.name, 1)
+        filtered_data = data.filter(lambda x: True)
+        self.assertEqual(1, filtered_data.count())
+        os.unlink(tempFile.name)
+        self.assertRaises(Exception, lambda: filtered_data.count())
+
+        rdd = self.sc.parallelize(range(100), 1)
+        self.assertEqual(100, rdd.map(str).count())
+
+    def test_accumulator_when_reuse_worker(self):
+        from pyspark.accumulators import INT_ACCUMULATOR_PARAM
+        acc1 = self.sc.accumulator(0, INT_ACCUMULATOR_PARAM)
+        self.sc.parallelize(range(100), 20).foreach(lambda x: acc1.add(x))
+        self.assertEqual(sum(range(100)), acc1.value)
+
+        acc2 = self.sc.accumulator(0, INT_ACCUMULATOR_PARAM)
+        self.sc.parallelize(range(100), 20).foreach(lambda x: acc2.add(x))
+        self.assertEqual(sum(range(100)), acc2.value)
+        self.assertEqual(sum(range(100)), acc1.value)
 
 
 class TestSparkSubmit(unittest.TestCase):
