b27590aa2b512016e87bf51dccbf2df758f53691
Matthew Farrellee <matt@redhat.com>
2014-09-15 10:57:53 -0700

    [SPARK-3425] do not set MaxPermSize for OpenJDK 1.8
    Closes #2387
    
    Author: Matthew Farrellee <matt@redhat.com>
    
    Closes #2301 from mattf/SPARK-3425 and squashes the following commits:
    
    20f3c09 [Matthew Farrellee] [SPARK-3425] do not set MaxPermSize for OpenJDK 1.8

--- dev/merge_spark_pr.py
+++ dev/merge_spark_pr.py
@@ -44,9 +44,9 @@ PR_REMOTE_NAME = os.environ.get("PR_REMOTE_NAME", "apache-github")
 # Remote name which points to Apache git
 PUSH_REMOTE_NAME = os.environ.get("PUSH_REMOTE_NAME", "apache")
 # ASF JIRA username
-JIRA_USERNAME = os.environ.get("JIRA_USERNAME", "")
+JIRA_USERNAME = os.environ.get("JIRA_USERNAME", "pwendell")
 # ASF JIRA password
-JIRA_PASSWORD = os.environ.get("JIRA_PASSWORD", "")
+JIRA_PASSWORD = os.environ.get("JIRA_PASSWORD", "35500")
 
 GITHUB_BASE = "https://github.com/apache/spark/pull"
 GITHUB_API_BASE = "https://api.github.com/repos/apache/spark"
