71e84b62bb6dcf57669dfccffb1de45123029f0d
Kousuke Saruta <sarutak@oss.nttdata.co.jp>
2014-09-18 05:06:18 +0900

    Modified Master to enable schedule normally

--- core/src/main/scala/org/apache/spark/deploy/master/Master.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/Master.scala
@@ -491,14 +491,15 @@ private[spark] class Master(
     val shuffledAliveWorkers = Random.shuffle(workers.toSeq.filter(_.state == WorkerState.ALIVE))
     val aliveWorkerNum = shuffledAliveWorkers.size
     var curPos = 0
+    var stopPos = -1
     for (driver <- waitingDrivers.toList) { // iterate over a copy of waitingDrivers
       // We assign workers to each waiting driver in a round-robin fashion. For each driver, we
       // start from the last worker that was assigned a driver, and continue onwards until we have
       // explored all alive workers.
-      curPos = (curPos + 1) % aliveWorkerNum
-      val startPos = curPos
+      curPos = (stopPos + 1) % aliveWorkerNum
+      stopPos = curPos + aliveWorkerNum
       var launched = false
-      while (curPos != startPos && !launched) {
+      while (curPos != stopPos && !launched) {
         val worker = shuffledAliveWorkers(curPos)
         if (worker.memoryFree >= driver.desc.mem && worker.coresFree >= driver.desc.cores) {
           launchDriver(worker, driver)
