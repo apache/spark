edc2353eb4b70e94162db6732564ce52c36ac749
GuoQiang Li <witgo@qq.com>
2014-09-21 23:07:55 +0800

    In some cases, the RDD.checkpoint does not work

--- core/src/test/scala/org/apache/spark/CheckpointSuite.scala
+++ core/src/test/scala/org/apache/spark/CheckpointSuite.scala
@@ -56,6 +56,17 @@ class CheckpointSuite extends FunSuite with LocalSparkContext with Logging {
     assert(flatMappedRDD.collect() === result)
   }
 
+  test("After call count method, checkpoint should also work") {
+    val parCollection = sc.makeRDD(1 to 4)
+    val flatMappedRDD = parCollection.flatMap(x => 1 to x)
+    flatMappedRDD.count
+    flatMappedRDD.checkpoint()
+    assert(flatMappedRDD.dependencies.head.rdd == parCollection)
+    val result = flatMappedRDD.collect()
+    assert(flatMappedRDD.dependencies.head.rdd != parCollection)
+    assert(flatMappedRDD.collect() === result)
+  }
+
   test("RDDs with one-to-one dependencies") {
     testRDD(_.map(x => x.toString))
     testRDD(_.flatMap(x => 1 to x))
