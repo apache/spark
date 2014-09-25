43fd10a38e39f022ee90f996bf3657441a070e82
jyotiska <jyotiska123@gmail.com>
2014-09-25 12:58:56 +0530

    lines broken to maintain 80 char limit

--- examples/src/main/python/sql.py
+++ examples/src/main/python/sql.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     sqlContext = SQLContext(sc)
 
     # RDD is created from a list of rows
-    some_rdd = sc.parallelize([Row(name="John", age=19), Row(name="Smith", age=23), Row(name="Sarah", age=18)])
+    some_rdd = sc.parallelize([Row(name="John", age=19), Row(name="Smith", 
+        age=23), Row(name="Sarah", age=18)])
     # Infer schema from the first row, create a SchemaRDD and print the schema
     some_schemardd = sqlContext.inferSchema(some_rdd)
     some_schemardd.printSchema()
@@ -34,7 +35,8 @@ if __name__ == "__main__":
     # Another RDD is created from a list of tuples
     another_rdd = sc.parallelize([("John", 19), ("Smith", 23), ("Sarah", 18)])
     # Schema with two fields - person_name and person_age
-    schema = StructType([StructField("person_name", StringType(), False), StructField("person_age", IntegerType(), False)])
+    schema = StructType([StructField("person_name", StringType(), False), 
+        StructField("person_age", IntegerType(), False)])
     # Create a SchemaRDD by applying the schema to the RDD and print the schema
     another_schemardd = sqlContext.applySchema(another_rdd, schema)
     another_schemardd.printSchema()
