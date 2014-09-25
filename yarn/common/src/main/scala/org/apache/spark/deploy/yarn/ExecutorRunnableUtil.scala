5e001187eb18d1b7e45c00f4170723bd8bf97530
Marcelo Vanzin <vanzin@cloudera.com>
2014-09-24 23:10:26 -0700

    [SPARK-2778] [yarn] Add yarn integration tests.
    This patch adds a couple of, currently, very simple integration tests
    to make sure both client and cluster modes are working. The tests don't
    do much yet other than run a simple job, but the plan is to enhance
    them after we get the framework in.
    
    The cluster tests are noisy, so redirect all log output to a file
    like other tests do. Copying the conf around sucks but it's less
    work than messing with maven/sbt and having to clean up other
    projects.
    
    Note the test is only added for yarn-stable. The code compiles
    against yarn-alpha but there are two issues I ran into that I
    could not overcome:
    - an old netty dependency kept creeping into the classpath and
      causing akka to not work, when using sbt; the old netty was
      correctly suppressed under maven.
    - MiniYARNCluster kept failing to execute containers because it
      did not create the NM's local dir itself; this is apparently
      a known behavior, but I'm not sure how to work around it.
    
    None of those issues are present with the stable Yarn.
    
    Also, these tests are a little slow to run. Apparently Spark doesn't
    yet tag tests (so that these could be isolated in a "slow" batch),
    so this is something to keep in mind.
    
    Author: Marcelo Vanzin <vanzin@cloudera.com>
    
    Closes #2257 from vanzin/yarn-tests and squashes the following commits:
    
    6d5b84e [Marcelo Vanzin] Fix wrong system property being set.
    8b0933d [Marcelo Vanzin] Merge branch 'master' into yarn-tests
    5c2b56f [Marcelo Vanzin] Use custom log4j conf for Yarn containers.
    ec73f17 [Marcelo Vanzin] More review feedback.
    67f5b02 [Marcelo Vanzin] Review feedback.
    f01517c [Marcelo Vanzin] Review feedback.
    68fbbbf [Marcelo Vanzin] Use older constructor available in older Hadoop releases.
    d07ef9a [Marcelo Vanzin] Merge branch 'master' into yarn-tests
    add8416 [Marcelo Vanzin] [SPARK-2778] [yarn] Add yarn integration tests.

--- yarn/common/src/main/scala/org/apache/spark/deploy/yarn/ExecutorRunnableUtil.scala
+++ yarn/common/src/main/scala/org/apache/spark/deploy/yarn/ExecutorRunnableUtil.scala
@@ -98,7 +98,7 @@ trait ExecutorRunnableUtil extends Logging {
     */
 
     // For log4j configuration to reference
-    javaOpts += "-D=spark.yarn.app.container.log.dir=" + ApplicationConstants.LOG_DIR_EXPANSION_VAR
+    javaOpts += ("-Dspark.yarn.app.container.log.dir=" + ApplicationConstants.LOG_DIR_EXPANSION_VAR)
 
     val commands = Seq(Environment.JAVA_HOME.$() + "/bin/java",
       "-server",
