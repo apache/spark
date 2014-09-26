b13019a00f1ac38db2630cdfe3016e4ad4549ed0
Alexander Ulanov <nashb@yandex.ru>
2014-09-26 15:55:14 +0400

    Minor style fixes

--- mllib/src/test/scala/org/apache/spark/mllib/ann/ANNSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/ann/ANNSuite.scala
@@ -38,105 +38,82 @@ class ANNSuite extends FunSuite with LocalSparkContext {
       (Vectors.dense(features), Vectors.dense(Array(label)))}
     val rddData = sc.parallelize(data, 2)
     val hiddenLayersTopology = Array[Int](hiddenSize)
-    val model = ArtificialNeuralNetwork.train(rddData, hiddenLayersTopology, 100, 1e-5)
+    val model = ArtificialNeuralNetwork.train(rddData, hiddenLayersTopology, 500, 1e-5)
     val predictionAndLabels = rddData.map { case(input, label) =>
       (model.predict(input)(0), label(0)) }.collect()
     assert(predictionAndLabels.forall { case(p, l) => (math.round(p) - l) == 0 })
   }
 
   test("Gradient of ANN") {
-
     val eps = 1e-6
     val accept = 1e-7
-
     val topologyArr = Array[Array[Int]](
       Array[Int](1, 5, 1),
       Array[Int](5, 10, 5, 3),
       Array[Int](128, 256, 128)
     )
-
     val rnd = new XORShiftRandom(0)
-
     var cnt = 0
     while( cnt<topologyArr.length ) {
-
       val topology = topologyArr(cnt)
       val L = topology.length - 1
       val noInp = topology(0)
       val noOut = topology(L)
       val annGradient = new ANNLeastSquaresGradient(topology)
       var noWeights = 0
-
       var l = 1
       while(l <= L) {
         noWeights += (topology(l - 1) + 1) * topology(l)
         l += 1
       }
-
       val arrWeights = new Array[Double](noWeights)
-
       var w = 0
       while(w < noWeights) {
         arrWeights(w) = rnd.nextDouble()
         w += 1
       }
-
       val arrInp = new Array[Double](noInp)
       val arrOut = new Array[Double](noOut)
       val arrData = new Array[Double](noInp + noOut)
-
       w = 0
       while(w < noInp) {
         arrInp(w) = rnd.nextDouble()
         arrData(w) = arrInp(w)
         w += 1
       }
-
       w = 0
       while(w < noOut) {
         arrOut(w) = rnd.nextDouble()
         arrData(noInp + w) = arrOut(w)
         w += 1
       }
-
       val data = Vectors.dense( arrData )
       val brzOut = Vectors.dense( arrOut ).toBreeze
       val weights = Vectors.dense( arrWeights )
       val gradient = annGradient.compute( data, 0.0, weights )._1
-
       val arrTmpWeights = new Array[Double]( noWeights )
       Array.copy(arrWeights, 0, arrTmpWeights, 0, noWeights )
       val tmpWeights = Vectors.dense( arrTmpWeights )
-
       w = 0
       while(w < noWeights)
       {
         arrTmpWeights(w) = arrTmpWeights(w) + eps
-
         val annModel1 = new ArtificialNeuralNetworkModel(weights, topology)
         val brzO1 = annModel1.predict(data).toBreeze
-
         val annModel2 = new ArtificialNeuralNetworkModel(tmpWeights, topology)
         val brzO2 = annModel2.predict(data).toBreeze
-
         val E1 = .5* (brzO1 - brzOut).dot(brzO1 - brzOut)
         val E2 = .5* (brzO2 - brzOut).dot(brzO2 - brzOut)
         val dEdW = ( E2 - E1 ) / eps
-
         val gradw = gradient(w)
         val err = dEdW - gradw
         assert(math.abs(err) < accept, 
       s"Difference between calculated and approximated gradient too large ($dEdW - $gradw = $err)"
         )
-
         arrTmpWeights(w) = arrTmpWeights(w) - eps
-
         w += 1
       }
-
       cnt += 1
     }
-
   }
-
 }
