9a576859ca757b44b609bf9b823df5d6b376a98b
Davies Liu <davies.liu@gmail.com>
2014-09-25 18:17:52 -0700

    fix python style

--- examples/src/main/python/streaming/network_wordcount.py
+++ examples/src/main/python/streaming/network_wordcount.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print >> sys.stderr, "Usage: wordcount <hostname> <port>"
         exit(-1)
-    ssc = StreamingContext(appName="PythonStreamingNetworkWordCount", 
+    ssc = StreamingContext(appName="PythonStreamingNetworkWordCount",
                            duration=Seconds(1))
 
     lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
     counts = lines.flatMap(lambda line: line.split(" "))\
                   .map(lambda word: (word, 1))\
-                  .reduceByKey(lambda a,b: a+b)
+                  .reduceByKey(lambda a, b: a+b)
     counts.pyprint()
 
     ssc.start()
