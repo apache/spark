2f645feb732a76640e2751a8f821dd8a2c076835
ravipesala <ravindra.pesala@huawei.com>
2014-10-01 23:53:21 -0700

    [SPARK-3371][SQL] Renaming a function expression with group by gives error
    The following code gives error.
    ```
    sqlContext.registerFunction("len", (s: String) => s.length)
    sqlContext.sql("select len(foo) as a, count(1) from t1 group by len(foo)").collect()
    ```
    Because SQl parser creates the aliases to the functions in grouping expressions with generated alias names. So if user gives the alias names to the functions inside projection then it does not match the generated alias name of grouping expression.
    This kind of queries are working in Hive.
    So the fix I have given that if user provides alias to the function in projection then don't generate alias in grouping expression,use the same alias.
    
    Author: ravipesala <ravindra.pesala@huawei.com>
    
    Closes #2511 from ravipesala/SPARK-3371 and squashes the following commits:
    
    9fb973f [ravipesala] Removed aliases to grouping expressions.
    f8ace79 [ravipesala] Fixed the testcase issue
    bad2fd0 [ravipesala] SPARK-3371 : Fixed Renaming a function expression with group by gives error

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -680,4 +680,9 @@ class SQLQuerySuite extends QueryTest with BeforeAndAfterAll {
       sql("SELECT CAST(TRUE AS STRING), CAST(FALSE AS STRING) FROM testData LIMIT 1"),
       ("true", "false") :: Nil)
   }
+  
+  test("SPARK-3371 Renaming a function expression with group by gives error") {
+    registerFunction("len", (s: String) => s.length)
+    checkAnswer(
+      sql("SELECT len(value) as temp FROM testData WHERE key = 1 group by len(value)"), 1)}    
 }
