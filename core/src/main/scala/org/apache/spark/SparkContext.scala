389090d33190acaaa72499d81cacce732ec34cf9
Kousuke Saruta <sarutak@oss.nttdata.co.jp>
2014-10-02 17:33:15 +0900

    Replaced taskScheduler.applicationId() with getApplicationId in SparkContext#postApplicationStart

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -1303,7 +1303,7 @@ class SparkContext(config: SparkConf) extends Logging {
   private def postApplicationStart() {
     // Note: this code assumes that the task scheduler has been initialized and has contacted
     // the cluster manager to get an application ID (in case the cluster manager provides one).
-    listenerBus.post(SparkListenerApplicationStart(appName, Some(taskScheduler.applicationId()),
+    listenerBus.post(SparkListenerApplicationStart(appName, Some(getApplicationId()),
       startTime, sparkUser))
   }
 
