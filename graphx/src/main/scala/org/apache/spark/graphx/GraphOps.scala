aed16b755b521dbcf6480dd62c178594937e8239
Ankur Dave <ankurdave@gmail.com>
2014-10-06 10:37:16 -0600

    Add coarsen operator

--- graphx/src/main/scala/org/apache/spark/graphx/GraphOps.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/GraphOps.scala
@@ -272,6 +272,24 @@ class GraphOps[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED]) extends Seriali
   }
 
   /**
+   * Merge edges that satisfy an edge predicate and collapse vertices connected by merged edges.
+   */
+  def coarsen(pred: EdgeTriplet[VD, ED] => Boolean, reduce: (VD, VD) => VD): Graph[VD, ED] = {
+    // Restrict graph to contractable edges
+    val subG = graph.subgraph(epred = pred)
+    // Compute connected component id for each vertex
+    val cc = subG.connectedComponents.vertices.cache()
+    // Merge all vertices in the same component
+    val toMerge = graph.vertices.innerJoin(cc) { (id, attr, cc) => (cc, attr) }.map(_._2)
+    val superV = graph.vertices.aggregateUsingIndex(toMerge, reduce)
+    // Link remaining edges between components
+    val invG = graph.subgraph(epred = e => !pred(e))
+    val remainingE = invG.outerJoinVertices(cc) { (id, _, ccOpt) => ccOpt.get }
+      .triplets.map { e => Edge(e.srcAttr, e.dstAttr, e.attr) }
+    Graph(superV, remainingE)
+  }
+
+  /**
    * Execute a Pregel-like iterative vertex-parallel abstraction.  The
    * user-defined vertex-program `vprog` is executed in parallel on
    * each vertex receiving any inbound messages and computing a new
