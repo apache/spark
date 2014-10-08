25a3c127cff84a61cf31ecfdefed9b83cc18dc19
ssjssh <ssj970763342@163.com>
2014-10-08 23:38:11 +0800

    add first test

--- core/src/main/scala/own/HelloWorld.scala
+++ core/src/main/scala/own/HelloWorld.scala
@@ -3,6 +3,6 @@ package own
 /**
  * Created by shenshijun on 14-10-8.
  */
-class HelloWorld {
+class HelloWorld  {
 
 }
--- core/src/test/scala/ssj/HelloWorld.scala
+++ core/src/test/scala/ssj/HelloWorld.scala
@@ -1,8 +1,23 @@
 package ssj
 
+import org.apache.spark.{LocalSparkContext, SparkConf, SparkContext}
+import org.scalatest.FunSuite
+
 /**
+ * 这儿继承了LocalSparkContext，保存了一个SparkContext变量
  * Created by shenshijun on 14-10-8.
  */
-class HelloWorld {
+class HelloWorld extends FunSuite with LocalSparkContext{
+
+  override def beforeAll() {
+    super.beforeAll()
+    val conf = new SparkConf().setAppName("HelloWorld").setMaster("local")
+    sc = new SparkContext(conf)
+  }
+
+  test("Parallelized rdd") {
+    println("hello world")
+    assert(1 == 1)
+  }
 
 }
