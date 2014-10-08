f8ec44d3304c2c28c176257e6a75102c655ee71d
ssjssh <ssj970763342@163.com>
2014-10-08 23:30:13 +0800

    use test to explore spark

---
+++ core/src/main/scala/own/HelloWorld.scala
@@ -0,0 +1,8 @@
+package own
+
+/**
+ * Created by shenshijun on 14-10-8.
+ */
+class HelloWorld {
+
+}
---
+++ core/src/test/scala/ssj/HelloWorld.scala
@@ -0,0 +1,8 @@
+package ssj
+
+/**
+ * Created by shenshijun on 14-10-8.
+ */
+class HelloWorld {
+
+}
