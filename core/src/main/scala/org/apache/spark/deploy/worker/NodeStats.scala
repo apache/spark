ff544fd272bd158c191e3559f6288d7c261df75b
Dennis J. McWherter Jr <dennis@microsonic.org>
2014-10-09 08:55:14 -0500

    Updated code to be scalastyle compliant (i.e. Apache open source header).

--- core/src/main/scala/org/apache/spark/deploy/worker/NodeStats.scala
+++ core/src/main/scala/org/apache/spark/deploy/worker/NodeStats.scala
@@ -1,12 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author: Dennis J. McWherter, Jr. (dmcwherter@yahoo-inc.com)
+ */
+
 package org.apache.spark.deploy.worker
 
 import scala.io.Source._
-
-/**
- * Linux worker node statistic gathering class
- *
- * @author Dennis J. McWherter, Jr. (dmcwherter@yahoo-inc.com)
- */
 
 /**
  * Data type to hold all statistics
@@ -19,7 +34,7 @@ import scala.io.Source._
 case class Statistics(cpuspeed: List[Float], diskspeed: List[Float], maxMem: Long, availMem: Long)
 
 /**
- *
+ * Node stats
  */
 class NodeStats {
   /**
@@ -54,7 +69,7 @@ class NodeStats {
     // https://www.kernel.org/doc/Documentation/ABI/testing/procfs-diskstats
     val src   = fromFile("/proc/diskstats")
     val lines = src.mkString.split("\n")
-    src.close
+    src.close()
     lines.map(_.trim.split("\\s+")).filter {
       // Heuristic to determine active disk(s)
       e => "0".equals(e(1)) && !"0".equals(e(6)) && !"0".equals(e(10))
