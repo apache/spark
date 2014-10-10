02d05751ea281d377ce52ad39ccd30e518d2ff5a
Davies Liu <davies.liu@gmail.com>
2014-10-10 14:27:24 -0700

    add wrapper for foreachRDD()

--- python/pyspark/streaming/dstream.py
+++ python/pyspark/streaming/dstream.py
@@ -150,6 +150,9 @@ class DStream(object):
         """
         Apply a function to each RDD in this DStream.
         """
+        if func.func_code.co_argcount == 1:
+            old_func = func
+            func = lambda t, rdd: old_func(rdd)
         jfunc = TransformFunction(self._sc, func, self._jrdd_deserializer)
         api = self._ssc._jvm.PythonDStream
         api.callForeachRDD(self._jdstream, jfunc)
