bda1c721e1e691b77a0ba1fd4291278b6be42d6f
cocoatomo <cocoatomo77@gmail.com>
2014-10-11 11:26:17 -0700

    [SPARK-3867][PySpark] ./python/run-tests failed when it run with Python 2.6 and unittest2 is not installed
    ./python/run-tests search a Python 2.6 executable on PATH and use it if available.
    When using Python 2.6, it is going to import unittest2 module which is not a standard library in Python 2.6, so it fails with ImportError.
    
    Author: cocoatomo <cocoatomo77@gmail.com>
    
    Closes #2759 from cocoatomo/issues/3867-unittest2-import-error and squashes the following commits:
    
    f068eb5 [cocoatomo] [SPARK-3867] ./python/run-tests failed when it run with Python 2.6 and unittest2 is not installed

--- python/pyspark/mllib/tests.py
+++ python/pyspark/mllib/tests.py
@@ -23,7 +23,11 @@ import sys
 from numpy import array, array_equal
 
 if sys.version_info[:2] <= (2, 6):
-    import unittest2 as unittest
+    try:
+        import unittest2 as unittest
+    except ImportError:
+        sys.stderr.write('Please install unittest2 to test with Python 2.6 or earlier')
+        sys.exit(1)
 else:
     import unittest
 
