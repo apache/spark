c4a883f76c5cc3187adac4cd2e921fa3fe37ded8
Praveen Seluka <pseluka@qubole.com>
2014-10-13 16:37:42 +0200

    scalastyle and logging fixes

--- core/src/main/scala/org/apache/spark/autoscale/AutoscaleClient.scala
+++ core/src/main/scala/org/apache/spark/autoscale/AutoscaleClient.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.autoscale
 
 import org.apache.spark.SparkEnv
@@ -25,16 +42,12 @@ class AutoscaleClient(actorSystem: ActorSystem) extends Logging {
     }
   }
   def addExecutors(count: Int) {
-    if (server != null)
-      server ! AddExecutors(count)
-    else
-      logInfo("Autoscale Server not registered")
+    if (server != null) server ! AddExecutors(count)
+    else logInfo("Autoscale Server not registered")
   }
   def deleteExecutors(execIds: List[String]) {
-    if (server!= null)
-      server ! DeleteExecutors(execIds)
-    else
-      logInfo("Autoscale Server not registered")
+    if (server!= null) server ! DeleteExecutors(execIds)
+    else logInfo("Autoscale Server not registered")
   }
 }
 
--- yarn/common/src/main/scala/org/apache/spark/deploy/yarn/AutoscaleServer.scala
+++ yarn/common/src/main/scala/org/apache/spark/deploy/yarn/AutoscaleServer.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.deploy.yarn
 
 import org.apache.spark.SparkEnv
@@ -47,12 +64,12 @@ private[spark] class AutoscaleServer(allocator: YarnAllocator, sparkConf: SparkC
         logInfo(s"Driver terminated or disconnected! Shutting down. $x")
         //finish(FinalApplicationStatus.SUCCEEDED)
       case RegisteredAutoscaleServer =>
-        logInfo("Autoscaler registered successfully")
+        logInfo("Autoscale Server registered successfully")
       case AddExecutors(count: Int) =>
-        logInfo("Autoscaler : request to add " + count + "executors")
+        logInfo("Request to add " + count + "executors")
         allocator.addExecutors(count)
       case DeleteExecutors(execIds: List[String]) =>
-        logInfo("Autoscaler: request to delete executor : " + execIds)
+        logInfo("Request to delete executors : " + execIds)
         allocator.deleteExecutors(execIds)
     }
 
