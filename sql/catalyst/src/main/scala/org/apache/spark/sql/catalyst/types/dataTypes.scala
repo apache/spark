46db277cc14bf3c1e4c4779baa8a40189b332d89
Daoyuan Wang <daoyuan.wang@intel.com>
2014-10-13 13:31:14 -0700

    [SPARK-3892][SQL] remove redundant type name
    Author: Daoyuan Wang <daoyuan.wang@intel.com>
    
    Closes #2747 from adrian-wang/typename and squashes the following commits:
    
    2824216 [Daoyuan Wang] remove redundant typeName
    fbaf340 [Daoyuan Wang] typename

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/types/dataTypes.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/types/dataTypes.scala
@@ -349,7 +349,6 @@ case object FloatType extends FractionalType {
 object ArrayType {
   /** Construct a [[ArrayType]] object with the given element type. The `containsNull` is true. */
   def apply(elementType: DataType): ArrayType = ArrayType(elementType, true)
-  def typeName: String = "array"
 }
 
 /**
@@ -395,8 +394,6 @@ case class StructField(name: String, dataType: DataType, nullable: Boolean) {
 object StructType {
   protected[sql] def fromAttributes(attributes: Seq[Attribute]): StructType =
     StructType(attributes.map(a => StructField(a.name, a.dataType, a.nullable)))
-
-  def typeName = "struct"
 }
 
 case class StructType(fields: Seq[StructField]) extends DataType {
@@ -459,8 +456,6 @@ object MapType {
    */
   def apply(keyType: DataType, valueType: DataType): MapType =
     MapType(keyType: DataType, valueType: DataType, true)
-
-  def simpleName = "map"
 }
 
 /**
