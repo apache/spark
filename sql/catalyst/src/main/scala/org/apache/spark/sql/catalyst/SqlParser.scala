4957c3265a2ce4b9829a24dd986a466f2fec28ed
Kousuke Saruta <sarutak@oss.nttdata.co.jp>
2014-10-15 07:46:55 +0900

    Improved variable name in SqlParser.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/SqlParser.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/SqlParser.scala
@@ -142,7 +142,7 @@ class SqlParser extends AbstractSparkSQLParser {
       (LIMIT  ~> expression).? ^^ {
         case d ~ p ~ r ~ f ~ g ~ h ~ o ~ l  =>
           val base = r.getOrElse(NoRelation)
-          val withFilter = f.map(f => Filter(f, base)).getOrElse(base)
+          val withFilter = f.map(Filter(_, base)).getOrElse(base)
           val withProjection = g
             .map(Aggregate(_, assignAliases(p), withFilter))
             .getOrElse(Project(assignAliases(p), withFilter))
