18c6b1147e909afcfcc7f397dd1083f5103157f7
yantangzhai <tyz0303@163.com>
2014-10-16 19:25:37 -0700

    [SPARK-3067] JobProgressPage could not show Fair Scheduler Pools section sometimes
    JobProgressPage could not show Fair Scheduler Pools section sometimes.
    SparkContext starts webui and then postEnvironmentUpdate. Sometimes JobProgressPage is accessed between webui starting and postEnvironmentUpdate, then the lazy val isFairScheduler will be false. The Fair Scheduler Pools section will not display any more.
    
    Author: yantangzhai <tyz0303@163.com>
    Author: YanTangZhai <hakeemzhai@tencent.com>
    
    Closes #1966 from YanTangZhai/SPARK-3067 and squashes the following commits:
    
    d4323f8 [yantangzhai] update [SPARK-3067] JobProgressPage could not show Fair Scheduler Pools section sometimes
    8a00106 [YanTangZhai] Merge pull request #6 from apache/master
    b6391cc [yantangzhai] revert [SPARK-3067] JobProgressPage could not show Fair Scheduler Pools section sometimes
    d2226cd [yantangzhai] [SPARK-3067] JobProgressPage could not show Fair Scheduler Pools section sometimes
    cbcba66 [YanTangZhai] Merge pull request #3 from apache/master
    aac7f7b [yantangzhai] [SPARK-3067] JobProgressPage could not show Fair Scheduler Pools section sometimes
    cdef539 [YanTangZhai] Merge pull request #1 from apache/master

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -238,7 +238,6 @@ class SparkContext(config: SparkConf) extends Logging {
       // For tests, do not enable the UI
       None
     }
-  ui.foreach(_.bind())
 
   /** A default Hadoop Configuration for the Hadoop code (e.g. file systems) that we reuse. */
   val hadoopConfiguration = SparkHadoopUtil.get.newConfiguration(conf)
@@ -341,6 +340,10 @@ class SparkContext(config: SparkConf) extends Logging {
 
   postEnvironmentUpdate()
   postApplicationStart()
+
+  // Bind the SparkUI after starting the task scheduler
+  // because certain pages and listeners depend on it
+  ui.foreach(_.bind())
 
   private[spark] var checkpointDir: Option[String] = None
 
