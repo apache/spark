62047c477804d31dc31a55f2b1ee1bd7e71554ec
coderxiang <shuoxiangpub@gmail.com>
2014-10-20 08:20:59 -0700

    style change and add documentation

--- mllib/src/main/scala/org/apache/spark/mllib/evaluation/RankingMetrics.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/evaluation/RankingMetrics.scala
@@ -34,82 +34,103 @@ class RankingMetrics[T: ClassTag](predictionAndLabels: RDD[(Array[T], Array[T])]
 
   /**
    * Compute the average precision of all the queries, truncated at ranking position k.
+   *
    * If for a query, the ranking algorithm returns n (n < k) results,
    * the precision value will be computed as #(relevant items retrived) / k.
+   * This formula also applies when the size of the ground truth set is less than k.
+   *
+   * If a query has an empty ground truth set, one will be returned.
    * See the following paper for detail:
    *
    * IR evaluation methods for retrieving highly relevant documents.
    *    K. Jarvelin and J. Kekalainen
    *
-   * @param k the position to compute the truncated precision
+   * @param k the position to compute the truncated precision, must be positive
    * @return the average precision at the first k ranking positions
    */
-  def precisionAt(k: Int): Double = predictionAndLabels.map { case (pred, lab) =>
-    val labSet = lab.toSet
-    val n = math.min(pred.length, k)
-    var i = 0
-    var cnt = 0
+  def precisionAt(k: Int): Double = {
+    require (k > 0,"ranking position k should be positive")
+    predictionAndLabels.map { case (pred, lab) =>
+      val labSet = lab.toSet
+      val n = math.min(pred.length, k)
+      var i = 0
+      var cnt = 0
 
-    while (i < n) {
-      if (labSet.contains(pred(i))) {
-        cnt += 1
+      while (i < n) {
+        if (labSet.contains(pred(i))) {
+          cnt += 1
+        }
+        i += 1
       }
-      i += 1
-    }
-    cnt.toDouble / k
-  }.mean
+      if (labSet.size == 0) 1.0 else cnt.toDouble / k
+    }.mean
+  }
 
   /**
    * Returns the mean average precision (MAP) of all the queries
    */
-  lazy val meanAveragePrecision: Double = predictionAndLabels.map { case (pred, lab) =>
-    val labSet = lab.toSet
-    var i = 0
-    var cnt = 0
-    var precSum = 0.0
-    val n = pred.length
+  lazy val meanAveragePrecision: Double = {
+    predictionAndLabels.map { case (pred, lab) =>
+      val labSet = lab.toSet
+      var i = 0
+      var cnt = 0
+      var precSum = 0.0
+      val n = pred.length
 
-    while (i < n) {
-      if (labSet.contains(pred(i))) {
-        cnt += 1
-        precSum += cnt.toDouble / (i + 1)
+      while (i < n) {
+        if (labSet.contains(pred(i))) {
+          cnt += 1
+          precSum += cnt.toDouble / (i + 1)
+        }
+        i += 1
       }
-      i += 1
-    }
-    precSum / labSet.size
-  }.mean
+      precSum / labSet.size
+    }.mean
+  }
 
   /**
    * Compute the average NDCG value of all the queries, truncated at ranking position k.
+   * The discounted cumulative gain at position k is computed as:
+   *    \sum_{i=1}^k (2^{relevance of ith item} - 1) / log(i + 1),
+   * and the NDCG is obtained by dividing the DCG value on the ground truth set. In the
+   * current implementation, the relevance value is binary.
+   *
    * If for a query, the ranking algorithm returns n (n < k) results, the NDCG value at
-   * at position n will be used. See the following paper for detail:
+   * at position n will be used. If the ground truth set contains n (n < k) results,
+   * the first n items will be used to compute the DCG value on the ground truth set.
+   *
+   * If a query has an empty ground truth set, zero will be returned.
+   *
+   * See the following paper for detail:
    *
    * IR evaluation methods for retrieving highly relevant documents.
    *    K. Jarvelin and J. Kekalainen
    *
-   * @param k the position to compute the truncated ndcg
+   * @param k the position to compute the truncated ndcg, must be positive
    * @return the average ndcg at the first k ranking positions
    */
-  def ndcgAt(k: Int): Double = predictionAndLabels.map { case (pred, lab) =>
-    val labSet = lab.toSet
-    val labSetSize = labSet.size
-    val n = math.min(math.max(pred.length, labSetSize), k)
-    var maxDcg = 0.0
-    var dcg = 0.0
-    var i = 0
+  def ndcgAt(k: Int): Double = {
+    require (k > 0,"ranking position k should be positive")
+    predictionAndLabels.map { case (pred, lab) =>
+      val labSet = lab.toSet
+      val labSetSize = labSet.size
+      val n = math.min(math.max(pred.length, labSetSize), k)
+      var maxDcg = 0.0
+      var dcg = 0.0
+      var i = 0
 
-    while (i < n) {
-      // Calculate 1/log2(i + 2)
-      val gain = math.log(2) / math.log(i + 2)
-      if (labSet.contains(pred(i))) {
-        dcg += gain
+      while (i < n) {
+        val gain = 1.0 / math.log(i + 2)
+        if (labSet.contains(pred(i))) {
+          dcg += gain
+        }
+        if (i < labSetSize) {
+          maxDcg += gain
+        }
+        i += 1
       }
-      if (i < labSetSize) {
-        maxDcg += gain
-      }
-      i += 1
-    }
-    dcg / maxDcg
-  }.mean
+      if (labSetSize == 0) 0.0 else dcg / maxDcg
+    }.mean
+  }
 
 }
