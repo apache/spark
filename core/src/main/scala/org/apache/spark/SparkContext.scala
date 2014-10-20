9866fbfacf90f319dc1e318077f7d433e1bcb222
GuoQiang Li <witgo@qq.com>
2014-10-20 23:04:09 +0800

    Spark UI returns 500 in yarn-client mode

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -239,6 +239,8 @@ class SparkContext(config: SparkConf) extends Logging {
       None
     }
 
+  ui.foreach(_.bind())
+
   /** A default Hadoop Configuration for the Hadoop code (e.g. file systems) that we reuse. */
   val hadoopConfiguration = SparkHadoopUtil.get.newConfiguration(conf)
 
@@ -340,10 +342,6 @@ class SparkContext(config: SparkConf) extends Logging {
 
   postEnvironmentUpdate()
   postApplicationStart()
-
-  // Bind the SparkUI after starting the task scheduler
-  // because certain pages and listeners depend on it
-  ui.foreach(_.bind())
 
   private[spark] var checkpointDir: Option[String] = None
 
