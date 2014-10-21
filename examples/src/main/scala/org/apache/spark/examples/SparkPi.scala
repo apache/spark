9a2fb7b7d59e3dbe2193112dd328e521cab3a9d9
huangzhaowei <carlmartinmax@gmail.com>
2014-10-21 17:18:27 +0800

    Input of the SparkPi is too big
    if the Input of the SparkPi is too big, it will  throw new UnsupportedOperationException("empty collection"), because the n may be a negative int number.

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -27,7 +27,7 @@ object SparkPi {
     val conf = new SparkConf().setAppName("Spark Pi")
     val spark = new SparkContext(conf)
     val slices = if (args.length > 0) args(0).toInt else 2
-    val n = 100000 * slices
+    val n = if (100000.toLong * slices > Int.MaxValue) Int.MaxValue - 1 else 100000 * slices
     val count = spark.parallelize(1 to n, slices).map { i =>
       val x = random * 2 - 1
       val y = random * 2 - 1
