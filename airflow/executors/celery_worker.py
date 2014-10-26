e902e548a12856c6f773e8fbe641e68b1250d35e
Krishna Puttaswamy <krishna.puttaswamy@airbnb.com>
2014-10-25 20:35:35 -0700

    moving celery settings to settings.py

--- airflow/executors/celery_worker.py
+++ airflow/executors/celery_worker.py
@@ -3,10 +3,13 @@ import time
 import logging
 from celery import Celery
 
+from airflow import settings
+
 # to start the celery worker, run the command:
 # "celery -A airflow.executors.celery_worker worker --loglevel=info"
 
-app = Celery('airflow.executors.celery_worker', backend='amqp', broker='amqp://')
+# app = Celery('airflow.executors.celery_worker', backend='amqp', broker='amqp://')
+app = Celery(settings.CELERY_APP_NAME, backend=settings.CELERY_BROKER, broker=settings.CELERY_RESULTS_BACKEND)
 
 @app.task (name='airflow.executors.celery_worker.execute_command')
 def execute_command(command):
