759af7ac349579967ea5929f5b019097318c28ee
Joseph K. Bradley <joseph@databricks.com>
2014-10-27 14:25:50 -0700

    Added more doc to UserDefineType

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/types/dataTypes.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/types/dataTypes.scala
@@ -570,7 +570,17 @@ case class MapType(
 
 /**
  * ::DeveloperApi::
- * The data type for User Defined Types.
+ * The data type for User Defined Types (UDTs).
+ *
+ * This interface allows a user to make their own classes more interoperable with SparkSQL;
+ * e.g., by creating a [[UserDefinedType]] for a class X, it becomes possible to create a SchemaRDD
+ * which has class X in the schema.
+ *
+ * For SparkSQL to recognize UDTs, the UDT must be registered in
+ * [[org.apache.spark.sql.catalyst.UDTRegistry]].  This registration can be done either
+ * explicitly by calling [[org.apache.spark.sql.catalyst.UDTRegistry.registerType()]] before using
+ * the UDT with SparkSQL, or implicitly by annotating the UDT with
+ * [[org.apache.spark.sql.catalyst.annotation.SQLUserDefinedType]].
  */
 @DeveloperApi
 abstract class UserDefinedType[UserType] extends DataType with Serializable {
