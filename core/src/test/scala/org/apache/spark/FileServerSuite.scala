d82c05b05f38828e95a67ab1ac6691077bbda465
Josh Rosen <joshrosen@databricks.com>
2014-10-28 00:04:16 -0700

    [SPARK-4107] Fix incorrect handling of read() and skip() return values
    `read()` may return fewer bytes than requested; when this occurred, the old code would silently return less data than requested, which might cause stream corruption errors.  `skip()` faces similar issues, too.
    
    This patch fixes several cases where we mis-handle these methods' return values.
    
    Author: Josh Rosen <joshrosen@databricks.com>
    
    Closes #2969 from JoshRosen/file-channel-read-fix and squashes the following commits:
    
    e724a9f [Josh Rosen] Fix similar issue of not checking skip() return value.
    cbc03ce [Josh Rosen] Update the other log message, too.
    01e6015 [Josh Rosen] file.getName -> file.getAbsolutePath
    d961d95 [Josh Rosen] Fix another issue in FileServerSuite.
    b9265d2 [Josh Rosen] Fix a similar (minor) issue in TestUtils.
    cd9d76f [Josh Rosen] Fix a similar error in Tachyon:
    3db0008 [Josh Rosen] Fix a similar read() error in Utils.offsetBytes().
    db985ed [Josh Rosen] Fix unsafe usage of FileChannel.read():
    
    Conflicts:
    	core/src/main/scala/org/apache/spark/network/ManagedBuffer.scala
    	core/src/main/scala/org/apache/spark/shuffle/IndexShuffleBlockManager.scala
    	core/src/main/scala/org/apache/spark/storage/DiskStore.scala
    	core/src/test/scala/org/apache/spark/FileServerSuite.scala

--- core/src/test/scala/org/apache/spark/FileServerSuite.scala
+++ core/src/test/scala/org/apache/spark/FileServerSuite.scala
@@ -20,6 +20,7 @@ package org.apache.spark
 import java.io._
 import java.util.jar.{JarEntry, JarOutputStream}
 
+import com.google.common.io.ByteStreams
 import com.google.common.io.Files
 import org.scalatest.FunSuite
 
@@ -60,12 +61,7 @@ class FileServerSuite extends FunSuite with LocalSparkContext {
     jar.putNextEntry(jarEntry)
 
     val in = new FileInputStream(textFile)
-    val buffer = new Array[Byte](10240)
-    var nRead = 0
-    while (nRead <= 0) {
-      nRead = in.read(buffer, 0, buffer.length)
-      jar.write(buffer, 0, nRead)
-    }
+    ByteStreams.copy(in, jar)
 
     in.close()
     jar.close()
