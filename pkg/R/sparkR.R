b084314c15ead203b077174296d28f9ca10e799c
oscaroboto <oscarjr@gmail.com>
2014-10-30 08:38:14 -0400

    removed ... from example

--- pkg/R/sparkR.R
+++ pkg/R/sparkR.R
@@ -34,7 +34,7 @@ sparkR.onLoad <- function(libname, pkgname) {
 #' sc <- sparkR.init("yarn-client", "SparkR", "/home/spark",
 #'                  list(spark.executor.memory="1g"),
 #'                  List(LD_LIBRARY_PATH="/directory of Java VM Library Files (libjvm.so) on worker nodes/"),
-#'                  c("jarfile1.jar","jarfile2.jar",...))
+#'                  c("jarfile1.jar","jarfile2.jar"))
 #'}
 
 sparkR.init <- function(
