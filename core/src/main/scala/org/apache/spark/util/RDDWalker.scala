47ccc227e5bdf14a1db20edfcf1b8f9c77b3b64a
Ilya Ganelin <ilya.ganelin@capitalone.com>
2014-10-30 18:06:04 -0400

    Started walker code

--- core/src/main/scala/org/apache/spark/util/RDDWalker.scala
+++ core/src/main/scala/org/apache/spark/util/RDDWalker.scala
@@ -1,8 +1,23 @@
 package org.apache.spark.util
 
-/**
- * Created by zjb238 on 10/30/14.
- */
-class RDDWalker {
+import org.apache.spark.rdd.RDD
 
+import scala.reflect.ClassTag
+
+/**
+ * This class allows execution of a function on an RDD and all of its dependencies. This is accomplished by
+ * walking the object graph linking these RDDs. This is useful for debugging internal RDD references.
+ * @param rddToWalk - The RDD to traverse along with its dependencies
+ * @tparam T - The type of the RDD
+ */
+class RDDWalker[T : ClassTag] (rddToWalk : RDD[T]){
+  val rdd = rddToWalk
+
+  /**
+   * Execute the passed function on the underlying RDD
+   * @param func - The function to execute on
+   */
+  def walk(func : (RDD[T])=>Unit): Unit ={
+
+  }
 }
