b4b1f0c4b6d693a9ab4c0d9950c70201d81fc0c2
Andrew Or <andrew@databricks.com>
2014-11-03 15:30:44 -0800

    4 tabs -> 2 tabs

--- network/yarn/src/main/java/org/apache/spark/network/yarn/YarnShuffleService.java
+++ network/yarn/src/main/java/org/apache/spark/network/yarn/YarnShuffleService.java
@@ -26,61 +26,61 @@ import org.slf4j.LoggerFactory;
  * External shuffle service used by Spark on Yarn.
  */
 public class YarnShuffleService extends AuxiliaryService {
-    private final Logger logger = LoggerFactory.getLogger(YarnShuffleService.class);
-    private static final JobTokenSecretManager secretManager = new JobTokenSecretManager();
+  private final Logger logger = LoggerFactory.getLogger(YarnShuffleService.class);
+  private static final JobTokenSecretManager secretManager = new JobTokenSecretManager();
 
-    private static final String SPARK_SHUFFLE_SERVICE_PORT_KEY = "spark.shuffle.service.port";
-    private static final int DEFAULT_SPARK_SHUFFLE_SERVICE_PORT = 7337;
+  private static final String SPARK_SHUFFLE_SERVICE_PORT_KEY = "spark.shuffle.service.port";
+  private static final int DEFAULT_SPARK_SHUFFLE_SERVICE_PORT = 7337;
 
-    public YarnShuffleService() {
-        super("spark_shuffle");
-        logger.info("Initializing Yarn shuffle service for Spark");
+  public YarnShuffleService() {
+    super("spark_shuffle");
+    logger.info("Initializing Yarn shuffle service for Spark");
+  }
+
+  /**
+   * Start the shuffle server with the given configuration.
+   */
+  @Override
+  protected void serviceInit(Configuration conf) {
+    try {
+      int port = conf.getInt(
+        SPARK_SHUFFLE_SERVICE_PORT_KEY, DEFAULT_SPARK_SHUFFLE_SERVICE_PORT);
+      TransportConf transportConf = new TransportConf(new SystemPropertyConfigProvider());
+      RpcHandler rpcHandler = new ExternalShuffleBlockHandler();
+      TransportContext transportContext = new TransportContext(transportConf, rpcHandler);
+      transportContext.createServer(port);
+    } catch (Exception e) {
+      logger.error("Exception in starting Yarn shuffle service for Spark", e);
     }
+  }
 
-    /**
-     * Start the shuffle server with the given configuration.
-     */
-    @Override
-    protected void serviceInit(Configuration conf) {
-        try {
-            int port = conf.getInt(
-                SPARK_SHUFFLE_SERVICE_PORT_KEY, DEFAULT_SPARK_SHUFFLE_SERVICE_PORT);
-            TransportConf transportConf = new TransportConf(new SystemPropertyConfigProvider());
-            RpcHandler rpcHandler = new ExternalShuffleBlockHandler();
-            TransportContext transportContext = new TransportContext(transportConf, rpcHandler);
-            transportContext.createServer(port);
-        } catch (Exception e) {
-            logger.error("Exception in starting Yarn shuffle service for Spark!", e);
-        }
-    }
+  @Override
+  public void initializeApplication(ApplicationInitializationContext context) {
+    ApplicationId appId = context.getApplicationId();
+    logger.debug("Initializing application " + appId + "!");
+  }
 
-    @Override
-    public void initializeApplication(ApplicationInitializationContext context) {
-        ApplicationId appId = context.getApplicationId();
-        logger.debug("Initializing application " + appId + "!");
-    }
+  @Override
+  public void stopApplication(ApplicationTerminationContext context) {
+    ApplicationId appId = context.getApplicationId();
+    logger.debug("Stopping application " + appId + "!");
+  }
 
-    @Override
-    public void stopApplication(ApplicationTerminationContext context) {
-        ApplicationId appId = context.getApplicationId();
-        logger.debug("Stopping application " + appId + "!");
-    }
+  @Override
+  public ByteBuffer getMetaData() {
+    logger.debug("Getting meta data");
+    return ByteBuffer.allocate(0);
+  }
 
-    @Override
-    public ByteBuffer getMetaData() {
-        logger.debug("Getting meta data");
-        return ByteBuffer.allocate(0);
-    }
+  @Override
+  public void initializeContainer(ContainerInitializationContext context) {
+    ContainerId containerId = context.getContainerId();
+    logger.debug("Initializing container " + containerId + "!");
+  }
 
-    @Override
-    public void initializeContainer(ContainerInitializationContext context) {
-        ContainerId containerId = context.getContainerId();
-        logger.debug("Initializing container " + containerId + "!");
-    }
-
-    @Override
-    public void stopContainer(ContainerTerminationContext context) {
-        ContainerId containerId = context.getContainerId();
-        logger.debug("Stopping container " + containerId + "!");
-    }
+  @Override
+  public void stopContainer(ContainerTerminationContext context) {
+    ContainerId containerId = context.getContainerId();
+    logger.debug("Stopping container " + containerId + "!");
+  }
 }
