cbe451a9e2b5298640ed4f06a89dd5790e9e7de7
Aaron Davidson <aaron@databricks.com>
2014-11-05 00:02:05 -0800

    Address comments

--- network/shuffle/src/test/java/org/apache/spark/network/shuffle/ExternalShuffleSecuritySuite.java
+++ network/shuffle/src/test/java/org/apache/spark/network/shuffle/ExternalShuffleSecuritySuite.java
@@ -100,6 +100,7 @@ public class ExternalShuffleSecuritySuite {
     public String getSaslUser(String appId) {
       return "user";
     }
+
     @Override
     public String getSecretKey(String appId) {
       if (!appId.equals(this.appId)) {
