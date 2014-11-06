9988e4d68c92b2339039838cfa80d48c27b78c75
Maxime Beauchemin <maximebeauchemin@gmail.com>
2014-11-06 06:44:19 +0000

    Moving to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
-from distutils.core import setup
 from pip.req import parse_requirements
+from setuptools import setup
 
 # Reading the requirements from pip's requirements.txt
 reqs = (str(ir.req) for ir in parse_requirements('requirements.txt'))
@@ -10,12 +10,8 @@ setup(
     version='0.1',
     packages=[
         'airflow',
-        'airflow.operators',
-        'airflow.executors',
-        'airflow.hooks',
-        'airflow.www',
-        'dags',
     ],
+    include_package_data=True,
     scripts=['airflow/bin/airflow'],
     install_requires=reqs,
     author='Maxime Beauchemin',
