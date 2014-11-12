9140f9bde1b6ecaa1f9c143f4560becd968db9f6
Dana Klassen <dana.klassen@shopify.com>
2014-11-12 14:23:22 -0500

    add the apache header and fix the units

--- core/src/main/scala/org/apache/spark/metrics/sink/LogSink.scala
+++ core/src/main/scala/org/apache/spark/metrics/sink/LogSink.scala
@@ -1,3 +1,20 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.apache.spark.metrics.sink
 
 import java.io.File
@@ -28,7 +45,7 @@ private[spark] class LogSink(val property: Properties, val registry: MetricRegis
 
     val pollUnit: TimeUnit = Option(property.getProperty(LOG_KEY_UNIT)) match {
       case Some(s) => TimeUnit.valueOf(s.toUpperCase())
-      case None => TimeUnit.valueOf(LOG_KEY_UNIT)
+      case None => TimeUnit.valueOf(LOG_DEFAULT_UNIT)
     }
 
     MetricsSystem.checkMinimalPollingPeriod(pollUnit, pollPeriod)
