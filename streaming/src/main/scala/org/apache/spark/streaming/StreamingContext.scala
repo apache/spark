d726e854f7be010ef321e38bfa64d7f1abfaccd1
jerryshao <saisai.shao@intel.com>
2014-11-13 22:13:19 +0800

    Add local[1] to the filter condition

--- streaming/src/main/scala/org/apache/spark/streaming/StreamingContext.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/StreamingContext.scala
@@ -121,7 +121,7 @@ class StreamingContext private[streaming] (
     }
   }
 
-  if (sc.conf.get("spark.master") == "local") {
+  if (sc.conf.get("spark.master") == "local" || sc.conf.get("spark.master") == "local[1]") {
     logWarning("spark.master should be set as local[n], n > 1 in local mode if you have receivers" +
       " to get data, otherwise the job cannot get enough resource to execute.")
   }
