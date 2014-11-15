40eb8b6ef3a67e36d0d9492c044981a1da76351d
Josh Rosen <joshrosen@databricks.com>
2014-11-14 23:46:25 -0800

    [SPARK-2321] Several progress API improvements / refactorings
    This PR refactors / extends the status API introduced in #2696.
    
    - Change StatusAPI from a mixin trait to a class.  Before, the new status API methods were directly accessible through SparkContext, whereas now they're accessed through a `sc.statusAPI` field.  As long as we were going to add these methods directly to SparkContext, the mixin trait seemed like a good idea, but this might be simpler to reason about and may avoid pitfalls that I've run into while attempting to refactor other parts of SparkContext to use mixins (see #3071, for example).
    - Change the name from SparkStatusAPI to SparkStatusTracker.
    - Make `getJobIdsForGroup(null)` return ids for jobs that aren't associated with any job group.
    - Add `getActiveStageIds()` and `getActiveJobIds()` methods that return the ids of whatever's currently active in this SparkContext.  This should simplify davies's progress bar code.
    
    Author: Josh Rosen <joshrosen@databricks.com>
    
    Closes #3197 from JoshRosen/progress-api-improvements and squashes the following commits:
    
    30b0afa [Josh Rosen] Rename SparkStatusAPI to SparkStatusTracker.
    d1b08d8 [Josh Rosen] Add missing newlines
    2cc7353 [Josh Rosen] Add missing file.
    d5eab1f [Josh Rosen] Add getActive[Stage|Job]Ids() methods.
    a227984 [Josh Rosen] getJobIdsForGroup(null) should return jobs for default group
    c47e294 [Josh Rosen] Remove StatusAPI mixin trait.

--- core/src/main/scala/org/apache/spark/api/java/JavaSparkContext.scala
+++ core/src/main/scala/org/apache/spark/api/java/JavaSparkContext.scala
@@ -105,6 +105,8 @@ class JavaSparkContext(val sc: SparkContext)
 
   private[spark] val env = sc.env
 
+  def statusTracker = new JavaSparkStatusTracker(sc)
+
   def isLocal: java.lang.Boolean = sc.isLocal
 
   def sparkUser: String = sc.sparkUser
@@ -133,25 +135,6 @@ class JavaSparkContext(val sc: SparkContext)
 
   /** Default min number of partitions for Hadoop RDDs when not given by user */
   def defaultMinPartitions: java.lang.Integer = sc.defaultMinPartitions
-
-
-  /**
-   * Return a list of all known jobs in a particular job group.  The returned list may contain
-   * running, failed, and completed jobs, and may vary across invocations of this method.  This
-   * method does not guarantee the order of the elements in its result.
-   */
-  def getJobIdsForGroup(jobGroup: String): Array[Int] = sc.getJobIdsForGroup(jobGroup)
-
-  /**
-   * Returns job information, or `null` if the job info could not be found or was garbage collected.
-   */
-  def getJobInfo(jobId: Int): SparkJobInfo = sc.getJobInfo(jobId).orNull
-
-  /**
-   * Returns stage information, or `null` if the stage info could not be found or was
-   * garbage collected.
-   */
-  def getStageInfo(stageId: Int): SparkStageInfo = sc.getStageInfo(stageId).orNull
 
   /** Distribute a local Scala collection to form an RDD. */
   def parallelize[T](list: java.util.List[T], numSlices: Int): JavaRDD[T] = {
