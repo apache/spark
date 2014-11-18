626f72b32aadcfbe9323385b9984210199ef2693
w00228970 <wangfei1@huawei.com>
2014-11-18 21:19:34 +0800

    fix parquet string issue for hive

--- sql/hive/src/test/scala/org/apache/spark/sql/parquet/HiveParquetSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/parquet/HiveParquetSuite.scala
@@ -106,6 +106,12 @@ class HiveParquetSuite extends FunSuite with BeforeAndAfterAll with BeforeAndAft
     compareRDDs(rddOrig, rddCopy, "testsource", ParquetTestData.testSchemaFieldNames)
   }
 
+  test("String issue for Parquet Table") {
+    sql("CREATE TABLE parquet_string(key STRING)")
+    sql("INSERT OVERWRITE TABLE parquet_string SELECT mystring FROM testsource")
+    sql("SELECT * FROM parquet_string").collect.foreach(println)
+  }
+
   private def compareRDDs(rddOne: Array[Row], rddTwo: Array[Row], tableName: String, fieldNames: Seq[String]) {
     var counter = 0
     (rddOne, rddTwo).zipped.foreach {
