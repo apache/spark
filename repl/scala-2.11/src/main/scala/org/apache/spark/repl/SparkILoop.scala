4850093f559daaeac01ba403d6de8ec09a06cd0b
zsxwing <zsxwing@gmail.com>
2014-11-18 17:52:57 +0800

    Print "Spark context available as sc." only when SparkContext is created successfully

--- repl/scala-2.11/src/main/scala/org/apache/spark/repl/SparkILoop.scala
+++ repl/scala-2.11/src/main/scala/org/apache/spark/repl/SparkILoop.scala
@@ -61,11 +61,14 @@ class SparkILoop(in0: Option[BufferedReader], protected val out: JPrintWriter)
   def initializeSpark() {
     intp.beQuietDuring {
       command( """
-         @transient val sc = org.apache.spark.repl.Main.createSparkContext();
+         @transient val sc = {
+           val _sc = org.apache.spark.repl.Main.createSparkContext()
+           println("Spark context available as sc.")
+           _sc
+         }
                """)
       command("import org.apache.spark.SparkContext._")
     }
-    echo("Spark context available as sc.")
   }
 
   /** Print a welcome message */
