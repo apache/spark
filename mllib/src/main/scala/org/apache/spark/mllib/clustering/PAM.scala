9b4131a3fee5e9cd5a7ac58c7718b78236412f7e
Jiang Fan <fjiang6@gmail.com>
2014-11-19 21:05:06 -0800

    add the PAMSuite.scala

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/PAM.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/PAM.scala
@@ -32,6 +32,7 @@ import org.apache.spark.storage.StorageLevel
 import java.nio.ByteBuffer
 import java.util.{Random => JavaRandom}
 import scala.util.hashing.MurmurHash3
+import org.apache.spark.util.random.XORShiftRandom
 
 /**
  * K-medoids clustering (PAM) with support for multiple parallel runs.
@@ -370,6 +371,53 @@ object PAM {
   val PAM_PARALLEL = "parallel"
 
   /**
+   * Trains a PAM model using the given set of parameters.
+   *
+   * @param data training points stored as `RDD[Array[Double]]`
+   * @param k number of clusters
+   * @param maxIterations max number of iterations
+   * @param minIterations min number of iterations
+   * @param runs number of parallel runs, defaults to 1. The best model is returned.
+   * @param initializationMode initialization model, either "random" or "PAM_PARALLEL" (default).
+   */
+  def train(
+             data: RDD[Vector],
+             k: Int,
+             maxIterations: Int,
+             minIterations: Int,
+             runs: Int,
+             initializationMode: String): PAMModel = {
+    new PAM().setK(k)
+      .setMaxIterations(maxIterations)
+      .setMinIterations(minIterations)
+      .setRuns(runs)
+      .setInitializationMode(initializationMode)
+      .run(data)
+  }
+
+  /**
+   * Trains a PAM model using specified parameters and the default values for unspecified.
+   */
+  def train(
+             data: RDD[Vector],
+             k: Int,
+             maxIterations: Int): PAMModel = {
+    train(data, k, maxIterations, 5, 1, PAM_PARALLEL)
+  }
+
+  /**
+   * Trains a PAM model using specified parameters and the default values for unspecified.
+   */
+  def train(
+             data: RDD[Vector],
+             k: Int,
+             maxIterations: Int,
+             runs: Int): PAMModel = {
+    train(data, k, maxIterations, 5, runs, PAM_PARALLEL)
+  }
+
+
+  /**
    * Returns the index of the closest center to the given point, as well as the squared distance.
    */
   def findClosest(
@@ -448,44 +496,3 @@ object PAM {
     sqDist
   }
 }
-
-  /**
-   * This class implements a XORShift random number generator algorithm
-   * Source:
-   * Marsaglia, G. (2003). Xorshift RNGs. Journal of Statistical Software, Vol. 8, Issue 14.
-   * @see <a href="http://www.jstatsoft.org/v08/i14/paper">Paper</a>
-   * This implementation is approximately 3.5 times faster than
-   * {java.util.Random java.util.Random}, partly because of the algorithm, but also due
-   * to renouncing thread safety. JDK's implementation uses an AtomicLong seed, this class
-   * uses a regular Long. We can forgo thread safety since we use a new instance of the RNG
-   * for each thread.
-   */
-  class XORShiftRandom(init: Long) extends JavaRandom(init) {
-
-    def this() = this(System.nanoTime)
-
-    var seed = XORShiftRandom.hashSeed(init)
-
-    // we need to just override next - this will be called by nextInt, nextDouble,
-    // nextGaussian, nextLong, etc.
-    override protected def next(bits: Int): Int = {
-      var nextSeed = seed ^ (seed << 21)
-      nextSeed ^= (nextSeed >>> 35)
-      nextSeed ^= (nextSeed << 4)
-      seed = nextSeed
-      (nextSeed & ((1L << bits) -1)).asInstanceOf[Int]
-    }
-
-    override def setSeed(s: Long) {
-      seed = XORShiftRandom.hashSeed(s)
-    }
-  }
-
-  /** Contains benchmark method and main method to run benchmark of the RNG */
-  object XORShiftRandom {
-    /** Hash seeds to have 0/1 bits throughout. */
-    def hashSeed(seed: Long): Long = {
-      val bytes = ByteBuffer.allocate(java.lang.Long.SIZE).putLong(seed).array()
-      MurmurHash3.bytesHash(bytes)
-    }
-  }
