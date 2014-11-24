d322c28f46b243a4cdaa65ae107b4e1c420ecb63
Thu Kyaw <trk007@gmail.com>
2014-11-24 15:19:20 -0500

    [SPARK-3928][SQL] Support wildcard matches on Parquet files.
    Loop through globStatus output and listStatus on directories. This will handle the case if input is not glob path but normal directory path, making it backward compatiable.

--- sql/core/src/main/scala/org/apache/spark/sql/parquet/ParquetTableOperations.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/parquet/ParquetTableOperations.scala
@@ -611,7 +611,9 @@ private[parquet] object FileSystemHelper {
       throw new IllegalArgumentException(
         s"ParquetTableOperations: path $path does not exist or is not a directory")
     }
-    fs.globStatus(path).map(_.getPath)
+    fs.globStatus(path)
+      .flatMap { status => if(status.isDir) fs.listStatus(status.getPath) else List(status) }
+      .map(_.getPath)
   }
 
     /**
