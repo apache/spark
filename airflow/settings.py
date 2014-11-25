0fdebe1ee62a6e8c433fb612a11c872fd262f8b1
Maxime Beauchemin <maximebeauchemin@gmail.com>
2014-11-25 08:06:39 -0800

    Adding a link to the task instances list

--- airflow/settings.py
+++ airflow/settings.py
@@ -1,33 +1,10 @@
-import os
 import sys
 
-import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-from configuration import getconf
+from airflow.configuration import getconf
 
-
-"""
-if 'AIRFLOW_HOME' not in os.environ:
-    os.environ['AIRFLOW_HOME'] = os.path.join(os.path.dirname(__file__), "..")
-AIRFLOW_HOME = os.environ['AIRFLOW_HOME']
-"""
-
-BASE_FOLDER = getconf().get('core', 'BASE_FOLDER')
-SQL_ALCHEMY_CONN = getconf().get('core', 'SQL_ALCHEMY_CONN')
-if BASE_FOLDER not in sys.path:
-    sys.path.append(BASE_FOLDER)
-
-Session = sessionmaker()
-#engine = create_engine('mysql://airflow:airflow@localhost/airflow')
-engine = create_engine(
-    SQL_ALCHEMY_CONN, pool_size=50)
-Session.configure(bind=engine)
-
-# can't move this to configuration due to ConfigParser interpolation
-LOG_FORMAT =  \
-    '[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
 
 HEADER = """\
        .__         _____.__
@@ -36,3 +13,18 @@ _____  |__|_______/ ____\  |   ______  _  __
  / __ \|  ||  | \/|  |  |  |_(  <_> )     /
 (____  /__||__|   |__|  |____/\____/ \/\_/
      \/"""
+
+BASE_FOLDER = getconf().get('core', 'BASE_FOLDER')
+SQL_ALCHEMY_CONN = getconf().get('core', 'SQL_ALCHEMY_CONN')
+if BASE_FOLDER not in sys.path:
+    sys.path.append(BASE_FOLDER)
+
+Session = sessionmaker()
+engine = create_engine(
+    SQL_ALCHEMY_CONN, pool_size=50)
+Session.configure(bind=engine)
+
+# can't move this to configuration due to ConfigParser interpolation
+LOG_FORMAT =  \
+    '[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
+
