f68a0bdf46ad5e91295ae334dd643bc1ecdb12a0
Jacky Li <jacky.likun@huawei.com>
2014-11-27 01:04:47 +0800

    add 2 apriori implemenation and fp-growth implementation

--- mllib/src/test/scala/org/apache/spark/mllib/fim/AprioriSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/fim/AprioriSuite.scala
@@ -1,20 +1,19 @@
 /*
- * Licensed until the Apache Software Foundation (ASF) under one or more
- * contribuuntilr license agreements.  See the NOTICE file distributed with
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file until You under the Apache License, Version 2.0
+ * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed until in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.spark.mllib.fim
 
 import org.apache.spark.SparkContext
@@ -24,94 +23,75 @@ import org.scalatest.FunSuite
 
 /**
  * scala test unit
- * using Practical Machine Learning Book data set to test apriori algorithm
+ * using Practical Machine Learning Book data test the apriori algorithm result by minSupport from 0.9 to 0.1
  */
 class AprioriSuite extends FunSuite with LocalSparkContext {
 
-  test("test FIM with Apriori dataset 1")
+  test("test FIM with AprioriByBroadcast dataset 1")
   {
+    val arr = AprioriSuite.createFIMDataSet1()
+    val target: (RDD[Array[String]], Double, SparkContext) => Array[(Set[String], Int)]= AprioriByBroadcast.apriori
 
-    // input data set
-    val input = Array[String](
-      "1 3 4",
-      "2 3 5",
-      "1 2 3 5",
-      "2 5")
-
-    // correct FIS answers
-    val answer1 = Array((Set("4")), (Set("5")), (Set("2")), (Set("3")), (Set("1")), (Set("4", "1")), (Set("5", "2")), (Set("3", "1")), (Set("5", "3")), (Set("2", "3")), (Set("2", "1")), (Set("5", "1")), (Set("4", "3")), (Set("5", "2", "3")), (Set("3", "1", "5")), (Set("3", "1", "2")), (Set("4", "1", "3")), (Set("5", "2", "1")), (Set("5", "2", "3", "1")))
-    val answer2 = Array((Set("4")), (Set("5")), (Set("2")), (Set("3")), (Set("1")), (Set("4", "1")), (Set("5", "2")), (Set("3", "1")), (Set("5", "3")), (Set("2", "3")), (Set("2", "1")), (Set("5", "1")), (Set("4", "3")), (Set("5", "2", "3")), (Set("3", "1", "5")), (Set("3", "1", "2")), (Set("4", "1", "3")), (Set("5", "2", "1")), (Set("5", "2", "3", "1")))
-    val answer3 = Array((Set("5")), (Set("2")), (Set("3")), (Set("1")), (Set("5", "2")), (Set("3", "1")), (Set("5", "3")), (Set("2", "3")), (Set("5", "2", "3")))
-    val answer4 = Array((Set("5")), (Set("2")), (Set("3")), (Set("1")), (Set("5", "2")), (Set("3", "1")), (Set("5", "3")), (Set("2", "3")), (Set("5", "2", "3")))
-    val answer5 = Array((Set("5")), (Set("2")), (Set("3")), (Set("1")), (Set("5", "2")), (Set("3", "1")), (Set("5", "3")), (Set("2", "3")), (Set("5", "2", "3")))
-    val answer6 = Array((Set("5")), (Set("2")), (Set("3")), (Set("5", "2")))
-    val answer7 = Array((Set("5")), (Set("2")), (Set("3")), (Set("5", "2")))
-    val answer8 = Array()
-    val answer9 = Array()
-
-    val target: (RDD[Array[String]], Double, SparkContext) => Array[(Set[String], Int)]= Apriori.apriori
-
-    val dataSet = sc.parallelize(input)
+    val dataSet = sc.parallelize(arr)
     val rdd = dataSet.map(line => line.split(" "))
 
-    val result9 = target(rdd, 0.9, sc)
-    assert(result9.length == answer9.length)
-
-    val result8 = target(rdd, 0.8, sc)
-    assert(result8.length == answer8.length)
-
-    val result7 = target(rdd, 0.7, sc)
-    assert(result7.length == answer7.length)
-    for (i <- 0 until result7.length){
-      assert(answer7(i).equals(result7(i)._1))
+    for (i <- 1 to 9){
+      println(s"frequent item set with support ${i/10d}")
+      target(rdd, i/10d, sc).foreach(x => print("(" + x._1 + "), "))
+      println()
     }
-
-    val result6 = target(rdd, 0.6, sc)
-    assert(result6.length == answer6.length)
-    for (i <- 0 until result6.length)
-      assert(answer6(i).equals(result6(i)._1))
-
-    val result5 = target(rdd, 0.5, sc)
-    assert(result5.length == answer5.length)
-    for (i <- 0 until result5.length)
-      assert(answer5(i).equals(result5(i)._1))
-
-    val result4 = target(rdd, 0.4, sc)
-    assert(result4.length == answer4.length)
-    for (i <- 0 until result4.length)
-      assert(answer4(i).equals(result4(i)._1))
-
-    val result3 = target(rdd, 0.3, sc)
-    assert(result3.length == answer3.length)
-    for (i <- 0 until result3.length)
-      assert(answer3(i).equals(result3(i)._1))
-
-    val result2 = target(rdd, 0.2, sc)
-    assert(result2.length == answer2.length)
-    for (i <- 0 until result2.length)
-      assert(answer2(i).equals(result2(i)._1))
-
-    val result1 = target(rdd, 0.1, sc)
-    assert(result1.length == answer1.length)
-    for (i <- 0 until result1.length)
-      assert(answer1(i).equals(result1(i)._1))
   }
 
-  test("test FIM with Apriori dataset 2")
+  test("test FIM with AprioriByBroadcast dataset 2")
   {
+    val arr = AprioriSuite.createFIMDataSet2()
+    val target: (RDD[Array[String]], Double, SparkContext) => Array[(Set[String], Int)]= AprioriByBroadcast.apriori
 
-    // input data set
-    val input = Array[String](
-      "r z h j p",
-      "z y x w v u t s",
-      "z",
-      "r x n o s",
-      "y r x z q t p",
-      "y z x e q s t m")
+    val dataSet = sc.parallelize(arr)
+    val rdd = dataSet.map(line => line.split(" "))
 
-    val target: (RDD[Array[String]], Double, SparkContext) => Array[(Set[String], Int)]= Apriori.apriori
+    assert(target(rdd,0.9,sc).length == 0)
 
-    val dataSet = sc.parallelize(input)
+    assert(target(rdd,0.8,sc).length == 1)
+
+    assert(target(rdd,0.7,sc).length == 1)
+
+    assert(target(rdd,0.6,sc).length == 2)
+
+    assert(target(rdd,0.5,sc).length == 18)
+
+    assert(target(rdd,0.4,sc).length == 18)
+
+    assert(target(rdd,0.3,sc).length == 54)
+
+    assert(target(rdd,0.2,sc).length == 54)
+
+    assert(target(rdd,0.1,sc).length == 625)
+
+  }
+
+  test("test FIM with AprioriByCartesian dataset 1")
+  {
+    val arr = AprioriSuite.createFIMDataSet1()
+    val target: (RDD[Array[String]], Double, SparkContext) => Array[(Set[String], Int)]= AprioriByCartesian.apriori
+
+    val dataSet = sc.parallelize(arr)
+    val rdd = dataSet.map(line => line.split(" "))
+
+    for (i <- 1 to 9){
+      println(s"frequent item set with support ${i/10d}")
+      target(rdd, i/10d, sc).foreach(x => print("(" + x._1 + "), "))
+      println()
+    }
+
+    
+  }
+  test("test FIM with AprioriByCartesian dataset 2")
+  {
+    val arr = AprioriSuite.createFIMDataSet2()
+    val target: (RDD[Array[String]], Double, SparkContext) => Array[(Set[String], Int)]= AprioriByCartesian.apriori
+
+    val dataSet = sc.parallelize(arr)
     val rdd = dataSet.map(line => line.split(" "))
 
     assert(target(rdd,0.9,sc).length == 0)
@@ -135,3 +115,34 @@ class AprioriSuite extends FunSuite with LocalSparkContext {
   }
 
 }
+
+/**
+ * create dataset
+ */
+object AprioriSuite
+{
+  /**
+   * create dataset using Practical Machine Learning Book data
+   * @return dataset
+   */
+  def createFIMDataSet1():Array[String] = {
+    val arr = Array[String](
+      "1 3 4",
+      "2 3 5",
+      "1 2 3 5",
+      "2 5")
+    arr
+  }
+
+  def createFIMDataSet2():Array[String] = {
+    val arr = Array[String](
+      "r z h j p",
+      "z y x w v u t s",
+      "z",
+      "r x n o s",
+      "y r x z q t p",
+      "y z x e q s t m")
+    arr
+  }
+}
+
