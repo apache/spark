a1080c06bfc4f2d52b57ca23e4eb1b1e906fb81e
carlmartin <carlmartinmax@gmail.com>
2014-11-28 14:45:55 +0800

    yarn-client web bug

--- yarn/common/src/main/scala/org/apache/spark/scheduler/cluster/YarnClientSchedulerBackend.scala
+++ yarn/common/src/main/scala/org/apache/spark/scheduler/cluster/YarnClientSchedulerBackend.scala
@@ -140,6 +140,7 @@ private[spark] class YarnClientSchedulerBackend(
    * Stop the scheduler. This assumes `start()` has already been called.
    */
   override def stop() {
+    stopExecutorLauncher
     assert(client != null, "Attempted to stop this scheduler before starting it!")
     stopping = true
     super.stop()
