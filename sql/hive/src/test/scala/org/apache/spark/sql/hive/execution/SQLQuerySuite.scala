25c9f855cca506ce22c812aa0677416ab049f3f5
ravipesala <ravindra.pesala@huawei.com>
2014-11-29 00:35:54 +0530

    updated

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/SQLQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/SQLQuerySuite.scala
@@ -163,16 +163,9 @@ class SQLQuerySuite extends QueryTest {
       sql("SELECT case when ~1=-2 then 1 else 0 end FROM src"),
       sql("SELECT 1 FROM src").collect().toSeq)
   }
-
+ 
  test("SPARK-4154 Query does not work if it has 'not between' in Spark SQL and HQL") {
     checkAnswer(sql("SELECT key FROM src WHERE key not between 0 and 10 order by key"), 
         sql("SELECT key FROM src WHERE key between 11 and 500 order by key").collect().toSeq)
-  }
-
- test("Supporting Coalesce function in Spark SQL") {
-   sql("SELECT * FROM src where key=477").registerTempTable("src1")
-   checkAnswer(sql("SELECT COALESCE(src1.key,src.key) FROM src left outer join src1 "
-       +"on src.key=src1.key order by src.key,src1.key"), 
-       sql("SELECT key FROM src order by key").collect().toSeq)
   }
 }
