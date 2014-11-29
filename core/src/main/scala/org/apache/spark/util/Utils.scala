5e4ac7c3af267fe4200af78de97acb99c0a1175d
Liang-Chi Hsieh <viirya@gmail.com>
2014-11-28 18:04:05 -0800

    [SPARK-4597] Use proper exception and reset variable in Utils.createTempDir()
    `File.exists()` and `File.mkdirs()` only throw `SecurityException` instead of `IOException`. Then, when an exception is thrown, `dir` should be reset too.
    
    Author: Liang-Chi Hsieh <viirya@gmail.com>
    
    Closes #3449 from viirya/fix_createtempdir and squashes the following commits:
    
    36cacbd [Liang-Chi Hsieh] Use proper exception and reset variable.
    
    (cherry picked from commit 49fe8797e64f10c574e0790b32a8c3fdc7e594a0)
    Signed-off-by: Josh Rosen <joshrosen@databricks.com>

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -263,7 +263,7 @@ private[spark] object Utils extends Logging {
         if (dir.exists() || !dir.mkdirs()) {
           dir = null
         }
-      } catch { case e: IOException => ; }
+      } catch { case e: SecurityException => dir = null; }
     }
 
     registerShutdownDeleteDir(dir)
