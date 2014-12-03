efa9b039c0fa4c1674c3ad7081e402ca8bc7ae40
YanTangZhai <hakeemzhai@tencent.com>
2014-12-03 14:47:52 +0800

    Update HiveQuerySuite.scala

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -413,12 +413,14 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
   createQueryTest("select null from table",
     "SELECT null FROM src LIMIT 1")
 
-  createQueryTest("predicates contains an empty AttributeSet() references",
-    """
-      |SELECT a FROM (
-      |  SELECT 1 AS a FROM src LIMIT 1 ) table
-      |WHERE abs(20141202) is not null
-    """.stripMargin)
+  test("predicates contains an empty AttributeSet() references") {
+    sql(
+      """
+        |SELECT a FROM (
+        |  SELECT 1 AS a FROM src LIMIT 1 ) table
+        |WHERE abs(20141202) is not null
+      """.stripMargin).collect()
+  }
 
   test("implement identity function using case statement") {
     val actual = sql("SELECT (CASE key WHEN key THEN key END) FROM src")
