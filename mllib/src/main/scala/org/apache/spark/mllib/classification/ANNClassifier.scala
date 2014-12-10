90f5ae56e3b829b4ddf389253a822e854e4a00da
Alexander Ulanov <nashb@yandex.ru>
2014-12-10 14:15:03 -0800

    Addressing bgreeven comment regarding labels sort, annotations

--- mllib/src/main/scala/org/apache/spark/mllib/classification/ANNClassifier.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/classification/ANNClassifier.scala
@@ -84,30 +84,74 @@ class ANNClassifier private(val labelToIndex: Map[Double, Int],
   }
 }
 
+/**
+ * Top level methods for training the classifier based on artificial neural network (ANN)
+ */
 object ANNClassifier {
 
+  /**
+   * Trains an ANN classifier.
+   *
+   * @param data RDD containing labeled points for training.
+   * @param hiddenLayersTopology number of nodes per hidden layer, excluding the bias nodes.
+   * @param initialWeights initial weights of underlying artificial neural network
+   * @param maxIterations specifies maximum number of training iterations.
+   * @param stepSize step size (not implemented)
+   * @param convergenceTol convergence tolerance for LBFGS
+   * @return ANN model.
+   */
   def train(data: RDD[LabeledPoint], hiddenLayersTopology: Array[Int],
             initialWeights: Vector, maxIterations: Int,
             stepSize: Double, convergenceTol: Double): ANNClassifierModel = {
-    val labelToIndex = data.map( lp => lp.label).distinct().collect().zipWithIndex.toMap
+    val labelToIndex = data.map( lp => lp.label).distinct().collect().sorted.zipWithIndex.toMap
     new ANNClassifier(labelToIndex, hiddenLayersTopology,
       initialWeights, maxIterations, stepSize, convergenceTol).run(data)
   }
 
+  /**
+   * Trains an ANN classifier.
+   *
+   * @param data RDD containing labeled points for training.
+   * @param hiddenLayersTopology number of nodes per hidden layer, excluding the bias nodes.
+   * @param maxIterations specifies maximum number of training iterations.
+   * @param stepSize step size (not implemented)
+   * @param convergenceTol convergence tolerance for LBFGS
+   * @return ANN classifier model.
+   */
   def train(data: RDD[LabeledPoint], hiddenLayersTopology: Array[Int], maxIterations: Int,
             stepSize: Double, convergenceTol: Double): ANNClassifierModel = {
     val initialWeights = randomWeights(data, hiddenLayersTopology)
     train(data, hiddenLayersTopology, initialWeights, maxIterations, stepSize, convergenceTol)
   }
 
+  /**
+   * Trains an already pre-trained ANN classifier.
+   * Assumes that the data has the same labels that the
+   * data that were used for training, or at least the
+   * subset of that labels
+   *
+   * @param data RDD containing labeled points for training.
+   * @param model a pre-trained ANN classifier model.
+   * @param maxIterations specifies maximum number of training iterations.
+   * @param stepSize step size (not implemented)
+   * @param convergenceTol convergence tolerance for LBFGS
+   * @return ANN classifier model.
+   */
   def train(data: RDD[LabeledPoint], model: ANNClassifierModel, maxIterations: Int,
             stepSize: Double, convergenceTol: Double): ANNClassifierModel = {
     val hiddenLayersTopology =
       model.annModel.topology.slice(1, model.annModel.topology.length - 1)
-    train(data, hiddenLayersTopology, model.annModel.weights,
-      maxIterations, stepSize, convergenceTol)
+    new ANNClassifier(model.labelToIndex, hiddenLayersTopology,
+      model.annModel.weights, maxIterations, stepSize, convergenceTol).run(data)
   }
 
+  /**
+   * Trains an ANN classifier with one hidden layer of size (featureCount / 2 + 1)
+   * with 2000 steps of size 1.0 and tolerance 1e-4
+   *
+   * @param data RDD containing labeled points for training.
+   * @return ANN classifier model.
+   */
   def train(data: RDD[LabeledPoint]): ANNClassifierModel = {
     val featureCount = data.first().features.size
     val hiddenSize = featureCount / 2 + 1
@@ -115,14 +159,33 @@ object ANNClassifier {
     train(data, hiddenLayersTopology, 2000, 1.0, 1e-4)
   }
 
-  /* TODO: remove duplication - the same analysis will be done in ANNClassifier.run() */
+  /**
+   * Returns random weights for the ANN classifier with the given hidden layers
+   * and data dimensionality, i.e. the weights for the following topology:
+   * [numFeatures -: hiddenLayers :- numLabels]
+   *
+   * @param data RDD containing labeled points for training.
+   * @param hiddenLayersTopology number of nodes per hidden layer, excluding the bias nodes.
+   * @param seed
+   * @return vector with random weights.
+   */
   def randomWeights(data: RDD[LabeledPoint],
                     hiddenLayersTopology: Array[Int], seed: Int): Vector = {
+    /* TODO: remove duplication - the same analysis will be done in ANNClassifier.run() */
     val labelCount = data.map( lp => lp.label).distinct().collect().length
     val featureCount = data.first().features.size
     ArtificialNeuralNetwork.randomWeights(featureCount, labelCount, hiddenLayersTopology, seed)
   }
 
+  /**
+   * Returns random weights for the ANN classifier with the given hidden layers
+   * and data dimensionality, i.e. the weights for the following topology:
+   * [numFeatures -: hiddenLayers :- numLabels]
+   *
+   * @param data RDD containing labeled points for training.
+   * @param hiddenLayersTopology number of nodes per hidden layer, excluding the bias nodes.
+   * @return vector with random weights.
+   */
   def randomWeights(data: RDD[LabeledPoint], hiddenLayersTopology: Array[Int]): Vector = {
     randomWeights(data, hiddenLayersTopology, Random.nextInt())
   }
