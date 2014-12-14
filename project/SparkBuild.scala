f71e783b38cf709cb1bf6dcfea45237d820c7bd1
Sean Owen <sowen@cloudera.com>
2014-12-14 18:29:40 +0000

    Enable assertions for SBT and Maven build

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -385,6 +385,7 @@ object TestSettings {
     javaOptions in Test += "-Dsun.io.serialization.extendedDebugInfo=true",
     javaOptions in Test ++= System.getProperties.filter(_._1 startsWith "spark")
       .map { case (k,v) => s"-D$k=$v" }.toSeq,
+    javaOptions in Test += "-ea",
     javaOptions in Test ++= "-Xmx3g -XX:PermSize=128M -XX:MaxNewSize=256m -XX:MaxPermSize=1g"
       .split(" ").toSeq,
     // This places test scope jars on the classpath of executors during tests.
