4e5db244073e17f34982c3e6c66ee895eceb8513
Maxime Beauchemin <maximebeauchemin@gmail.com>
2014-12-15 08:29:05 -0800

    Adding macro ds_add

--- airflow/macros/__init__.py
+++ airflow/macros/__init__.py
@@ -2,3 +2,11 @@ from random import random
 from datetime import datetime
 import time
 import hive
+
+
+def ds_add(ds, days):
+    '''Add or subtract days from a YYYY-MM-DD'''
+    ds = datetime.strptime(ds, '%Y-%m-%d')
+    if days:
+        ds = ds + timedelta(days)
+    return ds.isoformat()[:10]
