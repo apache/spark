31322c647b34b880f2d54a4b2c171cd6df6eed66
cafreeman <cfreeman@alteryx.com>
2014-12-18 09:54:46 -0600

    Update SparkR_IDE_Setup.sh
    Changed .Rprofile path to ~/.Rprofile to ensure that it goes to the right place instead of whatever the current working directory is.

--- SparkR_IDE_Setup.sh
+++ SparkR_IDE_Setup.sh
@@ -9,7 +9,7 @@ sudo yum install rstudio-0.98.1091-x86_64.rpm
 
 # Add SparkR directory to .libPaths() in order to import SparkR into an Rstudio session
 
-cat <<EOT >> .Rprofile
+cat <<EOT >> ~/.Rprofile
 lib_path <- .libPaths()
 
 lib_path <- c(lib_path,"/home/cloudera/SparkR-pkg/lib")
