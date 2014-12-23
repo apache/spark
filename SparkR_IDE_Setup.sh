6e6cb6294b7ee229c0df3ac90df896a907738ba8
cafreeman <cfreeman@alteryx.com>
2014-12-23 15:39:14 -0600

    Update SparkR_IDE_Setup.sh
    Cleaned up the code to remove a few superfluous components

--- SparkR_IDE_Setup.sh
+++ SparkR_IDE_Setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Download RStudio
-if [ ! -f /rstudio-0.98.1091-x86_64.rpm ]; then
-	wget  http://download1.rstudio.org/rstudio-0.98.1091-x86_64.rpm
+if [ ! -f rstudio-0.98.1091-x86_64.rpm ]; then
+  wget  http://download1.rstudio.org/rstudio-0.98.1091-x86_64.rpm
 fi
 
 # Install using the rpm via yum
 
 sudo yum install rstudio-0.98.1091-x86_64.rpm
 
-sudo rm rstudio-0.98.1091-x86_64.rpm
+sudo rstudio-0.98.1091-x86_64.rpm
 
 # Add SparkR directory to .libPaths() in order to import SparkR into an Rstudio session
 
