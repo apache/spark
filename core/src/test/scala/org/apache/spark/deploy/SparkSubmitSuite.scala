4f4031ddb9bc6661a483ae3b8c89c7538ce816e0
Josh Rosen <joshrosen@databricks.com>
2014-12-24 16:18:49 -0800

    Add note on why SparkSubmitSuite needs ResetSystemProperties

--- core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
@@ -27,6 +27,8 @@ import org.apache.spark.util.{ResetSystemProperties, Utils}
 import org.scalatest.FunSuite
 import org.scalatest.Matchers
 
+// Note: this suite mixes in ResetSystemProperties because SparkSubmit.main() sets a bunch
+// of properties that neeed to be cleared after tests.
 class SparkSubmitSuite extends FunSuite with Matchers with ResetSystemProperties {
   def beforeAll() {
     System.setProperty("spark.testing", "true")
