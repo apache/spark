60ea4bd7b35c068ce405841ba7befcd1c95bbf45
Sandy Ryza <sandy@cloudera.com>
2014-12-24 16:35:36 -0800

    Fix scalastyle

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/YarnAllocator.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/YarnAllocator.scala
@@ -95,7 +95,7 @@ private[yarn] class YarnAllocator(
   // Additional memory overhead.
   protected val memoryOverhead: Int = sparkConf.getInt("spark.yarn.executor.memoryOverhead",
     math.max((MEMORY_OVERHEAD_FACTOR * executorMemory).toInt, MEMORY_OVERHEAD_MIN))
-  /// Number of cores per executor.
+  // Number of cores per executor.
   protected val executorCores = args.executorCores
   // Resource capability requested for each executors
   private val resource = Resource.newInstance(executorMemory + memoryOverhead, executorCores)
