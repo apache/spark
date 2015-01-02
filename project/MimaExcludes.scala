53722b94149c9c12735875133d5d01c7fb87d136
Yadong Qi <qiyadong2010@gmail.com>
2015-01-02 15:09:41 -0800

    [SPARK-3325][Streaming] Add a parameter to the method print in class DStream
    This PR is a fixed version of the original PR #3237 by watermen and scwf.
    This adds the ability to specify how many elements to print in `DStream.print`.
    
    Author: Yadong Qi <qiyadong2010@gmail.com>
    Author: q00251598 <qiyadong@huawei.com>
    Author: Tathagata Das <tathagata.das1565@gmail.com>
    Author: wangfei <wangfei1@huawei.com>
    
    Closes #3865 from tdas/print-num and squashes the following commits:
    
    cd34e9e [Tathagata Das] Fix bug
    7c09f16 [Tathagata Das] Merge remote-tracking branch 'apache-github/master' into HEAD
    bb35d1a [Yadong Qi] Update MimaExcludes.scala
    f8098ca [Yadong Qi] Update MimaExcludes.scala
    f6ac3cb [Yadong Qi] Update MimaExcludes.scala
    e4ed897 [Yadong Qi] Update MimaExcludes.scala
    3b9d5cf [wangfei] fix conflicts
    ec8a3af [q00251598] move to  Spark 1.3
    26a70c0 [q00251598] extend the Python DStream's print
    b589a4b [q00251598] add another print function

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -54,6 +54,9 @@ object MimaExcludes {
             ProblemFilters.exclude[MissingMethodProblem](
               "org.apache.spark.mllib.linalg.Matrices.rand")
           ) ++ Seq(
+            // SPARK-3325
+            ProblemFilters.exclude[MissingMethodProblem](
+              "org.apache.spark.streaming.api.java.JavaDStreamLike.print"),
             // SPARK-2757
             ProblemFilters.exclude[IncompatibleResultTypeProblem](
               "org.apache.spark.streaming.flume.sink.SparkAvroCallbackHandler." +
