9504c6c654063e3d7780ee162a9acb9e8e090706
Marcelo Vanzin <vanzin@cloudera.com>
2015-01-08 17:15:13 -0800

    [SPARK-4048] Enhance and extend hadoop-provided profile.
    This change does a few things to make the hadoop-provided profile more useful:
    
    - Create new profiles for other libraries / services that might be provided by the infrastructure
    - Simplify and fix the poms so that the profiles are only activated while building assemblies.
    - Fix tests so that they're able to run when the profiles are activated
    - Add a new env variable to be used by distributions that use these profiles to provide the runtime
      classpath for Spark jobs and daemons.
    
    Author: Marcelo Vanzin <vanzin@cloudera.com>
    
    Closes #2982 from vanzin/SPARK-4048 and squashes the following commits:
    
    82eb688 [Marcelo Vanzin] Add a comment.
    eb228c0 [Marcelo Vanzin] Fix borked merge.
    4e38f4e [Marcelo Vanzin] Merge branch 'master' into SPARK-4048
    9ef79a3 [Marcelo Vanzin] Alternative way to propagate test classpath to child processes.
    371ebee [Marcelo Vanzin] Review feedback.
    52f366d [Marcelo Vanzin] Merge branch 'master' into SPARK-4048
    83099fc [Marcelo Vanzin] Merge branch 'master' into SPARK-4048
    7377e7b [Marcelo Vanzin] Merge branch 'master' into SPARK-4048
    322f882 [Marcelo Vanzin] Fix merge fail.
    f24e9e7 [Marcelo Vanzin] Merge branch 'master' into SPARK-4048
    8b00b6a [Marcelo Vanzin] Merge branch 'master' into SPARK-4048
    9640503 [Marcelo Vanzin] Cleanup child process log message.
    115fde5 [Marcelo Vanzin] Simplify a comment (and make it consistent with another pom).
    e3ab2da [Marcelo Vanzin] Fix hive-thriftserver profile.
    7820d58 [Marcelo Vanzin] Fix CliSuite with provided profiles.
    1be73d4 [Marcelo Vanzin] Restore flume-provided profile.
    d1399ed [Marcelo Vanzin] Restore jetty dependency.
    82a54b9 [Marcelo Vanzin] Remove unused profile.
    5c54a25 [Marcelo Vanzin] Fix HiveThriftServer2Suite with *-provided profiles.
    1fc4d0b [Marcelo Vanzin] Update dependencies for hive-thriftserver.
    f7b3bbe [Marcelo Vanzin] Add snappy to hadoop-provided list.
    9e4e001 [Marcelo Vanzin] Remove duplicate hive profile.
    d928d62 [Marcelo Vanzin] Redirect child stderr to parent's log.
    4d67469 [Marcelo Vanzin] Propagate SPARK_DIST_CLASSPATH on Yarn.
    417d90e [Marcelo Vanzin] Introduce "SPARK_DIST_CLASSPATH".
    2f95f0d [Marcelo Vanzin] Propagate classpath to child processes during testing.
    1adf91c [Marcelo Vanzin] Re-enable maven-install-plugin for a few projects.
    284dda6 [Marcelo Vanzin] Rework the "hadoop-provided" profile, add new ones.

--- core/src/test/scala/org/apache/spark/DriverSuite.scala
+++ core/src/test/scala/org/apache/spark/DriverSuite.scala
@@ -35,7 +35,7 @@ class DriverSuite extends FunSuite with Timeouts {
     forAll(masters) { (master: String) =>
       failAfter(60 seconds) {
         Utils.executeAndGetOutput(
-          Seq("./bin/spark-class", "org.apache.spark.DriverWithoutCleanup", master),
+          Seq(s"$sparkHome/bin/spark-class", "org.apache.spark.DriverWithoutCleanup", master),
           new File(sparkHome),
           Map("SPARK_TESTING" -> "1", "SPARK_HOME" -> sparkHome))
       }
