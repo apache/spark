25cc00a17175952fe7e21209583a754574581b37
Reynold Xin <rxin@databricks.com>
2015-01-14 19:19:50 -0800

    Add implicit conversion back.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveContext.scala
@@ -20,6 +20,7 @@ package org.apache.spark.sql.hive
 import java.io.{BufferedReader, InputStreamReader, PrintStream}
 import java.sql.{Date, Timestamp}
 
+import scala.collection.JavaConversions._
 import scala.language.implicitConversions
 import scala.reflect.runtime.universe.TypeTag
 
