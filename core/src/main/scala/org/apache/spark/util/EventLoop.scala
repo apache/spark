aefa1cee5233e6ef8f1e3ca074ea84bf3f46ed70
zsxwing <zsxwing@gmail.com>
2015-01-16 10:24:17 +0800

    Add protected to on*** methods

--- core/src/main/scala/org/apache/spark/util/EventLoop.scala
+++ core/src/main/scala/org/apache/spark/util/EventLoop.scala
@@ -99,12 +99,12 @@ private[spark] abstract class EventLoop[E](name: String) extends Logging {
   /**
    * Invoked when `start()` is called but before the event thread starts.
    */
-  def onStart(): Unit = {}
+  protected def onStart(): Unit = {}
 
   /**
    * Invoked when `stop()` is called and the event thread exits.
    */
-  def onStop(): Unit = {}
+  protected def onStop(): Unit = {}
 
   /**
    * Invoked in the event thread when polling events from the event queue.
@@ -113,12 +113,12 @@ private[spark] abstract class EventLoop[E](name: String) extends Logging {
    * and cannot process events in time. If you want to call some blocking actions, run them in
    * another thread.
    */
-  def onReceive(event: E): Unit
+  protected def onReceive(event: E): Unit
 
   /**
    * Invoked if `onReceive` throws any non fatal error. Any non fatal error thrown from `onError`
    * will be ignored.
    */
-  def onError(e: Throwable): Unit
+  protected def onError(e: Throwable): Unit
 
 }
--- core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
@@ -43,7 +43,6 @@ class DAGSchedulerEventProcessLoopTester(dagScheduler: DAGScheduler)
       case NonFatal(e) => onError(e)
     }
   }
-
 }
 
 /**
