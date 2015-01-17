ac9b278bbad00749b1e1e5eef202dc3d9cefbae8
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-01-17 09:21:51 -0800

    Hard coding install_requires

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-import os
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
-# Reading the requirements from pip's requirements.txt
-reqs = (str(ir.req) for ir in parse_requirements('requirements.txt'))
+# To generate install_requires from requirements.txt
+# reqs = (str(ir.req) for ir in parse_requirements('requirements.txt'))
 
 setup(
     name='airflow',
-    version='0.11',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     scripts=['airflow/bin/airflow'],
-    install_requires=reqs,
+    install_requires=[
+        'celery', 'chartkick',
+        'flask', 'flask-admin', 'flask-bootstrap', 'flask-cache',
+        'flask-login', 'hive-thrift-py', 'jinja2',
+        'markdown', 'mysql-python', 'pandas', 'pygments', 'pyhive',
+        'python-dateutil', 'requests', 'setproctitle',
+        'sphinx', 'sphinx-rtd-theme', 'Sphinx-PyPI-upload',
+        'sqlalchemy', 'thrift'
+    ],
     author='Maxime Beauchemin',
     author_email='maximebeauchemin@gmail.com',
     url='https://github.com/mistercrunch/Airflow',
-    download_url='https://github.com/mistercrunch/Airflow/tarball/0.11',
+    download_url='https://github.com/mistercrunch/Airflow/tarball/0.1',
 )
