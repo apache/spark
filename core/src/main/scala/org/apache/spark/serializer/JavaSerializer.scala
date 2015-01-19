b349b77509229eee3ea5a7f3fbad6737b82d2e95
Reynold Xin <rxin@databricks.com>
2015-01-18 21:55:01 -0800

    [SPARK-5307] SerializationDebugger to help debug NotSerializableException - take 2
    This patch adds a SerializationDebugger that is used to add serialization path to
    a NotSerializableException. When a NotSerializableException is encountered, the debugger
    visits the object graph to find the path towards the object that cannot be serialized,
    and constructs information to help user to find the object.
    
    Compared with an earlier attempt, this one provides extra information including
    field names, array offsets, writeExternal calls, etc.

--- core/src/main/scala/org/apache/spark/serializer/JavaSerializer.scala
+++ core/src/main/scala/org/apache/spark/serializer/JavaSerializer.scala
@@ -39,7 +39,12 @@ private[spark] class JavaSerializationStream(out: OutputStream, counterReset: In
    * the stream 'resets' object class descriptions have to be re-written)
    */
   def writeObject[T: ClassTag](t: T): SerializationStream = {
-    objOut.writeObject(t)
+    try {
+      objOut.writeObject(t)
+    } catch {
+      case e: NotSerializableException =>
+        throw SerializationDebugger.improveException(t, e)
+    }
     counter += 1
     if (counterReset > 0 && counter >= counterReset) {
       objOut.reset()
