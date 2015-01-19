705b860f59148a536b59e07033c3905302f7dfcc
chuckreporting <chuckreporting@gmail.com>
2015-01-19 11:52:38 +0000

    Update startScript.sh

--- startScript.sh
+++ startScript.sh
@@ -8,7 +8,7 @@ export SPARK_LOCAL_IP=${HOST_IP_ADDRESS}
 
 if [[ "${EXECUTION_MODE}" = "master" ]]
 then
-    /root/spark1.3/sbin/start-master.sh
+    nohup /root/spark1.3/sbin/start-master.sh &
 elif [[ "${EXECUTION_MODE}" = "worker" ]]
 then
     /root/spark1.3/bin/spark-class org.apache.spark.deploy.worker.Worker spark://${MASTER_IP_ADDRESS}:${MASTER_PORT}
