caa191e3a395128e4e432d3097512ff62cc78c92
chuckreporting <chuckreporting@gmail.com>
2015-01-22 12:22:46 +0000

    Update startScript.sh

--- startScript.sh
+++ startScript.sh
@@ -2,16 +2,16 @@
 
 
 HOST_IP_ADDRESS=`sh getEth0Inet.sh`
-export SPARK_MASTER_IP=${MASTER_IP_ADDRESS}
 export SPARK_LOCAL_IP=${HOST_IP_ADDRESS}
-
 
 if [[ "${EXECUTION_MODE}" = "master" ]]
 then
+    export SPARK_MASTER_IP=${HOST_IP_ADDRESS}
     /root/spark1.3/sbin/start-master.sh
     /bin/bash
 elif [[ "${EXECUTION_MODE}" = "worker" ]]
 then
+    export SPARK_MASTER_IP=${MASTER_IP_ADDRESS}
     /root/spark1.3/bin/spark-class org.apache.spark.deploy.worker.Worker spark://${MASTER_IP_ADDRESS}:${MASTER_PORT}
 else
     /bin/bash
