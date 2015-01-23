53e350f13727793d7add22913d0040f5f4ef5735
Arthur Wiedmer <arthur.wiedmer@gmail.com>
2015-01-23 01:30:43 +0000

    [closest_ds macro] linting
    Following flake8 recs + changing return type of helper function

--- airflow/macros/hive.py
+++ airflow/macros/hive.py
@@ -1,6 +1,7 @@
 from airflow.configuration import conf
 import datetime
 
+
 def max_partition(
         table, schema="default",
         hive_conn_id=conf.get('hooks', 'HIVE_DEFAULT_CONN_ID')):
@@ -10,45 +11,60 @@ def max_partition(
     hh = HiveHook(hive_conn_id=hive_conn_id)
     return hh.max_partition(schema=schema, table_name=table)
 
-def closest_date(target_ds, ds_list, before_target = None):
+
+def _closest_date(target_dt, date_list, before_target=None):
     '''
     This function finds the date in a list closest to the target date.
     An optional paramter can be given to get the closest before or after.
 
-    :param target_ds: The target date
-    :type target_ds: datetime.date
-    :param ds_list: The list of dates to search
-    :type ds_list: datetime.date list
+    :param target_dt: The target date
+    :type target_dt: datetime.date
+    :param date_list: The list of dates to search
+    :type date_list: datetime.date list
     :param before_target: closest before or after the target
     :type before_target: bool or None
     :returns: The closest date
     :rtype: datetime.date or None
     '''
-    fb = lambda d: d - target_ds if d > target_ds else datetime.timedelta.max
-    fa = lambda d: d - target_ds if d < target_ds else datetime.timedelta.min
-    fnone = lambda d: target_ds - d if d < target_ds else d - target_ds
-    if before is None:
-        return min(ds_list, key = fnone )
-    if before:
-        return min(ds_list, key = fb )
+    fb = lambda d: d - target_dt if d > target_dt else datetime.timedelta.max
+    fa = lambda d: d - target_dt if d < target_dt else datetime.timedelta.min
+    fnone = lambda d: target_dt - d if d < target_dt else d - target_dt
+    if before_target is None:
+        return min(date_list, key=fnone).date()
+    if before_target:
+        return min(date_list, key=fb).date()
     else:
-        return min(ds_list, key = fa )
+        return min(date_list, key=fa).date()
 
 
 def closest_ds_partition(
-        table, ds_partition, before=True, schema="default",
+        table, ds, before=True, schema="default",
         hive_conn_id=conf.get('hooks', 'HIVE_DEFAULT_CONN_ID')):
+    '''
+    This function finds the date in a list closest to the target date.
+    An optional paramter can be given to get the closest before or after.
+
+    :param table: A hive table name
+    :type table: str
+    :param ds: A datestamp '%Y-%m-%d' i.e. 'yyyy-mm-dd'
+    :type ds: datetime.date list
+    :param before: closest before (True), after (False) or either side of ds
+    :type before: bool or None
+    :returns: The closest date
+    :rtype: str or None
+
+    '''
     from airflow.hooks.hive_hook import HiveHook
     if '.' in table:
         schema, table = table.split('.')
-    target_ds = datetime.datetime.strptime(ds_partition, '%Y-%m-%d')
+    target_dt = datetime.datetime.strptime(ds, '%Y-%m-%d')
     hh = HiveHook(hive_conn_id=hive_conn_id)
     partitions = hh.get_partitions(schema=schema, table_name=table)
-    parts = [ datetime.datetime.strptime(p, '%Y-%m-%d') for p in partitions ]
+    parts = [datetime.datetime.strptime(p, '%Y-%m-%d') for p in partitions]
     if partitions is None or partitions == []:
         return None
-    if ds_partition in partitions:
-        return ds_partition
+    if ds in partitions:
+        return ds
     else:
-        closest_ds = closest_date(target_ds, parts, before_target = before)
+        closest_ds = _closest_date(target_dt, parts, before_target=before)
         return closest_ds.isoformat()
