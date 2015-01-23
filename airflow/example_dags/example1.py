40849b8efb038b10327c64acdc9f9a3b32b4327e
Maxime <maxime.beauchemin@airbnb.com>
2015-01-23 18:10:40 +0000

    Improving the examples dags a little

--- airflow/example_dags/example1.py
+++ airflow/example_dags/example1.py
@@ -2,23 +2,22 @@ from airflow.operators import BashOperator, DummyOperator
 from airflow.models import DAG
 from datetime import datetime
 
-default_args = {
-    'owner': 'max',
-    'start_date': datetime(2014, 11, 1),
+args = {
+    'owner': 'airflow',
+    'start_date': datetime(2015, 1, 1),
 }
 
-dag = DAG(dag_id='example_1')
-# dag = DAG(dag_id='example_1', executor=SequentialExecutor())
+dag = DAG(dag_id='example1')
 
 cmd = 'ls -l'
 run_this_last = DummyOperator(
     task_id='run_this_last',
-    default_args=default_args)
+    default_args=args)
 dag.add_task(run_this_last)
 
 run_this = BashOperator(
     task_id='run_after_loop', bash_command='echo 1',
-    default_args=default_args)
+    default_args=args)
 dag.add_task(run_this)
 run_this.set_downstream(run_this_last)
 for i in range(9):
@@ -26,13 +25,13 @@ for i in range(9):
     task = BashOperator(
         task_id='runme_'+i,
         bash_command='sleep 5',
-        default_args=default_args)
+        default_args=args)
     task.set_downstream(run_this)
     dag.add_task(task)
 
 task = BashOperator(
     task_id='also_run_this',
     bash_command='ls -l',
-    default_args=default_args)
+    default_args=args)
 dag.add_task(task)
 task.set_downstream(run_this_last)
--- airflow/example_dags/example2.py
+++ airflow/example_dags/example2.py
@@ -2,34 +2,34 @@ from airflow.operators import BashOperator
 from airflow.models import DAG
 from datetime import datetime
 
-default_args = {
-    'owner': 'mistercrunch',
-    'start_date': datetime(2014, 10, 1),
+args = {
+    'owner': 'airflow',
+    'start_date': datetime(2015, 1, 1),
     'depends_on_past': True,
 }
 
-dag = DAG(dag_id='example_2')
+dag = DAG(dag_id='example2')
 
 cmd = 'ls -l'
-run_this_last = BashOperator(task_id='run_this_last', bash_command='echo 1', **default_args)
+run_this_last = BashOperator(
+    task_id='run_this_last', bash_command='echo 1', default_args=args)
 dag.add_task(run_this_last)
 
-run_this = BashOperator(task_id='run_this', bash_command='echo 1', **default_args)
+run_this = BashOperator(
+    task_id='run_this', bash_command='echo 1', default_args=args)
 dag.add_task(run_this)
 run_this.set_downstream(run_this_last)
 
 for i in range(10):
     i = str(i)
     task = BashOperator(
-            task_id='runme_'+i,
-            bash_command='sleep 10',
-            default_args=default_args)
+        task_id='runme_'+i,
+        bash_command='sleep 10',
+        default_args=args)
     task.set_downstream(run_this)
     dag.add_task(task)
 
-cmd = """\
-echo {{ params.tables.the_table }}
-"""
+cmd = "echo {{ params.tables.the_table }}"
 task = BashOperator(
     task_id='also_run_this', bash_command=cmd,
     params={
@@ -37,11 +37,7 @@ task = BashOperator(
             'the_table': 'da_table',
         }
     },
-    **default_args)
+    default_args=args)
 dag.add_task(task)
 task.set_downstream(run_this_last)
 task.set_upstream(run_this)
-
-#dag.tree_view()
-#dag.db_merge()
-#dag.run()
