3c89f4a081e8e8d225ae9fe577914ef10353cd5b
Reynold Xin <rxin@databricks.com>
2015-01-28 15:45:46 -0800

    Package.

--- sql/core/src/main/scala/org/apache/spark/sql/package.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/package.scala
@@ -38,11 +38,4 @@ package object sql {
    */
   @DeveloperApi
   protected[sql] type Strategy = org.apache.spark.sql.catalyst.planning.GenericStrategy[SparkPlan]
-
-  /** Converts $"col name" into an [[Column]]. */
-//  implicit class StringToColumn(val sc: StringContext) extends AnyVal {
-//    def $(args: Any*): ColumnName = {
-//      new ColumnName(sc.s(args :_*))
-//    }
-//  }
 }
