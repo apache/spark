415268e19ebf9d48cb4a50773f446ebbe8a902cd
Xiangrui Meng <meng@databricks.com>
2015-01-28 15:53:38 -0800

    remove inherit_doc from __init__

--- python/pyspark/ml/pipeline.py
+++ python/pyspark/ml/pipeline.py
@@ -18,22 +18,10 @@
 from abc import ABCMeta, abstractmethod
 
 from pyspark.ml.param import Param, Params
+from pyspark.ml.util import inherit_doc
+
 
 __all__ = ['Estimator', 'Transformer', 'Pipeline', 'PipelineModel']
-
-
-def inherit_doc(cls):
-    for name, func in vars(cls).items():
-        # only inherit docstring for public functions
-        if name.startswith("_"):
-            continue
-        if not func.__doc__:
-            for parent in cls.__bases__:
-                parent_func = getattr(parent, name, None)
-                if parent_func and getattr(parent_func, "__doc__", None):
-                    func.__doc__ = parent_func.__doc__
-                    break
-    return cls
 
 
 @inherit_doc
