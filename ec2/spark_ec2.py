ce2a2075f19b72a6dcede3c44cdacb72c52c14ee
liuchang0812 <liuchang0812@gmail.com>
2015-01-30 17:10:25 +0000

    pep8

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -1008,14 +1008,15 @@ def real_main():
 
     if opts.identity_file is not None:
         if not os.path.exists(opts.identity_file):
-            print >> stderr, "ERROR: The identity file '{f}' doesn't exist.".format(f=opts.identity_file)
+            print >> stderr,\
+                "ERROR: The identity file '{f}' doesn't exist.".format(f=opts.identity_file)
             sys.exit(1)
 
         file_mode = os.stat(opts.identity_file).st_mode
-        if not (file_mode & S_IRUSR) or  not oct(file_mode)[-2:] == '00':
+        if not (file_mode & S_IRUSR) or not oct(file_mode)[-2:] == '00':
             print >> stderr, "ERROR: The identity file must be accessible only by you."
             sys.exit(1)
- 
+
     if opts.ebs_vol_num > 8:
         print >> stderr, "ebs-vol-num cannot be greater than 8"
         sys.exit(1)
