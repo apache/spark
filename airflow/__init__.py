19950660a96f4da74c4bf37d43e6bc6d2f3aa743
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-02-01 20:53:32 -0800

    v0.3 pushing to pypi

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -1,3 +1,3 @@
 from models import DAG
 
-__version__ = "0.2.3.9"
+__version__ = "0.3"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Kept manually in sync with airflow.__version__
-version = '0.2.3.9'
+version = '0.3'
 
 setup(
     name='airflow',
