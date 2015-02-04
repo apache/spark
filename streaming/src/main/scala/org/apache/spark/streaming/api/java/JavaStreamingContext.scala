4cf4cba08f1757ec0d9bffdfae6db719a4fb5a3f
zsxwing <zsxwing@gmail.com>
2015-02-04 00:40:28 -0800

    [SPARK-5379][Streaming] Add awaitTerminationOrTimeout
    Added `awaitTerminationOrTimeout` to return if the waiting time elapsed:
    * `true` if it's stopped.
    * `false` if the waiting time elapsed before returning from the method.
    * throw the reported error if it's thrown during the execution.
    
    Also deprecated `awaitTermination(timeout: Long)`.
    
    Author: zsxwing <zsxwing@gmail.com>
    
    Closes #4171 from zsxwing/SPARK-5379 and squashes the following commits:
    
    c9e660b [zsxwing] Add a unit test for awaitTerminationOrTimeout
    8a89f92 [zsxwing] Add awaitTerminationOrTimeout to python
    cdc820b [zsxwing] Add awaitTerminationOrTimeout

--- streaming/src/main/scala/org/apache/spark/streaming/api/java/JavaStreamingContext.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/api/java/JavaStreamingContext.scala
@@ -597,8 +597,21 @@ class JavaStreamingContext(val ssc: StreamingContext) extends Closeable {
    * will be thrown in this thread.
    * @param timeout time to wait in milliseconds
    */
+  @deprecated("Use awaitTerminationOrTimeout(Long) instead", "1.3.0")
   def awaitTermination(timeout: Long): Unit = {
     ssc.awaitTermination(timeout)
+  }
+
+  /**
+   * Wait for the execution to stop. Any exceptions that occurs during the execution
+   * will be thrown in this thread.
+   *
+   * @param timeout time to wait in milliseconds
+   * @return `true` if it's stopped; or throw the reported error during the execution; or `false`
+   *         if the waiting time elapsed before returning from the method.
+   */
+  def awaitTerminationOrTimeout(timeout: Long): Boolean = {
+    ssc.awaitTerminationOrTimeout(timeout)
   }
 
   /**
