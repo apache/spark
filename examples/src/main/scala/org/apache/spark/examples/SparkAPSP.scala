5a0f18e74921e3851a673b35b3d18aa097d8780a
deeppradhan <deeppradhan@gmail.com>
2015-02-04 14:22:59 +0530

    Create SparkAPSP.scala
    The input to the program is an edge list file. The format should be the following, where each pair is an edge <src_id  dest_id>
    
    1 2
    2 1
    2 3
    2 4
    3 2
    3 3
    4 2
    4 3

---
+++ examples/src/main/scala/org/apache/spark/examples/SparkAPSP.scala
@@ -0,0 +1,48 @@
+package org.apache.spark.examples
+
+import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkContext.rddToPairRDDFunctions
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel._
+import scala.util._
+
+object SparkAPSP {
+
+def main(args: Array[String])
+{
+ 	if(args.length < 1)
+	{
+	  System.err.println("Usage: SparkAPSP <file>")
+	  System.exit(1)
+	}
+	
+	val conf = new SparkConf().setAppName("Diameter Estimation")
+	val sc = new SparkContext(conf)
+	
+	val lines = sc.textFile(args(0))
+	var edges = lines.map(s =>{  
+	val val = s.split("\\s+")
+	(val(0).toLong,val(1).toLong)
+	}).distinct().cache()
+
+	var distances = edges.map(pair => (pair, 1)).cache()
+	var prevDistsSize = 0L
+	var distsSize = distances.count()
+	
+	while (prevDistsSize < distsSize) 
+	{
+  	val newDists = distances.map {case ((a, b), dist) => (b, (a, dist))}.join(edges)
+   	.map {case (b, ((a, dist), c)) => ((a, c), dist + 1)}.cache() 
+
+    distances = distances.union(newDists).reduceByKey((a, b) => math.min(a,b)).cache()
+
+ 	  prevDistsSize = distsSize 
+ 	  distsSize = distances.count() 
+	}
+	
+	val finalres = distances.collect()
+	finalres.foreach(res => println(res._1 + " -> distance " + res._2))
+}	
+}
