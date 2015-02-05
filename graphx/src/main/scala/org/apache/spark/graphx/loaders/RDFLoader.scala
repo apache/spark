686af40a8a14d391791883c013428a0d91376897
lukovnikov <lukovnikov@denis>
2015-02-05 01:20:08 +0100

    style fixed

--- graphx/src/main/scala/org/apache/spark/graphx/loaders/RDFLoader.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/loaders/RDFLoader.scala
@@ -1,3 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.graphx.loaders
+
+
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{Logging, SparkContext}
 import org.apache.spark.graphx.impl.{EdgePartitionBuilder, GraphImpl}
@@ -34,7 +54,7 @@ object RDFLoader extends Logging {
   def loadNTriples(
       sc: SparkContext,
       path: String,
-      //numPartitions: Int = -1,
+      // numPartitions: Int = -1,
       edgeStorageLevel: StorageLevel = StorageLevel.MEMORY_ONLY,
       vertexStorageLevel: StorageLevel = StorageLevel.MEMORY_ONLY)
     : Graph[String, String] =
@@ -52,7 +72,7 @@ object RDFLoader extends Logging {
       }
     })
     .distinct()
-    //.repartition(numPartitions)
+    // .repartition(numPartitions)
     .map(name => 
       name match {
         case propvalregex(pre, value) => (gethash("<" + pre + "> " + value), value)
