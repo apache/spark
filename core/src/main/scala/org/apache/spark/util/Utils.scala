3feb798cb6e0c400939e5cdfa8ba3c8842de1ae4
Makoto Fukuhara <fukuo33@gmail.com>
2015-02-06 11:11:38 -0800

    [SPARK-5618][Spark Core][Minor] Optimise utility code.
    Author: Makoto Fukuhara <fukuo33@gmail.com>
    
    Closes #4396 from fukuo33/fix-unnecessary-regex and squashes the following commits:
    
    cd07fd6 [Makoto Fukuhara] fix unnecessary regex.
    
    (cherry picked from commit 4cdb26c174e479a144950d12e1ad180f361af1fd)
    Signed-off-by: Andrew Or <andrew@databricks.com>

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -1162,9 +1162,9 @@ private[spark] object Utils extends Logging {
     // finding the call site of a method.
     val SPARK_CORE_CLASS_REGEX =
       """^org\.apache\.spark(\.api\.java)?(\.util)?(\.rdd)?(\.broadcast)?\.[A-Z]""".r
-    val SCALA_CLASS_REGEX = """^scala""".r
+    val SCALA_CORE_CLASS_PREFIX = "scala"
     val isSparkCoreClass = SPARK_CORE_CLASS_REGEX.findFirstIn(className).isDefined
-    val isScalaClass = SCALA_CLASS_REGEX.findFirstIn(className).isDefined
+    val isScalaClass = className.startsWith(SCALA_CORE_CLASS_PREFIX)
     // If the class is a Spark internal class or a Scala class, then exclude.
     isSparkCoreClass || isScalaClass
   }
