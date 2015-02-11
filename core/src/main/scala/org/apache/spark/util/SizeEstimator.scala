798ab88188e4ba8d0a65abac3b9dd7de64da406e
Sandy Ryza <sandy@cloudera.com>
2015-02-11 14:36:16 -0800

    Update documentation and add to SparkContext

--- core/src/main/scala/org/apache/spark/util/SizeEstimator.scala
+++ core/src/main/scala/org/apache/spark/util/SizeEstimator.scala
@@ -36,8 +36,7 @@ import org.apache.spark.util.collection.OpenHashSet
  * Based on the following JavaWorld article:
  * http://www.javaworld.com/javaworld/javaqa/2003-12/02-qa-1226-sizeof.html
  */
-@DeveloperApi
-object SizeEstimator extends Logging {
+private[spark] object SizeEstimator extends Logging {
 
   // Sizes of primitive types
   private val BYTE_SIZE    = 1
