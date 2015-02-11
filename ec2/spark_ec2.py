2f52489d293eb4ececcd7021ec5439a7e1f2fe02
Patrick Wendell <patrick@databricks.com>
2015-02-10 21:54:55 -0800

    Updating versions for Spark 1.3

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -40,7 +40,7 @@ from datetime import datetime
 from optparse import OptionParser
 from sys import stderr
 
-SPARK_EC2_VERSION = "1.2.0"
+SPARK_EC2_VERSION = "1.3.0"
 SPARK_EC2_DIR = os.path.dirname(os.path.realpath(__file__))
 
 VALID_SPARK_VERSIONS = set([
@@ -56,6 +56,8 @@ VALID_SPARK_VERSIONS = set([
     "1.1.0",
     "1.1.1",
     "1.2.0",
+    "1.2.1",
+    "1.3.0",
 ])
 
 DEFAULT_SPARK_VERSION = SPARK_EC2_VERSION
