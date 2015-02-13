05e1085aeed4378343ae88538cfb1c5bd2fa587f
Davies Liu <davies@databricks.com>
2015-02-13 15:27:49 -0800

    check ending mark in non-block way

--- core/src/main/scala/org/apache/spark/api/python/PythonRDD.scala
+++ core/src/main/scala/org/apache/spark/api/python/PythonRDD.scala
@@ -144,9 +144,12 @@ private[spark] class PythonRDD(
                 stream.readFully(update)
                 accumulator += Collections.singletonList(update)
               }
+
               // Check whether the worker is ready to be re-used.
-              if (stream.readInt() == SpecialLengths.END_OF_STREAM) {
-                if (reuse_worker) {
+              if (reuse_worker) {
+                // Tt has a high possibility that the ending mark is already available,
+                // And current task should not be blocked by checking it
+                if (stream.available() >= 4 && stream.readInt() == SpecialLengths.END_OF_STREAM) {
                   env.releasePythonWorker(pythonExec, envVars.toMap, worker)
                   released = true
                 }
