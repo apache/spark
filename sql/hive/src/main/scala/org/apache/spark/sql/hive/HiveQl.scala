94bacac73de78656a48d248a58d2ca8363c3e1c8
Yin Huai <yhuai@databricks.com>
2015-02-15 20:44:43 -0800

    Add SHOW TABLES to the list of noExplainCommands.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveQl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveQl.scala
@@ -126,6 +126,7 @@ private[hive] object HiveQl {
   // Commands that we do not need to explain.
   protected val noExplainCommands = Seq(
     "TOK_DESCTABLE",
+    "TOK_SHOWTABLES",
     "TOK_TRUNCATETABLE"     // truncate table" is a NativeCommand, does not need to explain.
   ) ++ nativeCommands
 
