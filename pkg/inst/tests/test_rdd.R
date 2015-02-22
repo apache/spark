57e005baf4a7c6a550eb81a169efe1651ca1effe
hlin09 <hlin09pu@gmail.com>
2015-02-21 21:37:25 -0500

    Revert "fix tests."
    This reverts commit fd836db89e785843be4fcd7868215770e00ffcd4.

--- pkg/inst/tests/test_rdd.R
+++ pkg/inst/tests/test_rdd.R
@@ -89,7 +89,7 @@ test_that("several transformations on RDD (a benchmark on PipelinedRDD)", {
   actual <- collect(rdd2)
   expected <- list(24, 24, 24, 24, 24,
                    168, 170, 172, 174, 176)
-  expect_equal(actual, expected)
+  expect_equal(actual, expected))
 })
 
 test_that("PipelinedRDD support actions: cache(), persist(), unpersist(), checkpoint()", {
