0c07fab88a891049b973ef020683d09b69062fd3
Lukasz Jastrzebski <lukasz.jastrzebski@gmail.com>
2015-02-26 21:09:47 -0800

    Incorporating comments for SPARK-2168

--- core/src/test/scala/org/apache/spark/deploy/history/HistoryServerSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/history/HistoryServerSuite.scala
@@ -14,40 +14,43 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.spark.deploy.history
 
+import javax.servlet.http.HttpServletRequest
+
+import scala.collection.mutable
+
+import org.apache.hadoop.fs.Path
+import org.mockito.Mockito.{when}
 import org.scalatest.FunSuite
 import org.scalatest.Matchers
 import org.scalatest.mock.MockitoSugar
-import javax.servlet.http.HttpServletRequest
-import org.mockito.Mockito.{when}
-import org.apache.hadoop.fs.Path
+
 import org.apache.spark.ui.SparkUI
-import scala.collection.mutable
 
 class HistoryServerSuite extends FunSuite with Matchers with MockitoSugar {
-	
-  val historyServer = mock[HistoryServer]
-  val request = mock[HttpServletRequest]
-  
-  test("generate history page with relative links") { 
+
+  test("generate history page with relative links") {
+    val historyServer = mock[HistoryServer]
+    val request = mock[HttpServletRequest]
     val ui = mock[SparkUI]
     val link = "/history/app1"
-    val info = new ApplicationHistoryInfo("app1", "app1", 0, 2, 1, "xxx", true )
+    val info = new ApplicationHistoryInfo("app1", "app1", 0, 2, 1, "xxx", true)
     when(historyServer.getApplicationList()).thenReturn(Seq(info))
     when(ui.basePath).thenReturn(link)
     when(historyServer.getProviderConfig()).thenReturn(Map[String, String]())
     val page = new HistoryPage(historyServer)
-    
+
     //when
     val response = page.render(request)
-    
+
     //then
-   val links = response \\ "a"
-   val justHrefs = for {
-     l <- links
-     attrs <- l.attribute("href")
-   } yield (attrs.toString)
-   justHrefs should contain(link)
+    val links = response \\ "a"
+    val justHrefs = for {
+      l <- links
+      attrs <- l.attribute("href")
+    } yield (attrs.toString)
+    justHrefs should contain(link)
   }
 }
