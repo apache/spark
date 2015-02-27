dcb46697d59fa77ac643e438b346eb28972d9e8f
Imran Rashid <irashid@cloudera.com>
2015-02-27 14:59:04 -0600

    add real test case for uploading large blocks (failing now)

--- core/src/test/scala/org/apache/spark/network/netty/NettyBlockTransferSuite.scala
+++ core/src/test/scala/org/apache/spark/network/netty/NettyBlockTransferSuite.scala
@@ -16,14 +16,17 @@
  */
 package org.apache.spark.network.netty
 
+import java.nio.ByteBuffer
 import java.util.concurrent.TimeUnit
 
 import org.apache.commons.io.IOUtils
 import org.apache.spark.network.BlockDataManager
-import org.apache.spark.network.buffer.{ManagedBuffer, LargeByteBufferHelper, NioManagedBuffer}
+import org.apache.spark.network.buffer._
 import org.apache.spark.network.shuffle.BlockFetchingListener
-import org.apache.spark.storage.ShuffleBlockId
-import org.apache.spark.{SecurityManager, SparkConf}
+import org.apache.spark.storage.{BlockId, StorageLevel, RDDBlockId, ShuffleBlockId}
+import org.apache.spark.{Logging, SecurityManager, SparkConf}
+import org.mockito.ArgumentCaptor
+import org.mockito.{Matchers => MockitoMatchers}
 import org.mockito.Mockito._
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.{Matchers, FunSuite}
@@ -31,28 +34,23 @@ import org.scalatest.{Matchers, FunSuite}
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.{Await, Promise}
 
-class NettyBlockTransferSuite extends FunSuite with Matchers with MockitoSugar {
+class NettyBlockTransferSuite extends FunSuite with Matchers with MockitoSugar with Logging {
 
   val conf = new SparkConf()
     .set("spark.app.id", "app-id")
   val securityManager = new SecurityManager(conf)
 
 
-
-  test("simple fetch") {
-
+  def fetchBlock(buf: LargeByteBuffer): ManagedBuffer = {
     val blockManager = mock[BlockDataManager]
     val blockId = ShuffleBlockId(0, 1, 2)
-    val buf = LargeByteBufferHelper.allocate(Integer.MAX_VALUE.toLong + 100l)
     val blockBuffer = new NioManagedBuffer(buf)
     when(blockManager.getBlockData(blockId)).thenReturn(blockBuffer)
 
     val from = new NettyBlockTransferService(conf, securityManager, numCores = 1)
     from.init(blockManager)
-    println("from: " + from.hostName + ":" + from.port)
     val to = new NettyBlockTransferService(conf, securityManager, numCores = 1)
     to.init(blockManager)
-    println("to: " + to.hostName + ":" + to.port)
 
     try {
       val promise = Promise[ManagedBuffer]()
@@ -69,17 +67,72 @@ class NettyBlockTransferSuite extends FunSuite with Matchers with MockitoSugar {
         })
 
       Await.ready(promise.future, FiniteDuration(100, TimeUnit.SECONDS))
-      val v = promise.future.value.get.get
-//      IOUtils.toString(v.createInputStream()) should equal(blockString)
-      println(v.nioByteBuffer().limit())
+      promise.future.value.get.get
     } finally {
       from.close()
       to.close()
     }
 
-
   }
 
+  ignore("simple fetch") {
+    val blockString = "Hello, world!"
+    val blockBuffer = LargeByteBufferHelper.asLargeByteBuffer(blockString.getBytes)
+    val fetched = fetchBlock(blockBuffer)
+
+    IOUtils.toString(fetched.createInputStream()) should equal(blockString)
+  }
+
+
+  def uploadBlock(buf: LargeByteBuffer) {
+
+    val fromBlockManager = mock[BlockDataManager]
+    val toBlockManager = mock[BlockDataManager]
+    val blockId = RDDBlockId(0, 1)
+    val blockBuffer = new NioManagedBuffer(buf)
+    val level = StorageLevel.DISK_ONLY //doesn't matter
+
+    val from = new NettyBlockTransferService(conf, securityManager, numCores = 1)
+    from.init(fromBlockManager)
+    val to = new NettyBlockTransferService(conf, securityManager, numCores = 1)
+    logTrace("to block manager = " + toBlockManager)
+    to.init(toBlockManager)
+
+    from.uploadBlock(to.hostName, to.port, "exec-1", blockId, blockBuffer, level)
+    //TODO how to get rid of this wait??
+    Thread.sleep(1000)
+    val bufferCaptor = ArgumentCaptor.forClass(classOf[ManagedBuffer])
+    verify(toBlockManager).putBlockData(MockitoMatchers.eq(blockId), bufferCaptor.capture(),
+      MockitoMatchers.eq(level))
+    val putBuffer = bufferCaptor.getValue()
+  }
+
+  test("simple upload") {
+    val buf = LargeByteBufferHelper.asLargeByteBuffer(Array[Byte](0,1,2,3))
+    uploadBlock(buf)
+  }
+
+
+  test("giant upload") {
+    val parts = (0 until 2).map{_ => ByteBuffer.allocate(Integer.MAX_VALUE - 100)}.toArray
+    val buf = new WrappedLargeByteBuffer(parts)
+    uploadBlock(buf)
+  }
+
+
+
+  def equivalentBuffers(exp: ManagedBuffer, act: ManagedBuffer): Unit = {
+    equivalentBuffers(exp.nioByteBuffer(), act.nioByteBuffer())
+  }
+
+  def equivalentBuffers(exp: LargeByteBuffer, act: LargeByteBuffer): Unit = {
+    assert(exp.capacity() === act.capacity())
+    assert(exp.remaining() === act.remaining())
+    while (exp.remaining() > 0) {
+      assert(exp.get() === act.get())
+    }
+
+  }
 
 
 
