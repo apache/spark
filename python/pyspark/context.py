24c92a41354c936a864a32c4dbae3fbfeeb95ce3
Davies Liu <davies@databricks.com>
2015-03-05 23:15:43 -0800

    fix style

--- python/pyspark/context.py
+++ python/pyspark/context.py
@@ -838,7 +838,8 @@ class SparkContext(object):
         # by runJob() in order to avoid having to pass a Python lambda into
         # SparkContext#runJob.
         mappedRDD = rdd.mapPartitions(partitionFunc)
-        port = self._jvm.PythonRDD.runJob(self._jsc.sc(), mappedRDD._jrdd, javaPartitions, allowLocal)
+        port = self._jvm.PythonRDD.runJob(self._jsc.sc(), mappedRDD._jrdd, javaPartitions,
+                                          allowLocal)
         return list(_load_from_socket(port, mappedRDD._jrdd_deserializer))
 
     def show_profiles(self):
