18fb481448abaa38ee16b8c2ca1cc976470e68e5
Daoyuan Wang <daoyuan.wang@intel.com>
2015-03-06 00:31:07 -0800

    enable variable substitute on test framework

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
@@ -23,6 +23,7 @@ import java.util.{Set => JavaSet}
 import org.apache.hadoop.hive.ql.exec.FunctionRegistry
 import org.apache.hadoop.hive.ql.io.avro.{AvroContainerInputFormat, AvroContainerOutputFormat}
 import org.apache.hadoop.hive.ql.metadata.Table
+import org.apache.hadoop.hive.ql.parse.VariableSubstitution
 import org.apache.hadoop.hive.ql.processors._
 import org.apache.hadoop.hive.serde2.RegexSerDe
 import org.apache.hadoop.hive.serde2.`lazy`.LazySimpleSerDe
@@ -153,7 +154,12 @@ class TestHiveContext(sc: SparkContext) extends HiveContext(sc) {
 
   val describedTable = "DESCRIBE (\\w+)".r
 
+  val vs = new VariableSubstitution()
+
   protected[hive] class HiveQLQueryExecution(hql: String)
+    extends this.SubstitutedHiveQLQueryExecution(vs.substitute(hiveconf, hql))
+
+  protected[hive] class SubstitutedHiveQLQueryExecution(hql: String)
     extends this.QueryExecution(HiveQl.parseSql(hql)) {
     def hiveExec(): Seq[String] = runSqlHive(hql)
     override def toString: String = hql + "\n" + super.toString
