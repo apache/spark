b4c0b2e49f7cc5cab62aa003057214fa56e4a17d
Davies Liu <davies.liu@gmail.com>
2015-03-06 12:41:16 -0800

    use fork package

--- pkg/inst/worker/daemon.R
+++ pkg/inst/worker/daemon.R
@@ -1,7 +1,13 @@
 # Worker daemon
 
+# try to load `fork`, or install
+FORK_URI <- "http://cran.r-project.org/src/contrib/Archive/fork/fork_1.2.4.tar.gz"
+tryCatch(library(fork), error = function(err) {
+  install.packages(FORK_URI, repos = NULL, type = "source")
+})
+
 rLibDir <- Sys.getenv("SPARKR_RLIBDIR")
-script <- paste(rLibDir, "SparkR/worker/worker.R", sep="/")
+script <- paste(rLibDir, "SparkR/worker/worker.R", sep = "/")
 
 # preload SparkR package, speedup worker
 .libPaths(c(rLibDir, .libPaths()))
@@ -11,15 +17,23 @@ port <- as.integer(Sys.getenv("SPARKR_WORKER_PORT"))
 inputCon <- socketConnection(port = port, blocking = TRUE, open = "rb")
 
 while (TRUE) {
-  inport <- SparkR:::readInt(inputCon)
-  if (length(inport) != 1) {
-    quit(save="no")
+  ready <- socketSelect(list(inputCon), timeout = 1)
+  if (ready) {
+    inport <- SparkR:::readInt(inputCon)
+    if (length(inport) != 1) {
+      quit(save="no")
+    }
+    p <- fork::fork(NULL)
+    if (p == 0) {
+      close(inputCon)
+      Sys.setenv(SPARKR_WORKER_PORT = inport)
+      source(script)
+      fork::exit(0)
+    }
   }
-  p <- parallel:::mcfork()
-  if (inherits(p, "masterProcess")) {
-    close(inputCon)
-    Sys.setenv(SPARKR_WORKER_PORT = inport)
-    source(script)
-    parallel:::mcexit(0)
+  # cleanup all the child process
+  status <- fork::wait(0, nohang = TRUE)
+  while (status[1] > 0) {
+    status <- fork::wait(0, nohang = TRUE)
   }
 }
