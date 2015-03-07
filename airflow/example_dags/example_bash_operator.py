f1453068493ca910e804838e223a7e07533e94e2
Mike Kerzhner <kerzhner@yahoo-inc.com>
2015-03-06 16:31:33 -0800

    Adding back example_dags

---
+++ airflow/example_dags/example_bash_operator.py
@@ -0,0 +1,31 @@
+from airflow.operators import BashOperator, DummyOperator
+from airflow.models import DAG
+from datetime import datetime
+
+args = {
+    'owner': 'airflow',
+    'start_date': datetime(2015, 1, 1),
+}
+
+dag = DAG(dag_id='example_bash_operator', default_args=args)
+
+cmd = 'ls -l'
+run_this_last = DummyOperator(task_id='run_this_last', dag=dag)
+
+run_this = BashOperator(
+    task_id='run_after_loop', bash_command='echo 1', dag=dag)
+run_this.set_downstream(run_this_last)
+
+for i in range(3):
+    i = str(i)
+    task = BashOperator(
+        task_id='runme_'+i,
+        bash_command='echo "{{ task_instance_key_str }}" && sleep ' + str(i),
+        dag=dag)
+    task.set_downstream(run_this)
+
+task = BashOperator(
+    task_id='also_run_this',
+    bash_command='echo "{{ macros.uuid.uuid1() }}"',
+    dag=dag)
+task.set_downstream(run_this_last)
---
+++ airflow/example_dags/example_python_operator.py
@@ -0,0 +1,41 @@
+from airflow.operators import PythonOperator
+from airflow.models import DAG
+from datetime import datetime
+
+import time
+from pprint import pprint
+
+args = {
+    'owner': 'airflow',
+    'start_date': datetime(2015, 1, 1),
+}
+
+dag = DAG(dag_id='example_python_operator', default_args=args)
+
+def my_sleeping_function(random_base):
+    '''This is a function that will run whithin the DAG execution'''
+    time.sleep(random_base)
+
+def print_context(ds, **kwargs):
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+run_this = PythonOperator(
+    task_id='print_the_context',
+    provide_context=True,
+    python_callable=print_context,
+    dag=dag)
+
+for i in range(10):
+    '''
+    Generating 10 sleeping task, sleeping from 0 to 9 seconds
+    respectively
+    '''
+    task = PythonOperator(
+        task_id='sleep_for_'+str(i),
+        python_callable=my_sleeping_function,
+        op_kwargs={'random_base': i},
+        dag=dag)
+
+    task.set_upstream(run_this)
