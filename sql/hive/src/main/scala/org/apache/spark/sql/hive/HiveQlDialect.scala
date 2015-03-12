1256d34408ed645aba5d80fc9e0e38ea4fb60738
Sea <huhaiyang@huawei.com>
2015-03-12 15:32:06 +0800

    Update HiveQlDialect.scala
    remove minor comment

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveQlDialect.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveQlDialect.scala
@@ -20,9 +20,6 @@ package org.apache.spark.sql.hive
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.dialect.Dialect
 
-/**
- * Created by Sally on 2015/3/8.
- */
 object HiveQlDialect extends Dialect {
   val name = "hiveql"
 
