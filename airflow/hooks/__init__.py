1d56f22f2e470ff5e9db0a24b3e4a4516cbf1811
Arthur Wiedmer <arthur.wiedmer@gmail.com>
2015-03-19 17:40:05 -0700

    [setup] selective features and imports for hooks and operators

--- airflow/hooks/__init__.py
+++ airflow/hooks/__init__.py
@@ -1,8 +1,30 @@
-from airflow.hooks.mysql_hook import MySqlHook
-from airflow.hooks.postgres_hook import PostgresHook
+import logging
+
+try:
+    from airflow.hooks.mysql_hook import MySqlHook
+except:
+    logging.INFO("Couldn't load MySQLHook")
+    pass
+
+try:
+    from airflow.hooks.postgres_hook import PostgresHook
+except:
+    logging.INFO("Couldn't import PostgreHook")
+    pass
+
 from airflow.hooks.hive_hooks import HiveCliHook
 from airflow.hooks.hive_hooks import HiveMetastoreHook
 from airflow.hooks.hive_hooks import HiveServer2Hook
 from airflow.hooks.presto_hook import PrestoHook
-from airflow.hooks.samba_hook import SambaHook
-from airflow.hooks.S3_hook import S3Hook
+
+try:
+    from airflow.hooks.samba_hook import SambaHook
+except:
+    logging.INFO("Couldn't import SambaHook")
+    pass
+
+try:
+    from airflow.hooks.S3_hook import S3Hook
+except:
+    logging.INFO("Couldn't import S3Hook")
+    pass
