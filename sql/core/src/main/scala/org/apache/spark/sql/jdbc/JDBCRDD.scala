e60fbf6c4597d0240ab1b472594774beb4f3d391
ypcat <ypcat6@gmail.com>
2015-03-22 15:49:13 +0800

    [SPARK-6408] [SQL] Fix JDBCRDD filtering string literals
    Author: ypcat <ypcat6@gmail.com>
    Author: Pei-Lun Lee <pllee@appier.com>
    
    Closes #5087 from ypcat/spark-6408 and squashes the following commits:
    
    1becc16 [ypcat] [SPARK-6408] [SQL] styling
    1bc4455 [ypcat] [SPARK-6408] [SQL] move nested function outside
    e57fa4a [ypcat] [SPARK-6408] [SQL] fix test case
    245ab6f [ypcat] [SPARK-6408] [SQL] add test cases for filtering quoted strings
    8962534 [Pei-Lun Lee] [SPARK-6408] [SQL] Fix filtering string literals
    
    (cherry picked from commit 9b1e1f20d4498bda72dd53a832110883a7ca41b5)
    Signed-off-by: Cheng Lian <lian@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/JDBCRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/JDBCRDD.scala
@@ -19,6 +19,7 @@ package org.apache.spark.sql.jdbc
 
 import java.sql.{Connection, DriverManager, ResultSet, ResultSetMetaData, SQLException}
 
+import org.apache.commons.lang.StringEscapeUtils.escapeSql
 import org.apache.spark.{Logging, Partition, SparkContext, TaskContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.expressions.{Row, SpecificMutableRow}
@@ -227,15 +228,23 @@ private[sql] class JDBCRDD(
   }
 
   /**
+   * Converts value to SQL expression.
+   */
+  private def compileValue(value: Any): Any = value match {
+    case stringValue: String => s"'${escapeSql(stringValue)}'"
+    case _ => value
+  }
+
+  /**
    * Turns a single Filter into a String representing a SQL expression.
    * Returns null for an unhandled filter.
    */
   private def compileFilter(f: Filter): String = f match {
-    case EqualTo(attr, value) => s"$attr = $value"
-    case LessThan(attr, value) => s"$attr < $value"
-    case GreaterThan(attr, value) => s"$attr > $value"
-    case LessThanOrEqual(attr, value) => s"$attr <= $value"
-    case GreaterThanOrEqual(attr, value) => s"$attr >= $value"
+    case EqualTo(attr, value) => s"$attr = ${compileValue(value)}"
+    case LessThan(attr, value) => s"$attr < ${compileValue(value)}"
+    case GreaterThan(attr, value) => s"$attr > ${compileValue(value)}"
+    case LessThanOrEqual(attr, value) => s"$attr <= ${compileValue(value)}"
+    case GreaterThanOrEqual(attr, value) => s"$attr >= ${compileValue(value)}"
     case _ => null
   }
 
