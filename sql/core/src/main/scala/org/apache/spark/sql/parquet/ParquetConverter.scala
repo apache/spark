60b9b96b227ff1821530e29210cc0c338ce97528
Daoyuan Wang <daoyuan.wang@intel.com>
2015-03-23 11:46:16 +0800

    [SPARK-4985] [SQL] parquet support for date type
    This PR might have some issues with #3732 ,
    and this would have merge conflicts with #3820 so the review can be delayed till that 2 were merged.
    
    Author: Daoyuan Wang <daoyuan.wang@intel.com>
    
    Closes #3822 from adrian-wang/parquetdate and squashes the following commits:
    
    2c5d54d [Daoyuan Wang] add a test case
    faef887 [Daoyuan Wang] parquet support for primitive date
    97e9080 [Daoyuan Wang] parquet support for date type
    
    (cherry picked from commit 4659468f369d69e7f777130e5e3b4c5d47a624f1)
    Signed-off-by: Cheng Lian <lian@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/parquet/ParquetConverter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/parquet/ParquetConverter.scala
@@ -127,6 +127,12 @@ private[sql] object CatalystConverter {
             parent.updateByte(fieldIndex, value.asInstanceOf[ByteType.JvmType])
         }
       }
+      case DateType => {
+        new CatalystPrimitiveConverter(parent, fieldIndex) {
+          override def addInt(value: Int): Unit =
+            parent.updateDate(fieldIndex, value.asInstanceOf[DateType.JvmType])
+        }
+      }
       case d: DecimalType => {
         new CatalystPrimitiveConverter(parent, fieldIndex) {
           override def addBinary(value: Binary): Unit =
@@ -190,6 +196,9 @@ private[parquet] abstract class CatalystConverter extends GroupConverter {
     updateField(fieldIndex, value)
 
   protected[parquet] def updateInt(fieldIndex: Int, value: Int): Unit =
+    updateField(fieldIndex, value)
+
+  protected[parquet] def updateDate(fieldIndex: Int, value: Int): Unit =
     updateField(fieldIndex, value)
 
   protected[parquet] def updateLong(fieldIndex: Int, value: Long): Unit =
@@ -387,6 +396,9 @@ private[parquet] class CatalystPrimitiveRowConverter(
 
   override protected[parquet] def updateInt(fieldIndex: Int, value: Int): Unit =
     current.setInt(fieldIndex, value)
+
+  override protected[parquet] def updateDate(fieldIndex: Int, value: Int): Unit =
+    current.update(fieldIndex, value)
 
   override protected[parquet] def updateLong(fieldIndex: Int, value: Long): Unit =
     current.setLong(fieldIndex, value)
