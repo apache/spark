85cf0636825d1997d64d0bdc04618f29b7222da1
Kousuke Saruta <sarutak@oss.nttdata.co.jp>
2015-03-24 16:13:25 +0000

    [SPARK-5559] [Streaming] [Test] Remove oppotunity we met flakiness when running FlumeStreamSuite
    When we run FlumeStreamSuite on Jenkins, sometimes we get error like as follows.
    
        sbt.ForkMain$ForkError: The code passed to eventually never returned normally. Attempted 52 times over 10.094849836 seconds. Last failure message: Error connecting to localhost/127.0.0.1:23456.
    	    at org.scalatest.concurrent.Eventually$class.tryTryAgain$1(Eventually.scala:420)
    	    at org.scalatest.concurrent.Eventually$class.eventually(Eventually.scala:438)
    	    at org.scalatest.concurrent.Eventually$.eventually(Eventually.scala:478)
    	    at org.scalatest.concurrent.Eventually$class.eventually(Eventually.scala:307)
    	   at org.scalatest.concurrent.Eventually$.eventually(Eventually.scala:478)
    	   at org.apache.spark.streaming.flume.FlumeStreamSuite.writeAndVerify(FlumeStreamSuite.scala:116)
               at org.apache.spark.streaming.flume.FlumeStreamSuite.org$apache$spark$streaming$flume$FlumeStreamSuite$$testFlumeStream(FlumeStreamSuite.scala:74)
    	   at org.apache.spark.streaming.flume.FlumeStreamSuite$$anonfun$3.apply$mcV$sp(FlumeStreamSuite.scala:66)
    	    at org.apache.spark.streaming.flume.FlumeStreamSuite$$anonfun$3.apply(FlumeStreamSuite.scala:66)
    	    at org.apache.spark.streaming.flume.FlumeStreamSuite$$anonfun$3.apply(FlumeStreamSuite.scala:66)
    	    at org.scalatest.Transformer$$anonfun$apply$1.apply$mcV$sp(Transformer.scala:22)
    	    at org.scalatest.OutcomeOf$class.outcomeOf(OutcomeOf.scala:85)
    	    at org.scalatest.OutcomeOf$.outcomeOf(OutcomeOf.scala:104)
    	    at org.scalatest.Transformer.apply(Transformer.scala:22)
    	    at org.scalatest.Transformer.apply(Transformer.scala:20)
        	    at org.scalatest.FunSuiteLike$$anon$1.apply(FunSuiteLike.scala:166)
    	    at org.scalatest.Suite$class.withFixture(Suite.scala:1122)
    	    at org.scalatest.FunSuite.withFixture(FunSuite.scala:1555)
    	    at org.scalatest.FunSuiteLike$class.invokeWithFixture$1(FunSuiteLike.scala:163)
    	   at org.scalatest.FunSuiteLike$$anonfun$runTest$1.apply(FunSuiteLike.scala:175)
    	    at org.scalatest.FunSuiteLike$$anonfun$runTest$1.apply(FunSuiteLike.scala:175)
    	    at org.scalatest.SuperEngine.runTestImpl(Engine.scala:306)
    	    at org.scalatest.FunSuiteLike$class.runTest(FunSuiteLike.scala:175)
    
    This error is caused by check-then-act logic  when it find free-port .
    
          /** Find a free port */
          private def findFreePort(): Int = {
            Utils.startServiceOnPort(23456, (trialPort: Int) => {
              val socket = new ServerSocket(trialPort)
              socket.close()
              (null, trialPort)
            }, conf)._2
          }
    
    Removing the check-then-act is not easy but we can reduce the chance of having the error by choosing random value for initial port instead of 23456.
    
    Author: Kousuke Saruta <sarutak@oss.nttdata.co.jp>
    
    Closes #4337 from sarutak/SPARK-5559 and squashes the following commits:
    
    16f109f [Kousuke Saruta] Added `require` to Utils#startServiceOnPort
    c39d8b6 [Kousuke Saruta] Merge branch 'SPARK-5559' of github.com:sarutak/spark into SPARK-5559
    1610ba2 [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-5559
    33357e3 [Kousuke Saruta] Changed "findFreePort" method in MQTTStreamSuite and FlumeStreamSuite so that it can choose valid random port
    a9029fe [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-5559
    9489ef9 [Kousuke Saruta] Merge branch 'master' of git://git.apache.org/spark into SPARK-5559
    8212e42 [Kousuke Saruta] Modified default port used in FlumeStreamSuite from 23456 to random value

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -1876,6 +1876,10 @@ private[spark] object Utils extends Logging {
       startService: Int => (T, Int),
       conf: SparkConf,
       serviceName: String = ""): (T, Int) = {
+
+    require(startPort == 0 || (1024 <= startPort && startPort < 65536),
+      "startPort should be between 1024 and 65535 (inclusive), or 0 for a random free port.")
+
     val serviceString = if (serviceName.isEmpty) "" else s" '$serviceName'"
     val maxRetries = portMaxRetries(conf)
     for (offset <- 0 to maxRetries) {
