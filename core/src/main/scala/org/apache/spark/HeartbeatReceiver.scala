c7d5422eb0414eb18357a89b155fc4f12db819bb
zsxwing <zsxwing@gmail.com>
2015-03-26 20:27:57 +0800

    Add comments for executorTimeoutMs and checkTimeoutIntervalMs

--- core/src/main/scala/org/apache/spark/HeartbeatReceiver.scala
+++ core/src/main/scala/org/apache/spark/HeartbeatReceiver.scala
@@ -50,9 +50,13 @@ private[spark] class HeartbeatReceiver(sc: SparkContext, scheduler: TaskSchedule
   // executor ID -> timestamp of when the last heartbeat from this executor was received
   private val executorLastSeen = new mutable.HashMap[String, Long]
 
+  // `spark.network.timeout` use `seconds`,
+  // while `spark.storage.blockManagerSlaveTimeoutMs` use `milliseconds`
   private val executorTimeoutMs = sc.conf.getOption("spark.network.timeout").map(_.toLong * 1000).
     getOrElse(sc.conf.getLong("spark.storage.blockManagerSlaveTimeoutMs", 120000))
 
+  // `spark.network.timeoutInterval` use `seconds`,
+  // while `spark.storage.blockManagerTimeoutIntervalMs` use `milliseconds`
   private val checkTimeoutIntervalMs =
     sc.conf.getOption("spark.network.timeoutInterval").map(_.toLong * 1000).
       getOrElse(sc.conf.getLong("spark.storage.blockManagerTimeoutIntervalMs", 60000))
