09fd80b2782be1ad38c1df63cdc3b501063e98a7
mcheah <mcheah@palantir.com>
2015-03-26 16:28:14 -0700

    Should be >= not >. Slightly more consistent error message.

--- core/src/main/scala/org/apache/spark/serializer/KryoSerializer.scala
+++ core/src/main/scala/org/apache/spark/serializer/KryoSerializer.scala
@@ -50,16 +50,16 @@ class KryoSerializer(conf: SparkConf)
   with Serializable {
 
   private val bufferSizeMb: Double = conf.getDouble("spark.kryoserializer.buffer.mb", 0.064)
-  if (bufferSizeMb > 2048) {
+  if (bufferSizeMb >= 2048) {
     throw new IllegalArgumentException("spark.kryoserializer.buffer.mb must be less than " +
-      s"2048 megabytes, got: + $bufferSizeMb mb.")
+      s"2048 mb, got: + $bufferSizeMb mb.")
   }
   private val bufferSize = (bufferSizeMb * 1024 * 1024).toInt
 
   val maxBufferSizeMb: Int = conf.getInt("spark.kryoserializer.buffer.max.mb", 64)
-  if (maxBufferSizeMb > 2048) {
+  if (maxBufferSizeMb >= 2048) {
     throw new IllegalArgumentException("spark.kryoserializer.buffer.max.mb must be less than " +
-      s"2048 megabytes, got: + $maxBufferSizeMb mb.")
+      s"2048 mb, got: + $maxBufferSizeMb mb.")
   }
   private val maxBufferSize = maxBufferSizeMb * 1024 * 1024
 
