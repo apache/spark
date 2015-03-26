1f98e2d13bc0b9731abbf28cfab35757eea38001
Michael Armbrust <michael@databricks.com>
2015-03-26 16:19:24 -0700

    revert change

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrame.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrame.scala
@@ -136,7 +136,7 @@ class DataFrame private[sql](
     })
   }
 
-  @transient protected[sql] val logicalPlan: LogicalPlan = queryExecution.analyzed match {
+  @transient protected[sql] val logicalPlan: LogicalPlan = queryExecution.logical match {
     // For various commands (like DDL) and queries with side effects, we force query optimization to
     // happen right away to let these side effects take place eagerly.
     case _: Command |
