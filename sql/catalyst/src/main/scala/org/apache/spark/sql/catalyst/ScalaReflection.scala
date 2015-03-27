524be86158be0a328eecb1634dfd12b6264e0836
Eran Medan <ehrann.mehdan@gmail.com>
2015-03-27 17:14:27 -0400

    better exception than scala.MatchError: Any
    Currently if trying to register an RDD as a table that has types that have no supported Schema representation - it would throw a match error. e.g. scala.MatchError: Any (of class scala.reflect.internal.Types$ClassNoArgsTypeRef)
    
    This fix is just to have a nicer error message thank a MatchError

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/ScalaReflection.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/ScalaReflection.scala
@@ -179,6 +179,7 @@ trait ScalaReflection {
       case t if t <:< definitions.ShortTpe => Schema(ShortType, nullable = false)
       case t if t <:< definitions.ByteTpe => Schema(ByteType, nullable = false)
       case t if t <:< definitions.BooleanTpe => Schema(BooleanType, nullable = false)
+      case other => sys.error(s"Schema for type $other is currently not supported")
     }
   }
 
