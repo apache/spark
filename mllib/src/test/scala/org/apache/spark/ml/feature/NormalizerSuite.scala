8a462da2490a3e3555a2dd58f61ea950a8e5a5ff
Xusen Yin <yinxusen@gmail.com>
2015-03-27 12:29:16 +0800

    remove duplications

--- mllib/src/test/scala/org/apache/spark/ml/feature/NormalizerSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/NormalizerSuite.scala
@@ -24,15 +24,19 @@ import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.mllib.util.TestingUtils._
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
-case class DataSet(features: Vector)
+private case class DataSet(features: Vector)
 
 class NormalizerSuite extends FunSuite with MLlibTestSparkContext {
 
   @transient var data: Array[Vector] = _
   @transient var dataFrame: DataFrame = _
+  @transient var normalizer: Normalizer = _
+  @transient var l1Normalized: Array[Vector] = _
+  @transient var l2Normalized: Array[Vector] = _
 
   override def beforeAll(): Unit = {
     super.beforeAll()
+
     data = Array(
       Vectors.sparse(3, Seq((0, -2.0), (1, 2.3))),
       Vectors.dense(0.0, 0.0, 0.0),
@@ -41,54 +45,64 @@ class NormalizerSuite extends FunSuite with MLlibTestSparkContext {
       Vectors.sparse(3, Seq((0, 5.7), (1, 0.72), (2, 2.7))),
       Vectors.sparse(3, Seq())
     )
-    val dataRDD = sc.parallelize(data, 2).map(DataSet)
+    l1Normalized = Array(
+      Vectors.sparse(3, Seq((0, -0.465116279), (1, 0.53488372))),
+      Vectors.dense(0.0, 0.0, 0.0),
+      Vectors.dense(0.12765957, -0.23404255, -0.63829787),
+      Vectors.sparse(3, Seq((1, 0.22141119), (2, 0.7785888))),
+      Vectors.dense(0.625, 0.07894737, 0.29605263),
+      Vectors.sparse(3, Seq())
+    )
+    l2Normalized = Array(
+      Vectors.sparse(3, Seq((0, -0.65617871), (1, 0.75460552))),
+      Vectors.dense(0.0, 0.0, 0.0),
+      Vectors.dense(0.184549876, -0.3383414, -0.922749378),
+      Vectors.sparse(3, Seq((1, 0.27352993), (2, 0.96186349))),
+      Vectors.dense(0.897906166, 0.113419726, 0.42532397),
+      Vectors.sparse(3, Seq())
+    )
+
     val sqlContext = new SQLContext(sc)
-    dataFrame = sqlContext.createDataFrame(dataRDD)
+    dataFrame = sqlContext.createDataFrame(sc.parallelize(data, 2).map(DataSet))
+    normalizer = new Normalizer()
+      .setInputCol("features")
+      .setOutputCol("normalized_features")
+  }
+
+  def collectResult(result: DataFrame): Array[Vector] = {
+    result.select("normalized_features").collect().map {
+      case Row(features: Vector) => features
+    }
+  }
+
+  def assertTypeOfVector(lhs: Array[Vector], rhs: Array[Vector]): Unit = {
+    assert((lhs, rhs).zipped.forall {
+      case (v1: DenseVector, v2: DenseVector) => true
+      case (v1: SparseVector, v2: SparseVector) => true
+      case _ => false
+    }, "The vector type should be preserved after normalization.")
+  }
+
+  def assertValues(lhs: Array[Vector], rhs: Array[Vector]): Unit = {
+    assert((lhs, rhs).zipped.forall { (vector1, vector2) =>
+      vector1 ~== vector2 absTol 1E-5
+    }, "The vector value is not correct after normalization.")
   }
 
   test("Normalization with default parameter") {
-    val normalizer = new Normalizer()
-      .setInputCol("features")
-      .setOutputCol("normalized_features")
-    val transformed = normalizer.transform(dataFrame)
-    val result = transformed.select("normalized_features").collect().map {
-      case Row(features: Vector) => features
-    }
+    val result = collectResult(normalizer.transform(dataFrame))
 
-    assert((data, result).zipped.forall {
-      case (v1: DenseVector, v2: DenseVector) => true
-      case (v1: SparseVector, v2: SparseVector) => true
-      case _ => false
-    }, "The vector type should be preserved after normalization.")
+    assertTypeOfVector(data, result)
 
-    assert(result(0) ~== Vectors.sparse(3, Seq((0, -0.65617871), (1, 0.75460552))) absTol 1E-5)
-    assert(result(1) ~== Vectors.dense(0.0, 0.0, 0.0) absTol 1E-5)
-    assert(result(2) ~== Vectors.dense(0.184549876, -0.3383414, -0.922749378) absTol 1E-5)
-    assert(result(3) ~== Vectors.sparse(3, Seq((1, 0.27352993), (2, 0.96186349))) absTol 1E-5)
-    assert(result(4) ~== Vectors.dense(0.897906166, 0.113419726, 0.42532397) absTol 1E-5)
-    assert(result(5) ~== Vectors.sparse(3, Seq()) absTol 1E-5)
+    assertValues(result, l2Normalized)
   }
   test("Normalization with setter") {
-    val normalizer = new Normalizer()
-      .setInputCol("features")
-      .setOutputCol("normalized_features")
     normalizer.setP(1)
-    val transformed = normalizer.transform(dataFrame)
-    val result = transformed.select("normalized_features").collect().map {
-      case Row(features: Vector) => features
-    }
 
-    assert((data, result).zipped.forall {
-      case (v1: DenseVector, v2: DenseVector) => true
-      case (v1: SparseVector, v2: SparseVector) => true
-      case _ => false
-    }, "The vector type should be preserved after normalization.")
+    val result = collectResult(normalizer.transform(dataFrame))
 
-    assert(result(0) ~== Vectors.sparse(3, Seq((0, -0.465116279), (1, 0.53488372))) absTol 1E-5)
-    assert(result(1) ~== Vectors.dense(0.0, 0.0, 0.0) absTol 1E-5)
-    assert(result(2) ~== Vectors.dense(0.12765957, -0.23404255, -0.63829787) absTol 1E-5)
-    assert(result(3) ~== Vectors.sparse(3, Seq((1, 0.22141119), (2, 0.7785888))) absTol 1E-5)
-    assert(result(4) ~== Vectors.dense(0.625, 0.07894737, 0.29605263) absTol 1E-5)
-    assert(result(5) ~== Vectors.sparse(3, Seq()) absTol 1E-5)
+    assertTypeOfVector(data, result)
+
+    assertValues(result, l1Normalized)
   }
 }
