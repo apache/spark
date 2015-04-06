ef1fc2f8b115a722ee769bbe847fa3d7a3c3ebd2
Davies Liu <davies@databricks.com>
2015-04-06 14:33:48 -0700

    fix tests

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -95,14 +95,13 @@ class DataFrame(object):
         """
         return DataFrameNaFunctions(self)
 
-    @ignore_unicode_prefix
     def toJSON(self, use_unicode=False):
         """Converts a :class:`DataFrame` into a :class:`RDD` of string.
 
         Each row is turned into a JSON document as one element in the returned RDD.
 
         >>> df.toJSON().first()
-        u'{"age":2,"name":"Alice"}'
+        '{"age":2,"name":"Alice"}'
         """
         rdd = self._jdf.toJSON()
         return RDD(rdd.toJavaRDD(), self._sc, UTF8Deserializer(use_unicode))
