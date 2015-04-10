7741d66943d81621898de496b00af3eb3e6968b7
cafreeman <cfreeman@alteryx.com>
2015-04-10 08:45:10 -0500

    Rename the SQL DataType function

--- sql/core/src/main/scala/org/apache/spark/sql/api/r/SQLUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/api/r/SQLUtils.scala
@@ -43,7 +43,7 @@ private[r] object SQLUtils {
     StructType(fields)
   }
 
-  def DataTypeObject(dataType: String): DataType = {
+  def getSQLDataType(dataType: String): DataType = {
     dataType match {
       case "byte" => org.apache.spark.sql.types.ByteType
       case "integer" => org.apache.spark.sql.types.IntegerType
@@ -62,7 +62,7 @@ private[r] object SQLUtils {
   }
 
   def createStructField(name: String, dataType: String, nullable: Boolean): StructField = {
-    val dtObj = DataTypeObject(dataType)
+    val dtObj = getSQLDataType(dataType)
     StructField(name, dtObj, nullable)
   }
 
