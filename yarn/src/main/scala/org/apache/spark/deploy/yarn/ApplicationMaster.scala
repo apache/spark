6cc5b3ed3c0c729f97956fa017d8eb7d6b43f90f
Cheolsoo Park <cheolsoop@netflix.com>
2015-04-13 13:45:10 -0500

    [SPARK-6662][YARN] Allow variable substitution in spark.yarn.historyServer.address
    In Spark on YARN, explicit hostname and port number need to be set for "spark.yarn.historyServer.address" in SparkConf to make the HISTORY link. If the history server address is known and static, this is usually not a problem.
    
    But in cloud, that is usually not true. Particularly in EMR, the history server always runs on the same node as with RM. So I could simply set it to ${yarn.resourcemanager.hostname}:18080 if variable substitution is allowed.
    
    In fact, Hadoop configuration already implements variable substitution, so if this property is read via YarnConf, this can be easily achievable.
    
    Author: Cheolsoo Park <cheolsoop@netflix.com>
    
    Closes #5321 from piaozhexiu/SPARK-6662 and squashes the following commits:
    
    e37de75 [Cheolsoo Park] Preserve the space between the Hadoop and Spark imports
    79757c6 [Cheolsoo Park] Incorporate review comments
    10e2917 [Cheolsoo Park] Add helper function that substitutes hadoop vars to SparkHadoopUtil
    589b52c [Cheolsoo Park] Revert "Allow variable substitution for spark.yarn. properties"
    ff9c35d [Cheolsoo Park] Allow variable substitution for spark.yarn. properties

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/ApplicationMaster.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/ApplicationMaster.scala
@@ -223,6 +223,7 @@ private[spark] class ApplicationMaster(
     val appId = client.getAttemptId().getApplicationId().toString()
     val historyAddress =
       sparkConf.getOption("spark.yarn.historyServer.address")
+        .map { text => SparkHadoopUtil.get.substituteHadoopVariables(text, yarnConf) }
         .map { address => s"${address}${HistoryServer.UI_PATH_PREFIX}/${appId}" }
         .getOrElse("")
 
