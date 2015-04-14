1dc044452faf11062aa6f54f01c5d29094ebc113
Ilya Ganelin <ilya.ganelin@capitalone.com>
2015-04-14 11:38:23 -0700

    Added ref equality check

--- core/src/test/scala/org/apache/spark/SparkContextSuite.scala
+++ core/src/test/scala/org/apache/spark/SparkContextSuite.scala
@@ -78,6 +78,7 @@ class SparkContextSuite extends FunSuite with LocalSparkContext {
     sc2 = SparkContext.getOrCreate(new SparkConf().setAppName("test2").setMaster("local"))
     assert(sc2.getConf.get("spark.app.name").equals("test"))
     assert(sc === sc2)
+    assert(sc eq sc2)
     
     // Try creating second context to confirm that it's still possible, if desired
     sc2 = new SparkContext(new SparkConf().setAppName("test3").setMaster("local")
