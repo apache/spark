e0c18f36a49e6f55fb30f00e5e38b5b0d7b18f24
Zhongshuai Pei <799203320@qq.com>
2015-04-16 10:12:46 +0800

    to adapter hive0.12
    hive0.12 do not have org.apache.hadoop.hive.ql.udf.generic.UDFCurrentDB

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/sqlUDFCurrentDB.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/sqlUDFCurrentDB.scala
@@ -1,18 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 package org.apache.spark.sql.hive
 
-import org.apache.hadoop.hive.ql.udf.generic.UDFCurrentDB
 import org.apache.hadoop.hive.ql.exec.Description
-import org.apache.hadoop.hive.ql.udf.generic.GenericUDF.DeferredObject
 import org.apache.hadoop.hive.ql.session.SessionState
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF
 
 // deterministic in the query range
 @Description(name = "current_database",
     value = "_FUNC_() - returns currently using database name")
-class sqlUDFCurrentDB extends UDFCurrentDB {
+class sqlUDFCurrentDB extends GenericUDF {
 
-  // This function just throws an exception in hive0.13
-  override def evaluate(arguments: Array[DeferredObject]): Object = {
-    SessionState.get().getCurrentDatabase()
+  def initialize(arguments: Array[ObjectInspector]): ObjectInspector = {
+    val database = SessionState.get.getCurrentDatabase
+    PrimitiveObjectInspectorFactory.getPrimitiveWritableConstantObjectInspector(
+      TypeInfoFactory.stringTypeInfo, new Text(database))
+  }
+
+  def evaluate(arguments: Array[GenericUDF.DeferredObject]): Object = {
+     SessionState.get.getCurrentDatabase
+  }
+
+  def getDisplayString(children: Array[String]): String = {
+    return "current_database()"
   }
 }
-
