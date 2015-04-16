16b76681808d0c84f3c63e83709c7440a1565f8d
rakeshchalasani <vnit.rakesh@gmail.com>
2015-04-15 23:46:02 -0400

    [SPARK-6867][MLlib] Add dropout regularization to logistic regression with SGD.
    Check JIRA https://issues.apache.org/jira/browse/SPARK-6867 for details.
    
    Author: Rakesh Chalasani <vnit.rakesh@gmail.com>
    
    Add gradient with dropout and logistic regression with dropout regularization
    
    Add LR dropout to examples

--- mllib/src/main/scala/org/apache/spark/mllib/optimization/Gradient.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/optimization/Gradient.scala
@@ -21,6 +21,8 @@ import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.mllib.linalg.{DenseVector, Vector, Vectors}
 import org.apache.spark.mllib.linalg.BLAS.{axpy, dot, scal}
 import org.apache.spark.mllib.util.MLUtils
+import breeze.stats.{distributions => brzDist}
+import breeze.linalg.{DenseVector => BDV}
 
 /**
  * :: DeveloperApi ::
@@ -255,6 +257,30 @@ class LogisticGradient(numClasses: Int) extends Gradient {
   }
 }
 
+/**
+ * :: DeveloperApi ::
+ * Compute gradient and loss for a multinomial logistic loss function with drop-out regularization,
+ * as used in multi-class classification (it is also used in binary logistic regression).
+ * Check [[LogisticGradient]] for details on multinomial logistic loss function.
+ * */
+@DeveloperApi
+class DropoutLogisticGradient(p: Double = 0.5, numClasses: Int) extends LogisticGradient(numClasses) {
+
+  def this(p: Double) = this(p, 2)
+
+  override def compute(
+      data: Vector,
+      label: Double,
+      weights: Vector,
+      cumGradient: Vector): Double = {
+
+    // Drop-out some features randomly with probability p.
+    val dropoutDist = brzDist.Bernoulli.distribution(p)
+    val droppedData = data.mapActive((idx:Int, value:Double) => (idx, if(dropoutDist.draw()) value else 0.0))
+    super.compute(droppedData, label, weights, cumGradient)
+  }
+
+}
 /**
  * :: DeveloperApi ::
  * Compute gradient and loss for a Least-squared loss function, as used in linear regression.
