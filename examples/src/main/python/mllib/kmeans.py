17e5a52f3af803969090ab9d824734b9c0108700
Hrishikesh Subramonian <hrishikesh.subramonian@flytxt.com>
2015-04-17 15:14:51 +0530

    python 3 support for computeCost

--- examples/src/main/python/mllib/kmeans.py
+++ examples/src/main/python/mllib/kmeans.py
@@ -43,5 +43,5 @@ if __name__ == "__main__":
     k = int(sys.argv[2])
     model = KMeans.train(data, k)
     print("Final centers: " + str(model.clusterCenters))
-    print("Total Cost: ", model.computeCost(data))
+    print("Total Cost: " + str(model.computeCost(data)))
     sc.stop()
