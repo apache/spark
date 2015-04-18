59e5e388c1bb20a8e86e0091c3210900f56fb90a
Aaron Davidson <aaron@databricks.com>
2015-04-18 10:46:05 -0700

    [SPARK-6955] Perform port retries at NettyBlockTransferService level
    Currently we're doing port retries in the TransportServer level, but this is not specified by the TransportContext API and it has other further-reaching impacts like causing undesirable behvior for the Yarn and Standalone shuffle services.

--- core/src/main/scala/org/apache/spark/network/netty/NettyBlockTransferService.scala
+++ core/src/main/scala/org/apache/spark/network/netty/NettyBlockTransferService.scala
@@ -58,11 +58,21 @@ class NettyBlockTransferService(conf: SparkConf, securityManager: SecurityManage
         securityManager.isSaslEncryptionEnabled()))
     }
     transportContext = new TransportContext(transportConf, rpcHandler)
-    clientFactory = transportContext.createClientFactory(clientBootstrap.toList)
-    server = transportContext.createServer(conf.getInt("spark.blockManager.port", 0),
-      serverBootstrap.toList)
+    clientFactory = transportContext.createClientFactory(bootstrap.toList)
+    server = createServer(serverBootstrap.toList)
     appId = conf.getAppId
     logInfo("Server created on " + server.getPort)
+  }
+
+  /** Creates and binds the TransportServer, possibly trying multiple ports. */
+  private def createServer(bootstraps: List[TransportServerBootstrap]): TransportServer = {
+    def startService(port: Int): (TransportServer, Int) = {
+      val server = transportContext.createServer(port, bootstraps)
+      (server, server.getPort)
+    }
+
+    val portToTry = conf.getInt("spark.blockManager.port", 0)
+    Utils.startServiceOnPort(portToTry, startService, conf, getClass.getName)._1
   }
 
   override def fetchBlocks(
