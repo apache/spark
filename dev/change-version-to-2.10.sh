7b8e10b8934853cea4c90da73c6a42c78297a3f6
Tijo Thomas <tijoparacka@gmail.com>
2015-04-23 17:42:33 +0530

    Modified for providing the base directories while finding the list of pom files and also while changing the version no

--- dev/change-version-to-2.10.sh
+++ dev/change-version-to-2.10.sh
@@ -18,9 +18,10 @@
 #
 
 # Note that this will not necessarily work as intended with non-GNU sed (e.g. OS X)
+BASEDIR=$(dirname $0)/..
 
-find . -name 'pom.xml' | grep -v target \
+find $BASEDIR -name 'pom.xml' | grep -v target \
   | xargs -I {} sed -i -e 's/\(artifactId.*\)_2.11/\1_2.10/g' {}
 
 # Also update <scala.binary.version> in parent POM
-sed -i -e '0,/<scala\.binary\.version>2.11</s//<scala.binary.version>2.10</' pom.xml
+sed -i -e '0,/<scala\.binary\.version>2.11</s//<scala.binary.version>2.10</' $BASEDIR/pom.xml
