aa20408837af0b31b55ec097e3df2e0e51e6217b
tianyi <tianyi.asiainfo@gmail.com>
2015-04-28 15:17:47 +0800

    fix style problem

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/UISeleniumSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/UISeleniumSuite.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.hive.thriftserver
 
 import java.sql.{DriverManager, Statement}
@@ -103,7 +120,7 @@ class UISeleniumSuite extends FunSuite with WebBrowser with Matchers with Before
       }
 
       eventually(timeout(10 seconds), interval(50 milliseconds)) {
-        go to (hc.sparkContext.ui.get.appUIAddress.stripSuffix("/")+"/ThriftServer")
+        go to (hc.sparkContext.ui.get.appUIAddress.stripSuffix("/") + "/ThriftServer")
         find(id("sessionstat")) should not be(None)
         find(id("sqlstat")) should not be(None)
 
