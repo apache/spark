203a29540841504324e11c350d5b92eb84bdb099
DB Tsai <dbt@netflix.com>
2015-04-28 17:53:43 -0700

    Add more documentation to LinearRegression in new ML framework.

--- mllib/src/main/scala/org/apache/spark/ml/regression/LinearRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/regression/LinearRegression.scala
@@ -110,59 +110,66 @@ class LinearRegression extends Regressor[Vector, LinearRegression, LinearRegress
     val yMean = statCounter.mean
     val yStd = math.sqrt(statCounter.variance)
 
-    val featuresMean = summarizer.mean.toArray
-    val featuresStd = summarizer.variance.toArray.map(math.sqrt)
+    val model = if (yStd != 0.0) {
+      val featuresMean = summarizer.mean.toArray
+      val featuresStd = summarizer.variance.toArray.map(math.sqrt)
 
-    // Since we implicitly do the feature scaling when we compute the cost function
-    // to improve the convergence, the effective regParam will be changed.
-    val effectiveRegParam = paramMap(regParam) / yStd
-    val effectiveL1RegParam = paramMap(elasticNetParam) * effectiveRegParam
-    val effectiveL2RegParam = (1.0 - paramMap(elasticNetParam)) * effectiveRegParam
+      // Since we implicitly do the feature scaling when we compute the cost function
+      // to improve the convergence, the effective regParam will be changed.
+      val effectiveRegParam = paramMap(regParam) / yStd
+      val effectiveL1RegParam = paramMap(elasticNetParam) * effectiveRegParam
+      val effectiveL2RegParam = (1.0 - paramMap(elasticNetParam)) * effectiveRegParam
 
-    val costFun = new LeastSquaresCostFun(instances, yStd, yMean,
-      featuresStd, featuresMean, effectiveL2RegParam)
+      val costFun = new LeastSquaresCostFun(instances, yStd, yMean,
+        featuresStd, featuresMean, effectiveL2RegParam)
 
-    val optimizer = if (paramMap(elasticNetParam) == 0.0 || effectiveRegParam == 0.0) {
-      new BreezeLBFGS[BDV[Double]](paramMap(maxIter), 10, paramMap(tol))
-    } else {
-      new BreezeOWLQN[Int, BDV[Double]](paramMap(maxIter), 10, effectiveL1RegParam, paramMap(tol))
-    }
-
-    val initialWeights = Vectors.zeros(numFeatures)
-    val states =
-      optimizer.iterations(new CachedDiffFunction(costFun), initialWeights.toBreeze.toDenseVector)
-
-    var state = states.next()
-    val lossHistory = mutable.ArrayBuilder.make[Double]
-
-    while (states.hasNext) {
-      lossHistory += state.value
-      state = states.next()
-    }
-    lossHistory += state.value
-
-    // TODO: Based on the sparsity of weights, we may convert the weights to the sparse vector.
-    // The weights are trained in the scaled space; we're converting them back to
-    // the original space.
-    val weights = {
-      val rawWeights = state.x.toArray.clone()
-      var i = 0
-      while (i < rawWeights.length) {
-        rawWeights(i) *= { if (featuresStd(i) != 0.0) yStd / featuresStd(i) else 0.0 }
-        i += 1
+      val optimizer = if (paramMap(elasticNetParam) == 0.0 || effectiveRegParam == 0.0) {
+        new BreezeLBFGS[BDV[Double]](paramMap(maxIter), 10, paramMap(tol))
+      } else {
+        new BreezeOWLQN[Int, BDV[Double]](paramMap(maxIter), 10,
+          effectiveL1RegParam, paramMap(tol))
       }
-      Vectors.dense(rawWeights)
+
+      val initialWeights = Vectors.zeros(numFeatures)
+      val states = optimizer.iterations(new CachedDiffFunction(costFun),
+        initialWeights.toBreeze.toDenseVector)
+
+      var state = states.next()
+      val lossHistory = mutable.ArrayBuilder.make[Double]
+
+      while (states.hasNext) {
+        lossHistory += state.value
+        state = states.next()
+      }
+      lossHistory += state.value
+
+      // TODO: Based on the sparsity of weights, we may convert the weights to the sparse vector.
+      // The weights are trained in the scaled space; we're converting them back to
+      // the original space.
+      val weights = {
+        val rawWeights = state.x.toArray.clone()
+        var i = 0
+        while (i < rawWeights.length) {
+          rawWeights(i) *= {
+            if (featuresStd(i) != 0.0) yStd / featuresStd(i) else 0.0
+          }
+          i += 1
+        }
+        Vectors.dense(rawWeights)
+      }
+
+      // The intercept in R's GLMNET is computed using closed form after the coefficients are
+      // converged. See the following discussion for detail.
+      // http://stats.stackexchange.com/questions/13617/how-is-the-intercept-computed-in-glmnet
+      val intercept = yMean - dot(weights, Vectors.dense(featuresMean))
+      new LinearRegressionModel(this, paramMap, weights, intercept)
+    } else {
+      new LinearRegressionModel(this, paramMap, Vectors.zeros(numFeatures), yMean)
     }
-
-    // The intercept in R's GLMNET is computed using closed form after the coefficients are
-    // converged. See the following discussion for detail.
-    // http://stats.stackexchange.com/questions/13617/how-is-the-intercept-computed-in-glmnet
-    val intercept = yMean - dot(weights, Vectors.dense(featuresMean))
-
     if (handlePersistence) {
       instances.unpersist()
     }
-    new LinearRegressionModel(this, paramMap, weights, intercept)
+    model
   }
 }
 
@@ -198,15 +205,74 @@ class LinearRegressionModel private[ml] (
  * Two LeastSquaresAggregator can be merged together to have a summary of loss and gradient of
  * the corresponding joint dataset.
  *
-
- *  * Compute gradient and loss for a Least-squared loss function, as used in linear regression.
- * This is correct for the averaged least squares loss function (mean squared error)
- *              L = 1/2n ||A weights-y||^2
- * See also the documentation for the precise formulation.
+ * For improving the convergence rate during the optimization process, and also preventing against
+ * features with very large variances exerting an overly large influence during model training,
+ * package like R's GLMNET performs the scaling to unit variance and removing the mean to reduce
+ * the condition number, and then trains the model in scaled space but returns the weights in
+ * the original scale. See page 9 in http://cran.r-project.org/web/packages/glmnet/glmnet.pdf
  *
- * @param weights weights/coefficients corresponding to features
+ * However, we don't want to apply the `StandardScaler` on the training dataset, and then cache
+ * the standardized dataset since it will create a lot of overhead. As a result, we perform the
+ * scaling implicitly when we compute the objective function. The following is the mathematical
+ * derivation.
  *
- * @param updater Updater to be used to update weights after every iteration.
+ * Note that we don't deal with intercept by adding bias here, because the intercept
+ * can be computed using closed form after the coefficients are converged.
+ * See this discussion for detail.
+ * http://stats.stackexchange.com/questions/13617/how-is-the-intercept-computed-in-glmnet
+ *
+ * The objective function in the scaled space is given by
+ * {{{
+ * L = 1/2n ||\sum_i w_i(x_i - \bar{x_i}) / \hat{x_i} - (y - \bar{y}) / \hat{y}||^2,
+ * }}}
+ * where \bar{x_i} is the mean of x_i, \hat{x_i} is the standard deviation of x_i,
+ * \bar{y} is the mean of label, and \hat{y} is the standard deviation of label.
+ *
+ * This can be rewritten as
+ * {{{
+ * L = 1/2n ||\sum_i (w_i/\hat{x_i})x_i - \sum_i (w_i/\hat{x_i})\bar{x_i} - y / \hat{y}
+ *     + \bar{y} / \hat{y}||^2
+ *   = 1/2n ||\sum_i w_i^\prime x_i - y / \hat{y} + offset||^2 = 1/2n diff^2
+ * }}}
+ * where w_i^\prime is the effective weights defined by w_i/\hat{x_i}, offset is
+ * {{{
+ * - \sum_i (w_i/\hat{x_i})\bar{x_i} + \bar{y} / \hat{y}.
+ * }}}, and diff is
+ * {{{
+ * \sum_i w_i^\prime x_i - y / \hat{y} + offset
+ * }}}
+ *
+ * Note that the effective weights and offset don't depend on training dataset,
+ * so they can be precomputed.
+ *
+ * Now, the first derivative of the objective function in scaled space is
+ * {{{
+ * \frac{\partial L}{\partial\w_i} = diff/N (x_i - \bar{x_i}) / \hat{x_i}
+ * }}}
+ * However, ($x_i - \bar{x_i}$) will densify the computation, so it's not
+ * an ideal formula when the training dataset is sparse format.
+ *
+ * This can be addressed by adding the dense \bar{x_i} / \har{x_i} terms
+ * in the end by keeping the sum of diff. The first derivative of total
+ * objective function from all the samples is
+ * {{{
+ * \frac{\partial L}{\partial\w_i} =
+ *     1/N \sum_j diff_j (x_{ij} - \bar{x_i}) / \hat{x_i}
+ *   = 1/N ((\sum_j diff_j x_{ij} / \hat{x_i}) - diffSum \bar{x_i}) / \hat{x_i})
+ *   = 1/N ((\sum_j diff_j x_{ij} / \hat{x_i}) + correction_i)
+ * }}},
+ * where correction_i = - diffSum \bar{x_i}) / \hat{x_i}
+ *
+ * As a result, the first term in the first derivative of the total objective function
+ * depends on training dataset, which can be computed in distributed fashion, and is sparse
+ * format friendly. We only have to loop through the whole gradientSum vector one time
+ * in the end for adding the correction terms back (in the driver, not in the executors).
+ *
+ * @param weights The weights/coefficients corresponding to the features.
+ * @param labelStd The standard deviation value of the label.
+ * @param labelMean The mean value of the label.
+ * @param featuresStd The standard deviation values of the features.
+ * @param featuresMean The mean values of the features.
  */
 private class LeastSquaresAggregator(
     weights: Vector,
@@ -301,19 +367,17 @@ private class LeastSquaresAggregator(
   def loss: Double = lossSum / totalCnt
 
   def gradient: Vector = {
-    val result = Vectors.dense(gradientSumArray.clone())
+    val resultArray = gradientSumArray.clone()
 
-    val correction = {
-      val temp = effectiveWeightsArray.clone()
-      var i = 0
-      while (i < temp.length) {
-        temp(i) *= featuresMean(i)
-        i += 1
+    // Adding the correction terms back to gradientSum;
+    // see the mathematical derivation for detail.
+    Vectors.dense(featuresMean).foreachActive { (index, value) =>
+      if (featuresStd(index) != 0.0 && value != 0.0) {
+        resultArray(index) -= diffSum * value / featuresStd(index)
       }
-      Vectors.dense(temp)
     }
 
-    axpy(-diffSum, correction, result)
+    val result = Vectors.dense(resultArray)
     scal(1.0 / totalCnt, result)
     result
   }
