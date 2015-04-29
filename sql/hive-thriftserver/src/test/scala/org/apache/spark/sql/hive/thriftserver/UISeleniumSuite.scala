c0f2fa0a136dc0cb0750c1d6c3cabfdfc843bc72
tianyi <tianyi.asiainfo@gmail.com>
2015-04-29 14:12:13 +0800

    extends HiveThriftJdbcTest to start/stop thriftserver for UI test

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/UISeleniumSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/UISeleniumSuite.scala
@@ -17,110 +17,79 @@
 
 package org.apache.spark.sql.hive.thriftserver
 
-import java.sql.{DriverManager, Statement}
 
-import org.apache.hadoop.hive.conf.HiveConf.ConfVars
-import org.apache.hive.jdbc.HiveDriver
-import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.sql.hive.thriftserver.HiveThriftServer2.HiveThriftServer2Listener
-import org.apache.spark.sql.hive.thriftserver.ui.ThriftServerTab
-import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.util.Random
+
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
-import org.scalatest.{Matchers, BeforeAndAfterAll, FunSuite}
+import org.scalatest.{Matchers, BeforeAndAfterAll}
 import org.scalatest.concurrent.Eventually._
 import org.scalatest.selenium.WebBrowser
 import org.scalatest.time.SpanSugar._
 
+import org.apache.hadoop.hive.conf.HiveConf.ConfVars
+import org.apache.spark.sql.hive.HiveContext
 
-import scala.io.Source
-import scala.util.Random
 
-/**
- * Created by tianyi on 4/27/15.
- */
-class UISeleniumSuite extends FunSuite with WebBrowser with Matchers with BeforeAndAfterAll {
+class UISeleniumSuite extends HiveThriftJdbcTest with WebBrowser with Matchers with BeforeAndAfterAll {
 
   implicit var webDriver: WebDriver = _
   var server: HiveThriftServer2 = _
   var hc: HiveContext = _
   val uiPort = 20000 + Random.nextInt(10000)
-  val listenPort = 10000 + Random.nextInt(10000)
+  override def mode: ServerMode.Value = ServerMode.binary
 
   override def beforeAll(): Unit = {
     webDriver = new HtmlUnitDriver
-    startThriftServer
+    super.beforeAll()
   }
 
   override def afterAll(): Unit = {
     if (webDriver != null) {
       webDriver.quit()
     }
-    if (server != null) {
-      stopThriftServer
+    super.afterAll()
+  }
+
+  override protected def serverStartCommand(port: Int) = {
+    val portConf = if (mode == ServerMode.binary) {
+      ConfVars.HIVE_SERVER2_THRIFT_PORT
+    } else {
+      ConfVars.HIVE_SERVER2_THRIFT_HTTP_PORT
     }
-  }
 
-  def withMultipleConnectionJdbcStatement(fs: (Statement => Unit)*) {
-    val user = System.getProperty("user.name")
-    Class.forName(classOf[HiveDriver].getCanonicalName)
-    val connections = fs.map {
-      _ => DriverManager.getConnection(s"jdbc:hive2://localhost:$listenPort/", user, "")
-    }
-    val statements = connections.map(_.createStatement())
-
-    try {
-      statements.zip(fs).map { case (s, f) => f(s) }
-    } finally {
-      statements.map(_.close())
-      connections.map(_.close())
-    }
-  }
-
-  def withJdbcStatement(f: Statement => Unit) {
-    withMultipleConnectionJdbcStatement(f)
-  }
-
-  /**
-   * Create a test SparkContext with the SparkUI enabled.
-   * It is safe to `get` the SparkUI directly from the SparkContext returned here.
-   */
-  private def startThriftServer: Unit = {
-    val conf = new SparkConf()
-      .setMaster("local")
-      .setAppName("test")
-      .set("spark.ui.enabled", "true")
-      .set("spark.ui.port", uiPort.toString)
-    hc = new HiveContext(new SparkContext(conf))
-    hc.hiveconf.set(ConfVars.HIVE_SERVER2_THRIFT_PORT.toString, listenPort.toString)
-    server = new HiveThriftServer2(hc)
-    server.init(hc.hiveconf)
-    server.start()
-    HiveThriftServer2.listener = new HiveThriftServer2Listener(server, hc.conf)
-    hc.sparkContext.addSparkListener(HiveThriftServer2.listener)
-    HiveThriftServer2.uiTab = Some(new ThriftServerTab(hc.sparkContext))
-  }
-
-  private def stopThriftServer: Unit = {
-    server.stop()
+    s"""$startScript
+        |  --master local
+        |  --hiveconf hive.root.logger=INFO,console
+        |  --hiveconf ${ConfVars.METASTORECONNECTURLKEY}=$metastoreJdbcUri
+        |  --hiveconf ${ConfVars.METASTOREWAREHOUSE}=$warehousePath
+        |  --hiveconf ${ConfVars.HIVE_SERVER2_THRIFT_BIND_HOST}=localhost
+        |  --hiveconf ${ConfVars.HIVE_SERVER2_TRANSPORT_MODE}=$mode
+        |  --hiveconf $portConf=$port
+        |  --driver-class-path ${sys.props("java.class.path")}
+        |  --conf spark.ui.enabled=true
+        |  --conf spark.ui.port=$uiPort
+     """.stripMargin.split("\\s+").toSeq
   }
 
   test("thrift server ui test") {
     withJdbcStatement(statement =>{
+      val baseURL = s"http://localhost:${uiPort}"
+
       val queries = Seq(
-        "DROP TABLE IF EXISTS test_map",
         "CREATE TABLE test_map(key INT, value STRING)",
         s"LOAD DATA LOCAL INPATH '${TestData.smallKv}' OVERWRITE INTO TABLE test_map")
 
       queries.foreach(statement.execute)
 
       eventually(timeout(10 seconds), interval(50 milliseconds)) {
-        go to (hc.sparkContext.ui.get.appUIAddress.stripSuffix("/"))
+        go to (baseURL)
         find(cssSelector("""ul li a[href*="ThriftServer"]""")) should not be(None)
       }
 
       eventually(timeout(10 seconds), interval(50 milliseconds)) {
-        go to (hc.sparkContext.ui.get.appUIAddress.stripSuffix("/") + "/ThriftServer")
+        go to (baseURL + "/ThriftServer")
         find(id("sessionstat")) should not be(None)
         find(id("sqlstat")) should not be(None)
 
