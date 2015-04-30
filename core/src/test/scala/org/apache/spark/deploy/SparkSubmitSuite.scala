47bf406d608c4777f5f383ba439608f673034a1d
Patrick Wendell <patrick@databricks.com>
2015-04-30 01:02:33 -0700

    [HOTFIX] Disabling flaky test (fix in progress as part of SPARK-7224)

--- core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
@@ -334,7 +334,7 @@ class SparkSubmitSuite extends FunSuite with Matchers with ResetSystemProperties
     runSparkSubmit(args)
   }
 
-  test("includes jars passed in through --packages") {
+  ignore("includes jars passed in through --packages") {
     val unusedJar = TestUtils.createJarWithClasses(Seq.empty)
     val packagesString = "com.databricks:spark-csv_2.10:0.1,com.databricks:spark-avro_2.10:0.1"
     val args = Seq(
