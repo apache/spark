856a571ef4f0338ccefb3b792e1e96b8f15a0884
Dean Chen <deanchen5@gmail.com>
2015-05-02 23:04:13 +0100

    [SPARK-3444] Fix typo in Dataframes.py introduced in []
    Author: Dean Chen <deanchen5@gmail.com>
    
    Closes #5866 from deanchen/patch-1 and squashes the following commits:
    
    0028bc4 [Dean Chen] Fix typo in Dataframes.py introduced in [SPARK-3444]

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -1,6 +1,6 @@
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
-# contir[butor license agreements.  See the NOTICE file distributed with
+# contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
 # The ASF licenses this file to You under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with
