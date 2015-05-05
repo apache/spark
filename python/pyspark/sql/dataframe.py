18340d7be55a6834918956555bf820c96769aa52
Burak Yavuz <brkyvz@gmail.com>
2015-05-05 11:01:25 -0700

    [SPARK-7243][SQL] Reduce  size for Contingency Tables in DataFrames
    Reduced take size from 1e8 to 1e6.
    
    cc rxin
    
    Author: Burak Yavuz <brkyvz@gmail.com>
    
    Closes #5900 from brkyvz/df-cont-followup and squashes the following commits:
    
    c11e762 [Burak Yavuz] fix grammar
    b30ace2 [Burak Yavuz] address comments
    a417ba5 [Burak Yavuz] [SPARK-7243][SQL] Reduce  size for Contingency Tables in DataFrames

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -934,10 +934,11 @@ class DataFrame(object):
     def crosstab(self, col1, col2):
         """
         Computes a pair-wise frequency table of the given columns. Also known as a contingency
-        table. The number of distinct values for each column should be less than 1e4. The first
-        column of each row will be the distinct values of `col1` and the column names will be the
-        distinct values of `col2`. The name of the first column will be `$col1_$col2`. Pairs that
-        have no occurrences will have `null` as their counts.
+        table. The number of distinct values for each column should be less than 1e4. At most 1e6
+        non-zero pair frequencies will be returned.
+        The first column of each row will be the distinct values of `col1` and the column names
+        will be the distinct values of `col2`. The name of the first column will be `$col1_$col2`.
+        Pairs that have no occurrences will have `null` as their counts.
         :func:`DataFrame.crosstab` and :func:`DataFrameStatFunctions.crosstab` are aliases.
 
         :param col1: The name of the first column. Distinct items will make the first item of
