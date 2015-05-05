23483e446f2cc56dd23fbfff95bd524c3fb196cd
Marcelo Vanzin <vanzin@cloudera.com>
2015-05-05 15:05:27 -0700

    Fat fingers.

--- core/src/main/scala/org/apache/spark/deploy/history/HistoryServer.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/HistoryServer.scala
@@ -89,7 +89,7 @@ class HistoryServer(
       // Since we may have applications with multiple attempts mixed with applications with a
       // single attempt, we need to try both. Try the single-attempt route first, and if an
       // error is raised, then try the multiple attempt route.
-      if (!loadAppUi(appId, None && (!attemptId.isDefined || !loadAppUi(appId, attemptId)) {
+      if (!loadAppUi(appId, None) && (!attemptId.isDefined || !loadAppUi(appId, attemptId))) {
         val msg = <div class="row-fluid">Application {appId} not found.</div>
         res.setStatus(HttpServletResponse.SC_NOT_FOUND)
         UIUtils.basicSparkPage(msg, "Not Found").foreach { n =>
