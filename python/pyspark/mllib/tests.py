8b631038e3f2df6628eeff1dd924c1c65509f6f2
Hrishikesh Subramonian <hrishikesh.subramonian@flytxt.com>
2015-05-05 07:57:39 -0700

    [SPARK-6612] [MLLIB] [PYSPARK] Python KMeans parity
    The following items are added to Python kmeans:
    
    kmeans - setEpsilon, setInitializationSteps
    KMeansModel - computeCost, k
    
    Author: Hrishikesh Subramonian <hrishikesh.subramonian@flytxt.com>
    
    Closes #5647 from FlytxtRnD/newPyKmeansAPI and squashes the following commits:
    
    b9e451b [Hrishikesh Subramonian] set seed to fixed value in doc test
    5fd3ced [Hrishikesh Subramonian] doc test corrections
    20b3c68 [Hrishikesh Subramonian] python 3 fixes
    4d4e695 [Hrishikesh Subramonian] added arguments in python tests
    21eb84c [Hrishikesh Subramonian] Python Kmeans - setEpsilon, setInitializationSteps, k and computeCost added.
    
    (cherry picked from commit 5995ada96b661546a80657f2c5ed20604593e4aa)
    Signed-off-by: Xiangrui Meng <meng@databricks.com>

--- python/pyspark/mllib/tests.py
+++ python/pyspark/mllib/tests.py
@@ -236,7 +236,8 @@ class ListTests(MLlibTestCase):
             [1.1, 0],
             [1.2, 0],
         ]
-        clusters = KMeans.train(self.sc.parallelize(data), 2, initializationMode="k-means||")
+        clusters = KMeans.train(self.sc.parallelize(data), 2, initializationMode="k-means||",
+                                initializationSteps=7, epsilon=1e-4)
         self.assertEquals(clusters.predict(data[0]), clusters.predict(data[1]))
         self.assertEquals(clusters.predict(data[2]), clusters.predict(data[3]))
 
@@ -246,9 +247,11 @@ class ListTests(MLlibTestCase):
         Y = range(0, 100, 10)
         data = [[x, y] for x, y in zip(X, Y)]
         clusters1 = KMeans.train(self.sc.parallelize(data),
-                                 3, initializationMode="k-means||", seed=42)
+                                 3, initializationMode="k-means||",
+                                 seed=42, initializationSteps=7, epsilon=1e-4)
         clusters2 = KMeans.train(self.sc.parallelize(data),
-                                 3, initializationMode="k-means||", seed=42)
+                                 3, initializationMode="k-means||",
+                                 seed=42, initializationSteps=7, epsilon=1e-4)
         centers1 = clusters1.centers
         centers2 = clusters2.centers
         for c1, c2 in zip(centers1, centers2):
