ea3077f19c18b5556a3632b36771aeb153746ff5
Liang-Chi Hsieh <viirya@gmail.com>
2015-05-07 16:22:45 -0700

    [SPARK-7277] [SQL] Throw exception if the property mapred.reduce.tasks is set to -1
    JIRA: https://issues.apache.org/jira/browse/SPARK-7277
    
    As automatically determining the number of reducers is not supported (`mapred.reduce.tasks` is set to `-1`), we should throw exception to users.
    
    Author: Liang-Chi Hsieh <viirya@gmail.com>
    
    Closes #5811 from viirya/no_neg_reduce_tasks and squashes the following commits:
    
    e518f96 [Liang-Chi Hsieh] Consider other wrong setting values.
    fd9c817 [Liang-Chi Hsieh] Merge remote-tracking branch 'upstream/master' into no_neg_reduce_tasks
    4ede705 [Liang-Chi Hsieh] Throw exception instead of warning message.
    68a1c70 [Liang-Chi Hsieh] Show warning message if mapred.reduce.tasks is set to -1.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/commands.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/commands.scala
@@ -84,8 +84,14 @@ case class SetCommand(
       logWarning(
         s"Property ${SQLConf.Deprecated.MAPRED_REDUCE_TASKS} is deprecated, " +
           s"automatically converted to ${SQLConf.SHUFFLE_PARTITIONS} instead.")
-      sqlContext.setConf(SQLConf.SHUFFLE_PARTITIONS, value)
-      Seq(Row(s"${SQLConf.SHUFFLE_PARTITIONS}=$value"))
+      if (value.toInt < 1) {
+        val msg = s"Setting negative ${SQLConf.Deprecated.MAPRED_REDUCE_TASKS} for automatically " +
+          "determining the number of reducers is not supported."
+        throw new IllegalArgumentException(msg)
+      } else {
+        sqlContext.setConf(SQLConf.SHUFFLE_PARTITIONS, value)
+        Seq(Row(s"${SQLConf.SHUFFLE_PARTITIONS}=$value"))
+      }
 
     // Configures a single property.
     case Some((key, Some(value))) =>
