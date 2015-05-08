b1b00a3e547bc657ceee92afb719bbb10056eb9e
Vinod K C <vinod.kc@huawei.com>
2015-05-08 11:15:05 +0530

    Removed relativeSD validation from python API,RDD.scala will do validation

--- python/pyspark/rdd.py
+++ python/pyspark/rdd.py
@@ -2237,10 +2237,7 @@ class RDD(object):
         >>> 16 < n < 24
         True
         """
-        if relativeSD < 0.000017:
-            raise ValueError("relativeSD should be greater than 0.000017")
-        if relativeSD > 0.37:
-            raise ValueError("relativeSD should be smaller than 0.37")
+
         # the hash space in Java is 2^32
         hashRDD = self.map(lambda x: portable_hash(x) & 0xFFFFFFFF)
         return hashRDD._to_java_object_rdd().countApproxDistinct(relativeSD)
