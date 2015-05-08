21bd7222e55b9cf684c072141998a0623a69f514
Jacky Li <jacky.likun@huawei.com>
2015-05-08 15:25:54 -0700

    [SPARK-4699] [SQL] Make caseSensitive configurable in spark sql analyzer
    based on #3558
    
    Author: Jacky Li <jacky.likun@huawei.com>
    Author: wangfei <wangfei1@huawei.com>
    Author: scwf <wangfei1@huawei.com>
    
    Closes #5806 from scwf/case and squashes the following commits:
    
    cd51712 [wangfei] fix compile
    d4b724f [wangfei] address michael's comment
    af512c7 [wangfei] fix conflicts
    4ef1be7 [wangfei] fix conflicts
    269cf21 [scwf] fix conflicts
    b73df6c [scwf] style issue
    9e11752 [scwf] improve SimpleCatalystConf
    b35529e [scwf] minor style
    a3f7659 [scwf] remove unsed imports
    2a56515 [scwf] fix conflicts
    6db4bf5 [scwf] also fix for HiveContext
    7fc4a98 [scwf] fix test case
    d5a9933 [wangfei] fix style
    eee75ba [wangfei] fix EmptyConf
    6ef31cf [wangfei] revert pom changes
    5d7c456 [wangfei] set CASE_SENSITIVE false in TestHive
    966e719 [wangfei] set CASE_SENSITIVE false in hivecontext
    fd30e25 [wangfei] added override
    69b3b70 [wangfei] fix AnalysisSuite
    5472b08 [wangfei] fix compile issue
    56034ca [wangfei] fix conflicts and improve for catalystconf
    664d1e9 [Jacky Li] Merge branch 'master' of https://github.com/apache/spark into case
    12eca9a [Jacky Li] solve conflict with master
    39e369c [Jacky Li] fix confilct after DataFrame PR
    dee56e9 [Jacky Li] fix test case failure
    05b09a3 [Jacky Li] fix conflict base on the latest master branch
    73c16b1 [Jacky Li] fix bug in sql/hive
    9bf4cc7 [Jacky Li] fix bug in catalyst
    005c56d [Jacky Li] make SQLContext caseSensitivity configurable
    6332e0f [Jacky Li] fix bug
    fcbf0d9 [Jacky Li] fix scalastyle check
    e7bca31 [Jacky Li] make caseSensitive configuration in Analyzer and Catalog
    91b1b96 [Jacky Li] make caseSensitive configurable in Analyzer
    f57f15c [Jacky Li] add testcase
    578d167 [Jacky Li] make caseSensitive configurable
    
    (cherry picked from commit 6dad76e5eba3c2925bfc9d142f31f7c2dc649886)
    Signed-off-by: Michael Armbrust <michael@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -19,19 +19,21 @@ package org.apache.spark.sql.catalyst.analysis
 
 import scala.collection.mutable.ArrayBuffer
 
-import org.apache.spark.util.collection.OpenHashSet
 import org.apache.spark.sql.AnalysisException
+import org.apache.spark.sql.catalyst.{SimpleCatalystConf, CatalystConf}
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.rules._
 import org.apache.spark.sql.types._
+import org.apache.spark.util.collection.OpenHashSet
 
 /**
  * A trivial [[Analyzer]] with an [[EmptyCatalog]] and [[EmptyFunctionRegistry]]. Used for testing
  * when all relations are already filled in and the analyzer needs only to resolve attribute
  * references.
  */
-object SimpleAnalyzer extends Analyzer(EmptyCatalog, EmptyFunctionRegistry, true)
+object SimpleAnalyzer
+  extends Analyzer(EmptyCatalog, EmptyFunctionRegistry, new SimpleCatalystConf(true))
 
 /**
  * Provides a logical query plan analyzer, which translates [[UnresolvedAttribute]]s and
@@ -41,11 +43,17 @@ object SimpleAnalyzer extends Analyzer(EmptyCatalog, EmptyFunctionRegistry, true
 class Analyzer(
     catalog: Catalog,
     registry: FunctionRegistry,
-    caseSensitive: Boolean,
+    conf: CatalystConf,
     maxIterations: Int = 100)
   extends RuleExecutor[LogicalPlan] with HiveTypeCoercion with CheckAnalysis {
 
-  val resolver = if (caseSensitive) caseSensitiveResolution else caseInsensitiveResolution
+  def resolver: Resolver = {
+    if (conf.caseSensitiveAnalysis) {
+      caseSensitiveResolution
+    } else {
+      caseInsensitiveResolution
+    }
+  }
 
   val fixedPoint = FixedPoint(maxIterations)
 
