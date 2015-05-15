fd07d22a2ace2b1e025ed19e2f311692ab02ccf1
Andrew Or <andrew@databricks.com>
2015-05-15 13:26:57 -0700

    Make MQTT lower case

--- external/mqtt/src/main/scala/org/apache/spark/streaming/mqtt/MQTTUtils.scala
+++ external/mqtt/src/main/scala/org/apache/spark/streaming/mqtt/MQTTUtils.scala
@@ -37,7 +37,7 @@ object MQTTUtils {
       brokerUrl: String,
       topic: String,
       storageLevel: StorageLevel = StorageLevel.MEMORY_AND_DISK_SER_2
-    ): ReceiverInputDStream[String] = ssc.withNamedScope("MQTT stream") {
+    ): ReceiverInputDStream[String] = ssc.withNamedScope("mqtt stream") {
     new MQTTInputDStream(ssc, brokerUrl, topic, storageLevel)
   }
 
