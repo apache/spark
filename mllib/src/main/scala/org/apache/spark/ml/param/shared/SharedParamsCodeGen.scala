cd3093e705b184df1291cd8f03331a9618993693
Joseph K. Bradley <joseph@databricks.com>
2015-05-19 10:57:47 -0700

    [SPARK-7678] [ML] Fix default random seed in HasSeed
    Changed shared param HasSeed to have default based on hashCode of class name, instead of random number.
    Also, removed fixed random seeds from Word2Vec and ALS.
    
    CC: mengxr
    
    Author: Joseph K. Bradley <joseph@databricks.com>
    
    Closes #6251 from jkbradley/scala-fixed-seed and squashes the following commits:
    
    0e37184 [Joseph K. Bradley] Fixed Word2VecSuite, ALSSuite in spark.ml to use original fixed random seeds
    678ec3a [Joseph K. Bradley] Removed fixed random seeds from Word2Vec and ALS. Changed shared param HasSeed to have default based on hashCode of class name, instead of random number.
    
    (cherry picked from commit 7b16e9f2118fbfbb1c0ba957161fe500c9aff82a)
    Signed-off-by: Xiangrui Meng <meng@databricks.com>

--- mllib/src/main/scala/org/apache/spark/ml/param/shared/SharedParamsCodeGen.scala
+++ mllib/src/main/scala/org/apache/spark/ml/param/shared/SharedParamsCodeGen.scala
@@ -53,7 +53,7 @@ private[shared] object SharedParamsCodeGen {
       ParamDesc[Int]("checkpointInterval", "checkpoint interval (>= 1)",
         isValid = "ParamValidators.gtEq(1)"),
       ParamDesc[Boolean]("fitIntercept", "whether to fit an intercept term", Some("true")),
-      ParamDesc[Long]("seed", "random seed", Some("Utils.random.nextLong()")),
+      ParamDesc[Long]("seed", "random seed", Some("this.getClass.getName.hashCode.toLong")),
       ParamDesc[Double]("elasticNetParam", "the ElasticNet mixing parameter, in range [0, 1]." +
         " For alpha = 0, the penalty is an L2 penalty. For alpha = 1, it is an L1 penalty.",
         isValid = "ParamValidators.inRange(0, 1)"),
