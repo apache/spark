b970770af29f77e49a34c0831de8410f406f50fa
Andrew Or <andrew@databricks.com>
2015-05-20 18:01:14 -0700

    Use existing cap

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/ApplicationMaster.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/ApplicationMaster.scala
@@ -345,10 +345,7 @@ private[spark] class ApplicationMaster(
               if (numPendingAllocate > 0) {
                 val currentAllocationInterval =
                   math.min(heartbeatInterval, nextAllocationInterval)
-                nextAllocationInterval *= 2
-                // avoid overflow
-                nextAllocationInterval = math.min(
-                  nextAllocationInterval, ApplicationMaster.MAX_RM_HEARTBEAT_INTERVAL_MS)
+                nextAllocationInterval = currentAllocationInterval * 2 // avoid overflow
                 currentAllocationInterval
               } else {
                 nextAllocationInterval = initialAllocationInterval
@@ -579,9 +576,6 @@ object ApplicationMaster extends Logging {
   private val EXIT_SC_NOT_INITED = 13
   private val EXIT_SECURITY = 14
   private val EXIT_EXCEPTION_USER_CLASS = 15
-
-  // cap on heartbeat interval between us and the resource manager
-  private val MAX_RM_HEARTBEAT_INTERVAL_MS = 10000
 
   private var master: ApplicationMaster = _
 
