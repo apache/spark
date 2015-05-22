956c4c910cb536a02128349f2250d0a5f9924d0c
Hari Shreedharan <hshreedharan@apache.org>
2015-05-21 20:24:28 -0500

    [SPARK-7657] [YARN] Add driver logs links in application UI, in cluster mode.
    This PR adds the URLs to the driver logs to `SparkListenerApplicationStarted` event, which is later used by the `ExecutorsListener` to populate the URLs to the driver logs in its own state. This info is then used when the UI is rendered to display links to the logs.
    
    Author: Hari Shreedharan <hshreedharan@apache.org>
    
    Closes #6166 from harishreedharan/am-log-link and squashes the following commits:
    
    943fc4f [Hari Shreedharan] Merge remote-tracking branch 'asf/master' into am-log-link
    9e5c04b [Hari Shreedharan] Merge remote-tracking branch 'asf/master' into am-log-link
    b3f9b9d [Hari Shreedharan] Updated comment based on feedback.
    0840a95 [Hari Shreedharan] Move the result and sc.stop back to original location, minor import changes.
    537a2f7 [Hari Shreedharan] Add test to ensure the log urls are populated and valid.
    4033725 [Hari Shreedharan] Adding comments explaining how node reports are used to get the log urls.
    6c5c285 [Hari Shreedharan] Import order.
    346f4ea [Hari Shreedharan] Review feedback fixes.
    629c1dc [Hari Shreedharan] Cleanup.
    99fb1a3 [Hari Shreedharan] Send the log urls in App start event, to ensure that other listeners are not affected.
    c0de336 [Hari Shreedharan] Ensure new unit test cleans up after itself.
    50cdae3 [Hari Shreedharan] Added unit test, made the approach generic.
    402e8e4 [Hari Shreedharan] Use `NodeReport` to get the URL for the logs. Also, make the environment variables generic so other cluster managers can use them as well.
    1cf338f [Hari Shreedharan] [SPARK-7657][YARN] Add driver link in application UI, in cluster mode.

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -1991,7 +1991,7 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
     // Note: this code assumes that the task scheduler has been initialized and has contacted
     // the cluster manager to get an application ID (in case the cluster manager provides one).
     listenerBus.post(SparkListenerApplicationStart(appName, Some(applicationId),
-      startTime, sparkUser, applicationAttemptId))
+      startTime, sparkUser, applicationAttemptId, schedulerBackend.getDriverLogUrls))
   }
 
   /** Post the application end event */
