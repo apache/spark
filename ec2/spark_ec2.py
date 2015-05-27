db244ae734564363fb193e0a3b4733e0d1b08803
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-05-26 19:04:15 -0700

    Make Python Lint happy

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -765,8 +765,7 @@ def setup_cluster(conn, master_nodes, slave_nodes, opts, deploy_ssh_key):
 
     # Clear SPARK_WORKER_INSTANCES if running on YARN
     if opts.hadoop_major_version == "yarn":
-      opts.worker_instances = ""
-
+        opts.worker_instances = ""
 
     # NOTE: We should clone the repository before running deploy_files to
     # prevent ec2-variables.sh from being overwritten
