be822597c80609a4d7d26b1ce3bfb6dde4f8e191
Daoyuan Wang <daoyuan.wang@intel.com>
2015-05-27 01:29:57 -0700

    change new exception to sys.err

--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashOuterJoin.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashOuterJoin.scala
@@ -48,7 +48,7 @@ case class HashOuterJoin(
     case LeftOuter => left.outputPartitioning
     case RightOuter => right.outputPartitioning
     case FullOuter => UnknownPartitioning(left.outputPartitioning.numPartitions)
-    case x => throw new Exception(s"HashOuterJoin should not take $x as the JoinType")
+    case x => sys.error(s"HashOuterJoin should not take $x as the JoinType")
   }
 
   override def requiredChildDistribution: Seq[ClusteredDistribution] =
@@ -63,7 +63,7 @@ case class HashOuterJoin(
       case FullOuter =>
         left.output.map(_.withNullability(true)) ++ right.output.map(_.withNullability(true))
       case x =>
-        throw new Exception(s"HashOuterJoin should not take $x as the JoinType")
+        sys.error(s"HashOuterJoin should not take $x as the JoinType")
     }
   }
 
@@ -216,7 +216,7 @@ case class HashOuterJoin(
               rightHashTable.getOrElse(key, EMPTY_LIST), joinedRow)
           }
 
-        case x => throw new Exception(s"HashOuterJoin should not take $x as the JoinType")
+        case x => sys.error(s"HashOuterJoin should not take $x as the JoinType")
       }
     }
   }
