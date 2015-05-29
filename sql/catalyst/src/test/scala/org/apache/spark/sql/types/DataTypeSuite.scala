62df047a3660ffe026aa64dff8e9f096d994a8f3
Reynold Xin <rxin@databricks.com>
2015-05-29 11:06:33 -0700

    HOTFIX: Scala style checker for DataTypeSuite.scala.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/types/DataTypeSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/types/DataTypeSuite.scala
@@ -120,7 +120,7 @@ class DataTypeSuite extends FunSuite {
   checkDefaultSize(DecimalType(10, 5), 4096)
   checkDefaultSize(DecimalType.Unlimited, 4096)
   checkDefaultSize(DateType, 4)
-  checkDefaultSize(TimestampType,12)
+  checkDefaultSize(TimestampType, 12)
   checkDefaultSize(StringType, 4096)
   checkDefaultSize(BinaryType, 4096)
   checkDefaultSize(ArrayType(DoubleType, true), 800)
