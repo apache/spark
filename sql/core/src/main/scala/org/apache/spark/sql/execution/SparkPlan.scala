bcb608ea0608e9e1ca891deaf65aa350e120dc35
baishuo <vc_java@hotmail.com>
2015-05-28 20:18:58 -0700

    change sparkContext in sparkPlan as val

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlan.scala
@@ -48,7 +48,7 @@ abstract class SparkPlan extends QueryPlan[SparkPlan] with Logging with Serializ
   @transient
   protected[spark] final val sqlContext = SparkPlan.currentContext.get()
 
-  protected def sparkContext = sqlContext.sparkContext
+  protected val sparkContext = sqlContext.sparkContext
 
   // sqlContext will be null when we are being deserialized on the slaves.  In this instance
   // the value of codegenEnabled will be set by the desserializer after the constructor has run.
