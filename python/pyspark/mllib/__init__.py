893fb7e5c78410fc522410de494a0b4dc8ff6669
MechCoder <manojkumarsivaraj334@gmail.com>
2015-06-03 00:40:56 +0530

    remove equal to

--- python/pyspark/mllib/__init__.py
+++ python/pyspark/mllib/__init__.py
@@ -25,7 +25,7 @@ from __future__ import absolute_import
 import numpy
 
 ver = [int(x) for x in numpy.version.version.split(.)[:2]]
-if ver <= [1, 4]:
+if ver < [1, 4]:
     raise Exception("MLlib requires NumPy 1.4+")
 
 __all__ = ['classification', 'clustering', 'feature', 'fpm', 'linalg', 'random',
