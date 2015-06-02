b68a83bb49cf4f0903dde262e98da6ae1ce62a3a
Harry Brundage <harry.brundage@gmail.com>
2015-06-01 21:49:46 -0400

    Neuter spark-env.sh to not include the YARN conf from the cluster unless we're in remote development mode

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -1,50 +1,31 @@
 #!/usr/bin/env bash
 
-# This file contains environment variables required to run Spark. Copy it as
-# spark-env.sh and edit that to configure Spark for your site.
-#
-# The following variables can be set in this file:
-# - SPARK_LOCAL_IP, to set the IP address Spark binds to on this node
-# - MESOS_NATIVE_LIBRARY, to point to your libmesos.so if you use Mesos
-# - SPARK_JAVA_OPTS, to set node-specific JVM options for Spark. Note that
-#   we recommend setting app-wide options in the application's driver program.
-#     Examples of node-specific options : -Dspark.local.dir, GC options
-#     Examples of app-wide options : -Dspark.serializer
-#
-# If using the standalone deploy mode, you can also set variables for it here:
-# - SPARK_MASTER_IP, to bind the master to a different IP address or hostname
-# - SPARK_MASTER_PORT / SPARK_MASTER_WEBUI_PORT, to use non-default ports
-# - SPARK_WORKER_CORES, to set the number of cores to use on this machine
-# - SPARK_WORKER_MEMORY, to set how much memory to use (e.g. 1000m, 2g)
-# - SPARK_WORKER_PORT / SPARK_WORKER_WEBUI_PORT
-# - SPARK_WORKER_INSTANCES, to set the number of worker processes per node
-# - SPARK_WORKER_DIR, to set the working directory of worker processes
 echoerr() { echo "$@" 1>&2; }
 FWDIR="$(cd `dirname $0`/..; pwd)"
 
-if [[ -z "$MASTER" ]]; then
-  if [ "$(uname)" == "Darwin" ]; then
-    echoerr "Sparkify: Connecting to chicago spark cluster ..."
-    export MASTER=spark://dn05.chi.shopify.com:7077
 
-    # Figure out the local IP to bind spark to for shell <-> master communication
-    vpn_interface=tap0;
-    get_ip_command="ifconfig $vpn_interface 2>&1 | grep 'inet' | awk '{print \$2}'"
-    if ifconfig $vpn_interface > /dev/null 2>&1; then
-      export SPARK_LOCAL_IP=`bash -c "$get_ip_command"`
-    else
-      echoerr "ERROR: could not find an VPN interface to connect to the Shopify Spark Cluster! Please connect your VPN client! See https://vault-unicorn.shopify.com/VPN---Servers ."
-      exit 1
-    fi
-  fi
+if [ "$(uname)" == "Darwin" ]; then
+  case "$PYTHON_ENV" in
+    'remote_development')
+        echoerr "Sparkify: Connecting to chicago spark cluster ..."
+        # Figure out the local IP to bind spark to for shell <-> master communication
+        vpn_interface=tap0;
+        get_ip_command="ifconfig $vpn_interface 2>&1 | grep 'inet' | awk '{print \$2}'"
+        if ifconfig $vpn_interface > /dev/null 2>&1; then
+          export SPARK_LOCAL_IP=`bash -c "$get_ip_command"`
+        else
+          echoerr "ERROR: could not find an VPN interface to connect to the Shopify Spark Cluster! Please connect your VPN client! See https://vault-unicorn.shopify.com/VPN---Servers ."
+          exit 1
+        fi
+
+        export HADOOP_CONF_DIR=$FWDIR/conf/conf.cloudera.yarn
+        ;;
+    'test'|'development')
+      export SPARK_LOCAL_IP=127.0.0.1
+      ;;
+  esac
 fi
 
 if which ipython > /dev/null; then
   export IPYTHON=1
 fi
-
-if [[ $MASTER == 'local' ]]; then
-  export SPARK_LOCAL_IP=127.0.0.1
-fi
-
-export HADOOP_CONF_DIR=$FWDIR/conf/conf.cloudera.yarn
