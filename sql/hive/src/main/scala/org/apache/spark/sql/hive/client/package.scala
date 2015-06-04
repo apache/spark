0a0347033e2a5edd5dba86326cf5d1787c1649a5
Marcelo Vanzin <vanzin@cloudera.com>
2015-06-04 14:28:22 -0700

    Try to fix tests by upgrading calcite dependency.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/package.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/package.scala
@@ -30,8 +30,8 @@ package object client {
     // Hive 0.14 depends on calcite 0.9.2-incubating-SNAPSHOT which does not exist in
     // maven central anymore, so override those with a version that exists.
     case object v14 extends HiveVersion("0.14.0",
-      Seq("org.apache.calcite:calcite-core:0.9.2-incubating",
-        "org.apache.calcite:calcite-avatica:0.9.2-incubating"))
+      Seq("org.apache.calcite:calcite-core:1.3.0-incubating",
+        "org.apache.calcite:calcite-avatica:1.3.0-incubating"))
   }
   // scalastyle:on
 
