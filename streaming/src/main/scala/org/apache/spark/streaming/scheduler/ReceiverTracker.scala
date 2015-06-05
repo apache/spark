16e84ecc68ec0fed718ed4933657793c793dffbb
Nishkam Ravi <nravi@cloudera.com>
2015-06-05 15:28:21 -0700

    Update ReceiverTracker.scala

--- streaming/src/main/scala/org/apache/spark/streaming/scheduler/ReceiverTracker.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/scheduler/ReceiverTracker.scala
@@ -317,7 +317,8 @@ class ReceiverTracker(ssc: StreamingContext, skipReceiverLaunch: Boolean = false
           val receiversWithPreferences = receivers.map(r => (r, Seq(r.preferredLocation.get)))
           ssc.sc.makeRDD[Receiver[_]](receiversWithPreferences)
         } else if (roundRobin) {
-          val roundRobinReceivers = (0 to (receivers.length - 1)).map(i => (receivers(i), locations(i)))
+          val roundRobinReceivers = (0 to (receivers.length - 1)).map(i => 
+	    (receivers(i), locations(i)))
           ssc.sc.makeRDD[Receiver[_]](roundRobinReceivers)
         } else {
           ssc.sc.makeRDD(receivers, receivers.size)
