5ac11d150da51385a988e4982a1082ff2df0f0ac
Josh Rosen <joshrosen@databricks.com>
2015-06-05 15:38:05 -0700

    Revert "Use local[*] instead of local[2]"
    This reverts commit 55041d2177a3174f633bee1c7be2e4e2266c9b69.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
@@ -49,7 +49,7 @@ import scala.collection.JavaConversions._
 object TestHive
   extends TestHiveContext(
     new SparkContext(
-      "local[*]",
+      "local[2]",
       "TestSQLContext",
       new SparkConf()
         .set("spark.sql.test", "")
