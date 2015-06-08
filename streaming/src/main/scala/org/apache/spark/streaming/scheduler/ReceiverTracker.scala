7f3e02881229a7b3a5ff11f86c62eca5933c7a1c
Nishkam Ravi <nravi@cloudera.com>
2015-06-08 13:31:11 -0700

    Update ReceiverTracker.scala, add unit test cases in SchedulerSuite

--- streaming/src/main/scala/org/apache/spark/streaming/scheduler/ReceiverTracker.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/scheduler/ReceiverTracker.scala
@@ -19,6 +19,7 @@ package org.apache.spark.streaming.scheduler
 
 import scala.collection.mutable.{ArrayBuffer, HashMap, SynchronizedMap}
 import scala.language.existentials
+import scala.math.max
 import org.apache.spark.rdd._
 
 import org.apache.spark.streaming.util.WriteAheadLogUtils
@@ -280,51 +281,33 @@ class ReceiverTracker(ssc: StreamingContext, skipReceiverLaunch: Boolean = false
       executors.diff(List(driver))
     }
 
-    /* Schedule receivers using preferredLocation if specified
-     * and round-robin otherwise
-     */
-    private def scheduleReceivers(receivers: Seq[Receiver[_]]): RDD[Receiver[_]] = {
-      // Location preferences are honored if all receivers have them
-      val hasLocationPreferences = receivers.map(_.preferredLocation.isDefined).reduce(_ && _)
-
-      // If no location preferences are specified, set host location for each receiver
-      // so as to distribute them evenly over executors in a round-robin fashion
-      // If num_receivers > num_executors, distribute receivers among executors
-      // If num_executors > num_receivers, distribute executors among receivers
+    /** Set host location(s) for each receiver so as to distribute them over
+     * executors in a round-robin fashion taking into account preferredLocation if set
+     */ 
+    private[streaming] def scheduleReceivers(receivers: Seq[Receiver[_]],
+      executors: List[String]): Array[ArrayBuffer[String]] = {
       val locations = new Array[ArrayBuffer[String]](receivers.length)
-      var roundRobin = false
-      if (!hasLocationPreferences && !ssc.sparkContext.isLocal) {
-        val executors = getExecutors(ssc)
+      if (!executors.isEmpty) {
         var i = 0
-        if (!executors.isEmpty) {
-          for (i <- 0 to (receivers.length - 1)) {
-            locations(i) = new ArrayBuffer[String]()
+        for (i <- 0 to (receivers.length - 1)) {
+          locations(i) = new ArrayBuffer[String]()
+          if (receivers(i).preferredLocation.isDefined) {
+            locations(i) += receivers(i).preferredLocation.get
           }
-          if (receivers.length >= executors.length) {
-            for (i <- 0 to (receivers.length - 1)) {
-              locations(i) += executors(i % executors.length)
-            }
-          } else {
-            for (i <- 0 to (executors.length - 1)) {
-              locations(i % receivers.length) += executors(i)
+        }
+
+        var count = 0;
+        for (i <- 0 to (max(receivers.length, executors.length) - 1)) {
+          if (!receivers(i % receivers.length).preferredLocation.isDefined) {
+            locations(i % receivers.length) += executors(count)
+            count += 1;
+            if (count == executors.length) {
+              count = 0;
             }
           }
-          roundRobin = true
         }
       }
-
-      val tempRDD =
-        if (hasLocationPreferences) {
-          val receiversWithPreferences = receivers.map(r => (r, Seq(r.preferredLocation.get)))
-          ssc.sc.makeRDD[Receiver[_]](receiversWithPreferences)
-        } else if (roundRobin) {
-          val roundRobinReceivers = (0 to (receivers.length - 1)).map(i =>
-            (receivers(i), locations(i)))
-          ssc.sc.makeRDD[Receiver[_]](roundRobinReceivers)
-        } else {
-          ssc.sc.makeRDD(receivers, receivers.size)
-        }
-      tempRDD
+      locations
     }
 
     /**
@@ -360,7 +343,16 @@ class ReceiverTracker(ssc: StreamingContext, skipReceiverLaunch: Boolean = false
         ssc.sparkContext.makeRDD(1 to 50, 50).map(x => (x, 1)).reduceByKey(_ + _, 20).collect()
       }
 
-      val tempRDD = scheduleReceivers(receivers)
+      // Get the list of executors and schedule receivers
+      val executors = getExecutors(ssc)
+      val locations = scheduleReceivers(receivers, executors)
+      val tempRDD =
+        if (locations(0) != null) {
+          val roundRobinReceivers = (0 to (receivers.length - 1)).map(i => (receivers(i), locations(i)))
+          ssc.sc.makeRDD[Receiver[_]](roundRobinReceivers)
+        } else {
+          ssc.sc.makeRDD(receivers, receivers.size)
+        }
 
       // Distribute the receivers and start them
       logInfo("Starting " + receivers.length + " receivers")
---
+++ streaming/src/test/scala/org/apache/spark/streaming/scheduler/SchedulerSuite.scala
@@ -0,0 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.streaming.scheduler
+
+import org.apache.spark.streaming._
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver._
+import org.apache.spark.util.Utils
+
+/** Testsuite for testing scheduling of streaming receivers */
+class SchedulerSuite extends TestSuiteBase {
+  val sparkConf = new SparkConf().setMaster("local[8]").setAppName("test")
+  val ssc = new StreamingContext(sparkConf, Milliseconds(100))
+  val tracker = new ReceiverTracker(ssc)
+  val launcher = new tracker.ReceiverLauncher()
+
+  test("receiver scheduling - no preferredLocation") {
+    val numReceivers = 10;
+    val receivers = (1 to numReceivers).map(i => new DummyReceiver)
+    val executors: List[String] = List("Host1", "Host2", "Host3", "Host4", "Host5")
+    val locations = launcher.scheduleReceivers(receivers, executors)
+    assert(locations(0)(0) === "Host1") 
+    assert(locations(4)(0) === "Host5")  
+    assert(locations(5)(0) === "Host1") 
+    assert(locations(9)(0) === "Host5")  
+  } 
+
+  test("receiver scheduling - no preferredLocation, numExecutors > numReceivers") {
+    val numReceivers = 3;
+    val receivers = (1 to numReceivers).map(i => new DummyReceiver)
+    val executors: List[String] = List("Host1", "Host2", "Host3", "Host4", "Host5")
+    val locations = launcher.scheduleReceivers(receivers, executors)
+    assert(locations(0)(0) === "Host1") 
+    assert(locations(2)(0) === "Host3")  
+    assert(locations(0)(1) === "Host4") 
+    assert(locations(1)(1) === "Host5")  
+  } 
+
+  test("receiver scheduling - all have preferredLocation") {
+    val numReceivers = 5;
+    val receivers = (1 to numReceivers).map(i => new DummyReceiver(host = Some("Host" + i)))
+    val executors: List[String] = List("Host1", "Host5", "Host4", "Host3", "Host2")
+    val locations = launcher.scheduleReceivers(receivers, executors)
+    assert(locations(1)(0) === "Host2") 
+    assert(locations(4)(0) === "Host5")  
+  } 
+
+  test("receiver scheduling - some have preferredLocation") {
+    val numReceivers = 3;
+    val receivers: Seq[Receiver[_]] = Seq(
+      new DummyReceiver(host = Some("Host2")),
+      new DummyReceiver,
+      new DummyReceiver)
+    val executors: List[String] = List("Host1", "Host2", "Host3", "Host4", "Host5")
+    val locations = launcher.scheduleReceivers(receivers, executors)
+    assert(locations(0)(0) === "Host2") 
+    assert(locations(1)(0) === "Host1") 
+    assert(locations(2)(0) === "Host2")
+    assert(locations(1)(1) === "Host3")
+  } 
+}
+
+/**
+ * Dummy receiver implementation for testing scheduling
+ */
+class DummyReceiver(host: Option[String] = None) extends Receiver[Int](StorageLevel.MEMORY_ONLY) {
+
+  def onStart() {
+  }
+
+  def onStop() {
+  }
+
+  override def preferredLocation: Option[String] = host
+}
