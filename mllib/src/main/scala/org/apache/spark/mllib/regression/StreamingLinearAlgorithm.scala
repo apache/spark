ff5cd78e2973ff5ea925451a0d875e85706675dc
Paavo <pparkkin@gmail.com>
2015-06-10 08:07:07 +0900

    Update strings to use interpolation.

--- mllib/src/main/scala/org/apache/spark/mllib/regression/StreamingLinearAlgorithm.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/regression/StreamingLinearAlgorithm.scala
@@ -85,12 +85,12 @@ abstract class StreamingLinearAlgorithm[
     data.foreachRDD { (rdd, time) =>
       if (!rdd.isEmpty) {
         model = Some(algorithm.run(rdd, model.get.weights))
-        logInfo("Model updated at time %s".format(time.toString))
+        logInfo(s"Model updated at time ${time.toString}")
         val display = model.get.weights.size match {
           case x if x > 100 => model.get.weights.toArray.take(100).mkString("[", ",", "...")
           case _ => model.get.weights.toArray.mkString("[", ",", "]")
         }
-        logInfo("Current model: weights, %s".format (display))
+        logInfo(s"Current model: weights, ${display}")
       }
     }
   }
