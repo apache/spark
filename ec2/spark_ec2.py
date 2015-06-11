c8d551d546979e126c91925487e30c353185e3ba
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-06-11 13:18:42 -0700

    [SPARK-8310] [EC2] Updates the master branch EC2 versions
    Will send another PR for `branch-1.4`
    
    Author: Shivaram Venkataraman <shivaram@cs.berkeley.edu>
    
    Closes #6764 from shivaram/SPARK-8310 and squashes the following commits:
    
    d8cd3b3 [Shivaram Venkataraman] This updates the master branch EC2 versions

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -51,7 +51,7 @@ else:
     raw_input = input
     xrange = range
 
-SPARK_EC2_VERSION = "1.3.1"
+SPARK_EC2_VERSION = "1.4.0"
 SPARK_EC2_DIR = os.path.dirname(os.path.realpath(__file__))
 
 VALID_SPARK_VERSIONS = set([
@@ -89,7 +89,7 @@ DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
 DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/mesos/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "branch-1.3"
+DEFAULT_SPARK_EC2_BRANCH = "branch-1.4"
 
 
 def setup_external_libs(libs):
