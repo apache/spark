066e44e29683061fc22c5e0e4bdd7f0b9a8355b6
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2015-06-11 11:23:44 -0700

    Update spark-ec2 branch to 1.4

--- ec2/spark_ec2.py
+++ ec2/spark_ec2.py
@@ -90,7 +90,7 @@ DEFAULT_SPARK_GITHUB_REPO = "https://github.com/apache/spark"
 
 # Default location to get the spark-ec2 scripts (and ami-list) from
 DEFAULT_SPARK_EC2_GITHUB_REPO = "https://github.com/mesos/spark-ec2"
-DEFAULT_SPARK_EC2_BRANCH = "branch-1.3"
+DEFAULT_SPARK_EC2_BRANCH = "branch-1.4"
 
 
 def setup_external_libs(libs):
