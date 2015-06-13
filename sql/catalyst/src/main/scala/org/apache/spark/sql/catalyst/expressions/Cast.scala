180ae25d90fb1a7b235f646cdc8180deede32608
Liang-Chi Hsieh <viirya@gmail.com>
2015-06-13 16:39:52 -0700

    [SPARK-8052] [SQL] Use java.math.BigDecimal for casting String to Decimal instead of using toDouble
    JIRA: https://issues.apache.org/jira/browse/SPARK-8052
    
    Author: Liang-Chi Hsieh <viirya@gmail.com>
    
    Closes #6645 from viirya/cast_string_integraltype and squashes the following commits:
    
    e19c6a3 [Liang-Chi Hsieh] For comment.
    c3e472a [Liang-Chi Hsieh] Add test.
    7ced9b0 [Liang-Chi Hsieh] Use java.math.BigDecimal for casting String to Decimal instead of using toDouble.
    
    (cherry picked from commit ddec45279ed1061f4c05fd0760309a53581d03f5)
    Signed-off-by: Reynold Xin <rxin@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Cast.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Cast.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.sql.catalyst.expressions
 
+import java.math.{BigDecimal => JavaBigDecimal}
 import java.sql.{Date, Timestamp}
 import java.text.{DateFormat, SimpleDateFormat}
 
@@ -325,7 +326,7 @@ case class Cast(child: Expression, dataType: DataType) extends UnaryExpression w
   private[this] def castToDecimal(from: DataType, target: DecimalType): Any => Any = from match {
     case StringType =>
       buildCast[UTF8String](_, s => try {
-        changePrecision(Decimal(s.toString.toDouble), target)
+        changePrecision(Decimal(new JavaBigDecimal(s.toString)), target)
       } catch {
         case _: NumberFormatException => null
       })
