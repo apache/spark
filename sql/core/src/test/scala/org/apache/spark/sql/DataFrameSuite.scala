fbd206586ab817f0b8152ad9f39674311934d664
Michael Armbrust <michael@databricks.com>
2015-06-14 11:36:27 +0000

    more style

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -133,10 +133,10 @@ class DataFrameSuite extends QueryTest {
       Row("a", 3) :: Row("b", 2) :: Row("c", 1) :: Nil
     )
   }
-  
+
   test("explode alias and star") {
     val df = Seq((Array("a"), 1)).toDF("a", "b")
-    
+
     checkAnswer(
       df.select(explode($"a").as("a"), $"*"),
       Row("a", Seq("a"), 1) :: Nil)
