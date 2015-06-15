e240040cc6d4c206498e8cfc27429f898ba5dab7
Ilya Ganelin <ilya.ganelin@capitalone.com>
2015-06-15 08:09:11 -0700

    Style

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -371,7 +371,7 @@ class StructType(DataType):
         else:
             self.fields = fields
             assert all(isinstance(f, StructField) for f in fields),\
-            "fields should be a list of StructField"
+                "fields should be a list of StructField"
 
     def add(self, name_or_struct_field, data_type=None, nullable=True, metadata=None):
         """
