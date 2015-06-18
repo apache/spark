435ba7e561dcbc74ee5d1044abd36fdad36a9f18
Bryan Cutler <bjcutler@us.ibm.com>
2015-06-18 16:40:34 -0700

    [SPARK-8444] Fixed style checks, increased sleep time to show empty queue

--- examples/src/main/python/streaming/queue_stream.py
+++ examples/src/main/python/streaming/queue_stream.py
@@ -32,19 +32,19 @@ if __name__ == "__main__":
 
     sc = SparkContext(appName="PythonStreamingQueueStream")
     ssc = StreamingContext(sc, 1)
-    
+
     # Create the queue through which RDDs can be pushed to
     # a QueueInputDStream
     rddQueue = []
     for i in xrange(5):
-        rddQueue += [ssc.sparkContext.parallelize([j for j in xrange(1,1001)], 10)]
-    
+        rddQueue += [ssc.sparkContext.parallelize([j for j in xrange(1, 1001)], 10)]
+
     # Create the QueueInputDStream and use it do some processing
     inputStream = ssc.queueStream(rddQueue)
     mappedStream = inputStream.map(lambda x: (x % 10, 1))
     reducedStream = mappedStream.reduceByKey(lambda a, b: a + b)
     reducedStream.pprint()
-    
+
     ssc.start()
-    time.sleep(5)
-    ssc.stop(stopSparkContext = True, stopGraceFully = True)
+    time.sleep(6)
+    ssc.stop(stopSparkContext=True, stopGraceFully=True)
