575a7c8ce9a2b62e56c17fdb8e0ef46d670a5f7a
Daoyuan Wang <daoyuan.wang@intel.com>
2015-06-18 23:54:19 -0700

    fix notserializable

--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/BroadcastLeftSemiJoinHash.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/BroadcastLeftSemiJoinHash.scala
@@ -58,7 +58,8 @@ case class BroadcastLeftSemiJoinHash(
           if (!rowKey.anyNull) {
             val keyExists = hashSet.contains(rowKey)
             if (!keyExists) {
-              hashSet.add(rowKey)
+              // rowKey may be not serializable (from codegen)
+              hashSet.add(rowKey.copy())
             }
           }
         }
