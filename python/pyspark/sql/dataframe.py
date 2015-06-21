ff676c9c2a9aadc2376b9b1d26a4211f4822cac7
dwmclary <dan.mcclary@gmail.com>
2015-06-20 22:31:55 -0700

    cleaned up python style, part2

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -1174,8 +1174,8 @@ class DataFrame(object):
             raise ValueError("cols must be a list or tuple of column names as strings.")
         if not support:
             support = 0.01
-        return DataFrame(self._jdf.stat().freqItemsFrame(_to_seq(self._sc, cols), support), \
-          self.sql_ctx)
+        return DataFrame(self._jdf.stat().freqItemsFrame(_to_seq(self._sc, cols), support),
+            self.sql_ctx)
 
     @since(1.4)
     def freqItems(self, cols, support=None):
@@ -1349,7 +1349,6 @@ class DataFrameStatFunctions(object):
 
     def freqItemsFrame(self, cols, support=None):
         return self.df.freqItemsFrame(cols, support)
-
     freqItemsFrame.__doc__ = DataFrame.freqItemsFrame.__doc__
 
 
