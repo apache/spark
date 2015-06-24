4219e66bf14ac06d81d7431cc497c492ab83b065
Burak Yavuz <brkyvz@gmail.com>
2015-06-23 18:13:53 -0700

    add example df to freqItems

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameStatFunctions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameStatFunctions.scala
@@ -181,6 +181,10 @@ final class DataFrameStatFunctions private[sql](df: DataFrame) {
    * @return A Local DataFrame with the Array of frequent items for each column.
    *
    * {{{
+   *    val rows = Seq.tabulate(100) { i =>
+   *      if (i % 2 == 0) (1, -1.0) else (i, i * -1.0)
+   *    }
+   *    val df = sqlContext.createDataFrame(rows).toDF("a", "b")
    *    // find the items with a frequency greater than 0.4 (observed 40% of the time) for columns
    *    // "a" and "b"
    *    val freqSingles = df.stat.freqItems(Seq("a", "b"), 0.4)
