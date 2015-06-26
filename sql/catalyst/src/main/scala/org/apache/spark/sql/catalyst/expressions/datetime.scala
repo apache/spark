02efc5da64d3f95024b860e50a4648d989496c9f
Tarek Auel <tarek.auel@gmail.com>
2015-06-26 16:01:49 -0700

    removed doubled code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/datetime.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/datetime.scala
@@ -27,27 +27,28 @@ import org.apache.spark.sql.types._
 import org.apache.spark.unsafe.types.UTF8String
 
 abstract class DateFormatExpression extends Expression { self: Product =>
+
+  protected val format: String
+
+  protected val caller: String
   
   protected val date: Expression
-  
-  protected val format: Expression
 
-  override def foldable: Boolean = date.foldable && format.foldable
+  override def foldable: Boolean = date.foldable
 
   override def nullable: Boolean = true
 
-  override def children: Seq[Expression] = Seq(date, format)
+  override def children: Seq[Expression] = Seq(date)
 
   override def eval(input: InternalRow): Any = {
     val valueLeft = date.eval(input)
     if (valueLeft == null) {
       null
     } else {
-      val valueRight = format.eval(input)
-      if (valueRight == null) {
+      if (format == null) {
         null
       } else {
-        val sdf = new SimpleDateFormat(valueRight.asInstanceOf[UTF8String].toString)
+        val sdf = new SimpleDateFormat(format)
         date.dataType match {
           case TimestampType =>
             UTF8String.fromString(sdf.format(new Date(valueLeft.asInstanceOf[Long] / 10000)))
@@ -55,11 +56,23 @@ abstract class DateFormatExpression extends Expression { self: Product =>
             UTF8String.fromString(sdf.format(DateTimeUtils.toJavaDate(valueLeft.asInstanceOf[Int])))
           case StringType =>
             UTF8String.fromString(
-              sdf.format(DateTimeUtils.stringToTime(valueLeft.asInstanceOf[UTF8String].toString)))
+              sdf.format(DateTimeUtils.stringToTime(valueLeft.toString)))
         }
       }
     }
   }
+
+  override def checkInputDataTypes(): TypeCheckResult =
+    date.dataType match {
+      case null => TypeCheckResult.TypeCheckSuccess
+      case _: DateType => TypeCheckResult.TypeCheckSuccess
+      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
+      case _: StringType => TypeCheckResult.TypeCheckSuccess
+      case _ =>
+        TypeCheckResult.TypeCheckFailure(s"$caller accepts date types as argument, " +
+          s" not ${date.dataType}")
+    }
+
 
   /**
    * Called by date format expressions to generate a code block that returns the result
@@ -78,48 +91,39 @@ abstract class DateFormatExpression extends Expression { self: Product =>
       ev: GeneratedExpressionCode,
       f: String => String): String = {
 
-    val sdf = "java.text.SimpleDateFormat"
-    val utf8 = "org.apache.spark.unsafe.types.UTF8String"
+    val sdf = classOf[SimpleDateFormat].getName
     val dtUtils = "org.apache.spark.sql.catalyst.util.DateTimeUtils"
 
     val eval1 = date.gen(ctx)
-    val eval2 = format.gen(ctx)
 
-    val varName = ctx.freshName("resultVar")
+    val parseInput = date.dataType match {
+      case StringType => s"new java.sql.Date($dtUtils.stringToTime(${eval1.primitive}.toString()).getTime())"
+      case TimestampType => s"new java.sql.Date(${eval1.primitive} / 10000)"
+      case DateType => s"$dtUtils.toJavaDate(${eval1.primitive})"
+    }
 
     s"""
       ${eval1.code}
       boolean ${ev.isNull} = ${eval1.isNull};
       ${ctx.javaType(dataType)} ${ev.primitive} = ${ctx.defaultValue(dataType)};
       if (!${ev.isNull}) {
-        ${eval2.code}
-        if (!${eval2.isNull}) {
-          $sdf sdf = new $sdf(${eval2.primitive}.toString());
-          Object o = ${eval1.primitive};
-          $utf8 $varName;
-          if (o instanceof ${ctx.boxedType(TimestampType)}) {
-            $varName = $utf8.fromString(sdf.format(new java.sql.Date(Long.parseLong(o.toString()) / 10000)));
-          } else if (o instanceof ${ctx.boxedType(DateType)}) {
-            $varName = $utf8.fromString(sdf.format($dtUtils.toJavaDate(Integer.parseInt(o.toString()))));
-          } else {
-            $varName = $utf8.fromString(sdf.format(new java.sql.Date($dtUtils.stringToTime(o.toString()).getTime())));
-          }
-          ${ev.primitive} = ${f(varName)};
-        } else {
-          ${ev.isNull} = true;
-        }
+        $sdf sdf = new $sdf("$format");
+        ${ctx.stringType} s = ${ctx.stringType}.fromString(sdf.format($parseInput));
+        ${ev.primitive} = ${f("s")};
+      } else {
+        ${ev.isNull} = true;
       }
     """
   }
   
 }
 
-case class DateFormatClass(date: Expression, format: Expression) extends DateFormatExpression {
+case class DateFormatClass(left: Expression, right: Expression) extends BinaryExpression {
 
   override def dataType: DataType = StringType
 
   override def checkInputDataTypes(): TypeCheckResult =
-    (date.dataType, format.dataType) match {
+    (left.dataType, right.dataType) match {
       case (null, _) => TypeCheckResult.TypeCheckSuccess
       case (_, null) => TypeCheckResult.TypeCheckSuccess
       case (_: DateType, _: StringType) => TypeCheckResult.TypeCheckSuccess
@@ -127,33 +131,69 @@ case class DateFormatClass(date: Expression, format: Expression) extends DateFor
       case (_: StringType, _: StringType) => TypeCheckResult.TypeCheckSuccess
       case _ =>
         TypeCheckResult.TypeCheckFailure(s"DateFormat accepts date types as first argument, " +
-          s"and string types as second, not ${date.dataType} and ${format.dataType}")
+          s"and string types as second, not ${left.dataType} and ${right.dataType}")
     }
 
-  override def toString: String = s"DateFormat($date, $format)"
+  override def toString: String = s"DateFormat($left, $right)"
 
-  override def genCode(ctx: CodeGenContext, ev: GeneratedExpressionCode): String = {
-    defineCodeGen(ctx, ev, c => s"$c")
+  override def eval(input: InternalRow): Any = {
+    val valueLeft = left.eval(input)
+    if (valueLeft == null) {
+      null
+    } else {
+      val valueRight = right.eval(input)
+      if (valueRight == null) {
+        null
+      } else {
+        val sdf = new SimpleDateFormat(valueRight.toString)
+        left.dataType match {
+          case TimestampType =>
+            UTF8String.fromString(sdf.format(new Date(valueLeft.asInstanceOf[Long] / 10000)))
+          case DateType =>
+            UTF8String.fromString(sdf.format(DateTimeUtils.toJavaDate(valueLeft.asInstanceOf[Int])))
+          case StringType =>
+            UTF8String.fromString(
+              sdf.format(DateTimeUtils.stringToTime(valueLeft.toString)))
+        }
+      }
+    }   
   }
 
+  override def genCode(ctx: CodeGenContext, ev: GeneratedExpressionCode): String = {
+    val sdf = classOf[SimpleDateFormat].getName
+    val dtUtils = "org.apache.spark.sql.catalyst.util.DateTimeUtils"
+
+    val eval1 = left.gen(ctx)
+    val eval2 = right.gen(ctx)
+
+    val parseInput = left.dataType match {
+      case StringType => s"new java.sql.Date($dtUtils.stringToTime(${eval1.primitive}.toString()).getTime())"
+      case TimestampType => s"new java.sql.Date(${eval1.primitive} / 10000)"
+      case DateType => s"$dtUtils.toJavaDate(${eval1.primitive})"
+    }
+
+    s"""
+      ${eval1.code}
+      ${eval2.code}
+      boolean ${ev.isNull} = ${eval1.isNull} || ${eval2.isNull};
+      ${ctx.javaType(dataType)} ${ev.primitive} = ${ctx.defaultValue(dataType)};
+      if (!${ev.isNull}) {
+        $sdf sdf = new $sdf(${eval2.primitive}.toString());
+        ${ev.primitive} = ${ctx.stringType}.fromString(sdf.format($parseInput));
+      } else {
+        ${ev.isNull} = true;
+      }
+    """
+  }
 }
 
 case class Year(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("y")
+  override protected val format: String = "y"
+
+  override protected val caller: String = "Year"
 
   override def dataType: DataType = IntegerType
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Year accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
 
   override def genCode(ctx: CodeGenContext, ev: GeneratedExpressionCode): String = {
     defineCodeGen(ctx, ev, c => s"Integer.parseInt($c.toString())")
@@ -168,8 +208,10 @@ case class Year(date: Expression) extends DateFormatExpression {
 }
 
 case class Quarter(date: Expression) extends DateFormatExpression {
-  
-  override protected val format: Expression = Literal("M")
+
+  override protected val format: String = "M"
+
+  override protected val caller: String = "Quarter"
 
   override def dataType: DataType = IntegerType
 
@@ -183,23 +225,13 @@ case class Quarter(date: Expression) extends DateFormatExpression {
       case s: UTF8String => (s.toString.toInt - 1) / 3 + 1
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Quarter accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
-
 }
 
 case class Month(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("M")
+  override protected val format: String = "M"
+
+  override protected val caller: String = "Month"
 
   override def dataType: DataType = IntegerType
 
@@ -215,22 +247,13 @@ case class Month(date: Expression) extends DateFormatExpression {
       case s: UTF8String => s.toString.toInt
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Month accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
 }
 
 case class Day(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("d")
+  override protected val format: String = "d"
+
+  override protected val caller: String = "Day"
 
   override def dataType: DataType = IntegerType
 
@@ -244,23 +267,13 @@ case class Day(date: Expression) extends DateFormatExpression {
       case s: UTF8String => s.toString.toInt
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Day accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
-
 }
 
 case class Hour(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("H")
+  override protected val format: String = "H"
+
+  override protected val caller: String = "Hour"
 
   override def dataType: DataType = IntegerType
 
@@ -274,22 +287,13 @@ case class Hour(date: Expression) extends DateFormatExpression {
       case s: UTF8String => s.toString.toInt
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Hour accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
 }
 
 case class Minute(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("m")
+  override protected val format: String = "m"
+
+  override protected val caller: String = "Minute"
 
   override def dataType: DataType = IntegerType
 
@@ -303,22 +307,13 @@ case class Minute(date: Expression) extends DateFormatExpression {
       case s: UTF8String => s.toString.toInt
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Minute accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
 }
 
 case class Second(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("s")
+  override protected val format: String = "s"
+
+  override protected val caller: String = "Second"
 
   override def dataType: DataType = IntegerType
 
@@ -332,22 +327,13 @@ case class Second(date: Expression) extends DateFormatExpression {
       case s: UTF8String => s.toString.toInt
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"Second accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
 }
 
 case class WeekOfYear(date: Expression) extends DateFormatExpression {
 
-  override protected val format: Expression = Literal("w")
+  override protected val format: String = "w"
+
+  override protected val caller: String = "WeekOfYear"
 
   override def dataType: DataType = IntegerType
 
@@ -361,15 +347,4 @@ case class WeekOfYear(date: Expression) extends DateFormatExpression {
       case s: UTF8String => s.toString.toInt
     }
   }
-
-  override def checkInputDataTypes(): TypeCheckResult =
-    date.dataType match {
-      case null => TypeCheckResult.TypeCheckSuccess
-      case _: DateType => TypeCheckResult.TypeCheckSuccess
-      case _: TimestampType => TypeCheckResult.TypeCheckSuccess
-      case _: StringType => TypeCheckResult.TypeCheckSuccess
-      case _ =>
-        TypeCheckResult.TypeCheckFailure(s"WeekOfYear accepts date types as argument, " +
-          s" not ${date.dataType}")
-    }
 }
--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/DateTimeFunctionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/DateTimeFunctionsSuite.scala
@@ -21,6 +21,7 @@ import java.sql.{Timestamp, Date}
 import java.text.SimpleDateFormat
 
 import org.apache.spark.SparkFunSuite
+import org.apache.spark.sql.types.{StringType, DateType}
 
 class DateTimeFunctionsSuite extends SparkFunSuite with ExpressionEvalHelper {
 
@@ -29,54 +30,64 @@ class DateTimeFunctionsSuite extends SparkFunSuite with ExpressionEvalHelper {
   val ts = new Timestamp(sdf.parse("2013-11-08 13:10:15").getTime)
 
   test("DateFormat") {
+    checkEvaluation(DateFormatClass(Literal.create(null, DateType), Literal("y")), null)
+    checkEvaluation(DateFormatClass(Literal(d), Literal.create(null, StringType)), null)
     checkEvaluation(DateFormatClass(Literal(d), Literal("y")), "2015")
     checkEvaluation(DateFormatClass(Literal(sdf.format(d)), Literal("y")), "2015")
     checkEvaluation(DateFormatClass(Literal(ts), Literal("y")), "2013")
   }
 
   test("Year") {
+    checkEvaluation(Year(Literal.create(null, DateType)), null)
     checkEvaluation(Year(Literal(d)), 2015)
     checkEvaluation(Year(Literal(sdf.format(d))), 2015)
     checkEvaluation(Year(Literal(ts)), 2013)
   }
 
   test("Quarter") {
+    checkEvaluation(Quarter(Literal.create(null, DateType)), null)
     checkEvaluation(Quarter(Literal(d)), 2)
     checkEvaluation(Quarter(Literal(sdf.format(d))), 2)
     checkEvaluation(Quarter(Literal(ts)), 4)
   }
 
   test("Month") {
+    checkEvaluation(Month(Literal.create(null, DateType)), null)
     checkEvaluation(Month(Literal(d)), 4)
     checkEvaluation(Month(Literal(sdf.format(d))), 4)
     checkEvaluation(Month(Literal(ts)), 11)
   }
 
   test("Day") {
+    checkEvaluation(Day(Literal.create(null, DateType)), null)
     checkEvaluation(Day(Literal(d)), 8)
     checkEvaluation(Day(Literal(sdf.format(d))), 8)
     checkEvaluation(Day(Literal(ts)), 8)
   }
 
   test("Hour") {
+    checkEvaluation(Hour(Literal.create(null, DateType)), null)
     checkEvaluation(Hour(Literal(d)), 0)
     checkEvaluation(Hour(Literal(sdf.format(d))), 13)
     checkEvaluation(Hour(Literal(ts)), 13)
   }
 
   test("Minute") {
+    checkEvaluation(Minute(Literal.create(null, DateType)), null)
     checkEvaluation(Minute(Literal(d)), 0)
     checkEvaluation(Minute(Literal(sdf.format(d))), 10)
     checkEvaluation(Minute(Literal(ts)), 10)
   }
 
   test("Seconds") {
+    checkEvaluation(Second(Literal.create(null, DateType)), null)
     checkEvaluation(Second(Literal(d)), 0)
     checkEvaluation(Second(Literal(sdf.format(d))), 15)
     checkEvaluation(Second(Literal(ts)), 15)
   }
 
   test("WeekOfYear") {
+    checkEvaluation(WeekOfYear(Literal.create(null, DateType)), null)
     checkEvaluation(WeekOfYear(Literal(d)), 15)
     checkEvaluation(WeekOfYear(Literal(sdf.format(d))), 15)
     checkEvaluation(WeekOfYear(Literal(ts)), 45)
