60dbdc038f5be6a9a631c9916ded8146fb070c42
Josh Rosen <joshrosen@databricks.com>
2015-06-26 01:37:44 -0700

    Account for int vs. long formatting change in Python 3

--- python/pyspark/serializers.py
+++ python/pyspark/serializers.py
@@ -44,8 +44,8 @@ which contains two batches of two objects:
 
 >>> rdd.glom().collect()
 [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]]
->>> rdd._jrdd.count()
-8L
+>>> int(rdd._jrdd.count())
+8
 >>> sc.stop()
 """
 
