2ce57601e95623206db5acd38da09dc84a44044f
Joshi <rekhajoshm@gmail.com>
2015-06-29 12:42:35 -0700

    Fix for SparkContext stop behavior

--- core/src/test/scala/org/apache/spark/SparkContextSuite.scala
+++ core/src/test/scala/org/apache/spark/SparkContextSuite.scala
@@ -275,16 +275,14 @@ class SparkContextSuite extends SparkFunSuite with LocalSparkContext {
   }
 
   test("calling multiple sc.stop() must not throw any exception") {
-    try {
+    noException should be thrownBy {
       sc = new SparkContext(new SparkConf().setAppName("test").setMaster("local"))
       val cnt = sc.parallelize(1 to 4).count()
       sc.cancelAllJobs()
       sc.stop()
       // call stop second time
       sc.stop()
-    } catch {
-      case e: Exception =>
-        fail("calling multiple sc.stop() must not have thrown any exception", e);
     }
   }
+
 }
