09ad6981bbe875e8b479cf0acc1fc2b2992c1384
Ilya Ganelin <ilya.ganelin@capitalone.com>
2015-06-30 16:42:47 -0400

    Update SubmitRestProtocolSuite.scala
    Fixed import ordering.

--- core/src/test/scala/org/apache/spark/deploy/rest/SubmitRestProtocolSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/rest/SubmitRestProtocolSuite.scala
@@ -19,8 +19,9 @@ package org.apache.spark.deploy.rest
 
 import java.lang.Boolean
 
-import org.apache.spark.{SparkConf, SparkFunSuite}
 import org.json4s.jackson.JsonMethods._
+
+import org.apache.spark.{SparkConf, SparkFunSuite}
 
 /**
  * Tests for the REST application submission protocol.
