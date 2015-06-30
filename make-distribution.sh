c38a005db066ad46f8897b379c3b48ee0bc2af66
Sun Rui <rui.sun@intel.com>
2015-07-01 07:34:22 +0800

    Unzipped SparkR binary package is still required for standalone and Mesos modes.

--- make-distribution.sh
+++ make-distribution.sh
@@ -218,6 +218,7 @@ cp -r "$SPARK_HOME/ec2" "$DISTDIR"
 # Copy SparkR if it exists
 if [ -d "$SPARK_HOME"/R/lib/SparkR ]; then
   mkdir -p "$DISTDIR"/R/lib
+  cp -r "$SPARK_HOME/R/lib/SparkR" "$DISTDIR"/R/lib
   cp "$SPARK_HOME/R/lib/sparkr.zip" "$DISTDIR"/R/lib
 fi
 
