433dbc72943ea64fe295c8dfc0a343bbbf815696
Feynman Liang <fliang@databricks.com>
2015-06-30 11:03:37 -0700

    Test refactoring

--- mllib/src/test/scala/org/apache/spark/ml/feature/DiscreteCosineTransformerSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/DiscreteCosineTransformerSuite.scala
@@ -30,30 +30,32 @@ import org.apache.spark.sql.{DataFrame, Row}
 case class DCTTestData(vec: Vector, wantedVec: Vector)
 
 class DiscreteCosineTransformerSuite extends SparkFunSuite with MLlibTestSparkContext {
-  import org.apache.spark.ml.feature.DiscreteCosineTransformerSuite._
 
   test("forward transform of discrete cosine matches jTransforms result") {
     val data = Vectors.dense((0 until 128).map(_ => 2D * math.random - 1D).toArray)
     val inverse = false
 
-    val dataset = sqlContext.createDataFrame(Seq(
-      DCTTestData(data, makeExpectedResult(data, inverse))
-    ))
-
-    val transformer = new DiscreteCosineTransformer()
-      .setInputCol("vec")
-      .setOutputCol("resultVec")
-      .setInverse(inverse)
-
-    testDCT(transformer, dataset)
+    testDCT(data, inverse)
   }
 
   test("inverse transform of discrete cosine matches jTransforms result") {
     val data = Vectors.dense((0 until 128).map(_ => 2D * math.random - 1D).toArray)
     val inverse = true
 
+    testDCT(data, inverse)
+  }
+
+  private def testDCT(data: Vector, inverse: Boolean): Unit = {
+    val expectedResultBuffer = data.toArray.clone()
+    if (inverse) {
+      (new DoubleDCT_1D(data.size)).inverse(expectedResultBuffer, true)
+    } else {
+      (new DoubleDCT_1D(data.size)).forward(expectedResultBuffer, true)
+    }
+    val expectedResult = Vectors.dense(expectedResultBuffer)
+
     val dataset = sqlContext.createDataFrame(Seq(
-      DCTTestData(data, makeExpectedResult(data, inverse))
+      DCTTestData(data, expectedResult)
     ))
 
     val transformer = new DiscreteCosineTransformer()
@@ -61,28 +63,11 @@ class DiscreteCosineTransformerSuite extends SparkFunSuite with MLlibTestSparkCo
       .setOutputCol("resultVec")
       .setInverse(inverse)
 
-    testDCT(transformer, dataset)
-  }
-}
-
-object DiscreteCosineTransformerSuite extends SparkFunSuite {
-
-  def makeExpectedResult(data: Vector, inverse: Boolean): Vector = {
-    val expectedResultBuffer = data.toArray.clone()
-    if (inverse) {
-      (new DoubleDCT_1D(data.size)).inverse(expectedResultBuffer, true)
-    } else {
-      (new DoubleDCT_1D(data.size)).forward(expectedResultBuffer, true)
-    }
-    Vectors.dense(expectedResultBuffer)
-  }
-
-  def testDCT(t: DiscreteCosineTransformer, dataset: DataFrame): Unit = {
-    t.transform(dataset)
+    transformer.transform(dataset)
       .select("resultVec", "wantedVec")
       .collect()
-      .foreach { case Row(resultVec: Vector, wantedVec : Vector) =>
-        assert(Vectors.sqdist(resultVec, wantedVec) < 1e-6)
-      }
+      .foreach { case Row(resultVec: Vector, wantedVec: Vector) =>
+      assert(Vectors.sqdist(resultVec, wantedVec) < 1e-6)
+    }
   }
 }
