742dcb0b3ff3f03fb164dacb9a5a9eb271a1d558
Cheng Lian <lian@databricks.com>
2015-06-29 18:31:09 -0700

    Escapes `/*' in the comment

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -824,8 +824,8 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
    * }}}
    *
    * @note Small files are preferred, large file is also allowable, but may cause bad performance.
-   * @note On some filesystems, `.../path/*` can be a more efficient way to read all files in a directory
-   *       rather than `.../path/` or `.../path`
+   * @note On some filesystems, <code>.../path/&#42;</code> can be a more efficient way to read all
+   *       files in a directory rather than `.../path/` or `.../path`
    *
    * @param minPartitions A suggestion value of the minimal splitting number for input data.
    */
@@ -874,8 +874,8 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
    * }}}
    *
    * @note Small files are preferred; very large files may cause bad performance.
-   * @note On some filesystems, `.../path/*` can be a more efficient way to read all files in a directory
-   *       rather than `.../path/` or `.../path`
+   * @note On some filesystems, <code>.../path/&#42;</code> can be a more efficient way to read all
+   *       files in a directory rather than `.../path/` or `.../path`
    *
    * @param minPartitions A suggestion value of the minimal splitting number for input data.
    */
