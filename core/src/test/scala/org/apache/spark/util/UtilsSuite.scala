6cff13a53eed560da434b625efc6b077f7265c12
Marcelo Vanzin <vanzin@cloudera.com>
2015-06-30 19:11:09 -0700

    [SPARK-3444] [core] Restore INFO level after log4j test.
    Otherwise other tests don't log anything useful...

--- core/src/test/scala/org/apache/spark/util/UtilsSuite.scala
+++ core/src/test/scala/org/apache/spark/util/UtilsSuite.scala
@@ -486,11 +486,17 @@ class UtilsSuite extends SparkFunSuite with ResetSystemProperties with Logging {
 
   // Test for using the util function to change our log levels.
   test("log4j log level change") {
-    Utils.setLogLevel(org.apache.log4j.Level.ALL)
-    assert(log.isInfoEnabled())
-    Utils.setLogLevel(org.apache.log4j.Level.ERROR)
-    assert(!log.isInfoEnabled())
-    assert(log.isErrorEnabled())
+    try {
+      Utils.setLogLevel(org.apache.log4j.Level.ALL)
+      assert(log.isInfoEnabled())
+      Utils.setLogLevel(org.apache.log4j.Level.ERROR)
+      assert(!log.isInfoEnabled())
+      assert(log.isErrorEnabled())
+    } finally {
+      // Best effort at undoing changes this test made. Level should be synchronized with the
+      // log4j.properties file used for testing.
+      Utils.setLogLevel(org.apache.log4j.Level.INFO)
+    }
   }
 
   test("deleteRecursively") {
