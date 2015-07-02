07feb07064998a299057b796b85a3274ce5566b8
Deron Eriksson <deron@us.ibm.com>
2015-07-02 11:23:22 -0700

    added linebreaks to make jquery more readable when adding html badge tags

--- docs/js/api-javadocs.js
+++ docs/js/api-javadocs.js
@@ -37,8 +37,14 @@ function addBadges(tag, html) {
   tags.each(function(index) {
     if ($(this).parent().is('td.colLast')) {
       $(this).parent().prepend(html);
-    } else if ($(this).parent('li.blockList').parent('ul.blockList').parent('div.description').parent().is('div.contentContainer')) {
-      var contentContainer = $(this).parent('li.blockList').parent('ul.blockList').parent('div.description').parent('div.contentContainer')
+    } else if ($(this).parent('li.blockList')
+                      .parent('ul.blockList')
+                      .parent('div.description')
+                      .parent().is('div.contentContainer')) {
+      var contentContainer = $(this).parent('li.blockList')
+                                    .parent('ul.blockList')
+                                    .parent('div.description')
+                                    .parent('div.contentContainer')
       var header = contentContainer.prev('div.header');
       if (header.length > 0) {
         header.prepend(html);
