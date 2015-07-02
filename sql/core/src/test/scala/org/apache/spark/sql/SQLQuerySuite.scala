0036696e12cd148908ac2d671f73a291054e509f
Josh Rosen <joshrosen@databricks.com>
2015-07-01 22:40:55 -0700

    Add regression test for SPARK-8782 (ORDER BY NULL)

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -1451,4 +1451,11 @@ class SQLQuerySuite extends QueryTest with BeforeAndAfterAll with SQLTestUtils {
       checkAnswer(sql("SELECT a.b FROM t ORDER BY b[0].d"), Row(Seq(Row(1))))
     }
   }
+
+  test("SPARK-8782: ORDER BY NULL") {
+    withTempTable("t") {
+      Seq((1, 2), (1, 2)).toDF("a", "b").registerTempTable("t")
+      checkAnswer(sql("SELECT * FROM t ORDER BY NULL"), Seq(Row(1, 2), Row(1, 2)))
+    }
+  }
 }
