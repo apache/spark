6398c57b971cd53de56e0fe701ea158ee4bec640
Maxime <maxime.beauchemin@airbnb.com>
2015-07-04 22:17:43 +0000

    Better docs for PrestoCheckOperator

--- airflow/operators/presto_check_operator.py
+++ airflow/operators/presto_check_operator.py
@@ -8,7 +8,30 @@ from airflow.utils import apply_defaults
 
 class PrestoCheckOperator(BaseOperator):
     """
-    Performs a simple check using sql code in a specific Presto database.
+    Performs checks against Presto. The ``PrestoCheckOperator`` expects
+    a sql query that will return a single row. Each value on that
+    first row is evaluated using python ``bool`` casting. If any of the
+    values return ``False`` the check is failed and errors out.
+
+    Note that Python bool casting evals the following as ``False``:
+    * False
+    * 0
+    * Empty string (``""``)
+    * Empty list (``[]``)
+    * Empty dictionary or set (``{}``)
+
+    Given a query like ``SELECT COUNT(*) FROM foo``, it will fail only if
+    the count ``== 0``. You can craft much more complex query that could,
+    for instance, check that the table has the same number of rows as
+    the source table upstream, or that the count of today's partition is
+    greater than yesterday's partition, or that a set of metrics are less
+    than 3 standard deviation for the 7 day average.
+
+    This operator can be used as a data quality check in your pipeline, and
+    depending on where you put it in your DAG, you have the choice to
+    stop the critical path, preventing from
+    publishing dubious data, or on the side and receive email alterts
+    without stopping the progress of the DAG.
 
     :param sql: the sql to be executed
     :type sql: string
