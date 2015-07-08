1f541be7f06c46ca011a08501087a04c95b6df4d
BenFradet <benjamin.fradet@gmail.com>
2015-07-08 19:19:28 +0200

    removed TestData use from DebuggingSuite

--- sql/core/src/test/scala/org/apache/spark/sql/execution/debug/DebuggingSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/debug/DebuggingSuite.scala
@@ -18,10 +18,12 @@
 package org.apache.spark.sql.execution.debug
 
 import org.apache.spark.SparkFunSuite
-import org.apache.spark.sql.TestData._
-import org.apache.spark.sql.test.TestSQLContext._
+import org.apache.spark.sql.test.TestSQLContext.implicits._
 
 class DebuggingSuite extends SparkFunSuite {
+  case class TestData(key: Int, value: String)
+  val testData = (1 to 100).map(i => TestData(i, i.toString)).toDF()
+
   test("DataFrame.debug()") {
     testData.debug()
   }
