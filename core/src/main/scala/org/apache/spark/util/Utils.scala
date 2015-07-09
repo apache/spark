dadfd60b4f65b93014e83f82b011f8981103604d
Michael Armbrust <michael@databricks.com>
2015-07-09 16:52:19 -0700

    Update Utils.scala

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -1808,12 +1808,12 @@ private[spark] object Utils extends Logging {
       val interpClass = classForName("spark.repl.Main")
       interpClass.getMethod("interp").invoke(null) != null
     } catch {
-       // Returning true seems to be a mistake.
-       // Currently changing it to false causes tests failures in Streaming.
-       // For a more detailed discussion, please, refer to
-       // https://github.com/apache/spark/pull/5835#issuecomment-101042271 and subsequent comments.
-       // Addressing this changed is tracked as https://issues.apache.org/jira/browse/SPARK-7527
-       case _: ClassNotFoundException => true
+      // Returning true seems to be a mistake.
+      // Currently changing it to false causes tests failures in Streaming.
+      // For a more detailed discussion, please, refer to
+      // https://github.com/apache/spark/pull/5835#issuecomment-101042271 and subsequent comments.
+      // Addressing this changed is tracked as https://issues.apache.org/jira/browse/SPARK-7527
+      case _: ClassNotFoundException => true
     }
   }
 
