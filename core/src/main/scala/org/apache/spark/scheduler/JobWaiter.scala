2c86a9e0ff4dd11ea0680cda45bb4f64c4e01aff
zsxwing <zsxwing@gmail.com>
2015-07-09 17:07:47 +0800

    Use tryFailure to support calling jobFailed multiple times

--- core/src/main/scala/org/apache/spark/scheduler/JobWaiter.scala
+++ core/src/main/scala/org/apache/spark/scheduler/JobWaiter.scala
@@ -62,7 +62,7 @@ private[spark] class JobWaiter[T](
     if (finishedTasks == totalTasks) {
       _jobFinished = true
       jobResult = JobSucceeded
-      promise.success()
+      promise.trySuccess()
       this.notifyAll()
     }
   }
@@ -70,7 +70,7 @@ private[spark] class JobWaiter[T](
   override def jobFailed(exception: Exception): Unit = synchronized {
     _jobFinished = true
     jobResult = JobFailed(exception)
-    promise.failure(exception)
+    promise.tryFailure(exception)
     this.notifyAll()
   }
 
