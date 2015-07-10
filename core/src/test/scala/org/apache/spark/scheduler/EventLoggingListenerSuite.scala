c11c68993a0c8b41eac6d6c5b3e50a1d42b8116f
nishkamravi2 <nishkamravi@gmail.com>
2015-07-09 20:57:30 -0700

    Update EventLoggingListenerSuite.scala
    Bug in the test

--- core/src/test/scala/org/apache/spark/scheduler/EventLoggingListenerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/EventLoggingListenerSuite.scala
@@ -180,7 +180,7 @@ class EventLoggingListenerSuite extends SparkFunSuite with LocalSparkContext wit
     // into SPARK-6688.
     val conf = getLoggingConf(testDirPath, compressionCodec)
       .set("spark.hadoop.fs.defaultFS", "unsupported://example.com")
-    val sc = new SparkContext("local-cluster[2,2,512]", "test", conf)
+    val sc = new SparkContext("local-cluster[2,2,1024]", "test", conf)
     assert(sc.eventLogger.isDefined)
     val eventLogger = sc.eventLogger.get
     val eventLogPath = eventLogger.logPath
