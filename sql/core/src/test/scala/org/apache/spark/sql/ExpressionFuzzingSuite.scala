f8daec768cd08a05a8b6564ac74d2fe8ced4b498
Josh Rosen <joshrosen@databricks.com>
2015-07-10 20:03:46 -0700

    Apply implicit casts (in a hacky way for now)

--- sql/core/src/test/scala/org/apache/spark/sql/ExpressionFuzzingSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/ExpressionFuzzingSuite.scala
@@ -21,10 +21,30 @@ import java.io.File
 
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.analysis.HiveTypeCoercion
 import org.apache.spark.sql.catalyst.expressions.codegen.GenerateMutableProjection
 import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.rules.RuleExecutor
 import org.apache.spark.sql.types.NullType
 import org.clapper.classutil.ClassFinder
+
+case object DummyAnalyzer extends RuleExecutor[LogicalPlan] {
+  override protected val batches: Seq[Batch] = Seq(
+    Batch("analysis", FixedPoint(100), HiveTypeCoercion.typeCoercionRules: _*)
+  )
+}
+
+
+case class DummyPlan(expr: Expression) extends LogicalPlan {
+  override def output: Seq[Attribute] = Seq.empty
+
+  /** Returns all of the expressions present in this query plan operator. */
+  override def expressions: Seq[Expression] = Seq(expr)
+
+  override def children: Seq[LogicalPlan] = Seq.empty
+}
+
 
 class ExpressionFuzzingSuite extends SparkFunSuite {
   lazy val expressionClasses: Seq[Class[_]] = {
@@ -36,24 +56,27 @@ class ExpressionFuzzingSuite extends SparkFunSuite {
   }
 
   expressionClasses.foreach(println)
+
   for (c <- expressionClasses) {
     val singleExprConstructor = c.getConstructors.filter { c =>
       c.getParameterTypes.toSeq == Seq(classOf[Expression])
     }
     singleExprConstructor.foreach { cons =>
-      try {
+      test(s"${c.getName}") {
         val expr: Expression = cons.newInstance(Literal(null)).asInstanceOf[Expression]
-        val row: InternalRow = new GenericInternalRow(Array[Any](null))
-        val gened = GenerateMutableProjection.generate(Seq(expr), Seq(AttributeReference("f", NullType)()))
-        gened()
-//        expr.eval(row)
-        println(s"Passed $c")
-      } catch {
-        case e: Exception =>
-          println(s"Got exception $e while testing $c")
-          e.printStackTrace(System.out)
+        val coercedExpr: Expression = {
+          val dummyPlan = DummyPlan(expr)
+          DummyAnalyzer.execute(dummyPlan).asInstanceOf[DummyPlan].expr
+        }
+        if (expr.checkInputDataTypes().isSuccess) {
+          val row: InternalRow = new GenericInternalRow(Array[Any](null))
+          val gened = GenerateMutableProjection.generate(Seq(coercedExpr), Seq(AttributeReference("f", NullType)()))
+          gened()
+          //        expr.eval(row)
+        } else {
+          println(s"Input types check failed for $c")
+        }
       }
-
     }
   }
 }
