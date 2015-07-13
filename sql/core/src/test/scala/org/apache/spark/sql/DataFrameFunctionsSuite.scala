2f74b3bbfda75f7125ba9f9a0b56dfef559c2497
BenFradet <benjamin.fradet@gmail.com>
2015-07-13 15:49:53 +0200

    removed case classes from DataFrameFunctionsSuite

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -116,14 +116,9 @@ class DataFrameFunctionsSuite extends QueryTest {
     checkAnswer(result, Seq(Row(Row("v", 5.0)), Row(Row("v", 5.0))))
   }
 
-  case class TestData2(a: Int, b: Int)
-  val testData2 = {
-    val df = (for { a <- 1 to 3; b <- 1 to 2 } yield (a, b))
-      .map(t => TestData2(t._1, t._2))
-      .toDF()
-    df.registerTempTable("testData2")
-    df
-  }
+  val testData2 = (for { a <- 1 to 3; b <- 1 to 2 } yield (a, b))
+      .map(t => (t._1, t._2))
+      .toDF("a", "b")
 
   test("constant functions") {
     checkAnswer(
