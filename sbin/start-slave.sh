0cac1d76ccf5b18b728062dbed2e07db30e18948
Ben Zimmer <ben.d.zimmer@gmail.com>
2015-07-13 10:15:47 -0500

    get fully qualified hostnames
    In previous releases, workers automatically found their fully qualified hostnames when started with start-slaves.sh / start-slave.sh.

--- sbin/start-slave.sh
+++ sbin/start-slave.sh
@@ -71,7 +71,7 @@ function start_instance {
   fi
   WEBUI_PORT=$(( $SPARK_WORKER_WEBUI_PORT + $WORKER_NUM - 1 ))
 
-  HOST_NAME=`hostname`
+  HOST_NAME=`hostname -f`
 
   "$sbin"/spark-daemon.sh start org.apache.spark.deploy.worker.Worker $WORKER_NUM \
      --host $HOST_NAME --webui-port "$WEBUI_PORT" $PORT_FLAG $PORT_NUM $MASTER "$@"
