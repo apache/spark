6664b7e82e39b369c740e70c6a2c0e633a9b5617
Joseph Gonzalez <joseph.e.gonzalez@gmail.com>
2015-07-13 10:28:34 -0700

    making requested changes

--- launcher/src/main/java/org/apache/spark/launcher/SparkLauncher.java
+++ launcher/src/main/java/org/apache/spark/launcher/SparkLauncher.java
@@ -27,9 +27,10 @@ import static org.apache.spark.launcher.CommandBuilderUtils.*;
 
 /** 
  * Launcher for Spark applications.
- *
+ * <p>
  * Use this class to start Spark applications programmatically. The class uses a builder pattern
  * to allow clients to configure the Spark application and launch it as a child process.
+ * </p>
  */
 public class SparkLauncher {
 
