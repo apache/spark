a3080342ceb3c4591e8fc086fbcba6a48db429a6
BenFradet <benjamin.fradet@gmail.com>
2015-07-15 00:01:26 +0200

    fixed scalastyle

--- sql/core/src/test/scala/org/apache/spark/sql/ColumnExpressionSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/ColumnExpressionSuite.scala
@@ -186,7 +186,7 @@ class ColumnExpressionSuite extends QueryTest with SQLTestUtils {
   case class TestData(key: Int, value: String)
   case class ComplexData(m: Map[String, Int], s: TestData, a: Seq[Int], b: Boolean)
   test("unary !") {
-    val complexData =Seq(
+    val complexData = Seq(
       ComplexData(Map("1" -> 1), TestData(1, "1"), Seq(1), true),
       ComplexData(Map("2" -> 2), TestData(2, "2"), Seq(2), false)
     ).toDF()
