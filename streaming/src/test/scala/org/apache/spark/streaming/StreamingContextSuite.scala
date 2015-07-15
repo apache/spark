f957796c4b3c3cd95edfc64500a045f7e810ee87
huangzhaowei <carlmartinmax@gmail.com>
2015-07-14 19:20:49 -0700

    [SPARK-8820] [STREAMING] Add a configuration to set checkpoint dir.
    Add a configuration to set checkpoint directory  for convenience to user.
    [Jira Address](https://issues.apache.org/jira/browse/SPARK-8820)
    
    Author: huangzhaowei <carlmartinmax@gmail.com>
    
    Closes #7218 from SaintBacchus/SPARK-8820 and squashes the following commits:
    
    d49fe4b [huangzhaowei] Rename the configuration name
    66ea47c [huangzhaowei] Add the unit test.
    dd0acc1 [huangzhaowei] [SPARK-8820][Streaming] Add a configuration to set checkpoint dir.

--- streaming/src/test/scala/org/apache/spark/streaming/StreamingContextSuite.scala
+++ streaming/src/test/scala/org/apache/spark/streaming/StreamingContextSuite.scala
@@ -115,6 +115,15 @@ class StreamingContextSuite extends SparkFunSuite with BeforeAndAfter with Timeo
     assert(ssc.conf.getTimeAsSeconds("spark.cleaner.ttl", "-1") === 10)
   }
 
+  test("checkPoint from conf") {
+    val checkpointDirectory = Utils.createTempDir().getAbsolutePath()
+
+    val myConf = SparkContext.updatedConf(new SparkConf(false), master, appName)
+    myConf.set("spark.streaming.checkpoint.directory", checkpointDirectory)
+    val ssc = new StreamingContext(myConf, batchDuration)
+    assert(ssc.checkpointDir != null)
+  }
+
   test("state matching") {
     import StreamingContextState._
     assert(INITIALIZED === INITIALIZED)
