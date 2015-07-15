8fcf814be0465cc1b52f877b0f1b3f6e5c72a16a
Reynold Xin <rxin@databricks.com>
2015-07-14 19:27:23 -0700

    Fixed ordering of cases.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/HiveTypeCoercion.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/HiveTypeCoercion.scala
@@ -666,6 +666,19 @@ object HiveTypeCoercion {
       // Skip nodes who's children have not been resolved yet.
       case e if !e.childrenResolved => e
 
+      case b @ BinaryOperator(left, right) if left.dataType != right.dataType =>
+        findTightestCommonTypeOfTwo(left.dataType, right.dataType).map { commonType =>
+          if (b.inputType.acceptsType(commonType)) {
+            // If the expression accepts the tighest common type, cast to that.
+            val newLeft = if (left.dataType == commonType) left else Cast(left, commonType)
+            val newRight = if (right.dataType == commonType) right else Cast(right, commonType)
+            b.makeCopy(Array(newLeft, newRight))
+          } else {
+            // Otherwise, don't do anything with the expression.
+            b
+          }
+        }.getOrElse(b)  // If there is no applicable conversion, leave expression unchanged.
+
       case e: ImplicitCastInputTypes if e.inputTypes.nonEmpty =>
         val children: Seq[Expression] = e.children.zip(e.inputTypes).map { case (in, expected) =>
           // If we cannot do the implicit cast, just use the original input.
@@ -684,28 +697,6 @@ object HiveTypeCoercion {
           }
         }
         e.withNewChildren(children)
-
-      case b @ BinaryOperator(left, right)
-        if left.dataType == NullType && right.dataType == NullType &&
-          !b.inputType.acceptsType(NullType) =>
-        // If both inputs are null type (from null literals), cast the null type into some
-        // specific type the expression expects, so expressions don't need to handle NullType
-        val newLeft = Cast(left, b.inputType.defaultConcreteType)
-        val newRight = Cast(right, b.inputType.defaultConcreteType)
-        b.makeCopy(Array(newLeft, newRight))
-
-      case b @ BinaryOperator(left, right) if left.dataType != right.dataType =>
-        findTightestCommonTypeOfTwo(left.dataType, right.dataType).map { commonType =>
-          if (b.inputType.acceptsType(commonType)) {
-            // If the expression accepts the tighest common type, cast to that.
-            val newLeft = if (left.dataType == commonType) left else Cast(left, commonType)
-            val newRight = if (right.dataType == commonType) right else Cast(right, commonType)
-            b.makeCopy(Array(newLeft, newRight))
-          } else {
-            // Otherwise, don't do anything with the expression.
-            b
-          }
-        }.getOrElse(b)  // If there is no applicable conversion, leave expression unchanged.
     }
 
     /**
