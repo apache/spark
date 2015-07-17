2bfbcb6d3343218f4147bf82d8e6dd5fc0b43874
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-07-17 00:25:03 -0700

    added unit test for size

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -289,6 +289,7 @@ class DataFrameFunctionsSuite extends QueryTest {
       (Map[Int, Int](), "y"),
       (Map[Int, Int](1 -> 1, 2 -> 2, 3 -> 3), "z")
     ).toDF("a", "b")
+
     checkAnswer(
       df.select(size("a")),
       Seq(Row(2), Row(0), Row(3))
