3998ef208f73b7af6e070f42f9ddfe01efb22890
Josh Rosen <joshrosen@databricks.com>
2015-07-17 23:03:42 -0700

    Remove unused code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/TypeUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/TypeUtils.scala
@@ -62,14 +62,4 @@ object TypeUtils {
     }
     x.length - y.length
   }
-
-  def compareFloats(x: Float, y: Float): Int = {
-    val xIsNan: Boolean = java.lang.Float.isNaN(x)
-    val yIsNan: Boolean = java.lang.Float.isNaN(y)
-    if ((xIsNan && yIsNan) || (x == y)) 0
-    else if (xIsNan) 1
-    else if (yIsNan) -1
-    else if (x > y) 1
-    else -1
-  }
 }
