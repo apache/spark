d4c7a7a3642a74ad40093c96c4bf45a62a470605
Tarek Auel <tarek.auel@googlemail.com>
2015-07-21 15:47:40 -0700

    [SPARK-9154] [SQL] codegen StringFormat
    Jira: https://issues.apache.org/jira/browse/SPARK-9154
    
    fixes bug of #7546
    
    marmbrus I can't reopen the other PR, because I didn't closed it. Can you trigger Jenkins?
    
    Author: Tarek Auel <tarek.auel@googlemail.com>
    
    Closes #7571 from tarekauel/SPARK-9154 and squashes the following commits:
    
    dcae272 [Tarek Auel] [SPARK-9154][SQL] build fix
    1487602 [Tarek Auel] Merge remote-tracking branch 'upstream/master' into SPARK-9154
    f512c5f [Tarek Auel] [SPARK-9154][SQL] build fix
    a943d3e [Tarek Auel] [SPARK-9154] implicit input cast, added tests for null, support for null primitives
    10b4de8 [Tarek Auel] [SPARK-9154][SQL] codegen removed fallback trait
    cd8322b [Tarek Auel] [SPARK-9154][SQL] codegen string format
    086caba [Tarek Auel] [SPARK-9154][SQL] codegen string format

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/stringOperations.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/stringOperations.scala
@@ -526,7 +526,7 @@ case class StringRPad(str: Expression, len: Expression, pad: Expression)
 /**
  * Returns the input formatted according do printf-style format strings
  */
-case class StringFormat(children: Expression*) extends Expression with CodegenFallback {
+case class StringFormat(children: Expression*) extends Expression with ImplicitCastInputTypes {
 
   require(children.nonEmpty, "printf() should take at least 1 argument")
 
@@ -535,6 +535,10 @@ case class StringFormat(children: Expression*) extends Expression with CodegenFa
   override def dataType: DataType = StringType
   private def format: Expression = children(0)
   private def args: Seq[Expression] = children.tail
+
+  override def inputTypes: Seq[AbstractDataType] =
+    StringType :: List.fill(children.size - 1)(AnyDataType)
+
 
   override def eval(input: InternalRow): Any = {
     val pattern = format.eval(input)
@@ -549,6 +553,42 @@ case class StringFormat(children: Expression*) extends Expression with CodegenFa
 
       UTF8String.fromString(sb.toString)
     }
+  }
+
+  override def genCode(ctx: CodeGenContext, ev: GeneratedExpressionCode): String = {
+    val pattern = children.head.gen(ctx)
+
+    val argListGen = children.tail.map(x => (x.dataType, x.gen(ctx)))
+    val argListCode = argListGen.map(_._2.code + "\n")
+
+    val argListString = argListGen.foldLeft("")((s, v) => {
+      val nullSafeString =
+        if (ctx.boxedType(v._1) != ctx.javaType(v._1)) {
+          // Java primitives get boxed in order to allow null values.
+          s"(${v._2.isNull}) ? (${ctx.boxedType(v._1)}) null : " +
+            s"new ${ctx.boxedType(v._1)}(${v._2.primitive})"
+        } else {
+          s"(${v._2.isNull}) ? null : ${v._2.primitive}"
+        }
+      s + "," + nullSafeString
+    })
+
+    val form = ctx.freshName("formatter")
+    val formatter = classOf[java.util.Formatter].getName
+    val sb = ctx.freshName("sb")
+    val stringBuffer = classOf[StringBuffer].getName
+    s"""
+      ${pattern.code}
+      boolean ${ev.isNull} = ${pattern.isNull};
+      ${ctx.javaType(dataType)} ${ev.primitive} = ${ctx.defaultValue(dataType)};
+      if (!${ev.isNull}) {
+        ${argListCode.mkString}
+        $stringBuffer $sb = new $stringBuffer();
+        $formatter $form = new $formatter($sb, ${classOf[Locale].getName}.US);
+        $form.format(${pattern.primitive}.toString() $argListString);
+        ${ev.primitive} = UTF8String.fromString($sb.toString());
+      }
+     """
   }
 
   override def prettyName: String = "printf"
