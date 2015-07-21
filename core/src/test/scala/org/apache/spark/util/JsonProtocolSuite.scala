3f237e74d46de13a0077352cc4740d66535c4a87
Ben <benjaminpiering@gmail.com>
2015-07-20 20:45:57 -0500

    Update JSON Protocol Tests

--- core/src/test/scala/org/apache/spark/util/JsonProtocolSuite.scala
+++ core/src/test/scala/org/apache/spark/util/JsonProtocolSuite.scala
@@ -84,7 +84,8 @@ class JsonProtocolSuite extends SparkFunSuite {
       new ExecutorInfo("Hostee.awesome.com", 11, logUrlMap))
     val executorRemoved = SparkListenerExecutorRemoved(executorRemovedTime, "exec2", "test reason")
     val executorMetricsUpdate = SparkListenerExecutorMetricsUpdate("exec3", Seq(
-      (1L, 2, 3, makeTaskMetrics(300L, 400L, 500L, 600L, 700, 800, hasHadoopInput = true, hasOutput = true))))
+      (1L, 2, 3,
+        makeTaskMetrics(300L, 400L, 500L, 600L, 700, 800, hasHadoopInput = true, hasOutput = true))))
 
     testEvent(stageSubmitted, stageSubmittedJsonString)
     testEvent(stageCompleted, stageCompletedJsonString)
