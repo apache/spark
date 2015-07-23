a4fd9d2718479528a01bd6720a46f2bddcf5e01b
zsxwing <zsxwing@gmail.com>
2015-07-24 01:13:34 +0800

    Fix WorkerWatcherSuite

--- core/src/main/scala/org/apache/spark/deploy/worker/WorkerWatcher.scala
+++ core/src/main/scala/org/apache/spark/deploy/worker/WorkerWatcher.scala
@@ -24,7 +24,8 @@ import org.apache.spark.rpc._
  * Actor which connects to a worker process and terminates the JVM if the connection is severed.
  * Provides fate sharing between a worker and its associated child processes.
  */
-private[spark] class WorkerWatcher(override val rpcEnv: RpcEnv, workerUrl: String)
+private[spark] class WorkerWatcher(
+    override val rpcEnv: RpcEnv, workerUrl: String, isTesting: Boolean = false)
   extends RpcEndpoint with Logging {
 
   logInfo(s"Connecting to worker $workerUrl")
@@ -38,8 +39,6 @@ private[spark] class WorkerWatcher(override val rpcEnv: RpcEnv, workerUrl: Strin
   // true rather than calling `System.exit`. The user can check `isShutDown` to know if
   // `exitNonZero` is called.
   private[deploy] var isShutDown = false
-  private[deploy] def setTesting(testing: Boolean) = isTesting = testing
-  private var isTesting = false
 
   // Lets us filter events only from the worker's actor system
   private val expectedAddress = RpcAddress.fromURIString(workerUrl)
