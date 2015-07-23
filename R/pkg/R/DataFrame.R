ecfb3127670c7f15e3a15e7f51fa578532480cda
Xiangrui Meng <meng@databricks.com>
2015-07-23 10:32:11 -0700

    [SPARK-9243] [Documentation] null -> zero in crosstab doc
    We forgot to update doc. brkyvz
    
    Author: Xiangrui Meng <meng@databricks.com>
    
    Closes #7608 from mengxr/SPARK-9243 and squashes the following commits:
    
    0ea3236 [Xiangrui Meng] null -> zero in crosstab doc

--- R/pkg/R/DataFrame.R
+++ R/pkg/R/DataFrame.R
@@ -1566,7 +1566,7 @@ setMethod("fillna",
 #' @return a local R data.frame representing the contingency table. The first column of each row
 #'         will be the distinct values of `col1` and the column names will be the distinct values
 #'         of `col2`. The name of the first column will be `$col1_$col2`. Pairs that have no
-#'         occurrences will have `null` as their counts.
+#'         occurrences will have zero as their counts.
 #'
 #' @rdname statfunctions
 #' @export
--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameStatFunctions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameStatFunctions.scala
@@ -77,7 +77,7 @@ final class DataFrameStatFunctions private[sql](df: DataFrame) {
    * pair frequencies will be returned.
    * The first column of each row will be the distinct values of `col1` and the column names will
    * be the distinct values of `col2`. The name of the first column will be `$col1_$col2`. Counts
-   * will be returned as `Long`s. Pairs that have no occurrences will have `null` as their counts.
+   * will be returned as `Long`s. Pairs that have no occurrences will have zero as their counts.
    * Null elements will be replaced by "null", and back ticks will be dropped from elements if they
    * exist.
    *
