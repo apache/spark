adc3c7f34c866f469d1f2d92e44568943539a784
Josh Rosen <joshrosen@databricks.com>
2015-07-23 13:31:06 -0700

    Test with random inputs of all types

--- sql/core/src/test/scala/org/apache/spark/sql/ExpressionFuzzingSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/ExpressionFuzzingSuite.scala
@@ -20,6 +20,8 @@ package org.apache.spark.sql
 import java.io.File
 import java.lang.reflect.Constructor
 
+import scala.util.{Try, Random}
+
 import org.clapper.classutil.ClassFinder
 
 import org.apache.spark.{Logging, SparkFunSuite}
@@ -29,7 +31,7 @@ import org.apache.spark.sql.catalyst.expressions.codegen.GenerateProjection
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.rules.RuleExecutor
-import org.apache.spark.sql.types.NullType
+import org.apache.spark.sql.types.{DecimalType, DataType, DataTypeTestUtils}
 
 /**
  * This test suite implements fuzz tests for expression code generation. It uses reflection to
@@ -40,10 +42,12 @@ import org.apache.spark.sql.types.NullType
  */
 class ExpressionFuzzingSuite extends SparkFunSuite with Logging {
 
+  val NUM_TRIALS_PER_EXPRESSION: Int = 100
+
   /**
-   * All subclasses of [[Expression]].
+   * All evaluable subclasses of [[Expression]].
    */
-  lazy val expressionSubclasses: Seq[Class[Expression]] = {
+  lazy val evaluableExpressionClasses: Seq[Class[Expression]] = {
     val classpathEntries: Seq[File] = System.getProperty("java.class.path")
       .split(File.pathSeparatorChar)
       .filter(_.contains("spark"))
@@ -53,6 +57,14 @@ class ExpressionFuzzingSuite extends SparkFunSuite with Logging {
     assert(allClasses.nonEmpty, "Could not find Spark classes on classpath.")
     ClassFinder.concreteSubclasses(classOf[Expression].getName, allClasses)
       .map(c => Class.forName(c.name).asInstanceOf[Class[Expression]]).toSeq
+      // We should only test evalulable expressions:
+      .filterNot(c => classOf[Unevaluable].isAssignableFrom(c))
+      // These expressions currently OOM because we try to pass in massive numeric literals:
+      .filterNot(_ == classOf[FormatNumber])
+      .filterNot(_ == classOf[StringSpace])
+      .filterNot(_ == classOf[StringLPad])
+      .filterNot(_ == classOf[StringRPad])
+      .filterNot(_ == classOf[Round])
   }
 
   def coerceTypes(expression: Expression): Expression = {
@@ -74,6 +86,17 @@ class ExpressionFuzzingSuite extends SparkFunSuite with Logging {
       .headOption
   }
 
+  def getRandomLiteral: Literal = {
+    // For now, filter out DecimalType since casts can lead to OOMs:
+    val allTypes = DataTypeTestUtils.atomicTypes.filterNot(_.isInstanceOf[DecimalType])
+    val dataTypesWithGenerators: Map[DataType, () => Any] = allTypes.map { dt =>
+      (dt, RandomDataGenerator.forType(dt, nullable = true, seed=None))
+    }.filter(_._2.isDefined).toMap.mapValues(_.get)
+    val (dt, generator) =
+      dataTypesWithGenerators.toSeq(Random.nextInt(dataTypesWithGenerators.size))
+    Literal.create(generator(), dt)
+  }
+
   def testExpression(expressionClass: Class[Expression]): Unit = {
     // Eventually, we should add support for testing multiple constructors. For now, though, we
     // only test the "best" one:
@@ -83,31 +106,38 @@ class ExpressionFuzzingSuite extends SparkFunSuite with Logging {
       maybeBestConstructor.get
     }
     val numChildren: Int = constructor.getParameterTypes.length
-    // Eventually, we should test with multiple types of child expressions. For now, though, we
-    // construct null literals for all child expressions and leave it up to the type coercion rules
-    // to cast them to the appropriate types.
-    val expression: Expression = {
-      val childExpressions: Seq[Expression] = Seq.fill(numChildren)(Literal.create(null, NullType))
-      coerceTypes(constructor.newInstance(childExpressions: _*))
+    // Construct random literals for all child expressions and leave it up to the type coercion
+    // rules to cast them to the appropriate types. Skip
+    for (_ <- 1 to NUM_TRIALS_PER_EXPRESSION) {
+      val expression: Expression = {
+        val childExpressions: Seq[Expression] = Seq.fill(numChildren)(getRandomLiteral)
+        coerceTypes(constructor.newInstance(childExpressions: _*))
+      }
+      logInfo(s"After type coercion, expression is $expression")
+      // Make sure that the resulting expression passes type checks.
+      val typecheckResult = expression.checkInputDataTypes()
+      if (typecheckResult.isFailure) {
+        logDebug(s"Type checks failed: $typecheckResult")
+      } else {
+        withClue(s"$expression") {
+          val inputRow = InternalRow.apply() // Can be empty since we're only using literals
+          val inputSchema = expression.children.map(c => AttributeReference("f", c.dataType)())
+
+          val interpretedProjection = new InterpretedProjection(Seq(expression), inputSchema)
+          val interpretedResult = interpretedProjection.apply(inputRow)
+
+          val maybeGenProjection = Try(GenerateProjection.generate(Seq(expression), inputSchema))
+          maybeGenProjection.foreach { generatedProjection =>
+            val generatedResult = generatedProjection.apply(inputRow)
+            assert(generatedResult === interpretedResult)
+          }
+        }
+      }
     }
-    logInfo(s"After type coercion, expression is $expression")
-    // Make sure that the resulting expression passes type checks.
-    val typecheckResult = expression.checkInputDataTypes()
-    assume(typecheckResult.isSuccess, s"Type checks failed: $typecheckResult")
-    // Attempt to generate code for this expression by using it to generate a projection.
-    val inputSchema = expression.children.map(c => AttributeReference("f", c.dataType)())
-    val generatedProjection = GenerateProjection.generate(Seq(expression), inputSchema)
-    val interpretedProjection = new InterpretedProjection(Seq(expression), inputSchema)
-    // Check that the answers agree for an input row consisting entirely of nulls, since the
-    // implicit type casts should make this safe
-    val inputRow = InternalRow.apply(Seq.fill(numChildren)(null))
-    val generatedResult = generatedProjection.apply(inputRow)
-    val interpretedResult = interpretedProjection.apply(inputRow)
-    assert(generatedResult === interpretedResult)
   }
 
   // Run the actual tests
-  expressionSubclasses.sortBy(_.getName).foreach { expressionClass =>
+  evaluableExpressionClasses.sortBy(_.getName).foreach { expressionClass =>
     test(s"${expressionClass.getName}") {
       testExpression(expressionClass)
     }
