0726c13a8ab2809160d414e52fd22f67254233af
Jan Omar <jan.omar@wooga.net>
2015-07-26 12:16:02 +0200

    use css to toggle visibility depending on connection type

--- airflow/www/static/jdbc.js
+++ airflow/www/static/jdbc.js
@@ -1,25 +1,17 @@
 /**
  * Created by janomar on 23/07/15.
  */
-  //ugly, refactor soon
   function toggleJdbc(connectionType) {
-       if (connectionType == 'jdbc') {
-           $("input[name='port']").parent().parent().hide();
-           $("input[name='schema']").parent().parent().hide();
-           //$("input[name='login']").parent().parent().hide();
-           //$("input[name='password']").parent().parent().hide();
-           $("input[name='extra']").parent().parent().hide();
-           $("input[name='jdbc_drv_clsname']").parent().parent().show();
-           $("input[name='jdbc_drv_path']").parent().parent().show();
+       isJdbc = connectionType == 'jdbc'
+       $("#port").parent().parent().toggleClass('hide', isJdbc)
+       $("#schema").parent().parent().toggleClass('hide', isJdbc)
+       $("#extra").parent().parent().toggleClass('hide', isJdbc)
+       $("#jdbc_drv_clsname").parent().parent().toggleClass('hide', !isJdbc)
+       $("#jdbc_drv_path").parent().parent().toggleClass('hide', !isJdbc)
+
+       if (isJdbc) {
            $("label[for='host']").text("Connection URL");
        } else {
-            $("input[name='port']").parent().parent().show();
-            $("input[name='schema']").parent().parent().show();
-            //$("input[name='login']").parent().parent().show();
-            //$("input[name='password']").parent().parent().show();
-            $("input[name='extra']").parent().parent().show();
-            $("input[name='jdbc_drv_clsname']").parent().parent().hide();
-            $("input[name='jdbc_drv_path']").parent().parent().hide();
             $("label[for='host']").text("Host");
        }
   }
