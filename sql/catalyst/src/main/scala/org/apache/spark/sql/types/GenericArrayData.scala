d4a38da40e8ff4e39f66fc428e4275c8ae7cd371
Wenchen Fan <cloud0fan@outlook.com>
2015-07-28 23:32:01 +0800

    remove sizeInBytes and add license

--- sql/catalyst/src/main/scala/org/apache/spark/sql/types/GenericArrayData.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/types/GenericArrayData.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.types
 
 import org.apache.spark.sql.catalyst.InternalRow
@@ -9,8 +26,6 @@ abstract class ArrayData extends SpecializedGetters with Serializable {
   def get(ordinal: Int): Any
 
   def numElements(): Int
-
-  def sizeInBytes(): Int
 
   // todo: need a more efficient way to iterate array type.
   def toArray(): Array[Any] = {
@@ -143,7 +158,4 @@ class GenericArrayData(array: Array[Any]) extends ArrayData {
   override def getArray(ordinal: Int, elementType: DataType): ArrayData = getAs(ordinal)
 
   override def numElements(): Int = array.length
-
-  // todo: figure out how to implement this.
-  override def sizeInBytes(): Int = ???
 }
