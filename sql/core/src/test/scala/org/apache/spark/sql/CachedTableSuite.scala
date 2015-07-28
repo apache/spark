a5455b1eae38f1e39ff47f30169be7ca4f4fd5ef
Feynman Liang <fliang@databricks.com>
2015-07-28 13:45:37 -0700

    Remove println

--- sql/core/src/test/scala/org/apache/spark/sql/CachedTableSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CachedTableSuite.scala
@@ -307,7 +307,6 @@ class CachedTableSuite extends QueryTest {
 
     Accumulators.synchronized {
       val accsSize = Accumulators.originals.size
-      println(accsSize)
       ctx.cacheTable("t1")
       ctx.cacheTable("t2")
       assert((accsSize + 2*4) === Accumulators.originals.size)
@@ -320,7 +319,6 @@ class CachedTableSuite extends QueryTest {
 
     Accumulators.synchronized {
       val accsSize = Accumulators.originals.size
-      println(accsSize)
       ctx.uncacheTable("t1")
       ctx.uncacheTable("t2")
       assert((accsSize - 2) === Accumulators.originals.size)
