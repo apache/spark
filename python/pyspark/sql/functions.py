7a22debf04ecd1f21bd7b74558029185582c45a3
Pedro Rodriguez <prodriguez@trulia.com>
2015-07-28 16:20:47 -0700

    Changed test to use strings instead of long/ints which are different between python 2 an 3

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1025,9 +1025,9 @@ def array_contains(col, value):
     :param col: name of column containing array
     :param value: value to check for in array
 
-    >>> df = sqlContext.createDataFrame([([1, 2, 3],), ([],)], ['data'])
-    >>> df.select(array_contains(df.data, 1L)).collect()
-    [Row(array_contains(data,1)=True), Row(array_contains(data,1)=False)]
+    >>> df = sqlContext.createDataFrame([(["a", "b", "c"],), ([],)], ['data'])
+    >>> df.select(array_contains(df.data, "a")).collect()
+    [Row(array_contains(data,"a")=True), Row(array_contains(data,"a")=False)]
     """
     sc = SparkContext._active_spark_context
     return Column(sc._jvm.functions.array_contains(_to_java_column(col), value))
