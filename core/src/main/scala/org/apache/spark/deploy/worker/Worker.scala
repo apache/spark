9cac7511d0c4cdfe74572bb9fa50ac86bb1aa837
CodingCat <zhunansjtu@gmail.com>
2015-07-27 23:28:50 -0400

    application is fine...

--- core/src/main/scala/org/apache/spark/deploy/worker/Worker.scala
+++ core/src/main/scala/org/apache/spark/deploy/worker/Worker.scala
@@ -126,9 +126,7 @@ private[worker] class Worker(
     WorkerWebUI.DEFAULT_RETAINED_EXECUTORS)
   val retainedDrivers = conf.getInt("spark.worker.ui.retainedDrivers",
     WorkerWebUI.DEFAULT_RETAINED_DRIVERS)
-  val retainedApplications = conf.getInt("spark.worker.ui.retainedApplications",
-    WorkerWebUI.DEFAULT_RETAINED_APPLICATIONS)
-
+  
   // The shuffle service is not actually started unless configured.
   private val shuffleService = new ExternalShuffleService(conf, securityMgr)
 
