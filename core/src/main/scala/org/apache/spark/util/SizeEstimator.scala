29493f12720dd9f02e8f199046f98f7a548756ea
Imran Rashid <irashid@cloudera.com>
2015-07-29 15:32:36 -0500

    prevent another potential overflow

--- core/src/main/scala/org/apache/spark/util/SizeEstimator.scala
+++ core/src/main/scala/org/apache/spark/util/SizeEstimator.scala
@@ -336,7 +336,7 @@ object SizeEstimator extends Logging {
     // hg.openjdk.java.net/jdk8/jdk8/hotspot/file/tip/src/share/vm/classfile/classFileParser.cpp
     var alignedSize = shellSize
     for (size <- fieldSizes if sizeCount(size) > 0) {
-      val count = sizeCount(size)
+      val count = sizeCount(size).toLong
       // If there are internal gaps, smaller field can fit in.
       alignedSize = math.max(alignedSize, alignSizeUp(shellSize, size) + size * count)
       shellSize += size * count
