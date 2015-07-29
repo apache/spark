051cc211b38adcf442c98f2cd8fc2e8d60ccce38
Steve Loughran <stevel@hortonworks.com>
2015-07-29 15:38:11 -0700

    SPARK-8064 switch to an unshaded version of hive-exec-core, which must have been built with Kryo 2.21. This currently looks for a (locally built) version 1.2.1.spark

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveShim.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveShim.scala
@@ -32,7 +32,7 @@ import com.esotericsoftware.kryo.io.{Input, Output}
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.hive.ql.exec.UDF
+import org.apache.hadoop.hive.ql.exec.{UDF, Utilities}
 import org.apache.hadoop.hive.ql.plan.{FileSinkDesc, TableDesc}
 import org.apache.hadoop.hive.serde2.ColumnProjectionUtils
 import org.apache.hadoop.hive.serde2.avro.{AvroGenericRecordWritable, AvroSerdeUtils}
@@ -147,12 +147,12 @@ private[hive] object HiveShim {
     }
 
     def deserializePlan[UDFType](is: java.io.InputStream, clazz: Class[_]): UDFType = {
-      deserializeObjectByKryo(ThreadKryoSerializer.get(), is, clazz)
+      deserializeObjectByKryo(Utilities.runtimeSerializationKryo.get(), is, clazz)
         .asInstanceOf[UDFType]
     }
 
     def serializePlan(function: AnyRef, out: java.io.OutputStream): Unit = {
-      serializeObjectByKryo(ThreadKryoSerializer.get(), function, out)
+      serializeObjectByKryo(Utilities.runtimeSerializationKryo.get(), function, out)
     }
 
     private var instance: AnyRef = null
