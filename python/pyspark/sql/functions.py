ffc0591b00aedfc6d54a33d66b35040eff601c9c
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-07-28 23:12:24 -0700

    fixed unit test

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1027,7 +1027,7 @@ def array_contains(col, value):
 
     >>> df = sqlContext.createDataFrame([(["a", "b", "c"],), ([],)], ['data'])
     >>> df.select(array_contains(df.data, "a")).collect()
-    [Row(array_contains(data,"a")=True), Row(array_contains(data,"a")=False)]
+    [Row(array_contains(data,a)=True), Row(array_contains(data,a)=False)]
     """
     sc = SparkContext._active_spark_context
     return Column(sc._jvm.functions.array_contains(_to_java_column(col), value))
