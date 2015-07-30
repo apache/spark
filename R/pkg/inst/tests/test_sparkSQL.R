157840d1b14502a4f25cff53633c927998c6ada1
Hossein <hossein@databricks.com>
2015-07-30 16:16:17 -0700

    [SPARK-8742] [SPARKR] Improve SparkR error messages for DataFrame API
    This patch improves SparkR error message reporting, especially with DataFrame API. When there is a user error (e.g., malformed SQL query), the message of the cause is sent back through the RPC and the R client reads it and returns it back to user.
    
    cc shivaram
    
    Author: Hossein <hossein@databricks.com>
    
    Closes #7742 from falaki/SPARK-8742 and squashes the following commits:
    
    4f643c9 [Hossein] Not logging exceptions in RBackendHandler
    4a8005c [Hossein] Returning stack track of causing exception from RBackendHandler
    5cf17f0 [Hossein] Adding unit test for error messages from SQLContext
    2af75d5 [Hossein] Reading error message in case of failure and stoping with that message
    f479c99 [Hossein] Wrting exception cause message in JVM

--- R/pkg/inst/tests/test_sparkSQL.R
+++ R/pkg/inst/tests/test_sparkSQL.R
@@ -1002,6 +1002,11 @@ test_that("crosstab() on a DataFrame", {
   expect_identical(expected, ordered)
 })
 
+test_that("SQL error message is returned from JVM", {
+  retError <- tryCatch(sql(sqlContext, "select * from blah"), error = function(e) e)
+  expect_equal(grepl("Table Not Found: blah", retError), TRUE)
+})
+
 unlink(parquetPath)
 unlink(jsonPath)
 unlink(jsonPathNa)
