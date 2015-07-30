9cc3a048141c6be4dcbd84dfd34fc7626b70671e
Joseph K. Bradley <joseph@databricks.com>
2015-07-30 14:08:14 -0700

    added checkpointing to GBTs

--- mllib/src/test/scala/org/apache/spark/ml/classification/GBTClassifierSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/classification/GBTClassifierSuite.scala
@@ -28,6 +28,7 @@ import org.apache.spark.mllib.tree.configuration.{Algo => OldAlgo}
 import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.util.Utils
 
 
 /**
@@ -74,6 +75,24 @@ class GBTClassifierSuite extends SparkFunSuite with MLlibTestSparkContext {
           .setStepSize(learningRate)
         compareAPIs(data, None, gbt, categoricalFeatures)
     }
+  }
+
+  test("Checkpointing") {
+    val tempDir = Utils.createTempDir()
+    val path = tempDir.toURI.toString
+    sc.setCheckpointDir(path)
+
+    val categoricalFeatures = Map.empty[Int, Int]
+    val df: DataFrame = TreeTests.setMetadata(data, categoricalFeatures, numClasses = 2)
+    val gbt = new GBTClassifier()
+      .setMaxDepth(2)
+      .setLossType("logistic")
+      .setMaxIter(5)
+      .setStepSize(0.1)
+      .setCheckpointInterval(2)
+    val model = gbt.fit(df)
+
+    Utils.deleteRecursively(tempDir)
   }
 
   // TODO: Reinstate test once runWithValidation is implemented   SPARK-7132
