3a4bb98ab8ce28459b7a7b732f294d121baf5f70
zsxwing <zsxwing@gmail.com>
2015-07-30 23:51:01 +0800

    Fix another potential NPE

--- streaming/src/main/scala/org/apache/spark/streaming/scheduler/ReceiverTracker.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/scheduler/ReceiverTracker.scala
@@ -289,8 +289,10 @@ class ReceiverTracker(ssc: StreamingContext, skipReceiverLaunch: Boolean = false
   }
 
   /** Update a receiver's maximum ingestion rate */
-  def sendRateUpdate(streamUID: Int, newRate: Long): Unit = {
-    endpoint.send(UpdateReceiverRateLimit(streamUID, newRate))
+  def sendRateUpdate(streamUID: Int, newRate: Long): Unit = synchronized {
+    if (isTrackerStarted) {
+      endpoint.send(UpdateReceiverRateLimit(streamUID, newRate))
+    }
   }
 
   /** Add new blocks for the given stream */
