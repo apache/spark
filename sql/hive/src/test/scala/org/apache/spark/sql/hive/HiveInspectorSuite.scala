e8f66823849580ae4227e72d62cf87e324c3a978
Wenchen Fan <cloud0fan@outlook.com>
2015-08-01 09:39:27 +0800

    skip MapData equality check in HiveInspectorSuite

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveInspectorSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveInspectorSuite.scala
@@ -147,6 +147,8 @@ class HiveInspectorSuite extends SparkFunSuite with HiveInspectors {
       case (r1: Array[Byte], r2: Array[Byte])
         if r1 != null && r2 != null && r1.length == r2.length =>
         r1.zip(r2).foreach { case (b1, b2) => assert(b1 === b2) }
+      // We don't support equality & ordering for map type, so skip it.
+      case (r1: MapData, r2: MapData) =>
       case (r1, r2) => assert(r1 === r2)
     }
   }
