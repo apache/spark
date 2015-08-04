73dedb589d06f7c7a525cc4f07721a77f480c434
Davies Liu <davies@databricks.com>
2015-08-04 09:07:09 -0700

    [SPARK-8246] [SQL] Implement get_json_object
    This is based on #7485 , thanks to NathanHowell
    
    Tests were copied from Hive, but do not seem to be super comprehensive. I've generally replicated Hive's unusual behavior rather than following a JSONPath reference, except for one case (as noted in the comments). I don't know if there is a way of fully replicating Hive's behavior without a slower TreeNode implementation, so I've erred on the side of performance instead.
    
    Author: Davies Liu <davies@databricks.com>
    Author: Yin Huai <yhuai@databricks.com>
    Author: Nathan Howell <nhowell@godaddy.com>
    
    Closes #7901 from davies/get_json_object and squashes the following commits:
    
    3ace9b9 [Davies Liu] Merge branch 'get_json_object' of github.com:davies/spark into get_json_object
    98766fc [Davies Liu] Merge branch 'master' of github.com:apache/spark into get_json_object
    a7dc6d0 [Davies Liu] Update JsonExpressionsSuite.scala
    c818519 [Yin Huai] new results.
    18ce26b [Davies Liu] fix tests
    6ac29fb [Yin Huai] Golden files.
    25eebef [Davies Liu] use HiveQuerySuite
    e0ac6ec [Yin Huai] Golden answer files.
    940c060 [Davies Liu] tweat code style
    44084c5 [Davies Liu] Merge branch 'master' of github.com:apache/spark into get_json_object
    9192d09 [Nathan Howell] Match Hive’s behavior for unwrapping arrays of one element
    8dab647 [Nathan Howell] [SPARK-8246] [SQL] Implement get_json_object

--- sql/hive/compatibility/src/test/scala/org/apache/spark/sql/hive/execution/HiveCompatibilitySuite.scala
+++ sql/hive/compatibility/src/test/scala/org/apache/spark/sql/hive/execution/HiveCompatibilitySuite.scala
@@ -266,6 +266,9 @@ class HiveCompatibilitySuite extends HiveQueryFileTest with BeforeAndAfter {
     // Hive returns string from UTC formatted timestamp, spark returns timestamp type
     "date_udf",
 
+    // Can't compare the result that have newline in it
+    "udf_get_json_object",
+
     // Unlike Hive, we do support log base in (0, 1.0], therefore disable this
     "udf7",
 
