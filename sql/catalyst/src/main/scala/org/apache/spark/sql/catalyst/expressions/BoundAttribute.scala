892d990b56087faff89b75e92f28cddfc8d7e38f
Herman van Hovell <hvanhovell@questtec.nl>
2015-08-04 17:14:19 -0400

    Dropped a number additions to AbstractBoundeReference.eval() after rebase. Put them back in.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/BoundAttribute.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/BoundAttribute.scala
@@ -56,7 +56,10 @@ abstract class AbstractBoundReference extends LeafExpression with NamedExpressio
         case StringType => input.getUTF8String(ordinal)
         case BinaryType => input.getBinary(ordinal)
         case CalendarIntervalType => input.getInterval(ordinal)
+        case t: DecimalType => input.getDecimal(ordinal, t.precision, t.scale)
         case t: StructType => input.getStruct(ordinal, t.size)
+        case _: ArrayType => input.getArray(ordinal)
+        case _: MapType => input.getMap(ordinal)
         case _ => input.get(ordinal, dataType)
       }
     }
