1a47be18ae492477784f7fd5c48a2cd6282b6fea
Davies Liu <davies@databricks.com>
2015-08-06 10:40:57 -0700

    use tungsten.enabled for both of codegen/unsafe

--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashSemiJoin.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashSemiJoin.scala
@@ -33,7 +33,8 @@ trait HashSemiJoin {
   override def output: Seq[Attribute] = left.output
 
   protected[this] def supportUnsafe: Boolean = {
-    (self.codegenEnabled && UnsafeProjection.canSupport(leftKeys)
+    (self.codegenEnabled && self.unsafeEnabled
+      && UnsafeProjection.canSupport(leftKeys)
       && UnsafeProjection.canSupport(rightKeys)
       && UnsafeProjection.canSupport(left.schema)
       && UnsafeProjection.canSupport(right.schema))
