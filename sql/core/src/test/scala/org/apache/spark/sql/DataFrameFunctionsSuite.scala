7d4715189ee879dfbd66427a7e29257c8ba61967
BenFradet <benjamin.fradet@gmail.com>
2015-08-06 22:33:25 +0200

    fixed unit tests in DataFrameFunctionsSuite

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -116,9 +116,13 @@ class DataFrameFunctionsSuite extends QueryTest {
     checkAnswer(result, Seq(Row(Row("v", 5.0)), Row(Row("v", 5.0))))
   }
 
-  val testData2 = (for { a <- 1 to 3; b <- 1 to 2 } yield (a, b))
+  val testData2 = {
+    val df = (for { a <- 1 to 3; b <- 1 to 2 } yield (a, b))
       .map(t => (t._1, t._2))
       .toDF("a", "b")
+    df.registerTempTable("testData2")
+    df
+  }
 
   test("constant functions") {
     checkAnswer(
