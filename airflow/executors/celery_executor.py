a559493bfa54d7a685e8e66139c52f0f2dd8e1bb
Jeremiah Lowin <jlowin@iHal.local>
2015-08-06 23:44:13 -0400

    Use list to allow modification in-place

--- airflow/executors/celery_executor.py
+++ airflow/executors/celery_executor.py
@@ -65,7 +65,7 @@ class CeleryExecutor(BaseExecutor):
     def sync(self):
         logging.debug(
             "Inquiring about {} celery task(s)".format(len(self.tasks)))
-        for key, async in self.tasks.items():
+        for key, async in list(self.tasks.items()):
             state = async.state
             if self.last_state[key] != state:
                 if state == celery_states.SUCCESS:
