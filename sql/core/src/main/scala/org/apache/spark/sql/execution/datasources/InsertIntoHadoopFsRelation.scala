17b690e632e26beea80b9bee29bc75b4f52dbb0b
Michael Armbrust <michael@databricks.com>
2015-08-07 13:19:32 -0700

    remove comment

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/InsertIntoHadoopFsRelation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/InsertIntoHadoopFsRelation.scala
@@ -98,9 +98,6 @@ private[sql] case class InsertIntoHadoopFsRelation(
       job.setOutputValueClass(classOf[InternalRow])
       FileOutputFormat.setOutputPath(job, qualifiedOutputPath)
 
-      // We create a DataFrame by applying the schema of relation to the data to make sure.
-      // We are writing data based on the expected schema,
-
       // A partitioned relation schema's can be different from the input logicalPlan, since
       // partition columns are all moved after data column. We Project to adjust the ordering.
       // TODO: this belongs in the analyzer.
