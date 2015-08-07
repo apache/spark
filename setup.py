9015e3062726154378d046059b7ff193b550de69
Jeremiah Lowin <jlowin@iHal.local>
2015-08-06 23:11:18 -0400

    add requirement 'future'

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         'flask-cache>=0.13.1',
         'flask-login>=0.2.11',
         'flower>=0.7.3',
+        'future>=0.15.0',
         'jinja2>=2.7.3',
         'markdown>=2.5.2',
         'pandas>=0.15.2',
