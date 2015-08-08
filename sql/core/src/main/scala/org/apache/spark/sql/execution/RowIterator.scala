fd439cb98bb496cebd0075c10c6ff0153f7dcfe8
Josh Rosen <joshrosen@databricks.com>
2015-08-08 15:02:07 -0700

    Move RowIterator to execution package

--- sql/core/src/main/scala/org/apache/spark/sql/execution/RowIterator.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/RowIterator.scala
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.spark.sql.execution.joins
+package org.apache.spark.sql.execution
 
 import java.util.NoSuchElementException
 
--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/SortMergeOuterJoin.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/SortMergeOuterJoin.scala
@@ -23,7 +23,7 @@ import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.{JoinType, LeftOuter, RightOuter}
 import org.apache.spark.sql.catalyst.plans.physical._
-import org.apache.spark.sql.execution.{BinaryNode, SparkPlan}
+import org.apache.spark.sql.execution.{BinaryNode, RowIterator, SparkPlan}
 
 /**
  * :: DeveloperApi ::
