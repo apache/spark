963af5fb6fc61531c54caf2c72d2ea1109213fd7
Yijie Shen <henry.yijieshen@gmail.com>
2015-08-09 09:42:55 +0800

    typo fix

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/predicates.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/predicates.scala
@@ -280,8 +280,7 @@ abstract class BinaryComparison extends BinaryOperator with Predicate {
     if (ctx.isPrimitiveType(left.dataType)
         && left.dataType != BooleanType // java boolean doesn't support > or < operator
         && left.dataType != FloatType
-        && left.dataType != DoubleType
-        && left.dataType != BooleanType) {
+        && left.dataType != DoubleType) {
       // faster version
       defineCodeGen(ctx, ev, (c1, c2) => s"$c1 $symbol $c2")
     } else {
