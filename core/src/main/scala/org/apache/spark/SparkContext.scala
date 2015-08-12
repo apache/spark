7f3e1ffcc2a5a3db839ccb96143b6d82d160e1e5
Niranjan Padmanabhan <niranjan.padmanabhan@cloudera.com>
2015-08-12 13:30:39 -0700

    Remove un-necessary s in logWarning

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -530,7 +530,7 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
     // Optionally scale number of executors dynamically based on workload. Exposed for testing.
     val dynamicAllocationEnabled = Utils.isDynamicAllocationEnabled(_conf)
     if (!dynamicAllocationEnabled && _conf.getBoolean("spark.dynamicAllocation.enabled", false)) {
-      logInfo(s"Dynamic Allocation and num executors both set, thus dynamic allocation disabled.")
+      logInfo("Dynamic Allocation and num executors both set, thus dynamic allocation disabled.")
     }
 
     _executorAllocationManager =
