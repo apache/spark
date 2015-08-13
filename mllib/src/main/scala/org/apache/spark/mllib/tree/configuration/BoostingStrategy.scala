df4f8c5b072725b63c525ef4522bef7e43c56608
Bryan Cutler <bjcutler@us.ibm.com>
2015-08-13 12:32:54 -0700

    resolved conflict with merge

--- mllib/src/main/scala/org/apache/spark/mllib/tree/configuration/BoostingStrategy.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/tree/configuration/BoostingStrategy.scala
@@ -36,13 +36,9 @@ import org.apache.spark.mllib.tree.loss.{LogLoss, SquaredError, Loss}
  *                     learning rate should be between in the interval (0, 1]
  * @param validationTol Useful when runWithValidation is used. If the error rate on the
  *                      validation input between two iterations is less than the validationTol
-<<<<<<< HEAD
- *                      then stop. Ignored when [[run]] is used.
- * @since 1.2.0
-=======
  *                      then stop.  Ignored when
  *                      [[org.apache.spark.mllib.tree.GradientBoostedTrees.run()]] is used.
->>>>>>> master
+ * @since 1.2.0
  */
 @Experimental
 case class BoostingStrategy(
