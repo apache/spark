239a8b989c5e470acaca8fdaa8ebb6ec8a62a17d
MechCoder <manojkumarsivaraj334@gmail.com>
2015-08-14 12:29:17 +0530

    use asterisk

--- python/pyspark/context.py
+++ python/pyspark/context.py
@@ -303,8 +303,8 @@ class SparkContext(object):
         A unique identifier for the Spark application.
         Its format depends on the scheduler implementation.
 
-        a] in case of local spark app something like 'local-1433865536131'
-        b] in case of YARN something like 'application_1433865536131_34483'
+        * in case of local spark app something like 'local-1433865536131'
+        * in case of YARN something like 'application_1433865536131_34483'
 
         >>> sc.applicationId  # doctest: +ELLIPSIS
         u'local-...'
