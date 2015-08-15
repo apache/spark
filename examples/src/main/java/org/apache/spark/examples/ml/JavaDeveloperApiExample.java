a85fb6c07fdda5c74d53d6373910dcf5db3ff111
Herman van Hovell <hvanhovell@questtec.nl>
2015-08-15 10:46:04 +0100

    [SPARK-9980] [BUILD] Fix SBT publishLocal error due to invalid characters in doc
    Tiny modification to a few comments ```sbt publishLocal``` work again.
    
    Author: Herman van Hovell <hvanhovell@questtec.nl>
    
    Closes #8209 from hvanhovell/SPARK-9980.

--- examples/src/main/java/org/apache/spark/examples/ml/JavaDeveloperApiExample.java
+++ examples/src/main/java/org/apache/spark/examples/ml/JavaDeveloperApiExample.java
@@ -124,7 +124,7 @@ class MyJavaLogisticRegression
 
   /**
    * Param for max number of iterations
-   * <p/>
+   * <p>
    * NOTE: The usual way to add a parameter to a model or algorithm is to include:
    * - val myParamName: ParamType
    * - def getMyParamName
@@ -222,7 +222,7 @@ class MyJavaLogisticRegressionModel
   /**
    * Create a copy of the model.
    * The copy is shallow, except for the embedded paramMap, which gets a deep copy.
-   * <p/>
+   * <p>
    * This is used for the defaul implementation of [[transform()]].
    *
    * In Java, we have to make this method public since Java does not understand Scala's protected
