fb0671f1b6e16107ce7a970ea02ece48123bf48a
Josh Rosen <joshrosen@databricks.com>
2015-08-16 15:01:21 -0700

    Move RandomDataFrameGenerator to own file.

--- sql/core/src/test/scala/org/apache/spark/sql/fuzzing/DataFrameFuzzingSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/fuzzing/DataFrameFuzzingSuite.scala
@@ -18,7 +18,6 @@
 package org.apache.spark.sql.fuzzing
 
 import java.lang.reflect.InvocationTargetException
-import java.util.concurrent.atomic.AtomicInteger
 
 import scala.reflect.runtime.{universe => ru}
 import scala.util.Random
@@ -30,76 +29,6 @@ import org.apache.spark.sql.catalyst.analysis.UnresolvedException
 import org.apache.spark.sql.catalyst.plans.JoinType
 import org.apache.spark.sql.types._
 import org.apache.spark.util.Utils
-
-class RandomDataFrameGenerator(seed: Long, sqlContext: SQLContext) {
-
-  private val rand = new Random(seed)
-  private val nextId = new AtomicInteger()
-
-  private def hasRandomDataGenerator(dataType: DataType): Boolean = {
-    RandomDataGenerator.forType(dataType).isDefined
-  }
-
-  def randomChoice[T](values: Seq[T]): T = {
-    values(rand.nextInt(values.length))
-  }
-
-  private val simpleTypes: Set[DataType] = {
-    DataTypeTestUtils.atomicTypes
-      .filter(hasRandomDataGenerator)
-      // Ignore decimal type since it can lead to OOM (see SPARK-9303). TODO: It would be better to
-      // only generate limited precision decimals instead.
-      .filterNot(_.isInstanceOf[DecimalType])
-  }
-
-  private val arrayTypes: Set[DataType] = {
-    DataTypeTestUtils.atomicArrayTypes
-      .filter(hasRandomDataGenerator)
-      // See above comment about DecimalType
-      .filterNot(_.elementType.isInstanceOf[DecimalType]).toSet
-  }
-
-  private def randomStructField(
-      allowComplexTypes: Boolean = false,
-      allowSpacesInColumnName: Boolean = false): StructField = {
-    val name = "c" + nextId.getAndIncrement + (if (allowSpacesInColumnName) " space" else "")
-    val candidateTypes: Seq[DataType] = Seq(
-      simpleTypes,
-      arrayTypes.filter(_ => allowComplexTypes),
-      // This does not allow complex types, limiting the depth of recursion:
-      if (allowComplexTypes) {
-        Set[DataType](randomStructType(numCols = rand.nextInt(2) + 1))
-      } else {
-        Set[DataType]()
-      }
-    ).flatten
-    val dataType = randomChoice(candidateTypes)
-    val nullable = rand.nextBoolean()
-    StructField(name, dataType, nullable)
-  }
-
-  private def randomStructType(
-      numCols: Int,
-      allowComplexTypes: Boolean = false,
-      allowSpacesInColumnNames: Boolean = false): StructType = {
-    StructType(Array.fill(numCols)(randomStructField(allowComplexTypes, allowSpacesInColumnNames)))
-  }
-
-  def randomDataFrame(
-      numCols: Int,
-      numRows: Int,
-      allowComplexTypes: Boolean = false,
-      allowSpacesInColumnNames: Boolean = false): DataFrame = {
-    val schema = randomStructType(numCols, allowComplexTypes, allowSpacesInColumnNames)
-    val rows = sqlContext.sparkContext.parallelize(1 to numRows).mapPartitions { iter =>
-      val rowGenerator = RandomDataGenerator.forType(schema, nullable = false, seed = Some(42)).get
-      iter.map(_ => rowGenerator().asInstanceOf[Row])
-    }
-    sqlContext.createDataFrame(rows, schema)
-  }
-
-}
-
 
 /**
  * This test suite generates random data frames, then applies random sequences of operations to
