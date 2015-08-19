d19f885fa71d53c92a23b0339c5cf569b2207b4f
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-08-19 11:44:44 +0900

    Add a rule to convert Long parameter and remove the special versins of `rand`/`randn`

--- sql/core/src/main/scala/org/apache/spark/sql/functions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/functions.scala
@@ -682,14 +682,6 @@ object functions {
    * Generate a random column with i.i.d. samples from U[0.0, 1.0].
    *
    * @group normal_funcs
-   * @since 1.5.0
-   */
-  def rand(seed: Int): Column = rand(Int.int2long(seed))
-
-  /**
-   * Generate a random column with i.i.d. samples from U[0.0, 1.0].
-   *
-   * @group normal_funcs
    * @since 1.4.0
    */
   def rand(): Column = rand(Utils.random.nextLong)
@@ -701,14 +693,6 @@ object functions {
    * @since 1.4.0
    */
   def randn(seed: Long): Column = Randn(seed)
-
-  /**
-   * Generate a column with i.i.d. samples from the standard normal distribution.
-   *
-   * @group normal_funcs
-   * @since 1.5.0
-   */
-  def randn(seed: Int): Column = randn(Int.int2long(seed))
 
   /**
    * Generate a column with i.i.d. samples from the standard normal distribution.
