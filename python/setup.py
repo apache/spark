5e02ee1e26642ab5ebe9e5ac4caf199bee737d3a
Auberon Lopez <alope107@CSM-26-110L-M2.att.net>
2015-08-19 12:59:26 -0700

    fixing style

--- python/setup.py
+++ python/setup.py
@@ -6,14 +6,14 @@ exec(compile(open("pyspark/pyspark_version.py").read(),
    "pyspark/pyspark_version.py", 'exec'))
 VERSION = __version__
 
-setup(name = 'pyspark',
-   version = VERSION,
-   description = 'Apache Spark Python API',
-   author = 'Prabin Banka',
-   author_email = 'prabin.banka@imaginea.com',
-   url = 'https://github.com/apache/spark/tree/master/python',
-   packages = ['pyspark', 'pyspark.mllib', 'pyspark.ml', 'pyspark.sql', 'pyspark.streaming'],
-   data_files = [('pyspark', ['pyspark/pyspark_version.py'])],
-   install_requires = ['numpy>=1.7', 'py4j==0.8.2.1', 'pandas'],
-   license = 'http://www.apache.org/licenses/LICENSE-2.0',
-   )
+setup(name='pyspark',
+    version=VERSION,
+    description='Apache Spark Python API',
+    author='Spark Developers',
+    author_email='dev@spark.apache.org',
+    url='https://github.com/apache/spark/tree/master/python',
+    packages=['pyspark', 'pyspark.mllib', 'pyspark.ml', 'pyspark.sql', 'pyspark.streaming'],
+    data_files=[('pyspark', ['pyspark/pyspark_version.py'])],
+    install_requires=['numpy>=1.7', 'py4j==0.8.2.1', 'pandas'],
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    )
