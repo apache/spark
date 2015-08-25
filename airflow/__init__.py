f0f41101cb345ef4b46ab525a71a46baa0e9c1dc
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-08-24 22:07:25 -0700

    v1.5.0

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -5,7 +5,7 @@ in their PYTHONPATH. airflow_login should be based off the
 `airflow.www.login`
 """
 from builtins import object
-__version__ = "1.4.0"
+__version__ = "1.5.0"
 
 import logging
 import os
