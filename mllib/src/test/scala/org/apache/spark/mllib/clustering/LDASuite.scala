05a84534fd3892a86b1f43fad02533bcdb3fcda7
Feynman Liang <fliang@databricks.com>
2015-08-25 10:56:10 -0700

    Rename optimizeAlpha to optimizeDocConcentration

--- mllib/src/test/scala/org/apache/spark/mllib/clustering/LDASuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/clustering/LDASuite.scala
@@ -423,7 +423,7 @@ class LDASuite extends SparkFunSuite with MLlibTestSparkContext {
     val k = 2
     val docs = sc.parallelize(toyData)
     val op = new OnlineLDAOptimizer().setMiniBatchFraction(1).setTau0(1024).setKappa(0.51)
-      .setGammaShape(100).setOptimzeAlpha(true).setSampleWithReplacement(false)
+      .setGammaShape(100).setOptimizeDocConcentration(true).setSampleWithReplacement(false)
     val lda = new LDA().setK(k)
       .setDocConcentration(1D / k)
       .setTopicConcentration(0.01)
