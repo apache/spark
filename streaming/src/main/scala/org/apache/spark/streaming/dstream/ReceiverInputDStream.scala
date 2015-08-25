75909377cb28d6c958ab6d92df9350b43e78564c
Tathagata Das <tathagata.das1565@gmail.com>
2015-08-24 19:23:41 -0700

    Added warning

--- streaming/src/main/scala/org/apache/spark/streaming/dstream/ReceiverInputDStream.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/dstream/ReceiverInputDStream.scala
@@ -119,6 +119,11 @@ abstract class ReceiverInputDStream[T: ClassTag](@transient ssc_ : StreamingCont
         val validBlockIds = blockIds.filter { id =>
           ssc.sparkContext.env.blockManager.master.contains(id)
         }
+        if (validBlockIds.size != blockIds.size) {
+          logWarning("Some blocks could not be recovered as they were not found in memory. " +
+            "To prevent such data loss, enabled Write Ahead Log (see programming guide " +
+            "for more details.") 
+        }
         new BlockRDD[T](ssc.sc, validBlockIds)
       }
     } else {
