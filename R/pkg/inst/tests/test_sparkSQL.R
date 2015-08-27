773ca037a43d464ce7f16fe693ca6034f09a35b7
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-08-26 18:14:32 -0700

    [MINOR] [SPARKR] Fix some validation problems in SparkR
    Getting rid of some validation problems in SparkR
    https://github.com/apache/spark/pull/7883
    
    cc shivaram
    
    ```
    inst/tests/test_Serde.R:26:1: style: Trailing whitespace is superfluous.
    
    ^~
    inst/tests/test_Serde.R:34:1: style: Trailing whitespace is superfluous.
    
    ^~
    inst/tests/test_Serde.R:37:38: style: Trailing whitespace is superfluous.
      expect_equal(class(x), "character")
                                         ^~
    inst/tests/test_Serde.R:50:1: style: Trailing whitespace is superfluous.
    
    ^~
    inst/tests/test_Serde.R:55:1: style: Trailing whitespace is superfluous.
    
    ^~
    inst/tests/test_Serde.R:60:1: style: Trailing whitespace is superfluous.
    
    ^~
    inst/tests/test_sparkSQL.R:611:1: style: Trailing whitespace is superfluous.
    
    ^~
    R/DataFrame.R:664:1: style: Trailing whitespace is superfluous.
    
    ^~~~~~~~~~~~~~
    R/DataFrame.R:670:55: style: Trailing whitespace is superfluous.
                    df <- data.frame(row.names = 1 : nrow)
                                                          ^~~~~~~~~~~~~~~~
    R/DataFrame.R:672:1: style: Trailing whitespace is superfluous.
    
    ^~~~~~~~~~~~~~
    R/DataFrame.R:686:49: style: Trailing whitespace is superfluous.
                        df[[names[colIndex]]] <- vec
                                                    ^~~~~~~~~~~~~~~~~~
    ```
    
    Author: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    Closes #8474 from yu-iskw/minor-fix-sparkr.

--- R/pkg/inst/tests/test_sparkSQL.R
+++ R/pkg/inst/tests/test_sparkSQL.R
@@ -608,7 +608,7 @@ test_that("subsetting", {
   df4 <- df[df$age %in% c(19, 30), 1:2]
   expect_equal(count(df4), 2)
   expect_equal(columns(df4), c("name", "age"))
-  
+
   df5 <- df[df$age %in% c(19), c(1,2)]
   expect_equal(count(df5), 1)
   expect_equal(columns(df5), c("name", "age"))
