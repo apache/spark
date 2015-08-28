afc1e1978b60a1589724771f8f9a5b94e3dd833c
wangxiaoyu8 <wangxiaoyu1@jd.com>
2015-08-28 09:05:28 +0800

    SPARK-10314 [CORE] RDD persist to OFF_HEAP tachyon got block rdd_x_x not found exception when parallelism is big than data split size

--- core/src/main/scala/org/apache/spark/storage/BlockManagerMasterEndpoint.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManagerMasterEndpoint.scala
@@ -489,7 +489,7 @@ private[spark] class BlockManagerInfo(
         logInfo("Added %s on ExternalBlockStore on %s (size: %s)".format(
           blockId, blockManagerId.hostPort, Utils.bytesToString(externalBlockStoreSize)))
       }
-      if (!blockId.isBroadcast && blockStatus.isCached) {
+      if (!blockId.isBroadcast && (blockStatus.isCached || blockStatus.storageLevel.useOffHeap)) {
         _cachedBlocks += blockId
       }
     } else if (_blocks.containsKey(blockId)) {
