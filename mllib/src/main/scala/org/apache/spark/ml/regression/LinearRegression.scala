6afde57ebf3d86ae3a0bb07cdb42875a7bbbd0ff
Holden Karau <holden@pigscanfly.ca>
2015-09-01 23:09:26 -0700

    switch to withColumn

--- mllib/src/main/scala/org/apache/spark/ml/regression/LinearRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/regression/LinearRegression.scala
@@ -300,7 +300,7 @@ class LinearRegressionModel private[ml] (
   private[regression] def evaluate(dataset: DataFrame): LinearRegressionSummary = {
     val t = udf { features: Vector => predict(features) }
     val predictionAndObservations = dataset
-      .select(col($(labelCol)), t(col($(featuresCol))).as($(predictionCol)))
+      .withColumn(col($(labelCol)), t(col($(featuresCol))).as($(predictionCol)))
 
     new LinearRegressionSummary(predictionAndObservations, $(predictionCol), $(labelCol))
   }
