cde592a29c21f752cb977934ee56809fdbb5f8b8
noelsmith <mail@noelsmith.com>
2015-09-06 22:18:25 +0100

    Added @since to mllib.feature

--- python/pyspark/__init__.py
+++ python/pyspark/__init__.py
@@ -67,6 +67,26 @@ def since(version):
 # for back compatibility
 from pyspark.sql import SQLContext, HiveContext, SchemaRDD, Row
 
+
+def since(version):
+    """
+    A decorator that annotates a function to append the version of Spark the function was added.
+    """
+    import re
+    indent_p = re.compile(r'\n( +)')
+
+    def deco(f):
+        if f.__doc__ is None:
+            f.__doc__ = ".. versionadded:: {}".format(version)
+            return f
+        else:
+            indents = indent_p.findall(f.__doc__)
+            indent = ' ' * (min(len(m) for m in indents) if indents else 0)
+            f.__doc__ = "{}\n\n{}.. versionadded:: {}".format(f.__doc__.rstrip(), indent, version)
+            return f
+    return deco
+
+
 __all__ = [
     "SparkConf", "SparkContext", "SparkFiles", "RDD", "StorageLevel", "Broadcast",
     "Accumulator", "AccumulatorParam", "MarshalSerializer", "PickleSerializer",
