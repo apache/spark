b82c17dfd547a1ff270799e7f5f430e646432923
Lei Wu <wulei.bj.cn@gmail.com>
2015-09-08 16:59:59 +0800

    Clear all the changes made in TaskSetManager.scala.

--- core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -18,7 +18,6 @@
 package org.apache.spark.scheduler
 
 import java.io.NotSerializableException
-import java.net.InetAddress
 import java.nio.ByteBuffer
 import java.util.Arrays
 import java.util.concurrent.ConcurrentLinkedQueue
