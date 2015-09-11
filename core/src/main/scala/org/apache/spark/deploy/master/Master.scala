ce4811232711ff059b683b29e11658826025c840
jerryshao <sshao@hortonworks.com>
2015-09-11 15:03:59 +0800

    Fix wrong executor state in Master

--- core/src/main/scala/org/apache/spark/deploy/master/Master.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/Master.scala
@@ -253,9 +253,24 @@ private[deploy] class Master(
       execOption match {
         case Some(exec) => {
           val appInfo = idToApp(appId)
+          val oldState = exec.state
           exec.state = state
-          if (state == ExecutorState.RUNNING) { appInfo.resetRetryCount() }
+
+          if (state == ExecutorState.RUNNING) {
+            assert(oldState == ExecutorState.LOADING,
+              s"executor $execId state transfer from $oldState to RUNNING is illegal")
+            appInfo.resetRetryCount()
+          }
+
           exec.application.driver.send(ExecutorUpdated(execId, state, message, exitStatus))
+
+          if (state == ExecutorState.LOADING) {
+            assert(oldState == ExecutorState.LAUNCHING,
+              s"executor $execId state transfer from $oldState to LOADING is illegal")
+            exec.application.driver.send(
+              ExecutorAdded(exec.id, exec.worker.id, exec.worker.hostPort, exec.cores, exec.memory))
+          }
+
           if (ExecutorState.isFinished(state)) {
             // Remove this executor from the worker and app
             logInfo(s"Removing executor ${exec.fullId} because it is $state")
@@ -702,8 +717,6 @@ private[deploy] class Master(
     worker.addExecutor(exec)
     worker.endpoint.send(LaunchExecutor(masterUrl,
       exec.application.id, exec.id, exec.application.desc, exec.cores, exec.memory))
-    exec.application.driver.send(ExecutorAdded(
-      exec.id, worker.id, worker.hostPort, exec.cores, exec.memory))
   }
 
   private def registerWorker(worker: WorkerInfo): Boolean = {
