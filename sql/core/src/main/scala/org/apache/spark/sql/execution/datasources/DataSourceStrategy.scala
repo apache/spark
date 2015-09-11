b3f02b29b02ae255f2cee71912e12e4f34d3935a
hyukjinkwon <gurwls223@gmail.com>
2015-09-11 19:52:49 +0900

    [SPARK-9182][SQL] Remove a newline

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
@@ -33,7 +33,6 @@ import org.apache.spark.sql.{SaveMode, Strategy, execution, sources, _}
 import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.{SerializableConfiguration, Utils}
 
-
 /**
  * A Strategy for planning scans over data sources defined using the sources API.
  */
