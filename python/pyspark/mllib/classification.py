d57886722b10a7cb060bc01aad0935bf67aadad5
noelsmith <mail@noelsmith.com>
2015-09-14 21:49:45 +0100

    Reinstated 3-part version numbers

--- python/pyspark/mllib/classification.py
+++ python/pyspark/mllib/classification.py
@@ -44,7 +44,7 @@ class LinearClassificationModel(LinearModel):
         super(LinearClassificationModel, self).__init__(weights, intercept)
         self._threshold = None
 
-    @since(1.4)
+    @since('1.4.0')
     def setThreshold(self, value):
         """
         .. note:: Experimental
@@ -58,7 +58,7 @@ class LinearClassificationModel(LinearModel):
         self._threshold = value
 
     @property
-    @since(1.4)
+    @since('1.4.0')
     def threshold(self):
         """
         .. note:: Experimental
@@ -69,7 +69,7 @@ class LinearClassificationModel(LinearModel):
         """
         return self._threshold
 
-    @since(1.4)
+    @since('1.4.0')
     def clearThreshold(self):
         """
         .. note:: Experimental
@@ -79,7 +79,7 @@ class LinearClassificationModel(LinearModel):
         """
         self._threshold = None
 
-    @since(1.4)
+    @since('1.4.0')
     def predict(self, test):
         """
         Predict values for a single data point or an RDD of points
@@ -162,7 +162,7 @@ class LogisticRegressionModel(LinearClassificationModel):
     >>> mcm.predict([0.0, 0.0, 0.3])
     2
 
-    .. versionadded:: 0.9
+    .. versionadded:: 0.9.1
     """
     def __init__(self, weights, intercept, numFeatures, numClasses):
         super(LogisticRegressionModel, self).__init__(weights, intercept)
@@ -178,7 +178,7 @@ class LogisticRegressionModel(LinearClassificationModel):
                                                                 self._dataWithBiasSize)
 
     @property
-    @since(1.4)
+    @since('1.4.0')
     def numFeatures(self):
         """
         Dimension of the features.
@@ -186,14 +186,15 @@ class LogisticRegressionModel(LinearClassificationModel):
         return self._numFeatures
 
     @property
-    @since(1.4)
+    @since('1.4.0')
     def numClasses(self):
         """
-        Number of possible outcomes for k classes classification problem in Multinomial Logistic Regression.
+        Number of possible outcomes for k classes classification problem in Multinomial
+        Logistic Regression.
         """
         return self._numClasses
 
-    @since(0.9)
+    @since('0.9.1')
     def predict(self, x):
         """
         Predict values for a single data point or an RDD of points
@@ -232,7 +233,7 @@ class LogisticRegressionModel(LinearClassificationModel):
                         best_class = i + 1
             return best_class
 
-    @since(1.4)
+    @since('1.4.0')
     def save(self, sc, path):
         """
         Save this model to the given path.
@@ -242,7 +243,7 @@ class LogisticRegressionModel(LinearClassificationModel):
         java_model.save(sc._jsc.sc(), path)
 
     @classmethod
-    @since(1.4)
+    @since('1.4.0')
     def load(cls, sc, path):
         """
         Load a model from the given path.
@@ -261,10 +262,10 @@ class LogisticRegressionModel(LinearClassificationModel):
 
 class LogisticRegressionWithSGD(object):
     """
-    .. versionadded:: 0.9
+    .. versionadded:: 0.9.1
     """
     @classmethod
-    @since(0.9)
+    @since('0.9.1')
     def train(cls, data, iterations=100, step=1.0, miniBatchFraction=1.0,
               initialWeights=None, regParam=0.01, regType="l2", intercept=False,
               validateData=True, convergenceTol=0.001):
@@ -313,10 +314,10 @@ class LogisticRegressionWithSGD(object):
 
 class LogisticRegressionWithLBFGS(object):
     """
-    .. versionadded:: 1.2
+    .. versionadded:: 1.2.0
     """
     @classmethod
-    @since(1.2)
+    @since('1.2.0')
     def train(cls, data, iterations=100, initialWeights=None, regParam=0.01, regType="l2",
               intercept=False, corrections=10, tolerance=1e-4, validateData=True, numClasses=2):
         """
@@ -429,13 +430,13 @@ class SVMModel(LinearClassificationModel):
     ... except:
     ...    pass
 
-    .. versionadded:: 0.9
+    .. versionadded:: 0.9.1
     """
     def __init__(self, weights, intercept):
         super(SVMModel, self).__init__(weights, intercept)
         self._threshold = 0.0
 
-    @since(0.9)
+    @since('0.9.1')
     def predict(self, x):
         """
         Predict values for a single data point or an RDD of points
@@ -451,7 +452,7 @@ class SVMModel(LinearClassificationModel):
         else:
             return 1 if margin > self._threshold else 0
 
-    @since(1.4)
+    @since('1.4.0')
     def save(self, sc, path):
         """
         Save this model to the given path.
@@ -461,7 +462,7 @@ class SVMModel(LinearClassificationModel):
         java_model.save(sc._jsc.sc(), path)
 
     @classmethod
-    @since(1.4)
+    @since('1.4.0')
     def load(cls, sc, path):
         """
         Load a model from the given path.
@@ -478,11 +479,11 @@ class SVMModel(LinearClassificationModel):
 
 class SVMWithSGD(object):
     """
-    .. versionadded:: 0.9
+    .. versionadded:: 0.9.1
     """
 
     @classmethod
-    @since(0.9)
+    @since('0.9.1')
     def train(cls, data, iterations=100, step=1.0, regParam=0.01,
               miniBatchFraction=1.0, initialWeights=None, regType="l2",
               intercept=False, validateData=True, convergenceTol=0.001):
@@ -575,14 +576,14 @@ class NaiveBayesModel(Saveable, Loader):
     ... except OSError:
     ...     pass
 
-    .. versionadded:: 0.9
+    .. versionadded:: 0.9.1
     """
     def __init__(self, labels, pi, theta):
         self.labels = labels
         self.pi = pi
         self.theta = theta
 
-    @since(0.9)
+    @since('0.9.1')
     def predict(self, x):
         """
         Return the most likely class for a data vector
@@ -605,7 +606,7 @@ class NaiveBayesModel(Saveable, Loader):
         java_model.save(sc._jsc.sc(), path)
 
     @classmethod
-    @since(1.4)
+    @since('1.4.0')
     def load(cls, sc, path):
         """
         Load a model from the given path.
@@ -621,11 +622,11 @@ class NaiveBayesModel(Saveable, Loader):
 
 class NaiveBayes(object):
     """
-    .. versionadded:: 0.9
+    .. versionadded:: 0.9.1
     """
 
     @classmethod
-    @since(0.9)
+    @since('0.9.1')
     def train(cls, data, lambda_=1.0):
         """
         Train a Naive Bayes model given an RDD of (label, features)
@@ -663,7 +664,7 @@ class StreamingLogisticRegressionWithSGD(StreamingLinearAlgorithm):
     :param regParam: L2 Regularization parameter.
     :param convergenceTol: A condition which decides iteration termination.
 
-    .. versionadded:: 1.5
+    .. versionadded:: 1.5.0
     """
     def __init__(self, stepSize=0.1, numIterations=50, miniBatchFraction=1.0, regParam=0.01,
                  convergenceTol=0.001):
@@ -676,7 +677,7 @@ class StreamingLogisticRegressionWithSGD(StreamingLinearAlgorithm):
         super(StreamingLogisticRegressionWithSGD, self).__init__(
             model=self._model)
 
-    @since(1.5)
+    @since('1.5.0')
     def setInitialWeights(self, initialWeights):
         """
         Set the initial value of weights.
@@ -690,7 +691,7 @@ class StreamingLogisticRegressionWithSGD(StreamingLinearAlgorithm):
             initialWeights, 0, initialWeights.size, 2)
         return self
 
-    @since(1.5)
+    @since('1.5.0')
     def trainOn(self, dstream):
         """Train the model on the incoming dstream."""
         self._validate(dstream)
