512ca8af85687cc1cd3aad5990ace8e7c169094f
Holden Karau <holden@pigscanfly.ca>
2015-09-16 11:38:07 -0700

    indentation fixes

--- mllib/src/main/scala/org/apache/spark/ml/feature/package-info.java
+++ mllib/src/main/scala/org/apache/spark/ml/feature/package-info.java
@@ -46,7 +46,6 @@
  *   import org.apache.spark.api.java.JavaRDD;
  *   import static org.apache.spark.sql.types.DataTypes.*;
  *   import org.apache.spark.sql.types.StructType;
- *   import org.apache.spark.sql.types.StructField;
  *   import org.apache.spark.sql.DataFrame;
  *   import org.apache.spark.sql.RowFactory;
  *   import org.apache.spark.sql.Row;
