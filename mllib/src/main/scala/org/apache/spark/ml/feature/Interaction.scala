569428d39d07f0073ce52864c813326910c01f46
Eric Liang <ekl@databricks.com>
2015-09-15 19:03:51 -0700

    Tue Sep 15 19:03:51 PDT 2015

---
+++ mllib/src/main/scala/org/apache/spark/ml/feature/Interaction.scala
@@ -0,0 +1,238 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.feature
+
+import scala.collection.mutable.{ArrayBuffer, ArrayBuilder}
+
+import org.apache.spark.SparkException
+import org.apache.spark.annotation.Experimental
+import org.apache.spark.ml.attribute._
+import org.apache.spark.ml.param._
+import org.apache.spark.ml.param.shared._
+import org.apache.spark.ml.util.Identifiable
+import org.apache.spark.ml.{Estimator, Model, Pipeline, PipelineModel, PipelineStage, Transformer}
+import org.apache.spark.mllib.linalg.{Vector, VectorUDT, Vectors}
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+/**
+ * :: Experimental ::
+ * Implements the transforms required for R-style feature interactions. In summary, once fitted to
+ * a dataset, this transformer jointly one-hot encodes all factor input columns, then scales
+ * the encoded vector by all numeric input columns. If only numeric columns are specified, the
+ * output column will be a one-length vector containing their product. During one-hot encoding,
+ * the last category will be preserved unless the interaction is trivial.
+ *
+ * See https://stat.ethz.ch/R-manual/R-devel/library/base/html/formula.html for more
+ * information about factor interactions in R formulae.
+ */
+@Experimental
+class Interaction(override val uid: String) extends Transformer
+  with HasInputCols with HasOutputCol {
+
+  def this() = this(Identifiable.randomUID("interaction"))
+
+  /** @group setParam */
+  def setInputCols(values: Array[String]): this.type = set(inputCols, values)
+
+  /** @group setParam */
+  def setOutputCol(value: String): this.type = set(outputCol, value)
+
+  // optimistic schema; does not contain any ML attributes
+  override def transformSchema(schema: StructType): StructType = {
+    checkParams()
+    StructType(schema.fields :+ StructField($(outputCol), new VectorUDT, true))
+  }
+
+  override def transform(dataset: DataFrame): DataFrame = {
+    checkParams()
+    val fieldIterators = getIterators(dataset)
+
+    def interact(vv: Any*): Vector = {
+      var indices = ArrayBuilder.make[Int]
+      var values = ArrayBuilder.make[Double]
+      var size = 1
+      indices += 0
+      values += 1.0
+      var fieldIndex = vv.length - 1
+      while (fieldIndex >= 0) {
+        val prevIndices = indices.result()
+        val prevValues = values.result()
+        val prevSize = size
+        val currentIterator = fieldIterators(fieldIndex)
+        indices = ArrayBuilder.make[Int]
+        values = ArrayBuilder.make[Double]
+        size *= currentIterator.size
+        currentIterator.foreachActive(vv(fieldIndex), (i, a) => {
+          var j = 0
+          while (j < prevIndices.length) {
+            indices += prevIndices(j) + i * prevSize
+            values += prevValues(j) * a
+            j += 1
+          }
+        })
+        fieldIndex -= 1
+      }
+      Vectors.sparse(size, indices.result(), values.result()).compressed
+    }
+
+    val interactFunc = udf { r: Row =>
+      interact(r.toSeq: _*)
+    }
+    val args = $(inputCols).map { c =>
+      dataset.schema(c).dataType match {
+        case DoubleType => dataset(c)
+        case _: VectorUDT => dataset(c)
+        case _: NumericType | BooleanType => dataset(c).cast(DoubleType)
+      }
+    }
+    val attrs = generateAttrs($(inputCols).map(col => dataset.schema(col)))
+    dataset.select(
+      col("*"),
+      interactFunc(struct(args: _*)).as($(outputCol), attrs.toMetadata()))
+  }
+
+  private def getIterators(dataset: DataFrame): Array[EncodingIterator] = {
+    def getCardinality(attr: Attribute): Int = {
+      attr match {
+        case nominal: NominalAttribute =>
+          nominal.getNumValues.getOrElse(
+            throw new SparkException("Nominal fields must have attr numValues defined."))
+        case _ =>
+          0  // treated as numeric
+      }
+    }
+    $(inputCols).map { col =>
+      val field = dataset.schema(col)
+      val cardinalities = field.dataType match {
+        case _: NumericType | BooleanType =>
+          Array(getCardinality(Attribute.fromStructField(field)))
+        case _: VectorUDT =>
+          val attrs = AttributeGroup.fromStructField(field).attributes.getOrElse(
+            throw new SparkException("Vector fields must have attributes defined."))
+          attrs.map(getCardinality).toArray
+      }
+      new EncodingIterator(cardinalities)
+    }.toArray
+  }
+
+  private def generateAttrs(schema: Seq[StructField]): AttributeGroup = {
+    var attrs: Seq[Attribute] = Nil
+    schema.reverse.map { field =>
+      val attrIterator = field.dataType match {
+        case _: NumericType | BooleanType =>
+          val attr = Attribute.fromStructField(field)
+          encodedAttrIterator(None, Seq(attr))
+        case _: VectorUDT =>
+          val group = AttributeGroup.fromStructField(field)
+          encodedAttrIterator(Some(group.name), group.attributes.get)
+      }
+      if (attrs.isEmpty) {
+        attrs = attrIterator
+      } else {
+        attrs = attrIterator.flatMap { head =>
+          attrs.map { tail =>
+            NumericAttribute.defaultAttr.withName(head.name.get + ":" + tail.name.get)
+          }
+        }
+      }
+    }
+    new AttributeGroup($(outputCol), attrs.toArray)
+  }
+
+  private def encodedAttrIterator(groupName: Option[String], attrs: Seq[Attribute]): Seq[Attribute] = {
+    def format(i: Int, attrName: Option[String], value: Option[String]): String = {
+      val parts = Seq(groupName, Some(attrName.getOrElse(i.toString)), value)
+      parts.flatten.mkString("_")
+    }
+    attrs.zipWithIndex.flatMap {
+      case (nominal: NominalAttribute, i) =>
+        if (nominal.values.isDefined) {
+          nominal.values.get.map(
+            v => BinaryAttribute.defaultAttr.withName(format(i, nominal.name, Some(v))))
+        } else {
+          Array.tabulate(nominal.getNumValues.get)(
+            j => BinaryAttribute.defaultAttr.withName(format(i, nominal.name, Some(j.toString))))
+        }
+      case (a: Attribute, i) =>
+        Seq(NumericAttribute.defaultAttr.withName(format(i, a.name, None)))
+    }
+  }
+
+  override def copy(extra: ParamMap): Interaction = defaultCopy(extra)
+
+  private def checkParams(): Unit = {
+    require(get(inputCols).isDefined, "Input cols must be defined first.")
+    require(get(outputCol).isDefined, "Output col must be defined first.")
+    require($(inputCols).length > 0, "Input cols must have non-zero length.")
+    require($(inputCols).distinct.length == $(inputCols).length, "Input cols must be distinct.")
+  }
+}
+
+/**  
+ * An iterator over VectorUDT or Double that one-hot encodes nominal fields in the output.
+ *
+ * @param cardinalities An array defining the cardinality of each vector sub-field, or a single
+ *                      value if the field is numeric. Fields with zero cardinality will not be
+ *                      one-hot encoded (output verbatim).
+ */
+// TODO(ekl) do we want to support drop-last like OneHotEncoder does?
+private[ml] class EncodingIterator(cardinalities: Array[Int]) {
+  /** The size of the output vector. */
+  val size = cardinalities.map(i => if (i > 0) i else 1).sum
+
+  /**
+   * @param value The row to iterate over, either a Double or Vector.
+   * @param f The callback to invoke on each non-zero (index, value) output pair.
+   */
+  def foreachActive(value: Any, f: (Int, Double) => Unit) = value match {
+    case d: Double =>
+      assert(cardinalities.length == 1)
+      val numOutputCols = cardinalities(0)
+      if (numOutputCols > 0) {
+        assert(d >= 0.0 && d == d.toInt, s"Values from column must be indices, but got $d.")
+        f(d.toInt, 1.0)
+      } else {
+        f(0, d)
+      }
+    case vec: Vector =>
+      assert(cardinalities.length == vec.size,
+        s"Vector column size was ${vec.size}, expected ${cardinalities.length}")
+      val dense = vec.toDense
+      var i = 0
+      var cur = 0
+      while (i < dense.size) {
+        val numOutputCols = cardinalities(i)
+        if (numOutputCols > 0) {
+          val x = dense.values(i).toInt
+          assert(x >= 0.0 && x == x.toInt, s"Values from column must be indices, but got $x.")
+          f(cur + x, 1.0)
+          cur += numOutputCols
+        } else {
+          f(cur, dense.values(i))
+          cur += 1
+        }
+        i += 1
+      }
+    case null =>
+      throw new SparkException("Values to interact cannot be null.")
+    case o =>
+      throw new SparkException(s"$o of type ${o.getClass.getName} is not supported.")
+  }
+}
---
+++ mllib/src/test/scala/org/apache/spark/ml/feature/InteractionSuite.scala
@@ -0,0 +1,211 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.feature
+
+import scala.collection.mutable.ArrayBuilder
+
+import org.apache.spark.{SparkException, SparkFunSuite}
+import org.apache.spark.ml.attribute._
+import org.apache.spark.ml.param.ParamsSuite
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.util.MLlibTestSparkContext
+import org.apache.spark.sql.functions.col
+
+class RInteractionSuite extends SparkFunSuite with MLlibTestSparkContext {
+  test("params") {
+    ParamsSuite.checkParams(new Interaction())
+  }
+
+  test("encoding iterator") {
+    def encode(cardinalities: Array[Int], value: Any): Vector = {
+      var indices = ArrayBuilder.make[Int]
+      var values = ArrayBuilder.make[Double]
+      val iter = new EncodingIterator(cardinalities)
+      iter.foreachActive(value, (i, v) => {
+        indices += i
+        values += v
+      })
+      Vectors.sparse(iter.size, indices.result(), values.result()).compressed
+    }
+    assert(encode(Array(0), 2.2) === Vectors.dense(2.2))
+    assert(encode(Array(3), Vectors.dense(1)) === Vectors.dense(0, 1, 0))
+    assert(encode(Array(0, 0), Vectors.dense(1.1, 2.2)) === Vectors.dense(1.1, 2.2))
+    assert(encode(Array(3, 0), Vectors.dense(1, 2.2)) === Vectors.dense(0, 1, 0, 2.2))
+    assert(encode(Array(2, 0), Vectors.dense(1, 2.2)) === Vectors.dense(0, 1, 2.2))
+    assert(encode(Array(2, 0, 1), Vectors.dense(0, 2.2, 0)) === Vectors.dense(1, 0, 2.2, 1))
+    intercept[SparkException] { encode(Array(0), "foo") }
+    intercept[SparkException] { encode(Array(0), null) }
+    intercept[AssertionError] { encode(Array(1), 2.2) }
+    intercept[AssertionError] { encode(Array(1), Vectors.dense(2.2)) }
+    intercept[AssertionError] { encode(Array(1), Vectors.dense(1.0, 2.0, 3.0)) }
+  }
+
+  test("new interaction") {
+    val data = sqlContext.createDataFrame(
+      Seq(
+        (0, "foo", true, 4, Vectors.dense(0.0, 0.0, 1.0), Vectors.dense(5.0, 3.0)),
+        (0, "bar", true, 4, Vectors.dense(1.0, 4.0, 2.0), Vectors.dense(4.0, 3.0)),
+        (0, "bar", true, 5, Vectors.dense(2.0, 5.0, 3.0), Vectors.dense(5.0, 3.0)),
+        (0, "baz", true, 5, Vectors.dense(3.0, 8.0, 4.0), Vectors.dense(5.0, 2.0)),
+        (0, "baz", false, 5, Vectors.dense(4.0, 9.0, 8.0), Vectors.dense(7.0, 1.0)),
+        (1, "baz", false, 5, Vectors.dense(5.0, 2.0, 9.0), Vectors.dense(2.0, 0.0)))
+      ).toDF("id", "a", "bin", "b", "test", "test2")
+    val attrs = new AttributeGroup(
+      "test",
+      Array[Attribute](
+        NominalAttribute.defaultAttr.withValues(Array("a", "b", "c", "d", "e", "f")),
+        NumericAttribute.defaultAttr.withName("magnitude"),
+        NominalAttribute.defaultAttr.withName("colors").withValues(
+          Array("green", "blue", "red", "violet", "yellow",
+            "orange", "black", "white", "azure", "gray"))))
+
+    val idAttr = NominalAttribute.defaultAttr.withValues(Array("red", "blue"))
+    val attrs2 = new AttributeGroup(
+      "test2",
+      Array[Attribute](
+        NumericAttribute.defaultAttr,
+        NominalAttribute.defaultAttr.withValues(Array("one", "two", "three", "four"))))
+    val df = data.select(
+      col("id").as("id", idAttr.toMetadata()), col("b"), col("bin"),
+      col("test").as("test", attrs.toMetadata()),
+      col("test2").as("test2", attrs2.toMetadata()))
+    df.collect.foreach(println)
+    println(df.schema)
+    df.schema.foreach { field =>
+      println(field.metadata)
+    }
+//    val trans = new Interaction().setInputCols(Array("id", "test2", "test")).setOutputCol("feature")
+    val trans = new Interaction().setInputCols(Array("id", "test2")).setOutputCol("feature")
+    val res = trans.transform(df)
+    res.collect.foreach(println)
+    println(res.schema)
+    res.schema.foreach { field =>
+      println(field.metadata)
+    }
+  }
+//
+//  test("parameter validation") {
+//    val data = sqlContext.createDataFrame(
+//      Seq((1, "foo", "zq"), (2, "bar", "zq"), (3, "bar", "zz"))
+//    ).toDF("id", "a", "b")
+//    def check(inputCols: Array[String], outputCol: String, expectOk: Boolean): Unit = {
+//      val interaction = new RInteraction()
+//      if (inputCols != null) {
+//        interaction.setInputCols(inputCols)
+//      }
+//      if (outputCol != null) {
+//        interaction.setOutputCol(outputCol)
+//      }
+//      if (expectOk) {
+//        interaction.transformSchema(data.schema)
+//        interaction.fit(data).transform(data).collect()
+//      } else {
+//        intercept[IllegalArgumentException] {
+//          interaction.fit(data)
+//        }
+//        intercept[IllegalArgumentException] {
+//          interaction.transformSchema(data.schema)
+//        }
+//      }
+//    }
+//    check(Array("a", "b"), "test", true)
+//    check(Array("id"), "test", true)
+//    check(Array("b"), "test", true)
+//    check(Array("b"), "test", true)
+//    check(Array(), "test", false)
+//    check(Array("a", "b", "b"), "id", false)
+//    check(Array("a", "b"), null, false)
+//    check(null, "test", false)
+//  }
+//
+//  test("numeric interaction") {
+//    val interaction = new RInteraction()
+//      .setInputCols(Array("b", "c", "d"))
+//      .setOutputCol("test")
+//    val original = sqlContext.createDataFrame(
+//      Seq((1, 2, 4, 2), (2, 3, 4, 1))
+//    ).toDF("a", "b", "c", "d")
+//    val model = interaction.fit(original)
+//    val result = model.transform(original)
+//    val expected = sqlContext.createDataFrame(
+//      Seq(
+//        (1, 2, 4, 2, 16.0),
+//        (2, 3, 4, 1, 12.0))
+//      ).toDF("a", "b", "c", "d", "test")
+//    assert(result.collect() === expected.collect())
+//    val attr = Attribute.decodeStructField(result.schema("test"), preserveName = true)
+//    val expectedAttr = new NumericAttribute(Some("b:c:d"), None)
+//    assert(attr === expectedAttr)
+//  }
+//
+//  test("factor interaction") {
+//    val interaction = new RInteraction()
+//      .setInputCols(Array("a", "b"))
+//      .setOutputCol("test")
+//    val original = sqlContext.createDataFrame(
+//      Seq((1, "foo", "zq"), (2, "bar", "zq"), (3, "bar", "zz"))
+//    ).toDF("id", "a", "b")
+//    val model = interaction.fit(original)
+//    val result = model.transform(original)
+//    val expected = sqlContext.createDataFrame(
+//      Seq(
+//        (1, "foo", "zq", Vectors.dense(0.0, 1.0, 0.0, 0.0)),
+//        (2, "bar", "zq", Vectors.dense(1.0, 0.0, 0.0, 0.0)),
+//        (3, "bar", "zz", Vectors.dense(0.0, 0.0, 1.0, 0.0)))
+//      ).toDF("id", "a", "b", "test")
+//    assert(result.collect() === expected.collect())
+//    val attrs = AttributeGroup.fromStructField(result.schema("test"))
+//    val expectedAttrs = new AttributeGroup(
+//      "test",
+//      Array[Attribute](
+//        new BinaryAttribute(Some("a_bar:b_zq"), Some(1)),
+//        new BinaryAttribute(Some("a_foo:b_zq"), Some(2)),
+//        new BinaryAttribute(Some("a_bar:b_zz"), Some(3)),
+//        new BinaryAttribute(Some("a_foo:b_zz"), Some(4))))
+//    assert(attrs === expectedAttrs)
+//  }
+//
+//  test("factor numeric interaction") {
+//    val interaction = new RInteraction()
+//      .setInputCols(Array("a", "b"))
+//      .setOutputCol("test")
+//    val original = sqlContext.createDataFrame(
+//      Seq((1, "foo", 4), (2, "bar", 4), (3, "bar", 5), (4, "baz", 5), (4, "baz", 5), (4, "baz", 5))
+//    ).toDF("id", "a", "b")
+//    val model = interaction.fit(original)
+//    val result = model.transform(original)
+//    val expected = sqlContext.createDataFrame(
+//      Seq(
+//        (1, "foo", 4, Vectors.dense(0.0, 0.0, 4.0)),
+//        (2, "bar", 4, Vectors.dense(0.0, 4.0, 0.0)),
+//        (3, "bar", 5, Vectors.dense(0.0, 5.0, 0.0)),
+//        (4, "baz", 5, Vectors.dense(5.0, 0.0, 0.0)),
+//        (4, "baz", 5, Vectors.dense(5.0, 0.0, 0.0)),
+//        (4, "baz", 5, Vectors.dense(5.0, 0.0, 0.0)))
+//      ).toDF("id", "a", "b", "test")
+//    assert(result.collect() === expected.collect())
+//    val attrs = AttributeGroup.fromStructField(result.schema("test"))
+//    val expectedAttrs = new AttributeGroup(
+//      "test",
+//      Array[Attribute](
+//        new BinaryAttribute(Some("a_baz:b"), Some(1)),
+//        new BinaryAttribute(Some("a_bar:b"), Some(2)),
+//        new BinaryAttribute(Some("a_foo:b"), Some(3))))
+//    assert(attrs === expectedAttrs)
+//  }
+}
