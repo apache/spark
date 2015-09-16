d1d8881e4117abe9a3a46155a793f3d2ce624ca0
Jian Feng <jzhang.chs@gmail.com>
2015-09-16 11:20:51 +0800

    [SPARK-10577] [PySpark] DataFrame hint for broadcast join #8777
    https://issues.apache.org/jira/browse/SPARK-10577
    
    add space after comma

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -195,7 +195,7 @@ def broadcast(df):
     """Marks a DataFrame as small enough for use in broadcast joins."""
 
     sc = SparkContext._active_spark_context
-    return DataFrame(sc._jvm.functions.broadcast(df._jdf),sc._jsc)
+    return DataFrame(sc._jvm.functions.broadcast(df._jdf), sc._jsc)
 
 
 @since(1.4)
