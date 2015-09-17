36d8b278d82e788bf583e8438fac524d0023311d
Jeff Zhang <zjffdu@apache.org>
2015-09-17 10:25:18 -0700

    [SPARK-10531] [CORE] AppId is set as AppName in status rest api
    Verify it manually.
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #8688 from zjffdu/SPARK-10531.

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -521,6 +521,7 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
     _applicationId = _taskScheduler.applicationId()
     _applicationAttemptId = taskScheduler.applicationAttemptId()
     _conf.set("spark.app.id", _applicationId)
+    _ui.foreach(_.setAppId(_applicationId))
     _env.blockManager.initialize(_applicationId)
 
     // The metrics system for Driver need to be set spark.app.id to app ID.
