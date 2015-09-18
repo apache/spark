93c7650ab60a839a9cbe8b4ea1d5eda93e53ebe0
linweizhong <linweizhong@huawei.com>
2015-09-17 22:25:24 -0700

    [SPARK-9522] [SQL] SparkSubmit process can not exit if kill application when HiveThriftServer was starting
    When we start HiveThriftServer, we will start SparkContext first, then start HiveServer2, if we kill application while HiveServer2 is starting then SparkContext will stop successfully, but SparkSubmit process can not exit.
    
    Author: linweizhong <linweizhong@huawei.com>
    
    Closes #7853 from Sephiroth-Lin/SPARK-9522.

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -97,7 +97,7 @@ class SparkContext(config: SparkConf) extends Logging with ExecutorAllocationCli
 
   val startTime = System.currentTimeMillis()
 
-  private val stopped: AtomicBoolean = new AtomicBoolean(false)
+  private[spark] val stopped: AtomicBoolean = new AtomicBoolean(false)
 
   private def assertNotStopped(): Unit = {
     if (stopped.get()) {
