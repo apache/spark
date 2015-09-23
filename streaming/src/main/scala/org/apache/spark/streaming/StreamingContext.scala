8a23ef59bc462370cf8729613aebbd1639f6be0f
Tathagata Das <tathagata.das1565@gmail.com>
2015-09-22 22:44:09 -0700

    [SPARK-10652] [SPARK-10742] [STREAMING] Set meaningful job descriptions for all streaming jobs
    Here is the screenshot after adding the job descriptions to threads that run receivers and the scheduler thread running the batch jobs.
    
    ## All jobs page
    * Added job descriptions with links to relevant batch details page
    ![image](https://cloud.githubusercontent.com/assets/663212/9924165/cda4a372-5cb1-11e5-91ca-d43a32c699e9.png)
    
    ## All stages page
    * Added stage descriptions with links to relevant batch details page
    ![image](https://cloud.githubusercontent.com/assets/663212/9923814/2cce266a-5cae-11e5-8a3f-dad84d06c50e.png)
    
    ## Streaming batch details page
    * Added the +details link
    ![image](https://cloud.githubusercontent.com/assets/663212/9921977/24014a32-5c98-11e5-958e-457b6c38065b.png)
    
    Author: Tathagata Das <tathagata.das1565@gmail.com>
    
    Closes #8791 from tdas/SPARK-10652.
    
    (cherry picked from commit 5548a254755bb84edae2768b94ab1816e1b49b91)
    Signed-off-by: Tathagata Das <tathagata.das1565@gmail.com>

--- streaming/src/main/scala/org/apache/spark/streaming/StreamingContext.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/StreamingContext.scala
@@ -200,6 +200,8 @@ class StreamingContext private[streaming] (
 
   private val startSite = new AtomicReference[CallSite](null)
 
+  private[streaming] def getStartSite(): CallSite = startSite.get()
+
   private var shutdownHookRef: AnyRef = _
 
   // The streaming scheduler and other threads started by the StreamingContext
@@ -750,7 +752,7 @@ object StreamingContext extends Logging {
         throw new IllegalStateException(
           "Only one StreamingContext may be started in this JVM. " +
             "Currently running StreamingContext was started at" +
-            activeContext.get.startSite.get.longForm)
+            activeContext.get.getStartSite().longForm)
       }
     }
   }
