975c2410b5df8be854073972a08e3d2de32c2e95
Holden Karau <holden@pigscanfly.ca>
2015-09-25 14:40:01 -0700

    Revert "Clear Spark property during yarn client stop as well"
    This reverts commit 67b0c821eaa763360b276accfcfd6eb2b13400c8.

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/Client.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/Client.scala
@@ -86,11 +86,7 @@ private[spark] class Client(
   private val fireAndForget = isClusterMode &&
     !sparkConf.getBoolean("spark.yarn.submit.waitAppCompletion", true)
 
-  def stop(): Unit = {
-    yarnClient.stop()
-    // Unset YARN mode system env variable, to allow switching between cluster types.
-    System.clearProperty("SPARK_YARN_MODE")
-  }
+  def stop(): Unit = yarnClient.stop()
 
   /**
    * Submit an application running our ApplicationMaster to the ResourceManager.
