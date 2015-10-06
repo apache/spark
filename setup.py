e78ec1f1dc858b3f445858e5552c1a066c02214d
Bolke de Bruin <bolke@xs4all.nl>
2015-10-06 15:43:44 +0200

    Port ci tests from snakebite in order to test against hadoop

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
 import sys
 
 # Kept manually in sync with airflow.__version__
 version = '1.5.1'
+
+class Tox(TestCommand):
+    user_options = [('tox-args=', None, "Arguments to pass to tox")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = ''
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        errno = tox.cmdline(args=self.tox_args.split())
+        sys.exit(errno)
 
 celery = [
     'celery>=3.1.17', 
@@ -92,4 +109,5 @@ setup(
     url='https://github.com/airbnb/airflow',
     download_url=(
         'https://github.com/airbnb/airflow/tarball/' + version),
+    cmdclass={'test': Tox},
 )
