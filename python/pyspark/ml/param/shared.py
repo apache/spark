c11372f2287f1e4a417f0786677e032a6303e964
Holden Karau <holden@pigscanfly.ca>
2015-10-07 17:50:35 -0700

    [SPARK-9774] [ML] [PYSPARK] Add python api for ml regression isotonicregression
    Add the Python API for isotonicregression.
    
    Author: Holden Karau <holden@pigscanfly.ca>
    
    Closes #8214 from holdenk/SPARK-9774-add-python-api-for-ml-regression-isotonicregression.

--- python/pyspark/ml/param/shared.py
+++ python/pyspark/ml/param/shared.py
@@ -570,6 +570,33 @@ class HasThresholds(Params):
         return self.getOrDefault(self.thresholds)
 
 
+class HasWeightCol(Params):
+    """
+    Mixin for param weightCol: weight column name. If this is not set or empty, we treat all instance weights as 1.0..
+    """
+
+    # a placeholder to make it appear in the generated doc
+    weightCol = Param(Params._dummy(), "weightCol", "weight column name. If this is not set or empty, we treat all instance weights as 1.0.")
+
+    def __init__(self):
+        super(HasWeightCol, self).__init__()
+        #: param for weight column name. If this is not set or empty, we treat all instance weights as 1.0.
+        self.weightCol = Param(self, "weightCol", "weight column name. If this is not set or empty, we treat all instance weights as 1.0.")
+
+    def setWeightCol(self, value):
+        """
+        Sets the value of :py:attr:`weightCol`.
+        """
+        self._paramMap[self.weightCol] = value
+        return self
+
+    def getWeightCol(self):
+        """
+        Gets the value of weightCol or its default value.
+        """
+        return self.getOrDefault(self.weightCol)
+
+
 class DecisionTreeParams(Params):
     """
     Mixin for Decision Tree parameters.
