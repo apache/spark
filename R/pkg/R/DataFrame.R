3cd2079433f35f707328c9500cd0120fa8cee226
Oscar D. Lara Yejas <olarayej@mail.usf.edu>
2015-10-09 22:47:12 +0100

    Added more tests

--- R/pkg/R/DataFrame.R
+++ R/pkg/R/DataFrame.R
@@ -2169,6 +2169,7 @@ setMethod("coltypes",
             # R's data types
             DATA_TYPES <- c("string"="character",
                             "long"="integer",
+                            "tinyint"="integer",
                             "short"="integer",
                             "integer"="integer",
                             "byte"="integer",
