50717599f1eb5bf2184a6b1df2e0aebabdebddec
Ankur Dave <ankurdave@gmail.com>
2015-10-13 00:36:00 -0700

    Fix override of KeyHint#transformExpressions{Up,Down}

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/basicOperators.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/basicOperators.scala
@@ -485,14 +485,16 @@ case class KeyHint(newKeys: Seq[Key], child: LogicalPlan) extends UnaryNode {
   /** Overridden here to apply `rule` to the keys as well as the child plan. */
   override def transformExpressionsDown(
       rule: PartialFunction[Expression, Expression]): this.type = {
-    KeyHint(newKeys.map(_.transformAttribute(rule.andThen(_.asInstanceOf[Attribute]))), child)
-      .asInstanceOf[this.type]
+    KeyHint(
+      newKeys.map(_.transformAttribute(rule.andThen(_.asInstanceOf[Attribute]))),
+      child.transformExpressionsDown(rule)).asInstanceOf[this.type]
   }
 
   /** Overridden here to apply `rule` to the keys as well as the child plan. */
   override def transformExpressionsUp(
       rule: PartialFunction[Expression, Expression]): this.type = {
-    KeyHint(newKeys.map(_.transformAttribute(rule.andThen(_.asInstanceOf[Attribute]))), child)
-    .asInstanceOf[this.type]
+    KeyHint(
+      newKeys.map(_.transformAttribute(rule.andThen(_.asInstanceOf[Attribute]))),
+      child.transformExpressionsUp(rule)).asInstanceOf[this.type]
   }
 }
