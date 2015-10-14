e423a2f94d16cbb05b0cedbb0c3defefd8c6f7c5
Wenchen Fan <cloud0fan@163.com>
2015-10-13 17:11:22 -0700

    [SPARK-11032] [SQL] correctly handle having
    We should not stop resolving having when the having condtion is resolved, or something like `count(1)` will crash.
    
    Author: Wenchen Fan <cloud0fan@163.com>
    
    Closes #9105 from cloud-fan/having.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -553,7 +553,7 @@ class Analyzer(
     def apply(plan: LogicalPlan): LogicalPlan = plan resolveOperators {
       case filter @ Filter(havingCondition,
              aggregate @ Aggregate(grouping, originalAggExprs, child))
-          if aggregate.resolved && !filter.resolved =>
+          if aggregate.resolved =>
 
         // Try resolving the condition of the filter as though it is in the aggregate clause
         val aggregatedCondition =
