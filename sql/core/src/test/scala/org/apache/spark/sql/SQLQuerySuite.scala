bb3b3627ac3fcd18be7fb07b6d0ba5eae0342fc3
Wenchen Fan <cloud0fan@163.com>
2015-10-13 17:11:22 -0700

    [SPARK-11032] [SQL] correctly handle having
    We should not stop resolving having when the having condtion is resolved, or something like `count(1)` will crash.
    
    Author: Wenchen Fan <cloud0fan@163.com>
    
    Closes #9105 from cloud-fan/having.
    
    (cherry picked from commit e170c22160bb452f98c340489ebf8390116a8cbb)
    Signed-off-by: Yin Huai <yhuai@databricks.com>
    
    Conflicts:
    	sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -1765,4 +1765,13 @@ class SQLQuerySuite extends QueryTest with SharedSQLContext {
       assert(sampled.count() == sampledOdd.count() + sampledEven.count())
     }
   }
+
+  test("SPARK-11032: resolve having correctly") {
+    withTempTable("src") {
+      Seq(1 -> "a").toDF("i", "j").registerTempTable("src")
+      checkAnswer(
+        sql("SELECT MIN(t.i) FROM (SELECT * FROM src WHERE i > 0) t HAVING(COUNT(1) > 0)"),
+        Row(1))
+    }
+  }
 }
