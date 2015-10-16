685f54547f9a76bbf51393ead9548e74fb0c0674
Bolke de Bruin <bolke@xs4all.nl>
2015-10-17 01:30:21 +0200

    Update script for misisng wheels (does not check versions)

---
+++ scripts/ci/wheel_factory.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import requirements
+import argparse
+import glob
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help="requirements.txt", type=str)
+parser.add_argument('wheeldir', help="wheeldir location", type=str)
+
+args = parser.parse_args()
+
+req_file = open(args.file, 'r')
+
+for req in requirements.parse(req_file):
+    if not glob.glob(args.wheeldir + os.path.pathsep + req.name + "*.whl"):
+        os.system("pip wheel --wheel-dir=" + args.wheeldir + " " + req.name + "".join(req.specs) + "".join(req.extras))
