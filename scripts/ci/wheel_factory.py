1bef4a38125cc992d3f02536ef579ba7a2245fa6
Bolke de Bruin <bolke@xs4all.nl>
2015-10-17 18:43:21 +0200

    different way of matching

--- scripts/ci/wheel_factory.py
+++ scripts/ci/wheel_factory.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import requirements
 import argparse
-import glob
 import os
 import re
-import fnmatch
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', help="requirements.txt", type=str)
@@ -14,15 +12,15 @@ args = parser.parse_args()
 
 req_file = open(args.file, 'r')
 
-def findfiles(which, where='.'):
+names = os.listdir(args.wheeldir)
+
+def wheel_exists(which):
     '''Returns list of filenames from `where` path matched by 'which'
        shell pattern. Matching is case-insensitive.'''
 
-    # TODO: recursive param with walk() filtering
-    rule = re.compile(fnmatch.translate(which), re.IGNORECASE)
-    return [name for name in os.listdir(where) if rule.match(name)]
+    return filter(lambda x: re.search(which, x, re.IGNORECASE), names)
 
 for req in requirements.parse(req_file):
     print "Checking " + args.wheeldir + os.path.sep + req.name + "*.whl"
-    if not findfiles(req.name + "*.whl", args.wheeldir):
+    if not wheel_exists(req.name):
         os.system("pip wheel --wheel-dir=" + args.wheeldir + " " + req.name + "".join(req.specs) + "".join(req.extras))
