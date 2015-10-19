2195fec125b2027c734ba4c6940b7fd8dfc5d10b
Liang-Chi Hsieh <viirya@appier.com>
2015-10-19 16:16:31 -0700

    [SPARK-11051][CORE] Do not allow local checkpointing after the RDD is materialized and checkpointed
    JIRA: https://issues.apache.org/jira/browse/SPARK-11051
    
    When a `RDD` is materialized and checkpointed, its partitions and dependencies are cleared. If we allow local checkpointing on it and assign `LocalRDDCheckpointData` to its `checkpointData`. Next time when the RDD is materialized again, the error will be thrown.
    
    Author: Liang-Chi Hsieh <viirya@appier.com>
    
    Closes #9072 from viirya/no-localcheckpoint-after-checkpoint.
    
    (cherry picked from commit a1413b3662250dd5e980e8b1f7c3dc4585ab4766)
    Signed-off-by: Andrew Or <andrew@databricks.com>

--- core/src/test/scala/org/apache/spark/CheckpointSuite.scala
+++ core/src/test/scala/org/apache/spark/CheckpointSuite.scala
@@ -241,9 +241,13 @@ class CheckpointSuite extends SparkFunSuite with LocalSparkContext with Logging 
     val rdd = new BlockRDD[Int](sc, Array[BlockId]())
     assert(rdd.partitions.size === 0)
     assert(rdd.isCheckpointed === false)
+    assert(rdd.isCheckpointedAndMaterialized === false)
     checkpoint(rdd, reliableCheckpoint)
+    assert(rdd.isCheckpointed === false)
+    assert(rdd.isCheckpointedAndMaterialized === false)
     assert(rdd.count() === 0)
     assert(rdd.isCheckpointed === true)
+    assert(rdd.isCheckpointedAndMaterialized === true)
     assert(rdd.partitions.size === 0)
   }
 
