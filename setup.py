8eae432288df3cda0ee591b3bd90d69b0b40f5f6
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-10-21 11:54:47 -0700

    Forcing flask admin 1.2.0

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
         'chartkick>=0.4.2, < 0.5',
         'dill>=0.2.2, <0.3',
         'flask>=0.10.1, <0.11',
-        'flask-admin>=1.2.0',
+        'flask-admin==1.2.0',
         'flask-cache>=0.13.1, <0.14',
         'flask-login>=0.2.11',
         'future>=0.15.0, <0.16',
