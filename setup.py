22c3d2535e8a2b3d40af43fee620e1fea5ea183d
Maxime Beauchemin <maximebeauchemin@gmail.com>
2015-10-21 10:20:42 -0700

    Rolling back some __future__ imports

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
