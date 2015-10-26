0ccb4ae38b797c10dfea45d762932bb07d1d9d53
Liang-Chi Hsieh <viirya@appier.com>
2015-10-27 01:33:17 +0800

    Keep full stack trace in captured exception.

--- python/pyspark/sql/utils.py
+++ python/pyspark/sql/utils.py
@@ -36,10 +36,11 @@ def capture_sql_exception(f):
             return f(*a, **kw)
         except py4j.protocol.Py4JJavaError as e:
             s = e.java_exception.toString()
+            stackTrace = '\n'.join(map(lambda x: x.toString(), e.java_exception.getStackTrace()))
             if s.startswith('org.apache.spark.sql.AnalysisException: '):
-                raise AnalysisException(s.split(': ', 1)[1])
+                raise AnalysisException(s.split(': ', 1)[1], stackTrace)
             if s.startswith('java.lang.IllegalArgumentException: '):
-                raise IllegalArgumentException(s.split(': ', 1)[1])
+                raise IllegalArgumentException(s.split(': ', 1)[1], stackTrace)
             raise
     return deco
 
