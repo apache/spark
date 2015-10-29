82fffabad5a85a7bffee8bce953a16f82a53484c
Josh Rosen <joshrosen@databricks.com>
2015-10-29 13:33:15 -0700

    Fix scalstyle.

--- core/src/test/scala/org/apache/spark/memory/StaticMemoryManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/memory/StaticMemoryManagerSuite.scala
@@ -35,7 +35,10 @@ class StaticMemoryManagerSuite extends MemoryManagerSuite {
       maxExecutionMem: Long,
       maxStorageMem: Long): (StaticMemoryManager, MemoryStore) = {
     val mm = new StaticMemoryManager(
-      conf, maxOnHeapExecutionMemory = maxExecutionMem, maxStorageMemory = maxStorageMem, numCores = 1)
+      conf,
+      maxOnHeapExecutionMemory = maxExecutionMem,
+      maxStorageMemory = maxStorageMem,
+      numCores = 1)
     val ms = makeMemoryStore(mm)
     (mm, ms)
   }
