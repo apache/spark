ab7b6cbdb2f28a484c5cc7c1d7e3ed9a2a2a7424
Nakul Jindal <njindal@us.ibm.com>
2015-10-30 08:55:16 -0700

    Removed spurios NonSparseVal field.

--- mllib/src/main/scala/org/apache/spark/mllib/linalg/Vectors.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/linalg/Vectors.scala
@@ -677,9 +677,7 @@ class SparseVector @Since("1.0.0") (
     s" ${values.length} values.")
   require(indices.length <= size, s"You provided ${indices.length} indices and values, " +
     s"which exceeds the specified vector size ${size}.")
-
-  @Since("1.6.0") val NonSparseVal = 0.0
-
+  
   override def toString: String =
     s"($size,${indices.mkString("[", ",", "]")},${values.mkString("[", ",", "]")})"
 
