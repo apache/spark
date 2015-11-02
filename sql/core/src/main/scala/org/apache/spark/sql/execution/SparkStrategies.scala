fad28d6126187f88b473fc35692248ad2cb00748
Andrew Ray <ray.andrew@gmail.com>
2015-11-02 12:10:02 -0600

    Reimplement Expand as a Generator - added unit tests for cube and rollup that actualy check the result - fixed bugs present in previous implementation of cube/rollup/groupingsets (SPARK-11275)

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
@@ -421,8 +421,6 @@ private[sql] abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
         }
       case logical.Filter(condition, child) =>
         execution.Filter(condition, planLater(child)) :: Nil
-      case e @ logical.Expand(_, _, _, child) =>
-        execution.Expand(e.projections, e.output, planLater(child)) :: Nil
       case a @ logical.Aggregate(group, agg, child) => {
         val useNewAggregation = sqlContext.conf.useSqlAggregate2 && sqlContext.conf.codegenEnabled
         if (useNewAggregation && a.newAggregation.isDefined) {
