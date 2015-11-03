d0a2f447a107a4139ada2102f0908597c6adc3a1
Mark Grover <grover.markgrover@gmail.com>
2015-11-03 05:18:34 -0800

    Fixing indentation based on Marcelo's comment

--- core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
@@ -189,8 +189,8 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val rpcEnv: Rp
       addressToExecutorId
         .get(remoteAddress)
         .foreach(removeExecutor(_, SlaveLost("Remote RPC client disassociated. Likely due to " +
-        "containers exceeding thresholds, or network issues. Check driver logs for WARN " +
-        "messages.")))
+          "containers exceeding thresholds, or network issues. Check driver logs for WARN " +
+          "messages.")))
     }
 
     // Make fake resource offers on just one executor
