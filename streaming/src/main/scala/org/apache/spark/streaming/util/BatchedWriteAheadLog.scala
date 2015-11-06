ec6d8da236c6a742ae523a81ebe4754013dc4a59
Burak Yavuz <brkyvz@gmail.com>
2015-11-05 17:32:39 -0800

    save changes before trying something dangerous

--- streaming/src/main/scala/org/apache/spark/streaming/util/BatchedWriteAheadLog.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/util/BatchedWriteAheadLog.scala
@@ -18,8 +18,8 @@
 package org.apache.spark.streaming.util
 
 import java.nio.ByteBuffer
-import java.util.concurrent.{LinkedBlockingQueue, TimeoutException}
-import java.util.{Iterator => JIterator, ArrayList => JList}
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.{Iterator => JIterator}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
@@ -28,13 +28,24 @@ import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
 import org.apache.spark.{SparkException, Logging}
-import org.apache.spark.util.{Utils, ThreadUtils}
+import org.apache.spark.util.Utils
 
 /**
- * A wrapper for a WriteAheadLog that batches records before writing data. All other methods will
- * be passed on to the wrapped class.
+ * A wrapper for a WriteAheadLog that batches records before writing data. Handles aggregation
+ * during writes, and de-aggregation in the `readAll` method. The end consumer has to handle
+ * de-aggregation after the `read` method. In addition, the `WriteAheadLogRecordHandle` returned
+ * after the write will contain the batch of records rather than individual records.
+ *
+ * When writing a batch of records, the `time` passed to the `wrappedLog` will be the timestamp
+ * of the latest record in the batch. This is very important in achieving correctness. Consider the
+ * following example:
+ * We receive records with timestamps 1, 3, 5, 7. We use "log-1" as the filename. Once we receive
+ * a clean up request for timestamp 3, we would clean up the file "log-1", and lose data regarding
+ * 5 and 7.
+ *
+ * All other methods of the WriteAheadLog interface will be passed on to the wrapped WriteAheadLog.
  */
-private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
+private[util] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
   extends WriteAheadLog with Logging {
 
   import BatchedWriteAheadLog._
@@ -46,7 +57,7 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
 
   // Whether the writer thread is active
   @volatile private var active: Boolean = true
-  protected val buffer = new JList[Record]()
+  private val buffer = new ArrayBuffer[Record]()
 
   private val batchedWriterThread = startBatchedWriterThread()
 
@@ -71,9 +82,8 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
   }
 
   /**
-   * Read all the existing logs from the log directory.
-   *
-   * This method is handled by the parent WriteAheadLog.
+   * Read all the existing logs from the log directory. The output of the wrapped WriteAheadLog
+   * will be de-aggregated.
    */
   override def readAll(): JIterator[ByteBuffer] = {
     wrappedLog.readAll().asScala.flatMap(deaggregate).asJava
@@ -90,7 +100,7 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
 
 
   /**
-   * Stop the batched writer thread, fulfill promises with failures and close parent writer.
+   * Stop the batched writer thread, fulfill promises with failures and close the wrapped WAL.
    */
   override def close(): Unit = {
     logInfo("BatchedWriteAheadLog shutting down.")
@@ -104,8 +114,8 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
     wrappedLog.close()
   }
 
-  /** Start the actual log writer on a separate thread. Visible (protected) for testing. */
-  protected def startBatchedWriterThread(): Thread = {
+  /** Start the actual log writer on a separate thread. */
+  private def startBatchedWriterThread(): Thread = {
     val thread = new Thread(new Runnable {
       override def run(): Unit = {
         while (active) {
@@ -124,11 +134,11 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
     thread
   }
 
-  /** Write all the records in the buffer to the write ahead log. Visible for testing. */
-  protected def flushRecords(): Unit = {
+  /** Write all the records in the buffer to the write ahead log. */
+  private def flushRecords(): Unit = {
     try {
-      buffer.add(walWriteQueue.take())
-      val numBatched = walWriteQueue.drainTo(buffer) + 1
+      buffer.append(walWriteQueue.take())
+      val numBatched = walWriteQueue.drainTo(buffer.asJava) + 1
       logDebug(s"Received $numBatched records from queue")
     } catch {
       case _: InterruptedException =>
@@ -136,11 +146,11 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
     }
     try {
       var segment: WriteAheadLogRecordHandle = null
-      if (buffer.size() > 0) {
-        logDebug(s"Batched ${buffer.size()} records for Write Ahead Log write")
-        // we take the latest record for the time to ensure that we don't clean up files earlier
-        // than the expiration date of the records
-        val time = buffer.get(buff).time
+      if (buffer.length > 0) {
+        logDebug(s"Batched ${buffer.length} records for Write Ahead Log write")
+        // We take the latest record for the timestamp. Please refer to the class Javadoc for
+        // detailed explanation
+        val time = buffer.last.time
         segment = wrappedLog.write(aggregate(buffer), time)
       }
       buffer.foreach(_.promise.success(segment))
@@ -155,16 +165,13 @@ private[streaming] class BatchedWriteAheadLog(wrappedLog: WriteAheadLog)
 }
 
 /** Static methods for aggregating and de-aggregating records. */
-private[streaming] object BatchedWriteAheadLog {
+private[util] object BatchedWriteAheadLog {
   /**
    * Wrapper class for representing the records that we will write to the WriteAheadLog. Coupled
    * with the timestamp for the write request of the record, and the promise that will block the
    * write request, while a separate thread is actually performing the write.
    */
-  private[util] case class Record(
-      data: ByteBuffer,
-      time: Long,
-      promise: Promise[WriteAheadLogRecordHandle])
+  case class Record(data: ByteBuffer, time: Long, promise: Promise[WriteAheadLogRecordHandle])
 
   /** Copies the byte array of a ByteBuffer. */
   private def getByteArray(buffer: ByteBuffer): Array[Byte] = {
