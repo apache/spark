c83588a70201307ef25ce4914c222c4c3da96b1d
Dominik Dahlem <dominik.dahlem@gmail.combination>
2015-11-10 15:12:40 +0000

    added message to require

--- mllib/src/main/scala/org/apache/spark/ml/evaluation/RegressionEvaluator.scala
+++ mllib/src/main/scala/org/apache/spark/ml/evaluation/RegressionEvaluator.scala
@@ -73,12 +73,14 @@ final class RegressionEvaluator @Since("1.4.0") (@Since("1.4.0") override val ui
   @Since("1.4.0")
   override def evaluate(dataset: DataFrame): Double = {
     val schema = dataset.schema
+    val predictionColName = $(predictionCol)
     val predictionType = schema($(predictionCol)).dataType
     require(predictionType == FloatType || predictionType == DoubleType,
-      s"Prediction column $(predictionCol) must be of type float or double, but not $(predictionType)")
+      s"Prediction column $predictionColName must be of type float or double, but not $predictionType")
+    val labelColName = $(labelCol)
     val labelType = schema($(labelCol)).dataType
     require(labelType == FloatType || labelType == DoubleType,
-      s"Label column $(labelCol) must be of type float or double, but not $(labelType)")
+      s"Label column $labelColName must be of type float or double, but not $labelType")
 
     val predictionAndLabels = dataset
       .select(col($(predictionCol)).cast(DoubleType), col($(labelCol)).cast(DoubleType))
