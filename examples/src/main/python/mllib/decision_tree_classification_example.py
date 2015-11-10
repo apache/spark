1fc997a153c351a34230c821be44204e105250fe
Xusen Yin <yinxusen@gmail.com>
2015-11-10 22:02:53 +0800

    fix python style

--- examples/src/main/python/mllib/decision_tree_classification_example.py
+++ examples/src/main/python/mllib/decision_tree_classification_example.py
@@ -35,12 +35,12 @@ if __name__ == "__main__":
     data = MLUtils.loadLibSVMFile(sc, 'data/mllib/sample_libsvm_data.txt')
     # Split the data into training and test sets (30% held out for testing)
     (trainingData, testData) = data.randomSplit([0.7, 0.3])
-    
+
     # Train a DecisionTree model.
     #  Empty categoricalFeaturesInfo indicates all features are continuous.
     model = DecisionTree.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
                                          impurity='gini', maxDepth=5, maxBins=32)
-    
+
     # Evaluate model on test instances and compute test error
     predictions = model.predict(testData.map(lambda x: x.features))
     labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     print('Test Error = ' + str(testErr))
     print('Learned classification tree model:')
     print(model.toDebugString())
-    
+
     # Save and load model
     model.save(sc, "target/tmp/myDecisionTreeClassificationModel")
     sameModel = DecisionTreeModel.load(sc, "target/tmp/myDecisionTreeClassificationModel")
--- examples/src/main/python/mllib/decision_tree_regression_example.py
+++ examples/src/main/python/mllib/decision_tree_regression_example.py
@@ -35,12 +35,12 @@ if __name__ == "__main__":
     data = MLUtils.loadLibSVMFile(sc, 'data/mllib/sample_libsvm_data.txt')
     # Split the data into training and test sets (30% held out for testing)
     (trainingData, testData) = data.randomSplit([0.7, 0.3])
-    
+
     # Train a DecisionTree model.
     #  Empty categoricalFeaturesInfo indicates all features are continuous.
     model = DecisionTree.trainRegressor(trainingData, categoricalFeaturesInfo={},
                                         impurity='variance', maxDepth=5, maxBins=32)
-    
+
     # Evaluate model on test instances and compute test error
     predictions = model.predict(testData.map(lambda x: x.features))
     labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     print('Test Mean Squared Error = ' + str(testMSE))
     print('Learned regression tree model:')
     print(model.toDebugString())
-    
+
     # Save and load model
     model.save(sc, "target/tmp/myDecisionTreeRegressionModel")
     sameModel = DecisionTreeModel.load(sc, "target/tmp/myDecisionTreeRegressionModel")
