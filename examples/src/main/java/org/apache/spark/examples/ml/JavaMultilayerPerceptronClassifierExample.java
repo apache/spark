c4b1f0b494aadd53d91c4b4d8d5b114a7a98d593
Yanbo Liang <ybliang8@gmail.com>
2015-11-10 18:06:27 +0800

    Python example code for Multilayer Perceptron Classification

---
+++ examples/src/main/java/org/apache/spark/examples/ml/JavaMultilayerPerceptronClassifierExample.java
@@ -0,0 +1,7 @@
+package org.apache.spark.examples.ml;
+
+/**
+ * Created by yanboliang on 15/11/10.
+ */
+public class JavaMultilayerPerceptronClassifierExample {
+}
---
+++ examples/src/main/scala/org/apache/spark/examples/ml/MultilayerPerceptronClassifierExample.scala
@@ -0,0 +1,8 @@
+package org.apache.spark.examples.ml
+
+/**
+ * Created by yanboliang on 15/11/10.
+ */
+object MultilayerPerceptronClassifierExample {
+
+}
