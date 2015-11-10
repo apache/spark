d9def91d058742d0d84ea9cdb9934b7707aeeec5
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-11-09 16:25:29 -0800

    [SPARK-11610][MLLIB][PYTHON][DOCS] Make the docs of LDAModel.describeTopics in Python more specific
    cc jkbradley
    
    Author: Yu ISHIKAWA <yuu.ishikawa@gmail.com>
    
    Closes #9577 from yu-iskw/SPARK-11610.

--- python/pyspark/mllib/clustering.py
+++ python/pyspark/mllib/clustering.py
@@ -734,6 +734,12 @@ class LDAModel(JavaModelWrapper, JavaSaveable, Loader):
         """Return the topics described by weighted terms.
 
         WARNING: If vocabSize and k are large, this can return a large object!
+
+        :param maxTermsPerTopic: Maximum number of terms to collect for each topic.
+            (default: vocabulary size)
+        :return: Array over topics. Each topic is represented as a pair of matching arrays:
+            (term indices, term weights in topic).
+            Each topic's terms are sorted in order of decreasing weight.
         """
         if maxTermsPerTopic is None:
             topics = self.call("describeTopics")
