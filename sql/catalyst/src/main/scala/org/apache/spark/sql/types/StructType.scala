1208354de8d4b07c1cc190f559965cf03e5e323f
mayuanwen <mayuanwen@qiyi.com>
2015-11-12 11:21:26 +0800

    fixed SPARK-11679

--- sql/catalyst/src/main/scala/org/apache/spark/sql/types/StructType.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/types/StructType.scala
@@ -328,7 +328,8 @@ object StructType extends AbstractDataType {
   def apply(fields: Seq[StructField]): StructType = StructType(fields.toArray)
 
   def apply(fields: java.util.List[StructField]): StructType = {
-    StructType(fields.toArray.asInstanceOf[Array[StructField]])
+    val temp = new Array[StructField](0)
+    StructType(fields.toArray(temp))
   }
 
   protected[sql] def fromAttributes(attributes: Seq[Attribute]): StructType =
