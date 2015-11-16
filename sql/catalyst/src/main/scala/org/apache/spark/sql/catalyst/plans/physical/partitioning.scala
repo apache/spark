d6a3618bfa573904ca83fc40a802a043fe649ea1
rmishra <rmishra@pivotal.io>
2015-11-16 15:03:02 +0530

    [SNAPPYDATA] New partitioning scheme where the expression order does not matter
    This is useful for situations where table partitioning and query join keys are in different order.
    
    Handle expressions in case insensitive manner.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/physical/partitioning.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/physical/partitioning.scala
@@ -232,6 +232,50 @@ case object SinglePartition extends Partitioning {
 /**
  * Represents a partitioning where rows are split up across partitions based on the hash
  * of `expressions`.  All rows where `expressions` evaluate to the same values are guaranteed to be
+ * in the same partition. Moreover while evaluating expressions if they are given in different order
+ * than this partitioning then also it is considered equal.
+ */
+case class OrderlessHashPartitioning(expressions: Seq[Expression], numPartitions: Int)
+    extends Expression with Partitioning with Unevaluable {
+
+
+  override def children: Seq[Expression] = expressions
+  override def nullable: Boolean = false
+  override def dataType: DataType = IntegerType
+
+  private def matchExpressions(otherExpression: Seq[Expression]): Boolean = {
+    expressions.length == otherExpression.length && expressions.forall(a =>
+      otherExpression.exists(e => e.semanticEquals(a)))
+  }
+
+  override def satisfies(required: Distribution): Boolean = required match {
+    case UnspecifiedDistribution => true
+    case ClusteredDistribution(requiredClustering) => {
+      matchExpressions(requiredClustering)
+    }
+    case _ => false
+  }
+
+  private def anyOrderEquals(other: HashPartitioning) : Boolean = {
+    other.numPartitions == this.numPartitions &&
+        matchExpressions(other.expressions)
+  }
+
+  override def compatibleWith(other: Partitioning): Boolean = other match {
+    case p: HashPartitioning => anyOrderEquals(p)
+    case _ => false
+  }
+
+  override def guarantees(other: Partitioning): Boolean = other match {
+    case o: HashPartitioning => anyOrderEquals(o)
+    case _ => false
+  }
+
+}
+
+/**
+ * Represents a partitioning where rows are split up across partitions based on the hash
+ * of `expressions`.  All rows where `expressions` evaluate to the same values are guaranteed to be
  * in the same partition.
  */
 case class HashPartitioning(expressions: Seq[Expression], numPartitions: Int)
