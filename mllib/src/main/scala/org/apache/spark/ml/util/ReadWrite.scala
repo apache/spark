a6dc7b29250efec2f493ad13cd6004b663ec860a
Joseph K. Bradley <joseph@databricks.com>
2015-11-16 17:12:39 -0800

    [SPARK-11612][ML] Pipeline and PipelineModel persistence
    Pipeline and PipelineModel extend Readable and Writable.  Persistence succeeds only when all stages are Writable.
    
    Note: This PR reinstates tests for other read/write functionality.  It should probably not get merged until [https://issues.apache.org/jira/browse/SPARK-11672] gets fixed.
    
    CC: mengxr
    
    Author: Joseph K. Bradley <joseph@databricks.com>
    
    Closes #9674 from jkbradley/pipeline-io.

--- mllib/src/main/scala/org/apache/spark/ml/util/ReadWrite.scala
+++ mllib/src/main/scala/org/apache/spark/ml/util/ReadWrite.scala
@@ -164,6 +164,8 @@ trait Readable[T] {
 
   /**
    * Reads an ML instance from the input path, a shortcut of `read.load(path)`.
+   *
+   * Note: Implementing classes should override this to be Java-friendly.
    */
   @Since("1.6.0")
   def load(path: String): T = read.load(path)
@@ -190,7 +192,7 @@ private[ml] object DefaultParamsWriter {
    *  - timestamp
    *  - sparkVersion
    *  - uid
-   *  - paramMap
+   *  - paramMap: These must be encodable using [[org.apache.spark.ml.param.Param.jsonEncode()]].
    */
   def saveMetadata(instance: Params, path: String, sc: SparkContext): Unit = {
     val uid = instance.uid
