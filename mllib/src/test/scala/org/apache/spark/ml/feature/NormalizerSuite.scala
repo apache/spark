e7f90164740d920bbdca06e90098b8bd8b775715
Joseph K. Bradley <joseph@databricks.com>
2015-11-17 12:43:56 -0800

    [SPARK-11769][ML] Add save, load to all basic Transformers
    This excludes Estimators and ones which include Vector and other non-basic types for Params or data.  This adds:
    * Bucketizer
    * DCT
    * HashingTF
    * Interaction
    * NGram
    * Normalizer
    * OneHotEncoder
    * PolynomialExpansion
    * QuantileDiscretizer
    * RFormula
    * SQLTransformer
    * StopWordsRemover
    * StringIndexer
    * Tokenizer
    * VectorAssembler
    * VectorSlicer
    
    CC: mengxr
    
    Author: Joseph K. Bradley <joseph@databricks.com>
    
    Closes #9755 from jkbradley/transformer-io.
    
    (cherry picked from commit d98d1cb000c8c4e391d73ae86efd09f15e5d165c)
    Signed-off-by: Xiangrui Meng <meng@databricks.com>

--- mllib/src/test/scala/org/apache/spark/ml/feature/NormalizerSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/NormalizerSuite.scala
@@ -18,13 +18,14 @@
 package org.apache.spark.ml.feature
 
 import org.apache.spark.SparkFunSuite
+import org.apache.spark.ml.util.DefaultReadWriteTest
 import org.apache.spark.mllib.linalg.{DenseVector, SparseVector, Vector, Vectors}
 import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.mllib.util.TestingUtils._
 import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
 
-class NormalizerSuite extends SparkFunSuite with MLlibTestSparkContext {
+class NormalizerSuite extends SparkFunSuite with MLlibTestSparkContext with DefaultReadWriteTest {
 
   @transient var data: Array[Vector] = _
   @transient var dataFrame: DataFrame = _
@@ -103,6 +104,14 @@ class NormalizerSuite extends SparkFunSuite with MLlibTestSparkContext {
     assertTypeOfVector(data, result)
 
     assertValues(result, l1Normalized)
+  }
+
+  test("read/write") {
+    val t = new Normalizer()
+      .setInputCol("myInputCol")
+      .setOutputCol("myOutputCol")
+      .setP(3.0)
+    testDefaultReadWrite(t)
   }
 }
 
