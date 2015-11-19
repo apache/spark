54008ced6c3a380cac71bb85b2a7da2a83fb8c20
Vikas Nelamangala <vikasnelamangala@Vikass-MacBook-Pro.local>
2015-11-19 12:32:48 +0530

    fixed import issue

--- examples/src/main/java/org/apache/spark/examples/mllib/JavaMulticlassClassificationMetricsExample.java
+++ examples/src/main/java/org/apache/spark/examples/mllib/JavaMulticlassClassificationMetricsExample.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.spark.examples.mllib
+package org.apache.spark.examples.mllib;
 // $example on$
 import scala.Tuple2;
 
