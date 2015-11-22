58164891be00627845da1fd8bce8906c20678d12
Richard W. Eggert II <richard.eggert@gmail.com>
2015-11-22 15:57:03 -0500

    Moved unit test for SPARK-4514 from AsyncRDDActionsSuite into StatusTrackerSuite
    This reverts commit 38b144226f6a3cb33558dc9100babd4d77534007.

--- core/src/test/scala/org/apache/spark/StatusTrackerSuite.scala
+++ core/src/test/scala/org/apache/spark/StatusTrackerSuite.scala
@@ -86,4 +86,17 @@ class StatusTrackerSuite extends SparkFunSuite with Matchers with LocalSparkCont
         Set(firstJobId, secondJobId))
     }
   }
+
+  test("getJobIdsForGroup() with takeAsync()") {
+    sc = new SparkContext("local", "test", new SparkConf(false))
+    sc.setJobGroup("my-job-group2", "description")
+    sc.statusTracker.getJobIdsForGroup("my-job-group2") should be (Seq.empty)
+    val firstJobFuture = sc.parallelize(1 to 1000, 1).takeAsync(1)
+    val firstJobId = eventually(timeout(10 seconds)) {
+      firstJobFuture.jobIds.head
+    }
+    eventually(timeout(10 seconds)) {
+      sc.statusTracker.getJobIdsForGroup("my-job-group2") should be (Seq(firstJobId))
+    }
+  }
 }
