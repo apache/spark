bcc7ab7c1e74eace20536485b4d9683560ae733e
Sean Owen <sowen@cloudera.com>
2015-11-25 14:03:07 +0000

    Update JPMML pmml-model to 1.2.7

--- mllib/src/main/scala/org/apache/spark/mllib/pmml/export/PMMLModelExport.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/pmml/export/PMMLModelExport.scala
@@ -37,12 +37,12 @@ private[mllib] trait PMMLModelExport {
 
   private def setHeader(pmml: PMML): Unit = {
     val version = getClass.getPackage.getImplementationVersion
-    val app = new Application().withName("Apache Spark MLlib").withVersion(version)
+    val app = new Application("Apache Spark MLlib").setVersion(version)
     val timestamp = new Timestamp()
-      .withContent(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date()))
+      .addContent(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(new Date()))
     val header = new Header()
-      .withApplication(app)
-      .withTimestamp(timestamp)
+      .setApplication(app)
+      .setTimestamp(timestamp)
     pmml.setHeader(header)
   }
 }
