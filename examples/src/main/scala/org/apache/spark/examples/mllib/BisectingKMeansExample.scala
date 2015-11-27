86f608546bfac909eb4e99bf49816596767feac8
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2015-11-27 10:22:57 +0900

    Use the fixed paramters

--- examples/src/main/scala/org/apache/spark/examples/mllib/BisectingKMeansExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/BisectingKMeansExample.scala
@@ -18,8 +18,6 @@
 package org.apache.spark.examples.mllib
 
 // scalastyle:off println
-import scopt.OptionParser
-
 import org.apache.spark.mllib.clustering.BisectingKMeans
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -29,54 +27,30 @@ import org.apache.spark.{SparkConf, SparkContext}
  *
  * Run with
  * {{{
- * bin/run-example mllib.BisectingKMeansExample [option] <input>
+ * bin/run-example mllib.BisectingKMeansExample
  * }}}
  */
 object BisectingKMeansExample {
 
-  case class Params(
-    input: String = null,
-    k: Int = 4) extends AbstractParams[Params]
-
   def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("mllib.BisectingKMeansExample")
+    val sc = new SparkContext(sparkConf)
 
-    val defaultParams = Params()
+    // Loads and parses data
+    def parse(line: String): Vector = Vectors.dense(line.split(" ").map(_.toDouble))
+    val data = sc.textFile("data/mllib/kmeans_data.txt").map(parse).cache()
 
-    def parse(line: String): Vector = {
-      Vectors.dense(line.split(",").map(_.toDouble))
+    // Clustering the data into 6 clusters by BisectingKMeans.
+    val bkm = new BisectingKMeans().setK(6)
+    val model = bkm.run(data)
+
+    // Show the compute cost and the cluster centers
+    println(s"Compute Cost: ${model.computeCost(data)}")
+    model.clusterCenters.zipWithIndex.foreach { case (center, idx) =>
+      println(s"Cluster Center ${idx}: ${center}")
     }
 
-    val parser = new OptionParser[Params]("DenseKMeans") {
-      head("BisectingKMeansExample: an example bisecting k-means app.")
-      opt[Int]('k', "k")
-        .required()
-        .text(s"number of clusters, required")
-        .action((x, c) => c.copy(k = x))
-      arg[String]("<input>")
-        .text("CSV file paths")
-        .required()
-        .action((x, c) => c.copy(input = x))
-    }
-
-    parser.parse(args, defaultParams).map { params =>
-      val sparkConf = new SparkConf().setAppName("mllib.BisectingKMeansExample")
-      val sc = new SparkContext(sparkConf)
-
-      val data = sc.textFile(params.input).map(parse).cache()
-      val k = params.k
-
-      val bkm = new BisectingKMeans().setK(k)
-      val model = bkm.run(data)
-
-      println(s"Compute Cost: ${model.computeCost(data)}")
-      model.clusterCenters.zipWithIndex.foreach { case (center, idx) =>
-        println(s"Cluster Center ${idx}: ${center}")
-      }
-
-      sc.stop()
-    }.getOrElse {
-      sys.exit(1)
-    }
+    sc.stop()
   }
 }
 // scalastyle:on println
