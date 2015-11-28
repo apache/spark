fdbe2a7828a08e58d104f7f27b2ded4a4dc3192a
Antonio Murgia <antonio.murgia2@studio.unibo.it>
2015-11-28 10:36:35 +0100

    [SPARK-11934] Fix Word2VecSuite code style

--- mllib/src/test/scala/org/apache/spark/mllib/feature/Word2VecSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/feature/Word2VecSuite.scala
@@ -94,10 +94,8 @@ class Word2VecSuite extends SparkFunSuite with MLlibTestSparkContext {
   }
 
   test("big model load / save") {
-    val word2VecMap = Map(
-      (0 to 9000)
-        .map(i => s"$i" -> Array.fill(1000)(0.1f)):_*
-    )
+    //create a model bigger than 32MB since 9000 * 1000 * 4 > 2^25
+    val word2VecMap = Map((0 to 9000).map(i => s"$i" -> Array.fill(1000)(0.1f)): _*)
     val model = new Word2VecModel(word2VecMap)
 
     val tempDir = Utils.createTempDir()
