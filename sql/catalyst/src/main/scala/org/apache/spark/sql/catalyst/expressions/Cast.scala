74a2306763161fc04c9d3e7de186a6b31617faf4
Wenchen Fan <wenchen@databricks.com>
2015-12-01 10:24:53 -0800

    [SPARK-11856][SQL] add type cast if the real type is different but compatible with encoder schema
    When we build the `fromRowExpression` for an encoder, we set up a lot of "unresolved" stuff and lost the required data type, which may lead to runtime error if the real type doesn't match the encoder's schema.
    For example, we build an encoder for `case class Data(a: Int, b: String)` and the real type is `[a: int, b: long]`, then we will hit runtime error and say that we can't construct class `Data` with int and long, because we lost the information that `b` should be a string.
    
    Author: Wenchen Fan <wenchen@databricks.com>
    
    Closes #9840 from cloud-fan/err-msg.
    
    (cherry picked from commit 9df24624afedd993a39ab46c8211ae153aedef1a)
    Signed-off-by: Michael Armbrust <michael@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Cast.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Cast.scala
@@ -915,3 +915,12 @@ case class Cast(child: Expression, dataType: DataType)
       """
   }
 }
+
+/**
+ * Cast the child expression to the target data type, but will throw error if the cast might
+ * truncate, e.g. long -> int, timestamp -> data.
+ */
+case class UpCast(child: Expression, dataType: DataType, walkedTypePath: Seq[String])
+  extends UnaryExpression with Unevaluable {
+  override lazy val resolved = false
+}
