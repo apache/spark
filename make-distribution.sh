a46c68b6ebcd257cebe42c4e255a8a1a9f831787
Josh Rosen <joshrosen@databricks.com>
2015-12-01 11:49:20 -0800

    [SPARK-12065] Upgrade Tachyon from 0.8.1 to 0.8.2
    This commit upgrades the Tachyon dependency from 0.8.1 to 0.8.2.
    
    Author: Josh Rosen <joshrosen@databricks.com>
    
    Closes #10054 from JoshRosen/upgrade-to-tachyon-0.8.2.

--- make-distribution.sh
+++ make-distribution.sh
@@ -33,7 +33,7 @@ SPARK_HOME="$(cd "`dirname "$0"`"; pwd)"
 DISTDIR="$SPARK_HOME/dist"
 
 SPARK_TACHYON=false
-TACHYON_VERSION="0.8.1"
+TACHYON_VERSION="0.8.2"
 TACHYON_TGZ="tachyon-${TACHYON_VERSION}-bin.tar.gz"
 TACHYON_URL="http://tachyon-project.org/downloads/files/${TACHYON_VERSION}/${TACHYON_TGZ}"
 
