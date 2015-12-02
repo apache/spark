2cc1556584e6e9f4efe2b25253a9432601f6abad
Xin Ren <iamshrek@126.com>
2015-12-01 16:55:52 -0800

    [SPARK-11155] style correction: File line length exceeds 100 characters

--- core/src/main/scala/org/apache/spark/status/api/v1/AllStagesResource.scala
+++ core/src/main/scala/org/apache/spark/status/api/v1/AllStagesResource.scala
@@ -59,7 +59,8 @@ private[v1] object AllStagesResource {
       stageUiData: StageUIData,
       includeDetails: Boolean): StageData = {
 
-    val firstTaskLaunchedTime = stageUiData.taskData.values.map(_.taskInfo.launchTime).filter(_ > 0).min
+    val firstTaskLaunchedTime =
+      stageUiData.taskData.values.map(_.taskInfo.launchTime).filter(_ > 0).min
 
     val taskData = if (includeDetails) {
       Some(stageUiData.taskData.map { case (k, v) => k -> convertTaskData(v) } )
