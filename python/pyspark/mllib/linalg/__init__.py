cff9e4785ffffce43870098a9a63b05924c29d6b
Holden Karau <holden@us.ibm.com>
2015-12-02 10:03:05 -0800

    Add parsing from JSON (same format as from Scala impl)

--- python/pyspark/mllib/linalg/__init__.py
+++ python/pyspark/mllib/linalg/__init__.py
@@ -26,6 +26,7 @@ SciPy is available in their environment.
 import sys
 import array
 import struct
+import json
 
 if sys.version >= '3':
     basestring = str
@@ -892,6 +893,25 @@ class Vectors(object):
         else:
             raise ValueError(
                 "Cannot find tokens '[' or '(' from the input string.")
+
+    @staticmethod
+    def fromJson(s):
+        """Parse a JSON representation back into the Vector.
+
+        >>> Vectors.fromJson("{\\"type\\":0,\\"size\\":2,\\"indices\\":[1],\\"values\\":[2.0]}")
+        SparseVector(2, {1: 2.0})
+        >>> Vectors.fromJson("{\\"type\\":1,\\"values\\":[1.0,2.0,43.0,4.0]}")
+        DenseVector([1.0, 2.0, 43.0, 4.0])
+        """
+        # Parse the input
+        parsed = json.loads(s)
+        vectorType = parsed["type"]
+        if vectorType == 0:
+            return Vectors.sparse(parsed["size"], zip(parsed["indices"], parsed["values"]))
+        elif vectorType == 1:
+            return Vectors.dense(parsed["values"])
+        else:
+            raise ValueError("do not regonize Vector type %d" % vectorType)
 
     @staticmethod
     def zeros(size):
