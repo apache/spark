f47d8cc9adb72b43ab8795246aca8ed5df55aa67
Jeff Zhang <zjffdu@apache.org>
2015-12-03 13:20:07 +0800

    change back to 1.7.0

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
@@ -162,7 +162,7 @@ final class DataFrameWriter private[sql](df: DataFrame) {
   /*
    * Specify the compression codec when saving it on hdfs
    *
-   * @since 1.6.0
+   * @since 1.7.0
    */
   def compress(codec: Class[_ <: CompressionCodec]): DataFrameWriter = {
     this.extraOptions += ("compression.codec" -> codec.getCanonicalName)
