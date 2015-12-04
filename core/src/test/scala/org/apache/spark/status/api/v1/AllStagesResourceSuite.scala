a3417962ebb91d85614fdee73d5ef27a43877528
Xin Ren <iamshrek@126.com>
2015-12-04 14:05:46 -0800

    [SPARK-11155] test 'firstTaskLaunchedTime' with 3 cases

--- core/src/test/scala/org/apache/spark/status/api/v1/AllStagesResourceSuite.scala
+++ core/src/test/scala/org/apache/spark/status/api/v1/AllStagesResourceSuite.scala
@@ -17,76 +17,89 @@
 
 package org.apache.spark.status.api.v1
 
-import org.scalatest.Matchers
+import java.util.Date
+import scala.collection.mutable.HashMap
+
 import org.apache.spark.SparkFunSuite
-import org.apache.spark.scheduler.StageInfo
-import org.apache.spark.ui.jobs.UIData.StageUIData
+import org.apache.spark.scheduler.{StageInfo, TaskInfo, TaskLocality}
+import org.apache.spark.ui.jobs.UIData.{StageUIData, TaskUIData}
 
-class AllStagesResourceSuite extends SparkFunSuite with Matchers {
 
-  test("test test") {
-    val s = "abc"
-    assert(s === "abc")
+class AllStagesResourceSuite extends SparkFunSuite {
+
+  test("test firstTaskLaunchedTime, there are no tasks") {
+
+    val status = StageStatus.PENDING
+    val stageInfo = new StageInfo(
+      1, 1, "stage 1", 10, Seq.empty, Seq.empty, "details abc", Seq.empty)
+    val includeDetails = false
+
+    val noTasks = new StageUIData()
+
+    var actual = AllStagesResource.stageUiToStageData(
+      status, stageInfo, noTasks, includeDetails)
+
+    assert(actual.firstTaskLaunchedTime == None)
   }
 
-  test("test class instantiation") {
+  test("test firstTaskLaunchedTime, there are tasks but none launched") {
+
+    val status = StageStatus.ACTIVE
+    val stageInfo = new StageInfo(
+      1, 1, "stage 1", 10, Seq.empty, Seq.empty, "details abc", Seq.empty)
+    val includeDetails = false
+
+    // generate some tasks, launched time is minus
+    val taskNoLaunched1 = new TaskUIData(
+      new TaskInfo(1, 1, 1, -100, "", "", TaskLocality.ANY, false), None, None)
+    val taskNoLaunched2 = new TaskUIData(
+      new TaskInfo(1, 1, 1, -200, "", "", TaskLocality.ANY, false), None, None)
+    val taskNoLaunched3 = new TaskUIData(
+      new TaskInfo(1, 1, 1, -300, "", "", TaskLocality.ANY, false), None, None)
+
+    // construct hashmap
+    var taskDataNoLaunched = new HashMap[Long, TaskUIData]
+    taskDataNoLaunched.put(1, taskNoLaunched1)
+    taskDataNoLaunched.put(2, taskNoLaunched2)
+    taskDataNoLaunched.put(3, taskNoLaunched3)
+
+    val tasksNoLaunched = new StageUIData()
+    tasksNoLaunched.taskData = taskDataNoLaunched
+
+    val actual = AllStagesResource.stageUiToStageData(
+      status, stageInfo, tasksNoLaunched, includeDetails)
+
+    assert(actual.firstTaskLaunchedTime == None)
+  }
+
+  test("test firstTaskLaunchedTime, there are tasks and some launched") {
+
     val status = StageStatus.COMPLETE
     val stageInfo = new StageInfo(
       1, 1, "stage 1", 10, Seq.empty, Seq.empty, "details abc", Seq.empty)
-    val stageUiData = new StageUIData()
     val includeDetails = false
 
+    // generate some tasks, min lauched time 1449255596000L
+    val taskLaunched1 = new TaskUIData(
+      new TaskInfo(1, 1, 1, 1449255596000L, "", "", TaskLocality.ANY, false), None, None)
+    val taskLaunched2 = new TaskUIData(
+      new TaskInfo(1, 1, 1, 1449255597000L, "", "", TaskLocality.ANY, false), None, None)
+    val taskLaunched3 = new TaskUIData(
+      new TaskInfo(1, 1, 1, -100, "", "", TaskLocality.ANY, false), None, None)
+
+    // construct hashmap
+    var taskDataLaunched = new HashMap[Long, TaskUIData]
+    taskDataLaunched.put(1, taskLaunched1)
+    taskDataLaunched.put(2, taskLaunched2)
+    taskDataLaunched.put(3, taskLaunched3)
+
+    val tasksLaunched = new StageUIData()
+    tasksLaunched.taskData = taskDataLaunched
+
     val actual = AllStagesResource.stageUiToStageData(
-      status, stageInfo, stageUiData, includeDetails)
-    val expected = new StageData(status, 1, 1, 0, 0, 0, 0, None, None, None,
-      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "stage 1", "details abc", "", Seq.empty, None, None)
+      status, stageInfo, tasksLaunched, includeDetails)
 
-    assert(actual.status == expected.status)
-    assert(actual.stageId == expected.stageId)
-    assert(actual.attemptId == expected.attemptId)
-    assert(actual.numActiveTasks == expected.numActiveTasks)
-    assert(actual.numCompleteTasks == expected.numCompleteTasks)
-    assert(actual.executorRunTime == expected.executorRunTime)
-    assert(actual.submissionTime == expected.submissionTime)
-    assert(actual.firstTaskLaunchedTime == expected.firstTaskLaunchedTime)
-    assert(actual.completionTime == expected.completionTime)
-    assert(actual.inputBytes == expected.inputBytes)
-    assert(actual.inputRecords == expected.inputRecords)
-    assert(actual.outputBytes == expected.outputBytes)
-    assert(actual.outputRecords == expected.outputRecords)
-    assert(actual.shuffleReadBytes == expected.shuffleReadBytes)
-    assert(actual.shuffleReadRecords == expected.shuffleReadRecords)
-    assert(actual.shuffleWriteBytes == expected.shuffleWriteBytes)
-    assert(actual.shuffleWriteRecords == expected.shuffleWriteRecords)
-    assert(actual.memoryBytesSpilled == expected.memoryBytesSpilled)
-    assert(actual.diskBytesSpilled == expected.diskBytesSpilled)
-    assert(actual.name == expected.name)
-    assert(actual.details == expected.details)
-    assert(actual.schedulingPool == expected.schedulingPool)
-    assert(actual.accumulatorUpdates == expected.accumulatorUpdates)
-    assert(actual.tasks == expected.tasks)
-    assert(actual.executorSummary == expected.executorSummary)
-
-
-//    println(actual.toString)
-//    println(expected.toString)
-
-//    actual should equal(expected)
-
-
-//    val expectedDup = new StageData(status, 1, 1, 0, 0, 0, 0, None, None, None,
-//      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "stage 1", "details abc", "", Seq.empty, None, None)
-
-    // below is false
-    // expected should equal (expectedDup)
-
-    // below is false
-    // assert(expectedDup equals expected)
-
-    // below is false
-    // assert(expectedDup == expected)
-
+    assert(actual.firstTaskLaunchedTime == Some(new Date(1449255596000L)))
   }
 
 }
-
