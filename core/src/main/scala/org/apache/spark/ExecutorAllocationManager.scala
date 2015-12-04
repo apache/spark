e04459c6c056bed936fb9be0e9a522c051b7b6c6
meiyoula <1039320815@qq.com>
2015-12-04 10:26:22 +0800

    fix scalastyle

--- core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
+++ core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
@@ -370,7 +370,7 @@ private[spark] class ExecutorAllocationManager(
     } else {
       logWarning(
         s"Unable to reach the cluster manager to request $numExecutorsTarget total executors!")
-      numExecutorsTarget = oldNumExecutorsTarget     
+      numExecutorsTarget = oldNumExecutorsTarget
       0
     }
   }
