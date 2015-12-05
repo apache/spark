9e43e56b260fa54a2c467d1bd8e17be3a9a987ab
Thoralf Gutierrez <thoralf.gutierrez@realimpactanalytics.com>
2015-12-05 22:56:52 +0100

    remove leading zeros for python3 compatibility

--- tests/models.py
+++ tests/models.py
@@ -7,7 +7,7 @@ from airflow import models
 class DagRunTest(unittest.TestCase):
     def test_id_for_date(self):
         run_id = models.DagRun.id_for_date(
-            datetime.datetime(2015, 01, 02, 03, 04, 05, 06, None))
+            datetime.datetime(2015, 1, 2, 3, 4, 5, 6, None))
         assert run_id == 'scheduled__2015-01-02T03:04:05', (
             'Generated run_id did not match expectations: {0}'.format(run_id))
 
