db2eca8c9a5da66c0d244dce53084dae5a7e583b
tedyu <yuzhihong@gmail.com>
2015-12-07 14:58:09 -0800

    [SPARK-11884] Drop multiple columns in the DataFrame API
    See the thread Ben started:
    http://search-hadoop.com/m/q3RTtveEuhjsr7g/
    
    This PR adds drop() method to DataFrame which accepts multiple column names
    
    Author: tedyu <yuzhihong@gmail.com>
    
    Closes #9862 from ted-yu/master.

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -378,6 +378,13 @@ class DataFrameSuite extends QueryTest with SharedSQLContext {
     assert(df.schema.map(_.name) === Seq("value"))
   }
 
+  test("drop columns using drop") {
+    val src = Seq((0, 2, 3)).toDF("a", "b", "c")
+    val df = src.drop("a", "b")
+    checkAnswer(df, Row(3))
+    assert(df.schema.map(_.name) === Seq("c"))
+  }
+
   test("drop unknown column (no-op)") {
     val df = testData.drop("random")
     checkAnswer(
