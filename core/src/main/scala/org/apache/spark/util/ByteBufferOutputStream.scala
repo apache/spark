4371281a3125c82645e42dfe8407d322364fbf9a
tedyu <yuzhihong@gmail.com>
2015-12-08 10:01:44 -0800

    [SPARK-12074] Avoid memory copy involving ByteBuffer.wrap(ByteArrayOutputStream.toByteArray)
    SPARK-12060 fixed JavaSerializerInstance.serialize
    This PR applies the same technique on two other classes.
    
    zsxwing
    
    Author: tedyu <yuzhihong@gmail.com>
    
    Closes #10177 from tedyu/master.

--- core/src/main/scala/org/apache/spark/util/ByteBufferOutputStream.scala
+++ core/src/main/scala/org/apache/spark/util/ByteBufferOutputStream.scala
@@ -23,7 +23,9 @@ import java.nio.ByteBuffer
 /**
  * Provide a zero-copy way to convert data in ByteArrayOutputStream to ByteBuffer
  */
-private[spark] class ByteBufferOutputStream extends ByteArrayOutputStream {
+private[spark] class ByteBufferOutputStream(capacity: Int) extends ByteArrayOutputStream(capacity) {
+
+  def this() = this(32)
 
   def toByteBuffer: ByteBuffer = {
     return ByteBuffer.wrap(buf, 0, count)
