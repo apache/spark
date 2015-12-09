f6fb4066e52baa6bf6c3680db6cdc967330a110c
Josh Rosen <joshrosen@databricks.com>
2015-12-08 16:46:20 -0800

    Avoid double-add to evictedBlocks.

--- core/src/test/scala/org/apache/spark/memory/MemoryManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/memory/MemoryManagerSuite.scala
@@ -103,8 +103,14 @@ private[memory] trait MemoryManagerSuite extends SparkFunSuite with BeforeAndAft
           mm.releaseStorageMemory(numBytesToFree)
           args.last.asInstanceOf[mutable.Buffer[(BlockId, BlockStatus)]].append(
             (null, BlockStatus(StorageLevel.MEMORY_ONLY, numBytesToFree, 0L, 0L)))
-          evictedBlocks.append(
-            (null, BlockStatus(StorageLevel.MEMORY_ONLY, numBytesToFree, 0L, 0L)))
+          // We need to add this call so that that the suite-level `evictedBlocks` is updated when
+          // execution evicts storage; in that case, args.last will not be equal to evictedBlocks
+          // because it will be a temporary buffer created inside of the MemoryManager rather than
+          // being passed in by the test code.
+          if (!(evictedBlocks eq args.last)) {
+            evictedBlocks.append(
+              (null, BlockStatus(StorageLevel.MEMORY_ONLY, numBytesToFree, 0L, 0L)))
+          }
           true
         } else {
           // No blocks were evicted because eviction would not free enough space.
