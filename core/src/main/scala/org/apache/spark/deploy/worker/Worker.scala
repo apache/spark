6659b4eb32b484db3d2d1dcd13be14d8305b2727
Shixiong Zhu <shixiong@databricks.com>
2015-12-12 21:58:55 -0800

    [SPARK-12267][CORE] Store the remote RpcEnv address to send the correct disconnetion message
    Author: Shixiong Zhu <shixiong@databricks.com>
    
    Closes #10261 from zsxwing/SPARK-12267.

--- core/src/main/scala/org/apache/spark/deploy/worker/Worker.scala
+++ core/src/main/scala/org/apache/spark/deploy/worker/Worker.scala
@@ -690,7 +690,7 @@ private[deploy] object Worker extends Logging {
     val conf = new SparkConf
     val args = new WorkerArguments(argStrings, conf)
     val rpcEnv = startRpcEnvAndEndpoint(args.host, args.port, args.webUiPort, args.cores,
-      args.memory, args.masters, args.workDir)
+      args.memory, args.masters, args.workDir, conf = conf)
     rpcEnv.awaitTermination()
   }
 
