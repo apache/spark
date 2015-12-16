0f6936b5f1c9b0be1c33b98ffb62a72ae0c3e2a8
hyukjinkwon <gurwls223@gmail.com>
2015-12-15 22:22:49 -0800

    [SPARK-12249][SQL] JDBC non-equality comparison operator not pushed down.
    https://issues.apache.org/jira/browse/SPARK-12249
    Currently `!=` operator is not pushed down correctly.
    I simply added a case for this.
    
    Author: hyukjinkwon <gurwls223@gmail.com>
    
    Closes #10233 from HyukjinKwon/SPARK-12249.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JDBCRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JDBCRDD.scala
@@ -281,6 +281,7 @@ private[sql] class JDBCRDD(
    */
   private def compileFilter(f: Filter): String = f match {
     case EqualTo(attr, value) => s"$attr = ${compileValue(value)}"
+    case Not(EqualTo(attr, value)) => s"$attr != ${compileValue(value)}"
     case LessThan(attr, value) => s"$attr < ${compileValue(value)}"
     case GreaterThan(attr, value) => s"$attr > ${compileValue(value)}"
     case LessThanOrEqual(attr, value) => s"$attr <= ${compileValue(value)}"
