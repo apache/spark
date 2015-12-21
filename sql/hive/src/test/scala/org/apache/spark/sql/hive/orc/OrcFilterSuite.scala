406e5ecae76d5686ecccc1541c6fe42751214e28
hyukjinkwon <gurwls223@gmail.com>
2015-12-21 17:30:21 +0900

    Test filter creation with string expressions.

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/orc/OrcFilterSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/orc/OrcFilterSuite.scala
@@ -17,10 +17,6 @@
 
 package org.apache.spark.sql.hive.orc
 
-import scala.collection.JavaConverters._
-
-import org.apache.hadoop.hive.ql.io.sarg.PredicateLeaf
-
 import org.apache.spark.sql.{Column, DataFrame, QueryTest}
 import org.apache.spark.sql.catalyst.dsl.expressions._
 import org.apache.spark.sql.catalyst.expressions._
@@ -31,10 +27,42 @@ import org.apache.spark.sql.execution.datasources.{DataSourceStrategy, LogicalRe
  * A test suite that tests ORC filter API based filter pushdown optimization.
  */
 class OrcFilterSuite extends QueryTest with OrcTest {
+  // Because `ExpressionTree` is not accessible at Hive 1.2.x, this should be checked
+  // in string form in order to check filter creation including logical expressions
+  // such as `and`, `or` or `not`. So, this test uses `SearchArgument.toString()`
+  // to produce string expression and then compare it to given templates below.
+  // This might have to be changed after Hive version is upgraded.
+  val isNullTmpl = """leaf-0 = (IS_NULL _1)
+                 |expr = leaf-0""".stripMargin.trim
+  val isNotNullTmpl = """leaf-0 = (IS_NULL _1)
+                    |expr = (not leaf-0)""".stripMargin.trim
+  val equalsTmpl = """leaf-0 = (EQUALS _1 %s)
+                     |expr = leaf-0""".stripMargin.trim
+  val notEqualsTmpl = """leaf-0 = (EQUALS _1 %s)
+                        |expr = (not leaf-0)""".stripMargin.trim
+  val nullSafeEqualsTmpl = """leaf-0 = (NULL_SAFE_EQUALS _1 %s)
+                            |expr = leaf-0""".stripMargin.trim
+  val lessThenTmpl = """leaf-0 = (LESS_THAN _1 %s)
+                       |expr = leaf-0""".stripMargin.trim
+  val greaterThenTmpl = """leaf-0 = (LESS_THAN_EQUALS _1 %s)
+                          |expr = (not leaf-0)""".stripMargin.trim
+  val lessThenEqualsTmpl = """leaf-0 = (LESS_THAN_EQUALS _1 %s)
+                             |expr = leaf-0""".stripMargin.trim
+  val greaterThenEqualsTmpl = """leaf-0 = (LESS_THAN _1 %s)
+                                |expr = (not leaf-0)""".stripMargin.trim
+  val notLessThen = """leaf-0 = (LESS_THAN _1 %s)
+                      |expr = (not leaf-0)""".stripMargin.trim
+  val andLessThenGreaterThenTmpl = """leaf-0 = (LESS_THAN _1 %s)
+                                     |leaf-1 = (LESS_THAN_EQUALS _1 %s)
+                                     |expr = (or leaf-0 (not leaf-1))""".stripMargin.trim
+  val orLessThenGreaterThenTmpl = """leaf-0 = (LESS_THAN _1 %s)
+                                    |leaf-1 = (LESS_THAN_EQUALS _1 %s)
+                                    |expr = (and leaf-0 (not leaf-1))""".stripMargin.trim
+
   private def checkFilterPredicate(
                                     df: DataFrame,
                                     predicate: Predicate,
-                                    filterOperator: PredicateLeaf.Operator): Unit = {
+                                    stringExpr: String): Unit = {
     val output = predicate.collect { case a: Attribute => a }.distinct
     val query = df
       .select(output.map(e => Column(e)): _*)
@@ -55,135 +83,155 @@ class OrcFilterSuite extends QueryTest with OrcTest {
     val maybeFilter = OrcFilters.createFilter(selectedFilters.toArray)
     assert(maybeFilter.isDefined, s"Couldn't generate filter predicate for $selectedFilters")
     maybeFilter.foreach { f =>
-      val operator = f.getLeaves.asScala.head.getOperator
-      assert(operator === filterOperator)
+      assert(f.toString == stringExpr)
     }
   }
 
   private def checkFilterPredicate
-  (predicate: Predicate, filterOperator: PredicateLeaf.Operator)
+  (predicate: Predicate, stringExpr: String)
   (implicit df: DataFrame): Unit = {
-    checkFilterPredicate(df, predicate, filterOperator)
+    checkFilterPredicate(df, predicate, stringExpr)
   }
 
   test("filter pushdown - boolean") {
     withOrcDataFrame((true :: false :: Nil).map(b => Tuple1.apply(Option(b)))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
     }
   }
 
   test("filter pushdown - integer") {
     withOrcDataFrame((1 to 4).map(i => Tuple1(Option(i)))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
 
-      checkFilterPredicate('_1 === 1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate('_1 <=> 1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
+      checkFilterPredicate('_1 === 1, equalsTmpl.format(1))
+      checkFilterPredicate('_1 <=> 1, nullSafeEqualsTmpl.format(1))
+      checkFilterPredicate('_1 !== 1, notEqualsTmpl.format(1))
 
-      checkFilterPredicate('_1 < 2, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate('_1 > 3, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 <= 1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 >= 4, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate('_1 < 2, lessThenTmpl.format(2))
+      checkFilterPredicate('_1 > 3, greaterThenTmpl.format(3))
+      checkFilterPredicate('_1 <= 1, lessThenEqualsTmpl.format(1))
+      checkFilterPredicate('_1 >= 4, greaterThenEqualsTmpl.format(4))
 
-      checkFilterPredicate(Literal(1) === '_1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate(Literal(1) <=> '_1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
-      checkFilterPredicate(Literal(2) > '_1, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate(Literal(3) < '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(1) >= '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(4) <= '_1, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(Literal(1) === '_1, equalsTmpl.format(1))
+      checkFilterPredicate(Literal(1) <=> '_1, nullSafeEqualsTmpl.format(1))
+      checkFilterPredicate(Literal(2) > '_1, lessThenTmpl.format(2))
+      checkFilterPredicate(Literal(3) < '_1, greaterThenTmpl.format(3))
+      checkFilterPredicate(Literal(1) >= '_1, lessThenEqualsTmpl.format(1))
+      checkFilterPredicate(Literal(4) <= '_1, greaterThenEqualsTmpl.format(4))
 
-      checkFilterPredicate(!('_1 < 4), PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(!('_1 < 4), notLessThen.format(4))
+      checkFilterPredicate('_1 < 2 || '_1 > 3, andLessThenGreaterThenTmpl.format(2, 3))
+      checkFilterPredicate('_1 < 2 && '_1 > 3, orLessThenGreaterThenTmpl.format(2, 3))
     }
   }
 
   test("filter pushdown - long") {
     withOrcDataFrame((1 to 4).map(i => Tuple1(Option(i.toLong)))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
 
-      checkFilterPredicate('_1 === 1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate('_1 <=> 1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
+      checkFilterPredicate('_1 === 1, equalsTmpl.format(1))
+      checkFilterPredicate('_1 <=> 1, nullSafeEqualsTmpl.format(1))
+      checkFilterPredicate('_1 !== 1, notEqualsTmpl.format(1))
 
-      checkFilterPredicate('_1 < 2, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate('_1 > 3, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 <= 1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 >= 4, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate('_1 < 2, lessThenTmpl.format(2))
+      checkFilterPredicate('_1 > 3, greaterThenTmpl.format(3))
+      checkFilterPredicate('_1 <= 1, lessThenEqualsTmpl.format(1))
+      checkFilterPredicate('_1 >= 4, greaterThenEqualsTmpl.format(4))
 
-      checkFilterPredicate(Literal(1) === '_1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate(Literal(1) <=> '_1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
-      checkFilterPredicate(Literal(2) > '_1, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate(Literal(3) < '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(1) >= '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(4) <= '_1, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(Literal(1) === '_1, equalsTmpl.format(1))
+      checkFilterPredicate(Literal(1) <=> '_1, nullSafeEqualsTmpl.format(1))
+      checkFilterPredicate(Literal(2) > '_1, lessThenTmpl.format(2))
+      checkFilterPredicate(Literal(3) < '_1, greaterThenTmpl.format(3))
+      checkFilterPredicate(Literal(1) >= '_1, lessThenEqualsTmpl.format(1))
+      checkFilterPredicate(Literal(4) <= '_1, greaterThenEqualsTmpl.format(4))
 
-      checkFilterPredicate(!('_1 < 4), PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(!('_1 < 4), notLessThen.format(4))
+      checkFilterPredicate('_1 < 2 || '_1 > 3, andLessThenGreaterThenTmpl.format(2, 3))
+      checkFilterPredicate('_1 < 2 && '_1 > 3, orLessThenGreaterThenTmpl.format(2, 3))
     }
   }
 
   test("filter pushdown - float") {
     withOrcDataFrame((1 to 4).map(i => Tuple1(Option(i.toFloat)))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
 
-      checkFilterPredicate('_1 === 1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate('_1 <=> 1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
+      checkFilterPredicate('_1 === 1, equalsTmpl.format(1.0))
+      checkFilterPredicate('_1 <=> 1, nullSafeEqualsTmpl.format(1.0))
+      checkFilterPredicate('_1 !== 1, notEqualsTmpl.format(1.0))
 
-      checkFilterPredicate('_1 < 2, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate('_1 > 3, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 <= 1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 >= 4, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate('_1 < 2, lessThenTmpl.format(2.0))
+      checkFilterPredicate('_1 > 3, greaterThenTmpl.format(3.0))
+      checkFilterPredicate('_1 <= 1, lessThenEqualsTmpl.format(1.0))
+      checkFilterPredicate('_1 >= 4, greaterThenEqualsTmpl.format(4.0))
 
-      checkFilterPredicate(Literal(1) === '_1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate(Literal(1) <=> '_1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
-      checkFilterPredicate(Literal(2) > '_1, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate(Literal(3) < '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(1) >= '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(4) <= '_1, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(Literal(1) === '_1, equalsTmpl.format(1.0))
+      checkFilterPredicate(Literal(1) <=> '_1, nullSafeEqualsTmpl.format(1.0))
+      checkFilterPredicate(Literal(2) > '_1, lessThenTmpl.format(2.0))
+      checkFilterPredicate(Literal(3) < '_1, greaterThenTmpl.format(3.0))
+      checkFilterPredicate(Literal(1) >= '_1, lessThenEqualsTmpl.format(1.0))
+      checkFilterPredicate(Literal(4) <= '_1, greaterThenEqualsTmpl.format(4.0))
 
-      checkFilterPredicate(!('_1 < 4), PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(!('_1 < 4), notLessThen.format(4.0))
+      checkFilterPredicate('_1 < 2 || '_1 > 3, andLessThenGreaterThenTmpl.format(2.0, 3.0))
+      checkFilterPredicate('_1 < 2 && '_1 > 3, orLessThenGreaterThenTmpl.format(2.0, 3.0))
     }
   }
 
   test("filter pushdown - double") {
     withOrcDataFrame((1 to 4).map(i => Tuple1(Option(i.toDouble)))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
 
-      checkFilterPredicate('_1 === 1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate('_1 <=> 1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
+      checkFilterPredicate('_1 === 1, equalsTmpl.format(1.0))
+      checkFilterPredicate('_1 <=> 1, nullSafeEqualsTmpl.format(1.0))
+      checkFilterPredicate('_1 !== 1, notEqualsTmpl.format(1.0))
 
-      checkFilterPredicate('_1 < 2, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate('_1 > 3, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 <= 1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 >= 4, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate('_1 < 2, lessThenTmpl.format(2.0))
+      checkFilterPredicate('_1 > 3, greaterThenTmpl.format(3.0))
+      checkFilterPredicate('_1 <= 1, lessThenEqualsTmpl.format(1.0))
+      checkFilterPredicate('_1 >= 4, greaterThenEqualsTmpl.format(4.0))
 
-      checkFilterPredicate(Literal(1) === '_1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate(Literal(1) <=> '_1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
-      checkFilterPredicate(Literal(2) > '_1, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate(Literal(3) < '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(1) >= '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal(4) <= '_1, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(Literal(1) === '_1, equalsTmpl.format(1.0))
+      checkFilterPredicate(Literal(1) <=> '_1, nullSafeEqualsTmpl.format(1.0))
+      checkFilterPredicate(Literal(2) > '_1, lessThenTmpl.format(2.0))
+      checkFilterPredicate(Literal(3) < '_1, greaterThenTmpl.format(3.0))
+      checkFilterPredicate(Literal(1) >= '_1, lessThenEqualsTmpl.format(1.0))
+      checkFilterPredicate(Literal(4) <= '_1, greaterThenEqualsTmpl.format(4.0))
 
-      checkFilterPredicate(!('_1 < 4), PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(!('_1 < 4), notLessThen.format(4.0))
+      checkFilterPredicate('_1 < 2 || '_1 > 3, andLessThenGreaterThenTmpl.format(2.0, 3.0))
+      checkFilterPredicate('_1 < 2 && '_1 > 3, orLessThenGreaterThenTmpl.format(2.0, 3.0))
     }
   }
 
   test("filter pushdown - string") {
     withOrcDataFrame((1 to 4).map(i => Tuple1(i.toString))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
 
-      checkFilterPredicate('_1 === "1", PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate('_1 <=> "1", PredicateLeaf.Operator.NULL_SAFE_EQUALS)
+      checkFilterPredicate('_1 === "1", equalsTmpl.format("1"))
+      checkFilterPredicate('_1 <=> "1", nullSafeEqualsTmpl.format("1"))
+      checkFilterPredicate('_1 !== "1", notEqualsTmpl.format("1"))
 
-      checkFilterPredicate('_1 < "2", PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate('_1 > "3", PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 <= "1", PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate('_1 >= "4", PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate('_1 < "2", lessThenTmpl.format("2"))
+      checkFilterPredicate('_1 > "3", greaterThenTmpl.format("3"))
+      checkFilterPredicate('_1 <= "1", lessThenEqualsTmpl.format("1"))
+      checkFilterPredicate('_1 >= "4", greaterThenEqualsTmpl.format("4"))
 
-      checkFilterPredicate(Literal("1") === '_1, PredicateLeaf.Operator.EQUALS)
-      checkFilterPredicate(Literal("1") <=> '_1, PredicateLeaf.Operator.NULL_SAFE_EQUALS)
-      checkFilterPredicate(Literal("2") > '_1, PredicateLeaf.Operator.LESS_THAN)
-      checkFilterPredicate(Literal("3") < '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal("1") >= '_1, PredicateLeaf.Operator.LESS_THAN_EQUALS)
-      checkFilterPredicate(Literal("4") <= '_1, PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(Literal("1") === '_1, equalsTmpl.format("1"))
+      checkFilterPredicate(Literal("1") <=> '_1, nullSafeEqualsTmpl.format("1"))
+      checkFilterPredicate(Literal("2") > '_1, lessThenTmpl.format("2"))
+      checkFilterPredicate(Literal("3") < '_1, greaterThenTmpl.format("3"))
+      checkFilterPredicate(Literal("1") >= '_1, lessThenEqualsTmpl.format("1"))
+      checkFilterPredicate(Literal("4") <= '_1, greaterThenEqualsTmpl.format("4"))
 
-      checkFilterPredicate(!('_1 < "4"), PredicateLeaf.Operator.LESS_THAN)
+      checkFilterPredicate(!('_1 < "4"), notLessThen.format("4"))
+      checkFilterPredicate('_1 < "2" || '_1 > "3", andLessThenGreaterThenTmpl.format("2", "3"))
+      checkFilterPredicate('_1 < "2" && '_1 > "3", orLessThenGreaterThenTmpl.format("2", "3"))
     }
   }
 
@@ -193,7 +241,8 @@ class OrcFilterSuite extends QueryTest with OrcTest {
     }
 
     withOrcDataFrame((1 to 4).map(i => Tuple1(i.b))) { implicit df =>
-      checkFilterPredicate('_1.isNull, PredicateLeaf.Operator.IS_NULL)
+      checkFilterPredicate('_1.isNull, isNullTmpl)
+      checkFilterPredicate('_1.isNotNull, isNotNullTmpl)
     }
   }
 }
