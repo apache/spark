64f95ec3b307d44af42ee021f707904eae3a7076
pierre-borckmans <pierre.borckmans@realimpactanalytics.com>
2015-12-22 23:00:30 +0100

    FIX test sc => sparkContext

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameComplexTypeSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameComplexTypeSuite.scala
@@ -45,7 +45,7 @@ class DataFrameComplexTypeSuite extends QueryTest with SharedSQLContext {
   }
 
   test("SPARK-12477 accessing null element in array field") {
-    val df = sc.parallelize(Seq((Seq("val1", null, "val2"),
+    val df = sparkContext.parallelize(Seq((Seq("val1", null, "val2"),
       Seq(Some(1), None, Some(2))))).toDF("s", "i")
     val nullStringRow = df.selectExpr("s[1]").collect()(0)
     assert(nullStringRow == org.apache.spark.sql.Row(null))
