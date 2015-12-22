c324534ef0eded7efe9e4b9762c925a62a4c84b3
gatorsmile <gatorsmile@gmail.com>
2015-12-22 14:04:33 -0800

    reverted it back.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/LocalTableScan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/LocalTableScan.scala
@@ -19,7 +19,7 @@ package org.apache.spark.sql.execution
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeSet}
+import org.apache.spark.sql.catalyst.expressions.Attribute
 
 
 /**
