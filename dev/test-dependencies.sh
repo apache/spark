8507ffd589211e1a7b4a5bc44f2d7ff6fa80dacc
Josh Rosen <joshrosen@databricks.com>
2015-12-23 14:36:37 -0800

    Support multiple Hadoop profiles.

--- dev/test-dependencies.sh
+++ dev/test-dependencies.sh
@@ -27,6 +27,10 @@ cd "$FWDIR"
 # NOTE: These should match those in the release publishing script
 HADOOP2_MODULE_PROFILES="-Phive-thriftserver -Pyarn -Phive"
 MVN="build/mvn --force"
+HADOOP_PROFILES=(
+    hadoop-2.3
+    hadoop-2.4
+)
 
 # We'll switch the version to a temp. one, publish POMs using that new version, then switch back to
 # the old version. We need to do this because the `dependency:build-classpath` task needs to
@@ -47,41 +51,46 @@ trap reset_version EXIT
 
 $MVN -q versions:set -DnewVersion=$TEMP_VERSION -DgenerateBackupPoms=false > /dev/null
 
-echo "Performing Maven install"
-$MVN $HADOOP2_MODULE_PROFILES jar:jar install:install \
-  -pl '!assembly' \
-  -pl '!examples' \
-  -pl '!external/flume-assembly' \
-  -pl '!external/kafka-assembly' \
-  -pl '!external/twitter' \
-  -pl '!external/flume' \
-  -pl '!external/mqtt' \
-  -pl '!external/mqtt-assembly' \
-  -pl '!external/zeromq' \
-  -pl '!external/kafka' \
-  -pl '!tags' \
-  -DskipTests
+# Generate manifests for each Hadoop profile:
+for HADOOP_PROFILE in "${HADOOP_PROFILES[@]}"; do
+  echo "Performing Maven install for $HADOOP_PROFILE"
+  $MVN $HADOOP2_MODULE_PROFILES -P$HADOOP_PROFILE jar:jar install:install \
+    -pl '!assembly' \
+    -pl '!examples' \
+    -pl '!external/flume-assembly' \
+    -pl '!external/kafka-assembly' \
+    -pl '!external/twitter' \
+    -pl '!external/flume' \
+    -pl '!external/mqtt' \
+    -pl '!external/mqtt-assembly' \
+    -pl '!external/zeromq' \
+    -pl '!external/kafka' \
+    -pl '!tags' \
+    -DskipTests
 
-echo "Generating dependency manifest"
-$MVN $HADOOP2_MODULE_PROFILES -Phadoop-2.4 dependency:build-classpath -pl assembly \
-  | grep "Building Spark Project Assembly" -A 5 \
-  | tail -n 1 | tr ":" "\n" | rev | cut -d "/" -f 1 | rev | sort \
-  | grep -v spark > dev/pr-deps-hadoop24
-
+  echo "Generating dependency manifest for $HADOOP_PROFILE"
+  mkdir -p dev/pr-deps
+  $MVN $HADOOP2_MODULE_PROFILES -P$HADOOP_PROFILE dependency:build-classpath -pl assembly \
+    | grep "Building Spark Project Assembly" -A 5 \
+    | tail -n 1 | tr ":" "\n" | rev | cut -d "/" -f 1 | rev | sort \
+    | grep -v spark > dev/pr-deps/$HADOOP_PROFILE
+done
 
 if [[ $@ == **replace-manifest** ]]; then
-  echo "Replacing manifest and creating new file at dev/spark-deps"
-  mv dev/pr-deps-hadoop24 dev/spark-deps-hadoop24
+  echo "Replacing manifests and creating new files at dev/deps"
+  rm -rf dev/deps
+  mv dev/pr-deps dev/deps
   exit 0
 fi
 
-set +e
-dep_diff="$(diff dev/pr-deps-hadoop24 dev/spark-deps-hadoop24)"
-set -e
-
-if [ "$dep_diff" != "" ]; then
-  echo "Spark's published dependencies DO NOT MATCH the manifest file (dev/spark-deps)."
-  echo "To update the manifest file, run './dev/test-dependencies --replace-manifest'."
-  echo "$dep_diff"
-  exit 1
-fi
+for HADOOP_PROFILE in "${HADOOP_PROFILES[@]}"; do
+  set +e
+  dep_diff="$(diff dev/pr-deps/$HADOOP_PROFILE dev/deps/$HADOOP_PROFILE)"
+  set -e
+  if [ "$dep_diff" != "" ]; then
+    echo "Spark's published dependencies DO NOT MATCH the manifest file (dev/spark-deps)."
+    echo "To update the manifest file, run './dev/test-dependencies --replace-manifest'."
+    echo "$dep_diff"
+    exit 1
+  fi
+done
