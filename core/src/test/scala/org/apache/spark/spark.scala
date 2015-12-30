c147b27315f4cf4ad3da7f9357046ed8bf81186d
Holden Karau <holden@us.ibm.com>
2015-12-30 12:23:45 -0800

    Use a package object to allow the current tests to use the old imports for the new location of the test libraries

---
+++ core/src/test/scala/org/apache/spark/spark.scala
@@ -0,0 +1,7 @@
+// Provide the LocalSparkContext and SharedSparkContext in org.apache.spark so the old tests work.
+package org.apache
+package object spark {
+  val LocalSparkContext = org.apache.spark.util.testing.LocalSparkContext
+  type LocalSparkContext = org.apache.spark.util.testing.LocalSparkContext
+  type SharedSparkContext = org.apache.spark.util.testing.SharedSparkContext
+}
