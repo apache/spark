abdbb5864f460baa47de15ae09dddfff57658d58
Cazen <Cazen@korea.com>
2015-12-30 08:15:57 +0000

    chz true to default of allowBackslashEscapingAnyCharacter and remove \ in comment

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameReader.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameReader.scala
@@ -257,7 +257,7 @@ class DataFrameReader private[sql](sqlContext: SQLContext) extends Logging {
    * </li>
    * <li>`allowNumericLeadingZeros` (default `false`): allows leading zeros in numbers
    * (e.g. 00012)</li>
-   * <li>`allowBackslashEscapingAnyCharacter` (default `false`): allows accepting quoting of all
+   * <li>`allowBackslashEscapingAnyCharacter` (default `true`): allows accepting quoting of all
    * character using backslash quoting mechanism</li>
    *
    * @since 1.6.0
