a0217257878525dc59fb8cac9566ffe6390ce0d6
tedyu <yuzhihong@gmail.com>
2015-12-29 18:04:07 -0800

    Address Michael's comment

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
@@ -119,7 +119,7 @@ final class DataFrameWriter private[sql](df: DataFrame) {
    * Partitions the output by the given columns on the file system. If specified, the output is
    * laid out on the file system similar to Hive's partitioning scheme.
    *
-   * This was initially applicable for Parquet but in 1.5+ covers JSON as well.
+   * This was initially applicable for Parquet but in 1.5+ covers JSON, text, ORC and avro as well.
    *
    * @since 1.4.0
    */
