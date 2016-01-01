e394a67daaaf8aae510f094d94593628d54f3203
Cheng Lian <lian@databricks.com>
2016-01-01 13:24:09 -0800

    [SPARK-12592][SQL][TEST] Don't mute Spark loggers in TestHive.reset()
    There's a hack done in `TestHive.reset()`, which intended to mute noisy Hive loggers. However, Spark testing loggers are also muted.
    
    Author: Cheng Lian <lian@databricks.com>
    
    Closes #10540 from liancheng/spark-12592.dont-mute-spark-loggers.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
@@ -410,7 +410,10 @@ class TestHiveContext(sc: SparkContext) extends HiveContext(sc) {
     try {
       // HACK: Hive is too noisy by default.
       org.apache.log4j.LogManager.getCurrentLoggers.asScala.foreach { log =>
-        log.asInstanceOf[org.apache.log4j.Logger].setLevel(org.apache.log4j.Level.WARN)
+        val logger = log.asInstanceOf[org.apache.log4j.Logger]
+        if (!logger.getName.contains("org.apache.spark")) {
+          logger.setLevel(org.apache.log4j.Level.WARN)
+        }
       }
 
       cacheManager.clearCache()
