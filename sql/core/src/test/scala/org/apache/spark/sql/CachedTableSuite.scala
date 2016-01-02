f74a9a227e5667b6ffec8f347eec369e1246b1f3
thomastechs <thomas.sebastian@tcs.com>
2016-01-02 22:26:24 +0530

    [SPARK-12533]Updated CachedTableSuite.scala
    Removed the AnalysisException class import command, as it throws (hidden) warning. The Analysis Exception is already available in this package

--- sql/core/src/test/scala/org/apache/spark/sql/CachedTableSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CachedTableSuite.scala
@@ -17,7 +17,7 @@
 
 package org.apache.spark.sql
 
-import org.apache.spark.sql.AnalysisException
+
 import org.apache.spark.sql.execution.Exchange
 import org.apache.spark.sql.execution.PhysicalRDD
 
