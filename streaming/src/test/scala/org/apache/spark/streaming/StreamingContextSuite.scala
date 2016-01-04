8b7ca1c973531caaf2eb89e3d544c409150e5826
Josh Rosen <joshrosen@databricks.com>
2016-01-04 13:57:58 -0800

    Remove old todo

--- streaming/src/test/scala/org/apache/spark/streaming/StreamingContextSuite.scala
+++ streaming/src/test/scala/org/apache/spark/streaming/StreamingContextSuite.scala
@@ -81,7 +81,6 @@ class StreamingContextSuite extends SparkFunSuite with BeforeAndAfter with Timeo
 
   test("from conf with settings") {
     val myConf = SparkContext.updatedConf(new SparkConf(false), master, appName)
-    // TODO(josh): Update these exmaples to use a different configuration.
     myConf.set("spark.dummyTimeConfig", "10s")
     ssc = new StreamingContext(myConf, batchDuration)
     assert(ssc.conf.getTimeAsSeconds("spark.dummyTimeConfig", "-1") === 10)
