7058dc115047258197f6c09eee404f1ccf41038d
Marcelo Vanzin <vanzin@cloudera.com>
2016-01-04 22:42:54 -0800

    [SPARK-3873][EXAMPLES] Import ordering fixes.
    Author: Marcelo Vanzin <vanzin@cloudera.com>
    
    Closes #10575 from vanzin/SPARK-3873-examples.

--- examples/src/main/scala/org/apache/spark/examples/ml/MultilayerPerceptronClassifierExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/ml/MultilayerPerceptronClassifierExample.scala
@@ -18,12 +18,12 @@
 // scalastyle:off println
 package org.apache.spark.examples.ml
 
-import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
 // $example on$
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 // $example off$
+import org.apache.spark.sql.SQLContext
 
 /**
  * An example for Multilayer Perceptron Classification.
--- examples/src/main/scala/org/apache/spark/examples/streaming/ZeroMQWordCount.scala
+++ examples/src/main/scala/org/apache/spark/examples/streaming/ZeroMQWordCount.scala
@@ -18,17 +18,17 @@
 // scalastyle:off println
 package org.apache.spark.examples.streaming
 
+import scala.language.implicitConversions
+
 import akka.actor.ActorSystem
 import akka.actor.actorRef2Scala
+import akka.util.ByteString
 import akka.zeromq._
 import akka.zeromq.Subscribe
-import akka.util.ByteString
 
+import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.zeromq._
-
-import scala.language.implicitConversions
-import org.apache.spark.SparkConf
 
 /**
  * A simple publisher for demonstration purposes, repeatedly publishes random Messages
