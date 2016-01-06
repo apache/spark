0233c074187059db7abb51d231063f99a86c60ce
BenFradet <benjamin.fradet@gmail.com>
2016-01-06 21:18:35 +0100

    rmd useless placeholder parameter

--- mllib/src/main/scala/org/apache/spark/ml/util/SchemaUtils.scala
+++ mllib/src/main/scala/org/apache/spark/ml/util/SchemaUtils.scala
@@ -55,7 +55,7 @@ private[spark] object SchemaUtils {
       msg: String = ""): Unit = {
     val actualDataType = schema(colName).dataType
     val message = if (msg != null && msg.trim.length > 0) " " + msg else ""
-    require(dataTypes.exists(actualDataType.equals(_)),
+    require(dataTypes.exists(actualDataType.equals),
       s"Column $colName must be of type equals to one of the following types " +
         s"${dataTypes.mkString("[", ", ", "]")} but was actually of type $actualDataType.$message")
   }
