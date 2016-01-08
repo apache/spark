851f911f5c97f609d007b1e5df2f149a855f56ef
Reynold Xin <rxin@databricks.com>
2016-01-07 21:15:43 -0800

    Fix indentation for the previous patch.

--- sql/core/src/test/scala/org/apache/spark/sql/SQLConfSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLConfSuite.scala
@@ -113,22 +113,20 @@ class SQLConfSuite extends QueryTest with SharedSQLContext {
 
     // Test overflow exception
     intercept[IllegalArgumentException] {
-    // This value exceeds Long.MaxValue
-    // Utils.byteStringAsBytes("90000000000g")
-    sqlContext.setConf(SQLConf.SHUFFLE_TARGET_POSTSHUFFLE_INPUT_SIZE.key, "90000000000g")
+      // This value exceeds Long.MaxValue
+      sqlContext.setConf(SQLConf.SHUFFLE_TARGET_POSTSHUFFLE_INPUT_SIZE.key, "90000000000g")
     }
 
     intercept[IllegalArgumentException] {
-    // This value less than Int.MinValue
-    // Utils.byteStringAsBytes("-90000000000g")
+      // This value less than Int.MinValue
       sqlContext.setConf(SQLConf.SHUFFLE_TARGET_POSTSHUFFLE_INPUT_SIZE.key, "-90000000000g")
-     }
+    }
+
     // Test invalid input
     intercept[IllegalArgumentException] {
-    // This value exceeds Long.MaxValue
-    // Utils.byteStringAsBytes("-1g")
-    sqlContext.setConf(SQLConf.SHUFFLE_TARGET_POSTSHUFFLE_INPUT_SIZE.key, "-1g")
-     }
+      // This value exceeds Long.MaxValue
+      sqlContext.setConf(SQLConf.SHUFFLE_TARGET_POSTSHUFFLE_INPUT_SIZE.key, "-1g")
+    }
     sqlContext.conf.clear()
   }
 }
