a763170bb48604d01f1b35e51818e12bda6b71fd
Shixiong Zhu <shixiong@databricks.com>
2016-01-12 14:27:05 -0800

    [SPARK-12652][PYSPARK] Upgrade Py4J to 0.9.1
    - [x] Upgrade Py4J to 0.9.1
    - [x] SPARK-12657: Revert SPARK-12617
    - [x] SPARK-12658: Revert SPARK-12511
      - Still keep the change that only reading checkpoint once. This is a manual change and worth to take a look carefully. https://github.com/zsxwing/spark/commit/bfd4b5c040eb29394c3132af3c670b1a7272457c
    - [x] Verify no leak any more after reverting our workarounds
    
    Author: Shixiong Zhu <shixiong@databricks.com>
    
    Closes #10692 from zsxwing/py4j-0.9.1.

--- python/pyspark/streaming/util.py
+++ python/pyspark/streaming/util.py
@@ -89,10 +89,11 @@ class TransformFunctionSerializer(object):
     it uses this class to invoke Python, which returns the serialized function
     as a byte array.
     """
-    def init(self, ctx, serializer, gateway=None):
+    def __init__(self, ctx, serializer, gateway=None):
         self.ctx = ctx
         self.serializer = serializer
         self.gateway = gateway or self.ctx._gateway
+        self.gateway.jvm.PythonDStream.registerSerializer(self)
         self.failure = None
 
     def dumps(self, id):
