95816d4c158d7e11cab8ac7bf28b9bb84026d33a
Feynman Liang <feynman.liang@gmail.com>
2016-01-18 20:30:27 +0000

    Fix indentation

--- mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
@@ -337,18 +337,18 @@ class LogisticRegression @Since("1.2.0") (
 
         if ($(fitIntercept)) {
           /*
-            For binary logistic regression, when we initialize the coefficients as zeros,
-            it will converge faster if we initialize the intercept such that
-            it follows the distribution of the labels.
+             For binary logistic regression, when we initialize the coefficients as zeros,
+             it will converge faster if we initialize the intercept such that
+             it follows the distribution of the labels.
 
-            {{{
-              P(0) = 1 / (1 + \exp(b)), and
-              P(1) = \exp(b) / (1 + \exp(b))
-            }}}, hence
-            {{{
-              b = \log{P(1) / P(0)} = \log{count_1 / count_0}
-            }}}
-          */
+             {{{
+               P(0) = 1 / (1 + \exp(b)), and
+               P(1) = \exp(b) / (1 + \exp(b))
+             }}}, hence
+             {{{
+               b = \log{P(1) / P(0)} = \log{count_1 / count_0}
+             }}}
+           */
           initialCoefficientsWithIntercept.toArray(numFeatures) = math.log(
             histogram(1) / histogram(0))
         }
