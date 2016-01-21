12ed0841b5d5cf171e9db9325bf9f61f3dd8046b
Josh Rosen <joshrosen@databricks.com>
2016-01-20 18:05:53 -0800

    Fix TaskResultGetterSuite.

--- core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskResultGetter.scala
@@ -78,6 +78,7 @@ private[spark] class TaskResultGetter(sparkEnv: SparkEnv, scheduler: TaskSchedul
               }
               val deserializedResult = serializer.get().deserialize[DirectTaskResult[_]](
                 serializedTaskResult.get)
+              sparkEnv.blockManager.release(blockId)
               sparkEnv.blockManager.master.removeBlock(blockId)
               (deserializedResult, size)
           }
