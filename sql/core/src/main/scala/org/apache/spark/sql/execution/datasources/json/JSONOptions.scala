2b7cdaa222082bc3aa826b3f1b4327b0e0af18c9
Brandon Bradley <bradleytastic@gmail.com>
2016-01-28 15:25:57 -0800

    [SPARK-12749][SQL] add json option to parse floating-point types as DecimalType
    I tried to add this via `USE_BIG_DECIMAL_FOR_FLOATS` option from Jackson with no success.
    
    Added test for non-complex types. Should I add a test for complex types?
    
    Author: Brandon Bradley <bradleytastic@gmail.com>
    
    Closes #10936 from blbradley/spark-12749.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/json/JSONOptions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/json/JSONOptions.scala
@@ -34,6 +34,8 @@ private[sql] class JSONOptions(
     parameters.get("samplingRatio").map(_.toDouble).getOrElse(1.0)
   val primitivesAsString =
     parameters.get("primitivesAsString").map(_.toBoolean).getOrElse(false)
+  val floatAsBigDecimal =
+    parameters.get("floatAsBigDecimal").map(_.toBoolean).getOrElse(false)
   val allowComments =
     parameters.get("allowComments").map(_.toBoolean).getOrElse(false)
   val allowUnquotedFieldNames =
