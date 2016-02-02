4bb32d2362badb322851244a6836e5276101d5d1
Tathagata Das <tathagata.das1565@gmail.com>
2016-02-02 13:53:15 -0800

    Added license

--- sql/core/src/main/scala/org/apache/spark/sql/QueryException.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/QueryException.scala
@@ -1,17 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql
 
-import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.annotation.Experimental
 import org.apache.spark.sql.execution.streaming.Offset
 
 /**
- * :: DeveloperApi ::
+ * :: Experimental ::
+ * Exception that stopped a [[ContinuousQuery]].
  *
  * @param message     Message of this exception
- * @param cause       Internal cause of the exception
+ * @param cause       Internal cause of this exception
  * @param startOffset Starting offset (if known) of the range of data in which exception occurred
  * @param endOffset   Ending offset (if known) of the range of data in exception occurred
  */
-@DeveloperApi
+@Experimental
 class QueryException private[sql](
     val message: String,
     val cause: Throwable,
