9021f36d767b00fb2942e01ac6caba53c1466152
BenFradet <benjamin.fradet@gmail.com>
2016-02-10 22:12:40 +0100

    fixed scalastyle

--- examples/src/main/scala/org/apache/spark/examples/ml/ALSExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/ml/ALSExample.scala
@@ -18,11 +18,13 @@
 // scalastyle:off println
 package org.apache.spark.examples.ml
 
-import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 // $example on$
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.recommendation.ALS
+// $example off$
+import org.apache.spark.sql.SQLContext
+// $example on$
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.DoubleType
 // $example off$
