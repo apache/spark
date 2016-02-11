0d50a22084eea91d4efb0a3ed3fa59b8d9680795
jayadevanmurali <jayadevan.m@tcs.com>
2016-02-11 21:21:03 +0100

    [SPARK-12982][SQL] Add table name validation in temp table registration
    Add the table name validation at the temp table creation
    
    Author: jayadevanmurali <jayadevan.m@tcs.com>
    
    Closes #11051 from jayadevanmurali/branch-0.2-SPARK-12982.

--- sql/core/src/main/scala/org/apache/spark/sql/SQLContext.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SQLContext.scala
@@ -720,7 +720,7 @@ class SQLContext private[sql](
    * only during the lifetime of this instance of SQLContext.
    */
   private[sql] def registerDataFrameAsTable(df: DataFrame, tableName: String): Unit = {
-    catalog.registerTable(TableIdentifier(tableName), df.logicalPlan)
+    catalog.registerTable(sqlParser.parseTableIdentifier(tableName), df.logicalPlan)
   }
 
   /**
