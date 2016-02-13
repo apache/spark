5af55770c68b2ee2387ac4b1d2acc50bfb99a159
BenFradet <benjamin.fradet@gmail.com>
2016-02-13 17:39:51 +0100

    simplified the java example by removing the setters

--- examples/src/main/java/org/apache/spark/examples/ml/JavaALSExample.java
+++ examples/src/main/java/org/apache/spark/examples/ml/JavaALSExample.java
@@ -42,47 +42,41 @@ public class JavaALSExample {
     private float rating;
     private long timestamp;
 
-    public int getUserId() {
-      return userId;
+    public Rating() {}
+
+    public Rating(int userId, int movieId, float rating, long timestamp) {
+      this.userId = userId;
+      this.movieId = movieId;
+      this.rating = rating;
+      this.timestamp = timestamp;
     }
 
-    public void setUserId(int userId) {
-      this.userId = userId;
+    public int getUserId() {
+      return userId;
     }
 
     public int getMovieId() {
       return movieId;
     }
 
-    public void setMovieId(int movieId) {
-      this.movieId = movieId;
-    }
-
     public float getRating() {
       return rating;
-    }
-
-    public void setRating(float rating) {
-      this.rating = rating;
     }
 
     public long getTimestamp() {
       return timestamp;
     }
 
-    public void setTimestamp(long timestamp) {
-      this.timestamp = timestamp;
-    }
-
     public static Rating parseRating(String str) {
       String[] fields = str.split("::");
-      assert(fields.length == 4);
-      Rating rating = new Rating();
-      rating.setUserId(Integer.parseInt(fields[0]));
-      rating.setMovieId(Integer.parseInt(fields[1]));
-      rating.setRating(Float.parseFloat(fields[2]));
-      rating.setTimestamp(Long.parseLong(fields[3]));
-      return rating;
+      if (fields.length != 4) {
+        throw new IllegalArgumentException("Each line must contain 4 fields");
+      }
+      int userId = Integer.parseInt(fields[0]);
+      int movieId = Integer.parseInt(fields[1]);
+      float rating = Float.parseFloat(fields[2]);
+      long timestamp = Long.parseLong(fields[3]);
+      return new Rating(userId, movieId, rating, timestamp);
     }
   }
   // $example off$
