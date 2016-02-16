f09edf6d0f178007f3d90334b435da5689efb4e0
Xin Ren <iamshrek@126.com>
2016-02-16 19:43:58 +0800

    [SPARK-13013] revert existing files to master version

--- examples/src/main/scala/org/apache/spark/examples/mllib/StreamingKMeansExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/StreamingKMeansExample.scala
@@ -20,41 +20,60 @@ package org.apache.spark.examples.mllib
 
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.clustering.StreamingKMeans
-// $example on$
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-// $example off$
 
+/**
+ * Estimate clusters on one stream of data and make predictions
+ * on another stream, where the data streams arrive as text files
+ * into two different directories.
+ *
+ * The rows of the training text files must be vector data in the form
+ * `[x1,x2,x3,...,xn]`
+ * Where n is the number of dimensions.
+ *
+ * The rows of the test text files must be labeled data in the form
+ * `(y,[x1,x2,x3,...,xn])`
+ * Where y is some identifier. n must be the same for train and test.
+ *
+ * Usage:
+ *   StreamingKMeansExample <trainingDir> <testDir> <batchDuration> <numClusters> <numDimensions>
+ *
+ * To run on your local machine using the two directories `trainingDir` and `testDir`,
+ * with updates every 5 seconds, 2 dimensions per data point, and 3 clusters, call:
+ *    $ bin/run-example mllib.StreamingKMeansExample trainingDir testDir 5 3 2
+ *
+ * As you add text files to `trainingDir` the clusters will continuously update.
+ * Anytime you add text files to `testDir`, you'll see predicted labels using the current model.
+ *
+ */
 object StreamingKMeansExample {
 
   def main(args: Array[String]) {
+    if (args.length != 5) {
+      System.err.println(
+        "Usage: StreamingKMeansExample " +
+          "<trainingDir> <testDir> <batchDuration> <numClusters> <numDimensions>")
+      System.exit(1)
+    }
 
-    val conf = new SparkConf().setAppName("StreamingKMeansExample")
-    val ssc = new StreamingContext(conf, Seconds(1.toLong))
+    val conf = new SparkConf().setMaster("local").setAppName("StreamingKMeansExample")
+    val ssc = new StreamingContext(conf, Seconds(args(2).toLong))
 
-    // $example on$
-    // we make an input stream of vectors for training,
-    // as well as a stream of labeled data points for testing
-    val trainingData = ssc.textFileStream("/training/data/dir").map(Vectors.parse)
-    val testData = ssc.textFileStream("/testing/data/dir").map(LabeledPoint.parse)
+    val trainingData = ssc.textFileStream(args(0)).map(Vectors.parse)
+    val testData = ssc.textFileStream(args(1)).map(LabeledPoint.parse)
 
-    // We create a model with random clusters and specify the number of clusters to find
-    val numDimensions = 3
-    val numClusters = 2
     val model = new StreamingKMeans()
-      .setK(numClusters)
+      .setK(args(3).toInt)
       .setDecayFactor(1.0)
-      .setRandomCenters(numDimensions, 0.0)
+      .setRandomCenters(args(4).toInt, 0.0)
 
-    // Now register the streams for training and testing and start the job,
-    // printing the predicted cluster assignments on new data points as they arrive.
     model.trainOn(trainingData)
     model.predictOnValues(testData.map(lp => (lp.label, lp.features))).print()
 
     ssc.start()
     ssc.awaitTermination()
-    // $example off$
   }
 }
 // scalastyle:on println
