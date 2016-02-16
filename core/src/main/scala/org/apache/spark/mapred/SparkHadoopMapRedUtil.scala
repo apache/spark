7ea0b3a1b38545cb9efd2415aa3af47366f04641
Thomas Graves <tgraves@prevailsail.corp.gq1.yahoo.com>
2016-02-16 19:16:28 +0000

    Change back to just log when needsTaskCommit=false

--- core/src/main/scala/org/apache/spark/mapred/SparkHadoopMapRedUtil.scala
+++ core/src/main/scala/org/apache/spark/mapred/SparkHadoopMapRedUtil.scala
@@ -90,13 +90,8 @@ object SparkHadoopMapRedUtil extends Logging {
         performCommit()
       }
     } else {
-      // Some other attempt committed the output, this generally means speculation, we need to mark
-      // this task as failure so accounting work correctly
-      val taskAttemptNumber = TaskContext.get().attemptNumber()
-      val message =
-        s"No need to commit output of task because needsTaskCommit=false: $mrTaskAttemptID"
-      logInfo(message)
-      throw new CommitDeniedException(message, jobId, splitId, taskAttemptNumber)
+      // Some other attempt committed the output, so we do nothing and signal success
+      logInfo(s"No need to commit output of task because needsTaskCommit=false: $mrTaskAttemptID")
     }
   }
 }
