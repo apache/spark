dfe248d6a22d6177b185ec8460486d9b7249247c
Daoyuan Wang <daoyuan.wang@intel.com>
2016-02-16 03:08:25 -0800

    refine tests

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala
@@ -236,7 +236,7 @@ class CliSuite extends SparkFunSuite with BeforeAndAfterAll with Logging {
   }
 
   test("SPARK-11624 Spark SQL CLI should set sessionState only once") {
-    runCliWithin(2.minute, Seq())(
-      "!echo \"This is a test for Spark-11624\"" -> "This is a test for Spark-11624")
+    runCliWithin(2.minute, Seq("-e", "!echo \"This is a test for Spark-11624\";"))(
+      "" -> "This is a test for Spark-11624")
   }
 }
