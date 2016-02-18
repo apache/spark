ba502d691de9a5717d2ecbb0601a2f78e4264900
Xin Ren <iamshrek@126.com>
2016-02-18 16:55:03 +0800

    [SPARK-13013] change java to be 2-indent

--- examples/src/main/java/org/apache/spark/examples/mllib/JavaLatentDirichletAllocationExample.java
+++ examples/src/main/java/org/apache/spark/examples/mllib/JavaLatentDirichletAllocationExample.java
@@ -33,55 +33,55 @@ import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.SparkConf;
 
 public class JavaLatentDirichletAllocationExample {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        SparkConf conf = new SparkConf().setAppName("JavaKLatentDirichletAllocationExample");
-        JavaSparkContext jsc = new JavaSparkContext(conf);
+    SparkConf conf = new SparkConf().setAppName("JavaKLatentDirichletAllocationExample");
+    JavaSparkContext jsc = new JavaSparkContext(conf);
 
-        // $example on$
-        // Load and parse the data
-        String path = "data/mllib/sample_lda_data.txt";
-        JavaRDD<String> data = jsc.textFile(path);
-        JavaRDD<Vector> parsedData = data.map(
-                new Function<String, Vector>() {
-                    public Vector call(String s) {
-                        String[] sarray = s.trim().split(" ");
-                        double[] values = new double[sarray.length];
-                        for (int i = 0; i < sarray.length; i++)
-                            values[i] = Double.parseDouble(sarray[i]);
-                        return Vectors.dense(values);
-                    }
+    // $example on$
+    // Load and parse the data
+    String path = "data/mllib/sample_lda_data.txt";
+    JavaRDD<String> data = jsc.textFile(path);
+    JavaRDD<Vector> parsedData = data.map(
+            new Function<String, Vector>() {
+                public Vector call(String s) {
+                    String[] sarray = s.trim().split(" ");
+                    double[] values = new double[sarray.length];
+                    for (int i = 0; i < sarray.length; i++)
+                        values[i] = Double.parseDouble(sarray[i]);
+                    return Vectors.dense(values);
                 }
-        );
-        // Index documents with unique IDs
-        JavaPairRDD<Long, Vector> corpus = JavaPairRDD.fromJavaRDD(parsedData.zipWithIndex().map(
-                new Function<Tuple2<Vector, Long>, Tuple2<Long, Vector>>() {
-                    public Tuple2<Long, Vector> call(Tuple2<Vector, Long> doc_id) {
-                        return doc_id.swap();
-                    }
-                }
-        ));
-        corpus.cache();
-
-        // Cluster the documents into three topics using LDA
-        LDAModel ldaModel = new LDA().setK(3).run(corpus);
-
-        // Output topics. Each is a distribution over words (matching word count vectors)
-        System.out.println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize()
-                + " words):");
-        Matrix topics = ldaModel.topicsMatrix();
-        for (int topic = 0; topic < 3; topic++) {
-            System.out.print("Topic " + topic + ":");
-            for (int word = 0; word < ldaModel.vocabSize(); word++) {
-                System.out.print(" " + topics.apply(word, topic));
             }
-            System.out.println();
+    );
+    // Index documents with unique IDs
+    JavaPairRDD<Long, Vector> corpus = JavaPairRDD.fromJavaRDD(parsedData.zipWithIndex().map(
+            new Function<Tuple2<Vector, Long>, Tuple2<Long, Vector>>() {
+                public Tuple2<Long, Vector> call(Tuple2<Vector, Long> doc_id) {
+                    return doc_id.swap();
+                }
+            }
+    ));
+    corpus.cache();
+
+    // Cluster the documents into three topics using LDA
+    LDAModel ldaModel = new LDA().setK(3).run(corpus);
+
+    // Output topics. Each is a distribution over words (matching word count vectors)
+    System.out.println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize()
+            + " words):");
+    Matrix topics = ldaModel.topicsMatrix();
+    for (int topic = 0; topic < 3; topic++) {
+        System.out.print("Topic " + topic + ":");
+        for (int word = 0; word < ldaModel.vocabSize(); word++) {
+            System.out.print(" " + topics.apply(word, topic));
         }
-
-        ldaModel.save(jsc.sc(), "myLDAModel");
-        DistributedLDAModel sameModel = DistributedLDAModel.load(jsc.sc(), "myLDAModel");
-        // $example off$
-
-        jsc.stop();
+        System.out.println();
     }
+
+    ldaModel.save(jsc.sc(), "myLDAModel");
+    DistributedLDAModel sameModel = DistributedLDAModel.load(jsc.sc(), "myLDAModel");
+    // $example off$
+
+    jsc.stop();
+  }
 }
