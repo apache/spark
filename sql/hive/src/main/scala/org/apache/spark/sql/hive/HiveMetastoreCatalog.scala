apache/spark

commit cb288da8f0eff5f4a4703d83cd12262a7b3c3340

Miscellaneous clean ups

sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveMetastoreCatalog.scala
@@ -311,14 +311,10 @@ private[hive] class HiveMetastoreCatalog(val client: HiveClient, hive: HiveConte
       assert(partitionColumns.isEmpty)
       assert(relation.partitionColumns.isEmpty)
 
-      // TODO: We need to place the SQL string in the view text here.
       CatalogTable(
         specifiedDatabase = Option(dbName),
         name = tblName,
         tableType = tableType,
-        schema = relation.schema.map { f =>
-          CatalogColumn(f.name, HiveMetastoreTypes.toMetastoreType(f.dataType))
-        },
         storage = CatalogStorageFormat(
           locationUri = Some(relation.paths.head),
           inputFormat = serde.inputFormat,
@@ -326,7 +322,11 @@ private[hive] class HiveMetastoreCatalog(val client: HiveClient, hive: HiveConte
           serde = serde.serde,
           serdeProperties = options
         ),
-        properties = tableProperties.toMap)
+        schema = relation.schema.map { f =>
+          CatalogColumn(f.name, HiveMetastoreTypes.toMetastoreType(f.dataType))
+        },
+        properties = tableProperties.toMap,
+        viewText = None) // TODO: We need to place the SQL string here
     }
 
     // TODO: Support persisting partitioned data source relations in Hive compatible format
@@ -771,47 +771,7 @@ private[hive] case class MetastoreRelation(
 
   override protected def otherCopyArgs: Seq[AnyRef] = table :: sqlContext :: Nil
 
-  private def toHiveColumn(c: CatalogColumn): FieldSchema = {
-    new FieldSchema(c.name, c.dataType, c.comment.orNull)
-  }
-
-  @transient val hiveQlTable: HiveTable = {
-    // We start by constructing an API table as Hive performs several important transformations
-    // internally when converting an API table to a QL table.
-    val tTable = new org.apache.hadoop.hive.metastore.api.Table()
-    tTable.setTableName(table.name)
-    tTable.setDbName(table.database)
-
-    val tableParameters = new java.util.HashMap[String, String]()
-    tTable.setParameters(tableParameters)
-    table.properties.foreach { case (k, v) => tableParameters.put(k, v) }
-
-    tTable.setTableType(table.tableType match {
-      case CatalogTableType.EXTERNAL_TABLE => HiveTableType.EXTERNAL_TABLE.toString
-      case CatalogTableType.MANAGED_TABLE => HiveTableType.MANAGED_TABLE.toString
-      case CatalogTableType.INDEX_TABLE => HiveTableType.INDEX_TABLE.toString
-      case CatalogTableType.VIRTUAL_VIEW => HiveTableType.VIRTUAL_VIEW.toString
-    })
-
-    val sd = new org.apache.hadoop.hive.metastore.api.StorageDescriptor()
-    tTable.setSd(sd)
-    sd.setCols(table.schema.map(toHiveColumn).asJava)
-    tTable.setPartitionKeys(table.partitionColumns.map(toHiveColumn).asJava)
-
-    table.storage.locationUri.foreach(sd.setLocation)
-    table.storage.inputFormat.foreach(sd.setInputFormat)
-    table.storage.outputFormat.foreach(sd.setOutputFormat)
-
-    val serdeInfo = new org.apache.hadoop.hive.metastore.api.SerDeInfo
-    table.storage.serde.foreach(serdeInfo.setSerializationLib)
-    sd.setSerdeInfo(serdeInfo)
-
-    val serdeParameters = new java.util.HashMap[String, String]()
-    table.storage.serdeProperties.foreach { case (k, v) => serdeParameters.put(k, v) }
-    serdeInfo.setParameters(serdeParameters)
-
-    new HiveTable(tTable)
-  }
+  @transient val hiveQlTable: HiveTable = client.toHiveTable(table)
 
   @transient override lazy val statistics: Statistics = Statistics(
     sizeInBytes = {
@@ -845,14 +805,11 @@ private[hive] case class MetastoreRelation(
     }
 
     rawPartitions.map { p =>
-      val tPartition = new org.apache.hadoop.hive.metastore.api.Partition
-      tPartition.setDbName(databaseName)
-      tPartition.setTableName(tableName)
-      tPartition.setValues(p.spec.values.toList.asJava)
+      val hp = client.toHivePartition(p, hiveQlTable)
 
-      val sd = new org.apache.hadoop.hive.metastore.api.StorageDescriptor()
-      tPartition.setSd(sd)
-      sd.setCols(table.schema.map(toHiveColumn).asJava)
+      // TODO: I'm not sure if the following is super necessary;
+      // the partitions will just get these fields from the table anyway, right?
+      val sd = hp.getTPartition.getSd
       p.storage.locationUri.foreach(sd.setLocation)
       p.storage.inputFormat.foreach(sd.setInputFormat)
       p.storage.outputFormat.foreach(sd.setOutputFormat)
@@ -867,7 +824,7 @@ private[hive] case class MetastoreRelation(
       p.storage.serdeProperties.foreach { case (k, v) => serdeParameters.put(k, v) }
       serdeInfo.setParameters(serdeParameters)
 
-      new Partition(hiveQlTable, tPartition)
+      hp
     }
   }
 

sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClient.scala
@@ -19,6 +19,8 @@ package org.apache.spark.sql.hive.client
 
 import java.io.PrintStream
 
+import org.apache.hadoop.hive.ql.metadata.{Partition => HivePartition, Table => HiveTable}
+
 import org.apache.spark.sql.catalyst.analysis._
 import org.apache.spark.sql.catalyst.catalog._
 import org.apache.spark.sql.catalyst.expressions.Expression
@@ -75,7 +77,7 @@ private[hive] trait HiveClient {
     getTableOption(dbName, tableName).getOrElse(throw new NoSuchTableException(dbName, tableName))
   }
 
-  /** Returns the metadata for the specified table or None if it doens't exist. */
+  /** Returns the metadata for the specified table or None if it doesn't exist. */
   def getTableOption(dbName: String, tableName: String): Option[CatalogTable]
 
   /** Creates a view with the given metadata. */
@@ -249,4 +251,12 @@ private[hive] trait HiveClient {
 
   /** Used for testing only.  Removes all metadata from this instance of Hive. */
   def reset(): Unit
+
+  // --------------------------- //
+  //  Helper conversion methods  //
+  // --------------------------- //
+
+  def toHiveTable(table: CatalogTable): HiveTable
+
+  def toHivePartition(p: CatalogTablePartition, ht: HiveTable): HivePartition
 }

sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -629,7 +629,7 @@ private[hive] class HiveClientImpl(
       comment = Option(hc.getComment))
   }
 
-  private def toHiveTable(table: CatalogTable): HiveTable = {
+  override def toHiveTable(table: CatalogTable): HiveTable = {
     val hiveTable = new HiveTable(table.database, table.name)
     hiveTable.setTableType(table.tableType match {
       case CatalogTableType.EXTERNAL_TABLE => HiveTableType.EXTERNAL_TABLE
@@ -663,7 +663,9 @@ private[hive] class HiveClientImpl(
     tbl
   }
 
-  private def toHivePartition(p: CatalogTablePartition, ht: HiveTable): HivePartition = {
+  override def toHivePartition(
+      p: CatalogTablePartition,
+      ht: HiveTable): HivePartition = {
     new HivePartition(ht, p.spec.asJava, p.storage.locationUri.map { l => new Path(l) }.orNull)
   }
 

sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
@@ -220,14 +220,10 @@ case class CatalogTable(
     viewOriginalText: Option[String] = None,
     viewText: Option[String] = None) {
 
-  require(
-    tableType == CatalogTableType.EXTERNAL_TABLE ||
-    tableType == CatalogTableType.INDEX_TABLE ||
-    tableType == CatalogTableType.MANAGED_TABLE ||
-    tableType == CatalogTableType.VIRTUAL_VIEW)
-
   /** Return the database this table was specified to belong to, assuming it exists. */
-  def database: String = specifiedDatabase.getOrElse(sys.error("database not resolved"))
+  def database: String = specifiedDatabase.getOrElse {
+    throw new AnalysisException(s"table $name did not specify database")
+  }
 
   /** Return the fully qualified name of this table, assuming the database was specified. */
   def qualifiedName: String = s"$database.$name"

sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/catalog/CatalogTestCases.scala
@@ -100,7 +100,7 @@ abstract class CatalogTestCases extends SparkFunSuite with BeforeAndAfterEach {
   private def newFunc(name: String): CatalogFunction = CatalogFunction(name, funcClass)
 
   /**
-   * Whether the catalog's table partitions equals the ones given.
+   * Whether the catalog's table partitions equal the ones given.
    * Note: Hive sets some random serde things, so we just compare the specs here.
    */
   private def catalogPartitionsEqual(

sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveCatalog.scala
@@ -111,12 +111,11 @@ private[spark] class HiveCatalog(client: HiveClient) extends Catalog with Loggin
   override def alterDatabase(dbDefinition: CatalogDatabase): Unit = withClient {
     val existingDb = getDatabase(dbDefinition.name)
     if (existingDb.properties == dbDefinition.properties) {
-      logWarning(s"Ignoring request to alter database ${dbDefinition.name} because " +
+      logWarning(s"Request to alter database ${dbDefinition.name} is a no-op because " +
         s"the provided database properties are the same as the old ones. Hive does not " +
         s"currently support altering other database fields.")
-    } else {
-      client.alterDatabase(dbDefinition)
     }
+    client.alterDatabase(dbDefinition)
   }
 
   override def getDatabase(db: String): CatalogDatabase = withClient {
@@ -175,7 +174,6 @@ private[spark] class HiveCatalog(client: HiveClient) extends Catalog with Loggin
   }
 
   override def getTable(db: String, table: String): CatalogTable = withClient {
-    requireDbExists(db)
     client.getTable(db, table)
   }
 
@@ -235,7 +233,6 @@ private[spark] class HiveCatalog(client: HiveClient) extends Catalog with Loggin
       table: String,
       specs: Seq[TablePartitionSpec],
       newSpecs: Seq[TablePartitionSpec]): Unit = withClient {
-    requireTableExists(db, table)
     client.renamePartitions(db, table, specs, newSpecs)
   }
 
@@ -243,7 +240,6 @@ private[spark] class HiveCatalog(client: HiveClient) extends Catalog with Loggin
       db: String,
       table: String,
       newParts: Seq[CatalogTablePartition]): Unit = withClient {
-    requireTableExists(db, table)
     client.alterPartitions(db, table, newParts)
   }
 
@@ -251,14 +247,12 @@ private[spark] class HiveCatalog(client: HiveClient) extends Catalog with Loggin
       db: String,
       table: String,
       spec: TablePartitionSpec): CatalogTablePartition = withClient {
-    requireTableExists(db, table)
     client.getPartition(db, table, spec)
   }
 
   override def listPartitions(
       db: String,
       table: String): Seq[CatalogTablePartition] = withClient {
-    requireTableExists(db, table)
     client.getAllPartitions(db, table)
   }
 

sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveQl.scala
@@ -380,14 +380,14 @@ private[hive] class HiveQl(conf: ParserConf) extends SparkQl(conf) with Logging 
             } else {
               CatalogTableType.MANAGED_TABLE
             },
-          schema = Seq.empty[CatalogColumn],
           storage = CatalogStorageFormat(
             locationUri = None,
             inputFormat = None,
             outputFormat = None,
             serde = None,
             serdeProperties = Map.empty[String, String]
-          ))
+          ),
+          schema = Seq.empty[CatalogColumn])
 
         // default storage type abbreviation (e.g. RCFile, ORC, PARQUET etc.)
         val defaultStorageType = hiveConf.getVar(HiveConf.ConfVars.HIVEDEFAULTFILEFORMAT)

sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveQlSuite.scala
@@ -72,10 +72,10 @@ class HiveQlSuite extends SparkFunSuite with BeforeAndAfterAll {
       CatalogColumn("hour", "string", comment = Some("hour of the day")) :: Nil)
     assert(desc.storage.serdeProperties ==
       Map((serdeConstants.SERIALIZATION_FORMAT, "\054"), (serdeConstants.FIELD_DELIM, "\054")))
-    assert(desc.storage.inputFormat == Option("org.apache.hadoop.hive.ql.io.RCFileInputFormat"))
-    assert(desc.storage.outputFormat == Option("org.apache.hadoop.hive.ql.io.RCFileOutputFormat"))
+    assert(desc.storage.inputFormat == Some("org.apache.hadoop.hive.ql.io.RCFileInputFormat"))
+    assert(desc.storage.outputFormat == Some("org.apache.hadoop.hive.ql.io.RCFileOutputFormat"))
     assert(desc.storage.serde ==
-      Option("org.apache.hadoop.hive.serde2.columnar.LazyBinaryColumnarSerDe"))
+      Some("org.apache.hadoop.hive.serde2.columnar.LazyBinaryColumnarSerDe"))
     assert(desc.properties == Map(("p1", "v1"), ("p2", "v2")))
   }
 
@@ -117,9 +117,9 @@ class HiveQlSuite extends SparkFunSuite with BeforeAndAfterAll {
       CatalogColumn("dt", "string", comment = Some("date type")) ::
       CatalogColumn("hour", "string", comment = Some("hour of the day")) :: Nil)
     assert(desc.storage.serdeProperties == Map())
-    assert(desc.storage.inputFormat == Option("parquet.hive.DeprecatedParquetInputFormat"))
-    assert(desc.storage.outputFormat == Option("parquet.hive.DeprecatedParquetOutputFormat"))
-    assert(desc.storage.serde == Option("parquet.hive.serde.ParquetHiveSerDe"))
+    assert(desc.storage.inputFormat == Some("parquet.hive.DeprecatedParquetInputFormat"))
+    assert(desc.storage.outputFormat == Some("parquet.hive.DeprecatedParquetOutputFormat"))
+    assert(desc.storage.serde == Some("parquet.hive.serde.ParquetHiveSerDe"))
     assert(desc.properties == Map(("p1", "v1"), ("p2", "v2")))
   }
 
@@ -134,9 +134,9 @@ class HiveQlSuite extends SparkFunSuite with BeforeAndAfterAll {
     assert(desc.schema == Seq.empty[CatalogColumn])
     assert(desc.viewText == None) // TODO will be SQLText
     assert(desc.storage.serdeProperties == Map())
-    assert(desc.storage.inputFormat == Option("org.apache.hadoop.mapred.TextInputFormat"))
+    assert(desc.storage.inputFormat == Some("org.apache.hadoop.mapred.TextInputFormat"))
     assert(desc.storage.outputFormat ==
-      Option("org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat"))
+      Some("org.apache.hadoop.hive.ql.io.IgnoreKeyTextOutputFormat"))
     assert(desc.storage.serde.isEmpty)
     assert(desc.properties == Map())
   }
@@ -169,9 +169,9 @@ class HiveQlSuite extends SparkFunSuite with BeforeAndAfterAll {
     assert(desc.schema == Seq.empty[CatalogColumn])
     assert(desc.viewText == None) // TODO will be SQLText
     assert(desc.storage.serdeProperties == Map(("serde_p1" -> "p1"), ("serde_p2" -> "p2")))
-    assert(desc.storage.inputFormat == Option("org.apache.hadoop.hive.ql.io.RCFileInputFormat"))
-    assert(desc.storage.outputFormat == Option("org.apache.hadoop.hive.ql.io.RCFileOutputFormat"))
-    assert(desc.storage.serde == Option("org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"))
+    assert(desc.storage.inputFormat == Some("org.apache.hadoop.hive.ql.io.RCFileInputFormat"))
+    assert(desc.storage.outputFormat == Some("org.apache.hadoop.hive.ql.io.RCFileOutputFormat"))
+    assert(desc.storage.serde == Some("org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe"))
     assert(desc.properties == Map(("tbl_p1" -> "p11"), ("tbl_p2" -> "p22")))
   }
 

commit d9a77230c927a71d6ee8fbc45185379ce97e6218

Fix linking error in tests
This was caused by cb288da, an attempt to clean up some duplicate
code. It turns out that HiveClient and HiveClientImpl cannot both
refer to Hive classes due to some classloader issues. Surprise...

This commit reverts part of the changes introduced in cb288da.

sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveMetastoreCatalog.scala
@@ -771,7 +771,48 @@ private[hive] case class MetastoreRelation(
 
   override protected def otherCopyArgs: Seq[AnyRef] = table :: sqlContext :: Nil
 
-  @transient val hiveQlTable: HiveTable = client.toHiveTable(table)
+  private def toHiveColumn(c: CatalogColumn): FieldSchema = {
+    new FieldSchema(c.name, c.dataType, c.comment.orNull)
+  }
+
+  // TODO: merge this with HiveClientImpl#toHiveTable
+  @transient val hiveQlTable: HiveTable = {
+    // We start by constructing an API table as Hive performs several important transformations
+      // internally when converting an API table to a QL table.
+    val tTable = new org.apache.hadoop.hive.metastore.api.Table()
+    tTable.setTableName(table.name)
+    tTable.setDbName(table.database)
+
+    val tableParameters = new java.util.HashMap[String, String]()
+    tTable.setParameters(tableParameters)
+    table.properties.foreach { case (k, v) => tableParameters.put(k, v) }
+
+    tTable.setTableType(table.tableType match {
+      case CatalogTableType.EXTERNAL_TABLE => HiveTableType.EXTERNAL_TABLE.toString
+      case CatalogTableType.MANAGED_TABLE => HiveTableType.MANAGED_TABLE.toString
+      case CatalogTableType.INDEX_TABLE => HiveTableType.INDEX_TABLE.toString
+      case CatalogTableType.VIRTUAL_VIEW => HiveTableType.VIRTUAL_VIEW.toString
+    })
+
+    val sd = new org.apache.hadoop.hive.metastore.api.StorageDescriptor()
+    tTable.setSd(sd)
+    sd.setCols(table.schema.map(toHiveColumn).asJava)
+    tTable.setPartitionKeys(table.partitionColumns.map(toHiveColumn).asJava)
+
+    table.storage.locationUri.foreach(sd.setLocation)
+    table.storage.inputFormat.foreach(sd.setInputFormat)
+    table.storage.outputFormat.foreach(sd.setOutputFormat)
+
+    val serdeInfo = new org.apache.hadoop.hive.metastore.api.SerDeInfo
+    table.storage.serde.foreach(serdeInfo.setSerializationLib)
+    sd.setSerdeInfo(serdeInfo)
+
+    val serdeParameters = new java.util.HashMap[String, String]()
+    table.storage.serdeProperties.foreach { case (k, v) => serdeParameters.put(k, v) }
+    serdeInfo.setParameters(serdeParameters)
+
+    new HiveTable(tTable)
+  }
 
   @transient override lazy val statistics: Statistics = Statistics(
     sizeInBytes = {
@@ -805,11 +846,14 @@ private[hive] case class MetastoreRelation(
     }
 
     rawPartitions.map { p =>
-      val hp = client.toHivePartition(p, hiveQlTable)
+      val tPartition = new org.apache.hadoop.hive.metastore.api.Partition
+      tPartition.setDbName(databaseName)
+      tPartition.setTableName(tableName)
+      tPartition.setValues(p.spec.values.toList.asJava)
 
-      // TODO: I'm not sure if the following is super necessary;
-      // the partitions will just get these fields from the table anyway, right?
-      val sd = hp.getTPartition.getSd
+      val sd = new org.apache.hadoop.hive.metastore.api.StorageDescriptor()
+      tPartition.setSd(sd)
+      sd.setCols(table.schema.map(toHiveColumn).asJava)
       p.storage.locationUri.foreach(sd.setLocation)
       p.storage.inputFormat.foreach(sd.setInputFormat)
       p.storage.outputFormat.foreach(sd.setOutputFormat)
@@ -824,7 +868,7 @@ private[hive] case class MetastoreRelation(
       p.storage.serdeProperties.foreach { case (k, v) => serdeParameters.put(k, v) }
       serdeInfo.setParameters(serdeParameters)
 
-      hp
+      new Partition(hiveQlTable, tPartition)
     }
   }
 

sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClient.scala
@@ -19,8 +19,6 @@ package org.apache.spark.sql.hive.client
 
 import java.io.PrintStream
 
-import org.apache.hadoop.hive.ql.metadata.{Partition => HivePartition, Table => HiveTable}
-
 import org.apache.spark.sql.catalyst.analysis._
 import org.apache.spark.sql.catalyst.catalog._
 import org.apache.spark.sql.catalyst.expressions.Expression
@@ -252,11 +250,4 @@ private[hive] trait HiveClient {
   /** Used for testing only.  Removes all metadata from this instance of Hive. */
   def reset(): Unit
 
-  // --------------------------- //
-  //  Helper conversion methods  //
-  // --------------------------- //
-
-  def toHiveTable(table: CatalogTable): HiveTable
-
-  def toHivePartition(p: CatalogTablePartition, ht: HiveTable): HivePartition
 }

sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -629,7 +629,7 @@ private[hive] class HiveClientImpl(
       comment = Option(hc.getComment))
   }
 
-  override def toHiveTable(table: CatalogTable): HiveTable = {
+  private def toHiveTable(table: CatalogTable): HiveTable = {
     val hiveTable = new HiveTable(table.database, table.name)
     hiveTable.setTableType(table.tableType match {
       case CatalogTableType.EXTERNAL_TABLE => HiveTableType.EXTERNAL_TABLE
@@ -663,7 +663,7 @@ private[hive] class HiveClientImpl(
     tbl
   }
 
-  override def toHivePartition(
+  private def toHivePartition(
       p: CatalogTablePartition,
       ht: HiveTable): HivePartition = {
     new HivePartition(ht, p.spec.asJava, p.storage.locationUri.map { l => new Path(l) }.orNull)