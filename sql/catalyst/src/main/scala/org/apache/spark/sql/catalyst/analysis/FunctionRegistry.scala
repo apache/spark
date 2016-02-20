a67344a6eeedb0c1ee0e4276eb95f7a4160368ed
Reynold Xin <rxin@databricks.com>
2016-02-19 22:44:20 -0800

    Revert "[SPARK-12567] [SQL] Add aes_{encrypt,decrypt} UDFs"
    This reverts commit 4f9a66481849dc867cf6592d53e0e9782361d20a.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
@@ -278,8 +278,6 @@ object FunctionRegistry {
     expression[ArrayContains]("array_contains"),
 
     // misc functions
-    expression[AesEncrypt]("aes_encrypt"),
-    expression[AesDecrypt]("aes_decrypt"),
     expression[Crc32]("crc32"),
     expression[Md5]("md5"),
     expression[Murmur3Hash]("hash"),
