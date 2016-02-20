b90a6c95f846dfba322aa8e83eb1555e30812472
Siddharth Anand <sanand@agari.com>
2016-02-20 01:10:19 +0000

    Fixing prints for Python3

--- airflow/example_dags/example_trigger_controller_dag.py
+++ airflow/example_dags/example_trigger_controller_dag.py
@@ -25,7 +25,7 @@ pp = pprint.PrettyPrinter(indent=4)
 # This function decides whether or not to Trigger the remote DAG
 def conditionally_trigger(context, dag_run_obj):
     c_p =context['params']['condition_param']
-    print "Controller DAG : conditionally_trigger = {}".format(c_p)
+    print("Controller DAG : conditionally_trigger = {}".format(c_p))
     if context['params']['condition_param']:
         dag_run_obj.payload = {'message' :context['params']['message'] }
 	pp.pprint(dag_run_obj.payload)
