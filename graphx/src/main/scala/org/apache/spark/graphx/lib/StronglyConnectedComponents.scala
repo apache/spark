1e514ef561d6df9d0a7a292ee53b2cf9d961e013
Zheng RuiFeng <ruifengz@foxmail.com>
2016-02-21 10:53:30 +0800

    add check for numIters

--- graphx/src/main/scala/org/apache/spark/graphx/lib/StronglyConnectedComponents.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/StronglyConnectedComponents.scala
@@ -25,17 +25,17 @@ import org.apache.spark.graphx._
 object StronglyConnectedComponents {
 
   /**
-   * Compute the strongly connected component (SCC) of each vertex and return a graph with the
-   * vertex value containing the lowest vertex id in the SCC containing that vertex.
-   *
-   * @tparam VD the vertex attribute type (discarded in the computation)
-   * @tparam ED the edge attribute type (preserved in the computation)
-   *
-   * @param graph the graph for which to compute the SCC
-   *
-   * @return a graph with vertex attributes containing the smallest vertex id in each SCC
-   */
+    * Compute the strongly connected component (SCC) of each vertex and return a graph with the
+    * vertex value containing the lowest vertex id in the SCC containing that vertex.
+    *
+    * @tparam VD the vertex attribute type (discarded in the computation)
+    * @tparam ED the edge attribute type (preserved in the computation)
+    * @param graph the graph for which to compute the SCC
+    * @return a graph with vertex attributes containing the smallest vertex id in each SCC
+    */
   def run[VD: ClassTag, ED: ClassTag](graph: Graph[VD, ED], numIter: Int): Graph[VertexId, ED] = {
+
+    require(numIter > 0)
 
     // the graph we update with final SCC ids, and the graph we return at the end
     var sccGraph = graph.mapVertices { case (vid, _) => vid }
@@ -56,8 +56,8 @@ object StronglyConnectedComponents {
 
         // get all vertices to be removed
         val finalVertices = sccWorkGraph.vertices
-            .filter { case (vid, (scc, isFinal)) => isFinal}
-            .mapValues { (vid, data) => data._1}
+          .filter { case (vid, (scc, isFinal)) => isFinal }
+          .mapValues { (vid, data) => data._1 }
 
         // write values to sccGraph
         sccGraph = sccGraph.outerJoinVertices(finalVertices) {
@@ -67,7 +67,7 @@ object StronglyConnectedComponents {
         sccWorkGraph = sccWorkGraph.subgraph(vpred = (vid, data) => !data._2).cache()
       } while (sccWorkGraph.numVertices < numVertices)
 
-      sccWorkGraph = sccWorkGraph.mapVertices{ case (vid, (color, isFinal)) => (vid, isFinal) }
+      sccWorkGraph = sccWorkGraph.mapVertices { case (vid, (color, isFinal)) => (vid, isFinal) }
 
       // collect min of all my neighbor's scc values, update if it's smaller than mine
       // then notify any neighbors with scc values larger than mine
