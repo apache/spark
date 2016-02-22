813be50ae04878d3195ff7f20598c11bab104aed
Shixiong Zhu <shixiong@databricks.com>
2016-02-22 14:38:14 -0800

    lang -> lang3

--- core/src/main/scala/org/apache/spark/ui/scope/RDDOperationGraph.scala
+++ core/src/main/scala/org/apache/spark/ui/scope/RDDOperationGraph.scala
@@ -20,7 +20,7 @@ package org.apache.spark.ui.scope
 import scala.collection.mutable
 import scala.collection.mutable.{ListBuffer, StringBuilder}
 
-import org.apache.commons.lang.StringEscapeUtils
+import org.apache.commons.lang3.StringEscapeUtils
 
 import org.apache.spark.Logging
 import org.apache.spark.scheduler.StageInfo
