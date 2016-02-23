5b2496c88c9be5888f0341f85dfedb04ab26fcc6
zhichao.li <zhichao.li@intel.com>
2016-02-23 15:28:41 +0800

    add unit test

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveTableScanSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveTableScanSuite.scala
@@ -17,8 +17,9 @@
 
 package org.apache.spark.sql.hive.execution
 
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.hive.test.TestHive
 import org.apache.spark.sql.hive.test.TestHive._
 import org.apache.spark.sql.hive.test.TestHive.implicits._
@@ -88,5 +89,33 @@ class HiveTableScanSuite extends HiveComparisonTest {
 
     assert(sql("select CaseSensitiveColName from spark_4959_2").head() === Row("hi"))
     assert(sql("select casesensitivecolname from spark_4959_2").head() === Row("hi"))
+  }
+
+  test("combine small files") {
+    def getPartitions[T](sc: SparkContext, rdd: RDD[T]): Array[Long] = {
+      sc.runJob(rdd, (x: Iterator[T]) => 1)
+    }
+    val partitionNum = 5
+    val partitionTable = "combine_small"
+    sql("set hive.exec.dynamic.partition.mode=nonstrict")
+    sql("set spark.sql.mapper.splitCombineSize=1000000")
+    val df = (1 to 100).map { i => (i, i) }.toDF("a", "b").repartition(100)
+    df.registerTempTable("temp")
+    sql(
+      s"""create table $partitionTable (a int, b string)
+          |partitioned by (c int)
+          |stored as orc""".stripMargin)
+    sql(
+      s"""insert into table $partitionTable partition(c)
+          |select a, b, (b % $partitionNum) as c from temp""".stripMargin)
+    val result = sql( s"""select * from $partitionTable order by a""")
+    val rddPartitions = getPartitions(TestHive.sparkContext, result.rdd)
+    // Ensure that there are only have 4 RDD partitions after combination
+    assert(rddPartitions.length == 5)
+    // Ensure that the result is the same as the original after combination
+    assert(
+      result.collect().map(_.toString()).deep
+      ==  (1 to 100).map{i => s"[$i,$i,${i % partitionNum}]"}.toArray.deep
+    )
   }
 }
