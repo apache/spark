e31017ef2f8b959124a50fa4d7fe21fea49ec446
Reynold Xin <rxin@databricks.com>
2016-02-24 00:44:01 -0800

    Revert "reset bytecode change"
    This reverts commit 9efd83cc43c74c7742de7e4f3b9dd4e3e365d6fc.

--- core/src/main/scala/org/apache/spark/TaskContext.scala
+++ core/src/main/scala/org/apache/spark/TaskContext.scala
@@ -114,7 +114,11 @@ abstract class TaskContext extends Serializable {
    * This will be called in all situations - success, failure, or cancellation.
    * An example use is for HadoopRDD to register a callback to close the input stream.
    */
-  def addTaskCompletionListener(f: (TaskContext) => Unit): TaskContext
+  def addTaskCompletionListener(f: (TaskContext) => Unit): TaskContext = {
+    addTaskCompletionListener(new TaskCompletionListener {
+      override def onTaskCompletion(context: TaskContext): Unit = f(context)
+    })
+  }
 
   /**
    * Adds a listener to be executed on task failure.
@@ -126,7 +130,11 @@ abstract class TaskContext extends Serializable {
    * Adds a listener to be executed on task failure.
    * Operations defined here must be idempotent, as `onTaskFailure` can be called multiple times.
    */
-  def addTaskFailureListener(f: (TaskContext, Throwable) => Unit): TaskContext
+  def addTaskFailureListener(f: (TaskContext, Throwable) => Unit): TaskContext = {
+    addTaskFailureListener(new TaskFailureListener {
+      override def onTaskFailure(context: TaskContext, error: Throwable): Unit = f(context, error)
+    })
+  }
 
   /**
    * The ID of the stage that this task belong to.
