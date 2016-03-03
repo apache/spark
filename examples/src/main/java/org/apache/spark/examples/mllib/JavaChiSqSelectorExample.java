3513e0f63ed88479052266db5ddc0f22aab175a2
Xin Ren <iamshrek@126.com>
2016-03-03 10:55:49 -0800

    [SPARK-13017] keep unused imports out of example on and off

--- examples/src/main/java/org/apache/spark/examples/mllib/JavaChiSqSelectorExample.java
+++ examples/src/main/java/org/apache/spark/examples/mllib/JavaChiSqSelectorExample.java
@@ -18,9 +18,10 @@
 package org.apache.spark.examples.mllib;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
 // $example on$
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.mllib.feature.ChiSqSelector;
 import org.apache.spark.mllib.feature.ChiSqSelectorModel;
@@ -28,7 +29,6 @@ import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.util.MLUtils;
 // $example off$
-import org.apache.spark.api.java.function.VoidFunction;
 
 public class JavaChiSqSelectorExample {
   public static void main(String[] args) {
