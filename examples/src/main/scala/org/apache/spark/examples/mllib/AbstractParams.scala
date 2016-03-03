dd66979d7b3b84e4bc136350dd0c2d85d40f2e73
Dongjoon Hyun <dongjoon@apache.org>
2016-03-02 22:50:05 -0800

    [HOT-FIX] Revert some deprecation for 2.10 compatibility.
    [SPARK-13627](#11479) broke 2.10 compatibility.
    This PR reverts some deprecated methods in [SPARK-13627].

--- examples/src/main/scala/org/apache/spark/examples/mllib/AbstractParams.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/AbstractParams.scala
@@ -38,7 +38,7 @@ abstract class AbstractParams[T: TypeTag] {
    */
   override def toString: String = {
     val tpe = tag.tpe
-    val allAccessors = tpe.decls.collect {
+    val allAccessors = tpe.declarations.collect {
       case m: MethodSymbol if m.isCaseAccessor => m
     }
     val mirror = runtimeMirror(getClass.getClassLoader)
