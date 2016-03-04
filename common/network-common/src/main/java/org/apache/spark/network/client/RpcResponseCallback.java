389ff38d55bff987fa8bc48932016088cab02ff3
Shixiong Zhu <shixiong@databricks.com>
2016-03-03 22:53:07 -0800

    [SPARK-13652][CORE] Copy ByteBuffer in sendRpcSync as it will be recycled
    ## What changes were proposed in this pull request?
    
    `sendRpcSync` should copy the response content because the underlying buffer will be recycled and reused.
    
    ## How was this patch tested?
    
    Jenkins unit tests.
    
    Author: Shixiong Zhu <shixiong@databricks.com>
    
    Closes #11499 from zsxwing/SPARK-13652.

--- common/network-common/src/main/java/org/apache/spark/network/client/RpcResponseCallback.java
+++ common/network-common/src/main/java/org/apache/spark/network/client/RpcResponseCallback.java
@@ -24,7 +24,12 @@ import java.nio.ByteBuffer;
  * failure.
  */
 public interface RpcResponseCallback {
-  /** Successful serialized result from server. */
+  /**
+   * Successful serialized result from server.
+   *
+   * After `onSuccess` returns, `response` will be recycled and its content will become invalid.
+   * Please copy the content of `response` if you want to use it after `onSuccess` returns.
+   */
   void onSuccess(ByteBuffer response);
 
   /** Exception either propagated from server or raised on client side. */
