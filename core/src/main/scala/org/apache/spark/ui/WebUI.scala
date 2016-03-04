ed7ec30ebce69872f9e17f321021a2b1c8385330
Devaraj K <devaraj@apache.org>
2016-03-04 11:15:46 +0530

    Env var changed to SPARK_LOCAL_IP in WebUI

--- core/src/main/scala/org/apache/spark/ui/WebUI.scala
+++ core/src/main/scala/org/apache/spark/ui/WebUI.scala
@@ -134,7 +134,7 @@ private[spark] abstract class WebUI(
   def bind() {
     assert(!serverInfo.isDefined, "Attempted to bind %s more than once!".format(className))
     try {
-      serverInfo = Some(startJettyServer(Option(conf.getenv("SPARK_PUBLIC_DNS"))
+      serverInfo = Some(startJettyServer(Option(conf.getenv("SPARK_LOCAL_IP"))
         .getOrElse("0.0.0.0"), port, sslOptions, handlers, conf, name))
       logInfo("Started %s at http://%s:%d".format(className, publicHostName, boundPort))
     } catch {
