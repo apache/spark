527f9d8235dabb98d58936b5c376b83a5909d48b
Rajesh Balamohan <rbalamohan@apache.org>
2016-03-04 10:59:40 +0000

    [SPARK-12925] Improve HiveInspectors.unwrap for StringObjectInspector.…
    Earlier fix did not copy the bytes and it is possible for higher level to reuse Text object. This was causing issues. Proposed fix now copies the bytes from Text. This still avoids the expensive encoding/decoding
    
    Author: Rajesh Balamohan <rbalamohan@apache.org>
    
    Closes #11477 from rajeshbalamohan/SPARK-12925.2.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveInspectors.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveInspectors.scala
@@ -320,9 +320,10 @@ private[hive] trait HiveInspectors {
       case hvoi: HiveCharObjectInspector =>
         UTF8String.fromString(hvoi.getPrimitiveJavaObject(data).getValue)
       case x: StringObjectInspector if x.preferWritable() =>
-        // Text is in UTF-8 already. No need to convert again via fromString
+        // Text is in UTF-8 already. No need to convert again via fromString. Copy bytes
         val wObj = x.getPrimitiveWritableObject(data)
-        UTF8String.fromBytes(wObj.getBytes, 0, wObj.getLength)
+        val result = wObj.copyBytes()
+        UTF8String.fromBytes(result, 0, result.length)
       case x: StringObjectInspector =>
         UTF8String.fromString(x.getPrimitiveJavaObject(data))
       case x: IntObjectInspector if x.preferWritable() => x.get(data)
