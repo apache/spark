aa75f9245d648310acb4628c1fc414c5e2ece9ca
Marcelo Vanzin <vanzin@cloudera.com>
2016-03-14 14:27:33 -0700

    [SPARK-13626][CORE] Avoid duplicate config deprecation warnings.
    Three different things were needed to get rid of spurious warnings:
    - silence deprecation warnings when cloning configuration
    - change the way SparkHadoopUtil instantiates SparkConf to silence
      warnings
    - avoid creating new SparkConf instances where it's not needed.
    
    On top of that, I changed the way that Logging.scala detects the repl;
    now it uses a method that is overridden in the repl's Main class, and
    the hack in Utils.scala is not needed anymore. This makes the 2.11 repl
    behave like the 2.10 one and set the default log level to WARN, which
    is a lot better. Previously, this wasn't working because the 2.11 repl
    triggers log initialization earlier than the 2.10 one.
    
    I also removed and simplified some other code in the 2.11 repl's Main
    to avoid replicating logic that already exists elsewhere in Spark.
    
    Tested the 2.11 repl in local and yarn modes.
    
    Author: Marcelo Vanzin <vanzin@cloudera.com>
    
    Closes #11510 from vanzin/SPARK-13626.

--- core/src/main/scala/org/apache/spark/Logging.scala
+++ core/src/main/scala/org/apache/spark/Logging.scala
@@ -43,7 +43,7 @@ private[spark] trait Logging {
   // Method to get or create the logger for this object
   protected def log: Logger = {
     if (log_ == null) {
-      initializeIfNecessary()
+      initializeLogIfNecessary(false)
       log_ = LoggerFactory.getLogger(logName)
     }
     log_
@@ -95,17 +95,17 @@ private[spark] trait Logging {
     log.isTraceEnabled
   }
 
-  private def initializeIfNecessary() {
+  protected def initializeLogIfNecessary(isInterpreter: Boolean): Unit = {
     if (!Logging.initialized) {
       Logging.initLock.synchronized {
         if (!Logging.initialized) {
-          initializeLogging()
+          initializeLogging(isInterpreter)
         }
       }
     }
   }
 
-  private def initializeLogging() {
+  private def initializeLogging(isInterpreter: Boolean): Unit = {
     // Don't use a logger in here, as this is itself occurring during initialization of a logger
     // If Log4j 1.2 is being used, but is not initialized, load a default properties file
     val binderClass = StaticLoggerBinder.getSingleton.getLoggerFactoryClassStr
@@ -127,11 +127,11 @@ private[spark] trait Logging {
         }
       }
 
-      if (Utils.isInInterpreter) {
+      if (isInterpreter) {
         // Use the repl's main class to define the default log level when running the shell,
         // overriding the root logger's config if they're different.
         val rootLogger = LogManager.getRootLogger()
-        val replLogger = LogManager.getLogger("org.apache.spark.repl.Main")
+        val replLogger = LogManager.getLogger(logName)
         val replLevel = Option(replLogger.getLevel()).getOrElse(Level.WARN)
         if (replLevel != rootLogger.getEffectiveLevel()) {
           System.err.printf("Setting default log level to \"%s\".\n", replLevel)
