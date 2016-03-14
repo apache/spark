8248579ec27a40de98fe1f3020d947c478981ebc
Burak Köse <burakks41@gmail.com>
2016-03-15 00:23:32 +0200

    add multi-language support for stop words

--- mllib/src/main/scala/org/apache/spark/ml/feature/StopWordsRemover.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/StopWordsRemover.scala
@@ -19,71 +19,49 @@ package org.apache.spark.ml.feature
 
 import org.apache.spark.annotation.{Experimental, Since}
 import org.apache.spark.ml.Transformer
-import org.apache.spark.ml.param.{BooleanParam, ParamMap, StringArrayParam}
 import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
+import org.apache.spark.ml.param.{BooleanParam, Param, ParamMap, StringArrayParam}
 import org.apache.spark.ml.util._
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.functions.{col, udf}
 import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
 
 /**
- * stop words list
- */
+  * stop words list
+  */
 private[spark] object StopWords {
 
-  /**
-   * Use the same default stopwords list as scikit-learn.
-   * The original list can be found from "Glasgow Information Retrieval Group"
-   * [[http://ir.dcs.gla.ac.uk/resources/linguistic_utils/stop_words]]
-   */
-  val English = Array( "a", "about", "above", "across", "after", "afterwards", "again",
-    "against", "all", "almost", "alone", "along", "already", "also", "although", "always",
-    "am", "among", "amongst", "amoungst", "amount", "an", "and", "another",
-    "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are",
-    "around", "as", "at", "back", "be", "became", "because", "become",
-    "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
-    "below", "beside", "besides", "between", "beyond", "bill", "both",
-    "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con",
-    "could", "couldnt", "cry", "de", "describe", "detail", "do", "done",
-    "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else",
-    "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
-    "everything", "everywhere", "except", "few", "fifteen", "fify", "fill",
-    "find", "fire", "first", "five", "for", "former", "formerly", "forty",
-    "found", "four", "from", "front", "full", "further", "get", "give", "go",
-    "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
-    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his",
-    "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed",
-    "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter",
-    "latterly", "least", "less", "ltd", "made", "many", "may", "me",
-    "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
-    "move", "much", "must", "my", "myself", "name", "namely", "neither",
-    "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone",
-    "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on",
-    "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
-    "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps",
-    "please", "put", "rather", "re", "same", "see", "seem", "seemed",
-    "seeming", "seems", "serious", "several", "she", "should", "show", "side",
-    "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone",
-    "something", "sometime", "sometimes", "somewhere", "still", "such",
-    "system", "take", "ten", "than", "that", "the", "their", "them",
-    "themselves", "then", "thence", "there", "thereafter", "thereby",
-    "therefore", "therein", "thereupon", "these", "they", "thick", "thin",
-    "third", "this", "those", "though", "three", "through", "throughout",
-    "thru", "thus", "to", "together", "too", "top", "toward", "towards",
-    "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
-    "very", "via", "was", "we", "well", "were", "what", "whatever", "when",
-    "whence", "whenever", "where", "whereafter", "whereas", "whereby",
-    "wherein", "whereupon", "wherever", "whether", "which", "while", "whither",
-    "who", "whoever", "whole", "whom", "whose", "why", "will", "with",
-    "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves")
+  def readStopWords(language: String): Array[String] = {
+    val is = getClass.getResourceAsStream(s"/$language.txt")
+    scala.io.Source.fromInputStream(is).getLines().toArray
+  }
+
+  lazy val Danish = readStopWords("/danish.txt")
+  lazy val Dutch = readStopWords("/dutch.txt")
+  lazy val English = readStopWords("/english.txt")
+  lazy val Finnish = readStopWords("/finnish.txt")
+  lazy val French = readStopWords("/french.txt")
+  lazy val German = readStopWords("/german.txt")
+  lazy val Hungarian = readStopWords("/hungarian.txt")
+  lazy val Italian = readStopWords("/italian.txt")
+  lazy val Norwegian = readStopWords("/norwegian.txt")
+  lazy val Portuguese = readStopWords("/portuguese.txt")
+  lazy val Russian = readStopWords("/russian.txt")
+  lazy val Spanish = readStopWords("/spanish.txt")
+  lazy val Swedish = readStopWords("/swedish.txt")
+  lazy val Turkish = readStopWords("/turkish.txt")
+
+  val languageMap = Map("danish" -> Danish, "dutch" -> Dutch, "english" -> English,
+    "finnish" -> Finnish, "french" -> French, "german" -> German, "hungarian" -> Hungarian,
+    "italian" -> Italian, "norwegian" -> Norwegian, "portuguese" -> Portuguese,
+    "russian" -> Russian, "spanish" -> Spanish, "swedish" -> Swedish, "turkish" -> Turkish)
 }
 
 /**
- * :: Experimental ::
- * A feature transformer that filters out stop words from input.
- * Note: null values from input array are preserved unless adding null to stopWords explicitly.
- * @see [[http://en.wikipedia.org/wiki/Stop_words]]
- */
+  * :: Experimental ::
+  * A feature transformer that filters out stop words from input.
+  * Note: null values from input array are preserved unless adding null to stopWords explicitly.
+  * @see [[http://en.wikipedia.org/wiki/Stop_words]]
+  */
 @Experimental
 class StopWordsRemover(override val uid: String)
   extends Transformer with HasInputCol with HasOutputCol with DefaultParamsWritable {
@@ -97,23 +75,26 @@ class StopWordsRemover(override val uid: String)
   def setOutputCol(value: String): this.type = set(outputCol, value)
 
   /**
-   * the stop words set to be filtered out
-   * Default: [[StopWords.English]]
-   * @group param
-   */
+    * the stop words set to be filtered out
+    * Default: [[StopWords.English]]
+    * @group param
+    */
   val stopWords: StringArrayParam = new StringArrayParam(this, "stopWords", "stop words")
 
   /** @group setParam */
-  def setStopWords(value: Array[String]): this.type = set(stopWords, value)
+  def setStopWords(value: Array[String]): this.type = {
+    set(stopWords, value)
+    set(language, "unknown")
+  }
 
   /** @group getParam */
   def getStopWords: Array[String] = $(stopWords)
 
   /**
-   * whether to do a case sensitive comparison over the stop words
-   * Default: false
-   * @group param
-   */
+    * whether to do a case sensitive comparison over the stop words
+    * Default: false
+    * @group param
+    */
   val caseSensitive: BooleanParam = new BooleanParam(this, "caseSensitive",
     "whether to do case-sensitive comparison during filtering")
 
@@ -123,21 +104,71 @@ class StopWordsRemover(override val uid: String)
   /** @group getParam */
   def getCaseSensitive: Boolean = $(caseSensitive)
 
-  setDefault(stopWords -> StopWords.English, caseSensitive -> false)
+  /**
+    * the language of stop words
+    * Default: "english"
+    * @group param
+    */
+  val language: Param[String] = new Param[String](this, "language", "stopwords language")
+
+  /** @group setParam */
+  def setLanguage(value: String): this.type = {
+    val lang = value.toLowerCase
+    require(StopWords.languageMap.contains(lang), s"$lang is not in language list")
+    set(language, lang)
+    set(stopWords, StopWords.languageMap(lang))
+  }
+
+  /** @group getParam */
+  def getLanguage: String = $(language)
+
+  /**
+    * the ignored stop words set to be ignored out
+    * Default: [[Array.empty]]
+    * @group param
+    */
+  val ignoredWords: StringArrayParam = new StringArrayParam(this, "ignoredWords",
+    "the ignored stop words set to be ignored out")
+
+  /** @group setParam */
+  def setIgnoredWords(value: Array[String]): this.type = set(ignoredWords, value)
+
+  /** @group getParam */
+  def getIgnoredWords: Array[String] = $(ignoredWords)
+
+  /**
+    * the additional stop words set to be filtered out
+    * Default: [[Array.empty]]
+    * @group param
+    */
+  val additionalWords: StringArrayParam = new StringArrayParam(this, "additionalWords",
+    "the additional stop words set to be filtered out")
+
+  /** @group setParam */
+  def setAdditionalWords(value: Array[String]): this.type = set(additionalWords, value)
+
+  /** @group getParam */
+  def getAdditionalWords: Array[String] = $(additionalWords)
+
+  setDefault(stopWords -> StopWords.English, language -> "en", ignoredWords -> Array.empty[String]
+    , additionalWords -> Array.empty[String], caseSensitive -> false)
 
   override def transform(dataset: DataFrame): DataFrame = {
     val outputSchema = transformSchema(dataset.schema)
     val t = if ($(caseSensitive)) {
-        val stopWordsSet = $(stopWords).toSet
-        udf { terms: Seq[String] =>
-          terms.filter(s => !stopWordsSet.contains(s))
-        }
-      } else {
-        val toLower = (s: String) => if (s != null) s.toLowerCase else s
-        val lowerStopWords = $(stopWords).map(toLower(_)).toSet
-        udf { terms: Seq[String] =>
-          terms.filter(s => !lowerStopWords.contains(toLower(s)))
-        }
+      val stopWordsSet = ($(stopWords) ++ $(additionalWords)).toSet - $(ignoredWords).toSet
+      udf { terms: Seq[String] =>
+        terms.filter(s => !stopWordsSet.contains(s))
+      }
+    } else {
+      val toLower = (s: String) => if (s != null) s.toLowerCase else s
+      val lowerStopWords = {
+        ($(stopWords) ++ $(additionalWords))
+          .map(toLower(_)).toSet - $(ignoredWords).map(toLower(_)).toSet
+      }
+      udf { terms: Seq[String] =>
+        terms.filter(s => !lowerStopWords.contains(toLower(s)))
+      }
     }
 
     val metadata = outputSchema($(outputCol)).metadata
