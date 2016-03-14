c0df7062c6136e16936b25ae669bfab658d0622c
Reynold Xin <rxin@databricks.com>
2016-03-14 13:58:54 -0700

    pep8

--- python/pyspark/__init__.py
+++ python/pyspark/__init__.py
@@ -74,7 +74,7 @@ def copy_func(f, name=None):
     # See
     # http://stackoverflow.com/questions/6527633/how-can-i-make-a-deepcopy-of-a-function-in-python
     fn = types.FunctionType(f.__code__, f.__globals__, name or f.__name__, f.__defaults__,
-        f.__closure__)
+                            f.__closure__)
     # in case f was given attrs (note this dict is a shallow copy):
     fn.__dict__.update(f.__dict__)
     return fn
