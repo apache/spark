d782f4fe8ac2803f1b3a720cd3f9228e180214ff
Yonathan Randolph <yonathan@liftigniter.com>
2016-03-16 09:34:04 +0000

    [SPARK-13906] Ensure that there are at least 2 dispatcher threads.
    ## What changes were proposed in this pull request?
    
    Force at least two dispatcher-event-loop threads. Since SparkDeploySchedulerBackend (in AppClient) calls askWithRetry to CoarseGrainedScheduler in the same process, there the driver needs at least two dispatcher threads to prevent the dispatcher thread from hanging.
    
    ## How was this patch tested?
    
    Manual.
    
    Author: Yonathan Randolph <yonathangmail.com>
    
    Author: Yonathan Randolph <yonathan@liftigniter.com>
    
    Closes #11728 from yonran/SPARK-13906.

--- core/src/main/scala/org/apache/spark/rpc/netty/Dispatcher.scala
+++ core/src/main/scala/org/apache/spark/rpc/netty/Dispatcher.scala
@@ -192,7 +192,7 @@ private[netty] class Dispatcher(nettyEnv: NettyRpcEnv) extends Logging {
   /** Thread pool used for dispatching messages. */
   private val threadpool: ThreadPoolExecutor = {
     val numThreads = nettyEnv.conf.getInt("spark.rpc.netty.dispatcher.numThreads",
-      Runtime.getRuntime.availableProcessors())
+      math.max(2, Runtime.getRuntime.availableProcessors()))
     val pool = ThreadUtils.newDaemonFixedThreadPool(numThreads, "dispatcher-event-loop")
     for (i <- 0 until numThreads) {
       pool.execute(new MessageLoop)
