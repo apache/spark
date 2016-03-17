10cf58e10ffb93961db41d3269fd48dab8ecf711
Devaraj K <devaraj@apache.org>
2016-03-17 14:40:14 +0530

    [SPARK-13965] [CORE] Driver should kill the other running task attempts if any one task attempt succeeds for the same task

--- core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -620,6 +620,14 @@ private[spark] class TaskSetManager(
     // Note: "result.value()" only deserializes the value when it's called at the first time, so
     // here "result.value()" just returns the value and won't block other threads.
     sched.dagScheduler.taskEnded(tasks(index), Success, result.value(), result.accumUpdates, info)
+    // Kill other task attempts if any as the one attempt succeeded
+    for (attemptInfo <- taskAttempts(index) if attemptInfo.attemptNumber != info.attemptNumber
+        && attemptInfo.running) {
+      logInfo("Killing attempt " + attemptInfo.attemptNumber + " for task " + attemptInfo.id +
+        " in stage " + taskSet.id + " (TID " + attemptInfo.taskId + ") on " + attemptInfo.host +
+        " as the attempt " + info.attemptNumber + " succeeded on " + info.host)
+      sched.backend.killTask(attemptInfo.taskId, attemptInfo.executorId, true)
+    }
     if (!successful(index)) {
       tasksSuccessful += 1
       logInfo("Finished task %s in stage %s (TID %d) in %d ms on %s (%d/%d)".format(
