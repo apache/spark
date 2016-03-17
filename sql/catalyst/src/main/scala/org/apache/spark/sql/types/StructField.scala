58514e73352a47579cf56e921172924245356c61
Francois Prunier <francois.prunier@hurence.com>
2016-03-17 16:29:08 +0100

    making StructField nullable field documentation clearer

--- sql/catalyst/src/main/scala/org/apache/spark/sql/types/StructField.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/types/StructField.scala
@@ -24,7 +24,9 @@ import org.json4s.JsonDSL._
  * A field inside a StructType.
  * @param name The name of this field.
  * @param dataType The data type of this field.
- * @param nullable Indicates if values of this field can be `null` values.
+ * @param nullable Indicates if values of this field can be `null` values. Note that this ONLY A HINT to the 
+ *                 optimiser, the calling code is responsible for ensuring that there is no null. It is NOT 
+ *                 a constraint that will be checked by the engine.
  * @param metadata The metadata of this field. The metadata should be preserved during
  *                 transformation if the content of the column is not modified, e.g, in selection.
  */
