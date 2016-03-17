d1b4572c454b108754882dd96daf1c3f52b75b50
Zack Shapiro <zack@nextdoor.com>
2016-03-17 14:07:37 -0700

    Adding support for ssl parameters.  (picking up from jthomas123)

--- airflow/hooks/postgres_hook.py
+++ airflow/hooks/postgres_hook.py
@@ -6,6 +6,8 @@ from airflow.hooks.dbapi_hook import DbApiHook
 class PostgresHook(DbApiHook):
     '''
     Interact with Postgres.
+    You can specify ssl parameters in the extra field of your connection
+    as ``{"sslmode": "require", "sslcert": "/path/to/cert.pem", etc}``.
     '''
     conn_name_attr = 'postgres_conn_id'
     default_conn_name = 'postgres_default'
@@ -13,9 +15,14 @@ class PostgresHook(DbApiHook):
 
     def get_conn(self):
         conn = self.get_connection(self.postgres_conn_id)
-        return psycopg2.connect(
+        conn_args = dict(
             host=conn.host,
             user=conn.login,
             password=conn.password,
             dbname=conn.schema,
             port=conn.port)
+        # check for ssl parameters in conn.extra
+        for arg_name, arg_val in conn.extra_dejson.items():
+            if arg_name in ['sslmode', 'sslcert', 'sslkey', 'sslrootcert', 'sslcrl']:
+                conn_args[arg_name] = arg_val
+        return psycopg2.connect(**conn_args)
