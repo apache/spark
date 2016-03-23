926a93e54b83f1ee596096f3301fef015705b627
Reynold Xin <rxin@databricks.com>
2016-03-22 23:43:09 -0700

    [SPARK-14088][SQL] Some Dataset API touch-up
    ## What changes were proposed in this pull request?
    1. Deprecated unionAll. It is pretty confusing to have both "union" and "unionAll" when the two do the same thing in Spark but are different in SQL.
    2. Rename reduce in KeyValueGroupedDataset to reduceGroups so it is more consistent with rest of the functions in KeyValueGroupedDataset. Also makes it more obvious what "reduce" and "reduceGroups" mean. Previously it was confusing because it could be reducing a Dataset, or just reducing groups.
    3. Added a "name" function, which is more natural to name columns than "as" for non-SQL users.
    4. Remove "subtract" function since it is just an alias for "except".
    
    ## How was this patch tested?
    All changes should be covered by existing tests. Also added couple test cases to cover "name".
    
    Author: Reynold Xin <rxin@databricks.com>
    
    Closes #11908 from rxin/SPARK-14088.

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -911,14 +911,24 @@ class DataFrame(object):
         """
         return self.groupBy().agg(*exprs)
 
+    @since(2.0)
+    def union(self, other):
+        """ Return a new :class:`DataFrame` containing union of rows in this
+        frame and another frame.
+
+        This is equivalent to `UNION ALL` in SQL. To do a SQL-style set union
+        (that does deduplication of elements), use this function followed by a distinct.
+        """
+        return DataFrame(self._jdf.unionAll(other._jdf), self.sql_ctx)
+
     @since(1.3)
     def unionAll(self, other):
         """ Return a new :class:`DataFrame` containing union of rows in this
         frame and another frame.
 
-        This is equivalent to `UNION ALL` in SQL.
+        .. note:: Deprecated in 2.0, use union instead.
         """
-        return DataFrame(self._jdf.unionAll(other._jdf), self.sql_ctx)
+        return self.union(other)
 
     @since(1.3)
     def intersect(self, other):
