c28fbd7e9e6dbbd5d22193520c52d254f71a6a11
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-03-25 21:26:45 -0700

    Linting

--- airflow/models.py
+++ airflow/models.py
@@ -798,17 +798,17 @@ class TaskInstance(Base):
         """
         task = self.task
 
-        if not task._downstream_task_ids:
+        if not task.downstream_task_ids:
             return True
 
         ti = session.query(func.count(TaskInstance.task_id)).filter(
             TaskInstance.dag_id == self.dag_id,
-            TaskInstance.task_id.in_(task._downstream_task_ids),
+            TaskInstance.task_id.in_(task.downstream_task_ids),
             TaskInstance.execution_date == self.execution_date,
             TaskInstance.state == State.SUCCESS,
         )
         count = ti[0][0]
-        return count == len(task._downstream_task_ids)
+        return count == len(task.downstream_task_ids)
 
     @provide_session
     def are_dependencies_met(
@@ -876,7 +876,7 @@ class TaskInstance(Base):
             )
             .filter(
                 TI.dag_id == self.dag_id,
-                TI.task_id.in_(task._upstream_task_ids),
+                TI.task_id.in_(task.upstream_task_ids),
                 TI.execution_date == self.execution_date,
                 TI.state.in_([
                     State.SUCCESS, State.FAILED,
@@ -884,7 +884,7 @@ class TaskInstance(Base):
             )
         )
         successes, skipped, failed, upstream_failed, done = qry.first()
-        upstream = len(task._upstream_task_ids)
+        upstream = len(task.upstream_task_ids)
         tr = task.trigger_rule
         upstream_done = done >= upstream
 
@@ -1767,9 +1767,17 @@ class BaseOperator(object):
         return [self.dag.get_task(tid) for tid in self._upstream_task_ids]
 
     @property
+    def upstream_task_ids(self):
+        return self._upstream_task_ids
+
+    @property
     def downstream_list(self):
         """@property: list of tasks directly downstream"""
         return [self.dag.get_task(tid) for tid in self._downstream_task_ids]
+
+    @property
+    def downstream_task_ids(self):
+        return self._downstream_task_ids
 
     def clear(
             self, start_date=None, end_date=None,
