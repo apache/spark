ebd210214470fb359b2868441fe9b609ed95df61
xin Wu <xinwu@us.ibm.com>
2016-03-28 18:54:42 -0700

    more

--- sql/core/src/main/scala/org/apache/spark/sql/expressions/WindowSpec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/expressions/WindowSpec.scala
@@ -33,8 +33,7 @@ import org.apache.spark.sql.catalyst.expressions._
 class WindowSpec private[sql](
     partitionSpec: Seq[Expression],
     orderSpec: Seq[SortOrder],
-    frame: catalyst.expressions.WindowFrame,
-    exclude: ExcludeClause =  ExcludeClause.defaultExclude ) {
+    frame: catalyst.expressions.WindowFrame) {
 
   /**
    * Defines the partitioning columns in a [[WindowSpec]].
@@ -139,7 +138,7 @@ class WindowSpec private[sql](
    * Converts this [[WindowSpec]] into a [[Column]] with an aggregate expression.
    */
   private[sql] def withAggregate(aggregate: Column): Column = {
-    val spec = WindowSpecDefinition(partitionSpec, orderSpec, frame, exclude)
+    val spec = WindowSpecDefinition(partitionSpec, orderSpec, frame)
     new Column(WindowExpression(aggregate.expr, spec))
   }
 }
