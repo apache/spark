2e975b64913c6572585339d2c780bcfb8a010c5a
KaiXinXiaoLei <huleilei1@huawei.com>
2016-03-30 16:20:27 +0800

    get attempID

--- core/src/main/scala/org/apache/spark/ui/UIUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/UIUtils.scala
@@ -386,7 +386,8 @@ private[spark] object UIUtils extends Logging {
           graphs.map { g =>
             val stageId = g.rootCluster.id.replaceAll(RDDOperationGraph.STAGE_CLUSTER_PREFIX, "")
             val skipped = g.rootCluster.name.contains("skipped").toString
-            <div class="stage-metadata" stage-id={stageId} skipped={skipped}>
+            val attemptId = RDDOperationGraph.stageIdAttemptedMap.getOrElse(stageId, "0")
+            <div class="stage-metadata" stage-id={stageId} skipped={skipped} attemptId={attemptId}>
               <div class="dot-file">{RDDOperationGraph.makeDotFile(g)}</div>
               { g.incomingEdges.map { e => <div class="incoming-edge">{e.fromId},{e.toId}</div> } }
               { g.outgoingEdges.map { e => <div class="outgoing-edge">{e.fromId},{e.toId}</div> } }
