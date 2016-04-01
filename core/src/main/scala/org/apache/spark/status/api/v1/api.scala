974e8d3701d892e770d49344a9752ffa9289c4e5
Alex Bozarth <ajbozart@us.ibm.com>
2016-04-01 16:18:09 -0700

    [SPARK-13241][WEB UI] Added long values for dates in ApplicationAttemptInfo API
    ## What changes were proposed in this pull request?
    
    Adding long values for each Date in the ApplicationAttemptInfo API for easier use in code
    
    ## How was the this patch tested?
    
    Tested with dev/run-tests
    
    Author: Alex Bozarth <ajbozart@us.ibm.com>
    
    Closes #11326 from ajbozarth/spark13241.

--- core/src/main/scala/org/apache/spark/status/api/v1/api.scala
+++ core/src/main/scala/org/apache/spark/status/api/v1/api.scala
@@ -38,7 +38,11 @@ class ApplicationAttemptInfo private[spark](
     val lastUpdated: Date,
     val duration: Long,
     val sparkUser: String,
-    val completed: Boolean = false)
+    val completed: Boolean = false) {
+    def getStartTimeEpoch: Long = startTime.getTime
+    def getEndTimeEpoch: Long = endTime.getTime
+    def getLastUpdatedEpoch: Long = lastUpdated.getTime
+}
 
 class ExecutorStageSummary private[spark](
     val taskTime : Long,
