a471c7f9eaa59d55dfff5b9d1a858f304a6b3a84
sureshthalamati <suresh.thalamati@gmail.com>
2016-04-01 18:33:31 +0200

    [SPARK-14133][SQL] Throws exception for unsupported create/drop/alter index , and lock/unlock operations.
    ## What changes were proposed in this pull request?
    
    This  PR  throws Unsupported Operation exception for create index, drop index, alter index , lock table , lock database, unlock table, and unlock database operations that are not supported in Spark SQL. Currently these operations are executed executed by Hive.
    
    Error:
    spark-sql> drop index my_index on my_table;
    Error in query:
    Unsupported operation: drop index(line 1, pos 0)
    
    ## How was this patch tested?
    Added test cases to HiveQuerySuite
    
    yhuai hvanhovell andrewor14
    
    Author: sureshthalamati <suresh.thalamati@gmail.com>
    
    Closes #12069 from sureshthalamati/unsupported_ddl_spark-14133.

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -1280,6 +1280,21 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
     assertUnsupportedFeature { sql("SHOW LOCKS my_table") }
   }
 
+  test("lock/unlock table and database commands are not supported") {
+    assertUnsupportedFeature { sql("LOCK TABLE my_table SHARED") }
+    assertUnsupportedFeature { sql("UNLOCK TABLE my_table") }
+    assertUnsupportedFeature { sql("LOCK DATABASE my_db SHARED") }
+    assertUnsupportedFeature { sql("UNLOCK DATABASE my_db") }
+  }
+
+  test("create/drop/alter index commands are not supported") {
+    assertUnsupportedFeature {
+      sql("CREATE INDEX my_index ON TABLE my_table(a) as 'COMPACT' WITH DEFERRED REBUILD")}
+    assertUnsupportedFeature { sql("DROP INDEX my_index ON my_table") }
+    assertUnsupportedFeature { sql("ALTER INDEX my_index ON my_table REBUILD")}
+    assertUnsupportedFeature {
+      sql("ALTER INDEX my_index ON my_table set IDXPROPERTIES (\"prop1\"=\"val1_new\")")}
+  }
 }
 
 // for SPARK-2180 test
