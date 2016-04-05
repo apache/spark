ff13a19679ecff0d1ff166af1c301602ddb55249
Cheng Lian <lian@databricks.com>
2016-04-05 20:29:42 +0800

    Adds withHadoopConf utility method

--- sql/core/src/test/scala/org/apache/spark/sql/test/SQLTestUtils.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/test/SQLTestUtils.scala
@@ -91,6 +91,23 @@ private[sql] trait SQLTestUtils
     sparkContext.hadoopConfiguration
   }
 
+  protected def withHadoopConf(pairs: (String, String)*)(f: => Unit): Unit = {
+    val (keys, _) = pairs.unzip
+    val originalValues = keys.map(key => Option(hadoopConfiguration.get(key)))
+
+    try {
+      pairs.foreach { case (key, value) =>
+        hadoopConfiguration.set(key, value)
+      }
+      f
+    } finally {
+      keys.zip(originalValues).foreach {
+        case (key, Some(value)) => hadoopConfiguration.set(key, value)
+        case (key, None) => hadoopConfiguration.unset(key)
+      }
+    }
+  }
+
   /**
    * Sets all SQL configurations specified in `pairs`, calls `f`, and then restore all SQL
    * configurations.
