f29298037e7efdcca900f1767cb1678db30e1fc6
Brian O'Neill <boneill@monetate.com>
2016-04-08 13:13:36 -0400

    Adding script to publish the jar to artifact bucket

---
+++ publish_distribution.sh
@@ -0,0 +1,1 @@
+aws s3 cp extras/kinesis-asl-assembly/target/spark-streaming-kinesis-asl-assembly_2.10-1.6.1.jar s3://artifact-monetate-dev/gradle/spark-streaming-kinesis-asl-assembly_2.10-1.6.1.monetate1.jar
