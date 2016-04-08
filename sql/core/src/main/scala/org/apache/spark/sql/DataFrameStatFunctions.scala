b64bd4eeedf5926eb644ae64f954eea2356de906
Liang-Chi Hsieh <simonh@tw.ibm.com>
2016-04-08 14:35:39 +0000

    Update comment.

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameStatFunctions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameStatFunctions.scala
@@ -72,7 +72,7 @@ final class DataFrameStatFunctions private[sql](df: DataFrame) {
 
   /**
    * Calculates the approximate quantiles of numerical columns of a DataFrame.
-   * @see approxQuantile for detailed description.
+   * @see #approxQuantile(String, Array[Double], Double) for detailed description.
    *
    * @param cols the names of the numerical columns.
    * @param probabilities a list of quantile probabilities
