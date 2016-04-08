3e29e372ff518827bae9dcd26087946fde476843
Andrew Or <andrew@databricks.com>
2016-04-07 17:49:39 -0700

    [SPARK-14468] Always enable OutputCommitCoordinator
    ## What changes were proposed in this pull request?
    
    `OutputCommitCoordinator` was introduced to deal with concurrent task attempts racing to write output, leading to data loss or corruption. For more detail, read the [JIRA description](https://issues.apache.org/jira/browse/SPARK-14468).
    
    Before: `OutputCommitCoordinator` is enabled only if speculation is enabled.
    After: `OutputCommitCoordinator` is always enabled.
    
    Users may still disable this through `spark.hadoop.outputCommitCoordination.enabled`, but they really shouldn't...
    
    ## How was this patch tested?
    
    `OutputCommitCoordinator*Suite`
    
    Author: Andrew Or <andrew@databricks.com>
    
    Closes #12244 from andrewor14/always-occ.

--- core/src/test/scala/org/apache/spark/scheduler/OutputCommitCoordinatorIntegrationSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/OutputCommitCoordinatorIntegrationSuite.scala
@@ -38,7 +38,7 @@ class OutputCommitCoordinatorIntegrationSuite
     super.beforeAll()
     val conf = new SparkConf()
       .set("master", "local[2,4]")
-      .set("spark.speculation", "true")
+      .set("spark.hadoop.outputCommitCoordination.enabled", "true")
       .set("spark.hadoop.mapred.output.committer.class",
         classOf[ThrowExceptionOnFirstAttemptOutputCommitter].getCanonicalName)
     sc = new SparkContext("local[2, 4]", "test", conf)
