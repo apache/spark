38d7c876d81de87a9c51e1f63dcb4d288ff02955
fwang1 <desperado.wf@gmail.com>
2016-04-09 11:42:16 -0700

    replace it with Ordering.By(_._2)

--- mllib/src/main/scala/org/apache/spark/ml/feature/CountVectorizer.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/CountVectorizer.scala
@@ -153,15 +153,9 @@ class CountVectorizer(override val uid: String)
     }.cache()
     val fullVocabSize = wordCounts.count()
 
-    val wordCountOrdering = new Ordering[(String, Long)]{
-      override def compare(x: (String, Long), y: (String,Long)): Int = {
-        if (x._2 > y._2) +1 else if (x._2 < y._2) -1 else 0
-      }
-    }
-
-    val vocab: Array[String] = wordCounts
-      .top(math.min(fullVocabSize, vocSize).toInt)(wordCountOrdering)
-      .map(_._1)
+    val vocab: Array[String] = wordCounts.
+    top(math.min(fullVocabSize, vocSize).toInt)(Ordering.by(_._2))
+    .map(_._1)
 
     require(vocab.length > 0, "The vocabulary size should be > 0. Lower minDF as necessary.")
     copyValues(new CountVectorizerModel(uid, vocab).setParent(this))
