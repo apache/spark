f959d7222ac01f06f54526a088aa66401cfb8a32
Zheng RuiFeng <ruifengz@foxmail.com>
2016-04-10 11:25:38 +0800

    create py example for quantile_discretizer

---
+++ examples/src/main/python/ml/quantile_discretizer_example.py
@@ -0,0 +1,40 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+# $example on$
+from pyspark.ml.feature import QuantileDiscretizer
+# $example off$
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="QuantileDiscretizerExample")
+    sqlContext = SQLContext(sc)
+
+    # $example on$
+    data = [(0, 18.0,), (1, 19.0,), (2, 8.0,), (3, 5.0,), (4, 2.2,)]
+    dataFrame = sqlContext.createDataFrame(data, ["id", "hour"])
+
+    discretizer = QuantileDiscretizer(numBuckets=3, inputCol="hour", outputCol="result")
+
+    result = discretizer.fit(dataFrame).transform(dataFrame)
+    result.show()
+    # $example off$
+
+    sc.stop()
