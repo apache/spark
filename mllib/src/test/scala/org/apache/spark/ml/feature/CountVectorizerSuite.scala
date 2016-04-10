c45aca5b0d6ef314c0f2896770b09b93a3a543c2
fwang1 <desperado.wf@gmail.com>
2016-04-10 01:13:25 -0700

    [SPARK-14497][ML] Use top instead of sortBy() to get top N frequent words as dict in ConutVectorizer
    ## What changes were proposed in this pull request?
    
    Replace sortBy() with top() to calculate the top N frequent words as dictionary.
    
    ## How was this patch tested?
    existing unit tests.  The terms with same TF would be sorted in descending order. The test would fail if hardcode the terms with same TF the dictionary like "c", "d"...
    
    Author: fwang1 <desperado.wf@gmail.com>
    
    Closes #12265 from lionelfeng/master.

--- mllib/src/test/scala/org/apache/spark/ml/feature/CountVectorizerSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/CountVectorizerSuite.scala
@@ -59,14 +59,15 @@ class CountVectorizerSuite extends SparkFunSuite with MLlibTestSparkContext
       (0, split("a b c d e"),
         Vectors.sparse(5, Seq((0, 1.0), (1, 1.0), (2, 1.0), (3, 1.0), (4, 1.0)))),
       (1, split("a a a a a a"), Vectors.sparse(5, Seq((0, 6.0)))),
-      (2, split("c"), Vectors.sparse(5, Seq((2, 1.0)))),
-      (3, split("b b b b b"), Vectors.sparse(5, Seq((1, 5.0)))))
+      (2, split("c c"), Vectors.sparse(5, Seq((2, 2.0)))),
+      (3, split("d"), Vectors.sparse(5, Seq((3, 1.0)))),
+      (4, split("b b b b b"), Vectors.sparse(5, Seq((1, 5.0)))))
     ).toDF("id", "words", "expected")
     val cv = new CountVectorizer()
       .setInputCol("words")
       .setOutputCol("features")
       .fit(df)
-    assert(cv.vocabulary === Array("a", "b", "c", "d", "e"))
+    assert(cv.vocabulary.toSet === Set("a", "b", "c", "d", "e"))
 
     cv.transform(df).select("features", "expected").collect().foreach {
       case Row(features: Vector, expected: Vector) =>
