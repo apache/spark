0ca109408005d7ed8971e583fec8027ef7c34c9e
Timothy Hunter <timhunter@databricks.com>
2016-04-12 11:56:58 -0700

    documentation and fixes

--- mllib/src/main/scala/org/apache/spark/ml/util/Instrumentation.scala
+++ mllib/src/main/scala/org/apache/spark/ml/util/Instrumentation.scala
@@ -17,15 +17,27 @@
 
 package org.apache.spark.ml.util
 
+import java.util.concurrent.atomic.AtomicLong
+
 import org.apache.spark.internal.Logging
-import org.apache.spark.ml.param.Param
 import org.apache.spark.ml.{Estimator, Model}
+import org.apache.spark.ml.param.Param
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Dataset
 
+/**
+ * A small wrapper that defines a training session for an estimator, and some methods to log
+ * useful information during this session.
+ *
+ * A new class is expected to be created within fit().
+ *
+ * @param estimator the estimator that is being fit
+ * @param dataset the training dataset
+ * @tparam E the type of the estimator
+ */
 private[ml] class Instrumentation[E <: Estimator[_]] private (
     estimator: E, dataset: RDD[_]) extends Logging {
-
+  private val id = Instrumentation.counter.incrementAndGet()
   init()
 
   private def init(): Unit = {
@@ -33,11 +45,19 @@ private[ml] class Instrumentation[E <: Estimator[_]] private (
       s" storageLevel=${dataset.getStorageLevel}")
   }
 
+  /**
+   * Logs a message with a prefix that uniquely identifies the prefix session.
+   */
   def log(msg: String): Unit = {
     val className = estimator.getClass.getSimpleName
-    logInfo(s"Instrumentation($className-${estimator.uid}-${dataset.hashCode()}):$msg")
+    logInfo(s"Instrumentation($className-${estimator.uid}-${dataset.hashCode()}-$id): $msg")
   }
 
+  /**
+   * Logs the value of the given parameters for the estimator being used in this session.
+ *
+   * @param params
+   */
   def logParams(params: Param[_]*): Unit = {
     val pairs = for {
       p <- params
@@ -56,6 +76,9 @@ private[ml] class Instrumentation[E <: Estimator[_]] private (
     log(s"numClasses=$num")
   }
 
+  /**
+   * Logs the successful completion of the training session and the value of the learned model.
+   */
   def logSuccess(model: Model[_]): Unit = {
     log(s"training finished")
   }
@@ -65,11 +88,19 @@ private[ml] class Instrumentation[E <: Estimator[_]] private (
  * Some common methods for logging information about a training session.
  */
 private[ml] object Instrumentation {
+  private val counter = new AtomicLong(0)
+
+  /**
+   * Creates an instrumentation object for a training session.
+   */
   def create[E <: Estimator[_]](
       estimator: E, dataset: Dataset[_]): Instrumentation[E] = {
-    new Instrumentation[E](estimator, dataset.rdd)
+    create[E](estimator, dataset.rdd)
   }
 
+  /**
+   * Creates an instrumentation object for a training session.
+   */
   def create[E <: Estimator[_]](
       estimator: E, dataset: RDD[_]): Instrumentation[E] = {
     new Instrumentation[E](estimator, dataset)
