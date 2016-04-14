7763f3c6d28a3246b40a849150746a220e03a112
Juliet Hougland <juliet@cloudera.com>
2016-04-14 07:11:37 -0700

    Adds setup.py

---
+++ python/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = __version__
+JAR_PATH = "MY AWESOME JAR PATH"
+
+setup(
+    name='pyspark',
+    version=VERSION,
+    description='Apache Spark Python API',
+    author='Spark Developers',
+    author_email='dev@spark.apache.org',
+    url='https://github.com/apache/spark/tree/master/python',
+    packages=['pyspark', 'pyspark.mllib', 'pyspark.ml', 'pyspark.sql', 'pyspark.streaming'],
+    include_package_data = True,
+    package_data={
+        'spark.jar': [JAR_PATH]},
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    install_requires=['py4j==0.9'],
+    extras_require = {
+        'ml': ['numpy>=1.7'],
+        'sql': ['pandas']
+    }
+)
