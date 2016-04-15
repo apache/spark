3401570a8aa737e04a214736ced2e9fe62ec7692
Joseph K. Bradley <joseph@databricks.com>
2016-04-15 14:29:35 -0700

    reverted some style fixes for easier backport

--- mllib/src/main/scala/org/apache/spark/ml/Pipeline.scala
+++ mllib/src/main/scala/org/apache/spark/ml/Pipeline.scala
@@ -297,7 +297,7 @@ class PipelineModel private[ml] (
   @Since("2.0.0")
   override def transform(dataset: Dataset[_]): DataFrame = {
     transformSchema(dataset.schema, logging = true)
-    stages.foldLeft(dataset.toDF())((cur, transformer) => transformer.transform(cur))
+    stages.foldLeft(dataset.toDF)((cur, transformer) => transformer.transform(cur))
   }
 
   @Since("1.2.0")
