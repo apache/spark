147e9f96d05220331cc931bbdd8d6192537e98f2
Burak Yavuz <burak@databricks.com>
2016-04-15 09:17:35 -0700

    fix check

--- python/pyspark/sql/streaming.py
+++ python/pyspark/sql/streaming.py
@@ -93,10 +93,6 @@ class Trigger(object):
         """
         pass
 
-    @staticmethod
-    def _is_subclass(instance):
-        return isinstance(instance, ProcessingTime)
-
 
 class ProcessingTime(Trigger):
     """A trigger that runs a query periodically based on the processing time. If `interval` is 0,
