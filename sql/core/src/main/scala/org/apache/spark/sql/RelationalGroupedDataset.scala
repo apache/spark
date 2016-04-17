c06fbeb7d34ab30d3b7df74f5ac2b5255840bffb
Wenchen Fan <wenchen@databricks.com>
2016-04-17 15:35:46 +0800

    fix compile

--- sql/core/src/main/scala/org/apache/spark/sql/RelationalGroupedDataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/RelationalGroupedDataset.scala
@@ -210,7 +210,7 @@ class RelationalGroupedDataset protected[sql](
   def agg(expr: Column, exprs: Column*): DataFrame = {
     toDF((expr +: exprs).map {
       case typed: TypedColumn[_, _] =>
-        typed.withInputType(df.resolvedTEncoder, df.logicalPlan.output).expr
+        typed.withInputType(df.unresolvedTEncoder.deserializer, df.logicalPlan.output).expr
       case c => c.expr
     })
   }
