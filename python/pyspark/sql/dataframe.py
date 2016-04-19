17318ceca0559ea2a85ce13a15923ccad74ee07f
felixcheung <felixcheung_m@hotmail.com>
2016-04-19 16:04:58 -0700

    add docstring note

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -329,6 +329,8 @@ class DataFrame(object):
     def unpersist(self, blocking=False):
         """Marks the :class:`DataFrame` as non-persistent, and remove all blocks for it from
         memory and disk.
+
+        .. note:: `blocking` default has changed to False to match Scala in 2.0.
         """
         self.is_cached = False
         self._jdf.unpersist(blocking)
