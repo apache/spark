c61a3ac09a907bc8101840d08bd26da98726d253
Dongjoon Hyun <dongjoon@apache.org>
2016-04-20 13:29:27 -0700

    Add dummy identity for test.

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -82,6 +82,8 @@ def identify_changed_files_from_git_commits(patch_sha, target_branch=None, targe
         run_cmd(['git', 'fetch', 'origin', str(target_branch+':'+target_branch)])
     else:
         diff_target = target_ref
+    run_cmd(['git', 'config', 'user.name', '"Apache Spark"'])
+    run_cmd(['git', 'config', 'user.email', '"dev@spark.apache.org"'])
     run_cmd(['git', 'rebase', diff_target])
     raw_output = subprocess.check_output(['git', 'diff', '--name-only', patch_sha, diff_target],
                                          universal_newlines=True)
