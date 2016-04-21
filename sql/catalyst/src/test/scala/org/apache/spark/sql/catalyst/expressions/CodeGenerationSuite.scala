e7afed92a21835bbb6d92df2dbd51fa872e2dbfa
Takuya UESHIN <ueshin@happy-camper.st>
2016-04-21 14:33:48 +0900

    Fix scalastyle.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CodeGenerationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CodeGenerationSuite.scala
@@ -22,8 +22,8 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.dsl.expressions._
 import org.apache.spark.sql.catalyst.expressions.codegen._
+import org.apache.spark.sql.catalyst.util.{ArrayBasedMapData, GenericArrayData}
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.catalyst.util.{ArrayBasedMapData,GenericArrayData}
 import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.ThreadUtils
 
