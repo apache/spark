605814e3d873e7c51b2fb187c9c71c29faa12dbc
Sun Rui <rui.sun@intel.com>
2016-04-22 16:25:53 +0800

    Fix import ordering.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/r/MapPartitionsRWrapper.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/r/MapPartitionsRWrapper.scala
@@ -22,7 +22,7 @@ import org.apache.spark.api.r.SerializationFormats
 import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.sql.api.r.SQLUtils._
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{StructType, BinaryType, StructField}
+import org.apache.spark.sql.types.{BinaryType, StructField, StructType}
 
 /**
  * A function wrapper that applies the given R function to each partition.
