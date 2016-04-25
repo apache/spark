4ea8bc79a7d9ee9f4ab85c3c52f3700ed8f033f9
Liang-Chi Hsieh <simonh@tw.ibm.com>
2016-04-25 04:56:16 +0000

    Fix scala style.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/createDataSourceTables.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/createDataSourceTables.scala
@@ -32,7 +32,7 @@ import org.apache.spark.sql.execution.datasources.{BucketSpec, DataSource, Hadoo
 import org.apache.spark.sql.internal.HiveSerDe
 import org.apache.spark.sql.sources.InsertableRelation
 import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.types.UTF8String 
+import org.apache.spark.unsafe.types.UTF8String
 
 /**
  * A command used to create a data source table.
