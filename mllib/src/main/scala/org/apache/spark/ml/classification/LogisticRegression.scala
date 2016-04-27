841c2d3818b1882c96572c4ff5991083c0795fd8
Yanbo Liang <ybliang8@gmail.com>
2016-04-27 01:47:18 -0700

    Add logWarning for LiR, LoR, AFTSR when fit w/o intercept on dataset with constant nonzero column

--- mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
@@ -333,6 +333,13 @@ class LogisticRegression @Since("1.2.0") (
         val featuresMean = summarizer.mean.toArray
         val featuresStd = summarizer.variance.toArray.map(math.sqrt)
 
+        if (!$(fitIntercept) && (0 to numFeatures).exists { i =>
+          featuresStd(i) == 0.0 && featuresMean(i) != 0.0 }) {
+          logWarning("Fitting LogisticRegressionModel without intercept on dataset with " +
+            "constant nonzero column, Spark ML produce same model as R glmnet but " +
+            "different from LIBSVM.")
+        }
+
         val regParamL1 = $(elasticNetParam) * $(regParam)
         val regParamL2 = (1.0 - $(elasticNetParam)) * $(regParam)
 
