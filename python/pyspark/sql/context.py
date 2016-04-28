2c0ea4261d3c1963e76d7de57b19d1f8e18b97f0
Andrew Or <andrew@databricks.com>
2016-04-27 23:11:06 -0700

    Fix JavaMember has no attribute wrapped

--- python/pyspark/sql/context.py
+++ python/pyspark/sql/context.py
@@ -97,7 +97,7 @@ class SQLContext(object):
         """
         if cls._instantiatedContext is None:
             jsqlContext = sc._jvm.SQLContext.getOrCreate(sc._jsc.sc())
-            sparkSession = SparkSession(sc, jsqlContext.sparkSession)
+            sparkSession = SparkSession(sc, jsqlContext.sparkSession())
             cls(sc, sparkSession, jsqlContext)
         return cls._instantiatedContext
 
