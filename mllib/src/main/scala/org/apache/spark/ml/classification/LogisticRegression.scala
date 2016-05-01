a6428292f78fd594f41a4a7bf254d40268f46305
Xusen Yin <yinxusen@gmail.com>
2016-05-01 12:29:01 -0700

    [SPARK-14931][ML][PYTHON] Mismatched default values between pipelines in Spark and PySpark - update
    ## What changes were proposed in this pull request?
    
    This PR is an update for [https://github.com/apache/spark/pull/12738] which:
    * Adds a generic unit test for JavaParams wrappers in pyspark.ml for checking default Param values vs. the defaults in the Scala side
    * Various fixes for bugs found
      * This includes changing classes taking weightCol to treat unset and empty String Param values the same way.
    
    Defaults changed:
    * Scala
     * LogisticRegression: weightCol defaults to not set (instead of empty string)
     * StringIndexer: labels default to not set (instead of empty array)
     * GeneralizedLinearRegression:
       * maxIter always defaults to 25 (simpler than defaulting to 25 for a particular solver)
       * weightCol defaults to not set (instead of empty string)
     * LinearRegression: weightCol defaults to not set (instead of empty string)
    * Python
     * MultilayerPerceptron: layers default to not set (instead of [1,1])
     * ChiSqSelector: numTopFeatures defaults to 50 (instead of not set)
    
    ## How was this patch tested?
    
    Generic unit test.  Manually tested that unit test by changing defaults and verifying that broke the test.
    
    Author: Joseph K. Bradley <joseph@databricks.com>
    Author: yinxusen <yinxusen@gmail.com>
    
    Closes #12816 from jkbradley/yinxusen-SPARK-14931.

--- mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
@@ -235,13 +235,12 @@ class LogisticRegression @Since("1.2.0") (
 
   /**
    * Whether to over-/under-sample training instances according to the given weights in weightCol.
-   * If empty, all instances are treated equally (weight 1.0).
-   * Default is empty, so all instances have weight one.
+   * If not set or empty String, all instances are treated equally (weight 1.0).
+   * Default is not set, so all instances have weight one.
    * @group setParam
    */
   @Since("1.6.0")
   def setWeightCol(value: String): this.type = set(weightCol, value)
-  setDefault(weightCol -> "")
 
   @Since("1.5.0")
   override def setThresholds(value: Array[Double]): this.type = super.setThresholds(value)
@@ -264,7 +263,7 @@ class LogisticRegression @Since("1.2.0") (
 
   protected[spark] def train(dataset: Dataset[_], handlePersistence: Boolean):
       LogisticRegressionModel = {
-    val w = if ($(weightCol).isEmpty) lit(1.0) else col($(weightCol))
+    val w = if (!isDefined(weightCol) || $(weightCol).isEmpty) lit(1.0) else col($(weightCol))
     val instances: RDD[Instance] =
       dataset.select(col($(labelCol)).cast(DoubleType), w, col($(featuresCol))).rdd.map {
         case Row(label: Double, weight: Double, features: Vector) =>
