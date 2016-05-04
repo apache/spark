a5dc3a4dc99feca4e71375dc51aa4a99eb1c0589
Cheng Lian <lian@databricks.com>
2016-05-04 17:51:13 +0800

    Fixe compilation error introduced while rebasing

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
@@ -32,7 +32,7 @@ import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
 import org.apache.spark.sql.catalyst.plans.logical.{Command, LogicalPlan, UnaryNode}
 import org.apache.spark.sql.catalyst.util.quoteIdentifier
 import org.apache.spark.sql.execution.datasources.PartitioningUtils
-import org.apache.spark.sql.types.{BooleanType, MetadataBuilder, StringType, StructType}
+import org.apache.spark.sql.types._
 import org.apache.spark.util.Utils
 
 case class CreateTableAsSelectLogicalPlan(
