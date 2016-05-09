77872275c31d65b70e9963ee11b5fd3c2162881c
Sandeep Singh <sandeep@techaddict.me>
2016-05-10 03:36:30 +0530

    work around for warehousePath problem

--- sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLSuite.scala
@@ -66,7 +66,7 @@ class DDLSuite extends QueryTest with SharedSQLContext with BeforeAndAfterEach {
 
   private def createDatabase(catalog: SessionCatalog, name: String): Unit = {
     catalog.createDatabase(
-      CatalogDatabase(name, "", sqlContext.conf.warehousePath, Map()),
+      CatalogDatabase(name, "", spark.sessionState.conf.warehousePath, Map()),
       ignoreIfExists = false)
   }
 
