e8bae89020c1db8573b76add89765f378c8a044b
Sandeep Singh <sandeep@techaddict.me>
2016-05-13 19:46:19 +0530

    ml clustering done

--- mllib/src/test/java/org/apache/spark/ml/clustering/JavaKMeansSuite.java
+++ mllib/src/test/java/org/apache/spark/ml/clustering/JavaKMeansSuite.java
@@ -21,37 +21,22 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
+import org.apache.spark.SharedSparkSession;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
 
-public class JavaKMeansSuite implements Serializable {
+public class JavaKMeansSuite extends SharedSparkSession implements Serializable {
 
   private transient int k = 5;
   private transient Dataset<Row> dataset;
-  private transient SparkSession spark;
 
-  @Before
-  public void setUp() {
-    spark = SparkSession.builder()
-      .master("local")
-      .appName("JavaKMeansSuite")
-      .getOrCreate();
+  public void customSetUp() {
     dataset = KMeansSuite.generateKMeansData(spark, 50, 3, k);
-  }
-
-  @After
-  public void tearDown() {
-    spark.stop();
-    spark = null;
   }
 
   @Test
