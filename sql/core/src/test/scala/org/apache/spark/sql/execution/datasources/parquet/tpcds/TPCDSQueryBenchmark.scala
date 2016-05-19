85b4ba2a77fc8876ef85fb2fd978c085e1c26d14
Sameer Agarwal <sameer@databricks.com>
2016-05-19 14:55:00 -0700

    Modify some queries. Combine previous benchmark

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/tpcds/TPCDSQueryBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/tpcds/TPCDSQueryBenchmark.scala
@@ -23,6 +23,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.expressions.SubqueryExpression
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.util._
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.util.Benchmark
@@ -45,18 +47,6 @@ object TPCDSQueryBenchmark {
 
   val spark = SparkSession.builder.config(conf).getOrCreate()
 
-  val queries = Seq(
-    "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11",
-    "q12", "q13", "q14a", "q14b", "q15", "q16", "q17", "q18", "q19", "q20",
-    "q21", "q22", "q23a", "q23b", "q24a", "q24b", "q25", "q26", "q27", "q28", "q29", "q30",
-    "q31", "q32", "q33", "q34", "q35", "q36", "q37", "q38", "q39a", "q39b", "q40",
-    "q41", "q42", "q43", "q44", "q45", "q46", "q47", "q48", "q49", "q50",
-    "q51", "q52", "q53", "q54", "q55", "q56", "q57", "q58", "q59", "q60",
-    "q61", "q62", "q63", "q64", "q65", "q66", "q67", "q68", "q69", "q70",
-    "q71", "q72", "q73", "q74", "q75", "q76", "q77", "q78", "q79", "q80",
-    "q81", "q82", "q83", "q84", "q85", "q86", "q87", "q88", "q89", "q90",
-    "q91", "q92", "q93", "q94", "q95", "q96", "q97", "q98", "q99", "ss_max")
-
   val tables = Seq("catalog_page", "catalog_returns", "customer", "customer_address",
     "customer_demographics", "date_dim", "household_demographics", "inventory", "item",
     "promotion", "store", "store_returns", "catalog_sales", "web_sales", "store_sales",
@@ -65,12 +55,12 @@ object TPCDSQueryBenchmark {
 
   def setupTables(dataLocation: String): Map[String, Long] = {
     tables.map { tableName =>
-      spark.read.parquet(s"$dataLocation/$tableName").registerTempTable(tableName)
+      spark.read.parquet(s"$dataLocation/$tableName").createOrReplaceTempView(tableName)
       tableName -> spark.table(tableName).count()
     }.toMap
   }
 
-  def tpcdsAll(dataLocation: String): Unit = {
+  def tpcdsAll(dataLocation: String, queries: Seq[String]): Unit = {
     require(dataLocation.nonEmpty,
       "please modify the value of dataLocation to point to your local TPCDS data")
     val tableSizes = setupTables(dataLocation)
@@ -79,11 +69,29 @@ object TPCDSQueryBenchmark {
     queries.foreach { name =>
       val queriesString = fileToString(new File(s"sql/core/src/test/scala/org/apache/spark/sql/" +
         s"execution/datasources/parquet/tpcds/queries/$name.sql"))
-      val numRows = spark.sql(queriesString).queryExecution.logical.map {
-        case ur@UnresolvedRelation(t: TableIdentifier, _) =>
-          tableSizes.getOrElse(t.table, 0L)
-        case _ => 0L
-      }.sum
+
+      // This is an indirect hack to estimate the size of each query's input by traversing the
+      // logical plan and adding up the sizes of all tables that appear in the plan. Note that this
+      // currently doesn't take WITH subqueries into account which might lead to fairly inaccurate
+      // per-row processing time for those cases.
+      val queryRelations = scala.collection.mutable.HashSet[String]()
+      spark.sql(queriesString).queryExecution.logical.map {
+        case ur @ UnresolvedRelation(t: TableIdentifier, _) =>
+          queryRelations.add(t.table)
+        case lp: LogicalPlan =>
+          lp.expressions.foreach { _ foreach {
+            case subquery: SubqueryExpression =>
+              subquery.plan.foreach {
+                case ur @ UnresolvedRelation(t: TableIdentifier, _) =>
+                  queryRelations.add(t.table)
+                case _ =>
+              }
+            case _ =>
+          }
+        }
+        case _ =>
+      }
+      val numRows = queryRelations.map(tableSizes.getOrElse(_, 0L)).sum
       val benchmark = new Benchmark("TPCDS Snappy (scale = 5)", numRows, 1)
       benchmark.addCase(name) { i =>
         spark.sql(queriesString).collect()
@@ -94,12 +102,31 @@ object TPCDSQueryBenchmark {
 
   def main(args: Array[String]): Unit = {
 
+    // List of all TPC-DS queries
+    val allQueries = Seq(
+      "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11",
+      "q12", "q13", "q14a", "q14b", "q15", "q16", "q17", "q18", "q19", "q20",
+      "q21", "q22", "q23a", "q23b", "q24a", "q24b", "q25", "q26", "q27", "q28", "q29", "q30",
+      "q31", "q32", "q33", "q34", "q35", "q36", "q37", "q38", "q39a", "q39b", "q40",
+      "q41", "q42", "q43", "q44", "q45", "q46", "q47", "q48", "q49", "q50",
+      "q51", "q52", "q53", "q54", "q55", "q56", "q57", "q58", "q59", "q60",
+      "q61", "q62", "q63", "q64", "q65", "q66", "q67", "q68", "q69", "q70",
+      "q71", "q72", "q73", "q74", "q75", "q76", "q77", "q78", "q79", "q80",
+      "q81", "q82", "q83", "q84", "q85", "q86", "q87", "q88", "q89", "q90",
+      "q91", "q92", "q93", "q94", "q95", "q96", "q97", "q98", "q99", "ss_max")
+
+    // These queries a subset of the TPCDS benchmark queries and are taken from
+    // https://github.com/databricks/spark-sql-perf/blob/master/src/main/scala/com/databricks/spark/
+    // sql/perf/tpcds/ImpalaKitQueries.scala
+    val commonQueries = Seq("q3", "q7", "q8", "q19", "q27", "q34", "q42", "q43", "q46", "q52",
+      "q53", "q55", "q59", "q63", "q65", "q68", "q73", "q79", "q82", "q89", "q98", "ss_max")
+
     // In order to run this benchmark, please follow the instructions at
     // https://github.com/databricks/spark-sql-perf/blob/master/README.md to generate the TPCDS data
     // locally (preferably with a scale factor of 5 for benchmarking). Thereafter, the value of
     // dataLocation below needs to be set to the location where the generated data is stored.
-    val dataLocation = "/Users/sameer/tpcds"
+    val dataLocation = ""
 
-    tpcdsAll(dataLocation)
+    tpcdsAll(dataLocation, queries = allQueries)
   }
 }
