539dfa205dacea72188642f15773a30a99f8e8ac
Zheng RuiFeng <ruifengz@foxmail.com>
2016-05-19 23:26:11 -0700

    [SPARK-15398][ML] Update the warning message to recommend ML usage
    ## What changes were proposed in this pull request?
    MLlib are not recommended to use, and some methods are even deprecated.
    Update the warning message to recommend ML usage.
    ```
      def showWarning() {
        System.err.println(
          """WARN: This is a naive implementation of Logistic Regression and is given as an example!
            |Please use either org.apache.spark.mllib.classification.LogisticRegressionWithSGD or
            |org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
            |for more conventional use.
          """.stripMargin)
      }
    ```
    To
    ```
      def showWarning() {
        System.err.println(
          """WARN: This is a naive implementation of Logistic Regression and is given as an example!
            |Please use org.apache.spark.ml.classification.LogisticRegression
            |for more conventional use.
          """.stripMargin)
      }
    ```
    
    ## How was this patch tested?
    local build
    
    Author: Zheng RuiFeng <ruifengz@foxmail.com>
    
    Closes #13190 from zhengruifeng/update_recd.
    
    (cherry picked from commit 47a2940da97caa55bbb8bb8ec1d51c9f6d5041c6)
    Signed-off-by: Xiangrui Meng <meng@databricks.com>

--- examples/src/main/scala/org/apache/spark/examples/SparkHdfsLR.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkHdfsLR.scala
@@ -31,8 +31,7 @@ import org.apache.spark._
  * Logistic regression based classification.
  *
  * This is an example implementation for learning how to use Spark. For more conventional use,
- * please refer to either org.apache.spark.mllib.classification.LogisticRegressionWithSGD or
- * org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS based on your needs.
+ * please refer to org.apache.spark.ml.classification.LogisticRegression.
  */
 object SparkHdfsLR {
   val D = 10   // Number of dimensions
@@ -54,8 +53,7 @@ object SparkHdfsLR {
   def showWarning() {
     System.err.println(
       """WARN: This is a naive implementation of Logistic Regression and is given as an example!
-        |Please use either org.apache.spark.mllib.classification.LogisticRegressionWithSGD or
-        |org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+        |Please use org.apache.spark.ml.classification.LogisticRegression
         |for more conventional use.
       """.stripMargin)
   }
