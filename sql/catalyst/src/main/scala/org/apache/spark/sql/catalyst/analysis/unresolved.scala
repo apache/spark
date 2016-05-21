9a8df0c9a46babb55dd8301e11721025cdfb8af5
Dilip Biswal <dbiswal@us.ibm.com>
2016-05-21 08:36:08 -0700

    [SPARK-15114][SQL] Column name generated by typed aggregate is super verbose
    ## What changes were proposed in this pull request?
    
    Generate a shorter default alias for `AggregateExpression `, In this PR, aggregate function name along with a index is used for generating the alias name.
    
    ```SQL
    val ds = Seq(1, 3, 2, 5).toDS()
    ds.select(typed.sum((i: Int) => i), typed.avg((i: Int) => i)).show()
    ```
    
    Output before change.
    ```SQL
    +-----------------------------------------------------------------------------------------------------------------------------+---------------------------------------------------------------------------------------------------------------------------------------------+
    |typedsumdouble(unresolveddeserializer(upcast(input[0, int], IntegerType, - root class: "scala.Int"), value#1), upcast(value))|typedaverage(unresolveddeserializer(upcast(input[0, int], IntegerType, - root class: "scala.Int"), value#1), newInstance(class scala.Tuple2))|
    +-----------------------------------------------------------------------------------------------------------------------------+---------------------------------------------------------------------------------------------------------------------------------------------+
    |                                                                                                                         11.0|                                                                                                                                         2.75|
    +-----------------------------------------------------------------------------------------------------------------------------+---------------------------------------------------------------------------------------------------------------------------------------------+
    ```
    Output after change:
    ```SQL
    +-----------------+---------------+
    |typedsumdouble_c1|typedaverage_c2|
    +-----------------+---------------+
    |             11.0|           2.75|
    +-----------------+---------------+
    ```
    
    Note: There is one test in ParquetSuites.scala which shows that that the system picked alias
    name is not usable and is rejected.  [test](https://github.com/apache/spark/blob/master/sql/hive/src/test/scala/org/apache/spark/sql/hive/parquetSuites.scala#L672-#L687)
    ## How was this patch tested?
    
    A new test was added in DataSetAggregatorSuite.
    
    Author: Dilip Biswal <dbiswal@us.ibm.com>
    
    Closes #13045 from dilipbiswal/spark-15114.
    
    (cherry picked from commit 5e1ee28984b169eaab5d2f832921d32cf09de915)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/unresolved.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/unresolved.scala
@@ -325,10 +325,13 @@ case class UnresolvedExtractValue(child: Expression, extraction: Expression)
  * Holds the expression that has yet to be aliased.
  *
  * @param child The computation that is needs to be resolved during analysis.
- * @param aliasName The name if specified to be associated with the result of computing [[child]]
+ * @param aliasFunc The function if specified to be called to generate an alias to associate
+ *                  with the result of computing [[child]]
  *
  */
-case class UnresolvedAlias(child: Expression, aliasName: Option[String] = None)
+case class UnresolvedAlias(
+    child: Expression,
+    aliasFunc: Option[Expression => String] = None)
   extends UnaryExpression with NamedExpression with Unevaluable {
 
   override def toAttribute: Attribute = throw new UnresolvedException(this, "toAttribute")
