65f9746362ac6fb227a5c8ff59717852b5ae87c4
Dongjoon Hyun <dongjoon@apache.org>
2016-05-21 14:36:04 -0700

    [SPARK-15466][SQL] Make `SparkSession` as the entry point to programming with RDD too

--- python/pyspark/sql/session.py
+++ python/pyspark/sql/session.py
@@ -272,6 +272,19 @@ class SparkSession(object):
 
         return DataFrame(jdf, self._wrapped)
 
+    @since(2.0)
+    def parallelize(self, c, numSlices=None):
+        """
+        Distribute a local Python collection to form an RDD. Using xrange
+        is recommended if the input represents a range for performance.
+
+        >>> spark.parallelize([0, 2, 3, 4, 6], 5).glom().collect()
+        [[0], [2], [3], [4], [6]]
+        >>> spark.parallelize(xrange(0, 6, 2), 5).glom().collect()
+        [[], [0], [], [2], [4]]
+        """
+        return self._sc.parallelize(c, numSlices)
+
     def _inferSchemaFromList(self, data):
         """
         Infer schema from list of Row or tuple.
