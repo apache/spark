d530712d369b72ec9c01cf2c14690615695bcd9e
Holden Karau <holden@us.ibm.com>
2016-05-21 20:21:47 -0700

    Start on a bit of ScalaDoc and mark classes as private

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/JythonUDF.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/JythonUDF.scala
@@ -30,8 +30,11 @@ import org.apache.spark.sql.Row
 
 /**
  * A serialized version of a Python lambda function to be executed in Jython.
+ *
+ * TODO: Consider if extending ScalaUDF is the right path forward
+ * TODO: Consider if pipelining multiple JythonUDFs is important
  */
-private[spark] class JythonUDF(
+private[sql] class JythonUDF(
     name: String,
     func: JythonFunction,
     sfunc: AnyRef,
@@ -39,7 +42,7 @@ private[spark] class JythonUDF(
     children: Seq[Expression])
   extends ScalaUDF(sfunc, dataType, children) {
 
-  // For the copy constructor
+  // For the copy constructor keep the same ScalaFunc.
   def this(sfunc: AnyRef, dataType: DataType, children: Seq[Expression],
     inputTypes: Seq[DataType], name: String, func: JythonFunction) = {
     this(name, func, sfunc, dataType, children)
@@ -50,10 +53,11 @@ private[spark] class JythonUDF(
       children)
   }
 
-  override def toString: String = s"JythonUDF_$name(${children.mkString(", ")})"
+  override def toString: String = s"$name(${children.mkString(", ")})"
 
   override def nullable: Boolean = true
 
+  // Pass the name and function when copying
   override protected def otherCopyArgs: Seq[AnyRef] = {
     List(name, func)
   }
@@ -61,36 +65,40 @@ private[spark] class JythonUDF(
 
 /**
  * A wrapper for a Jython function, contains all necessary context to run the function in Jython.
+ *
+ *
+ * @param src  Python lambda expression as a string
+ * @param pythonVars  Variables to be set before the function, as a base 64 encoded pickle map of
+ *                    name and value.
+ * @param imports  Python imports as a base 64 encoded pickle set of module, name, target.
  */
-case class JythonFunction(src: String, pythonVars: String, imports: String) {
+private[sql] case class JythonFunction(src: String, pythonVars: String, imports: String) {
   val className = s"__reservedPandaClass"
   val code = s"""
-from base64 import b64decode
-import pickle
-import os
-import sys
-sys.path.extend(os.environ["PYTHONPATH"].split(":"))
-
-pythonVars = pickle.loads(b64decode('${pythonVars}'))
-imports = pickle.loads(b64decode('${imports}'))
-if imports is not None:
-  for module, name, target in imports:
-      exec "from %s import %s as %s" % (module, name, target)
-if pythonVars is not None:
-  for k, v in pythonVars.iteritems():
-    exec "%s = v" % k
-class ${className}(object):
-  def __init__(self):
-    self.call = ${src}
-${className}_instance = ${className}()
-"""
+              |from base64 import b64decode
+              |import pickle
+              |import os
+              |import sys
+              |sys.path.extend(os.environ["PYTHONPATH"].split(":"))
+              |
+              |pythonVars = pickle.loads(b64decode('${pythonVars}'))
+              |imports = pickle.loads(b64decode('${imports}'))
+              |if imports is not None:
+              |  for module, name, target in imports:
+              |      exec "from %s import %s as %s" % (module, name, target)
+              |if pythonVars is not None:
+              |  for k, v in pythonVars.iteritems():
+              |    exec "%s = v" % k
+              |class ${className}(object):
+              |  def __init__(self):
+              |    self.call = ${src}
+              |${className}_instance = ${className}()""".stripMargin('|')
   val lazyFunc = new LazyJythonFunc(code, className)
 
   /**
    * Compile this function to a Scala function.
    */
   def toScalaFunc(converter: Any => Any, children: Int): AnyRef = {
-    println("Making for " + code)
     children match {
       case 0 => () => converter(lazyFunc.scalaFunc())
       case 1 => (ar1: AnyRef) => converter(lazyFunc.scalaFunc(ar1))
@@ -103,8 +111,11 @@ ${className}_instance = ${className}()
 /**
  * Since the compiled code functions aren't, delay compilation till we get to the worker
  * but we also want to minimize the number of compiles we do on the workers.
+ *
+ * @params code  The code representing the python class to be evaluated.
+ * @params className  The name of the primary class to be called.
  */
-class LazyJythonFunc(code: String, className: String) extends Serializable {
+private[sql] class LazyJythonFunc(code: String, className: String) extends Serializable {
   @transient lazy val jython = JythonFunc.jython
   @transient lazy val ctx = {
     val sctx = new SimpleScriptContext()
@@ -123,7 +134,10 @@ class LazyJythonFunc(code: String, className: String) extends Serializable {
   }
 }
 
-object JythonConverter {
+/**
+ * Constructs converters for Jython return types to Scala types based on the specified data type.
+ */
+private[sql] object JythonConverter {
   // Needs to be on the worker - not properly serializable.
   @transient lazy val fieldsPyStr = new PyString("__fields__")
 
@@ -173,9 +187,9 @@ object JythonConverter {
 /**
  * Companion object for Jython script engine requirements.
  *
- * Starting the Jython script engine is slow, so try and do it infrequently
+ * Starting the Jython script engine is slow, so try and do it infrequently.
  */
-object JythonFunc {
+private[sql] object JythonFunc {
   lazy val mgr = new ScriptEngineManager()
   lazy val jython = mgr.getEngineByName("python")
 }
