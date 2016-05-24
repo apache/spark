8b78d3f83328e24b86af65ee63f446ef7ebf4047
Imran Rashid <irashid@cloudera.com>
2016-05-23 21:28:42 -0500

    more notes

--- core/src/test/scala/org/apache/spark/scheduler/SchedulerPerformanceSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/SchedulerPerformanceSuite.scala
@@ -57,8 +57,10 @@ class SchedulerPerformanceSuite extends SchedulerIntegrationSuite[MultiExecutorM
     var itrs = 0
     val totalMs = withBackend(backend) {
       val start = System.currentTimeMillis()
-      while (System.currentTimeMillis() - start < 30000 ) {
+      while (System.currentTimeMillis() - start < 10000 ) {
+//        while (System.currentTimeMillis() - start < 10000  && itrs == 0) {
         withClue(s"failure in iteration = $itrs") {
+          val itrStart = System.currentTimeMillis()
           val jobFuture = submit(simpleWorkload(N), (0 until N).toArray)
           // Note: Do not call Await.ready(future) because that calls `scala.concurrent.blocking`,
           // which causes concurrent SQL executions to fail if a fork-join pool is used. Note that
@@ -66,6 +68,10 @@ class SchedulerPerformanceSuite extends SchedulerIntegrationSuite[MultiExecutorM
           // safe to pass in null here. For more detail, see SPARK-13747.
           val awaitPermission = null.asInstanceOf[scala.concurrent.CanAwait]
           jobFuture.ready(Duration.Inf)(awaitPermission)
+          // scalastyle:off println
+          println(s"Iteration $itrs finished in" +
+            s" ${Utils.msDurationToString(System.currentTimeMillis() - itrStart)}")
+          // scalastyle:on println
           assertDataStructuresEmpty(noFailure = true)
           itrs += 1
         }
@@ -231,45 +237,87 @@ class SchedulerPerformanceSuite extends SchedulerIntegrationSuite[MultiExecutorM
   }
 
   val badExecs = (0 until 2).map{_.toString}.toSet
-  val badHosts = Set[String]()
 
   // note this is *very* unlikely to succeed without blacklisting, even though its only
   // one bad executor out of 20.  When a task fails, it gets requeued immediately -- and guess
   // which is the only executor which has a free slot?  Bingo, the one it just failed on
-  testScheduler(
-    "COMPARE D bad execs with simple blacklist",
-    extraConfs = Seq(
-      "spark.scheduler.executorTaskBlacklistTime" -> "10000000",
-      "spark.scheduler.blacklist.advancedStrategy" -> "false",
-      "spark.testing.nHosts" -> "2",
-      "spark.testing.nExecutorsPerHost" -> "2"
-    )
-  ) {
-    runBadExecJob(100, badExecs, badHosts)
+  Seq(
+    ("bad execs with simple blacklist", "false", Set[String]()),
+    ("bad execs with advanced blacklist", "true", Set[String]()),
+    ("bad hosts with advanced blacklist", "true", Set[String]("host-0"))
+  ).foreach { case (name, strategy, badHosts) =>
+    testScheduler(
+      s"COMPARE D $name",
+      extraConfs = Seq(
+        "spark.scheduler.executorTaskBlacklistTime" -> "10000000",
+        "spark.scheduler.blacklist.advancedStrategy" -> strategy
+      )
+    ) {
+      runBadExecJob(3000, badExecs, badHosts)
+    }
   }
 
-  testScheduler(
-    "COMPARE D bad execs with advanced blacklist",
-    extraConfs = Seq(
-      "spark.scheduler.executorTaskBlacklistTime" -> "10000000",
-      "spark.scheduler.blacklist.advancedStrategy" -> "true",
-      "spark.testing.nHosts" -> "2",
-      "spark.testing.nExecutorsPerHost" -> "2"
-    )
-  ) {
-    runBadExecJob(100, badExecs, badHosts)
-  }
 
-  testScheduler(
-    "COMPARE D bad host with advanced blacklist",
-    extraConfs = Seq(
-      "spark.scheduler.executorTaskBlacklistTime" -> "10000000",
-      "spark.scheduler.blacklist.advancedStrategy" -> "true",
-      "spark.testing.nHosts" -> "2",
-      "spark.testing.nExecutorsPerHost" -> "2"
-    )
-  ) {
-    runBadExecJob(100, badExecs, Set("host-0"))
-  }
+  /*
+  Here's how you can get into really slow scheduling, even with the simple blacklist.  Say there
+  is just one bad executor.  You've got a bunch of tasks to run, and you schedule all available
+  slots.  Then one task fails on your bad executor.  You don't re-schedule that task on the bad
+  executor, but you do think you've got one open slot, so you try to find the next task you can
+  schedule.  Since you've got a massive backlog of tasks, you just take the next task and schedule
+  it on your bad executor.  The task fails again.
+
+  This repeats a while, and now you've gone through and failed a bunch of tasks on this one bad
+  executor.  But each time, you clear the cache of invalid executors, so you do a bunch of work
+  to recompute the set of OK executors.  This is *really* expensive, and doesn't help you at all
+  anyway.
+
+
+
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO BlacklistTracker: Blacklisting executors Set() for task StageAndPartition(8,38)
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO TaskSetManager: Starting task 38.0 in stage 8.0 (TID 21056, host-2, partition 38, PROCESS_LOCAL, 5112 bytes)
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO BlacklistTracker: Blacklisting executors Set() for task StageAndPartition(8,39)
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO TaskSetManager: Starting task 39.0 in stage 8.0 (TID 21057, host-0, partition 39, PROCESS_LOCAL, 5112 bytes)
+16/05/23 20:53:57.871 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.871 mock backend thread INFO BlacklistTracker: Blacklisting executors Set() for task StageAndPartition(8,40)
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO DAGScheduler: ShuffleMapStage 5 (RDD at SchedulerIntegrationSuite.scala:360) finished in 1.731 s
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO DAGScheduler: looking for newly runnable stages
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO DAGScheduler: running: Set(ShuffleMapStage 8)
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO DAGScheduler: waiting: Set(ResultStage 9, ShuffleMapStage 6)
+16/05/23 20:53:57.871 dag-scheduler-event-loop INFO DAGScheduler: failed: Set()
+16/05/23 20:53:57.872 mock backend thread INFO TaskSetManager: Starting task 40.0 in stage 8.0 (TID 21058, host-0, partition 40, PROCESS_LOCAL, 5112 bytes)
+16/05/23 20:53:57.872 task-result-getter-2 WARN TaskSetManager: Lost task 39.0 in stage 8.0 (TID 21057, host-0): java.lang.RuntimeException: bad exec 1
+        at org.apache.spark.scheduler.SchedulerPerformanceSuite.backendWithBadExecs(SchedulerPerformanceSuite.scala:218)
+        at org.apache.spark.scheduler.SchedulerPerformanceSuite$$anonfun$runBadExecJob$1.apply$mcV$sp(SchedulerPerformanceSuite.scala:236)
+        at org.apache.spark.scheduler.SchedulerIntegrationSuite$$anon$2.run(SchedulerIntegrationSuite.scala:194)
+
+16/05/23 20:53:57.872 task-result-getter-2 INFO BlacklistTracker: invalidating blacklist cache
+16/05/23 20:53:57.872 dag-scheduler-event-loop INFO DAGScheduler: Submitting ShuffleMapStage 6 (MockRDD 5), which has no missing parents
+16/05/23 20:53:57.872 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.872 mock backend thread INFO BlacklistTracker: Blacklisting executors Set(1) for task StageAndPartition(8,39)
+16/05/23 20:53:57.872 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.872 mock backend thread INFO BlacklistTracker: Blacklisting executors Set() for task StageAndPartition(8,41)
+16/05/23 20:53:57.872 mock backend thread INFO TaskSetManager: Starting task 41.0 in stage 8.0 (TID 21059, host-0, partition 41, PROCESS_LOCAL, 5112 bytes)
+16/05/23 20:53:57.872 task-result-getter-3 INFO TaskSetManager: Lost task 40.0 in stage 8.0 (TID 21058) on executor host-0: java.lang.RuntimeException (bad exec 1) [duplicate 1]
+16/05/23 20:53:57.872 task-result-getter-3 INFO BlacklistTracker: invalidating blacklist cache
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set(1) for task StageAndPartition(8,40)
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set(1) for task StageAndPartition(8,39)
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set() for task StageAndPartition(8,42)
+16/05/23 20:53:57.873 mock backend thread INFO TaskSetManager: Starting task 42.0 in stage 8.0 (TID 21060, host-0, partition 42, PROCESS_LOCAL, 5112 bytes)
+16/05/23 20:53:57.873 task-result-getter-1 INFO TaskSetManager: Lost task 41.0 in stage 8.0 (TID 21059) on executor host-0: java.lang.RuntimeException (bad exec 1) [duplicate 2]
+16/05/23 20:53:57.873 task-result-getter-1 INFO BlacklistTracker: invalidating blacklist cache
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set(1) for task StageAndPartition(8,41)
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set(1) for task StageAndPartition(8,40)
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set(1) for task StageAndPartition(8,39)
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting nodes Set() for stage 8
+16/05/23 20:53:57.873 mock backend thread INFO BlacklistTracker: Blacklisting executors Set() for task StageAndPartition(8,43)
+
+   */
 
 }
