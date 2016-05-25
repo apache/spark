5ac9b9ba3094714f1d153a3f96690fa312b794ae
wm624@hotmail.com <wm624@hotmail.com>
2016-05-24 22:25:49 -0700

    remove extra space and add ...

--- R/pkg/R/DataFrame.R
+++ R/pkg/R/DataFrame.R
@@ -1446,9 +1446,9 @@ setMethod("[", signature(x = "SparkDataFrame"),
 setMethod("subset", signature(x = "SparkDataFrame"),
           function(x, subset, select, drop = F, ...) {
             if (missing(subset)) {
-                x[, select, drop = drop]
-            } else  {
-                x[subset, select, drop = drop]
+                x[, select, drop = drop, ...]
+            } else {
+                x[subset, select, drop = drop, ...]
             }
           })
 
