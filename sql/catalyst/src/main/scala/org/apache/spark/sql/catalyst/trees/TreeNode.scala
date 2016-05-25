8307b348b62f0653cccd04cd543cf77281bc4b85
Huaxin Gao <huaxing@us.ibm.com>
2016-05-24 17:11:53 -0700

    [SPARK-15491][SQL]fix assertion failure for JDBC DataFrame to JSON

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/trees/TreeNode.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/trees/TreeNode.scala
@@ -594,7 +594,9 @@ abstract class TreeNode[BaseType <: TreeNode[BaseType]] extends Product {
     // returns null if the product type doesn't have a primary constructor, e.g. HiveFunctionWrapper
     case p: Product => try {
       val fieldNames = getConstructorParameterNames(p.getClass)
-      val fieldValues = p.productIterator.toSeq
+      val fieldValues = fieldNames.map { name =>
+        p.getClass.getMethod(name).invoke(p)
+      }
       assert(fieldNames.length == fieldValues.length)
       ("product-class" -> JString(p.getClass.getName)) :: fieldNames.zip(fieldValues).map {
         case (name, value) => name -> parseToJson(value)
