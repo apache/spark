7ee79f350ccb876305505f8d6aba2fee51f8fffa
Marcelo Vanzin <vanzin@cloudera.com>
2016-05-26 16:42:39 -0700

    Use flatMap.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -277,24 +277,24 @@ object SparkBuild extends PomBuild {
     // additional discussion and explanation.
     javacOptions in (Compile, compile) ++= Seq(
       "-target", javacJVMVersion.value
-    ) ++ sys.env.get("JAVA_7_HOME").map { jdk7 =>
+    ) ++ sys.env.get("JAVA_7_HOME").toSeq.flatMap { jdk7 =>
       if (javacJVMVersion.value == "1.7") {
         Seq("-bootclasspath", s"$jdk7/jre/lib/rt.jar")
       } else {
         Nil
       }
-    }.getOrElse(Nil),
+    },
 
     scalacOptions in Compile ++= Seq(
       s"-target:jvm-${scalacJVMVersion.value}",
       "-sourcepath", (baseDirectory in ThisBuild).value.getAbsolutePath  // Required for relative source links in scaladoc
-    ) ++ sys.env.get("JAVA_7_HOME").map { jdk7 =>
+    ) ++ sys.env.get("JAVA_7_HOME").toSeq.flatMap { jdk7 =>
       if (javacJVMVersion.value == "1.7") {
         Seq("-javabootclasspath", s"$jdk7/jre/lib/rt.jar")
       } else {
         Nil
       }
-    }.getOrElse(Nil),
+    },
 
     // Implements -Xfatal-warnings, ignoring deprecation warnings.
     // Code snippet taken from https://issues.scala-lang.org/browse/SI-8410.
