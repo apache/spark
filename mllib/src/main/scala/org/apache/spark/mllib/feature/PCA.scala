826bcc2b3b6f822e829a5868db08e5f07dc9a2ca
Zheng RuiFeng <ruifengz@foxmail.com>
2016-05-27 19:18:12 +0800

    fix one nit

--- mllib/src/main/scala/org/apache/spark/mllib/feature/PCA.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/feature/PCA.scala
@@ -40,8 +40,9 @@ class PCA @Since("1.4.0") (@Since("1.4.0") val k: Int) {
    */
   @Since("1.4.0")
   def fit(sources: RDD[Vector]): PCAModel = {
-    require(k < sources.first().size,
-      s"source vector size is ${sources.first().size} must be greater than k=$k")
+    val numFeatures = sources.first().size
+    require(k < numFeatures,
+      s"source vector size is $numFeatures must be greater than k=$k")
 
     val mat = new RowMatrix(sources)
     val (pc, explainedVariance) = mat.computePrincipalComponentsAndExplainedVariance(k)
