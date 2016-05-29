5bdc4470f4b2531aba94b1c67ac9a479e34c945c
gatorsmile <gatorsmile@gmail.com>
2016-05-28 20:47:39 -0700

    revert it back

--- sql/core/src/test/scala/org/apache/spark/sql/execution/joins/BroadcastJoinSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/joins/BroadcastJoinSuite.scala
@@ -48,7 +48,7 @@ class BroadcastJoinSuite extends QueryTest with SQLTestUtils {
       .setMaster("local-cluster[2,1,1024]")
       .setAppName("testing")
     val sc = new SparkContext(conf)
-    spark = SparkSession.builder().config(sc.getConf).getOrCreate()
+    spark = SparkSession.builder.getOrCreate()
   }
 
   override def afterAll(): Unit = {
