e56c715b676397ee712ed8f32c51f1a3705f919a
Holden Karau <holden@us.ibm.com>
2016-06-09 17:47:03 -0700

    Start working on Python 2.6 support (although supposed to be dropped in 2.1 anyways - see SPARK-12661 originally slated for 2.0)

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1803,12 +1803,12 @@ class UserDefinedJythonFunction(object):
                 return v.__module__.startswith("pyspark")
 
             # Sort out PySpark and non PySpark requirements
-            req_vars = {k: v for k, v in req.items() if not isClass(v) or not isInternal(v)}
-            req_imports = {k: v for k, v in req.items() if isClass(v) and isInternal(v)}
+            req_vars = dict((k, v) for k, v in req.items() if not isClass(v) or not isInternal(v))
+            req_imports = dict((k, v) for k, v in req.items() if isClass(v) and isInternal(v))
             if req_vars:
                 serialized_vars = b64encode(ser.dumps(req_vars)).decode("utf-8")
             if req_imports:
-                formatted_imports = {(v.__module__, v.__name__, k) for k, v in req_imports.items()}
+                formatted_imports = list((v.__module__, v.__name__, k) for k, v in req_imports.items())
                 serialized_imports = b64encode(ser.dumps(formatted_imports)).decode("utf-8")
 
         ctx = SQLContext.getOrCreate(sc)
