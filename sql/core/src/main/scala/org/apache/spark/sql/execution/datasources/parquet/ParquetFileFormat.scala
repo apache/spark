4f3ee3cccba78911530767feef99a07794428b73
Takeshi YAMAMURO <linguin.m.s@gmail.com>
2016-06-11 13:05:13 +0900

    Update how to decide numParallelism

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetFileFormat.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetFileFormat.scala
@@ -797,8 +797,7 @@ private[sql] object ParquetFileFormat extends Logging {
 
     // Set the number of partitions to prevent following schema reads from generating many tasks
     // in case of a small number of parquet files.
-    val numParallelism = Math.min(partialFileStatusInfo.size + 1,
-      sparkSession.sparkContext.defaultParallelism)
+    val numParallelism = Math.min(partialFileStatusInfo.size + 1, 10000)
 
     // Issues a Spark job to read Parquet schema in parallel.
     val partiallyMergedSchemas =
