84ed71ad89f3571f317694fd5803ce341a3b46b8
gatorsmile <gatorsmile@gmail.com>
2016-06-11 09:00:58 -0700

    updated the test cases

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/InsertIntoHiveTableSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/InsertIntoHiveTableSuite.scala
@@ -167,59 +167,71 @@ class InsertIntoHiveTableSuite extends QueryTest with TestHiveSingleton with Bef
   }
 
   test("INSERT OVERWRITE - partition IF NOT EXISTS") {
-    val tmpDir = Utils.createTempDir()
-    val selQuery = "select c1, p1, p2 from table_with_partition"
-    sql(
-      s"""
-         |CREATE TABLE table_with_partition(c1 string)
-         |PARTITIONED by (p1 string,p2 string)
-         |location '${tmpDir.toURI.toString}'
-        """.stripMargin)
-    sql(
-      """
-        |INSERT OVERWRITE TABLE table_with_partition
-        |partition (p1='a',p2='b')
-        |SELECT 'blarr'
-      """.stripMargin)
+    withTempDir { tmpDir =>
+      val table = "table_with_partition"
+      withTable(table) {
+        val selQuery = s"select c1, p1, p2 from $table"
+        sql(
+          s"""
+             |CREATE TABLE $table(c1 string)
+             |PARTITIONED by (p1 string,p2 string)
+             |location '${tmpDir.toURI.toString}'
+           """.stripMargin)
+        sql(
+          s"""
+             |INSERT OVERWRITE TABLE $table
+             |partition (p1='a',p2='b')
+             |SELECT 'blarr'
+           """.stripMargin)
+        checkAnswer(
+          sql(selQuery),
+          Row("blarr", "a", "b"))
 
-    checkAnswer(
-      sql(selQuery),
-      Row("blarr", "a", "b"))
+        sql(
+          s"""
+             |INSERT OVERWRITE TABLE $table
+             |partition (p1='a',p2='b')
+             |SELECT 'blarr2'
+           """.stripMargin)
+        checkAnswer(
+          sql(selQuery),
+          Row("blarr2", "a", "b"))
 
-    sql(
-      """
-        |INSERT OVERWRITE TABLE table_with_partition
-        |partition (p1='a',p2='b')
-        |SELECT 'blarr2'
-      """.stripMargin)
+        var e = intercept[AnalysisException] {
+          sql(
+            s"""
+               |INSERT OVERWRITE TABLE $table
+               |partition (p1='a',p2) IF NOT EXISTS
+               |SELECT 'blarr3', 'newPartition'
+             """.stripMargin)
+        }
+        assert(e.getMessage.contains(
+          "Dynamic partitions do not support IF NOT EXISTS. Specified partitions with value: [p2]"))
 
-    checkAnswer(
-      sql(selQuery),
-      Row("blarr2", "a", "b"))
+        e = intercept[AnalysisException] {
+          sql(
+            s"""
+               |INSERT OVERWRITE TABLE $table
+               |partition (p1='a',p2) IF NOT EXISTS
+               |SELECT 'blarr3', 'b'
+             """.stripMargin)
+        }
+        assert(e.getMessage.contains(
+          "Dynamic partitions do not support IF NOT EXISTS. Specified partitions with value: [p2]"))
 
-    val e = intercept[AnalysisException] {
-      sql(
-        """
-          |INSERT OVERWRITE TABLE table_with_partition
-          |partition (p1='a',p2) IF NOT EXISTS
-          |SELECT 'blarr3'
-        """.stripMargin)
+        // If the partition already exists, the insert will overwrite the data
+        // unless users specify IF NOT EXISTS
+        sql(
+          s"""
+             |INSERT OVERWRITE TABLE $table
+             |partition (p1='a',p2='b') IF NOT EXISTS
+             |SELECT 'blarr3'
+           """.stripMargin)
+        checkAnswer(
+          sql(selQuery),
+          Row("blarr2", "a", "b"))
+      }
     }
-    assert(e.getMessage.contains(
-      "Dynamic partitions do not support IF NOT EXISTS. Specified partitions with value: [p2]"))
-
-    // If the partition already exists, the insert will overwrite the data
-    // unless users specify IF NOT EXISTS
-    sql(
-      """
-        |INSERT OVERWRITE TABLE table_with_partition
-        |partition (p1='a',p2='b') IF NOT EXISTS
-        |SELECT 'blarr3'
-      """.stripMargin)
-
-    checkAnswer(
-      sql(selQuery),
-      Row("blarr2", "a", "b"))
   }
 
   test("Insert ArrayType.containsNull == false") {
