b3f5539b45f86309b17394a9f7ba88d82dcd124f
avulanov <nashb@yandex.ru>
2016-06-11 18:45:31 -0700

    Test scalastyle fix

--- mllib/src/test/scala/org/apache/spark/ml/feature/StackedAutoencoderSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/StackedAutoencoderSuite.scala
@@ -20,8 +20,8 @@ package org.apache.spark.ml.feature
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.ml.util.TestingUtils._
+import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.sql.Row
 
 class StackedAutoencoderSuite extends SparkFunSuite with MLlibTestSparkContext {
