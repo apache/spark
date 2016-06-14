d15000ac5c0aaf058413e5d7c64a327b0be80e20
Tom Magrino <tmagrino@fb.com>
2016-06-14 12:23:24 -0700

    Changes to address UI consistency comments by @ajbozarth.

--- core/src/main/scala/org/apache/spark/ui/jobs/StagePage.scala
+++ core/src/main/scala/org/apache/spark/ui/jobs/StagePage.scala
@@ -1194,7 +1194,7 @@ private[ui] class TaskDataSource(
         override def compare(x: TaskTableRowData, y: TaskTableRowData): Int =
           Ordering.String.compare(x.error, y.error)
       }
-      case "Logs" => new Ordering[TaskTableRowData] {
+      case "Executor Logs" => new Ordering[TaskTableRowData] {
         override def compare(x: TaskTableRowData, y: TaskTableRowData): Int =
           if (x.logs.isEmpty == y.logs.isEmpty) {
             return 0
@@ -1312,7 +1312,7 @@ private[ui] class TaskPagedTable(
           Nil
         }} ++
         Seq(("Errors", "")) ++
-        Seq(("Logs", ""))
+        Seq(("Executor Logs", ""))
 
     if (!taskHeadersAndCssClasses.map(_._1).contains(sortColumn)) {
       throw new IllegalArgumentException(s"Unknown column: $sortColumn")
@@ -1401,9 +1401,7 @@ private[ui] class TaskPagedTable(
       }}
       {errorMessageCell(task.error)}
       <td>
-      {if (task.logs.isEmpty) {
-        "No Logs Found"
-      } else task.logs.map {
+      {task.logs.map {
         case (logName, logUrl) => <div><a href={logUrl}>{logName}</a></div>
       }}
       </td>
