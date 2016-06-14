398161285321c976e850f18b486e512efbe0d24e
Holden Karau <holden@us.ibm.com>
2016-06-13 19:21:48 -0700

    oook lets try 86ing mathjax but... welll w/e

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -32,7 +32,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'epytext',
-    'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
