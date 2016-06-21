d842309c749b817bdacb6a57bccba74cc7c0fbf4
Holden Karau <holden@us.ibm.com>
2016-06-21 10:32:57 -0700

    Revert "Support both methods"
    This reverts commit 4431daa9552426f02a993778eb5a5f2c2d87b183.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
@@ -179,25 +179,19 @@ private[hive] class TestHiveSparkSession(
   hiveFilesTemp.mkdir()
   ShutdownHookManager.registerShutdownDeleteDir(hiveFilesTemp)
 
-  lazy val inRepoTests = {
-    if (System.getProperty("user.dir").endsWith("sql" + File.separator + "hive")) {
-      new File("src" + File.separator + "test" + File.separator + "resources" + File.separator)
-    } else {
-      new File("sql" + File.separator + "hive" + File.separator + "src" + File.separator + "test" +
-        File.separator + "resources")
-    }
+  val inRepoTests = if (System.getProperty("user.dir").endsWith("sql" + File.separator + "hive")) {
+    new File("src" + File.separator + "test" + File.separator + "resources" + File.separator)
+  } else {
+    new File("sql" + File.separator + "hive" + File.separator + "src" + File.separator + "test" +
+      File.separator + "resources")
   }
 
   def getHiveFile(path: String): File = {
-    // Attempt to load from class loader, fall back to old system property based.
-    val resourcePath = Option(Thread.currentThread().getContextClassLoader.getResource(path))
-    resourcePath.map(rp => new File(rp.getFile)).getOrElse{
-      val stripped = path.replaceAll("""\.\.\/""", "").replace('/', File.separatorChar)
-        hiveDevHome
-        .map(new File(_, stripped))
-        .filter(_.exists)
-        .getOrElse(new File(inRepoTests, stripped))
-    }
+    val stripped = path.replaceAll("""\.\.\/""", "").replace('/', File.separatorChar)
+    hiveDevHome
+      .map(new File(_, stripped))
+      .filter(_.exists)
+      .getOrElse(new File(inRepoTests, stripped))
   }
 
   val describedTable = "DESCRIBE (\\w+)".r
