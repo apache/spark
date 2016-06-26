87ecf4df128f710215d42840450792659d060190
杨浩 <yanghaogn@163.com>
2016-06-26 16:25:18 +0800

    avoid overflow
    As explained in https://github.com/apache/spark/pull/2874
    
    ```
    The max size of the input seq was Int.MaxValue - 1
    ```

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -30,7 +30,7 @@ object SparkPi {
       .appName("Spark Pi")
       .getOrCreate()
     val slices = if (args.length > 0) args(0).toInt else 2
-    val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
+    val n = math.min(100000L * slices, Int.MaxValue - 1).toInt // avoid overflow
     val count = spark.sparkContext.parallelize(1 to n, slices).map { i =>
       val x = random * 2 - 1
       val y = random * 2 - 1
