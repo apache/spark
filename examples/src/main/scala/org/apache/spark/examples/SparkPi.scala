f91e55eb6a9c81189292989a40d9a4e76dc9309c
杨浩 <yanghaogn@163.com>
2016-06-26 12:12:56 +0800

    make rdd count be n

--- examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -31,7 +31,7 @@ object SparkPi {
       .getOrCreate()
     val slices = if (args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
-    val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
+    val count = spark.sparkContext.parallelize(1 to n, slices).map { i =>
       val x = random * 2 - 1
       val y = random * 2 - 1
       if (x*x + y*y < 1) 1 else 0
