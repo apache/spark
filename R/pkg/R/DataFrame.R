c86d29b2e6bfda05124c20ba3c6db9275c24faa8
WeichenXu <WeichenXu123@outlook.com>
2016-06-28 12:12:20 -0700

    [MINOR][SPARKR] update sparkR DataFrame.R comment
    ## What changes were proposed in this pull request?
    
    update sparkR DataFrame.R comment
    SQLContext ==> SparkSession
    
    ## How was this patch tested?
    
    N/A
    
    Author: WeichenXu <WeichenXu123@outlook.com>
    
    Closes #13946 from WeichenXu123/sparkR_comment_update_sparkSession.
    
    (cherry picked from commit d59ba8e30751bbf91d49f5530b8242a12bbfb569)
    Signed-off-by: Shivaram Venkataraman <shivaram@cs.berkeley.edu>

--- R/pkg/R/DataFrame.R
+++ R/pkg/R/DataFrame.R
@@ -466,7 +466,7 @@ setMethod("createOrReplaceTempView",
 
 #' (Deprecated) Register Temporary Table
 #'
-#' Registers a SparkDataFrame as a Temporary Table in the SQLContext
+#' Registers a SparkDataFrame as a Temporary Table in the SparkSession
 #' @param x A SparkDataFrame
 #' @param tableName A character vector containing the name of the table
 #'
@@ -493,7 +493,7 @@ setMethod("registerTempTable",
 
 #' insertInto
 #'
-#' Insert the contents of a SparkDataFrame into a table registered in the current SQL Context.
+#' Insert the contents of a SparkDataFrame into a table registered in the current SparkSession.
 #'
 #' @param x A SparkDataFrame
 #' @param tableName A character vector containing the name of the table
