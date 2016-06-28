35438fb0ad3bcda5c5a3a0ccde1a620699d012db
Davies Liu <davies@databricks.com>
2016-06-28 14:09:38 -0700

    [SPARK-16175] [PYSPARK] handle None for UDT
    ## What changes were proposed in this pull request?
    
    Scala UDT will bypass all the null and will not pass them into serialize() and deserialize() of UDT, this PR update the Python UDT to do this as well.
    
    ## How was this patch tested?
    
    Added tests.
    
    Author: Davies Liu <davies@databricks.com>
    
    Closes #13878 from davies/udt_null.

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -648,10 +648,13 @@ class UserDefinedType(DataType):
         return cls._cached_sql_type
 
     def toInternal(self, obj):
-        return self._cachedSqlType().toInternal(self.serialize(obj))
+        if obj is not None:
+            return self._cachedSqlType().toInternal(self.serialize(obj))
 
     def fromInternal(self, obj):
-        return self.deserialize(self._cachedSqlType().fromInternal(obj))
+        v = self._cachedSqlType().fromInternal(obj)
+        if v is not None:
+            return self.deserialize(v)
 
     def serialize(self, obj):
         """
