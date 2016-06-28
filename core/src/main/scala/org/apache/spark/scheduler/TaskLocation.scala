0aed80a6721f83c57b74d93dd225d29cf6e8e937
Tom Magrino <tmagrino@fb.com>
2016-06-28 01:01:00 -0700

    Refactored code and added test case.
    In response to @srowen and @zsxwing's comments.

--- core/src/main/scala/org/apache/spark/scheduler/TaskLocation.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskLocation.scala
@@ -64,18 +64,21 @@ private[spark] object TaskLocation {
 
   /**
    * Create a TaskLocation from a string returned by getPreferredLocations.
-   * These strings have the form [executorLocationTag][hostname][executorid], [hostname], or
+   * These strings have the form executor_[hostname]_[executorid], [hostname], or
    * hdfs_cache_[hostname], depending on whether the location is cached.
    */
   def apply(str: String): TaskLocation = {
     val hstr = str.stripPrefix(inMemoryLocationTag)
     if (hstr.equals(str)) {
       if (str.startsWith(executorLocationTag)) {
-        val splits = str.split("_", 3)
-        if (splits.length != 3) {
+        val hostAndExecutorId = str.stripPrefix(executorLocationTag)
+        val splits = hostAndExecutorId.split("_", 2)
+        if (splits.length != 2) {
           throw new IllegalArgumentException("Illegal executor location format: " + str)
         }
-        new ExecutorCacheTaskLocation(splits(1), splits(2))
+        val host = splits(0)
+        val executorId = splits(1)
+        new ExecutorCacheTaskLocation(host, executorId)
       } else {
         new HostTaskLocation(str)
       }
