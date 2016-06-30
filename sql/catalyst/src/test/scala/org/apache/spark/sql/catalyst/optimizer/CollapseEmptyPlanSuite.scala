3350b6661f6d11a1f234ee3fc6c7ec9ffba19382
Dongjoon Hyun <dongjoon@apache.org>
2016-06-30 02:29:00 -0700

    Improve Joins according to the comments.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/CollapseEmptyPlanSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/CollapseEmptyPlanSuite.scala
@@ -20,7 +20,7 @@ package org.apache.spark.sql.catalyst.optimizer
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.dsl.expressions._
 import org.apache.spark.sql.catalyst.dsl.plans._
-import org.apache.spark.sql.catalyst.plans.{FullOuter, Inner, LeftAnti, PlanTest}
+import org.apache.spark.sql.catalyst.plans._
 import org.apache.spark.sql.catalyst.plans.logical.{LocalRelation, LogicalPlan}
 import org.apache.spark.sql.catalyst.rules.RuleExecutor
 
@@ -28,15 +28,85 @@ class CollapseEmptyPlanSuite extends PlanTest {
   object Optimize extends RuleExecutor[LogicalPlan] {
     val batches =
       Batch("CollapseEmptyPlan", Once,
+        CombineUnions,
+        ReplaceDistinctWithAggregate,
+        ReplaceExceptWithAntiJoin,
+        ReplaceIntersectWithSemiJoin,
         PushDownPredicate,
         PruneFilters,
         CollapseEmptyPlan) :: Nil
   }
 
+  object OptimizeWithoutCollapseEmptyPlan extends RuleExecutor[LogicalPlan] {
+    val batches =
+      Batch("CollapseEmptyPlan", Once,
+        CombineUnions,
+        ReplaceDistinctWithAggregate,
+        ReplaceExceptWithAntiJoin,
+        ReplaceIntersectWithSemiJoin,
+        PushDownPredicate,
+        PruneFilters) :: Nil
+  }
+
   val testRelation1 = LocalRelation.fromExternalRows(Seq('a.int), data = Seq(Row(1)))
   val testRelation2 = LocalRelation.fromExternalRows(Seq('b.int), data = Seq(Row(1)))
+  val testRelation3 = LocalRelation.fromExternalRows(Seq('c.int), data = Seq(Row(1)))
 
-  test("Collapse one empty local relation") {
+  test("Binary(or Higher) Logical Plans - Collapse empty union") {
+    val query = testRelation1
+      .where(false)
+      .union(testRelation2.where(false))
+
+    val optimized = Optimize.execute(query.analyze)
+    val correctAnswer = LocalRelation('a.int)
+
+    comparePlans(optimized, correctAnswer)
+  }
+
+  test("Binary Logical Plans - Collapse joins") {
+    // Testcases are tuples of (left predicate, right predicate, joinType, correct answer)
+    // Note that `None` is used to compare with OptimizeWithoutCollapseEmptyPlan.
+    val testcases = Seq(
+      (true, true, Inner, None),
+      (true, true, LeftOuter, None),
+      (true, true, RightOuter, None),
+      (true, true, FullOuter, None),
+      (true, true, LeftAnti, None),
+      (true, true, LeftSemi, None),
+
+      (true, false, Inner, Some(LocalRelation('a.int, 'b.int))),
+      (true, false, LeftOuter, None),
+      (true, false, RightOuter, Some(LocalRelation('a.int, 'b.int))),
+      (true, false, FullOuter, None),
+      (true, false, LeftAnti, None),
+      (true, false, LeftSemi, None),
+
+      (false, true, Inner, Some(LocalRelation('a.int, 'b.int))),
+      (false, true, LeftOuter, Some(LocalRelation('a.int, 'b.int))),
+      (false, true, RightOuter, None),
+      (false, true, FullOuter, None),
+      (false, true, LeftAnti, Some(LocalRelation('a.int))),
+      (false, true, LeftSemi, Some(LocalRelation('a.int))),
+
+      (false, false, Inner, Some(LocalRelation('a.int, 'b.int))),
+      (false, false, LeftOuter, Some(LocalRelation('a.int, 'b.int))),
+      (false, false, RightOuter, Some(LocalRelation('a.int, 'b.int))),
+      (false, false, FullOuter, Some(LocalRelation('a.int, 'b.int))),
+      (false, false, LeftAnti, Some(LocalRelation('a.int))),
+      (false, false, LeftSemi, Some(LocalRelation('a.int)))
+    )
+
+    testcases.foreach { case (left, right, jt, answer) =>
+      val query = testRelation1
+        .where(left)
+        .join(testRelation2.where(right), joinType = jt, condition = Some('a.attr == 'b.attr))
+      val optimized = Optimize.execute(query.analyze)
+      val correctAnswer = answer.getOrElse(OptimizeWithoutCollapseEmptyPlan.execute(query.analyze))
+      comparePlans(optimized, correctAnswer)
+    }
+  }
+
+  test("Unary Logical Plans - Collapse one empty local relation") {
     val query = testRelation1
       .where(false)
       .select('a)
@@ -50,7 +120,7 @@ class CollapseEmptyPlanSuite extends PlanTest {
     comparePlans(optimized, correctAnswer)
   }
 
-  test("Don't collapse one non-empty local relation") {
+  test("Unary Logical Plans - Don't collapse one non-empty local relation") {
     val query = testRelation1
       .where(true)
       .groupBy('a)('a)
@@ -68,52 +138,7 @@ class CollapseEmptyPlanSuite extends PlanTest {
     comparePlans(optimized, correctAnswer.analyze)
   }
 
-  test("Collapse one non-empty and one empty local relations INNER join") {
-    val query = testRelation1
-      .where(true)
-      .join(testRelation2.where(false), joinType = Inner, condition = Some('a.attr == 'b.attr))
-
-    val optimized = Optimize.execute(query.analyze)
-    val correctAnswer = LocalRelation('a.int, 'b.int).analyze
-
-    comparePlans(optimized, correctAnswer.analyze)
-  }
-
-  test("Collapse two empty local relations join") {
-    val query = testRelation1
-      .where(false)
-      .join(testRelation2.where(false), joinType = FullOuter, condition = Some('a.attr == 'b.attr))
-
-    val optimized = Optimize.execute(query.analyze)
-    val correctAnswer = LocalRelation('a.int, 'b.int)
-
-    comparePlans(optimized, correctAnswer)
-  }
-
-  test("Don't collapse one non-empty and one empty local relations LeftAnti join") {
-    val query = testRelation1
-      .where(true)
-      .join(testRelation2.where(false), joinType = LeftAnti, condition = Some('a.attr == 'b.attr))
-
-    val optimized = Optimize.execute(query.analyze)
-    val correctAnswer = testRelation1
-      .join(LocalRelation('b.int), joinType = LeftAnti, condition = Some('a.attr == 'b.attr))
-
-    comparePlans(optimized, correctAnswer.analyze)
-  }
-
-  test("Don't collapse two non-empty local relations") {
-    val query = testRelation1
-      .join(testRelation2, condition = Some('a.attr == 'b.attr))
-
-    val optimized = Optimize.execute(query.analyze)
-    val correctAnswer = testRelation1
-      .join(testRelation2, condition = Some('a.attr == 'b.attr))
-
-    comparePlans(optimized, correctAnswer.analyze)
-  }
-
-  test("Collapse non-aggregating expressions on empty plan") {
+  test("Unary Logical Plans - Collapse non-aggregating expressions on empty plan") {
     val query = testRelation1
       .where(false)
       .groupBy('a)('a)
@@ -124,7 +149,18 @@ class CollapseEmptyPlanSuite extends PlanTest {
     comparePlans(optimized, correctAnswer)
   }
 
-  test("Don't collapse aggregating expressions on empty plan") {
+  test("Unary Logical Plans - Collapse non-aggregating complex expressions on empty plan") {
+    val query = testRelation1
+      .where(false)
+      .groupBy('a)('a, ('a + 1).as('x))
+
+    val optimized = Optimize.execute(query.analyze)
+    val correctAnswer = LocalRelation('a.int, 'x.int).analyze
+
+    comparePlans(optimized, correctAnswer)
+  }
+
+  test("Unary Logical Plans - Don't collapse aggregating expressions on empty plan") {
     val query = testRelation1
       .where(false)
       .groupBy('a)(count('a))
