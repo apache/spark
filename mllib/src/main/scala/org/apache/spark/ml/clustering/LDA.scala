11a077cd3e86c169465375c24ac50ad28801f2e2
Yuhao Yang <yuhao.yang@intel.com>
2016-06-30 09:39:22 +0800

    style and since

--- mllib/src/main/scala/org/apache/spark/ml/clustering/LDA.scala
+++ mllib/src/main/scala/org/apache/spark/ml/clustering/LDA.scala
@@ -880,12 +880,13 @@ class LDA @Since("1.6.0") (
   }
 }
 
-
+@Since("2.0.0")
 object LDA extends DefaultParamsReadable[LDA] {
 
   /** Get dataset for spark.mllib LDA */
-  private[clustering] def getOldDataset(dataset: Dataset[_], featuresCol: String):
-      RDD[(Long, OldVector)] = {
+  private[clustering] def getOldDataset(
+       dataset: Dataset[_],
+       featuresCol: String): RDD[(Long, OldVector)] = {
     dataset
       .withColumn("docId", monotonicallyIncreasingId())
       .select("docId", featuresCol)
