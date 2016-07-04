6782ad94a300b8ca0cb0515e43efcea583df0601
Sandeep Singh <sandeep@techaddict.me>
2016-07-04 12:37:24 +0530

    mapjoin_subquery2 added

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -1565,6 +1565,49 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
       |drop table if exists sales;
       |drop table if exists things;
     """.stripMargin)
+
+  createQueryTest("mapjoin_subquery2",
+    """
+      |drop table if exists x;
+      |drop table if exists y;
+      |drop table if exists z;
+      |
+      |CREATE TABLE x (name STRING, id INT)
+      |ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+      |
+      |CREATE TABLE y (id INT, name STRING)
+      |ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+      |
+      |CREATE TABLE z (id INT, name STRING)
+      |ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t';
+      |
+      |load data local inpath '../../data/files/x.txt' INTO TABLE x;
+      |load data local inpath '../../data/files/y.txt' INTO TABLE y;
+      |load data local inpath '../../data/files/z.txt' INTO TABLE z;
+      |
+      |set hive.auto.convert.join=true;
+      |set hive.auto.convert.join.noconditionaltask=true;
+      |set hive.auto.convert.join.noconditionaltask.size=10000;
+      |
+      |-- Since the inputs are small, it should be automatically converted to mapjoin
+      |
+      |EXPLAIN
+      |SELECT subq.key1, subq.value1, subq.key2, subq.value2, z.id, z.name
+      |FROM
+      |(SELECT x.id as key1, x.name as value1, y.id as key2, y.name as value2
+      | FROM y JOIN x ON (x.id = y.id)) subq
+      | JOIN z ON (subq.key1 = z.id);
+      |
+      |SELECT subq.key1, subq.value1, subq.key2, subq.value2, z.id, z.name
+      |FROM
+      |(SELECT x.id as key1, x.name as value1, y.id as key2, y.name as value2
+      | FROM y JOIN x ON (x.id = y.id)) subq
+      | JOIN z ON (subq.key1 = z.id);
+      |
+      |drop table if exists x;
+      |drop table if exists y;
+      |drop table if exists z;
+    """.stripMargin)
   // scalastyle:on
 }
 
