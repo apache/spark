7ff3301c36be24e6178ad1939559250e08bee910
Sandeep Singh <sandeep@techaddict.me>
2016-07-04 13:43:50 +0530

    varchar_join1 added

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -2280,6 +2280,44 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
       |drop table if exists varchar_2;
     """.stripMargin)
 
+  createQueryTest("varchar_join1",
+    """
+      |drop table if exists varchar_join1_vc1;
+      |drop table if exists varchar_join1_vc2;
+      |drop table if exists varchar_join1_str;
+      |
+      |create table  varchar_join1_vc1 (
+      |  c1 int,
+      |  c2 varchar(10)
+      |);
+      |
+      |create table  varchar_join1_vc2 (
+      |  c1 int,
+      |  c2 varchar(20)
+      |);
+      |
+      |create table  varchar_join1_str (
+      |  c1 int,
+      |  c2 string
+      |);
+      |
+      |load data local inpath '../../data/files/vc1.txt' into table varchar_join1_vc1;
+      |load data local inpath '../../data/files/vc1.txt' into table varchar_join1_vc2;
+      |load data local inpath '../../data/files/vc1.txt' into table varchar_join1_str;
+      |
+      |-- Join varchar with same length varchar
+      |select * from varchar_join1_vc1 a join varchar_join1_vc1 b on (a.c2 = b.c2) order by a.c1;
+      |
+      |-- Join varchar with different length varchar
+      |select * from varchar_join1_vc1 a join varchar_join1_vc2 b on (a.c2 = b.c2) order by a.c1;
+      |
+      |-- Join varchar with string
+      |select * from varchar_join1_vc1 a join varchar_join1_str b on (a.c2 = b.c2) order by a.c1;
+      |
+      |drop table if exists varchar_join1_vc1;
+      |drop table if exists varchar_join1_vc2;
+      |drop table if exists varchar_join1_str;
+    """.stripMargin)
   // scalastyle:on
 }
 
