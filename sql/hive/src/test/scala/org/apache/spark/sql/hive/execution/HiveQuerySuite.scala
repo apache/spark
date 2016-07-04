7105d5676d51ec4a7f22d2088d7d5b8c6f78c72c
Sandeep Singh <sandeep@techaddict.me>
2016-07-04 12:30:08 +0530

    lateral_view_cp added

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -1527,6 +1527,18 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
       |drop table if exists insert_compressed;
     """.stripMargin)
 
+  createQueryTest("lateral_view_cp",
+    """
+      |--HIVE 3226
+      |drop table if exists array_valued_src;
+      |create table array_valued_src (key string, value array<string>);
+      |insert overwrite table array_valued_src select key, array(value) from src;
+      |
+      |-- replace sel(*) to sel(exprs) for reflecting CP result properly
+      |explain select count(val) from (select a.key as key, b.value as array_val from src a join array_valued_src b on a.key=b.key) i lateral view explode (array_val) c as val;
+      |select count(val) from (select a.key as key, b.value as array_val from src a join array_valued_src b on a.key=b.key) i lateral view explode (array_val) c as val;
+    """.stripMargin)
+
   // scalastyle:on
 }
 
