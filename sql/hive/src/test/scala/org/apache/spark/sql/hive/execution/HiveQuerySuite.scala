770e796679fae1e2ff306ef565a3af53d4e14cd5
Sandeep Singh <sandeep@techaddict.me>
2016-07-04 13:30:04 +0530

    udaf_covar_pop added

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -2085,6 +2085,26 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
       |select t,key,value from timestamp_lazy distribute by t sort by key ASC, value ASC;
     """.stripMargin)
 
+  createQueryTest("udaf_covar_pop",
+    """
+      |DROP TABLE IF EXISTS covar_tab;
+      |CREATE TABLE covar_tab (a INT, b INT, c INT)
+      |ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
+      |STORED AS TEXTFILE;
+      |LOAD DATA LOCAL INPATH '../../data/files/covar_tab.txt' OVERWRITE
+      |INTO TABLE covar_tab;
+      |
+      |DESCRIBE FUNCTION covar_pop;
+      |DESCRIBE FUNCTION EXTENDED covar_pop;
+      |SELECT covar_pop(b, c) FROM covar_tab WHERE a < 1;
+      |SELECT covar_pop(b, c) FROM covar_tab WHERE a < 3;
+      |SELECT covar_pop(b, c) FROM covar_tab WHERE a = 3;
+      |SELECT a, covar_pop(b, c) FROM covar_tab GROUP BY a ORDER BY a;
+      |SELECT covar_pop(b, c) FROM covar_tab;
+      |
+      |DROP TABLE IF EXISTS covar_tab;
+    """.stripMargin)
+
   // scalastyle:on
 }
 
