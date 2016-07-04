7dbffcdd6dc76b8e8d6a9cd6eeb24323a6b740c3
Michael Allman <michael@videoamp.com>
2016-07-04 21:16:17 +0100

    [SPARK-16353][BUILD][DOC] Missing javadoc options for java unidoc
    Link to Jira issue: https://issues.apache.org/jira/browse/SPARK-16353
    
    ## What changes were proposed in this pull request?
    
    The javadoc options for the java unidoc generation are ignored when generating the java unidoc. For example, the generated `index.html` has the wrong HTML page title. This can be seen at http://spark.apache.org/docs/latest/api/java/index.html.
    
    I changed the relevant setting scope from `doc` to `(JavaUnidoc, unidoc)`.
    
    ## How was this patch tested?
    
    I ran `docs/jekyll build` and verified that the java unidoc `index.html` has the correct HTML page title.
    
    Author: Michael Allman <michael@videoamp.com>
    
    Closes #14031 from mallman/spark-16353.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -723,8 +723,7 @@ object Unidoc {
         .map(_.filterNot(_.getCanonicalPath.contains("org/apache/hadoop")))
     },
 
-    // Javadoc options: create a window title, and group key packages on index page
-    javacOptions in doc := Seq(
+    javacOptions in (JavaUnidoc, unidoc) := Seq(
       "-windowtitle", "Spark " + version.value.replaceAll("-SNAPSHOT", "") + " JavaDoc",
       "-public",
       "-noqualifier", "java.lang"
