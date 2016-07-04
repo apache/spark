cc689104f803c2bdf1a257b7edb06f126a9ab918
Sandeep Singh <sandeep@techaddict.me>
2016-07-04 13:37:03 +0530

    union_date added

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -2206,6 +2206,40 @@ class HiveQuerySuite extends HiveComparisonTest with BeforeAndAfter {
       |select * from t8 order by c1;
     """.stripMargin)
 
+  createQueryTest("union_date",
+    """
+      |drop table if exists union_date_1;
+      |drop table if exists union_date_2;
+      |
+      |create table union_date_1 (
+      |  ORIGIN_CITY_NAME string,
+      |  DEST_CITY_NAME string,
+      |  FL_DATE date,
+      |  ARR_DELAY float,
+      |  FL_NUM int
+      |);
+      |
+      |create table union_date_2 (
+      |  ORIGIN_CITY_NAME string,
+      |  DEST_CITY_NAME string,
+      |  FL_DATE date,
+      |  ARR_DELAY float,
+      |  FL_NUM int
+      |);
+      |
+      |LOAD DATA LOCAL INPATH '../../data/files/flights_join.txt' OVERWRITE INTO TABLE union_date_1;
+      |LOAD DATA LOCAL INPATH '../../data/files/flights_join.txt' OVERWRITE INTO TABLE union_date_2;
+      |
+      |select * from (
+      |  select fl_num, fl_date from union_date_1
+      |  union all
+      |  select fl_num, fl_date from union_date_2
+      |) union_result order by fl_date, fl_num;
+      |
+      |drop table if exists union_date_1;
+      |drop table if exists union_date_2;
+    """.stripMargin)
+
   // scalastyle:on
 }
 
