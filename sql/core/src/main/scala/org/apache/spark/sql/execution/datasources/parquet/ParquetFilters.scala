ffaa666673e91a771f1d58267d085483ed68e56c
hyukjinkwon <gurwls223@gmail.com>
2016-07-06 19:26:32 +0900

    Simply do not find nested ones

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetFilters.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetFilters.scala
@@ -188,12 +188,6 @@ private[sql] object ParquetFilters {
       fields.filter { f =>
         !f.metadata.contains(StructType.metadataKeyForOptionalField) ||
           !f.metadata.getBoolean(StructType.metadataKeyForOptionalField)
-      }.flatMap { f =>
-        f.dataType match {
-          case IntegerType | LongType | FloatType => Some(f)
-          case DoubleType | StringType | BinaryType | BooleanType => Some(f)
-          case _ => None
-        }
       }.map(f => f.name -> f.dataType)
     case _ => Array.empty[(String, DataType)]
   }
