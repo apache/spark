97a158e9e91c800b2be7682d6cc77b86d047626c
Dongjoon Hyun <dongjoon@apache.org>
2016-07-07 14:57:59 -0700

    [SPARK-16425][R] `describe()` should not fail with non-numeric columns

--- R/pkg/inst/tests/testthat/test_sparkSQL.R
+++ R/pkg/inst/tests/testthat/test_sparkSQL.R
@@ -1804,11 +1804,11 @@ test_that("describe() and summarize() on a DataFrame", {
   expect_equal(collect(stats)[2, "age"], "24.5")
   expect_equal(collect(stats)[3, "age"], "7.7781745930520225")
   stats <- describe(df)
-  expect_equal(collect(stats)[4, "name"], "Andy")
+  expect_equal(columns(summary(df)), c("summary", "age"))
   expect_equal(collect(stats)[5, "age"], "30")
 
   stats2 <- summary(df)
-  expect_equal(collect(stats2)[4, "name"], "Andy")
+  expect_equal(columns(summary(df)), c("summary", "age"))
   expect_equal(collect(stats2)[5, "age"], "30")
 
   # Test base::summary is working
