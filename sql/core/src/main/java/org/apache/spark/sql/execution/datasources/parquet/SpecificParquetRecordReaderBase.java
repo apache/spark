b6f395b2b661eaca6fe406d56a5fee43d7afa415
Andrew Duffy <aduffy@palantir.com>
2016-07-12 21:41:22 +0200

    Fix for Parquet filter pushdown
    Use of previous deprecated constructor defaults to null metadata, which
    prevents pushdown from reaching the Parquet level.

--- sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/SpecificParquetRecordReaderBase.java
+++ sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/SpecificParquetRecordReaderBase.java
@@ -205,8 +205,7 @@ public abstract class SpecificParquetRecordReaderBase<T> extends RecordReader<Vo
       }
     }
     this.sparkSchema = new ParquetSchemaConverter(config).convert(requestedSchema);
-    this.reader = new ParquetFileReader(
-        config, footer.getFileMetaData(), file, blocks, requestedSchema.getColumns());
+    this.reader = new ParquetFileReader(config, footer.getFileMetaData(), file, blocks, requestedSchema.getColumns());
     for (BlockMetaData block : blocks) {
       this.totalRowCount += block.getRowCount();
     }
