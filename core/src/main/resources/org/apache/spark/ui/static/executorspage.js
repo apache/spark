5cf514d4a9047955028b450d33263cb863987112
Kishor Patil <kpatil@yahoo-inc.com>
2016-07-13 03:36:50 +0000

    Fixed decimal precision for disk usage displayed

--- core/src/main/resources/org/apache/spark/ui/static/executorspage.js
+++ core/src/main/resources/org/apache/spark/ui/static/executorspage.js
@@ -47,11 +47,11 @@ function formatStatus(status, type) {
     }
 }
 
-function formatBytes(bytes,type) {
+function formatBytes(bytes, type) {
     if(type !== 'display') return bytes;
-    if(bytes == 0) return '0 B';
+    if(bytes == 0) return '0.0 B';
     var k = 1000;
-    var dm = 3;
+    var dm = 1;
     var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     var i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
