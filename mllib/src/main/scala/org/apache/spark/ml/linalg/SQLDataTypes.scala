4b2199476d60740cb8adbe1e0bfb5e949860132f
Joseph K. Bradley <joseph@databricks.com>
2016-07-13 13:51:27 -0700

    added Since version to SQLDataTypes

--- mllib/src/main/scala/org/apache/spark/ml/linalg/SQLDataTypes.scala
+++ mllib/src/main/scala/org/apache/spark/ml/linalg/SQLDataTypes.scala
@@ -17,13 +17,14 @@
 
 package org.apache.spark.ml.linalg
 
-import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.annotation.{DeveloperApi, Since}
 import org.apache.spark.sql.types.DataType
 
 /**
  * :: DeveloperApi ::
  * SQL data types for vectors and matrices.
  */
+@Since("2.0.0")
 @DeveloperApi
 object SQLDataTypes {
 
