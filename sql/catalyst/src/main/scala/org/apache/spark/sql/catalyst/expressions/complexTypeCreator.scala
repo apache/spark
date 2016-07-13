34db9cf5357e478d3f31133015fc8a9cbe14ace8
Sandeep Singh <sandeep@techaddict.me>
2016-07-13 09:17:42 +0530

    fix inputdatatype check

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/complexTypeCreator.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/complexTypeCreator.scala
@@ -420,12 +420,14 @@ case class StringToMap(text: Expression, pairDelim: Expression, keyValueDelim: E
 
   override def checkInputDataTypes(): TypeCheckResult = {
     if (children.map(_.dataType).forall(_ == StringType)) {
-      TypeCheckResult.TypeCheckSuccess
-    } else if (Seq(pairDelim, keyValueDelim).forall(_.foldable)) {
-      TypeCheckResult.TypeCheckFailure(s"String To Map's all arguments must be of type string.")
+      if (Seq(pairDelim, keyValueDelim).forall(_.foldable)) {
+        TypeCheckResult.TypeCheckSuccess
+      } else {
+        TypeCheckResult.TypeCheckFailure(
+          s"String To Map's arguments must be foldable, but got $children.")
+      }
     } else {
-      TypeCheckResult.TypeCheckFailure(
-        s"String To Map's arguments must be foldable, but got $children.")
+      TypeCheckResult.TypeCheckFailure(s"String To Map's all arguments must be of type string.")
     }
   }
 
