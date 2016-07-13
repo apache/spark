c7059880c2ce771a380dec9d3821dbc497fd501e
Sandeep Singh <sandeep@techaddict.me>
2016-07-13 22:38:38 +0530

    fix error

--- sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala
@@ -417,6 +417,6 @@ class StringFunctionsSuite extends QueryTest with SharedSQLContext {
       sql("select str_to_map(null,null)").collect()
     }.getMessage
 
-    assert(Seq(m1, m2, m3).forall(_.contains("both delimiters must be of type string.")))
+    assert(Seq(m1, m2, m3).forall(_.contains("all arguments must be of type string.")))
   }
 }
