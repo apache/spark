8ec635fe7403baf5149e3f6714872bf706b37cd7
Liwei Lin <lwlin7@gmail.com>
2016-07-15 10:12:02 +0800

    Fix foreachPartition

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/ForeachSink.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/ForeachSink.scala
@@ -21,6 +21,7 @@ import org.apache.spark.TaskContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Dataset, Encoder, ForeachWriter}
 import org.apache.spark.sql.catalyst.plans.logical.CatalystSerde
+import org.apache.spark.sql.execution.SQLExecution
 
 /**
  * A [[Sink]] that forwards all data into [[ForeachWriter]] according to the contract defined by
@@ -47,15 +48,22 @@ class ForeachSink[T : Encoder](writer: ForeachWriter[T]) extends Sink with Seria
     // method supporting incremental planning. But in the long run, we should generally make newly
     // created Datasets use `IncrementalExecution` where necessary (which is SPARK-16264 tries to
     // resolve).
+    //
+    // Another thing worthy noting is, by introducing this `incrementalExecution`, we've also
+    // introduced one extra round of physical planning; refer to SPARK-16545 for details. Again,
+    // let's revisit this when SPARK-16264 is resolved.
 
     val datasetWithIncrementalExecution =
       new Dataset(data.sparkSession, data.logicalPlan, implicitly[Encoder[T]]) {
+
+        private var incrementalExecution: IncrementalExecution = _
+
         override lazy val rdd: RDD[T] = {
           val objectType = exprEnc.deserializer.dataType
           val deserialized = CatalystSerde.deserialize[T](logicalPlan)
 
           // was originally: sparkSession.sessionState.executePlan(deserialized) ...
-          val incrementalExecution = new IncrementalExecution(
+          incrementalExecution = new IncrementalExecution(
             this.sparkSession,
             deserialized,
             data.queryExecution.asInstanceOf[IncrementalExecution].outputMode,
@@ -65,7 +73,27 @@ class ForeachSink[T : Encoder](writer: ForeachWriter[T]) extends Sink with Seria
             rows.map(_.get(0, objectType))
           }.asInstanceOf[RDD[T]]
         }
+
+        /**
+         * This function's original implementation was:
+         * ```
+         *   def foreachPartition(f: Iterator[T] => Unit): Unit = withNewExecutionId {
+         *    rdd.foreachPartition(f)
+         *  }
+         * ```
+         *
+         * Below we overwrite it and provide `withNewExecutionId` with `this.incrementExecution`
+         * rather than with `this.queryExecution`; doing this would save one round of unnecessary
+         * physical planning. Refer to SPARK-16545 for details.
+         */
+        override def foreachPartition(f: Iterator[T] => Unit): Unit = {
+          rdd // call rdd first to set incrementalExecution properly
+          SQLExecution.withNewExecutionId(this.sparkSession, this.incrementalExecution) {
+            rdd.foreachPartition(f)
+          }
+        }
       }
+
     datasetWithIncrementalExecution.foreachPartition { iter =>
       if (writer.open(TaskContext.getPartitionId(), batchId)) {
         var isFailed = false
