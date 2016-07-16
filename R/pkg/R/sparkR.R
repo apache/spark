1f013653d22068c00c199ba7a64ec2acdf07aed5
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-07-16 23:24:01 +0200

    fix warning

--- R/pkg/R/sparkR.R
+++ R/pkg/R/sparkR.R
@@ -156,7 +156,8 @@ sparkR.sparkContext <- function(
   existingPort <- Sys.getenv("EXISTING_SPARKR_BACKEND_PORT", "")
   if (existingPort != "") {
     if (length(sparkPackages) != 0) {
-      warning("sparkPackages has no effect when using spark-submit/sparkR shell, use --packages")
+      warning(paste("sparkPackages has no effect when using spark-submit or sparkR shell",
+      "please use the --packages commandline instead", sep = ","))
     }
     backendPort <- existingPort
   } else {
