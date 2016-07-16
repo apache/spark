c0dca31439c1aed242aca514be050d44a5598f52
krishnakalyan3 <krishnakalyan3@gmail.com>
2016-07-16 19:33:06 +0200

    character limit

--- R/pkg/R/sparkR.R
+++ R/pkg/R/sparkR.R
@@ -156,7 +156,7 @@ sparkR.sparkContext <- function(
   existingPort <- Sys.getenv("EXISTING_SPARKR_BACKEND_PORT", "")
   if (existingPort != "") {
     if (length(sparkPackages) != 0) {
-      warning("sparkPackages has no effect when using spark-submit or sparkR shell, please use the --packages commandline instead")
+      warning("sparkPackages has no effect when using spark-submit or sparkR shell, use the --packages commandline instead")
     }
     backendPort <- existingPort
   } else {
