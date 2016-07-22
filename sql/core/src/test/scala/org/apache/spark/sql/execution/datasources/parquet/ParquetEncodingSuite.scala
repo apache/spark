16e6b91688ad8f73336b7729745189e2bd7f880f
Sameer Agarwal <sameerag@cs.berkeley.edu>
2016-07-22 15:35:45 -0700

    style

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetEncodingSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetEncodingSuite.scala
@@ -18,9 +18,9 @@ package org.apache.spark.sql.execution.datasources.parquet
 
 import scala.collection.JavaConverters._
 
-import org.apache.spark.sql.test.SharedSQLContext
-
 import org.apache.parquet.hadoop.ParquetOutputFormat
+
+import org.apache.spark.sql.test.SharedSQLContext
 
 // TODO: this needs a lot more testing but it's currently not easy to test with the parquet
 // writer abstractions. Revisit.
