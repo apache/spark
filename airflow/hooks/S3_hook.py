2c931abb36826f96caefcc2663da8262816a5b1d
J C Lawrence <claw@kanga.nu>
2016-07-26 09:47:36 -0400

    [AIRFLOW-362] Import __future__ division
    Fix integer division fencepost error on S3 chunked transfers
    
    Closes #1683 from clearclaw/master

--- airflow/hooks/S3_hook.py
+++ airflow/hooks/S3_hook.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import division
 from future import standard_library
 standard_library.install_aliases()
 import logging
