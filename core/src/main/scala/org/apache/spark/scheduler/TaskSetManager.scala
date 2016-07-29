3ed44fcfa2824ed1b9f9522138e231e61d347949
Sumedh Wale <swale@snappydata.io>
2016-07-29 10:30:42 +0530

    [SNAPPYDATA] Try hard to not schedule on others if ExecutorCacheTaskLocation is alive

--- core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -312,7 +312,9 @@ private[spark] class TaskSetManager(
 
       // Check for node-local tasks
       if (TaskLocality.isAllowed(locality, TaskLocality.NODE_LOCAL)) {
-        for (index <- speculatableTasks if canRunOnHost(index)) {
+        for (index <- speculatableTasks if canRunOnHost(index) &&
+            // don't return executor-local tasks that are still alive
+            canRunOnExecutor(execId, index)) {
           val locations = tasks(index).preferredLocations.map(_.host)
           if (locations.contains(host)) {
             speculatableTasks -= index
@@ -335,7 +337,9 @@ private[spark] class TaskSetManager(
       // Check for rack-local tasks
       if (TaskLocality.isAllowed(locality, TaskLocality.RACK_LOCAL)) {
         for (rack <- sched.getRackForHost(host)) {
-          for (index <- speculatableTasks if canRunOnHost(index)) {
+          for (index <- speculatableTasks if canRunOnHost(index)
+            // don't return executor-local tasks that are still alive
+            if canRunOnExecutor(execId, index)) {
             val racks = tasks(index).preferredLocations.map(_.host).flatMap(sched.getRackForHost)
             if (racks.contains(rack)) {
               speculatableTasks -= index
@@ -347,7 +351,9 @@ private[spark] class TaskSetManager(
 
       // Check for non-local tasks
       if (TaskLocality.isAllowed(locality, TaskLocality.ANY)) {
-        for (index <- speculatableTasks if canRunOnHost(index)) {
+        for (index <- speculatableTasks if canRunOnHost(index) &&
+            // don't return executor-local tasks that are still alive
+            canRunOnExecutor(execId, index)) {
           speculatableTasks -= index
           return Some((index, TaskLocality.ANY))
         }
@@ -355,6 +361,17 @@ private[spark] class TaskSetManager(
     }
 
     None
+  }
+
+  private def canRunOnExecutor(execId: String, taskId: Int): Boolean = {
+    val locations = tasks(taskId).preferredLocations
+    locations.isEmpty || locations.exists {
+      case e: ExecutorCacheTaskLocation => execId == e.executorId
+      case _ => false
+    } || locations.collectFirst {
+      case e: ExecutorCacheTaskLocation if sched.isExecutorAlive(e.executorId)
+          && !executorIsBlacklisted(e.executorId, taskId) => false
+    }.getOrElse(true)
   }
 
   /**
@@ -371,7 +388,9 @@ private[spark] class TaskSetManager(
     }
 
     if (TaskLocality.isAllowed(maxLocality, TaskLocality.NODE_LOCAL)) {
-      for (index <- dequeueTaskFromList(execId, getPendingTasksForHost(host))) {
+      for (index <- dequeueTaskFromList(execId, getPendingTasksForHost(host))
+        // don't return executor-local tasks that are still alive
+        if canRunOnExecutor(execId, index)) {
         return Some((index, TaskLocality.NODE_LOCAL, false))
       }
     }
@@ -387,13 +406,17 @@ private[spark] class TaskSetManager(
       for {
         rack <- sched.getRackForHost(host)
         index <- dequeueTaskFromList(execId, getPendingTasksForRack(rack))
+        // don't return executor-local tasks that are still alive
+        if canRunOnExecutor(execId, index)
       } {
         return Some((index, TaskLocality.RACK_LOCAL, false))
       }
     }
 
     if (TaskLocality.isAllowed(maxLocality, TaskLocality.ANY)) {
-      for (index <- dequeueTaskFromList(execId, allPendingTasks)) {
+      for (index <- dequeueTaskFromList(execId, allPendingTasks)
+        // don't return executor-local tasks that are still alive
+        if canRunOnExecutor(execId, index)) {
         return Some((index, TaskLocality.ANY, false))
       }
     }
