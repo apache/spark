9853aa7407a1b57fbf64a57df8e19afa46941340
Sean Owen <sowen@cloudera.com>
2016-08-07 12:14:57 +0100

    Default random seed for ML classes should be random not fixed to class hashCode

--- mllib/src/main/scala/org/apache/spark/ml/param/shared/SharedParamsCodeGen.scala
+++ mllib/src/main/scala/org/apache/spark/ml/param/shared/SharedParamsCodeGen.scala
@@ -67,7 +67,7 @@ private[shared] object SharedParamsCodeGen {
         isValid = "ParamValidators.inArray(Array(\"skip\", \"error\"))"),
       ParamDesc[Boolean]("standardization", "whether to standardize the training features" +
         " before fitting the model", Some("true")),
-      ParamDesc[Long]("seed", "random seed", Some("this.getClass.getName.hashCode.toLong")),
+      ParamDesc[Long]("seed", "random seed", Some("new java.util.Random().nextLong()")),
       ParamDesc[Double]("elasticNetParam", "the ElasticNet mixing parameter, in range [0, 1]." +
         " For alpha = 0, the penalty is an L2 penalty. For alpha = 1, it is an L1 penalty",
         isValid = "ParamValidators.inRange(0, 1)"),
