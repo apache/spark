7c01da8085416673e7abf77b9e45107daf80fcf1
Jeff Zhang <zjffdu@apache.org>
2016-08-10 15:43:50 +0800

    fix code style

--- python/pyspark/ml/linalg/__init__.py
+++ python/pyspark/ml/linalg/__init__.py
@@ -519,7 +519,6 @@ class SparseVector(Vector):
                         "Indices %s and %s are not strictly increasing"
                         % (self.indices[i], self.indices[i + 1]))
 
-
         assert np.max(self.indices) < self.size, \
             "Index %d is out of the the size of vector with size=%d" \
             % (np.max(self.indices), self.size)
