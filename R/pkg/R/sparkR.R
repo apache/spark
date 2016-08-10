214ba66a030bc3a718c567a742b0db44bf911d61
Junyang Qian <junyangq@databricks.com>
2016-08-10 11:18:23 -0700

    [SPARK-16579][SPARKR] add install.spark function
    ## What changes were proposed in this pull request?
    
    Add an install_spark function to the SparkR package. User can run `install_spark()` to install Spark to a local directory within R.
    
    Updates:
    
    Several changes have been made:
    
    - `install.spark()`
        - check existence of tar file in the cache folder, and download only if not found
        - trial priority of mirror_url look-up: user-provided -> preferred mirror site from apache website -> hardcoded backup option
        - use 2.0.0
    
    - `sparkR.session()`
        - can install spark when not found in `SPARK_HOME`
    
    ## How was this patch tested?
    
    Manual tests, running the check-cran.sh script added in #14173.
    
    Author: Junyang Qian <junyangq@databricks.com>
    
    Closes #14258 from junyangq/SPARK-16579.

--- R/pkg/R/sparkR.R
+++ R/pkg/R/sparkR.R
@@ -365,6 +365,23 @@ sparkR.session <- function(
     }
     overrideEnvs(sparkConfigMap, paramMap)
   }
+  # do not download if it is run in the sparkR shell
+  if (!nzchar(master) || is_master_local(master)) {
+    if (!is_sparkR_shell()) {
+      if (is.na(file.info(sparkHome)$isdir)) {
+        msg <- paste0("Spark not found in SPARK_HOME: ",
+                      sparkHome,
+                      " .\nTo search in the cache directory. ",
+                      "Installation will start if not found.")
+        message(msg)
+        packageLocalDir <- install.spark()
+        sparkHome <- packageLocalDir
+      } else {
+        msg <- paste0("Spark package is found in SPARK_HOME: ", sparkHome)
+        message(msg)
+      }
+    }
+  }
 
   if (!exists(".sparkRjsc", envir = .sparkREnv)) {
     sparkExecutorEnvMap <- new.env()
