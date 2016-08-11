2f4566ce8b93e9cc3da40cd70bedae2e93c2c147
avulanov <nashb@yandex.ru>
2016-08-11 12:27:07 +0300

    Fix warehousePath for 2.0

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -691,8 +691,10 @@ private[sql] class SQLConf extends Serializable with CatalystConf with Logging {
 
   def variableSubstituteDepth: Int = getConf(VARIABLE_SUBSTITUTE_DEPTH)
 
-  def warehousePath: String = new Path(getConf(WAREHOUSE_PATH)).toString
-
+  def warehousePath: String = {
+    new Path(getConf(WAREHOUSE_PATH).replace("${system:user.dir}",
+      System.getProperty("user.dir"))).toString
+  }
   override def orderByOrdinal: Boolean = getConf(ORDER_BY_ORDINAL)
 
   override def groupByOrdinal: Boolean = getConf(GROUP_BY_ORDINAL)
