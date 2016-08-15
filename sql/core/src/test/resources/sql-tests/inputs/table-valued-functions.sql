7bac3af6748e63148368f53f088d765ceef69ac1
Eric Liang <ekl@databricks.com>
2016-08-15 16:43:02 -0700

    Mon Aug 15 16:43:02 PDT 2016

---
+++ sql/core/src/test/resources/sql-tests/inputs/table-valued-functions.sql
@@ -0,0 +1,17 @@
+-- unresolved function
+select * from dummy(3);
+
+-- range call with end
+select * from range(5);
+
+-- range call with start and end
+select * from range(5, 10);
+
+-- range call with step
+select * from range(0, 10, 2);
+
+-- range call with numPartitions
+select * from range(0, 10, 1, 200);
+
+-- range call error
+select * from range(2, 'x');
