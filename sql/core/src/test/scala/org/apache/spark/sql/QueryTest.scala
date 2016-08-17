394d5986617e65852422afeb8d755e38795bbe25
Wenchen Fan <wenchen@databricks.com>
2016-08-17 09:31:22 -0700

    [SPARK-17102][SQL] bypass UserDefinedGenerator for json format check
    ## What changes were proposed in this pull request?
    
    We use reflection to convert `TreeNode` to json string, and currently don't support arbitrary object. `UserDefinedGenerator` takes a function object, so we should skip json format test for it, or the tests can be flacky, e.g. `DataFrameSuite.simple explode`, this test always fail with scala 2.10(branch 1.6 builds with scala 2.10 by default), but pass with scala 2.11(master branch builds with scala 2.11 by default).
    
    ## How was this patch tested?
    
    N/A
    
    Author: Wenchen Fan <wenchen@databricks.com>
    
    Closes #14679 from cloud-fan/json.
    
    (cherry picked from commit 928ca1c6d12b23d84f9b6205e22d2e756311f072)
    Signed-off-by: Yin Huai <yhuai@databricks.com>

--- sql/core/src/test/scala/org/apache/spark/sql/QueryTest.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/QueryTest.scala
@@ -242,9 +242,10 @@ abstract class QueryTest extends PlanTest {
       case p if p.getClass.getSimpleName == "MetastoreRelation" => return
       case _: MemoryPlan => return
     }.transformAllExpressions {
-      case a: ImperativeAggregate => return
+      case _: ImperativeAggregate => return
       case _: TypedAggregateExpression => return
       case Literal(_, _: ObjectType) => return
+      case _: UserDefinedGenerator => return
     }
 
     // bypass hive tests before we fix all corner cases in hive module.
