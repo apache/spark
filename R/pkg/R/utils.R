5540366efad10d0611eb9701d7dcf40ea7be528e
Jeff Zhang <zjffdu@apache.org>
2016-08-18 16:43:52 +0800

    [SPARK-17125][SPARKR] Allow to specify spark config using non-string type in SparkR

--- R/pkg/R/utils.R
+++ R/pkg/R/utils.R
@@ -630,7 +630,7 @@ convertNamedListToEnv <- function(namedList) {
 
   env <- new.env()
   for (name in names) {
-    env[[name]] <- namedList[[name]]
+    env[[name]] <- as.character(namedList[[name]])
   }
   env
 }
