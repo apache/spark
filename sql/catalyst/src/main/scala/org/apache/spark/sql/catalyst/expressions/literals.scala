b482c09fa22c5762a355f95820e4ba3e2517fb77
Reynold Xin <rxin@databricks.com>
2016-08-18 19:02:32 -0700

    HOTFIX: compilation broken due to protected ctor.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/literals.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/literals.scala
@@ -163,8 +163,7 @@ object DecimalLiteral {
 /**
  * In order to do type checking, use Literal.create() instead of constructor
  */
-case class Literal protected (value: Any, dataType: DataType)
-  extends LeafExpression with CodegenFallback {
+case class Literal (value: Any, dataType: DataType) extends LeafExpression with CodegenFallback {
 
   override def foldable: Boolean = true
   override def nullable: Boolean = value == null
