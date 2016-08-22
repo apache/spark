d358569e05a4fc44304374927f01869b8448a028
Jagadeesan <as2@us.ibm.com>
2016-08-22 05:43:25 -0400

    [SPARK-17095] [Documentation] [Latex and Scala doc do not play nicely]

--- mllib/src/main/scala/org/apache/spark/ml/regression/GeneralizedLinearRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/regression/GeneralizedLinearRegression.scala
@@ -196,9 +196,11 @@ class GeneralizedLinearRegression @Since("2.0.0") (@Since("2.0.0") override val 
   /**
    * Sets the regularization parameter for L2 regularization.
    * The regularization term is
-   * {{{
-   *   0.5 * regParam * L2norm(coefficients)^2
-   * }}}
+   * <p><blockquote>
+   *    $$
+   *    0.5 * regParam * L2norm(coefficients)^2
+   *    $$
+   * </blockquote></p>
    * Default is 0.0.
    *
    * @group setParam
