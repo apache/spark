d207eadc27e9d0fb9180572b2415fad10823decf
Weiqing Yang <yangweiqing001@gmail.com>
2016-08-23 10:00:20 -0700

    Make code more readable

--- examples/src/main/java/org/apache/spark/examples/sql/streaming/JavaStructuredNetworkWordCount.java
+++ examples/src/main/java/org/apache/spark/examples/sql/streaming/JavaStructuredNetworkWordCount.java
@@ -63,10 +63,10 @@ public final class JavaStructuredNetworkWordCount {
     // Split the lines into words
     Dataset<String> words = lines.as(Encoders.STRING())
       .flatMap(new FlatMapFunction<String, String>() {
-      @Override
-      public Iterator<String> call(String x) {
-        return Arrays.asList(x.split(" ")).iterator();
-      }
+        @Override
+        public Iterator<String> call(String x) {
+          return Arrays.asList(x.split(" ")).iterator();
+        }
     }, Encoders.STRING());
 
     // Generate running word count
