ce19b11e4dc8fa199edffa8821b895125810f4f2
Xin Ren <iamshrek@126.com>
2016-08-25 17:53:10 -0700

    val plan is not used

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -1452,7 +1452,6 @@ class DataFrameSuite extends QueryTest with SharedSQLContext {
     withSQLConf("spark.sql.autoBroadcastJoinThreshold" -> "2") {
       val df = spark.range(100).toDF()
       val join = df.join(df, "id")
-      val plan = join.queryExecution.executedPlan
       checkAnswer(join, df)
       assert(
         join.queryExecution.executedPlan.collect { case e: ShuffleExchange => true }.size === 1)
