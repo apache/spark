0a47e948d775116e275ad652cdba3c9c4adc1111
Junyang Qian <junyangq@databricks.com>
2016-08-28 00:09:09 -0700

    Fix R style check.

--- R/pkg/R/mllib.R
+++ R/pkg/R/mllib.R
@@ -994,8 +994,10 @@ setMethod("spark.survreg", signature(data = "SparkDataFrame", formula = "formula
 #' @export
 #' @examples
 #' \dontrun{
+#' # nolint start
 #' # An example "path/to/file" can be
 #' # paste0(Sys.getenv("SPARK_HOME"), "/data/mllib/sample_lda_libsvm_data.txt")
+#' # nolint end
 #' text <- read.df("path/to/file", source = "libsvm")
 #' model <- spark.lda(data = text, optimizer = "em")
 #'
