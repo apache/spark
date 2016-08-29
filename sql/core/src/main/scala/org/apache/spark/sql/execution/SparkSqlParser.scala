2db1fbf7ed980747177435290d1cad25ab145aec
wangzhenhua <wangzhenhua@huawei.com>
2016-08-29 20:05:47 +0800

    Revert "fix path"
    This reverts commit aeca9ba4b4bedc8f5a59869cb72123a15d8c4921.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkSqlParser.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkSqlParser.scala
@@ -388,11 +388,9 @@ class SparkSqlAstBuilder(conf: SQLConf) extends AstBuilder {
    * }}}
    */
   override def visitLoadData(ctx: LoadDataContext): LogicalPlan = withOrigin(ctx) {
-    val str = ctx.path.getText
-    val pathStr = str.substring(1, str.length - 1)
     LoadDataCommand(
       table = visitTableIdentifier(ctx.tableIdentifier),
-      path = pathStr,
+      path = string(ctx.path),
       isLocal = ctx.LOCAL != null,
       isOverwrite = ctx.OVERWRITE != null,
       partition = Option(ctx.partitionSpec).map(visitNonOptionalPartitionSpec)
@@ -829,9 +827,7 @@ class SparkSqlAstBuilder(conf: SQLConf) extends AstBuilder {
    * Create location string.
    */
   override def visitLocationSpec(ctx: LocationSpecContext): String = withOrigin(ctx) {
-//    string(ctx.STRING)
-    val str = ctx.STRING().getText
-    str.substring(1, str.length - 1)
+    string(ctx.STRING)
   }
 
   /**
