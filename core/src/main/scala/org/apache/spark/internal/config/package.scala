22ddf01727afa9cf57456c9f198a2bdbd9f1b028
Alex Bozarth <ajbozart@us.ibm.com>
2016-08-30 16:33:54 -0500

    [SPARK-17243][WEB UI] Spark 2.0 History Server won't load with very large application history
    With the new History Server the summary page loads the application list via the the REST API, this makes it very slow to impossible to load with large (10K+) application history. This pr fixes this by adding the `spark.history.ui.maxApplications` conf to limit the number of applications the History Server displays. This is accomplished using a new optional `limit` param for the `applications` api. (Note this only applies to what the summary page displays, all the Application UI's are still accessible if the user knows the App ID and goes to the Application UI directly.)
    
    I've also added a new test for the `limit` param in `HistoryServerSuite.scala`
    
    Manual testing and dev/run-tests
    
    Author: Alex Bozarth <ajbozart@us.ibm.com>
    
    Closes #14835 from ajbozarth/spark17243.

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -108,4 +108,8 @@ package object config {
   private[spark] val UI_RETAINED_TASKS = ConfigBuilder("spark.ui.retainedTasks")
     .intConf
     .createWithDefault(100000)
+
+  // To limit how many applications are shown in the History Server summary ui
+  private[spark] val HISTORY_UI_MAX_APPS =
+    ConfigBuilder("spark.history.ui.maxApplications").intConf.createWithDefault(Integer.MAX_VALUE)
 }
