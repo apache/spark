344af4bab1c343129e2980ccce9fdefa7fa54c96
hbhanawat <hemant9379@gmail.com>
2016-09-09 23:23:43 +0530

    [SNAP-846][CLUSTER] Ensuring that Uncaught exceptions are handled in the Snappy side and do not cause a system.exit (#2)
    Instead of using SparkUncaughtExceptionHandler, executor now gets the uncaught exception handler and uses it to handle the exception. But if it is a local mode, it still uses the SparkUncaughtExceptionHandler
     
    A test has been added in the Snappy side PR for the same.

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -402,7 +402,12 @@ private[spark] class Executor(
           // Don't forcibly exit unless the exception was inherently fatal, to avoid
           // stopping other tasks unnecessarily.
           if (Utils.isFatalError(t)) {
-            SparkUncaughtExceptionHandler.uncaughtException(t)
+            if (!isLocal) {
+              Thread.getDefaultUncaughtExceptionHandler.
+                  uncaughtException(Thread.currentThread(), t)
+            } else {
+              SparkUncaughtExceptionHandler.uncaughtException(t)
+            }
           }
 
       } finally {
