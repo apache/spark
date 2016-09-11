ae335ae05dda586a86e39c82ce4f8cdcf5aaa6d0
gatorsmile <gatorsmile@gmail.com>
2016-09-11 16:59:01 -0700

    address comments.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/ddl.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/ddl.scala
@@ -20,14 +20,15 @@ package org.apache.spark.sql.execution.datasources
 import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.catalog.CatalogTable
-import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.plans.QueryPlan
 import org.apache.spark.sql.catalyst.plans.logical.{Command, LogicalPlan}
 import org.apache.spark.sql.execution.command.RunnableCommand
 import org.apache.spark.sql.types._
 
-case class CreateTable(tableDesc: CatalogTable, mode: SaveMode, query: Option[LogicalPlan])
-  extends LogicalPlan with Command {
+case class CreateTable(
+    tableDesc: CatalogTable,
+    mode: SaveMode,
+    query: Option[LogicalPlan]) extends Command {
   assert(tableDesc.provider.isDefined, "The table to be created must have a provider.")
 
   if (query.isEmpty) {
@@ -35,10 +36,6 @@ case class CreateTable(tableDesc: CatalogTable, mode: SaveMode, query: Option[Lo
       mode == SaveMode.ErrorIfExists || mode == SaveMode.Ignore,
       "create table without data insertion can only use ErrorIfExists or Ignore as SaveMode.")
   }
-
-  override def output: Seq[Attribute] = Seq.empty[Attribute]
-
-  override def children: Seq[LogicalPlan] = Seq.empty[LogicalPlan]
 
   override def innerChildren: Seq[QueryPlan[_]] = query.toSeq
 }
