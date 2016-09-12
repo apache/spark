c17bf4e768be614c806bbd913096538f89b380e5
Peng <peng.meng@intel.com>
2016-09-12 18:15:51 +0800

    minor change

--- mllib/src/test/scala/org/apache/spark/mllib/evaluation/BinaryClassificationMetricsSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/evaluation/BinaryClassificationMetricsSuite.scala
@@ -140,7 +140,8 @@ class BinaryClassificationMetricsSuite extends SparkFunSuite with MLlibTestSpark
         (0.0, 0.0), (0.0, 0.25), (0.2, 0.25), (0.2, 0.5), (0.2, 0.75),
         (0.4, 0.75), (0.6, 0.75), (0.6, 1.0), (0.8, 1.0), (1.0, 1.0),
         (1.0, 1.0)
-      ) == originalROC)
+      ) ==
+      originalROC)
 
     val numBins = 4
 
