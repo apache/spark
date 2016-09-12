a88af25fb820a6bd7f857a62bef50b7c1a816cdb
David Navas <davidn@clearstorydata.com>
2016-09-12 10:58:12 -0700

    Implement BitSet.clearUntil and use it during merge joins

--- core/src/main/scala/org/apache/spark/util/collection/BitSet.scala
+++ core/src/main/scala/org/apache/spark/util/collection/BitSet.scala
@@ -58,6 +58,20 @@ class BitSet(numBits: Int) extends Serializable {
   }
 
   /**
+   * Clear all the bits up to a given index
+   */
+  def clearUntil(bitIndex: Int) {
+    val wordIndex = bitIndex >> 6 // divide by 64
+    var i = 0
+    while(i < wordIndex) { words(i) = 0; i += 1 }
+    if(wordIndex < words.length) {
+      // Set the remaining bits (note that the mask could still be zero)
+      val mask = ~(-1L << (bitIndex & 0x3f))
+      words(wordIndex) &= ~mask
+    }
+  }
+
+  /**
    * Compute the bit-wise AND of the two sets returning the
    * result.
    */
