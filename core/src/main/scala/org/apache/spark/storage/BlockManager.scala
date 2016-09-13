f69a5ea6eff2c6b9f1e07a5d1551c67cdee5ed2e
Josh Rosen <joshrosen@databricks.com>
2016-09-13 15:24:22 -0700

    Consolidate exceptionWasThrown=true path.

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -876,18 +876,19 @@ private[spark] class BlockManager(
           blockInfoManager.unlock(blockId)
         }
       } else {
-        // If an exception was thrown then it's possible that the code in `putBody` has already
-        // notified the master about the availability of this block, so we need to send an update
-        // to remove this block location.
-        removeBlockInternal(
-          blockId, tellMaster = tellMaster && putBlockInfo.tellMaster && exceptionWasThrown)
         if (exceptionWasThrown) {
+          // If an exception was thrown then it's possible that the code in `putBody` has already
+          // notified the master about the availability of this block, so we need to send an update
+          // to remove this block location.
+          removeBlockInternal(blockId, tellMaster = tellMaster)
           // The `putBody` code may have also added a new block status to TaskMetrics, so we need
           // to cancel that out by overwriting it with an empty block status. We only do this if
           // the finally block was entered via an exception because doing this unconditionally would
           // cause us to send empty block statuses for every block that failed to be cached due to
           // a memory shortage (which is an expected failure, unlike an uncaught exception).
           addUpdatedBlockStatusToTaskMetrics(blockId, BlockStatus.empty)
+        } else {
+          removeBlock(blockId, tellMaster = false)
         }
         logWarning(s"Putting block $blockId failed")
       }
