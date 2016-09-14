b3c71274cb0d09f8290246ab28fa13ae8ecced10
David Navas <davidn@clearstorydata.com>
2016-09-14 07:05:18 -0700

    Use Arrays.fill, set function return type, remove redundant mask negation

--- core/src/main/scala/org/apache/spark/util/collection/BitSet.scala
+++ core/src/main/scala/org/apache/spark/util/collection/BitSet.scala
@@ -17,6 +17,8 @@
 
 package org.apache.spark.util.collection
 
+import java.util.Arrays
+
 /**
  * A simple, fixed-size bit set implementation. This implementation is fast because it avoids
  * safety/bound checking.
@@ -35,21 +37,14 @@ class BitSet(numBits: Int) extends Serializable {
   /**
    * Clear all set bits.
    */
-  def clear(): Unit = {
-    var i = 0
-    while (i < numWords) {
-      words(i) = 0L
-      i += 1
-    }
-  }
+  def clear(): Unit = Arrays.fill(words, 0)
 
   /**
    * Set all the bits up to a given index
    */
-  def setUntil(bitIndex: Int) {
+  def setUntil(bitIndex: Int): Unit = {
     val wordIndex = bitIndex >> 6 // divide by 64
-    var i = 0
-    while(i < wordIndex) { words(i) = -1; i += 1 }
+    Arrays.fill(words, 0, wordIndex, -1)
     if(wordIndex < words.length) {
       // Set the remaining bits (note that the mask could still be zero)
       val mask = ~(-1L << (bitIndex & 0x3f))
@@ -60,14 +55,13 @@ class BitSet(numBits: Int) extends Serializable {
   /**
    * Clear all the bits up to a given index
    */
-  def clearUntil(bitIndex: Int) {
+  def clearUntil(bitIndex: Int): Unit = {
     val wordIndex = bitIndex >> 6 // divide by 64
-    var i = 0
-    while(i < wordIndex) { words(i) = 0; i += 1 }
+    Arrays.fill(words, 0, wordIndex, 0)
     if(wordIndex < words.length) {
-      // Set the remaining bits (note that the mask could still be zero)
-      val mask = ~(-1L << (bitIndex & 0x3f))
-      words(wordIndex) &= ~mask
+      // Set the remaining bits
+      val mask = -1L << (bitIndex & 0x3f)
+      words(wordIndex) &= mask
     }
   }
 
