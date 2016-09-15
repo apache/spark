447ab821deeb0500a78080c7d7597705664e3240
Justin Pihony <justin.pihony@gmail.com>
2016-09-15 14:40:29 -0400

    Add semicolons to my java

--- examples/src/main/java/org/apache/spark/examples/sql/JavaSQLDataSourceExample.java
+++ examples/src/main/java/org/apache/spark/examples/sql/JavaSQLDataSourceExample.java
@@ -245,11 +245,11 @@ public class JavaSQLDataSourceExample {
       .option("password", "password")
       .load();
 
-    Properties connectionProperties = new Properties()
-    connectionProperties.put("user", "username")
-    connectionProperties.put("password", "password")
+    Properties connectionProperties = new Properties();
+    connectionProperties.put("user", "username");
+    connectionProperties.put("password", "password");
     Dataset<Row> jdbcDF2 = spark.read
-      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties);
 
     // Saving data to a JDBC source
     jdbcDF.write
@@ -258,10 +258,10 @@ public class JavaSQLDataSourceExample {
       .option("dbtable", "schema.tablename")
       .option("user", "username")
       .option("password", "password")
-      .save()
+      .save();
 
     jdbcDF2.write
-      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties);
     // $example off:jdbc_dataset$
   }
 }
