c9dcdc4058c47a82c3dfa99b6e10aa034981c700
Justin Pihony <justin.pihony@gmail.com>
2016-09-15 14:08:13 -0400

    Documentation for java and python

--- examples/src/main/java/org/apache/spark/examples/sql/JavaSQLDataSourceExample.java
+++ examples/src/main/java/org/apache/spark/examples/sql/JavaSQLDataSourceExample.java
@@ -235,6 +235,8 @@ public class JavaSQLDataSourceExample {
 
   private static void runJdbcDatasetExample(SparkSession spark) {
     // $example on:jdbc_dataset$
+    // Note: JDBC loading and saving can be achieved via either the load/save or jdbc methods
+    // Loading data from a JDBC source
     Dataset<Row> jdbcDF = spark.read()
       .format("jdbc")
       .option("url", "jdbc:postgresql:dbserver")
@@ -242,6 +244,24 @@ public class JavaSQLDataSourceExample {
       .option("user", "username")
       .option("password", "password")
       .load();
+
+    Properties connectionProperties = new Properties()
+    connectionProperties.put("user", "username")
+    connectionProperties.put("password", "password")
+    Dataset<Row> jdbcDF2 = spark.read
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
+
+    // Saving data to a JDBC source
+    jdbcDF.write
+      .format("jdbc")
+      .option("url", "jdbc:postgresql:dbserver")
+      .option("dbtable", "schema.tablename")
+      .option("user", "username")
+      .option("password", "password")
+      .save()
+
+    jdbcDF2.write
+      .jdbc("jdbc:postgresql:dbserver", "schema.tablename", connectionProperties)
     // $example off:jdbc_dataset$
   }
 }
