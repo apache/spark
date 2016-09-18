3357bc76df10e975b94c6dd96d1df6bb30daef5e
Wenchen Fan <wenchen@databricks.com>
2016-09-18 21:15:35 +0800

    [SPARK-17541][SQL] fix some DDL bugs about table management when same-name temp view exists
    ## What changes were proposed in this pull request?
    
    In `SessionCatalog`, we have several operations(`tableExists`, `dropTable`, `loopupRelation`, etc) that handle both temp views and metastore tables/views. This brings some bugs to DDL commands that want to handle temp view only or metastore table/view only. These bugs are:
    
    1. `CREATE TABLE USING` will fail if a same-name temp view exists
    2. `Catalog.dropTempView`will un-cache and drop metastore table if a same-name table exists
    3. `saveAsTable` will fail or have unexpected behaviour if a same-name temp view exists.
    
    These bug fixes are pulled out from https://github.com/apache/spark/pull/14962 and targets both master and 2.0 branch
    
    ## How was this patch tested?
    
    new regression tests
    
    Author: Wenchen Fan <wenchen@databricks.com>
    
    Closes #15099 from cloud-fan/fix-view.

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
@@ -361,7 +361,12 @@ final class DataFrameWriter[T] private[sql](ds: Dataset[T]) {
       throw new AnalysisException("Cannot create hive serde table with saveAsTable API")
     }
 
-    val tableExists = df.sparkSession.sessionState.catalog.tableExists(tableIdent)
+    val sessionState = df.sparkSession.sessionState
+    val db = tableIdent.database.getOrElse(sessionState.catalog.getCurrentDatabase)
+    val tableIdentWithDB = tableIdent.copy(database = Some(db))
+    // Pass a table identifier with database part, so that `tableExists` won't check temp views
+    // unexpectedly.
+    val tableExists = sessionState.catalog.tableExists(tableIdentWithDB)
 
     (tableExists, mode) match {
       case (true, SaveMode.Ignore) =>
@@ -387,7 +392,7 @@ final class DataFrameWriter[T] private[sql](ds: Dataset[T]) {
           bucketSpec = getBucketSpec
         )
         val cmd = CreateTable(tableDesc, mode, Some(df.logicalPlan))
-        df.sparkSession.sessionState.executePlan(cmd).toRdd
+        sessionState.executePlan(cmd).toRdd
     }
   }
 
