2ed1069ea17d4f511a245f59c0479839d483a6c4
Dongjoon Hyun <dongjoon@apache.org>
2016-09-27 11:25:21 -0700

    Ignore EXTENDED and FORMATTED again for `transient_lastDdlTime`.

--- sql/core/src/test/resources/sql-tests/inputs/describe.sql
+++ sql/core/src/test/resources/sql-tests/inputs/describe.sql
@@ -4,15 +4,15 @@ ALTER TABLE t ADD PARTITION (c='Us', d=1);
 
 DESC t;
 
--- Ignore these because there exists timestamp results, e.g., `Create Table`
+-- Ignore these because there exist timestamp results, e.g., `Create Table`.
 -- DESC EXTENDED t;
 -- DESC FORMATTED t;
 
 DESC t PARTITION (c='Us', d=1);
 
-DESC EXTENDED t PARTITION (c='Us', d=1);
-
-DESC FORMATTED t PARTITION (c='Us', d=1);
+-- Ignore these because there exist timestamp results, e.g., transient_lastDdlTime.
+-- DESC EXTENDED t PARTITION (c='Us', d=1);
+-- DESC FORMATTED t PARTITION (c='Us', d=1);
 
 -- NoSuchPartitionException: Partition not found in table
 DESC t PARTITION (c='Us', d=2);
