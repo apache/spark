a3468f45169a9a072d6ae2d730a7aeb062339ec1
Bryan Cutler <cutlerb@gmail.com>
2016-10-06 17:24:11 -0700

    upgraded version of Pyrolite to 4.13 which supports serialization of BinaryType StructField

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -1702,6 +1702,14 @@ class SQLTests(ReusedPySparkTestCase):
             "does_not_exist",
             lambda: spark.catalog.uncacheTable("does_not_exist"))
 
+    def test_BinaryType_serialization(self):
+        # Pyrolite version <= 4.9 could not serialize BinaryType with Python3 SPARK-17808
+        schema = StructType([StructField('mybytes', BinaryType())])
+        data = [[bytearray(b'here is my data')],
+                [bytearray(b'and here is some more')],]
+        df = self.spark.createDataFrame(data, schema=schema)
+        df.collect()
+
 
 class HiveSparkSubmitTests(SparkSubmitTests):
 
