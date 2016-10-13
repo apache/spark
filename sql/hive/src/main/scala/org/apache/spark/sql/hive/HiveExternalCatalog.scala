0a8e51a5e4cfd3275eff12e9fbbeb3fb487990aa
gatorsmile <gatorsmile@gmail.com>
2016-10-13 21:36:39 +0800

    [SPARK-17657][SQL] Disallow Users to Change Table Type
    ### What changes were proposed in this pull request?
    Hive allows users to change the table type from `Managed` to `External` or from `External` to `Managed` by altering table's property `EXTERNAL`. See the JIRA: https://issues.apache.org/jira/browse/HIVE-1329
    
    So far, Spark SQL does not correctly support it, although users can do it. Many assumptions are broken in the implementation. Thus, this PR is to disallow users to change it.
    
    In addition, we also do not allow users to set the property `EXTERNAL` when creating a table.
    
    ### How was this patch tested?
    Added test cases
    
    Author: gatorsmile <gatorsmile@gmail.com>
    
    Closes #15230 from gatorsmile/alterTableSetExternal.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
@@ -112,6 +112,11 @@ private[spark] class HiveExternalCatalog(conf: SparkConf, hadoopConf: Configurat
         s"as table property keys may not start with '$DATASOURCE_PREFIX' or '$STATISTICS_PREFIX':" +
         s" ${invalidKeys.mkString("[", ", ", "]")}")
     }
+    // External users are not allowed to set/switch the table type. In Hive metastore, the table
+    // type can be switched by changing the value of a case-sensitive table property `EXTERNAL`.
+    if (table.properties.contains("EXTERNAL")) {
+      throw new AnalysisException("Cannot set or change the preserved property key: 'EXTERNAL'")
+    }
   }
 
   // --------------------------------------------------------------------------
