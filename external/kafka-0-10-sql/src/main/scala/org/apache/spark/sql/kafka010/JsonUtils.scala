c45ded7109474fcb40f03c772192eb38398f328a
cody koeninger <cody@koeninger.org>
2016-10-13 23:23:02 -0500

    [SPARK-17812][SQL][KAFKA] parse json for topicpartitions and offsets

---
+++ external/kafka-0-10-sql/src/main/scala/org/apache/spark/sql/kafka010/JsonUtils.scala
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.kafka010
+
+import scala.collection.mutable.{ ArrayBuffer, HashMap }
+import scala.util.control.NonFatal
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.kafka.common.TopicPartition
+
+private object JsonUtils {
+  private val mapper = new ObjectMapper()
+
+  def partitions(str: String): Array[TopicPartition] = {
+    try {
+      val res = new ArrayBuffer[TopicPartition]()
+      val topics = mapper.readTree(str).fields
+      while (topics.hasNext) {
+        val node = topics.next
+        val topic = node.getKey
+        val parts = node.getValue.elements
+        while (parts.hasNext) {
+          res.append(new TopicPartition(topic, parts.next().asInt))
+        }
+      }
+      res.toArray
+    } catch {
+      case NonFatal(x) =>
+        throw new IllegalArgumentException(
+          s"""Expected e.g. {"topicA":[0,1],"topicB":[0,1]}, got $str""")
+    }
+  }
+
+  def partitionOffsets(str: String): Map[TopicPartition, Long] = {
+    try {
+      val res = new HashMap[TopicPartition, Long]
+      val topics = mapper.readTree(str).fields
+      while (topics.hasNext) {
+        val node = topics.next
+        val topic = node.getKey
+        val parts = node.getValue.fields
+        while (parts.hasNext) {
+          val node = parts.next
+          val part = node.getKey.toInt
+          val offset = node.getValue.asLong
+          res += new TopicPartition(topic, part) -> offset
+        }
+      }
+      res.toMap
+    } catch {
+      case NonFatal(x) =>
+        throw new IllegalArgumentException(
+          s"""Expected e.g. {"topicA":{"0":23,"1":-1},"topicB":{"0":-2}}, got $str""")
+    }
+  }
+}
