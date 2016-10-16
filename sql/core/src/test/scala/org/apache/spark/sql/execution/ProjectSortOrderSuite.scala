9e2eb1e15cbf305d7d4fb971e02ecfad948d4106
Liang-Chi Hsieh <viirya@gmail.com>
2016-10-16 00:10:46 +0000

    Improve test cases.

--- sql/core/src/test/scala/org/apache/spark/sql/execution/ProjectSortOrderSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/ProjectSortOrderSuite.scala
@@ -17,7 +17,7 @@
 
 package org.apache.spark.sql.execution
 
-import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.{Column, DataFrame, Row}
 import org.apache.spark.sql.catalyst.dsl.expressions._
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.test.SharedSQLContext
@@ -39,24 +39,36 @@ class ProjectSortOrderSuite extends SparkPlanTest with SharedSQLContext {
   test("TakeOrderedAndProjectExec should have correct outputOrdering") {
     val df = generateInputData()
     val input = df.queryExecution.sparkPlan
+
+    // Sort order: ["a".asc, "b + 1".asc]
     val sortOrder = Seq(SortOrder(input.output(0), Ascending),
       SortOrder(input.output(1) + 1, Ascending))
 
     // The project list changes outputOrdering.
+    // Project list: [-"a" + 1]
+    // Sort order: Nil
     val take1 = TakeOrderedAndProjectExec(limit, sortOrder,
       Seq(Alias(UnaryMinus(input.output(0)), "c")()), input)
     assert(take1.outputOrdering == List())
 
+    // Project list: ["a"]
+    // Sort order: ["a".asc]
     val take2 = TakeOrderedAndProjectExec(limit, sortOrder, Seq(input.output(0)), input)
     assert(take2.outputOrdering == sortOrder(0) :: Nil)
 
+    // Project list: ["a", "b"]
+    // Sort order: ["a".asc, "b + 1".asc]
     val take3 = TakeOrderedAndProjectExec(limit, sortOrder, input.output, input)
     assert(take3.outputOrdering == sortOrder(0) :: sortOrder(1) :: Nil)
 
+    // Project list: ["b"]
+    // Sort order: Nil
     val take4 = TakeOrderedAndProjectExec(limit, sortOrder, Seq(input.output(1)), input)
     assert(take4.outputOrdering == List())
 
     // Alias doesn't change outputOrdering.
+    // Project list: ["a" as "c"]
+    // Sort order: ["a".asc]
     val take5 = TakeOrderedAndProjectExec(limit, sortOrder,
       Seq(Alias(input.output(0), "c")()), input)
     assert(take5.outputOrdering == sortOrder(0) :: Nil)
@@ -64,25 +76,41 @@ class ProjectSortOrderSuite extends SparkPlanTest with SharedSQLContext {
 
   test("Project should have correct outputOrdering") {
     val df = generateInputData()
-    val sorted = df.sort(df("a"), df("b"))
+    val sorted = df.sort(Column(df("b").expr + 1).desc, df("a").asc)
     val input = sorted.queryExecution.sparkPlan
+    // Sort order: ["b + 1".desc, "a".asc]
     val sortOrder = input.outputOrdering
 
     // The project list changes outputOrdering.
+    // Project list: [-"a" as "c"]
+    // Sort order: Nil
     val project1 = ProjectExec(Seq(Alias(UnaryMinus(input.output(0)), "c")()), input)
     assert(project1.outputOrdering == List())
 
+    // Project list: [a]
+    // Sort order: Nil
     val project2 = ProjectExec(Seq(input.output(0)), input)
-    assert(project2.outputOrdering == sortOrder(0) :: Nil)
+    assert(project2.outputOrdering == List())
 
-    val project3 = ProjectExec(input.output, input)
-    assert(project3.outputOrdering == sortOrder(0) :: sortOrder(1) :: Nil)
+    // Project list: [b]
+    // Sort order: "b + 1".desc
+    val project3 = ProjectExec(Seq(input.output(1)), input)
+    assert(project3.outputOrdering == sortOrder(0) :: Nil)
 
-    val project4 = ProjectExec(Seq(input.output(1)), input)
-    assert(project4.outputOrdering == List())
+    // Project list: [a, b]
+    // Sort order: ["b + 1".desc, "a".asc]
+    val project4 = ProjectExec(input.output, input)
+    assert(project4.outputOrdering == sortOrder(0) :: sortOrder(1) :: Nil)
+
+    // Project list: [b, a]
+    // Sort order: ["b + 1".desc, "a".asc]
+    val project5 = ProjectExec(input.output.reverse, input)
+    assert(project5.outputOrdering == sortOrder(0) :: sortOrder(1) :: Nil)
 
     // Alias doesn't change outputOrdering.
-    val project5 = ProjectExec(Seq(Alias(input.output(0), "c")()), input)
-    assert(project5.outputOrdering == sortOrder(0) :: Nil)
+    // Project list: ["b" as "c"]
+    // Sort order: "b + 1".desc
+    val project6 = ProjectExec(Seq(Alias(input.output(1), "c")()), input)
+    assert(project6.outputOrdering == sortOrder(0) :: Nil)
   }
 }
