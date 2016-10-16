cb2e06d2e31e113dc29f5212fc9e05ba7d87fa8d
Holden Karau <holden@us.ibm.com>
2016-10-16 11:47:52 -0700

    So MANIFEST and setup can't refer to things above the root of the project, so create symlinks so we can package the JARs with it

--- python/setup.py
+++ python/setup.py
@@ -1,28 +1,40 @@
+import os
 from setuptools import setup, find_packages
 
 VERSION = '2.1.0-SNAPSHOT'
 JARS_PATH = "../assembly/target/scala-2.11/jars/*.jar"
+SCRIPTS = "../bin/*"
+TEMP_PATH = "deps"
 
-setup(
-    name='pyspark',
-    version=VERSION,
-    description='Apache Spark Python API',
-    author='Spark Developers',
-    author_email='dev@spark.apache.org',
-    url='https://github.com/apache/spark/tree/master/python',
-    packages=['pyspark',
-              'pyspark.mllib',
-              'pyspark.ml',
-              'pyspark.sql',
-              'pyspark.streaming'],
-    include_package_data=True,
-    package_data={
-        'jars': [JARS_PATH]},
-    license='http://www.apache.org/licenses/LICENSE-2.0',
-    install_requires=['py4j==0.10.3'],
-    extras_require={
-        'ml': ['numpy>=1.7'],
-        'mllib': ['numpy<=1.7'],
-        'sql': ['pandas']
-    }
-)
+# Construct links for setup
+if os.path.isfile(TEMP_PATH):
+    os.remove(TEMP_PATH)
+try:
+    os.symlink("../", TEMP_PATH)
+
+    setup(
+        name='pyspark',
+        version=VERSION,
+        description='Apache Spark Python API',
+        author='Spark Developers',
+        author_email='dev@spark.apache.org',
+        url='https://github.com/apache/spark/tree/master/python',
+        packages=['pyspark',
+                  'pyspark.mllib',
+                  'pyspark.ml',
+                  'pyspark.sql',
+                  'pyspark.streaming'],
+        include_package_data=True,
+        package_data={
+            'pyspark': [JARS_PATH]},
+        scripts=[SCRIPTS],
+        license='http://www.apache.org/licenses/LICENSE-2.0',
+        install_requires=['py4j==0.10.3'],
+        extras_require={
+            'ml': ['numpy>=1.7'],
+            'mllib': ['numpy<=1.7'],
+            'sql': ['pandas']
+        }
+    )
+finally:
+    os.remove(TEMP_PATH)
