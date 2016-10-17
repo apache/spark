beb1b45573787dfabd9228a8df71dac08df8ca76
Liang-Chi Hsieh <viirya@gmail.com>
2016-10-17 16:28:23 +0000

    Improve tests.

--- sql/core/src/test/scala/org/apache/spark/sql/execution/StopAfterExecSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/StopAfterExecSuite.scala
@@ -19,7 +19,7 @@ package org.apache.spark.sql.execution
 
 import java.lang.{Integer => JavaInteger}
 
-import org.apache.spark.sql.{Column, QueryTest, Row}
+import org.apache.spark.sql.{Column, DataFrame, QueryTest, Row}
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.execution.StopAfterExec
 import org.apache.spark.sql.functions._
@@ -29,6 +29,17 @@ import org.apache.spark.sql.types._
 
 class StopAfterExecSuite extends QueryTest with SharedSQLContext {
   import testImplicits._
+
+  private def checkIfStopAfterExists(df: DataFrame, shouldBeStopAfter: Boolean): Unit = {
+    val stopAfter = df.queryExecution.executedPlan.collect {
+      case _: StopAfterExec => true
+    }
+    if (shouldBeStopAfter) {
+      assert(stopAfter.nonEmpty)
+    } else {
+      assert(stopAfter.isEmpty)
+    }
+  }
 
   test("Filter on sorted data: StopAfter should not change filter results") {
     val N = 10L << 5
@@ -45,25 +56,19 @@ class StopAfterExecSuite extends QueryTest with SharedSQLContext {
     Seq("true", "false").map { codeGen =>
       withSQLConf(SQLConf.WHOLESTAGE_CODEGEN_ENABLED.key -> codeGen) {
         val df2 = sortedDF.filter("id <= 2").toDF()
-        assert(df2.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        checkIfStopAfterExists(df2, shouldBeStopAfter = true)
         checkAnswer(
           df2,
           Row(0) :: Row(1) :: Row(2) :: Nil)
 
         val df3 = sortedDF.filter("id < 10").groupBy().sum().toDF()
-        assert(df3.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        checkIfStopAfterExists(df3, shouldBeStopAfter = true)
         checkAnswer(
           df3,
           Row(45) :: Nil)
 
         val df4 = sortedDF2.filter("a < 3 AND b < 5")
-        assert(df4.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        checkIfStopAfterExists(df4, shouldBeStopAfter = true)
         checkAnswer(
           df4,
           Row(1, 1) :: Row(1, 2) :: Row(1, 3) :: Row(1, 4) ::
@@ -74,66 +79,55 @@ class StopAfterExecSuite extends QueryTest with SharedSQLContext {
     sortedDF2.unpersist()
   }
 
-  test("Only do StopAfter if predicates are on the prefix sort by attributes") {
-    // int int2 int3 int4
+  test("Only do StopAfter if filtering predicates are on the first sort order expression") {
+    //   a    b    c    d
     //   1    2    3    4
     //   2    3    4    5
     //   3    4    5    6
-    val df = Seq(1, 2, 3).map(i => (i, i + 1, i + 2, i + 3)).toDF("int", "int2", "int3", "int4")
-      .sort($"int", $"int3", $"int2" + 1, $"int4").persist()
+    // Only the filtering predicates depending on attribute "a" can be stopped early.
+    val df = Seq(1, 2, 3).map(i => (i, i + 1, i + 2, i + 3)).toDF("a", "b", "c", "d")
+      .sort($"a", $"c", $"b" + 1, $"d").persist()
 
     Seq("true", "false").map { codeGen =>
       withSQLConf(SQLConf.WHOLESTAGE_CODEGEN_ENABLED.key -> codeGen) {
-        // filter on "int": StopAfter
-        val df2 = df.filter("int <= 2").toDF()
-        assert(df2.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        // filter on "a": StopAfter
+        val df2 = df.filter("a <= 2").toDF()
+        checkIfStopAfterExists(df2, shouldBeStopAfter = true)
         checkAnswer(
           df2,
           Row(1, 2, 3, 4) :: Row(2, 3, 4, 5) :: Nil)
 
-        // filter on "int2": no StopAfter
-        val df3 = df.filter("int2 <= 2").toDF()
-        assert(df3.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.isEmpty)
+        // filter on "b": no StopAfter
+        val df3 = df.filter("b <= 2").toDF()
+        checkIfStopAfterExists(df3, shouldBeStopAfter = false)
         checkAnswer(
           df3,
           Row(1, 2, 3, 4) :: Nil)
 
-        // filter on "int3": no StopAfter
-        val df4 = df.filter("int3 <= 4").toDF()
-        assert(df4.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.isEmpty)
+        // filter on "c": no StopAfter
+        val df4 = df.filter("c <= 4").toDF()
+        checkIfStopAfterExists(df4, shouldBeStopAfter = false)
         checkAnswer(
           df4,
           Row(1, 2, 3, 4) :: Row(2, 3, 4, 5) :: Nil)
 
-        // filter on "int4": no StopAfter
-        val df5 = df.filter("int4 <= 6").toDF()
-        assert(df5.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.isEmpty)
+        // filter on "d": no StopAfter
+        val df5 = df.filter("d <= 6").toDF()
+        checkIfStopAfterExists(df5, shouldBeStopAfter = false)
         checkAnswer(
           df5,
           Row(1, 2, 3, 4) :: Row(2, 3, 4, 5) :: Row(3, 4, 5, 6) :: Nil)
 
-        // filter on "int" and "int2": StopAfter
-        val df6 = df.filter("int <= 2 AND int2 < 3").toDF()
-        assert(df6.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        // filter on "a" and "b": StopAfter
+        val df6 = df.filter("a <= 2 AND b < 3").toDF()
+        checkIfStopAfterExists(df6, shouldBeStopAfter = true)
         checkAnswer(
           df6,
           Row(1, 2, 3, 4) :: Nil)
 
-        // filter on "int" and "int3": StopAfter
-        val df7 = df.filter("int <= 2 AND int3 <= 5").toDF()
-        assert(df7.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        // filter on "a" and "c": StopAfter
+        val df7 = df.filter("a <= 2 AND c <= 5").toDF()
+        checkIfStopAfterExists(df7, shouldBeStopAfter = true)
         checkAnswer(
           df7,
           Row(1, 2, 3, 4) :: Row(2, 3, 4, 5) :: Nil)
@@ -146,175 +140,129 @@ class StopAfterExecSuite extends QueryTest with SharedSQLContext {
     val N = 10L << 5
     val df = spark.range(N).sort("id").persist()
     val df2 = df.filter("id < rand()").toDF()
-    assert(df2.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    checkIfStopAfterExists(df2, shouldBeStopAfter = false)
     df.unpersist()
   }
 
-  test("No StopAfter if predicates contain non sort-order attribute") {
-    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort("int").persist()
-    val df2 = df.filter("int <= 2")
-    assert(df2.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+  test("No StopAfter if predicates only contain non sort-order attribute") {
+    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort("a").persist()
+    val df2 = df.filter("a <= 2")
+    checkIfStopAfterExists(df2, shouldBeStopAfter = true)
 
-    val df3 = df.filter("int2 <= 2")
-    assert(df3.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df3 = df.filter("b <= 2")
+    checkIfStopAfterExists(df3, shouldBeStopAfter = false)
 
-    val df4 = df.filter("int <= 2 AND int2 <= 2")
-    assert(df4.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    val df4 = df.filter("a <= 2 AND b <= 2")
+    checkIfStopAfterExists(df4, shouldBeStopAfter = true)
 
     df.unpersist()
   }
 
   test("StopAfter if child plan is sorted on non-attribute expressions") {
-    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort($"int" + 1).persist()
-    val df2 = df.filter("int <= 2")
-    assert(df2.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort($"a" + 1).persist()
+    val df2 = df.filter("a <= 2")
+    checkIfStopAfterExists(df2, shouldBeStopAfter = false)
 
-    val df3 = df.filter("int + 1 <= 2")
-    assert(df3.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    val df3 = df.filter("a + 1 <= 2")
+    checkIfStopAfterExists(df3, shouldBeStopAfter = true)
 
     df.unpersist()
   }
 
   test("No StopAfter if predicate is not binary comparison between attribute and literal") {
-    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort("int", "int2").persist()
-    // TODO: Once we can normalize "int + 1 <= 2" to "int <= 1" for StopAfter node,
+    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort("a", "b").persist()
+    // TODO: Once we can normalize "a + 1 <= 2" to "a <= 1" for StopAfter node,
     //       We can change the following test to positive one.
-    val df2 = df.filter("int + 1 <= 2")
-    assert(df2.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df2 = df.filter("a + 1 <= 2")
+    checkIfStopAfterExists(df2, shouldBeStopAfter = false)
 
-    val df3 = df.filter("int < 2 AND int + 1 < 5")
-    assert(df3.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    val df3 = df.filter("a < 2 AND a + 1 < 5")
+    checkIfStopAfterExists(df3, shouldBeStopAfter = true)
 
-    val df4 = df.filter("int is null")
-    assert(df4.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df4 = df.filter("a is null")
+    checkIfStopAfterExists(df4, shouldBeStopAfter = false)
 
-    val df5 = df.filter("int < int2")
-    assert(df5.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df5 = df.filter("a < b")
+    checkIfStopAfterExists(df5, shouldBeStopAfter = false)
 
     df.unpersist()
   }
 
   test("No StopAfter for disjunctive predicates") {
-    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort("int", "int2").persist()
+    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort("a", "b").persist()
 
-    val df1 = df.filter("int < 2 OR int < 0")
-    assert(df1.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df2 = df.filter("a < 2 OR a < 0")
+    checkIfStopAfterExists(df2, shouldBeStopAfter = false)
 
-    val df2 = df.filter("(int < 2 AND int2 < 2) OR int < 0")
-    assert(df2.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    val df3 = df.filter("(a < 2 AND b < 2) OR a < 0")
+    checkIfStopAfterExists(df3, shouldBeStopAfter = false)
 
-    val df3 = df.filter("int < 2 AND (int2 < 2 OR int < 0)")
-    assert(df3.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    val df4 = df.filter("a < 2 AND (b < 2 OR a < 0)")
+    checkIfStopAfterExists(df4, shouldBeStopAfter = true)
 
     df.unpersist()
   }
 
   test("No StopAfter if data contains null") {
     val df = Seq((new JavaInteger(1), 2), (null, 3))
-      .toDF("int", "int2").sort("int", "int2").persist()
+      .toDF("a", "b").sort("a", "b").persist()
+
     val output = df.queryExecution.executedPlan.output
 
-    // attribute "int" is nullable. Can't apply StopAfter with predicate on it.
-    assert(output(0).name.equalsIgnoreCase("int"))
+    // attribute "a" is nullable. Can't apply StopAfter with predicate on it.
+    assert(output(0).name.equalsIgnoreCase("a"))
     assert(output(0).nullable == true)
-    assert(df.filter("int < 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
 
-    assert(df.filter("int < 2 AND int2 < 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    checkIfStopAfterExists(df.filter("a < 2"), shouldBeStopAfter = false)
+    checkIfStopAfterExists(df.filter("a < 2 AND b < 2"), shouldBeStopAfter = false)
 
     df.unpersist()
   }
 
   test("Only do StopAfter if predicate direction matching sort direction") {
     // Sort in ascending way. Matching LessThan and LessThanOrEqual.
-    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort("int").persist()
+    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort("a").persist()
 
     // LessThan
-    assert(df.filter("int < 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    checkIfStopAfterExists(df.filter("a < 2"), shouldBeStopAfter = true)
 
     // LessThanOrEqual
-    assert(df.filter("int <= 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    checkIfStopAfterExists(df.filter("a <= 2"), shouldBeStopAfter = true)
 
     // Sort in descending way. Matching GreaterThan and GreaterThanOrEqual.
-    val df2 = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort($"int".desc).persist()
+    val df2 = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort($"a".desc).persist()
 
     // GreaterThan
-    assert(df2.filter("int > 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    checkIfStopAfterExists(df2.filter("a > 2"), shouldBeStopAfter = true)
 
     // GreaterThanOrEqual
-    assert(df2.filter("int >= 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.nonEmpty)
+    checkIfStopAfterExists(df2.filter("a >= 2"), shouldBeStopAfter = true)
 
     // Equal doesn't match ascending or descending ways.
-    assert(df.filter("int = 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
-
-    assert(df2.filter("int = 2").queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    checkIfStopAfterExists(df.filter("a = 2"), shouldBeStopAfter = false)
+    checkIfStopAfterExists(df2.filter("a = 2"), shouldBeStopAfter = false)
 
     df.unpersist()
     df2.unpersist()
   }
 
   test("No StopAfter if predicates are not the same direction") {
-    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("int", "int2").sort("int", "int2").persist()
+    val df = Seq(1, 2, 3).map(i => (i, i + 1)).toDF("a", "b").sort("a", "b").persist()
 
     Seq("true", "false").map { codeGen =>
       withSQLConf(SQLConf.WHOLESTAGE_CODEGEN_ENABLED.key -> codeGen) {
-        // "int <= 1" && "3 > int" can be normalized as "int <= 1" && "int < 3".
-        val df2 = df.filter("(int <= 1) AND (3 > int)")
-        assert(df2.queryExecution.executedPlan.collect {
-          case _: StopAfterExec => true
-        }.nonEmpty)
+        // "a <= 1" && "3 > a" can be normalized as "a <= 1" && "a < 3".
+        val df2 = df.filter("(a <= 1) AND (3 > a)")
+        checkIfStopAfterExists(df2, shouldBeStopAfter = true)
         checkAnswer(
           df2,
           Row(1, 2) :: Nil)
       }
     }
 
-    // "int <= 1" && "3 < int" can be normalized as "int <= 1" && "int > 3".
-    val df3 = df.filter("(int <= 1) AND (3 < int)")
-    assert(df3.queryExecution.executedPlan.collect {
-      case _: StopAfterExec => true
-    }.isEmpty)
+    // "a <= 1" && "3 < a" can be normalized as "a <= 1" && "a > 3".
+    val df3 = df.filter("(a <= 1) AND (3 < a)")
+    checkIfStopAfterExists(df3, shouldBeStopAfter = false)
 
     df.unpersist()
   }
