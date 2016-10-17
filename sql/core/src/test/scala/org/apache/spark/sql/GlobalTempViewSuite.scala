2f5664bb58cc2627fc4e51803b9c1b99f81e6db4
Wenchen Fan <wenchen@databricks.com>
2016-10-17 15:48:18 +0800

    backport global temp view API

---
+++ sql/core/src/test/scala/org/apache/spark/sql/GlobalTempViewSuite.scala
@@ -0,0 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql
+
+import org.apache.spark.sql.test.SharedSQLContext
+
+class GlobalTempViewSuite extends QueryTest with SharedSQLContext {
+  import testImplicits._
+
+  test("createGlobalTempView is an alias of registerTempTable") {
+    Seq(1 -> "a").toDF("i", "j").createGlobalTempView("global_view")
+    checkAnswer(sql("SELECT i ,j FROM global_view"), Row(1, "a"))
+    sqlContext.dropTempTable("global_view")
+    intercept[AnalysisException](sql("SELECT * FROM global_view"))
+  }
+}
