fb15d7e3e6b3be7c8c69d776649f4d556656f3f0
Holden Karau <holden@us.ibm.com>
2016-10-18 10:38:40 -0700

    Some progress we need to use SDIST but is ok

--- python/setup.py
+++ python/setup.py
@@ -5,16 +5,24 @@ from setuptools import setup, find_packages
 VERSION = '2.1.0-SNAPSHOT'
 # A temporary path so we can access above the Python project root and fetch scripts and jars we need
 TEMP_PATH = "deps"
-JARS_PATH = "%s/assembly/target/scala-2.11/jars/*.jar" % TEMP_PATH
-SCRIPTS = "%s/bin/*" % TEMP_PATH
+SPARK_HOME = os.path.abspath("../")
+JARS_PATH = "%s/assembly/target/scala-2.11/jars/" % SPARK_HOME
+SCRIPTS_PATH = "%s/bin" % SPARK_HOME
+SCRIPTS = "%s/bin" % TEMP_PATH
+JARS = "%s/jars" % TEMP_PATH
+
 
 # Construct links for setup
 try:
-    os.symlink("../", TEMP_PATH)
+    os.mkdir(TEMP_PATH)
 except:
-    print("temp path for symlink to parent already exists %s" % TEMP_PATH, file=sys.stderr)
+    print("Temp path for symlink to parent already exists %s" % TEMP_PATH, file=sys.stderr)
+    exit(-1)
 
 try:
+    os.symlink(JARS_PATH, JARS)
+    os.symlink(SCRIPTS_PATH, SCRIPTS)
+
     setup(
         name='pyspark',
         version=VERSION,
@@ -29,8 +37,8 @@ try:
                   'pyspark.streaming'],
         include_package_data=True,
         package_data={
-            'pyspark': [JARS_PATH]},
-        scripts=[SCRIPTS],
+            'pyspark': [JARS]},
+        scripts=[SCRIPTS + "/*"],
         license='http://www.apache.org/licenses/LICENSE-2.0',
         install_requires=['py4j==0.10.3'],
         extras_require={
@@ -40,4 +48,7 @@ try:
         }
     )
 finally:
-    os.remove(TEMP_PATH)
+    True
+#    os.remove("%s/jars" % TEMP_PATH)
+#    os.remove("%s/bin" % TEMP_PATH)
+#    os.rmdir(TEMP_PATH)
