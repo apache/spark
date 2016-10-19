10892fb676b6e70e4eb5039a8581f29edae59226
hyukjinkwon <gurwls223@gmail.com>
2016-10-19 23:41:16 +0900

    jsonExpressions OK

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/jsonExpressions.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/jsonExpressions.scala
@@ -109,7 +109,18 @@ private[this] object SharedFactory {
  * of the extracted json object. It will return null if the input json string is invalid.
  */
 @ExpressionDescription(
-  usage = "_FUNC_(json_txt, path) - Extract a json object from path")
+  usage = "_FUNC_(json_txt, path) - Extracts a json object from path.",
+  extended = """
+    _FUNC_(json_txt, path)
+
+      Arguments:
+        json_txt - string type expression that represents JSON document.
+        path - string type expression that represents path for JSON document.
+
+      Examples:
+        > SELECT _FUNC_('{"a":"b"}', '$.a');
+         b
+  """)
 case class GetJsonObject(json: Expression, path: Expression)
   extends BinaryExpression with ExpectsInputTypes with CodegenFallback {
 
@@ -325,7 +336,18 @@ case class GetJsonObject(json: Expression, path: Expression)
 
 // scalastyle:off line.size.limit
 @ExpressionDescription(
-  usage = "_FUNC_(jsonStr, p1, p2, ..., pn) - like get_json_object, but it takes multiple names and return a tuple. All the input parameters and output column types are string.")
+  usage = "_FUNC_(jsonStr, p1, p2, ..., pn) - like get_json_object, but it takes multiple names and return a tuple. All the input parameters and output column types are string.",
+  extended = """
+    _FUNC_(jsonStr, p1, p2, ..., pn)
+
+      Arguments:
+        json_txt - string type expression that represents JSON document.
+        p - string type expression that represents key name for JSON document.
+
+      Examples:
+        > SELECT _FUNC_('{"a":1, "b":2}', 'a', 'b');
+         1	2
+  """)
 // scalastyle:on line.size.limit
 case class JsonTuple(children: Seq[Expression])
   extends Generator with CodegenFallback {
