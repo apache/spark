06a39e11f7a47a3e027e20043ef4ec524e1468a8
Xiangrui Meng <meng@databricks.com>
2016-10-19 22:09:15 -0700

    add initializeStateForPartition to Projection

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/package.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/package.scala
@@ -64,7 +64,15 @@ package object expressions  {
    * column of the new row. If the schema of the input row is specified, then the given expression
    * will be bound to that schema.
    */
-  abstract class Projection extends (InternalRow => InternalRow)
+  abstract class Projection extends (InternalRow => InternalRow) {
+
+    /**
+     * Initialize internal state given the current partition index.
+     * This is used by non-deterministic expressions to set the initial state
+     * The default implementation does nothing.
+     */
+    def initializeStateForPartition(partitionIndex: Int): Unit = {}
+  }
 
   /**
    * Converts a [[InternalRow]] to another Row given a sequence of expression that define each
