e940cb9b6b5ea8f5f34cb3643f15ebb15715d142
Eric Liang <ekl@databricks.com>
2016-10-20 13:10:12 -0700

    Thu Oct 20 13:10:12 PDT 2016

---
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/PartitionProviderCompatibilitySuite.scala
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.hive
+
+import java.io.File
+
+import org.apache.spark.metrics.source.HiveCatalogMetrics
+import org.apache.spark.sql.{AnalysisException, QueryTest}
+import org.apache.spark.sql.hive.test.TestHiveSingleton
+import org.apache.spark.sql.test.SQLTestUtils
+
+class PartitionProviderCompatibilitySuite
+  extends QueryTest with TestHiveSingleton with SQLTestUtils {
+
+  private def setupPartitionedDatasourceTable(tableName: String, dir: File): Unit = {
+    // TODO(ekl) make these mixed-case fields once support for that is fixed
+    spark.range(5).selectExpr("id as fieldone", "id as partcol1", "id as partcol2").write
+      .partitionBy("partcol1", "partcol2")
+      .mode("overwrite")
+      .parquet(dir.getAbsolutePath)
+
+    spark.sql(s"""
+      |create table $tableName (fieldone long, partcol1 int, partcol2 int)
+      |using parquet
+      |options (path "${dir.getAbsolutePath}")
+      |partitioned by (partcol1, partcol2)""".stripMargin)
+  }
+
+  private def verifyIsLegacyTable(tableName: String): Unit = {
+    val unsupportedCommands = Seq(
+      s"SHOW PARTITIONS $tableName")
+
+    for (cmd <- unsupportedCommands) {
+      val e = intercept[AnalysisException] {
+        spark.sql(s"show partitions $tableName")
+      }
+      assert(e.getMessage.contains("partition metadata is not stored in the Hive metastore"), e)
+    }
+  }
+
+  private def verifyIsNewTable(tableName: String): Unit = {
+    withSQLConf("spark.sql.hive.filesourcePartitionManagement" -> "true") {
+      assert(spark.sql(s"show partitions $tableName").count() == 5)
+      HiveCatalogMetrics.reset()
+
+      // sanity check table performance
+      assert(spark.sql(s"select * from $tableName where partcol1 < 2").count() == 2)
+      assert(HiveCatalogMetrics.METRIC_PARTITIONS_FETCHED.getCount() == 2)
+      assert(HiveCatalogMetrics.METRIC_FILES_DISCOVERED.getCount() == 2)
+    }
+  }
+
+  test("convert partition provider to hive with repair table") {
+    withTable("test") {
+      withTempDir { dir =>
+        withSQLConf("spark.sql.hive.filesourcePartitionManagement" -> "false") {
+          setupPartitionedDatasourceTable("test", dir)
+        }
+        withSQLConf("spark.sql.hive.filesourcePartitionManagement" -> "true") {
+          verifyIsLegacyTable("test")
+          spark.sql("msck repair table test")
+          verifyIsNewTable("test")
+        }
+      }
+    }
+  }
+}
