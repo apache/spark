2ec32063a2fee26a42c74026eaf287189a4ec0ac
Xiangrui Meng <meng@databricks.com>
2016-10-20 02:41:52 -0700

    fix partition initialization in local relation

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
@@ -1060,6 +1060,7 @@ object ConvertToLocalRelation extends Rule[LogicalPlan] {
     case Project(projectList, LocalRelation(output, data))
         if !projectList.exists(hasUnevaluableExpr) =>
       val projection = new InterpretedProjection(projectList, output)
+      projection.initializeStatesForPartition(0)
       LocalRelation(projectList.map(_.toAttribute), data.map(projection))
   }
 
