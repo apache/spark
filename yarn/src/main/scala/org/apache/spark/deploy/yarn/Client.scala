af2bbb5533509eff53e054d6bc11038a7f163be9
Jagadeesan <as2@us.ibm.com>
2016-10-21 09:48:24 +0100

    [SPARK-17960][PYSPARK][UPGRADE TO PY4J 0.10.4]
    ## What changes were proposed in this pull request?
    
    1) Upgrade the Py4J version on the Java side
    2) Update the py4j src zip file we bundle with Spark
    
    ## How was this patch tested?
    
    Existing doctests & unit tests pass
    
    Author: Jagadeesan <as2@us.ibm.com>
    
    Closes #15514 from jagadeesanas2/SPARK-17960.

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/Client.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/Client.scala
@@ -1179,7 +1179,7 @@ private[spark] class Client(
         val pyArchivesFile = new File(pyLibPath, "pyspark.zip")
         require(pyArchivesFile.exists(),
           s"$pyArchivesFile not found; cannot run pyspark application in YARN mode.")
-        val py4jFile = new File(pyLibPath, "py4j-0.10.3-src.zip")
+        val py4jFile = new File(pyLibPath, "py4j-0.10.4-src.zip")
         require(py4jFile.exists(),
           s"$py4jFile not found; cannot run pyspark application in YARN mode.")
         Seq(pyArchivesFile.getAbsolutePath(), py4jFile.getAbsolutePath())
