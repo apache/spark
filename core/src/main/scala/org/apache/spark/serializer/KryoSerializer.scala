976e336672c66df8de8b4dc2e96d98a187d103ee
Sandeep Singh <sandeep@techaddict.me>
2016-10-22 08:49:42 +0300

    add comment

--- core/src/main/scala/org/apache/spark/serializer/KryoSerializer.scala
+++ core/src/main/scala/org/apache/spark/serializer/KryoSerializer.scala
@@ -79,6 +79,7 @@ class KryoSerializer(conf: SparkConf)
     .filter(!_.isEmpty)
 
   private val avroSchemas = conf.getAvroSchema
+  // whether to use unsafe based IO for serialization
   private val useUnsafe = conf.getBoolean("spark.kryo.unsafe", false)
 
   def newKryoOutput(): KryoOutput =
