03f6a47bcc71b0b200390eee98500dbc0e434984
Zheng RuiFeng <ruifengz@foxmail.com>
2016-10-25 17:01:55 +0800

    del_val_m

--- mllib/src/main/scala/org/apache/spark/ml/clustering/BisectingKMeans.scala
+++ mllib/src/main/scala/org/apache/spark/ml/clustering/BisectingKMeans.scala
@@ -264,9 +264,9 @@ class BisectingKMeans @Since("2.0.0") (
     val model = copyValues(new BisectingKMeansModel(uid, parentModel).setParent(this))
     val summary = new BisectingKMeansSummary(
       model.transform(dataset), $(predictionCol), $(featuresCol), $(k))
-    val m = model.setSummary(summary)
-    instr.logSuccess(m)
-    m
+    model.setSummary(summary)
+    instr.logSuccess(model)
+    model
   }
 
   @Since("2.0.0")
