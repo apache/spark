5c2a3b779a3d2f434efecfd505847871d8551156
gatorsmile <gatorsmile@gmail.com>
2016-10-26 00:38:34 -0700

    [SPARK-17693][SQL] Fixed Insert Failure To Data Source Tables when the Schema has the Comment Field
    ### What changes were proposed in this pull request?
    ```SQL
    CREATE TABLE tab1(col1 int COMMENT 'a', col2 int) USING parquet
    INSERT INTO TABLE tab1 SELECT 1, 2
    ```
    The insert attempt will fail if the target table has a column with comments. The error is strange to the external users:
    ```
    assertion failed: No plan for InsertIntoTable Relation[col1#15,col2#16] parquet, false, false
    +- Project [1 AS col1#19, 2 AS col2#20]
       +- OneRowRelation$
    ```
    
    This PR is to fix the above bug by checking the metadata when comparing the schema between the table and the query. If not matched, we also copy the metadata. This is an alternative to https://github.com/apache/spark/pull/15266
    
    ### How was this patch tested?
    Added a test case
    
    Author: gatorsmile <gatorsmile@gmail.com>
    
    Closes #15615 from gatorsmile/insertDataSourceTableWithCommentSolution2.

--- sql/core/src/test/scala/org/apache/spark/sql/sources/InsertSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/sources/InsertSuite.scala
@@ -185,6 +185,48 @@ class InsertSuite extends DataSourceTest with SharedSQLContext {
     )
   }
 
+  test("INSERT INTO TABLE with Comment in columns") {
+    val tabName = "tab1"
+    withTable(tabName) {
+      sql(
+        s"""
+           |CREATE TABLE $tabName(col1 int COMMENT 'a', col2 int)
+           |USING parquet
+         """.stripMargin)
+      sql(s"INSERT INTO TABLE $tabName SELECT 1, 2")
+
+      checkAnswer(
+        sql(s"SELECT col1, col2 FROM $tabName"),
+        Row(1, 2) :: Nil
+      )
+    }
+  }
+
+  test("INSERT INTO TABLE - complex type but different names") {
+    val tab1 = "tab1"
+    val tab2 = "tab2"
+    withTable(tab1, tab2) {
+      sql(
+        s"""
+           |CREATE TABLE $tab1 (s struct<a: string, b: string>)
+           |USING parquet
+         """.stripMargin)
+      sql(s"INSERT INTO TABLE $tab1 SELECT named_struct('col1','1','col2','2')")
+
+      sql(
+        s"""
+           |CREATE TABLE $tab2 (p struct<c: string, d: string>)
+           |USING parquet
+         """.stripMargin)
+      sql(s"INSERT INTO TABLE $tab2 SELECT * FROM $tab1")
+
+      checkAnswer(
+        spark.table(tab1),
+        spark.table(tab2)
+      )
+    }
+  }
+
   test("it is not allowed to write to a table while querying it.") {
     val message = intercept[AnalysisException] {
       sql(
