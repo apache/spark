328e77d4a1e248f5fb2dee707113da21a04fa302
Chan Chor Pang <ChorPang.Chan@access-company.com>
2016-10-26 16:39:40 +0900

    add path /streamingapi

--- streaming/src/main/scala/org/apache/spark/streaming/status/api/v1/StreamingApiRootResource.scala
+++ streaming/src/main/scala/org/apache/spark/streaming/status/api/v1/StreamingApiRootResource.scala
@@ -5,13 +5,36 @@ import javax.servlet.ServletContext
 import org.apache.spark.status.api.v1.UIRoot
 import org.eclipse.jetty.server.handler.ContextHandler
 import javax.ws.rs.core.Context
+import org.eclipse.jetty.servlet.ServletHolder
+import org.eclipse.jetty.servlet.ServletContextHandler
+import com.sun.jersey.spi.container.servlet.ServletContainer
+import com.sun.jersey.api.core.ResourceConfig
+import org.apache.spark.status.api.v1.SecurityFilter
 
-@Path("/streaming/v1")
+@Path("/v1")
 private[v1] class StreamingApiRootResource extends UIRootFromServletContext{
 
   @Path("streaminginfo")
   def getStreamingInfo(): StreamingInfoResource = {
     new StreamingInfoResource(uiRoot)
+  }
+}
+
+private[spark] object StreamingApiRootResource {
+
+  def getServletHandler(uiRoot: UIRoot): ServletContextHandler = {
+    val jerseyContext = new ServletContextHandler(ServletContextHandler.NO_SESSIONS)
+    jerseyContext.setContextPath("/streamingapi")
+    val holder: ServletHolder = new ServletHolder(classOf[ServletContainer])
+    holder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass",
+      "com.sun.jersey.api.core.PackagesResourceConfig")
+    holder.setInitParameter("com.sun.jersey.config.property.packages",
+      "org.apache.spark.streaming.status.api.v1")
+    //holder.setInitParameter(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS,
+    //  classOf[SecurityFilter].getCanonicalName)
+    UIRootFromServletContext.setUiRoot(jerseyContext, uiRoot)
+    jerseyContext.addServlet(holder, "/*")
+    jerseyContext
   }
 }
 
