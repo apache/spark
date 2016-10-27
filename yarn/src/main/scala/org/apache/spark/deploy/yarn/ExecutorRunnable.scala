b6780ccf8e8739c08f4e7b7c73a20d32119aa0d0
sheepduke <sheepduke@gmail.com>
2016-10-27 13:34:53 +0800

    Modifed source code to meet Spark code style.

--- yarn/src/main/scala/org/apache/spark/deploy/yarn/ExecutorRunnable.scala
+++ yarn/src/main/scala/org/apache/spark/deploy/yarn/ExecutorRunnable.scala
@@ -209,10 +209,12 @@ private[yarn] class ExecutorRunnable(
     YarnSparkHadoopUtil.addOutOfMemoryErrorArgument(javaOpts)
 
     // Add support for extra executor prefix.
-    val commandPrefix = (if (sys.env.contains("SPARK_COMMAND_PREFIX")) sys.env.get("SPARK_COMMAND_PREFIX") else "")
+    val commandPrefix = (if (sys.env.contains("SPARK_COMMAND_PREFIX"))
+    	sys.env.get("SPARK_COMMAND_PREFIX") else "");
 
     val commands = prefixEnv ++ Seq(
-      commandPrefix + " " + YarnSparkHadoopUtil.expandEnvironment(Environment.JAVA_HOME) + "/bin/java",
+      commandPrefix + " " + YarnSparkHadoopUtil.expandEnvironment(Environment.JAVA_HOME) +
+        "/bin/java",
       "-server") ++
       javaOpts ++
       Seq("org.apache.spark.executor.CoarseGrainedExecutorBackend",
