439def9e5fd46f0650cba24c9232c3f62349023e
WeichenXu <WeichenXu123@outlook.com>
2016-10-28 23:54:54 +0800

    several minor updates V2

--- core/src/main/scala/org/apache/spark/rdd/ZipPartitionsRDDUtils.scala
+++ core/src/main/scala/org/apache/spark/rdd/ZipPartitionsRDDUtils.scala
@@ -19,69 +19,105 @@ package org.apache.spark.rdd
 
 import scala.reflect.ClassTag
 
+/**
+ * The `ZipPartitionsRDDUtils` contains a series of `zipPartitionsWithPreferredLocation` helper
+ * functions which are similar to `RDD.zipPartitions`, but it can be used to specify the
+ * zipPartitions task preferred locations to be consistent with the fisrt zipped RDD.
+ *
+ * If the fisrt zipped RDD do not have preferred location, it will fallback to the default
+ * `zipPartition` preferred location strategy.
+ */
 object ZipPartitionsRDDUtils {
 
   /**
-   * The zipPartitionsWithPreferredLocation helper function is similar to RDD.zipPartitions, but
-   * it can be used to specify the zipPartitions task preferred locations to be consistent with
-   * the fisrt zipped RDD. If the fisrt zipped RDD do not have preferred location,
-   * it will fallback to the default `zipPartition` preferred location strategy.
-   * This helper function can be used when one large RDD zipped with other small RDDs, we can set
-   * the first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data locality.
+   * This helper function can be used when one large RDD zipped with another one small RDD, we
+   * can set first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data
+   * locality.
    *
-   * `preservesPartitioner` indicates whether the input function preserves the partitioner, which
+   * `preservesPartitioning` indicates whether the input function preserves the partitioner, which
    * should be `false` unless this is a pair RDD and the input function doesn't modify the keys.
    */
   def zipPartitionsWithPreferredLocation[A: ClassTag, B: ClassTag, V: ClassTag]
-      (majorRdd: RDD[A], rdd2: RDD[B], preservesPartitioner: Boolean)
+      (majorRdd: RDD[A], rdd2: RDD[B], preservesPartitioning: Boolean)
       (f: (Iterator[A], Iterator[B]) => Iterator[V]): RDD[V] = {
     val sc = majorRdd.sparkContext
     majorRdd.withScope {
-      new ZippedPartitionsRDD2(sc, sc.clean(f), majorRdd, rdd2, preservesPartitioner,
+      new ZippedPartitionsRDD2(sc, sc.clean(f), majorRdd, rdd2, preservesPartitioning,
         useFirstParentPreferredLocations = true)
     }
   }
 
+  /**
+   * This helper function can be used when one large RDD zipped with another one small RDD, we
+   * can set first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data
+   * locality.
+   */
   def zipPartitionsWithPreferredLocation[A: ClassTag, B: ClassTag, V: ClassTag]
       (majorRdd: RDD[A], rdd2: RDD[B])
       (f: (Iterator[A], Iterator[B]) => Iterator[V]): RDD[V] = {
-    zipPartitionsWithPreferredLocation(majorRdd, rdd2, preservesPartitioner = false)(f)
+    zipPartitionsWithPreferredLocation(majorRdd, rdd2, preservesPartitioning = false)(f)
   }
 
+  /**
+   * This helper function can be used when one large RDD zipped with another two small RDDs, we
+   * can set first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data
+   * locality.
+   *
+   * `preservesPartitioning` indicates whether the input function preserves the partitioner, which
+   * should be `false` unless this is a pair RDD and the input function doesn't modify the keys.
+   */
   def zipPartitionsWithPreferredLocation[A: ClassTag, B: ClassTag, C: ClassTag, V: ClassTag]
-      (majorRdd: RDD[A], rdd2: RDD[B], rdd3: RDD[C], preservesPartitioner: Boolean)
+      (majorRdd: RDD[A], rdd2: RDD[B], rdd3: RDD[C], preservesPartitioning: Boolean)
       (f: (Iterator[A], Iterator[B], Iterator[C]) => Iterator[V]): RDD[V] = {
     val sc = majorRdd.sparkContext
     majorRdd.withScope {
       new ZippedPartitionsRDD3(sc, sc.clean(f), majorRdd, rdd2, rdd3,
-        preservesPartitioner, useFirstParentPreferredLocations = true)
+        preservesPartitioning, useFirstParentPreferredLocations = true)
     }
   }
 
+  /**
+   * This helper function can be used when one large RDD zipped with another two small RDDs, we
+   * can set first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data
+   * locality.
+   */
   def zipPartitionsWithPreferredLocation[A: ClassTag, B: ClassTag, C: ClassTag, V: ClassTag]
       (majorRdd: RDD[A], rdd2: RDD[B], rdd3: RDD[C])
       (f: (Iterator[A], Iterator[B], Iterator[C]) => Iterator[V]): RDD[V] = {
-    zipPartitionsWithPreferredLocation(majorRdd, rdd2, rdd3, preservesPartitioner = false)(f)
+    zipPartitionsWithPreferredLocation(majorRdd, rdd2, rdd3, preservesPartitioning = false)(f)
   }
 
+  /**
+   * This helper function can be used when one large RDD zipped with another three small RDDs, we
+   * can set first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data
+   * locality.
+   *
+   * `preservesPartitioning` indicates whether the input function preserves the partitioner, which
+   * should be `false` unless this is a pair RDD and the input function doesn't modify the keys.
+   */
   def zipPartitionsWithPreferredLocation
       [A: ClassTag, B: ClassTag, C: ClassTag, D: ClassTag, V: ClassTag]
       (majorRdd: RDD[A], rdd2: RDD[B], rdd3: RDD[C], rdd4: RDD[D],
-        preservesPartitioner: Boolean)
+        preservesPartitioning: Boolean)
       (f: (Iterator[A], Iterator[B], Iterator[C], Iterator[D]) => Iterator[V]): RDD[V] = {
     val sc = majorRdd.sparkContext
     majorRdd.withScope {
       new ZippedPartitionsRDD4(sc, sc.clean(f), majorRdd, rdd2, rdd3, rdd4,
-        preservesPartitioner, useFirstParentPreferredLocations = true)
+        preservesPartitioning, useFirstParentPreferredLocations = true)
     }
   }
 
+  /**
+   * This helper function can be used when one large RDD zipped with another three small RDDs, we
+   * can set first zipped RDD (the `majorRdd` parameter) to be the large RDD to improve data
+   * locality.
+   */
   def zipPartitionsWithPreferredLocation
       [A: ClassTag, B: ClassTag, C: ClassTag, D: ClassTag, V: ClassTag]
       (majorRdd: RDD[A], rdd2: RDD[B], rdd3: RDD[C], rdd4: RDD[D])
       (f: (Iterator[A], Iterator[B], Iterator[C], Iterator[D]) => Iterator[V]): RDD[V] = {
     zipPartitionsWithPreferredLocation(
-      majorRdd, rdd2, rdd3, rdd4, preservesPartitioner = false)(f)
+      majorRdd, rdd2, rdd3, rdd4, preservesPartitioning = false)(f)
   }
 
 }
