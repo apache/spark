29620cd8da6b8b4d749ab02d217ae0f47cfa7e04
Jacek Laskowski <jacek@japila.pl>
2016-10-29 14:55:08 +0200

    Proper index

--- core/src/test/scala/org/apache/spark/ui/UISuite.scala
+++ core/src/test/scala/org/apache/spark/ui/UISuite.scala
@@ -189,7 +189,7 @@ class UISuite extends SparkFunSuite {
       val ui = sc.ui.get
       val splitUIAddress = ui.webUrl.split(':')
       val boundPort = ui.boundPort
-      assert(splitUIAddress(3).toInt == boundPort)
+      assert(splitUIAddress(2).toInt == boundPort)
     }
   }
 
