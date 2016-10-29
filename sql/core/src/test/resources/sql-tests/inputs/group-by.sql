8986207252e244cfadf9c5bc34264346b585bf18
jiangxingbo <jiangxb1987@gmail.com>
2016-10-29 19:47:38 +0800

    add test cases.

--- sql/core/src/test/resources/sql-tests/inputs/group-by.sql
+++ sql/core/src/test/resources/sql-tests/inputs/group-by.sql
@@ -15,3 +15,26 @@ select 'foo', approx_count_distinct(int_col) from myview where int_col == 0 grou
 
 -- group-by should not produce any rows (sort aggregate).
 select 'foo', max(struct(int_col)) from myview where int_col == 0 group by 1;
+
+-- Test data.
+CREATE OR REPLACE TEMPORARY VIEW testData AS SELECT * FROM VALUES
+(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (null, 1), (3, null), (null, null)
+AS testData(a, b);
+
+-- Aggregate with empty GroupBy expressions.
+SELECT a, COUNT(b) FROM testData;
+SELECT COUNT(a), COUNT(b) FROM testData;
+
+-- Aggregate with non-empty GroupBy expressions.
+SELECT a, COUNT(b) FROM testData GROUP BY a;
+SELECT a, COUNT(b) FROM testData GROUP BY b;
+SELECT COUNT(a), COUNT(b) FROM testData GROUP BY a;
+
+-- Aggregate with complex GroupBy expressions.
+SELECT a + b, COUNT(b) FROM testData GROUP BY a + b;
+SELECT a + 2, COUNT(b) FROM testData GROUP BY a + 1;
+SELECT a + 1 + 1, COUNT(b) FROM testData GROUP BY a + 1;
+
+-- Aggregate with nulls.
+SELECT SKEWNESS(a), KURTOSIS(a), MIN(a), MAX(a), AVG(a), VARIANCE(a), STDDEV(a), SUM(a), COUNT(a)
+FROM testData;
