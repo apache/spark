d80ef1b479b99f4254edca1b671f740c0851a68a
ahshahid <ashahid@snappydata.io>
2016-10-30 11:10:26 -0700

    Bootstrap perf (#16)
    Change involves:
    1) Reducing the generated code size when writing struct having all fields of same data type. 
    2) Fixing an issue in WholeStageCodeGenExec, where a plan supporting CodeGen was not being prefixed by InputAdapter in case, the node did not participate in whole stage code gen.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/GenerateUnsafeProjection.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/GenerateUnsafeProjection.scala
@@ -109,13 +109,56 @@ object GenerateUnsafeProjection extends CodeGenerator[Seq[Expression], UnsafePro
 
         val writeField = dt match {
           case t: StructType =>
-            s"""
+            val isHomogenousStruct = {
+              var i = 1
+              val ref =  ctx.javaType(t.fields(0).dataType)
+              var broken = false || !ctx.isPrimitiveType(ref) || t.length <=1
+              while( !broken && i < t.length) {
+                if(ctx.javaType(t.fields(i).dataType) != ref) {
+                  broken = true
+                }
+                i +=1
+              }
+              !broken
+            }
+            if(isHomogenousStruct) {
+              val counter = ctx.freshName("counter")
+              val rowWriterChild = ctx.freshName("rowWriterChild")
+
+              s"""
               // Remember the current cursor so that we can calculate how many bytes are
               // written later.
+
+              final int $tmpCursor = $bufferHolder.cursor;
+
+                 if (${input.value} instanceof UnsafeRow) {
+                   ${writeUnsafeData(ctx, s"((UnsafeRow) ${input.value})", bufferHolder)};
+                 } else {
+                      $rowWriterClass $rowWriterChild = new $rowWriterClass($bufferHolder, ${t.length});
+                      $rowWriterChild.reset();
+                      for(int $counter = 0; $counter < ${t.length}; ++$counter) {
+                           if (${input.value}.isNullAt($index)) {
+                             $rowWriterChild.setNullAt($index);
+                           }else {
+                             $rowWriterChild.write($counter, ${ctx.getValue(input.value, t.fields(0).dataType,
+                               counter)});
+                           }
+                       }
+                 }
+                 $rowWriter.setOffsetAndSize($index, $tmpCursor, $bufferHolder.cursor - $tmpCursor);
+            """
+
+
+            }else {
+              s"""
+              // Remember the current cursor so that we can calculate how many bytes are
+              // written later.
+
               final int $tmpCursor = $bufferHolder.cursor;
               ${writeStructToBuffer(ctx, input.value, t.map(_.dataType), bufferHolder)}
               $rowWriter.setOffsetAndSize($index, $tmpCursor, $bufferHolder.cursor - $tmpCursor);
             """
+            }
 
           case a @ ArrayType(et, _) =>
             s"""
