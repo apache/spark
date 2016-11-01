4d2672a408b6061b0e20b622960f7c6f14271275
Wang Lei <lei.wang@kongming-inc.com>
2016-11-01 13:42:10 +0000

    [SPARK-18114][MESOS] Fix mesos cluster scheduler generage command option error
    Enclose --conf option value with "" to support multi value configs like spark.driver.extraJavaOptions, without "", driver will fail to start.
    
    Jenkins Tests.
    
    Test in our production environment, also unit tests, It is a very small change.
    
    Author: Wang Lei <lei.wang@kongming-inc.com>
    
    Closes #15643 from LeightonWong/messos-cluster.
    
    (cherry picked from commit 9b377aa49f14af31f54164378d60e0fdea2142e5)
    Signed-off-by: Sean Owen <sowen@cloudera.com>

--- core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
@@ -448,7 +448,7 @@ private[spark] class MesosClusterScheduler(
     }
     desc.schedulerProperties
       .filter { case (key, _) => !replicatedOptionsBlacklist.contains(key) }
-      .foreach { case (key, value) => options ++= Seq("--conf", s"$key=${shellEscape(value)}") }
+      .foreach { case (key, value) => options ++= Seq("--conf", s""""$key=${shellEscape(value)}"""".stripMargin) }
     options
   }
 
