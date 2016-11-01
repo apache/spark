2542f7fcc9b19b0b577afb4a6bd6152909e0dd0c
jiangxingbo <jiangxb1987@gmail.com>
2016-11-01 19:19:19 +0800

    modify test cases.

--- sql/core/src/test/resources/sql-tests/inputs/group-by.sql
+++ sql/core/src/test/resources/sql-tests/inputs/group-by.sql
@@ -1,21 +1,3 @@
--- Temporary data.
-create temporary view myview as values 128, 256 as v(int_col);
-
--- group by should produce all input rows,
-select int_col, count(*) from myview group by int_col;
-
--- group by should produce a single row.
-select 'foo', count(*) from myview group by 1;
-
--- group-by should not produce any rows (whole stage code generation).
-select 'foo' from myview where int_col == 0 group by 1;
-
--- group-by should not produce any rows (hash aggregate).
-select 'foo', approx_count_distinct(int_col) from myview where int_col == 0 group by 1;
-
--- group-by should not produce any rows (sort aggregate).
-select 'foo', max(struct(int_col)) from myview where int_col == 0 group by 1;
-
 -- Test data.
 CREATE OR REPLACE TEMPORARY VIEW testData AS SELECT * FROM VALUES
 (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (null, 1), (3, null), (null, null)
@@ -29,6 +11,15 @@ SELECT COUNT(a), COUNT(b) FROM testData;
 SELECT a, COUNT(b) FROM testData GROUP BY a;
 SELECT a, COUNT(b) FROM testData GROUP BY b;
 SELECT COUNT(a), COUNT(b) FROM testData GROUP BY a;
+
+-- Whole stage code generation.
+SELECT 'foo' FROM testData WHERE a = 0 GROUP BY a;
+
+-- Hash aggregate.
+SELECT 'foo', APPROX_COUNT_DISTINCT(a) FROM testData WHERE a = 0 GROUP BY a;
+
+-- Sort aggregate.
+SELECT 'foo', MAX(STRUCT(a)) FROM testData WHERE a = 0 GROUP BY a;
 
 -- Aggregate with complex GroupBy expressions.
 SELECT a + b, COUNT(b) FROM testData GROUP BY a + b;
