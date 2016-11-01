e9b8b6185cb70de77d58027fe7fc12efa1834b9f
hyukjinkwon <gurwls223@gmail.com>
2016-11-01 12:46:41 -0700

    [SPARK-17764][SQL] Add `to_json` supporting to convert nested struct column to JSON string
    ## What changes were proposed in this pull request?
    
    This PR proposes to add `to_json` function in contrast with `from_json` in Scala, Java and Python.
    
    It'd be useful if we can convert a same column from/to json. Also, some datasources do not support nested types. If we are forced to save a dataframe into those data sources, we might be able to work around by this function.
    
    The usage is as below:
    
    ``` scala
    val df = Seq(Tuple1(Tuple1(1))).toDF("a")
    df.select(to_json($"a").as("json")).show()
    ```
    
    ``` bash
    +--------+
    |    json|
    +--------+
    |{"_1":1}|
    +--------+
    ```
    ## How was this patch tested?
    
    Unit tests in `JsonFunctionsSuite` and `JsonExpressionsSuite`.
    
    Author: hyukjinkwon <gurwls223@gmail.com>
    
    Closes #15354 from HyukjinKwon/SPARK-17764.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JacksonGenerator.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JacksonGenerator.scala
@@ -15,15 +15,14 @@
  * limitations under the License.
  */
 
-package org.apache.spark.sql.execution.datasources.json
+package org.apache.spark.sql.catalyst.json
 
 import java.io.Writer
 
 import com.fasterxml.jackson.core._
 
-import org.apache.spark.sql.catalyst.expressions.SpecializedGetters
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.json.JSONOptions
+import org.apache.spark.sql.catalyst.expressions.SpecializedGetters
 import org.apache.spark.sql.catalyst.util.{ArrayData, DateTimeUtils, MapData}
 import org.apache.spark.sql.types._
 
