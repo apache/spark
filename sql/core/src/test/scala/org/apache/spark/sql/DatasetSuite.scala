4c679b59b34ea01474baa5d4ac4ce89133a1720d
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2016-11-07 23:26:57 +0900

    addressed review comments

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -988,7 +988,7 @@ class DatasetSuite extends QueryTest with SharedSQLContext {
     }
   }
 
-  test("identity map for primitive array") {
+  test("identity map for primitive arrays") {
     val arrayByte = Array(1.toByte, 2.toByte, 3.toByte)
     val arrayInt = Array(1, 2, 3)
     val arrayLong = Array(1.toLong, 2.toLong, 3.toLong)
