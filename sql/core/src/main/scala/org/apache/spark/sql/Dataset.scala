ce2bb904c921f005a8064b76c8ad245b1087d3e0
Bill Chambers <bill@databricks.com>
2016-11-08 15:13:22 -0800

    even more clarification

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -1613,6 +1613,8 @@ class Dataset[T] private[sql](
 
   /**
    * Returns a new Dataset by sampling a fraction of rows, using a user-supplied seed.
+   * Note: this is NOT guaranteed to provide exactly the fraction specified of the 
+   * Dataset.
    *
    * @param withReplacement Sample with replacement or not.
    * @param fraction Fraction of rows to generate.
@@ -1632,6 +1634,8 @@ class Dataset[T] private[sql](
 
   /**
    * Returns a new Dataset by sampling a fraction of rows, using a random seed.
+   * Note: this is NOT guaranteed to provide exactly the fraction specified of the 
+   * Dataset.
    *
    * @param withReplacement Sample with replacement or not.
    * @param fraction Fraction of rows to generate.
