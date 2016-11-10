0f8fac6079592fef2c6d605c2a26701414c32fb4
Tsuyoshi Ozawa <ozawa@apache.org>
2016-11-10 19:48:16 +0900

    Fix SparkSQLExample to use absolute path

--- examples/src/main/scala/org/apache/spark/examples/sql/SparkSQLExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/sql/SparkSQLExample.scala
@@ -60,7 +60,8 @@ object SparkSQLExample {
 
   private def runBasicDataFrameExample(spark: SparkSession): Unit = {
     // $example on:create_df$
-    val df = spark.read.json("examples/src/main/resources/people.json")
+    val df = spark.read.json(
+      "file:///" + System.getProperty("user.dir") + "/examples/src/main/resources/people.json")
 
     // Displays the content of the DataFrame to stdout
     df.show()
@@ -179,7 +180,8 @@ object SparkSQLExample {
     primitiveDS.map(_ + 1).collect() // Returns: Array(2, 3, 4)
 
     // DataFrames can be converted to a Dataset by providing a class. Mapping will be done by name
-    val path = "examples/src/main/resources/people.json"
+    val path =
+      "file:///" + System.getProperty("user.dir") + "/examples/src/main/resources/people.json"
     val peopleDS = spark.read.json(path).as[Person]
     peopleDS.show()
     // +----+-------+
@@ -199,7 +201,8 @@ object SparkSQLExample {
 
     // Create an RDD of Person objects from a text file, convert it to a Dataframe
     val peopleDF = spark.sparkContext
-      .textFile("examples/src/main/resources/people.txt")
+      .textFile(
+        "file:///" + System.getProperty("user.dir") + "/examples/src/main/resources/people.txt")
       .map(_.split(","))
       .map(attributes => Person(attributes(0), attributes(1).trim.toInt))
       .toDF()
@@ -240,7 +243,8 @@ object SparkSQLExample {
     import spark.implicits._
     // $example on:programmatic_schema$
     // Create an RDD
-    val peopleRDD = spark.sparkContext.textFile("examples/src/main/resources/people.txt")
+    val peopleRDD = spark.sparkContext.textFile(
+      "file:///" + System.getProperty("user.dir") + "/examples/src/main/resources/people.txt")
 
     // The schema is encoded in a string
     val schemaString = "name age"
