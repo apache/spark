b61ab720a6be921a1bfdf87f319ae9ef66a12955
hyukjinkwon <gurwls223@gmail.com>
2016-11-18 17:39:15 +0900

    Minor indentation cleanup

--- core/src/main/scala/org/apache/spark/rdd/SequenceFileRDDFunctions.scala
+++ core/src/main/scala/org/apache/spark/rdd/SequenceFileRDDFunctions.scala
@@ -29,8 +29,8 @@ import org.apache.spark.internal.Logging
  * Extra functions available on RDDs of (key, value) pairs to create a Hadoop SequenceFile,
  * through an implicit conversion.
  *
- * @note This can't be part of PairRDDFunctions because
- * we need more implicit parameters to convert our keys and values to Writable.
+ * @note This can't be part of PairRDDFunctions because we need more implicit parameters to
+ * convert our keys and values to Writable.
  */
 class SequenceFileRDDFunctions[K <% Writable: ClassTag, V <% Writable : ClassTag](
     self: RDD[(K, V)],
