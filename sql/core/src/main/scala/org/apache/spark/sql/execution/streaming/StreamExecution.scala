879c6e1449074badeb6da73fb10fdd6efcb5838c
Burak Yavuz <brkyvz@gmail.com>
2016-11-19 19:29:55 -0800

    make test a bit more complex

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamExecution.scala
@@ -440,7 +440,7 @@ class StreamExecution(
           LocalRelation(output)
         }
     }
-
+    println(replacements)
     // Rewire the plan to use the new attributes that were returned by the source.
     val replacementMap = AttributeMap(replacements)
     val triggerLogicalPlan = withNewSources transformAllExpressions {
