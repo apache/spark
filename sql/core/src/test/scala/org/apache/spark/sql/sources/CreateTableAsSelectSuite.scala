dbdbd1ed2a678edc34abe6f4a6808574ff6ab038
Dilip Biswal <dbiswal@us.ibm.com>
2016-11-21 23:22:03 -0800

    Review comment

--- sql/core/src/test/scala/org/apache/spark/sql/sources/CreateTableAsSelectSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/sources/CreateTableAsSelectSuite.scala
@@ -252,7 +252,7 @@ class CreateTableAsSelectSuite
 
   test("specifying the column list for CTAS") {
     withTable("t") {
-      val e = intercept[AnalysisException] {
+      val e = intercept[ParseException] {
         sql("CREATE TABLE t (a int, b int) USING parquet AS SELECT 1, 2")
       }.getMessage
       assert(e.contains("Schema may not be specified in a Create Table As Select (CTAS)"))
