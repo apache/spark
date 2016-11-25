bd15842e7b146cf292d9c29b896362412b22b8c1
Liang-Chi Hsieh <viirya@gmail.com>
2016-11-25 07:06:26 +0000

    Add partition column test.

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/orc/vectorized/VectorizedSparkOrcNewRecordReaderSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/orc/vectorized/VectorizedSparkOrcNewRecordReaderSuite.scala
@@ -32,13 +32,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit
 import org.scalatest.BeforeAndAfterAll
 
 import org.apache.spark.sql._
-import org.apache.spark.sql.catalyst.{InternalRow, TableIdentifier}
-import org.apache.spark.sql.execution.datasources.{LogicalRelation, RecordReaderIterator}
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.execution.vectorized.ColumnarBatch
-import org.apache.spark.sql.hive.{HiveUtils, MetastoreRelation}
 import org.apache.spark.sql.hive.orc._
-import org.apache.spark.sql.hive.test.TestHive._
-import org.apache.spark.sql.hive.test.TestHive.implicits._
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types._
 import org.apache.spark.unsafe.types.UTF8String
@@ -87,93 +83,127 @@ class VectorizedSparkOrcNewRecordReaderSuite extends QueryTest with BeforeAndAft
     orcRecordReader
   }
 
-  test("Read/write types: batch processing") {
-    val data = (0 to 255).map { i =>
-      (s"$i", i, i.toLong, i.toFloat, i.toDouble, i.toShort, i.toByte, i % 2 == 0,
-        s"$i".getBytes(StandardCharsets.UTF_8))
-    }
-    val dataRows = data.map { x =>
-      InternalRow(UTF8String.fromString(x._1), x._2, x._3, x._4, x._5, x._6, x._7, x._8, x._9)
-    }
+  val partitionSchemas = Seq(
+    StructType(Nil),
+    new StructType().add("p1", IntegerType).add("p2", LongType))
 
-    withOrcFile(data) { file =>
-      val requiredSchema = new StructType()
-        .add("_1", StringType)
-        .add("_2", IntegerType)
-        .add("_3", LongType)
-        .add("_4", FloatType)
-        .add("_5", DoubleType)
-        .add("_6", ShortType)
-        .add("_7", ByteType)
-        .add("_8", BooleanType)
-        .add("_9", BinaryType)
-      val partitionSchema = StructType(Nil)
-      val partitionValues = InternalRow.empty
-      val reader = getVectorizedOrcReader(file, requiredSchema, partitionSchema, partitionValues)
-      assert(reader.nextKeyValue())
+  val partitionValues = Seq(
+    InternalRow.empty,
+    InternalRow(1, 2L))
 
-      // The schema is supported by ColumnarBatch.
-      val nextValue = reader.getCurrentValue()
-      assert(nextValue.isInstanceOf[ColumnarBatch])
+  val partitionSettings = partitionSchemas.zip(partitionValues)
 
-      val batch = nextValue.asInstanceOf[ColumnarBatch]
+  partitionSettings.map { case (partitionSchema, partitionValue) =>
+    val doPartition = partitionValue != InternalRow.empty
+    val partitionTitle = if (doPartition) "with partition" else ""
 
-      assert(batch.numCols() == 9)
-      assert(batch.numRows() == 256)
-      assert(batch.numValidRows() == 256)
-      assert(batch.capacity() > 0)
-      assert(batch.rowIterator().hasNext == true)
+    test(s"Read/write types: batch processing $partitionTitle") {
+      val colNum = if (doPartition) 11 else 9
+      val data = (0 to 255).map { i =>
+        (s"$i", i, i.toLong, i.toFloat, i.toDouble, i.toShort, i.toByte, i % 2 == 0,
+          s"$i".getBytes(StandardCharsets.UTF_8))
+      }
+      val expectedRows = data.map { x =>
+        val data = Seq(UTF8String.fromString(x._1), x._2, x._3, x._4, x._5, x._6, x._7, x._8, x._9)
+        val dataWithPartition = if (doPartition) {
+          data ++ Seq(1, 2L)
+        } else {
+          data
+        }
+        InternalRow.fromSeq(dataWithPartition)
+      }
 
-      assert(batch.column(0).getUTF8String(0).toString() == "0")
-      assert(batch.column(0).isNullAt(0) == false)
-      assert(batch.column(1).getInt(0) == 0)
-      assert(batch.column(1).isNullAt(0) == false)
-      assert(batch.column(4).getDouble(0) == 0.0)
-      assert(batch.column(4).isNullAt(0) == false)
+      withOrcFile(data) { file =>
+        val requiredSchema = new StructType()
+          .add("_1", StringType)
+          .add("_2", IntegerType)
+          .add("_3", LongType)
+          .add("_4", FloatType)
+          .add("_5", DoubleType)
+          .add("_6", ShortType)
+          .add("_7", ByteType)
+          .add("_8", BooleanType)
+          .add("_9", BinaryType)
+        val reader = getVectorizedOrcReader(file, requiredSchema, partitionSchema, partitionValue)
+        assert(reader.nextKeyValue())
 
-      val it = batch.rowIterator()
-      dataRows.map { row =>
-        assert(it.hasNext())
-        assert(it.next().copy() == row)
+        // The schema is supported by ColumnarBatch.
+        val nextValue = reader.getCurrentValue()
+        assert(nextValue.isInstanceOf[ColumnarBatch])
+
+        val batch = nextValue.asInstanceOf[ColumnarBatch]
+
+        assert(batch.numCols() == colNum)
+        assert(batch.numRows() == 256)
+        assert(batch.numValidRows() == 256)
+        assert(batch.capacity() > 0)
+        assert(batch.rowIterator().hasNext == true)
+
+        assert(batch.column(0).getUTF8String(0).toString() == "0")
+        assert(batch.column(0).isNullAt(0) == false)
+        assert(batch.column(1).getInt(0) == 0)
+        assert(batch.column(1).isNullAt(0) == false)
+        assert(batch.column(4).getDouble(0) == 0.0)
+        assert(batch.column(4).isNullAt(0) == false)
+
+        val it = batch.rowIterator()
+        expectedRows.map { row =>
+          assert(it.hasNext())
+          assert(it.next().copy() == row)
+        }
       }
     }
-  }
 
-  test("Read/write types: no batch processing") {
-    val colNum = spark.conf.get(SQLConf.WHOLESTAGE_MAX_NUM_FIELDS.key).toInt + 1
-    val data = (0 to 255).map { i =>
-      Row.fromSeq((i to colNum + i - 1).toSeq)
-    }
-
-    withTempPath { file =>
-      val fields = (1 to colNum).map { idx =>
-        StructField(s"_$idx", IntegerType)
+    test(s"Read/write types: no batch processing $partitionTitle") {
+      val dataColNum = spark.conf.get(SQLConf.WHOLESTAGE_MAX_NUM_FIELDS.key).toInt + 1
+      val colNum = if (doPartition) {
+        dataColNum + 2
+      } else {
+        dataColNum
       }
-      val requiredSchema = StructType(fields.toArray)
-      spark.createDataFrame(sparkContext.parallelize(data), requiredSchema)
-        .write.orc(file.getCanonicalPath)
-      val path = file.getCanonicalPath
 
-      val partitionSchema = StructType(Nil)
-      val partitionValues = InternalRow.empty
-      val reader = getVectorizedOrcReader(path, requiredSchema, partitionSchema, partitionValues)
-      assert(reader.nextKeyValue())
+      val data = (0 to 255).map { i =>
+        Row.fromSeq((i to dataColNum + i - 1).toSeq)
+      }
 
-      // Column number exceeds SQLConf.WHOLESTAGE_MAX_NUM_FIELDS,
-      // so batch processing is not supported.
-      val nextValue = reader.getCurrentValue()
-      assert(nextValue.isInstanceOf[ColumnarBatch.Row])
+      val expectedRows = data.map { x =>
+        val data = x.toSeq
+        val dataWithPartition = if (doPartition) {
+          data ++ Seq(1, 2L)
+        } else {
+          data
+        }
+        InternalRow.fromSeq(dataWithPartition)
+      }
 
-      val batchRow = nextValue.asInstanceOf[ColumnarBatch.Row]
+      withTempPath { file =>
+        val fields = (1 to dataColNum).map { idx =>
+          StructField(s"_$idx", IntegerType)
+        }
+        val requiredSchema = StructType(fields.toArray)
+        spark.createDataFrame(sparkContext.parallelize(data), requiredSchema)
+          .write.orc(file.getCanonicalPath)
+        val path = file.getCanonicalPath
 
-      assert(batchRow.numFields() == colNum)
+        val reader = getVectorizedOrcReader(path, requiredSchema, partitionSchema, partitionValue)
+        assert(reader.nextKeyValue())
 
-      var idx = 0
-      while (reader.nextKeyValue()) {
-        val row = data(idx)
-        val batchRow = reader.getCurrentValue().asInstanceOf[ColumnarBatch.Row].copy()
-        assert(batchRow.toSeq(requiredSchema) === row.toSeq)
-        idx += 1
+        // Column number exceeds SQLConf.WHOLESTAGE_MAX_NUM_FIELDS,
+        // so batch processing is not supported.
+        val nextValue = reader.getCurrentValue()
+        assert(nextValue.isInstanceOf[ColumnarBatch.Row])
+
+        val batchRow = nextValue.asInstanceOf[ColumnarBatch.Row]
+
+        assert(batchRow.numFields() == colNum)
+
+        var idx = 0
+        while (reader.nextKeyValue()) {
+          val row = expectedRows(idx)
+          val batchRow = reader.getCurrentValue().asInstanceOf[ColumnarBatch.Row].copy()
+          assert(batchRow === row)
+          idx += 1
+        }
       }
     }
   }
