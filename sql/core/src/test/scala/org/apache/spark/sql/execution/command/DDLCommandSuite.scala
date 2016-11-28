41430e2d8f6bcbba35ae3ddfee29898685332766
Wenchen Fan <wenchen@databricks.com>
2016-11-28 08:46:00 -0800

    [SPARK-17732][SQL] Revert ALTER TABLE DROP PARTITION should support comparators
    ## What changes were proposed in this pull request?
    
    https://github.com/apache/spark/pull/15704 will fail if we use int literal in `DROP PARTITION`, and we have reverted it in branch-2.1.
    
    This PR reverts it in master branch, and add a regression test for it, to make sure the master branch is healthy.
    
    ## How was this patch tested?
    
    new regression test
    
    Author: Wenchen Fan <wenchen@databricks.com>
    
    Closes #16036 from cloud-fan/revert.

--- sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLCommandSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLCommandSuite.scala
@@ -21,7 +21,6 @@ import scala.reflect.{classTag, ClassTag}
 
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.catalyst.catalog._
-import org.apache.spark.sql.catalyst.expressions.{And, AttributeReference, EqualTo, Literal}
 import org.apache.spark.sql.catalyst.parser.ParseException
 import org.apache.spark.sql.catalyst.plans.PlanTest
 import org.apache.spark.sql.catalyst.plans.logical.Project
@@ -613,12 +612,8 @@ class DDLCommandSuite extends PlanTest {
     val expected1_table = AlterTableDropPartitionCommand(
       tableIdent,
       Seq(
-        And(
-          EqualTo(AttributeReference("dt", StringType)(), Literal.create("2008-08-08", StringType)),
-          EqualTo(AttributeReference("country", StringType)(), Literal.create("us", StringType))),
-        And(
-          EqualTo(AttributeReference("dt", StringType)(), Literal.create("2009-09-09", StringType)),
-          EqualTo(AttributeReference("country", StringType)(), Literal.create("uk", StringType)))),
+        Map("dt" -> "2008-08-08", "country" -> "us"),
+        Map("dt" -> "2009-09-09", "country" -> "uk")),
       ifExists = true,
       purge = false)
     val expected2_table = expected1_table.copy(ifExists = false)
