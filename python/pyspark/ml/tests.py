3e235f347225f06c083ce5b52de2a8e0ab59c6c3
Jeff Zhang <zjffdu@apache.org>
2016-11-29 20:51:27 -0800

    [SPARK-15819][PYSPARK][ML] Add KMeanSummary in KMeans of PySpark
    ## What changes were proposed in this pull request?
    
    Add python api for KMeansSummary
    ## How was this patch tested?
    
    unit test added
    
    Author: Jeff Zhang <zjffdu@apache.org>
    
    Closes #13557 from zjffdu/SPARK-15819.

--- python/pyspark/ml/tests.py
+++ python/pyspark/ml/tests.py
@@ -1129,6 +1129,21 @@ class TrainingSummaryTest(SparkSessionTestCase):
         self.assertEqual(len(s.clusterSizes), 2)
         self.assertEqual(s.k, 2)
 
+    def test_kmeans_summary(self):
+        data = [(Vectors.dense([0.0, 0.0]),), (Vectors.dense([1.0, 1.0]),),
+                (Vectors.dense([9.0, 8.0]),), (Vectors.dense([8.0, 9.0]),)]
+        df = self.spark.createDataFrame(data, ["features"])
+        kmeans = KMeans(k=2, seed=1)
+        model = kmeans.fit(df)
+        self.assertTrue(model.hasSummary)
+        s = model.summary
+        self.assertTrue(isinstance(s.predictions, DataFrame))
+        self.assertEqual(s.featuresCol, "features")
+        self.assertEqual(s.predictionCol, "prediction")
+        self.assertTrue(isinstance(s.cluster, DataFrame))
+        self.assertEqual(len(s.clusterSizes), 2)
+        self.assertEqual(s.k, 2)
+
 
 class OneVsRestTests(SparkSessionTestCase):
 
