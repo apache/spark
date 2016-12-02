27c102deb1701fe62f776fe4da61dac959270b73
Nathan Howell <nhowell@godaddy.com>
2016-12-02 00:00:48 +0000

    Improve test coverage of `UTFString.write`

--- common/unsafe/src/main/java/org/apache/spark/unsafe/types/UTF8String.java
+++ common/unsafe/src/main/java/org/apache/spark/unsafe/types/UTF8String.java
@@ -148,11 +148,19 @@ public final class UTF8String implements Comparable<UTF8String>, Externalizable,
   }
 
   public void writeTo(OutputStream out) throws IOException {
-    if (base instanceof byte[] &&
-        offset >= BYTE_ARRAY_OFFSET &&
-        offset < (long) Integer.MAX_VALUE) {
-      // the offset includes the object header... but it is only needed for unsafe copies
-      out.write((byte[]) base, (int) offset - BYTE_ARRAY_OFFSET, numBytes);
+    if (base instanceof byte[] && offset >= BYTE_ARRAY_OFFSET) {
+      final byte[] bytes = (byte[]) base;
+
+      // the offset includes an object header... this is only needed for unsafe copies
+      final long arrayOffset = offset - BYTE_ARRAY_OFFSET;
+
+      // verify that the offset and length points somewhere inside the byte array
+      // and that the offset can safely be truncated to a 32-bit integer
+      if ((long) bytes.length < arrayOffset + numBytes) {
+        throw new ArrayIndexOutOfBoundsException();
+      }
+
+      out.write(bytes, (int) arrayOffset, numBytes);
     } else {
       out.write(getBytes());
     }
