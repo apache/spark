787acfe28afc259954c7b20026dc50bb6fdb3e3f
Yuming Wang <wgyumg@gmail.com>
2016-12-04 07:35:39 +0800

    Compatible with hive-0.12 and hive-0.13

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveShim.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveShim.scala
@@ -590,7 +590,8 @@ private[client] class Shim_v0_13 extends Shim_v0_12 {
       } else {
         logDebug(s"Hive metastore filter is '$filter'.")
         val tryDirectSqlConfVar = HiveConf.ConfVars.METASTORE_TRY_DIRECT_SQL
-        val tryDirectSql = hive.getMSC.getMetaConf(tryDirectSqlConfVar.varname).toBoolean
+        val tryDirectSql = hive.getMSC.getConfigValue(tryDirectSqlConfVar.varname,
+          tryDirectSqlConfVar.getDefaultValue).toBoolean
         try {
           // Hive may throw an exception when calling this method in some circumstances, such as
           // when filtering on a non-string partition column when the hive config key
