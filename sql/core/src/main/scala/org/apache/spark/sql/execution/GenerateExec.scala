20c350a5e4097c25f9711e37c28464df548a5766
Liang-Chi Hsieh <viirya@gmail.com>
2016-12-05 17:50:43 -0800

    [SPARK-18634][PYSPARK][SQL] Corruption and Correctness issues with exploding Python UDFs
    ## What changes were proposed in this pull request?
    
    As reported in the Jira, there are some weird issues with exploding Python UDFs in SparkSQL.
    
    The following test code can reproduce it. Notice: the following test code is reported to return wrong results in the Jira. However, as I tested on master branch, it causes exception and so can't return any result.
    
        >>> from pyspark.sql.functions import *
        >>> from pyspark.sql.types import *
        >>>
        >>> df = spark.range(10)
        >>>
        >>> def return_range(value):
        ...   return [(i, str(i)) for i in range(value - 1, value + 1)]
        ...
        >>> range_udf = udf(return_range, ArrayType(StructType([StructField("integer_val", IntegerType()),
        ...                                                     StructField("string_val", StringType())])))
        >>>
        >>> df.select("id", explode(range_udf(df.id))).show()
        Traceback (most recent call last):
          File "<stdin>", line 1, in <module>
          File "/spark/python/pyspark/sql/dataframe.py", line 318, in show
            print(self._jdf.showString(n, 20))
          File "/spark/python/lib/py4j-0.10.4-src.zip/py4j/java_gateway.py", line 1133, in __call__
          File "/spark/python/pyspark/sql/utils.py", line 63, in deco
            return f(*a, **kw)
          File "/spark/python/lib/py4j-0.10.4-src.zip/py4j/protocol.py", line 319, in get_return_value py4j.protocol.Py4JJavaError: An error occurred while calling o126.showString.: java.lang.AssertionError: assertion failed
            at scala.Predef$.assert(Predef.scala:156)
            at org.apache.spark.sql.execution.CodegenSupport$class.consume(WholeStageCodegenExec.scala:120)
            at org.apache.spark.sql.execution.GenerateExec.consume(GenerateExec.scala:57)
    
    The cause of this issue is, in `ExtractPythonUDFs` we insert `BatchEvalPythonExec` to run PythonUDFs in batch. `BatchEvalPythonExec` will add extra outputs (e.g., `pythonUDF0`) to original plan. In above case, the original `Range` only has one output `id`. After `ExtractPythonUDFs`, the added `BatchEvalPythonExec` has two outputs `id` and `pythonUDF0`.
    
    Because the output of `GenerateExec` is given after analysis phase, in above case, it is the combination of `id`, i.e., the output of `Range`, and `col`. But in planning phase, we change `GenerateExec`'s child plan to `BatchEvalPythonExec` with additional output attributes.
    
    It will cause no problem in non wholestage codegen. Because when evaluating the additional attributes are projected out the final output of `GenerateExec`.
    
    However, as `GenerateExec` now supports wholestage codegen, the framework will input all the outputs of the child plan to `GenerateExec`. Then when consuming `GenerateExec`'s output data (i.e., calling `consume`), the number of output attributes is different to the output variables in wholestage codegen.
    
    To solve this issue, this patch only gives the generator's output to `GenerateExec` after analysis phase. `GenerateExec`'s output is the combination of its child plan's output and the generator's output. So when we change `GenerateExec`'s child, its output is still correct.
    
    ## How was this patch tested?
    
    Added test cases to PySpark.
    
    Please review http://spark.apache.org/contributing.html before opening a pull request.
    
    Author: Liang-Chi Hsieh <viirya@gmail.com>
    
    Closes #16120 from viirya/fix-py-udf-with-generator.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/GenerateExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/GenerateExec.scala
@@ -51,16 +51,25 @@ private[execution] sealed case class LazyIterator(func: () => TraversableOnce[In
  *              it.
  * @param outer when true, each input row will be output at least once, even if the output of the
  *              given `generator` is empty. `outer` has no effect when `join` is false.
- * @param output the output attributes of this node, which constructed in analysis phase,
- *               and we can not change it, as the parent node bound with it already.
+ * @param generatorOutput the qualified output attributes of the generator of this node, which
+ *                        constructed in analysis phase, and we can not change it, as the
+ *                        parent node bound with it already.
  */
 case class GenerateExec(
     generator: Generator,
     join: Boolean,
     outer: Boolean,
-    output: Seq[Attribute],
+    generatorOutput: Seq[Attribute],
     child: SparkPlan)
   extends UnaryExecNode with CodegenSupport {
+
+  override def output: Seq[Attribute] = {
+    if (join) {
+      child.output ++ generatorOutput
+    } else {
+      generatorOutput
+    }
+  }
 
   override lazy val metrics = Map(
     "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"))
