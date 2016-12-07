acb6ac5da7a5694cc3270772c6d68933b7d761dc
Shixiong Zhu <shixiong@databricks.com>
2016-12-07 10:30:05 -0800

    [SPARK-18764][CORE] Add a warning log when skipping a corrupted file
    ## What changes were proposed in this pull request?
    
    It's better to add a warning log when skipping a corrupted file. It will be helpful when we want to finish the job first, then find them in the log and fix these files.
    
    ## How was this patch tested?
    
    Jenkins
    
    Author: Shixiong Zhu <shixiong@databricks.com>
    
    Closes #16192 from zsxwing/SPARK-18764.
    
    (cherry picked from commit dbf3e298a1a35c0243f087814ddf88034ff96d66)
    Signed-off-by: Shixiong Zhu <shixiong@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/FileScanRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/FileScanRDD.scala
@@ -138,6 +138,7 @@ class FileScanRDD(
                     }
                   } catch {
                     case e: IOException =>
+                      logWarning(s"Skipped the rest content in the corrupted file: $currentFile", e)
                       finished = true
                       null
                   }
