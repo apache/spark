45bb563a6ce8355de59f0e76a86e8f1a6a7dc15d
Zheng RuiFeng <ruifengz@foxmail.com>
2016-12-08 13:21:56 +0800

    fix isotonic

--- mllib/src/test/scala/org/apache/spark/ml/clustering/GaussianMixtureSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/clustering/GaussianMixtureSuite.scala
@@ -18,7 +18,7 @@
 package org.apache.spark.ml.clustering
 
 import org.apache.spark.SparkFunSuite
-import org.apache.spark.ml.linalg.{DenseMatrix, Matrices, Vector, Vectors}
+import org.apache.spark.ml.linalg._
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.ml.stat.distribution.MultivariateGaussian
 import org.apache.spark.ml.util.{DefaultReadWriteTest, MLTestingUtils}
