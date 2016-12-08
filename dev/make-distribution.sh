c15916aebd8e3991f45d244e298c0362785a1e81
Shivaram Venkataraman <shivaram@cs.berkeley.edu>
2016-12-08 15:32:01 -0800

    Fix spelling

--- dev/make-distribution.sh
+++ dev/make-distribution.sh
@@ -228,7 +228,7 @@ if [ "$MAKE_R" == "true" ]; then
   # Install source package to get it to generate vignettes, etc.
   # Do not source the check-cran.sh - it should be run from where it is for it to set SPARK_HOME
   NO_TESTS=1 CLEAN_INSTALL=1 "$SPARK_HOME/"R/check-cran.sh
-  # Make a copy of R source package matchin the Spark release version.
+  # Make a copy of R source package matching the Spark release version.
   cp $SPARK_HOME/R/SparkR_"$R_PACKAGE_VERSION".tar.gz $SPARK_HOME/R/SparkR_"$VERSION".tar.gz
   popd > /dev/null
 else
