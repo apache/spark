e2aec8a0d1fd3c2a5697067d07139be9c7dc1ab5
Sumedh Wale <swale@snappydata.io>
2016-12-10 00:55:19 +0530

    adding kryo serialization missing in LongHashedRelation

--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashedRelation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashedRelation.scala
@@ -754,7 +754,8 @@ private[execution] final class LongToUnsafeRowMap(val mm: TaskMemoryManager, cap
 
 private[joins] class LongHashedRelation(
     private var nFields: Int,
-    private var map: LongToUnsafeRowMap) extends HashedRelation with Externalizable {
+    private var map: LongToUnsafeRowMap) extends HashedRelation
+    with Externalizable with KryoSerializable {
 
   private var resultRow: UnsafeRow = new UnsafeRow(nFields)
 
@@ -796,10 +797,21 @@ private[joins] class LongHashedRelation(
     out.writeObject(map)
   }
 
+  override def write(kryo: Kryo, output: Output): Unit = {
+    output.writeInt(nFields)
+    kryo.writeClassAndObject(output, map)
+  }
+
   override def readExternal(in: ObjectInput): Unit = {
     nFields = in.readInt()
     resultRow = new UnsafeRow(nFields)
     map = in.readObject().asInstanceOf[LongToUnsafeRowMap]
+  }
+
+  override def read(kryo: Kryo, input: Input): Unit = {
+    nFields = input.readInt()
+    resultRow = new UnsafeRow(nFields)
+    map = kryo.readClassAndObject(input).asInstanceOf[LongToUnsafeRowMap]
   }
 }
 
