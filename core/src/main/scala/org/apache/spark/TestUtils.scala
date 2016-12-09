88c38ffd659e7c5256cce112e1607554059060a2
hyukjinkwon <gurwls223@gmail.com>
2016-12-09 18:34:34 +0900

    Fix path-related and JarEntry-related test failures and skip some tests failed on Windows due to path length limitation

--- core/src/main/scala/org/apache/spark/TestUtils.scala
+++ core/src/main/scala/org/apache/spark/TestUtils.scala
@@ -93,7 +93,9 @@ private[spark] object TestUtils {
     val jarStream = new JarOutputStream(jarFileStream, new java.util.jar.Manifest())
 
     for (file <- files) {
-      val jarEntry = new JarEntry(Paths.get(directoryPrefix.getOrElse(""), file.getName).toString)
+      // The `name` for the argument in `JarEntry` should use / for its separator. This is
+      // ZIP specification.
+      val jarEntry = new JarEntry(s"${directoryPrefix.getOrElse("")}/${file.getName}")
       jarStream.putNextEntry(jarEntry)
 
       val in = new FileInputStream(file)
