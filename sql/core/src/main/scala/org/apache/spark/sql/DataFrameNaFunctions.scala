79492924bfad40f36aa132e58376da98b0727eac
bravo-zhang <mzhang1230@gmail.com>
2016-12-09 09:44:39 -0800

    Use pattern matching for null case

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameNaFunctions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameNaFunctions.scala
@@ -342,10 +342,8 @@ final class DataFrameNaFunctions private[sql](df: DataFrame) {
     }
 
     // replacementMap is either Map[String, String] or Map[Double, Double] or Map[Boolean,Boolean]
-    val replacementMap: Map[_, _] =
-      if (replacement.head._2 == null)
-        replacement
-      else replacement.head._2 match {
+    val replacementMap: Map[_, _] = replacement.head._2 match {
+        case null => replacement
         case v: String => replacement
         case v: Boolean => replacement
         case _ => replacement.map { case (k, v) => (convertToDouble(k), convertToDouble(v)) }
