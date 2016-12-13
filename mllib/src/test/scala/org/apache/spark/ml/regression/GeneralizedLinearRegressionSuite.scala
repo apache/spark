6e6c48b79065666e1e896eec76e1ffa8cb751b6e
actuaryzhang <actuaryzhang10@gmail.com>
2016-12-12 22:35:38 -0800

    Add test case for non-integer Binomial weights (that will round up)

--- mllib/src/test/scala/org/apache/spark/ml/regression/GeneralizedLinearRegressionSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/regression/GeneralizedLinearRegressionSuite.scala
@@ -709,14 +709,14 @@ class GeneralizedLinearRegressionSuite
 
        A <- matrix(c(0, 1, 2, 3, 5, 2, 1, 3), 4, 2)
        b <- c(1, 0.5, 1, 0)
-       w <- c(1, 2, 3, 4)
+       w <- c(1, 2.0, 0.3, 4.7)
        df <- as.data.frame(cbind(A, b))
      */
     val datasetWithWeight = Seq(
       Instance(1.0, 1.0, Vectors.dense(0.0, 5.0).toSparse),
       Instance(0.5, 2.0, Vectors.dense(1.0, 2.0)),
       Instance(1.0, 0.3, Vectors.dense(2.0, 1.0)),
-      Instance(0.0, 4.0, Vectors.dense(3.0, 3.0))
+      Instance(0.0, 4.7, Vectors.dense(3.0, 3.0))
     ).toDF()
 
     /*
@@ -726,33 +726,33 @@ class GeneralizedLinearRegressionSuite
        summary(model)
 
        Deviance Residuals:
-              1        2        3        4
-         0.2509   0.1755   1.2578  -0.6835
-        
+             1        2        3        4
+        0.2404   0.1965   1.2824  -0.6916
+
        Coefficients:
-           Estimate Std. Error z value Pr(>|z|)
-       x1  -1.6256     1.2575  -1.293    0.196
-       x2   0.6886     0.9201   0.748    0.454
-        
+          Estimate Std. Error z value Pr(>|z|)
+       x1  -1.6901     1.2764  -1.324    0.185
+       x2   0.7059     0.9449   0.747    0.455
+
        (Dispersion parameter for binomial family taken to be 1)
-        
-           Null deviance: 7.3474  on 4  degrees of freedom
-       Residual deviance: 2.1431  on 2  degrees of freedom
-       AIC: 5.9472
-        
+
+           Null deviance: 8.3178  on 4  degrees of freedom
+       Residual deviance: 2.2193  on 2  degrees of freedom
+       AIC: 5.9915
+
        Number of Fisher Scoring iterations: 5
 
        residuals(model, type="pearson")
               1         2         3         4
-       0.1788113  0.1761654  1.9726225 -0.4904407
+       0.171217  0.197406  2.085864 -0.495332
 
        residuals(model, type="working")
               1         2         3         4
-       1.031973  0.251061 13.970798 -1.060133
+       1.029315  0.281881 15.502768 -1.052203
 
        residuals(model, type="response")
-               1          2          3          4
-       0.03098285  0.06180621  0.92842213 -0.05672214
+              1          2          3          4
+       0.028480  0.069123  0.935495 -0.049613
     */
     val trainer = new GeneralizedLinearRegression()
       .setFamily("binomial")
@@ -761,21 +761,21 @@ class GeneralizedLinearRegressionSuite
 
     val model = trainer.fit(datasetWithWeight)
 
-    val coefficientsR = Vectors.dense(Array(-1.6256351, 0.6885697))
+    val coefficientsR = Vectors.dense(Array(-1.690134, 0.705929))
     val interceptR = 0.0
-    val devianceResidualsR = Array(0.2509, 0.1755, 1.2578, -0.6835)
-    val pearsonResidualsR = Array(0.1788113, 0.1761654, 1.9726225, -0.4904407)
-    val workingResidualsR = Array(1.031973, 0.251061, 13.970798, -1.060133)
-    val responseResidualsR = Array(0.03098285, 0.06180621, 0.92842213, -0.05672214)
-    val seCoefR = Array(1.257499, 0.920116)
-    val tValsR = Array(-1.292752, 0.748351)
-    val pValsR = Array(0.196097, 0.454248)
+    val devianceResidualsR = Array(0.2404, 0.1965, 1.2824, -0.6916)
+    val pearsonResidualsR = Array(0.171217, 0.197406, 2.085864, -0.495332)
+    val workingResidualsR = Array(1.029315, 0.281881, 15.502768, -1.052203)
+    val responseResidualsR = Array(0.02848, 0.069123, 0.935495, -0.049613)
+    val seCoefR = Array(1.276417, 0.944934)
+    val tValsR = Array(-1.324124, 0.747068)
+    val pValsR = Array(0.185462, 0.455023)
     val dispersionR = 1.0
-    val nullDevianceR = 7.3474
-    val residualDevianceR = 2.1431
+    val nullDevianceR = 8.3178
+    val residualDevianceR = 2.2193
     val residualDegreeOfFreedomNullR = 4
     val residualDegreeOfFreedomR = 2
-    val aicR = 5.9472
+    val aicR = 5.991537
 
     val summary = model.summary
     val devianceResiduals = summary.residuals()
