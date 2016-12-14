8ef005931a242d087f4879805571be0660aefaf9
wm624@hotmail.com <wm624@hotmail.com>
2016-12-13 18:52:05 -0800

    [MINOR][SPARKR] fix kstest example error and add unit test
    ## What changes were proposed in this pull request?
    
    While adding vignettes for kstest, I found some errors in the example:
    1. There is a typo of kstest;
    2. print.summary.KStest doesn't work with the example;
    
    Fix the example errors;
    Add a new unit test for print.summary.KStest;
    
    ## How was this patch tested?
    Manual test;
    Add new unit test;
    
    Author: wm624@hotmail.com <wm624@hotmail.com>
    
    Closes #16259 from wangmiao1981/ks.
    
    (cherry picked from commit f2ddabfa09fda26ff0391d026dd67545dab33e01)
    Signed-off-by: Yanbo Liang <ybliang8@gmail.com>

--- R/pkg/inst/tests/testthat/test_mllib.R
+++ R/pkg/inst/tests/testthat/test_mllib.R
@@ -986,6 +986,12 @@ test_that("spark.kstest", {
   expect_equal(stats$p.value, rStats$p.value, tolerance = 1e-4)
   expect_equal(stats$statistic, unname(rStats$statistic), tolerance = 1e-4)
   expect_match(capture.output(stats)[1], "Kolmogorov-Smirnov test summary:")
+
+  # Test print.summary.KSTest
+  printStats <- capture.output(print.summary.KSTest(stats))
+  expect_match(printStats[1], "Kolmogorov-Smirnov test summary:")
+  expect_match(printStats[5],
+               "Low presumption against null hypothesis: Sample follows theoretical distribution. ")
 })
 
 test_that("spark.randomForest", {
