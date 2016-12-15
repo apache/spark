2847738d9e57e8b16003c2f2520572f5e76dbf2f
Shixiong Zhu <shixiong@databricks.com>
2016-12-15 10:51:56 -0800

    Address TD's comments

--- sql/core/src/test/scala/org/apache/spark/sql/streaming/FileStreamSourceSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/streaming/FileStreamSourceSuite.scala
@@ -1066,64 +1066,44 @@ class FileStreamSourceSuite extends FileStreamSourceTest {
       // Prepare two files: 1.txt, 2.txt, and make sure they have different modified time.
       val f1 = stringToFile(new File(src, "1.txt"), "1")
       val f2 = stringToFile(new File(src, "2.txt"), "2")
-      eventually(timeout(streamingTimeout)) {
-        f2.setLastModified(System.currentTimeMillis())
-        assert(f1.lastModified < f2.lastModified)
+      f2.setLastModified(f1.lastModified + 1000)
+
+      def runTwoBatchesAndVerifyResults(
+          latestFirst: Boolean,
+          firstBatch: String,
+          secondBatch: String): Unit = {
+        val fileStream = createFileStream(
+          "text",
+          src.getCanonicalPath,
+          options = Map("latestFirst" -> latestFirst.toString, "maxFilesPerTrigger" -> "1"))
+        val clock = new StreamManualClock()
+        testStream(fileStream)(
+          StartStream(trigger = ProcessingTime(10), triggerClock = clock),
+          AssertOnQuery { _ =>
+            // Block until the first batch finishes.
+            eventually(timeout(streamingTimeout)) {
+              assert(clock.isStreamWaitingAt(0))
+            }
+            true
+          },
+          CheckLastBatch(firstBatch),
+          AdvanceManualClock(10),
+          AssertOnQuery { _ =>
+            // Block until the second batch finishes.
+            eventually(timeout(streamingTimeout)) {
+              assert(clock.isStreamWaitingAt(10))
+            }
+            true
+          },
+          CheckLastBatch(secondBatch)
+        )
       }
 
       // Read oldest files first, so the first batch is "1", and the second batch is "2".
-      val fileStream = createFileStream(
-        "text",
-        src.getCanonicalPath,
-        options = Map("latestFirst" -> "false", "maxFilesPerTrigger" -> "1"))
-      val clock = new StreamManualClock()
-      testStream(fileStream)(
-        StartStream(trigger = ProcessingTime(10), triggerClock = clock),
-        AssertOnQuery { _ =>
-          // Block until the first batch finishes.
-          eventually(timeout(streamingTimeout)) {
-            assert(clock.isStreamWaitingAt(0))
-          }
-          true
-        },
-        CheckLastBatch("1"),
-        AdvanceManualClock(10),
-        AssertOnQuery { _ =>
-          // Block until the second batch finishes.
-          eventually(timeout(streamingTimeout)) {
-            assert(clock.isStreamWaitingAt(10))
-          }
-          true
-        },
-        CheckLastBatch("2")
-      )
+      runTwoBatchesAndVerifyResults(latestFirst = false, firstBatch = "1", secondBatch = "2")
 
       // Read latest files first, so the first batch is "2", and the second batch is "1".
-      val fileStream2 = createFileStream(
-        "text",
-        src.getCanonicalPath,
-        options = Map("latestFirst" -> "true", "maxFilesPerTrigger" -> "1"))
-      val clock2 = new StreamManualClock()
-      testStream(fileStream2)(
-        StartStream(trigger = ProcessingTime(10), triggerClock = clock2),
-        AssertOnQuery { _ =>
-          // Block until the first batch finishes.
-          eventually(timeout(streamingTimeout)) {
-            assert(clock2.isStreamWaitingAt(0))
-          }
-          true
-        },
-        CheckLastBatch("2"),
-        AdvanceManualClock(10),
-        AssertOnQuery { _ =>
-          // Block until the second batch finishes.
-          eventually(timeout(streamingTimeout)) {
-            assert(clock2.isStreamWaitingAt(10))
-          }
-          true
-        },
-        CheckLastBatch("1")
-      )
+      runTwoBatchesAndVerifyResults(latestFirst = true, firstBatch = "2", secondBatch = "1")
     }
   }
 }
