7c4883f12c4f00d0e2a80e78c3de88a0f16622bf
Zakaria_Hili <zakahili@gmail.com>
2016-12-15 09:58:32 +0100

    Eliminate the extra fit() method

--- mllib/src/main/scala/org/apache/spark/ml/clustering/KMeans.scala
+++ mllib/src/main/scala/org/apache/spark/ml/clustering/KMeans.scala
@@ -304,10 +304,10 @@ class KMeans @Since("1.5.0") (
   override def fit(dataset: Dataset[_]): KMeansModel = {
     transformSchema(dataset.schema, logging = true)
 
+    val handlePersistence = dataset.rdd.getStorageLevel == StorageLevel.NONE
     val instances: RDD[OldVector] = dataset.select(col($(featuresCol))).rdd.map {
       case Row(point: Vector) => OldVectors.fromML(point)
     }
-    val handlePersistence = instances.getStorageLevel == StorageLevel.NONE
 
     if (handlePersistence) {
       instances.persist(StorageLevel.MEMORY_AND_DISK)
