94dce7728881b02ed10e82120f9c46ed55b24144
saturday_s <shi.indetail@gmail.com>
2016-12-22 12:19:56 +0900

    Fix style issue.

--- streaming/src/main/scala/org/apache/spark/status/api/v1/streaming/api.scala
+++ streaming/src/main/scala/org/apache/spark/status/api/v1/streaming/api.scala
@@ -22,58 +22,54 @@ import java.util.Date
 import org.apache.spark.streaming.ui.StreamingJobProgressListener._
 
 class StreamingStatistics private[spark](
-  val startTime: Date,
-  val batchDuration: Long,
-  val numReceivers: Int,
-  val numActiveReceivers: Int,
-  val numInactiveReceivers: Int,
-  val numTotalCompletedBatches: Long,
-  val numRetainedCompletedBatches: Long,
-  val numActiveBatches: Long,
-  val numProcessedRecords: Long,
-  val numReceivedRecords: Long,
-  val avgInputRate: Option[Double],
-  val avgSchedulingDelay: Option[Long],
-  val avgProcessingTime: Option[Long],
-  val avgTotalDelay: Option[Long]
-)
+    val startTime: Date,
+    val batchDuration: Long,
+    val numReceivers: Int,
+    val numActiveReceivers: Int,
+    val numInactiveReceivers: Int,
+    val numTotalCompletedBatches: Long,
+    val numRetainedCompletedBatches: Long,
+    val numActiveBatches: Long,
+    val numProcessedRecords: Long,
+    val numReceivedRecords: Long,
+    val avgInputRate: Option[Double],
+    val avgSchedulingDelay: Option[Long],
+    val avgProcessingTime: Option[Long],
+    val avgTotalDelay: Option[Long])
 
 class ReceiverInfo private[spark](
-  val streamId: Int,
-  val streamName: String,
-  val isActive: Option[Boolean],
-  val executorId: Option[String],
-  val executorHost: Option[String],
-  val lastErrorTime: Option[Date],
-  val lastErrorMessage: Option[String],
-  val lastError: Option[String],
-  val avgEventRate: Option[Double],
-  val eventRates: Seq[(Long, Double)]
-)
+    val streamId: Int,
+    val streamName: String,
+    val isActive: Option[Boolean],
+    val executorId: Option[String],
+    val executorHost: Option[String],
+    val lastErrorTime: Option[Date],
+    val lastErrorMessage: Option[String],
+    val lastError: Option[String],
+    val avgEventRate: Option[Double],
+    val eventRates: Seq[(Long, Double)])
 
 class BatchInfo private[spark](
-  val batchId: Long,
-  val batchTime: Date,
-  val status: String,
-  val batchDuration: Long,
-  val inputSize: Long,
-  val schedulingDelay: Option[Long],
-  val processingTime: Option[Long],
-  val totalDelay: Option[Long],
-  val numActiveOutputOps: Int,
-  val numCompletedOutputOps: Int,
-  val numFailedOutputOps: Int,
-  val numTotalOutputOps: Int,
-  val firstFailureReason: Option[String]
-)
+    val batchId: Long,
+    val batchTime: Date,
+    val status: String,
+    val batchDuration: Long,
+    val inputSize: Long,
+    val schedulingDelay: Option[Long],
+    val processingTime: Option[Long],
+    val totalDelay: Option[Long],
+    val numActiveOutputOps: Int,
+    val numCompletedOutputOps: Int,
+    val numFailedOutputOps: Int,
+    val numTotalOutputOps: Int,
+    val firstFailureReason: Option[String])
 
 class OutputOperationInfo private[spark](
-  val outputOpId: OutputOpId,
-  val name: String,
-  val description: String,
-  val startTime: Option[Date],
-  val endTime: Option[Date],
-  val duration: Option[Long],
-  val failureReason: Option[String],
-  val jobIds: Seq[SparkJobId]
-)
+    val outputOpId: OutputOpId,
+    val name: String,
+    val description: String,
+    val startTime: Option[Date],
+    val endTime: Option[Date],
+    val duration: Option[Long],
+    val failureReason: Option[String],
+    val jobIds: Seq[SparkJobId])
