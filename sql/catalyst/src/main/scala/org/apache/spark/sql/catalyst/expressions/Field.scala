b441ef6bcdb74a5ac33132a52318e3aa18041d81
GuoChenzhao <chenzhao.guo@intel.com>
2016-12-28 21:00:40 +0800

    Add doGenCode and modify the function description

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Field.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Field.scala
@@ -4,16 +4,22 @@ import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
 import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
 import org.apache.spark.sql.catalyst.util.TypeUtils
-import org.apache.spark.sql.types.{AtomicType, DataType, IntegerType, NullType}
+import org.apache.spark.sql.types.{AtomicType, DataType, IntegerType}
 /**
-  * A function that returns the greatest value of all parameters, skipping null values.
-  * It takes at least 2 parameters, and returns null iff all parameters are null.
+  * A function that returns the index of str in (str1, str2, ...) list or 0 if not found.
+  * It takes at least 2 parameters, and all parameters' types should be subtypes of AtomicTppe
   */
-
+@ExpressionDescription(
+  usage = "_FUNC_(str, str1, str2, ...) - Returns the index of str in the str1,str2,... list or 0 if not found.",
+  extended = """
+    Examples:
+      > SELECT _FUNC_(10, 9, 3, 10, 4);
+       3
+             """)
 case class Field(children: Seq[Expression]) extends Expression {
 
   override def nullable: Boolean = false
-  override def foldable: Boolean = children.forall(_.nullable)
+  override def foldable: Boolean = children.forall(_.foldable)
 
   private lazy val ordering = TypeUtils.getInterpretedOrdering(children(0).dataType)
 
@@ -21,15 +27,17 @@ case class Field(children: Seq[Expression]) extends Expression {
     if (children.length <= 1) {
       TypeCheckResult.TypeCheckFailure(s"FIELD requires at least 2 arguments")
     }
-    else if (children.map(_.dataType).count(t => (!classOf[AtomicType].isAssignableFrom(t.getClass()))) == 0) {
+    else if (!children.forall(_.dataType.isInstanceOf[AtomicType])) {
       TypeCheckResult.TypeCheckFailure(s"FIELD requires all arguments to be of AtomicType")
     }
     else
       TypeCheckResult.TypeCheckSuccess
   }
+
   override def dataType: DataType = IntegerType
+
   override def eval(input: InternalRow): Int = {
-    val param1 = children.head.eval(input)
+    val target = children.head.eval(input)
     val targetDataType = children.head.dataType
     def findEqual(target: Any, params: Seq[Expression], index: Int): Int = {
       params match {
@@ -38,33 +46,48 @@ case class Field(children: Seq[Expression]) extends Expression {
         case _ => findEqual(target, params.tail, index + 1)
       }
     }
-    findEqual(param1, children.tail, 1)
+    findEqual(target, children.tail, 1)
   }
+
   protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
     val evalChildren = children.map(_.genCode(ctx))
     val target = evalChildren(0)
+    val targetDataType = children(0).dataType
     val rest = evalChildren.drop(1)
+    val restDataType = children.drop(1).map(_.dataType)
 
-//    def updateEval(eval: ExprCode): String = {
-//      s"""
-//        ${eval.code}
-//        if (${target.isNull}) {
-//          if(${eval.isNull}) {
-//            ${ev.value}
-//          }
-//        }
-//        if (!${eval.isNull} && (${ev.isNull} ||
-//          ${ctx.genGreater(dataType, eval.value, ev.value)})) {
-//          ${ev.value} = ${eval.value};
-//        }
-//      """
-//    }
+    def updateEval(evalWithIndex: Tuple2[Tuple2[ExprCode, DataType], Int]): String = {
+      val ((eval, dataType), index) = evalWithIndex
+      s"""
+        ${eval.code}
+        if (${dataType.equals(targetDataType)} && ${ctx.genEqual(targetDataType, eval.value, target.value)}) {
+          ${ev.value} = ${index};
+        }
+      """
+    }
+
+    def genIfElseStructure(code1: String, code2: String): String = {
+      s"""
+         ${code1}
+         else {
+          ${code2}
+         }
+       """
+    }
+
+    def dataTypeEqualsTarget(evalWithIndex: Tuple2[Tuple2[ExprCode, DataType], Int]): Boolean = {
+      val ((eval, dataType), index) = evalWithIndex
+      if (dataType.equals(targetDataType))
+        true
+      else
+        false
+    }
 
     ev.copy(code = s"""
       ${target.code}
       boolean ${ev.isNull} = false;
-      int ${ev.value} = 2;
+      int ${ev.value} = 0;
+      ${rest.zip(restDataType).zipWithIndex.map(x => (x._1, x._2 + 1)).filter(dataTypeEqualsTarget).map(updateEval).reduceRight(genIfElseStructure)}
       """)
   }
-
 }
