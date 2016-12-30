e73b60f93badf2c3ca023cdec3bb520a72e6a2fd
Ilya Matiach <ilmat@microsoft.com>
2016-12-30 15:20:43 -0500

    Fixed scala style empty line

--- mllib/src/main/scala/org/apache/spark/ml/classification/GBTClassifier.scala
+++ mllib/src/main/scala/org/apache/spark/ml/classification/GBTClassifier.scala
@@ -20,6 +20,7 @@ package org.apache.spark.ml.classification
 import com.github.fommil.netlib.BLAS.{getInstance => blas}
 import org.json4s.{DefaultFormats, JObject}
 import org.json4s.JsonDSL._
+
 import org.apache.spark.annotation.Since
 import org.apache.spark.internal.Logging
 import org.apache.spark.ml.feature.LabeledPoint
