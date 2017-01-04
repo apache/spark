b28d9ca5e553e453b34d6199549d845ff5b6e1e2
Holden Karau <holden@us.ibm.com>
2017-01-03 17:06:24 -0800

    Install numpy so we can check the mllib linalg imports

--- dev/pip-sanity-check.py
+++ dev/pip-sanity-check.py
@@ -19,6 +19,7 @@ from __future__ import print_function
 
 from pyspark.sql import SparkSession
 from pyspark.ml.param import Params
+from pyspark.mllib.linalg import *
 import sys
 
 if __name__ == "__main__":
