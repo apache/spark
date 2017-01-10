c94288b57b5ce2232e58e35cada558d8d5b8ec6e
Dongjoon Hyun <dongjoon@apache.org>
2017-01-10 13:27:55 +0000

    [SPARK-18857][SQL] Don't use `Iterator.duplicate` for `incrementalCollect` in Thrift Server
    ## What changes were proposed in this pull request?
    
    To support `FETCH_FIRST`, SPARK-16563 used Scala `Iterator.duplicate`. However,
    Scala `Iterator.duplicate` uses a **queue to buffer all items between both iterators**,
    this causes GC and hangs for queries with large number of rows. We should not use this,
    especially for `spark.sql.thriftServer.incrementalCollect`.
    
    https://github.com/scala/scala/blob/2.12.x/src/library/scala/collection/Iterator.scala#L1262-L1300
    
    ## How was this patch tested?
    
    Pass the existing tests.
    
    Author: Dongjoon Hyun <dongjoon@apache.org>
    
    Closes #16440 from dongjoon-hyun/SPARK-18857.
    
    (cherry picked from commit a2c6adcc5d2702d2f0e9b239517353335e5f911e)
    Signed-off-by: Sean Owen <sowen@cloudera.com>

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -295,6 +295,13 @@ object SQLConf {
     .stringConf
     .createOptional
 
+  val THRIFTSERVER_INCREMENTAL_COLLECT =
+    SQLConfigBuilder("spark.sql.thriftServer.incrementalCollect")
+      .internal()
+      .doc("When true, enable incremental collection for execution in Thrift Server.")
+      .booleanConf
+      .createWithDefault(false)
+
   val THRIFTSERVER_UI_STATEMENT_LIMIT =
     SQLConfigBuilder("spark.sql.thriftserver.ui.retainedStatements")
       .doc("The number of SQL statements kept in the JDBC/ODBC web UI history.")
