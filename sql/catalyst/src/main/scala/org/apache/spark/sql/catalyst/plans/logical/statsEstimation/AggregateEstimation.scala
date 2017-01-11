a6155135690433988aa0cbf22f260f52a235e9f5
wangzhenhua <wangzhenhua@huawei.com>
2017-01-10 22:34:44 -0800

    [SPARK-19149][SQL] Unify two sets of statistics in LogicalPlan
    ## What changes were proposed in this pull request?
    
    Currently we have two sets of statistics in LogicalPlan: a simple stats and a stats estimated by cbo, but the computing logic and naming are quite confusing, we need to unify these two sets of stats.
    
    ## How was this patch tested?
    
    Just modify existing tests.
    
    Author: wangzhenhua <wangzhenhua@huawei.com>
    Author: Zhenhua Wang <wzh_zju@163.com>
    
    Closes #16529 from wzhfy/unifyStats.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/statsEstimation/AggregateEstimation.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/statsEstimation/AggregateEstimation.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.sql.catalyst.plans.logical.statsEstimation
 
+import org.apache.spark.sql.catalyst.CatalystConf
 import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, Statistics}
 
@@ -28,13 +29,13 @@ object AggregateEstimation {
    * Estimate the number of output rows based on column stats of group-by columns, and propagate
    * column stats for aggregate expressions.
    */
-  def estimate(agg: Aggregate): Option[Statistics] = {
-    val childStats = agg.child.statistics
+  def estimate(conf: CatalystConf, agg: Aggregate): Option[Statistics] = {
+    val childStats = agg.child.stats(conf)
     // Check if we have column stats for all group-by columns.
     val colStatsExist = agg.groupingExpressions.forall { e =>
       e.isInstanceOf[Attribute] && childStats.attributeStats.contains(e.asInstanceOf[Attribute])
     }
-    if (rowCountsExist(agg.child) && colStatsExist) {
+    if (rowCountsExist(conf, agg.child) && colStatsExist) {
       // Multiply distinct counts of group-by columns. This is an upper bound, which assumes
       // the data contains all combinations of distinct values of group-by columns.
       var outputRows: BigInt = agg.groupingExpressions.foldLeft(BigInt(1))(
