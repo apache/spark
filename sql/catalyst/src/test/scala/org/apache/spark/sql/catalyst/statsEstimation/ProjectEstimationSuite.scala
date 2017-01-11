57c50ca46ca89916aefce1066e4845b9b8bd5c72
wangzhenhua <wangzhenhua@huawei.com>
2017-01-11 15:00:58 -0800

    [SPARK-19132][SQL] Add test cases for row size estimation and aggregate estimation
    ## What changes were proposed in this pull request?
    
    In this pr, we add more test cases for project and aggregate estimation.
    
    ## How was this patch tested?
    
    Add test cases.
    
    Author: wangzhenhua <wangzhenhua@huawei.com>
    
    Closes #16551 from wzhfy/addTests.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/statsEstimation/ProjectEstimationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/statsEstimation/ProjectEstimationSuite.scala
@@ -17,35 +17,122 @@
 
 package org.apache.spark.sql.catalyst.statsEstimation
 
+import java.sql.{Date, Timestamp}
+
 import org.apache.spark.sql.catalyst.expressions.{Alias, AttributeMap, AttributeReference}
 import org.apache.spark.sql.catalyst.plans.logical._
-import org.apache.spark.sql.catalyst.plans.logical.statsEstimation.EstimationUtils._
-import org.apache.spark.sql.types.IntegerType
+import org.apache.spark.sql.types._
 
 
 class ProjectEstimationSuite extends StatsEstimationTestBase {
 
-  test("estimate project with alias") {
-    val ar1 = AttributeReference("key1", IntegerType)()
-    val ar2 = AttributeReference("key2", IntegerType)()
-    val colStat1 = ColumnStat(2, Some(1), Some(2), 0, 4, 4)
-    val colStat2 = ColumnStat(1, Some(10), Some(10), 0, 4, 4)
+  test("project with alias") {
+    val (ar1, colStat1) = (attr("key1"), ColumnStat(distinctCount = 2, min = Some(1),
+      max = Some(2), nullCount = 0, avgLen = 4, maxLen = 4))
+    val (ar2, colStat2) = (attr("key2"), ColumnStat(distinctCount = 1, min = Some(10),
+      max = Some(10), nullCount = 0, avgLen = 4, maxLen = 4))
 
     val child = StatsTestPlan(
       outputList = Seq(ar1, ar2),
-      stats = Statistics(
-        sizeInBytes = 2 * (4 + 4),
-        rowCount = Some(2),
-        attributeStats = AttributeMap(Seq(ar1 -> colStat1, ar2 -> colStat2))))
+      rowCount = 2,
+      attributeStats = AttributeMap(Seq(ar1 -> colStat1, ar2 -> colStat2)))
 
-    val project = Project(Seq(ar1, Alias(ar2, "abc")()), child)
+    val proj = Project(Seq(ar1, Alias(ar2, "abc")()), child)
     val expectedColStats = Seq("key1" -> colStat1, "abc" -> colStat2)
-    val expectedAttrStats = toAttributeMap(expectedColStats, project)
-    // The number of rows won't change for project.
+    val expectedAttrStats = toAttributeMap(expectedColStats, proj)
     val expectedStats = Statistics(
-      sizeInBytes = 2 * getRowSize(project.output, expectedAttrStats),
+      sizeInBytes = 2 * (8 + 4 + 4),
       rowCount = Some(2),
       attributeStats = expectedAttrStats)
-    assert(project.stats(conf) == expectedStats)
+    assert(proj.stats(conf) == expectedStats)
+  }
+
+  test("project on empty table") {
+    val (ar1, colStat1) = (attr("key1"), ColumnStat(distinctCount = 0, min = None, max = None,
+      nullCount = 0, avgLen = 4, maxLen = 4))
+    val child = StatsTestPlan(
+      outputList = Seq(ar1),
+      rowCount = 0,
+      attributeStats = AttributeMap(Seq(ar1 -> colStat1)))
+    checkProjectStats(
+      child = child,
+      projectAttrMap = child.attributeStats,
+      expectedSize = 1,
+      expectedRowCount = 0)
+  }
+
+  test("test row size estimation") {
+    val dec1 = new java.math.BigDecimal("1.000000000000000000")
+    val dec2 = new java.math.BigDecimal("8.000000000000000000")
+    val d1 = Date.valueOf("2016-05-08")
+    val d2 = Date.valueOf("2016-05-09")
+    val t1 = Timestamp.valueOf("2016-05-08 00:00:01")
+    val t2 = Timestamp.valueOf("2016-05-09 00:00:02")
+
+    val columnInfo: AttributeMap[ColumnStat] = AttributeMap(Seq(
+      AttributeReference("cbool", BooleanType)() -> ColumnStat(distinctCount = 2,
+        min = Some(false), max = Some(true), nullCount = 0, avgLen = 1, maxLen = 1),
+      AttributeReference("cbyte", ByteType)() -> ColumnStat(distinctCount = 2,
+        min = Some(1L), max = Some(2L), nullCount = 0, avgLen = 1, maxLen = 1),
+      AttributeReference("cshort", ShortType)() -> ColumnStat(distinctCount = 2,
+        min = Some(1L), max = Some(3L), nullCount = 0, avgLen = 2, maxLen = 2),
+      AttributeReference("cint", IntegerType)() -> ColumnStat(distinctCount = 2,
+        min = Some(1L), max = Some(4L), nullCount = 0, avgLen = 4, maxLen = 4),
+      AttributeReference("clong", LongType)() -> ColumnStat(distinctCount = 2,
+        min = Some(1L), max = Some(5L), nullCount = 0, avgLen = 8, maxLen = 8),
+      AttributeReference("cdouble", DoubleType)() -> ColumnStat(distinctCount = 2,
+        min = Some(1.0), max = Some(6.0), nullCount = 0, avgLen = 8, maxLen = 8),
+      AttributeReference("cfloat", FloatType)() -> ColumnStat(distinctCount = 2,
+        min = Some(1.0), max = Some(7.0), nullCount = 0, avgLen = 4, maxLen = 4),
+      AttributeReference("cdecimal", DecimalType.SYSTEM_DEFAULT)() -> ColumnStat(distinctCount = 2,
+        min = Some(dec1), max = Some(dec2), nullCount = 0, avgLen = 16, maxLen = 16),
+      AttributeReference("cstring", StringType)() -> ColumnStat(distinctCount = 2,
+        min = None, max = None, nullCount = 0, avgLen = 3, maxLen = 3),
+      AttributeReference("cbinary", BinaryType)() -> ColumnStat(distinctCount = 2,
+        min = None, max = None, nullCount = 0, avgLen = 3, maxLen = 3),
+      AttributeReference("cdate", DateType)() -> ColumnStat(distinctCount = 2,
+        min = Some(d1), max = Some(d2), nullCount = 0, avgLen = 4, maxLen = 4),
+      AttributeReference("ctimestamp", TimestampType)() -> ColumnStat(distinctCount = 2,
+        min = Some(t1), max = Some(t2), nullCount = 0, avgLen = 8, maxLen = 8)
+    ))
+    val columnSizes = columnInfo.map { case (attr, colStat) =>
+      (attr, attr.dataType match {
+        case StringType => colStat.avgLen + 8 + 4
+        case _ => colStat.avgLen
+      })
+    }
+    val child = StatsTestPlan(
+      outputList = columnInfo.keys.toSeq,
+      rowCount = 2,
+      attributeStats = columnInfo)
+
+    // Row with single column
+    columnInfo.keys.foreach { attr =>
+      checkProjectStats(
+        child = child,
+        projectAttrMap = AttributeMap(attr -> columnInfo(attr) :: Nil),
+        expectedSize = 2 * (8 + columnSizes(attr)),
+        expectedRowCount = 2)
+    }
+
+    // Row with multiple columns
+    checkProjectStats(
+      child = child,
+      projectAttrMap = columnInfo,
+      expectedSize = 2 * (8 + columnSizes.values.sum),
+      expectedRowCount = 2)
+  }
+
+  private def checkProjectStats(
+      child: LogicalPlan,
+      projectAttrMap: AttributeMap[ColumnStat],
+      expectedSize: BigInt,
+      expectedRowCount: BigInt): Unit = {
+    val proj = Project(projectAttrMap.keys.toSeq, child)
+    val expectedStats = Statistics(
+      sizeInBytes = expectedSize,
+      rowCount = Some(expectedRowCount),
+      attributeStats = projectAttrMap)
+    assert(proj.stats(conf) == expectedStats)
   }
 }
