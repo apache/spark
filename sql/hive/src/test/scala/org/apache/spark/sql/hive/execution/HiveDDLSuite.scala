c1cdcad23dfacc11cb97489e468c74266cc7d57e
gatorsmile <gatorsmile@gmail.com>
2017-01-13 16:24:49 -0800

    fix.

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
@@ -247,6 +247,16 @@ class HiveDDLSuite
     }
   }
 
+  test("SPARK-19129: drop partition with a empty string will drop the whole table") {
+    val sentenceData = spark.createDataFrame(Seq((0, "a"), (1, "b"))).toDF("partCol1", "name")
+    sentenceData.write.mode("overwrite").partitionBy("partCol1").saveAsTable("partition_table")
+    val e = intercept[AnalysisException] {
+      spark.sql("alter table partition_table drop partition(partCol1='')")
+    }.getMessage
+    assert(e.contains("Partition spec is invalid. The spec ([partCol1=]) contains an empty " +
+      "partition column value"))
+  }
+
   test("add/drop partitions - external table") {
     val catalog = spark.sessionState.catalog
     withTempDir { tmpDir =>
