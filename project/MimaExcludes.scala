2bc811c532aabec157540cd95f6f096b4038d1cd
Wenchen Fan <wenchen@databricks.com>
2017-01-17 12:54:50 +0800

    [SPARK-19148][SQL] do not expose the external table concept in Catalog
    ## What changes were proposed in this pull request?
    
    In https://github.com/apache/spark/pull/16296 , we reached a consensus that we should hide the external/managed table concept to users and only expose custom table path.
    
    This PR renames `Catalog.createExternalTable` to `createTable`(still keep the old versions for backward compatibility), and only set the table type to EXTERNAL if `path` is specified in options.
    
    ## How was this patch tested?
    
    new tests in `CatalogSuite`
    
    Author: Wenchen Fan <wenchen@databricks.com>
    
    Closes #16528 from cloud-fan/create-table.

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -43,7 +43,10 @@ object MimaExcludes {
     ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.sql.catalog.Catalog.recoverPartitions"),
 
     // [SPARK-18537] Add a REST api to spark streaming
-    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.streaming.scheduler.StreamingListener.onStreamingStarted")
+    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.streaming.scheduler.StreamingListener.onStreamingStarted"),
+
+    // [SPARK-19148][SQL] do not expose the external table concept in Catalog
+    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.sql.catalog.Catalog.createTable")
   )
 
   // Exclude rules for 2.1.x
