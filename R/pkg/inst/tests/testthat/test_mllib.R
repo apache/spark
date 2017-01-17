3b17c3d37ba70b3929a5de70c607dbffe7e074e3
wm624@hotmail.com <wm624@hotmail.com>
2017-01-17 15:58:20 -0800

    fix a unit test

--- R/pkg/inst/tests/testthat/test_mllib.R
+++ R/pkg/inst/tests/testthat/test_mllib.R
@@ -874,7 +874,7 @@ test_that("spark.lda with libsvm", {
   model2 <- read.ml(modelPath)
   stats2 <- summary(model2)
 
-  expect_false(stats2$isDistributed)
+  expect_true(stats2$isDistributed)
   expect_equal(logLikelihood, stats2$logLikelihood)
   expect_equal(logPerplexity, stats2$logPerplexity)
   expect_equal(vocabSize, stats2$vocabSize)
