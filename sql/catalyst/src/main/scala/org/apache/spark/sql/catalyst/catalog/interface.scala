72164eb02c1b7acd836a5038fddb8bcd8225a1c6
windpiger <songjun@outlook.com>
2017-01-24 13:40:59 +0800

    optimize the code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
@@ -187,11 +187,10 @@ case class CatalogTable(
    * schema of this table's partition columns
    */
   def partitionSchema: StructType = {
-    assert(schema.takeRight(partitionColumnNames.length).map(_.name) == partitionColumnNames)
+    val partitionFields = schema.takeRight(partitionColumnNames.length)
+    assert(partitionFields.map(_.name) == partitionColumnNames)
 
-    StructType(schema.filter {
-      c => partitionColumnNames.contains(c.name)
-    })
+    StructType(partitionFields)
   }
 
   /** Return the database this table was specified to belong to, assuming it exists. */
