0c24291b2738d2c71b59decc60b9e33524b8f84d
uncleGen <hustyugm@gmail.com>
2017-01-24 18:37:15 +0800

    SPARK-19349: Check resource ready to avoid multiple receivers to be scheduled on the same node.

--- core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
@@ -422,6 +422,8 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val rpcEnv: Rp
 
   def sufficientResourcesRegistered(): Boolean = true
 
+  def allResourcesRegistered(): Boolean = true
+
   override def isReady(): Boolean = {
     if (sufficientResourcesRegistered) {
       logInfo("SchedulerBackend is ready for scheduling beginning after " +
