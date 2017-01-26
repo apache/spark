5184c6e2f108d3db15205ad806e6529b5e0b4259
Bolke de Bruin <bolke@xs4all.nl>
2017-01-26 14:34:08 +0100

    [AIRFLOW-783] Fix py3 incompatibility in BaseTaskRunner

--- airflow/task_runner/base_task_runner.py
+++ airflow/task_runner/base_task_runner.py
@@ -89,7 +89,7 @@ class BaseTaskRunner(LoggingMixin):
 
     def _read_task_logs(self, stream):
         while True:
-            line = stream.readline()
+            line = stream.readline().decode('utf-8')
             if len(line) == 0:
                 break
             self.logger.info('Subtask: {}'.format(line.rstrip('\n')))
