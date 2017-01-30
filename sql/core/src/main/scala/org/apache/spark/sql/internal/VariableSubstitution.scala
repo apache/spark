90bf25b9c89433da3d80b76d9f4b20d9adb186d8
hyukjinkwon <gurwls223@gmail.com>
2017-01-31 00:31:40 +0900

    Leave it if I can't fix rather than removing

--- sql/core/src/main/scala/org/apache/spark/sql/internal/VariableSubstitution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/VariableSubstitution.scala
@@ -20,7 +20,8 @@ package org.apache.spark.sql.internal
 import org.apache.spark.internal.config._
 
 /**
- * A helper class that enables substitution.
+ * A helper class that enables substitution using syntax like
+ * `${var}`, `${system:var}` and `${env:var}`.
  *
  * Variable substitution is controlled by `SQLConf.variableSubstituteEnabled`.
  */
