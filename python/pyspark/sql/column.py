e4ae5be61db63ecc2b4b67ca61d5fb00c17134d5
zero323 <zero323@users.noreply.github.com>
2017-02-01 20:39:16 +0100

    Support slice arguments in Column.__getitem__

--- python/pyspark/sql/column.py
+++ python/pyspark/sql/column.py
@@ -180,7 +180,6 @@ class Column(object):
 
     # container operators
     __contains__ = _bin_op("contains")
-    __getitem__ = _bin_op("apply")
 
     # bitwise operators
     bitwiseOR = _bin_op("bitwiseOR")
@@ -235,6 +234,14 @@ class Column(object):
         if item.startswith("__"):
             raise AttributeError(item)
         return self.getField(item)
+
+    def __getitem__(self, k):
+        if isinstance(k, slice):
+            if k.step is not None:
+                raise ValueError("slice with step is not supported.")
+            return self.substr(k.start, k.stop)
+        else:
+            return _bin_op("apply")(self, k)
 
     def __iter__(self):
         raise TypeError("Column is not iterable")
