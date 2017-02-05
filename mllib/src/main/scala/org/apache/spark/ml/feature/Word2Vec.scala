05eaf59fa34d6f8d721e48b3300271d347aae699
Asher Krim <akrim@hubspot.com>
2017-02-05 15:05:32 -0500

    provide methods to return synonyms directly, without wrapping them in a dataframe
    In performance sensitive applications (such as user facing apis) the roundtrip to and from dataframes is costly and unnecessary

--- mllib/src/main/scala/org/apache/spark/ml/feature/Word2Vec.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/Word2Vec.scala
@@ -232,11 +232,11 @@ class Word2VecModel private[ml] (
   @Since("1.5.0")
   def findSynonyms(word: String, num: Int): DataFrame = {
     val spark = SparkSession.builder().getOrCreate()
-    spark.createDataFrame(wordVectors.findSynonyms(word, num)).toDF("word", "similarity")
+    spark.createDataFrame(findSynonymsLocal(word, num)).toDF("word", "similarity")
   }
 
   /**
-   * Find "num" number of words whose vector representation most similar to the supplied vector.
+   * Find "num" number of words whose vector representation is most similar to the supplied vector.
    * If the supplied vector is the vector representation of a word in the model's vocabulary,
    * that word will be in the results.  Returns a dataframe with the words and the cosine
    * similarities between the synonyms and the given word vector.
@@ -244,7 +244,28 @@ class Word2VecModel private[ml] (
   @Since("2.0.0")
   def findSynonyms(vec: Vector, num: Int): DataFrame = {
     val spark = SparkSession.builder().getOrCreate()
-    spark.createDataFrame(wordVectors.findSynonyms(vec, num)).toDF("word", "similarity")
+    spark.createDataFrame(findSynonymsLocal(vec, num)).toDF("word", "similarity")
+  }
+
+  /**
+   * Find "num" number of words whose vector representation is most similar to the supplied vector.
+   * If the supplied vector is the vector representation of a word in the model's vocabulary,
+   * that word will be in the results. Returns an array of the words and the cosine
+   * similarities between the synonyms and the given word vector.
+   */
+  @Since("2.2.0")
+  def findSynonymsLocal(vec: Vector, num: Int): Array[(String, Double)] = {
+    wordVectors.findSynonyms(vec, num)
+  }
+
+  /**
+   * Find "num" number of words closest in similarity to the given word, not
+   * including the word itself. Returns a dataframe with the words and the
+   * cosine similarities between the synonyms and the given word.
+   */
+  @Since("2.2.0")
+  def findSynonymsLocal(word: String, num: Int): Array[(String, Double)] = {
+    wordVectors.findSynonyms(word, num)
   }
 
   /** @group setParam */
