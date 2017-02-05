8aaef3f352acb8ec5071cb2dd8fe2a66b1c1f5ee
windpiger <songjun@outlook.com>
2017-02-05 19:06:37 +0800

    [SPARK-19463][SQL]refresh cache after the InsertIntoHadoopFsRelationCommand

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/InsertIntoHadoopFsRelationCommand.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/InsertIntoHadoopFsRelationCommand.scala
@@ -132,6 +132,9 @@ case class InsertIntoHadoopFsRelationCommand(
             }
           }
         }
+
+        sparkSession.catalog.refreshByPath(outputPath.toString)
+
       }
 
       FileFormatWriter.write(
