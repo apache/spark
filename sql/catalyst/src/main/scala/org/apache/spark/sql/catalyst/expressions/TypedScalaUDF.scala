e73c84ff75e66dee9a395c9913109a965d7d68f4
Koert Kuipers <koert@tresata.com>
2017-02-09 15:04:36 -0500

    got something working but not sure how good this is yet

---
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/TypedScalaUDF.scala
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.expressions
+
+import scala.reflect.ClassTag
+
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
+import org.apache.spark.sql.types.DataType
+
+case class TypedScalaUDF[T1, R](
+    function: (T1) => R,
+    t1Encoder: ExpressionEncoder[T1],
+    rEncoder: ExpressionEncoder[R],
+    child1: Expression,
+    inputTypes: Seq[DataType] = Nil
+  ) extends Expression with ImplicitCastInputTypes with NonSQLExpression {
+
+  override def children: Seq[Expression] = Seq(child1)
+
+  // something is wrong with encoders and Option so this is used to handle that
+  private val t1IsOption = t1Encoder.clsTag == implicitly[ClassTag[Option[_]]] 
+  private val rIsOption = rEncoder.clsTag == implicitly[ClassTag[Option[_]]]
+
+  override val dataType: DataType =
+    if (rEncoder.flat || rIsOption)
+      rEncoder.schema.head.dataType
+    else
+      rEncoder.schema
+
+  override val nullable: Boolean =
+    if (rEncoder.flat)
+      rEncoder.schema.head.nullable
+    else
+      true
+
+  val boundT1Encoder: ExpressionEncoder[T1] = t1Encoder.resolveAndBind()
+
+  def internalRow(x: Any): InternalRow = InternalRow(x)
+
+  override def eval(input: InternalRow): Any = {
+    val rowIn = if (t1Encoder.flat || t1IsOption) InternalRow(child1.eval(input)) else child1.eval(input).asInstanceOf[InternalRow]
+    val t1 = boundT1Encoder.fromRow(rowIn)
+    val rowOut = rEncoder.toRow(function(t1))
+    if (rEncoder.flat || rIsOption) rowOut.get(0, dataType) else rowOut
+  }
+
+  override def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
+    val typedScalaUDF = ctx.addReferenceObj("typedScalaUDF", this, classOf[TypedScalaUDF[_, _]].getName)
+
+    // codegen for children expressions
+    val eval1 = child1.genCode(ctx)
+    val evalCode1 = eval1.code.mkString
+
+    // codegen for this expression
+    val rowInTerm = ctx.freshName("rowIn")
+    val rowIn = if (t1Encoder.flat || t1IsOption)
+      s"${classOf[InternalRow].getName} $rowInTerm = ${eval1.isNull} ? ${typedScalaUDF}.internalRow(null) : ${typedScalaUDF}.internalRow(${eval1.value});"
+    else
+      s"${classOf[InternalRow].getName} $rowInTerm = (${classOf[InternalRow].getName}) ${eval1.value};"
+    val t1Term = ctx.freshName("t1")
+    val t1 = s"Object $t1Term = ${typedScalaUDF}.boundT1Encoder().fromRow($rowInTerm);"
+    val rTerm = ctx.freshName("r")
+    val r = s"Object $rTerm = ${typedScalaUDF}.function().apply($t1Term);"
+    val rowOutTerm = ctx.freshName("rowOut")
+    val rowOut = s"${classOf[InternalRow].getName} $rowOutTerm = ${typedScalaUDF}.rEncoder().toRow($rTerm);"
+    val resultTerm = ctx.freshName("result")
+    val result = if (rEncoder.flat || rIsOption)
+      s"Object $resultTerm = ${rowOutTerm}.get(0, ${typedScalaUDF}.dataType());"
+    else
+      s"Object $resultTerm = ${rowOutTerm};"
+
+    // put it all in place
+    ev.copy(code = s"""
+      $evalCode1
+      $rowIn
+      //System.out.println("rowIn is " + $rowInTerm);
+      $t1
+      //System.out.println("t1 is " + $t1Term);
+      $r
+      //System.out.println("r is " + $rTerm);
+      $rowOut
+      //System.out.println("rowOut is " + $rowOutTerm);
+      $result
+      //System.out.println("result is " + $resultTerm);
+      
+      boolean ${ev.isNull} = $resultTerm == null;
+      ${ctx.javaType(dataType)} ${ev.value} = ${ctx.defaultValue(dataType)};
+      if (!${ev.isNull}) {
+        ${ev.value} = (${ctx.boxedType(dataType)}) $resultTerm;
+      }
+    """)
+  }
+
+  override def toString: String = s"TypedUDF(${children.mkString(", ")})"
+}
---
+++ sql/core/src/test/scala/org/apache/spark/sql/TypedUDFSuite.scala
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql
+
+import org.apache.spark.sql.test.SharedSQLContext
+import org.apache.spark.sql.test.SQLTestData._
+import org.apache.spark.sql.functions.{ col, udf, typedUdf }
+
+case class Blah(a: Int, b: String)
+
+class TypedUDFSuite extends QueryTest with SharedSQLContext {
+  import testImplicits._
+
+  test("typedUdf") {
+    import java.lang.{ Integer => JInt }
+
+    val df1 = Seq((1, "a", 1: JInt), (2, "b", 2: JInt), (3, "c", null)).toDF("x", "y", "z").select(
+      typedUdf{ () => false }.apply() as "f0",
+      typedUdf{ x: Int => x * 2 }.apply('x) as "f1",
+      typedUdf{ y: String => y + "!" }.apply('y) as "f2",
+      typedUdf{ blah: Blah => blah.copy(a = blah.a + 1) }.apply('x, 'y) as "f3",
+      typedUdf{ (x: Int, y: String) => (x + 1, y) }.apply('x, 'y) as "f4",
+      typedUdf{ z: Int => z + 1 }.apply('z) as "f5",
+      typedUdf{ z: Option[Int] => z.map(_ + 1) }.apply('z) as "f6",
+      typedUdf[JInt, JInt]{ z => if (z != null) z + 1 else null }.apply('z) as "f7",
+      typedUdf{ x: Int => x + 1 }.apply(col("x") * 2) as "f8"
+    )
+    df1.printSchema
+    df1.explain
+    df1.show
+
+    val df2 = testData
+      .withColumn("z", udf{ (x: JInt) => if (x > 10) x else null }.apply('value) as 'z)
+      .select(
+        typedUdf{ () => false }.apply() as "f0",
+        typedUdf{ x: Int => x * 2 }.apply('key) as "f1",
+        typedUdf{ y: String => y + "!" }.apply('value) as "f2",
+        typedUdf{ blah: Blah => blah.copy(a = blah.a + 1) }.apply('key, 'value) as "f3",
+        typedUdf{ (x: Int, y: String) => (x + 1, y) }.apply('key, 'value) as "f4",
+        typedUdf{ z: Int => z + 1 }.apply('z) as "f5",
+        typedUdf{ z: Option[Int] => z.map(_ + 1) }.apply('z) as "f6",
+        typedUdf[JInt, JInt]{ z => if (z != null) z + 1 else null }.apply('z) as "f7",
+        typedUdf{ x: Int => x + 1 }.apply(col("key") * 2) as "f8"
+      )
+    df2.printSchema
+    df2.explain
+    df2.show
+  }
+
+}
