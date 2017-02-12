358769c6a5bedbbe4e94a940d55896e6a1b74096
cenyuhai <261810726@qq.com>
2017-02-12 12:59:37 +0800

    fix error in create table syntax

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveQuerySuite.scala
@@ -1239,7 +1239,7 @@ class HiveQuerySuite extends HiveComparisonTest with SQLTestUtils with BeforeAnd
 
   createQueryTest("SPARK-19490 case sensitivity when filtering hive partition columns",
     """
-      |  CREATE TABLE partition_test (key Int) partitioned by date;
+      |  CREATE TABLE partition_test (key Int) partitioned by (date string);
       |  SELECT * FROM partition_test where DATE = '20170101';
     """.stripMargin)
 }
