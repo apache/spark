fc3deeab536e4d977033f03e773e6f1383d8f979
erenavsarogullari <erenavsarogullari@gmail.com>
2017-02-12 22:35:55 +0000

    Pool variables are addressed via separated PR

--- core/src/main/scala/org/apache/spark/scheduler/Pool.scala
+++ core/src/main/scala/org/apache/spark/scheduler/Pool.scala
@@ -37,8 +37,8 @@ private[spark] class Pool(
 
   val schedulableQueue = new ConcurrentLinkedQueue[Schedulable]
   val schedulableNameToSchedulable = new ConcurrentHashMap[String, Schedulable]
-  val weight = initWeight
-  val minShare = initMinShare
+  var weight = initWeight
+  var minShare = initMinShare
   var runningTasks = 0
   var priority = 0
 
