91b9fd2cec7ef8ef9ab4bcf2c5468ef19139f647
windpiger <songjun@outlook.com>
2017-02-21 11:54:13 +0800

    remove a log info

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SharedState.scala
@@ -62,8 +62,7 @@ private[sql] class SharedState(val sparkContext: SparkContext) extends Logging {
       // When neither spark.sql.warehouse.dir nor hive.metastore.warehouse.dir is set,
       // we will set hive.metastore.warehouse.dir to the default value of spark.sql.warehouse.dir.
       val sparkWarehouseDir = sparkContext.conf.get(WAREHOUSE_PATH)
-      logInfo(s"${WAREHOUSE_PATH.key} is set, Setting " +
-        s"hive.metastore.warehouse.dir ($hiveWarehouseDir) to the value of " +
+      logInfo(s"Setting hive.metastore.warehouse.dir ($hiveWarehouseDir) to the value of " +
         s"${WAREHOUSE_PATH.key} ('$sparkWarehouseDir').")
       if (hiveWarehouseDir != null) {
         sparkContext.hadoopConfiguration.set("hive.metastore.warehouse.dir", sparkWarehouseDir)
