ce1a82acc497af32818fc1f11ac332ada2a73515
GuoChenzhao <chenzhao.guo@intel.com>
2017-02-22 22:34:50 +0800

    Add trim function, change to java/HashMap and refactor

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/NGrams.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/NGrams.scala
@@ -1,17 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.catalyst.expressions.aggregate
 
 import java.nio.ByteBuffer
+import java.util.HashMap
 
-import com.google.common.primitives.{Chars, Doubles, Ints}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
-import org.apache.spark.sql.catalyst.analysis.TypeCheckResult.{TypeCheckFailure, TypeCheckSuccess}
+import org.apache.spark.sql.catalyst.analysis.TypeCheckResult._
 import org.apache.spark.sql.catalyst.expressions.aggregate.NGrams.NGramBuffer
 import org.apache.spark.sql.catalyst.expressions.{Expression, ExpressionDescription, ImplicitCastInputTypes, Literal}
-import org.apache.spark.sql.catalyst.util.{GenericArrayData, QuantileSummaries}
+import org.apache.spark.sql.catalyst.util.GenericArrayData
 import org.apache.spark.sql.types._
-import org.apache.spark.util.collection.OpenHashMap
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.unsafe.types.UTF8String
 
@@ -43,11 +59,11 @@ case class NGrams(child: Expression,
 
   def this(child: Expression, nExpression: Expression, kExpression: Expression,
            accuracyExpression: Expression) = {
-    this(child, nExpression, kExpression, accuracyExpression, 0, 0)
+    this(child, nExpression, kExpression, NGrams.getAccuracy(kExpression, accuracyExpression), 0, 0)
   }
 
   def this(child: Expression, nExpression: Expression, kExpression: Expression) = {
-    this(child, nExpression, kExpression, Literal(NGrams.DEFAULT_NGRAM_ACCURACY))
+    this(child, nExpression, kExpression, Literal(0))
   }
 
   private lazy val n: Int = nExpression.eval().asInstanceOf[Int]
@@ -74,7 +90,7 @@ case class NGrams(child: Expression,
   }
 
   override def createAggregationBuffer(): NGramBuffer = {
-    new NGramBuffer(n, k, accuracy, new OpenHashMap[List[UTF8String], Double]())
+    new NGramBuffer(n, k, accuracy, new HashMap[List[UTF8String], Double]())
   }
 
   override def update(buffer: NGramBuffer, inputRow: InternalRow): NGramBuffer = {
@@ -84,20 +100,22 @@ case class NGrams(child: Expression,
       val nGrams = getNGrams(values, n)
       nGrams.foreach(buffer.add(_))
     }
+    buffer.trim()
     buffer
-  }
-
-  private def getNGrams(values: List[UTF8String], n: Int): List[List[UTF8String]] = {
-    values.sliding(n).toList
   }
 
   override def merge(buffer: NGramBuffer, input: NGramBuffer): NGramBuffer = {
     buffer.merge(input)
+    buffer.trim()
     buffer
   }
 
   override def eval(buffer: NGramBuffer): Any = {
     buffer.getTopKNGrams()
+  }
+
+  private def getNGrams(values: List[UTF8String], n: Int): List[List[UTF8String]] = {
+    values.sliding(n).toList
   }
 
   override def withNewMutableAggBufferOffset(newOffset: Int): NGrams =
@@ -126,40 +144,55 @@ case class NGrams(child: Expression,
 
 object NGrams {
 
-  val DEFAULT_NGRAM_ACCURACY: Int = 10000
+  val DEFAULT_ACCURACY: Int = 1000
+
+  private def getAccuracy(kExpression: Expression, accuracyExpression: Expression): Expression = {
+    val accuracy = accuracyExpression.eval().asInstanceOf[Int]
+    val k = kExpression.eval().asInstanceOf[Int]
+    Literal(Math.max(accuracy, DEFAULT_ACCURACY / k))
+  }
 
   val kryoSerializer: KryoSerializer = new KryoSerializer(new SparkConf())
 
+  import collection.JavaConverters._
+
   class NGramBuffer(val n: Int,
                     val k: Int,
-                    val accuracy: Int,
-                    val ngramFrequencyMap: OpenHashMap[List[UTF8String], Double]) {
-
+                    val precisionFactor: Int,
+                    val frequencyMap: HashMap[List[UTF8String], Double]) {
     def add(ng: List[UTF8String]): Unit = {
-      var currentFrequency: Double = ngramFrequencyMap(ng)
+      var currentFrequency: Double = frequencyMap.get(ng)
       if (currentFrequency == null.asInstanceOf[Double]) {
         currentFrequency = 1.0D
       } else {
         currentFrequency += 1
       }
-      ngramFrequencyMap.update(ng, currentFrequency)
+      frequencyMap.put(ng, currentFrequency)
     }
 
     def merge(other: NGramBuffer): Unit = {
-      other.ngramFrequencyMap.iterator.foreach(mergeSingleKVPair)
+      other.frequencyMap.asScala.foreach((keyValuePair: (List[UTF8String], Double)) => {
+        val key = keyValuePair._1
+        val value = keyValuePair._2
+        val originalValue = frequencyMap.getOrDefault(key, 0.0D)
+        frequencyMap.put(key, originalValue + value)
+      })
     }
 
-    def mergeSingleKVPair(keyValuePair: (List[UTF8String], Double)): Unit = {
-      val key = keyValuePair._1
-      val value = keyValuePair._2
-      ngramFrequencyMap.changeValue(key, value, _ + value)
+    def trim(): Unit = {
+      if (frequencyMap.size() > 2 * k * precisionFactor) {
+        val orderedWithIndex = frequencyMap.asScala.iterator.toList.sortWith(_._2 < _._2).zipWithIndex
+        orderedWithIndex.takeWhile(_._2 < frequencyMap.size() - k * precisionFactor).map(_._1).
+          foreach(keyValuePair => frequencyMap.remove(keyValuePair._1))
+      }
     }
 
     def getTopKNGrams() = {
-      ngramFrequencyMap.iterator.toList.sortWith(_._2 > _._2).zipWithIndex.takeWhile(_._2 < k).map(_._1).toList
+      frequencyMap.asScala.iterator.toList.sortWith(_._2 > _._2).zipWithIndex.takeWhile(_._2 < k).map(_._1).toList
     }
 
   }
+
   class NGramBufferSerializer {
 
     final def serialize(obj: NGramBuffer): Array[Byte] = {
--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/aggregate/NGramsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/aggregate/NGramsSuite.scala
@@ -1,17 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.catalyst.expressions.aggregate
 
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
-import org.apache.spark.sql.catalyst.expressions.{BoundReference, Cast, CreateArray, ExpressionEvalHelper, Literal}
+import org.apache.spark.sql.catalyst.expressions.{ExpressionEvalHelper, Literal}
 import org.apache.spark.sql.catalyst.util.GenericArrayData
 import org.apache.spark.sql.types._
 import org.apache.spark.unsafe.types.UTF8String
-
-/**
-  * Created by gcz on 17-2-20.
-  */
 
 case class MyBoundReference(size: Int, dataType: DataType, nullable: Boolean)
   extends LeafExpression {
@@ -48,7 +61,6 @@ case class MyBoundReference(size: Int, dataType: DataType, nullable: Boolean)
     }
   }
 }
-
 
 class NGramsSuite extends SparkFunSuite with ExpressionEvalHelper{
 
