dcc50315703f9958f0e75f5c9b91ff60c1e08af5
actuaryzhang <actuaryzhang10@gmail.com>
2017-02-23 11:12:02 -0800

    [SPARK-19682][SPARKR] Issue warning (or error) when subset method "[[" takes vector index
    ## What changes were proposed in this pull request?
    The `[[` method is supposed to take a single index and return a column. This is different from base R which takes a vector index.  We should check for this and issue warning or error when vector index is supplied (which is very likely given the behavior in base R).
    
    Currently I'm issuing a warning message and just take the first element of the vector index. We could change this to an error it that's better.
    
    ## How was this patch tested?
    new tests
    
    Author: actuaryzhang <actuaryzhang10@gmail.com>
    
    Closes #17017 from actuaryzhang/sparkRSubsetter.

--- R/pkg/inst/tests/testthat/test_sparkSQL.R
+++ R/pkg/inst/tests/testthat/test_sparkSQL.R
@@ -1015,6 +1015,18 @@ test_that("select operators", {
   expect_is(df[[2]], "Column")
   expect_is(df[["age"]], "Column")
 
+  expect_warning(df[[1:2]],
+                 "Subset index has length > 1. Only the first index is used.")
+  expect_is(suppressWarnings(df[[1:2]]), "Column")
+  expect_warning(df[[c("name", "age")]],
+                 "Subset index has length > 1. Only the first index is used.")
+  expect_is(suppressWarnings(df[[c("name", "age")]]), "Column")
+
+  expect_warning(df[[1:2]] <- df[[1]],
+                 "Subset index has length > 1. Only the first index is used.")
+  expect_warning(df[[c("name", "age")]] <- df[[1]],
+                 "Subset index has length > 1. Only the first index is used.")
+
   expect_is(df[, 1, drop = F], "SparkDataFrame")
   expect_equal(columns(df[, 1, drop = F]), c("name"))
   expect_equal(columns(df[, "age", drop = F]), c("age"))
