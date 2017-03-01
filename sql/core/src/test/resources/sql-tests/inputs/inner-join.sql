15fae5029e7c45d3b2a3108e24ca235a6a6fccfc
Stan Zhai <zhaishidan@haizhi.com>
2017-03-01 12:16:24 +0800

    add `Propagate in inner join` test case into FoldablePropagationSuite

--- sql/core/src/test/resources/sql-tests/inputs/inner-join.sql
+++ sql/core/src/test/resources/sql-tests/inputs/inner-join.sql
@@ -15,11 +15,3 @@ SELECT a, 'b' AS tag FROM t4;
 
 -- SPARK-19766 Constant alias columns in INNER JOIN should not be folded by FoldablePropagation rule
 SELECT tb.* FROM ta INNER JOIN tb ON ta.a = tb.a AND ta.tag = tb.tag;
-
--- Clean up
-DROP VIEW IF EXISTS t1;
-DROP VIEW IF EXISTS t2;
-DROP VIEW IF EXISTS t3;
-DROP VIEW IF EXISTS t4;
-DROP VIEW IF EXISTS ta;
-DROP VIEW IF EXISTS tb;
