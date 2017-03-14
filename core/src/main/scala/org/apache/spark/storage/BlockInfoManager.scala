5da4bcffa1b39ea8c83fe63a09e68297be371784
Sean Owen <sowen@cloudera.com>
2017-03-14 14:12:59 +0000

    Oops, fix compile error

--- core/src/main/scala/org/apache/spark/storage/BlockInfoManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockInfoManager.scala
@@ -340,7 +340,7 @@ private[storage] class BlockInfoManager extends Logging {
     val blocksWithReleasedLocks = mutable.ArrayBuffer[BlockId]()
 
     val readLocks = synchronized {
-      readLocksByTask.remove(taskAttemptId).getOrElse(ImmutableMultiset.of())
+      readLocksByTask.remove(taskAttemptId).getOrElse(ImmutableMultiset.of[BlockId]())
     }
     val writeLocks = synchronized {
       writeLocksByTask.remove(taskAttemptId).getOrElse(Seq.empty)
