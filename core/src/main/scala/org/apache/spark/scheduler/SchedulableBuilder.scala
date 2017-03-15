09a95e26a66d7aa8880518604da4e0d4f3f083ce
erenavsarogullari <erenavsarogullari@gmail.com>
2017-03-15 15:57:51 -0700

    [SPARK-18066][CORE][TESTS] Add Pool usage policies test coverage for FIFO & FAIR Schedulers
    ## What changes were proposed in this pull request?
    
    The following FIFO & FAIR Schedulers Pool usage cases need to have unit test coverage :
    - FIFO Scheduler just uses **root pool** so even if `spark.scheduler.pool` property is set, related pool is not created and `TaskSetManagers` are added to **root pool**.
    - FAIR Scheduler uses `default pool` when `spark.scheduler.pool` property is not set. This can be happened when
      - `Properties` object is **null**,
      - `Properties` object is **empty**(`new Properties()`),
      - **default pool** is set(`spark.scheduler.pool=default`).
    - FAIR Scheduler creates a **new pool** with **default values** when `spark.scheduler.pool` property points a **non-existent** pool. This can be happened when **scheduler allocation file** is not set or it does not contain related pool.
    ## How was this patch tested?
    
    New Unit tests are added.
    
    Author: erenavsarogullari <erenavsarogullari@gmail.com>
    
    Closes #15604 from erenavsarogullari/SPARK-18066.

--- core/src/main/scala/org/apache/spark/scheduler/SchedulableBuilder.scala
+++ core/src/main/scala/org/apache/spark/scheduler/SchedulableBuilder.scala
@@ -191,8 +191,11 @@ private[spark] class FairSchedulableBuilder(val rootPool: Pool, conf: SparkConf)
         parentPool = new Pool(poolName, DEFAULT_SCHEDULING_MODE,
           DEFAULT_MINIMUM_SHARE, DEFAULT_WEIGHT)
         rootPool.addSchedulable(parentPool)
-        logInfo("Created pool: %s, schedulingMode: %s, minShare: %d, weight: %d".format(
-          poolName, DEFAULT_SCHEDULING_MODE, DEFAULT_MINIMUM_SHARE, DEFAULT_WEIGHT))
+        logWarning(s"A job was submitted with scheduler pool $poolName, which has not been " +
+          "configured. This can happen when the file that pools are read from isn't set, or " +
+          s"when that file doesn't contain $poolName. Created $poolName with default " +
+          s"configuration (schedulingMode: $DEFAULT_SCHEDULING_MODE, " +
+          s"minShare: $DEFAULT_MINIMUM_SHARE, weight: $DEFAULT_WEIGHT)")
       }
     }
     parentPool.addSchedulable(manager)
