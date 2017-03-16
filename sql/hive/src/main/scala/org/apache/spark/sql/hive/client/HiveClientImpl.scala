e8ef7d115ec34643c5af767298e20559f007aa33
Dongjoon Hyun <dongjoon@apache.org>
2017-03-16 03:55:32 -0700

    Replace `getUserFromAuthenticator` because it is introduced in 0.13.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -851,7 +851,7 @@ private[hive] object HiveClientImpl {
       hiveTable.setFields(schema.asJava)
     }
     hiveTable.setPartCols(partCols.asJava)
-    conf.foreach(c => hiveTable.setOwner(SessionState.getUserFromAuthenticator()))
+    conf.foreach(c => hiveTable.setOwner(SessionState.get().getAuthenticator().getUserName()))
     hiveTable.setCreateTime((table.createTime / 1000).toInt)
     hiveTable.setLastAccessTime((table.lastAccessTime / 1000).toInt)
     table.storage.locationUri.map(CatalogUtils.URIToString(_)).foreach { loc =>
