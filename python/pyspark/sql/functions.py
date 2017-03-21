654c5121fd26a85036787882d3d2c3b56360b686
uncleGen <hustyugm@gmail.com>
2017-03-21 15:49:11 +0800

    bug fix: window operator miss the `watermark` metadata of time column

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1163,7 +1163,10 @@ def window(timeColumn, windowDuration, slideDuration=None, startTime=None):
             raise TypeError("%s should be provided as a string" % fieldName)
 
     sc = SparkContext._active_spark_context
-    time_col = _to_java_column(timeColumn)
+    if isinstance(timeColumn, Column):
+        raise TypeError("timeColumn should be the name of time column, and provided as a string.")
+    else:
+        time_col = _to_java_column(timeColumn)
     check_string_field(windowDuration, "windowDuration")
     if slideDuration and startTime:
         check_string_field(slideDuration, "slideDuration")
