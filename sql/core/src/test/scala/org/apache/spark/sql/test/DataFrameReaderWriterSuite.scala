0ae7d29ea4186892e812a402904772db0d902456
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2017-03-21 16:40:10 +0900

    fix scala style error

--- sql/core/src/test/scala/org/apache/spark/sql/test/DataFrameReaderWriterSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/test/DataFrameReaderWriterSuite.scala
@@ -26,7 +26,6 @@ import org.scalatest.BeforeAndAfter
 import org.apache.spark.internal.io.FileCommitProtocol.TaskCommitMessage
 import org.apache.spark.internal.io.HadoopMapReduceCommitProtocol
 import org.apache.commons.lang3.exception.ExceptionUtils
-
 import org.scalatest.BeforeAndAfter
 
 import org.apache.spark.sql._
