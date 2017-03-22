32b382aefea90b72b27bc39dbe7528c13d669c7a
hyukjinkwon <gurwls223@gmail.com>
2017-03-22 09:52:37 -0700

    [SPARK-19949][SQL][FOLLOW-UP] Clean up parse modes and update related comments
    ## What changes were proposed in this pull request?
    
    This PR proposes to make `mode` options in both CSV and JSON to use `cass object` and fix some related comments related previous fix.
    
    Also, this PR modifies some tests related parse modes.
    
    ## How was this patch tested?
    
    Modified unit tests in both `CSVSuite.scala` and `JsonSuite.scala`.
    
    Author: hyukjinkwon <gurwls223@gmail.com>
    
    Closes #17377 from HyukjinKwon/SPARK-19949.

--- python/pyspark/sql/readwriter.py
+++ python/pyspark/sql/readwriter.py
@@ -369,10 +369,8 @@ class DataFrameReader(OptionUtils):
         :param maxCharsPerColumn: defines the maximum number of characters allowed for any given
                                   value being read. If None is set, it uses the default value,
                                   ``-1`` meaning unlimited length.
-        :param maxMalformedLogPerPartition: sets the maximum number of malformed rows Spark will
-                                            log for each partition. Malformed records beyond this
-                                            number will be ignored. If None is set, it
-                                            uses the default value, ``10``.
+        :param maxMalformedLogPerPartition: this parameter is no longer used since Spark 2.2.0.
+                                            If specified, it is ignored.
         :param mode: allows a mode for dealing with corrupt records during parsing. If None is
                      set, it uses the default value, ``PERMISSIVE``.
 
