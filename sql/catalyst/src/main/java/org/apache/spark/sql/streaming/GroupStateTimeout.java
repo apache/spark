82b598b963a21ae9d6a2a9638e86b4165c2a78c9
Tathagata Das <tathagata.das1565@gmail.com>
2017-03-22 12:30:36 -0700

    [SPARK-20057][SS] Renamed KeyedState to GroupState in mapGroupsWithState
    ## What changes were proposed in this pull request?
    
    Since the state is tied a "group" in the "mapGroupsWithState" operations, its better to call the state "GroupState" instead of a key. This would make it more general if you extends this operation to RelationGroupedDataset and python APIs.
    
    ## How was this patch tested?
    Existing unit tests.
    
    Author: Tathagata Das <tathagata.das1565@gmail.com>
    
    Closes #17385 from tdas/SPARK-20057.

--- sql/catalyst/src/main/java/org/apache/spark/sql/streaming/GroupStateTimeout.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/streaming/GroupStateTimeout.java
@@ -24,31 +24,31 @@ import org.apache.spark.sql.catalyst.plans.logical.*;
 /**
  * Represents the type of timeouts possible for the Dataset operations
  * `mapGroupsWithState` and `flatMapGroupsWithState`. See documentation on
- * `KeyedState` for more details.
+ * `GroupState` for more details.
  *
  * @since 2.2.0
  */
 @Experimental
 @InterfaceStability.Evolving
-public class KeyedStateTimeout {
+public class GroupStateTimeout {
 
   /**
    * Timeout based on processing time. The duration of timeout can be set for each group in
-   * `map/flatMapGroupsWithState` by calling `KeyedState.setTimeoutDuration()`. See documentation
-   * on `KeyedState` for more details.
+   * `map/flatMapGroupsWithState` by calling `GroupState.setTimeoutDuration()`. See documentation
+   * on `GroupState` for more details.
    */
-  public static KeyedStateTimeout ProcessingTimeTimeout() { return ProcessingTimeTimeout$.MODULE$; }
+  public static GroupStateTimeout ProcessingTimeTimeout() { return ProcessingTimeTimeout$.MODULE$; }
 
   /**
    * Timeout based on event-time. The event-time timestamp for timeout can be set for each
-   * group in `map/flatMapGroupsWithState` by calling `KeyedState.setTimeoutTimestamp()`.
+   * group in `map/flatMapGroupsWithState` by calling `GroupState.setTimeoutTimestamp()`.
    * In addition, you have to define the watermark in the query using `Dataset.withWatermark`.
    * When the watermark advances beyond the set timestamp of a group and the group has not
    * received any data, then the group times out. See documentation on
-   * `KeyedState` for more details.
+   * `GroupState` for more details.
    */
-  public static KeyedStateTimeout EventTimeTimeout() { return EventTimeTimeout$.MODULE$; }
+  public static GroupStateTimeout EventTimeTimeout() { return EventTimeTimeout$.MODULE$; }
 
   /** No timeout. */
-  public static KeyedStateTimeout NoTimeout() { return NoTimeout$.MODULE$; }
+  public static GroupStateTimeout NoTimeout() { return NoTimeout$.MODULE$; }
 }
