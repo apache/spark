0a455be971a601c50e11e6d8a7e631f18ad54d76
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2017-03-22 16:22:19 +0900

    address review comment

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameImplicitsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameImplicitsSuite.scala
@@ -53,13 +53,13 @@ class DataFrameImplicitsSuite extends QueryTest with SharedSQLContext {
   }
 
   test("SPARK-19959: df[java.lang.Long].collect includes null throws NullPointerException") {
-    val dfInt = sparkContext.parallelize(Seq[java.lang.Integer](0, null, 2), 1).toDF
-    assert(dfInt.collect === Array(Row(0), Row(null), Row(2)))
-    val dfLong = sparkContext.parallelize(Seq[java.lang.Long](0L, null, 2L), 1).toDF
-    assert(dfLong.collect === Array(Row(0L), Row(null), Row(2L)))
-    val dfFloat = sparkContext.parallelize(Seq[java.lang.Float](0.0F, null, 2.0F), 1).toDF
-    assert(dfFloat.collect === Array(Row(0.0F), Row(null), Row(2.0F)))
-    val dfDouble = sparkContext.parallelize(Seq[java.lang.Double](0.0D, null, 2.0D), 1).toDF
-    assert(dfDouble.collect === Array(Row(0.0D), Row(null), Row(2.0D)))
+    checkAnswer(sparkContext.parallelize(Seq[java.lang.Integer](0, null, 2), 1).toDF,
+      Seq(Row(0), Row(null), Row(2)))
+    checkAnswer(sparkContext.parallelize(Seq[java.lang.Long](0L, null, 2L), 1).toDF,
+      Seq(Row(0L), Row(null), Row(2L)))
+    checkAnswer(sparkContext.parallelize(Seq[java.lang.Float](0.0F, null, 2.0F), 1).toDF,
+      Seq(Row(0.0F), Row(null), Row(2.0F)))
+    checkAnswer(sparkContext.parallelize(Seq[java.lang.Double](0.0D, null, 2.0D), 1).toDF,
+      Seq(Row(0.0D), Row(null), Row(2.0D)))
   }
 }
