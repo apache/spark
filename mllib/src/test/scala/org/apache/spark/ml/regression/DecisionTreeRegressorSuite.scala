22ee03d27c528b2a07d8d7e2a1467de8a09257dc
颜发才（Yan Facai） <facai.yan@gmail.com>
2017-03-28 10:02:25 +0800

    TST: remove redundant unit test

--- mllib/src/test/scala/org/apache/spark/ml/regression/DecisionTreeRegressorSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/regression/DecisionTreeRegressorSuite.scala
@@ -178,20 +178,6 @@ class DecisionTreeRegressorSuite
       TreeTests.allParamSettings ++ Map("maxDepth" -> 0),
       TreeTests.allParamSettings ++ Map("maxDepth" -> 0), checkModelData)
   }
-
-  test("SPARK-20043: " +
-       "ImpurityCalculator builder fails for uppercase impurity type in model read/write") {
-    val rdd = TreeTests.getTreeReadWriteData(sc)
-    val data: DataFrame =
-      TreeTests.setMetadata(rdd, Map.empty[Int, Int], numClasses = 0)
-
-    val dt = new DecisionTreeRegressor()
-      .setImpurity("Variance")
-      .setMaxDepth(2)
-    val model = dt.fit(data)
-
-    testDefaultReadWrite(model)
-  }
 }
 
 private[ml] object DecisionTreeRegressorSuite extends SparkFunSuite {
