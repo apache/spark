7af4945fbfb309f7a7784cba2b1fc4cb4945fba0
Syrux <pokcyril@hotmail.com>
2017-04-08 12:17:04 +0200

    [SPARK-20265][MLlib] Improve Prefix'span pre-processing efficiency

--- mllib/src/main/scala/org/apache/spark/mllib/fpm/PrefixSpan.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/fpm/PrefixSpan.scala
@@ -174,8 +174,8 @@ class PrefixSpan private (
         if (result.nonEmpty) {
           containsFreqItems = true
           allItems ++= result.sorted
+          allItems += 0
         }
-        allItems += 0
       }
       if (containsFreqItems) {
         Iterator.single(allItems.result())
