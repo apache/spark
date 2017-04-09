cb5440787d676d2c74983ddcd1df31b38d009d71
DB Tsai <dbt@netflix.com>
2017-04-09 00:57:45 -0700

    na.fill will change the values in long or integer when the default value is in double

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameNaFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameNaFunctionsSuite.scala
@@ -146,6 +146,13 @@ class DataFrameNaFunctionsSuite extends QueryTest with SharedSQLContext {
     )
 
     checkAnswer(
+      Seq[(java.lang.Long, java.lang.Double)]((null, 3.14), (9123146099426677101L, null),
+        (9123146560113991650L, 1.6), (null, null)).toDF("a", "b").na.fill(0.2),
+      Row(0, 3.14) :: Row(9123146099426677101L, 0.3) :: Row(9123146560113991650L, 1.6)
+        :: Row(0, 0.2) :: Nil
+    )
+
+    checkAnswer(
       Seq[(java.lang.Long, java.lang.Double)]((null, 1.23), (3L, null), (4L, 3.45))
         .toDF("a", "b").na.fill(2.34),
       Row(2, 1.23) :: Row(3, 2.34) :: Row(4, 3.45) :: Nil
