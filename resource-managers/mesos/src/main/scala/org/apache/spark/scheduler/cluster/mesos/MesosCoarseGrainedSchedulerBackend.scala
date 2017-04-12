70ed95ad8c73c1c1ff46dcf191b26f96c51ea09e
antiout <uzbssr@gmail.com>
2017-04-12 02:17:30 -0400

    Removed hardcoded blacklist functionality, must be controled by BlacklistTracker

--- resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosCoarseGrainedSchedulerBackend.scala
+++ resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosCoarseGrainedSchedulerBackend.scala
@@ -55,9 +55,6 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
   with org.apache.mesos.Scheduler
   with MesosSchedulerUtils {
 
-  // Blacklist a slave after this many failures
-  private val MAX_SLAVE_FAILURES = 2
-
   private val maxCoresOption = conf.getOption("spark.cores.max").map(_.toInt)
 
   // Maximum number of cores to acquire
@@ -460,7 +457,6 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
       cpus + totalCoresAcquired <= maxCores &&
       mem <= offerMem &&
       numExecutors() < executorLimit &&
-      slaves.get(slaveId).map(_.taskFailures).getOrElse(0) < MAX_SLAVE_FAILURES &&
       meetsPortRequirements
   }
 
@@ -515,15 +511,6 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
         for (gpus <- gpusByTaskId.get(taskId)) {
           totalGpusAcquired -= gpus
           gpusByTaskId -= taskId
-        }
-        // If it was a failure, mark the slave as failed for blacklisting purposes
-        if (TaskState.isFailed(state)) {
-          slave.taskFailures += 1
-
-          if (slave.taskFailures >= MAX_SLAVE_FAILURES) {
-            logInfo(s"Blacklisting Mesos slave $slaveId due to too many failures; " +
-                "is Spark installed on it?")
-          }
         }
         executorTerminated(d, slaveId, taskId, s"Executor finished with state $state")
         // In case we'd rejected everything before but have now lost a node
@@ -650,6 +637,5 @@ private[spark] class MesosCoarseGrainedSchedulerBackend(
 
 private class Slave(val hostname: String) {
   val taskIDs = new mutable.HashSet[String]()
-  var taskFailures = 0
   var shuffleRegistered = false
 }
