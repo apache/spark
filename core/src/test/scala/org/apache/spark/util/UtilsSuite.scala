14b0d72d5fffb69694a2442ade6399161f99545c
Mark Grover <mark@apache.org>
2017-04-21 21:40:31 -0700

    Updating the unit test in UtilsSuite to include a regular property with a sensitive value

--- core/src/test/scala/org/apache/spark/util/UtilsSuite.scala
+++ core/src/test/scala/org/apache/spark/util/UtilsSuite.scala
@@ -1013,6 +1013,8 @@ class UtilsSuite extends SparkFunSuite with ResetSystemProperties with Logging {
     secretKeys.foreach { key => sparkConf.set(key, "sensitive_value") }
     // Set a non-secret key
     sparkConf.set("spark.regular.property", "regular_value")
+    // Set a property with a regular key but secret in the value
+    sparkConf.set("spark.sensitive.property", "has_secret_in_value")
 
     // Redact sensitive information
     val redactedConf = Utils.redact(sparkConf, sparkConf.getAll).toMap
@@ -1020,5 +1022,7 @@ class UtilsSuite extends SparkFunSuite with ResetSystemProperties with Logging {
     // Assert that secret information got redacted while the regular property remained the same
     secretKeys.foreach { key => assert(redactedConf(key) === Utils.REDACTION_REPLACEMENT_TEXT) }
     assert(redactedConf("spark.regular.property") === "regular_value")
+    assert(redactedConf("spark.sensitive.property") === Utils.REDACTION_REPLACEMENT_TEXT)
+
   }
 }
