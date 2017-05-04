f21897fc157ce467f2b2edb5631b31787883accd
zero323 <zero323@users.noreply.github.com>
2017-05-04 01:51:37 -0700

    [SPARK-20544][SPARKR] R wrapper for input_file_name
    ## What changes were proposed in this pull request?
    
    Adds wrapper for `o.a.s.sql.functions.input_file_name`
    
    ## How was this patch tested?
    
    Existing unit tests, additional unit tests, `check-cran.sh`.
    
    Author: zero323 <zero323@users.noreply.github.com>
    
    Closes #17818 from zero323/SPARK-20544.

--- R/pkg/inst/tests/testthat/test_sparkSQL.R
+++ R/pkg/inst/tests/testthat/test_sparkSQL.R
@@ -1402,6 +1402,11 @@ test_that("column functions", {
   expect_equal(collect(df2)[[3, 1]], FALSE)
   expect_equal(collect(df2)[[3, 2]], TRUE)
 
+  # Test that input_file_name()
+  actual_names <- sort(collect(distinct(select(df, input_file_name()))))
+  expect_equal(length(actual_names), 1)
+  expect_equal(basename(actual_names[1, 1]), basename(jsonPath))
+
   df3 <- select(df, between(df$name, c("Apache", "Spark")))
   expect_equal(collect(df3)[[1, 1]], TRUE)
   expect_equal(collect(df3)[[2, 1]], FALSE)
