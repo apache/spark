92a71a667dd3e13664015f2a9dd2a39e2c1514eb
Josh Rosen <joshrosen@databricks.com>
2017-05-10 16:50:57 -0700

    [SPARK-20685] Fix BatchPythonEvaluation bug in case of single UDF w/ repeated arg.
    ## What changes were proposed in this pull request?
    
    There's a latent corner-case bug in PySpark UDF evaluation where executing a `BatchPythonEvaluation` with a single multi-argument UDF where _at least one argument value is repeated_ will crash at execution with a confusing error.
    
    This problem was introduced in #12057: the code there has a fast path for handling a "batch UDF evaluation consisting of a single Python UDF", but that branch incorrectly assumes that a single UDF won't have repeated arguments and therefore skips the code for unpacking arguments from the input row (whose schema may not necessarily match the UDF inputs due to de-duplication of repeated arguments which occurred in the JVM before sending UDF inputs to Python).
    
    This fix here is simply to remove this special-casing: it turns out that the code in the "multiple UDFs" branch just so happens to work for the single-UDF case because Python treats `(x)` as equivalent to `x`, not as a single-argument tuple.
    
    ## How was this patch tested?
    
    New regression test in `pyspark.python.sql.tests` module (tested and confirmed that it fails before my fix).
    
    Author: Josh Rosen <joshrosen@databricks.com>
    
    Closes #17927 from JoshRosen/SPARK-20685.
    
    (cherry picked from commit 8ddbc431d8b21d5ee57d3d209a4f25e301f15283)
    Signed-off-by: Xiao Li <gatorsmile@gmail.com>

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -324,6 +324,12 @@ class SQLTests(ReusedPySparkTestCase):
         [row] = self.spark.sql("SELECT double(double(1) + 1)").collect()
         self.assertEqual(row[0], 6)
 
+    def test_single_udf_with_repeated_argument(self):
+        # regression test for SPARK-20685
+        self.spark.catalog.registerFunction("add", lambda x, y: x + y, IntegerType())
+        row = self.spark.sql("SELECT add(1, 1)").first()
+        self.assertEqual(tuple(row), (2, ))
+
     def test_multiple_udfs(self):
         self.spark.catalog.registerFunction("double", lambda x: x * 2, IntegerType())
         [row] = self.spark.sql("SELECT double(1), double(2)").collect()
