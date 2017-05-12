63876b55296ab1522c947d238c565af719449079
hyukjinkwon <gurwls223@gmail.com>
2017-05-12 16:42:58 +0800

    [SPARK-20639][SQL] Add single argument support for to_timestamp in SQL with documentation improvement
    ## What changes were proposed in this pull request?
    
    This PR proposes three things as below:
    
    - Use casting rules to a timestamp in `to_timestamp` by default (it was `yyyy-MM-dd HH:mm:ss`).
    
    - Support single argument for `to_timestamp` similarly with APIs in other languages.
    
      For example, the one below works
    
      ```
      import org.apache.spark.sql.functions._
      Seq("2016-12-31 00:12:00.00").toDF("a").select(to_timestamp(col("a"))).show()
      ```
    
      prints
    
      ```
      +----------------------------------------+
      |to_timestamp(`a`, 'yyyy-MM-dd HH:mm:ss')|
      +----------------------------------------+
      |                     2016-12-31 00:12:00|
      +----------------------------------------+
      ```
    
      whereas this does not work in SQL.
    
      **Before**
    
      ```
      spark-sql> SELECT to_timestamp('2016-12-31 00:12:00');
      Error in query: Invalid number of arguments for function to_timestamp; line 1 pos 7
      ```
    
      **After**
    
      ```
      spark-sql> SELECT to_timestamp('2016-12-31 00:12:00');
      2016-12-31 00:12:00
      ```
    
    - Related document improvement for SQL function descriptions and other API descriptions accordingly.
    
      **Before**
    
      ```
      spark-sql> DESCRIBE FUNCTION extended to_date;
      ...
      Usage: to_date(date_str, fmt) - Parses the `left` expression with the `fmt` expression. Returns null with invalid input.
      Extended Usage:
          Examples:
            > SELECT to_date('2016-12-31', 'yyyy-MM-dd');
             2016-12-31
      ```
    
      ```
      spark-sql> DESCRIBE FUNCTION extended to_timestamp;
      ...
      Usage: to_timestamp(timestamp, fmt) - Parses the `left` expression with the `format` expression to a timestamp. Returns null with invalid input.
      Extended Usage:
          Examples:
            > SELECT to_timestamp('2016-12-31', 'yyyy-MM-dd');
             2016-12-31 00:00:00.0
      ```
    
      **After**
    
      ```
      spark-sql> DESCRIBE FUNCTION extended to_date;
      ...
      Usage:
          to_date(date_str[, fmt]) - Parses the `date_str` expression with the `fmt` expression to
            a date. Returns null with invalid input. By default, it follows casting rules to a date if
            the `fmt` is omitted.
    
      Extended Usage:
          Examples:
            > SELECT to_date('2009-07-30 04:17:52');
             2009-07-30
            > SELECT to_date('2016-12-31', 'yyyy-MM-dd');
             2016-12-31
      ```
    
      ```
      spark-sql> DESCRIBE FUNCTION extended to_timestamp;
      ...
       Usage:
          to_timestamp(timestamp[, fmt]) - Parses the `timestamp` expression with the `fmt` expression to
            a timestamp. Returns null with invalid input. By default, it follows casting rules to
            a timestamp if the `fmt` is omitted.
    
      Extended Usage:
          Examples:
            > SELECT to_timestamp('2016-12-31 00:12:00');
             2016-12-31 00:12:00
            > SELECT to_timestamp('2016-12-31', 'yyyy-MM-dd');
             2016-12-31 00:00:00
      ```
    
    ## How was this patch tested?
    
    Added tests in `datetime.sql`.
    
    Author: hyukjinkwon <gurwls223@gmail.com>
    
    Closes #17901 from HyukjinKwon/to_timestamp_arg.

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -144,12 +144,6 @@ _functions_2_1 = {
                'measured in radians.',
 }
 
-_functions_2_2 = {
-    'to_date': 'Converts a string date into a DateType using the (optionally) specified format.',
-    'to_timestamp': 'Converts a string timestamp into a timestamp type using the ' +
-                    '(optionally) specified format.',
-}
-
 # math functions that take two arguments as input
 _binary_mathfunctions = {
     'atan2': 'Returns the angle theta from the conversion of rectangular coordinates (x, y) to' +
@@ -987,9 +981,10 @@ def months_between(date1, date2):
 def to_date(col, format=None):
     """Converts a :class:`Column` of :class:`pyspark.sql.types.StringType` or
     :class:`pyspark.sql.types.TimestampType` into :class:`pyspark.sql.types.DateType`
-    using the optionally specified format. Default format is 'yyyy-MM-dd'.
-    Specify formats according to
+    using the optionally specified format. Specify formats according to
     `SimpleDateFormats <http://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html>`_.
+    By default, it follows casting rules to :class:`pyspark.sql.types.DateType` if the format
+    is omitted (equivalent to ``col.cast("date")``).
 
     >>> df = spark.createDataFrame([('1997-02-28 10:30:00',)], ['t'])
     >>> df.select(to_date(df.t).alias('date')).collect()
@@ -1011,9 +1006,10 @@ def to_date(col, format=None):
 def to_timestamp(col, format=None):
     """Converts a :class:`Column` of :class:`pyspark.sql.types.StringType` or
     :class:`pyspark.sql.types.TimestampType` into :class:`pyspark.sql.types.DateType`
-    using the optionally specified format. Default format is 'yyyy-MM-dd HH:mm:ss'. Specify
-    formats according to
+    using the optionally specified format. Specify formats according to
     `SimpleDateFormats <http://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html>`_.
+    By default, it follows casting rules to :class:`pyspark.sql.types.TimestampType` if the format
+    is omitted (equivalent to ``col.cast("timestamp")``).
 
     >>> df = spark.createDataFrame([('1997-02-28 10:30:00',)], ['t'])
     >>> df.select(to_timestamp(df.t).alias('dt')).collect()
