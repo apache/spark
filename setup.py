ae61987945774ea2f9df53eaedac87fb378234a4
Kengo Seki <sekikn@apache.org>
2017-05-13 21:03:51 +0200

    [AIRFLOW-1180] Fix flask-wtf version for test_csrf_rejection
    For now, SecurityTests.test_csrf_rejection fails
    because flask-wtf version specified in setup.py is
    too old.
    This PR fixes it.
    
    Closes #2280 from sekikn/AIRFLOW-1180

--- setup.py
+++ setup.py
@@ -226,7 +226,7 @@ def do_setup():
             'flask-cache>=0.13.1, <0.14',
             'flask-login==0.2.11',
             'flask-swagger==0.2.13',
-            'flask-wtf==0.12',
+            'flask-wtf==0.14',
             'funcsigs==1.0.0',
             'future>=0.16.0, <0.17',
             'gitpython>=2.0.2',
