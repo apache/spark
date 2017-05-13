afcd0914e6e0e19cb3848b1f6190d981e66f5af5
zero323 <zero323@users.noreply.github.com>
2017-05-13 03:00:51 +0200

    Capitalization

--- R/pkg/R/mllib_wrapper.R
+++ R/pkg/R/mllib_wrapper.R
@@ -46,8 +46,8 @@ setClass("JavaMLWritable", representation(jobj = "jobj"))
 #  Save the ML model to the output path.
 
 #' @param object a fitted ML model.
-#' @param path The directory where the model is saved.
-#' @param overwrite Overwrites or not if the output path already exists. Default is FALSE
+#' @param path the directory where the model is saved.
+#' @param overwrite overwrites or not if the output path already exists. Default is FALSE
 #'                  which means throw exception if the output path exists.
 #'
 #' @aliases write.ml,JavaMLWritable,character-method
