d0b14cf42899cd082a47f821ba407043ef994468
Takeshi Yamamuro <yamamuro@apache.org>
2017-05-15 16:24:55 +0800

    [SPARK-20730][SQL] Add an optimizer rule to combine nested Concat
    ## What changes were proposed in this pull request?
    This pr added a new Optimizer rule to combine nested Concat. The master supports a pipeline operator '||' to concatenate strings in #17711 (This pr is follow-up). Since the parser currently generates nested Concat expressions, the optimizer needs to combine the nested expressions.
    
    ## How was this patch tested?
    Added tests in `CombineConcatSuite` and `SQLQueryTestSuite`.
    
    Author: Takeshi Yamamuro <yamamuro@apache.org>
    
    Closes #17970 from maropu/SPARK-20730.

--- sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
@@ -4,3 +4,7 @@ select format_string();
 
 -- A pipe operator for string concatenation
 select 'a' || 'b' || 'c';
+
+-- Check if catalyst combine nested `Concat`s
+EXPLAIN EXTENDED SELECT (col1 || col2 || col3 || col4) col
+FROM (SELECT id col1, id col2, id col3, id col4 FROM range(10));
