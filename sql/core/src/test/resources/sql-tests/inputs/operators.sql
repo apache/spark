fd342769010cee18dfd6983b9d7cc7e4eea472de
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2017-05-21 20:30:06 +0900

    fix test failures

--- sql/core/src/test/resources/sql-tests/inputs/operators.sql
+++ sql/core/src/test/resources/sql-tests/inputs/operators.sql
@@ -80,3 +80,8 @@ select 1 > 0.00001;
 
 -- mod
 select mod(7, 2), mod(7, 0), mod(0, 2), mod(7, null), mod(null, 2), mod(null, null);
+
+-- length
+select BIT_LENGTH('abc');
+select CHARACTER_LENGTH('abc');
+select OCTET_LENGTH('abc');
