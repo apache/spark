10db5ce4bc383ad7fb3db6c68d4d7f51ff759a02
liuzhaokun <liu.zhaokun@zte.com.cn>
2017-05-25 18:39:46 +0800

    fix the string message

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -1004,7 +1004,7 @@ private[spark] object Utils extends Logging {
           for (child <- listFilesSafely(file)) {
             try {
               deleteRecursively(child)
-              logTrace(file.getAbsolutePath + s" has been deleted")
+              logTrace(file.getAbsolutePath + " has been deleted")
             } catch {
               // In case of multiple exceptions, only last one will be thrown
               case ioe: IOException => savedIOException = ioe
