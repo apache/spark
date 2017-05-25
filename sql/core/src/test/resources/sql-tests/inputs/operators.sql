93baa57adac91ad0bb5cd791f2044ab8882b8637
liuxian <liu.xian3@zte.com.cn>
2017-05-25 10:13:14 +0800

    Fix:if the input parameter is float type for ceil or floor,the result is not we expected

--- sql/core/src/test/resources/sql-tests/inputs/operators.sql
+++ sql/core/src/test/resources/sql-tests/inputs/operators.sql
@@ -64,12 +64,9 @@ select cot(-1);
 select ceiling(0);
 select ceiling(1);
 select ceil(1234567890123456);
-select ceil(12345678901234567);
 select ceiling(1234567890123456);
-select ceiling(12345678901234567);
 
 -- floor
 select floor(0);
 select floor(1);
 select floor(1234567890123456);
-select floor(12345678901234567);
