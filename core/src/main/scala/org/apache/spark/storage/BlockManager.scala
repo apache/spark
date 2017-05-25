bb1801e278609ba378632c533665960cc80bd007
Li Yichao <lyc@zhihu.com>
2017-05-25 16:23:25 +0800

    Fix scala style.

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -179,7 +179,8 @@ private[spark] class BlockManager(
   // standard BlockTransferService to directly connect to other Executors.
   private[spark] val shuffleClient = if (externalShuffleServiceEnabled) {
     val transConf = SparkTransportConf.fromSparkConf(conf, "shuffle", numUsableCores)
-    new ExternalShuffleClient(transConf, securityManager, securityManager.isAuthenticationEnabled(), registrationTimeout)
+    new ExternalShuffleClient(transConf, securityManager,
+      securityManager.isAuthenticationEnabled(), registrationTimeout)
   } else {
     blockTransferService
   }
