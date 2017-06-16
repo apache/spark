ad21d5d840b14db9beca19a2823e5a7f44401b14
antoine <antoine.prang@gmail.com>
2017-06-16 02:56:44 +0200

    [SPARK-18838][CORE] ExecutorAllocationManager dedicated queue
    The ExecutorAllocationManager is now in a dedicated asynchronous queue.
    This listener suffer a lot of event drops. Put it in a dedicated queue
    decrease a lot the chance of them
    
      ## How was this patch tested?
      unit test  + manual tests have been run on the cluster

--- core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
+++ core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
@@ -217,7 +217,7 @@ private[spark] class ExecutorAllocationManager(
    * the scheduling task.
    */
   def start(): Unit = {
-    listenerBus.addListener(listener)
+    listenerBus.addIsolatedListener(listener, None)
 
     val scheduleTask = new Runnable() {
       override def run(): Unit = {
