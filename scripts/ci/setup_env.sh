fb21bcbcc1ffaaf78fde2e0d9a9b1414c346ec51
Bolke de Bruin <bolke@xs4all.nl>
2017-06-16 08:41:54 -0400

    Re-enable caching for hadoop components

--- scripts/ci/setup_env.sh
+++ scripts/ci/setup_env.sh
@@ -110,8 +110,6 @@ if $ONLY_DOWNLOAD; then
 fi
 
 echo "Extracting ${HADOOP_HOME}/hadoop.tar.gz into $HADOOP_HOME"
-# TODO: remove this below after first successful build
-rm -rf ${TRAVIS_CACHE}/${HADOOP_DISTRO}/hadoop.tar.gz
 tar zxf ${TRAVIS_CACHE}/${HADOOP_DISTRO}/hadoop.tar.gz --strip-components 1 -C $HADOOP_HOME
 
 if [ $? != 0 ]; then
@@ -128,8 +126,6 @@ fi
 
 echo "Downloading and unpacking hive"
 curl -z ${TRAVIS_CACHE}/hive/hive.tar.gz -o ${TRAVIS_CACHE}/hive/hive.tar.gz -L ${HIVE_URL}
-# TODO: remove this below after first successful build
-rm -rf ${TRAVIS_CACHE}/hive/hive.tar.gz
 tar zxf ${TRAVIS_CACHE}/hive/hive.tar.gz --strip-components 1 -C ${HIVE_HOME}
 
 if [ $? != 0 ]; then
