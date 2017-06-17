3b15f7b49cffec5b2de15c8402c7701c9cb2824c
10129659 <chen.yanshan@zte.com.cn>
2017-06-17 09:52:59 +0800

    Remove the Try block

--- core/src/main/scala/org/apache/spark/deploy/SparkSubmitArguments.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkSubmitArguments.scala
@@ -278,14 +278,11 @@ private[deploy] class SparkSubmitArguments(args: Seq[String], env: Map[String, S
     if (pyFiles != null && !isPython) {
       SparkSubmit.printErrorAndExit("--py-files given but primary resource is not a Python script")
     }
-    if (totalExecutorCores != null && executorCores != null) {
-      val totalCores = Try(totalExecutorCores.toInt).getOrElse(-1)
-      val coresPerExecutor = Try(executorCores.toInt).getOrElse(-1)
-      if (totalCores % coresPerExecutor != 0) {
-        SparkSubmit.printWarning(s"Total executor cores: ${totalExecutorCores} is not " +
-          s"divisible by cores per executor: ${executorCores}, " +
-          s"the left cores: ${totalCores % coresPerExecutor} will not be allocated")
-      }
+    if (totalExecutorCores != null && executorCores != null
+        && (totalExecutorCores.toInt % executorCores.toInt) != 0) {
+      SparkSubmit.printWarning(s"Total executor cores: ${totalExecutorCores} is not " +
+        s"divisible by cores per executor: ${executorCores}, " +
+        s"the left cores: ${totalExecutorCores.toInt % executorCores.toInt} will not be allocated")
     }
 
     if (master.startsWith("yarn")) {
