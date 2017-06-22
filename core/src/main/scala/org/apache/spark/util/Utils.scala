d3f8846dbbccb52318d1f71cd4e5ecd352ae738e
Devaraj K <devaraj@apache.org>
2017-06-21 22:04:08 -0700

    [SPARK-21170] [CORE] Utils.tryWithSafeFinallyAndFailureCallbacks throws IllegalArgumentException: Self-suppression not permitted

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -1349,7 +1349,7 @@ private[spark] object Utils extends Logging {
         finallyBlock
       } catch {
         case t: Throwable =>
-          if (originalThrowable != null) {
+          if (originalThrowable != null && originalThrowable != t) {
             originalThrowable.addSuppressed(t)
             logWarning(s"Suppressing exception in finally: " + t.getMessage, t)
             throw originalThrowable
@@ -1387,8 +1387,10 @@ private[spark] object Utils extends Logging {
           catchBlock
         } catch {
           case t: Throwable =>
-            originalThrowable.addSuppressed(t)
-            logWarning(s"Suppressing exception in catch: " + t.getMessage, t)
+            if (originalThrowable != t) {
+              originalThrowable.addSuppressed(t)
+              logWarning(s"Suppressing exception in catch: " + t.getMessage, t)
+            }
         }
         throw originalThrowable
     } finally {
@@ -1396,7 +1398,7 @@ private[spark] object Utils extends Logging {
         finallyBlock
       } catch {
         case t: Throwable =>
-          if (originalThrowable != null) {
+          if (originalThrowable != null && originalThrowable != t) {
             originalThrowable.addSuppressed(t)
             logWarning(s"Suppressing exception in finally: " + t.getMessage, t)
             throw originalThrowable
