ba255bfda792d58aaded892e49c6cf48f0391159
Nick Pentreath <nickp@za.ibm.com>
2017-06-22 12:52:12 +0200

    Clean up

--- mllib/src/main/scala/org/apache/spark/ml/feature/FeatureHasher.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/FeatureHasher.scala
@@ -17,28 +17,26 @@
 
 package org.apache.spark.ml.feature
 
-import org.apache.spark.SparkException
 import org.apache.spark.annotation.Since
 import org.apache.spark.ml.Transformer
 import org.apache.spark.ml.attribute.AttributeGroup
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.param.{IntParam, ParamMap, ParamValidators}
-import org.apache.spark.ml.param.shared.{HasInputCol, HasInputCols, HasOutputCol}
+import org.apache.spark.ml.param.shared.{HasInputCols, HasOutputCol}
 import org.apache.spark.ml.util.{DefaultParamsWritable, Identifiable, SchemaUtils}
+import org.apache.spark.mllib.feature.{HashingTF => OldHashingTF}
 import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.hash.Murmur3_x86_32._
-import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.Utils
 import org.apache.spark.util.collection.OpenHashMap
 
 
-@Since("2.2.0")
-class FeatureHasher(@Since("2.2.0") override val uid: String) extends Transformer
+@Since("2.3.0")
+class FeatureHasher(@Since("2.3.0") override val uid: String) extends Transformer
   with HasInputCols with HasOutputCol with DefaultParamsWritable {
 
-  @Since("2.2.0")
+  @Since("2.3.0")
   def this() = this(Identifiable.randomUID("featureHasher"))
 
   /**
@@ -46,88 +44,73 @@ class FeatureHasher(@Since("2.2.0") override val uid: String) extends Transforme
    * (default = 2^18^)
    * @group param
    */
-  @Since("2.2.0")
+  @Since("2.3.0")
   val numFeatures = new IntParam(this, "numFeatures", "number of features (> 0)",
     ParamValidators.gt(0))
 
   setDefault(numFeatures -> (1 << 18))
 
   /** @group getParam */
-  @Since("2.2.0")
+  @Since("2.3.0")
   def getNumFeatures: Int = $(numFeatures)
 
   /** @group setParam */
-  @Since("2.2.0")
+  @Since("2.3.0")
   def setNumFeatures(value: Int): this.type = set(numFeatures, value)
 
   /** @group setParam */
-  @Since("2.2.0")
+  @Since("2.3.0")
   def setInputCols(values: String*): this.type = setInputCols(values.toArray)
 
   /** @group setParam */
-  @Since("2.2.0")
+  @Since("2.3.0")
   def setInputCols(value: Array[String]): this.type = set(inputCols, value)
 
   /** @group setParam */
-  @Since("2.2.0")
+  @Since("2.3.0")
   def setOutputCol(value: String): this.type = set(outputCol, value)
 
   override def transform(dataset: Dataset[_]): DataFrame = {
+    val hashFunc: Any => Int = OldHashingTF.murmur3Hash
+    val numFeatures = $(numFeatures)
+
     val os = transformSchema(dataset.schema)
 
-    val inputFields = $(inputCols).map(c => dataset.schema(c))
-    val featureCols = inputFields.map { f =>
-      f.dataType match {
-        case DoubleType | StringType => dataset(f.name)
-        case _: NumericType | BooleanType => dataset(f.name).cast(DoubleType).alias(f.name)
+    val featureCols = $(inputCols).map { colName =>
+      val field = dataset.schema(colName)
+      field.dataType match {
+        case DoubleType | StringType => dataset(field.name)
+        case _: NumericType | BooleanType => dataset(field.name).cast(DoubleType).alias(field.name)
       }
     }
 
     val realFields = os.fields.filter(f => f.dataType.isInstanceOf[NumericType]).map(_.name).toSet
-    val hashFunc: Any => Int = FeatureHasher.murmur3Hash
 
     def hashFeatures = udf { row: Row =>
       val map = new OpenHashMap[Int, Double]()
-      $(inputCols).foreach { case field =>
-        val (rawIdx, value) = if (realFields(field)) {
-          val value = row.getDouble(row.fieldIndex(field))
-          val hash = hashFunc(field)
+      $(inputCols).foreach { case colName =>
+        val fieldIndex = row.fieldIndex(colName)
+        val (rawIdx, value) = if (realFields(colName)) {
+          val value = row.getDouble(fieldIndex)
+          val hash = hashFunc(colName)
           (hash, value)
         } else {
-          val value = row.getString(row.fieldIndex(field))
-          val fieldName = s"$field=$value"
+          val value = row.getString(fieldIndex)
+          val fieldName = s"$colName=$value"
           val hash = hashFunc(fieldName)
           (hash, 1.0)
         }
-        val idx = Utils.nonNegativeMod(rawIdx, $(numFeatures))
+        val idx = Utils.nonNegativeMod(rawIdx, numFeatures)
         map.changeValue(idx, value, v => v + value)
         (idx, value)
       }
-      Vectors.sparse($(numFeatures), map.toSeq)
+      Vectors.sparse(numFeatures, map.toSeq)
     }
 
+    val metadata = os($(outputCol)).metadata
     dataset.select(
       col("*"),
-      hashFeatures(struct(featureCols: _*)).as($(outputCol))) // , featureAttrs.toMetadata()))
-
-    /*
-    dataset.select($(inputCols).map(col(_)): _*).map { case row =>
-      val map = new OpenHashMap[Int, Double]()
-      val s = $(inputCols).zipWithIndex.map { case (field, i) =>
-        if (realFields(field)) {
-          val value = row.getDouble(i)
-          val hash = hashFunc(field)
-          val idx = Utils.nonNegativeMod(hash, $(numFeatures))
-          (idx, value)
-        } else {
-          val value = row.getString(i)
-          val idx = Utils.nonNegativeMod(hashFunc(value), $(numFeatures))
-          (idx, 1.0)
-        }
-      }
-      Row(Vectors.sparse($(numFeatures), s.toSeq))
-    }.toDF($(outputCol))
-    */
+      hashFeatures(struct(featureCols: _*)).as($(outputCol), metadata))
   }
 
   override def copy(extra: ParamMap): Transformer = defaultCopy(extra)
@@ -139,33 +122,5 @@ class FeatureHasher(@Since("2.2.0") override val uid: String) extends Transforme
     })
     val attrGroup = new AttributeGroup($(outputCol), $(numFeatures))
     SchemaUtils.appendColumn(schema, attrGroup.toStructField())
-  }
-}
-
-object FeatureHasher {
-
-  private val seed = 42
-
-  /**
-   * Calculate a hash code value for the term object using
-   * Austin Appleby's MurmurHash 3 algorithm (MurmurHash3_x86_32).
-   * This is the default hash algorithm used from Spark 2.0 onwards.
-   */
-  private[feature] def murmur3Hash(term: Any): Int = {
-    term match {
-      case null => seed
-      case b: Boolean => hashInt(if (b) 1 else 0, seed)
-      case b: Byte => hashInt(b, seed)
-      case s: Short => hashInt(s, seed)
-      case i: Int => hashInt(i, seed)
-      case l: Long => hashLong(l, seed)
-      case f: Float => hashInt(java.lang.Float.floatToIntBits(f), seed)
-      case d: Double => hashLong(java.lang.Double.doubleToLongBits(d), seed)
-      case s: String =>
-        val utf8 = UTF8String.fromString(s)
-        hashUnsafeBytes(utf8.getBaseObject, utf8.getBaseOffset, utf8.numBytes(), seed)
-      case _ => throw new SparkException("HashingTF with murmur3 algorithm does not " +
-        s"support type ${term.getClass.getCanonicalName} of input data.")
-    }
   }
 }
