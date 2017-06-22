0be1e6572110d7d550f69fd86d3dd4e96660fde6
Nick Pentreath <nickp@za.ibm.com>
2017-06-22 12:52:37 +0200

    Add tests

--- mllib/src/test/scala/org/apache/spark/ml/feature/FeatureHasherSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/FeatureHasherSuite.scala
@@ -23,67 +23,59 @@ import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.ml.param.ParamsSuite
 import org.apache.spark.ml.util.DefaultReadWriteTest
 import org.apache.spark.ml.util.TestingUtils._
-import org.apache.spark.mllib.feature.{HashingTF => MLlibHashingTF}
 import org.apache.spark.mllib.util.MLlibTestSparkContext
-import org.apache.spark.util.Utils
 
-class HashingTFSuite extends SparkFunSuite with MLlibTestSparkContext with DefaultReadWriteTest {
+class FeatureHasherSuite extends SparkFunSuite
+  with MLlibTestSparkContext
+  with DefaultReadWriteTest {
 
   import testImplicits._
   import HashingTFSuite.murmur3FeatureIdx
 
   test("params") {
-    ParamsSuite.checkParams(new HashingTF)
+    ParamsSuite.checkParams(new FeatureHasher)
   }
 
-  test("hashingTF") {
-    val df = Seq((0, "a a b b c d".split(" ").toSeq)).toDF("id", "words")
+  test("specify input cols using varargs or array") {
+    val featureHasher1 = new FeatureHasher()
+      .setInputCols("int", "double", "float", "stringNum", "string")
+    val featureHasher2 = new FeatureHasher()
+      .setInputCols(Array("int", "double", "float", "stringNum", "string"))
+    assert(featureHasher1.getInputCols === featureHasher2.getInputCols)
+  }
+
+  test("feature hashing") {
+    val df = Seq(
+      (2, 2.0, 2.0f, "2", "foo"),
+      (3, 3.0, 3.0f, "3", "bar")
+    ).toDF("int", "double", "float", "stringNum", "string")
     val n = 100
-    val hashingTF = new HashingTF()
-      .setInputCol("words")
+    val featureHasher = new FeatureHasher()
+      .setInputCols("int", "double", "float", "stringNum", "string")
       .setOutputCol("features")
       .setNumFeatures(n)
-    val output = hashingTF.transform(df)
+    val output = featureHasher.transform(df)
     val attrGroup = AttributeGroup.fromStructField(output.schema("features"))
     require(attrGroup.numAttributes === Some(n))
-    val features = output.select("features").first().getAs[Vector](0)
-    // Assume perfect hash on "a", "b", "c", and "d".
+    val features = output.select("features").as[Vector].collect()
+    // Assume perfect hash on field names
     def idx: Any => Int = murmur3FeatureIdx(n)
-    val expected = Vectors.sparse(n,
-      Seq((idx("a"), 2.0), (idx("b"), 2.0), (idx("c"), 1.0), (idx("d"), 1.0)))
-    assert(features ~== expected absTol 1e-14)
-  }
 
-  test("applying binary term freqs") {
-    val df = Seq((0, "a a b c c c".split(" ").toSeq)).toDF("id", "words")
-    val n = 100
-    val hashingTF = new HashingTF()
-        .setInputCol("words")
-        .setOutputCol("features")
-        .setNumFeatures(n)
-        .setBinary(true)
-    val output = hashingTF.transform(df)
-    val features = output.select("features").first().getAs[Vector](0)
-    def idx: Any => Int = murmur3FeatureIdx(n)  // Assume perfect hash on input features
-    val expected = Vectors.sparse(n,
-      Seq((idx("a"), 1.0), (idx("b"), 1.0), (idx("c"), 1.0)))
-    assert(features ~== expected absTol 1e-14)
+    // check expected indices
+    val expected = Seq(
+      Vectors.sparse(n, Seq((idx("int"), 2.0), (idx("double"), 2.0), (idx("float"), 2.0),
+        (idx("stringNum=2"), 1.0), (idx("string=foo"), 1.0))),
+      Vectors.sparse(n, Seq((idx("int"), 3.0), (idx("double"), 3.0), (idx("float"), 3.0),
+        (idx("stringNum=3"), 1.0), (idx("string=bar"), 1.0)))
+    )
+    assert(features.zip(expected).forall { case (e, a) => e ~== a absTol 1e-14 })
   }
 
   test("read/write") {
-    val t = new HashingTF()
-      .setInputCol("myInputCol")
+    val t = new FeatureHasher()
+      .setInputCols(Array("myCol1", "myCol2", "myCol3"))
       .setOutputCol("myOutputCol")
       .setNumFeatures(10)
     testDefaultReadWrite(t)
   }
-
-}
-
-object HashingTFSuite {
-
-  private[feature] def murmur3FeatureIdx(numFeatures: Int)(term: Any): Int = {
-    Utils.nonNegativeMod(MLlibHashingTF.murmur3Hash(term), numFeatures)
-  }
-
 }
