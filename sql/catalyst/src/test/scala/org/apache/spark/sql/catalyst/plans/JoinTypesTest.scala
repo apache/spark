9fc9a0ad567dfb28d22d94321fcef0ea3b1ae73b
BartekH <bartekhamielec@gmail.com>
2017-06-28 11:44:41 +0200

    remove apply method from JoinType execution

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/plans/JoinTypesTest.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/plans/JoinTypesTest.scala
@@ -23,40 +23,40 @@ import org.apache.spark.SparkFunSuite
 class JoinTypesTest extends SparkFunSuite {
 
   test("construct an Inner type") {
-    assert(JoinType.apply("inner") === Inner)
+    assert(JoinType("inner") === Inner)
   }
 
   test("construct a FullOuter type") {
-    assert(JoinType.apply("fullouter") === FullOuter)
-    assert(JoinType.apply("full_outer") === FullOuter)
-    assert(JoinType.apply("outer") === FullOuter)
-    assert(JoinType.apply("full") === FullOuter)
+    assert(JoinType("fullouter") === FullOuter)
+    assert(JoinType("full_outer") === FullOuter)
+    assert(JoinType("outer") === FullOuter)
+    assert(JoinType("full") === FullOuter)
   }
 
   test("construct a LeftOuter type") {
-    assert(JoinType.apply("leftouter") === LeftOuter)
-    assert(JoinType.apply("left_outer") === LeftOuter)
-    assert(JoinType.apply("left") === LeftOuter)
+    assert(JoinType("leftouter") === LeftOuter)
+    assert(JoinType("left_outer") === LeftOuter)
+    assert(JoinType("left") === LeftOuter)
   }
 
   test("construct a RightOuter type") {
-    assert(JoinType.apply("rightouter") === RightOuter)
-    assert(JoinType.apply("right_outer") === RightOuter)
-    assert(JoinType.apply("right") === RightOuter)
+    assert(JoinType("rightouter") === RightOuter)
+    assert(JoinType("right_outer") === RightOuter)
+    assert(JoinType("right") === RightOuter)
   }
 
   test("construct a LeftSemi type") {
-    assert(JoinType.apply("leftsemi") === LeftSemi)
-    assert(JoinType.apply("left_semi") === LeftSemi)
+    assert(JoinType("leftsemi") === LeftSemi)
+    assert(JoinType("left_semi") === LeftSemi)
   }
 
   test("construct a LeftAnti type") {
-    assert(JoinType.apply("leftanti") === LeftAnti)
-    assert(JoinType.apply("left_anti") === LeftAnti)
+    assert(JoinType("leftanti") === LeftAnti)
+    assert(JoinType("left_anti") === LeftAnti)
   }
 
   test("construct a Cross type") {
-    assert(JoinType.apply("cross") === Cross)
+    assert(JoinType("cross") === Cross)
   }
 
 }
