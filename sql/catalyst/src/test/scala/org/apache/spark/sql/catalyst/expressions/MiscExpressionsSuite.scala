f4bea7061d09246c8fcaa97865043a70228913e3
Mario Molina <mmolimar@gmail.com>
2017-06-28 01:22:57 -0500

    Tests for data_type function

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/MiscExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/MiscExpressionsSuite.scala
@@ -17,8 +17,15 @@
 
 package org.apache.spark.sql.catalyst.expressions
 
+import java.math.{BigDecimal => JavaBigDecimal}
+import java.sql.{Date, Timestamp}
+
+import scala.math.BigDecimal
+
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.sql.types._
+import org.apache.spark.unsafe.types.CalendarInterval
+
 
 class MiscExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper {
 
@@ -37,6 +44,27 @@ class MiscExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper {
     }
     checkEvaluation(AssertTrue(Literal.create(true, BooleanType)), null)
     checkEvaluation(AssertTrue(Cast(Literal(1), BooleanType)), null)
+  }
+
+  test("data_type") {
+    checkEvaluation(GetDataType(Literal.create(null, NullType)), null)
+    checkEvaluation(GetDataType(Literal(false)), "boolean")
+    checkEvaluation(GetDataType(Literal(0: Byte)), "tinyint")
+    checkEvaluation(GetDataType(Literal(0: Short)), "smallint")
+    checkEvaluation(GetDataType(Literal(0)), "int")
+    checkEvaluation(GetDataType(Literal(0L)), "bigint")
+    checkEvaluation(GetDataType(Literal(0.0f)), "float")
+    checkEvaluation(GetDataType(Literal(0.0)), "double")
+    checkEvaluation(GetDataType(Literal(new Decimal().set(0.0))), "decimal(2,1)")
+    checkEvaluation(GetDataType(Literal(0.0: BigDecimal)), "decimal(1,1)")
+    checkEvaluation(GetDataType(Literal(new JavaBigDecimal(0.0d.toString))), "decimal(1,1)")
+    checkEvaluation(GetDataType(Literal(new Timestamp(0L))), "timestamp")
+    checkEvaluation(GetDataType(Literal(new Date(0L))), "date")
+    checkEvaluation(GetDataType(Literal(Array(0.0, 0.1))), "array<double>")
+    checkEvaluation(GetDataType(Literal(Array(0.0, 0.1))), "array<double>")
+    checkEvaluation(GetDataType(Literal("".getBytes)), "binary")
+    checkEvaluation(GetDataType(Literal(new CalendarInterval(0, 0))), "calendarinterval")
+    checkEvaluation(GetDataType(Literal("a")), "string")
   }
 
   test("uuid") {
