b26df6159bc5bc7f459e9eba32afe05cc00b855f
Yanbo Liang <ybliang8@gmail.com>
2017-06-29 10:32:32 +0800

    [SPARK-14657][SPARKR][ML] RFormula w/o intercept should output reference category when encoding string terms
    ## What changes were proposed in this pull request?
    
    Please see [SPARK-14657](https://issues.apache.org/jira/browse/SPARK-14657) for detail of this bug.
    I searched online and test some other cases, found when we fit R glm model(or other models powered by R formula) w/o intercept on a dataset including string/category features, one of the categories in the first category feature is being used as reference category, we will not drop any category for that feature.
    I think we should keep consistent semantics between Spark RFormula and R formula.
    ## How was this patch tested?
    
    Add standard unit tests.
    
    cc mengxr
    
    Author: Yanbo Liang <ybliang8@gmail.com>
    
    Closes #12414 from yanboliang/spark-14657.

--- mllib/src/main/scala/org/apache/spark/ml/feature/RFormula.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/RFormula.scala
@@ -205,12 +205,20 @@ class RFormula @Since("1.5.0") (@Since("1.5.0") override val uid: String)
     }.toMap
 
     // Then we handle one-hot encoding and interactions between terms.
+    var keepReferenceCategory = false
     val encodedTerms = resolvedFormula.terms.map {
       case Seq(term) if dataset.schema(term).dataType == StringType =>
         val encodedCol = tmpColumn("onehot")
-        encoderStages += new OneHotEncoder()
+        var encoder = new OneHotEncoder()
           .setInputCol(indexed(term))
           .setOutputCol(encodedCol)
+        // Formula w/o intercept, one of the categories in the first category feature is
+        // being used as reference category, we will not drop any category for that feature.
+        if (!hasIntercept && !keepReferenceCategory) {
+          encoder = encoder.setDropLast(false)
+          keepReferenceCategory = true
+        }
+        encoderStages += encoder
         prefixesToRewrite(encodedCol + "_") = term + "_"
         encodedCol
       case Seq(term) =>
