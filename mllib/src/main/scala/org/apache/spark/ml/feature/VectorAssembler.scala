bfffb8189af07d3ca309df8e7f6676308de58884
Yan Facai (颜发才) <facai.yan@gmail.com>
2017-07-04 14:36:31 +0800

    CLN: replace isInstanceOf

--- mllib/src/main/scala/org/apache/spark/ml/feature/VectorAssembler.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/VectorAssembler.scala
@@ -116,7 +116,7 @@ class VectorAssembler @Since("1.4.0") (@Since("1.4.0") override val uid: String)
     val inputDataTypes = inputColNames.map(name => (name, schema(name).dataType))
     inputDataTypes.foreach {
       case (_, _: NumericType | BooleanType) =>
-      case (_, t) if t.isInstanceOf[VectorUDT] =>
+      case (_, _: VectorUDT) =>
       case (name, other) =>
         throw new IllegalArgumentException(s"Data type $other of column $name is not supported.")
     }
