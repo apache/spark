826dcfd43d6da28ca5b9bfa2d719d6c9ba090e5a
hyukjinkwon <gurwls223@gmail.com>
2017-07-04 17:28:36 +0900

    Get rid of uesless imports

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -30,16 +30,6 @@ import pickle
 import functools
 import time
 import datetime
-import traceback
-
-if sys.version_info[:2] <= (2, 6):
-    try:
-        import unittest2 as unittest
-    except ImportError:
-        sys.stderr.write('Please install unittest2 to test with Python 2.6 or earlier')
-        sys.exit(1)
-else:
-    import unittest
 
 import py4j
 try:
