716fba9e36a8a4f02aa2d56a5375118e7cc8e6be
Jacek Laskowski <jacek@japila.pl>
2017-07-06 18:11:41 -0700

    [SPARK-21329][SS] Make EventTimeWatermarkExec explicitly UnaryExecNode
    ## What changes were proposed in this pull request?
    
    Making EventTimeWatermarkExec explicitly UnaryExecNode
    
    /cc tdas zsxwing
    
    ## How was this patch tested?
    
    Local build.
    
    Author: Jacek Laskowski <jacek@japila.pl>
    
    Closes #18509 from jaceklaskowski/EventTimeWatermarkExec-UnaryExecNode.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/EventTimeWatermarkExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/EventTimeWatermarkExec.scala
@@ -21,7 +21,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{Attribute, UnsafeProjection}
 import org.apache.spark.sql.catalyst.plans.logical.EventTimeWatermark
-import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.execution.{SparkPlan, UnaryExecNode}
 import org.apache.spark.sql.types.MetadataBuilder
 import org.apache.spark.unsafe.types.CalendarInterval
 import org.apache.spark.util.AccumulatorV2
@@ -81,7 +81,7 @@ class EventTimeStatsAccum(protected var currentStats: EventTimeStats = EventTime
 case class EventTimeWatermarkExec(
     eventTime: Attribute,
     delay: CalendarInterval,
-    child: SparkPlan) extends SparkPlan {
+    child: SparkPlan) extends UnaryExecNode {
 
   val eventTimeStats = new EventTimeStatsAccum()
   val delayMs = EventTimeWatermark.getDelayMs(delay)
@@ -117,6 +117,4 @@ case class EventTimeWatermarkExec(
       a
     }
   }
-
-  override def children: Seq[SparkPlan] = child :: Nil
 }
