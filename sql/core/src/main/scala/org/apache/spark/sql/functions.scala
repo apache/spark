c1011919c2ade597271a3f0c84d394bf1f2896df
Takeshi Yamamuro <yamamuro@apache.org>
2017-07-07 23:05:38 -0700

    [SPARK-21281][SQL] Use string types by default if array and map have no argument
    ## What changes were proposed in this pull request?
    This pr modified code to use string types by default if `array` and `map` in functions have no argument. This behaviour is the same with Hive one;
    ```
    hive> CREATE TEMPORARY TABLE t1 AS SELECT map();
    hive> DESCRIBE t1;
    _c0   map<string,string>
    
    hive> CREATE TEMPORARY TABLE t2 AS SELECT array();
    hive> DESCRIBE t2;
    _c0   array<string>
    ```
    
    ## How was this patch tested?
    Added tests in `DataFrameFunctionsSuite`.
    
    Author: Takeshi Yamamuro <yamamuro@apache.org>
    
    Closes #18516 from maropu/SPARK-21281.

--- sql/core/src/main/scala/org/apache/spark/sql/functions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/functions.scala
@@ -1565,10 +1565,7 @@ object functions {
    * @since 1.5.0
    */
   @scala.annotation.varargs
-  def greatest(exprs: Column*): Column = withExpr {
-    require(exprs.length > 1, "greatest requires at least 2 arguments.")
-    Greatest(exprs.map(_.expr))
-  }
+  def greatest(exprs: Column*): Column = withExpr { Greatest(exprs.map(_.expr)) }
 
   /**
    * Returns the greatest value of the list of column names, skipping null values.
@@ -1672,10 +1669,7 @@ object functions {
    * @since 1.5.0
    */
   @scala.annotation.varargs
-  def least(exprs: Column*): Column = withExpr {
-    require(exprs.length > 1, "least requires at least 2 arguments.")
-    Least(exprs.map(_.expr))
-  }
+  def least(exprs: Column*): Column = withExpr { Least(exprs.map(_.expr)) }
 
   /**
    * Returns the least value of the list of column names, skipping null values.
