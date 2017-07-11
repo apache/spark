f415e648a913aae358ceb99c6ca2e8f80150ef42
Xingbo Jiang <xingbo.jiang@databricks.com>
2017-07-11 14:52:08 +0800

    order the results

--- sql/core/src/test/resources/sql-tests/inputs/window.sql
+++ sql/core/src/test/resources/sql-tests/inputs/window.sql
@@ -1,21 +1,23 @@
 -- Test data.
 CREATE OR REPLACE TEMPORARY VIEW testData AS SELECT * FROM VALUES
-(1, "a"), (1, "a"), (2, "a"), (1, "b"), (2, "b"), (3, "b"), (null, "a"), (3, null), (null, null)
+(null, "a"), (1, "a"), (1, "a"), (2, "a"), (1, "b"), (2, "b"), (3, "b"), (null, null), (3, null)
 AS testData(val, cate);
 
 -- RowsBetween
-SELECT val, cate, count(val) OVER(PARTITION BY cate ORDER BY val ROWS CURRENT ROW) FROM testData;
+SELECT val, cate, count(val) OVER(PARTITION BY cate ORDER BY val ROWS CURRENT ROW) FROM testData
+ORDER BY cate, val;
 SELECT val, cate, sum(val) OVER(PARTITION BY cate ORDER BY val
-ROWS BETWEEN UNBOUNDED PRECEDING AND 1 FOLLOWING) FROM testData;
+ROWS BETWEEN UNBOUNDED PRECEDING AND 1 FOLLOWING) FROM testData ORDER BY cate, val;
 
 -- RangeBetween
-SELECT val, cate, count(val) OVER(PARTITION BY cate ORDER BY val RANGE 1 PRECEDING) FROM testData;
+SELECT val, cate, count(val) OVER(PARTITION BY cate ORDER BY val RANGE 1 PRECEDING) FROM testData
+ORDER BY cate, val;
 SELECT val, cate, sum(val) OVER(PARTITION BY cate ORDER BY val
-RANGE BETWEEN CURRENT ROW AND 1 FOLLOWING) FROM testData;
+RANGE BETWEEN CURRENT ROW AND 1 FOLLOWING) FROM testData ORDER BY cate, val;
 
 -- RangeBetween with reverse OrderBy
 SELECT val, cate, sum(val) OVER(PARTITION BY cate ORDER BY val DESC
-RANGE BETWEEN CURRENT ROW AND 1 FOLLOWING) FROM testData;
+RANGE BETWEEN CURRENT ROW AND 1 FOLLOWING) FROM testData ORDER BY cate, val;
 
 -- Window functions
 SELECT val, cate,
@@ -41,16 +43,16 @@ row_number() OVER(PARTITION BY cate ORDER BY val),
 var_pop(val) OVER(PARTITION BY cate ORDER BY val),
 var_samp(val) OVER(PARTITION BY cate ORDER BY val),
 approx_count_distinct(val) OVER(PARTITION BY cate ORDER BY val)
-FROM testData;
+FROM testData ORDER BY cate, val;
 
 -- Null inputs
-SELECT val, cate, avg(null) OVER(PARTITION BY cate ORDER BY val) FROM testData;
+SELECT val, cate, avg(null) OVER(PARTITION BY cate ORDER BY val) FROM testData ORDER BY cate, val;
 
 -- OrderBy not specified
-SELECT val, cate, row_number() OVER(PARTITION BY cate) FROM testData;
+SELECT val, cate, row_number() OVER(PARTITION BY cate) FROM testData ORDER BY cate, val;
 
 -- Over clause is empty
-SELECT val, cate, sum(val) OVER(), avg(val) OVER() FROM testData;
+SELECT val, cate, sum(val) OVER(), avg(val) OVER() FROM testData ORDER BY cate, val;
 
 -- first_value()/last_value() over ()
 SELECT val, cate,
@@ -60,4 +62,4 @@ first_value(true, true) OVER(),
 last_value(false) OVER(),
 last_value(false, false) OVER(),
 last_value(true, true) OVER()
-FROM testData;
+FROM testData ORDER BY cate, val;
