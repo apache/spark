264b0f36cedacd9a22b45a3e14b2186230432be6
Marcelo Vanzin <vanzin@cloudera.com>
2017-07-18 13:36:10 -0700

    [SPARK-21408][CORE] Better default number of RPC dispatch threads.
    Instead of using the host's cpu count, use the number of cores allocated
    for the Spark process when sizing the RPC dispatch thread pool. This avoids
    creating large thread pools on large machines when the number of allocated
    cores is small.
    
    Tested by verifying number of threads with spark.executor.cores set
    to 1 and 4; same thing for YARN AM.
    
    Author: Marcelo Vanzin <vanzin@cloudera.com>
    
    Closes #18639 from vanzin/SPARK-21408.

--- core/src/main/scala/org/apache/spark/rpc/netty/Dispatcher.scala
+++ core/src/main/scala/org/apache/spark/rpc/netty/Dispatcher.scala
@@ -32,8 +32,11 @@ import org.apache.spark.util.ThreadUtils
 
 /**
  * A message dispatcher, responsible for routing RPC messages to the appropriate endpoint(s).
+ *
+ * @param numUsableCores Number of CPU cores allocated to the process, for sizing the thread pool.
+ *                       If 0, will consider the available CPUs on the host.
  */
-private[netty] class Dispatcher(nettyEnv: NettyRpcEnv) extends Logging {
+private[netty] class Dispatcher(nettyEnv: NettyRpcEnv, numUsableCores: Int) extends Logging {
 
   private class EndpointData(
       val name: String,
@@ -192,8 +195,10 @@ private[netty] class Dispatcher(nettyEnv: NettyRpcEnv) extends Logging {
 
   /** Thread pool used for dispatching messages. */
   private val threadpool: ThreadPoolExecutor = {
+    val availableCores =
+      if (numUsableCores > 0) numUsableCores else Runtime.getRuntime.availableProcessors()
     val numThreads = nettyEnv.conf.getInt("spark.rpc.netty.dispatcher.numThreads",
-      math.max(2, Runtime.getRuntime.availableProcessors()))
+      math.max(2, availableCores))
     val pool = ThreadUtils.newDaemonFixedThreadPool(numThreads, "dispatcher-event-loop")
     for (i <- 0 until numThreads) {
       pool.execute(new MessageLoop)
