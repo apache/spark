c711ff5363a0608c11b97c915bfc8b8cdbf2ba95
hyukjinkwon <gurwls223@gmail.com>
2017-07-25 20:02:54 +0900

    Minor cleanup

--- sql/create-docs.sh
+++ sql/create-docs.sh
@@ -17,36 +17,33 @@
 # limitations under the License.
 #
 
-# Script to create SQL API docs. This requires `mkdocs`.
-# Also, it needs to build Spark first.
-
-# After running this script the html docs can be found in
-# $SPARK_HOME/sql/docs/site
+# Script to create SQL API docs. This requires `mkdocs` and to build
+# Spark first. After running this script the html docs can be found in
+# $SPARK_HOME/sql/site
 
 set -o pipefail
 set -e
 
 FWDIR="$(cd "`dirname "${BASH_SOURCE[0]}"`"; pwd)"
 SPARK_HOME="$(cd "`dirname "${BASH_SOURCE[0]}"`"/..; pwd)"
-WAREHOUSE_DIR="$FWDIR/_spark-warehouse"
 
 if ! hash python 2>/dev/null; then
-  echo "Missing python in the path, skipping SQL documentation generation."
+  echo "Missing python in your path, skipping SQL documentation generation."
   exit 0
 fi
 
 if ! hash mkdocs 2>/dev/null; then
-  echo "Missing mkdocs in the path, skipping SQL documentation generation."
+  echo "Missing mkdocs in your path, skipping SQL documentation generation."
   exit 0
 fi
 
-# Now create markdown file
+# Now create the markdown file
 rm -fr docs
 mkdir docs
 echo "Generating markdown files for SQL documentation."
 "$SPARK_HOME/bin/spark-submit" gen-sql-markdown.py
 
-# Now create HTML files
+# Now create the HTML files
 echo "Generating HTML files for SQL documentation."
 mkdocs build --clean
 rm -fr docs
