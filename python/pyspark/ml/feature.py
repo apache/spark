fb2ce45b08d45e6deff628e032b060d97a37ee60
Zheng RuiFeng <ruifengz@foxmail.com>
2017-07-27 20:32:33 +0800

    revert Binarizer in py side

--- python/pyspark/ml/feature.py
+++ python/pyspark/ml/feature.py
@@ -61,8 +61,7 @@ __all__ = ['Binarizer',
 
 
 @inherit_doc
-class Binarizer(JavaTransformer, HasInputCol, HasOutputCol, HasThreshold,
-                JavaMLReadable, JavaMLWritable):
+class Binarizer(JavaTransformer, HasInputCol, HasOutputCol, JavaMLReadable, JavaMLWritable):
     """
     Binarize a column of continuous features given a threshold.
 
@@ -108,6 +107,20 @@ class Binarizer(JavaTransformer, HasInputCol, HasOutputCol, HasThreshold,
         """
         kwargs = self._input_kwargs
         return self._set(**kwargs)
+
+    @since("1.4.0")
+    def setThreshold(self, value):
+        """
+        Sets the value of :py:attr:`threshold`.
+        """
+        return self._set(threshold=value)
+
+    @since("1.4.0")
+    def getThreshold(self):
+        """
+        Gets the value of threshold or its default value.
+        """
+        return self.getOrDefault(self.threshold)
 
 
 class LSHParams(Params):
