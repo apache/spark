91697079a29138b7581e64f2aa79247fa1a4e4af
Dmitry Parfenchik <d.parfenchik@criteo.com>
2017-07-30 22:06:32 +0200

    [SPARK-21254] [WebUI] Performance improvement: further reducing DOM manipulations
    Refactoring incomplete requests filter behavior due to inefficency in DOM
    manipulations. We were traversing DOM 2 more times just to hide columns
    that we could have avoided rendering in mustache. Factoring this logic in
    mustache template (`showCompletedColumn`) saves 70-80ms on 10k+ rows.

--- core/src/main/resources/org/apache/spark/ui/static/historypage.js
+++ core/src/main/resources/org/apache/spark/ui/static/historypage.js
@@ -148,12 +148,14 @@ $(document).ready(function() {
         "uiroot": uiRoot,
         "applications": array,
         "hasMultipleAttempts": hasMultipleAttempts,
+        "showCompletedColumn": !requestedIncomplete,
       }
 
       $.get("static/historypage-template.html", function(template) {
         var apps = $(Mustache.render($(template).filter("#history-summary-template").html(),data));
         var selector = "#history-summary-table";
         var attemptIdColumnName = 'attemptId';
+        var startedColumnName = 'started';
         var defaultSortColumn = completedColumnName = 'completed';
         var durationColumnName = 'duration';
         var conf = {
@@ -161,7 +163,7 @@ $(document).ready(function() {
             {name: 'appId', type: "appid-numeric"},
             {name: 'appName'},
             {name: attemptIdColumnName},
-            {name: 'started'},
+            {name: startedColumnName},
             {name: completedColumnName},
             {name: durationColumnName, type: "title-numeric"},
             {name: 'user'},
@@ -180,11 +182,10 @@ $(document).ready(function() {
           conf.columns = removeColumnByName(conf.columns, attemptIdColumnName);
         }
 
+        var defaultSortColumn = completedColumnName;
         if (requestedIncomplete) {
-          var completedCells = apps.find(".completedColumn");
-          for (i = 0; i < completedCells.length; i++) {
-            completedCells[i].style.display='none';
-          }
+          defaultSortColumn = startedColumnName;
+          conf.columns = removeColumnByName(conf.columns, completedColumnName);
         }
         conf.order = [[ getColumnIndex(conf.columns, defaultSortColumn), "desc" ]];
         conf.columnDefs = [
