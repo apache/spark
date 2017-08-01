c60751c259788ed81a1d2f1f5483fae2caa09839
Ilan Filonenko <if56@cornell.edu>
2017-08-01 12:03:43 -0700

    Fix bug with null arguments

--- core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
@@ -653,7 +653,9 @@ object SparkSubmit extends CommandLineUtils {
       if (args.isPython) {
         childArgs ++= Array("--primary-py-file", args.primaryResource)
         childArgs ++= Array("--main-class", "org.apache.spark.deploy.PythonRunner")
-        childArgs ++= Array("--other-py-files", args.pyFiles)
+        if (args.pyFiles != null) {
+          childArgs ++= Array("--other-py-files", args.pyFiles)
+        }
       } else {
         childArgs ++= Array("--primary-java-resource", args.primaryResource)
         childArgs ++= Array("--main-class", args.mainClass)
