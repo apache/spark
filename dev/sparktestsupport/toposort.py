5a6af72a1439512eeca0676bf7da092b12221aa6
Diogo Munaro <diogo.munaro@corp.globo.com>
2017-08-02 14:49:38 -0300

    Fixing tests for jenkins

--- dev/sparktestsupport/toposort.py
+++ dev/sparktestsupport/toposort.py
@@ -60,15 +60,13 @@ items in the preceeding sets.
     # Find all items that don't depend on anything.
     extra_items_in_deps = _reduce(set.union, data.values()) - set(data.keys())
     # Add empty dependences where needed.
-    data.update({item: set() for item in extra_items_in_deps})
+    data.update(dict((item, set()) for item in extra_items_in_deps))
     while True:
         ordered = set(item for item, dep in data.items() if len(dep) == 0)
         if not ordered:
             break
         yield ordered
-        data = {item: (dep - ordered)
-                for item, dep in data.items()
-                if item not in ordered}
+        data = dict((item, (dep - ordered)) for item, dep in data.items() if item not in ordered)
     if len(data) != 0:
         raise ValueError('Cyclic dependencies exist among these items: {}'.format(
             ', '.join(repr(x) for x in data.items())))
