fa92a7be709e78db8e8f50dca8e13855c1034fde
Jose Torres <joseph-torres@databricks.com>
2017-08-07 12:27:16 -0700

    [SPARK-21565][SS] Propagate metadata in attribute replacement.
    ## What changes were proposed in this pull request?
    
    Propagate metadata in attribute replacement during streaming execution. This is necessary for EventTimeWatermarks consuming replaced attributes.
    
    ## How was this patch tested?
    new unit test, which was verified to fail before the fix
    
    Author: Jose Torres <joseph-torres@databricks.com>
    
    Closes #18840 from joseph-torres/SPARK-21565.
    
    (cherry picked from commit cce25b360ee9e39d9510134c73a1761475eaf4ac)
    Signed-off-by: Shixiong Zhu <shixiong@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamExecution.scala
@@ -631,7 +631,8 @@ class StreamExecution(
     // Rewire the plan to use the new attributes that were returned by the source.
     val replacementMap = AttributeMap(replacements)
     val triggerLogicalPlan = withNewSources transformAllExpressions {
-      case a: Attribute if replacementMap.contains(a) => replacementMap(a)
+      case a: Attribute if replacementMap.contains(a) =>
+        replacementMap(a).withMetadata(a.metadata)
       case ct: CurrentTimestamp =>
         CurrentBatchTimestamp(offsetSeqMetadata.batchTimestampMs,
           ct.dataType)
