0b272094c8a490e16066e85fc48ad59c4ccf0468
Marcelo Vanzin <vanzin@cloudera.com>
2017-08-08 13:27:56 -0700

    Break out and expand Hive_2_1_DDLSuite.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
@@ -115,7 +115,7 @@ private[spark] class HiveExternalCatalog(conf: SparkConf, hadoopConf: Configurat
    * should interpret these special data source properties and restore the original table metadata
    * before returning it.
    */
-  private def getRawTable(db: String, table: String): CatalogTable = withClient {
+  private[hive] def getRawTable(db: String, table: String): CatalogTable = withClient {
     client.getTable(db, table)
   }
 
