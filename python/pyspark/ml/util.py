ba4402cac561714850ac2eabc38c9891af6d1960
Ajay Saini <ajays725@gmail.com>
2017-08-08 16:59:17 -0700

    Fixed dictionary update

--- python/pyspark/ml/util.py
+++ python/pyspark/ml/util.py
@@ -419,8 +419,8 @@ class DefaultParamsWriter(MLWriter):
         basicMetadata = {"class": cls, "timestamp": long(round(time.time() * 1000)),
                          "sparkVersion": sc.version, "uid": uid, "paramMap": jsonParams}
         if extraMetadata is not None:
-            for key, value in extraMetadata:
-                basicMetadata[key] = value
+            for key in extraMetadata:
+                basicMetadata[key] = extraMetadata[key]
         return json.dumps(basicMetadata, separators=[',',  ':'])
 
 
