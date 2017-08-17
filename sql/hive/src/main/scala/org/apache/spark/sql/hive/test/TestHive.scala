ae9e42479253a9cd30423476405377f2d7952137
gatorsmile <gatorsmile@gmail.com>
2017-08-17 13:00:37 -0700

    [SQL][MINOR][TEST] Set spark.unsafe.exceptionOnMemoryLeak to true
    ## What changes were proposed in this pull request?
    When running IntelliJ, we are unable to capture the exception of memory leak detection.
    > org.apache.spark.executor.Executor: Managed memory leak detected
    
    Explicitly setting `spark.unsafe.exceptionOnMemoryLeak` in SparkConf when building the SparkSession, instead of reading it from system properties.
    
    ## How was this patch tested?
    N/A
    
    Author: gatorsmile <gatorsmile@gmail.com>
    
    Closes #18967 from gatorsmile/setExceptionOnMemoryLeak.

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/test/TestHive.scala
@@ -55,7 +55,8 @@ object TestHive
           "org.apache.spark.sql.hive.execution.PairSerDe")
         .set("spark.sql.warehouse.dir", TestHiveContext.makeWarehouseDir().toURI.getPath)
         // SPARK-8910
-        .set("spark.ui.enabled", "false")))
+        .set("spark.ui.enabled", "false")
+        .set("spark.unsafe.exceptionOnMemoryLeak", "true")))
 
 
 case class TestHiveVersion(hiveClient: HiveClient)
