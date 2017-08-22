962f658476eef78a54887a27553f4aa42badb8ae
Bryan Cutler <cutlerb@gmail.com>
2017-08-22 11:50:37 -0700

    cleanup

--- examples/src/main/scala/org/apache/spark/examples/ml/FeatureHasherExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/ml/FeatureHasherExample.scala
@@ -31,18 +31,18 @@ object FeatureHasherExample {
       .getOrCreate()
 
     // $example on$
-    val data = Seq(
+    val dataset = spark.createDataFrame(Seq(
       (2.2, true, "1", "foo"),
       (3.3, false, "2", "bar"),
       (4.4, false, "3", "baz"),
-      (5.5, false, "4", "foo"))
-    val dataFrame = spark.createDataFrame(data).toDF("real", "bool", "stringNum", "string")
+      (5.5, false, "4", "foo")
+    )).toDF("real", "bool", "stringNum", "string")
 
     val hasher = new FeatureHasher()
       .setInputCols("real", "bool", "stringNum", "string")
       .setOutputCol("features")
 
-    val featurized = hasher.transform(dataFrame)
+    val featurized = hasher.transform(dataset)
     featurized.show(false)
     // $example off$
 
