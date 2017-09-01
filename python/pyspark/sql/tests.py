5cd8ea99f084bee40ee18a0c8e33d0ca0aa6bb60
hyukjinkwon <gurwls223@gmail.com>
2017-09-01 13:01:23 +0900

    [SPARK-21779][PYTHON] Simpler DataFrame.sample API in Python
    ## What changes were proposed in this pull request?
    
    This PR make `DataFrame.sample(...)` can omit `withReplacement` defaulting `False`, consistently with equivalent Scala / Java API.
    
    In short, the following examples are allowed:
    
    ```python
    >>> df = spark.range(10)
    >>> df.sample(0.5).count()
    7
    >>> df.sample(fraction=0.5).count()
    3
    >>> df.sample(0.5, seed=42).count()
    5
    >>> df.sample(fraction=0.5, seed=42).count()
    5
    ```
    
    In addition, this PR also adds some type checking logics as below:
    
    ```python
    >>> df = spark.range(10)
    >>> df.sample().count()
    ...
    TypeError: withReplacement (optional), fraction (required) and seed (optional) should be a bool, float and number; however, got [].
    >>> df.sample(True).count()
    ...
    TypeError: withReplacement (optional), fraction (required) and seed (optional) should be a bool, float and number; however, got [<type 'bool'>].
    >>> df.sample(42).count()
    ...
    TypeError: withReplacement (optional), fraction (required) and seed (optional) should be a bool, float and number; however, got [<type 'int'>].
    >>> df.sample(fraction=False, seed="a").count()
    ...
    TypeError: withReplacement (optional), fraction (required) and seed (optional) should be a bool, float and number; however, got [<type 'bool'>, <type 'str'>].
    >>> df.sample(seed=[1]).count()
    ...
    TypeError: withReplacement (optional), fraction (required) and seed (optional) should be a bool, float and number; however, got [<type 'list'>].
    >>> df.sample(withReplacement="a", fraction=0.5, seed=1)
    ...
    TypeError: withReplacement (optional), fraction (required) and seed (optional) should be a bool, float and number; however, got [<type 'str'>, <type 'float'>, <type 'int'>].
    ```
    
    ## How was this patch tested?
    
    Manually tested, unit tests added in doc tests and manually checked the built documentation for Python.
    
    Author: hyukjinkwon <gurwls223@gmail.com>
    
    Closes #18999 from HyukjinKwon/SPARK-21779.

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -2108,6 +2108,24 @@ class SQLTests(ReusedPySparkTestCase):
         plan = df1.join(df2.hint("broadcast"), "id")._jdf.queryExecution().executedPlan()
         self.assertEqual(1, plan.toString().count("BroadcastHashJoin"))
 
+    def test_sample(self):
+        self.assertRaisesRegexp(
+            TypeError,
+            "should be a bool, float and number",
+            lambda: self.spark.range(1).sample())
+
+        self.assertRaises(
+            TypeError,
+            lambda: self.spark.range(1).sample("a"))
+
+        self.assertRaises(
+            TypeError,
+            lambda: self.spark.range(1).sample(seed="abc"))
+
+        self.assertRaises(
+            IllegalArgumentException,
+            lambda: self.spark.range(1).sample(-1.0))
+
     def test_toDF_with_schema_string(self):
         data = [Row(key=i, value=str(i)) for i in range(100)]
         rdd = self.sc.parallelize(data, 5)
