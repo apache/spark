ccb889f28da94fc580ecdb49fad738c960a5caf4
Zhipeng Zhang <zhipeng.zhang@inf.ethz.ch>
2017-09-04 08:52:19 +0200

    test

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -14,7 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*
+ * ghandzhipeng Test for Branch
+ */
 package org.apache.spark
 
 import java.io._
