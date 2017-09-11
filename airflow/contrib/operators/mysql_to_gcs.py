e83012589bc6ac97421d462e32ab80ae05bb9817
Mike Ghen <mike.ghen@gmail.com>
2017-09-11 13:10:31 +0200

    [AIRFLOW-1586] Add mapping for date type to mysql_to_gcs operator
    Closes #2589 from mikeghen/bug/airflow-1586

--- airflow/contrib/operators/mysql_to_gcs.py
+++ airflow/contrib/operators/mysql_to_gcs.py
@@ -210,6 +210,7 @@ class MySqlToGoogleCloudStorageOperator(BaseOperator):
             FIELD_TYPE.TINY: 'INTEGER',
             FIELD_TYPE.BIT: 'INTEGER',
             FIELD_TYPE.DATETIME: 'TIMESTAMP',
+            FIELD_TYPE.DATE: 'TIMESTAMP',
             FIELD_TYPE.DECIMAL: 'FLOAT',
             FIELD_TYPE.NEWDECIMAL: 'FLOAT',
             FIELD_TYPE.DOUBLE: 'FLOAT',
