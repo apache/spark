a995edc26d9e4d6803973095b3092a5ca50639d6
Zhenhua Wang <wangzhenhua@huawei.com>
2017-09-14 15:57:08 +0800

    fix comments

--- sql/core/src/test/resources/sql-tests/inputs/describe-table-column.sql
+++ sql/core/src/test/resources/sql-tests/inputs/describe-table-column.sql
@@ -1,19 +1,17 @@
 -- Test temp table
-CREATE TEMPORARY VIEW desc_col_temp_table (key int COMMENT 'column_comment') USING PARQUET;
+CREATE TEMPORARY VIEW desc_col_temp_view (key int COMMENT 'column_comment') USING PARQUET;
 
-DESC desc_col_temp_table key;
+DESC desc_col_temp_view key;
 
-DESC EXTENDED desc_col_temp_table key;
+DESC EXTENDED desc_col_temp_view key;
 
-DESC FORMATTED desc_col_temp_table key;
+DESC FORMATTED desc_col_temp_view key;
 
 -- Describe a column with qualified name
-DESC FORMATTED desc_col_temp_table desc_col_temp_table.key;
+DESC FORMATTED desc_col_temp_view desc_col_temp_view.key;
 
 -- Describe a non-existent column
-DESC desc_col_temp_table key1;
-
-DROP VIEW desc_col_temp_table;
+DESC desc_col_temp_view key1;
 
 -- Test persistent table
 CREATE TABLE desc_col_table (key int COMMENT 'column_comment') USING PARQUET;
@@ -26,8 +24,6 @@ DESC EXTENDED desc_col_table key;
 
 DESC FORMATTED desc_col_table key;
 
-DROP TABLE desc_col_table;
-
 -- Test complex columns
 CREATE TABLE desc_complex_col_table (`a.b` int, col struct<x:int, y:string>) USING PARQUET;
 
@@ -37,5 +33,9 @@ DESC FORMATTED desc_complex_col_table col;
 
 -- Describe a nested column
 DESC FORMATTED desc_complex_col_table col.x;
+
+DROP VIEW desc_col_temp_view;
+
+DROP TABLE desc_col_table;
 
 DROP TABLE desc_complex_col_table;
