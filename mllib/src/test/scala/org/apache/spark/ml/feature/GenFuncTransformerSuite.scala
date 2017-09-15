795619f6989a66708c41042a2338e9e343a352d9
Hosur Narhari <hosur.narhari@impetus.co.in>
2017-09-15 16:49:30 +0530

    adding license header to GenFuncTransformerSuite

--- mllib/src/test/scala/org/apache/spark/ml/feature/GenFuncTransformerSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/feature/GenFuncTransformerSuite.scala
@@ -1,12 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.ml.feature
 
 import org.apache.spark.SparkFunSuite
-import org.apache.spark.mllib.util.MLlibTestSparkContext
-import org.apache.spark.ml.util.DefaultReadWriteTest
 import org.apache.spark.ml.param.ParamsSuite
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.types.StructField
+import org.apache.spark.ml.util.DefaultReadWriteTest
+import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
 
 
 class GenFuncTransformerSuite 
