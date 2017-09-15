7d3a6d6f8b4e618d8ada9ff766609a3a98663338
Daniel Fernandez <daniel@grandata.com>
2017-09-15 14:11:28 -0300

    Code indentation fixed in JDBCSuite

--- sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCSuite.scala
@@ -1103,9 +1103,8 @@ class JDBCSuite extends SparkFunSuite
        """.stripMargin)
 
       val df3 = sql("SELECT * FROM test_sessionInitStatement")
-      assert(df3.collect() === Array(Row(21519, 1234))
-    )
-  }
+      assert(df3.collect() === Array(Row(21519, 1234)))
+    }
 
   test("Hive dialect registration") {
     assert(JdbcDialects.get("jdbc:hive2://127.0.0.1/db") == HiveDialect)
