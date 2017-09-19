824856b9f2080cbc2e99ac75a9592f7386322e0b
zuotingbing <zuo.tingbing9@zte.com.cn>
2017-09-19 19:33:32 +0800

    fix code review

--- core/src/main/scala/org/apache/spark/scheduler/EventLoggingListener.scala
+++ core/src/main/scala/org/apache/spark/scheduler/EventLoggingListener.scala
@@ -347,11 +347,12 @@ private[spark] object EventLoggingListener extends Logging {
   def openEventLog(log: Path, fs: FileSystem): InputStream = {
     val in = new BufferedInputStream(fs.open(log))
 
+    // Compression codec is encoded as an extension, e.g. app_123.lzf
+    // Since we sanitize the app ID to not include periods, it is safe to split on it
+    val logName = log.getName.stripSuffix(IN_PROGRESS)
+    val codecName: Option[String] = logName.split("\\.").tail.lastOption
+    
     try {
-      // Compression codec is encoded as an extension, e.g. app_123.lzf
-      // Since we sanitize the app ID to not include periods, it is safe to split on it
-      val logName = log.getName.stripSuffix(IN_PROGRESS)
-      val codecName: Option[String] = logName.split("\\.").tail.lastOption
       val codec = codecName.map { c =>
         codecMap.getOrElseUpdate(c, CompressionCodec.createCodec(new SparkConf, c))
       }
