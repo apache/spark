1a244460294a774008b6c49fb8d6a6f10f6c63e6
Robert Kruszewski <robert@robert.sx>
2017-09-25 11:55:52 +0100

    Correct publish location (#268)

--- dev/publish.sh
+++ dev/publish.sh
@@ -20,10 +20,10 @@ make_dist() {
   build_flags="$1"
   shift 1
   hadoop_name="hadoop-palantir"
-  artifact_name="spark-dist_2.11-${hadoop_name}-${version}.tgz"
+  artifact_name="spark-dist_2.11-${hadoop_name}"
   file_name="spark-dist-${version}-${hadoop_name}.tgz"
   ./dev/make-distribution.sh --name "hadoop-palantir" --tgz "$@" $build_flags
-  curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T $file_name "https://api.bintray.com/content/palantir/releases/spark/${version}/org/apache/spark/${artifact_name}/${version}/${artifact_name}"
+  curl -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD -T $file_name "https://api.bintray.com/content/palantir/releases/spark/${version}/org/apache/spark/${artifact_name}/${version}/${artifact_name}-${version}.tgz"
 }
 
 publish_artifacts | tee -a "$CIRCLE_ARTIFACTS/publish.log"
