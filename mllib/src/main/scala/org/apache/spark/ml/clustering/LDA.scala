b32905138d598b084903c3a5fabc2bf02e47b37a
Peng Meng <peng.meng@intel.com>
2017-10-12 13:03:16 +0800

    fix doc

--- mllib/src/main/scala/org/apache/spark/ml/clustering/LDA.scala
+++ mllib/src/main/scala/org/apache/spark/ml/clustering/LDA.scala
@@ -225,14 +225,15 @@ private[clustering] trait LDAParams extends Params with HasFeaturesCol with HasM
    * For Online optimizer only: [[optimizer]] = "online".
    *
    * A (positive) learning parameter that controls the convergence of variational inference.
-   * Smaller value will lead to more accuracy model and longer training time.
+   * Smaller value will lead to a more converged model and longer training time.
+   * Default: 1e-3.
    *
    * @group expertParam
    */
   @Since("2.3.0")
   final val epsilon = new DoubleParam(this, "epsilon", "(For online optimizer)" +
     " A (positive) learning parameter that controls the convergence of variational inference." +
-    " Smaller value will lead to more accuracy model and longer training time.",
+    " Smaller value will lead to a more converged model and longer training time.",
     ParamValidators.gt(0))
 
   /** @group expertGetParam */
