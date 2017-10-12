03e88b0ce8fc6dbf87ea970ba58abc79ff9c2a7f
Wang Gengliang <ltnwgl@gmail.com>
2017-10-12 11:20:56 -0700

    revise as per review comments

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/misc.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/misc.scala
@@ -119,7 +119,7 @@ case class CurrentDatabase() extends LeafExpression with Unevaluable {
 // scalastyle:on line.size.limit
 case class Uuid() extends LeafExpression {
 
-  override def isDeterministic: Boolean = false
+  override lazy val deterministic: Boolean = false
 
   override def nullable: Boolean = false
 
