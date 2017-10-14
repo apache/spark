f42e35175969d8d7363e008a586a6f6982290447
Bryan Cutler <cutlerb@gmail.com>
2017-10-14 14:45:51 -0700

    updated function name to_arrow_type

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -1628,7 +1628,7 @@ def to_arrow_schema(schema):
     """ Convert a schema from Spark to Arrow
     """
     import pyarrow as pa
-    fields = [pa.field(field.name, toArrowType(field.dataType)) for field in schema]
+    fields = [pa.field(field.name, to_arrow_type(field.dataType)) for field in schema]
     return pa.schema(fields)
 
 
