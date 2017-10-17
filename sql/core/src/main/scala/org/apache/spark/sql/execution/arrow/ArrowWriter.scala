7266304fc68f3e5ca26a4dda543f18837b013e29
Bryan Cutler <cutlerb@gmail.com>
2017-10-17 13:29:20 -0700

    remove check for valid timezone in vector for ArrowWriter

--- sql/core/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala
@@ -56,11 +56,7 @@ object ArrowWriter {
       case (StringType, vector: NullableVarCharVector) => new StringWriter(vector)
       case (BinaryType, vector: NullableVarBinaryVector) => new BinaryWriter(vector)
       case (DateType, vector: NullableDateDayVector) => new DateWriter(vector)
-      case (TimestampType, vector: NullableTimeStampMicroTZVector)
-        // TODO: Should be able to access timezone from vector
-        if field.getType.isInstanceOf[ArrowType.Timestamp] &&
-          field.getType.asInstanceOf[ArrowType.Timestamp].getTimezone != null =>
-        new TimestampWriter(vector)
+      case (TimestampType, vector: NullableTimeStampMicroTZVector) => new TimestampWriter(vector)
       case (ArrayType(_, _), vector: ListVector) =>
         val elementVector = createFieldWriter(vector.getDataVector())
         new ArrayWriter(vector, elementVector)
