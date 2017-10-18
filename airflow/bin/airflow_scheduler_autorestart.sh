97a2393f644ff438986843c04a47754a37ae0bc5
Matthew Schmoyer <mattschmo@gmail.com>
2017-10-18 21:41:01 +0200

    [AIRFLOW-1722] Fix typo in scheduler autorestart output filename
    Extra r in /tmp/airflow_scheduler_errors.txt has
    been removed.
    
    Closes #2693 from mschmo/AIRFLOW-1722

--- airflow/bin/airflow_scheduler_autorestart.sh
+++ airflow/bin/airflow_scheduler_autorestart.sh
@@ -14,6 +14,6 @@
 while echo "Running"; do
     airflow scheduler -n 5
     echo "Scheduler crashed with exit code $?.  Respawning.." >&2
-    date >> /tmp/airflow_scheduler_errrors.txt
+    date >> /tmp/airflow_scheduler_errors.txt
     sleep 1
 done
