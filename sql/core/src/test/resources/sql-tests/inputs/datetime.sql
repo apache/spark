7e7a25c68499c1f3c1f41a223658a546a975015a
donnyzone <wellfengzhu@gmail.com>
2017-10-27 18:41:09 +0800

    add new test

--- sql/core/src/test/resources/sql-tests/inputs/datetime.sql
+++ sql/core/src/test/resources/sql-tests/inputs/datetime.sql
@@ -12,14 +12,16 @@ select dayofweek('2007-02-03'), dayofweek('2009-07-30'), dayofweek('2017-05-27')
 -- [SPARK-22333]: timeFunctionCall has conflicts with columnReference
 create temporary view ttf1 as select * from values
   (1, 2),
-  (2, 3),
+  (2, 3)
   as ttf1(current_date, current_timestamp);
   
 select current_date, current_timestamp from ttf1;
 
 create temporary view ttf2 as select * from values
   (1, 2),
-  (2, 3),
+  (2, 3)
   as ttf2(a, b);
   
 select current_date = current_date(), current_timestamp = current_timestamp(), a, b from ttf2;
+
+select a, b from ttf2 order by a, current_date
