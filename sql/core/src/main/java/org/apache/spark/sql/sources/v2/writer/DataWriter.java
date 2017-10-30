a462f1846188ba556218c424ca48dc9664142b06
Zhenhua Wang <wzh_zju@163.com>
2017-10-30 10:21:05 -0700

    [SPARK-22400][SQL] rename some APIs and classes to make their meaning clearer
    ## What changes were proposed in this pull request?
    
    Both `ReadSupport` and `ReadTask` have a method called `createReader`, but they create different things. This could cause some confusion for data source developers. The same issue exists between `WriteSupport` and `DataWriterFactory`, both of which have a method called `createWriter`. This PR renames the method of `ReadTask`/`DataWriterFactory` to `createDataReader`/`createDataWriter`.
    
    Besides, the name of `RowToInternalRowDataWriterFactory` is not correct, because it actually converts `InternalRow`s to `Row`s. It should be renamed `InternalRowDataWriterFactory`.
    
    ## How was this patch tested?
    
    Only renaming, should be covered by existing tests.
    
    Author: Zhenhua Wang <wzh_zju@163.com>
    
    Closes #19610 from wzhfy/rename.

--- sql/core/src/main/java/org/apache/spark/sql/sources/v2/writer/DataWriter.java
+++ sql/core/src/main/java/org/apache/spark/sql/sources/v2/writer/DataWriter.java
@@ -20,7 +20,7 @@ package org.apache.spark.sql.sources.v2.writer;
 import org.apache.spark.annotation.InterfaceStability;
 
 /**
- * A data writer returned by {@link DataWriterFactory#createWriter(int, int)} and is
+ * A data writer returned by {@link DataWriterFactory#createDataWriter(int, int)} and is
  * responsible for writing data for an input RDD partition.
  *
  * One Spark task has one exclusive data writer, so there is no thread-safe concern.
@@ -34,7 +34,7 @@ import org.apache.spark.annotation.InterfaceStability;
  * {@link DataSourceV2Writer#commit(WriterCommitMessage[])} with commit messages from other data
  * writers. If this data writer fails(one record fails to write or {@link #commit()} fails), an
  * exception will be sent to the driver side, and Spark will retry this writing task for some times,
- * each time {@link DataWriterFactory#createWriter(int, int)} gets a different `attemptNumber`,
+ * each time {@link DataWriterFactory#createDataWriter(int, int)} gets a different `attemptNumber`,
  * and finally call {@link DataSourceV2Writer#abort(WriterCommitMessage[])} if all retry fail.
  *
  * Besides the retry mechanism, Spark may launch speculative tasks if the existing writing task
@@ -64,7 +64,7 @@ public interface DataWriter<T> {
 
   /**
    * Commits this writer after all records are written successfully, returns a commit message which
-   * will be send back to driver side and pass to
+   * will be sent back to driver side and passed to
    * {@link DataSourceV2Writer#commit(WriterCommitMessage[])}.
    *
    * The written data should only be visible to data source readers after
