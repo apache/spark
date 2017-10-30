2bcc2ea6fd0ca9f12959246bb9ee6796cb7a90a0
donnyzone <wellfengzhu@gmail.com>
2017-10-30 11:08:36 +0800

    2.2-backport

--- sql/core/src/test/resources/sql-tests/inputs/datetime.sql
+++ sql/core/src/test/resources/sql-tests/inputs/datetime.sql
@@ -2,3 +2,20 @@
 
 -- [SPARK-16836] current_date and current_timestamp literals
 select current_date = current_date(), current_timestamp = current_timestamp();
+
+-- [SPARK-22333]: timeFunctionCall has conflicts with columnReference
+create temporary view ttf1 as select * from values
+  (1, 2),
+  (2, 3)
+  as ttf1(current_date, current_timestamp);
+  
+select current_date, current_timestamp from ttf1;
+
+create temporary view ttf2 as select * from values
+  (1, 2),
+  (2, 3)
+  as ttf2(a, b);
+  
+select current_date = current_date(), current_timestamp = current_timestamp(), a, b from ttf2;
+
+select a, b from ttf2 order by a, current_date;
