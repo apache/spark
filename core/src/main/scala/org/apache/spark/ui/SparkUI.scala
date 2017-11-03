9670d6f8b3fed58f556b9050814f7c61bc5d65be
guoxiaolong <guo.xiaolong1@zte.com.cn>
2017-11-03 11:29:37 +0800

    Restore code, do not calculate run time.

--- core/src/main/scala/org/apache/spark/ui/SparkUI.scala
+++ core/src/main/scala/org/apache/spark/ui/SparkUI.scala
@@ -120,7 +120,7 @@ private[spark] class SparkUI private (
         attemptId = None,
         startTime = new Date(startTime),
         endTime = new Date(-1),
-        duration = System.currentTimeMillis() - startTime,
+        duration = 0,
         lastUpdated = new Date(startTime),
         sparkUser = getSparkUser,
         completed = false,
