1ef1f708cdc526484fd2edae3e463a233e750d05
ptkool <michael.styles@shopify.com>
2017-11-06 19:06:10 -0500

    Fix PyDoc test

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -876,7 +876,7 @@ def dayofweek(col):
 
     >>> df = spark.createDataFrame([('2015-04-08',)], ['dt'])
     >>> df.select(dayofweek('dt').alias('day')).collect()
-    [Row(day=8)]
+    [Row(day=4)]
     """
     sc = SparkContext._active_spark_context
     return Column(sc._jvm.functions.dayofweek(_to_java_column(col)))
