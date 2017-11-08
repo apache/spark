ab5280944d2c82debed68c4df1c1684bb0db88c6
hyukjinkwon <gurwls223@gmail.com>
2017-11-08 20:20:33 +0900

    classloader -> class loader

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -1811,7 +1811,7 @@ class SparkContext(config: SparkConf) extends Logging {
    *
    * @param path can be either a local file, a file in HDFS (or other Hadoop-supported filesystems),
    * an HTTP, HTTPS or FTP URI, or local:/path for a file on every worker node.
-   * @param addToCurrentClassLoader if true will add the jar to the current threads' classloader.
+   * @param addToCurrentClassLoader if true will add the jar to the current threads' class loader.
    * In general adding to the current threads' class loader will impact all other application
    * threads unless they have explicitly changed their class loader.
    */
