11b60af737a04d931356aa74ebf3c6cf4a6b08d6
Zhenhua Wang <wangzhenhua@huawei.com>
2017-11-14 16:41:43 +0100

    [SPARK-17074][SQL] Generate equi-height histogram in column statistics
    ## What changes were proposed in this pull request?
    
    Equi-height histogram is effective in cardinality estimation, and more accurate than basic column stats (min, max, ndv, etc) especially in skew distribution. So we need to support it.
    
    For equi-height histogram, all buckets (intervals) have the same height (frequency).
    In this PR, we use a two-step method to generate an equi-height histogram:
    1. use `ApproximatePercentile` to get percentiles `p(0), p(1/n), p(2/n) ... p((n-1)/n), p(1)`;
    2. construct range values of buckets, e.g. `[p(0), p(1/n)], [p(1/n), p(2/n)] ... [p((n-1)/n), p(1)]`, and use `ApproxCountDistinctForIntervals` to count ndv in each bucket. Each bucket is of the form: `(lowerBound, higherBound, ndv)`.
    
    ## How was this patch tested?
    
    Added new test cases and modified some existing test cases.
    
    Author: Zhenhua Wang <wangzhenhua@huawei.com>
    Author: Zhenhua Wang <wzh_zju@163.com>
    
    Closes #19479 from wzhfy/generate_histogram.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -31,7 +31,6 @@ import org.apache.spark.internal.config._
 import org.apache.spark.network.util.ByteUnit
 import org.apache.spark.sql.catalyst.analysis.Resolver
 import org.apache.spark.sql.catalyst.expressions.codegen.CodeGenerator
-import org.apache.spark.util.collection.unsafe.sort.UnsafeExternalSorter
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // This file defines the configuration options for Spark SQL.
@@ -835,6 +834,33 @@ object SQLConf {
       .doubleConf
       .createWithDefault(0.05)
 
+  val HISTOGRAM_ENABLED =
+    buildConf("spark.sql.statistics.histogram.enabled")
+      .doc("Generates histograms when computing column statistics if enabled. Histograms can " +
+        "provide better estimation accuracy. Currently, Spark only supports equi-height " +
+        "histogram. Note that collecting histograms takes extra cost. For example, collecting " +
+        "column statistics usually takes only one table scan, but generating equi-height " +
+        "histogram will cause an extra table scan.")
+      .booleanConf
+      .createWithDefault(false)
+
+  val HISTOGRAM_NUM_BINS =
+    buildConf("spark.sql.statistics.histogram.numBins")
+      .internal()
+      .doc("The number of bins when generating histograms.")
+      .intConf
+      .checkValue(num => num > 1, "The number of bins must be large than 1.")
+      .createWithDefault(254)
+
+  val PERCENTILE_ACCURACY =
+    buildConf("spark.sql.statistics.percentile.accuracy")
+      .internal()
+      .doc("Accuracy of percentile approximation when generating equi-height histograms. " +
+        "Larger value means better accuracy. The relative error can be deduced by " +
+        "1.0 / PERCENTILE_ACCURACY.")
+      .intConf
+      .createWithDefault(10000)
+
   val AUTO_SIZE_UPDATE_ENABLED =
     buildConf("spark.sql.statistics.size.autoUpdate.enabled")
       .doc("Enables automatic update for table size once table's data is changed. Note that if " +
@@ -1240,6 +1266,12 @@ class SQLConf extends Serializable with Logging {
   def sessionLocalTimeZone: String = getConf(SQLConf.SESSION_LOCAL_TIMEZONE)
 
   def ndvMaxError: Double = getConf(NDV_MAX_ERROR)
+
+  def histogramEnabled: Boolean = getConf(HISTOGRAM_ENABLED)
+
+  def histogramNumBins: Int = getConf(HISTOGRAM_NUM_BINS)
+
+  def percentileAccuracy: Int = getConf(PERCENTILE_ACCURACY)
 
   def cboEnabled: Boolean = getConf(SQLConf.CBO_ENABLED)
 
