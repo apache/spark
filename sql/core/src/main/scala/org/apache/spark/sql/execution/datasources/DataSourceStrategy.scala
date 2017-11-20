e5407902c92b3936395ae1d38eba8bf20dfcbb43
Jia Li <jiali@us.ibm.com>
2017-11-19 23:20:00 -0800

    address comment to add PR discussion number for reference

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
@@ -497,6 +497,7 @@ object DataSourceStrategy {
         Some(sources.IsNotNull(a.name))
 
       case expressions.And(left, right) =>
+        // See SPARK-12218 and PR 10362 for detailed discussion
         for {
           leftFilter <- translateFilter(left)
           rightFilter <- translateFilter(right)
