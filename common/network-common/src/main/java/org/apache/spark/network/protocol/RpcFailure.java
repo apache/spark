6991297a2ec66dfee4fe3bc1b5ae049053d30c38
Jirka Kremser <jkremser@redhat.com>
2017-11-23 17:49:55 +0100

    [SPARK-22594][CORE] Handling spark-submit and master version mismatch

--- common/network-common/src/main/java/org/apache/spark/network/protocol/RpcFailure.java
+++ common/network-common/src/main/java/org/apache/spark/network/protocol/RpcFailure.java
@@ -24,6 +24,7 @@ import io.netty.buffer.ByteBuf;
 public final class RpcFailure extends AbstractMessage implements ResponseMessage {
   public final long requestId;
   public final String errorString;
+  public static final long EMPTY_REQUEST_ID = Long.MIN_VALUE;
 
   public RpcFailure(long requestId, String errorString) {
     this.requestId = requestId;
