80c8da977e339defb88faf4114027347e5c67548
yucai <yucai.yu@intel.com>
2017-11-24 20:46:31 +0800

    Remove unnecessary logDebug

--- core/src/main/scala/org/apache/spark/shuffle/IndexShuffleBlockResolver.scala
+++ core/src/main/scala/org/apache/spark/shuffle/IndexShuffleBlockResolver.scala
@@ -194,7 +194,6 @@ private[spark] class IndexShuffleBlockResolver(
   override def getBlockData(blockId: ShuffleBlockId): ManagedBuffer = {
     // The block is actually going to be a range of a single map output file for this map, so
     // find out the consolidated file, then the offset within that from our index
-    logDebug(s"Fetch block data for $blockId")
     val indexFile = getIndexFile(blockId.shuffleId, blockId.mapId)
 
     // SPARK-22982: if this FileInputStream's position is seeked forward by another piece of code
