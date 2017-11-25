a0b6658a65d98158a8bf610a57edb240b99c8839
Sean Owen <sowen@cloudera.com>
2017-11-25 12:25:29 -0600

    Set -ea and -Xss4m consistently for tests

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -789,7 +789,7 @@ object TestSettings {
     javaOptions in Test ++= System.getProperties.asScala.filter(_._1.startsWith("spark"))
       .map { case (k,v) => s"-D$k=$v" }.toSeq,
     javaOptions in Test += "-ea",
-    javaOptions in Test ++= "-Xmx3g -Xss4096k"
+    javaOptions in Test ++= "-Xmx3g -Xss4m"
       .split(" ").toSeq,
     javaOptions += "-Xmx3g",
     // Exclude tags defined in a system property
