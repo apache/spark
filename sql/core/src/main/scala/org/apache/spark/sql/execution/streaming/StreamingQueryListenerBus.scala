60035fa865fd85cc7e9441d2dc55d46693b16dee
Shixiong Zhu <zsxwing@gmail.com>
2017-11-28 14:03:50 -0800

    Use a separate query for StreamingQueryListenerBus

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamingQueryListenerBus.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamingQueryListenerBus.scala
@@ -40,7 +40,7 @@ class StreamingQueryListenerBus(sparkListenerBus: LiveListenerBus)
 
   import StreamingQueryListener._
 
-  sparkListenerBus.addToSharedQueue(this)
+  sparkListenerBus.addToQueue(this, "streams")
 
   /**
    * RunIds of active queries whose events are supposed to be forwarded by this ListenerBus
