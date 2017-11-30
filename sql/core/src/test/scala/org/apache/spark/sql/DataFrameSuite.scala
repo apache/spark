f6cd38873e06380faf4dfb771b3839c68278996c
Adrian Ionescu <adrian@databricks.com>
2017-11-30 12:40:53 +0100

    Revert "handle empty partition-by expr list as RoundRobin"
    This reverts commit 66b192d

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -356,10 +356,6 @@ class DataFrameSuite extends QueryTest with SharedSQLContext {
     checkAnswer(
       testData.select('key).repartition(10).select('key),
       testData.select('key).collect().toSeq)
-
-    checkAnswer(
-      testData.select('key).repartition(10).select(spark_partition_id(), 'key),
-      testData.select('key).repartition(10, Seq.empty: _*).select(spark_partition_id(), 'key))
   }
 
   test("repartition with SortOrder") {
@@ -398,13 +394,6 @@ class DataFrameSuite extends QueryTest with SharedSQLContext {
       data2d.toDF("a", "b").repartitionByRange(data2d.size, $"a".desc, $"b")
         .select(spark_partition_id().as("id"), $"a", $"b"),
       data2d.toDF("a", "b").repartitionByRange(data2d.size, $"a".desc, $"b".asc)
-        .select(spark_partition_id().as("id"), $"a", $"b"))
-
-    // .repartitionByRange() with no partition-by expressions behaves the same as .repartition()
-    checkAnswer(
-      data2d.toDF("a", "b").repartitionByRange(data2d.size, Seq.empty: _*)
-        .select(spark_partition_id().as("id"), $"a", $"b"),
-      data2d.toDF("a", "b").repartition(data2d.size)
         .select(spark_partition_id().as("id"), $"a", $"b"))
   }
 
