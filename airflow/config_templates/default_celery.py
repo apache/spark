bdafb12f8dd9e6fcf508b0df869f9cc45bb9593d
Bolke de Bruin <bolke@xs4all.nl>
2017-12-05 10:14:50 +0100

    [AIRFLOW-342] Do not use amqp, rpc as result backend
    amqp and rpc (and redis most likely) cannot store
    results for tasks
    long enough.
    
    Closes #2830 from bolkedebruin/AIRFLOW-342

--- airflow/config_templates/default_celery.py
+++ airflow/config_templates/default_celery.py
@@ -18,6 +18,7 @@ from airflow.exceptions import AirflowConfigException, AirflowException
 from airflow import configuration
 from airflow.utils.log.logging_mixin import LoggingMixin
 
+log = LoggingMixin().log
 
 broker_transport_options = configuration.getsection('celery_broker_transport_options')
 if broker_transport_options is None:
@@ -41,7 +42,6 @@ celery_ssl_active = False
 try:
     celery_ssl_active = configuration.getboolean('celery', 'CELERY_SSL_ACTIVE')
 except AirflowConfigException as e:
-    log = LoggingMixin().log
     log.warning("Celery Executor will run without SSL")
 
 try:
@@ -60,3 +60,7 @@ except Exception as e:
                            'Please ensure you want to use '
                            'SSL and/or have all necessary certs and key ({}).'.format(e))
 
+result_backend = DEFAULT_CELERY_CONFIG['result_backend']
+if 'amqp' in result_backend or 'redis' in result_backend or 'rpc' in result_backend:
+    log.warning("You have configured a result_backend of %s, it is highly recommended "
+                "to use an alternative result_backend (i.e. a database).", result_backend)
