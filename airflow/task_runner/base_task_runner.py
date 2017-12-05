a9ceca5e04d3bdfbd34adc4ef5bf95c5cbf5c954
William Pursell <williamp@wepay.com>
2017-12-05 11:22:50 -0800

    [AIRFLOW-1876] Write subtask id to task log header
    Closes #2835 from wrp/subtask-id

--- airflow/task_runner/base_task_runner.py
+++ airflow/task_runner/base_task_runner.py
@@ -97,7 +97,9 @@ class BaseTaskRunner(LoggingMixin):
                 line = line.decode('utf-8')
             if len(line) == 0:
                 break
-            self.log.info(u'Subtask: %s', line.rstrip('\n'))
+            self.log.info(u'Job {}: Subtask {} %s'.format(
+                self._task_instance.job_id, self._task_instance.task_id),
+                line.rstrip('\n'))
 
     def run_command(self, run_with, join_args=False):
         """
