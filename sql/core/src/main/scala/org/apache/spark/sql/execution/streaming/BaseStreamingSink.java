633804387baa128ddd261bceda934a22745126e8
Jose Torres <jose@databricks.com>
2017-12-06 13:39:20 -0800

    note interfaces are temporary

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/BaseStreamingSink.java
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/BaseStreamingSink.java
@@ -2,6 +2,9 @@ package org.apache.spark.sql.execution.streaming;
 
 /**
  * The shared interface between V1 and V2 streaming sinks.
+ *
+ * This is a temporary interface for compatibility during migration. It should not be implemented
+ * directly, and will be removed in future versions.
  */
 public interface BaseStreamingSink {
 }
--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/BaseStreamingSource.java
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/BaseStreamingSource.java
@@ -4,6 +4,9 @@ import org.apache.spark.sql.sources.v2.reader.Offset;
 
 /**
  * The shared interface between V1 streaming sources and V2 streaming readers.
+ *
+ * This is a temporary interface for compatibility during migration. It should not be implemented
+ * directly, and will be removed in future versions.
  */
 public interface BaseStreamingSource {
   /**
