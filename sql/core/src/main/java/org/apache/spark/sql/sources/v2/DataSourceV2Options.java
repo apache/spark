2be448260d8115969e3d89b913c733943f151915
Sunitha Kambhampati <skambha@us.ibm.com>
2017-12-07 20:59:47 +0800

    [SPARK-22452][SQL] Add getInt, getLong, getBoolean to DataSourceV2Options
    - Implemented methods getInt, getLong, getBoolean for DataSourceV2Options
    - Added new unit tests to exercise these methods
    
    Author: Sunitha Kambhampati <skambha@us.ibm.com>
    
    Closes #19902 from skambha/spark22452.

--- sql/core/src/main/java/org/apache/spark/sql/sources/v2/DataSourceV2Options.java
+++ sql/core/src/main/java/org/apache/spark/sql/sources/v2/DataSourceV2Options.java
@@ -49,4 +49,35 @@ public class DataSourceV2Options {
   public Optional<String> get(String key) {
     return Optional.ofNullable(keyLowerCasedMap.get(toLowerCase(key)));
   }
+
+  /**
+   * Returns the boolean value to which the specified key is mapped,
+   * or defaultValue if there is no mapping for the key. The key match is case-insensitive
+   */
+  public boolean getBoolean(String key, boolean defaultValue) {
+    String lcaseKey = toLowerCase(key);
+    return keyLowerCasedMap.containsKey(lcaseKey) ?
+      Boolean.parseBoolean(keyLowerCasedMap.get(lcaseKey)) : defaultValue;
+  }
+
+  /**
+   * Returns the integer value to which the specified key is mapped,
+   * or defaultValue if there is no mapping for the key. The key match is case-insensitive
+   */
+  public int getInt(String key, int defaultValue) {
+    String lcaseKey = toLowerCase(key);
+    return keyLowerCasedMap.containsKey(lcaseKey) ?
+      Integer.parseInt(keyLowerCasedMap.get(lcaseKey)) : defaultValue;
+  }
+
+  /**
+   * Returns the long value to which the specified key is mapped,
+   * or defaultValue if there is no mapping for the key. The key match is case-insensitive
+   */
+  public long getLong(String key, long defaultValue) {
+    String lcaseKey = toLowerCase(key);
+    return keyLowerCasedMap.containsKey(lcaseKey) ?
+      Long.parseLong(keyLowerCasedMap.get(lcaseKey)) : defaultValue;
+  }
+
 }
