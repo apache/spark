7c46b333f314c032caa5a6b838c91f2027e2ab93
Jose Torres <jose@databricks.com>
2017-12-11 12:49:21 -0800

    clarify start offset

--- sql/core/src/main/java/org/apache/spark/sql/sources/v2/reader/ContinuousDataReader.java
+++ sql/core/src/main/java/org/apache/spark/sql/sources/v2/reader/ContinuousDataReader.java
@@ -26,7 +26,7 @@ import java.io.IOException;
  */
 public interface ContinuousDataReader<T> extends DataReader<T> {
     /**
-     * Get the offset of the current record.
+     * Get the offset of the current record, or the start offset if no records have been read.
      *
      * The execution engine will call this method along with get() to keep track of the current
      * offset. When an epoch ends, the offset of the previous record in each partition will be saved
