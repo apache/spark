f36706ae46f4b0562b1d85627a526fcf642e913c
Peng Meng <peng.meng@intel.com>
2017-12-12 11:30:21 +0800

    reset code

--- core/src/main/scala/org/apache/spark/util/BoundedPriorityQueue.scala
+++ core/src/main/scala/org/apache/spark/util/BoundedPriorityQueue.scala
@@ -51,10 +51,6 @@ private[spark] class BoundedPriorityQueue[A](maxSize: Int)(implicit ord: Orderin
     this
   }
 
-  def poll(): A = {
-    underlying.poll()
-  }
-
   override def +=(elem1: A, elem2: A, elems: A*): this.type = {
     this += elem1 += elem2 ++= elems
   }
