0e36ba6212bc24b3185e385914fbf2d62cbfb6da
WeichenXu <weichen.xu@databricks.com>
2017-12-12 21:28:24 -0800

    [SPARK-22644][ML][TEST] Make ML testsuite support StructuredStreaming test
    ## What changes were proposed in this pull request?
    
    We need to add some helper code to make testing ML transformers & models easier with streaming data. These tests might help us catch any remaining issues and we could encourage future PRs to use these tests to prevent new Models & Transformers from having issues.
    
    I add a `MLTest` trait which extends `StreamTest` trait, and override `createSparkSession`. So ML testsuite can only extend `MLTest`, to use both ML & Stream test util functions.
    
    I only modify one testcase in `LinearRegressionSuite`, for first pass review.
    
    Link to #19746
    
    ## How was this patch tested?
    
    `MLTestSuite` added.
    
    Author: WeichenXu <weichen.xu@databricks.com>
    
    Closes #19843 from WeichenXu123/ml_stream_test_helper.

--- sql/core/src/test/scala/org/apache/spark/sql/test/TestSQLContext.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/test/TestSQLContext.scala
@@ -24,7 +24,7 @@ import org.apache.spark.sql.internal.{SessionState, SessionStateBuilder, SQLConf
 /**
  * A special `SparkSession` prepared for testing.
  */
-private[sql] class TestSparkSession(sc: SparkContext) extends SparkSession(sc) { self =>
+private[spark] class TestSparkSession(sc: SparkContext) extends SparkSession(sc) { self =>
   def this(sparkConf: SparkConf) {
     this(new SparkContext("local[2]", "test-sql-context",
       sparkConf.set("spark.sql.testkey", "true")))
