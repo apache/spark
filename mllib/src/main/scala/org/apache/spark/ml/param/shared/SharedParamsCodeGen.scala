f5ade2af4ff10954dc5db70fa4fef1dc16b8af71
Yanbo Liang <ybliang8@gmail.com>
2017-12-12 17:37:01 -0800

    [SPARK-21087][ML][FOLLOWUP] Sync SharedParamsCodeGen and sharedParams.
    ## What changes were proposed in this pull request?
    #19208 modified ```sharedParams.scala```, but didn't generated by ```SharedParamsCodeGen.scala```. This involves mismatch between them.
    
    ## How was this patch tested?
    Existing test.
    
    Author: Yanbo Liang <ybliang8@gmail.com>
    
    Closes #19958 from yanboliang/spark-21087.

--- mllib/src/main/scala/org/apache/spark/ml/param/shared/SharedParamsCodeGen.scala
+++ mllib/src/main/scala/org/apache/spark/ml/param/shared/SharedParamsCodeGen.scala
@@ -84,10 +84,10 @@ private[shared] object SharedParamsCodeGen {
       ParamDesc[String]("solver", "the solver algorithm for optimization", finalFields = false),
       ParamDesc[Int]("aggregationDepth", "suggested depth for treeAggregate (>= 2)", Some("2"),
         isValid = "ParamValidators.gtEq(2)", isExpertParam = true),
-      ParamDesc[Boolean]("collectSubModels", "If set to false, then only the single best " +
-        "sub-model will be available after fitting. If set to true, then all sub-models will be " +
-        "available. Warning: For large models, collecting all sub-models can cause OOMs on the " +
-        "Spark driver.",
+      ParamDesc[Boolean]("collectSubModels", "whether to collect a list of sub-models trained " +
+        "during tuning. If set to false, then only the single best sub-model will be available " +
+        "after fitting. If set to true, then all sub-models will be available. Warning: For " +
+        "large models, collecting all sub-models can cause OOMs on the Spark driver",
         Some("false"), isExpertParam = true)
     )
 
