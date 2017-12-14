a5f1957f7501038f72fe539f06fca7d494c1dfec
gatorsmile <gatorsmile@gmail.com>
2017-12-14 12:08:01 -0800

    fix.

--- sql/core/src/test/scala/org/apache/spark/sql/TPCHQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/TPCHQuerySuite.scala
@@ -57,9 +57,9 @@ class TPCHQuerySuite extends QueryTest with SharedSQLContext with BeforeAndAfter
     sql(
       """
         |CREATE TABLE `orders` (
-        |`o_orderkey` BIGINT, `o_custkey` BIGINT, `o_orderstatus` STRING, `o_totalprice` DECIMAL(10,0),
-        |`o_orderdate` DATE, `o_orderpriority` STRING, `o_clerk` STRING, `o_shippriority` INT,
-        |`o_comment` STRING)
+        |`o_orderkey` BIGINT, `o_custkey` BIGINT, `o_orderstatus` STRING,
+        |`o_totalprice` DECIMAL(10,0), `o_orderdate` DATE, `o_orderpriority` STRING,
+        |`o_clerk` STRING, `o_shippriority` INT, `o_comment` STRING)
         |USING parquet
       """.stripMargin)
 
