9680a4ed2613d2d7ef02afb52030cd90f75deb88
soonmok-kwon <soonmok.kwon@navercorp.com>
2017-12-19 15:52:43 +0900

    use case

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/csv/CSVSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/csv/CSVSuite.scala
@@ -484,7 +484,6 @@ class CSVSuite extends QueryTest with SharedSQLContext with SQLTestUtils {
 
   test("save csv with quote escaping, using charToEscapeQuoteEscaping option") {
     withTempPath { path =>
-      
       // original text
       val df1 = Seq(
         """You are "beautiful"""",
