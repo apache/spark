334e0dc2d97cda90c2d1060bc028ee58af347fb6
Jose Torres <jose@databricks.com>
2017-12-20 10:43:10 -0800

    [SPARK-22824] Restore old offset for binary compatibility
    ## What changes were proposed in this pull request?
    
    Some users depend on source compatibility with the org.apache.spark.sql.execution.streaming.Offset class. Although this is not a stable interface, we can keep it in place for now to simplify upgrades to 2.3.
    
    Author: Jose Torres <jose@databricks.com>
    
    Closes #20012 from joseph-torres/binary-compat.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/Source.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/Source.scala
@@ -18,7 +18,6 @@
 package org.apache.spark.sql.execution.streaming
 
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.sources.v2.reader.Offset
 import org.apache.spark.sql.types.StructType
 
 /**
