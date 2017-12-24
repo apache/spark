8186a34178b108f71bea4f7b21080a2b527b445e
chetkhatri <ckhatrimanjal@gmail.com>
2017-12-24 16:55:56 +0530

    SPARK-22896 Improvement in String interpolation

--- examples/src/main/scala/org/apache/spark/examples/graphx/Analytics.scala
+++ examples/src/main/scala/org/apache/spark/examples/graphx/Analytics.scala
@@ -145,9 +145,9 @@ object Analytics extends Logging {
           // TriangleCount requires the graph to be partitioned
           .partitionBy(partitionStrategy.getOrElse(RandomVertexCut)).cache()
         val triangles = TriangleCount.run(graph)
-        println("Triangles: " + triangles.vertices.map {
+        println(s"Triangles: ${triangles.vertices.map {
           case (vid, data) => data.toLong
-        }.reduce(_ + _) / 3)
+        }.reduce(_ + _) / 3}")
         sc.stop()
 
       case _ =>
