74d41d7070a0f6c0e11e8a081785ffbc95d86dba
chetkhatri <ckhatrimanjal@gmail.com>
2017-12-26 22:31:08 +0530

    graphx changed got merged to mllib example

--- examples/src/main/scala/org/apache/spark/examples/mllib/AssociationRulesExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/mllib/AssociationRulesExample.scala
@@ -42,9 +42,8 @@ object AssociationRulesExample {
     val results = ar.run(freqItemsets)
 
     results.collect().foreach { rule =>
-      println("[" + rule.antecedent.mkString(",")
-        + "=>"
-        + rule.consequent.mkString(",") + "]," + rule.confidence)
+    println(s"[${rule.antecedent.mkString(",")}=>${rule.consequent.mkString(",")} ]" +
+        s" ${rule.confidence}")
     }
     // $example off$
 
