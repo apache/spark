5e4d7d8d7da250cacc15d8a2247098f0b42a3def
Bolke de Bruin <bolke@xs4all.nl>
2017-12-29 12:28:51 +0100

    [AIRFLOW-XXX] Pin sqlalchemy dependency

--- setup.py
+++ setup.py
@@ -226,7 +226,7 @@ def do_setup():
             'python-nvd3==0.14.2',
             'requests>=2.5.1, <3',
             'setproctitle>=1.1.8, <2',
-            'sqlalchemy>=0.9.8',
+            'sqlalchemy>=1.1.15, <1.2.0',
             'sqlalchemy-utc>=0.9.0',
             'tabulate>=0.7.5, <0.8.0',
             'thrift>=0.9.2',
