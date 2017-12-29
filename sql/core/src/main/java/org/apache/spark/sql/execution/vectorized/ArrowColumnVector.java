d2c5c2b4ea803ac8d1f08a5f79af1076f9e5bd2b
Bryan Cutler <cutlerb@gmail.com>
2017-12-28 22:04:19 -0800

    fix import order

--- sql/core/src/main/java/org/apache/spark/sql/execution/vectorized/ArrowColumnVector.java
+++ sql/core/src/main/java/org/apache/spark/sql/execution/vectorized/ArrowColumnVector.java
@@ -18,13 +18,14 @@
 package org.apache.spark.sql.execution.vectorized;
 
 import com.google.common.collect.ImmutableList;
+
 import io.netty.buffer.ArrowBuf;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.vector.*;
 import org.apache.arrow.vector.complex.*;
 import org.apache.arrow.vector.holders.NullableVarCharHolder;
-
 import org.apache.arrow.vector.ipc.message.ArrowFieldNode;
+
 import org.apache.spark.sql.execution.arrow.ArrowUtils;
 import org.apache.spark.sql.types.*;
 import org.apache.spark.unsafe.types.UTF8String;
