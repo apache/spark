3c08f3d6b7ec58735260de687bb74b104e6f7009
gatorsmile <gatorsmile@gmail.com>
2018-01-07 08:41:22 +0800

    fix

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -3619,15 +3619,15 @@ class VectorizedUDFTests(ReusedSQLTestCase):
     def test_register_vectorized_udf_basic(self):
         from pyspark.sql.functions import pandas_udf
         from pyspark.rdd import PythonEvalType
-        twoArgsPandasUDF = pandas_udf(lambda x, y: len(x) + y, StringType())
+        twoArgsPandasUDF = pandas_udf(lambda x: len(x), IntegerType())
         self.assertEqual(twoArgsPandasUDF.deterministic, True)
         self.assertEqual(twoArgsPandasUDF.evalType, PythonEvalType.SQL_PANDAS_SCALAR_UDF)
         newPandasUDF = self.spark.catalog.registerFunction(
             "twoArgsPandasUDF", twoArgsPandasUDF, IntegerType())
         self.assertEqual(newPandasUDF.deterministic, True)
         self.assertEqual(newPandasUDF.evalType, PythonEvalType.SQL_PANDAS_SCALAR_UDF)
-        [row] = self.spark.sql("SELECT twoArgsPandasUDF('test', 1)").collect()
-        self.assertEqual(row[0], 5)
+        [row] = self.spark.sql("SELECT twoArgsPandasUDF('test')").collect()
+        self.assertEqual(row[0], 4)
 
     def test_register_nondeterministic_vectorized_udf_basic(self):
         from pyspark.sql.functions import pandas_udf
