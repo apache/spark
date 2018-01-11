d2cfed308d343fb55c5fd7c0d30bcbb987948632
hyukjinkwon <gurwls223@gmail.com>
2018-01-12 00:31:05 +0900

    Clarify the length of each series is of each batch within scalar Pandas UDF

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -2184,6 +2184,11 @@ def pandas_udf(f=None, returnType=None, functionType=None):
        |         8|      JOHN DOE|          22|
        +----------+--------------+------------+
 
+       .. note:: The length of `pandas.Series` within a scalar UDF is not of the whole input column
+           but of the batch internally used, and it is called for each batch. Therefore,
+           this can be used, for example, to ensure the length of each returned `pandas.Series`
+           but should not be used as the length of the whole input.
+
     2. GROUP_MAP
 
        A group map UDF defines transformation: A `pandas.DataFrame` -> A `pandas.DataFrame`
