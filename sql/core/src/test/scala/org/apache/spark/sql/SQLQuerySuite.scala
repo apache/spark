489e0689046b9bc31e9f89d0b6f3990bd282288c
Dongjoon Hyun <dongjoon@apache.org>
2018-01-17 14:32:18 +0800

    [SPARK-23072][SQL][TEST] Add a Unicode schema test for file-based data sources
    ## What changes were proposed in this pull request?
    
    After [SPARK-20682](https://github.com/apache/spark/pull/19651), Apache Spark 2.3 is able to read ORC files with Unicode schema. Previously, it raises `org.apache.spark.sql.catalyst.parser.ParseException`.
    
    This PR adds a Unicode schema test for CSV/JSON/ORC/Parquet file-based data sources. Note that TEXT data source only has [a single column with a fixed name 'value'](https://github.com/apache/spark/blob/master/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/text/TextFileFormat.scala#L71).
    
    ## How was this patch tested?
    
    Pass the newly added test case.
    
    Author: Dongjoon Hyun <dongjoon@apache.org>
    
    Closes #20266 from dongjoon-hyun/SPARK-23072.

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -2757,20 +2757,4 @@ class SQLQuerySuite extends QueryTest with SharedSQLContext {
       }
     }
   }
-
-  // Only New OrcFileFormat supports this
-  Seq(classOf[org.apache.spark.sql.execution.datasources.orc.OrcFileFormat].getCanonicalName,
-      "parquet").foreach { format =>
-    test(s"SPARK-15474 Write and read back non-emtpy schema with empty dataframe - $format") {
-      withTempPath { file =>
-        val path = file.getCanonicalPath
-        val emptyDf = Seq((true, 1, "str")).toDF.limit(0)
-        emptyDf.write.format(format).save(path)
-
-        val df = spark.read.format(format).load(path)
-        assert(df.schema.sameType(emptyDf.schema))
-        checkAnswer(df, emptyDf)
-      }
-    }
-  }
 }
