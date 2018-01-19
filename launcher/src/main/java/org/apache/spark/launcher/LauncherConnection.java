95bac2773ee7adab9f57aa4377ff2e998353f02f
Marcelo Vanzin <vanzin@cloudera.com>
2018-01-19 10:19:25 -0800

    Feedback.

--- launcher/src/main/java/org/apache/spark/launcher/LauncherConnection.java
+++ launcher/src/main/java/org/apache/spark/launcher/LauncherConnection.java
@@ -53,7 +53,7 @@ abstract class LauncherConnection implements Closeable, Runnable {
   public void run() {
     try {
       FilteredObjectInputStream in = new FilteredObjectInputStream(socket.getInputStream());
-      while (!closed) {
+      while (isOpen()) {
         Message msg = (Message) in.readObject();
         handle(msg);
       }
@@ -96,7 +96,7 @@ abstract class LauncherConnection implements Closeable, Runnable {
 
   @Override
   public synchronized void close() throws IOException {
-    if (!closed) {
+    if (isOpen()) {
       closed = true;
       socket.close();
     }
