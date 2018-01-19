54c1fae12df654c7713ac5e7eb4da7bb2f785401
Sameer Agarwal <sameerag@apache.org>
2018-01-19 01:38:08 -0800

    [BUILD][MINOR] Fix java style check issues
    ## What changes were proposed in this pull request?
    
    This patch fixes a few recently introduced java style check errors in master and release branch.
    
    As an aside, given that [java linting currently fails](https://github.com/apache/spark/pull/10763
    ) on machines with a clean maven cache, it'd be great to find another workaround to [re-enable the java style checks](https://github.com/apache/spark/blob/3a07eff5af601511e97a05e6fea0e3d48f74c4f0/dev/run-tests.py#L577) as part of Spark PRB.
    
    /cc zsxwing JoshRosen srowen for any suggestions
    
    ## How was this patch tested?
    
    Manual Check
    
    Author: Sameer Agarwal <sameerag@apache.org>
    
    Closes #20323 from sameeragarwal/java.
    
    (cherry picked from commit 9c4b99861cda3f9ec44ca8c1adc81a293508190c)
    Signed-off-by: Sameer Agarwal <sameerag@apache.org>

--- sql/core/src/main/java/org/apache/spark/sql/vectorized/ArrowColumnVector.java
+++ sql/core/src/main/java/org/apache/spark/sql/vectorized/ArrowColumnVector.java
@@ -556,8 +556,9 @@ public final class ArrowColumnVector extends ColumnVector {
   /**
    * Any call to "get" method will throw UnsupportedOperationException.
    *
-   * Access struct values in a ArrowColumnVector doesn't use this accessor. Instead, it uses getStruct() method defined
-   * in the parent class. Any call to "get" method in this class is a bug in the code.
+   * Access struct values in a ArrowColumnVector doesn't use this accessor. Instead, it uses
+   * getStruct() method defined in the parent class. Any call to "get" method in this class is a
+   * bug in the code.
    *
    */
   private static class StructAccessor extends ArrowVectorAccessor {
