dfd828b718bcda85d2b822c8a96ae593c14850e8
Takuya UESHIN <ueshin@databricks.com>
2018-01-24 13:07:23 +0900

    Fix Java style check issues.

--- core/src/test/java/org/apache/spark/launcher/SparkLauncherSuite.java
+++ core/src/test/java/org/apache/spark/launcher/SparkLauncherSuite.java
@@ -24,7 +24,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
