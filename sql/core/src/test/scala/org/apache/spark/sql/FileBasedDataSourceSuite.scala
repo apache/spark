51b0db5272858995a3b3e389a851f4e4aa04c651
Henry Robinson <henry@cloudera.com>
2018-01-23 21:39:20 -0800

    Test changes

--- sql/core/src/test/scala/org/apache/spark/sql/FileBasedDataSourceSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/FileBasedDataSourceSuite.scala
@@ -23,7 +23,7 @@ class FileBasedDataSourceSuite extends QueryTest with SharedSQLContext {
   import testImplicits._
 
   private val allFileBasedDataSources = Seq("orc", "parquet", "csv", "json", "text")
-  private val nameWithSpecialChars = s"sp&cial%c hars"
+  private val nameWithSpecialChars = "sp&cial%c hars"
 
   allFileBasedDataSources.foreach { format =>
     test(s"Writing empty datasets should not fail - $format") {
@@ -68,7 +68,7 @@ class FileBasedDataSourceSuite extends QueryTest with SharedSQLContext {
     }
   }
 
-  Seq("orc", "parquet").foreach { format =>
+  allFileBasedDataSources.foreach { format =>
     test(s"SPARK-22146 read files containing special characters using $format") {
       withTempDir { dir =>
         val tmpFile = s"$dir/$nameWithSpecialChars"
@@ -79,18 +79,16 @@ class FileBasedDataSourceSuite extends QueryTest with SharedSQLContext {
     }
   }
 
-  // Separate test case for text-based formats that support multiLine as an option.
-  Seq("json", "csv", "text").foreach { format =>
-    Seq(true, false).foreach { multiline =>
-      test("SPARK-22146 / SPARK-23148 read files containing special characters " +
-        s"using $format multiline: $multiline") {
-        withTempDir { dir =>
-          val tmpFile = s"$dir/$nameWithSpecialChars"
-          spark.createDataset(Seq("a", "b")).write.format(format).save(tmpFile)
-          val reader = spark.read.format(format).option("multiLine", multiline)
-          val fileContent = reader.load(tmpFile)
-          checkAnswer(fileContent, Seq(Row("a"), Row("b")))
-        }
+  // Separate test case for formats that support multiLine as an option.
+  Seq("json", "csv").foreach { format =>
+    test("SPARK-23148 read files containing special characters " +
+      s"using $format with multiline enabled") {
+      withTempDir { dir =>
+        val tmpFile = s"$dir/$nameWithSpecialChars"
+        spark.createDataset(Seq("a", "b")).write.format(format).save(tmpFile)
+        val reader = spark.read.format(format).option("multiLine", true)
+        val fileContent = reader.load(tmpFile)
+        checkAnswer(fileContent, Seq(Row("a"), Row("b")))
       }
     }
   }
