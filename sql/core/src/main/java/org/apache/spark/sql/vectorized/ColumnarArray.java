82e74445ad11c12402804ff083615e2e615fa5da
Takuya UESHIN <ueshin@databricks.com>
2018-02-01 21:25:02 +0900

    [SPARK-23280][SQL][FOLLOWUP] Fix Java style check issues.
    ## What changes were proposed in this pull request?
    
    This is a follow-up of #20450 which broke lint-java checks.
    This pr fixes the lint-java issues.
    
    ```
    [ERROR] src/main/java/org/apache/spark/sql/vectorized/ColumnVector.java:[20,8] (imports) UnusedImports: Unused import - org.apache.spark.sql.catalyst.util.MapData.
    [ERROR] src/main/java/org/apache/spark/sql/vectorized/ColumnarArray.java:[21,8] (imports) UnusedImports: Unused import - org.apache.spark.sql.catalyst.util.MapData.
    [ERROR] src/main/java/org/apache/spark/sql/vectorized/ColumnarRow.java:[22,8] (imports) UnusedImports: Unused import - org.apache.spark.sql.catalyst.util.MapData.
    ```
    
    ## How was this patch tested?
    
    Checked manually in my local environment.
    
    Author: Takuya UESHIN <ueshin@databricks.com>
    
    Closes #20468 from ueshin/issues/SPARK-23280/fup1.

--- sql/core/src/main/java/org/apache/spark/sql/vectorized/ColumnarArray.java
+++ sql/core/src/main/java/org/apache/spark/sql/vectorized/ColumnarArray.java
@@ -18,7 +18,6 @@ package org.apache.spark.sql.vectorized;
 
 import org.apache.spark.annotation.InterfaceStability;
 import org.apache.spark.sql.catalyst.util.ArrayData;
-import org.apache.spark.sql.catalyst.util.MapData;
 import org.apache.spark.sql.types.*;
 import org.apache.spark.unsafe.types.CalendarInterval;
 import org.apache.spark.unsafe.types.UTF8String;
