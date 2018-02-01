73da3b6968630d9e2cafc742ccb6d4eb54957df4
Wenchen Fan <wenchen@databricks.com>
2018-02-01 10:48:34 -0800

    [SPARK-23293][SQL] fix data source v2 self join
    ## What changes were proposed in this pull request?
    
    `DataSourceV2Relation` should extend `MultiInstanceRelation`, to take care of self-join.
    
    ## How was this patch tested?
    
    a new test
    
    Author: Wenchen Fan <wenchen@databricks.com>
    
    Closes #20466 from cloud-fan/dsv2-selfjoin.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Relation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Relation.scala
@@ -17,13 +17,15 @@
 
 package org.apache.spark.sql.execution.datasources.v2
 
+import org.apache.spark.sql.catalyst.analysis.MultiInstanceRelation
 import org.apache.spark.sql.catalyst.expressions.AttributeReference
 import org.apache.spark.sql.catalyst.plans.logical.{LeafNode, Statistics}
 import org.apache.spark.sql.sources.v2.reader._
 
 case class DataSourceV2Relation(
     fullOutput: Seq[AttributeReference],
-    reader: DataSourceReader) extends LeafNode with DataSourceReaderHolder {
+    reader: DataSourceReader)
+  extends LeafNode with MultiInstanceRelation with DataSourceReaderHolder {
 
   override def canEqual(other: Any): Boolean = other.isInstanceOf[DataSourceV2Relation]
 
@@ -32,6 +34,10 @@ case class DataSourceV2Relation(
       Statistics(sizeInBytes = r.getStatistics.sizeInBytes().orElse(conf.defaultSizeInBytes))
     case _ =>
       Statistics(sizeInBytes = conf.defaultSizeInBytes)
+  }
+
+  override def newInstance(): DataSourceV2Relation = {
+    copy(fullOutput = fullOutput.map(_.newInstance()))
   }
 }
 
