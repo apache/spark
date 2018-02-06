23634cebb9cdc5a9fea185743baabba88bd17443
“attilapiros” <piros.attila.zsolt@gmail.com>
2018-02-06 21:21:58 +0100

    applying review comments 5.1

--- core/src/main/scala/org/apache/spark/status/api/v1/OneApplicationResource.scala
+++ core/src/main/scala/org/apache/spark/status/api/v1/OneApplicationResource.scala
@@ -55,23 +55,23 @@ private[v1] class AbstractApplicationResource extends BaseAppResource {
   @Path("executors/{executorId}/threads")
   def threadDump(@PathParam("executorId") executorId: String): Array[ThreadStackTrace] =
     withUI { ui =>
-      if (executorId != SparkContext.DRIVER_IDENTIFIER && !executorId.forall(Character.isDigit)) {
-        throw new BadParameterException(
-          s"Invalid executorId: neither '${SparkContext.DRIVER_IDENTIFIER}' nor number.")
-      }
+    if (executorId != SparkContext.DRIVER_IDENTIFIER && !executorId.forall(Character.isDigit)) {
+      throw new BadParameterException(
+        s"Invalid executorId: neither '${SparkContext.DRIVER_IDENTIFIER}' nor number.")
+    }
 
-      val safeSparkContext = ui.sc.getOrElse {
-        throw new ServiceUnavailable("Thread dumps not available through the history server.")
-      }
+    val safeSparkContext = ui.sc.getOrElse {
+      throw new ServiceUnavailable("Thread dumps not available through the history server.")
+    }
 
-      ui.store.asOption(ui.store.executorSummary(executorId)) match {
-        case Some(executorSummary) if executorSummary.isActive =>
-            val safeThreadDump = safeSparkContext.getExecutorThreadDump(executorId).getOrElse {
-              throw new NotFoundException("No thread dump is available.")
-            }
-            return safeThreadDump
-        case Some(_) => throw new BadParameterException("Executor is not active.")
-        case _ => throw new NotFoundException("Executor does not exist.")
+    ui.store.asOption(ui.store.executorSummary(executorId)) match {
+      case Some(executorSummary) if executorSummary.isActive =>
+          val safeThreadDump = safeSparkContext.getExecutorThreadDump(executorId).getOrElse {
+            throw new NotFoundException("No thread dump is available.")
+          }
+          return safeThreadDump
+      case Some(_) => throw new BadParameterException("Executor is not active.")
+      case _ => throw new NotFoundException("Executor does not exist.")
     }
   }
 
