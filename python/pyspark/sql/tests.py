63c5bf13ce5cd3b8d7e7fb88de881ed207fde720
Takuya UESHIN <ueshin@databricks.com>
2018-02-06 18:30:50 +0900

    [SPARK-23334][SQL][PYTHON] Fix pandas_udf with return type StringType() to handle str type properly in Python 2.
    ## What changes were proposed in this pull request?
    
    In Python 2, when `pandas_udf` tries to return string type value created in the udf with `".."`, the execution fails. E.g.,
    
    ```python
    from pyspark.sql.functions import pandas_udf, col
    import pandas as pd
    
    df = spark.range(10)
    str_f = pandas_udf(lambda x: pd.Series(["%s" % i for i in x]), "string")
    df.select(str_f(col('id'))).show()
    ```
    
    raises the following exception:
    
    ```
    ...
    
    java.lang.AssertionError: assertion failed: Invalid schema from pandas_udf: expected StringType, got BinaryType
    	at scala.Predef$.assert(Predef.scala:170)
    	at org.apache.spark.sql.execution.python.ArrowEvalPythonExec$$anon$2.<init>(ArrowEvalPythonExec.scala:93)
    
    ...
    ```
    
    Seems like pyarrow ignores `type` parameter for `pa.Array.from_pandas()` and consider it as binary type when the type is string type and the string values are `str` instead of `unicode` in Python 2.
    
    This pr adds a workaround for the case.
    
    ## How was this patch tested?
    
    Added a test and existing tests.
    
    Author: Takuya UESHIN <ueshin@databricks.com>
    
    Closes #20507 from ueshin/issues/SPARK-23334.

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -3922,6 +3922,15 @@ class ScalarPandasUDF(ReusedSQLTestCase):
         res = df.select(str_f(col('str')))
         self.assertEquals(df.collect(), res.collect())
 
+    def test_vectorized_udf_string_in_udf(self):
+        from pyspark.sql.functions import pandas_udf, col
+        import pandas as pd
+        df = self.spark.range(10)
+        str_f = pandas_udf(lambda x: pd.Series(map(str, x)), StringType())
+        actual = df.select(str_f(col('id')))
+        expected = df.select(col('id').cast('string'))
+        self.assertEquals(expected.collect(), actual.collect())
+
     def test_vectorized_udf_datatype_string(self):
         from pyspark.sql.functions import pandas_udf, col
         df = self.spark.range(10).select(
