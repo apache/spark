49771ac8da8e68e8412d9f5d181953eaf0de7973
Jacek Laskowski <jacek@japila.pl>
2018-02-09 18:18:30 -0600

    [MINOR][HIVE] Typo fixes
    ## What changes were proposed in this pull request?
    
    Typo fixes (with expanding a Hive property)
    
    ## How was this patch tested?
    
    local build. Awaiting Jenkins
    
    Author: Jacek Laskowski <jacek@japila.pl>
    
    Closes #20550 from jaceklaskowski/hiveutils-typos.
    
    (cherry picked from commit 557938e2839afce26a10a849a2a4be8fc4580427)
    Signed-off-by: Sean Owen <sowen@cloudera.com>

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveUtils.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveUtils.scala
@@ -304,7 +304,7 @@ private[spark] object HiveUtils extends Logging {
         throw new IllegalArgumentException(
           "Builtin jars can only be used when hive execution version == hive metastore version. " +
             s"Execution: $builtinHiveVersion != Metastore: $hiveMetastoreVersion. " +
-            "Specify a vaild path to the correct hive jars using $HIVE_METASTORE_JARS " +
+            s"Specify a valid path to the correct hive jars using ${HIVE_METASTORE_JARS.key} " +
             s"or change ${HIVE_METASTORE_VERSION.key} to $builtinHiveVersion.")
       }
 
@@ -324,7 +324,7 @@ private[spark] object HiveUtils extends Logging {
       if (jars.length == 0) {
         throw new IllegalArgumentException(
           "Unable to locate hive jars to connect to metastore. " +
-            "Please set spark.sql.hive.metastore.jars.")
+            s"Please set ${HIVE_METASTORE_JARS.key}.")
       }
 
       logInfo(
