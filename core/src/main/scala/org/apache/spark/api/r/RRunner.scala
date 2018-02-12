ce1b9e8d3fded71718d1345a8d02bc5896a52b9c
Gregory Manis <gmanis@palantir.com>
2018-02-12 15:47:24 -0500

    Launch daemon from conda directory if conda is enabled

--- core/src/main/scala/org/apache/spark/api/r/RRunner.scala
+++ core/src/main/scala/org/apache/spark/api/r/RRunner.scala
@@ -350,7 +350,9 @@ private[r] object RRunner {
     val rConnectionTimeout = sparkConf.getInt(
       "spark.r.backendConnectionTimeout", SparkRDefaults.DEFAULT_CONNECTION_TIMEOUT)
     val rOptions = "--vanilla"
-    val rLibDir = RUtils.sparkRPackagePath(isDriver = false)
+    val rLibDir = condaEnv.map { conda =>
+      Seq(conda.condaEnvDir + "/lib/R/library", RUtils.sparkRPackagePath(isDriver = false))
+    }.getOrElse(RUtils.sparkRPackagePath(isDriver = false))
     val rExecScript = rLibDir(0) + "/SparkR/worker/" + script
     val pb = new ProcessBuilder(Arrays.asList(rCommand, rOptions, rExecScript))
     // Activate the conda environment by setting the right env variables if applicable.
