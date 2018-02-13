93d38794dd261ee1bbe2497470ee43de1186ef3c
Maxim Gekk <maxim.gekk@databricks.com>
2018-02-13 11:54:52 +0100

    testFile is moved into the test to make more local because it is used only in the test

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/json/JsonSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/json/JsonSuite.scala
@@ -2065,10 +2065,14 @@ class JsonSuite extends QueryTest with SharedSQLContext with TestJsonData {
   }
 
   test("json in UTF-16 with BOM") {
+    val testFile = {
+      val fileName = "json-tests/utf16WithBOM.json"
+      Thread.currentThread().getContextClassLoader.getResource(fileName).toString
+    }
     val schema = new StructType().add("firstName", StringType).add("lastName", StringType)
     val jsonDF = spark.read.schema(schema)
       .option("mode", "DROPMALFORMED")
-      .json(testFile("json-tests/utf16WithBOM.json"))
+      .json(testFile)
 
     checkAnswer(jsonDF, Seq(
       Row("Chris", "Baird"), Row("Doug", "Rood")
