e4c5fb12d732029727ba6772732734c511c1f6fa
Marcin Kurczych <marcin.kurczych@iqvia.com>
2018-02-20 16:12:09 +0100

    [SPARK-23464][MESOS] Fix mesos cluster scheduler options double-escaping
    Don't enclose --conf option value with "", as they are already escaped by shellEscape
    and additional wrapping cancels out this escaping, causing driver fail to start.
    
    This reverts commit 9b377aa49f14af31f54164378d60e0fdea2142e5 [SPARK-18114].
    
    Manual test with driver command logging added.
    
    Author: Marcin Kurczych <marcin.kurczych@iqvia.com>

--- resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
+++ resource-managers/mesos/src/main/scala/org/apache/spark/scheduler/cluster/mesos/MesosClusterScheduler.scala
@@ -530,7 +530,7 @@ private[spark] class MesosClusterScheduler(
       .filter { case (key, _) => !replicatedOptionsBlacklist.contains(key) }
       .toMap
     (defaultConf ++ driverConf).foreach { case (key, value) =>
-      options ++= Seq("--conf", s""""$key=${shellEscape(value)}"""".stripMargin) }
+      options ++= Seq("--conf", s"$key=${shellEscape(value)}") }
 
     options
   }
