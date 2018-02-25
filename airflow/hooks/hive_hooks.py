4118e71de3bc4d9802e5b1710cd8f29a488a02fb
Naoya Kanai <naoya.kanai@airbnb.com>
2018-02-25 12:23:14 +0100

    [AIRFLOW-1035][AIRFLOW-1053] import unicode_literals to parse Unicode in HQL
    Closes #3053 from naoyak/AIRFLOW-1053

--- airflow/hooks/hive_hooks.py
+++ airflow/hooks/hive_hooks.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 from six.moves import zip
 from past.builtins import basestring
 
