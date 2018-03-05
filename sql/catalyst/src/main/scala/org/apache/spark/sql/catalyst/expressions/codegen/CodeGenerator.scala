e073d0325156da280ab931d7f1d4fe8c09e9be3a
万两 <wanliang@wacai.com>
2018-03-05 18:48:15 +0800

    use default value in sql conf

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala
@@ -1510,7 +1510,7 @@ object CodeGenerator extends Logging {
    * weak keys/values and thus does not respond to memory pressure.
    */
   private val cache = CacheBuilder.newBuilder()
-    .maximumSize(SparkEnv.get.conf.getInt(SQLConf.CODEGEN_COMPILE_MAX_CACHE_SIZE.key, 100))
+    .maximumSize(SparkEnv.get.conf.getInt(SQLConf.CODEGEN_COMPILE_MAX_CACHE_SIZE.key, SQLConf.CODEGEN_COMPILE_MAX_CACHE_SIZE.defaultValue.get))
     .build(
       new CacheLoader[CodeAndComment, (GeneratedClass, Int)]() {
         override def load(code: CodeAndComment): (GeneratedClass, Int) = {
