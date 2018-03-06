f4118c03d7ea8cdd9ade5ec44db3e1bcb5f45535
Ye Zhou <yezhou@linkedin.com>
2018-03-05 18:19:17 -0800

    [SPARK-23608][CORE][WebUI] Add synchronization in SHS between attachSparkUI and detachSparkUI functions to avoid concurrent modification issue to Jetty Handlers.

--- core/src/main/scala/org/apache/spark/deploy/history/HistoryServer.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/HistoryServer.scala
@@ -148,14 +148,14 @@ class HistoryServer(
       appId: String,
       attemptId: Option[String],
       ui: SparkUI,
-      completed: Boolean) {
+      completed: Boolean) = this.synchronized {
     assert(serverInfo.isDefined, "HistoryServer must be bound before attaching SparkUIs")
     ui.getHandlers.foreach(attachHandler)
     addFilters(ui.getHandlers, conf)
   }
 
   /** Detach a reconstructed UI from this server. Only valid after bind(). */
-  override def detachSparkUI(appId: String, attemptId: Option[String], ui: SparkUI): Unit = {
+  override def detachSparkUI(appId: String, attemptId: Option[String], ui: SparkUI): Unit = this.synchronized {
     assert(serverInfo.isDefined, "HistoryServer must be bound before detaching SparkUIs")
     ui.getHandlers.foreach(detachHandler)
     provider.onUIDetached(appId, attemptId, ui)
