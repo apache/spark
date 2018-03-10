d3e480c02516afdaedb6a1a4d3275b7f36d54835
DylanGuedes <djmgguedes@gmail.com>
2018-03-10 11:30:56 -0300

    Uses isinstance instead of typeof
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -485,11 +485,11 @@ class DataFrame(object):
         if not isinstance(name, str):
             raise TypeError("name should be provided as str, got {0}".format(type(name)))
 
-        allowed = [str, list, float, int]
+        allowed_types = (basestring, list, float, int)
         for p in parameters:
-            if not type(p) in allowed:
+            if not isinstance(p, allowed_types):
                 raise TypeError(
-                    "all parameters should be in {0}, got {1} of type {2}".format(allowed, p, type(p)))
+                    "all parameters should be in {0}, got {1} of type {2}".format(allowed_types, p, type(p)))
 
         jdf = self._jdf.hint(name, self._jseq(parameters))
         return DataFrame(jdf, self.sql_ctx)
