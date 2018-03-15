05590f5b9ed6e78206a013c2eac6e811f69abbf6
Adam Winstanley <awinstan@amazon.com>
2018-03-15 07:54:07 -0700

    Revert all changes to the generated Avro classes except for those required for the fixed_column to simplify the PR.

--- sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/VectorizedColumnReader.java
+++ sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/VectorizedColumnReader.java
@@ -32,7 +32,6 @@ import org.apache.parquet.schema.PrimitiveType;
 
 import org.apache.spark.sql.catalyst.util.DateTimeUtils;
 import org.apache.spark.sql.execution.vectorized.WritableColumnVector;
-import org.apache.spark.sql.types.BinaryType;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.DecimalType;
 
