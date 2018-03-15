65b92e9e5fb447d5dc90ab821338bb837059eb08
Dan Sanduleac <dsanduleac@palantir.com>
2018-03-15 20:42:36 +0000

    Super fancy bin packing

--- project/CirclePlugin.scala
+++ project/CirclePlugin.scala
@@ -17,15 +17,21 @@
 
 import scala.annotation.tailrec
 
-import sbt._
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import sbt.Keys._
+import sbt._
 import sbt.plugins.JvmPlugin
+import scalaz.Dequeue
 
 //noinspection ScalaStyle
 object CirclePlugin extends AutoPlugin {
   lazy val Circle = config("circle").extend(Test).hide
 
   case class ProjectTests(project: ProjectRef, tests: Seq[TestDefinition])
+  case class ProjectTest(project: ProjectRef, test: TestDefinition)
 
   val circleTestsByProject = taskKey[Option[Seq[ProjectTests]]]("The tests that should be run under this circle node, if circle is set up")
   val copyTestReportsToCircle: TaskKey[Boolean] = taskKey("Copy the test reports to circle. Expects CIRCLE_TEST_REPORTS to be defined")
@@ -42,38 +48,111 @@ object CirclePlugin extends AutoPlugin {
     ProjectTests(thisProjectRef.value, Defaults.detectTests.value)
   }
 
+  private[this] lazy val mapper = new ObjectMapper() with ScalaObjectMapper
+  mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+  mapper.registerModule(DefaultScalaModule)
+  case class TestResult(classname: String, result: String, run_time: Double, source: String)
+  case class TestKey(source: String, classname: String)
+
+  // Through this magical command we established that the average class run_time is 7.737
+  // jq <~/results.json '.tests | map(select(.result != "skipped")) | group_by(.classname) | map(map(.run_time) | add) | (add / length)'
+  private[this] val AVERAGE_TEST_CLASS_RUN_TIME = 7.737d
+
   override def globalSettings: Seq[Def.Setting[_]] = List(
     circleTestsByProject := {
+      val log = streams.value.log
+
       if (sys.env contains "CIRCLE_NODE_INDEX") {
         val index = sys.env("CIRCLE_NODE_INDEX").toInt
-        val total = sys.env("CIRCLE_NODE_TOTAL").toInt
+        val totalNodes = sys.env("CIRCLE_NODE_TOTAL").toInt
         val byProject: Seq[ProjectTests] = testsByProject.all(ScopeFilter(inAnyProject, inConfigurations(Test))).value
 
-        // need a stable sort of projects
-        val sortedProjects = byProject.sortBy(_.project.project).toList
+        val testsByKey = byProject.iterator
+            .flatMap(pt => pt.tests.map(ProjectTest(pt.project, _)))
+            .toStream
+            .groupBy(pt => TestKey(pt.project.project, pt.test.name))
 
-        val totalTests = sortedProjects.iterator.map(_.tests.size).sum
-        val from = index * totalTests / total
-        val to = (index + 1) * totalTests / total
+        import collection.JavaConverters._
+        // Get timings and sum them up by TestKey = (source, classname)
+        val testResultsFile = sys.env.get("CIRCLE_INTERNAL_TASK_DATA")
+            .map(taskData => file(taskData) / "circle-test-results/results.json")
+        val testTimings = try {
+          testResultsFile
+              .map(file => mapper.readValues[TestResult](mapper.getFactory.createParser(file)))
+              .map(_.asScala)
+              .getOrElse(Iterator())
+              .toStream
+              .groupBy(result => TestKey(result.source, result.classname))
+              .mapValues(_.foldLeft(0.0d)(_ + _.run_time))
+        } catch {
+          case e: Exception =>
+            log.warn(f"Couldn't read test results file: $testResultsFile:%n${e.getStackTraceString}")
+            Map.empty[TestKey, Nothing]
+        }
 
-        // We allow a slice of [from, to) from all tests across all projects (in the order of sortedProjects)
-        // We then filter out every other
+        val allTestsTimings = testsByKey.keys
+            .iterator
+            .map(key => key -> testTimings.getOrElse(key, AVERAGE_TEST_CLASS_RUN_TIME))
+            .toMap
+
+        val totalTestTime = allTestsTimings.valuesIterator.sum
+        val timePerNode = totalTestTime / totalNodes
+//        val from = index * totalTestTime / total
+//        val to = (index + 1) * totalTestTime / total
+
+        // Now, do bin packing. Sort first by runTime, then by key, to get a stable sort.
+        implicit val testKeyOrdering = Ordering.by((tk: TestKey) => (tk.source, tk.classname))
+        val tests = Dequeue[(TestKey, Double)](
+            allTestsTimings.toIndexedSeq.sortBy { case (key, runTime) => (runTime, key) } : _*)
 
         @tailrec
-        def process(projectsLeft: List[ProjectTests], testsSoFar: Int, acc: List[ProjectTests]): List[ProjectTests] = {
-          val from1 = from - testsSoFar
-          val to1 = to - testsSoFar
-          projectsLeft match {
-            case ProjectTests(proj, tests) :: rest =>
-              val out = ProjectTests(proj, tests.iterator.zipWithIndex.collect {
-                case (td, idx) if idx >= from1 && idx < to1 => td
-              }.toList)
-              process(rest, testsSoFar + tests.size, out :: acc)
-            case _ =>
-              acc
+        def process(tests: Dequeue[(TestKey, Double)],
+                    soFar: Double = 0d,
+                    takeLeft: Boolean = true,
+                    acc: List[TestKey] = Nil,
+                    groups: List[List[TestKey]] = Nil): List[List[TestKey]] = {
+
+          if (acc.isEmpty && tests.isEmpty) {
+            return groups
           }
+
+          val candidates = ((if (takeLeft) Stream(tests.uncons.map(_ -> true)) else Stream.empty)
+              #::: tests.unsnoc.map(_ -> false)
+              #:: Stream.empty)
+              .flatMap(_.toOption)
+
+          candidates
+              .collectFirst {
+                case x@(((key, runTime), _), _) if soFar + runTime <= timePerNode => x
+              } match {
+                case None =>
+                  if (acc.isEmpty) {
+                    if (tests.isEmpty) {
+                      groups
+                    } else {
+                      // We have a new bucket, but we can't fit any remaining test in it.
+                      // In this case, fit all remaining tests in the last issued bucket.
+                      val lastGroup +: restGroups = groups
+                      (tests.toStream.map(_._1) ++: lastGroup) :: restGroups
+                    }
+                  } else {
+                    process(tests, 0d, takeLeft = true, Nil, acc :: groups)
+                  }
+                case Some((((key, runTime), rest), fromLeft)) =>
+                    process(rest, soFar + runTime, fromLeft, key :: acc, groups)
+            }
         }
-        Some(process(sortedProjects, 0, Nil))
+
+        val bucket = process(tests).lift.apply(index).getOrElse(Nil)
+
+        val groupedByProject = bucket.flatMap(testsByKey.apply)
+            .groupBy(_.project)
+            .mapValues(_.map(_.test))
+            .iterator
+            .map { case (proj, tests) => ProjectTests(proj, tests) }
+            .toIndexedSeq
+
+        Some(groupedByProject)
       } else {
         None
       }
---
+++ project/build.sbt
@@ -0,0 +1,4 @@
+
+val jacksonVersion = "2.9.4"
+libraryDependencies += "com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion
+libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
