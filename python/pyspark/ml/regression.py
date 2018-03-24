ab0b04da689f2723e2d928c6f178cbb203194a99
Kevin Yu <qyu@us.ibm.com>
2018-03-23 21:06:47 -0700

    address comment

--- python/pyspark/ml/regression.py
+++ python/pyspark/ml/regression.py
@@ -336,10 +336,10 @@ class LinearRegressionSummary(JavaWrapper):
     @since("2.0.0")
     def r2(self):
         """
-        Returns R^2^, the coefficient of determination.
+        Returns R^2, the coefficient of determination.
 
         .. seealso:: `Wikipedia coefficient of determination \
-        <http://en.wikipedia.org/wiki/Coefficient_of_determination>`
+        <http://en.wikipedia.org/wiki/Coefficient_of_determination>`_
 
         .. note:: This ignores instance weights (setting all to 1.0) from
             `LinearRegression.weightCol`. This will change in later Spark
@@ -351,10 +351,10 @@ class LinearRegressionSummary(JavaWrapper):
     @since("2.4.0")
     def r2adj(self):
         """
-        Returns Adjusted R^2^, the adjusted coefficient of determination.
+        Returns Adjusted R^2, the adjusted coefficient of determination.
 
         .. seealso:: `Wikipedia coefficient of determination \
-        <https://en.wikipedia.org/wiki/Coefficient_of_determination>`
+        <https://en.wikipedia.org/wiki/Coefficient_of_determination#Adjusted_R2>`_
 
         .. note:: This ignores instance weights (setting all to 1.0) from
             `LinearRegression.weightCol`. This will change in later Spark versions.
