1c39dfaef09538ad63e4d5d6d9a343c9bfe9f8d3
Liang-Chi Hsieh <viirya@gmail.com>
2018-03-26 20:45:20 +0200

    [SPARK-23599][SQL][BACKPORT-2.3] Use RandomUUIDGenerator in Uuid expression
    ## What changes were proposed in this pull request?
    
    As stated in Jira, there are problems with current `Uuid` expression which uses `java.util.UUID.randomUUID` for UUID generation.
    
    This patch uses the newly added `RandomUUIDGenerator` for UUID generation. So we can make `Uuid` deterministic between retries.
    
    This backports SPARK-23599 to Spark 2.3.
    
    ## How was this patch tested?
    
    Added tests.
    
    Author: Liang-Chi Hsieh <viirya@gmail.com>
    
    Closes #20903 from viirya/SPARK-23599-2.3.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/ExpressionEvalHelper.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/ExpressionEvalHelper.scala
@@ -162,6 +162,7 @@ trait ExpressionEvalHelper extends GeneratorDrivenPropertyChecks {
         Alias(expression, s"Optimized($expression)1")() ::
           Alias(expression, s"Optimized($expression)2")() :: Nil),
       expression)
+    plan.initialize(0)
 
     val unsafeRow = plan(inputRow)
     val input = if (inputRow == EmptyRow) "" else s", input: $inputRow"
