13b6633d8194d1cad71bea1ceb699716aa1ea402
Gengliang Wang <gengliang.wang@databricks.com>
2018-04-02 17:06:36 +0800

    fix indent

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/SessionCatalog.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/SessionCatalog.scala
@@ -313,16 +313,16 @@ class SessionCatalog(
 
   def validateTableLocation(table: CatalogTable): Unit = {
     // SPARK-19724: the default location of a managed table should be non-existent or empty.
-   if (table.tableType == CatalogTableType.MANAGED && !conf.allowNonemptyManagedTableLocation) {
-     val tableLocation =
-       new Path(table.storage.locationUri.getOrElse(defaultTablePath(table.identifier)))
-     val fs = tableLocation.getFileSystem(hadoopConf)
-
-     if (fs.exists(tableLocation) && fs.listStatus(tableLocation).nonEmpty) {
-       throw new AnalysisException(s"Can not create the managed table('${table.identifier}')" +
-         s". The associated location('${tableLocation.toString}') already exists.")
-     }
-   }
+    if (table.tableType == CatalogTableType.MANAGED && !conf.allowNonemptyManagedTableLocation) {
+      val tableLocation =
+        new Path(table.storage.locationUri.getOrElse(defaultTablePath(table.identifier)))
+      val fs = tableLocation.getFileSystem(hadoopConf)
+    
+      if (fs.exists(tableLocation) && fs.listStatus(tableLocation).nonEmpty) {
+        throw new AnalysisException(s"Can not create the managed table('${table.identifier}')" +
+          s". The associated location('${tableLocation.toString}') already exists.")
+      }
+    }
   }
 
   /**
