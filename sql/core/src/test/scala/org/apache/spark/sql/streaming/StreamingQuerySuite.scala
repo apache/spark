a2e9eb2b0e8cce25e64edbff1fdadbf315ac245b
Jose Torres <torres.joseph.f+github@gmail.com>
2018-04-02 14:00:17 -0700

    fix commit

--- sql/core/src/test/scala/org/apache/spark/sql/streaming/StreamingQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/streaming/StreamingQuerySuite.scala
@@ -26,8 +26,8 @@ import org.scalactic.TolerantNumerics
 import org.scalatest.BeforeAndAfter
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.mockito.MockitoSugar
-import org.apache.spark.SparkException
 
+import org.apache.spark.SparkException
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.catalyst.expressions.UnsafeRow
