7bcb662312462496a895e8bf54cee9200bf7798c
Robert Kruszewski <robertk@palantir.com>
2018-04-06 18:32:37 -0700

    Fix checkstyle

--- common/unsafe/src/test/java/org/apache/spark/unsafe/types/UTF8StringSuite.java
+++ common/unsafe/src/test/java/org/apache/spark/unsafe/types/UTF8StringSuite.java
@@ -25,7 +25,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import com.google.common.collect.ImmutableMap;
-import org.apache.spark.unsafe.Platform;
 import org.apache.spark.unsafe.memory.ByteArrayMemoryBlock;
 import org.apache.spark.unsafe.memory.OnHeapMemoryBlock;
 import org.junit.Test;
