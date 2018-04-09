c1791bf9f0cdd8074a19130e05be52d60f1e618c
Bryan Cutler <cutlerb@gmail.com>
2018-04-09 12:26:54 -0700

    remove unused import

--- sql/core/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala
@@ -21,7 +21,6 @@ import scala.collection.JavaConverters._
 
 import org.apache.arrow.vector._
 import org.apache.arrow.vector.complex._
-import org.apache.arrow.vector.types.pojo.ArrowType
 
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.SpecializedGetters
