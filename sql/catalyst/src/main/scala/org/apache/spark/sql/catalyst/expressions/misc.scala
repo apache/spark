bcb086f9940da2b26a34ae73553fc6924d587ec2
Arseniy Tashoyan <tashoyan@gmail.com>
2018-04-12 01:40:02 +0300

    [SPARK-23693][SQL] Remove unused code related to old UUID implementation

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/misc.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/misc.scala
@@ -17,13 +17,9 @@
 
 package org.apache.spark.sql.catalyst.expressions
 
-import java.util.UUID
-
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.codegen._
-import org.apache.spark.sql.catalyst.util.RandomUUIDGenerator
 import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.types.UTF8String
 
 /**
  * Print the result of an expression to stderr (used for debugging codegen).
@@ -113,47 +109,4 @@ case class CurrentDatabase() extends LeafExpression with Unevaluable {
   override def foldable: Boolean = true
   override def nullable: Boolean = false
   override def prettyName: String = "current_database"
-}
-
-// scalastyle:off line.size.limit
-@ExpressionDescription(
-  usage = "_FUNC_() - Returns an universally unique identifier (UUID) string. The value is returned as a canonical UUID 36-character string.",
-  examples = """
-    Examples:
-      > SELECT _FUNC_();
-       46707d92-02f4-4817-8116-a4c3b23e6266
-  """)
-// scalastyle:on line.size.limit
-@deprecated
-case class Uuid(randomSeed: Option[Long] = None) extends LeafExpression with Stateful {
-
-  def this() = this(None)
-
-  override lazy val resolved: Boolean = randomSeed.isDefined
-
-  override def nullable: Boolean = false
-
-  override def dataType: DataType = StringType
-
-  @transient private[this] var randomGenerator: RandomUUIDGenerator = _
-
-  override protected def initializeInternal(partitionIndex: Int): Unit =
-    randomGenerator = RandomUUIDGenerator(randomSeed.get + partitionIndex)
-
-  override protected def evalInternal(input: InternalRow): Any =
-    randomGenerator.getNextUUIDUTF8String()
-
-  override def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
-    val randomGen = ctx.freshName("randomGen")
-    ctx.addMutableState("org.apache.spark.sql.catalyst.util.RandomUUIDGenerator", randomGen,
-      forceInline = true,
-      useFreshName = false)
-    ctx.addPartitionInitializationStatement(s"$randomGen = " +
-      "new org.apache.spark.sql.catalyst.util.RandomUUIDGenerator(" +
-      s"${randomSeed.get}L + partitionIndex);")
-    ev.copy(code = s"final UTF8String ${ev.value} = $randomGen.getNextUUIDUTF8String();",
-      isNull = FalseLiteral)
-  }
-
-  override def freshCopy(): Uuid = Uuid(randomSeed)
 }
