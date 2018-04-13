b91dbebe0048c85d771aaaf238dc11b2f4d36f55
WeichenXu <weichen.xu@databricks.com>
2018-04-13 18:41:43 +0800

    fix python style

--- python/pyspark/ml/stat.py
+++ python/pyspark/ml/stat.py
@@ -376,12 +376,11 @@ class SummaryBuilder(JavaWrapper):
 
         :param featuresCol:
          a column that contains features Vector object.
-        :param weightCol
+        :param weightCol:
          a column that contains weight value. Default weight is 1.0.
         :return:
          an aggregate column that contains the statistics. The exact content of this
          structure is determined during the creation of the builder.
-
         """
         featuresCol, weightCol = Summarizer._check_param(featuresCol, weightCol)
         return Column(self._js.summary(featuresCol._jc, weightCol._jc))
