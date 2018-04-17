4391b0a94e7ddbf53043e6723b7e6e63655f5759
Maxim Gekk <maxim.gekk@databricks.com>
2018-04-17 21:21:49 +0200

    Improving test title

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/JsonExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/JsonExpressionsSuite.scala
@@ -709,7 +709,7 @@ class JsonExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper with 
     }
   }
 
-  test("from_json support json to struct with map[string, struct]") {
+  test("from_json support json to struct with map[string, integer] field") {
     val input = """{"test":{"a":1}}"""
     val schema = new StructType().add("test", MapType(StringType, IntegerType))
     val output = InternalRow(ArrayBasedMapData(Map(UTF8String.fromString("a") -> 1)))
