a70539727261a111fc5d886f2509229f913dd00f
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2018-04-18 17:14:34 +0100

    fix python test failure

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1864,13 +1864,13 @@ def array_position(col, value):
 
 
 @since(2.4)
-def element_at(col, index):
+def element_at(col, extraction):
     """
-    Collection function: returns element of array at given index in value if col is array.
-    returns value for the given key in value if col is map.
+    Collection function: returns element of array at given index in extraction if col is array.
+    returns value for the given key in extraction if col is map.
 
     :param col: name of column containing array or map
-    :param index: index to check for in array or key to check for in map
+    :param extraction: index to check for in array or key to check for in map
 
     .. note:: The position is not zero based, but 1 based index.
 
@@ -1883,7 +1883,7 @@ def element_at(col, index):
     [Row(element_at(data, a)=1.0), Row(element_at(data, a)=None)]
     """
     sc = SparkContext._active_spark_context
-    return Column(sc._jvm.functions.element_at(_to_java_column(col), value))
+    return Column(sc._jvm.functions.element_at(_to_java_column(col), extraction))
 
 
 @since(1.4)
