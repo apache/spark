8b84493461f57a619f8c0a03967af0c2c6814ae9
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2018-04-19 11:59:17 +0900

    [SPARK-23919][SQL] Add array_position function
    ## What changes were proposed in this pull request?
    
    The PR adds the SQL function `array_position`. The behavior of the function is based on Presto's one.
    
    The function returns the position of the first occurrence of the element in array x (or 0 if not found) using 1-based index as BigInt.
    
    ## How was this patch tested?
    
    Added UTs
    
    Author: Kazuaki Ishizaki <ishizaki@jp.ibm.com>
    
    Closes #21037 from kiszk/SPARK-23919.

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1845,6 +1845,23 @@ def array_contains(col, value):
     return Column(sc._jvm.functions.array_contains(_to_java_column(col), value))
 
 
+@since(2.4)
+def array_position(col, value):
+    """
+    Collection function: Locates the position of the first occurrence of the given value
+    in the given array. Returns null if either of the arguments are null.
+
+    .. note:: The position is not zero based, but 1 based index. Returns 0 if the given
+        value could not be found in the array.
+
+    >>> df = spark.createDataFrame([(["c", "b", "a"],), ([],)], ['data'])
+    >>> df.select(array_position(df.data, "a")).collect()
+    [Row(array_position(data, a)=3), Row(array_position(data, a)=0)]
+    """
+    sc = SparkContext._active_spark_context
+    return Column(sc._jvm.functions.array_position(_to_java_column(col), value))
+
+
 @since(1.4)
 def explode(col):
     """Returns a new row for each element in the given array or map.
