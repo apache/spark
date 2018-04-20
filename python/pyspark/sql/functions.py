619f6b1dfa809716741e54853c5dddb3e413e22b
Maxim Gekk <maxim.gekk@databricks.com>
2018-04-20 13:47:27 +0200

    Fix the example for python 3

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -2016,7 +2016,7 @@ def from_json(col, schema, options={}):
     [Row(json=Row(a=1))]
     >>> schema = MapType(StringType(), IntegerType())
     >>> df.select(from_json(df.value, schema).alias("json")).collect()
-    [Row(json={u'a': 1})]
+    [Row(json={'a': 1})]
     >>> data = [(1, '''[{"a": 1}]''')]
     >>> schema = ArrayType(StructType([StructField("a", IntegerType())]))
     >>> df = spark.createDataFrame(data, ("key", "value"))
