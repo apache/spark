c02a2b47bbaac792eb23978002d16df4a4148d31
Gengliang Wang <gengliang.wang@databricks.com>
2018-04-23 21:39:08 +0800

    remove useless code

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Relation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Relation.scala
@@ -52,7 +52,7 @@ case class DataSourceV2Relation(
       AttributeReference(f.name, f.dataType, f.nullable, f.metadata)()))
   }
 
-  private[sql] lazy val v2Options: DataSourceOptions = makeV2Options(options)
+  private lazy val v2Options: DataSourceOptions = makeV2Options(options)
 
   lazy val (
       reader: DataSourceReader,
