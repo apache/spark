87e8a572be14381da9081365d9aa2cbf3253a32c
hyukjinkwon <gurwls223@apache.org>
2018-04-24 16:18:20 +0800

    [SPARK-24054][R] Add array_position function / element_at functions
    ## What changes were proposed in this pull request?
    
    This PR proposes to add array_position and element_at in R side too.
    
    array_position:
    
    ```r
    df <- createDataFrame(cbind(model = rownames(mtcars), mtcars))
    mutated <- mutate(df, v1 = create_array(df$gear, df$am, df$carb))
    head(select(mutated, array_position(mutated$v1, 1)))
    ```
    
    ```
      array_position(v1, 1.0)
    1                       2
    2                       2
    3                       2
    4                       3
    5                       0
    6                       3
    ```
    
    element_at:
    
    ```r
    df <- createDataFrame(cbind(model = rownames(mtcars), mtcars))
    mutated <- mutate(df, v1 = create_array(df$mpg, df$cyl, df$hp))
    head(select(mutated, element_at(mutated$v1, 1)))
    ```
    
    ```
      element_at(v1, 1.0)
    1                21.0
    2                21.0
    3                22.8
    4                21.4
    5                18.7
    6                18.1
    ```
    
    ```r
    df <- createDataFrame(cbind(model = rownames(mtcars), mtcars))
    mutated <- mutate(df, v1 = create_map(df$model, df$cyl))
    head(select(mutated, element_at(mutated$v1, "Valiant")))
    ```
    
    ```
      element_at(v3, Valiant)
    1                      NA
    2                      NA
    3                      NA
    4                      NA
    5                      NA
    6                       6
    ```
    
    ## How was this patch tested?
    
    Unit tests were added in `R/pkg/tests/fulltests/test_sparkSQL.R` and manually tested. Documentation was manually built and verified.
    
    Author: hyukjinkwon <gurwls223@apache.org>
    
    Closes #21130 from HyukjinKwon/sparkr_array_position_element_at.

--- R/pkg/tests/fulltests/test_sparkSQL.R
+++ R/pkg/tests/fulltests/test_sparkSQL.R
@@ -1479,23 +1479,32 @@ test_that("column functions", {
   df5 <- createDataFrame(list(list(a = "010101")))
   expect_equal(collect(select(df5, conv(df5$a, 2, 16)))[1, 1], "15")
 
-  # Test array_contains() and sort_array()
+  # Test array_contains(), array_position(), element_at() and sort_array()
   df <- createDataFrame(list(list(list(1L, 2L, 3L)), list(list(6L, 5L, 4L))))
   result <- collect(select(df, array_contains(df[[1]], 1L)))[[1]]
   expect_equal(result, c(TRUE, FALSE))
+
+  result <- collect(select(df, array_position(df[[1]], 1L)))[[1]]
+  expect_equal(result, c(1, 0))
+
+  result <- collect(select(df, element_at(df[[1]], 1L)))[[1]]
+  expect_equal(result, c(1, 6))
 
   result <- collect(select(df, sort_array(df[[1]], FALSE)))[[1]]
   expect_equal(result, list(list(3L, 2L, 1L), list(6L, 5L, 4L)))
   result <- collect(select(df, sort_array(df[[1]])))[[1]]
   expect_equal(result, list(list(1L, 2L, 3L), list(4L, 5L, 6L)))
 
-  # Test map_keys() and map_values()
+  # Test map_keys(), map_values() and element_at()
   df <- createDataFrame(list(list(map = as.environment(list(x = 1, y = 2)))))
   result <- collect(select(df, map_keys(df$map)))[[1]]
   expect_equal(result, list(list("x", "y")))
 
   result <- collect(select(df, map_values(df$map)))[[1]]
   expect_equal(result, list(list(1, 2)))
+
+  result <- collect(select(df, element_at(df$map, "y")))[[1]]
+  expect_equal(result, 2)
 
   # Test that stats::lag is working
   expect_equal(length(lag(ldeaths, 12)), 72)
