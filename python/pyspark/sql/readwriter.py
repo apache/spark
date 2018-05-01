52a420ff62fabcc649d7acf85a72a3c0cd6437e8
Dongjoon Hyun <dongjoon@apache.org>
2018-05-01 09:06:23 +0800

    [SPARK-23853][PYSPARK][TEST] Run Hive-related PySpark tests only for `-Phive`
    ## What changes were proposed in this pull request?
    
    When `PyArrow` or `Pandas` are not available, the corresponding PySpark tests are skipped automatically. Currently, PySpark tests fail when we are not using `-Phive`. This PR aims to skip Hive related PySpark tests when `-Phive` is not given.
    
    **BEFORE**
    ```bash
    $ build/mvn -DskipTests clean package
    $ python/run-tests.py --python-executables python2.7 --modules pyspark-sql
    File "/Users/dongjoon/spark/python/pyspark/sql/readwriter.py", line 295, in pyspark.sql.readwriter.DataFrameReader.table
    ...
    IllegalArgumentException: u"Error while instantiating 'org.apache.spark.sql.hive.HiveExternalCatalog':"
    **********************************************************************
       1 of   3 in pyspark.sql.readwriter.DataFrameReader.table
    ***Test Failed*** 1 failures.
    ```
    
    **AFTER**
    ```bash
    $ build/mvn -DskipTests clean package
    $ python/run-tests.py --python-executables python2.7 --modules pyspark-sql
    ...
    Tests passed in 138 seconds
    
    Skipped tests in pyspark.sql.tests with python2.7:
    ...
        test_hivecontext (pyspark.sql.tests.HiveSparkSubmitTests) ... skipped 'Hive is not available.'
    ```
    
    ## How was this patch tested?
    
    This is a test-only change. First, this should pass the Jenkins. Then, manually do the following.
    
    ```bash
    build/mvn -DskipTests clean package
    python/run-tests.py --python-executables python2.7 --modules pyspark-sql
    ```
    
    Author: Dongjoon Hyun <dongjoon@apache.org>
    
    Closes #21141 from dongjoon-hyun/SPARK-23853.
    
    (cherry picked from commit b857fb549f3bf4e6f289ba11f3903db0a3696dec)
    Signed-off-by: hyukjinkwon <gurwls223@apache.org>

--- python/pyspark/sql/readwriter.py
+++ python/pyspark/sql/readwriter.py
@@ -956,7 +956,7 @@ def _test():
     globs = pyspark.sql.readwriter.__dict__.copy()
     sc = SparkContext('local[4]', 'PythonTest')
     try:
-        spark = SparkSession.builder.enableHiveSupport().getOrCreate()
+        spark = SparkSession.builder.getOrCreate()
     except py4j.protocol.Py4JError:
         spark = SparkSession(sc)
 
