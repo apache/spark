8509284e1ec048d5afa87d41071c0429924e45c9
Imran Rashid <irashid@cloudera.com>
2018-05-03 10:59:18 -0500

    [SPARK-23433][CORE] Late zombie task completions update all tasksets
    Fetch failure lead to multiple tasksets which are active for a given
    stage.  While there is only one "active" version of the taskset, the
    earlier attempts can still have running tasks, which can complete
    successfully.  So a task completion needs to update every taskset
    so that it knows the partition is completed.  That way the final active
    taskset does not try to submit another task for the same partition,
    and so that it knows when it is completed and when it should be
    marked as a "zombie".
    
    Added a regression test.
    
    Author: Imran Rashid <irashid@cloudera.com>
    
    Closes #21131 from squito/SPARK-23433.
    
    (cherry picked from commit 94641fe6cc68e5977dd8663b8f232a287a783acb)
    Signed-off-by: Imran Rashid <irashid@cloudera.com>

--- core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -74,6 +74,8 @@ private[spark] class TaskSetManager(
   val ser = env.closureSerializer.newInstance()
 
   val tasks = taskSet.tasks
+  private[scheduler] val partitionToIndex = tasks.zipWithIndex
+    .map { case (t, idx) => t.partitionId -> idx }.toMap
   val numTasks = tasks.length
   val copiesRunning = new Array[Int](numTasks)
 
@@ -154,7 +156,7 @@ private[spark] class TaskSetManager(
   private[scheduler] val speculatableTasks = new HashSet[Int]
 
   // Task index, start and finish time for each task attempt (indexed by task ID)
-  private val taskInfos = new HashMap[Long, TaskInfo]
+  private[scheduler] val taskInfos = new HashMap[Long, TaskInfo]
 
   // Use a MedianHeap to record durations of successful tasks so we know when to launch
   // speculative tasks. This is only used when speculation is enabled, to avoid the overhead
@@ -755,6 +757,9 @@ private[spark] class TaskSetManager(
       logInfo("Ignoring task-finished event for " + info.id + " in stage " + taskSet.id +
         " because task " + index + " has already completed successfully")
     }
+    // There may be multiple tasksets for this stage -- we let all of them know that the partition
+    // was completed.  This may result in some of the tasksets getting completed.
+    sched.markPartitionCompletedInAllTaskSets(stageId, tasks(index).partitionId)
     // This method is called by "TaskSchedulerImpl.handleSuccessfulTask" which holds the
     // "TaskSchedulerImpl" lock until exiting. To avoid the SPARK-7655 issue, we should not
     // "deserialize" the value when holding a lock to avoid blocking other threads. So we call
@@ -763,6 +768,19 @@ private[spark] class TaskSetManager(
     // here "result.value()" just returns the value and won't block other threads.
     sched.dagScheduler.taskEnded(tasks(index), Success, result.value(), result.accumUpdates, info)
     maybeFinishTaskSet()
+  }
+
+  private[scheduler] def markPartitionCompleted(partitionId: Int): Unit = {
+    partitionToIndex.get(partitionId).foreach { index =>
+      if (!successful(index)) {
+        tasksSuccessful += 1
+        successful(index) = true
+        if (tasksSuccessful == numTasks) {
+          isZombie = true
+        }
+        maybeFinishTaskSet()
+      }
+    }
   }
 
   /**
