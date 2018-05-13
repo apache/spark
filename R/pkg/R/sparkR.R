739ca64fdf730c1328d0b33dab07d0df02424791
Felix Cheung <felixcheung_m@hotmail.com>
2018-05-13 14:24:05 -0700

    change generic to work with other toJSON fix lintr

--- R/pkg/R/sparkR.R
+++ R/pkg/R/sparkR.R
@@ -194,7 +194,7 @@ sparkR.sparkContext <- function(
 
     # Don't use readString() so that we can provide a useful
     # error message if the R and Java versions are mismatched.
-    authSecretLen = readInt(f)
+    authSecretLen <- readInt(f)
     if (length(authSecretLen) == 0 || authSecretLen == 0) {
       stop("Unexpected EOF in JVM connection data. Mismatched versions?")
     }
