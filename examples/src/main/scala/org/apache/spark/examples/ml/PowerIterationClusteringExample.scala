a718d4a6f7ea03ffea22eba6f55d99006c482606
Shahid <shahidki31@gmail.com>
2018-05-22 01:56:16 +0530

    Example code for Power Iteration Clustering

--- examples/src/main/scala/org/apache/spark/examples/ml/PowerIterationClusteringExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/ml/PowerIterationClusteringExample.scala
@@ -18,13 +18,10 @@
 // scalastyle:off println
 package org.apache.spark.examples.ml
 
-import org.apache.log4j.{Level, Logger}
-
 // $example on$
 import org.apache.spark.ml.clustering.PowerIterationClustering
+import org.apache.spark.sql.SparkSession
 // $example off$
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-
 
  /**
   * An example demonstrating power iteration clustering.
@@ -36,79 +33,33 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object PowerIterationClusteringExample {
 
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName(s"${this.getClass.getSimpleName}")
-      .getOrCreate()
+   def main(args: Array[String]): Unit = {
+     val spark = SparkSession
+       .builder
+       .appName(s"${this.getClass.getSimpleName}")
+       .getOrCreate()
 
-    Logger.getRootLogger.setLevel(Level.WARN)
+     // $example on$
+     val dataset = spark.createDataFrame(Seq(
+       (0L, Array(1L, 2L, 4L), Array(0.9, 0.9, 0.1)),
+       (1L, Array(0L, 2L), Array(0.9, 0.9)),
+       (2L, Array(0L, 1L), Array(0.9, 0.9)),
+       (3L, Array(4L), Array(0.9)),
+       (4L, Array(0L, 3L), Array(0.1, 0.9))
+     )).toDF("id", "neighbors", "similarities")
 
-    // $example on$
+     // Trains a PIC model.
+     val model = new PowerIterationClustering().
+       setK(2).
+       setInitMode("degree").
+       setMaxIter(20)
 
-    // Generates data.
-    val radius1 = 1.0
-    val numPoints1 = 5
-    val radius2 = 4.0
-    val numPoints2 = 20
+     val prediction = model.transform(dataset).select("id", "prediction")
 
-    val dataset = generatePICData(spark, radius1, radius2, numPoints1, numPoints2)
+     //  Shows the cluster assignment
+     prediction.show()
+     // $example off$
 
-    // Trains a PIC model.
-    val model = new PowerIterationClustering().
-      setK(2).
-      setInitMode("degree").
-      setMaxIter(20)
-
-    val prediction = model.transform(dataset).select("id", "prediction")
-
-    //  Shows the result.
-    //  println("Cluster Assignment: ")
-    val result = prediction.collect().map {
-      row => (row(1), row(0))
-    }.groupBy(_._1).mapValues(_.map(_._2))
-
-    result.foreach {
-      case (cluster, points) => println(s"$cluster -> [${points.mkString(",")}]")
-    }
-    // $example off$
-
-    spark.stop()
-  }
-
-  def generatePICData(spark: SparkSession,
-                      r1: Double,
-                      r2: Double,
-                      n1: Int,
-                      n2: Int): DataFrame = {
-    val n = n1 + n2
-    val points = genCircle(r1, n1) ++ genCircle(r2, n2)
-
-    val rows = for (i <- 0 until n) yield {
-      val neighbors = for (j <- 0 until i) yield {
-        j.toLong
-      }
-      val similarities = for (j <- 0 until i) yield {
-        sim(points(i), points(j))
-      }
-      (i.toLong, neighbors.toArray, similarities.toArray)
-    }
-    spark.createDataFrame(rows).toDF("id", "neighbors", "similarities")
-  }
-
-  /** Generates a circle of points. */
-  private def genCircle(r: Double, n: Int): Array[(Double, Double)] = {
-    Array.tabulate(n) { i =>
-      val theta = 2.0 * math.Pi * i / n
-      (r * math.cos(theta), r * math.sin(theta))
-    }
-  }
-
-  /** Computes Gaussian similarity. */
-  private def sim(x: (Double, Double), y: (Double, Double)): Double = {
-    val dist2 = (x._1 - y._1) * (x._1 - y._1) + (x._2 - y._2) * (x._2 - y._2)
-    math.exp(-dist2 / 2.0)
-  }
-}
-
-// scalastyle:on println
+     spark.stop()
+   }
+ }
