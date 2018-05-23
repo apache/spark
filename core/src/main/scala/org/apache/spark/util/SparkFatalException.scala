f5a5a812ee32994244d29a27ddeb5730748cd212
jinxing <jinxing6042@126.com>
2018-05-23 21:22:50 +0800

    refine doc and defensive code.

--- core/src/main/scala/org/apache/spark/util/SparkFatalException.scala
+++ core/src/main/scala/org/apache/spark/util/SparkFatalException.scala
@@ -20,5 +20,8 @@ package org.apache.spark.util
  * SPARK-24294: To bypass scala bug: https://github.com/scala/bug/issues/9554, we catch
  * fatal throwable in {@link scala.concurrent.Future}'s body, and re-throw
  * SparkFatalException, which wraps the fatal throwable inside.
+ * Note that SparkFatalException should only be thrown from a {@link scala.concurrent.Future},
+ * which is run by using ThreadUtils.awaitResult. ThreadUtils.awaitResult will catch
+ * it and re-throw the original exception/error.
  */
 private[spark] final class SparkFatalException(val throwable: Throwable) extends Exception
