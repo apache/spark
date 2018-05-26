536e27102888efa2146ec889ad68ed7e10f5e560
Gengliang Wang <gengliang.wang@databricks.com>
2018-05-26 20:32:55 +0800

    add bucket

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetWriteBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetWriteBenchmark.scala
@@ -31,6 +31,8 @@ import org.apache.spark.util.{Benchmark, Utils}
  *  spark-submit --class <this class> --jars <spark sql test jar>
  */
 object ParquetWriteBenchmark {
+  val tempTable = "temp"
+  val format = "orc"
   val conf = new SparkConf()
   conf.set("spark.sql.parquet.compression.codec", "snappy")
 
@@ -53,6 +55,14 @@ object ParquetWriteBenchmark {
     try f finally tableNames.foreach(spark.catalog.dropTempView)
   }
 
+  def withTable(tableNames: String*)(f: => Unit): Unit = {
+    try f finally {
+      tableNames.foreach { name =>
+        spark.sql(s"DROP TABLE IF EXISTS $name")
+      }
+    }
+  }
+
   def withSQLConf(pairs: (String, String)*)(f: => Unit): Unit = {
     val (keys, values) = pairs.unzip
     val currentValues = keys.map(key => Try(spark.conf.get(key)).toOption)
@@ -65,13 +75,13 @@ object ParquetWriteBenchmark {
     }
   }
 
-  def runSQL(name: String, sql: String, values: Int): Unit = {
-    withTempTable("t1") {
-      spark.range(values).createOrReplaceTempView("t1")
+  def runSQL(values: Int, name: String, sql: String, table: String = "t"): Unit = {
+    withTempTable(tempTable) {
+      spark.range(values).createOrReplaceTempView(tempTable)
       val benchmark = new Benchmark(name, values)
       benchmark.addCase("Parquet Writer") { _ =>
-        withTempPath { dir =>
-          spark.sql(sql).write.parquet(dir.getCanonicalPath)
+        withTable(table) {
+          spark.sql(sql)
         }
       }
       benchmark.run()
@@ -86,9 +96,9 @@ object ParquetWriteBenchmark {
     ------------------------------------------------------------------------------------------------
     Parquet Writer                                2536 / 2610          6.2         161.3       1.0X
     */
-    runSQL(name = "Output Single Int Column",
-      sql = "select cast(id as INT) as id from t1",
-      values = values)
+    runSQL(values = values,
+      name = "Output Single Int Column",
+      sql = s"create table t using $format as select cast(id as INT) as id from $tempTable")
   }
 
   def intStringWriteBenchmark(values: Int): Unit = {
@@ -99,36 +109,37 @@ object ParquetWriteBenchmark {
     ------------------------------------------------------------------------------------------------
     Parquet Writer                                4644 / 4673          2.3         442.9       1.0X
     */
-    runSQL(name = "Output Int and String Column",
-      sql = "select cast(id as INT) as c1, cast(id as STRING) as c2 from t1",
-      values = values)
+    runSQL(values = values,
+      name = "Output Int and String Column",
+      sql = s"create table t using $format as select cast(id as INT)" +
+        s" as c1, cast(id as STRING) as c2 from $tempTable")
   }
 
   def partitionTableWriteBenchmark(values: Int): Unit = {
-    withTempTable("t1") {
-      spark.range(values).createOrReplaceTempView("t1")
-      val benchmark = new Benchmark("Partitioned Table", values)
-      benchmark.addCase("Parquet Writer") { _ =>
-        withTempPath { dir =>
-          spark.sql("select id % 2 as p, cast(id as INT) as id from t1")
-            .write.partitionBy("p").parquet(dir.getCanonicalPath)
-        }
-      }
+    /*
+    Intel(R) Core(TM) i7-6920HQ CPU @ 2.90GHz
 
-      /*
-      Intel(R) Core(TM) i7-6920HQ CPU @ 2.90GHz
+    Partitioned Table:                    Best/Avg Time(ms)    Rate(M/s)   Per Row(ns)   Relative
+    ---------------------------------------------------------------------------------------------
+    Parquet Writer                             4163 / 4173          3.8         264.7       1.0X
+    */
+    runSQL(values = values,
+      name = "Partitioned Table",
+      sql = s"create table t using $format partitioned by (p) as select id % 2 as p," +
+        s" cast(id as INT) as id from $tempTable")
+  }
 
-      Partitioned Table:                    Best/Avg Time(ms)    Rate(M/s)   Per Row(ns)   Relative
-      ---------------------------------------------------------------------------------------------
-      Parquet Writer                             4163 / 4173          3.8         264.7       1.0X
-      */
-      benchmark.run()
-    }
+  def clusteredTableWriteBenchmark(values: Int): Unit = {
+    runSQL(values = values,
+      name = "Clustered Table",
+      sql = s"create table t using $format CLUSTERED by (p) INTO 8 buckets as select id as p," +
+        s" cast(id as INT) as id from $tempTable")
   }
 
   def main(args: Array[String]): Unit = {
     intWriteBenchmark(1024 * 1024 * 15)
     intStringWriteBenchmark(1024 * 1024 * 10)
     partitionTableWriteBenchmark(1024 * 1024 * 15)
+    clusteredTableWriteBenchmark(1024 * 1024 * 15)
   }
 }
