1b36f148891ac41ef36a40366f87dd5405cb3751
Marco Gaido <marcogaido91@gmail.com>
2018-05-30 11:18:04 -0700

    [SPARK-23901][SQL] Add masking functions
    ## What changes were proposed in this pull request?
    
    The PR adds the masking function as they are described in Hive's documentation: https://cwiki.apache.org/confluence/display/Hive/LanguageManual+UDF#LanguageManualUDF-DataMaskingFunctions.
    This means that only `string`s are accepted as parameter for the masking functions.
    
    ## How was this patch tested?
    
    added UTs
    
    Author: Marco Gaido <marcogaido91@gmail.com>
    
    Closes #21246 from mgaido91/SPARK-23901.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
@@ -432,6 +432,14 @@ object FunctionRegistry {
     expression[ArrayRepeat]("array_repeat"),
     CreateStruct.registryEntry,
 
+    // mask functions
+    expression[Mask]("mask"),
+    expression[MaskFirstN]("mask_first_n"),
+    expression[MaskLastN]("mask_last_n"),
+    expression[MaskShowFirstN]("mask_show_first_n"),
+    expression[MaskShowLastN]("mask_show_last_n"),
+    expression[MaskHash]("mask_hash"),
+
     // misc functions
     expression[AssertTrue]("assert_true"),
     expression[Crc32]("crc32"),
