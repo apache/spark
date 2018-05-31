dc24da23341eafb8163ce5292166048750b7f6ed
Sean Owen <srowen@gmail.com>
2018-05-31 09:34:39 -0700

    [WEBUI] Avoid possibility of script in query param keys
    As discussed separately, this avoids the possibility of XSS on certain request param keys.
    
    CC vanzin
    
    Author: Sean Owen <srowen@gmail.com>
    
    Closes #21464 from srowen/XSS2.
    
    (cherry picked from commit 698b9a0981f0ec322e15d6ac89cc38c8f49ed33d)
    Signed-off-by: Marcelo Vanzin <vanzin@cloudera.com>

--- core/src/main/scala/org/apache/spark/ui/jobs/AllJobsPage.scala
+++ core/src/main/scala/org/apache/spark/ui/jobs/AllJobsPage.scala
@@ -206,7 +206,9 @@ private[ui] class AllJobsPage(parent: JobsTab, store: AppStatusStore) extends We
       jobs: Seq[v1.JobData],
       killEnabled: Boolean): Seq[Node] = {
     // stripXSS is called to remove suspicious characters used in XSS attacks
-    val allParameters = request.getParameterMap.asScala.toMap.mapValues(_.map(UIUtils.stripXSS))
+    val allParameters = request.getParameterMap.asScala.toMap.map { case (k, v) =>
+      UIUtils.stripXSS(k) -> v.map(UIUtils.stripXSS).toSeq
+    }
     val parameterOtherTable = allParameters.filterNot(_._1.startsWith(jobTag))
       .map(para => para._1 + "=" + para._2(0))
 
