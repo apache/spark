67ae6ebaebbb537f24db51be1f96759d5c6463ff
Lee Dongjin <dongjin@apache.org>
2018-06-06 16:43:45 +0900

    Implement pyspark parity

--- python/pyspark/ml/tests.py
+++ python/pyspark/ml/tests.py
@@ -681,6 +681,13 @@ class FeatureTests(SparkSessionTestCase):
         self.assertEqual(stopWordRemover.getStopWords(), stopwords)
         transformedDF = stopWordRemover.transform(dataset)
         self.assertEqual(transformedDF.head().output, [])
+        # with locale
+        stopwords = ["BELKİ"]
+        dataset = sqlContext.createDataFrame([Row(input=["belki"])])
+        stopWordRemover.setStopWords(stopwords).setLocale("tr")
+        self.assertEqual(stopWordRemover.getStopWords(), stopwords)
+        transformedDF = stopWordRemover.transform(dataset)
+        self.assertEqual(transformedDF.head().output, [])
 
     def test_count_vectorizer_with_binary(self):
         dataset = self.spark.createDataFrame([
