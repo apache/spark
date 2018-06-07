1c5feed81628f077a174def353fed38caa56d01c
Shahid <shahidki31@gmail.com>
2018-06-07 23:36:32 +0530

    Example code for Power Iteration Clustering

--- examples/src/main/java/org/apache/spark/examples/ml/JavaPowerIterationClusteringExample.java
+++ examples/src/main/java/org/apache/spark/examples/ml/JavaPowerIterationClusteringExample.java
@@ -46,7 +46,6 @@ public class JavaPowerIterationClusteringExample {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("JavaPowerIterationClustering")
-                .master("local")
                 .getOrCreate();
 
         // $example on$
