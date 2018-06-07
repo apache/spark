90450e0950b334af4373c247fe3b0ed0cc6fe6c0
Shahid <shahidki31@gmail.com>
2018-06-08 01:18:36 +0530

    Example code for Power Iteration Clustering

--- examples/src/main/java/org/apache/spark/examples/ml/JavaPowerIterationClusteringExample.java
+++ examples/src/main/java/org/apache/spark/examples/ml/JavaPowerIterationClusteringExample.java
@@ -32,53 +32,40 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 // $example off$
 
-/**
- * An example demonstrating PowerIterationClusteringExample.
- * Run with
- * <pre>
- * bin/run-example ml.JavaPowerIterationClusteringExample
- * </pre>
- */
 public class JavaPowerIterationClusteringExample {
+  public static void main(String[] args) {
+    // Create a SparkSession.
+    SparkSession spark = SparkSession
+      .builder()
+      .appName("JavaPowerIterationClustering")
+      .getOrCreate();
 
-    public static void main(String[] args) {
-        // Create a SparkSession.
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("JavaPowerIterationClustering")
-                .getOrCreate();
+    // $example on$
+    List<Row> data = Arrays.asList(
+      RowFactory.create(0L, 1L, 1.0),
+      RowFactory.create(0L, 2L, 1.0),
+      RowFactory.create(1L, 2L, 1.0),
+      RowFactory.create(3L, 4L, 1.0),
+      RowFactory.create(4L, 0L, 0.1)
+    );
 
-        // $example on$
-        List<Row> data = Arrays.asList(
-                RowFactory.create(0L, 1L, 1.0),
-                RowFactory.create(0L, 2L, 1.0),
-                RowFactory.create(1L, 2L, 1.0),
-                RowFactory.create(3L, 4L, 1.0),
-                RowFactory.create(4L, 0L, 0.1)
-        );
+    StructType schema = new StructType(new StructField[]{
+      new StructField("src", DataTypes.LongType, false, Metadata.empty()),
+      new StructField("dst", DataTypes.LongType, false, Metadata.empty()),
+      new StructField("weight", DataTypes.DoubleType, false, Metadata.empty())
+    });
 
-        StructType schema = new StructType(new StructField[]{
-                new StructField("src", DataTypes.LongType, false, Metadata.empty()),
-                new StructField("dst", DataTypes.LongType,  false, Metadata.empty()),
-                new StructField("weight", DataTypes.DoubleType, false, Metadata.empty())
-        });
+    Dataset<Row> df = spark.createDataFrame(data, schema);
 
-        Dataset<Row> df = spark.createDataFrame(data, schema);
+    PowerIterationClustering model = new PowerIterationClustering()
+      .setK(2)
+      .setMaxIter(10)
+      .setInitMode("degree")
+      .setWeightCol("weight");
 
-        Dataset<Row> result = new PowerIterationClustering()
-                .setK(2)
-                .setMaxIter(10)
-                .setInitMode("degree")
-                .setWeightCol("weight")
-                .assignClusters(df);
-
-        // Printing results
-        System.out.println("Clustering results [id, cluster]");
-        for (Row row : result.select("id", "cluster").collectAsList()) {
-            System.out.println("[" + row.get(0) + ", " + row.get(1) + "]");
-        }
-        // $example off$
-
-        spark.stop();
-    }
+    Dataset<Row> result = model.assignClusters(df);
+    result.show(false);
+    // $example off$
+    spark.stop();
+  }
 }
