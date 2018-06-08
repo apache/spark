ab81767d7504bc3016786780902d8c3997e37f64
arash-rai <arash.rai@airbnb.com>
2018-06-08 11:50:05 -0700

    [AIRFLOW-2578] Add option to use proxies in JiraHook
    Closes #3480 from arashrai/master

--- airflow/contrib/hooks/jira_hook.py
+++ airflow/contrib/hooks/jira_hook.py
@@ -32,9 +32,11 @@ class JiraHook(BaseHook, LoggingMixin):
     :type jira_conn_id: string
     """
     def __init__(self,
-                 jira_conn_id='jira_default'):
+                 jira_conn_id='jira_default',
+                 proxies=None):
         super(JiraHook, self).__init__(jira_conn_id)
         self.jira_conn_id = jira_conn_id
+        self.proxies = proxies
         self.client = None
         self.get_conn()
 
@@ -73,7 +75,8 @@ class JiraHook(BaseHook, LoggingMixin):
                                        options=extra_options,
                                        basic_auth=(conn.login, conn.password),
                                        get_server_info=get_server_info,
-                                       validate=validate)
+                                       validate=validate,
+                                       proxies=self.proxies)
                 except JIRAError as jira_error:
                     raise AirflowException('Failed to create jira client, jira error: %s'
                                            % str(jira_error))
