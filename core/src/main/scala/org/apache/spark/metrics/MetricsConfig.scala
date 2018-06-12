836ffdec985eac62396005726d5fb49a9e3455a5
John Zhuge <jzhuge@netflix.com>
2018-06-11 22:27:56 -0700

    [SPARK-5152][CORE] Read metrics config file from Hadoop file system
    Enable metrics config file to be read directly from Hadoop file system.
    ```
    spark-submit --conf spark.metrics.conf=hdfs://<nn>:8020/config/spark-metrics.properties
    spark-submit --conf spark.metrics.conf=s3a://<bucket>/config/spark-metrics.properties
    ```
    
    There is no need to distribute the config file if either HDFS or S3A is used.
    
    If no URI scheme is specified, assume file://. This ensures backwards compatibility.
    
    Unit tests:
    - MetricsSystemSuite
    - MetricsConfigSuite
    - TaskContextSuite
    
    Manual tests:
    - Local file without URI scheme
    - Local file with file://
    - HDFS file path
    - S3 file path
    
    Author: John Zhuge <jzhuge@apache.org>

--- core/src/main/scala/org/apache/spark/metrics/MetricsConfig.scala
+++ core/src/main/scala/org/apache/spark/metrics/MetricsConfig.scala
@@ -20,6 +20,8 @@ package org.apache.spark.metrics
 import java.io.{FileInputStream, InputStream}
 import java.util.Properties
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 import scala.util.matching.Regex
@@ -129,8 +131,11 @@ private[spark] class MetricsConfig(conf: SparkConf) extends Logging {
     var is: InputStream = null
     try {
       is = path match {
-        case Some(f) => new FileInputStream(f)
-        case None => Utils.getSparkClassLoader.getResourceAsStream(DEFAULT_METRICS_CONF_FILENAME)
+        case Some(f) =>
+          val hadoopPath = new Path(Utils.resolveURI(f))
+          Utils.getHadoopFileSystem(hadoopPath.toUri, new Configuration()).open(hadoopPath)
+        case None =>
+          Utils.getSparkClassLoader.getResourceAsStream(DEFAULT_METRICS_CONF_FILENAME)
       }
 
       if (is != null) {
