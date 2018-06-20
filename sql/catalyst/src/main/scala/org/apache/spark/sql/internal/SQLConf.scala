64a1b7dc759736c72530b6ca5dff3a86a953829a
Maxim Gekk <maxim.gekk@databricks.com>
2018-06-20 20:03:08 +0200

    Changing config description

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1316,7 +1316,7 @@ object SQLConf {
     .createWithDefault(true)
 
   val LEGACY_SIZE_OF_NULL = buildConf("spark.sql.legacy.sizeOfNull")
-    .doc("If it is set to true, size of null returns -1. This is legacy behavior of Hive. " +
+    .doc("If it is set to true, size of null returns -1. This behavior was inherited from Hive. " +
       "The size function returns null for null input if the flag is disabled.")
     .booleanConf
     .createWithDefault(true)
