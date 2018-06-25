9a25ee3aee476919cb2dd97a320556c6dcad0a4d
Marco Gaido <marcogaido91@gmail.com>
2018-06-25 23:44:20 +0800

    [SPARK-24633][SQL] Fix codegen when split is required for arrays_zip
    ## What changes were proposed in this pull request?
    
    In function array_zip, when split is required by the high number of arguments, a codegen error can happen.
    
    The PR fixes codegen for cases when splitting the code is required.
    
    ## How was this patch tested?
    
    added UT
    
    Author: Marco Gaido <marcogaido91@gmail.com>
    
    Closes #21621 from mgaido91/SPARK-24633.

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -556,6 +556,17 @@ class DataFrameFunctionsSuite extends QueryTest with SharedSQLContext {
     checkAnswer(df8.selectExpr("arrays_zip(v1, v2)"), expectedValue8)
   }
 
+  test("SPARK-24633: arrays_zip splits input processing correctly") {
+    Seq("true", "false").foreach { wholestageCodegenEnabled =>
+      withSQLConf(SQLConf.WHOLESTAGE_CODEGEN_ENABLED.key -> wholestageCodegenEnabled) {
+        val df = spark.range(1)
+        val exprs = (0 to 5).map(x => array($"id" + lit(x)))
+        checkAnswer(df.select(arrays_zip(exprs: _*)),
+          Row(Seq(Row(0, 1, 2, 3, 4, 5))))
+      }
+    }
+  }
+
   test("map size function") {
     val df = Seq(
       (Map[Int, Int](1 -> 1, 2 -> 2), "x"),
