83ed0823fbd4219c5462f0991509b08507ee10c6
Takeshi Yamamuro <yamamuro@apache.org>
2018-06-29 10:47:24 +0900

    Fix

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/csv/CSVSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/csv/CSVSuite.scala
@@ -1586,8 +1586,12 @@ class CSVSuite extends QueryTest with SharedSQLContext with SQLTestUtils with Te
         val dir = path.getAbsolutePath
         spark.range(10).selectExpr("id % 2 AS p", "id AS c0", "id AS c1").write.partitionBy("p")
           .option("header", "true").csv(dir)
-        val df = spark.read.option("header", "true").csv(dir).selectExpr("sum(p)", "avg(c0)")
-        checkAnswer(df, Row(5, 4.5))
+        var df = spark.read.option("header", true).csv(dir).selectExpr("sum(p)", "count(c0)")
+        checkAnswer(df, Row(5, 10))
+
+        // empty required column case
+        df = spark.read.option("header", true).csv(dir).selectExpr("sum(p)")
+        checkAnswer(df, Row(5))
       }
     }
   }
