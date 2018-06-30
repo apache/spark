640cd4d563dc8d4a70508e290180b54bc9c9eb46
Patrick McGloin <mcgloin.patrick@gmail.com>
2018-06-30 07:57:06 +0200

    Updated documentation of from_json as requested in review

--- sql/core/src/main/scala/org/apache/spark/sql/functions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/functions.scala
@@ -3269,9 +3269,9 @@ object functions {
     from_json(e, schema.asInstanceOf[DataType], options)
 
   /**
-   * (Scala-specific) Parses a column containing a JSON string into a `MapType` with `StringType`
-   * as keys type, `StructType` or `ArrayType` of `StructType`s with the specified schema.
-   * Returns `null`, in the case of an unparseable string.
+   * (Scala-specific) Parses a column containing a JSON string into a `MapType`, `StructType`
+   * or `ArrayType` of `StructType`s with the specified schema. Returns `null`, in the case
+   * of an unparseable string.
    *
    * @param e a string column containing JSON data.
    * @param schema the schema to use when parsing the json string
@@ -3301,9 +3301,9 @@ object functions {
     from_json(e, schema, options.asScala.toMap)
 
   /**
-   * (Java-specific) Parses a column containing a JSON string into a `MapType` with `StringType`
-   * as keys type, `StructType` or `ArrayType` of `StructType`s with the specified schema.
-   * Returns `null`, in the case of an unparseable string.
+   * (Java-specific) Parses a column containing a JSON string into a `MapType`, `StructType`
+   * or `ArrayType` of `StructType`s with the specified schema. Returns `null`, in the case
+   * of an unparseable string.
    *
    * @param e a string column containing JSON data.
    * @param schema the schema to use when parsing the json string
@@ -3330,9 +3330,9 @@ object functions {
     from_json(e, schema, Map.empty[String, String])
 
   /**
-   * Parses a column containing a JSON string into a `MapType` with `StringType` as keys type,
-   * `StructType` or `ArrayType` of `StructType`s with the specified schema.
-   * Returns `null`, in the case of an unparseable string.
+   * Parses a column containing a JSON string into a `MapType`, `StructType` or `ArrayType`
+   * of `StructType`s with the specified schema. Returns `null`, in the case of an unparseable
+   * string.
    *
    * @param e a string column containing JSON data.
    * @param schema the schema to use when parsing the json string
@@ -3344,9 +3344,9 @@ object functions {
     from_json(e, schema, Map.empty[String, String])
 
   /**
-   * (Java-specific) Parses a column containing a JSON string into a `MapType` with `StringType`
-   * as keys type, `StructType` or `ArrayType` of `StructType`s with the specified schema.
-   * Returns `null`, in the case of an unparseable string.
+   * (Java-specific) Parses a column containing a JSON string into a `MapType`, `StructType`
+   * or `ArrayType` of `StructType`s with the specified schema. Returns `null`, in the case of
+   * an unparseable string.
    *
    * @param e a string column containing JSON data.
    * @param schema the schema to use when parsing the json string as a json string. In Spark 2.1,
@@ -3361,9 +3361,9 @@ object functions {
   }
 
   /**
-   * (Scala-specific) Parses a column containing a JSON string into a `MapType` with `StringType`
-   * as keys type, `StructType` or `ArrayType` of `StructType`s with the specified schema.
-   * Returns `null`, in the case of an unparseable string.
+   * (Scala-specific) Parses a column containing a JSON string into a `MapType`, StructType`
+   * or `ArrayType` of `StructType`s with the specified schema. Returns `null`, in the case
+   * of an unparseable string.
    *
    * @param e a string column containing JSON data.
    * @param schema the schema to use when parsing the json string as a json string, it could be a
