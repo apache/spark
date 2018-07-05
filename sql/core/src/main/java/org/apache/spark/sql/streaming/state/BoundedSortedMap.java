2fd7b7ce4f703635c703339fb4990024cfa74255
Jungtaek Lim <kabhwan@gmail.com>
2018-07-05 21:32:02 +0900

    Apply some optimization into putAll, and add various tests on putAll

--- sql/core/src/main/java/org/apache/spark/sql/streaming/state/BoundedSortedMap.java
+++ sql/core/src/main/java/org/apache/spark/sql/streaming/state/BoundedSortedMap.java
@@ -18,22 +18,23 @@ package org.apache.spark.sql.streaming.state;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
 /**
  * This class implements bounded {@link java.util.SortedMap} based on {@link java.util.TreeMap}.
- *
+ * <p/>
  * As TreeMap does, this implementation sorts elements in natural order, and cuts off
  * smaller elements to retain at most bigger N elements.
- *
+ * <p/>
  * You can provide reversed order of comparator to retain smaller elements instead.
- *
+ * <p/>
  * This class is not thread-safe, so synchronization would be needed to use this concurrently.
  *
  * @param <K> key type
  * @param <V> value type
  */
-public class BoundedSortedMap<K, V> extends TreeMap<K, V> {
+public final class BoundedSortedMap<K, V> extends TreeMap<K, V> {
 
   private final int limit;
 
@@ -41,7 +42,7 @@ public class BoundedSortedMap<K, V> extends TreeMap<K, V> {
    * Constructor
    *
    * @param comparator comparator instance to compare between keys
-   * @param limit      bounded size
+   * @param limit bounded size
    */
   public BoundedSortedMap(Comparator<K> comparator, int limit) {
     super(comparator);
@@ -49,24 +50,20 @@ public class BoundedSortedMap<K, V> extends TreeMap<K, V> {
   }
 
   @Override
-  public void putAll(Map<? extends K, ? extends V> map) {
-    for (Map.Entry<? extends K, ? extends V> entry : map.entrySet()) {
-      put(entry.getKey(), entry.getValue());
-    }
-  }
-
-  @Override
   public V put(K key, V value) {
     // This method doesn't guarantee thread-safety, so it must be guarded with synchronization
-
-    while (size() > limit) {
-      remove(lastKey());
+    if (size() > limit) {
+      throw new IllegalStateException("BoundedSortedMap is broken: already out of bound.");
     }
 
     if (size() == limit) {
       K lk = lastKey();
-      if (comparator().compare(lk, key) > 0) {
+      int comp = comparator().compare(lk, key);
+      if (comp > 0) {
         remove(lk);
+        return super.put(key, value);
+      } else if (comp == 0) {
+        // just overwrite it without explicitly removing
         return super.put(key, value);
       }
 
@@ -76,4 +73,74 @@ public class BoundedSortedMap<K, V> extends TreeMap<K, V> {
 
     return super.put(key, value);
   }
+
+  @Override
+  public void putAll(Map<? extends K, ? extends V> map) {
+    if (size() > limit) {
+      throw new IllegalStateException("BoundedSortedMap is broken: already out of bound.");
+    }
+
+    if (isApplicableOfPutAll0(map)) {
+      SortedMap<? extends K, ? extends V> paramMap = (SortedMap<? extends K, ? extends V>) map;
+      putAll0(paramMap);
+    } else {
+      // fail back to put all entries one by one
+      for (Map.Entry<? extends K, ? extends V> entry : map.entrySet()) {
+        put(entry.getKey(), entry.getValue());
+      }
+    }
+  }
+
+  private boolean isApplicableOfPutAll0(Map<? extends K, ? extends V> map) {
+    if (map instanceof SortedMap) {
+      SortedMap<? extends K, ? extends V> paramMap = (SortedMap<? extends K, ? extends V>) map;
+
+      return (paramMap.comparator() == null && comparator() == null) || (
+          paramMap.comparator() != null && paramMap.comparator().equals(comparator()));
+    }
+
+    return false;
+  }
+
+  private void putAll0(SortedMap<? extends K, ? extends V> map) {
+    // assuming isApplicableOfPutAll0 returns true, which means comparators in both maps
+    // guarantee same ordering of key
+
+    // if first key of this map is bigger (if it's natural ordering) than last key of parameter,
+    // all of elements in this map will be evicted.
+    // clear the map and put parameter's entries until reaching limit.
+    K fk = firstKey();
+    if (comparator().compare(fk, map.lastKey()) > 0) {
+      clear();
+      for (Map.Entry<? extends K, ? extends V> entry : map.entrySet()) {
+        // safe to directly call super.put
+        super.put(entry.getKey(), entry.getValue());
+
+        if (size() == limit) {
+          break;
+        }
+      }
+
+      return;
+    }
+
+    for (Map.Entry<? extends K, ? extends V> entry : map.entrySet()) {
+      if (size() == limit) {
+        K lk = lastKey();
+
+        // if the map is reached the limit and last key of this map is smaller than first key of
+        // parameter map, no need to add remaining entries from parameter map.
+        if (comparator().compare(lk, entry.getKey()) < 0) {
+          return;
+        } else {
+          // remove last key to ensure free space before putting new entity
+          remove(lk);
+        }
+      }
+
+      // safe to directly call super.put
+      super.put(entry.getKey(), entry.getValue());
+    }
+  }
+
 }
--- sql/core/src/test/java/test/org/apache/spark/sql/streaming/state/BoundedSortedMapSuite.java
+++ sql/core/src/test/java/test/org/apache/spark/sql/streaming/state/BoundedSortedMapSuite.java
@@ -16,11 +16,20 @@
  */
 package test.org.apache.spark.sql.streaming.state;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.spark.sql.streaming.state.BoundedSortedMap;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class BoundedSortedMapSuite {
 
@@ -32,9 +41,8 @@ public class BoundedSortedMapSuite {
     map.put(2, 2);
     map.put(1, 1);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(1), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(2), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 2));
+    assertMap(map, expected);
   }
 
   @Test
@@ -47,9 +55,8 @@ public class BoundedSortedMapSuite {
 
     map.put(0, 0);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(0), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(1), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(0, 0), IntPair.of(1, 1));
+    assertMap(map, expected);
   }
 
   @Test
@@ -63,9 +70,8 @@ public class BoundedSortedMapSuite {
     // 3 is being cut off
     map.put(2, 2);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(1), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(2), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 2));
+    assertMap(map, expected);
   }
 
   @Test
@@ -77,11 +83,24 @@ public class BoundedSortedMapSuite {
     map.put(1, 1);
 
     // 3 is not added
-    Assert.assertNull(map.put(3, 3));
+    map.put(3, 3);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(1), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(2), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 2));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testOverwriteBiggestOfElementToFullOfMap() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.naturalOrder(), 2);
+
+    map.put(2, 2);
+    map.put(1, 1);
+
+    map.put(2, 3);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 3));
+    assertMap(map, expected);
   }
 
   @Test
@@ -92,9 +111,8 @@ public class BoundedSortedMapSuite {
     map.put(2, 2);
     map.put(1, 1);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(2), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(1), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(2, 2), IntPair.of(1, 1));
+    assertMap(map, expected);
   }
 
   @Test
@@ -108,9 +126,8 @@ public class BoundedSortedMapSuite {
     // 1 is being cut off
     map.put(3, 3);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(3), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(2), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(3, 3), IntPair.of(2, 2));
+    assertMap(map, expected);
   }
 
   @Test
@@ -124,9 +141,8 @@ public class BoundedSortedMapSuite {
     // 1 is being cut off
     map.put(2, 2);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(3), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(2), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(3, 3), IntPair.of(2, 2));
+    assertMap(map, expected);
   }
 
   @Test
@@ -138,10 +154,247 @@ public class BoundedSortedMapSuite {
     map.put(2, 2);
 
     // 1 is not added
-    Assert.assertNull(map.put(1, 1));
+    map.put(1, 1);
 
-    Assert.assertEquals(2, map.size());
-    Assert.assertEquals(Integer.valueOf(3), map.firstKey());
-    Assert.assertEquals(Integer.valueOf(2), map.lastKey());
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(3, 3), IntPair.of(2, 2));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testReversedOrderOverwriteSmallestOfElementToFullOfMap() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.reverseOrder(), 2);
+
+    map.put(3, 3);
+    map.put(2, 2);
+
+    map.put(2, 3);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(3, 3), IntPair.of(2, 3));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testPutAllNonSortedMap() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.naturalOrder(), 3);
+
+    map.put(1, 1);
+    map.put(2, 2);
+
+    Map<Integer, Integer> param = new HashMap<>();
+    param.put(2, 12);
+    param.put(1, 11);
+    param.put(3, 13);
+    param.put(4, 14);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 11), IntPair.of(2, 12),
+        IntPair.of(3, 13));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testPutAllSortedMapWithSameOrderingAllKeysAreSmallerThanFirstKey() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.naturalOrder(), 3);
+
+    map.put(7, 7);
+    map.put(8, 8);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.naturalOrder());
+    param.put(1, 11);
+    param.put(2, 12);
+    param.put(3, 13);
+    param.put(4, 14);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 11), IntPair.of(2, 12),
+        IntPair.of(3, 13));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testPutAllSortedMapWithSameOrderingAllKeysAreBiggerThanLastKey() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.naturalOrder(), 3);
+
+    map.put(1, 1);
+    map.put(2, 2);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.naturalOrder());
+    param.put(3, 13);
+    param.put(4, 14);
+    param.put(5, 15);
+    param.put(6, 16);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 2),
+        IntPair.of(3, 13));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testPutAllSortedMapWithSameOrderingOverlappedKeyRange() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.naturalOrder(), 4);
+
+    map.put(1, 1);
+    map.put(3, 3);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.naturalOrder());
+    param.put(2, 12);
+    param.put(4, 14);
+    param.put(6, 16);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 12),
+        IntPair.of(3, 3), IntPair.of(4, 14));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testPutAllSortedMapWithDifferentOrdering() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.naturalOrder(), 4);
+
+    map.put(1, 1);
+    map.put(3, 3);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.reverseOrder());
+    param.put(6, 16);
+    param.put(4, 14);
+    param.put(2, 12);
+
+    map.putAll(param);
+
+    // respecting map's ordering
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(1, 1), IntPair.of(2, 12),
+        IntPair.of(3, 3), IntPair.of(4, 14));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testReversedOrderPutAllNonSortedMap() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.reverseOrder(), 3);
+
+    map.put(2, 2);
+    map.put(1, 1);
+
+    Map<Integer, Integer> param = new HashMap<>();
+    param.put(2, 12);
+    param.put(1, 11);
+    param.put(3, 13);
+    param.put(4, 14);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(4, 14), IntPair.of(3, 13),
+        IntPair.of(2, 12));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testReversedOrderPutAllSortedMapWithSameOrderingAllKeysAreSmallerThanLastKey() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.reverseOrder(), 3);
+
+    map.put(9, 9);
+    map.put(8, 8);
+    map.put(7, 7);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.reverseOrder());
+    param.put(1, 11);
+    param.put(2, 12);
+    param.put(3, 13);
+    param.put(4, 14);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(9, 9), IntPair.of(8, 8),
+        IntPair.of(7, 7));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testReversedOrderPutAllSortedMapWithSameOrderingAllKeysAreBiggerThanFirstKey() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.reverseOrder(), 3);
+
+    map.put(2, 2);
+    map.put(1, 1);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.reverseOrder());
+    param.put(3, 13);
+    param.put(4, 14);
+    param.put(5, 15);
+    param.put(6, 16);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(6, 16), IntPair.of(5, 15),
+        IntPair.of(4, 14));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testReversedOrderPutAllSortedMapWithSameOrderingOverlappedKeyRange() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.reverseOrder(), 4);
+
+    map.put(3, 3);
+    map.put(1, 1);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.reverseOrder());
+    param.put(6, 16);
+    param.put(4, 14);
+    param.put(2, 12);
+
+    map.putAll(param);
+
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(6, 16), IntPair.of(4, 14),
+        IntPair.of(3, 3), IntPair.of(2, 12));
+    assertMap(map, expected);
+  }
+
+  @Test
+  public void testReversedOrderPutAllSortedMapWithDifferentOrdering() {
+    BoundedSortedMap<Integer, Integer> map = new BoundedSortedMap<Integer, Integer>(
+        Comparator.reverseOrder(), 4);
+
+    map.put(3, 3);
+    map.put(1, 1);
+
+    SortedMap<Integer, Integer> param = new TreeMap<>(Comparator.naturalOrder());
+    param.put(2, 12);
+    param.put(4, 14);
+    param.put(6, 16);
+
+    map.putAll(param);
+
+    // respecting map's ordering
+    List<Pair<Integer, Integer>> expected = Lists.newArrayList(IntPair.of(6, 16), IntPair.of(4, 14),
+        IntPair.of(3, 3), IntPair.of(2, 12));
+    assertMap(map, expected);
+  }
+
+  private void assertMap(Map<Integer, Integer> map, List<Pair<Integer, Integer>> expectedEntities) {
+    Assert.assertEquals(expectedEntities.size(), map.size());
+    Assert.assertEquals(expectedEntities.stream().map(Pair::getKey).collect(Collectors.toSet()),
+        map.keySet());
+
+    expectedEntities.forEach(entity -> Assert.assertEquals(entity.getValue(),
+        map.get(entity.getKey())));
+  }
+
+  private static class IntPair {
+    static ImmutablePair of(Integer key, Integer value) {
+      return new ImmutablePair<>(key, value);
+    }
   }
 }
