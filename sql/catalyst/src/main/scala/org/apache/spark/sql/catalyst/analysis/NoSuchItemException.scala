70835b7db99526d77c32fef72f59afe61c76174f
zhoukang <zhoukang199191@gmail.com>
2018-07-12 17:33:46 +0800

    Refine comment

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/NoSuchItemException.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/NoSuchItemException.scala
@@ -43,8 +43,7 @@ class NoSuchPermanentFunctionException(db: String, func: String)
 class NoSuchFunctionException(db: String, func: String, rootCause: Option[String] = None)
   extends AnalysisException(
     s"Undefined function: '$func'. This function is neither a registered temporary function nor " +
-    s"a permanent function registered in the database '$db'." +
-    s" Exception thrown during look up: ${rootCause.getOrElse("None")}")
+    s"a permanent function registered in the database '$db'." + rootCause.getOrElse(""))
 
 class NoSuchPartitionsException(db: String, table: String, specs: Seq[TablePartitionSpec])
   extends AnalysisException(
