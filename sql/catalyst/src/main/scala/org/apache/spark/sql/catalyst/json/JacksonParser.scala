201ef7107bd696f65dff53272b490354e0b88327
Marco Gaido <marcogaido91@gmail.com>
2018-07-19 23:29:29 -0700

    [SPARK-24268][SQL] Use datatype.catalogString in error messages
    ## What changes were proposed in this pull request?
    
    As stated in https://github.com/apache/spark/pull/21321, in the error messages we should use `catalogString`. This is not the case, as SPARK-22893 used `simpleString` in order to have the same representation everywhere and it missed some places.
    
    The PR unifies the messages using alway the `catalogString` representation of the dataTypes in the messages.
    
    ## How was this patch tested?
    
    existing/modified UTs
    
    Author: Marco Gaido <marcogaido91@gmail.com>
    
    Closes #21804 from mgaido91/SPARK-24268_catalog.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JacksonParser.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JacksonParser.scala
@@ -143,7 +143,8 @@ class JacksonParser(
             case "NaN" => Float.NaN
             case "Infinity" => Float.PositiveInfinity
             case "-Infinity" => Float.NegativeInfinity
-            case other => throw new RuntimeException(s"Cannot parse $other as FloatType.")
+            case other => throw new RuntimeException(
+              s"Cannot parse $other as ${FloatType.catalogString}.")
           }
       }
 
@@ -158,7 +159,8 @@ class JacksonParser(
             case "NaN" => Double.NaN
             case "Infinity" => Double.PositiveInfinity
             case "-Infinity" => Double.NegativeInfinity
-            case other => throw new RuntimeException(s"Cannot parse $other as DoubleType.")
+            case other =>
+              throw new RuntimeException(s"Cannot parse $other as ${DoubleType.catalogString}.")
           }
       }
 
