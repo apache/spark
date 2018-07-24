49fd23a3ee0269e2b974648f4a823c1d0b6c12ec
Juhwi Lee <sohappyhwi@gmail.com>
2018-07-24 11:24:31 +0200

    [AIRFLOW-2792] change parameter in post requests.
    Closes #3633 from happyjulie/AIRFLOW-2792

--- airflow/hooks/druid_hook.py
+++ airflow/hooks/druid_hook.py
@@ -64,7 +64,7 @@ class DruidHook(BaseHook):
     def submit_indexing_job(self, json_index_spec):
         url = self.get_conn_url()
 
-        req_index = requests.post(url, data=json_index_spec, headers=self.header)
+        req_index = requests.post(url, json=json_index_spec, headers=self.header)
         if (req_index.status_code != 200):
             raise AirflowException('Did not get 200 when '
                                    'submitting the Druid job to {}'.format(url))
