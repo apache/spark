03c0926bed03bcedd035be3a743d08b664ec5006
Koert Kuipers <koert@tresata.com>
2018-07-25 12:02:47 -0400

    add precendence to doc for spark.sql.sources.partitionOverwriteMode

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1336,8 +1336,10 @@ object SQLConf {
         "those partitions that have data written into it at runtime. By default we use static " +
         "mode to keep the same behavior of Spark prior to 2.3. Note that this config doesn't " +
         "affect Hive serde tables, as they are always overwritten with dynamic mode. This can " +
-        "also be set as an output option for a data source using key partitionOverwriteMode, " +
-        "e.g. dataframe.write.option(\"partitionOverwriteMode\", \"dynamic\").save(path)")
+        "also be set as an output option for a data source using key partitionOverwriteMode " +
+        "(which takes precendence over this setting), e.g. " +
+        "dataframe.write.option(\"partitionOverwriteMode\", \"dynamic\").save(path)."
+      )
       .stringConf
       .transform(_.toUpperCase(Locale.ROOT))
       .checkValues(PartitionOverwriteMode.values.map(_.toString))
