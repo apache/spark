17f469bc808e076b45fffcedb0147991fa4c41f3
Koert Kuipers <koert@tresata.com>
2018-07-25 13:06:03 -0700

    [SPARK-24860][SQL] Support setting of partitionOverWriteMode in output options for writing DataFrame
    ## What changes were proposed in this pull request?
    
    Besides spark setting spark.sql.sources.partitionOverwriteMode also allow setting partitionOverWriteMode per write
    
    ## How was this patch tested?
    
    Added unit test in InsertSuite
    
    Please review http://spark.apache.org/contributing.html before opening a pull request.
    
    Author: Koert Kuipers <koert@tresata.com>
    
    Closes #21818 from koertkuipers/feat-partition-overwrite-mode-per-write.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1360,7 +1360,11 @@ object SQLConf {
         "overwriting. In dynamic mode, Spark doesn't delete partitions ahead, and only overwrite " +
         "those partitions that have data written into it at runtime. By default we use static " +
         "mode to keep the same behavior of Spark prior to 2.3. Note that this config doesn't " +
-        "affect Hive serde tables, as they are always overwritten with dynamic mode.")
+        "affect Hive serde tables, as they are always overwritten with dynamic mode. This can " +
+        "also be set as an output option for a data source using key partitionOverwriteMode " +
+        "(which takes precendence over this setting), e.g. " +
+        "dataframe.write.option(\"partitionOverwriteMode\", \"dynamic\").save(path)."
+      )
       .stringConf
       .transform(_.toUpperCase(Locale.ROOT))
       .checkValues(PartitionOverwriteMode.values.map(_.toString))
