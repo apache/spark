ef2303d444fb60c316ed8f971fc086917be39124
hyukjinkwon <gurwls223@apache.org>
2018-07-26 16:40:43 +0800

    Just keep comma

--- dev/merge_spark_pr.py
+++ dev/merge_spark_pr.py
@@ -319,7 +319,7 @@ def choose_jira_assignee(issue, asf_jira):
                     annotations.append("Commentor")
                 print("[%d] %s (%s)" % (idx, author.displayName, ",".join(annotations)))
             raw_assignee = input(
-                "Enter number of user, or userid to assign to (blank to leave unassigned):")
+                "Enter number of user, or userid, to assign to (blank to leave unassigned):")
             if raw_assignee == "":
                 return None
             else:
