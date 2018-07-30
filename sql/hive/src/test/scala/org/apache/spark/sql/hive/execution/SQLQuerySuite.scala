4a798e726396da41cf9fab6900434bac6669c20a
Robert Kruszewski <robertk@palantir.com>
2018-07-30 14:10:30 +0100

    Ignore SQLQuerySuite 'SPARK-18355 Read data from a hive table with a new column'

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/SQLQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/SQLQuerySuite.scala
@@ -2098,7 +2098,7 @@ class SQLQuerySuite extends QueryTest with SQLTestUtils with TestHiveSingleton {
   }
 
   Seq("orc", "parquet").foreach { format =>
-    test(s"SPARK-18355 Read data from a hive table with a new column - $format") {
+    ignore(s"SPARK-18355 Read data from a hive table with a new column - $format") {
       val client =
         spark.sharedState.externalCatalog.unwrapped.asInstanceOf[HiveExternalCatalog].client
 
