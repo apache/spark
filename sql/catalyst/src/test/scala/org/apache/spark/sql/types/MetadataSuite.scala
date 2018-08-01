17485a703f686441ee1569b3ccfd3547af13f397
Kaya Kupferschmidt <k.kupferschmidt@dimajix.de>
2018-08-01 21:34:41 +0200

    SPARK-24742: Fixed indendation and improved assertions in unittests

--- sql/catalyst/src/test/scala/org/apache/spark/sql/types/MetadataSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/types/MetadataSuite.scala
@@ -20,55 +20,55 @@ package org.apache.spark.sql.types
 import org.apache.spark.SparkFunSuite
 
 class MetadataSuite extends SparkFunSuite {
-    test("String Metadata") {
-        val meta = new MetadataBuilder().putString("key", "value").build()
-        assert(meta == meta)
-        assert(meta.## != 0)
-        assert(meta.getString("key") == "value")
-        assert(meta.contains("key"))
-        intercept[NoSuchElementException](meta.getString("no_such_key"))
-        intercept[ClassCastException](meta.getBoolean("key"))
-    }
+  test("String Metadata") {
+    val meta = new MetadataBuilder().putString("key", "value").build()
+    assert(meta === meta)
+    assert(meta.## !== 0)
+    assert(meta.getString("key") === "value")
+    assert(meta.contains("key"))
+    intercept[NoSuchElementException](meta.getString("no_such_key"))
+    intercept[ClassCastException](meta.getBoolean("key"))
+  }
 
-    test("Long Metadata") {
-        val meta = new MetadataBuilder().putLong("key", 12).build()
-        assert(meta == meta)
-        assert(meta.## != 0)
-        assert(meta.getLong("key") == 12)
-        assert(meta.contains("key"))
-        intercept[NoSuchElementException](meta.getLong("no_such_key"))
-        intercept[ClassCastException](meta.getBoolean("key"))
-    }
+  test("Long Metadata") {
+    val meta = new MetadataBuilder().putLong("key", 12).build()
+    assert(meta === meta)
+    assert(meta.## !== 0)
+    assert(meta.getLong("key") === 12)
+    assert(meta.contains("key"))
+    intercept[NoSuchElementException](meta.getLong("no_such_key"))
+    intercept[ClassCastException](meta.getBoolean("key"))
+  }
 
-    test("Double Metadata") {
-        val meta = new MetadataBuilder().putDouble("key", 12).build()
-        assert(meta == meta)
-        assert(meta.## != 0)
-        assert(meta.getDouble("key") == 12)
-        assert(meta.contains("key"))
-        intercept[NoSuchElementException](meta.getDouble("no_such_key"))
-        intercept[ClassCastException](meta.getBoolean("key"))
-    }
+  test("Double Metadata") {
+    val meta = new MetadataBuilder().putDouble("key", 12).build()
+    assert(meta === meta)
+    assert(meta.## !== 0)
+    assert(meta.getDouble("key") === 12)
+    assert(meta.contains("key"))
+    intercept[NoSuchElementException](meta.getDouble("no_such_key"))
+    intercept[ClassCastException](meta.getBoolean("key"))
+  }
 
-    test("Boolean Metadata") {
-        val meta = new MetadataBuilder().putBoolean("key", true).build()
-        assert(meta == meta)
-        assert(meta.## != 0)
-        assert(meta.getBoolean("key") == true)
-        assert(meta.contains("key"))
-        intercept[NoSuchElementException](meta.getBoolean("no_such_key"))
-        intercept[ClassCastException](meta.getString("key"))
-    }
+  test("Boolean Metadata") {
+    val meta = new MetadataBuilder().putBoolean("key", true).build()
+    assert(meta === meta)
+    assert(meta.## !== 0)
+    assert(meta.getBoolean("key") === true)
+    assert(meta.contains("key"))
+    intercept[NoSuchElementException](meta.getBoolean("no_such_key"))
+    intercept[ClassCastException](meta.getString("key"))
+  }
 
-    test("Null Metadata") {
-        val meta = new MetadataBuilder().putNull("key").build()
-        assert(meta == meta)
-        assert(meta.## != 0)
-        assert(meta.getString("key") == null)
-        assert(meta.getDouble("key") == 0)
-        assert(meta.getLong("key") == 0)
-        assert(meta.getBoolean("key") == false)
-        assert(meta.contains("key"))
-        intercept[NoSuchElementException](meta.getLong("no_such_key"))
-    }
+  test("Null Metadata") {
+    val meta = new MetadataBuilder().putNull("key").build()
+    assert(meta === meta)
+    assert(meta.## !== 0)
+    assert(meta.getString("key") === null)
+    assert(meta.getDouble("key") === 0)
+    assert(meta.getLong("key") === 0)
+    assert(meta.getBoolean("key") === false)
+    assert(meta.contains("key"))
+    intercept[NoSuchElementException](meta.getLong("no_such_key"))
+  }
 }
