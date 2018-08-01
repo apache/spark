0ce98171fc025b0c1807b5f4da22695a548beb86
zhangchj1990 <479224070@qq.com>
2018-08-01 11:07:13 +0800

    增加自测模块

--- zhangchj/src/main/scala/com/jd/zhangchj/core/WordCount.scala
+++ zhangchj/src/main/scala/com/jd/zhangchj/core/WordCount.scala
@@ -7,8 +7,8 @@ object WordCount {
 
   def main(args: Array[String]): Unit = {
     val logFile: String = "/home/zhangchj/sources/spark/README.md" // Should be some file on your system
-    val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
-    val sc = new SparkContext(conf)
+    //val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+    val sc = new SparkContext("local[*]","WordCount")
     val logData = sc.textFile(logFile, 2).cache()
   /*  val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
