483d1828067f7f55cc80b2ce773596d66bd16ef8
DB Tsai <d_tsai@apple.com>
2018-08-06 11:26:15 -0700

    fix first author bug

--- dev/merge_spark_pr.py
+++ dev/merge_spark_pr.py
@@ -166,9 +166,9 @@ def merge_pr(pr_num, target_ref, title, body, pr_repo_desc):
     merge_message_flags += ["-m", "Closes #%s from %s." % (pr_num, pr_repo_desc)]
 
     authors = "Authored-by:" if len(distinct_authors) == 1 else "Lead-authored-by:"
-    authors += " %s <%s>\n" % (committer_name, committer_email)
-    distinct_authors.pop(0)
-    authors += "\n".join(["Co-authored-by: %s" % a for a in distinct_authors])
+    authors += " %s" % (distinct_authors.pop(0))
+    if len(distinct_authors) > 0:
+        authors += "\n" + "\n".join(["Co-authored-by: %s" % a for a in distinct_authors])
     authors += "\n" + "Signed-off-by: %s <%s>" % (committer_name, committer_email)
 
     merge_message_flags += ["-m", authors]
