b74a7122d2e24d0a24508f6b5c710456a9d11740
Kaxil Naik <kaxilnaik@gmail.com>
2018-08-07 15:15:15 +0100

    [AIRFLOW-2864] Fix docstrings for SubDagOperator (#3712)
    The docstrings are currently in the `__init__` method, due to which they are not automatically shown in the Sphinx documentation.

--- airflow/operators/subdag_operator.py
+++ airflow/operators/subdag_operator.py
@@ -25,6 +25,18 @@ from airflow.utils.db import provide_session
 
 
 class SubDagOperator(BaseOperator):
+    """
+    This runs a sub dag. By convention, a sub dag's dag_id
+    should be prefixed by its parent and a dot. As in `parent.child`.
+
+    :param subdag: the DAG object to run as a subdag of the current DAG.
+    :type subdag: airflow.DAG.
+    :param dag: the parent DAG for the subdag.
+    :type dag: airflow.DAG.
+    :param executor: the executor for this subdag. Default to use SequentialExecutor.
+        Please find AIRFLOW-74 for more details.
+    :type executor: airflow.executors.
+    """
 
     template_fields = tuple()
     ui_color = '#555'
@@ -37,18 +49,6 @@ class SubDagOperator(BaseOperator):
             subdag,
             executor=SequentialExecutor(),
             *args, **kwargs):
-        """
-        This runs a sub dag. By convention, a sub dag's dag_id
-        should be prefixed by its parent and a dot. As in `parent.child`.
-
-        :param subdag: the DAG object to run as a subdag of the current DAG.
-        :type subdag: airflow.DAG.
-        :param dag: the parent DAG for the subdag.
-        :type dag: airflow.DAG.
-        :param executor: the executor for this subdag. Default to use SequentialExecutor.
-                         Please find AIRFLOW-74 for more details.
-        :type executor: airflow.executors.
-        """
         import airflow.models
         dag = kwargs.get('dag') or airflow.models._CONTEXT_MANAGER_DAG
         if not dag:
