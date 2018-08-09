935ed36efb908fae8bec3521c7c018350daad665
Yuming Wang <yumwang@ebay.com>
2018-08-09 23:21:51 +0800

    Add findInCommonType

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/TypeCoercion.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/TypeCoercion.scala
@@ -231,6 +231,16 @@ object TypeCoercion {
       })
   }
 
+  private def findInCommonType(types: Seq[DataType], conf: SQLConf): Option[DataType] = {
+    val (stringTypes, nonStringTypes) = types.partition(hasStringType(_))
+    (stringTypes.distinct ++ nonStringTypes).foldLeft[Option[DataType]](Some(NullType))((r, c) =>
+      r match {
+        case Some(d) => findCommonTypeForBinaryComparison(d, c, conf)
+          .orElse(findWiderTypeWithoutStringPromotionForTwo(d, c))
+        case _ => None
+      })
+  }
+
   /**
    * Similar to [[findWiderTypeForTwo]] that can handle decimal types, but can't promote to
    * string. If the wider decimal type exceeds system limitation, this rule will truncate
@@ -486,17 +496,9 @@ object TypeCoercion {
         }
 
       case i @ In(a, b) if b.exists(_.dataType != a.dataType) =>
-        if (b.map(_.dataType).distinct.size == 1) {
-          findCommonTypeForBinaryComparison(a.dataType, b.head.dataType, conf)
-            .orElse(findWiderTypeWithoutStringPromotionForTwo(a.dataType, b.head.dataType)) match {
-            case Some(finalDataType) => i.withNewChildren(i.children.map(Cast(_, finalDataType)))
-            case None => i
-          }
-        } else {
-          findWiderCommonType(i.children.map(_.dataType)) match {
-            case Some(finalDataType) => i.withNewChildren(i.children.map(Cast(_, finalDataType)))
-            case None => i
-          }
+        findInCommonType(i.children.map(_.dataType), conf) match {
+          case Some(finalDataType) => i.withNewChildren(i.children.map(Cast(_, finalDataType)))
+          case None => i
         }
     }
   }
