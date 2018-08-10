51f8792007672899324c6a615be55f0179284401
hyukjinkwon <gurwls223@apache.org>
2018-08-10 09:10:59 +0800

    330m -> 340m

--- dev/run-tests-jenkins.py
+++ dev/run-tests-jenkins.py
@@ -182,7 +182,7 @@ def main():
 
     # format: http://linux.die.net/man/1/timeout
     # must be less than the timeout configured on Jenkins (currently 400m)
-    tests_timeout = "330m"
+    tests_timeout = "340m"
 
     # Array to capture all test names to run on the pull request. These tests are represented
     # by their file equivalents in the dev/tests/ directory.
