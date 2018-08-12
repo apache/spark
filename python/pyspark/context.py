5bc7598b25dbf5ea4b3e0f149aa31fb03a5310f9
Tynan CR <tynancr@gmail.com>
2018-08-12 08:13:09 -0500

    Fix typos
    ## What changes were proposed in this pull request?
    
    Small typo fixes in Pyspark. These were the only ones I stumbled across after looking around for a while.
    
    ## How was this patch tested?
    
    Manually
    
    Please review http://spark.apache.org/contributing.html before opening a pull request.
    
    Closes #22016 from tynan-cr/typo-fix-pyspark.
    
    Authored-by: Tynan CR <tynancr@gmail.com>
    Signed-off-by: Sean Owen <srowen@gmail.com>

--- python/pyspark/context.py
+++ python/pyspark/context.py
@@ -126,7 +126,7 @@ class SparkContext(object):
         self.environment = environment or {}
         # java gateway must have been launched at this point.
         if conf is not None and conf._jconf is not None:
-            # conf has been initialized in JVM properly, so use conf directly. This represent the
+            # conf has been initialized in JVM properly, so use conf directly. This represents the
             # scenario that JVM has been launched before SparkConf is created (e.g. SparkContext is
             # created and then stopped, and we create a new SparkConf and new SparkContext again)
             self._conf = conf
