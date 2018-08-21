f40c600bb9630cccbfc8b6e62530c8ee3e4ee6a7
Tathagata Das <tathagata.das1565@gmail.com>
2018-08-21 16:22:42 -0700

    Change import order

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/statefulOperators.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/statefulOperators.scala
@@ -17,10 +17,10 @@
 
 package org.apache.spark.sql.execution.streaming
 
-import scala.collection.JavaConverters._
-
 import java.util.UUID
 import java.util.concurrent.TimeUnit._
+
+import scala.collection.JavaConverters._
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
