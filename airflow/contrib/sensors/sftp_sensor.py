7ea7dd2031c32a5dd14e4fd74f415f938a69e3ad
Taylor D. Edmiston <tedmiston@gmail.com>
2018-08-22 12:27:05 -0400

    [AIRFLOW-2909] Deprecate airflow.operators.sensors module (#3760)

--- airflow/contrib/sensors/sftp_sensor.py
+++ airflow/contrib/sensors/sftp_sensor.py
@@ -20,7 +20,7 @@
 import logging
 from paramiko import SFTP_NO_SUCH_FILE
 from airflow.contrib.hooks.sftp_hook import SFTPHook
-from airflow.operators.sensors import BaseSensorOperator
+from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
 
 
