b0c4483e9506d8af6f12b6d41848ece52ffe9bc4
Imran Rashid <irashid@cloudera.com>
2018-08-28 12:10:09 -0500

    feedback

--- python/pyspark/taskcontext.py
+++ python/pyspark/taskcontext.py
@@ -110,6 +110,8 @@ def _load_from_socket(port, auth_secret):
     connection has been closed.
     """
     (sockfile, sock) = local_connect_and_auth(port, auth_secret)
+    # The barrier() call may block forever, so no timeout
+    sock.settimeout(None)
     # Make a barrier() function call.
     write_int(BARRIER_FUNCTION, sockfile)
     sockfile.flush()
