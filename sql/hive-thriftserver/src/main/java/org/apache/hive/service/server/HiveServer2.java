fbced52e5687cd5eb6a06c3b9bca5cbeb9343002
Steve Loughran <stevel@hortonworks.com>
2018-08-28 22:02:57 +0100

    [SPARK-25183] Hive Thrift Server shutdown hook: use AbstractFunction0<BoxUnit>  as the base class for the  hook. Add a log message to indicate when it is called
    Change-Id: I6f15f02b5f25162bc4a2ccf17caf3a72d7e6c147

--- sql/hive-thriftserver/src/main/java/org/apache/hive/service/server/HiveServer2.java
+++ sql/hive-thriftserver/src/main/java/org/apache/hive/service/server/HiveServer2.java
@@ -20,6 +20,9 @@ package org.apache.hive.service.server;
 
 import java.util.Properties;
 
+import scala.runtime.AbstractFunction0;
+import scala.runtime.BoxedUnit;
+
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
@@ -38,6 +41,7 @@ import org.apache.hive.service.cli.CLIService;
 import org.apache.hive.service.cli.thrift.ThriftBinaryCLIService;
 import org.apache.hive.service.cli.thrift.ThriftCLIService;
 import org.apache.hive.service.cli.thrift.ThriftHttpCLIService;
+
 import org.apache.spark.util.ShutdownHookManager;
 
 /**
@@ -70,14 +74,20 @@ public class HiveServer2 extends CompositeService {
     // Add a shutdown hook for catching SIGTERM & SIGINT
     // this must be higher than the Hadoop Filesystem priority of 10,
     // which the default priority is.
+    // The signature of the callback must match that of a scala () -> Unit
+    // function
     ShutdownHookManager.addShutdownHook(
-        () -> {
-          try {
-            stop();
-          } catch (Throwable e) {
-            LOG.warn("Ignoring Exception while stopping Hive Server from shutdown hook", e);
+        new AbstractFunction0<BoxedUnit>() {
+          public BoxedUnit apply() {
+            try {
+              LOG.info("Hive Server Shutdown hook invoked");
+              stop();
+            } catch (Throwable e) {
+              LOG.warn("Ignoring Exception while stopping Hive Server from shutdown hook",
+                  e);
+            }
+            return BoxedUnit.UNIT;
           }
-          return null;
         });
   }
 
@@ -100,7 +110,6 @@ public class HiveServer2 extends CompositeService {
   @Override
   public synchronized void stop() {
     LOG.info("Shutting down HiveServer2");
-    HiveConf hiveConf = this.getHiveConf();
     super.stop();
   }
 
