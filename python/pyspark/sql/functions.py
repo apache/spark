ca23ea363fe2beaecc4cf16385256aabbdd7d626
Parker Hegstrom <phegstrom@palantir.com>
2018-08-30 11:01:36 -0400

    adding python support

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1669,20 +1669,33 @@ def repeat(col, n):
     return Column(sc._jvm.functions.repeat(_to_java_column(col), n))
 
 
-@since(1.5)
+@since(2.4)
 @ignore_unicode_prefix
-def split(str, pattern):
+def split(str, regex, limit=-1):
     """
-    Splits str around pattern (pattern is a regular expression).
+    Splits str around matches of the given regex.
 
-    .. note:: pattern is a string represent the regular expression.
+    :param str: a string expression to split
+    :param regex: a string representing a regular expression. The regex string should be
+                  a Java regular expression.
+    :param limit: an integer expression which controls the number of times the regex is applied.
+            * ``limit > 0``: The resulting array's length will not be more than `limit`, and the resulting
+                             array's last entry will contain all input beyond the last matched regex.
+            * ``limit < 0``: `regex` will be applied as many times as possible, and the resulting
+                             array can be of any size.
+            * ``limit = 0``: `regex` will be applied as many times as possible, the resulting array can
+                             be of any size, and trailing empty strings will be discarded.
 
-    >>> df = spark.createDataFrame([('ab12cd',)], ['s',])
-    >>> df.select(split(df.s, '[0-9]+').alias('s')).collect()
-    [Row(s=[u'ab', u'cd'])]
+    >>> df = spark.createDataFrame([('oneAtwoBthreeC',)], ['s',])
+    >>> df.select(pyspark.sql.functions.split(df.s, '[ABC]', 2).alias('s')).collect()
+    [Row(s=[u'one', u'twoBthreeC'])]
+    >>> df.select(pyspark.sql.functions.split(df.s, '[ABC]', -1).alias('s')).collect()
+    [Row(s=[u'one', u'two', u'three', u''])]
+    >>> df.select(pyspark.sql.functions.split(df.s, '[ABC]', 0).alias('s')).collect()
+    [Row(s=[u'one', u'two', u'three'])]
     """
     sc = SparkContext._active_spark_context
-    return Column(sc._jvm.functions.split(_to_java_column(str), pattern))
+    return Column(sc._jvm.functions.split(_to_java_column(str), regex, limit))
 
 
 @ignore_unicode_prefix
