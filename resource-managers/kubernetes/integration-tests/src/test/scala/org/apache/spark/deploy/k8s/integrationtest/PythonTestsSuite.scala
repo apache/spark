75742a37687a7eb3ebaa34069ac7a62521a4e2f8
Ilan Filonenko <if56@cornell.edu>
2018-08-30 01:26:27 -0400

    add python.worker.reuse

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/PythonTestsSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/PythonTestsSuite.scala
@@ -79,6 +79,7 @@ private[spark] trait PythonTestsSuite { k8sSuite: KubernetesSuite =>
       .set("spark.kubernetes.pyspark.pythonVersion", "3")
       .set("spark.kubernetes.memoryOverheadFactor", s"$memOverheadConstant")
       .set("spark.executor.pyspark.memory", s"${additionalMemory}Mi")
+      .set("spark.python.worker.reuse", "false")
     runSparkApplicationAndVerifyCompletion(
       appResource = PYSPARK_FILES,
       mainClass = "",
