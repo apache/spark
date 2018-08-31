9cd662292d63d4280be1c7924bcb297d9f8feeb0
Nihar Sheth <niharrsheth@gmail.com>
2018-08-31 15:33:46 -0700

    style and doc fixes

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -137,13 +137,17 @@ private[spark] class Executor(
   env.serializerManager.setDefaultClassLoader(replClassLoader)
 
   private val pluginList = conf.get(EXECUTOR_PLUGINS)
-  if (pluginList != Nil) logDebug(s"Loading the following plugins: ${pluginList.mkString(", ")}")
+  if (pluginList.nonEmpty) {
+    logDebug(s"Loading the following plugins: ${pluginList.mkString(", ")}")
+  }
   // Load executor plugins
   Thread.currentThread().setContextClassLoader(replClassLoader)
   private val executorPlugins =
     Utils.loadExtensions(classOf[ExecutorPlugin], pluginList, conf)
   executorPlugins.foreach(_.init())
-  if (pluginList != Nil) logDebug("Finished loading plugins")
+  if (pluginList.nonEmpty) {
+    logDebug("Finished loading plugins")
+  }
 
   // Max size of direct result. If task result is bigger than this, we use the block manager
   // to send the result back.
