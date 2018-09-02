a9878eb82eb1fb605d7d9360c534f0dc26e04bd7
Kaxil Naik <kaxilnaik@gmail.com>
2018-09-03 00:19:51 +0100

    [AIRFLOW-2476] Allow tabulate up to 0.8.2 (#3835)

--- setup.py
+++ setup.py
@@ -322,7 +322,7 @@ def do_setup():
             'requests>=2.5.1, <3',
             'setproctitle>=1.1.8, <2',
             'sqlalchemy>=1.1.15, <1.2.0',
-            'tabulate>=0.7.5, <0.8.0',
+            'tabulate>=0.7.5, <=0.8.2',
             'tenacity==4.8.0',
             'thrift>=0.9.2',
             'tzlocal>=1.4',
