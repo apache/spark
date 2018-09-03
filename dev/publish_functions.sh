11651f9a663e51543a1176f5c6f37f5a9d27b43a
Onur Satici <onursatici@users.noreply.github.com>
2018-09-03 15:43:13 +0100

    Remove unused DOCKER_PLUGIN_PROJECT_ROOT env var (#403)

--- dev/publish_functions.sh
+++ dev/publish_functions.sh
@@ -22,7 +22,7 @@ publish_artifacts() {
   echo "</server></servers></settings>" >> $tmp_settings
 
   ./build/mvn -T 1C --settings $tmp_settings -DskipTests "${PALANTIR_FLAGS[@]}" deploy
-  ./gradlew -p $DOCKER_PLUGIN_PROJECT_DIR --info bintrayUpload
+  ./gradlew --info bintrayUpload
 }
 
 make_dist() {
