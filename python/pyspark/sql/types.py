2218fe526a559f90bb46917f9d00d8edb7c84bee
liyuanjian <liyuanjian@baidu.com>
2018-09-06 10:17:29 -0700

    [SPARK-25072][PYSPARK] Forbid extra value for custom Row
    ## What changes were proposed in this pull request?
    
    Add value length check in `_create_row`, forbid extra value for custom Row in PySpark.
    
    ## How was this patch tested?
    
    New UT in pyspark-sql
    
    Closes #22140 from xuanyuanking/SPARK-25072.
    
    Lead-authored-by: liyuanjian <liyuanjian@baidu.com>
    Co-authored-by: Yuanjian Li <xyliyuanjian@gmail.com>
    Signed-off-by: Bryan Cutler <cutlerb@gmail.com>

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -1500,6 +1500,9 @@ class Row(tuple):
     # let object acts like class
     def __call__(self, *args):
         """create new Row object"""
+        if len(args) > len(self):
+            raise ValueError("Can not create Row with fields %s, expected %d values "
+                             "but got %s" % (self, len(self), args))
         return _create_row(self, args)
 
     def __getitem__(self, item):
