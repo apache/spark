89fff2ac653fa05ae92ad09ba8307649d6972900
LantaoJin <jinlantao@gmail.com>
2018-09-06 22:04:56 +0800

    [SPARK-25357][SQL] Abbreviated metadata in DataSourceScanExec results in incomplete information in event log

--- sql/core/src/main/scala/org/apache/spark/sql/execution/DataSourceScanExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/DataSourceScanExec.scala
@@ -54,7 +54,7 @@ trait DataSourceScanExec extends LeafExecNode with CodegenSupport {
   override def simpleString: String = {
     val metadataEntries = metadata.toSeq.sorted.map {
       case (key, value) =>
-        key + ": " + StringUtils.abbreviate(redact(value), 100)
+        key + ": " + redact(value)
     }
     val metadataStr = Utils.truncatedString(metadataEntries, " ", ", ", "")
     s"$nodeNamePrefix$nodeName${Utils.truncatedString(output, "[", ",", "]")}$metadataStr"
