711963a98d4a46399f95b1969b7b952ae8866c52
gatorsmile <gatorsmile@gmail.com>
2018-09-07 10:38:52 -0700

    Revert "[SPARK-10399][SPARK-23879][FOLLOWUP][CORE] Free unused off-heap memory in MemoryBlockSuite"
    This reverts commit c3a86faa53c9e49efd595802adc38a6d412ce681.

--- common/unsafe/src/test/java/org/apache/spark/unsafe/memory/MemoryBlockSuite.java
+++ common/unsafe/src/test/java/org/apache/spark/unsafe/memory/MemoryBlockSuite.java
@@ -120,8 +120,6 @@ public class MemoryBlockSuite {
     } catch (Exception expected) {
       Assert.assertThat(expected.getMessage(), containsString("should not be larger than"));
     }
-
-    memory.setPageNumber(MemoryBlock.NO_PAGE_NUMBER);
   }
 
   @Test
@@ -167,13 +165,11 @@ public class MemoryBlockSuite {
     int length = 56;
 
     check(memory, obj, offset, length);
-    memoryAllocator.free(memory);
 
     long address = Platform.allocateMemory(112);
     memory = new OffHeapMemoryBlock(address, length);
     obj = memory.getBaseObject();
     offset = memory.getBaseOffset();
     check(memory, obj, offset, length);
-    Platform.freeMemory(address);
   }
 }
