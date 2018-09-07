6d7bc5af454341f6d9bfc1e903148ad7ba8de6f9
Dilip Biswal <dbiswal@us.ibm.com>
2018-09-06 23:35:02 -0700

    [SPARK-25267][SQL][TEST] Disable ConvertToLocalRelation in the test cases of sql/core and sql/hive
    ## What changes were proposed in this pull request?
    In SharedSparkSession and TestHive, we need to disable the rule ConvertToLocalRelation for better test case coverage.
    ## How was this patch tested?
    Identify the failures after excluding "ConvertToLocalRelation" rule.
    
    Closes #22270 from dilipbiswal/SPARK-25267-final.
    
    Authored-by: Dilip Biswal <dbiswal@us.ibm.com>
    Signed-off-by: gatorsmile <gatorsmile@gmail.com>

--- mllib/src/test/scala/org/apache/spark/ml/util/MLTest.scala
+++ mllib/src/test/scala/org/apache/spark/ml/util/MLTest.scala
@@ -21,12 +21,13 @@ import java.io.File
 
 import org.scalatest.Suite
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{DebugFilesystem, SparkConf, SparkContext}
 import org.apache.spark.ml.{PredictionModel, Transformer}
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Row}
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.streaming.StreamTest
 import org.apache.spark.sql.test.TestSparkSession
 import org.apache.spark.util.Utils
@@ -35,6 +36,13 @@ trait MLTest extends StreamTest with TempDirectory { self: Suite =>
 
   @transient var sc: SparkContext = _
   @transient var checkpointDir: String = _
+
+  protected override def sparkConf = {
+    new SparkConf()
+      .set("spark.hadoop.fs.file.impl", classOf[DebugFilesystem].getName)
+      .set("spark.unsafe.exceptionOnMemoryLeak", "true")
+      .set(SQLConf.CODEGEN_FALLBACK.key, "false")
+  }
 
   protected override def createSparkSession: TestSparkSession = {
     new TestSparkSession(new SparkContext("local[2]", "MLlibUnitTest", sparkConf))
