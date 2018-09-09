1cfda448255d5b4a0df88148e0f6acd88aa6e318
Ilan Filonenko <if56@cornell.edu>
2018-09-08 22:18:06 -0700

    [SPARK-25021][K8S] Add spark.executor.pyspark.memory limit for K8S
    ## What changes were proposed in this pull request?
    
    Add spark.executor.pyspark.memory limit for K8S
    
    ## How was this patch tested?
    
    Unit and Integration tests
    
    Closes #22298 from ifilonenko/SPARK-25021.
    
    Authored-by: Ilan Filonenko <if56@cornell.edu>
    Signed-off-by: Holden Karau <holden@pigscanfly.ca>

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/features/bindings/PythonDriverFeatureStep.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/features/bindings/PythonDriverFeatureStep.scala
@@ -21,6 +21,7 @@ import scala.collection.JavaConverters._
 import io.fabric8.kubernetes.api.model.{ContainerBuilder, EnvVarBuilder, HasMetadata}
 
 import org.apache.spark.deploy.k8s.{KubernetesConf, KubernetesDriverSpecificConf, KubernetesUtils, SparkPod}
+import org.apache.spark.deploy.k8s.Config.APP_RESOURCE_TYPE
 import org.apache.spark.deploy.k8s.Constants._
 import org.apache.spark.deploy.k8s.features.KubernetesFeatureConfigStep
 
@@ -68,7 +69,8 @@ private[spark] class PythonDriverFeatureStep(
 
     SparkPod(pod.pod, withPythonPrimaryContainer)
   }
-  override def getAdditionalPodSystemProperties(): Map[String, String] = Map.empty
+  override def getAdditionalPodSystemProperties(): Map[String, String] =
+    Map(APP_RESOURCE_TYPE.key -> "python")
 
   override def getAdditionalKubernetesResources(): Seq[HasMetadata] = Seq.empty
 }
