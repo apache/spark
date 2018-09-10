1b9ae7e2b1399766b7848afb61dc400a894e8c0d
Yuming Wang <yumwang@ebay.com>
2018-09-10 13:47:19 +0800

    [SPARK-25313][SQL][FOLLOW-UP] Fix InsertIntoHiveDirCommand output schema in Parquet issue
    ## What changes were proposed in this pull request?
    
    How to reproduce:
    ```scala
    spark.sql("CREATE TABLE tbl(id long)")
    spark.sql("INSERT OVERWRITE TABLE tbl VALUES 4")
    spark.sql("CREATE VIEW view1 AS SELECT id FROM tbl")
    spark.sql(s"INSERT OVERWRITE LOCAL DIRECTORY '/tmp/spark/parquet' " +
      "STORED AS PARQUET SELECT ID FROM view1")
    spark.read.parquet("/tmp/spark/parquet").schema
    scala> spark.read.parquet("/tmp/spark/parquet").schema
    res10: org.apache.spark.sql.types.StructType = StructType(StructField(id,LongType,true))
    ```
    The schema should be `StructType(StructField(ID,LongType,true))` as we `SELECT ID FROM view1`.
    
    This pr fix this issue.
    
    ## How was this patch tested?
    
    unit tests
    
    Closes #22359 from wangyum/SPARK-25313-FOLLOW-UP.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
@@ -803,6 +803,25 @@ class HiveDDLSuite
     }
   }
 
+  test("SPARK-25313 Insert overwrite directory should output correct schema") {
+    withSQLConf(CONVERT_METASTORE_PARQUET.key -> "false") {
+      withTable("tbl") {
+        withView("view1") {
+          spark.sql("CREATE TABLE tbl(id long)")
+          spark.sql("INSERT OVERWRITE TABLE tbl VALUES 4")
+          spark.sql("CREATE VIEW view1 AS SELECT id FROM tbl")
+          withTempPath { path =>
+            spark.sql(s"INSERT OVERWRITE LOCAL DIRECTORY '${path.getCanonicalPath}' " +
+              "STORED AS PARQUET SELECT ID FROM view1")
+            val expectedSchema = StructType(Seq(StructField("ID", LongType, true)))
+            assert(spark.read.parquet(path.toString).schema == expectedSchema)
+            checkAnswer(spark.read.parquet(path.toString), Seq(Row(4)))
+          }
+        }
+      }
+    }
+  }
+
   test("alter table partition - storage information") {
     sql("CREATE TABLE boxes (height INT, length INT) PARTITIONED BY (width INT)")
     sql("INSERT OVERWRITE TABLE boxes PARTITION (width=4) SELECT 4, 4")
