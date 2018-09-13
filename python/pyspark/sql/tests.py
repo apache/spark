ce6d47a8fc2f8255ec2b1d91d63833de79b6c14d
Dilip Biswal <dbiswal@us.ibm.com>
2018-09-12 19:42:12 -0700

    python test failure

--- python/pyspark/sql/tests.py
+++ python/pyspark/sql/tests.py
@@ -1498,8 +1498,7 @@ class SQLTests(ReusedSQLTestCase):
         from pyspark.sql.functions import array_contains
 
         df = self.spark.createDataFrame([(["1", "2", "3"],), ([],)], ['data'])
-        actual = df.select(array_contains(df.data, 1).alias('b')).collect()
-        # The value argument can be implicitly castable to the element's type of the array.
+        actual = df.select(array_contains(df.data, "1").alias('b')).collect()
         self.assertEqual([Row(b=True), Row(b=False)], actual)
 
     def test_between_function(self):
