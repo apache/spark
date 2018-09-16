3556a61241e3f4910673f1bbf701905870ed09ea
Suryanarayana GARLAPATI <suryanarayana.garlapati@nokia.com>
2018-09-15 23:37:26 -0500

    [SPARK-25442][SQL][K8S] Support STS to run in k8s deployments with spark deployment mode as cluster.

--- core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
@@ -292,7 +292,7 @@ private[spark] class SparkSubmit extends Logging {
         error("Cluster deploy mode is not applicable to Spark shells.")
       case (_, CLUSTER) if isSqlShell(args.mainClass) =>
         error("Cluster deploy mode is not applicable to Spark SQL shell.")
-      case (_, CLUSTER) if isThriftServer(args.mainClass) =>
+      case (_, CLUSTER) if (clusterManager != KUBERNETES) && isThriftServer(args.mainClass) =>
         error("Cluster deploy mode is not applicable to Spark Thrift server.")
       case _ =>
     }
