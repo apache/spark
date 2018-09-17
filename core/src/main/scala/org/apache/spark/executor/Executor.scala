3e0d9536512300d27201e1d5cc4d9b5755a47871
Mukul Murthy <mukul.murthy@gmail.com>
2018-09-17 14:55:21 -0700

    Don't send zero accumulators for metrics in heartbeat

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -799,7 +799,8 @@ private[spark] class Executor(
       if (taskRunner.task != null) {
         taskRunner.task.metrics.mergeShuffleReadMetrics()
         taskRunner.task.metrics.setJvmGCTime(curGCTime - taskRunner.startGCTime)
-        accumUpdates += ((taskRunner.taskId, taskRunner.task.metrics.accumulators()))
+        accumUpdates +=
+          ((taskRunner.taskId, taskRunner.task.metrics.accumulators().filterNot(_.isZero)))
       }
     }
 
