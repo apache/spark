51ae87ecdfa46505231f06ff88ed88c8f7a01c85
Yuming Wang <yumwang@ebay.com>
2018-09-21 01:49:39 +0800

    Fix scala doc issue

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetBenchmark.scala
@@ -26,9 +26,9 @@ import org.apache.spark.util.{Benchmark, BenchmarkBase => FileBenchmarkBase}
 /**
  * Benchmark for Dataset typed operations comparing with DataFrame and RDD versions.
  * To run this benchmark:
- * 1. without sbt: bin/spark-submit --class <this class> <spark sql test jar>
- * 2. build/sbt "sql/test:runMain <this class>"
- * 3. generate result: SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "sql/test:runMain <this class>"
+ * 1. without sbt: bin/spark-submit --class [this class] [spark sql test jar]
+ * 2. build/sbt "sql/test:runMain [this class]"
+ * 3. generate result: SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "sql/test:runMain [this class]"
  *    Results will be written to "benchmarks/DatasetBenchmark-results.txt".
  */
 object DatasetBenchmark extends FileBenchmarkBase {
