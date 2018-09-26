998cdb8aa472c93c9d44a0a6f0cd8593b098483c
Yuming Wang <yumwang@ebay.com>
2018-09-26 14:51:24 +0800

    Add javax.jdo to SharedClass

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/IsolatedClientLoader.scala
@@ -189,6 +189,7 @@ private[hive] class IsolatedClientLoader(
     (name.startsWith("com.google") && !name.startsWith("com.google.cloud")) ||
     name.startsWith("java.lang.") ||
     name.startsWith("java.net") ||
+    name.startsWith("javax.jdo") ||
     sharedPrefixes.exists(name.startsWith)
   }
 
