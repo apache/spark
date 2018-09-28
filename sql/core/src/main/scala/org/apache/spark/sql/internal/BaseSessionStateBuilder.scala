32e0a78c712094b16d9af7fcacb0b045186d3550
Russell Spitzer <Russell.Spitzer@gmail.com>
2018-09-27 20:58:21 -0500

    [SPARK-25560][SQL] Allow FunctionInjection in SparkExtensions
    This allows an implementer of Spark Session Extensions to utilize a
    method "injectFunction" which will add a new function to the default
    Spark Session Catalog.

--- sql/core/src/main/scala/org/apache/spark/sql/internal/BaseSessionStateBuilder.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/BaseSessionStateBuilder.scala
@@ -95,7 +95,8 @@ abstract class BaseSessionStateBuilder(
    * This either gets cloned from a pre-existing version or cloned from the built-in registry.
    */
   protected lazy val functionRegistry: FunctionRegistry = {
-    parentState.map(_.functionRegistry).getOrElse(FunctionRegistry.builtin).clone()
+    parentState.map(_.functionRegistry.clone())
+      .getOrElse(extensions.registerFunctions(FunctionRegistry.builtin.clone()))
   }
 
   /**
