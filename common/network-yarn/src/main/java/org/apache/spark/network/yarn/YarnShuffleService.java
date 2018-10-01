4237f0cda610624ea353ae79ae432eb301b5d394
marek.simunek <marek.simunek@firma.seznam.cz>
2018-10-01 11:04:37 -0500

    [SPARK-18364][YARN] Expose metrics for YarnShuffleService
    ## What changes were proposed in this pull request?
    
    This PR is follow-up of closed  https://github.com/apache/spark/pull/17401 which only ended due to of inactivity, but its still nice feature to have.
    Given review by jerryshao taken in consideration and edited:
    - VisibleForTesting deleted because of dependency conflicts
    - removed unnecessary reflection for `MetricsSystemImpl`
    - added more available types for gauge
    
    ## How was this patch tested?
    
    Manual deploy of new yarn-shuffle jar into a Node Manager and verifying that the metrics appear in the Node Manager-standard location. This is JMX with an query endpoint running on `hostname:port`
    
    Resulting metrics look like this:
    ```
    curl -sk -XGET hostname:port |  grep -v '#' | grep 'shuffleService'
    hadoop_nodemanager_openblockrequestlatencymillis_rate15{name="shuffleService",} 0.31428910657834713
    hadoop_nodemanager_blocktransferratebytes_rate15{name="shuffleService",} 566144.9983653595
    hadoop_nodemanager_blocktransferratebytes_ratemean{name="shuffleService",} 2464409.9678099006
    hadoop_nodemanager_openblockrequestlatencymillis_rate1{name="shuffleService",} 1.2893844732240272
    hadoop_nodemanager_registeredexecutorssize{name="shuffleService",} 2.0
    hadoop_nodemanager_openblockrequestlatencymillis_ratemean{name="shuffleService",} 1.255574678369966
    hadoop_nodemanager_openblockrequestlatencymillis_count{name="shuffleService",} 315.0
    hadoop_nodemanager_openblockrequestlatencymillis_rate5{name="shuffleService",} 0.7661929192569739
    hadoop_nodemanager_registerexecutorrequestlatencymillis_ratemean{name="shuffleService",} 0.0
    hadoop_nodemanager_registerexecutorrequestlatencymillis_count{name="shuffleService",} 0.0
    hadoop_nodemanager_registerexecutorrequestlatencymillis_rate1{name="shuffleService",} 0.0
    hadoop_nodemanager_registerexecutorrequestlatencymillis_rate5{name="shuffleService",} 0.0
    hadoop_nodemanager_blocktransferratebytes_count{name="shuffleService",} 6.18271213E8
    hadoop_nodemanager_registerexecutorrequestlatencymillis_rate15{name="shuffleService",} 0.0
    hadoop_nodemanager_blocktransferratebytes_rate5{name="shuffleService",} 1154114.4881816586
    hadoop_nodemanager_blocktransferratebytes_rate1{name="shuffleService",} 574745.0749848988
    ```
    
    Closes #22485 from mareksimunek/SPARK-18364.
    
    Lead-authored-by: marek.simunek <marek.simunek@firma.seznam.cz>
    Co-authored-by: Andrew Ash <andrew@andrewash.com>
    Signed-off-by: Thomas Graves <tgraves@apache.org>

--- common/network-yarn/src/main/java/org/apache/spark/network/yarn/YarnShuffleService.java
+++ common/network-yarn/src/main/java/org/apache/spark/network/yarn/YarnShuffleService.java
@@ -35,6 +35,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.permission.FsPermission;
+import org.apache.hadoop.metrics2.impl.MetricsSystemImpl;
+import org.apache.hadoop.metrics2.lib.DefaultMetricsSystem;
 import org.apache.hadoop.yarn.api.records.ContainerId;
 import org.apache.hadoop.yarn.server.api.*;
 import org.apache.spark.network.util.LevelDBProvider;
@@ -167,6 +169,15 @@ public class YarnShuffleService extends AuxiliaryService {
 
       TransportConf transportConf = new TransportConf("shuffle", new HadoopConfigProvider(conf));
       blockHandler = new ExternalShuffleBlockHandler(transportConf, registeredExecutorFile);
+
+      // register metrics on the block handler into the Node Manager's metrics system.
+      YarnShuffleServiceMetrics serviceMetrics =
+        new YarnShuffleServiceMetrics(blockHandler.getAllMetrics());
+
+      MetricsSystemImpl metricsSystem = (MetricsSystemImpl) DefaultMetricsSystem.instance();
+      metricsSystem.register(
+        "sparkShuffleService", "Metrics on the Spark Shuffle Service", serviceMetrics);
+      logger.info("Registered metrics with Hadoop's DefaultMetricsSystem");
 
       // If authentication is enabled, set up the shuffle server to use a
       // special RPC handler that filters out unauthenticated fetch requests
