1a613326a690482b1c2c09aae3c5041c68ac924e
Justin Uang <juang@palantir.com>
2018-10-02 14:01:11 -0400

    address cr

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/CSVOptions.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/CSVOptions.scala
@@ -197,7 +197,7 @@ class CSVOptions(
     settings.setEmptyValue("")
     settings.setMaxCharsPerColumn(maxCharsPerColumn)
     settings.setUnescapedQuoteHandling(UnescapedQuoteHandling.STOP_AT_DELIMITER)
-    settings.setLineSeparatorDetectionEnabled(multiLine)
+    settings.setLineSeparatorDetectionEnabled(multiLine == true)
 
     settings
   }
