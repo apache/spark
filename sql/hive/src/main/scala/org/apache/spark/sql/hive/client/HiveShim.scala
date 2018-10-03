2ad9cf4923c19127efbdeef3fe6e3cd3e3f728ff
Karthik Manamcheri <karthik@cloudera.com>
2018-10-03 11:09:01 -0500

    Improved log messages

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveShim.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveShim.scala
@@ -746,6 +746,12 @@ private[client] class Shim_v0_13 extends Shim_v0_12 {
         getAllPartitionsMethod.invoke(hive, table).asInstanceOf[JSet[Partition]]
       } else {
         logDebug(s"Hive metastore filter is '$filter'.")
+        val tryDirectSqlConfVar = HiveConf.ConfVars.METASTORE_TRY_DIRECT_SQL
+        // We should get this config value from the metaStore. otherwise hit SPARK-18681.
+        // To be compatible with hive-0.12 and hive-0.13, In the future we can achieve this by:
+        // val tryDirectSql = hive.getMetaConf(tryDirectSqlConfVar.varname).toBoolean
+        val tryDirectSql = hive.getMSC.getConfigValue(tryDirectSqlConfVar.varname,
+          tryDirectSqlConfVar.defaultBoolVal.toString).toBoolean
         try {
           // Hive may throw an exception when calling this method in some circumstances, such as
           // when filtering on a non-string partition column.
@@ -753,11 +759,22 @@ private[client] class Shim_v0_13 extends Shim_v0_12 {
             .asInstanceOf[JArrayList[Partition]]
         } catch {
           case ex: InvocationTargetException if ex.getCause.isInstanceOf[MetaException] =>
-            logWarning("Caught Hive MetaException attempting to get partition metadata by " +
-              "filter from Hive. Falling back to fetching all partition metadata, which will " +
-              "degrade performance. Enable direct SQL mode in hive metastore to attempt " +
-              "to improve performance. However, Hive's direct SQL mode is an optimistic " +
-              "optimization and does not guarantee improved performance.")
+            if (!tryDirectSql) {
+              logWarning("Caught Hive MetaException attempting to get partition metadata by " +
+                "filter from Hive. Falling back to fetching all partition metadata, which will " +
+                "degrade performance. Modifying your Hive metastore configuration to set " +
+                s"${tryDirectSqlConfVar.varname} to true may resolve this problem.")
+            } else {
+              logWarning("Caught Hive MetaException attempting to get partition metadata by " +
+                "filter from Hive. Hive metastore's direct SQL feature has been enabled, but it " +
+                "is an optimistic optimization and not guaranteed to work. Falling back to " +
+                "fetching all partition metadata, which will degrade performance (for the " +
+                "current query). If you see this error consistently, you can set the Spark " +
+                s"configuration setting ${SQLConf.HIVE_MANAGE_FILESOURCE_PARTITIONS.key} to " +
+                "false as a work around, however this will result in degraded performance. " +
+                "Please report a bug to Hive stating that direct SQL is failing consistently for " +
+                "the specified query: https://issues.apache.org/jira/browse/HIVE")
+            }
             // HiveShim clients are expected to handle a superset of the requested partitions
             getAllPartitionsMethod.invoke(hive, table).asInstanceOf[JSet[Partition]]
         }
