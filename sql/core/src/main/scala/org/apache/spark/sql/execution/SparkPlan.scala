5a4176b7a355212da9ec87a5b00cb455500a1b05
Wenchen Fan <wenchen@databricks.com>
2018-10-04 20:15:21 +0800

    [SPARK-25602][SQL] SparkPlan.getByteArrayRdd should not consume the input when not necessary
    ## What changes were proposed in this pull request?
    
    In `SparkPlan.getByteArrayRdd`, we should only call `it.hasNext` when the limit is not hit, as `iter.hasNext` may produce one row and buffer it, and cause wrong metrics.
    
    ## How was this patch tested?
    
    new tests
    
    Closes #22621 from cloud-fan/range.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlan.scala
@@ -250,7 +250,9 @@ abstract class SparkPlan extends QueryPlan[SparkPlan] with Logging with Serializ
       val codec = CompressionCodec.createCodec(SparkEnv.get.conf)
       val bos = new ByteArrayOutputStream()
       val out = new DataOutputStream(codec.compressedOutputStream(bos))
-      while (iter.hasNext && (n < 0 || count < n)) {
+      // `iter.hasNext` may produce one row and buffer it, we should only call it when the limit is
+      // not hit.
+      while ((n < 0 || count < n) && iter.hasNext) {
         val row = iter.next().asInstanceOf[UnsafeRow]
         out.writeInt(row.getSizeInBytes)
         row.writeToStream(out, buffer)
