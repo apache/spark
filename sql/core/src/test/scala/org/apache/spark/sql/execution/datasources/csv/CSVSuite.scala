dd16ca349dd20626b6a8ae00f6dd445c269104b8
Maxim Gekk <maxim.gekk@databricks.com>
2018-10-06 11:12:33 +0200

    Test for backward slash as the delimiter

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/csv/CSVSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/csv/CSVSuite.scala
@@ -1820,4 +1820,10 @@ class CSVSuite extends QueryTest with SharedSQLContext with SQLTestUtils with Te
     checkAnswer(spark.read.option("multiLine", true).schema(schema).csv(input), Row(null))
     assert(spark.read.csv(input).collect().toSet == Set(Row()))
   }
+
+  test("using the backward slash as the delimiter") {
+    val input = Seq("""abc\1""").toDS()
+    val df = spark.read.option("delimiter", "\\").csv(input)
+    checkAnswer(df, Row("abc", "1"))
+  }
 }
