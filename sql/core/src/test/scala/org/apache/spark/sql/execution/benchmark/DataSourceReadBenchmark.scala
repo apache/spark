cf61f1c4df40b681f2db8cf233b8fbc0df88598b
Peter Toth <peter.toth@gmail.com>
2018-10-10 17:40:55 +0200

    [SPARK-25662][TEST] fix spark-submit command

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/DataSourceReadBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/DataSourceReadBenchmark.scala
@@ -36,7 +36,7 @@ import org.apache.spark.sql.vectorized.ColumnVector
  * Benchmark to measure data source read performance.
  * To run this benchmark:
  * {{{
- *   1. without sbt: bin/spark-submit --class <this class> <spark sql test jar>
+ *   1. without sbt: bin/spark-submit --class <this class> --jars <spark core test jar>,<spark catalyst test jar> <spark sql test jar>
  *   2. build/sbt "sql/test:runMain <this class>"
  *   3. generate result: SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "sql/test:runMain <this class>"
  *      Results will be written to "benchmarks/DataSourceReadBenchmark-results.txt".
