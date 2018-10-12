2bb2b93a2f9df63b4f1fbcbe31ef24585643498b
Luke Bodeen <luke_bodeen@yahoo.com>
2018-10-12 06:44:28 -0500

    [AIRFLOW-3119] Enable debugging with Celery(#3950)
    This will enable --loglevel when launching a 
    celery worker and inherit that LOGGING_LEVEL 
    setting from airflow.cfg

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -1040,6 +1040,7 @@ def worker(args):
         'queues': args.queues,
         'concurrency': args.concurrency,
         'hostname': args.celery_hostname,
+        'loglevel': conf.get('core', 'LOGGING_LEVEL'),
     }
 
     if args.daemon:
