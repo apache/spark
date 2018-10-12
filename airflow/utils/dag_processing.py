111dea274f91a0aed4c8fdf1e3c05585d86eb838
Xiaodong <xd_deng@hotmail.com>
2018-10-12 14:09:02 +0800

    [AIRFLOW-3183] Fix bug in DagFileProcessorManager.max_runs_reached() (#4031)
    The condition is intended to ensure the function
    will return False if any file's run_count is still smaller
    than max_run. But the operator used here is "!=".
    Instead, it should be "<".
    
    This is because in DagFileProcessorManager,
    there is no statement helping limit the upper
    limit of run_count. It's possible that
    files' run_count will be bigger than max_run.
    In such case, max_runs_reached() method
    may fail its purpose.

--- airflow/utils/dag_processing.py
+++ airflow/utils/dag_processing.py
@@ -578,7 +578,7 @@ class DagFileProcessorManager(LoggingMixin):
         if self._max_runs == -1:  # Unlimited runs.
             return False
         for file_path in self._file_paths:
-            if self._run_count[file_path] != self._max_runs:
+            if self._run_count[file_path] < self._max_runs:
                 return False
         if self._run_count[self._heart_beat_key] < self._max_runs:
             return False
