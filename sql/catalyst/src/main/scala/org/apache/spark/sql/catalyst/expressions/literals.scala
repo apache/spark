9115d26642496b1ca1fdf0d310d47ee0ebeb3222
Takeshi Yamamuro <yamamuro@apache.org>
2018-10-15 19:43:19 +0900

    Fix

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/literals.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/literals.scala
@@ -40,9 +40,10 @@ import org.json4s.JsonAST._
 import org.apache.spark.sql.AnalysisException
 import org.apache.spark.sql.catalyst.{CatalystTypeConverters, InternalRow, ScalaReflection}
 import org.apache.spark.sql.catalyst.expressions.codegen._
-import org.apache.spark.sql.catalyst.util.DateTimeUtils
+import org.apache.spark.sql.catalyst.util.{ArrayData, DateTimeUtils, MapData}
 import org.apache.spark.sql.types._
 import org.apache.spark.unsafe.types._
+import org.apache.spark.util.Utils
 
 object Literal {
   val TrueLiteral: Literal = Literal(true, BooleanType)
@@ -196,6 +197,31 @@ object Literal {
     case other =>
       throw new RuntimeException(s"no default for type $dataType")
   }
+
+  private[expressions] def validateLiteralValue(v: Any, dataType: DataType): Unit = {
+    def doValidate(v: Any, dataType: DataType): Boolean = dataType match {
+      case BooleanType => v.isInstanceOf[Boolean]
+      case ByteType => v.isInstanceOf[Byte]
+      case ShortType => v.isInstanceOf[Short]
+      case IntegerType | DateType => v.isInstanceOf[Int]
+      case LongType | TimestampType => v.isInstanceOf[Long]
+      case FloatType => v.isInstanceOf[Float]
+      case DoubleType => v.isInstanceOf[Double]
+      case _: DecimalType => v.isInstanceOf[Decimal]
+      case CalendarIntervalType => v.isInstanceOf[CalendarInterval]
+      case BinaryType => v.isInstanceOf[Array[Byte]]
+      case StringType => v.isInstanceOf[UTF8String]
+      case _: StructType => v.isInstanceOf[InternalRow]
+      case _: ArrayType => v.isInstanceOf[ArrayData]
+      case _: MapType => v.isInstanceOf[MapData]
+      case ObjectType(cls) => cls.isInstance(v)
+      case udt: UserDefinedType[_] => doValidate(v, udt.sqlType)
+      case _ => false
+    }
+    require(v == null || doValidate(v, dataType),
+      s"Literal must have a corresponding value to ${dataType.catalogString}, " +
+      s"but ${if (v != null) s"class ${Utils.getSimpleName(v.getClass)}" else "null"} found.")
+  }
 }
 
 /**
@@ -239,6 +265,8 @@ object DecimalLiteral {
  * In order to do type checking, use Literal.create() instead of constructor
  */
 case class Literal (value: Any, dataType: DataType) extends LeafExpression {
+
+  Literal.validateLiteralValue(value, dataType)
 
   override def foldable: Boolean = true
   override def nullable: Boolean = value == null
