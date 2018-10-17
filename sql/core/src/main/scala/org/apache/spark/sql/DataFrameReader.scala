2d6319f83aa5c636c41c772a0d2d124f5a843856
Maxim Gekk <maxim.gekk@databricks.com>
2018-10-17 09:32:05 +0800

    [SPARK-25393][SQL] Adding new function from_csv()
    ## What changes were proposed in this pull request?
    
    The PR adds new function `from_csv()` similar to `from_json()` to parse columns with CSV strings. I added the following methods:
    ```Scala
    def from_csv(e: Column, schema: StructType, options: Map[String, String]): Column
    ```
    and this signature to call it from Python, R and Java:
    ```Scala
    def from_csv(e: Column, schema: String, options: java.util.Map[String, String]): Column
    ```
    
    ## How was this patch tested?
    
    Added new test suites `CsvExpressionsSuite`, `CsvFunctionsSuite` and sql tests.
    
    Closes #22379 from MaxGekk/from_csv.
    
    Lead-authored-by: Maxim Gekk <maxim.gekk@databricks.com>
    Co-authored-by: Maxim Gekk <max.gekk@gmail.com>
    Co-authored-by: Hyukjin Kwon <gurwls223@gmail.com>
    Co-authored-by: hyukjinkwon <gurwls223@apache.org>
    Signed-off-by: hyukjinkwon <gurwls223@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameReader.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameReader.scala
@@ -22,16 +22,17 @@ import java.util.{Locale, Properties}
 import scala.collection.JavaConverters._
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.univocity.parsers.csv.CsvParser
 
 import org.apache.spark.Partition
 import org.apache.spark.annotation.InterfaceStability
 import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.csv.{CSVHeaderChecker, CSVOptions, UnivocityParser}
 import org.apache.spark.sql.catalyst.json.{CreateJacksonParser, JacksonParser, JSONOptions}
+import org.apache.spark.sql.catalyst.util.FailureSafeParser
 import org.apache.spark.sql.execution.command.DDLUtils
-import org.apache.spark.sql.execution.datasources.{DataSource, FailureSafeParser}
+import org.apache.spark.sql.execution.datasources.DataSource
 import org.apache.spark.sql.execution.datasources.csv._
 import org.apache.spark.sql.execution.datasources.jdbc._
 import org.apache.spark.sql.execution.datasources.json.TextInputJsonDataSource
