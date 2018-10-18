6c843e881163c3cf581584f95a82fba5697b42a0
Marco Gaido <marcogaido91@gmail.com>
2018-10-18 10:32:25 -0700

    [SPARK-25758][ML] Deprecate computeCost on BisectingKMeans
    ## What changes were proposed in this pull request?
    
    The PR proposes to deprecate the `computeCost` method on `BisectingKMeans` in favor of the adoption of `ClusteringEvaluator` in order to evaluate the clustering.
    
    ## How was this patch tested?
    
    NA
    
    Closes #22756 from mgaido91/SPARK-25758.
    
    Authored-by: Marco Gaido <marcogaido91@gmail.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- python/pyspark/ml/clustering.py
+++ python/pyspark/ml/clustering.py
@@ -540,7 +540,13 @@ class BisectingKMeansModel(JavaModel, JavaMLWritable, JavaMLReadable):
         """
         Computes the sum of squared distances between the input points
         and their corresponding cluster centers.
+
+        ..note:: Deprecated in 2.4.0. It will be removed in 3.0.0. Use ClusteringEvaluator instead.
+           You can also get the cost on the training dataset in the summary.
         """
+        warnings.warn("Deprecated in 2.4.0. It will be removed in 3.0.0. Use ClusteringEvaluator "
+                      "instead. You can also get the cost on the training dataset in the summary.",
+                      DeprecationWarning)
         return self._call_java("computeCost", dataset)
 
     @property
