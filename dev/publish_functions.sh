2d7841f6c3b27d1460ec35b8a223f30658fc45b3
mcheah <mcheah@palantir.com>
2018-10-17 18:09:32 -0700

    Do git clean in publish_functions.sh

--- dev/publish_functions.sh
+++ dev/publish_functions.sh
@@ -22,6 +22,7 @@ publish_artifacts() {
   echo "</server></servers></settings>" >> $tmp_settings
 
   ./build/mvn -T 1C --settings $tmp_settings -DskipTests "${PALANTIR_FLAGS[@]}" deploy
+  git clean -dfx
   ./gradlew --info bintrayUpload
 }
 
