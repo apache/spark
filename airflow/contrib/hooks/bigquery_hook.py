cc360e5fd470794b58124fc5433cf541e1ea766b
Kaxil Naik <kaxilnaik@gmail.com>
2018-10-22 10:29:30 +0100

    [AIRFLOW-461] Restore parameter position for BQ run_load method (#4077)

--- airflow/contrib/hooks/bigquery_hook.py
+++ airflow/contrib/hooks/bigquery_hook.py
@@ -832,8 +832,8 @@ class BigQueryBaseCursor(LoggingMixin):
 
     def run_load(self,
                  destination_project_dataset_table,
-                 source_uris,
                  schema_fields=None,
+                 source_uris,
                  source_format='CSV',
                  create_disposition='CREATE_IF_NEEDED',
                  skip_leading_rows=0,
