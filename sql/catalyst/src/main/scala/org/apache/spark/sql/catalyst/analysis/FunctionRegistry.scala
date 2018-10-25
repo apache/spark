7e919e3efa3d669a6893b47c737e553795d94347
Wenchen Fan <wenchen@databricks.com>
2018-10-25 12:42:17 +0800

    remove newly added map related functions from FunctionRegistry

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
@@ -433,13 +433,9 @@ object FunctionRegistry {
     expression[ArrayRemove]("array_remove"),
     expression[ArrayDistinct]("array_distinct"),
     expression[ArrayTransform]("transform"),
-    expression[MapFilter]("map_filter"),
     expression[ArrayFilter]("filter"),
     expression[ArrayExists]("exists"),
     expression[ArrayAggregate]("aggregate"),
-    expression[TransformValues]("transform_values"),
-    expression[TransformKeys]("transform_keys"),
-    expression[MapZipWith]("map_zip_with"),
     expression[ZipWith]("zip_with"),
 
     CreateStruct.registryEntry,
