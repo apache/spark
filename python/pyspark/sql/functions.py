1757a603fa123e3a81a7bfc06f9b58ee328f11b0
Wenchen Fan <wenchen@databricks.com>
2018-10-27 00:43:16 +0800

    HOT-FIX pyspark import

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -27,7 +27,7 @@ if sys.version < "3":
 
 from pyspark import since, SparkContext
 from pyspark.rdd import ignore_unicode_prefix, PythonEvalType
-from pyspark.sql.column import Column, _to_java_column, _to_seq
+from pyspark.sql.column import Column, _to_java_column, _to_seq, _create_column_from_literal
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.types import StringType, DataType
 # Keep UserDefinedFunction import for backwards compatible import; moved in SPARK-22409
