64b94d8c1e7720d56a5b9f1fbaed244caad328d8
marengaz <marengaz@users.noreply.github.com>
2018-10-29 14:28:52 +0000

    [AIRFLOW-3260] Correct misleading BigQuery error (#4098)

--- airflow/contrib/operators/bigquery_operator.py
+++ airflow/contrib/operators/bigquery_operator.py
@@ -185,7 +185,7 @@ class BigQueryOperator(BaseOperator):
     def on_kill(self):
         super(BigQueryOperator, self).on_kill()
         if self.bq_cursor is not None:
-            self.log.info('Canceling running query due to execution timeout')
+            self.log.info('Cancelling running query')
             self.bq_cursor.cancel_query()
 
 
