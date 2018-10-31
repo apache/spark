68dde3481ea458b0b8deeec2f99233c2d4c1e056
Marcelo Vanzin <vanzin@cloudera.com>
2018-10-31 13:00:10 -0500

    [SPARK-23781][CORE] Merge token renewer functionality into HadoopDelegationTokenManager.
    This avoids having two classes to deal with tokens; now the above
    class is a one-stop shop for dealing with delegation tokens. The
    YARN backend extends that class instead of doing composition like
    before, resulting in a bit less code there too.
    
    The renewer functionality is basically the same code that used to
    be in YARN's AMCredentialRenewer. That is also the reason why the
    public API of HadoopDelegationTokenManager is a little bit odd;
    the YARN AM has some odd requirements for how this all should be
    initialized, and the weirdness is needed currently to support that.
    
    Tested:
    - YARN with stress app for DT renewal
    - Mesos and K8S with basic kerberos tests (both tgt and keytab)
    
    Closes #22624 from vanzin/SPARK-23781.
    
    Authored-by: Marcelo Vanzin <vanzin@cloudera.com>
    Signed-off-by: Imran Rashid <irashid@cloudera.com>

--- core/src/main/scala/org/apache/spark/deploy/security/HadoopFSDelegationTokenProvider.scala
+++ core/src/main/scala/org/apache/spark/deploy/security/HadoopFSDelegationTokenProvider.scala
@@ -30,7 +30,7 @@ import org.apache.spark.{SparkConf, SparkException}
 import org.apache.spark.internal.Logging
 import org.apache.spark.internal.config._
 
-private[deploy] class HadoopFSDelegationTokenProvider(fileSystems: Configuration => Set[FileSystem])
+private[deploy] class HadoopFSDelegationTokenProvider(fileSystems: () => Set[FileSystem])
     extends HadoopDelegationTokenProvider with Logging {
 
   // This tokenRenewalInterval will be set in the first call to obtainDelegationTokens.
@@ -44,8 +44,7 @@ private[deploy] class HadoopFSDelegationTokenProvider(fileSystems: Configuration
       hadoopConf: Configuration,
       sparkConf: SparkConf,
       creds: Credentials): Option[Long] = {
-
-    val fsToGetTokens = fileSystems(hadoopConf)
+    val fsToGetTokens = fileSystems()
     val fetchCreds = fetchDelegationTokens(getTokenRenewer(hadoopConf), fsToGetTokens, creds)
 
     // Get the token renewal interval if it is not set. It will only be called once.
