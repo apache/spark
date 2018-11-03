8c59caaa9649f7bf3c05cc3a62da5eb5df6b4b05
Maxim Gekk <max.gekk@gmail.com>
2018-11-03 18:28:01 +0300

    Removing unneeded test

--- sql/core/src/test/resources/sql-tests/inputs/csv-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/csv-functions.sql
@@ -21,4 +21,3 @@ select to_csv(named_struct('time', to_timestamp('2015-08-26', 'yyyy-MM-dd')), ma
 -- Check if errors handled
 select to_csv(named_struct('a', 1, 'b', 2), named_struct('mode', 'PERMISSIVE'));
 select to_csv(named_struct('a', 1, 'b', 2), map('mode', 1));
-select to_csv();
