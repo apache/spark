10da1120d52f61e98bcd929d7ad59220a93d59f7
Matt Molek <mpmolek@gmail.com>
2018-11-03 15:33:02 -0400

    [SPARK-25934] Don't propagate SPARK_CONF_DIR from spark submit

--- core/src/main/scala/org/apache/spark/deploy/rest/RestSubmissionClient.scala
+++ core/src/main/scala/org/apache/spark/deploy/rest/RestSubmissionClient.scala
@@ -418,8 +418,8 @@ private[spark] object RestSubmissionClient {
   private[rest] def filterSystemEnvironment(env: Map[String, String]): Map[String, String] = {
     env.filterKeys { k =>
       // SPARK_HOME is filtered out because it is usually wrong on the remote machine (SPARK-12345)
-      (k.startsWith("SPARK_") && k != "SPARK_ENV_LOADED" && k != "SPARK_HOME") ||
-        k.startsWith("MESOS_")
+      (k.startsWith("SPARK_") && k != "SPARK_ENV_LOADED" && k != "SPARK_HOME"
+        && k != "SPARK_CONF_DIR") || k.startsWith("MESOS_")
     }
   }
 }
