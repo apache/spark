6ac6d7d0cd82e284a48957ee96f548fede8ee128
Matt Molek <mpmolek@gmail.com>
2018-11-06 12:57:46 -0500

    Put blacklisted env vars in a set

--- core/src/main/scala/org/apache/spark/deploy/rest/RestSubmissionClient.scala
+++ core/src/main/scala/org/apache/spark/deploy/rest/RestSubmissionClient.scala
@@ -408,6 +408,7 @@ private[spark] class RestSubmissionClient(master: String) extends Logging {
 }
 
 private[spark] object RestSubmissionClient {
+  private val BLACKLISTED_SPARK_ENV_VARS = Set("SPARK_ENV_LOADED", "SPARK_HOME", "SPARK_CONF_DIR")
   private val REPORT_DRIVER_STATUS_INTERVAL = 1000
   private val REPORT_DRIVER_STATUS_MAX_TRIES = 10
   val PROTOCOL_VERSION = "v1"
@@ -419,8 +420,7 @@ private[spark] object RestSubmissionClient {
     env.filterKeys { k =>
       // SPARK_HOME and SPARK_CONF_DIR are filtered out because they are usually wrong
       // on the remote machine (SPARK-12345) (SPARK-25934)
-      (k.startsWith("SPARK_") && k != "SPARK_ENV_LOADED" && k != "SPARK_HOME"
-        && k != "SPARK_CONF_DIR") || k.startsWith("MESOS_")
+      (k.startsWith("SPARK_") && !BLACKLISTED_SPARK_ENV_VARS.contains(k)) || k.startsWith("MESOS_")
     }
   }
 }
