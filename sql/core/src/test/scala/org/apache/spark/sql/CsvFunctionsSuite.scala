93da7604c2d74f97b12e9210f0bcaf038774ea41
Maxim Gekk <max.gekk@gmail.com>
2018-11-06 12:04:41 +0300

    Fix imports

--- sql/core/src/test/scala/org/apache/spark/sql/CsvFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CsvFunctionsSuite.scala
@@ -21,6 +21,7 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 import scala.collection.JavaConverters._
+
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.test.SharedSQLContext
 import org.apache.spark.sql.types._
