b1d9111fc6d8305e4720111029a9212083e717d7
Kaxil Naik <kaxilnaik@gmail.com>
2018-11-07 11:07:15 +0000

    [AIRFLOW-XXX] Pin Flask App Builder to Fix CI (#4150)

--- setup.py
+++ setup.py
@@ -298,7 +298,7 @@ def do_setup():
             'croniter>=0.3.17, <0.4',
             'dill>=0.2.2, <0.3',
             'flask>=0.12.4, <0.13',
-            'flask-appbuilder>=1.12, <2.0.0',
+            'flask-appbuilder==1.12.1',
             'flask-admin==1.4.1',
             'flask-caching>=1.3.3, <1.4.0',
             'flask-login>=0.3, <0.5',
