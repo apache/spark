79551f558dafed41177b605b0436e9340edf5712
Maxim Gekk <max.gekk@gmail.com>
2018-11-09 09:45:06 +0800

    [SPARK-25945][SQL] Support locale while parsing date/timestamp from CSV/JSON
    ## What changes were proposed in this pull request?
    
    In the PR, I propose to add new option `locale` into CSVOptions/JSONOptions to make parsing date/timestamps in local languages possible. Currently the locale is hard coded to `Locale.US`.
    
    ## How was this patch tested?
    
    Added two tests for parsing a date from CSV/JSON - `ноя 2018`.
    
    Closes #22951 from MaxGekk/locale.
    
    Authored-by: Maxim Gekk <max.gekk@gmail.com>
    Signed-off-by: hyukjinkwon <gurwls223@apache.org>

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CsvExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CsvExpressionsSuite.scala
@@ -17,7 +17,8 @@
 
 package org.apache.spark.sql.catalyst.expressions
 
-import java.util.Calendar
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Locale}
 
 import org.scalatest.exceptions.TestFailedException
 
@@ -208,5 +209,21 @@ class CsvExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper with P
         gmtId),
       "2015-12-31T16:00:00"
     )
+  }
+
+  test("parse date with locale") {
+    Seq("en-US", "ru-RU").foreach { langTag =>
+      val locale = Locale.forLanguageTag(langTag)
+      val date = new SimpleDateFormat("yyyy-MM-dd").parse("2018-11-05")
+      val schema = new StructType().add("d", DateType)
+      val dateFormat = "MMM yyyy"
+      val sdf = new SimpleDateFormat(dateFormat, locale)
+      val dateStr = sdf.format(date)
+      val options = Map("dateFormat" -> dateFormat, "locale" -> langTag)
+
+      checkEvaluation(
+        CsvToStructs(schema, options, Literal.create(dateStr), gmtId),
+        InternalRow(17836)) // number of days from 1970-01-01
+    }
   }
 }
