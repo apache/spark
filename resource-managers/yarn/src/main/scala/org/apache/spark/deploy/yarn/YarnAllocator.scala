e7ff43d0056e38033ae2a2edf81b296797bb46e0
Yuming Wang <yumwang@ebay.com>
2018-11-14 15:46:58 +0800

    Make memLimitExceededLogMessage more clean

--- resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/YarnAllocator.scala
+++ resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/YarnAllocator.scala
@@ -745,8 +745,12 @@ private object YarnAllocator {
   def memLimitExceededLogMessage(diagnostics: String, pattern: Pattern): String = {
     val matcher = pattern.matcher(diagnostics)
     val diag = if (matcher.find()) " " + matcher.group() + "." else ""
+    val additional = if (pattern == VMEM_EXCEEDED_PATTERN) {
+      s" or disabling ${YarnConfiguration.NM_VMEM_CHECK_ENABLED} because of YARN-4714"
+    } else {
+      ""
+    }
     s"Container killed by YARN for exceeding memory limits. $diag " +
-      "Consider boosting spark.yarn.executor.memoryOverhead or " +
-      "disabling yarn.nodemanager.vmem-check-enabled because of YARN-4714."
+      s"Consider boosting ${EXECUTOR_MEMORY_OVERHEAD.key}$additional."
   }
 }
