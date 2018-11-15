63c70e5591b82fe16dcfd7a30d892dccb79bc72e
Marco Gaido <marcogaido91@gmail.com>
2018-11-15 09:10:44 +0100

    simplify ut

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -2556,31 +2556,27 @@ class DataFrameSuite extends QueryTest with SharedSQLContext {
   }
 
   test("SPARK-26057: attribute deduplication on already analyzed plans") {
-    withTempView("cc", "p", "c") {
-      val df1 = Seq(("1-1", "sp", 6)).toDF("id", "layout", "n")
-      df1.createOrReplaceTempView("cc")
-      val df2 = Seq(("sp", 1)).toDF("layout", "ts")
-      df2.createOrReplaceTempView("p")
-      val df3 = Seq(("1-1", "sp", 3)).toDF("id", "layout", "ts")
-      df3.createOrReplaceTempView("c")
+    withTempView("a", "b", "c", "v") {
+      val df1 = Seq(("1-1", 6)).toDF("id", "n")
+      df1.createOrReplaceTempView("a")
+      val df3 = Seq("1-1").toDF("id")
+      df3.createOrReplaceTempView("b")
       spark.sql(
         """
-          |SELECT cc.id, cc.layout, count(*) as m
-          |FROM cc
-          |JOIN p USING(layout)
+          |SELECT a.id, n as m
+          |FROM a
           |WHERE EXISTS(
           |  SELECT 1
-          |  FROM c
-          |  WHERE c.id = cc.id AND c.layout = cc.layout AND c.ts > p.ts)
-          |GROUP BY cc.id, cc.layout
-        """.stripMargin).createOrReplaceTempView("pcc")
+          |  FROM b
+          |  WHERE b.id = a.id)
+        """.stripMargin).createOrReplaceTempView("v")
       val res = spark.sql(
         """
-          |SELECT cc.id, cc.layout, n, m
-          |  FROM cc
-          |  LEFT OUTER JOIN pcc ON pcc.id = cc.id AND pcc.layout = cc.layout
+          |SELECT a.id, n, m
+          |  FROM a
+          |  LEFT OUTER JOIN v ON v.id = a.id
         """.stripMargin)
-      checkAnswer(res, Row("1-1", "sp", 6, 1))
+      checkAnswer(res, Row("1-1", 6, 6))
 
     }
   }
