9880d31ff4f4982a6fae630c8a670341646a1954
Nihar Sheth <niharrsheth@gmail.com>
2018-11-26 11:06:02 -0800

    [SPARK-25960][K8S] Support subpath mounting with Kubernetes
    ## What changes were proposed in this pull request?
    
    This PR adds configurations to use subpaths with Spark on k8s. Subpaths (https://kubernetes.io/docs/concepts/storage/volumes/#using-subpath) allow the user to specify a path within a volume to use instead of the volume's root.
    
    ## How was this patch tested?
    
    Added unit tests. Ran SparkPi on a cluster with event logging pointed at a subpath-mount and verified the driver host created and used the subpath.
    
    Closes #23026 from NiharS/k8s_subpath.
    
    Authored-by: Nihar Sheth <niharrsheth@gmail.com>
    Signed-off-by: Marcelo Vanzin <vanzin@cloudera.com>

--- resource-managers/kubernetes/core/src/test/scala/org/apache/spark/scheduler/cluster/k8s/KubernetesExecutorBuilderSuite.scala
+++ resource-managers/kubernetes/core/src/test/scala/org/apache/spark/scheduler/cluster/k8s/KubernetesExecutorBuilderSuite.scala
@@ -107,6 +107,7 @@ class KubernetesExecutorBuilderSuite extends SparkFunSuite {
     val volumeSpec = KubernetesVolumeSpec(
       "volume",
       "/tmp",
+      "",
       false,
       KubernetesHostPathVolumeConf("/checkpoint"))
     val conf = KubernetesConf(
