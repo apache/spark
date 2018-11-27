e1e97aabb45cf611221ee7baa8d0351b32fbb770
Mark Pavey <markpavey@exabre.co.uk>
2018-11-27 09:39:09 +0000

    [SPARK-26137][CORE] Use File.separatorChar instead of File.separator

--- core/src/main/scala/org/apache/spark/deploy/DependencyUtils.scala
+++ core/src/main/scala/org/apache/spark/deploy/DependencyUtils.scala
@@ -19,7 +19,6 @@ package org.apache.spark.deploy
 
 import java.io.File
 import java.net.URI
-import java.util.regex.Pattern
 
 import org.apache.commons.lang3.StringUtils
 import org.apache.hadoop.conf.Configuration
@@ -62,8 +61,7 @@ private[deploy] object DependencyUtils extends Logging {
       hadoopConf: Configuration,
       secMgr: SecurityManager): String = {
     val targetDir = Utils.createTempDir()
-    val fileSeparator = Pattern.quote(File.separator)
-    val userJarName = userJar.split(fileSeparator).last
+    val userJarName = userJar.split(File.separatorChar).last
     Option(jars)
       .map {
         resolveGlobPaths(_, hadoopConf)
