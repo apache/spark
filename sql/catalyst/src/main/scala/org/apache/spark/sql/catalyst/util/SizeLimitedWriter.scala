3171cf31ae8001e5c37b31668154bbf2b6b4411f
Dave DeCaprio <daved@alum.mit.edu>
2018-11-28 17:10:35 -0600

    Coverted to javadoc style multiline comment

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/SizeLimitedWriter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/SizeLimitedWriter.scala
@@ -20,28 +20,29 @@ package org.apache.spark.sql.catalyst.util
 import java.io.Writer
 
 class WriterSizeException(val attemptedSize: Long, val charLimit: Long) extends Exception(
-	s"Attempted to write $attemptedSize characters to a writer that is limited to $charLimit")
+  s"Attempted to write $attemptedSize characters to a writer that is limited to $charLimit")
 
 /**
-	* This class is used to control the size of generated writers.  Guarantees that the total number
-	* of characters written will be less than the specified size.
-	*
-	* Checks size before writing and throws a WriterSizeException if the total size would count the limit.
-	*/
+ * This class is used to control the size of generated writers.  Guarantees that the total number
+ * of characters written will be less than the specified size.
+ *
+ * Checks size before writing and throws a WriterSizeException if the total size would count the
+ * limit.
+ */
 class SizeLimitedWriter(underlying: Writer, charLimit: Long) extends Writer {
 
-	var charsWritten: Long = 0
+  var charsWritten: Long = 0
 
-	override def write(cbuf: Array[Char], off: Int, len: Int): Unit = {
-		val newLength = charsWritten + Math.min(cbuf.length - off, len)
-		if(newLength > charLimit) {
-			throw new WriterSizeException(newLength, charLimit)
-		}
-		charsWritten = newLength
-		underlying.write(cbuf, off, len)
-	}
+  override def write(cbuf: Array[Char], off: Int, len: Int): Unit = {
+    val newLength = charsWritten + Math.min(cbuf.length - off, len)
+    if (newLength > charLimit) {
+      throw new WriterSizeException(newLength, charLimit)
+    }
+    charsWritten = newLength
+    underlying.write(cbuf, off, len)
+  }
 
-	override def flush(): Unit = underlying.flush()
+  override def flush(): Unit = underlying.flush()
 
-	override def close(): Unit = underlying.close()
+  override def close(): Unit = underlying.close()
 }
