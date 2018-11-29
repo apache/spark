5c7af7765260d8eeb9e106a36dd9d57b4b029e67
Maxim Gekk <max.gekk@gmail.com>
2018-11-29 10:31:31 -0600

    [SPARK-26081][SQL] Prevent empty files for empty partitions in Text datasources
    ## What changes were proposed in this pull request?
    
    In the PR, I propose to postpone creation of `OutputStream`/`Univocity`/`JacksonGenerator` till the first row should be written. This prevents creation of empty files for empty partitions. So, no need to open and to read such files back while loading data from the location.
    
    ## How was this patch tested?
    
    Added tests for Text, JSON and CSV datasource where empty dataset is written but should not produce any files.
    
    Closes #23052 from MaxGekk/text-empty-files.
    
    Lead-authored-by: Maxim Gekk <max.gekk@gmail.com>
    Co-authored-by: Maxim Gekk <maxim.gekk@databricks.com>
    Signed-off-by: Sean Owen <sean.owen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/CSVFileFormat.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/CSVFileFormat.scala
@@ -169,13 +169,19 @@ private[csv] class CsvOutputWriter(
     context: TaskAttemptContext,
     params: CSVOptions) extends OutputWriter with Logging {
 
-  private val charset = Charset.forName(params.charset)
+  private var univocityGenerator: Option[UnivocityGenerator] = None
 
-  private val writer = CodecStreams.createOutputStreamWriter(context, new Path(path), charset)
+  override def write(row: InternalRow): Unit = {
+    val gen = univocityGenerator.getOrElse {
+      val charset = Charset.forName(params.charset)
+      val os = CodecStreams.createOutputStreamWriter(context, new Path(path), charset)
+      val newGen = new UnivocityGenerator(dataSchema, os, params)
+      univocityGenerator = Some(newGen)
+      newGen
+    }
 
-  private val gen = new UnivocityGenerator(dataSchema, writer, params)
+    gen.write(row)
+  }
 
-  override def write(row: InternalRow): Unit = gen.write(row)
-
-  override def close(): Unit = gen.close()
+  override def close(): Unit = univocityGenerator.map(_.close())
 }
