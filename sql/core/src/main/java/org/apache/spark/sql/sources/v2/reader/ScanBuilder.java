2b2c94a3ee89630047bcdd416a977e0d1cdb1926
Wenchen Fan <wenchen@databricks.com>
2018-11-30 00:02:43 -0800

    [SPARK-25528][SQL] data source v2 API refactor (batch read)
    ## What changes were proposed in this pull request?
    
    This is the first step of the data source v2 API refactor [proposal](https://docs.google.com/document/d/1uUmKCpWLdh9vHxP7AWJ9EgbwB_U6T3EJYNjhISGmiQg/edit?usp=sharing)
    
    It adds the new API for batch read, without removing the old APIs, as they are still needed for streaming sources.
    
    More concretely, it adds
    1. `TableProvider`, works like an anonymous catalog
    2. `Table`, represents a structured data set.
    3. `ScanBuilder` and `Scan`, a logical represents of data source scan
    4. `Batch`, a physical representation of data source batch scan.
    
    ## How was this patch tested?
    
    existing tests
    
    Closes #23086 from cloud-fan/refactor-batch.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: gatorsmile <gatorsmile@gmail.com>

--- sql/core/src/main/java/org/apache/spark/sql/sources/v2/reader/ScanBuilder.java
+++ sql/core/src/main/java/org/apache/spark/sql/sources/v2/reader/ScanBuilder.java
@@ -18,25 +18,13 @@
 package org.apache.spark.sql.sources.v2.reader;
 
 import org.apache.spark.annotation.Evolving;
-import org.apache.spark.sql.types.StructType;
 
 /**
- * A mix-in interface for {@link ScanBuilder}. Data sources can implement this
- * interface to push down required columns to the data source and only read these columns during
- * scan to reduce the size of the data to be read.
+ * An interface for building the {@link Scan}. Implementations can mixin SupportsPushDownXYZ
+ * interfaces to do operator pushdown, and keep the operator pushdown result in the returned
+ * {@link Scan}.
  */
 @Evolving
-public interface SupportsPushDownRequiredColumns extends ScanBuilder {
-
-  /**
-   * Applies column pruning w.r.t. the given requiredSchema.
-   *
-   * Implementation should try its best to prune the unnecessary columns or nested fields, but it's
-   * also OK to do the pruning partially, e.g., a data source may not be able to prune nested
-   * fields, and only prune top-level columns.
-   *
-   * Note that, {@link ScanConfig#readSchema()} implementation should take care of the column
-   * pruning applied here.
-   */
-  void pruneColumns(StructType requiredSchema);
+public interface ScanBuilder {
+  Scan build();
 }
