69a2978673c8f383dfd755add32a983ba83e449d
Hyukjin Kwon <gurwls223@apache.org>
2018-12-03 14:03:10 -0800

    [SPARK-26033][SPARK-26034][PYTHON][FOLLOW-UP] Small cleanup and deduplication in ml/mllib tests
    ## What changes were proposed in this pull request?
    
    This PR is a small follow up that puts some logic and functions into smaller scope and make it localized, and deduplicate.
    
    ## How was this patch tested?
    
    Manually tested. Jenkins tests as well.
    
    Closes #23200 from HyukjinKwon/followup-SPARK-26034-SPARK-26033.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Bryan Cutler <cutlerb@gmail.com>

--- python/pyspark/mllib/tests/test_algorithms.py
+++ python/pyspark/mllib/tests/test_algorithms.py
@@ -26,10 +26,8 @@ from py4j.protocol import Py4JJavaError
 from pyspark.mllib.fpm import FPGrowth
 from pyspark.mllib.recommendation import Rating
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.testing.mllibutils import make_serializer, MLlibTestCase
-
-
-ser = make_serializer()
+from pyspark.serializers import PickleSerializer
+from pyspark.testing.mllibutils import MLlibTestCase
 
 
 class ListTests(MLlibTestCase):
@@ -265,6 +263,7 @@ class ListTests(MLlibTestCase):
 class ALSTests(MLlibTestCase):
 
     def test_als_ratings_serialize(self):
+        ser = PickleSerializer()
         r = Rating(7, 1123, 3.14)
         jr = self.sc._jvm.org.apache.spark.mllib.api.python.SerDe.loads(bytearray(ser.dumps(r)))
         nr = ser.loads(bytes(self.sc._jvm.org.apache.spark.mllib.api.python.SerDe.dumps(jr)))
@@ -273,6 +272,7 @@ class ALSTests(MLlibTestCase):
         self.assertAlmostEqual(r.rating, nr.rating, 2)
 
     def test_als_ratings_id_long_error(self):
+        ser = PickleSerializer()
         r = Rating(1205640308657491975, 50233468418, 1.0)
         # rating user id exceeds max int value, should fail when pickled
         self.assertRaises(Py4JJavaError, self.sc._jvm.org.apache.spark.mllib.api.python.SerDe.loads,
