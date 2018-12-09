33460c58a9274e22bd662858c71292275ae4aa24
Wenchen Fan <wenchen@databricks.com>
2018-12-09 10:50:41 -0800

    [SPARK-26021][2.4][SQL][FOLLOWUP] only deal with NaN and -0.0 in UnsafeWriter
    backport https://github.com/apache/spark/pull/23239 to 2.4
    
    ---------
    
    ## What changes were proposed in this pull request?
    
    A followup of https://github.com/apache/spark/pull/23043
    
    There are 4 places we need to deal with NaN and -0.0:
    1. comparison expressions. `-0.0` and `0.0` should be treated as same. Different NaNs should be treated as same.
    2. Join keys. `-0.0` and `0.0` should be treated as same. Different NaNs should be treated as same.
    3. grouping keys. `-0.0` and `0.0` should be assigned to the same group. Different NaNs should be assigned to the same group.
    4. window partition keys. `-0.0` and `0.0` should be treated as same. Different NaNs should be treated as same.
    
    The case 1 is OK. Our comparison already handles NaN and -0.0, and for struct/array/map, we will recursively compare the fields/elements.
    
    Case 2, 3 and 4 are problematic, as they compare `UnsafeRow` binary directly, and different NaNs have different binary representation, and the same thing happens for -0.0 and 0.0.
    
    To fix it, a simple solution is: normalize float/double when building unsafe data (`UnsafeRow`, `UnsafeArrayData`, `UnsafeMapData`). Then we don't need to worry about it anymore.
    
    Following this direction, this PR moves the handling of NaN and -0.0 from `Platform` to `UnsafeWriter`, so that places like `UnsafeRow.setFloat` will not handle them, which reduces the perf overhead. It's also easier to add comments explaining why we do it in `UnsafeWriter`.
    
    ## How was this patch tested?
    
    existing tests
    
    Closes #23265 from cloud-fan/minor.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- common/unsafe/src/main/java/org/apache/spark/unsafe/Platform.java
+++ common/unsafe/src/main/java/org/apache/spark/unsafe/Platform.java
@@ -120,11 +120,6 @@ public final class Platform {
   }
 
   public static void putFloat(Object object, long offset, float value) {
-    if (Float.isNaN(value)) {
-      value = Float.NaN;
-    } else if (value == -0.0f) {
-      value = 0.0f;
-    }
     _UNSAFE.putFloat(object, offset, value);
   }
 
@@ -133,11 +128,6 @@ public final class Platform {
   }
 
   public static void putDouble(Object object, long offset, double value) {
-    if (Double.isNaN(value)) {
-      value = Double.NaN;
-    } else if (value == -0.0d) {
-      value = 0.0d;
-    }
     _UNSAFE.putDouble(object, offset, value);
   }
 
