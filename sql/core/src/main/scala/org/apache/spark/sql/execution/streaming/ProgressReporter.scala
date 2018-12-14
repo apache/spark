63a405ddcb3f9d7ce9a7726fcf40afe3906db699
Gabor Somogyi <gabor.g.somogyi@gmail.com>
2018-12-13 16:12:55 -0800

    [SPARK-23886][SS] Update query status for ContinuousExecution
    ## What changes were proposed in this pull request?
    
    Added query status updates to ContinuousExecution.
    
    ## How was this patch tested?
    
    Existing unit tests + added ContinuousQueryStatusAndProgressSuite.
    
    Closes #23095 from gaborgsomogyi/SPARK-23886.
    
    Authored-by: Gabor Somogyi <gabor.g.somogyi@gmail.com>
    Signed-off-by: Marcelo Vanzin <vanzin@cloudera.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/ProgressReporter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/ProgressReporter.scala
@@ -114,7 +114,6 @@ trait ProgressReporter extends Logging {
     logDebug("Starting Trigger Calculation")
     lastTriggerStartTimestamp = currentTriggerStartTimestamp
     currentTriggerStartTimestamp = triggerClock.getTimeMillis()
-    currentStatus = currentStatus.copy(isTriggerActive = true)
     currentTriggerStartOffsets = null
     currentTriggerEndOffsets = null
     currentDurationsMs.clear()
