cd1cb5c9883e1207b79034377cf3e091dbe5f72b
DB Tsai <d_tsai@apple.com>
2018-12-22 10:35:14 -0800

    [SPARK-26402][SQL] Accessing nested fields with different cases in case insensitive mode
    ## What changes were proposed in this pull request?
    
    GetStructField with different optional names should be semantically equal. We will use this as building block to compare the nested fields used in the plans to be optimized by catalyst optimizer.
    
    This PR also fixes a bug below that accessing nested fields with different cases in case insensitive mode will result `AnalysisException`.
    
    ```
    sql("create table t (s struct<i: Int>) using json")
    sql("select s.I from t group by s.i")
    ```
    which is currently failing
    ```
    org.apache.spark.sql.AnalysisException: expression 'default.t.`s`' is neither present in the group by, nor is it an aggregate function
    ```
    as cloud-fan pointed out.
    
    ## How was this patch tested?
    
    New tests are added.
    
    Closes #23353 from dbtsai/nestedEqual.
    
    Lead-authored-by: DB Tsai <d_tsai@apple.com>
    Co-authored-by: DB Tsai <dbtsai@dbtsai.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -2937,6 +2937,25 @@ class SQLQuerySuite extends QueryTest with SharedSQLContext {
       }
     }
   }
+
+  test("SPARK-26402: accessing nested fields with different cases in case insensitive mode") {
+    withSQLConf(SQLConf.CASE_SENSITIVE.key -> "true") {
+      val msg = intercept[AnalysisException] {
+        withTable("t") {
+          sql("create table t (s struct<i: Int>) using json")
+          checkAnswer(sql("select s.I from t group by s.i"), Nil)
+        }
+      }.message
+      assert(msg.contains("No such struct field I in i"))
+    }
+
+    withSQLConf(SQLConf.CASE_SENSITIVE.key -> "false") {
+      withTable("t") {
+        sql("create table t (s struct<i: Int>) using json")
+        checkAnswer(sql("select s.I from t group by s.i"), Nil)
+      }
+    }
+  }
 }
 
 case class Foo(bar: Option[String])
