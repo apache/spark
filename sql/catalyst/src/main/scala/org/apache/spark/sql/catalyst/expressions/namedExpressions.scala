b3032c985fec90e97c9c53851948fb47109520df
Kevin Yu <qyu@us.ibm.com>
2018-12-27 22:26:37 +0800

    [SPARK-25892][SQL] Change AttributeReference.withMetadata's return type to AttributeReference
    ## What changes were proposed in this pull request?
    
    Currently the `AttributeReference.withMetadata` method have return type `Attribute`, the rest of with methods in the `AttributeReference` return type are `AttributeReference`, as the [spark-25892](https://issues.apache.org/jira/browse/SPARK-25892?jql=project%20%3D%20SPARK%20AND%20component%20in%20(ML%2C%20PySpark%2C%20SQL)) mentioned.
    This PR will change `AttributeReference.withMetadata` method's return type from `Attribute` to `AttributeReference`.
    ## How was this patch tested?
    
    Run all `sql/test,` `catalyst/test` and `org.apache.spark.sql.execution.streaming.*`
    
    Closes #22918 from kevinyu98/spark-25892.
    
    Authored-by: Kevin Yu <qyu@us.ibm.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/namedExpressions.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/namedExpressions.scala
@@ -311,7 +311,7 @@ case class AttributeReference(
     }
   }
 
-  override def withMetadata(newMetadata: Metadata): Attribute = {
+  override def withMetadata(newMetadata: Metadata): AttributeReference = {
     AttributeReference(name, dataType, nullable, newMetadata)(exprId, qualifier)
   }
 
