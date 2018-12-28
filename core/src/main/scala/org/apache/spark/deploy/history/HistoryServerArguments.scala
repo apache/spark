a3640178480054dca894102ff7a95e52c8edb15e
Takuya UESHIN <ueshin@databricks.com>
2018-12-28 21:40:44 +0900

    Revert a change.

--- core/src/main/scala/org/apache/spark/deploy/history/HistoryServerArguments.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/HistoryServerArguments.scala
@@ -79,7 +79,7 @@ private[history] class HistoryServerArguments(conf: SparkConf, args: Array[Strin
       |
       |  spark.history.fs.logDirectory      Directory where app logs are stored
       |                                     (default: file:/tmp/spark-events)
-      |  spark.history.fs.update.interval   How often to reload log data from storage
+      |  spark.history.fs.updateInterval    How often to reload log data from storage
       |                                     (in seconds, default: 10)
       |""".stripMargin)
     // scalastyle:on println
