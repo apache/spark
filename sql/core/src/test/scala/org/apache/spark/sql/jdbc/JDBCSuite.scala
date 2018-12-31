1c487b7be152c6235db1a268486fb73c45f7b2d1
Thomas D'Silva <tdsilva@apache.org>
2018-12-30 20:09:32 -0800

    Fix formatting

--- sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCSuite.scala
@@ -58,8 +58,11 @@ class JDBCSuite extends QueryTest
 
   val testH2DialectTinyInt = new JdbcDialect {
     override def canHandle(url: String): Boolean = url.startsWith("jdbc:h2")
-    override def getCatalystType(sqlType: Int, typeName: String, size: Int, md: MetadataBuilder) :
-    Option[DataType] = {
+    override def getCatalystType(
+        sqlType: Int,
+        typeName: String,
+        size: Int,
+        md: MetadataBuilder): Option[DataType] = {
       sqlType match {
         case java.sql.Types.TINYINT => Some(ByteType)
         case _ => None
