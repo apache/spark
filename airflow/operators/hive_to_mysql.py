0a0c88ae071a8507c5ac51c3b97f1de2750a7e72
Felix <feluelle@users.noreply.github.com>
2019-01-02 08:50:12 +0100

    [AIRFLOW-3580] Adds tests for HiveToMySqlTransfer (#4387)
    - adds tests for hive_to_mysql operator
    - refactoring

--- airflow/operators/hive_to_mysql.py
+++ airflow/operators/hive_to_mysql.py
@@ -57,22 +57,20 @@ class HiveToMySqlTransfer(BaseOperator):
     :type bulk_load: bool
     """
 
-    template_fields = ('sql', 'mysql_table', 'mysql_preoperator',
-                       'mysql_postoperator')
+    template_fields = ('sql', 'mysql_table', 'mysql_preoperator', 'mysql_postoperator')
     template_ext = ('.sql',)
     ui_color = '#a0e08c'
 
     @apply_defaults
-    def __init__(
-            self,
-            sql,
-            mysql_table,
-            hiveserver2_conn_id='hiveserver2_default',
-            mysql_conn_id='mysql_default',
-            mysql_preoperator=None,
-            mysql_postoperator=None,
-            bulk_load=False,
-            *args, **kwargs):
+    def __init__(self,
+                 sql,
+                 mysql_table,
+                 hiveserver2_conn_id='hiveserver2_default',
+                 mysql_conn_id='mysql_default',
+                 mysql_preoperator=None,
+                 mysql_postoperator=None,
+                 bulk_load=False,
+                 *args, **kwargs):
         super(HiveToMySqlTransfer, self).__init__(*args, **kwargs)
         self.sql = sql
         self.mysql_table = mysql_table
@@ -84,28 +82,31 @@ class HiveToMySqlTransfer(BaseOperator):
 
     def execute(self, context):
         hive = HiveServer2Hook(hiveserver2_conn_id=self.hiveserver2_conn_id)
-        self.log.info("Extracting data from Hive: %s", self.sql)
 
+        self.log.info("Extracting data from Hive: %s", self.sql)
         if self.bulk_load:
-            tmpfile = NamedTemporaryFile()
-            hive.to_csv(self.sql, tmpfile.name, delimiter='\t',
-                        lineterminator='\n', output_header=False,
+            tmp_file = NamedTemporaryFile()
+            hive.to_csv(self.sql,
+                        tmp_file.name,
+                        delimiter='\t',
+                        lineterminator='\n',
+                        output_header=False,
                         hive_conf=context_to_airflow_vars(context))
         else:
-            results = hive.get_records(self.sql)
+            hive_results = hive.get_records(self.sql)
 
         mysql = MySqlHook(mysql_conn_id=self.mysql_conn_id)
+
         if self.mysql_preoperator:
             self.log.info("Running MySQL preoperator")
             mysql.run(self.mysql_preoperator)
 
         self.log.info("Inserting rows into MySQL")
-
         if self.bulk_load:
-            mysql.bulk_load(table=self.mysql_table, tmp_file=tmpfile.name)
-            tmpfile.close()
+            mysql.bulk_load(table=self.mysql_table, tmp_file=tmp_file.name)
+            tmp_file.close()
         else:
-            mysql.insert_rows(table=self.mysql_table, rows=results)
+            mysql.insert_rows(table=self.mysql_table, rows=hive_results)
 
         if self.mysql_postoperator:
             self.log.info("Running MySQL postoperator")
---
+++ tests/operators/test_hive_to_mysql.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+from mock import patch, PropertyMock
+
+from airflow.operators.hive_to_mysql import HiveToMySqlTransfer
+from airflow.utils.operator_helpers import context_to_airflow_vars
+
+
+class TestHiveToMySqlTransfer(unittest.TestCase):
+
+    def setUp(self):
+        self.kwargs = dict(
+            sql='sql',
+            mysql_table='table',
+            hiveserver2_conn_id='hiveserver2_default',
+            mysql_conn_id='mysql_default',
+            task_id='test_hive_to_mysql',
+            dag=None
+        )
+
+    @patch('airflow.operators.hive_to_mysql.MySqlHook')
+    @patch('airflow.operators.hive_to_mysql.HiveServer2Hook')
+    def test_execute(self, mock_hive_hook, mock_mysql_hook):
+        HiveToMySqlTransfer(**self.kwargs).execute(context={})
+
+        mock_hive_hook.assert_called_once_with(hiveserver2_conn_id=self.kwargs['hiveserver2_conn_id'])
+        mock_hive_hook.return_value.get_records.assert_called_once_with(self.kwargs['sql'])
+        mock_mysql_hook.assert_called_once_with(mysql_conn_id=self.kwargs['mysql_conn_id'])
+        mock_mysql_hook.return_value.insert_rows.assert_called_once_with(
+            table=self.kwargs['mysql_table'],
+            rows=mock_hive_hook.return_value.get_records.return_value
+        )
+
+    @patch('airflow.operators.hive_to_mysql.MySqlHook')
+    @patch('airflow.operators.hive_to_mysql.HiveServer2Hook')
+    def test_execute_mysql_preoperator(self, mock_hive_hook, mock_mysql_hook):
+        self.kwargs.update(dict(mysql_preoperator='preoperator'))
+
+        HiveToMySqlTransfer(**self.kwargs).execute(context={})
+
+        mock_mysql_hook.return_value.run.assert_called_once_with(self.kwargs['mysql_preoperator'])
+
+    @patch('airflow.operators.hive_to_mysql.MySqlHook')
+    @patch('airflow.operators.hive_to_mysql.HiveServer2Hook')
+    def test_execute_with_mysql_postoperator(self, mock_hive_hook, mock_mysql_hook):
+        self.kwargs.update(dict(mysql_postoperator='postoperator'))
+
+        HiveToMySqlTransfer(**self.kwargs).execute(context={})
+
+        mock_mysql_hook.return_value.run.assert_called_once_with(self.kwargs['mysql_postoperator'])
+
+    @patch('airflow.operators.hive_to_mysql.MySqlHook')
+    @patch('airflow.operators.hive_to_mysql.NamedTemporaryFile')
+    @patch('airflow.operators.hive_to_mysql.HiveServer2Hook')
+    def test_execute_bulk_load(self, mock_hive_hook, mock_tmp_file, mock_mysql_hook):
+        type(mock_tmp_file).name = PropertyMock(return_value='tmp_file')
+        context = {}
+        self.kwargs.update(dict(bulk_load=True))
+
+        HiveToMySqlTransfer(**self.kwargs).execute(context=context)
+
+        mock_tmp_file.assert_called_once_with()
+        mock_hive_hook.return_value.to_csv.assert_called_once_with(
+            self.kwargs['sql'],
+            mock_tmp_file.return_value.name,
+            delimiter='\t',
+            lineterminator='\n',
+            output_header=False,
+            hive_conf=context_to_airflow_vars(context)
+        )
+        mock_mysql_hook.return_value.bulk_load.assert_called_once_with(
+            table=self.kwargs['mysql_table'],
+            tmp_file=mock_tmp_file.return_value.name
+        )
+        mock_tmp_file.return_value.close.assert_called_once_with()
