da7af2812b5b928c24cbc6afea374fde745643d3
Pralabh Kumar <pkumar2@linkedin.com>
2019-01-04 22:28:37 +0530

    Indentation change & config and logging position changed in TopologyMapper

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -188,49 +188,49 @@ package object config {
     .checkValue(_ >= 0, "The off-heap memory size must not be negative")
     .createWithDefault(0)
 
-    private[spark] val MEMORY_STORAGE_FRACTION = ConfigBuilder("spark.memory.storageFraction")
-      .doc("Amount of storage memory immune to eviction, expressed as a fraction of the " +
-        "size of the region set aside by spark.memory.fraction. The higher this is, the " +
-        "less working memory may be available to execution and tasks may spill to disk more " +
-        "often. Leaving this at the default value is recommended. ")
-      .doubleConf
-      .createWithDefault(0.5)
+  private[spark] val MEMORY_STORAGE_FRACTION = ConfigBuilder("spark.memory.storageFraction")
+    .doc("Amount of storage memory immune to eviction, expressed as a fraction of the " +
+      "size of the region set aside by spark.memory.fraction. The higher this is, the " +
+      "less working memory may be available to execution and tasks may spill to disk more " +
+      "often. Leaving this at the default value is recommended. ")
+    .doubleConf
+    .createWithDefault(0.5)
 
-    private[spark] val MEMORY_FRACTION = ConfigBuilder("spark.memory.fraction")
-      .doc("Fraction of (heap space - 300MB) used for execution and storage. The " +
-        "lower this is, the more frequently spills and cached data eviction occur. " +
-        "The purpose of this config is to set aside memory for internal metadata, " +
-        "user data structures, and imprecise size estimation in the case of sparse, " +
-        "unusually large records. Leaving this at the default value is recommended.  ")
-      .doubleConf
-      .createWithDefault(0.6)
+  private[spark] val MEMORY_FRACTION = ConfigBuilder("spark.memory.fraction")
+    .doc("Fraction of (heap space - 300MB) used for execution and storage. The " +
+      "lower this is, the more frequently spills and cached data eviction occur. " +
+      "The purpose of this config is to set aside memory for internal metadata, " +
+      "user data structures, and imprecise size estimation in the case of sparse, " +
+      "unusually large records. Leaving this at the default value is recommended.  ")
+    .doubleConf
+    .createWithDefault(0.6)
 
-    private[spark] val MEMORY_USE_LEGACY_MODE = ConfigBuilder("spark.memory.useLegacyMode")
-      .doc("Whether to enable the legacy memory management mode used in Spark 1.5 and before. " +
-        "The legacy mode rigidly partitions the heap space into fixed-size regions, potentially " +
-        "leading to excessive spilling if the application was not tuned. " +
-        "The following deprecated memory fraction configurations are not " +
-        "read unless this is enabled: spark.shuffle.memoryFraction , " +
-        "spark.storage.memoryFraction, spark.storage.unrollFraction")
-      .booleanConf
-      .createWithDefault(false)
+  private[spark] val MEMORY_USE_LEGACY_MODE = ConfigBuilder("spark.memory.useLegacyMode")
+    .doc("Whether to enable the legacy memory management mode used in Spark 1.5 and before. " +
+      "The legacy mode rigidly partitions the heap space into fixed-size regions, potentially " +
+      "leading to excessive spilling if the application was not tuned. " +
+      "The following deprecated memory fraction configurations are not " +
+      "read unless this is enabled: spark.shuffle.memoryFraction , " +
+      "spark.storage.memoryFraction, spark.storage.unrollFraction")
+    .booleanConf
+    .createWithDefault(false)
 
-    private[spark] val STORAGE_MEMORY_FRACTION = ConfigBuilder("spark.storage.memoryFraction")
-      .doc("(deprecated) This is read only if spark.memory.useLegacyMode is enabled. " +
-        "Fraction of Java heap to use for Spark's memory cache. " +
-        "This should not be larger than the \"old\" generation of objects in the JVM, which " +
-        "by default is given 0.6 of the heap, but you can increase it if you configure " +
-        "your own old generation size")
-      .doubleConf
-      .createWithDefault(0.6)
+  private[spark] val STORAGE_MEMORY_FRACTION = ConfigBuilder("spark.storage.memoryFraction")
+    .doc("(deprecated) This is read only if spark.memory.useLegacyMode is enabled. " +
+      "Fraction of Java heap to use for Spark's memory cache. " +
+      "This should not be larger than the \"old\" generation of objects in the JVM, which " +
+      "by default is given 0.6 of the heap, but you can increase it if you configure " +
+      "your own old generation size")
+    .doubleConf
+    .createWithDefault(0.6)
 
-    private[spark] val STORAGE_SAFETY_FRACTION = ConfigBuilder("spark.storage.safetyFraction")
-      .doubleConf
-      .createWithDefault(0.9)
+  private[spark] val STORAGE_SAFETY_FRACTION = ConfigBuilder("spark.storage.safetyFraction")
+    .doubleConf
+    .createWithDefault(0.9)
 
-    private[spark] val STORAGE_UNROLL_FRACTION = ConfigBuilder("spark.storage.unrollFraction")
-      .doubleConf
-      .createWithDefault(0.2)
+  private[spark] val STORAGE_UNROLL_FRACTION = ConfigBuilder("spark.storage.unrollFraction")
+    .doubleConf
+    .createWithDefault(0.2)
 
   private[spark] val STORAGE_UNROLL_MEMORY_THRESHOLD =
     ConfigBuilder("spark.storage.unrollMemoryThreshold")
