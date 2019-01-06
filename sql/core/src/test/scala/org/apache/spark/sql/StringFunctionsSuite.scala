00c72fe673f4ab4af03d98ac079831bf6adb27a7
hhh <wennshm@aliyun.com>
2019-01-07 00:08:20 +0800

    fix test bug

--- sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala
@@ -104,19 +104,19 @@ class StringFunctionsSuite extends QueryTest with SharedSQLContext {
     // every group has a name
     checkAnswer(df.select(
       regexp_to_map($"a", "(?<first>\\d+)-(?<second>\\d+)-(?<third>\\d+)") as "temp"
-    ).selectExpr("temp.second, temp.first"),
+    ).selectExpr("temp.second", "temp.first"),
       Row("200", "100") :: Row("300", "200") :: Row("100", "500") :: Nil)
 
     // every group does not has a name
     checkAnswer(df.select(
       regexp_to_map($"a", "(\\d+)-(\\d+)-(\\d+)") as "temp"
-    ).selectExpr("temp.column2, temp.column1"),
+    ).selectExpr("temp.column2", "temp.column1"),
       Row("200", "100") :: Row("300", "200") :: Row("100", "500") :: Nil)
 
     // some groups do not has a name, while others have
     checkAnswer(df.select(
       regexp_to_map($"a", "(?<first>\\d+)-(\\d+)-(?<third>\\d+)") as "temp"
-    ).selectExpr("temp.column2, temp.first"),
+    ).selectExpr("temp.column2", "temp.first"),
       Row("200", "100") :: Row("300", "200") :: Row("100", "500") :: Nil)
   }
 
