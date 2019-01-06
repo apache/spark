ac682b6ecad6e741a332ba45d1f13c8ea92c3b68
Dongjoon Hyun <dongjoon@apache.org>
2019-01-05 19:00:48 -0800

    Address Hyukjin's comment

--- sql/core/src/main/java/org/apache/spark/sql/sources/v2/reader/streaming/StreamingReadSupport.java
+++ sql/core/src/main/java/org/apache/spark/sql/sources/v2/reader/streaming/StreamingReadSupport.java
@@ -22,6 +22,7 @@ import org.apache.spark.sql.sources.v2.reader.ReadSupport;
 /**
  * A base interface for streaming read support. Data sources should implement concrete streaming
  * read support interfaces: {@link MicroBatchReadSupport} or {@link ContinuousReadSupport}.
+ * This is exposed for a testing purpose.
  */
 public interface StreamingReadSupport extends ReadSupport {
 
