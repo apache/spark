31b0b046206a1f2316d69a083c8c38ad7b7b95ab
Oleksii Shkarupin <a.shkarupin@gmail.com>
2019-01-08 19:09:58 +0200

    [SPARK-26538][SQL] add a comment explaining the issue

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/PostgresDialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/PostgresDialect.scala
@@ -60,8 +60,12 @@ private object PostgresDialect extends JdbcDialect {
     case "bytea" => Some(BinaryType)
     case "timestamp" | "timestamptz" | "time" | "timetz" => Some(TimestampType)
     case "date" => Some(DateType)
-    case "numeric" | "decimal" if precision != 0 => Some(DecimalType.bounded(precision, scale))
-    case "numeric" | "decimal" => Some(DecimalType.SYSTEM_DEFAULT)
+    case "numeric" | "decimal" => if (precision > 0) {
+      Some(DecimalType.bounded(precision, scale))
+    } else {
+      // SPARK-26538: handle numeric without explicit precision and scale.
+      Some(DecimalType. SYSTEM_DEFAULT)
+    }
     case _ => None
   }
 
