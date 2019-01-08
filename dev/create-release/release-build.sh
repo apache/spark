5805b35496001e1d658d4cd8039ea1518d280d60
shane knapp <incomplete@gmail.com>
2019-01-08 11:44:26 -0800

    add -x for testing

--- dev/create-release/release-build.sh
+++ dev/create-release/release-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x  # testing only
+
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
