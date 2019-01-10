b8be4ae608ba19d9decff39b0ced7afeaf4f928c
shane knapp <incomplete@gmail.com>
2019-01-10 10:37:06 -0800

    remove -x debugging

--- dev/create-release/release-build.sh
+++ dev/create-release/release-build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
-set -x  # testing only
-
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
