1c02b7d4edb2d7ef6ec0f1cfbe7aa41e9ef80327
Devaraj K <devaraj@apache.org>
2019-01-09 20:16:47 -0800

    Merge branch 'master' into SPARK-22404

--- resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/YarnRMClient.scala
+++ resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/YarnRMClient.scala
@@ -80,8 +80,7 @@ private[spark] class YarnRMClient extends Logging {
       driverUrl: String,
       driverRef: RpcEndpointRef,
       securityMgr: SecurityManager,
-      localResources: Map[String, LocalResource],
-      appAttemptId: ApplicationAttemptId): YarnAllocator = {
+      localResources: Map[String, LocalResource]): YarnAllocator = {
     require(registered, "Must register AM before creating allocator.")
     new YarnAllocator(driverUrl, driverRef, conf, sparkConf, amClient, appAttemptId, securityMgr,
       localResources, new SparkRackResolver())
