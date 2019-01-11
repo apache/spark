87c2c11e742a8b35699f68ec2002f817c56bef87
John Zhuge <jzhuge@apache.org>
2019-01-11 09:21:13 -0800

    [SPARK-26576][SQL] Broadcast hint not applied to partitioned table
    ## What changes were proposed in this pull request?
    
    Make sure broadcast hint is applied to partitioned tables.
    
    Since the issue exists in branch 2.0 to 2.4, but not in master, I created this PR for branch-2.4.
    
    ## How was this patch tested?
    
    - A new unit test in PruneFileSourcePartitionsSuite
    - Unit test suites touched by SPARK-14581: JoinOptimizationSuite, FilterPushdownSuite, ColumnPruningSuite, and PruneFiltersSuite
    
    cloud-fan davies rxin
    
    Closes #23507 from jzhuge/SPARK-26576.
    
    Authored-by: John Zhuge <jzhuge@apache.org>
    Signed-off-by: gatorsmile <gatorsmile@gmail.com>
    (cherry picked from commit b9eb0e85de3317a7f4c89a90082f7793b645c6ea)
    Signed-off-by: gatorsmile <gatorsmile@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/planning/patterns.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/planning/patterns.scala
@@ -65,9 +65,6 @@ object PhysicalOperation extends PredicateHelper {
         val substitutedCondition = substitute(aliases)(condition)
         (fields, filters ++ splitConjunctivePredicates(substitutedCondition), other, aliases)
 
-      case h: ResolvedHint =>
-        collectProjectsAndFilters(h.child)
-
       case other =>
         (None, Nil, other, Map.empty)
     }
