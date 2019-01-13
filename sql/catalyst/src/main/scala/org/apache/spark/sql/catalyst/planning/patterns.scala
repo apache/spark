434929ada811d914979f17f40cf3d5d5e343ebb2
John Zhuge <jzhuge@apache.org>
2019-01-12 23:27:19 -0800

    Do not remove dead code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/planning/patterns.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/planning/patterns.scala
@@ -68,6 +68,9 @@ object PhysicalOperation extends PredicateHelper {
         val substitutedCondition = substitute(aliases)(condition)
         (fields, filters ++ splitConjunctivePredicates(substitutedCondition), other, aliases)
 
+      case h: ResolvedHint =>
+        collectProjectsAndFilters(h.child)
+
       case other =>
         (None, Nil, other, Map.empty)
     }
