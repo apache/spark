31318ba8eb5b136277fc1672813cb57e68989abf
Fokko Driesprong <fokko@driesprong.frl>
2019-01-19 17:37:03 +0100

    [AIRFLOW-3731] Constrain mysqlclient to <1.4 (#4558)
    To maintain Python2 compatibility

--- setup.py
+++ setup.py
@@ -200,7 +200,7 @@ kubernetes = ['kubernetes>=3.0.0',
               'cryptography>=2.0.0']
 ldap = ['ldap3>=2.5.1']
 mssql = ['pymssql>=2.1.1']
-mysql = ['mysqlclient>=1.3.6']
+mysql = ['mysqlclient>=1.3.6,<1.4']
 oracle = ['cx_Oracle>=5.1.2']
 password = [
     'bcrypt>=2.0.0',
