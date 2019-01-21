3eb761155a68be963d24fba4e03f3c019628addf
Hyukjin Kwon <gurwls223@apache.org>
2019-01-21 11:53:50 +0800

    Debug 3

--- python/run-tests.py
+++ python/run-tests.py
@@ -142,7 +142,8 @@ def run_individual_python_test(target_dir, test_name, pyspark_python):
                 key = (pyspark_python, test_name)
                 SKIPPED_TESTS[key] = skipped_tests
             per_test_output.close()
-        except:
+        except Exception as e:
+            LOGGER.info("Exception thrown %s" % e)
             import traceback
             print_red("\nGot an exception while trying to store "
                       "skipped test output:\n%s" % traceback.format_exc())
