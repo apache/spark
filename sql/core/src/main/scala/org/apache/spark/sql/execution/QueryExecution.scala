4082aa319b0d94911da7c17ec95ef673edab4307
Dave DeCaprio <daved@alum.mit.edu>
2019-01-20 20:16:08 -0600

    Scalastyle

--- sql/core/src/main/scala/org/apache/spark/sql/execution/QueryExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/QueryExecution.scala
@@ -20,6 +20,7 @@ package org.apache.spark.sql.execution
 import java.io.{BufferedWriter, OutputStreamWriter}
 
 import org.apache.hadoop.fs.Path
+
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{AnalysisException, SparkSession}
 import org.apache.spark.sql.catalyst.{InternalRow, QueryPlanningTracker}
