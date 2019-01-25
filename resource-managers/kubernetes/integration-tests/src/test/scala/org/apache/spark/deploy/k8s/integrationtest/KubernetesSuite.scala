a8f127a737f2376e25e0acc190b82549f646e811
Rob Vesse <rvesse@dotnetrdf.org>
2019-01-25 17:06:58 +0000

    [SPARK-26729][K8S] Make image names under test configurable
    Allow specifying system properties to customise the image names for the
    images used in the integration testing.  Useful if your CI/CD pipeline
    or policy requires using a different naming format.

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
@@ -116,9 +116,9 @@ class KubernetesSuite extends SparkFunSuite
     sparkHomeDir = Paths.get(sparkDirProp)
     require(sparkHomeDir.toFile.isDirectory,
       s"No directory found for spark home specified at $sparkHomeDir.")
-    image = testImageRef("spark")
-    pyImage = testImageRef("spark-py")
-    rImage = testImageRef("spark-r")
+    image = testImageRef(Option(System.getProperty(CONFIG_KEY_IMAGE_JVM)).getOrElse("spark"))
+    pyImage = testImageRef(Option(System.getProperty(CONFIG_KEY_IMAGE_PYTHON)).getOrElse("spark-py"))
+    rImage = testImageRef(Option(System.getProperty(CONFIG_KEY_IMAGE_R)).getOrElse("spark-r"))
 
     val scalaVersion = scala.util.Properties.versionNumberString
       .split("\\.")
