ce35819770c65029ad8b60528317aeab843b6d02
Maxim Gekk <max.gekk@gmail.com>
2019-01-26 11:07:39 +0100

    First version

---
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/DateTimeBenchmark.scala
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql
+
+import org.apache.spark.sql.execution.benchmark.SqlBasedBenchmark
+import org.apache.spark.sql.functions._
+
+/**
+ * Synthetic benchmark for date and timestamp functions.
+ * To run this benchmark:
+ * {{{
+ *   1. without sbt:
+ *      bin/spark-submit --class <this class> --jars <spark core test jar> <spark catalyst test jar>
+ *   2. build/sbt "catalyst/test:runMain <this class>"
+ *   3. generate result:
+ *      SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "catalyst/test:runMain <this class>"
+ *      Results will be written to "benchmarks/DateTimeBenchmark-results.txt".
+ * }}}
+ */
+object DateTimeBenchmark extends SqlBasedBenchmark {
+  import spark.implicits._
+
+  def getYear(cardinality: Int): Unit = {
+    def yearBenchmark(expr: String): Unit = {
+      spark.range(cardinality)
+        .selectExpr(expr)
+        .select(year('input))
+        .write.format("noop").save()
+    }
+    codegenBenchmark("year from date", cardinality) {
+      yearBenchmark("DATE '2019-01-26' AS input")
+    }
+    codegenBenchmark("year from timestamp", cardinality) {
+      yearBenchmark("DATE '2019-01-26 10:46:00' AS input")
+    }
+  }
+
+  override def runBenchmarkSuite(mainArgs: Array[String]): Unit = {
+    val cardinality = 100000000
+    runBenchmark("Extract components") {
+      getYear(cardinality)
+    }
+  }
+}
