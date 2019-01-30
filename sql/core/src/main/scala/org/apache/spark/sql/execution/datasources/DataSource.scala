5bcb6c571264b71c6b3b0ee0e351b845fa74dc60
Gengliang Wang <gengliang.wang@databricks.com>
2019-01-30 23:24:35 +0800

    follow up

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSource.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSource.scala
@@ -412,7 +412,7 @@ case class DataSource(
           hs.partitionSchema.map(_.name),
           "in the partition schema",
           equality)
-        DataSourceUtils.verifyReadSchema(hs.fileFormat, hs.dataSchema)
+        DataSourceUtils.verifySchema(hs.fileFormat, hs.dataSchema)
       case _ =>
         SchemaUtils.checkColumnNameDuplication(
           relation.schema.map(_.name),
