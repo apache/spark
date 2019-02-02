995b33a63cc7c6384738b759ff33e53670d025bf
xiaodeshan <xiaodeshan@xiaomi.com>
2019-02-01 18:38:27 -0600

    [SPARK-26714][CORE][WEBUI] Show 0 partition job in WebUI
    ## What changes were proposed in this pull request?
    
    When the job's partiton is zero, it will still get a jobid but not shown in ui. It's strange. This PR is to show this job in ui.
    
    Example:
    In bash:
    mkdir -p /home/test/testdir
    
    sc.textFile("/home/test/testdir")
    
    Some logs:
    
    ```
    19/01/24 17:26:19 INFO FileInputFormat: Total input paths to process : 0
    19/01/24 17:26:19 INFO SparkContext: Starting job: collect at WordCount.scala:9
    19/01/24 17:26:19 INFO DAGScheduler: Job 0 finished: collect at WordCount.scala:9, took 0.003735 s
    ```
    
    ## How was this patch tested?
    
    UT
    
    Closes #23637 from deshanxiao/spark-26714.
    
    Authored-by: xiaodeshan <xiaodeshan@xiaomi.com>
    Signed-off-by: Sean Owen <sean.owen@databricks.com>

--- core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
+++ core/src/main/scala/org/apache/spark/scheduler/DAGScheduler.scala
@@ -693,6 +693,11 @@ private[spark] class DAGScheduler(
 
     val jobId = nextJobId.getAndIncrement()
     if (partitions.size == 0) {
+      val time = clock.getTimeMillis()
+      listenerBus.post(
+        SparkListenerJobStart(jobId, time, Seq[StageInfo](), properties))
+      listenerBus.post(
+        SparkListenerJobEnd(jobId, time, JobSucceeded))
       // Return immediately if the job is running 0 tasks
       return new JobWaiter[U](this, jobId, 0, resultHandler)
     }
