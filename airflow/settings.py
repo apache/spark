6cfadcde7aea21be4b5e7258b3a600cbd04e9529
Ryo Okubo <syucream1031@gmail.com>
2019-02-06 14:54:43 +0900

    [AIRFLOW-3814] Add exception details to warning log (#4651)
    * Add exception details to warning log
    
    * Fix log format

--- airflow/settings.py
+++ airflow/settings.py
@@ -80,8 +80,8 @@ try:
             port=conf.getint('scheduler', 'statsd_port'),
             prefix=conf.get('scheduler', 'statsd_prefix'))
         Stats = statsd
-except (socket.gaierror, ImportError):
-    log.warning("Could not configure StatsClient, using DummyStatsLogger instead.")
+except (socket.gaierror, ImportError) as e:
+    log.warning("Could not configure StatsClient: %s, using DummyStatsLogger instead.", e)
 
 HEADER = '\n'.join([
     r'  ____________       _____________',
