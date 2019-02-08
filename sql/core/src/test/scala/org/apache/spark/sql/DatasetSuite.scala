8136803c6d06fc7f23aca8533799385c96642c89
Liang-Chi Hsieh <viirya@gmail.com>
2019-02-08 09:43:43 +0800

    Split and enhance test case.

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -1681,20 +1681,28 @@ class DatasetSuite extends QueryTest with SharedSQLContext {
     checkAnswer(ds, Seq(Row("a"), Row("b"), Row("c")))
   }
 
-  test("Prune nested serializers") {
-    def testSerializer(df: DataFrame, structFields: Seq[Seq[String]]): Unit = {
-      val serializer = df.queryExecution.optimizedPlan.collect {
-        case s: SerializeFromObject => s
-      }.head
 
-      val structs = serializer.serializer(0).collect {
+  // This methods checks if the given DataFrame has specified struct fields in object
+  // serializer. The varargs parameter `structFields` is the struct fields for object
+  // serializers. The first `structFields` is aligned with first serializer and ditto
+  // for other `structFields`.
+  private def testSerializer(df: DataFrame, structFields: Seq[Seq[String]]*): Unit = {
+    val serializer = df.queryExecution.optimizedPlan.collect {
+      case s: SerializeFromObject => s
+    }.head
+
+    serializer.serializer.zip(structFields).foreach { case (serializer, fields) =>
+      val structs = serializer.collect {
         case c: CreateNamedStruct => c
       }
-      assert(structs.size == structFields.size)
-      structs.zip(structFields).foreach { case (struct, fieldNames) =>
+      assert(structs.size == fields.size)
+      structs.zip(fields).foreach { case (struct, fieldNames) =>
         assert(struct.names.map(_.toString) == fieldNames)
       }
     }
+  }
+
+  test("Prune nested serializers: struct") {
     withSQLConf(SQLConf.SERIALIZER_NESTED_SCHEMA_PRUNING_ENABLED.key -> "true") {
       val data = Seq((("a", 1, ("aa", 1.0)), 1), (("b", 2, ("bb", 2.0)), 2),
         (("c", 3, ("cc", 3.0)), 3))
@@ -1715,24 +1723,30 @@ class DatasetSuite extends QueryTest with SharedSQLContext {
       val df4 = ds.select("_1._3._1", "_1._2")
       testSerializer(df4, Seq(Seq("_2", "_3"), Seq("_1")))
       checkAnswer(df4, Seq(Row("aa", 1), Row("bb", 2), Row("cc", 3)))
+    }
+  }
 
+  test("Prune nested serializers: array of struct") {
+    withSQLConf(SQLConf.SERIALIZER_NESTED_SCHEMA_PRUNING_ENABLED.key -> "true") {
       val arrayData = Seq((Seq(("a", 1, ("a_1", 11)), ("b", 2, ("b_1", 22))), 1, ("aa", 1.0)),
         (Seq(("c", 3, ("c_1", 33)), ("d", 4, ("d_1", 44))), 2, ("bb", 2.0)))
       val arrayDs = arrayData.toDS().map(t => (t._1, t._2 + 1, t._3))
-      val df5 = arrayDs.select("_1._1")
+      val df1 = arrayDs.select("_1._1")
       // The serializer creates array of struct of one field "_1".
-      testSerializer(df5, Seq(Seq("_1")))
-      checkAnswer(df5, Seq(Row(Seq("a", "b")), Row(Seq("c", "d"))))
+      testSerializer(df1, Seq(Seq("_1")))
+      checkAnswer(df1, Seq(Row(Seq("a", "b")), Row(Seq("c", "d"))))
 
-      val df6 = arrayDs.select("_3._2")
-      testSerializer(df6, Seq(Seq("_2")))
-      checkAnswer(df6, Seq(Row(1.0), Row(2.0)))
+      val df2 = arrayDs.select("_3._2")
+      testSerializer(df2, Seq(Seq("_2")))
+      checkAnswer(df2, Seq(Row(1.0), Row(2.0)))
 
-      val df7 = arrayDs.select("_1._1", "_1._3._2", "_3._1")
-      // The serializer creates array of struct of two fields ("_1", "_3") and the field "_3" is
-      // a struct of one field "_2".
-      testSerializer(df7, Seq(Seq("_1", "_3"), Seq("_2")))
-      checkAnswer(df7, Seq(Row(Seq("a", "b"), Seq(11, 22), "aa"),
+      // This is a more complex case. We select two root fields "_1" and "_3".
+      // The first serializer creates array of struct of two fields ("_1", "_3") and
+      // the field "_3" is a struct of one field "_2".
+      // The second serializer creates a struct of just one field "_1".
+      val df3 = arrayDs.select("_1._1", "_1._3._2", "_3._1")
+      testSerializer(df3, Seq(Seq("_1", "_3"), Seq("_2")), Seq(Seq("_1")))
+      checkAnswer(df3, Seq(Row(Seq("a", "b"), Seq(11, 22), "aa"),
         Row(Seq("c", "d"), Seq(33, 44), "bb")))
     }
   }
