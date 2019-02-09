436cef54d9ed1c1f893fe9c53005664b85c8a9da
Ben Weber <bgweber@gmail.com>
2019-02-08 16:03:12 -0800

    Update tuning.py

--- python/pyspark/ml/tuning.py
+++ python/pyspark/ml/tuning.py
@@ -308,6 +308,9 @@ class CrossValidator(Estimator, ValidatorParams, HasParallelism, HasCollectSubMo
 
             validation.unpersist()
             train.unpersist()
+           
+        if eva.applyAbsoluteTransform():
+            metrics = np.absolute(metrics) 
 
         if eva.isLargerBetter():
             bestIndex = np.argmax(metrics)
