d9cda08e0d63c5b68f919ae761bf4a707664b058
Peter Toth <peter.toth@gmail.com>
2019-02-16 10:12:20 +0100

    extend UT
    Change-Id: I0866cd507e7d1cb3b3bc5ec006d3fe9e1dff5617

--- sql/core/src/test/scala/org/apache/spark/sql/SubquerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SubquerySuite.scala
@@ -21,6 +21,7 @@ import scala.collection.mutable.ArrayBuffer
 
 import org.apache.spark.sql.catalyst.expressions.SubqueryExpression
 import org.apache.spark.sql.catalyst.plans.logical.{Join, LogicalPlan, Sort}
+import org.apache.spark.sql.execution.datasources.FileScanRDD
 import org.apache.spark.sql.execution.{ExecSubqueryExpression, FileSourceScanExec, WholeStageCodegenExec}
 import org.apache.spark.sql.test.SharedSQLContext
 
@@ -1288,12 +1289,15 @@ class SubquerySuite extends QueryTest with SharedSQLContext {
       spark.range(2).write.saveAsTable("b")
 
       val df = sql("SELECT * FROM a WHERE p <= (SELECT MIN(id) FROM b)")
+      checkAnswer(df, Seq(Row(0, 0), Row(2, 0)))
       assert(df.queryExecution.executedPlan match {
-        case WholeStageCodegenExec(FileSourceScanExec(_, _, _, partitionFilters, _, _, _))
-          if partitionFilters.exists(ExecSubqueryExpression.hasSubquery) => true
+        case WholeStageCodegenExec(fs @ FileSourceScanExec(_, _, _, partitionFilters, _, _, _))
+          if partitionFilters.exists(ExecSubqueryExpression.hasSubquery) &&
+            fs.inputRDDs().forall(
+              _.asInstanceOf[FileScanRDD].filePartitions.forall(
+                _.files.forall(_.filePath.contains("p=0")))) => true
         case _ => false
       })
-      checkAnswer(df, Seq(Row(0, 0), Row(2, 0)))
     }
   }
 
