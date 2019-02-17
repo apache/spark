9e91a2f13ff3020c7c1eb02f83bb615fd2c21fe9
Takeshi Yamamuro <yamamuro@apache.org>
2019-02-17 13:48:25 +0900

    Add a new column in the doc

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/parser/TableIdentifierParserSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/parser/TableIdentifierParserSuite.scala
@@ -59,10 +59,7 @@ class TableIdentifierParserSuite extends SparkFunSuite with SQLHelper {
     "natural", "union", "intersect", "except", "database", "on", "join", "cross", "select", "from",
     "where", "having", "from", "to", "table", "with", "not")
 
-  val otherNonReservedKeywords = Seq(
-    "check", "collate", "overlaps", "only", "primary", "references", "unique")
-
-  val ansiReservedKeywords = Seq("all", "and", "any", "as", "authorization", "both", "case",
+  val reservedKeywordsInAnsi = Seq("all", "and", "any", "as", "authorization", "both", "case",
     "cast", "check", "collate", "column", "constraint", "create", "cross", "current_date",
     "current_time", "current_timestamp", "current_user", "distinct", "else", "end", "except",
     "false", "fetch", "for", "foreign", "from", "full", "grant", "group", "having", "in", "inner",
@@ -71,9 +68,9 @@ class TableIdentifierParserSuite extends SparkFunSuite with SQLHelper {
     "some", "table", "then", "to", "trailing", "union", "unique", "user", "using", "when",
     "where", "with")
 
-  val ansiNonReservedKeywords = Seq("abs", "absolute", "acos", "action", "add", "after", "allocate",
-    "alter", "analyze", "are", "archive", "array", "array_agg", "array_max_cardinality", "asc",
-    "asensitive", "asin", "assertion", "asymmetric", "at", "atan", "atomic", "avg", "before",
+  val nonReservedKeywordsInAnsi = Seq("abs", "absolute", "acos", "action", "add", "after",
+    "allocate", "alter", "analyze", "are", "archive", "array", "array_agg", "array_max_cardinality",
+    "asc", "asensitive", "asin", "assertion", "asymmetric", "at", "atan", "atomic", "avg", "before",
     "begin", "begin_frame", "begin_partition", "between", "bigint", "binary", "bit", "bit_length",
     "blob", "boolean", "breadth", "bucket", "buckets", "by", "cache", "call", "called",
     "cardinality", "cascade", "cascaded", "catalog", "ceil", "ceiling", "change", "char",
@@ -134,6 +131,74 @@ class TableIdentifierParserSuite extends SparkFunSuite with SQLHelper {
     "work", "write", "year", "zone"
   )
 
+  val nonReservedKeywordsInNonAnsi = Seq("abs", "absolute", "acos", "action", "add", "after",
+    "all", "allocate", "alter", "analyze", "and", "any", "archive", "are", "array", "array_agg",
+    "array_max_cardinality", "as", "asc", "asensitive", "asin", "assertion", "asymmetric", "at",
+    "atan", "atomic", "authorization", "avg", "before", "begin", "begin_frame", "begin_partition",
+    "between", "bigint", "binary", "bit", "bit_length", "blob", "boolean", "both", "breadth",
+    "bucket", "buckets", "by", "cache", "call", "called", "cardinality", "cascade", "cascaded",
+    "case", "cast", "catalog", "ceil", "ceiling", "change", "char", "char_length", "character",
+    "character_length", "check", "classifier", "clear", "clob", "close", "cluster", "clustered",
+    "coalesce", "codegen", "collate", "collation", "collect", "collection", "column", "columns",
+    "comment", "commit", "compact", "compactions", "compute", "concatenate", "condition", "connect",
+    "connection", "constraint", "constraints", "constructor", "contains", "continue", "convert",
+    "copy", "corr", "corresponding", "cos", "cosh", "cost", "count", "covar_pop", "covar_samp",
+    "create", "cross", "cube", "cume_dist", "current", "current_catalog", "current_date",
+    "current_default_transform_group", "current_path", "current_role", "current_row",
+    "current_schema", "current_time", "current_timestamp", "current_transform_group_for_type",
+    "current_user", "cursor", "cycle", "data", "database", "databases", "date", "day",
+    "dbproperties", "deallocate", "dec", "decfloat", "decimal", "declare", "default", "deferrable",
+    "deferred", "define", "defined", "delete", "delimited", "dense_rank", "depth", "deref", "desc",
+    "describe", "descriptor", "deterministic", "dfs", "diagnostics", "directories", "directory",
+    "disconnect", "distinct", "distribute", "div", "do", "domain", "double", "drop", "dynamic",
+    "each", "element", "else", "elseif", "empty", "end", "end_frame", "end_partition", "equals",
+    "escape", "escaped", "every", "except", "exception", "exchange", "exec", "execute", "exists",
+    "exit", "exp", "explain", "export", "extended", "external", "extract", "false", "fetch",
+    "fields", "fileformat", "filter", "first", "first_value", "float", "following", "for",
+    "foreign", "format", "formatted", "found", "frame_row", "free", "from", "full", "function",
+    "functions", "fusion", "general", "get", "global", "go", "goto", "grant", "group", "grouping",
+    "groups", "handler", "having", "hold", "hour", "identity", "if", "ignore", "immediate",
+    "import", "in", "index", "indexes", "indicator", "initial", "initially", "inner", "inout",
+    "inpath", "input", "inputformat", "insensitive", "insert", "int", "integer", "intersect",
+    "intersection", "interval", "into", "is", "isolation", "items", "iterate", "join", "json_array",
+    "json_arrayagg", "json_exists", "json_object", "json_objectagg", "json_query", "json_table",
+    "json_table_primitive", "json_value", "key", "keys", "lag", "language", "large", "last",
+    "last_value", "lateral", "lazy", "lead", "leading", "leave", "left", "level", "like",
+    "like_regex", "limit", "lines", "list", "listagg", "ln", "load", "local", "localtime",
+    "localtimestamp", "location", "locator", "lock", "locks", "log", "log10", "logical", "loop",
+    "lower", "macro", "map", "match", "match_number", "match_recognize", "matches", "max", "member",
+    "merge", "method", "min", "minute", "mod", "modifies", "module", "month", "msck", "multiset",
+    "names", "national", "natural", "nchar", "nclob", "new", "next", "no", "none", "normalize",
+    "not", "nth_value", "ntile", "null", "nullif", "nulls", "numeric", "object",
+    "occurrences_regex", "octet_length", "of", "offset", "old", "omit", "on", "one", "only", "open",
+    "option", "options", "or", "order", "ordinality", "out", "outer", "output", "outputformat",
+    "over", "overlaps", "overlay", "overwrite", "pad", "parameter", "partial", "partition",
+    "partitioned", "partitions", "path", "pattern", "per", "percent", "percent_rank",
+    "percentile_cont", "percentile_disc", "percentlit", "period", "pivot", "portion", "power",
+    "precedes", "preceding", "precision", "prepare", "preserve", "primary", "principals", "prior",
+    "privileges", "procedure", "ptf", "public", "purge", "range", "rank", "read", "reads", "real",
+    "recordreader", "recordwriter", "recover", "recursive", "reduce", "ref", "references",
+    "referencing", "refresh", "regr_avgx", "regr_avgy", "regr_count", "regr_intercept", "regr_r2",
+    "regr_slope", "regr_sxx", "regr_sxy", "regr_syy", "relative", "release", "rename", "repair",
+    "repeat", "replace", "reset", "resignal", "restrict", "result", "return", "returns", "revoke",
+    "right", "rlike", "role", "roles", "rollback", "rollup", "routine", "row", "row_number", "rows",
+    "running", "savepoint", "schema", "scope", "scroll", "search", "second", "section", "seek",
+    "select", "sensitive", "separated", "serde", "serdeproperties", "session", "session_user",
+    "set", "sets", "show", "signal", "similar", "sin", "sinh", "size", "skewed", "skip", "smallint",
+    "some", "sort", "sorted", "space", "specific", "specifictype", "sql", "sqlcode", "sqlerror",
+    "sqlexception", "sqlstate", "sqlwarning", "sqrt", "start", "state", "static", "statistics",
+    "stddev_pop", "stddev_samp", "stored", "stratify", "struct", "submultiset", "subset",
+    "substring", "substring_regex", "succeeds", "sum", "symmetric", "system", "system_time",
+    "system_user", "table", "tables", "tablesample", "tan", "tanh", "tblproperties", "temporary",
+    "terminated", "then", "time", "timestamp", "timezone_hour", "timezone_minute", "to", "touch",
+    "trailing", "transaction", "transactions", "transform", "translate", "translate_regex",
+    "translation", "treat", "trigger", "trim", "trim_array", "true", "truncate", "uescape",
+    "unarchive", "unbounded", "uncache", "under", "undo", "union", "unique", "unknown", "unlock",
+    "unnest", "unset", "until", "update", "upper", "usage", "use", "user", "value", "value_of",
+    "values", "var_pop", "var_samp", "varbinary", "varchar", "varying", "versioning", "view",
+    "when", "whenever", "where", "while", "width_bucket", "window", "with", "within", "without",
+    "work", "write", "year", "zone")
+
   test("table identifier") {
     // Regular names.
     assert(TableIdentifier("q") === parseTableIdentifier("q"))
@@ -155,7 +220,7 @@ class TableIdentifierParserSuite extends SparkFunSuite with SQLHelper {
 
   test("table identifier - reserved/non-reserved keywords if ANSI mode enabled") {
     withSQLConf(SQLConf.ANSI_SQL_PARSER.key -> "true") {
-      ansiReservedKeywords.foreach { keyword =>
+      reservedKeywordsInAnsi.foreach { keyword =>
         val errMsg = intercept[ParseException] {
           parseTableIdentifier(keyword)
         }.getMessage
@@ -164,7 +229,7 @@ class TableIdentifierParserSuite extends SparkFunSuite with SQLHelper {
         assert(TableIdentifier(keyword) === parseTableIdentifier(s"`$keyword`"))
         assert(TableIdentifier(keyword, Option("db")) === parseTableIdentifier(s"db.`$keyword`"))
       }
-      ansiNonReservedKeywords.foreach { keyword =>
+      nonReservedKeywordsInAnsi.foreach { keyword =>
         assert(TableIdentifier(keyword) === parseTableIdentifier(s"$keyword"))
         assert(TableIdentifier(keyword, Option("db")) === parseTableIdentifier(s"db.$keyword"))
       }
@@ -181,8 +246,12 @@ class TableIdentifierParserSuite extends SparkFunSuite with SQLHelper {
   }
 
   test("table identifier - non reserved keywords") {
-    // Hive keywords are allowed.
-    (hiveNonReservedKeywords ++ otherNonReservedKeywords).foreach { nonReserved =>
+    nonReservedKeywordsInNonAnsi.foreach { nonReserved =>
+      assert(TableIdentifier(nonReserved) === parseTableIdentifier(nonReserved))
+    }
+
+    // Additionally checks if Hive keywords can be allowed.
+    hiveNonReservedKeywords.foreach { nonReserved =>
       assert(TableIdentifier(nonReserved) === parseTableIdentifier(nonReserved))
     }
   }
