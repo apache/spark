77986af9f43a0380a35205fa7fb3a5dc923e6e28
Hellsen83 <erik.christiansen83@gmail.com>
2019-02-22 19:34:06 +0100

    added transform() method to DataFrame API.

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -2046,6 +2046,28 @@ class DataFrame(object):
         jdf = self._jdf.toDF(self._jseq(cols))
         return DataFrame(jdf, self.sql_ctx)
 
+    @since(3.0)
+    def transform(self, func):
+        """Returns a new class:`DataFrame`. Concise syntax for chaining custom transformations.
+
+        :param func: a function that takes and returns a class:`DataFrame`.
+
+        >>> df = spark.createDataFrame([(1.1, 2), (2.2, 1)], ["float", "int"])
+        >>> def cast_all_to_str(input_df):
+        ...     return input_df.select([col(col_name).cast("string") for col_name in input_df.columns])
+        >>> df.transform(cast_all_to_str).show()
+        +------+----+
+        | float| int|
+        +------+----+
+        |   1.1|  2 |
+        |   2.2|  1 |
+        +------+----+
+        """
+        result = func(self)
+        assert isinstance(result, DataFrame), "Func returned an instance of type [%s], " \
+                                              "should have been DataFrame." % type(result)
+        return result
+
     @since(1.3)
     def toPandas(self):
         """
