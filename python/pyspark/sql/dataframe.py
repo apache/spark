73016ced1f93ce6aa62fe00e846e7cf33140add3
Hellsen83 <erik.christiansen83@gmail.com>
2019-02-25 19:21:00 +0100

    added second method to emphasize chaining, resolved missing import bug

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -2052,11 +2052,19 @@ class DataFrame(object):
 
         :param func: a function that takes and returns a class:`DataFrame`.
 
-        >>> df = spark.createDataFrame([Row(a=170.1, b=75.0)])
+        >>> from pyspark.sql.functions import col
+        >>> df = spark.createDataFrame([(1, 1.0), (2, 2.0)], ["int", "float"])
         >>> def cast_all_to_int(input_df):
-        ...     return input_df.select([col(c_name).cast("int") for c_name in input_df.columns])
-        >>> df.transform(cast_all_to_int).collect()
-        [Row(a=170), Row(b=75)]
+        ...     return input_df.select([col(col_name).cast("int") for col_name in input_df.columns])
+        >>> def sort_columns_asc(input_df):
+        ...     return input_df.select(*sorted(input_df.columns))
+        >>> df.transform(cast_all_to_int).transform(sort_columns_asc).show()
+        +-----+---+
+        |float|int|
+        +-----+---+
+        |    1|  1|
+        |    2|  2|
+        +-----+---+
         """
         result = func(self)
         assert isinstance(result, DataFrame), "Func returned an instance of type [%s], " \
