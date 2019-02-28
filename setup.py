71140dd2dfb63f16254420b8ba3a4a62b5919f45
RosterIn <48057736+RosterIn@users.noreply.github.com>
2019-02-28 08:05:49 +0200

    [AIRFLOW-2767] - Upgrade gunicorn to 19.5.0 to avoid moderate-severity CVE (#4795)
    Upgrade gunicorn to 19.5.0 to avoid moderate-severity CVE

--- setup.py
+++ setup.py
@@ -302,7 +302,7 @@ def do_setup():
             'funcsigs==1.0.0',
             'future>=0.16.0, <0.17',
             'gitpython>=2.0.2',
-            'gunicorn>=19.4.0, <20.0',
+            'gunicorn>=19.5.0, <20.0',
             'iso8601>=0.1.12',
             'json-merge-patch==0.2',
             'jinja2>=2.7.3, <=2.10.0',
