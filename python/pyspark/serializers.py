174ad9907706f78f8b58628ffd5349c91b51e1b7
Bryan Cutler <cutlerb@gmail.com>
2019-02-27 17:39:41 -0800

    remove unnecessary str addition

--- python/pyspark/serializers.py
+++ python/pyspark/serializers.py
@@ -301,7 +301,7 @@ def _create_batch(series, timezone, safecheck, assign_cols_by_name):
     for s, t in series:
         if t is not None and pa.types.is_struct(t):
             if not isinstance(s, pd.DataFrame):
-                raise ValueError("A field of type StructType expects a pandas.DataFrame, " +
+                raise ValueError("A field of type StructType expects a pandas.DataFrame, "
                                  "but got: %s" % str(type(s)))
 
             # Assign result columns by schema name if user labeled with strings, else use position
