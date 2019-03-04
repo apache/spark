b7b64dd5d12b0ec6196e4b5bd46c938b8ed70411
Jagadesh Kiran <jagadesh.n@in.verizon.com>
2019-03-04 15:47:11 +0530

    [SPARK-26860][PySpark] [SparkR] Fix for RangeBetween and RowsBetween docs to be in sync with spark documentation

--- python/pyspark/sql/window.py
+++ python/pyspark/sql/window.py
@@ -112,14 +112,14 @@ class Window(object):
         window = Window.partitionBy("category").orderBy("id").rowsBetween(Window.currentRow, 1)
         df.withColumn("sum",func.sum("id").over(window)).show()
 
-        id category sum
+        # id category sum
 
-        1       b    3
-        2       b    5
-        3       b    3
-        1       a    2
-        1       a    3
-        2       a    2
+        # 1       b    3
+        # 2       b    5
+        # 3       b    3
+        # 1       a    2
+        # 1       a    3
+        # 2       a    2
 
         """
 
@@ -173,14 +173,14 @@ class Window(object):
         window = Window.partitionBy("category").orderBy("id").rangeBetween(Window.currentRow, 1)
         df.withColumn("sum",func.sum("id").over(window)).show()
 
-        id category sum
+        # id category sum
 
-        1       b    3
-        2       b    5
-        3       b    3
-        1       a    4
-        1       a    4
-        2       a    2
+        # 1       b    3
+        # 2       b    5
+        # 3       b    3
+        # 1       a    4
+        # 1       a    4
+        # 2       a    2
 
         """
 
