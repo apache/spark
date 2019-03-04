990bd3c668ff24aac64427afe0d04c0886bb2f48
Sean Owen <sean.owen@databricks.com>
2019-03-05 08:03:39 +0900

    [SPARK-26016][DOCS] Clarify that text DataSource read/write, and RDD methods that read text, always use UTF-8
    ## What changes were proposed in this pull request?
    
    Clarify that text DataSource read/write, and RDD methods that read text, always use UTF-8 as they use Hadoop's implementation underneath. I think these are all the places that this needs a mention in the user-facing docs.
    
    ## How was this patch tested?
    
    Doc tests.
    
    Closes #23962 from srowen/SPARK-26016.
    
    Authored-by: Sean Owen <sean.owen@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/streaming/DataStreamReader.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/streaming/DataStreamReader.scala
@@ -387,6 +387,7 @@ final class DataStreamReader private[sql](sparkSession: SparkSession) extends Lo
   /**
    * Loads text files and returns a `DataFrame` whose schema starts with a string column named
    * "value", and followed by partitioned columns if there are any.
+   * The text files must be encoded as UTF-8.
    *
    * By default, each line in the text files is a new row in the resulting DataFrame. For example:
    * {{{
@@ -414,6 +415,7 @@ final class DataStreamReader private[sql](sparkSession: SparkSession) extends Lo
   /**
    * Loads text file(s) and returns a `Dataset` of String. The underlying schema of the Dataset
    * contains a single string column named "value".
+   * The text files must be encoded as UTF-8.
    *
    * If the directory structure of the text files contains partitioning information, those are
    * ignored in the resulting Dataset. To include partitioning information as columns, use `text`.
