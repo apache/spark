1bfb5405f8ecc899e7785abb5640fb72abc411dc
Philip Stutz <philip.stutz@gmail.com>
2019-03-04 16:03:17 +0000

    Throw `UnsupportedOperationException` when `entityTables` is called

--- graph/cypher/src/main/scala/org/apache/spark/graph/cypher/SparkCypherSession.scala
+++ graph/cypher/src/main/scala/org/apache/spark/graph/cypher/SparkCypherSession.scala
@@ -5,11 +5,11 @@ import org.apache.spark.graph.api.io.{ReaderConfig, WriterConfig}
 import org.apache.spark.graph.cypher.SparkTable.DataFrameTable
 import org.apache.spark.graph.cypher.adapters.MappingAdapter._
 import org.apache.spark.graph.cypher.adapters.RelationalGraphAdapter
-import org.apache.spark.graph.cypher.io.ReadWriteGraph.GraphImporter
+import org.apache.spark.graph.cypher.io.ReadWriteGraph._
 import org.apache.spark.sql.SparkSession
 import org.opencypher.okapi.api.types.{CTNode, CTRelationship}
 import org.opencypher.okapi.api.value.CypherValue.CypherMap
-import org.opencypher.okapi.impl.exception.IllegalArgumentException
+import org.opencypher.okapi.impl.exception.{IllegalArgumentException, UnsupportedOperationException}
 import org.opencypher.okapi.relational.api.graph.{RelationalCypherGraph, RelationalCypherGraphFactory, RelationalCypherSession}
 import org.opencypher.okapi.relational.api.planning.RelationalCypherResult
 import org.opencypher.okapi.relational.api.table.RelationalEntityTableFactory
@@ -34,11 +34,15 @@ class SparkCypherSession(override val sparkSession: SparkSession) extends Relati
 
   override val records: SparkCypherRecordsFactory = SparkCypherRecordsFactory()
 
-  override val graphs: RelationalCypherGraphFactory[DataFrameTable] = new RelationalCypherGraphFactory[DataFrameTable]() {
-    override implicit val session: RelationalCypherSession[DataFrameTable] = sparkCypherSession
+  override val graphs: RelationalCypherGraphFactory[DataFrameTable] = {
+    new RelationalCypherGraphFactory[DataFrameTable]() {
+      override implicit val session: RelationalCypherSession[DataFrameTable] = sparkCypherSession
+    }
   }
 
-  override def entityTables: RelationalEntityTableFactory[DataFrameTable] = ???
+  override def entityTables: RelationalEntityTableFactory[DataFrameTable] = {
+    throw UnsupportedOperationException("Graph construction with `CONSTRUCT` is not supported in Cypher 9")
+  }
 
   override def createGraph(nodes: Seq[NodeFrame], relationships: Seq[RelationshipFrame] = Seq.empty): PropertyGraph = {
     require(nodes.nonEmpty, "Creating a graph requires at least one NodeDataFrame")
@@ -80,18 +84,19 @@ class SparkCypherSession(override val sparkSession: SparkSession) extends Relati
   }
 
   override private[spark] def writeGraph(graph: PropertyGraph, config: WriterConfig): Unit = {
-    import org.apache.spark.graph.cypher.io.ReadWriteGraph._
     val relationalGraph = toRelationalGraph(graph)
     val graphDirectoryStructure = SparkGraphDirectoryStructure(config.path)
 
     relationalGraph.schema.labelCombinations.combos.foreach { combo =>
-      relationalGraph.canonicalNodeTable(combo).write
+      relationalGraph.canonicalNodeTable(combo)
+        .write
         .format(config.source)
         .mode(config.mode)
         .save(graphDirectoryStructure.pathToNodeTable(combo))
     }
     relationalGraph.schema.relationshipTypes.foreach { relType =>
-      relationalGraph.canonicalRelationshipTable(relType).write
+      relationalGraph.canonicalRelationshipTable(relType)
+        .write
         .format(config.source)
         .mode(config.mode)
         .save(graphDirectoryStructure.pathToRelationshipTable(relType))
