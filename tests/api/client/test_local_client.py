5848c05e625732207d9b5b3dfa27b69aaa894eec
Peter van 't Hof <ffinfo@users.noreply.github.com>
2019-03-06 13:15:54 +0100

    [AIRFLOW-4024] Improve local client tests (#4850)
    * Improve local client tests
    
    * remove self.session
    
    * Fixing import

--- tests/api/client/test_local_client.py
+++ tests/api/client/test_local_client.py
@@ -24,11 +24,14 @@ from freezegun import freeze_time
 from mock import patch
 
 from airflow import AirflowException
-from airflow.api.client.local_client import Client
 from airflow import models
-from airflow import settings
+from airflow.api.client.local_client import Client
+from airflow.example_dags import example_bash_operator
+from airflow.models import DagModel, DagBag
 from airflow.utils import timezone
+from airflow.utils.db import create_session
 from airflow.utils.state import State
+from tests.test_utils.db import clear_db_pools
 
 EXECDATE = timezone.utcnow()
 EXECDATE_NOFRACTIONS = EXECDATE.replace(microsecond=0)
@@ -40,35 +43,29 @@ class TestLocalClient(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         super(TestLocalClient, cls).setUpClass()
-        session = settings.Session()
-        session.query(models.Pool).delete()
-        session.commit()
-        session.close()
+        DagBag(example_bash_operator.__file__).get_dag("example_bash_operator").sync_to_db()
 
     def setUp(self):
         super(TestLocalClient, self).setUp()
+        clear_db_pools()
         self.client = Client(api_base_url=None, auth=None)
-        self.session = settings.Session()
 
     def tearDown(self):
-        self.session.query(models.Pool).delete()
-        self.session.commit()
-        self.session.close()
+        clear_db_pools()
         super(TestLocalClient, self).tearDown()
 
     @patch.object(models.DAG, 'create_dagrun')
     def test_trigger_dag(self, mock):
-        client = self.client
         test_dag_id = "example_bash_operator"
         models.DagBag(include_examples=True)
 
         # non existent
         with self.assertRaises(AirflowException):
-            client.trigger_dag(dag_id="blablabla")
+            self.client.trigger_dag(dag_id="blablabla")
 
         with freeze_time(EXECDATE):
             # no execution date, execution date should be set automatically
-            client.trigger_dag(dag_id=test_dag_id)
+            self.client.trigger_dag(dag_id=test_dag_id)
             mock.assert_called_once_with(run_id="manual__{0}".format(EXECDATE_ISO),
                                          execution_date=EXECDATE_NOFRACTIONS,
                                          state=State.RUNNING,
@@ -77,7 +74,7 @@ class TestLocalClient(unittest.TestCase):
             mock.reset_mock()
 
             # execution date with microseconds cutoff
-            client.trigger_dag(dag_id=test_dag_id, execution_date=EXECDATE)
+            self.client.trigger_dag(dag_id=test_dag_id, execution_date=EXECDATE)
             mock.assert_called_once_with(run_id="manual__{0}".format(EXECDATE_ISO),
                                          execution_date=EXECDATE_NOFRACTIONS,
                                          state=State.RUNNING,
@@ -87,7 +84,7 @@ class TestLocalClient(unittest.TestCase):
 
             # run id
             run_id = "my_run_id"
-            client.trigger_dag(dag_id=test_dag_id, run_id=run_id)
+            self.client.trigger_dag(dag_id=test_dag_id, run_id=run_id)
             mock.assert_called_once_with(run_id=run_id,
                                          execution_date=EXECDATE_NOFRACTIONS,
                                          state=State.RUNNING,
@@ -97,7 +94,7 @@ class TestLocalClient(unittest.TestCase):
 
             # test conf
             conf = '{"name": "John"}'
-            client.trigger_dag(dag_id=test_dag_id, conf=conf)
+            self.client.trigger_dag(dag_id=test_dag_id, conf=conf)
             mock.assert_called_once_with(run_id="manual__{0}".format(EXECDATE_ISO),
                                          execution_date=EXECDATE_NOFRACTIONS,
                                          state=State.RUNNING,
@@ -107,16 +104,16 @@ class TestLocalClient(unittest.TestCase):
 
     def test_delete_dag(self):
         key = "my_dag_id"
-        session = settings.Session()
-        DM = models.DagModel
-        self.assertEqual(session.query(DM).filter(DM.dag_id == key).count(), 0)
 
-        session.add(DM(dag_id=key))
-        session.commit()
-        self.assertEqual(session.query(DM).filter(DM.dag_id == key).count(), 1)
+        with create_session() as session:
+            self.assertEqual(session.query(DagModel).filter(DagModel.dag_id == key).count(), 0)
+            session.add(DagModel(dag_id=key))
 
-        self.client.delete_dag(dag_id=key)
-        self.assertEqual(session.query(DM).filter(DM.dag_id == key).count(), 0)
+        with create_session() as session:
+            self.assertEqual(session.query(DagModel).filter(DagModel.dag_id == key).count(), 1)
+
+            self.client.delete_dag(dag_id=key)
+            self.assertEqual(session.query(DagModel).filter(DagModel.dag_id == key).count(), 0)
 
     def test_get_pool(self):
         self.client.create_pool(name='foo', slots=1, description='')
@@ -132,10 +129,13 @@ class TestLocalClient(unittest.TestCase):
     def test_create_pool(self):
         pool = self.client.create_pool(name='foo', slots=1, description='')
         self.assertEqual(pool, ('foo', 1, ''))
-        self.assertEqual(self.session.query(models.Pool).count(), 1)
+        with create_session() as session:
+            self.assertEqual(session.query(models.Pool).count(), 1)
 
     def test_delete_pool(self):
         self.client.create_pool(name='foo', slots=1, description='')
-        self.assertEqual(self.session.query(models.Pool).count(), 1)
+        with create_session() as session:
+            self.assertEqual(session.query(models.Pool).count(), 1)
         self.client.delete_pool(name='foo')
-        self.assertEqual(self.session.query(models.Pool).count(), 0)
+        with create_session() as session:
+            self.assertEqual(session.query(models.Pool).count(), 0)
