f47dfae1e633c9995aca227a4bd13bc3446f7ccb
Maxim Gekk <max.gekk@gmail.com>
2019-03-10 18:52:23 +0300

    Save time zoneId

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/TimestampFormatter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/TimestampFormatter.scala
@@ -20,8 +20,8 @@ package org.apache.spark.sql.catalyst.util
 import java.text.ParseException
 import java.time._
 import java.time.format.DateTimeParseException
-import java.time.temporal.{TemporalAccessor, TemporalQueries}
 import java.time.temporal.ChronoField.MICRO_OF_SECOND
+import java.time.temporal.TemporalQueries
 import java.util.{Locale, TimeZone}
 import java.util.concurrent.TimeUnit.SECONDS
 
@@ -48,11 +48,12 @@ class Iso8601TimestampFormatter(
     locale: Locale) extends TimestampFormatter with DateTimeFormatterHelper {
   @transient
   protected lazy val formatter = getOrCreateFormatter(pattern, locale)
+  private val timeZoneId = timeZone.toZoneId
 
   override def parse(s: String): Long = {
     val parsed = formatter.parse(s)
     val parsedZoneId = parsed.query(TemporalQueries.zone())
-    val zoneId = if (parsedZoneId == null) timeZone.toZoneId else parsedZoneId
+    val zoneId = if (parsedZoneId == null) timeZoneId else parsedZoneId
     val zonedDateTime = toZonedDateTime(parsed, zoneId)
     val epochSeconds = zonedDateTime.toEpochSecond
     val microsOfSecond = zonedDateTime.get(MICRO_OF_SECOND)
@@ -62,7 +63,7 @@ class Iso8601TimestampFormatter(
 
   override def format(us: Long): String = {
     val instant = DateTimeUtils.microsToInstant(us)
-    formatter.withZone(timeZone.toZoneId).format(instant)
+    formatter.withZone(timeZoneId).format(instant)
   }
 }
 
