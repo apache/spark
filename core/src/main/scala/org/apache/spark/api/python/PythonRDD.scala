57d251c473c1fce48a8f0b95042dc69f6550ee0c
Bryan Cutler <cutlerb@gmail.com>
2019-03-12 11:33:58 -0700

    rebased to fix SocketAuthServer name

--- core/src/main/scala/org/apache/spark/api/python/PythonRDD.scala
+++ core/src/main/scala/org/apache/spark/api/python/PythonRDD.scala
@@ -164,7 +164,7 @@ private[spark] object PythonRDD extends Logging {
   }
 
   def toLocalIteratorAndServe[T](rdd: RDD[T]): Array[Any] = {
-    val (port, secret) = PythonServer.setupOneConnectionServer(
+    val (port, secret) = SocketAuthServer.setupOneConnectionServer(
         authHelper, "serve toLocalIterator") { s =>
       val out = new DataOutputStream(s.getOutputStream)
       val in = new DataInputStream(s.getInputStream)
