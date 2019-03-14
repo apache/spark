c1917b5f6be42d04f1f79618cff30b0906b4f7b1
Holden Karau <holden@pigscanfly.ca>
2019-03-13 18:19:03 -0700

    Try and log our exit process

--- resource-managers/kubernetes/docker/src/main/dockerfiles/spark/decom.sh
+++ resource-managers/kubernetes/docker/src/main/dockerfiles/spark/decom.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 set -x
+LOG=/dev/termination-log
 echo "hi"
-echo "Decom adventures" > /dev/termination-log || echo "logging is hard"
+echo "Starting decom adventures" > ${LOG} || echo "logging is hard"
+date | tee -a ${LOG}
 WORKER_PID=$(ps axf | grep java |grep org.apache.spark.executor.CoarseGrainedExecutorBackend | grep -v grep  |  awk '{print "kill -9 " $1}')
-kill -s SIGPWR ${WORKER_PID}
-killall -s SIGPWR java
-waitpid ${WORKER_PID}
-sleep 30
+echo "Using worker pid $WORKER_PID" | tee -a ${LOG}
+kill -s SIGPWR ${WORKER_PID} | tee -a ${LOG}
+killall -s SIGPWR java  | tee -a ${LOG}
+waitpid ${WORKER_PID} | tee -a ${LOG}
+sleep 30 | tee -a ${LOG}
+echo "Done" | tee -a ${LOG}
+date | tee -a ${LOG}
