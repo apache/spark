0982c11ed8985898e15b4a5017b397dd629a42bb
Holden Karau <holden@pigscanfly.ca>
2019-03-13 18:27:00 -0700

    Wait that awk statement wasn't doing anything for me

--- resource-managers/kubernetes/docker/src/main/dockerfiles/spark/decom.sh
+++ resource-managers/kubernetes/docker/src/main/dockerfiles/spark/decom.sh
@@ -5,7 +5,7 @@ export LOG=/dev/termination-log
 echo "hi"
 echo "Starting decom adventures" > ${LOG} || echo "logging is hard"
 date | tee -a ${LOG}
-WORKER_PID=$(ps axf | grep java |grep org.apache.spark.executor.CoarseGrainedExecutorBackend | grep -v grep  |  awk '{print $1}')
+WORKER_PID=$(ps axf | grep java |grep org.apache.spark.executor.CoarseGrainedExecutorBackend | grep -v grep)
 echo "Using worker pid $WORKER_PID" | tee -a ${LOG}
 kill -s SIGPWR ${WORKER_PID} | tee -a ${LOG}
 killall -s SIGPWR java  | tee -a ${LOG}
