5595b5e4d4c1e3001d85e9ea60cd7f1609b8beac
Maxim Gekk <max.gekk@gmail.com>
2019-03-16 10:58:22 +0300

    Regen reference data for PySpark ML

--- python/pyspark/ml/clustering.py
+++ python/pyspark/ml/clustering.py
@@ -1200,10 +1200,10 @@ class PowerIterationClustering(HasMaxIter, HasWeightCol, JavaParams, JavaMLReada
     +---+-------+
     |id |cluster|
     +---+-------+
-    |0  |1      |
-    |1  |1      |
-    |2  |1      |
-    |3  |1      |
+    |0  |0      |
+    |1  |0      |
+    |2  |0      |
+    |3  |0      |
     |4  |1      |
     |5  |0      |
     +---+-------+
