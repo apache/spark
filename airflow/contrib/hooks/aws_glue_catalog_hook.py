afb108d3b7658b71c1b27bdf7e6c6e2dd1962e35
Bryan Yang <kenshin2004528@gmail.com>
2019-03-20 07:48:22 +0800

    [AIRFLOW-4124] add get_table and get_table_location in aws_glue_hook and tests (#4942)

--- airflow/contrib/hooks/aws_glue_catalog_hook.py
+++ airflow/contrib/hooks/aws_glue_catalog_hook.py
@@ -116,3 +116,37 @@ class AwsGlueCatalogHook(AwsHook):
             return True
         else:
             return False
+
+    def get_table(self, database_name, table_name):
+        """
+        Get the information of the table
+
+        :param database_name: Name of hive database (schema) @table belongs to
+        :type database_name: str
+        :param table_name: Name of hive table
+        :type table_name: str
+        :rtype: dict
+
+        >>> hook = AwsGlueCatalogHook()
+        >>> r = hook.get_table('db', 'table_foo')
+        >>> r['Name'] = 'table_foo'
+        """
+
+        result = self.get_conn().get_table(DatabaseName=database_name, Name=table_name)
+
+        return result['Table']
+
+    def get_table_location(self, database_name, table_name):
+        """
+        Get the physical location of the table
+
+        :param database_name: Name of hive database (schema) @table belongs to
+        :type database_name: str
+        :param table_name: Name of hive table
+        :type table_name: str
+        :return: str
+        """
+
+        table = self.get_table(database_name, table_name)
+
+        return table['StorageDescriptor']['Location']
--- tests/contrib/hooks/test_aws_glue_catalog_hook.py
+++ tests/contrib/hooks/test_aws_glue_catalog_hook.py
@@ -18,6 +18,7 @@
 
 import unittest
 
+import boto3
 from airflow.contrib.hooks.aws_glue_catalog_hook import AwsGlueCatalogHook
 
 try:
@@ -30,10 +31,30 @@ try:
 except ImportError:
     import mock
 
+DB_NAME = 'db'
+TABLE_NAME = 'table'
+TABLE_INPUT = {
+    "Name": TABLE_NAME,
+    "StorageDescriptor": {
+        "Columns": [
+            {
+                "Name": "string",
+                "Type": "string",
+                "Comment": "string"
+            }
+        ],
+        "Location": "s3://mybucket/{}/{}".format(DB_NAME, TABLE_NAME),
+    }
+}
 
 @unittest.skipIf(mock_glue is None,
                  "Skipping test because moto.mock_glue is not available")
 class TestAwsGlueCatalogHook(unittest.TestCase):
+
+    @mock_glue
+    def setUp(self):
+        self.client = boto3.client('glue', region_name='us-east-1')
+        self.hook = AwsGlueCatalogHook(region_name="us-east-1")
 
     @mock_glue
     def test_get_conn_returns_a_boto3_connection(self):
@@ -104,6 +125,54 @@ class TestAwsGlueCatalogHook(unittest.TestCase):
         hook = AwsGlueCatalogHook(region_name="us-east-1")
 
         self.assertFalse(hook.check_for_partition('db', 'tbl', 'expr'))
+
+    @mock_glue
+    def test_get_table_exists(self):
+        self.client.create_database(
+            DatabaseInput={
+                'Name': DB_NAME
+            }
+        )
+        self.client.create_table(
+            DatabaseName=DB_NAME,
+            TableInput=TABLE_INPUT
+        )
+
+        result = self.hook.get_table(DB_NAME, TABLE_NAME)
+
+        self.assertEqual(result['Name'], TABLE_INPUT['Name'])
+        self.assertEqual(result['StorageDescriptor']['Location'],
+                         TABLE_INPUT['StorageDescriptor']['Location'])
+
+    @mock_glue
+    def test_get_table_not_exists(self):
+        self.client.create_database(
+            DatabaseInput={
+                'Name': DB_NAME
+            }
+        )
+        self.client.create_table(
+            DatabaseName=DB_NAME,
+            TableInput=TABLE_INPUT
+        )
+
+        with self.assertRaises(Exception):
+            self.hook.get_table(DB_NAME, 'dummy_table')
+
+    @mock_glue
+    def test_get_table_location(self):
+        self.client.create_database(
+            DatabaseInput={
+                'Name': DB_NAME
+            }
+        )
+        self.client.create_table(
+            DatabaseName=DB_NAME,
+            TableInput=TABLE_INPUT
+        )
+
+        result = self.hook.get_table_location(DB_NAME, TABLE_NAME)
+        self.assertEqual(result, TABLE_INPUT['StorageDescriptor']['Location'])
 
 
 if __name__ == '__main__':
