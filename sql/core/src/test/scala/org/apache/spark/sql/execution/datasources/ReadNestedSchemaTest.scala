4f148b9e402dd6b86d347d439b7ee1921c9b98be
Dongjoon Hyun <dhyun@apple.com>
2019-03-19 09:25:50 -0700

    Address comments

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/ReadNestedSchemaTest.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/ReadNestedSchemaTest.scala
@@ -19,7 +19,7 @@ package org.apache.spark.sql.execution.datasources
 
 import java.io.File
 
-import org.apache.spark.sql.Row
+import org.apache.spark.sql.{DataFrame, Row}
 
 
 /**
@@ -27,13 +27,9 @@ import org.apache.spark.sql.Row
  */
 trait AddNestedColumnTest extends ReadSchemaTest {
 
-  test("add a nested column at the end of the leaf struct column") {
+  private def testAdd(df1: DataFrame, df2: DataFrame, expectedRows: Seq[Row]): Unit = {
     withTempPath { dir =>
       val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4, 'c7', 5)) c2")
 
       val dir1 = s"$path${File.separator}part=one"
       val dir2 = s"$path${File.separator}part=two"
@@ -47,88 +43,44 @@ trait AddNestedColumnTest extends ReadSchemaTest {
         .options(options)
         .load(path)
 
-      checkAnswer(df, Seq(
+      checkAnswer(df, expectedRows)
+    }
+  }
+
+  test("add a nested column at the end of the leaf struct column") {
+    testAdd(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4, 'c7', 5)) c2"),
+      Seq(
         Row(1, Row(2, Row(3, 4, null)), "one"),
         Row(1, Row(2, Row(3, 4, 5)), "two")))
-    }
   }
 
   test("add a nested column in the middle of the leaf struct column") {
-    withTempPath { dir =>
-      val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c7', 5, 'c6', 4)) c2")
-
-      val dir1 = s"$path${File.separator}part=one"
-      val dir2 = s"$path${File.separator}part=two"
-
-      df1.write.format(format).options(options).save(dir1)
-      df2.write.format(format).options(options).save(dir2)
-
-      val df = spark.read
-        .schema(df2.schema)
-        .format(format)
-        .options(options)
-        .load(path)
-
-      checkAnswer(df, Seq(
+    testAdd(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c7', 5, 'c6', 4)) c2"),
+      Seq(
         Row(1, Row(2, Row(3, null, 4)), "one"),
         Row(1, Row(2, Row(3, 5, 4)), "two")))
-    }
   }
 
   test("add a nested column at the end of the middle struct column") {
-    withTempPath { dir =>
-      val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4), 'c7', 5) c2")
-
-      val dir1 = s"$path${File.separator}part=one"
-      val dir2 = s"$path${File.separator}part=two"
-
-      df1.write.format(format).options(options).save(dir1)
-      df2.write.format(format).options(options).save(dir2)
-
-      val df = spark.read
-        .schema(df2.schema)
-        .format(format)
-        .options(options)
-        .load(path)
-
-      checkAnswer(df, Seq(
+    testAdd(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4), 'c7', 5) c2"),
+      Seq(
         Row(1, Row(2, Row(3, 4), null), "one"),
         Row(1, Row(2, Row(3, 4), 5), "two")))
-    }
   }
 
   test("add a nested column in the middle of the middle struct column") {
-    withTempPath { dir =>
-      val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 1 c1, named_struct('c3', 2, 'c7', 5, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-
-      val dir1 = s"$path${File.separator}part=one"
-      val dir2 = s"$path${File.separator}part=two"
-
-      df1.write.format(format).options(options).save(dir1)
-      df2.write.format(format).options(options).save(dir2)
-
-      val df = spark.read
-        .schema(df2.schema)
-        .format(format)
-        .options(options)
-        .load(path)
-
-      checkAnswer(df, Seq(
+    testAdd(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c7', 5, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      Seq(
         Row(1, Row(2, null, Row(3, 4)), "one"),
         Row(1, Row(2, 5, Row(3, 4)), "two")))
-    }
   }
 }
 
@@ -137,19 +89,17 @@ trait AddNestedColumnTest extends ReadSchemaTest {
  */
 trait HideNestedColumnTest extends ReadSchemaTest {
 
-  test("hide a nested column at the end of the leaf struct column") {
+  private def testHide(df1: DataFrame, df2: DataFrame, df3: DataFrame, expectedRows: Seq[Row]) = {
     withTempPath { dir =>
       val path = dir.getCanonicalPath
 
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 5 c1, named_struct('c3', 6, 'c4', named_struct('c5', 7, 'c6', 8, 'c7', 9)) c2")
-
       val dir1 = s"$path${File.separator}part=one"
       val dir2 = s"$path${File.separator}part=two"
+      val dir3 = s"$path${File.separator}part=three"
 
       df1.write.format(format).options(options).save(dir1)
       df2.write.format(format).options(options).save(dir2)
+      df3.write.format(format).options(options).save(dir3)
 
       val df = spark.read
         .schema(df1.schema)
@@ -157,88 +107,52 @@ trait HideNestedColumnTest extends ReadSchemaTest {
         .options(options)
         .load(path)
 
-      checkAnswer(df, Seq(
-        Row(1, Row(2, Row(3, 4)), "one"),
-        Row(5, Row(6, Row(7, 8)), "two")))
+      checkAnswer(df, expectedRows)
     }
+  }
+
+  test("hide a nested column at the end of the leaf struct column") {
+    testHide(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 5 c1, named_struct('c3', 6, 'c4', named_struct('c5', 7, 'c6', 8, 'c7', 9)) c2"),
+      sql("SELECT 0 c1, named_struct('c3', 1, 'c4', named_struct('c5', 2, 'c6', 3, 'c8', 4)) c2"),
+      Seq(
+        Row(1, Row(2, Row(3, 4)), "one"),
+        Row(5, Row(6, Row(7, 8)), "two"),
+        Row(0, Row(1, Row(2, 3)), "three")))
   }
 
   test("hide a nested column in the middle of the leaf struct column") {
-    withTempPath { dir =>
-      val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 5 c1, named_struct('c3', 6, 'c4', named_struct('c5', 7, 'c7', 8, 'c6', 9)) c2")
-
-      val dir1 = s"$path${File.separator}part=one"
-      val dir2 = s"$path${File.separator}part=two"
-
-      df1.write.format(format).options(options).save(dir1)
-      df2.write.format(format).options(options).save(dir2)
-
-      val df = spark.read
-        .schema(df1.schema)
-        .format(format)
-        .options(options)
-        .load(path)
-
-      checkAnswer(df, Seq(
+    testHide(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 5 c1, named_struct('c3', 6, 'c4', named_struct('c5', 7, 'c7', 8, 'c6', 9)) c2"),
+      sql("SELECT 0 c1, named_struct('c3', 1, 'c4', named_struct('c7', 2, 'c5', 3, 'c6', 4)) c2"),
+      Seq(
         Row(1, Row(2, Row(3, 4)), "one"),
-        Row(5, Row(6, Row(7, 9)), "two")))
-    }
+        Row(5, Row(6, Row(7, 9)), "two"),
+        Row(0, Row(1, Row(3, 4)), "three")))
   }
 
   test("hide a nested column at the end of the middle struct column") {
-    withTempPath { dir =>
-      val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 5 c1, named_struct('c3', 6, 'c4', named_struct('c5', 7, 'c6', 8), 'c7', 9) c2")
-
-      val dir1 = s"$path${File.separator}part=one"
-      val dir2 = s"$path${File.separator}part=two"
-
-      df1.write.format(format).options(options).save(dir1)
-      df2.write.format(format).options(options).save(dir2)
-
-      val df = spark.read
-        .schema(df1.schema)
-        .format(format)
-        .options(options)
-        .load(path)
-
-      checkAnswer(df, Seq(
+    testHide(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 5 c1, named_struct('c3', 6, 'c4', named_struct('c5', 7, 'c6', 8), 'c7', 9) c2"),
+      sql("SELECT 0 c1, named_struct('c3', 1, 'c4', named_struct('c5', 2, 'c6', 3), 'c8', 4) c2"),
+      Seq(
         Row(1, Row(2, Row(3, 4)), "one"),
-        Row(5, Row(6, Row(7, 8)), "two")))
-    }
+        Row(5, Row(6, Row(7, 8)), "two"),
+        Row(0, Row(1, Row(2, 3)), "three")))
   }
 
   test("hide a nested column in the middle of the middle struct column") {
-    withTempPath { dir =>
-      val path = dir.getCanonicalPath
-
-      val df1 = sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2")
-      val df2 =
-        sql("SELECT 5 c1, named_struct('c3', 6, 'c7', 7, 'c4', named_struct('c5', 8, 'c6', 9)) c2")
-
-      val dir1 = s"$path${File.separator}part=one"
-      val dir2 = s"$path${File.separator}part=two"
-
-      df1.write.format(format).options(options).save(dir1)
-      df2.write.format(format).options(options).save(dir2)
-
-      val df = spark.read
-        .schema(df1.schema)
-        .format(format)
-        .options(options)
-        .load(path)
-
-      checkAnswer(df, Seq(
+    testHide(
+      sql("SELECT 1 c1, named_struct('c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      sql("SELECT 5 c1, named_struct('c3', 6, 'c7', 7, 'c4', named_struct('c5', 8, 'c6', 9)) c2"),
+      sql("SELECT 0 c1, named_struct('c7', 1, 'c3', 2, 'c4', named_struct('c5', 3, 'c6', 4)) c2"),
+      Seq(
         Row(1, Row(2, Row(3, 4)), "one"),
-        Row(5, Row(6, Row(8, 9)), "two")))
-    }
+        Row(5, Row(6, Row(8, 9)), "two"),
+        Row(0, Row(2, Row(3, 4)), "three")))
   }
 }
 
