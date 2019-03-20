f3ba73a5f54cc233424cee4fdfd3a61674b2b48e
Darcy Shen <sadhen@zoho.com>
2019-03-19 20:28:46 -0700

    [SPARK-27160][SQL] Fix DecimalType when building orc filters
    DecimalType Literal should not be casted to Long.
    
    eg. For `df.filter("x < 3.14")`, assuming df (x in DecimalType) reads from a ORC table and uses the native ORC reader with predicate push down enabled, we will push down the `x < 3.14` predicate to the ORC reader via a SearchArgument.
    
    OrcFilters will construct the SearchArgument, but not handle the DecimalType correctly.
    
    The previous impl will construct `x < 3` from `x < 3.14`.
    
    ```
    $ sbt
    > sql/testOnly *OrcFilterSuite
    > sql/testOnly *OrcQuerySuite -- -z "27160"
    ```
    
    Closes #24092 from sadhen/spark27160.
    
    Authored-by: Darcy Shen <sadhen@zoho.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/orc/OrcQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/orc/OrcQuerySuite.scala
@@ -37,7 +37,7 @@ import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, LogicalRelation, RecordReaderIterator}
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.SharedSQLContext
-import org.apache.spark.sql.types.{IntegerType, StructType}
+import org.apache.spark.sql.types._
 import org.apache.spark.util.Utils
 
 case class AllDataTypesWithNonPrimitiveType(
@@ -595,6 +595,22 @@ abstract class OrcQueryTest extends OrcTest {
         testAllCorruptFilesWithoutSchemaInfer()
       }.getMessage
       assert(m4.contains("Malformed ORC file"))
+    }
+  }
+
+  test("SPARK-27160 Predicate pushdown correctness on DecimalType for ORC") {
+    withTempPath { dir =>
+      withSQLConf(SQLConf.ORC_FILTER_PUSHDOWN_ENABLED.key -> "true") {
+        val path = dir.getCanonicalPath
+        Seq(BigDecimal(0.1), BigDecimal(0.2), BigDecimal(-0.3))
+          .toDF("x").write.orc(path)
+        val df = spark.read.orc(path)
+        checkAnswer(df.filter("x >= 0.1"), Seq(Row(0.1), Row(0.2)))
+        checkAnswer(df.filter("x > 0.1"), Seq(Row(0.2)))
+        checkAnswer(df.filter("x <= 0.15"), Seq(Row(0.1), Row(-0.3)))
+        checkAnswer(df.filter("x < 0.1"), Seq(Row(-0.3)))
+        checkAnswer(df.filter("x == 0.2"), Seq(Row(0.2)))
+      }
     }
   }
 }
