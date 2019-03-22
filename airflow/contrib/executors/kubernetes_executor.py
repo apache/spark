e8c395093d06946d1be5ee072116bc2994dad0a7
Paul Bramhall <paulwamp@gmail.com>
2019-03-22 15:24:55 +0000

    [AIRFLOW-3737] Kubernetes executor cannot handle long dag/task names (#4636)
    Kubernetes has a 63char limit on label values, so when running either a
    subdag or a dag with a name with a length longer than 63 chars, the pod
    creation process will fail with the following error:
    
        [2019-01-21 08:07:05,337] {rest.py:219} DEBUG - response body:
        {"kind":"Status","apiVersion":"v1","metadata":{},"status":"Failure","message":"unable
        to parse requirement: invalid label value:
        \"very_long_dag_name.very_long_task_name\": must be no more than 63
        characters","reason":"BadRequest","code":400}
    
    Annotations however, allow for a relatively unrestricted length when
    storing them against pods, and as such would be better suited in this
    situation.
    
    Generate deterministic random data for dag_id and task_id
    To ensure testing consistency, when calling the '_gen_random_string()'
    function, we want to set the seed dependant on the string length and
    iteration within the `cases.extend` loop.
    This allows random values to be created (as required) but ensuring
    consistency of these values during each and every time we run tests
    against the code.

--- airflow/contrib/executors/kubernetes_executor.py
+++ airflow/contrib/executors/kubernetes_executor.py
@@ -16,6 +16,8 @@
 # under the License.
 
 import base64
+import hashlib
+import re
 import json
 import multiprocessing
 from queue import Queue
@@ -400,7 +402,9 @@ class AirflowKubernetesScheduler(LoggingMixin):
         pod = self.worker_configuration.make_pod(
             namespace=self.namespace, worker_uuid=self.worker_uuid,
             pod_id=self._create_pod_id(dag_id, task_id),
-            dag_id=dag_id, task_id=task_id, try_number=try_number,
+            dag_id=self._make_safe_label_value(dag_id),
+            task_id=self._make_safe_label_value(task_id),
+            try_number=try_number,
             execution_date=self._datetime_to_label_safe_datestring(execution_date),
             airflow_command=command, kube_executor_config=kube_executor_config
         )
@@ -478,6 +482,27 @@ class AirflowKubernetesScheduler(LoggingMixin):
         return safe_pod_id
 
     @staticmethod
+    def _make_safe_label_value(string):
+        """
+        Valid label values must be 63 characters or less and must be empty or begin and
+        end with an alphanumeric character ([a-z0-9A-Z]) with dashes (-), underscores (_),
+        dots (.), and alphanumerics between.
+
+        If the label value is then greater than 63 chars once made safe, or differs in any
+        way from the original value sent to this function, then we need to truncate to
+        53chars, and append it with a unique hash.
+        """
+        MAX_LABEL_LEN = 63
+
+        safe_label = re.sub(r'^[^a-z0-9A-Z]*|[^a-zA-Z0-9_\-\.]|[^a-z0-9A-Z]*$', '', string)
+
+        if len(safe_label) > MAX_LABEL_LEN or string != safe_label:
+            safe_hash = hashlib.md5(string.encode()).hexdigest()[:9]
+            safe_label = safe_label[:MAX_LABEL_LEN - len(safe_hash) - 1] + "-" + safe_hash
+
+        return safe_label
+
+    @staticmethod
     def _create_pod_id(dag_id, task_id):
         safe_dag_id = AirflowKubernetesScheduler._strip_unsafe_kubernetes_special_chars(
             dag_id)
@@ -519,17 +544,44 @@ class AirflowKubernetesScheduler(LoggingMixin):
             self.log.warn("could not get try_number as an int: %s", labels.get('try_number', '1'))
 
         try:
-            return (
-                labels['dag_id'], labels['task_id'],
-                self._label_safe_datestring_to_datetime(labels['execution_date']),
-                try_num,
-            )
+            dag_id = labels['dag_id']
+            task_id = labels['task_id']
+            ex_time = self._label_safe_datestring_to_datetime(labels['execution_date'])
         except Exception as e:
             self.log.warn(
-                'Error while converting labels to key; labels: %s; exception: %s',
+                'Error while retrieving labels; labels: %s; exception: %s',
                 labels, e
             )
             return None
+
+        with create_session() as session:
+            tasks = (
+                session
+                .query(TaskInstance)
+                .filter_by(execution_date=ex_time).all()
+            )
+            self.log.info(
+                'Checking %s task instances.',
+                len(tasks)
+            )
+            for task in tasks:
+                if (
+                    self._make_safe_label_value(task.dag_id) == dag_id and
+                    self._make_safe_label_value(task.task_id) == task_id and
+                    task.execution_date == ex_time
+                ):
+                    self.log.info(
+                        'Found matching task %s-%s (%s) with current state of %s',
+                        task.dag_id, task.task_id, task.execution_date, task.state
+                    )
+                    dag_id = task.dag_id
+                    task_id = task.task_id
+                    return (dag_id, task_id, ex_time, try_num)
+        self.log.warn(
+            'Failed to find and match task details to a pod; labels: %s',
+            labels
+        )
+        return None
 
 
 class KubernetesExecutor(BaseExecutor, LoggingMixin):
@@ -568,10 +620,16 @@ class KubernetesExecutor(BaseExecutor, LoggingMixin):
         )
 
         for task in queued_tasks:
-            dict_string = "dag_id={},task_id={},execution_date={},airflow-worker={}" \
-                .format(task.dag_id, task.task_id,
-                        AirflowKubernetesScheduler._datetime_to_label_safe_datestring(
-                            task.execution_date), self.worker_uuid)
+            dict_string = (
+                "dag_id={},task_id={},execution_date={},airflow-worker={}".format(
+                    AirflowKubernetesScheduler._make_safe_label_value(task.dag_id),
+                    AirflowKubernetesScheduler._make_safe_label_value(task.task_id),
+                    AirflowKubernetesScheduler._datetime_to_label_safe_datestring(
+                        task.execution_date
+                    ),
+                    self.worker_uuid
+                )
+            )
             kwargs = dict(label_selector=dict_string)
             pod_list = self.kube_client.list_namespaced_pod(
                 self.kube_config.kube_namespace, **kwargs)
--- tests/contrib/executors/test_kubernetes_executor.py
+++ tests/contrib/executors/test_kubernetes_executor.py
@@ -43,8 +43,12 @@ except ImportError:
 
 class TestAirflowKubernetesScheduler(unittest.TestCase):
     @staticmethod
-    def _gen_random_string(str_len):
-        return ''.join([random.choice(string.printable) for _ in range(str_len)])
+    def _gen_random_string(seed, str_len):
+        char_list = []
+        for char_seed in range(str_len):
+            random.seed(str(seed) * char_seed)
+            char_list.append(random.choice(string.printable))
+        return ''.join(char_list)
 
     def _cases(self):
         cases = [
@@ -56,26 +60,50 @@ class TestAirflowKubernetesScheduler(unittest.TestCase):
         ]
 
         cases.extend([
-            (self._gen_random_string(200), self._gen_random_string(200))
-            for _ in range(100)
+            (self._gen_random_string(seed, 200), self._gen_random_string(seed, 200))
+            for seed in range(100)
         ])
 
         return cases
 
     @staticmethod
-    def _is_valid_name(name):
+    def _is_valid_pod_id(name):
         regex = r"^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$"
         return (
             len(name) <= 253 and
             all(ch.lower() == ch for ch in name) and
             re.match(regex, name))
 
+    @staticmethod
+    def _is_safe_label_value(value):
+        regex = r'^[^a-z0-9A-Z]*|[^a-zA-Z0-9_\-\.]|[^a-z0-9A-Z]*$'
+        return (
+            len(value) <= 63 and
+            re.match(regex, value))
+
     @unittest.skipIf(AirflowKubernetesScheduler is None,
                      'kubernetes python package is not installed')
     def test_create_pod_id(self):
         for dag_id, task_id in self._cases():
             pod_name = AirflowKubernetesScheduler._create_pod_id(dag_id, task_id)
-            self.assertTrue(self._is_valid_name(pod_name))
+            self.assertTrue(self._is_valid_pod_id(pod_name))
+
+    def test_make_safe_label_value(self):
+        for dag_id, task_id in self._cases():
+            safe_dag_id = AirflowKubernetesScheduler._make_safe_label_value(dag_id)
+            self.assertTrue(self._is_safe_label_value(safe_dag_id))
+            safe_task_id = AirflowKubernetesScheduler._make_safe_label_value(task_id)
+            self.assertTrue(self._is_safe_label_value(safe_task_id))
+            id = "my_dag_id"
+            self.assertEqual(
+                id,
+                AirflowKubernetesScheduler._make_safe_label_value(id)
+            )
+            id = "my_dag_id_" + "a" * 64
+            self.assertEqual(
+                "my_dag_id_" + "a" * 43 + "-0ce114c45",
+                AirflowKubernetesScheduler._make_safe_label_value(id)
+            )
 
     @unittest.skipIf(AirflowKubernetesScheduler is None,
                      "kubernetes python package is not installed")
