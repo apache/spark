40acf579bcd8d2538a68e9bd86041048dbc94318
marengaz <marengaz@users.noreply.github.com>
2019-03-22 16:43:50 +0000

    [AIRFLOW-4144] add description of is_delete_operator_pod (#4943)
    [AIRFLOW-4144] add description of is_delete_operator_pod

--- airflow/contrib/operators/kubernetes_pod_operator.py
+++ airflow/contrib/operators/kubernetes_pod_operator.py
@@ -80,6 +80,10 @@ class KubernetesPodOperator(BaseOperator):
         /airflow/xcom/return.json in the container will also be pushed to an
         XCom when the container completes.
     :type do_xcom_push: bool
+    :param is_delete_operator_pod: What to do when the pod reaches its final
+        state, or the execution is interrupted.
+        If False (default): do nothing, If True: delete the pod
+    :type is_delete_operator_pod: bool
     :param hostnetwork: If True enable host networking on the pod
     :type hostnetwork: bool
     :param tolerations: A list of kubernetes tolerations
