071ad3ff12b2a4bbf6f753c1dfcd3a48fac3f289
Takeshi Yamamuro <yamamuro@apache.org>
2019-03-23 09:55:19 +0900

    Fix

--- sql/core/src/main/scala/org/apache/spark/sql/execution/CacheManager.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/CacheManager.scala
@@ -161,15 +161,7 @@ class CacheManager extends Logging {
     val relation = cachedData.cachedRepresentation
     val (rowCount, newColStats) =
       CommandUtils.computeColumnStats(sparkSession, relation, column)
-
-    relation.synchronized {
-      val oldStats = relation.statsOfPlanToCache
-      val newStats = oldStats.copy(
-        rowCount = Some(rowCount),
-        attributeStats = AttributeMap((oldStats.attributeStats ++ newColStats).toSeq)
-      )
-      relation.statsOfPlanToCache = newStats
-    }
+    relation.updateStats(rowCount, newColStats)
   }
 
   /**
