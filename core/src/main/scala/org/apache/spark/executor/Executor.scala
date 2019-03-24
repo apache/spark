9941e93d8d7f8327629ab3e6a7b6bf6229fb7dbf
Ajith <ajith2489@gmail.com>
2019-03-24 12:55:46 +0530

    update

--- core/src/main/scala/org/apache/spark/executor/Executor.scala
+++ core/src/main/scala/org/apache/spark/executor/Executor.scala
@@ -832,12 +832,13 @@ private[spark] class Executor(
     }
 
     val message = Heartbeat(executorId, accumUpdates.toArray, env.blockManager.blockManagerId)
-    val intervalMs = conf.getTimeAsMs("spark.executor.heartbeatInterval", "10s")
+    val heartbeatIntervalInSec = conf.getTimeAsMs("spark.executor.heartbeatInterval", "10s")
+      .millis.toSeconds.seconds
     try {
       val response =
         heartbeatReceiverRef.askSync[HeartbeatResponse](
           message,
-          new RpcTimeout(intervalMs.millis.toSeconds.seconds, "spark.executor.heartbeatInterval"))
+          new RpcTimeout(heartbeatIntervalInSec, "spark.executor.heartbeatInterval"))
       if (response.reregisterBlockManager) {
         logInfo("Told to re-register on heartbeat")
         env.blockManager.reregister()
