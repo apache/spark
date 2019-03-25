8561d26edd1a4ec85ff4bcf9e897b508dbd20e9b
liuxian <liu.xian3@zte.com.cn>
2019-03-25 14:47:40 -0700

    [SPARK-27256][CORE][SQL] If the configuration is used to set the number of bytes, we'd better use `bytesConf`'.
    ## What changes were proposed in this pull request?
    Currently, if we want to configure `spark.sql.files.maxPartitionBytes` to 256 megabytes, we must set  `spark.sql.files.maxPartitionBytes=268435456`, which is very unfriendly to users.
    
    And if we set it like this:`spark.sql.files.maxPartitionBytes=256M`, we will  encounter this exception:
    ```
    Exception in thread "main" java.lang.IllegalArgumentException:
     spark.sql.files.maxPartitionBytes should be long, but was 256M
            at org.apache.spark.internal.config.ConfigHelpers$.toNumber(ConfigBuilder.scala)
    ```
    This PR use `bytesConf` to replace `longConf` or `intConf`,  if the configuration is used to set the number of bytes.
    Configuration change list:
    `spark.files.maxPartitionBytes`
    `spark.files.openCostInBytes`
    `spark.shuffle.sort.initialBufferSize`
    `spark.shuffle.spill.initialMemoryThreshold`
    `spark.sql.autoBroadcastJoinThreshold`
    `spark.sql.files.maxPartitionBytes`
    `spark.sql.files.openCostInBytes`
    `spark.sql.defaultSizeInBytes`
    ## How was this patch tested?
    1.Existing unit tests
    2.Manual testing
    
    Closes #24187 from 10110346/bytesConf.
    
    Authored-by: liuxian <liu.xian3@zte.com.cn>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -254,7 +254,7 @@ object SQLConf {
       "command <code>ANALYZE TABLE &lt;tableName&gt; COMPUTE STATISTICS noscan</code> has been " +
       "run, and file-based data source tables where the statistics are computed directly on " +
       "the files of data.")
-    .longConf
+    .bytesConf(ByteUnit.BYTE)
     .createWithDefault(10L * 1024 * 1024)
 
   val LIMIT_SCALE_UP_FACTOR = buildConf("spark.sql.limit.scaleUpFactor")
@@ -853,7 +853,7 @@ object SQLConf {
 
   val FILES_MAX_PARTITION_BYTES = buildConf("spark.sql.files.maxPartitionBytes")
     .doc("The maximum number of bytes to pack into a single partition when reading files.")
-    .longConf
+    .bytesConf(ByteUnit.BYTE)
     .createWithDefault(128 * 1024 * 1024) // parquet.block.size
 
   val FILES_OPEN_COST_IN_BYTES = buildConf("spark.sql.files.openCostInBytes")
@@ -1156,7 +1156,7 @@ object SQLConf {
       s"which is larger than `${AUTO_BROADCASTJOIN_THRESHOLD.key}` to be more conservative. " +
       "That is to say by default the optimizer will not choose to broadcast a table unless it " +
       "knows for sure its size is small enough.")
-    .longConf
+    .bytesConf(ByteUnit.BYTE)
     .createWithDefault(Long.MaxValue)
 
   val NDV_MAX_ERROR =
