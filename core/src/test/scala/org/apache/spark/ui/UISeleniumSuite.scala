39ea3571c289ef51429cfa83f00758ef6806a611
Shixiong Zhu <zsxwing@gmail.com>
2019-04-05 15:26:02 -0700

    interrupt task

--- core/src/test/scala/org/apache/spark/ui/UISeleniumSuite.scala
+++ core/src/test/scala/org/apache/spark/ui/UISeleniumSuite.scala
@@ -736,6 +736,7 @@ class UISeleniumSuite extends SparkFunSuite with WebBrowser with Matchers with B
       additionalConfs = Map(
         EXECUTOR_HEARTBEAT_INTERVAL.key -> "10ms",
         LIVE_ENTITY_UPDATE_STALENESS_LIMIT.key -> "10ms"))) { sc =>
+      sc.setLocalProperty(SparkContext.SPARK_JOB_INTERRUPT_ON_CANCEL, "true")
       val f = sc.parallelize(1 to 1000, 1000).foreachAsync { _ =>
         // Make the task never finish so there won't be any task start/end events after the first 2
         // tasks start.
