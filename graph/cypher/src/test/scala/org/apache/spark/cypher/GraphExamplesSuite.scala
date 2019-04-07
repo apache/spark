557715e203b5a3b31cb0780f0cea12795faeedab
Martin Junghanns <martin.junghanns@neotechnology.com>
2019-04-07 11:27:26 +0200

    Add license headers

--- graph/cypher/src/test/scala/org/apache/spark/cypher/GraphExamplesSuite.scala
+++ graph/cypher/src/test/scala/org/apache/spark/cypher/GraphExamplesSuite.scala
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.apache.spark.cypher
 
 import org.apache.spark.SparkFunSuite
@@ -72,7 +90,7 @@ class GraphExamplesSuite extends SparkFunSuite with SharedCypherContext {
 
     val relsDF: DataFrame = spark.createDataFrame(Seq(
       (0L, 0L, 1L, true, false, Some(1984)),
-      (1L, 2L, 1L, false, true, None),
+      (1L, 2L, 1L, false, true, None)
     )).toDF("$ID", "$SOURCE_ID", "$TARGET_ID", ":KNOWS", ":TEACHES", "since")
 
     val graph: PropertyGraph = cypherSession.createGraph(nodeDF, relsDF)
