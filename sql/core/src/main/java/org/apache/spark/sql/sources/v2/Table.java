c86fb8af548a1d9da9ebc26a8ace22da01beda39
Wenchen Fan <wenchen@databricks.com>
2019-04-08 14:45:25 +0800

    revert some doc changes

--- sql/core/src/main/java/org/apache/spark/sql/sources/v2/Table.java
+++ sql/core/src/main/java/org/apache/spark/sql/sources/v2/Table.java
@@ -27,8 +27,8 @@ import java.util.Set;
  * implementation can be a directory on the file system, a topic of Kafka, or a table in the
  * catalog, etc.
  * <p>
- * This interface can mixin {@link SupportsRead} and {@link SupportsWrite} to provide data reading
- * and writing ability.
+ * This interface can mixin the following interfaces to support different operations, like
+ * {@code SupportsRead}.
  */
 @Evolving
 public interface Table {
