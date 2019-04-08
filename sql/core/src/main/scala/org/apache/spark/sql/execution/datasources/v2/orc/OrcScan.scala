c8de0a9cddf9d5df6b3e1684691904f70737b96c
Gengliang Wang <gengliang.wang@databricks.com>
2019-04-08 15:14:02 +0800

    [SPARK-27384][SQL] File source V2: Prune unnecessary partition columns
    ## What changes were proposed in this pull request?
    
    When scanning file sources, we can prune unnecessary partition columns on constructing input partitions, so that:
    1. Reduce the data transformation from Driver to Executors
    2. Make it easier to implement columnar batch readers, since the partition columns are already pruned.
    ## How was this patch tested?
    
    Existing unit tests.
    
    Closes #24296 from gengliangwang/prunePartitionValue.
    
    Authored-by: Gengliang Wang <gengliang.wang@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/orc/OrcScan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/orc/OrcScan.scala
@@ -32,15 +32,18 @@ case class OrcScan(
     hadoopConf: Configuration,
     fileIndex: PartitioningAwareFileIndex,
     dataSchema: StructType,
-    readSchema: StructType,
+    readDataSchema: StructType,
+    readPartitionSchema: StructType,
     options: CaseInsensitiveStringMap)
-  extends FileScan(sparkSession, fileIndex, readSchema, options) {
+  extends FileScan(sparkSession, fileIndex, readDataSchema, readPartitionSchema) {
   override def isSplitable(path: Path): Boolean = true
 
   override def createReaderFactory(): PartitionReaderFactory = {
     val broadcastedConf = sparkSession.sparkContext.broadcast(
       new SerializableConfiguration(hadoopConf))
+    // The partition values are already truncated in `FileScan.partitions`.
+    // We should use `readPartitionSchema` as the partition schema here.
     OrcPartitionReaderFactory(sparkSession.sessionState.conf, broadcastedConf,
-      dataSchema, fileIndex.partitionSchema, readSchema)
+      dataSchema, readDataSchema, readPartitionSchema)
   }
 }
