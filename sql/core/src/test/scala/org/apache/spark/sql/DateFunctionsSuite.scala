50bb8c1e908249100a717ad7b1beb63cad9655c8
Maxim Gekk <max.gekk@gmail.com>
2019-04-09 22:38:11 +0200

    Import SQLConf

--- sql/core/src/test/scala/org/apache/spark/sql/DateFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DateFunctionsSuite.scala
@@ -24,6 +24,7 @@ import java.util.concurrent.TimeUnit
 
 import org.apache.spark.sql.catalyst.util.DateTimeUtils
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.SharedSQLContext
 import org.apache.spark.unsafe.types.CalendarInterval
 
