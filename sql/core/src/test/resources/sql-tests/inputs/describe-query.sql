189743fc3331f27fbb0c7a3259c3935001b15707
Dilip Biswal <dbiswal@us.ibm.com>
2019-04-15 21:26:45 +0800

    [SPARK-27444][SQL][FOLLOWUP][MINOR][TEST] Add a test for describing multi select query.
    ## What changes were proposed in this pull request?
    This is a minor pr to add a test to describe a multi select query.
    
    ## How was this patch tested?
    Added a test in describe-query.sql
    
    Closes #24370 from dilipbiswal/describe-query-multiselect-test.
    
    Authored-by: Dilip Biswal <dbiswal@us.ibm.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/test/resources/sql-tests/inputs/describe-query.sql
+++ sql/core/src/test/resources/sql-tests/inputs/describe-query.sql
@@ -10,11 +10,11 @@ DESC SELECT 10.00D as col1;
 DESC QUERY SELECT key FROM desc_temp1 UNION ALL select CAST(1 AS DOUBLE);
 DESC QUERY VALUES(1.00D, 'hello') as tab1(col1, col2);
 DESC QUERY FROM desc_temp1 a SELECT *;
-
-
--- Error cases.
 DESC WITH s AS (SELECT 'hello' as col1) SELECT * FROM s;
 DESCRIBE QUERY WITH s AS (SELECT * from desc_temp1) SELECT * FROM s;
+DESCRIBE SELECT * FROM (FROM desc_temp2 select * select *);
+
+-- Error cases.
 DESCRIBE INSERT INTO desc_temp1 values (1, 'val1');
 DESCRIBE INSERT INTO desc_temp1 SELECT * FROM desc_temp2;
 DESCRIBE
