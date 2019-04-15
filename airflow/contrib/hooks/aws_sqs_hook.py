1c6aa5e912cb777ad2a6a9b42097c64e3f0d2f71
Kaxil Naik <kaxilnaik@gmail.com>
2019-04-15 03:26:03 +0100

    [AIRFLOW-XXX] Fix docstrings of SQSHook (#5099)

--- airflow/contrib/hooks/aws_sqs_hook.py
+++ airflow/contrib/hooks/aws_sqs_hook.py
@@ -23,6 +23,7 @@ from airflow.contrib.hooks.aws_hook import AwsHook
 class SQSHook(AwsHook):
     """
     Get the SQS client using boto3 library
+
     :return: SQS client
     :rtype: botocore.client.SQS
     """
@@ -30,42 +31,40 @@ class SQSHook(AwsHook):
     def get_conn(self):
         return self.get_client_type('sqs')
 
-    """
-    Create queue using connection object
-    :param queue_name: name of the queue.
-    :type queue_name: str
-    :param attributes: additional attributes for the queue (default: None)
-    :type attributes: dict
-
-        For details of the attributes parameter see :py:meth:`botocore.client.SQS.create_queue`
-
-    :return: dict with the information about the queue
-    :rtype: dict
-
-        For details of the returned value see :py:meth:`botocore.client.SQS.create_queue`
-    """
     def create_queue(self, queue_name, attributes=None):
+        """
+        Create queue using connection object
+
+        :param queue_name: name of the queue.
+        :type queue_name: str
+        :param attributes: additional attributes for the queue (default: None)
+            For details of the attributes parameter see :py:meth:`botocore.client.SQS.create_queue`
+        :type attributes: dict
+
+        :return: dict with the information about the queue
+            For details of the returned value see :py:meth:`botocore.client.SQS.create_queue`
+        :rtype: dict
+        """
         return self.get_conn().create_queue(QueueName=queue_name, Attributes=attributes or {})
 
-    """
-    Send message to the queue
-    :param queue_url: queue url
-    :type queue_url: str
-    :param message_body: the contents of the message
-    :type message_body: str
-    :param delay_seconds: seconds to delay the message
-    :type delay_seconds: int
-    :param message_attributes: additional attributes for the message (default: None)
-    :type message_attributes: dict
-
-        For details of the attributes parameter see :py:meth:`botocore.client.SQS.send_message`
-
-    :return: dict with the information about the message sent
-    :rtype: dict
-
-        For details of the returned value see :py:meth:`botocore.client.SQS.send_message`
-    """
     def send_message(self, queue_url, message_body, delay_seconds=0, message_attributes=None):
+        """
+        Send message to the queue
+
+        :param queue_url: queue url
+        :type queue_url: str
+        :param message_body: the contents of the message
+        :type message_body: str
+        :param delay_seconds: seconds to delay the message
+        :type delay_seconds: int
+        :param message_attributes: additional attributes for the message (default: None)
+            For details of the attributes parameter see :py:meth:`botocore.client.SQS.send_message`
+        :type message_attributes: dict
+
+        :return: dict with the information about the message sent
+            For details of the returned value see :py:meth:`botocore.client.SQS.send_message`
+        :rtype: dict
+        """
         return self.get_conn().send_message(QueueUrl=queue_url,
                                             MessageBody=message_body,
                                             DelaySeconds=delay_seconds,
