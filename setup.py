cb4e39a1ab903237cca6f9f965e5dd256a268491
Kaxil Naik <kaxilnaik@gmail.com>
2019-04-17 03:25:50 +0100

    [AIRFLOW-4299] Upgrade to Celery 4.3.0 to fix crashing workers (#5116)

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ azure = [
 ]
 cassandra = ['cassandra-driver>=3.13.0']
 celery = [
-    'celery>=4.1.1, <4.2.0',
+    'celery~=4.3',
     'flower>=0.7.3, <1.0',
     'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
 ]
