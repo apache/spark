a71d4b861302db6101a399db9ef562d716f3c26e
Felix Uellendall <feluelle@users.noreply.github.com>
2019-04-17 23:05:22 +0200

    [AIRFLOW-4338] Change k8s pod_request_factory to use yaml safe_load (#5120)
    - fixes yaml.load deprecation warning

--- airflow/contrib/kubernetes/kubernetes_request_factory/pod_request_factory.py
+++ airflow/contrib/kubernetes/kubernetes_request_factory/pod_request_factory.py
@@ -42,7 +42,7 @@ spec:
 
     def create(self, pod):
         # type: (Pod) -> dict
-        req = yaml.load(self._yaml)
+        req = yaml.safe_load(self._yaml)
         self.extract_name(pod, req)
         self.extract_labels(pod, req)
         self.extract_image(pod, req)
@@ -110,7 +110,7 @@ spec:
 
     def create(self, pod):
         # type: (Pod) -> dict
-        req = yaml.load(self._yaml)
+        req = yaml.safe_load(self._yaml)
         self.extract_name(pod, req)
         self.extract_labels(pod, req)
         self.extract_image(pod, req)
