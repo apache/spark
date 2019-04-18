70874b7b2c60a1f1b1a4bd0b14c2a2866bc08480
Gengliang Wang <gengliang.wang@databricks.com>
2019-04-18 11:49:44 +0800

    add org.apache.spark.sql.hive.StatisticsSuite

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -463,6 +463,7 @@ object SparkParallelTestGrouping {
     "org.apache.spark.sql.catalyst.expressions.CastSuite",
     "org.apache.spark.sql.catalyst.expressions.MathExpressionsSuite",
     "org.apache.spark.sql.hive.HiveExternalCatalogSuite",
+    "org.apache.spark.sql.hive.StatisticsSuite",
     "org.apache.spark.sql.hive.execution.HiveCompatibilitySuite",
     "org.apache.spark.sql.hive.client.VersionsSuite",
     "org.apache.spark.sql.hive.HiveExternalCatalogVersionsSuite",
