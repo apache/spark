e4a28c57a5def0ba11d3c0b517b4d935082be24a
shane knapp <incomplete@gmail.com>
2019-04-19 09:28:51 -0700

    remove testing path

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -518,8 +518,7 @@ def main():
         hadoop_version = os.environ.get("AMPLAB_JENKINS_BUILD_PROFILE", "hadoop2.6")
         test_env = "amplab_jenkins"
         # add path for Python3 in Jenkins if we're calling from a Jenkins machine
-        # TODO(sknapp):  s/-new// before merging
-        os.environ["PATH"] = "/home/anaconda/envs/py3k-new/bin:" + os.environ.get("PATH")
+        os.environ["PATH"] = "/home/anaconda/envs/py3k/bin:" + os.environ.get("PATH")
     else:
         # else we're running locally and can use local settings
         build_tool = "sbt"
