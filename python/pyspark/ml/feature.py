5bf5d9d854db53541956dedb03e2de8eecf65b81
Andrew-Crosby <37139900+Andrew-Crosby@users.noreply.github.com>
2019-04-23 13:53:33 -0700

    [SPARK-26970][PYTHON][ML] Add Spark ML interaction transformer to PySpark
    ## What changes were proposed in this pull request?
    
    Adds the Spark ML Interaction transformer to PySpark
    
    ## How was this patch tested?
    
    - Added Python doctest
    - Ran the newly added example code
    - Manually confirmed that a PipelineModel that contains an Interaction transformer can now be loaded in PySpark
    
    Closes #24426 from Andrew-Crosby/pyspark-interaction-transformer.
    
    Lead-authored-by: Andrew-Crosby <37139900+Andrew-Crosby@users.noreply.github.com>
    Co-authored-by: Andrew-Crosby <andrew.crosby@autotrader.co.uk>
    Signed-off-by: Bryan Cutler <cutlerb@gmail.com>

--- python/pyspark/ml/feature.py
+++ python/pyspark/ml/feature.py
@@ -39,6 +39,7 @@ __all__ = ['Binarizer',
            'IDF', 'IDFModel',
            'Imputer', 'ImputerModel',
            'IndexToString',
+           'Interaction',
            'MaxAbsScaler', 'MaxAbsScalerModel',
            'MinHashLSH', 'MinHashLSHModel',
            'MinMaxScaler', 'MinMaxScalerModel',
@@ -1225,6 +1226,59 @@ class ImputerModel(JavaModel, JavaMLReadable, JavaMLWritable):
         which are used to replace the missing values in the input DataFrame.
         """
         return self._call_java("surrogateDF")
+
+
+@inherit_doc
+class Interaction(JavaTransformer, HasInputCols, HasOutputCol, JavaMLReadable, JavaMLWritable):
+    """
+    Implements the feature interaction transform. This transformer takes in Double and Vector type
+    columns and outputs a flattened vector of their feature interactions. To handle interaction,
+    we first one-hot encode any nominal features. Then, a vector of the feature cross-products is
+    produced.
+
+    For example, given the input feature values `Double(2)` and `Vector(3, 4)`, the output would be
+    `Vector(6, 8)` if all input features were numeric. If the first feature was instead nominal
+    with four categories, the output would then be `Vector(0, 0, 0, 0, 3, 4, 0, 0)`.
+
+    >>> df = spark.createDataFrame([(0.0, 1.0), (2.0, 3.0)], ["a", "b"])
+    >>> interaction = Interaction(inputCols=["a", "b"], outputCol="ab")
+    >>> interaction.transform(df).show()
+    +---+---+-----+
+    |  a|  b|   ab|
+    +---+---+-----+
+    |0.0|1.0|[0.0]|
+    |2.0|3.0|[6.0]|
+    +---+---+-----+
+    ...
+    >>> interactionPath = temp_path + "/interaction"
+    >>> interaction.save(interactionPath)
+    >>> loadedInteraction = Interaction.load(interactionPath)
+    >>> loadedInteraction.transform(df).head().ab == interaction.transform(df).head().ab
+    True
+
+    .. versionadded:: 3.0.0
+    """
+
+    @keyword_only
+    def __init__(self, inputCols=None, outputCol=None):
+        """
+        __init__(self, inputCols=None, outputCol=None):
+        """
+        super(Interaction, self).__init__()
+        self._java_obj = self._new_java_obj("org.apache.spark.ml.feature.Interaction", self.uid)
+        self._setDefault()
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    @keyword_only
+    @since("3.0.0")
+    def setParams(self, inputCols=None, outputCol=None):
+        """
+        setParams(self, inputCols=None, outputCol=None)
+        Sets params for this Interaction.
+        """
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
 
 
 @inherit_doc
