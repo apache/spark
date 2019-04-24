bfcf69bdba0d0b04b996c33dab394dc03e2030fa
qb-tarushg <tarush.grover@quantumblack.com>
2019-04-24 20:15:22 +0530

    committed method mean average precision at k

--- mllib/src/test/scala/org/apache/spark/mllib/evaluation/RankingMetricsSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/evaluation/RankingMetricsSuite.scala
@@ -46,6 +46,7 @@ class RankingMetricsSuite extends SparkFunSuite with MLlibTestSparkContext {
     assert(map ~== 0.355026 absTol eps)
 
     assert(metrics.meanAveragePrecisionAt(1) ~== 0.333334 absTol eps)
+
     assert(metrics.meanAveragePrecisionAt(2) ~== 0.25 absTol eps)
     assert(metrics.meanAveragePrecisionAt(3) ~== 0.24074 absTol eps)
 
