528b05a9f5109fb6b07213a99ab6e1879e4883eb
“attilapiros” <piros.attila.zsolt@gmail.com>
2019-04-30 23:23:01 +0200

    java checkstyle fix

--- common/network-shuffle/src/main/java/org/apache/spark/network/shuffle/BlockTransferClient.java
+++ common/network-shuffle/src/main/java/org/apache/spark/network/shuffle/BlockTransferClient.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package org.apache.spark.network.shuffle;
+package org.apache.spark.network.shuffle;
 
 public interface BlockTransferClient {
 
