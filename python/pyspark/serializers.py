d9bcacf94b93fe76542b5c1fd852559075ef6faa
Liang-Chi Hsieh <viirya@gmail.com>
2019-05-04 13:21:08 +0900

    [SPARK-27629][PYSPARK] Prevent Unpickler from intervening each unpickling
    ## What changes were proposed in this pull request?
    
    In SPARK-27612, one correctness issue was reported. When protocol 4 is used to pickle Python objects, we found that unpickled objects were wrong. A temporary fix was proposed by not using highest protocol.
    
    It was found that Opcodes.MEMOIZE was appeared in the opcodes in protocol 4. It is suspect to this issue.
    
    A deeper dive found that Opcodes.MEMOIZE stores objects into internal map of Unpickler object. We use single Unpickler object to unpickle serialized Python bytes. Stored objects intervenes next round of unpickling, if the map is not cleared.
    
    We has two options:
    
    1. Continues to reuse Unpickler, but calls its close after each unpickling.
    2. Not to reuse Unpickler and create new Unpickler object in each unpickling.
    
    This patch takes option 1.
    
    ## How was this patch tested?
    
    Passing the test added in SPARK-27612 (#24519).
    
    Closes #24521 from viirya/SPARK-27629.
    
    Authored-by: Liang-Chi Hsieh <viirya@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/serializers.py
+++ python/pyspark/serializers.py
@@ -62,12 +62,11 @@ import itertools
 if sys.version < '3':
     import cPickle as pickle
     from itertools import izip as zip, imap as map
-    pickle_protocol = 2
 else:
     import pickle
     basestring = unicode = str
     xrange = range
-    pickle_protocol = 3
+pickle_protocol = pickle.HIGHEST_PROTOCOL
 
 from pyspark import cloudpickle
 from pyspark.util import _exception_message
