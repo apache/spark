c3174e9aa3130bd6e79a195b2f1f08b78e951f05
Ivan Vergiliev <ivan.vergiliev@gmail.com>
2019-05-08 12:17:52 +0300

    Minor formatting and style changes

--- sql/core/v2.3.5/src/main/scala/org/apache/spark/sql/execution/datasources/orc/OrcFilters.scala
+++ sql/core/v2.3.5/src/main/scala/org/apache/spark/sql/execution/datasources/orc/OrcFilters.scala
@@ -251,9 +251,8 @@ private[sql] object OrcFilters extends OrcFiltersBase {
         } yield Or(lhs, rhs)
 
       case Not(child) =>
-        val filteredSubtree =
-          trimNonConvertibleSubtreesImpl(dataTypeMap, child, canPartialPushDownConjuncts = false)
-        filteredSubtree.map(Not(_))
+        trimNonConvertibleSubtreesImpl(dataTypeMap, child, canPartialPushDownConjuncts = false)
+            .map(Not)
 
       // NOTE: For all case branches dealing with leaf predicates below, the additional `startAnd()`
       // call is mandatory.  ORC `SearchArgument` builder requires that all leaf predicates must be
