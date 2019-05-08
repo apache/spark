8329e7debdaf6db9f3a52094bbc5dc4c1e2771ea
gengjiaan <gengjiaan@360.cn>
2019-05-08 11:43:03 +0800

    [SPARK-27649][SS] Unify the way use 'spark.network.timeout'
    ## What changes were proposed in this pull request?
    
    For historical reasons, structured streaming still has some old way of use
    `spark.network.timeout`
    , even though
    `org.apache.spark.internal.config.Network.NETWORK_TIMEOUT`
    is now available.
    
    ## How was this patch tested?
    Exists UT.
    
    Closes #24545 from beliefer/unify-spark-network-timeout.
    
    Authored-by: gengjiaan <gengjiaan@360.cn>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- external/kafka-0-10-sql/src/main/scala/org/apache/spark/sql/kafka010/KafkaRelation.scala
+++ external/kafka-0-10-sql/src/main/scala/org/apache/spark/sql/kafka010/KafkaRelation.scala
@@ -22,6 +22,7 @@ import java.util.UUID
 import org.apache.kafka.common.TopicPartition
 
 import org.apache.spark.internal.Logging
+import org.apache.spark.internal.config.Network.NETWORK_TIMEOUT
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.catalyst.InternalRow
@@ -47,9 +48,7 @@ private[kafka010] class KafkaRelation(
 
   private val pollTimeoutMs = sourceOptions.getOrElse(
     "kafkaConsumer.pollTimeoutMs",
-    (sqlContext.sparkContext.conf.getTimeAsSeconds(
-      "spark.network.timeout",
-      "120s") * 1000L).toString
+    (sqlContext.sparkContext.conf.get(NETWORK_TIMEOUT) * 1000L).toString
   ).toLong
 
   override def schema: StructType = KafkaOffsetReader.kafkaSchema
