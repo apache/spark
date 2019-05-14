e30c6ab34fabd112233c1e59871804dd9191e76f
Jose Torres <torres.joseph.f+github@gmail.com>
2019-05-14 12:33:52 -0700

    move to right place

--- python/pyspark/sql/tests/test_functions.py
+++ python/pyspark/sql/tests/test_functions.py
@@ -278,6 +278,22 @@ class FunctionsTests(ReusedSQLTestCase):
             df.select(df.name).orderBy(functions.desc_nulls_last('name')).collect(),
             [Row(name=u'Tom'), Row(name=u'Alice'), Row(name=None)])
 
+    def test_input_file_name_reset_for_rdd(self):
+        from pyspark.sql.functions import udf, input_file_name
+        rdd = self.sc.textFile('python/test_support/hello/hello.txt').map(lambda x: {'data': x})
+        df = self.spark.createDataFrame(rdd, StructType([StructField('data', StringType(), True)]))
+        df.select(input_file_name().alias('file')).collect()
+
+        non_file_df = self.spark.range(0, 100, 1, 100).select(input_file_name().alias('file'))
+
+        results = non_file_df.collect()
+        self.assertTrue(len(results) == 100)
+
+        # [SC-12160]: if everything was properly reset after the last job, this should return
+        # empty string rather than the file read in the last job.
+        for result in results:
+            assert(result[0] == '')
+
 
 if __name__ == "__main__":
     import unittest
