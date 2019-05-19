36faf8a2098282976712f302e222e35ac3aae11b
williamwong <william1104@gmail.com>
2019-05-19 22:53:30 +0800

    also remove the tryWithFinallyBlock function

--- sql/core/src/test/scala/org/apache/spark/sql/test/SQLTestUtils.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/test/SQLTestUtils.scala
@@ -308,33 +308,6 @@ private[sql] trait SQLTestUtilsBase
     }
   }
 
-  /**
-   * Executes the given tryBlock and then the given finallyBlock no matter whether tryBlock throws
-   * an exception. If both tryBlock and finallyBlock throw exceptions, the exception thrown
-   * from the finallyBlock with be added to the exception thrown from tryBlock as a
-   * suppress exception. It helps to avoid masking the exception from tryBlock with exception
-   * from finallyBlock
-   */
-  private def withFinallyBlock(tryBlock: => Unit)(finallyBlock: => Unit): Unit = {
-    var fromTryBlock : Throwable = null
-    try tryBlock catch {
-      case cause : Throwable =>
-        fromTryBlock = cause
-        throw cause
-    } finally {
-      if (fromTryBlock != null) {
-        try finallyBlock catch {
-          case fromFinallyBlock : Throwable =>
-            fromTryBlock.addSuppressed(fromFinallyBlock)
-            throw fromTryBlock
-        }
-      } else {
-        finallyBlock
-      }
-    }
-  }
-
-
 
   // Blocking uncache table for tests
   protected def uncacheTable(tableName: String): Unit = {
