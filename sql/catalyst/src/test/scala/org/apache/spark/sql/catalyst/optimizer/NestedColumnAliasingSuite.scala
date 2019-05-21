412401bfc8cff66b2b4d78256e8fbc8aebf029bd
Liang-Chi Hsieh <viirya@gmail.com>
2019-05-21 21:54:36 +0800

    Use DSL.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/NestedColumnAliasingSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/NestedColumnAliasingSuite.scala
@@ -225,14 +225,13 @@ class NestedColumnAliasingSuite extends SchemaPruningTest {
     val nestedRelation = LocalRelation('a.struct('b.struct('c.int,
       'd.struct('f.int, 'g.int), 'e.int)))
 
+    // `a.b`
     val first = GetStructField('a, 0, Some("b"))
-    val second = Add(
-      GetStructField(
-        GetStructField('a, 0, Some("b")), 0, Some("c")),
-      Literal(1))
-    val last = GetStructField(
-      GetStructField(
-        GetStructField('a, 0, Some("b")), 1, Some("d")), 0, Some("f"))
+    // `a.b.c` + 1
+    val second = GetStructField(GetStructField('a, 0, Some("b")), 0, Some("c")) + Literal(1)
+    // `a.b.d.f`
+    val last = GetStructField(GetStructField(
+      GetStructField('a, 0, Some("b")), 1, Some("d")), 0, Some("f"))
 
     val query = nestedRelation
       .limit(5)
@@ -247,11 +246,8 @@ class NestedColumnAliasingSuite extends SchemaPruningTest {
       .select(first.as(aliases(0)))
       .limit(5)
       .select($"${aliases(0)}".as("a.b"),
-        Add(
-          GetStructField($"${aliases(0)}", 0, Some("c")),
-          Literal(1)).as("(a.b.c + 1)"),
-        GetStructField(
-          GetStructField($"${aliases(0)}", 1, Some("d")), 0, Some("f")).as("a.b.d.f"))
+        (GetStructField($"${aliases(0)}", 0, Some("c")) + Literal(1)).as("(a.b.c + 1)"),
+        GetStructField(GetStructField($"${aliases(0)}", 1, Some("d")), 0, Some("f")).as("a.b.d.f"))
       .analyze
 
     comparePlans(optimized, expected)
