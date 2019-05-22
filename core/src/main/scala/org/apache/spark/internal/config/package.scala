2c89b426a0845e552400a757ec3853f52b8485e0
gengjiaan <gengjiaan@360.cn>
2019-05-22 17:27:02 +0800

    Fix Scala style.

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -64,8 +64,8 @@ package object config {
       " (e.g YARN and Kubernetes), in MiB unless otherwise specified." +
       "Note: These non-heap memory including off-heap memory (when spark.memory.offHeap.enabled=true)" +
       " and memory used by other non-driver processes running in the same container." +
-      "The maximum memory size of container to running driver is determined by the sum of" +
-      " `spark.driver.memoryOverhead` and `spark.driver.memory`.")
+      "The maximum memory size of container to running driver is determined by the sum of " +
+      "spark.driver.memoryOverhead and spark.driver.memory.")
     .bytesConf(ByteUnit.MiB)
     .createOptional
 
@@ -192,9 +192,9 @@ package object config {
     .doc("Amount of non-heap memory to be allocated per executor process in cluster mode " +
       "(e.g YARN and Kubernetes), in MiB unless otherwise specified." +
       "Note: These non-heap memory including off-heap memory (when spark.memory.offHeap.enabled=true)" +
-      " and memory used by other non-executor processes running in the same container."
-      "The maximum memory size of container to running executor is determined by the sum of" +
-      " `spark.executor.memoryOverhead` and `spark.executor.memory`.")
+      " and memory used by other non-executor processes running in the same container." +
+      "The maximum memory size of container to running executor is determined by the sum of " +
+      "spark.executor.memoryOverhead and spark.executor.memory.")
     .bytesConf(ByteUnit.MiB)
     .createOptional
 
