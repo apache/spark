4f79142f2579bb30713a5758e3fd0760122ba323
zhengruifeng <ruifengz@foxmail.com>
2019-05-23 18:58:29 +0800

    init

--- mllib/src/main/scala/org/apache/spark/mllib/evaluation/MultilabelMetrics.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/evaluation/MultilabelMetrics.scala
@@ -17,6 +17,8 @@
 
 package org.apache.spark.mllib.evaluation
 
+import scala.collection.mutable
+
 import org.apache.spark.annotation.Since
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
@@ -38,76 +40,62 @@ class MultilabelMetrics @Since("1.2.0") (predictionAndLabels: RDD[(Array[Double]
       (r.getSeq[Double](0).toArray, r.getSeq[Double](1).toArray)
     })
 
-  private lazy val numDocs: Long = predictionAndLabels.count()
+  /**
+   * Use MultilabelSummarizer to calculate all summary statistics of predictions
+   * and labels on one pass.
+   */
+  private lazy val summary: MultilabelSummary = {
+    predictionAndLabels
+      .treeAggregate(new MultilabelSummarizer)(
+        (summary, sample) => summary.add(sample._1, sample._2),
+        (sum1, sum2) => sum1.merge(sum2)
+      )
+  }
 
-  private lazy val numLabels: Long = predictionAndLabels.flatMap { case (_, labels) =>
-    labels}.distinct().count()
 
   /**
    * Returns subset accuracy
    * (for equal sets of labels)
    */
   @Since("1.2.0")
-  lazy val subsetAccuracy: Double = predictionAndLabels.filter { case (predictions, labels) =>
-    predictions.deep == labels.deep
-  }.count().toDouble / numDocs
+  lazy val subsetAccuracy: Double = summary.subsetAccuracy
 
   /**
    * Returns accuracy
    */
   @Since("1.2.0")
-  lazy val accuracy: Double = predictionAndLabels.map { case (predictions, labels) =>
-    labels.intersect(predictions).length.toDouble /
-      (labels.length + predictions.length - labels.intersect(predictions).length)}.sum / numDocs
+  lazy val accuracy: Double = summary.accuracy
 
 
   /**
    * Returns Hamming-loss
    */
   @Since("1.2.0")
-  lazy val hammingLoss: Double = predictionAndLabels.map { case (predictions, labels) =>
-    labels.length + predictions.length - 2 * labels.intersect(predictions).length
-  }.sum / (numDocs * numLabels)
+  lazy val hammingLoss: Double = summary.hammingLoss
 
   /**
    * Returns document-based precision averaged by the number of documents
    */
   @Since("1.2.0")
-  lazy val precision: Double = predictionAndLabels.map { case (predictions, labels) =>
-    if (predictions.length > 0) {
-      predictions.intersect(labels).length.toDouble / predictions.length
-    } else {
-      0
-    }
-  }.sum / numDocs
+  lazy val precision: Double = summary.precision
 
   /**
    * Returns document-based recall averaged by the number of documents
    */
   @Since("1.2.0")
-  lazy val recall: Double = predictionAndLabels.map { case (predictions, labels) =>
-    labels.intersect(predictions).length.toDouble / labels.length
-  }.sum / numDocs
+  lazy val recall: Double = summary.recall
 
   /**
    * Returns document-based f1-measure averaged by the number of documents
    */
   @Since("1.2.0")
-  lazy val f1Measure: Double = predictionAndLabels.map { case (predictions, labels) =>
-    2.0 * predictions.intersect(labels).length / (predictions.length + labels.length)
-  }.sum / numDocs
+  lazy val f1Measure: Double = summary.f1Measure
 
-  private lazy val tpPerClass = predictionAndLabels.flatMap { case (predictions, labels) =>
-    predictions.intersect(labels)
-  }.countByValue()
+  private lazy val tpPerClass = summary.tpPerClass
 
-  private lazy val fpPerClass = predictionAndLabels.flatMap { case (predictions, labels) =>
-    predictions.diff(labels)
-  }.countByValue()
+  private lazy val fpPerClass = summary.fpPerClass
 
-  private lazy val fnPerClass = predictionAndLabels.flatMap { case(predictions, labels) =>
-    labels.diff(predictions)
-  }.countByValue()
+  private lazy val fnPerClass = summary.fnPerClass
 
   /**
    * Returns precision for a given label (category)
@@ -178,4 +166,161 @@ class MultilabelMetrics @Since("1.2.0") (predictionAndLabels: RDD[(Array[Double]
    */
   @Since("1.2.0")
   lazy val labels: Array[Double] = tpPerClass.keys.toArray.sorted
+}
+
+
+/**
+ * Trait for statistical summary for multi-label metrics.
+ */
+private[evaluation] trait MultilabelSummary {
+
+  def numDocs: Long
+
+  def numLabels: Long
+
+  def subsetAccuracy: Double
+
+  def accuracy: Double
+
+  def hammingLoss: Double
+
+  def precision: Double
+
+  def recall: Double
+
+  def f1Measure: Double
+
+  def tpPerClass: Map[Double, Long]
+
+  def fpPerClass: Map[Double, Long]
+
+  def fnPerClass: Map[Double, Long]
+}
+
+
+private[evaluation] class MultilabelSummarizer extends MultilabelSummary with Serializable {
+
+  private var docCnt = 0L
+  private val labelSet = mutable.Set.empty[Double]
+  private var subsetAccuracyCnt = 0L
+  private var accuracySum = 0.0
+  private var hammingLossSum = 0L
+  private var precisionSum = 0.0
+  private var recallSum = 0.0
+  private var f1MeasureSum = 0.0
+  private val tpPerClass_ = mutable.Map.empty[Double, Long]
+  private val fpPerClass_ = mutable.Map.empty[Double, Long]
+  private val fnPerClass_ = mutable.Map.empty[Double, Long]
+
+  /**
+   * Add a new sample (predictions and labels) to this summarizer, and update
+   * the statistical summary.
+   *
+   * @return This MultilabelSummarizer object.
+   */
+  def add(predictions: Array[Double], labels: Array[Double]): this.type = {
+    val intersection = predictions.intersect(labels)
+
+    docCnt += 1L
+
+    labels.foreach(labelSet.add)
+
+    if (predictions.deep == labels.deep) {
+      subsetAccuracyCnt += 1
+    }
+
+    accuracySum += intersection.length.toDouble /
+      (labels.length + predictions.length - intersection.length)
+
+    hammingLossSum += labels.length + predictions.length - 2 * intersection.length
+
+    if (predictions.length > 0) {
+      precisionSum += intersection.length.toDouble / predictions.length
+    }
+
+    recallSum += intersection.length.toDouble / labels.length
+
+    f1MeasureSum += 2.0 * intersection.length / (predictions.length + labels.length)
+
+    intersection.foreach { k =>
+      val v = tpPerClass_.getOrElse(k, 0L)
+      tpPerClass_.update(k, v + 1)
+    }
+
+    predictions.diff(labels).foreach { k =>
+      val v = tpPerClass_.getOrElse(k, 0L)
+      fpPerClass_.update(k, v + 1)
+    }
+
+    labels.diff(predictions).foreach { k =>
+      val v = fnPerClass_.getOrElse(k, 0L)
+      fnPerClass_.update(k, v + 1)
+    }
+
+    this
+  }
+
+  /**
+   * Merge another MultilabelSummarizer, and update the statistical summary.
+   * (Note that it's in place merging; as a result, `this` object will be modified.)
+   *
+   * @param other The other MultilabelSummarizer to be merged.
+   * @return This MultilabelSummarizer object.
+   */
+  def merge(other: MultilabelSummarizer): this.type = {
+    docCnt += other.docCnt
+
+    other.labelSet.foreach(labelSet.add)
+
+    subsetAccuracyCnt += other.subsetAccuracyCnt
+
+    accuracySum += other.accuracySum
+
+    hammingLossSum += other.hammingLossSum
+
+    precisionSum += other.precisionSum
+
+    recallSum += other.recallSum
+
+    f1MeasureSum += other.f1MeasureSum
+
+    other.tpPerClass_.foreach { case (k, v1) =>
+      val v0 = tpPerClass_.getOrElse(k, 0L)
+      tpPerClass_.update(k, v0 + v1)
+    }
+
+    other.fpPerClass_.foreach { case (k, v1) =>
+      val v0 = fpPerClass_.getOrElse(k, 0L)
+      fpPerClass_.update(k, v0 + v1)
+    }
+
+    other.fnPerClass_.foreach { case (k, v1) =>
+      val v0 = fnPerClass_.getOrElse(k, 0L)
+      fnPerClass_.update(k, v0 + v1)
+    }
+
+    this
+  }
+
+  override def numDocs: Long = docCnt
+
+  override def numLabels: Long = labelSet.size.toLong
+
+  override def subsetAccuracy: Double = subsetAccuracyCnt.toDouble / numDocs
+
+  override def accuracy: Double = accuracySum / numDocs
+
+  override def hammingLoss: Double = hammingLossSum.toDouble / numDocs / numLabels
+
+  override def precision: Double = precisionSum / numDocs
+
+  override def recall: Double = recallSum / numDocs
+
+  override def f1Measure: Double = f1MeasureSum / numDocs
+
+  override def tpPerClass: Map[Double, Long] = tpPerClass_.toMap
+
+  override def fpPerClass: Map[Double, Long] = fpPerClass_.toMap
+
+  override def fnPerClass: Map[Double, Long] = fnPerClass_.toMap
 }
