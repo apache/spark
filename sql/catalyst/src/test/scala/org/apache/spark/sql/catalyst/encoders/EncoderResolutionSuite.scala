c520108e2e5b79d0508333675c9a9ef1bcd38e01
Gengliang Wang <gengliang.wang@databricks.com>
2019-06-06 13:14:53 +0800

    fix EncoderResolutionSuite.scala

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/encoders/EncoderResolutionSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/encoders/EncoderResolutionSuite.scala
@@ -230,12 +230,12 @@ class EncoderResolutionSuite extends PlanTest {
   castSuccess[Long, String]
   castSuccess[Int, java.math.BigDecimal]
   castSuccess[Long, java.math.BigDecimal]
-  castSuccess[java.math.BigDecimal, Double]
+  castSuccess[Double, java.math.BigDecimal]
 
   castFail[Long, Int]
   castFail[java.sql.Timestamp, java.sql.Date]
   castFail[java.math.BigDecimal, Long]
-  castFail[Double, java.math.BigDecimal]
+  castFail[java.math.BigDecimal, Double]
   castFail[java.math.BigDecimal, Int]
   castFail[String, Long]
 
