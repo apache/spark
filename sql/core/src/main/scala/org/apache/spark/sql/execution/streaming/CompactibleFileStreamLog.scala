bd0a04baab0fe30da8621e35eb69bbdcf84acbf1
Jungtaek Lim (HeartSaVioR) <kabhwan@gmail.com>
2019-06-14 20:34:18 -0700

    [SPARK-26949][SS] Prevent 'purge' to remove needed batch files in CompactibleFileStreamLog
    ## What changes were proposed in this pull request?
    
    This patch proposes making `purge` in `CompactibleFileStreamLog` to throw `UnsupportedOperationException` to prevent purging necessary batch files, as well as adding javadoc to document its behavior. Actually it would only break when latest compaction batch is requested to be purged, but caller wouldn't be aware of this so safer to just prevent it.
    
    ## How was this patch tested?
    
    Added UT.
    
    Closes #23850 from HeartSaVioR/SPARK-26949.
    
    Authored-by: Jungtaek Lim (HeartSaVioR) <kabhwan@gmail.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/CompactibleFileStreamLog.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/CompactibleFileStreamLog.scala
@@ -163,6 +163,16 @@ abstract class CompactibleFileStreamLog[T <: AnyRef : ClassTag](
   }
 
   /**
+   * CompactibleFileStreamLog maintains logs by itself, and manual purging might break internal
+   * state, specifically which latest compaction batch is purged.
+   *
+   * To simplify the situation, this method just throws UnsupportedOperationException regardless
+   * of given parameter, and let CompactibleFileStreamLog handles purging by itself.
+   */
+  override def purge(thresholdBatchId: Long): Unit = throw new UnsupportedOperationException(
+    s"Cannot purge as it might break internal state.")
+
+  /**
    * Compacts all logs before `batchId` plus the provided `logs`, and writes them into the
    * corresponding `batchId` file. It will delete expired files as well if enabled.
    */
