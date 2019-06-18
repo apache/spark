dc53e83726e2329d6a6f882ed54b7a25e7fd519f
Robert (Bobby) Evans <bobby@apache.org>
2019-06-18 13:56:58 -0500

    Addressed review comments

--- sql/core/src/test/scala/org/apache/spark/sql/SparkSessionExtensionSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SparkSessionExtensionSuite.scala
@@ -336,52 +336,36 @@ case class NoCloseColumnVector(wrapped: ColumnVector) extends ColumnVector(wrapp
     // Empty
   }
 
-
   override def hasNull: Boolean = wrapped.hasNull
-
 
   override def numNulls(): Int = wrapped.numNulls
 
-
   override def isNullAt(rowId: Int): Boolean = wrapped.isNullAt(rowId)
-
 
   override def getBoolean(rowId: Int): Boolean = wrapped.getBoolean(rowId)
 
-
   override def getByte(rowId: Int): Byte = wrapped.getByte(rowId)
-
 
   override def getShort(rowId: Int): Short = wrapped.getShort(rowId)
 
-
   override def getInt(rowId: Int): Int = wrapped.getInt(rowId)
-
 
   override def getLong(rowId: Int): Long = wrapped.getLong(rowId)
 
-
   override def getFloat(rowId: Int): Float = wrapped.getFloat(rowId)
-
 
   override def getDouble(rowId: Int): Double = wrapped.getDouble(rowId)
 
-
   override def getArray(rowId: Int): ColumnarArray = wrapped.getArray(rowId)
 
-
   override def getMap(ordinal: Int): ColumnarMap = wrapped.getMap(ordinal)
-
 
   override def getDecimal(rowId: Int, precision: Int, scale: Int): Decimal =
     wrapped.getDecimal(rowId, precision, scale)
 
-
   override def getUTF8String(rowId: Int): UTF8String = wrapped.getUTF8String(rowId)
 
-
   override def getBinary(rowId: Int): Array[Byte] = wrapped.getBinary(rowId)
-
 
   override protected def getChild(ordinal: Int): ColumnVector = wrapped.getChild(ordinal)
 }
@@ -418,7 +402,6 @@ trait ColumnarExpression extends Expression with Serializable {
   }
 
   override def hashCode(): Int = super.hashCode()
-
 }
 
 object ColumnarBindReferences extends Logging {
