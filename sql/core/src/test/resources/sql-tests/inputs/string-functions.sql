754b228f695853f2f4fdf9255797747665180d92
Yuming Wang <yumwang@ebay.com>
2019-06-18 13:28:29 -0700

    [SPARK-28093][SQL] Fix TRIM/LTRIM/RTRIM function parameter order issue
    ## What changes were proposed in this pull request?
    
    This pr fix `TRIM`/`LTRIM`/`RTRIM` function parameter order issue, otherwise:
    
    ```sql
    spark-sql> SELECT trim('yxTomxx', 'xyz'), trim('xxxbarxxx', 'x');
    z
    spark-sql> SELECT ltrim('zzzytest', 'xyz'), ltrim('xyxXxyLAST WORD', 'xy');
    xyz
    spark-sql> SELECT rtrim('testxxzx', 'xyz'), rtrim('TURNERyxXxy', 'xy');
    xy
    spark-sql>
    ```
    
    ```sql
    postgres=# SELECT trim('yxTomxx', 'xyz'), trim('xxxbarxxx', 'x');
     btrim | btrim
    -------+-------
     Tom   | bar
    (1 row)
    
    postgres=# SELECT ltrim('zzzytest', 'xyz'), ltrim('xyxXxyLAST WORD', 'xy');
     ltrim |    ltrim
    -------+--------------
     test  | XxyLAST WORD
    (1 row)
    
    postgres=# SELECT rtrim('testxxzx', 'xyz'), rtrim('TURNERyxXxy', 'xy');
     rtrim |   rtrim
    -------+-----------
     test  | TURNERyxX
    (1 row)
    ```
    
    ## How was this patch tested?
    
    unit tests
    
    Closes #24902 from wangyum/SPARK-28093.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
@@ -38,3 +38,13 @@ SELECT substring('Spark SQL', 5, 1);
 SELECT substring('Spark SQL' from 5);
 SELECT substring('Spark SQL' from -3);
 SELECT substring('Spark SQL' from 5 for 1);
+
+-- trim/ltrim/rtrim
+SELECT trim('yxTomxx', 'xyz');
+SELECT trim('xxxbarxxx', 'x');
+SELECT ltrim('zzzytest', 'xyz');
+SELECT ltrim('zzzytestxyz', 'xyz');
+SELECT ltrim('xyxXxyLAST WORD', 'xy');
+SELECT rtrim('testxxzx', 'xyz');
+SELECT rtrim('xyztestxxzx', 'xyz');
+SELECT rtrim('TURNERyxXxy', 'xy');
