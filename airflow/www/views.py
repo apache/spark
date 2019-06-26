82b7b8fc5d5769efb56131eb8a847c92f3f812ef
James Meickle <jmeickle@quantopian.com>
2019-06-26 05:49:41 -0400

    [AIRFLOW-3217] Button to toggle line wrapping in log and code views  (#4277)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -450,7 +450,8 @@ class Airflow(AirflowBaseView):
         return self.render_template(
             'airflow/dag_code.html', html_code=html_code, dag=dag, title=dag_id,
             root=request.args.get('root'),
-            demo_mode=conf.getboolean('webserver', 'demo_mode'))
+            demo_mode=conf.getboolean('webserver', 'demo_mode'),
+            wrapped=conf.getboolean('webserver', 'default_wrap'))
 
     @expose('/dag_details')
     @has_dag_access(can_dag_read=True)
@@ -645,7 +646,7 @@ class Airflow(AirflowBaseView):
             logs=logs, dag=dag_model, title="Log by attempts",
             dag_id=dag_id, task_id=task_id,
             execution_date=execution_date, form=form,
-            root=root)
+            root=root, wrapped=conf.getboolean('webserver', 'default_wrap'))
 
     @expose('/elasticsearch')
     @has_dag_access(can_dag_read=True)
