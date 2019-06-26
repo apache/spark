dbf97e858a803957e0c8e8cd879aa35c0757abd2
Angers <angers.zhu@gamil.com>
2019-06-27 00:15:29 +0800

    for check scala import style

--- sql/core/src/main/scala/org/apache/spark/sql/internal/SessionState.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/internal/SessionState.scala
@@ -21,6 +21,7 @@ import java.io.File
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
+
 import org.apache.spark.SparkContext
 import org.apache.spark.annotation.{Experimental, Unstable}
 import org.apache.spark.sql._
