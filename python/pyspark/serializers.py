63f98370f6316bb020618ff02a82986c57a7a715
WeichenXu <weichen.xu@databricks.com>
2019-06-27 19:58:49 +0800

    init pr

--- python/pyspark/serializers.py
+++ python/pyspark/serializers.py
@@ -68,6 +68,8 @@ else:
     xrange = range
 pickle_protocol = pickle.HIGHEST_PROTOCOL
 
+from types import GeneratorType
+
 from pyspark import cloudpickle
 from pyspark.util import _exception_message
 
@@ -235,6 +237,12 @@ class ArrowStreamSerializer(Serializer):
                 if writer is None:
                     writer = pa.RecordBatchStreamWriter(stream, batch.schema)
                 writer.write_batch(batch)
+        except BaseException as be:
+            if isinstance(iterator, GeneratorType):
+                # if the iterator is a generator, it may include some code for
+                # closing resources.
+                iterator.close()
+            raise be
         finally:
             if writer is not None:
                 writer.close()
