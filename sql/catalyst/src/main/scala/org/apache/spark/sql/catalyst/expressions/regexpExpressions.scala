a0ceae1fca85723d26db1469141b48c81b19dbf4
gengjiaan <gengjiaan@360.cn>
2019-07-04 18:31:12 +0800

    Optimize code and update TableIdentifierParserSuite.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/regexpExpressions.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/regexpExpressions.scala
@@ -152,7 +152,7 @@ case class Like(
     } else {
       val pattern = ctx.freshName("pattern")
       val rightStr = ctx.freshName("rightStr")
-      val escapeChar = escapeCharOpt.getOrElse("\\\\")
+      val escapeChar = escapeCharOpt.getOrElse("\\\\\\\\")
       nullSafeCodeGen(ctx, ev, (eval1, eval2) => {
         s"""
           String $rightStr = $eval2.toString();
