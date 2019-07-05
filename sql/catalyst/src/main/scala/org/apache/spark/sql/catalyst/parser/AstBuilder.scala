4b1fed88f08cfa803b240fc2c072023d5ed50e7a
gengjiaan <gengjiaan@360.cn>
2019-07-05 10:41:05 +0800

    Optimize code.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilder.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilder.scala
@@ -1240,7 +1240,7 @@ class AstBuilder(conf: SQLConf) extends SqlBaseBaseVisitor[AnyRef] with Logging 
       case SqlBaseParser.IN =>
         invertIfNotDefined(In(e, ctx.expression.asScala.map(expression)))
       case SqlBaseParser.LIKE =>
-        val escapeOpt = Option(ctx.escapeChar).map({ str =>
+        val escapeOpt = Option(ctx.escapeChar).map{ str =>
           if (str.getText.length() > 1) {
             throw new ParseException("Invalid escape string." +
               "Escape string must be empty or one character.", ctx)
