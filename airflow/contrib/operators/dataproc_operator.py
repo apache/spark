fb93df142bafbfbb59dcb42a2d80932a4f2e2b06
eladkal <45845474+eladkal@users.noreply.github.com>
2019-07-11 15:52:46 +0300

    [AIRFLOW-3495] Validate one of query and query_uri passed to DataProcSparkSqlOperator (#5510)
    DataProcSparkSqlOperator and DataProcHiveOperator are working either with query
    or query_uri. Passing both doesn't make sense so check at construction time.

--- airflow/contrib/operators/dataproc_operator.py
+++ airflow/contrib/operators/dataproc_operator.py
@@ -808,6 +808,8 @@ class DataProcHiveOperator(DataProcJobBaseOperator):
         self.query = query
         self.query_uri = query_uri
         self.variables = variables
+        if self.query is not None and self.query_uri is not None:
+            raise AirflowException('Only one of `query` and `query_uri` can be passed.')
 
     def execute(self, context):
         self.create_job_template()
@@ -850,6 +852,8 @@ class DataProcSparkSqlOperator(DataProcJobBaseOperator):
         self.query = query
         self.query_uri = query_uri
         self.variables = variables
+        if self.query is not None and self.query_uri is not None:
+            raise AirflowException('Only one of `query` and `query_uri` can be passed.')
 
     def execute(self, context):
         self.create_job_template()
