560aa43736dcf85f7e49391ce8fe0a8da8d30f69
Liang-Chi Hsieh <viirya@gmail.com>
2019-07-12 17:37:47 +0800

    Remove usage of cgi.escape.

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -375,7 +375,12 @@ class DataFrame(object):
         by 'spark.sql.repl.eagerEval.enabled', this only called by REPL you are
         using support eager evaluation with HTML.
         """
-        import cgi
+        try:
+            from html import escape as html_escape
+        except ImportError:
+            # Python 2
+            from cgi import escape as html_escape
+
         if not self._support_repr_html:
             self._support_repr_html = True
         if self.sql_ctx._conf.isReplEagerEvalEnabled():
@@ -390,11 +395,11 @@ class DataFrame(object):
 
             html = "<table border='1'>\n"
             # generate table head
-            html += "<tr><th>%s</th></tr>\n" % "</th><th>".join(map(lambda x: cgi.escape(x), head))
+            html += "<tr><th>%s</th></tr>\n" % "</th><th>".join(map(lambda x: html_escape(x), head))
             # generate table rows
             for row in row_data:
                 html += "<tr><td>%s</td></tr>\n" % "</td><td>".join(
-                    map(lambda x: cgi.escape(x), row))
+                    map(lambda x: html_escape(x), row))
             html += "</table>\n"
             if has_more_data:
                 html += "only showing top %d %s\n" % (
