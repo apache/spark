c3ae8c82e345d31320a1b754985d685259375861
herman <herman@databricks.com>
2019-07-16 23:10:41 +0200

    Fix planner suite.

--- sql/core/src/test/scala/org/apache/spark/sql/execution/PlannerSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/PlannerSuite.scala
@@ -712,9 +712,9 @@ class PlannerSuite extends SharedSQLContext {
     outputPlan match {
       case SortMergeJoinExec(leftKeys, rightKeys, _, _,
              SortExec(_, _,
-               ShuffleExchangeExec(HashPartitioning(leftPartitioningExpressions, _), _), _),
+               ShuffleExchangeExec(HashPartitioning(leftPartitioningExpressions, _), _, _), _),
              SortExec(_, _,
-               ShuffleExchangeExec(HashPartitioning(rightPartitioningExpressions, _), _), _)) =>
+               ShuffleExchangeExec(HashPartitioning(rightPartitioningExpressions, _), _, _), _)) =>
         assert(leftKeys === smjExec.leftKeys)
         assert(rightKeys === smjExec.rightKeys)
         assert(leftKeys === leftPartitioningExpressions)
