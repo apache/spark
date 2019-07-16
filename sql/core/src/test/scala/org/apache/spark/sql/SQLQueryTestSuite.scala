71882f119e72934a02d4c177f0d52c785e2df79f
Yuming Wang <yumwang@ebay.com>
2019-07-16 08:46:01 -0700

    [SPARK-28343][FOLLOW-UP][SQL][TEST] Enable spark.sql.function.preferIntegralDivision for PostgreSQL testing
    ## What changes were proposed in this pull request?
    
    This PR enables `spark.sql.function.preferIntegralDivision` for PostgreSQL testing.
    
    ## How was this patch tested?
    
    N/A
    
    Closes #25170 from wangyum/SPARK-28343-2.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestSuite.scala
@@ -293,6 +293,7 @@ class SQLQueryTestSuite extends QueryTest with SharedSQLContext {
         // PostgreSQL enabled cartesian product by default.
         localSparkSession.conf.set(SQLConf.CROSS_JOINS_ENABLED.key, true)
         localSparkSession.conf.set(SQLConf.ANSI_SQL_PARSER.key, true)
+        localSparkSession.conf.set(SQLConf.PREFER_INTEGRAL_DIVISION.key, true)
       case _ =>
     }
 
