42cf16bd46a6e33226540516b0344c52e543e57e
Terry Kim <yuminkim@gmail.com>
2019-07-18 00:02:18 -0700

    Address PR comments

--- sql/core/src/test/resources/sql-tests/inputs/udf/udf-intersect-all.sql
+++ sql/core/src/test/resources/sql-tests/inputs/udf/udf-intersect-all.sql
@@ -1,6 +1,4 @@
 -- This test file was converted from intersect-all.sql.
--- Note that currently registered UDF returns a string. So there are some differences, for instance
--- in string cast within UDF in Scala and Python.
 
 CREATE TEMPORARY VIEW tab1 AS SELECT * FROM VALUES
     (1, 2), 
@@ -21,32 +19,32 @@ CREATE TEMPORARY VIEW tab2 AS SELECT * FROM VALUES
     AS tab2(k, v);
 
 -- Basic INTERSECT ALL
-SELECT * FROM tab1
+SELECT udf(k), v FROM tab1
 INTERSECT ALL
-SELECT * FROM tab2;
+SELECT k, udf(v) FROM tab2;
 
 -- INTERSECT ALL same table in both branches
-SELECT * FROM tab1
+SELECT k, udf(v) FROM tab1
 INTERSECT ALL
-SELECT * FROM tab1 WHERE udf(k) = 1;
+SELECT udf(k), v FROM tab1 WHERE udf(k) = 1;
 
 -- Empty left relation
-SELECT * FROM tab1 WHERE k > udf(2)
+SELECT udf(k), udf(v) FROM tab1 WHERE k > udf(2)
 INTERSECT ALL
-SELECT * FROM tab2;
+SELECT udf(k), udf(v) FROM tab2;
 
 -- Empty right relation
-SELECT * FROM tab1
+SELECT udf(k), v FROM tab1
 INTERSECT ALL
-SELECT * FROM tab2 WHERE CAST(udf(k) AS BIGINT) > CAST(udf(3) AS BIGINT);
+SELECT udf(k), v FROM tab2 WHERE udf(udf(k)) > 3;
 
 -- Type Coerced INTERSECT ALL
-SELECT * FROM tab1
+SELECT udf(k), v FROM tab1
 INTERSECT ALL
 SELECT CAST(udf(1) AS BIGINT), CAST(udf(2) AS BIGINT);
 
 -- Error as types of two side are not compatible
-SELECT * FROM tab1
+SELECT k, udf(v) FROM tab1
 INTERSECT ALL
 SELECT array(1), udf(2);
 
@@ -56,45 +54,45 @@ INTERSECT ALL
 SELECT udf(k), udf(v) FROM tab2;
 
 -- Basic
-SELECT * FROM tab2
+SELECT udf(k), v FROM tab2
 INTERSECT ALL
-SELECT * FROM tab1
+SELECT k, udf(v) FROM tab1
 INTERSECT ALL
-SELECT * FROM tab2;
+SELECT udf(k), udf(v) FROM tab2;
 
 -- Chain of different `set operations
-SELECT * FROM tab1
+SELECT udf(k), v FROM tab1
 EXCEPT
-SELECT * FROM tab2
+SELECT k, udf(v) FROM tab2
 UNION ALL
-SELECT * FROM tab1
+SELECT k, udf(udf(v)) FROM tab1
 INTERSECT ALL
-SELECT * FROM tab2
+SELECT udf(k), v FROM tab2
 ;
 
 -- Chain of different `set operations
-SELECT * FROM tab1
+SELECT udf(k), udf(v) FROM tab1
 EXCEPT
-SELECT * FROM tab2
+SELECT udf(k), v FROM tab2
 EXCEPT
-SELECT * FROM tab1
+SELECT k, udf(v) FROM tab1
 INTERSECT ALL
-SELECT * FROM tab2
+SELECT udf(k), udf(udf(v)) FROM tab2
 ;
 
 -- test use parenthesis to control order of evaluation
 (
   (
     (
-      SELECT * FROM tab1
+      SELECT udf(k), v FROM tab1
       EXCEPT
-      SELECT * FROM tab2
+      SELECT k, udf(v) FROM tab2
     )
     EXCEPT
-    SELECT * FROM tab1
+    SELECT udf(k), udf(v) FROM tab1
   )
   INTERSECT ALL
-  SELECT * FROM tab2
+  SELECT udf(k), udf(v) FROM tab2
 )
 ;
 
@@ -104,14 +102,14 @@ FROM   (SELECT udf(tab1.k),
                udf(tab2.v)
         FROM   tab1 
                JOIN tab2 
-                 ON CAST(udf(tab1.k) AS BIGINT) = CAST(udf(tab2.k) AS BIGINT))
+                 ON udf(udf(tab1.k)) = tab2.k)
 INTERSECT ALL 
 SELECT * 
 FROM   (SELECT udf(tab1.k),
                udf(tab2.v)
         FROM   tab1 
                JOIN tab2 
-                 ON CAST(udf(tab1.k) AS BIGINT) = CAST(udf(tab2.k) AS BIGINT));
+                 ON udf(tab1.k) = udf(udf(tab2.k)));
 
 -- Join under intersect all (2)
 SELECT * 
@@ -119,19 +117,19 @@ FROM   (SELECT udf(tab1.k),
                udf(tab2.v)
         FROM   tab1 
                JOIN tab2 
-                 ON CAST(udf(tab1.k) AS BIGINT) = CAST(udf(tab2.k) AS BIGINT))
+                 ON udf(tab1.k) = udf(tab2.k))
 INTERSECT ALL 
 SELECT * 
 FROM   (SELECT udf(tab2.v) AS k,
                udf(tab1.k) AS v
         FROM   tab1 
                JOIN tab2 
-                 ON CAST(udf(tab1.k) AS BIGINT) = CAST(udf(tab2.k) AS BIGINT));
+                 ON tab1.k = udf(tab2.k));
 
 -- Group by under intersect all
-SELECT CAST(udf(v) AS BIGINT) FROM tab1 GROUP BY v
+SELECT udf(v) FROM tab1 GROUP BY v
 INTERSECT ALL
-SELECT CAST(udf(k) AS BIGINT) FROM tab2 GROUP BY k;
+SELECT udf(udf(k)) FROM tab2 GROUP BY k;
 
 -- Test pre spark2.4 behaviour of set operation precedence
 -- All the set operators are given equal precedence and are evaluated
@@ -140,21 +138,21 @@ SELECT CAST(udf(k) AS BIGINT) FROM tab2 GROUP BY k;
 -- Set the property
 SET spark.sql.legacy.setopsPrecedence.enabled= true;
 
-SELECT * FROM tab1
+SELECT udf(k), v FROM tab1
 EXCEPT
-SELECT * FROM tab2
+SELECT k, udf(v) FROM tab2
 UNION ALL
-SELECT * FROM tab1
+SELECT udf(k), udf(v) FROM tab1
 INTERSECT ALL
-SELECT * FROM tab2;
+SELECT udf(udf(k)), udf(v) FROM tab2;
 
-SELECT * FROM tab1
+SELECT k, udf(v) FROM tab1
 EXCEPT
-SELECT * FROM tab2
+SELECT udf(k), v FROM tab2
 UNION ALL
-SELECT * FROM tab1
+SELECT udf(k), udf(v) FROM tab1
 INTERSECT
-SELECT * FROM tab2;
+SELECT udf(k), udf(udf(v)) FROM tab2;
 
 -- Restore the property
 SET spark.sql.legacy.setopsPrecedence.enabled = false;
