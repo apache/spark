b3293fcb37f9582b721e24de1f22eb215cf82ad8
Liang-Chi Hsieh <viirya@gmail.com>
2019-07-21 10:44:50 +0800

    fix error when PythonUDF is used in group by.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/PythonUDF.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/PythonUDF.scala
@@ -67,4 +67,12 @@ case class PythonUDF(
     exprId = resultId)
 
   override def nullable: Boolean = true
+
+  /**
+   * `resultId` can be seen as cosmetic variation in PythonUDF, as it doesn't affect the result.
+   */
+  override lazy val canonicalized: Expression = {
+    val canonicalizedChildren = children.map(_.canonicalized)
+    Canonicalize.execute(this.copy(resultId = ExprId(-1)).withNewChildren(canonicalizedChildren))
+  }
 }
--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -2189,4 +2189,46 @@ class DataFrameSuite extends QueryTest with SharedSQLContext {
           |*(1) Range (0, 10, step=1, splits=2)""".stripMargin))
     }
   }
+
+  test("SPARK-28445: PythonUDF in grouping key and aggregate expressions") {
+    import IntegratedUDFTestUtils._
+
+    val scalaTestUDF = TestScalaUDF(name = "scalaUDF")
+    registerTestUDF(scalaTestUDF, spark)
+
+    val pythonTestUDF = TestPythonUDF(name = "pyUDF")
+    registerTestUDF(pythonTestUDF, spark)
+
+    withTempView("testData") {
+      sql(
+        """CREATE OR REPLACE TEMPORARY VIEW testData AS
+          |SELECT * FROM VALUES
+          |(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (null, 1), (3, null), (null, null)
+          |AS testData(a, b)""".stripMargin)
+
+      val df = sql("SELECT scalaUDF(a + 1), scalaUDF(COUNT(b)) FROM testData " +
+        "GROUP BY scalaUDF(a + 1)")
+      val df2 = sql("SELECT pyUDF(a + 1), pyUDF(COUNT(b)) FROM testData GROUP BY pyUDF(a + 1)")
+      checkAnswer(df, df2)
+
+      val df3 = sql("SELECT scalaUDF(a + 1) + 1, scalaUDF(COUNT(b)) FROM testData " +
+        "GROUP BY scalaUDF(a + 1)")
+      val df4 = sql("SELECT pyUDF(a + 1) + 1, pyUDF(COUNT(b)) FROM testData GROUP BY pyUDF(a + 1)")
+      checkAnswer(df3, df4)
+
+      // PythonUDF in aggregate expression has grouping key in its arguments.
+      val df5 = sql("SELECT scalaUDF(scalaUDF(a + 1)), scalaUDF(COUNT(b)) FROM testData " +
+        "GROUP BY scalaUDF(a + 1)")
+      val df6 = sql("SELECT pyUDF(pyUDF(a + 1)), pyUDF(COUNT(b)) FROM testData " +
+        "GROUP BY pyUDF(a + 1)")
+      checkAnswer(df5, df6)
+
+      // PythonUDF over grouping key is argument to aggregate function.
+      val df7 = sql("SELECT scalaUDF(scalaUDF(a + 1)), scalaUDF(COUNT(scalaUDF(a + 1))) " +
+        "FROM testData GROUP BY scalaUDF(a + 1)")
+      val df8 = sql("SELECT pyUDF(pyUDF(a + 1)), pyUDF(COUNT(pyUDF(a + 1))) " +
+        "FROM testData GROUP BY pyUDF(a + 1)")
+      checkAnswer(df7, df8)
+    }
+  }
 }
