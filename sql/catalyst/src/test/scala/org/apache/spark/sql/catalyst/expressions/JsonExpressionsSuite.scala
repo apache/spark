022667cea666190bea651a3873234700a472326c
Yuming Wang <yumwang@ebay.com>
2019-07-22 20:53:59 -0700

    [SPARK-28469][SQL] Change CalendarIntervalType's readable string representation from calendarinterval to interval
    ## What changes were proposed in this pull request?
    
    This PR change `CalendarIntervalType`'s readable string representation from `calendarinterval` to `interval`.
    
    ## How was this patch tested?
    
    Existing UT
    
    Closes #25225 from wangyum/SPARK-28469.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/JsonExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/JsonExpressionsSuite.scala
@@ -696,7 +696,7 @@ class JsonExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper with 
     val struct2 = Literal.create(null, schema2)
     StructsToJson(Map.empty, struct2, gmtId).checkInputDataTypes() match {
       case TypeCheckResult.TypeCheckFailure(msg) =>
-        assert(msg.contains("Unable to convert column a of type calendarinterval to JSON"))
+        assert(msg.contains("Unable to convert column a of type interval to JSON"))
       case _ => fail("from_json should not work on interval map value type.")
     }
   }
