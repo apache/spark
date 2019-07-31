a55e757523d0736faef5c75cfdc97e7ea8e5f99b
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-07-31 12:02:27 +0100

    [AIRFLOW-2891] Make DockerOperator container_name be templateable (#5696)

--- airflow/operators/docker_operator.py
+++ airflow/operators/docker_operator.py
@@ -51,8 +51,8 @@ class DockerOperator(BaseOperator):
     :type api_version: str
     :param command: Command to be run in the container. (templated)
     :type command: str or list
-    :param container_name: Name of the container.
-    :type container_name: str
+    :param container_name: Name of the container. Optional (templated)
+    :type container_name: str or None
     :param cpus: Number of CPUs to assign to the container.
         This value gets multiplied with 1024. See
         https://docs.docker.com/engine/reference/run/#cpu-share-constraint
@@ -116,7 +116,7 @@ class DockerOperator(BaseOperator):
         greater than 0. If omitted uses system default.
     :type shm_size: int
     """
-    template_fields = ('command', 'environment',)
+    template_fields = ('command', 'environment', 'container_name')
     template_ext = ('.sh', '.bash',)
 
     @apply_defaults
