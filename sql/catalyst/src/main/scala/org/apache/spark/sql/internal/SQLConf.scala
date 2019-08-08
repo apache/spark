3586cdd24d9f5cb7d3f642a3da6a26ced1f88cea
Yuming Wang <yumwang@ebay.com>
2019-08-08 10:42:24 +0900

    [SPARK-28395][FOLLOW-UP][SQL] Make spark.sql.function.preferIntegralDivision internal
    ## What changes were proposed in this pull request?
    
    This PR makes `spark.sql.function.preferIntegralDivision` to internal configuration because it is only used for PostgreSQL test cases.
    
    More details:
    https://github.com/apache/spark/pull/25158#discussion_r309764541
    
    ## How was this patch tested?
    
    N/A
    
    Closes #25376 from wangyum/SPARK-28395-2.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1532,8 +1532,9 @@ object SQLConf {
     .createWithDefault(false)
 
   val PREFER_INTEGRAL_DIVISION = buildConf("spark.sql.function.preferIntegralDivision")
+    .internal()
     .doc("When true, will perform integral division with the / operator " +
-      "if both sides are integral types.")
+      "if both sides are integral types. This is for PostgreSQL test cases only.")
     .booleanConf
     .createWithDefault(false)
 
