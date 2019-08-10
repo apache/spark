10071b6aba89b0bf5e39e9daebc907bbffdd284d
Yuming Wang <yumwang@ebay.com>
2019-08-10 16:45:59 -0700

    [SPARK-28675][SQL] Remove maskCredentials and use redactOptions
    ## What changes were proposed in this pull request?
    
    This PR replaces `CatalogUtils.maskCredentials` with `SQLConf.get.redactOptions` to match other redacts.
    
    ## How was this patch tested?
    
    unit test and manual tests:
    Before this PR:
    ```sql
    spark-sql> DESC EXTENDED test_spark_28675;
    id	int	NULL
    
    # Detailed Table Information
    Database	default
    Table	test_spark_28675
    Owner	root
    Created Time	Fri Aug 09 08:23:17 GMT-07:00 2019
    Last Access	Wed Dec 31 17:00:00 GMT-07:00 1969
    Created By	Spark 3.0.0-SNAPSHOT
    Type	MANAGED
    Provider	org.apache.spark.sql.jdbc
    Location	file:/user/hive/warehouse/test_spark_28675
    Serde Library	org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe
    InputFormat	org.apache.hadoop.mapred.SequenceFileInputFormat
    OutputFormat	org.apache.hadoop.hive.ql.io.HiveSequenceFileOutputFormat
    Storage Properties	[url=###, driver=com.mysql.jdbc.Driver, dbtable=test_spark_28675]
    
    spark-sql> SHOW TABLE EXTENDED LIKE 'test_spark_28675';
    default	test_spark_28675	false	Database: default
    Table: test_spark_28675
    Owner: root
    Created Time: Fri Aug 09 08:23:17 GMT-07:00 2019
    Last Access: Wed Dec 31 17:00:00 GMT-07:00 1969
    Created By: Spark 3.0.0-SNAPSHOT
    Type: MANAGED
    Provider: org.apache.spark.sql.jdbc
    Location: file:/user/hive/warehouse/test_spark_28675
    Serde Library: org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe
    InputFormat: org.apache.hadoop.mapred.SequenceFileInputFormat
    OutputFormat: org.apache.hadoop.hive.ql.io.HiveSequenceFileOutputFormat
    Storage Properties: [url=###, driver=com.mysql.jdbc.Driver, dbtable=test_spark_28675]
    Schema: root
     |-- id: integer (nullable = true)
    
    ```
    
    After this PR:
    ```sql
    spark-sql> DESC EXTENDED test_spark_28675;
    id	int	NULL
    
    # Detailed Table Information
    Database	default
    Table	test_spark_28675
    Owner	root
    Created Time	Fri Aug 09 08:19:49 GMT-07:00 2019
    Last Access	Wed Dec 31 17:00:00 GMT-07:00 1969
    Created By	Spark 3.0.0-SNAPSHOT
    Type	MANAGED
    Provider	org.apache.spark.sql.jdbc
    Location	file:/user/hive/warehouse/test_spark_28675
    Serde Library	org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe
    InputFormat	org.apache.hadoop.mapred.SequenceFileInputFormat
    OutputFormat	org.apache.hadoop.hive.ql.io.HiveSequenceFileOutputFormat
    Storage Properties	[url=*********(redacted), driver=com.mysql.jdbc.Driver, dbtable=test_spark_28675]
    
    spark-sql> SHOW TABLE EXTENDED LIKE 'test_spark_28675';
    default	test_spark_28675	false	Database: default
    Table: test_spark_28675
    Owner: root
    Created Time: Fri Aug 09 08:19:49 GMT-07:00 2019
    Last Access: Wed Dec 31 17:00:00 GMT-07:00 1969
    Created By: Spark 3.0.0-SNAPSHOT
    Type: MANAGED
    Provider: org.apache.spark.sql.jdbc
    Location: file:/user/hive/warehouse/test_spark_28675
    Serde Library: org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe
    InputFormat: org.apache.hadoop.mapred.SequenceFileInputFormat
    OutputFormat: org.apache.hadoop.hive.ql.io.HiveSequenceFileOutputFormat
    Storage Properties: [url=*********(redacted), driver=com.mysql.jdbc.Driver, dbtable=test_spark_28675]
    Schema: root
     |-- id: integer (nullable = true)
    ```
    
    Closes #25395 from wangyum/SPARK-28675.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
@@ -74,7 +74,7 @@ case class CatalogStorageFormat(
     inputFormat.foreach(map.put("InputFormat", _))
     outputFormat.foreach(map.put("OutputFormat", _))
     if (compressed) map.put("Compressed", "")
-    CatalogUtils.maskCredentials(properties) match {
+    SQLConf.get.redactOptions(properties) match {
       case props if props.isEmpty => // No-op
       case props =>
         map.put("Storage Properties", props.map(p => p._1 + "=" + p._2).mkString("[", ", ", "]"))
