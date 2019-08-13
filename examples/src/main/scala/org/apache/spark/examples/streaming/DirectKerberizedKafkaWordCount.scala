7fdd8580c4fe97de89dac648eb3f9114c0a5242c
hongdd <jn_hdd@163.com>
2019-08-13 10:15:27 +0800

    Update DirectKerberizedKafkaWordCount.scala

--- examples/src/main/scala/org/apache/spark/examples/streaming/DirectKerberizedKafkaWordCount.scala
+++ examples/src/main/scala/org/apache/spark/examples/streaming/DirectKerberizedKafkaWordCount.scala
@@ -29,7 +29,7 @@ import org.apache.spark.streaming.kafka010._
 
 /**
  * Consumes messages from one or more topics in Kafka and does wordcount.
- * Usage: DirectKafkaWordCount <brokers> <topics>
+ * Usage: DirectKerberizedKafkaWordCount <brokers> <topics>
  *   <brokers> is a list of one or more Kafka brokers
  *   <groupId> is a consumer group name to consume from topics
  *   <topics> is a list of one or more kafka topics to consume from
@@ -46,7 +46,7 @@ object DirectKerberizedKafkaWordCount {
   def main(args: Array[String]) {
     if (args.length < 3) {
       System.err.println(s"""
-                            |Usage: DirectKafkaWordCount <brokers> <groupId> <topics>
+                            |Usage: DirectKerberizedKafkaWordCount <brokers> <groupId> <topics>
                             |  <brokers> is a list of one or more Kafka brokers
                             |  <groupId> is a consumer group name to consume from topics
                             |  <topics> is a list of one or more kafka topics to consume from
@@ -60,7 +60,7 @@ object DirectKerberizedKafkaWordCount {
     val Array(brokers, groupId, topics) = args
 
     // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("DirectKafkaWordCount")
+    val sparkConf = new SparkConf().setAppName("DirectKerberizedKafkaWordCount")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
 
     // Create direct kafka stream with brokers and topics
