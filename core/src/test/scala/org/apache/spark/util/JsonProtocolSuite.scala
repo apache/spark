247bebcf94df77883ac245aea63e7e871fc7aa44
Kousuke Saruta <sarutak@oss.nttdata.com>
2019-08-12 22:38:10 -0700

    [SPARK-28561][WEBUI] DAG viz for barrier-execution mode
    ## What changes were proposed in this pull request?
    
    In the current UI, we cannot identify which RDDs are barrier. Visualizing it will make easy to debug.
    Following images are shown after this change.
    
    ![Screenshot from 2019-07-30 16-30-35](https://user-images.githubusercontent.com/4736016/62110508-83cec100-b2e9-11e9-83b9-bc2e485a4cbe.png)
    ![Screenshot from 2019-07-30 16-31-09](https://user-images.githubusercontent.com/4736016/62110509-83cec100-b2e9-11e9-9e2e-47c4dae23a52.png)
    
    The boxes in pale green mean barrier (We might need to discuss which color is proper).
    
    ## How was this patch tested?
    
    Tested manually.
    The images above are shown by following operations.
    
    ```
    val rdd1 = sc.parallelize(1 to 10)
    val rdd2 = sc.parallelize(1 to 10)
    val rdd3 = rdd1.zip(rdd2).barrier.mapPartitions(identity(_))
    val rdd4 = rdd3.map(identity(_))
    val rdd5 = rdd4.reduceByKey(_+_)
    rdd5.collect
    ```
    
    Closes #25296 from sarutak/barrierexec-dagviz.
    
    Authored-by: Kousuke Saruta <sarutak@oss.nttdata.com>
    Signed-off-by: Xingbo Jiang <xingbo.jiang@databricks.com>

--- core/src/test/scala/org/apache/spark/util/JsonProtocolSuite.scala
+++ core/src/test/scala/org/apache/spark/util/JsonProtocolSuite.scala
@@ -366,14 +366,14 @@ class JsonProtocolSuite extends SparkFunSuite {
   test("RDDInfo backward compatibility (scope, parent IDs, callsite)") {
     // "Scope" and "Parent IDs" were introduced in Spark 1.4.0
     // "Callsite" was introduced in Spark 1.6.0
-    val rddInfo = new RDDInfo(1, "one", 100, StorageLevel.NONE, Seq(1, 6, 8),
+    val rddInfo = new RDDInfo(1, "one", 100, StorageLevel.NONE, false, Seq(1, 6, 8),
       "callsite", Some(new RDDOperationScope("fable")))
     val oldRddInfoJson = JsonProtocol.rddInfoToJson(rddInfo)
       .removeField({ _._1 == "Parent IDs"})
       .removeField({ _._1 == "Scope"})
       .removeField({ _._1 == "Callsite"})
     val expectedRddInfo = new RDDInfo(
-      1, "one", 100, StorageLevel.NONE, Seq.empty, "", scope = None)
+      1, "one", 100, StorageLevel.NONE, false, Seq.empty, "", scope = None)
     assertEquals(expectedRddInfo, JsonProtocol.rddInfoFromJson(oldRddInfoJson))
   }
 
@@ -857,7 +857,8 @@ private[spark] object JsonProtocolSuite extends Assertions {
   }
 
   private def makeRddInfo(a: Int, b: Int, c: Int, d: Long, e: Long) = {
-    val r = new RDDInfo(a, "mayor", b, StorageLevel.MEMORY_AND_DISK, Seq(1, 4, 7), a.toString)
+    val r =
+      new RDDInfo(a, "mayor", b, StorageLevel.MEMORY_AND_DISK, false, Seq(1, 4, 7), a.toString)
     r.numCachedPartitions = c
     r.memSize = d
     r.diskSize = e
