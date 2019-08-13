4d5965ecb48685faed63a751100433a273695e5b
Nikita Gorbachevsky <nikitag@playtika.com>
2019-08-13 14:36:23 +0300

    [SPARK-28709][DSTREAMS] - Fix StreamingContext leak through StreamingJobProgressListener on stop

--- core/src/main/scala/org/apache/spark/ui/WebUI.scala
+++ core/src/main/scala/org/apache/spark/ui/WebUI.scala
@@ -93,6 +93,7 @@ private[spark] abstract class WebUI(
     attachHandler(renderJsonHandler)
     val handlers = pageToHandlers.getOrElseUpdate(page, ArrayBuffer[ServletContextHandler]())
     handlers += renderHandler
+    handlers += renderJsonHandler
   }
 
   /** Attaches a handler to this UI. */
