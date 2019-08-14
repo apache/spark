34fbd029f70c9f51de93ab3d3bfc6c72bb8d5bf3
Kaxil Naik <kaxilnaik@gmail.com>
2019-08-14 10:39:40 +0100

    [AIRFLOW-5209] Bump Sphinx version to fix doc build (#5814)

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ doc = [
     'sphinx-argparse>=0.1.13',
     'sphinx-autoapi==1.0.0',
     'sphinx-rtd-theme>=0.1.6',
-    'sphinx>=1.2.3',
+    'sphinx>=2.1.2',
     'sphinxcontrib-httpdomain>=1.7.0',
 ]
 docker = ['docker~=3.0']
