b9844149d342408326c8d6c142779a1fd027e70d
HyukjinKwon <gurwls223@apache.org>
2019-08-14 18:55:50 +0900

    Add JAVA_HOME into PATH as well

--- dev/run-tests-jenkins.py
+++ dev/run-tests-jenkins.py
@@ -184,6 +184,7 @@ def main():
     if "test-hadoop3.2" in ghprb_pull_title:
         os.environ["AMPLAB_JENKINS_BUILD_PROFILE"] = "hadoop3.2"
         os.environ["JAVA_HOME"] = "/usr/java/jdk-11.0.1"
+        os.environ["PATH"] = "%s/bin:%s" % (os.environ["JAVA_HOME"], os.environ["PATH"])
 
     build_display_name = os.environ["BUILD_DISPLAY_NAME"]
     build_url = os.environ["BUILD_URL"]
