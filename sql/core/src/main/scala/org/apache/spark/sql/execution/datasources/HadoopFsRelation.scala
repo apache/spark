d0eb108df3d16f5dae2a9f14d767b48eeceb13e8
shahid <shahidki31@gmail.com>
2019-08-15 17:10:15 +0530

    address comments

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/HadoopFsRelation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/HadoopFsRelation.scala
@@ -72,8 +72,8 @@ case class HadoopFsRelation(
     val compressionFactor = sqlContext.conf.fileCompressionFactor
     val defaultSize = (location.sizeInBytes * compressionFactor).toLong
     location match {
-      case cfi: CatalogFileIndex if sparkSession.sessionState.conf.fallBackToHdfsForStatsEnabled
-        && defaultSize == sqlContext.conf.defaultSizeInBytes =>
+      case cfi: CatalogFileIndex if sparkSession.sessionState.conf.fallBackToHdfsForStatsEnabled &&
+        defaultSize == sqlContext.conf.defaultSizeInBytes =>
         CommandUtils.getSizeInBytesFallBackToHdfs(sparkSession, new Path(cfi.table.location),
           defaultSize)
       case _ => defaultSize
