4d491f394e93943848d26fb06414308528d2ad30
Jarek Potiuk <jarek.potiuk@polidea.com>
2019-08-15 20:06:18 -0400

    [AIRFLOW-5225] Consistent licence for all JS files (#5827)

--- airflow/www/static/js/datetime-utils.js
+++ airflow/www/static/js/datetime-utils.js
@@ -1,22 +1,21 @@
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
+ * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
-
 export const moment = require('moment-timezone');
 export const defaultFormat = 'YYYY-MM-DD, HH:mm:ss';
 export const defaultFormatWithTZ = 'YYYY-MM-DD, HH:mm:ss z';
@@ -60,7 +59,7 @@ export const converAndFormatUTC = (datetime, tz) => {
 }
 
 export const secondsToString = (seconds) => {
-  let numdays    = Math.floor((seconds % 31536000) / 86400); 
+  let numdays    = Math.floor((seconds % 31536000) / 86400);
   let numhours   = Math.floor(((seconds % 31536000) % 86400) / 3600);
   let numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
   let numseconds = Math.floor((((seconds % 31536000) % 86400) % 3600) % 60);
