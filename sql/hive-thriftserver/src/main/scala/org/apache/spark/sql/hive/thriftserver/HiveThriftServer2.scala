f0834d3a7f945759bbbcba970d24a49ba1d08421
Dongjoon Hyun <dhyun@apple.com>
2019-08-18 16:54:24 -0700

    Revert "[SPARK-28527][SQL][TEST] Re-run all the tests in SQLQueryTestSuite via Thrift Server"
    This reverts commit efbb0359024d1f77e207ca5fc78b3c32b4138558.

--- sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/HiveThriftServer2.scala
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/HiveThriftServer2.scala
@@ -53,7 +53,7 @@ object HiveThriftServer2 extends Logging {
    * Starts a new thrift server with the given context.
    */
   @DeveloperApi
-  def startWithContext(sqlContext: SQLContext): HiveThriftServer2 = {
+  def startWithContext(sqlContext: SQLContext): Unit = {
     val server = new HiveThriftServer2(sqlContext)
 
     val executionHive = HiveUtils.newClientForExecution(
@@ -69,7 +69,6 @@ object HiveThriftServer2 extends Logging {
     } else {
       None
     }
-    server
   }
 
   def main(args: Array[String]) {
