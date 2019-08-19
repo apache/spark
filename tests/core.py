43e39bcea9548075a62590e43c3bdf39fdd720ab
Bas Harenslak <BasPH@users.noreply.github.com>
2019-08-19 06:50:11 +0200

    [AIRFLOW-5160] Remove example DAG count test (#5775)

--- tests/core.py
+++ tests/core.py
@@ -77,7 +77,6 @@ from airflow.utils.timezone import datetime
 from airflow.hooks import hdfs_hook
 from tests.test_utils.config import conf_vars
 
-NUM_EXAMPLE_DAGS = 19
 DEV_NULL = '/dev/null'
 TEST_DAG_FOLDER = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), 'dags')
@@ -698,9 +697,6 @@ class CoreTest(unittest.TestCase):
 
         self.assertEqual(context['tomorrow_ds'], '2015-01-02')
         self.assertEqual(context['tomorrow_ds_nodash'], '20150102')
-
-    def test_import_examples(self):
-        self.assertEqual(len(self.dagbag.dags), NUM_EXAMPLE_DAGS)
 
     def test_local_task_job(self):
         TI = TaskInstance
