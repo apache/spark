a4d7360a8e16516199575a013c11ca97f68d755c
Yuanjian Li <xyliyuanjian@gmail.com>
2019-08-20 13:47:59 +0800

    [SPARK-28699][CORE] Fix a corner case for aborting indeterminate stage
    Change the logic of collecting the indeterminate stage, we should look at stages from mapStage, not failedStage during handle FetchFailed.
    
    In the fetch failed error handle logic, the original logic of collecting indeterminate stage from the fetch failed stage. And in the scenario of the fetch failed happened in the first task of this stage, this logic will cause the indeterminate stage to resubmit partially. Eventually, we are capable of getting correctness bug.
    
    It makes the corner case of indeterminate stage abort as expected.
    
    New UT in DAGSchedulerSuite.
    Run below integrated test with `local-cluster[5, 2, 5120]`, and set `spark.sql.execution.sortBeforeRepartition`=false, it will abort the indeterminate stage as expected:
    ```
    import scala.sys.process._
    import org.apache.spark.TaskContext
    
    val res = spark.range(0, 10000 * 10000, 1).map{ x => (x % 1000, x)}
    // kill an executor in the stage that performs repartition(239)
    val df = res.repartition(113).map{ x => (x._1 + 1, x._2)}.repartition(239).map { x =>
      if (TaskContext.get.attemptNumber == 0 && TaskContext.get.partitionId < 1 && TaskContext.get.stageAttemptNumber == 0) {
        throw new Exception("pkill -f -n java".!!)
      }
      x
    }
    val r2 = df.distinct.count()
    ```
    
    Closes #25498 from xuanyuanking/SPARK-28699-followup.
    
    Authored-by: Yuanjian Li <xyliyuanjian@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit 0d3a783cc57ed09650ee31851a19728d8f16cd0c)
    Signed-off-by: Yuanjian Li <xyliyuanjian@gmail.com>

--- core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
@@ -2527,27 +2527,10 @@ class DAGSchedulerSuite extends SparkFunSuite with LocalSparkContext with TimeLi
       FetchFailed(makeBlockManagerId("hostC"), shuffleId2, 0, 0, "ignored"),
       null))
 
-    val failedStages = scheduler.failedStages.toSeq
-    assert(failedStages.length == 2)
-    // Shuffle blocks of "hostC" is lost, so first task of the `shuffleMapRdd2` needs to retry.
-    assert(failedStages.collect {
-      case stage: ShuffleMapStage if stage.shuffleDep.shuffleId == shuffleId2 => stage
-    }.head.findMissingPartitions() == Seq(0))
-    // The result stage is still waiting for its 2 tasks to complete
-    assert(failedStages.collect {
-      case stage: ResultStage => stage
-    }.head.findMissingPartitions() == Seq(0, 1))
-
-    scheduler.resubmitFailedStages()
-
-    // The first task of the `shuffleMapRdd2` failed with fetch failure
-    runEvent(makeCompletionEvent(
-      taskSets(3).tasks(0),
-      FetchFailed(makeBlockManagerId("hostA"), shuffleId1, 0, 0, "ignored"),
-      null))
-
-    // The job should fail because Spark can't rollback the shuffle map stage.
-    assert(failure != null && failure.getMessage.contains("Spark cannot rollback"))
+    // The second shuffle map stage need to rerun, the job will abort for the indeterminate
+    // stage rerun.
+    assert(failure != null && failure.getMessage
+      .contains("Spark cannot rollback the ShuffleMapStage 1"))
   }
 
   private def assertResultStageFailToRollback(mapRdd: MyRDD): Unit = {
