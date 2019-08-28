a9fa633353ab7d729e883e2e9d394528e51f5525
David Vogelbacher <dvogelbacher@palantir.com>
2019-08-27 20:42:20 -0400

    nits

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlan.scala
@@ -353,7 +353,7 @@ abstract class SparkPlan extends QueryPlan[SparkPlan] with Logging with Serializ
     val byteArrayRdd = getByteArrayRdd()
 
     val results = ArrayBuffer[InternalRow]()
-    val decoder = new SizeLimitingByteArrayDecoder(schema.fields.length, sqlContext.conf)
+    val decoder = new SizeLimitingByteArrayDecoder(schema.length, sqlContext.conf)
     byteArrayRdd.collect().foreach { countAndBytes =>
       decoder.decodeUnsafeRows(countAndBytes._2).foreach(results.+=)
     }
@@ -363,7 +363,7 @@ abstract class SparkPlan extends QueryPlan[SparkPlan] with Logging with Serializ
   private[spark] def executeCollectIterator(): (Long, Iterator[InternalRow]) = {
     val countsAndBytes = getByteArrayRdd().collect()
     val total = countsAndBytes.map(_._1).sum
-    val decoder = new SizeLimitingByteArrayDecoder(schema.fields.length, sqlContext.conf)
+    val decoder = new SizeLimitingByteArrayDecoder(schema.length, sqlContext.conf)
     val rows = countsAndBytes.iterator
       .flatMap(countAndBytes => decoder.decodeUnsafeRows(countAndBytes._2))
     (total, rows)
@@ -375,7 +375,7 @@ abstract class SparkPlan extends QueryPlan[SparkPlan] with Logging with Serializ
    * @note Triggers multiple jobs (one for each partition).
    */
   def executeToIterator(): Iterator[InternalRow] = {
-    val decoder = new SizeLimitingByteArrayDecoder(schema.fields.length, sqlContext.conf)
+    val decoder = new SizeLimitingByteArrayDecoder(schema.length, sqlContext.conf)
     getByteArrayRdd().map(_._2).toLocalIterator.flatMap(decoder.decodeUnsafeRows)
   }
 
@@ -402,7 +402,7 @@ abstract class SparkPlan extends QueryPlan[SparkPlan] with Logging with Serializ
     val buf = new ArrayBuffer[InternalRow]
     val totalParts = childRDD.partitions.length
     var partsScanned = 0
-    val decoder = new SizeLimitingByteArrayDecoder(schema.fields.length, sqlContext.conf)
+    val decoder = new SizeLimitingByteArrayDecoder(schema.length, sqlContext.conf)
     while (buf.length < n && partsScanned < totalParts) {
       // The number of partitions to try in this iteration. It is ok for this number to be
       // greater than totalParts because we actually cap it at totalParts in runJob.
