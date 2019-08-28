3b5850b0dbeeedfce0ac0c3ce53ad656ecafcb74
Gengliang Wang <gengliang.wang@databricks.com>
2019-08-28 12:44:02 +0800

    revise

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1645,7 +1645,7 @@ object SQLConf {
       .doc("When inserting a value into a column with different data type, Spark will perform " +
         "type coercion. Currently, we support 3 policies for the type coercion rules: ANSI, " +
         "legacy and strict. With ANSI policy, Spark performs the type coercion as per ANSI SQL. " +
-        "In practice, it follows PostgreSQL. It disallows certain type unreasonable conversions " +
+        "In practice, it follows PostgreSQL. It disallows certain unreasonable type conversions " +
         "such as converting `string` to `int` or `double` to `boolean`. " +
         "With legacy policy, Spark allows the type coercion as long as it is a valid `Cast`, " +
         "which is very loose. e.g. converting `string` to `int` or `double` to `boolean` is " +
