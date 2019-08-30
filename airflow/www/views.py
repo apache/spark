dc0018b2f2532cc86da4dc0d9e5d076edb189d9f
Kaxil Naik <kaxilnaik@gmail.com>
2019-08-30 15:20:48 +0100

    [AIRFLOW-5357] Fix Content-Type for exported variables.json file (#5962)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -2244,6 +2244,7 @@ class VariableModelView(AirflowModelView):
 
         response = make_response(json.dumps(var_dict, sort_keys=True, indent=4))
         response.headers["Content-Disposition"] = "attachment; filename=variables.json"
+        response.headers["Content-Type"] = "application/json; charset=utf-8"
         return response
 
     @expose('/varimport', methods=["POST"])
