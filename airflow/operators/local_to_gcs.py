c35b93218e623b7530e84ffbce829618fd303469
Tomek <tomasz.urbaszek@polidea.com>
2019-09-02 19:50:47 +0200

    [AIRFLOW-5384] Improve dst param info in FileToGCSOperator (#5985)
    This commit add more info about dst parameter to indicate that the path
    must include file name.

--- airflow/operators/local_to_gcs.py
+++ airflow/operators/local_to_gcs.py
@@ -33,7 +33,8 @@ class FileToGoogleCloudStorageOperator(BaseOperator):
 
     :param src: Path to the local file. (templated)
     :type src: str
-    :param dst: Destination path within the specified bucket. (templated)
+    :param dst: Destination path within the specified bucket, it must be the full file path
+        to destination object on GCS, including GCS object (ex. `path/to/file.txt`) (templated)
     :type dst: str
     :param bucket: The bucket to upload to. (templated)
     :type bucket: str
