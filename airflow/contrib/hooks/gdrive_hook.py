a39ace92a82d06c6e5f5968448fc1f6b83b4445a
Jarek Potiuk <jarek.potiuk@polidea.com>
2019-09-06 13:55:36 +0200

    fixup! [AIRFLOW-4964] Add BigQuery Data Transfer Hook and Operator (#5769)

--- airflow/contrib/hooks/gdrive_hook.py
+++ airflow/contrib/hooks/gdrive_hook.py
@@ -47,7 +47,6 @@ class GoogleDriveHook(GoogleCloudBaseHook):
     ) -> None:
         super().__init__(gcp_conn_id, delegate_to)
         self.api_version = api_version
-        self.num_retries = self._get_field("num_retries", 5)
 
     def get_conn(self) -> Any:
         """
