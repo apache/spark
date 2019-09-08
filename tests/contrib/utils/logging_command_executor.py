0ffc14c30aeb0162c086228621b8a6e5fd63c227
Tomek <tomasz.urbaszek@polidea.com>
2019-09-08 10:36:50 +0200

    [AIRFLOW-5425] Use logging not printing in LoggingCommandExecutor (#6032)
    This will show proper message in log (yellow color) which will be harder to miss.

--- tests/contrib/utils/logging_command_executor.py
+++ tests/contrib/utils/logging_command_executor.py
@@ -39,7 +39,7 @@ class LoggingCommandExecutor(LoggingMixin):
             self.log.info("Stdout: {}".format(output))
             self.log.info("Stderr: {}".format(err))
             if retcode:
-                print("Error when executing '{}'".format(" ".join(cmd)))
+                self.log.warning("Error when executing %s", " ".join(cmd))
             return retcode
 
     def check_output(self, cmd):
