e9d22e1704731e01b9f63e25fcf74b313009827a
Marco Gaido <marcogaido91@gmail.com>
2019-09-09 17:43:08 +0200

    [SPARK-28939][SQL][BACKPORT-2.4] Propagate SQLConf for plans executed by toRdd

--- sql/core/src/main/scala/org/apache/spark/sql/execution/QueryExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/QueryExecution.scala
@@ -77,7 +77,8 @@ class QueryExecution(val sparkSession: SparkSession, val logical: LogicalPlan) {
   lazy val executedPlan: SparkPlan = prepareForExecution(sparkPlan)
 
   /** Internal version of the RDD. Avoids copies and has no schema */
-  lazy val toRdd: RDD[InternalRow] = executedPlan.execute()
+  lazy val toRdd: RDD[InternalRow] = new SQLExecutionRDD(
+    executedPlan.execute(), sparkSession.sessionState.conf)
 
   /**
    * Prepares a planned [[SparkPlan]] for execution by inserting shuffle operations and internal
