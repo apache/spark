14246c69657fd9a3b3ca868a99686d0364ef7fb7
LantaoJin <jinlantao@gmail.com>
2019-09-10 11:22:06 +0900

    [SPARK-28988][SQL][TESTS] Fix invalid tests in CliSuite
    ### What changes were proposed in this pull request?
    
    https://github.com/apache/spark/blob/1f056eb313de792a279c3c7c21b5bccd652c631b/sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala#L221 is not strong enough. It will success if class not found.
    
    https://github.com/apache/spark/blob/1f056eb313de792a279c3c7c21b5bccd652c631b/sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala#L305 is also incorrect. Whatever the right side value is, it always succeeds.
    
    ### Why are the changes needed?
    Unit tests should failed if the class not found.
    
    ### Does this PR introduce any user-facing change?
    No
    
    ### How was this patch tested?
    Exist UTs
    
    Closes #25724 from LantaoJin/SPARK-28988.
    
    Authored-by: LantaoJin <jinlantao@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala
@@ -27,7 +27,7 @@ import scala.concurrent.Promise
 import scala.concurrent.duration._
 
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars
-import org.apache.hadoop.hive.contrib.udaf.example.UDAFExampleMax
+import org.apache.hadoop.hive.contrib.udf.example.UDFExampleFormat
 import org.scalatest.BeforeAndAfterAll
 
 import org.apache.spark.SparkFunSuite
@@ -218,8 +218,8 @@ class CliSuite extends SparkFunSuite with BeforeAndAfterAll with Logging {
         -> "",
       "INSERT INTO TABLE t1 SELECT key, val FROM sourceTable;"
         -> "",
-      "SELECT count(key) FROM t1;"
-        -> "5",
+      "SELECT collect_list(array(val)) FROM t1;"
+        -> """[["val_238"],["val_86"],["val_311"],["val_27"],["val_165"]]""",
       "DROP TABLE t1;"
         -> "",
       "DROP TABLE sourceTable;"
@@ -301,8 +301,8 @@ class CliSuite extends SparkFunSuite with BeforeAndAfterAll with Logging {
     runCliWithin(
       1.minute,
       Seq("--conf", s"spark.hadoop.${ConfVars.HIVEAUXJARS}=$hiveContribJar"))(
-      s"CREATE TEMPORARY FUNCTION example_max AS '${classOf[UDAFExampleMax].getName}';" -> "",
-      "SELECT example_max(1);" -> "1"
+      s"CREATE TEMPORARY FUNCTION example_format AS '${classOf[UDFExampleFormat].getName}';" -> "",
+      "SELECT example_format('%o', 93);" -> "135"
     )
   }
 }
