36e2d1b2d98b58c16c703df8def4d8be1603f4b0
Dongjoon Hyun <dhyun@apple.com>
2019-09-11 12:31:00 -0700

    Use field names like JSON.

--- core/src/main/scala/org/apache/spark/status/api/v1/PrometheusResource.scala
+++ core/src/main/scala/org/apache/spark/status/api/v1/PrometheusResource.scala
@@ -16,6 +16,7 @@
  */
 package org.apache.spark.status.api.v1
 
+import java.util.Arrays
 import javax.ws.rs._
 import javax.ws.rs.core.MediaType
 
@@ -25,6 +26,14 @@ import org.glassfish.jersey.servlet.ServletContainer
 
 import org.apache.spark.ui.SparkUI
 
+/**
+ * This aims to expose `Executor Task Metrics` like REST API which is documented in
+ *
+ *    https://spark.apache.org/docs/latest/monitoring.html#executor-task-metrics.
+ *
+ * The field order follows `ExecutorSummary` and `StageData`.
+ * Note that this is not the same with `ExecutorSource`.
+ */
 @Path("/detail")
 private[v1] class PrometheusResource extends ApiRequestContext {
   @GET
@@ -37,6 +46,7 @@ private[v1] class PrometheusResource extends ApiRequestContext {
     store.executorList(true).foreach { executor =>
       val prefix = s"metrics_${appId}_${executor.id}_executor_"
       sb.append(s"${prefix}rddBlocks_Count ${executor.rddBlocks}\n")
+      sb.append(s"${prefix}memoryUsed_Count ${executor.memoryUsed}\n")
       sb.append(s"${prefix}diskUsed_Count ${executor.diskUsed}\n")
       sb.append(s"${prefix}totalCores_Count ${executor.totalCores}\n")
       sb.append(s"${prefix}maxTasks_Count ${executor.maxTasks}\n")
@@ -86,8 +96,8 @@ private[v1] class PrometheusResource extends ApiRequestContext {
       }
     }
 
-    store.activeStages().foreach { stage =>
-      val prefix = f"metrics_${appId}_${stage.attemptId}_executor_"
+    store.stageList(null).foreach { stage =>
+      val prefix = f"metrics_${appId}_${stage.stageId}_${stage.attemptId}_stage_"
       sb.append(s"${prefix}numTasks_Count ${stage.numTasks}\n")
       sb.append(s"${prefix}numActiveTasks_Count ${stage.numActiveTasks}\n")
       sb.append(s"${prefix}numCompleteTasks_Count ${stage.numCompleteTasks}\n")
@@ -96,8 +106,8 @@ private[v1] class PrometheusResource extends ApiRequestContext {
       sb.append(s"${prefix}numCompletedIndices_Count ${stage.numCompletedIndices}\n")
       sb.append(s"${prefix}executorDeserializeTime_Count ${stage.executorDeserializeTime}\n")
       sb.append(s"${prefix}executorDeserializeCpuTime_Count ${stage.executorDeserializeCpuTime}\n")
-      sb.append(s"${prefix}runTime_Count ${stage.executorRunTime}\n")
-      sb.append(s"${prefix}cpuTime_Count ${stage.executorCpuTime}\n")
+      sb.append(s"${prefix}executorRunTime_Count ${stage.executorRunTime}\n")
+      sb.append(s"${prefix}executorCpuTime_Count ${stage.executorCpuTime}\n")
       sb.append(s"${prefix}resultSize_Count ${stage.resultSize}\n")
       sb.append(s"${prefix}jvmGcTime_Count ${stage.jvmGcTime}\n")
       sb.append(s"${prefix}resultSerializationTime_Count ${stage.resultSerializationTime}\n")
@@ -118,6 +128,7 @@ private[v1] class PrometheusResource extends ApiRequestContext {
       sb.append(s"${prefix}shuffleReadBytes_Count ${stage.shuffleReadBytes}\n")
       sb.append(s"${prefix}shuffleReadRecords_Count ${stage.shuffleReadRecords}\n")
       sb.append(s"${prefix}shuffleWriteBytes_Count ${stage.shuffleWriteBytes}\n")
+      sb.append(s"${prefix}shuffleWriteTime_Count ${stage.shuffleWriteTime}\n")
       sb.append(s"${prefix}shuffleWriteRecords_Count ${stage.shuffleWriteRecords}\n")
     }
     sb.toString
@@ -125,7 +136,6 @@ private[v1] class PrometheusResource extends ApiRequestContext {
 }
 
 private[spark] object PrometheusResource {
-
   def getServletHandler(uiRoot: UIRoot): ServletContextHandler = {
     val jerseyContext = new ServletContextHandler(ServletContextHandler.NO_SESSIONS)
     jerseyContext.setContextPath("/metrics")
