75dc86d55e8d144a1ea8f911b39817a482fd033b
Shanyu Zhao <shzhao@microsoft.com>
2019-09-13 15:25:41 -0700

    Update comments for start()
    Signed-off-by: Shanyu Zhao <shzhao@microsoft.com>

--- core/src/main/scala/org/apache/spark/deploy/history/ApplicationHistoryProvider.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/ApplicationHistoryProvider.scala
@@ -115,7 +115,8 @@ private[history] abstract class ApplicationHistoryProvider {
   def stop(): Unit = { }
 
   /**
-   * Called when the server is starting up.
+   * Called when the server is starting up. Implement this function to init the provider and start
+   * background threads. With this function we can start provider later after it is created.
    */
   def start(): Unit = { }
 
