65600048b5b8f235d40a08080e5c7b1721d1d7d6
Alexis-Gu-Ar <39233477+Alexis-Gu-Ar@users.noreply.github.com>
2019-09-15 16:37:35 -0500

    [AIRFLOW-5490] Fix incorrect None comparison (#6109)

--- airflow/www/security.py
+++ airflow/www/security.py
@@ -340,8 +340,8 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         pvms = (
             sesh.query(sqla_models.PermissionView)
             .filter(or_(
-                sqla_models.PermissionView.permission == None,  # noqa pylint: disable=singleton-comparison
-                sqla_models.PermissionView.view_menu == None,  # noqa pylint: disable=singleton-comparison
+                sqla_models.PermissionView.permission is None,  # noqa pylint: disable=singleton-comparison
+                sqla_models.PermissionView.view_menu is None,  # noqa pylint: disable=singleton-comparison
             ))
         )
         # Since FAB doesn't define ON DELETE CASCADE on these tables, we need
