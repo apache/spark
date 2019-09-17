03305bea4e8663f7bcd12963b489b1e8755c382f
Yuanjian Li <xyliyuanjian@gmail.com>
2019-09-17 20:44:11 +0800

    Add test for checking ambiguous field

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameNaFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameNaFunctionsSuite.scala
@@ -231,7 +231,7 @@ class DataFrameNaFunctionsSuite extends QueryTest with SharedSparkSession {
     }
   }
 
-  test("fill for join operation") {
+  def createDFsWithSameFieldsName(): (DataFrame, DataFrame) = {
     val df1 = Seq(
       ("f1-1", "f2", null),
       ("f1-2", null, null),
@@ -243,12 +243,27 @@ class DataFrameNaFunctionsSuite extends QueryTest with SharedSparkSession {
       ("f1-2", "f2", null),
       ("f1-3", "f2", "f4-1")
     ).toDF("f1", "f2", "f4")
+    (df1, df2)
+  }
+
+  test("fill unambiguous field for join operation") {
+    val (df1, df2) = createDFsWithSameFieldsName()
     val joined_df = df1.join(df2, Seq("f1"), joinType = "left_outer")
     checkAnswer(joined_df.na.fill("", cols = Seq("f4")),
       Row("f1-1", "f2", null, null, "") ::
         Row("f1-2", null, null, "f2", "") ::
         Row("f1-3", "f2", "f3-1", "f2", "f4-1") ::
         Row("f1-4", "f2", "f3-1", null, "") :: Nil)
+  }
+
+  test("fill ambiguous field for join operation") {
+    val (df1, df2) = createDFsWithSameFieldsName()
+    val joined_df = df1.join(df2, Seq("f1"), joinType = "left_outer")
+
+    val message = intercept[AnalysisException] {
+      joined_df.na.fill("", cols = Seq("f2"))
+    }.getMessage
+    assert(message.contains("Reference 'f2' is ambiguous"))
   }
 
   test("replace") {
