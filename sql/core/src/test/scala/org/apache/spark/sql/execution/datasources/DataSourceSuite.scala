6887d4f669af9ebd378cf3f3ec679a6dfeebb772
Arwin Tio <Arwin.tio@adroll.com>
2019-09-23 00:11:49 -0700

    Add tests

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/DataSourceSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/DataSourceSuite.scala
@@ -19,52 +19,129 @@ package org.apache.spark.sql.execution.datasources
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileStatus, Path, RawLocalFileSystem}
+import org.apache.spark.sql.AnalysisException
 import org.apache.spark.sql.test.SharedSparkSession
 
 class DataSourceSuite extends SharedSparkSession {
   import TestPaths._
 
-  test("test everything works") {
-    val hadoopConf = new Configuration()
-    hadoopConf.set("fs.mockFs.impl", classOf[MockFileSystem].getName)
-
+  test("test glob and non glob paths") {
     val allPaths = DataSource.checkAndGlobPathIfNecessary(
       Seq(
-        nonGlobPath1.toString,
-        nonGlobPath2.toString,
+        path1.toString,
+        path2.toString,
         globPath1.toString,
         globPath2.toString,
       ),
       hadoopConf,
       checkEmptyGlobPath = true,
-      checkFilesExist = true
+      checkFilesExist = true,
     )
 
-    assertThrows(allPaths.equals(expectedResultPaths))
+    assert(allPaths.equals(allPathsInFs))
+  }
+
+  test("test glob paths") {
+    val allPaths = DataSource.checkAndGlobPathIfNecessary(
+      Seq(
+        globPath1.toString,
+        globPath2.toString,
+      ),
+      hadoopConf,
+      checkEmptyGlobPath = true,
+      checkFilesExist = true,
+    )
+
+    assert(
+      allPaths.equals(
+        Seq(
+          globPath1Result1,
+          globPath1Result2,
+          globPath2Result1,
+          globPath2Result2,
+        )
+      )
+    )
+  }
+
+  test("test non glob paths") {
+    val allPaths = DataSource.checkAndGlobPathIfNecessary(
+      Seq(
+        path1.toString,
+        path2.toString,
+      ),
+      hadoopConf,
+      checkEmptyGlobPath = true,
+      checkFilesExist = true,
+    )
+
+    assert(
+      allPaths.equals(
+        Seq(
+          path1,
+          path2,
+        )
+      )
+    )
+  }
+
+  test("test non existent paths") {
+    assertThrows[AnalysisException](
+      DataSource.checkAndGlobPathIfNecessary(
+        Seq(
+          path1.toString,
+          path2.toString,
+          nonExistentPath.toString,
+        ),
+        hadoopConf,
+        checkEmptyGlobPath = true,
+        checkFilesExist = true,
+      )
+    )
+  }
+
+  test("test non existent glob paths") {
+    assertThrows[AnalysisException](
+      DataSource.checkAndGlobPathIfNecessary(
+        Seq(
+          globPath1.toString,
+          globPath2.toString,
+          nonExistentGlobPath.toString,
+        ),
+        hadoopConf,
+        checkEmptyGlobPath = true,
+        checkFilesExist = true,
+      )
+    )
   }
 }
 
 object TestPaths {
-  val nonGlobPath1: Path = new Path("mockFs:///somepath1")
-  val nonGlobPath2: Path = new Path("mockFs:///somepath2")
+  val hadoopConf = new Configuration()
+  hadoopConf.set("fs.mockFs.impl", classOf[MockFileSystem].getName)
+
+  val path1: Path = new Path("mockFs:///somepath1")
+  val path2: Path = new Path("mockFs:///somepath2")
   val globPath1: Path = new Path("mockFs:///globpath1*")
   val globPath2: Path = new Path("mockFs:///globpath2*")
+
+  val nonExistentPath: Path = new Path("mockFs:///nonexistentpath")
+  val nonExistentGlobPath: Path = new Path("mockFs:///nonexistentpath*")
 
   val globPath1Result1: Path = new Path("mockFs:///globpath1/path1")
   val globPath1Result2: Path = new Path("mockFs:///globpath1/path2")
   val globPath2Result1: Path = new Path("mockFs:///globpath2/path1")
   val globPath2Result2: Path = new Path("mockFs:///globpath2/path2")
 
-  val expectedResultPaths = Seq(
-    globPath1,
-    globPath2,
+  val allPathsInFs = Seq(
+    path1,
+    path2,
     globPath1Result1,
     globPath1Result2,
     globPath2Result1,
     globPath2Result2,
   )
 
-  val mockNonGlobPaths = Set(nonGlobPath1, nonGlobPath2)
   val mockGlobResults: Map[Path, Array[FileStatus]] = Map(
     globPath1 ->
       Array(
@@ -89,7 +166,7 @@ class MockFileSystem extends RawLocalFileSystem {
   import TestPaths._
 
   override def exists(f: Path): Boolean = {
-    mockNonGlobPaths.contains(f)
+    allPathsInFs.contains(f)
   }
 
   override def globStatus(pathPattern: Path): Array[FileStatus] = {
