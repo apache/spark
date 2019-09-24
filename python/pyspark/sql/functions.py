05a32cadede1efb1aaa9ff0664e751b3198cbd42
sheepstop <yangting617@gmail.com>
2019-09-24 18:57:54 +0900

    [SPARK-28678][DOC] Specify that array indices start at 1 for function slice in R Scala Python
    ### What changes were proposed in this pull request?
    Added "array indices start at 1" in annotation to make it clear for the usage of function slice, in R Scala Python component
    
    ### Why are the changes needed?
    It will throw exception if the value stare is 0, but array indices start at 0 most of times in other scenarios.
    
    ### Does this PR introduce any user-facing change?
    Yes, more info provided to user.
    
    ### How was this patch tested?
    No tests added, only doc change.
    
    Closes #25704 from sheepstop/master.
    
    Authored-by: sheepstop <yangting617@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit 81de9d3c296da9e632376e720189674401bd6639)
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -1907,7 +1907,7 @@ def arrays_overlap(a1, a2):
 def slice(x, start, length):
     """
     Collection function: returns an array containing  all the elements in `x` from index `start`
-    (or starting from the end if `start` is negative) with the specified `length`.
+    (array indices start at 1, or from the end if `start` is negative) with the specified `length`.
     >>> df = spark.createDataFrame([([1, 2, 3],), ([4, 5],)], ['x'])
     >>> df.select(slice(df.x, 2, 2).alias("sliced")).collect()
     [Row(sliced=[2, 3]), Row(sliced=[5])]
