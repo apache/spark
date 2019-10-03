27b2bcd690b98a8f4c8ef67227cc57ec54847d57
Maxim Gekk <max.gekk@gmail.com>
2019-10-03 10:50:42 +0300

    Test for casting from string to interval

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CastSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CastSuite.scala
@@ -816,6 +816,8 @@ class CastSuite extends SparkFunSuite with ExpressionEvalHelper {
       new CalendarInterval(15, -3 * CalendarInterval.MICROS_PER_DAY), CalendarIntervalType),
       StringType),
       "interval 1 years 3 months -3 days")
+    checkEvaluation(Cast(Literal("INTERVAL 1 Second 1 microsecond"), CalendarIntervalType),
+      new CalendarInterval(0, 1000001))
   }
 
   test("cast string to boolean") {
