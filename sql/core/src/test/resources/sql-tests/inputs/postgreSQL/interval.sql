26ec823f0d34946c2da2a803c64fe1fb437fae90
Maxim Gekk <max.gekk@gmail.com>
2019-10-07 10:32:02 +0300

    Fix aliases quoting

--- sql/core/src/test/resources/sql-tests/inputs/postgreSQL/interval.sql
+++ sql/core/src/test/resources/sql-tests/inputs/postgreSQL/interval.sql
@@ -6,14 +6,14 @@
 -- SET IntervalStyle to postgres;
 
 -- check acceptance of "time zone style"
-SELECT INTERVAL '01:00' AS "One hour";
-SELECT INTERVAL '+02:00' AS "Two hours";
-SELECT INTERVAL '-08:00' AS "Eight hours";
-SELECT INTERVAL '-1 +02:03' AS "22 hours ago...";
-SELECT INTERVAL '-1 days +02:03' AS "22 hours ago...";
-SELECT INTERVAL '1.5 weeks' AS "Ten days twelve hours";
-SELECT INTERVAL '1.5 months' AS "One month 15 days";
-SELECT INTERVAL '10 years -11 month -12 days +13:14' AS "9 years...";
+SELECT INTERVAL '01:00' AS `One hour`;
+SELECT INTERVAL '+02:00' AS `Two hours`;
+SELECT INTERVAL '-08:00' AS `Eight hours`;
+SELECT INTERVAL '-1 +02:03' AS `22 hours ago...`;
+SELECT INTERVAL '-1 days +02:03' AS `22 hours ago...`;
+SELECT INTERVAL '1.5 weeks' AS `Ten days twelve hours`;
+SELECT INTERVAL '1.5 months' AS `One month 15 days`;
+SELECT INTERVAL '10 years -11 month -12 days +13:14' AS `9 years...`;
 
 CREATE TABLE INTERVAL_TBL (f1 interval);
 
@@ -143,12 +143,12 @@ select '100000000y 10mon -1000000000d -100000h -10min -10.000001s ago'::interval
 
 -- test justify_hours() and justify_days()
 
-SELECT justify_hours(interval '6 months 3 days 52 hours 3 minutes 2 seconds') as "6 mons 5 days 4 hours 3 mins 2 seconds";
-SELECT justify_days(interval '6 months 36 days 5 hours 4 minutes 3 seconds') as "7 mons 6 days 5 hours 4 mins 3 seconds";
+SELECT justify_hours(interval '6 months 3 days 52 hours 3 minutes 2 seconds') as `6 mons 5 days 4 hours 3 mins 2 seconds`;
+SELECT justify_days(interval '6 months 36 days 5 hours 4 minutes 3 seconds') as `7 mons 6 days 5 hours 4 mins 3 seconds`;
 
 -- test justify_interval()
 
-SELECT justify_interval(interval '1 month -1 hour') as "1 month -1 hour";
+SELECT justify_interval(interval '1 month -1 hour') as `1 month -1 hour`;
 
 -- test fractional second input, and detection of duplicate units
 SET DATESTYLE = 'ISO';
@@ -224,17 +224,17 @@ SELECT interval '1 2:03.5678' minute to second(2);
 SELECT interval '1 2:03:04.5678' minute to second(2);
 
 -- test casting to restricted precision (bug #14479)
-SELECT f1, f1::INTERVAL DAY TO MINUTE AS "minutes",
-  (f1 + INTERVAL '1 month')::INTERVAL MONTH::INTERVAL YEAR AS "years"
+SELECT f1, f1::INTERVAL DAY TO MINUTE AS `minutes`,
+  (f1 + INTERVAL '1 month')::INTERVAL MONTH::INTERVAL YEAR AS `years`
   FROM interval_tbl;
 
 -- test inputting and outputting SQL standard interval literals
 SET IntervalStyle TO sql_standard;
-SELECT  interval '0'                       AS "zero",
-        interval '1-2' year to month       AS "year-month",
-        interval '1 2:03:04' day to second AS "day-time",
-        - interval '1-2'                   AS "negative year-month",
-        - interval '1 2:03:04'             AS "negative day-time";
+SELECT  interval '0'                       AS zero,
+        interval '1-2' year to month       AS `year-month`,
+        interval '1 2:03:04' day to second AS `day-time`,
+        - interval '1-2'                   AS `negative year-month`,
+        - interval '1 2:03:04'             AS `negative day-time`;
 
 -- test input of some not-quite-standard interval values in the sql style
 SET IntervalStyle TO postgres;
@@ -252,38 +252,38 @@ SELECT  interval '1 day -1 hours',
 
 -- test outputting iso8601 intervals
 SET IntervalStyle to iso_8601;
-select  interval '0'                                AS "zero",
-        interval '1-2'                              AS "a year 2 months",
-        interval '1 2:03:04'                        AS "a bit over a day",
-        interval '2:03:04.45679'                    AS "a bit over 2 hours",
-        (interval '1-2' + interval '3 4:05:06.7')   AS "all fields",
-        (interval '1-2' - interval '3 4:05:06.7')   AS "mixed sign",
-        (- interval '1-2' + interval '3 4:05:06.7') AS "negative";
+select  interval '0'                                AS zero,
+        interval '1-2'                              AS `a year 2 months`,
+        interval '1 2:03:04'                        AS `a bit over a day`,
+        interval '2:03:04.45679'                    AS `a bit over 2 hours`,
+        (interval '1-2' + interval '3 4:05:06.7')   AS `all fields`,
+        (interval '1-2' - interval '3 4:05:06.7')   AS `mixed sign`,
+        (- interval '1-2' + interval '3 4:05:06.7') AS negative;
 
 -- test inputting ISO 8601 4.4.2.1 "Format With Time Unit Designators"
 SET IntervalStyle to sql_standard;
-select  interval 'P0Y'                    AS "zero",
-        interval 'P1Y2M'                  AS "a year 2 months",
-        interval 'P1W'                    AS "a week",
-        interval 'P1DT2H3M4S'             AS "a bit over a day",
-        interval 'P1Y2M3DT4H5M6.7S'       AS "all fields",
-        interval 'P-1Y-2M-3DT-4H-5M-6.7S' AS "negative",
-        interval 'PT-0.1S'                AS "fractional second";
+select  interval 'P0Y'                    AS zero,
+        interval 'P1Y2M'                  AS `a year 2 months`,
+        interval 'P1W'                    AS `a week`,
+        interval 'P1DT2H3M4S'             AS `a bit over a day`,
+        interval 'P1Y2M3DT4H5M6.7S'       AS `all fields`,
+        interval 'P-1Y-2M-3DT-4H-5M-6.7S' AS negative,
+        interval 'PT-0.1S'                AS `fractional second`;
 
 -- test inputting ISO 8601 4.4.2.2 "Alternative Format"
 SET IntervalStyle to postgres;
-select  interval 'P00021015T103020'       AS "ISO8601 Basic Format",
-        interval 'P0002-10-15T10:30:20'   AS "ISO8601 Extended Format";
+select  interval 'P00021015T103020'       AS `ISO8601 Basic Format`,
+        interval 'P0002-10-15T10:30:20'   AS `ISO8601 Extended Format`;
 
 -- Make sure optional ISO8601 alternative format fields are optional.
-select  interval 'P0002'                  AS "year only",
-        interval 'P0002-10'               AS "year month",
-        interval 'P0002-10-15'            AS "year month day",
-        interval 'P0002T1S'               AS "year only plus time",
-        interval 'P0002-10T1S'            AS "year month plus time",
-        interval 'P0002-10-15T1S'         AS "year month day plus time",
-        interval 'PT10'                   AS "hour only",
-        interval 'PT10:30'                AS "hour minute";
+select  interval 'P0002'                  AS `year only`,
+        interval 'P0002-10'               AS `year month`,
+        interval 'P0002-10-15'            AS `year month day`,
+        interval 'P0002T1S'               AS `year only plus time`,
+        interval 'P0002-10T1S'            AS `year month plus time`,
+        interval 'P0002-10-15T1S'         AS `year month day plus time`,
+        interval 'PT10'                   AS `hour only`,
+        interval 'PT10:30'                AS `hour minute`;
 
 -- test a couple rounding cases that changed since 8.3 w/ HAVE_INT64_TIMESTAMP.
 SET IntervalStyle to postgres_verbose;
