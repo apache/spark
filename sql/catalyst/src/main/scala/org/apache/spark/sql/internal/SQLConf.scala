b3eba2949366d46a2508f3a97a16bccb89249253
gwang3 <gwang3@ebay.com>
2019-10-08 15:44:42 -0700

    [SPARK-29189][FOLLOW-UP][SQL] Beautify config name
    ### What changes were proposed in this pull request?
    Beautify comment
    
    ### Why are the changes needed?
    The config name now is pretty weird.
    
    ### Does this PR introduce any user-facing change?
    No
    
    ### How was this patch tested?
    No test.
    
    Closes #26054 from wangshisan/SPARK-29189.
    
    Authored-by: gwang3 <gwang3@ebay.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -833,7 +833,7 @@ object SQLConf {
       .createWithDefault(10000)
 
   val IGNORE_DATA_LOCALITY =
-    buildConf("spark.sql.sources.ignore.datalocality")
+    buildConf("spark.sql.sources.ignoreDataLocality.enabled")
       .doc("If true, Spark will not fetch the block locations for each file on " +
         "listing files. This speeds up file listing, but the scheduler cannot " +
         "schedule tasks to take advantage of data locality. It can be particularly " +
