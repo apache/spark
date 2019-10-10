cc7493fa21fcad67a15fcf14a02d29b90c71c068
Sean Owen <sean.owen@databricks.com>
2019-10-09 17:00:48 -0700

    [SPARK-29416][CORE][ML][SQL][MESOS][TESTS] Use .sameElements to compare arrays, instead of .deep (gone in 2.13)
    ### What changes were proposed in this pull request?
    
    Use `.sameElements` to compare (non-nested) arrays, as `Arrays.deep` is removed in 2.13 and wasn't the best way to do this in the first place.
    
    ### Why are the changes needed?
    
    To compile with 2.13.
    
    ### Does this PR introduce any user-facing change?
    
    None.
    
    ### How was this patch tested?
    
    Existing tests.
    
    Closes #26073 from srowen/SPARK-29416.
    
    Authored-by: Sean Owen <sean.owen@databricks.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- resource-managers/mesos/src/test/scala/org/apache/spark/scheduler/cluster/mesos/MesosSchedulerUtilsSuite.scala
+++ resource-managers/mesos/src/test/scala/org/apache/spark/scheduler/cluster/mesos/MesosSchedulerUtilsSuite.scala
@@ -63,12 +63,12 @@ class MesosSchedulerUtilsSuite extends SparkFunSuite with Matchers with MockitoS
 
   def arePortsEqual(array1: Array[(Long, Long)], array2: Array[(Long, Long)])
     : Boolean = {
-    array1.sortBy(identity).deep == array2.sortBy(identity).deep
+    array1.sortBy(identity).sameElements(array2.sortBy(identity))
   }
 
   def arePortsEqual(array1: Array[Long], array2: Array[Long])
     : Boolean = {
-    array1.sortBy(identity).deep == array2.sortBy(identity).deep
+    array1.sortBy(identity).sameElements(array2.sortBy(identity))
   }
 
   def getRangesFromResources(resources: List[Resource]): List[(Long, Long)] = {
