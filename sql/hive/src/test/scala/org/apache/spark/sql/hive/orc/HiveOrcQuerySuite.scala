28c49e7a5d81e3bca2c21ca285e8a1ab837b3bb2
Gengliang Wang <gengliang.wang@databricks.com>
2019-10-14 14:58:14 +0200

    fix more failures

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/orc/HiveOrcQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/orc/HiveOrcQuerySuite.scala
@@ -212,6 +212,7 @@ class HiveOrcQuerySuite extends OrcQueryTest with TestHiveSingleton {
 
   // SPARK-28885 String value is not allowed to be stored as numeric type with
   // ANSI store assignment policy.
+  // TODO: re-enable the test case when SPARK-29462 is fixed.
   ignore("SPARK-23340 Empty float/double array columns raise EOFException") {
     withSQLConf(HiveUtils.CONVERT_METASTORE_ORC.key -> "false") {
       withTable("spark_23340") {
