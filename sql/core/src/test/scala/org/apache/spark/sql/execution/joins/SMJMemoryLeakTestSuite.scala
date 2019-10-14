830adffe6d8eca65fe0aabf629043a3dbd06bd6b
Min Shen <mshen@linkedin.com>
2019-10-14 10:16:55 -0700

    Fix Scala style issue

--- sql/core/src/test/scala/org/apache/spark/sql/execution/joins/SMJMemoryLeakTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/joins/SMJMemoryLeakTestSuite.scala
@@ -39,7 +39,8 @@ class SMJMemoryLeakTestSuite extends SparkPlanTest with SharedSQLContext {
     require(input.length > 0 && n > 0)
     n match {
       case 1 => for (el <- input) yield List(el)
-      case _ => for (el <- input; perm <- permutationsWithRepetitions(input, n - 1)) yield el :: perm
+      case _ =>
+        for (el <- input; perm <- permutationsWithRepetitions(input, n - 1)) yield el :: perm
     }
   }
 
