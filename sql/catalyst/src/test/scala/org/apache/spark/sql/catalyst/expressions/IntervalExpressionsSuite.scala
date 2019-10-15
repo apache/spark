b4dc59ac6eb141c4abd909faeb1b6eef6d765299
Maxim Gekk <max.gekk@gmail.com>
2019-10-15 12:39:01 +0300

    Remove unused import

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/IntervalExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/IntervalExpressionsSuite.scala
@@ -17,8 +17,6 @@
 
 package org.apache.spark.sql.catalyst.expressions
 
-import scala.language.implicitConversions
-
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.unsafe.types.CalendarInterval.fromString
 
