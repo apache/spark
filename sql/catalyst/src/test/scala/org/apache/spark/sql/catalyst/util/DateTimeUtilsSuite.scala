17edc79d220ba29fe768cc3a5ed5930bcecbded9
Rahul Mahadev <rahul.mahadev@databricks.com>
2019-10-16 11:19:34 -0700

    Removed println

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/util/DateTimeUtilsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/util/DateTimeUtilsSuite.scala
@@ -458,7 +458,6 @@ class DateTimeUtilsSuite extends SparkFunSuite with Matchers {
 
   test("trailing characters while converting string to timestamp") {
     val s = UTF8String.fromString("2019-10-31T10:59:23Z:::")
-    println(s)
     val time = DateTimeUtils.stringToTimestamp(s, defaultZoneId)
     assert(time == None)
   }
