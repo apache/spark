23c01c53185574ef50adc0e2c42b63ec22775c5d
Rahul Mahadev <rahul.mahadev@databricks.com>
2019-10-16 11:02:39 -0700

    Fix for ArrayOutofBoundsException while converting string to timestamp

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DateTimeUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DateTimeUtils.scala
@@ -286,7 +286,7 @@ object DateTimeUtils {
             i += 1
           }
         } else {
-          if (b == ':' || b == ' ') {
+          if (i < 9 && (b == ':' || b == ' ')) {
             segments(i) = currentSegmentValue
             currentSegmentValue = 0
             i += 1
