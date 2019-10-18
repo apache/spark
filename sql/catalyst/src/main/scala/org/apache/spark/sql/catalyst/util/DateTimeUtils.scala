a25d8b441f81d1a8650c3f28378b687bf64e18ae
Rahul Mahadev <rahul.mahadev@databricks.com>
2019-10-18 15:50:16 -0700

    Fixed ArrayIndexOutOfBounds when converting from string to timestamp (2.4)

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DateTimeUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DateTimeUtils.scala
@@ -378,7 +378,7 @@ object DateTimeUtils {
             i += 1
           }
         } else {
-          if (b == ':' || b == ' ') {
+          if (i < 9 && (b == ':' || b == ' ')) {
             segments(i) = currentSegmentValue
             currentSegmentValue = 0
             i += 1
