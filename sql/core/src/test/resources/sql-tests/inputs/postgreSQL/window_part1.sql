6cd07c8a45f53f8aeb44915cce72aa623cff5501
DylanGuedes <djmgguedes@gmail.com>
2019-10-21 21:44:10 -0300

    change how to insert values
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- sql/core/src/test/resources/sql-tests/inputs/postgreSQL/window_part1.sql
+++ sql/core/src/test/resources/sql-tests/inputs/postgreSQL/window_part1.sql
@@ -12,17 +12,16 @@ CREATE TABLE empsalary (
     enroll_date date
 ) USING parquet;
 
-INSERT INTO empsalary VALUES
-('develop', 10, 5200, '2007-08-01'),
-('sales', 1, 5000, '2006-10-01'),
-('personnel', 5, 3500, '2007-12-10'),
-('sales', 4, 4800, '2007-08-08'),
-('personnel', 2, 3900, '2006-12-23'),
-('develop', 7, 4200, '2008-01-01'),
-('develop', 9, 4500, '2008-01-01'),
-('sales', 3, 4800, '2007-08-01'),
-('develop', 8, 6000, '2006-10-01'),
-('develop', 11, 5200, '2007-08-15');
+INSERT INTO empsalary VALUES ('develop', 10, 5200, '2007-08-01');
+INSERT INTO empsalary VALUES ('sales', 1, 5000, '2006-10-01');
+INSERT INTO empsalary VALUES ('personnel', 5, 3500, '2007-12-10');
+INSERT INTO empsalary VALUES ('sales', 4, 4800, '2007-08-08');
+INSERT INTO empsalary VALUES ('personnel', 2, 3900, '2006-12-23');
+INSERT INTO empsalary VALUES ('develop', 7, 4200, '2008-01-01');
+INSERT INTO empsalary VALUES ('develop', 9, 4500, '2008-01-01');
+INSERT INTO empsalary VALUES ('sales', 3, 4800, '2007-08-01');
+INSERT INTO empsalary VALUES ('develop', 8, 6000, '2006-10-01');
+INSERT INTO empsalary VALUES ('develop', 11, 5200, '2007-08-15');
 
 SELECT depname, empno, salary, sum(salary) OVER (PARTITION BY depname) FROM empsalary ORDER BY depname, salary;
 
