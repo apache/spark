3fb819999438ececc8752927bfc3afcfcade279c
Kent Yao <yaooqinn@hotmail.com>
2019-10-23 10:56:44 +0800

    rm example to fix build

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/misc.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/misc.scala
@@ -167,21 +167,13 @@ case class Uuid(randomSeed: Option[Long] = None) extends LeafExpression with Sta
 // scalastyle:off line.size.limit
 @ExpressionDescription(
   usage =
-    """_FUNC_() - Returns the Spark version. The string contains 2 fields, the first being a release or snapshot version and the second being a git revision.""".stripMargin,
-  examples = s"""
-    Examples:
-      > SELECT _FUNC_();
-       ${SPARK_VERSION + " " + SPARK_REVISION}
-  """,
+    """_FUNC_() - Returns the Spark version. The string contains 2 fields, the first being a release or snapshot version and the second being a git revision.""",
   since = """3.0.0""")
 // scalastyle:on line.size.limit
 case class Version() extends LeafExpression with CodegenFallback {
   override def nullable: Boolean = false
-
   override def dataType: DataType = StringType
-
   override def eval(input: InternalRow): Any = {
     UTF8String.fromString(SPARK_VERSION + " " + SPARK_REVISION)
   }
-
 }
