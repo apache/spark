dcf5eaf1a6c0330a9460e168c1c3fee21998ba65
stczwd <qcsd2011@163.com>
2019-10-24 10:25:04 -0700

    [SPARK-29444][FOLLOWUP] add doc and python parameter for ignoreNullFields in json generating
    # What changes were proposed in this pull request?
    Add description for ignoreNullFields, which is commited in #26098 , in DataFrameWriter and readwriter.py.
    Enable user to use ignoreNullFields in pyspark.
    
    ### Does this PR introduce any user-facing change?
    No
    
    ### How was this patch tested?
    run unit tests
    
    Closes #26227 from stczwd/json-generator-doc.
    
    Authored-by: stczwd <qcsd2011@163.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- python/pyspark/sql/readwriter.py
+++ python/pyspark/sql/readwriter.py
@@ -788,7 +788,7 @@ class DataFrameWriter(OptionUtils):
 
     @since(1.4)
     def json(self, path, mode=None, compression=None, dateFormat=None, timestampFormat=None,
-             lineSep=None, encoding=None):
+             lineSep=None, encoding=None, ignoreNullFields=None):
         """Saves the content of the :class:`DataFrame` in JSON format
         (`JSON Lines text format or newline-delimited JSON <http://jsonlines.org/>`_) at the
         specified path.
@@ -817,13 +817,15 @@ class DataFrameWriter(OptionUtils):
                         the default UTF-8 charset will be used.
         :param lineSep: defines the line separator that should be used for writing. If None is
                         set, it uses the default value, ``\\n``.
+        :param ignoreNullFields: Whether to ignore null fields when generating JSON objects.
+                        If None is set, it uses the default value, ``true``.
 
         >>> df.write.json(os.path.join(tempfile.mkdtemp(), 'data'))
         """
         self.mode(mode)
         self._set_opts(
             compression=compression, dateFormat=dateFormat, timestampFormat=timestampFormat,
-            lineSep=lineSep, encoding=encoding)
+            lineSep=lineSep, encoding=encoding, ignoreNullFields=ignoreNullFields)
         self._jwrite.json(path)
 
     @since(1.4)
