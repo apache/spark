5e72d95c819f91fcc7ae04645593f752583c78ba
Xingbo Jiang <xingbo.jiang@databricks.com>
2019-10-24 21:01:54 +0200

    update

--- R/pkg/R/sparkR.R
+++ R/pkg/R/sparkR.R
@@ -336,8 +336,8 @@ sparkR.session <- function(
 
   # Check if version number of SparkSession matches version number of SparkR package
   jvmVersion <- callJMethod(sparkSession, "version")
-  # Remove -SNAPSHOT from jvm versions
-  jvmVersionStrip <- gsub("-SNAPSHOT", "", jvmVersion)
+  # Remove postfix from jvm versions
+  jvmVersionStrip <- gsub("-preview", "", jvmVersion)
   rPackageVersion <- paste0(packageVersion("SparkR"))
 
   if (jvmVersionStrip != rPackageVersion) {
