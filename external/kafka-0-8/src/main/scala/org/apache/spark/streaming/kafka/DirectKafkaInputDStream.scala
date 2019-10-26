c05caee6a2ba0ed2a1b71c54c4be5b4ef880f65a
dengziming <dengziming@growingio.com>
2019-10-26 17:38:40 +0800

    Optimize the display of Kafka metadata and sort by the number of messages

--- external/kafka-0-8/src/main/scala/org/apache/spark/streaming/kafka/DirectKafkaInputDStream.scala
+++ external/kafka-0-8/src/main/scala/org/apache/spark/streaming/kafka/DirectKafkaInputDStream.scala
@@ -167,9 +167,10 @@ class DirectKafkaInputDStream[
     val description = offsetRanges.filter { offsetRange =>
       // Don't display empty ranges.
       offsetRange.fromOffset != offsetRange.untilOffset
-    }.map { offsetRange =>
+    }.toSeq.sortBy(-_.count()).map { offsetRange =>
       s"topic: ${offsetRange.topic}\tpartition: ${offsetRange.partition}\t" +
-        s"offsets: ${offsetRange.fromOffset} to ${offsetRange.untilOffset}"
+        s"offsets: ${offsetRange.fromOffset} to ${offsetRange.untilOffset}\t" +
+        s"count: ${offsetRange.count()}"
     }.mkString("\n")
     // Copy offsetRanges to immutable.List to prevent from being modified by the user
     val metadata = Map(
