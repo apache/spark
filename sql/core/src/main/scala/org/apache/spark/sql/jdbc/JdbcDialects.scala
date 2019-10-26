404bb34e9a3fa8f432bce62823dcde07bdbbdb2f
yong.tian1 <yong.tian1@dmall.com>
2019-10-26 09:38:17 +0800

    jdbc dialect matching is not case sensitive

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/JdbcDialects.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/JdbcDialects.scala
@@ -58,7 +58,7 @@ case class JdbcType(databaseTypeDefinition : String, jdbcNullType : Int)
 @DeveloperApi
 abstract class JdbcDialect extends Serializable {
   /**
-   * The tag of the database, it must be lowercase，the subsequent will match
+   * The tag of the database, it must be lowercase, the subsequent will match
    * the corresponding dialect through this tag, so it must be strictly one-to-one
    * correspondence with jdbc url,such as:
    * mysql  url:  jdbc:mysql://localhost/db  dbTag: mysql
