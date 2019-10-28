94f27fd521e1c137d543bc528740d40bec6fb07b
Thomas Graves <tgraves@nvidia.com>
2019-10-28 08:46:00 -0500

    make ExecutorResourceRequest private for now

--- core/src/main/scala/org/apache/spark/resource/ExecutorResourceRequest.scala
+++ core/src/main/scala/org/apache/spark/resource/ExecutorResourceRequest.scala
@@ -33,7 +33,7 @@ package org.apache.spark.resource
  * This api is currently private until the rest of the pieces are in place and then it
  * will become public.
  */
-class ExecutorResourceRequest(
+private[spark] class ExecutorResourceRequest(
     val resourceName: String,
     val amount: Int,
     val units: String = "",
