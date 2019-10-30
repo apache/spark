4eb35376683463c83f4e9e94b41a05ec86583cc4
Matt Stillwell <18670089+mstill3@users.noreply.github.com>
2019-10-30 18:23:21 -0400

    Change the syntax back to how it was

--- python/pyspark/sql/session.py
+++ python/pyspark/sql/session.py
@@ -151,9 +151,9 @@ class SparkSession(object):
             new one based on the options set in this builder.
 
             This method first checks whether there is a valid global default SparkSession, and if
-            there is, return that one. Otherwise if no valid global default SparkSession exists,
-            then the method creates a new SparkSession and assigns the newly created SparkSession
-            as the global default.
+            yes, return that one. If no valid global default SparkSession exists, the method
+            creates a new SparkSession and assigns the newly created SparkSession as the global
+            default.
 
             >>> s1 = SparkSession.builder.config("k1", "v1").getOrCreate()
             >>> s1.conf.get("k1") == "v1"
