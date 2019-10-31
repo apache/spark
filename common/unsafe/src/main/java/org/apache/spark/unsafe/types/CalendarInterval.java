e963e4a4fd3ca54d379020bc7a4a4e85a8c68af7
Kent Yao <yaooqinn@hotmail.com>
2019-10-31 13:36:13 +0800

    [SPARK-29679][SQL] Make interval type camparable and orderable

--- common/unsafe/src/main/java/org/apache/spark/unsafe/types/CalendarInterval.java
+++ common/unsafe/src/main/java/org/apache/spark/unsafe/types/CalendarInterval.java
@@ -24,10 +24,12 @@ import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
+import scala.math.Ordered;
+
 /**
  * The internal representation of interval type.
  */
-public final class CalendarInterval implements Serializable {
+public final class CalendarInterval implements Serializable, Ordered<CalendarInterval> {
   public static final long MICROS_PER_MILLI = 1000L;
   public static final long MICROS_PER_SECOND = MICROS_PER_MILLI * 1000;
   public static final long MICROS_PER_MINUTE = MICROS_PER_SECOND * 60;
@@ -80,6 +82,23 @@ public final class CalendarInterval implements Serializable {
   @Override
   public int hashCode() {
     return Objects.hash(months, days, microseconds);
+  }
+
+  @Override
+  public int compare(CalendarInterval that) {
+    int mc = this.months - that.months;
+    if (mc == 0) {
+      long mic = this.microseconds - that.microseconds;
+      if (mic == 0) {
+        return 0;
+      } else if (mic > 0) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      return mc;
+    }
   }
 
   @Override
---
+++ sql/core/src/test/resources/sql-tests/inputs/interval.sql
@@ -0,0 +1,23 @@
+-- test for intervals
+
+-- greater than or equal
+select interval '1 day' > interval '23 hour';
+select interval '-1 day' >= interval '-23 hour';
+select interval '-1 day' > null;
+select null > interval '-1 day';
+
+-- less than or equal
+select interval '1 minutes' < interval '1 hour';
+select interval '-1 day' >= interval '-23 hour';
+
+-- equal
+select interval '1 minutes' = interval '1 hour';
+select interval '1 minutes' = null;
+select null = interval '-1 day';
+
+-- null safe equal
+select interval '1 minutes' <=> null;
+select null <=> interval '1 minutes';
+
+-- complex interval representation
+select INTERVAL '9 years 1 months -1 weeks -4 days -10 hours -46 minutes' > interval '1 minutes';
