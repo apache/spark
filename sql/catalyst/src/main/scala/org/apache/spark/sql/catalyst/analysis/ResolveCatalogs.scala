4a7ff28ec95a288973ade3dad8e0bc6d4fbde265
Terry Kim <yuminkim@gmail.com>
2019-10-31 10:47:43 +0800

    [SPARK-29592][SQL] ALTER TABLE (set partition location) should look up catalog/table like v2 commands
    ### What changes were proposed in this pull request?
    
    Update `AlterTableSetLocationStatement` to store `partitionSpec` and make `ALTER TABLE a.b.c PARTITION(...) SET LOCATION 'loc'` fail if `partitionSpec` is set with unsupported message.
    
    ### Why are the changes needed?
    
    It's important to make all the commands have the same table resolution behavior, to avoid confusing end-users. e.g.
    
    ```
    USE my_catalog
    DESC t // success and describe the table t from my_catalog
    ALTER TABLE t PARTITION(...) SET LOCATION 'loc' // report set location with partition spec is not supported.
    ```
    ### Does this PR introduce any user-facing change?
    
    yes. When running ALTER TABLE (set partition location), Spark fails the command if the current catalog is set to a v2 catalog, or the table name specified a v2 catalog.
    
    ### How was this patch tested?
    
    New unit tests
    
    Closes #26304 from imback82/alter_table_partition_loc.
    
    Authored-by: Terry Kim <yuminkim@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveCatalogs.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveCatalogs.scala
@@ -73,7 +73,11 @@ class ResolveCatalogs(val catalogManager: CatalogManager)
       createAlterTable(nameParts, catalog, tableName, changes)
 
     case AlterTableSetLocationStatement(
-         nameParts @ NonSessionCatalog(catalog, tableName), newLoc) =>
+         nameParts @ NonSessionCatalog(catalog, tableName), partitionSpec, newLoc) =>
+      if (partitionSpec.nonEmpty) {
+        throw new AnalysisException(
+          "ALTER TABLE SET LOCATION does not support partition for v2 tables.")
+      }
       val changes = Seq(TableChange.setProperty("location", newLoc))
       createAlterTable(nameParts, catalog, tableName, changes)
 
