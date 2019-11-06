5b7fad93edef8f02bbe96824dd73036eaddc1ec5
Aman Omer <amanomer1996@gmail.com>
2019-11-06 18:39:46 +0900

    [SPARK-29462] The data type of "array()" should be array<null>
    During creation of array, if CreateArray does not gets any children to set data type for array, it will create an array of null type .
    
    When empty array is created, it should be declared as array<null>.
    
    No
    
    Tested manually
    
    Closes #26324 from amanomer/29462.
    
    Authored-by: Aman Omer <amanomer1996@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -3499,12 +3499,9 @@ class DataFrameFunctionsSuite extends QueryTest with SharedSparkSession {
     ).foreach(assertValuesDoNotChangeAfterCoalesceOrUnion(_))
   }
 
-  test("SPARK-21281 use string types by default if array and map have no argument") {
+  test("SPARK-21281 use string types by default if map have no argument") {
     val ds = spark.range(1)
     var expectedSchema = new StructType()
-      .add("x", ArrayType(StringType, containsNull = false), nullable = false)
-    assert(ds.select(array().as("x")).schema == expectedSchema)
-    expectedSchema = new StructType()
       .add("x", MapType(StringType, StringType, valueContainsNull = false), nullable = false)
     assert(ds.select(map().as("x")).schema == expectedSchema)
   }
@@ -3576,6 +3573,13 @@ class DataFrameFunctionsSuite extends QueryTest with SharedSparkSession {
       df.selectExpr("like(str, pattern, escape)").collect()
     }.getMessage
     assert(nonFoldableError.contains("The 'escape' parameter must be a string literal"))
+  }
+
+  test("SPARK-29462: Use null type by default if array have no argument") {
+    val ds = spark.range(1)
+    var expectedSchema = new StructType()
+      .add("x", ArrayType(NullType, containsNull = false), nullable = false)
+    assert(ds.select(array().as("x")).schema == expectedSchema)
   }
 }
 
