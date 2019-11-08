e026412d9c77ceade19da9b51859f5d9b0eb0634
Kent Yao <yaooqinn@hotmail.com>
2019-11-08 22:45:11 +0800

    [SPARK-29679][SQL] Make interval type comparable and orderable
    ### What changes were proposed in this pull request?
    
    interval type support >, >=, <, <=, =, <=>, order by, min,max..
    
    ### Why are the changes needed?
    
    Part of SPARK-27764 Feature Parity between PostgreSQL and Spark
    ### Does this PR introduce any user-facing change?
    
    yes, we now support compare intervals
    
    ### How was this patch tested?
    
    add ut
    
    Closes #26337 from yaooqinn/SPARK-29679.
    
    Authored-by: Kent Yao <yaooqinn@hotmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

---
+++ sql/core/src/test/resources/sql-tests/inputs/interval.sql
@@ -0,0 +1,43 @@
+-- test for intervals
+
+-- greater than or equal
+select interval '1 day' > interval '23 hour';
+select interval '-1 day' >= interval '-23 hour';
+select interval '-1 day' > null;
+select null > interval '-1 day';
+
+-- less than or equal
+select interval '1 minutes' < interval '1 hour';
+select interval '-1 day' <= interval '-23 hour';
+
+-- equal
+select interval '1 year' = interval '360 days';
+select interval '1 year 2 month' = interval '420 days';
+select interval '1 year' = interval '365 days';
+select interval '1 month' = interval '30 days';
+select interval '1 minutes' = interval '1 hour';
+select interval '1 minutes' = null;
+select null = interval '-1 day';
+
+-- null safe equal
+select interval '1 minutes' <=> null;
+select null <=> interval '1 minutes';
+
+-- complex interval representation
+select INTERVAL '9 years 1 months -1 weeks -4 days -10 hours -46 minutes' > interval '1 minutes';
+
+-- ordering
+select cast(v as interval) i from VALUES ('1 seconds'), ('4 seconds'), ('3 seconds') t(v) order by i;
+
+-- unlimited days
+select interval '1 month 120 days' > interval '2 month';
+select interval '1 month 30 days' = interval '2 month';
+
+-- unlimited microseconds
+select interval '1 month 29 days 40 hours' > interval '2 month';
+
+-- max
+select max(cast(v as interval)) from VALUES ('1 seconds'), ('4 seconds'), ('3 seconds') t(v);
+
+-- min
+select min(cast(v as interval)) from VALUES ('1 seconds'), ('4 seconds'), ('3 seconds') t(v);
