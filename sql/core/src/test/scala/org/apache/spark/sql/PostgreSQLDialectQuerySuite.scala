53c9a9bb66accb289af1a9c7163519fe72c3b85a
wuyi <ngone_5451@163.com>
2019-11-11 20:56:26 +0800

    fix PostgreSQLDialectQuerySuite

--- sql/core/src/test/scala/org/apache/spark/sql/PostgreSQLDialectQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/PostgreSQLDialectQuerySuite.scala
@@ -36,7 +36,8 @@ class PostgreSQLDialectQuerySuite extends QueryTest with SharedSparkSession {
     }
 
     Seq("o", "abc", "").foreach { input =>
-      checkAnswer(sql(s"select cast('$input' as boolean)"), Row(null))
+      intercept[IllegalArgumentException](
+        checkAnswer(sql(s"select cast('$input' as boolean)"), Row(null)))
     }
   }
 }
