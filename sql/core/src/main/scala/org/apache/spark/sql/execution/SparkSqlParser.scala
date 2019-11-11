0ae26a627060c576d9daea23bd2eb17e4ec81b55
lajin <lajin@ebay.com>
2019-11-11 10:55:51 +0800

    fix upstream conflict

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkSqlParser.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkSqlParser.scala
@@ -638,7 +638,7 @@ class SparkSqlAstBuilder(conf: SQLConf) extends AstBuilder(conf) {
   override def visitCreateTableLike(ctx: CreateTableLikeContext): LogicalPlan = withOrigin(ctx) {
     val targetTable = visitTableIdentifier(ctx.target)
     val sourceTable = visitTableIdentifier(ctx.source)
-    val provider = Option(ctx.tableProvider).map(_.qualifiedName.getText)
+    val provider = Option(ctx.tableProvider).map(_.multipartIdentifier.getText)
     val location = Option(ctx.locationSpec).map(visitLocationSpec)
     CreateTableLikeCommand(targetTable, sourceTable, provider, location, ctx.EXISTS != null)
   }
