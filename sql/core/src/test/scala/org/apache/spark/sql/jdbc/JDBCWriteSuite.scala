eda6360354b3d32453672ccdb368053516c8c285
shivsood <shivsood@microsoft.com>
2019-11-17 07:06:50 -0600

    [SPARK-29644][SQL][2.4] Corrected ShortType and ByteType mapping to SmallInt and TinyInt in JDBCUtils
    This is a port SPARK-29644 to 2.4
    
    ### What changes were proposed in this pull request?
    
    Corrected ShortType and ByteType mapping to SmallInt and TinyInt, corrected setter methods to set ShortType and ByteType as setShort() and setByte(). Changes in JDBCUtils.scala
    Fixed Unit test cases to where applicable and added new E2E test cases in to test table read/write using ShortType and ByteType.
    
    Problems
    - In master in JDBCUtils.scala line number 547 and 551 have a problem where ShortType and ByteType are set as Integers rather than set as Short and Byte respectively.
    
    ```
    case ShortType =>
        (stmt: PreparedStatement, row: Row, pos: Int) =>
        stmt.setInt(pos + 1, row.getShort(pos))
        The issue was pointed out by maropu
    
    case ByteType =>
        (stmt: PreparedStatement, row: Row, pos: Int) =>
         stmt.setInt(pos + 1, row.getByte(pos))
    ```
    
    - Also at line JDBCUtils.scala 247 TinyInt is interpreted wrongly as IntergetType in getCatalystType()
    ``` case java.sql.Types.TINYINT => IntegerType ```
    
    - At line 172 ShortType was wrongly interpreted as IntegerType
    ``` case ShortType => Option(JdbcType("INTEGER", java.sql.Types.SMALLINT)) ```
    
    - All thru out tests, ShortType and ByteType were being interpreted as IntegerTypes.
    
    ### Why are the changes needed?
    Given type should be set using the right type.
    
    ### Does this PR introduce any user-facing change?
    Yes.
    - User will now be able to create tables where dataframe contains ByteType when using JDBC connector in overwrite mode.
    - Users will see a SQL side table are created with the right data type. ShortType in spark will translate to smallint and ByteType to TinyInt on the SQL side. This will resulting in small size of db tables where applicable.
    
    ### How was this patch tested?
    Corrected Unit test cases where applicable. Validated in CI/CD
    Added/fixed test case in MsSqlServerIntegrationSuite.scala, PostgresIntegrationSuite.scala , MySQLIntegrationSuite.scala to
    write/read tables from dataframe with cols as shorttype and bytetype. Validated by manual as follows.
    ```
    ./build/mvn install -DskipTests
    ./build/mvn test -Pdocker-integration-tests -pl :spark-docker-integration-tests_2.12
    ```
    
    Closes #26549 from shivsood/port_29644_2.4.
    
    Authored-by: shivsood <shivsood@microsoft.com>
    Signed-off-by: Sean Owen <sean.owen@databricks.com>

--- sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCWriteSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCWriteSuite.scala
@@ -543,4 +543,46 @@ class JDBCWriteSuite extends SharedSQLContext with BeforeAndAfter {
     }.getMessage
     assert(errMsg.contains("Statement was canceled or the session timed out"))
   }
+
+  test("SPARK-29644: Write tables with ShortType") {
+    import testImplicits._
+    val df = Seq(-32768.toShort, 0.toShort, 1.toShort, 38.toShort, 32768.toShort).toDF("a")
+    val tablename = "shorttable"
+    df.write
+      .format("jdbc")
+      .mode("overwrite")
+      .option("url", url)
+      .option("dbtable", tablename)
+      .save()
+    val df2 = spark.read
+      .format("jdbc")
+      .option("url", url)
+      .option("dbtable", tablename)
+      .load()
+    assert(df.count == df2.count)
+    val rows = df2.collect()
+    val colType = rows(0).toSeq.map(x => x.getClass.toString)
+    assert(colType(0) == "class java.lang.Short")
+  }
+
+  test("SPARK-29644: Write tables with ByteType") {
+    import testImplicits._
+    val df = Seq(-127.toByte, 0.toByte, 1.toByte, 38.toByte, 128.toByte).toDF("a")
+    val tablename = "bytetable"
+    df.write
+      .format("jdbc")
+      .mode("overwrite")
+      .option("url", url)
+      .option("dbtable", tablename)
+      .save()
+    val df2 = spark.read
+      .format("jdbc")
+      .option("url", url)
+      .option("dbtable", tablename)
+      .load()
+    assert(df.count == df2.count)
+    val rows = df2.collect()
+    val colType = rows(0).toSeq.map(x => x.getClass.toString)
+    assert(colType(0) == "class java.lang.Byte")
+  }
 }
