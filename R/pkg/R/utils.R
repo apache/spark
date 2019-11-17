6d9d3f158fd84323e122e6917e1c30dcdde5c635
Dongjoon Hyun <dhyun@apple.com>
2019-11-17 15:57:19 -0800

    update

--- R/pkg/R/utils.R
+++ R/pkg/R/utils.R
@@ -750,7 +750,9 @@ launchScript <- function(script, combinedArgs, wait = FALSE, stdout = "", stderr
   if (.Platform$OS.type == "windows") {
     scriptWithArgs <- paste(script, combinedArgs, sep = " ")
     # on Windows, intern = F seems to mean output to the console. (documentation on this is missing)
-    shell(scriptWithArgs, translate = TRUE, wait = wait, intern = wait) # nolint
+    # nolint start
+    shell(scriptWithArgs, translate = TRUE, wait = wait, intern = wait)
+    # nolint end
   } else {
     # http://stat.ethz.ch/R-manual/R-devel/library/base/html/system2.html
     # stdout = F means discard output
