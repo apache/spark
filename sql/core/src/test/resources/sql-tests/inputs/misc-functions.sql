eb2d7e64b2f7fddd4af564a1f59182ac68f9efb0
Kent Yao <yaooqinn@hotmail.com>
2019-11-20 00:23:54 +0800

    rm cast

--- sql/core/src/test/resources/sql-tests/inputs/misc-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/misc-functions.sql
@@ -6,6 +6,6 @@ select typeof(true);
 select typeof(1Y), typeof(1S), typeof(1), typeof(1L);
 select typeof(cast(1.0 as float)), typeof(1.0D), typeof(1.2);
 select typeof(date '1986-05-23'),  typeof(timestamp '1986-05-23'), typeof(interval '23 days');
-select typeof(x'ABCD'), typeof('SPARK'), typeof(cast('SPARK' as char(10))), typeof(cast('SPARK' as varchar(10)));
+select typeof(x'ABCD'), typeof('SPARK');
 select typeof(array(1, 2)), typeof(map(1, 2));
 select typeof(struct(a, b)) from values (1, 'SPARK') tbl(a, b);
