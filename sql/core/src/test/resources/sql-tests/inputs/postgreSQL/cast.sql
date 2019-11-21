111d673348384eb1ee4d5ee05545cea7dba76cb9
Aman Omer <amanomer1996@gmail.com>
2019-11-21 16:07:00 +0530

    Test cases updated

--- sql/core/src/test/resources/sql-tests/inputs/postgreSQL/cast.sql
+++ sql/core/src/test/resources/sql-tests/inputs/postgreSQL/cast.sql
@@ -5,3 +5,6 @@ SELECT CAST(CAST(1 AS boolean) AS timestamp);
 SELECT CAST(CAST(1 AS byte) AS timestamp);
 SELECT CAST(CAST(1 AS short) AS timestamp);
 SELECT CAST(CAST(1 AS double) AS timestamp);
+SELECT CAST(CAST('2019' AS date) AS timestamp)
+SELECT CAST(NULL AS timestamp)
+SELECT CAST('2019' AS timestamp)
