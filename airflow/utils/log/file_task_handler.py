c4d5ea27eb985024833e15898015ad26339c26d9
badfeanor <38539069+badfeanor@users.noreply.github.com>
2019-11-22 11:26:45 +0300

    Add coding to fix Cyrillic output (#6631)

--- airflow/utils/log/file_task_handler.py
+++ airflow/utils/log/file_task_handler.py
@@ -125,6 +125,7 @@ class FileTaskHandler(logging.Handler):
                     pass
 
                 response = requests.get(url, timeout=timeout)
+                response.encoding = "utf-8"
 
                 # Check if the resource was properly fetched
                 response.raise_for_status()
