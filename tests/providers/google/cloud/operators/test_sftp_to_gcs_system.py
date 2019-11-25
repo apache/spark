df35957fd4bab86ca6d2c923899569fb23139f56
TobKed <tobiasz.kedzierski@polidea.com>
2019-11-25 17:25:27 +0100

    [AIRFLOW-6042] Fix folder path in SFTPToGcsExampleDagsSystemTest (#6637)

--- tests/providers/google/cloud/operators/test_sftp_to_gcs_system.py
+++ tests/providers/google/cloud/operators/test_sftp_to_gcs_system.py
@@ -20,7 +20,7 @@
 
 from tests.gcp.utils.gcp_authenticator import GCP_GCS_KEY
 from tests.providers.google.cloud.operators.test_sftp_to_gcs_system_helper import SFTPtoGcsTestHelper
-from tests.test_utils.gcp_system_helpers import GCP_DAG_FOLDER, provide_gcp_context, skip_gcp_system
+from tests.test_utils.gcp_system_helpers import CLOUD_DAG_FOLDER, provide_gcp_context, skip_gcp_system
 from tests.test_utils.system_tests_class import SystemTest
 
 
@@ -41,7 +41,7 @@ class SFTPToGcsExampleDagsSystemTest(SystemTest):
 
     @provide_gcp_context(GCP_GCS_KEY)
     def test_run_example_dag(self):
-        self.run_dag("example_sftp_to_gcs", GCP_DAG_FOLDER)
+        self.run_dag("example_sftp_to_gcs", CLOUD_DAG_FOLDER)
 
     @provide_gcp_context(GCP_GCS_KEY)
     def tearDown(self):
