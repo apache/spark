bc58197b3ba67f3c008943fea18e13a003c7cad6
Liang-Chi Hsieh <viirya@gmail.com>
2019-11-26 22:29:15 -0800

    Combine tests.

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveExternalCatalogSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveExternalCatalogSuite.scala
@@ -108,7 +108,7 @@ class HiveExternalCatalogSuite extends ExternalCatalogSuite {
     assert(bucketString.contains("10"))
   }
 
-  test("SPARK-30050: analyze table should not erase the bucketing metadata at hive side") {
+  test("SPARK-30050: analyze/rename table should not erase the bucketing metadata at hive side") {
     val catalog = newBasicCatalog()
     externalCatalog.client.runSqlHive(
       """
@@ -126,26 +126,12 @@ class HiveExternalCatalogSuite extends ExternalCatalogSuite {
     val bucketString2 = externalCatalog.client.runSqlHive("DESC FORMATTED db1.t")
       .filter(_.contains("Num Buckets")).head
     assert(bucketString2.contains("10"))
-  }
-
-  test("SPARK-30050: rename table should not erase the bucketing metadata at hive side") {
-    val catalog = newBasicCatalog()
-    externalCatalog.client.runSqlHive(
-      """
-        |CREATE TABLE db1.t(a string, b string)
-        |CLUSTERED BY (a, b) SORTED BY (a, b) INTO 10 BUCKETS
-        |STORED AS PARQUET
-      """.stripMargin)
-
-    val bucketString1 = externalCatalog.client.runSqlHive("DESC FORMATTED db1.t")
-      .filter(_.contains("Num Buckets")).head
-    assert(bucketString1.contains("10"))
 
     catalog.renameTable("db1", "t", "t2")
 
-    val bucketString2 = externalCatalog.client.runSqlHive("DESC FORMATTED db1.t2")
+    val bucketString3 = externalCatalog.client.runSqlHive("DESC FORMATTED db1.t2")
       .filter(_.contains("Num Buckets")).head
-    assert(bucketString2.contains("10"))
+    assert(bucketString3.contains("10"))
   }
 
   test("SPARK-23001: NullPointerException when running desc database") {
