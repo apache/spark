b9cae37750480ed9c00f46a592cd3910b58b081f
Kent Yao <yaooqinn@hotmail.com>
2019-12-05 22:03:44 +0800

    [SPARK-29774][SQL] Date and Timestamp type +/- null should be null as Postgres
    # What changes were proposed in this pull request?
    Add an analyzer rule to convert unresolved `Add`, `Subtract`, etc. to `TimeAdd`, `DateAdd`, etc. according to the following policy:
    ```scala
     /**
       * For [[Add]]:
       * 1. if both side are interval, stays the same;
       * 2. else if one side is interval, turns it to [[TimeAdd]];
       * 3. else if one side is date, turns it to [[DateAdd]] ;
       * 4. else stays the same.
       *
       * For [[Subtract]]:
       * 1. if both side are interval, stays the same;
       * 2. else if the right side is an interval, turns it to [[TimeSub]];
       * 3. else if one side is timestamp, turns it to [[SubtractTimestamps]];
       * 4. else if the right side is date, turns it to [[DateDiff]]/[[SubtractDates]];
       * 5. else if the left side is date, turns it to [[DateSub]];
       * 6. else turns it to stays the same.
       *
       * For [[Multiply]]:
       * 1. If one side is interval, turns it to [[MultiplyInterval]];
       * 2. otherwise, stays the same.
       *
       * For [[Divide]]:
       * 1. If the left side is interval, turns it to [[DivideInterval]];
       * 2. otherwise, stays the same.
       */
    ```
    Besides, we change datetime functions from implicit cast types to strict ones, all available type coercions happen in `DateTimeOperations` coercion rule.
    ### Why are the changes needed?
    
    Feature Parity between PostgreSQL and Spark, and make the null semantic consistent with Spark.
    
    ### Does this PR introduce any user-facing change?
    
    1. date_add/date_sub functions only accept int/tinynit/smallint as the second arg, double/string etc, are forbidden like hive, which produce weird results.
    
    ### How was this patch tested?
    
    add ut
    
    Closes #26412 from yaooqinn/SPARK-29774.
    
    Authored-by: Kent Yao <yaooqinn@hotmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/test/resources/sql-tests/inputs/datetime.sql
+++ sql/core/src/test/resources/sql-tests/inputs/datetime.sql
@@ -30,12 +30,48 @@ select weekday('2007-02-03'), weekday('2009-07-30'), weekday('2017-05-27'), week
 
 select year('1500-01-01'), month('1500-01-01'), dayOfYear('1500-01-01');
 
-select date '2001-09-28' + 7;
-select 7 + date '2001-09-28';
-select date '2001-10-01' - 7;
-select date '2001-10-01' - date '2001-09-28';
-select date'2020-01-01' - timestamp'2019-10-06 10:11:12.345678';
-select timestamp'2019-10-06 10:11:12.345678' - date'2020-01-01';
 
 select date '2019-01-01\t';
 select timestamp '2019-01-01\t';
+
+-- time add/sub
+select timestamp'2011-11-11 11:11:11' + interval '2' day;
+select timestamp'2011-11-11 11:11:11' - interval '2' day;
+select date'2011-11-11 11:11:11' + interval '2' second;
+select date'2011-11-11 11:11:11' - interval '2' second;
+select '2011-11-11' - interval '2' day;
+select '2011-11-11 11:11:11' - interval '2' second;
+select '1' - interval '2' second;
+select 1 - interval '2' second;
+
+-- subtract timestamps
+select date'2020-01-01' - timestamp'2019-10-06 10:11:12.345678';
+select timestamp'2019-10-06 10:11:12.345678' - date'2020-01-01';
+select timestamp'2019-10-06 10:11:12.345678' - null;
+select null - timestamp'2019-10-06 10:11:12.345678';
+
+-- date add/sub
+select date_add('2011-11-11', 1Y);
+select date_add('2011-11-11', 1S);
+select date_add('2011-11-11', 1);
+select date_add('2011-11-11', 1L);
+select date_add('2011-11-11', 1.0);
+select date_add('2011-11-11', 1E1);
+select date_add('2011-11-11', '1');
+select date_add(date'2011-11-11', 1);
+select date_add(timestamp'2011-11-11', 1);
+select date_sub(date'2011-11-11', 1);
+select date_sub(timestamp'2011-11-11', 1);
+select date_sub(null, 1);
+select date_sub(date'2011-11-11', null);
+select date'2011-11-11' + 1E1;
+select null + date '2001-09-28';
+select date '2001-09-28' + 7Y;
+select 7S + date '2001-09-28';
+select date '2001-10-01' - 7;
+select date '2001-09-28' + null;
+select date '2001-09-28' - null;
+
+-- subtract dates
+select null - date '2019-10-06';
+select date '2001-10-01' - date '2001-09-28';
