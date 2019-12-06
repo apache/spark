b1fbb415979b407b27c866c597e3114c81852bb4
Liang-Chi Hsieh <liangchi@uber.com>
2019-12-05 17:22:41 -0800

    Address comments.

--- sql/core/src/test/scala/org/apache/spark/sql/sources/InsertSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/sources/InsertSuite.scala
@@ -272,32 +272,42 @@ class InsertSuite extends DataSourceTest with SharedSparkSession {
 
   test("it is allowed to write to a table while querying it for dynamic partition overwrite.") {
     Seq(PartitionOverwriteMode.DYNAMIC.toString,
-        PartitionOverwriteMode.STATIC.toString).foreach { usedMode =>
-      withSQLConf(SQLConf.PARTITION_OVERWRITE_MODE.key -> usedMode) {
+        PartitionOverwriteMode.STATIC.toString).foreach { mode =>
+      withSQLConf(SQLConf.PARTITION_OVERWRITE_MODE.key -> mode) {
         withTable("insertTable") {
           sql(
             """
               |CREATE TABLE insertTable(i int, part1 int, part2 int) USING PARQUET
               |PARTITIONED BY (part1, part2)
-           """.stripMargin)
+            """.stripMargin)
 
           sql("INSERT INTO TABLE insertTable PARTITION(part1=1, part2=1) SELECT 1")
           checkAnswer(spark.table("insertTable"), Row(1, 1, 1))
+          sql("INSERT OVERWRITE TABLE insertTable PARTITION(part1=1, part2=2) SELECT 2")
+          checkAnswer(spark.table("insertTable"), Row(1, 1, 1) :: Row(2, 1, 2) :: Nil)
 
-          if (usedMode == PartitionOverwriteMode.DYNAMIC.toString) {
+          if (mode == PartitionOverwriteMode.DYNAMIC.toString) {
             sql(
               """
                 |INSERT OVERWRITE TABLE insertTable PARTITION(part1=1, part2)
                 |SELECT i + 1, part2 FROM insertTable
               """.stripMargin)
-            checkAnswer(spark.table("insertTable"), Row(2, 1, 1))
+            checkAnswer(spark.table("insertTable"), Row(2, 1, 1) :: Row(3, 1, 2) :: Nil)
+
+            sql(
+              """
+                |INSERT OVERWRITE TABLE insertTable PARTITION(part1=1, part2)
+                |SELECT i + 1, part2 + 1 FROM insertTable
+              """.stripMargin)
+            checkAnswer(spark.table("insertTable"),
+              Row(2, 1, 1) :: Row(3, 1, 2) :: Row(4, 1, 3) :: Nil)
           } else {
             val message = intercept[AnalysisException] {
               sql(
                 """
                   |INSERT OVERWRITE TABLE insertTable PARTITION(part1=1, part2)
                   |SELECT i + 1, part2 FROM insertTable
-              """.stripMargin)
+                """.stripMargin)
             }.getMessage
             assert(
               message.contains("Cannot overwrite a path that is also being read from."),
