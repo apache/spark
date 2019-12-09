25dab227c15d897c96ce4e1fca2f887b07eaec61
zhengruifeng <ruifengz@foxmail.com>
2019-12-09 14:06:12 +0800

    init pr

--- mllib/src/main/scala/org/apache/spark/ml/feature/RobustScaler.scala
+++ mllib/src/main/scala/org/apache/spark/ml/feature/RobustScaler.scala
@@ -163,12 +163,12 @@ class RobustScaler (override val uid: String)
       new QuantileSummaries(QuantileSummaries.defaultCompressThreshold, localRelativeError))(
       seqOp = (s, v) => s.insert(v),
       combOp = (s1, s2) => s1.compress.merge(s2.compress)
-    ).map { case (i, s) =>
+    ).mapValues{ s =>
       // confirm compression before query
       val s2 = s.compress
       val range = s2.query(localUpper).get - s2.query(localLower).get
       val median = s2.query(0.5).get
-      (i, (range, median))
+      (range, median)
     }.collectAsMap()
 
     val ranges = Array.ofDim[Double](numFeatures)
