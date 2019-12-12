cdeaf4cba5eb5195f630024a31d8b1a31ce24294
John Ayad <johnhany97@gmail.com>
2019-12-12 13:38:44 +0000

    Link to Java Class

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/datetimeExpressions.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/datetimeExpressions.scala
@@ -595,7 +595,7 @@ case class WeekOfYear(child: Expression) extends UnaryExpression with ImplicitCa
 
 // scalastyle:off line.size.limit
 @ExpressionDescription(
-  usage = "_FUNC_(timestamp, fmt) - Converts `timestamp` to a value of string in the format specified by the date format `fmt`.",
+  usage = "_FUNC_(timestamp, fmt) - Converts `timestamp` to a value of string in the format specified by the date format `fmt`. See [[java.text.SimpleDateFormat]] for valid date and time format patterns.",
   examples = """
     Examples:
       > SELECT _FUNC_('2016-04-08', 'y');
