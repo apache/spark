fd39b6db346d8cfe592fb97653cb68df4f6d6434
Aaron Lau <aaron.lau@datadoghq.com>
2019-12-12 08:42:18 -0600

    [SQL] Typo in HashedRelation error
    ### What changes were proposed in this pull request?
    
    Fixed typo in exception message of HashedRelations
    
    ### Why are the changes needed?
    
    Better exception messages
    
    ### Does this PR introduce any user-facing change?
    
    No
    
    ### How was this patch tested?
    
    No tests needed
    
    Closes #26822 from aaron-lau/master.
    
    Authored-by: Aaron Lau <aaron.lau@datadoghq.com>
    Signed-off-by: Sean Owen <srowen@gmail.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashedRelation.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/HashedRelation.scala
@@ -335,7 +335,7 @@ private[joins] object UnsafeHashedRelation {
         if (!success) {
           binaryMap.free()
           // scalastyle:off throwerror
-          throw new SparkOutOfMemoryError("There is no enough memory to build hash map")
+          throw new SparkOutOfMemoryError("There is not enough memory to build hash map")
           // scalastyle:on throwerror
         }
       }
