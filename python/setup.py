98cd80aac0708094a1a66a4e9c629f24f0fd762a
Yuming Wang <yumwang@ebay.com>
2019-12-12 09:43:33 +0800

    Verify remove virtualenv

--- python/setup.py
+++ python/setup.py
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
 import glob
 import os
 import sys
