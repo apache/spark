02896e38fcf42da5cd05c350cb2622ccd80fc061
Yuming Wang <yumwang@ebay.com>
2019-12-12 10:06:44 +0800

    Fix: update-alternatives: error: alternative path /usr/bin/python2.7 doesn't exist

--- python/setup.py
+++ python/setup.py
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import print_function
 import glob
 import os
 import sys
