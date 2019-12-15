ab39411e01ede1339548d9adf186300ca6cda134
xiaodeshan <xiaodeshan@xiaomi.com>
2019-12-14 17:15:30 -0800

    [SPARK-25100][CORE] Register TaskCommitMessage to KyroSerializer
    ## What changes were proposed in this pull request?
    
    Fix the bug when invoking saveAsNewAPIHadoopDataset to store data, the job will fail because the class TaskCommitMessage hasn't be registered if serializer is KryoSerializer and spark.kryo.registrationRequired is true
    
    ## How was this patch tested?
    
    UT
    
    Closes #26714 from deshanxiao/SPARK-25100.
    
    Authored-by: xiaodeshan <xiaodeshan@xiaomi.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- core/src/test/scala/org/apache/spark/serializer/KryoSerializerSuite.scala
+++ core/src/test/scala/org/apache/spark/serializer/KryoSerializerSuite.scala
@@ -34,6 +34,7 @@ import org.roaringbitmap.RoaringBitmap
 import org.apache.spark.{SharedSparkContext, SparkConf, SparkFunSuite}
 import org.apache.spark.internal.config._
 import org.apache.spark.internal.config.Kryo._
+import org.apache.spark.internal.io.FileCommitProtocol.TaskCommitMessage
 import org.apache.spark.scheduler.HighlyCompressedMapStatus
 import org.apache.spark.serializer.KryoTest._
 import org.apache.spark.storage.BlockManagerId
@@ -355,6 +356,28 @@ class KryoSerializerSuite extends SparkFunSuite with SharedSparkContext {
       mapTaskId += 1
       ser.serialize(HighlyCompressedMapStatus(
         BlockManagerId("exec-1", "host", 1234), blockSizes, mapTaskId))
+    }
+  }
+
+  test("registration of TaskCommitMessage") {
+    val conf = new SparkConf(false)
+    conf.set(KRYO_REGISTRATION_REQUIRED, true)
+
+    val ser = new KryoSerializer(conf).newInstance()
+    // In HadoopMapReduceCommitProtocol#commitTask
+    val addedAbsPathFiles: mutable.Map[String, String] = mutable.Map()
+    addedAbsPathFiles.put("test1", "test1")
+    addedAbsPathFiles.put("test2", "test2")
+
+    val partitionPaths: mutable.Set[String] = mutable.Set()
+    partitionPaths.add("test3")
+
+    val taskCommitMessage1 = new TaskCommitMessage(addedAbsPathFiles.toMap -> partitionPaths.toSet)
+    val taskCommitMessage2 = new TaskCommitMessage(Map.empty -> Set.empty)
+    Seq(taskCommitMessage1, taskCommitMessage2).foreach { taskCommitMessage =>
+      val obj1 = ser.deserialize[TaskCommitMessage](ser.serialize(taskCommitMessage)).obj
+      val obj2 = taskCommitMessage.obj
+      assert(obj1 == obj2)
     }
   }
 
