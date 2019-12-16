fb8912e8f5f11bc319e7bce85fd2092186fd96d8
Maxim Gekk <max.gekk@gmail.com>
2019-12-17 00:42:27 +0300

    Add filterToExpression

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/csv/CSVFilters.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/csv/CSVFilters.scala
@@ -17,11 +17,89 @@
 
 package org.apache.spark.sql.catalyst.csv
 
+import scala.util.Try
+
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.sources.Filter
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.sources
 import org.apache.spark.sql.types.StructType
 
-class CSVFilters(filters: Seq[Filter], schema: StructType) {
+class CSVFilters(filters: Seq[sources.Filter], schema: StructType) {
+  private def reorderFilters(): Array[Seq[sources.Filter]] = {
+    val numFields = schema.fields.length
+    val ordered = new Array[Seq[sources.Filter]](numFields)
+    for (filter <- filters) {
+      val maxIndex = filter.references.map(schema.fieldIndex).max
+      if (maxIndex < numFields) {
+        val f = ordered(maxIndex)
+        ordered(maxIndex) = if (f == null) Seq(filter) else f :+ filter
+      } else {
+        throw new IllegalArgumentException(
+          s"The filter $filter has an attribute out of the schema $schema: $maxIndex")
+      }
+    }
+    ordered
+  }
+
+  private def toRef(attr: String): Option[BoundReference] = {
+    schema.getFieldIndex(attr).map { index =>
+      val field = schema(index)
+      BoundReference(schema.fieldIndex(attr), field.dataType, field.nullable)
+    }
+  }
+
+  private def toLiteral(value: Any): Option[Literal] = {
+    Try(Literal(value)).toOption
+  }
+
+  private def zip[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = a.zip(b).headOption
+
+  private def zipAttributeAndValue(name: String, value: Any): Option[(BoundReference, Literal)] = {
+    zip(toRef(name), toLiteral(value))
+  }
+
+  private def filterToExpression(filter: sources.Filter): Option[Expression] = {
+    def translate(filter: sources.Filter): Option[Expression] = filter match {
+      case sources.And(left, right) =>
+        zip(translate(left), translate(right)).map(And.tupled)
+      case sources.Or(left, right) =>
+        zip(translate(left), translate(right)).map(Or.tupled)
+      case sources.Not(child) =>
+        translate(child).map(Not)
+      case sources.EqualTo(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(EqualTo.tupled)
+      case sources.EqualNullSafe(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(EqualNullSafe.tupled)
+      case sources.IsNull(attribute) =>
+        toRef(attribute).map(IsNull)
+      case sources.IsNotNull(attribute) =>
+        toRef(attribute).map(IsNotNull)
+      case sources.In(attribute, values) =>
+        val literals = values.toSeq.flatMap(toLiteral)
+        if (literals.length == values.length) {
+          toRef(attribute).map(In(_, literals))
+        } else {
+          None
+        }
+      case sources.GreaterThan(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(GreaterThan.tupled)
+      case sources.GreaterThanOrEqual(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(GreaterThanOrEqual.tupled)
+      case sources.LessThan(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(LessThan.tupled)
+      case sources.LessThanOrEqual(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(LessThanOrEqual.tupled)
+      case sources.StringContains(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(Contains.tupled)
+      case sources.StringStartsWith(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(StartsWith.tupled)
+      case sources.StringEndsWith(attribute, value) =>
+        zipAttributeAndValue(attribute, value).map(EndsWith.tupled)
+      case _ => None
+    }
+    translate(filter)
+  }
+
   def skipRow(row: InternalRow): Boolean = {
     true
   }
