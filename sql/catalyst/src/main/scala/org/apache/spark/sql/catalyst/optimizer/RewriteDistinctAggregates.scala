0cabcb621f3cc27ee9ba8a50fc9b16edd1695026
gengjiaan <gengjiaan@360.cn>
2019-12-17 17:58:07 +0800

    Adjust code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/RewriteDistinctAggregates.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/RewriteDistinctAggregates.scala
@@ -77,16 +77,16 @@ import org.apache.spark.sql.types.IntegerType
  *
  * Second example: aggregate function without distinct and with filter clauses (in sql):
  * {{{
- *   SELECT 
- *     COUNT(DISTINCT cat1) as cat1_cnt, 
- *     COUNT(DISTINCT cat2) as cat2_cnt, 
+ *   SELECT
+ *     COUNT(DISTINCT cat1) as cat1_cnt,
+ *     COUNT(DISTINCT cat2) as cat2_cnt,
  *     SUM(value) FILTER (
- *       WHERE 
+ *       WHERE
  *         id > 1
- *     ) AS total 
- *  FROM 
- *    data 
- *  GROUP BY 
+ *     ) AS total
+ *  FROM
+ *    data
+ *  GROUP BY
  *    key
  * }}}
  *
