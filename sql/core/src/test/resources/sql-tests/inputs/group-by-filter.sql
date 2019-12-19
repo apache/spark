97bb440cd7447aff7e53b5da9fa0162cb230481b
gengjiaan <gengjiaan@360.cn>
2019-12-19 10:59:14 +0800

    Optimize code

--- sql/core/src/test/resources/sql-tests/inputs/group-by-filter.sql
+++ sql/core/src/test/resources/sql-tests/inputs/group-by-filter.sql
@@ -1,7 +1,7 @@
 -- Test filter clause for aggregate expression.
 
 -- Test data.
-CREATE OR REPLACE  TEMPORARY VIEW testData AS SELECT * FROM VALUES
+CREATE OR REPLACE TEMPORARY VIEW testData AS SELECT * FROM VALUES
 (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2), (null, 1), (3, null), (null, null)
 AS testData(a, b);
 
