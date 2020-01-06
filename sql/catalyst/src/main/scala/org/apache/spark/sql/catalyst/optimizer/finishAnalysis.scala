26258b0bb285644ea9d1b43f7ac20a7e02c5d6f4
angerszhu <angers.zhu@gmail.com>
2020-01-06 11:57:10 +0800

    Update finishAnalysis.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/finishAnalysis.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/finishAnalysis.scala
@@ -53,9 +53,9 @@ object ReplaceExpressions extends Rule[LogicalPlan] {
 }
 
 /**
- * Rewritten uncorrelated exists subquery to use ScalarSubquery
+ * Rewrite non correlated exists subquery to use ScalarSubquery
  *   WHERE EXISTS (SELECT A FROM TABLE B WHERE COL1 > 10)
- * will be rewrite to
+ * will be rewritten to
  *   WHERE (SELECT 1 FROM (SELECT A FROM TABLE B WHERE COL1 > 10) LIMIT 1) IS NOT NULL
  */
 object RewriteNonCorrelatedExists extends Rule[LogicalPlan] {
