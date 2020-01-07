c1457f0def64dc20cd0cad922012b99e0aa0aa13
uncleGen <hustyugm@gmail.com>
2020-01-07 16:28:46 +0800

    fix wrong inactive query duration

--- sql/core/src/main/scala/org/apache/spark/sql/streaming/ui/StreamingQueryPage.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/streaming/ui/StreamingQueryPage.scala
@@ -18,7 +18,6 @@
 package org.apache.spark.sql.streaming.ui
 
 import java.text.SimpleDateFormat
-import java.util.TimeZone
 import javax.servlet.http.HttpServletRequest
 
 import scala.xml.Node
@@ -26,6 +25,7 @@ import scala.xml.Node
 import org.apache.commons.lang3.StringEscapeUtils
 
 import org.apache.spark.internal.Logging
+import org.apache.spark.sql.catalyst.util.DateTimeUtils.getTimeZone
 import org.apache.spark.sql.execution.streaming.StreamQueryStore
 import org.apache.spark.sql.streaming.StreamingQuery
 import org.apache.spark.sql.streaming.ui.UIUtils._
@@ -34,7 +34,7 @@ import org.apache.spark.ui.{UIUtils => SparkUIUtils, WebUIPage}
 class StreamingQueryPage(parent: StreamingQueryTab, store: StreamQueryStore)
   extends WebUIPage("") with Logging {
   val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-  df.setTimeZone(TimeZone.getDefault)
+  df.setTimeZone(getTimeZone("UTC"))
 
   override def render(request: HttpServletRequest): Seq[Node] = {
     val content = generateStreamingQueryTable(request)
@@ -80,9 +80,7 @@ class StreamingQueryPage(parent: StreamingQueryTab, store: StreamQueryStore)
     } else {
       withNoProgress(query, {
         val endTimeMs = query.lastProgress.timestamp
-        val startTimeMs = query.recentProgress.head.timestamp
-        SparkUIUtils.formatDurationVerbose(
-          df.parse(endTimeMs).getTime - df.parse(startTimeMs).getTime)
+        SparkUIUtils.formatDurationVerbose(df.parse(endTimeMs).getTime - timeSinceStart)
       }, "-")
     }
 
