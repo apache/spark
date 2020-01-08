a93b9966358f4e21818cf2d42cf65cd8dcd3fa41
zhengruifeng <ruifengz@foxmail.com>
2020-01-09 00:07:42 +0900

    [MINOR][ML][INT] Array.fill(0) -> Array.ofDim; Array.empty -> Array.emptyIntArray
    ### What changes were proposed in this pull request?
    1, for primitive types `Array.fill(n)(0)` -> `Array.ofDim(n)`;
    2, for `AnyRef` types `Array.fill(n)(null)` -> `Array.ofDim(n)`;
    3, for primitive types `Array.empty[XXX]` -> `Array.emptyXXXArray`
    
    ### Why are the changes needed?
    `Array.ofDim` avoid assignments;
    `Array.emptyXXXArray` avoid create new object;
    
    ### Does this PR introduce any user-facing change?
    No
    
    ### How was this patch tested?
    existing testsuites
    
    Closes #27133 from zhengruifeng/minor_fill_ofDim.
    
    Authored-by: zhengruifeng <ruifengz@foxmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
@@ -568,14 +568,14 @@ class LogisticRegression @Since("1.2.0") (
           s"coefficients will be zeros. Training is not needed.")
         val constantLabelIndex = Vectors.dense(histogram).argmax
         val coefMatrix = new SparseMatrix(numCoefficientSets, numFeatures,
-          new Array[Int](numCoefficientSets + 1), Array.empty[Int], Array.empty[Double],
+          new Array[Int](numCoefficientSets + 1), Array.emptyIntArray, Array.emptyDoubleArray,
           isTransposed = true).compressed
         val interceptVec = if (isMultinomial) {
           Vectors.sparse(numClasses, Seq((constantLabelIndex, Double.PositiveInfinity)))
         } else {
           Vectors.dense(if (numClasses == 2) Double.PositiveInfinity else Double.NegativeInfinity)
         }
-        (coefMatrix, interceptVec, Array.empty[Double])
+        (coefMatrix, interceptVec, Array.emptyDoubleArray)
       } else {
         if (!$(fitIntercept) && isConstantLabel) {
           instr.logWarning(s"All labels belong to a single class and fitIntercept=false. It's a " +
@@ -855,7 +855,7 @@ class LogisticRegression @Since("1.2.0") (
             Friedman, et al. "Regularization Paths for Generalized Linear Models via
               Coordinate Descent," https://core.ac.uk/download/files/153/6287975.pdf
            */
-          val centers = Array.fill(numFeatures)(0.0)
+          val centers = Array.ofDim[Double](numFeatures)
           denseCoefficientMatrix.foreachActive { case (i, j, v) =>
             centers(j) += v
           }
