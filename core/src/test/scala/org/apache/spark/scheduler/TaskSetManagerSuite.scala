b1099b99d569a93af1960d916279b6f7f879bfc7
Ajith <ajith2489@gmail.com>
2020-01-08 21:10:18 +0530

    Style

--- core/src/test/scala/org/apache/spark/scheduler/TaskSetManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/TaskSetManagerSuite.scala
@@ -1920,7 +1920,7 @@ class TaskSetManagerSuite
     TestUtils.waitUntilExecutorsUp(sc, 2, 60000)
 
     val tasks = Array.tabulate[Task[_]](2)(partition => new FakeLongTasks(stageId = 0, partition))
-    val taskSet : TaskSet = new TaskSet(tasks, stageId = 0, stageAttemptId = 0, priority = 0, null)
+    val taskSet: TaskSet = new TaskSet(tasks, stageId = 0, stageAttemptId = 0, priority = 0, null)
     val stageId = taskSet.stageId
     val stageAttemptId = taskSet.stageAttemptId
     sched.submitTasks(taskSet)
