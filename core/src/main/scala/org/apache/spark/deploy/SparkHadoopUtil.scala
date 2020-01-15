262396964471157569e01e384672a313bdcfcf2c
郭黎明 <guoliming@geetest.com>
2020-01-15 17:55:27 +0800

    split the spark user and the hdfs user on executor

--- core/src/main/scala/org/apache/spark/deploy/SparkHadoopUtil.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkHadoopUtil.scala
@@ -67,7 +67,7 @@ class SparkHadoopUtil extends Logging {
   }
 
   def createSparkUser(): UserGroupInformation = {
-    val user = Utils.getCurrentUserName()
+    val user = Option(System.getenv("HADOOP_USER_NAME")).getOrElse(Utils.getCurrentUserName())
     logDebug("creating UGI for user: " + user)
     val ugi = UserGroupInformation.createRemoteUser(user)
     transferCredentials(UserGroupInformation.getCurrentUser(), ugi)
