d6cecbfc3b393294c2a1deabf8a4ed5b77f96010
Ray <7869818+rayrrr@users.noreply.github.com>
2020-01-15 15:35:37 -0500

    [AIRFLOW-6573] bump `text-unidecode` version for Python 3.7 (#7179)

--- setup.py
+++ setup.py
@@ -483,7 +483,7 @@ def do_setup():
             'tabulate>=0.7.5, <0.9',
             'tenacity==4.12.0',
             'termcolor==1.1.0',
-            'text-unidecode==1.2',
+            'text-unidecode==1.3',
             'thrift>=0.9.2',
             'typing;python_version<"3.6"',
             'typing-extensions>=3.7.4;python_version<"3.8"',
