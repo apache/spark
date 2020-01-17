1a9de8c31fe6a91c7fd6c9196e6882ef9779420b
Maxim Gekk <max.gekk@gmail.com>
2020-01-17 11:44:49 +0900

    [SPARK-30499][SQL] Remove SQL config spark.sql.execution.pandas.respectSessionTimeZone
    ### What changes were proposed in this pull request?
    In the PR, I propose to remove the SQL config `spark.sql.execution.pandas.respectSessionTimeZone` which has been deprecated since Spark 2.3.
    
    ### Why are the changes needed?
    To improve code maintainability.
    
    ### Does this PR introduce any user-facing change?
    Yes.
    
    ### How was this patch tested?
    by running python tests, https://spark.apache.org/docs/latest/building-spark.html#pyspark-tests-with-maven-or-sbt
    
    Closes #27218 from MaxGekk/remove-respectSessionTimeZone.
    
    Authored-by: Maxim Gekk <max.gekk@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/sql/pandas/conversion.py
+++ python/pyspark/sql/pandas/conversion.py
@@ -64,10 +64,7 @@ class PandasConversionMixin(object):
         import numpy as np
         import pandas as pd
 
-        if self.sql_ctx._conf.pandasRespectSessionTimeZone():
-            timezone = self.sql_ctx._conf.sessionLocalTimeZone()
-        else:
-            timezone = None
+        timezone = self.sql_ctx._conf.sessionLocalTimeZone()
 
         if self.sql_ctx._conf.arrowPySparkEnabled():
             use_arrow = True
@@ -231,10 +228,7 @@ class SparkConversionMixin(object):
         from pyspark.sql.pandas.utils import require_minimum_pandas_version
         require_minimum_pandas_version()
 
-        if self._wrapped._conf.pandasRespectSessionTimeZone():
-            timezone = self._wrapped._conf.sessionLocalTimeZone()
-        else:
-            timezone = None
+        timezone = self._wrapped._conf.sessionLocalTimeZone()
 
         # If no schema supplied by user then get the names of columns only
         if schema is None:
