801403943bdfbe4febbf1aac90845b5d7e1c03d6
Kaxil Naik <kaxilnaik@gmail.com>
2020-01-22 00:46:45 +0530

    [AIRFLOW-6608] Change logging level for PythonOperator Env exports (#7229)

--- airflow/operators/bash.py
+++ airflow/operators/bash.py
@@ -96,10 +96,9 @@ class BashOperator(BaseOperator):
             env = os.environ.copy()
 
         airflow_context_vars = context_to_airflow_vars(context, in_env_var_format=True)
-        self.log.info('Exporting the following env vars:\n%s',
-                      '\n'.join(["{}={}".format(k, v)
-                                 for k, v in
-                                 airflow_context_vars.items()]))
+        self.log.debug('Exporting the following env vars:\n%s',
+                       '\n'.join(["{}={}".format(k, v)
+                                  for k, v in airflow_context_vars.items()]))
         env.update(airflow_context_vars)
 
         self.lineage_data = self.bash_command
