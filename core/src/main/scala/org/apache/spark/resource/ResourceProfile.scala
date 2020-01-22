585df549c32b869c82aa6608245fbb3baebe8017
Thomas Graves <tgraves@nvidia.com>
2020-01-22 08:46:10 -0600

    Fix merge issues

--- core/src/main/scala/org/apache/spark/resource/ResourceProfile.scala
+++ core/src/main/scala/org/apache/spark/resource/ResourceProfile.scala
@@ -22,6 +22,7 @@ import java.util.concurrent.atomic.AtomicInteger
 import javax.annotation.concurrent.GuardedBy
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 import org.apache.spark.{SparkConf, SparkException}
 import org.apache.spark.annotation.Evolving
