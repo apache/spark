0265727cc65a021a16f3d8da286382d7cdd9d264
zero323 <mszymkiewicz@gmail.com>
2020-01-24 20:28:21 +0100

    Switch to double quotes

--- R/pkg/tests/run-all.R
+++ R/pkg/tests/run-all.R
@@ -17,7 +17,7 @@
 
 # TODO Remove before merging SPARK-23435
 if (grepl("^1\\.\\d+\\.\\d+$", installed.packages()["testthat", "Version"])) {
-  install.packages("testthat", repos = 'https://cloud.r-project.org/')
+  install.packages("testthat", repos = "https://cloud.r-project.org/")
 }
 
 library(testthat)
