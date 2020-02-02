6b38b1bb8d4158afb555821598bb58533ae21bf4
Xinbin Huang <bin.huangxb@gmail.com>
2020-02-02 01:14:14 -0800

    [AIRFLOW-6626] Add email on failure or retry to default config (#7249)

--- tests/models/test_baseoperator.py
+++ tests/models/test_baseoperator.py
@@ -249,6 +249,20 @@ class TestBaseOperator(unittest.TestCase):
         self.assertEqual(task.resources.cpus.qty, 1)
         self.assertEqual(task.resources.ram.qty, 1024)
 
+    def test_default_email_on_actions(self):
+        test_task = DummyOperator(task_id='test_default_email_on_actions')
+        assert test_task.email_on_retry is True
+        assert test_task.email_on_failure is True
+
+    def test_email_on_actions(self):
+        test_task = DummyOperator(
+            task_id='test_default_email_on_actions',
+            email_on_retry=False,
+            email_on_failure=True
+        )
+        assert test_task.email_on_retry is False
+        assert test_task.email_on_failure is True
+
 
 class TestBaseOperatorMethods(unittest.TestCase):
     def test_cross_downstream(self):
