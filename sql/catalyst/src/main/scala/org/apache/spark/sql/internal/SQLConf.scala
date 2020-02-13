afde20a2cd19a65d8b68dc88c1d04fb166539e4e
yi.wu <yi.wu@databricks.com>
2020-02-13 22:11:52 +0800

    arrayExistsFollowsThreeValuedLogic

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2115,7 +2115,7 @@ object SQLConf {
     .createOptional
 
   val LEGACY_ARRAY_EXISTS_FOLLOWS_THREE_VALUED_LOGIC =
-    buildConf("spark.sql.legacy.arrayExistsFollowsThreeValuedLogic")
+    buildConf("spark.sql.legacy.arrayExistsFollowsThreeValuedLogic.enabled")
       .internal()
       .doc("When true, the ArrayExists will follow the three-valued boolean logic.")
       .booleanConf
