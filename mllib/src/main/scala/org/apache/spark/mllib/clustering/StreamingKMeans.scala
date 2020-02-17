c22b05b06cd85513f2516c2173fd8a32decf2ca0
zhengruifeng <ruifengz@foxmail.com>
2020-02-17 16:13:59 +0800

    nit

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/StreamingKMeans.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/StreamingKMeans.scala
@@ -125,9 +125,8 @@ class StreamingKMeansModel @Since("1.2.0") (
     }
 
     // Check whether the smallest cluster is dying. If so, split the largest cluster.
-    val weightsWithIndex = clusterWeights.view.zipWithIndex
-    val (maxWeight, largest) = weightsWithIndex.maxBy(_._1)
-    val (minWeight, smallest) = weightsWithIndex.minBy(_._1)
+    val (maxWeight, largest) = clusterWeights.iterator.zipWithIndex.maxBy(_._1)
+    val (minWeight, smallest) = clusterWeights.iterator.zipWithIndex.minBy(_._1)
     if (minWeight < 1e-8 * maxWeight) {
       logInfo(s"Cluster $smallest is dying. Split the largest cluster $largest into two.")
       val weight = (maxWeight + minWeight) / 2.0
