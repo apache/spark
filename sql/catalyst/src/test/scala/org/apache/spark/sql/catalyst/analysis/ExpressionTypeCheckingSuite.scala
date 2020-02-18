fbd660c0d02c3965b8b9828773c578890691e8f7
Wenchen Fan <wenchen@databricks.com>
2020-02-18 19:42:13 +0800

    Revert "[SPARK-29663][SQL] Support sum with interval type values"
    This reverts commit 44b8fbcc58daaa61693b6608fcfc540db71a9c37.

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/ExpressionTypeCheckingSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/ExpressionTypeCheckingSuite.scala
@@ -158,7 +158,7 @@ class ExpressionTypeCheckingSuite extends SparkFunSuite {
 
     assertError(Min(Symbol("mapField")), "min does not support ordering on type")
     assertError(Max(Symbol("mapField")), "max does not support ordering on type")
-    assertError(Sum(Symbol("booleanField")), "requires (numeric or interval) type")
+    assertError(Sum(Symbol("booleanField")), "function sum requires numeric type")
     assertError(Average(Symbol("booleanField")), "requires (numeric or interval) type")
   }
 
