b4dfb1c3f11bd806e5ae34cd071de22bb92db76e
lijunqing <lijunqing@baidu.com>
2020-02-21 00:24:07 +0800

    update suite case for insert
    Change-Id: Ia5ef914b7a2def5d8e16cc39e4b4cd917b94b88b

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveExternalCatalogSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveExternalCatalogSuite.scala
@@ -168,7 +168,7 @@ class HiveExternalCatalogSuite extends ExternalCatalogSuite {
     // test create hive table failed
     val tblName = "table_not_exists"
     assertThrows[QueryExecutionException](externalCatalog.client.runSqlHive(
-      s"CREATE TABLE $tblName(n into)"))
+      s"CREATE TABLE $tblName(s into)"))
 
     // test describe table failed
     assertThrows[QueryExecutionException](externalCatalog.client.runSqlHive(
@@ -176,7 +176,7 @@ class HiveExternalCatalogSuite extends ExternalCatalogSuite {
 
     // test wrong insert queries
     assertThrows[QueryExecutionException](externalCatalog.client.runSqlHive(
-      s"INSERT into directory /tmp/test select 1 as a"))
+      s"INSERT overwrite directory \"fs://localhost/tmp\" select 1 as a"))
 
     // test drop hive table which not exists
     assertThrows[QueryExecutionException](externalCatalog.client.runSqlHive(
