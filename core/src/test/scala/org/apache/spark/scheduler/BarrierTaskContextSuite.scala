7c259acd3e447450a18bcdeea47d14ac8514b1bc
sarthfrey-db <sarth.frey@databricks.com>
2020-02-20 16:24:59 -0800

    revert and relax test assertion

--- core/src/test/scala/org/apache/spark/scheduler/BarrierTaskContextSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/BarrierTaskContextSuite.scala
@@ -93,7 +93,10 @@ class BarrierTaskContextSuite extends SparkFunSuite with LocalSparkContext {
     val error = intercept[SparkException] {
       rdd2.collect()
     }.getMessage
-    assert(error.contains("does not match the current synchronized requestMethod"))
+    assert(
+      error.contains("does not match the current synchronized requestMethod") ||
+      error.contains("not properly killed")
+    )
   }
 
   test("successively sync with allGather and barrier") {
