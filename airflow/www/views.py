63751c3e53d0240617dd29b2e0833b50a8a261d7
Kaxil Naik <kaxilnaik@gmail.com>
2020-02-22 09:40:11 +0000

    [AIRFLOW-6872] Fix: Show Git Version in UI (#7493)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -23,6 +23,7 @@ import json
 import logging
 import math
 import os
+import pkgutil
 import socket
 import traceback
 from collections import defaultdict
@@ -2056,9 +2057,7 @@ class VersionView(AirflowBaseView):
         # Get the Git repo and git hash
         git_version = None
         try:
-            with open(os.path.join(*[settings.AIRFLOW_HOME,
-                                   'airflow', 'git_version'])) as f:
-                git_version = f.readline()
+            git_version = str(pkgutil.get_data('airflow', 'git_version'), encoding="UTF-8")
         except Exception as e:
             logging.error(e)
 
