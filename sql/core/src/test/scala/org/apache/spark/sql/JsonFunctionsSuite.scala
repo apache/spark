19b5380c7d75b38d0ac98bb4ecb200fb49d6419d
Maxim Gekk <max.gekk@gmail.com>
2020-03-04 21:46:52 +0300

    Add a test

--- sql/core/src/test/scala/org/apache/spark/sql/JsonFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/JsonFunctionsSuite.scala
@@ -653,4 +653,11 @@ class JsonFunctionsSuite extends QueryTest with SharedSparkSession {
       assert(json_tuple_result === len)
     }
   }
+
+  test("schema_of_json - infers the schema of foldable JSON string") {
+    val input = regexp_replace(lit("""{"item_id": 1, "item_price": 0.1}"""), "item_", "")
+    checkAnswer(
+      spark.range(1).select(schema_of_json(input)),
+      Seq(Row("struct<id:bigint,price:double>")))
+  }
 }
