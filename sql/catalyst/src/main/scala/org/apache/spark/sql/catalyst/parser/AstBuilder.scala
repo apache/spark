519b4ca4436f4884e027ed8b7ef4bcb357210eb1
angerszhu <angers.zhu@gmail.com>
2020-03-11 15:48:24 +0800

    not change window frame action

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilder.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilder.scala
@@ -1693,6 +1693,8 @@ class AstBuilder(conf: SQLConf) extends SqlBaseBaseVisitor[AnyRef] with Logging 
     val partition = ctx.partition.asScala.map(expression)
     val order = if (ctx.sortItem.asScala.nonEmpty) {
       ctx.sortItem.asScala.map(visitSortItem)
+    } else if (ctx.windowFrame != null && ctx.windowFrame.frameType.getType == SqlBaseParser.RANGE) {
+      ctx.sortItem.asScala.map(visitSortItem)
     } else {
       // Same default behaviors like hive, when order spec is null
       // set partition spec expression as order spec
