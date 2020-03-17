abba354a6558bfba47f8621fa6e60bc1f3b00280
Wenchen Fan <wenchen@databricks.com>
2020-03-17 12:01:29 +0800

    [SPARK-31166][SQL] UNION map<null, null> and other maps should not fail
    After https://github.com/apache/spark/pull/27542, `map()` returns `map<null, null>` instead of `map<string, string>`. However, this breaks queries which union `map()` and other maps.
    
    The reason is, `TypeCoercion` rules and `Cast` think it's illegal to cast null type map key to other types, as it makes the key nullable, but it's actually legal. This PR fixes it.
    
    To avoid breaking queries.
    
    Yes, now some queries that work in 2.x can work in 3.0 as well.
    
    new test
    
    Closes #27926 from cloud-fan/bug.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit d7b97a1d0daf65710317321490a833f696a46f21)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -3456,6 +3456,12 @@ class SQLQuerySuite extends QueryTest with SharedSparkSession with AdaptiveSpark
       """.stripMargin)
     checkAnswer(df2, Row(1) :: Nil)
   }
+
+  test("SPARK-31166: UNION map<null, null> and other maps should not fail") {
+    checkAnswer(
+      sql("(SELECT map()) UNION ALL (SELECT map(1, 2))"),
+      Seq(Row(Map[Int, Int]()), Row(Map(1 -> 2))))
+  }
 }
 
 case class Foo(bar: Option[String])
