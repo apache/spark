3e2b2c206bc2eb60a5e65bad64b92f49121a877c
Kent Yao <yaooqinn@hotmail.com>
2020-03-18 21:51:20 +0800

    add comments and more tests

--- sql/core/src/test/resources/sql-tests/inputs/datetime.sql
+++ sql/core/src/test/resources/sql-tests/inputs/datetime.sql
@@ -108,5 +108,7 @@ select date_format(timestamp '2019-10-06', 'yyyy-MM-dd uuee');
 select date_format(timestamp '2019-10-06', 'yyyy-MM-dd uucc');
 select date_format(timestamp '2019-10-06', 'yyyy-MM-dd uuuu');
 
-select date_format(timestamp '2019-10-06', "yyyy-MM-dd'T'HH:mm:ss''SSSS");
-select date_format(timestamp '2019-10-06', "''");
+select to_timestamp("2019-10-06T10:11:12'12", "yyyy-MM-dd'T'HH:mm:ss''SSSS"); -- middle
+select to_timestamp("2019-10-06T10:11:12'", "yyyy-MM-dd'T'HH:mm:ss''"); -- tail
+select to_timestamp("'2019-10-06T10:11:12", "''yyyy-MM-dd'T'HH:mm:ss"); -- head
+select to_timestamp("P2019-10-06T10:11:12", "'P'yyyy-MM-dd'T'HH:mm:ss"); -- head but as single quote
