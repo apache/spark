a9f1f5d3a6e4a9957ca66d82d8e651f730a4eda1
Jungtaek Lim (HeartSaVioR) <kabhwan.opensource@gmail.com>
2020-03-19 10:04:35 +0900

    Apply Janino 3.0.16, and remove all hack around using Jitpack

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -229,7 +229,6 @@ object SparkBuild extends PomBuild {
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
       DefaultMavenRepository,
       Resolver.mavenLocal,
-      "jitpack" at "https://jitpack.io",
       Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     ),
     externalResolvers := resolvers.value,
