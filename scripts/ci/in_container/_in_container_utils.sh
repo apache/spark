6ec51b38e70cf8f1562fa075c70e5febd13a09df
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-03-21 15:46:46 +0100

    Fixes unclean installation of Airflow 1.10 (#7796)
    Removal of airrflow installed via -e . leaves .egg-info folder
    that makes subsequent airflow installation not clean.
    
    This folder should be removed before install

--- scripts/ci/in_container/_in_container_utils.sh
+++ scripts/ci/in_container/_in_container_utils.sh
@@ -310,14 +310,12 @@ function send_kubernetes_logs_to_file_io() {
 }
 
 function install_released_airflow_version() {
-    pip uninstall apache-airflow -y || true
+    pip uninstall -y apache-airflow || true
     find /root/airflow/ -type f -print0 | xargs rm -f --
     if [[ ${1} == "1.10.2" || ${1} == "1.10.1" ]]; then
         export SLUGIFY_USES_TEXT_UNIDECODE=yes
     fi
+    rm -rf "${AIRFLOW_SOURCES}"/*.egg-info
     INSTALLS=("apache-airflow==${1}" "werkzeug<1.0.0")
-    if [[ ${1} == "1.10.2" || ${1} == "1.10.1" ]]; then
-        echo
-    fi
     pip install --upgrade "${INSTALLS[@]}"
 }
