5ac35c442ef8dacb4f672506ccb84ac3c82cbeeb
Qianyang Yu <qyu@us.ibm.com>
2020-03-30 13:49:58 -0700

    address comments

--- examples/src/main/java/org/apache/spark/examples/ml/JavaANOVATestExample.java
+++ examples/src/main/java/org/apache/spark/examples/ml/JavaANOVATestExample.java
@@ -49,16 +49,16 @@ public class JavaANOVATestExample {
 
     // $example on$
     List<Row> data = Arrays.asList(
-      RowFactory.create(3, Vectors.dense(1.7, 4.4, 7.6, 5.8, 9.6, 2.3)),
-      RowFactory.create(2, Vectors.dense(8.8, 7.3, 5.7, 7.3, 2.2, 4.1)),
-      RowFactory.create(1, Vectors.dense(1.2, 9.5, 2.5, 3.1, 8.7, 2.5)),
-      RowFactory.create(2, Vectors.dense(3.7, 9.2, 6.1, 4.1, 7.5, 3.8)),
-      RowFactory.create(4, Vectors.dense(8.9, 5.2, 7.8, 8.3, 5.2, 3.0)),
-      RowFactory.create(4, Vectors.dense(7.9, 8.5, 9.2, 4.0, 9.4, 2.1))
+      RowFactory.create(3.0, Vectors.dense(1.7, 4.4, 7.6, 5.8, 9.6, 2.3)),
+      RowFactory.create(2.0, Vectors.dense(8.8, 7.3, 5.7, 7.3, 2.2, 4.1)),
+      RowFactory.create(1.0, Vectors.dense(1.2, 9.5, 2.5, 3.1, 8.7, 2.5)),
+      RowFactory.create(2.0, Vectors.dense(3.7, 9.2, 6.1, 4.1, 7.5, 3.8)),
+      RowFactory.create(4.0, Vectors.dense(8.9, 5.2, 7.8, 8.3, 5.2, 3.0)),
+      RowFactory.create(4.0, Vectors.dense(7.9, 8.5, 9.2, 4.0, 9.4, 2.1))
     );
 
     StructType schema = new StructType(new StructField[]{
-      new StructField("label", DataTypes.IntegerType, false, Metadata.empty()),
+      new StructField("label", DataTypes.DoubleType, false, Metadata.empty()),
       new StructField("features", new VectorUDT(), false, Metadata.empty()),
     });
 
--- examples/src/main/python/ml/anova_test_example.py
+++ examples/src/main/python/ml/anova_test_example.py
@@ -18,30 +18,30 @@
 """
 An example for ANOVA testing.
 Run with:
-  bin/spark-submit examples/src/main/python/ml/anova_example.py
+  bin/spark-submit examples/src/main/python/ml/anova_test_example.py
 """
 
 from __future__ import print_function
 
 from pyspark.sql import SparkSession
 # $example on$
-from pyspark.ml.stat import ANOVATest
 from pyspark.ml.linalg import Vectors
+from pyspark.ml.stat import ANOVATest
 # $example off$
 
 if __name__ == "__main__":
     spark = SparkSession\
         .builder\
-        .appName("ANOVAExample")\
+        .appName("ANOVATestExample")\
         .getOrCreate()
 
     # $example on$
-    data = [(3, Vectors.dense([1.7, 4.4, 7.6, 5.8, 9.6, 2.3])),
-            (2, Vectors.dense([8.8, 7.3, 5.7, 7.3, 2.2, 4.1])),
-            (1, Vectors.dense([1.2, 9.5, 2.5, 3.1, 8.7, 2.5])),
-            (2, Vectors.dense([3.7, 9.2, 6.1, 4.1, 7.5, 3.8])),
-            (4, Vectors.dense([8.9, 5.2, 7.8, 8.3, 5.2, 3.0])),
-            (4, Vectors.dense([7.9, 8.5, 9.2, 4.0, 9.4, 2.1]))]
+    data = [(3.0, Vectors.dense([1.7, 4.4, 7.6, 5.8, 9.6, 2.3])),
+            (2.0, Vectors.dense([8.8, 7.3, 5.7, 7.3, 2.2, 4.1])),
+            (1.0, Vectors.dense([1.2, 9.5, 2.5, 3.1, 8.7, 2.5])),
+            (2.0, Vectors.dense([3.7, 9.2, 6.1, 4.1, 7.5, 3.8])),
+            (4.0, Vectors.dense([8.9, 5.2, 7.8, 8.3, 5.2, 3.0])),
+            (4.0, Vectors.dense([7.9, 8.5, 9.2, 4.0, 9.4, 2.1]))]
     df = spark.createDataFrame(data, ["label", "features"])
 
     r = ANOVATest.test(df, "features", "label").head()
