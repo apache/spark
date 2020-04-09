a9517a98ede7ba0829fb0a70ef16420bd5899038
Alex Merritt <lecaran@gmail.com>
2020-04-09 17:27:47 -0400

    Make linter happy

--- streaming/src/test/scala/org/apache/spark/streaming/ReceiverSuite.scala
+++ streaming/src/test/scala/org/apache/spark/streaming/ReceiverSuite.scala
@@ -288,7 +288,7 @@ class ReceiverSuite extends TestSuiteBase with TimeLimits with Serializable {
         blockGeneratorListener: BlockGeneratorListener): BlockGenerator = {
       null
     }
-  } 
+  }
 }
 
 
