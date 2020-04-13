e6c50df0d8e583a834c80824d60d2fb19764d8d0
jiake <ke.a.jia@intel.com>
2020-04-13 14:00:58 +0800

    resolve comments

--- sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/CustomShuffleReaderExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/CustomShuffleReaderExec.scala
@@ -107,22 +107,28 @@ case class CustomShuffleReaderExec private(
   private def sendPartitionDataSizeMetrics(
       executionId: String,
       partitionMetrics: SQLMetric): Unit = {
-    val mapStats = shuffleStage.get.mapStats.get.bytesByPartitionId
-    var sum = 0L
-    partitionSpecs.foreach {
-      case CoalescedPartitionSpec(startReducerIndex, endReducerIndex) =>
-        val dataSize = startReducerIndex.until(endReducerIndex).map(mapStats(_)).sum
-        metrics("partitionDataSize").set(dataSize)
-        SQLMetrics.postDriverMetricUpdates(sparkContext, executionId, Seq{partitionMetrics})
-        sum += dataSize
-      case p: PartialReducerPartitionSpec =>
-        metrics("partitionDataSize").set(p.dataSize)
-        SQLMetrics.postDriverMetricUpdates(sparkContext, executionId, Seq{partitionMetrics})
-        sum += p.dataSize
-      case p => throw new IllegalStateException("unexpected " + p)
+    val mapStats = shuffleStage.get.mapStats
+    if (mapStats.isEmpty) {
+      metrics("partitionDataSize").set(0)
+      SQLMetrics.postDriverMetricUpdates(sparkContext, executionId, Seq{partitionMetrics})
+    } else {
+      var sum = 0L
+      partitionSpecs.foreach {
+        case CoalescedPartitionSpec(startReducerIndex, endReducerIndex) =>
+          val dataSize = startReducerIndex.until(endReducerIndex).map(
+            mapStats.get.bytesByPartitionId(_)).sum
+          metrics("partitionDataSize").set(dataSize)
+          SQLMetrics.postDriverMetricUpdates(sparkContext, executionId, Seq{partitionMetrics})
+          sum += dataSize
+        case p: PartialReducerPartitionSpec =>
+          metrics("partitionDataSize").set(p.dataSize)
+          SQLMetrics.postDriverMetricUpdates(sparkContext, executionId, Seq{partitionMetrics})
+          sum += p.dataSize
+        case p => throw new IllegalStateException("unexpected " + p)
+      }
+      // Set sum value to "partitionDataSize" metric.
+      metrics("partitionDataSize").set(sum)
     }
-    // Set sum value to "partitionDataSize" metric.
-    metrics("partitionDataSize").set(sum)
   }
 
   @transient override lazy val metrics: Map[String, SQLMetric] = {
@@ -156,7 +162,7 @@ case class CustomShuffleReaderExec private(
     SQLMetrics.postDriverMetricUpdates(sparkContext, executionId,
       metrics.filter(_._1 != "partitionDataSize").values.toSeq)
 
-    if(!isLocalReader) {
+    if(!isLocalReader && shuffleStage.get.mapStats.isDefined) {
       sendPartitionDataSizeMetrics(executionId, metrics.get("partitionDataSize").get)
     }
     shuffleStage.map { stage =>
