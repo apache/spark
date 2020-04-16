ea053bc9810bf23d3a333e1a4a4e68249071f79e
Kousuke Saruta <sarutak@oss.nttdata.com>
2020-04-17 04:42:47 +0900

    Fixed the usage of getopts in do-release-docker.sh.

--- dev/create-release/do-release-docker.sh
+++ dev/create-release/do-release-docker.sh
@@ -54,7 +54,7 @@ WORKDIR=
 IMGTAG=latest
 JAVA=
 RELEASE_STEP=
-while getopts "d:hj:ns:t:" opt; do
+while getopts ":d:hj:ns:t:" opt; do
   case $opt in
     d) WORKDIR="$OPTARG" ;;
     n) DRY_RUN=1 ;;
