8a741702aca6a8c61b51873d806639b1a6c3ae49
yi.wu <yi.wu@databricks.com>
2020-04-20 21:50:50 +0800

    val future

--- core/src/main/scala/org/apache/spark/rpc/RpcEndpointRef.scala
+++ core/src/main/scala/org/apache/spark/rpc/RpcEndpointRef.scala
@@ -114,9 +114,7 @@ private[spark] class RpcAbortException(message: String) extends Exception(messag
  * A wrapper for [[Future]] but add abort method.
  * This is used in long run RPC and provide an approach to abort the RPC.
  */
-private[spark] class AbortableRpcFuture[T: ClassTag](f: Future[T], onAbort: Throwable => Unit) {
-
+private[spark]
+class AbortableRpcFuture[T: ClassTag](val future: Future[T], onAbort: Throwable => Unit) {
   def abort(t: Throwable): Unit = onAbort(t)
-
-  def future: Future[T] = f
 }
