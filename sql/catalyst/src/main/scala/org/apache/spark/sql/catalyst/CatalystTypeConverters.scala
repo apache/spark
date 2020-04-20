ac0b27a04f762962b0f58ad3423127e432dc07f6
Max Gekk <max.gekk@gmail.com>
2020-04-20 23:41:49 +0300

    Revert "Remove useJava8DateTimeApi from convertToScala()"
    This reverts commit d52fe37f2fa6062ca9e7b0f9750b97746c337303.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/CatalystTypeConverters.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/CatalystTypeConverters.scala
@@ -478,7 +478,10 @@ object CatalystTypeConverters {
    * This method is slow, and for batch conversion you should be using converter
    * produced by createToScalaConverter.
    */
-  def convertToScala(catalystValue: Any, dataType: DataType): Any = {
-    createToScalaConverter(dataType, useJava8DateTimeApi = false)(catalystValue)
+  def convertToScala(
+      catalystValue: Any,
+      dataType: DataType,
+      useJava8DateTimeApi: Boolean = SQLConf.get.datetimeJava8ApiEnabled): Any = {
+    createToScalaConverter(dataType, useJava8DateTimeApi)(catalystValue)
   }
 }
