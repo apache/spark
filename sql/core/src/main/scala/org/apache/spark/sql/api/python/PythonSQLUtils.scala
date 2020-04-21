284ab7008a8cbb5f9df1d27652b4ad0b622c2395
Kent Yao <yaooqinn@hotmail.com>
2020-04-21 09:53:58 +0800

    fix test

--- sql/core/src/main/scala/org/apache/spark/sql/api/python/PythonSQLUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/api/python/PythonSQLUtils.scala
@@ -43,7 +43,7 @@ private[sql] object PythonSQLUtils {
   def listSQLConfigs(): Array[(String, String, String, String)] = {
     val conf = new SQLConf()
     // Py4J doesn't seem to translate Seq well, so we convert to an Array.
-    conf.getAllDefinedConfs.toArray
+    conf.getAllDefinedConfs.filterNot(p => SQLConf.staticConfKeys.contains(p._1)).toArray
   }
 
   def listStaticSQLConfigs(): Array[(String, String, String, String)] = {
