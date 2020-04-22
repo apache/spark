1dc14d503c5b58deab195f82e0f7357a0fda7f20
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-04-22 12:50:36 +0200

    WTForms 2.3.0 break our Flask apps (#8512)
    * WTForms 2.3.0 break our Flask apps

--- setup.py
+++ setup.py
@@ -598,6 +598,7 @@ INSTALL_REQUIREMENTS = [
     'tzlocal>=1.4,<2.0.0',
     'unicodecsv>=0.14.1',
     'werkzeug<1.0.0',
+    'WTforms<2.3.0',  # TODO: Remove after https://github.com/dpgaspar/Flask-AppBuilder/issues/1356 is fixed and released.
 ]
 
 
