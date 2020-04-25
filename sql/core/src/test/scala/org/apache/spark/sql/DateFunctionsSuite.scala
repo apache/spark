eb54e6c3e8490cb63b52ccea65b6ca4f195fbcf9
Kent Yao <yaooqinn@hotmail.com>
2020-04-25 09:39:44 -0700

    [SPARK-31528][SQL][3.0] Remove millennium, century, decade from trunc/date_trunc fucntions
    The PR backport https://github.com/apache/spark/pull/28313 from master to 3.0
    
    ### What changes were proposed in this pull request?
    
    Similar to https://jira.apache.org/jira/browse/SPARK-31507, millennium, century, and decade are not commonly used in most modern platforms.
    
    For example
    Negative:
    https://docs.snowflake.com/en/sql-reference/functions-date-time.html#supported-date-and-time-parts
    https://prestodb.io/docs/current/functions/datetime.html#date_trunc
    https://teradata.github.io/presto/docs/148t/functions/datetime.html#date_trunc
    https://www.oracletutorial.com/oracle-date-functions/oracle-trunc/
    
    Positive:
    https://docs.aws.amazon.com/redshift/latest/dg/r_Dateparts_for_datetime_functions.html
    https://www.postgresql.org/docs/9.1/functions-datetime.html#FUNCTIONS-DATETIME-TRUNC
    
    This PR removes these `fmt`s support for trunc and date_trunc functions.
    
    ### Why are the changes needed?
    
    clean uncommon datetime unit for easy maintenance, we can add them back if they are found very useful later.
    
    ### Does this PR introduce any user-facing change?
    no, targeting 3.0.0, these are newly added in 3.0.0
    
    ### How was this patch tested?
    
    remove and modify existing units tests
    
    Closes #28340 from yaooqinn/SPARK-31528-30.
    
    Authored-by: Kent Yao <yaooqinn@hotmail.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/DateFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DateFunctionsSuite.scala
@@ -474,10 +474,6 @@ class DateFunctionsSuite extends QueryTest with SharedSparkSession {
     checkAnswer(
       df.selectExpr("trunc(t, 'Month')"),
       Seq(Row(Date.valueOf("2015-07-01")), Row(Date.valueOf("2014-12-01"))))
-
-    checkAnswer(
-      df.selectExpr("trunc(t, 'decade')"),
-      Seq(Row(Date.valueOf("2010-01-01")), Row(Date.valueOf("2010-01-01"))))
   }
 
   test("function date_trunc") {
@@ -529,17 +525,15 @@ class DateFunctionsSuite extends QueryTest with SharedSparkSession {
       df.selectExpr("date_trunc('MILLISECOND', t)"),
       Seq(Row(Timestamp.valueOf("2015-07-22 10:01:40.123")),
         Row(Timestamp.valueOf("2014-12-31 05:29:06.123"))))
+  }
 
+  test("unsupported fmt fields for trunc/date_trunc results null") {
+    Seq("INVALID", "decade", "century", "millennium", "whatever", null).foreach { f =>
     checkAnswer(
-      df.selectExpr("date_trunc('DECADE', t)"),
-      Seq(Row(Timestamp.valueOf("2010-01-01 00:00:00")),
-        Row(Timestamp.valueOf("2010-01-01 00:00:00"))))
-
-    Seq("century", "millennium").foreach { level =>
-      checkAnswer(
-        df.selectExpr(s"date_trunc('$level', t)"),
-        Seq(Row(Timestamp.valueOf("2001-01-01 00:00:00")),
-          Row(Timestamp.valueOf("2001-01-01 00:00:00"))))
+      Seq(Date.valueOf("2014-12-31"))
+        .toDF("dt")
+        .selectExpr(s"date_trunc('$f', dt)", "trunc(dt, '$f')"),
+      Row(null, null))
     }
   }
 
