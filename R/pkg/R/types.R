8004d221bf860faf057d2f997d5b15f1592eb5c8
Michael Chirico <michael.chirico@grabtaxi.com>
2020-04-27 23:27:55 +0800

    typo

--- R/pkg/R/types.R
+++ R/pkg/R/types.R
@@ -95,7 +95,7 @@ checkSchemaInArrow <- function(schema) {
 
   # Both cases below produce a corrupt value for unknown reason. It needs to be investigated.
   field_strings = sapply(schema$fields(), function(x) x$dataType.toString())
-  if (any(field_strings == "FloatType"))) {
+  if (any(field_strings == "FloatType")) {
     stop("Arrow optimization in R does not support float type yet.")
   }
   if (any(field_strings == "BinaryType")) {
