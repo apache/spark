3aa659ce29877f386a24da9d04e66069d04afaa8
Max Gekk <max.gekk@gmail.com>
2020-05-02 17:54:36 +0900

    [MINOR][SQL][TESTS] Disable UI in SQL benchmarks by default
    ### What changes were proposed in this pull request?
    Set `spark.ui.enabled` to `false` in `SqlBasedBenchmark.getSparkSession`. This disables UI in all SQL benchmarks by default.
    
    ### Why are the changes needed?
    UI overhead lowers numbers in the `Relative` column and impacts on `Stdev` in benchmark results.
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Checked by running `DateTimeRebaseBenchmark`.
    
    Closes #28432 from MaxGekk/ui-off-in-benchmarks.
    
    Authored-by: Max Gekk <max.gekk@gmail.com>
    Signed-off-by: Takeshi Yamamuro <yamamuro@apache.org>
    (cherry picked from commit 13dddee9a8490ead00ff00bd741db4a170dfd759)
    Signed-off-by: Takeshi Yamamuro <yamamuro@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/SqlBasedBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/SqlBasedBenchmark.scala
@@ -18,6 +18,7 @@
 package org.apache.spark.sql.execution.benchmark
 
 import org.apache.spark.benchmark.{Benchmark, BenchmarkBase}
+import org.apache.spark.internal.config.UI.UI_ENABLED
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.SaveMode.Overwrite
 import org.apache.spark.sql.catalyst.plans.SQLHelper
@@ -37,6 +38,7 @@ trait SqlBasedBenchmark extends BenchmarkBase with SQLHelper {
       .appName(this.getClass.getCanonicalName)
       .config(SQLConf.SHUFFLE_PARTITIONS.key, 1)
       .config(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key, 1)
+      .config(UI_ENABLED.key, false)
       .getOrCreate()
   }
 
