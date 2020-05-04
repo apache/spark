99d50269dbd7d60c67b8f6ce43123d8023a8f307
Jalpan Randeri <randerij@amazon.com>
2020-05-03 22:29:56 -0700

    fix me to remove category conversion for pyarrow > 16

--- python/pyspark/sql/pandas/serializers.py
+++ python/pyspark/sql/pandas/serializers.py
@@ -156,6 +156,7 @@ class ArrowStreamPandasSerializer(ArrowStreamSerializer):
             if t is not None and pa.types.is_timestamp(t):
                 s = _check_series_convert_timestamps_internal(s, self._timezone)
             elif type(s.dtype) == pd.CategoricalDtype:
+                # FIXME: This can be removed once minimum pyarrow version is >= 0.16.1
                 s = s.astype(s.dtypes.categories.dtype)
             try:
                 array = pa.Array.from_pandas(s, mask=mask, type=t, safe=self._safecheck)
