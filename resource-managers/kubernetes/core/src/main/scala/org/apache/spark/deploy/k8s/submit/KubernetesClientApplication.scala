14c4cfa0719912d35ab09bc52f650d2627a96960
Stijn De Haes <stijndehaes@gmail.com>
2020-05-04 08:29:08 +0200

    Fetch latest state before continuing

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/submit/KubernetesClientApplication.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/submit/KubernetesClientApplication.scala
@@ -23,6 +23,7 @@ import java.util.Properties
 
 import io.fabric8.kubernetes.api.model._
 import io.fabric8.kubernetes.client.{KubernetesClient, KubernetesClientException, Watch}
+import io.fabric8.kubernetes.client.Watcher.Action
 import scala.collection.mutable
 import scala.util.control.NonFatal
 import util.control.Breaks._
@@ -145,10 +146,9 @@ private[spark] class Client(
     breakable {
       while (true) {
         try {
-            watch = kubernetesClient
-              .pods()
-              .withName(driverPodName)
-              .watch(watcher)
+            val podWithName = kubernetesClient.pods().withName(driverPodName)
+            watch = podWithName.watch(watcher)
+            watcher.eventReceived(Action.MODIFIED, podWithName.get())
             watcher.watchOrStop(sId)
             break
         } catch {
