0577bb2c4c94b61ec9e09ab40b8dceca4e02584c
zhengruifeng <ruifengz@foxmail.com>
2020-05-06 12:29:53 +0800

    fix doc

--- mllib-local/src/main/scala/org/apache/spark/ml/impl/Utils.scala
+++ mllib-local/src/main/scala/org/apache/spark/ml/impl/Utils.scala
@@ -81,8 +81,9 @@ private[spark] object Utils {
 
   /**
    * When `x` is positive and large, computing `math.log(1 + math.exp(x))` will lead to arithmetic
-   * overflow. This will happen when `x > 709.78` which is not a very large number.
-   * It can be addressed by rewriting the formula into `x + math.log1p(math.exp(-x))` when `x > 0`.
+   * overflow. This will happen when `x &gt; 709.78` which is not a very large number.
+   * It can be addressed by rewriting the formula into `x + math.log1p(math.exp(-x))`
+   * when `x` is positive.
    * @param x a floating-point value as input.
    * @return the result of `math.log(1 + math.exp(x))`.
    */
