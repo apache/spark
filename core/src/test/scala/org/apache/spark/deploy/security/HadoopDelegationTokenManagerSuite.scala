32df81371ce18e0ef22c0df618c2d4340285872b
Kent Yao <yaooqinn@hotmail.com>
2020-05-06 21:56:01 +0800

    interval

--- core/src/test/scala/org/apache/spark/deploy/security/HadoopDelegationTokenManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/security/HadoopDelegationTokenManagerSuite.scala
@@ -101,7 +101,7 @@ class HadoopDelegationTokenManagerSuite extends SparkFunSuite {
       // The workaround here is to periodically repeat this process with a timeout , since we are
       // using Hadoop 2.7.4 as default.
       // https://issues.apache.org/jira/browse/SPARK-31631
-      eventually(timeout(10.seconds), interval(100.milliseconds)) {
+      eventually(timeout(10.seconds), interval(1.second)) {
         try {
           kdc = new MiniKdc(kdcConf, kdcDir)
           kdc.start()
