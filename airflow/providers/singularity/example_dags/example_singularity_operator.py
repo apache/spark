42c59755affd49cd35bea8464e2a4c9256084d88
Ash Berlin-Taylor <ash_github@firemirror.com>
2020-05-09 00:49:51 +0100

    Update example SingularityOperator DAG (#8790)
    The main thing I was fixning here was `start_date=utcnow()` which is
    always going to be wrong (discovered via a test in #8772).
    
    While I was updating the DAG I updated it to use context manager, and
    shift operators.

--- airflow/providers/singularity/example_dags/example_singularity_operator.py
+++ airflow/providers/singularity/example_dags/example_singularity_operator.py
@@ -16,16 +16,16 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.singularity.operators.singularity import SingularityOperator
+from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.utcnow(),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -33,30 +33,31 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(
-    'singularity_sample', default_args=default_args, schedule_interval=timedelta(minutes=10))
+with DAG('singularity_sample',
+         default_args=default_args,
+         schedule_interval=timedelta(minutes=10),
+         start_date=days_ago(0)) as dag:
 
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
-    dag=dag)
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
+        dag=dag)
 
-t2 = BashOperator(
-    task_id='sleep',
-    bash_command='sleep 5',
-    retries=3,
-    dag=dag)
+    t2 = BashOperator(
+        task_id='sleep',
+        bash_command='sleep 5',
+        retries=3,
+        dag=dag)
 
-t3 = SingularityOperator(command='/bin/sleep 30',
-                         image='docker://busybox:1.30.1',
-                         task_id='singularity_op_tester',
-                         dag=dag)
+    t3 = SingularityOperator(command='/bin/sleep 30',
+                             image='docker://busybox:1.30.1',
+                             task_id='singularity_op_tester',
+                             dag=dag)
 
-t4 = BashOperator(
-    task_id='print_hello',
-    bash_command='echo "hello world!!!"',
-    dag=dag)
+    t4 = BashOperator(
+        task_id='print_hello',
+        bash_command='echo "hello world!!!"',
+        dag=dag)
 
-t1.set_downstream(t2)
-t1.set_downstream(t3)
-t3.set_downstream(t4)
+    t1 >> [t2, t3]
+    t3 >> t4
