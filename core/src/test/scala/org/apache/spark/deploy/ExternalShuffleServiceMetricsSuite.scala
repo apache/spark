57b55eacaaa37b2b5aae2620fb29e046ee736b08
Dongjoon Hyun <dongjoon@apache.org>
2020-05-07 17:50:32 -0700

    [SPARK-31646][FOLLOWUP][TESTS] Add clean up code and disable irrelevent conf
    (cherry picked from commit 24fac1e0c70a783b4d240607639ff20d7dd24191)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- core/src/test/scala/org/apache/spark/deploy/ExternalShuffleServiceMetricsSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/ExternalShuffleServiceMetricsSuite.scala
@@ -20,7 +20,7 @@ package org.apache.spark.deploy
 import scala.collection.JavaConverters._
 
 import org.apache.spark.{SecurityManager, SparkConf, SparkFunSuite}
-import org.apache.spark.internal.config.SHUFFLE_SERVICE_ENABLED
+import org.apache.spark.internal.config.{SHUFFLE_SERVICE_DB_ENABLED, SHUFFLE_SERVICE_ENABLED}
 import org.apache.spark.util.Utils
 
 class ExternalShuffleServiceMetricsSuite extends SparkFunSuite {
@@ -32,6 +32,7 @@ class ExternalShuffleServiceMetricsSuite extends SparkFunSuite {
     super.beforeAll()
     sparkConf = new SparkConf()
     sparkConf.set(SHUFFLE_SERVICE_ENABLED, true)
+    sparkConf.set(SHUFFLE_SERVICE_DB_ENABLED, false)
     sparkConf.set("spark.local.dir", System.getProperty("java.io.tmpdir"))
     Utils.loadDefaultSparkProperties(sparkConf, null)
     val securityManager = new SecurityManager(sparkConf)
@@ -40,7 +41,10 @@ class ExternalShuffleServiceMetricsSuite extends SparkFunSuite {
   }
 
   override def afterAll(): Unit = {
-    externalShuffleService.stop()
+    if (externalShuffleService != null) {
+      externalShuffleService.stop()
+    }
+    super.afterAll()
   }
 
   test("SPARK-31646: metrics should be registered") {
