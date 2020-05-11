ef17f703d2a5536ede5b19d8c1c7dfcf112e299d
Liang Zhang <liang.zhang@databricks.com>
2020-05-11 16:17:16 -0700

    fix error argument is of length zero

--- R/pkg/R/serialize.R
+++ R/pkg/R/serialize.R
@@ -58,7 +58,7 @@ writeObject <- function(con, object, writeType = TRUE) {
   # Checking types is needed here, since 'is.na' only handles atomic vectors,
   # lists and pairlists
   if (type %in% c("integer", "character", "logical", "double", "numeric")) {
-    if (is.na(object)) {
+    if (is.null(object) || is.na(object)) {
       object <- NULL
       type <- "NULL"
     }
