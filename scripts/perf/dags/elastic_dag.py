5ae76d8cc0d7946582b8562a9bceb09715462121
Ash Berlin-Taylor <ash_github@firemirror.com>
2020-05-11 14:42:16 +0100

    Option to set end_date for performance testing dag. (#8817)
    I wanted an option to run a specific number of dag runs to completion,
    so this feature lets me control the end_date of the dag without having
    to know exactly what value it would have.
    
    The "is string" check is more simplistic than it needs to be, but it's
    Good Enough for now for an optional feature.

--- scripts/perf/dags/elastic_dag.py
+++ scripts/perf/dags/elastic_dag.py
@@ -155,9 +155,16 @@ START_DATE_ENV = os.environ.get("PERF_START_AGO", "1h")
 START_DATE = datetime.now() - parse_time_delta(START_DATE_ENV)
 SCHEDULE_INTERVAL_ENV = os.environ.get("PERF_SCHEDULE_INTERVAL", "@once")
 SCHEDULE_INTERVAL = parse_schedule_interval(SCHEDULE_INTERVAL_ENV)
+
 SHAPE = DagShape(os.environ["PERF_SHAPE"])
 
 args = {"owner": "airflow", "start_date": START_DATE}
+
+if "PERF_MAX_RUNS" in os.environ:
+    if isinstance(SCHEDULE_INTERVAL, str):
+        raise ValueError("Can't set max runs with string-based schedule_interval")
+    num_runs = int(os.environ["PERF_MAX_RUNS"])
+    args['end_date'] = START_DATE + (SCHEDULE_INTERVAL * (num_runs - 1))
 
 for dag_no in range(1, DAG_COUNT + 1):
     dag = DAG(
