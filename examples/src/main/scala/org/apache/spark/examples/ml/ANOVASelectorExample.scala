76edc3de6bddb5b44dc0caf3c1aa37ebdeea9d53
Huaxin Gao <huaxing@us.ibm.com>
2020-05-13 17:02:14 -0700

    [SPARK-31708][ML][DOCS] Add docs and examples for ANOVASelector and FValueSelector

--- examples/src/main/scala/org/apache/spark/examples/ml/ANOVASelectorExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/ml/ANOVASelectorExample.scala
@@ -19,42 +19,48 @@
 package org.apache.spark.examples.ml
 
 // $example on$
-import org.apache.spark.ml.linalg.{Vector, Vectors}
-import org.apache.spark.ml.stat.ANOVATest
+import org.apache.spark.ml.feature.ANOVASelector
+import org.apache.spark.ml.linalg.Vectors
 // $example off$
 import org.apache.spark.sql.SparkSession
 
 /**
- * An example for ANOVA testing.
+ * An example for ANOVASelector.
  * Run with
  * {{{
- * bin/run-example ml.ANOVATestExample
+ * bin/run-example ml.ANOVASelectorExample
  * }}}
  */
-object ANOVATestExample {
-
+object ANOVASelectorExample {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder
-      .appName("ANOVATestExample")
+      .appName("ANOVASelectorExample")
       .getOrCreate()
     import spark.implicits._
 
     // $example on$
     val data = Seq(
-      (3.0, Vectors.dense(1.7, 4.4, 7.6, 5.8, 9.6, 2.3)),
-      (2.0, Vectors.dense(8.8, 7.3, 5.7, 7.3, 2.2, 4.1)),
-      (3.0, Vectors.dense(1.2, 9.5, 2.5, 3.1, 8.7, 2.5)),
-      (2.0, Vectors.dense(3.7, 9.2, 6.1, 4.1, 7.5, 3.8)),
-      (4.0, Vectors.dense(8.9, 5.2, 7.8, 8.3, 5.2, 3.0)),
-      (4.0, Vectors.dense(7.9, 8.5, 9.2, 4.0, 9.4, 2.1))
+      (1, Vectors.dense(1.7, 4.4, 7.6, 5.8, 9.6, 2.3), 3.0),
+      (2, Vectors.dense(8.8, 7.3, 5.7, 7.3, 2.2, 4.1), 2.0),
+      (3, Vectors.dense(1.2, 9.5, 2.5, 3.1, 8.7, 2.5), 3.0),
+      (4, Vectors.dense(3.7, 9.2, 6.1, 4.1, 7.5, 3.8), 2.0),
+      (5, Vectors.dense(8.9, 5.2, 7.8, 8.3, 5.2, 3.0), 4.0),
+      (6, Vectors.dense(7.9, 8.5, 9.2, 4.0, 9.4, 2.1), 4.0)
     )
 
-    val df = data.toDF("label", "features")
-    val anova = ANOVATest.test(df, "features", "label").head
-    println(s"pValues = ${anova.getAs[Vector](0)}")
-    println(s"degreesOfFreedom ${anova.getSeq[Int](1).mkString("[", ",", "]")}")
-    println(s"fValues ${anova.getAs[Vector](2)}")
+    val df = spark.createDataset(data).toDF("id", "features", "label")
+
+    val selector = new ANOVASelector()
+      .setNumTopFeatures(1)
+      .setFeaturesCol("features")
+      .setLabelCol("label")
+      .setOutputCol("selectedFeatures")
+
+    val result = selector.fit(df).transform(df)
+
+    println(s"ANOVASelector output with top ${selector.getNumTopFeatures} features selected")
+    result.show()
     // $example off$
 
     spark.stop()
