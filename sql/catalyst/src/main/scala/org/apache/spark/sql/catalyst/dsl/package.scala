f9486d57b45a2e8c93ec41939d748a252a993598
Yuanjian Li <xyliyuanjian@gmail.com>
2020-05-16 04:37:18 +0000

    [SPARK-31663][SQL] Grouping sets with having clause returns the wrong result
    - Resolve the havingcondition with expanding the GROUPING SETS/CUBE/ROLLUP expressions together in `ResolveGroupingAnalytics`:
        - Change the operations resolving directions to top-down.
        - Try resolving the condition of the filter as though it is in the aggregate clause by reusing the function in `ResolveAggregateFunctions`
        - Push the aggregate expressions into the aggregate which contains the expanded operations.
    - Use UnresolvedHaving for all having clause.
    
    Correctness bug fix. See the demo and analysis in SPARK-31663.
    
    Yes, correctness bug fix for HAVING with GROUPING SETS.
    
    New UTs added.
    
    Closes #28501 from xuanyuanking/SPARK-31663.
    
    Authored-by: Yuanjian Li <xyliyuanjian@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit 86bd37f37eb1e534c520dc9a02387debf9fa05a1)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/dsl/package.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/dsl/package.scala
@@ -368,7 +368,7 @@ package object dsl {
           groupingExprs: Expression*)(
           aggregateExprs: Expression*)(
           havingCondition: Expression): LogicalPlan = {
-        AggregateWithHaving(havingCondition,
+        UnresolvedHaving(havingCondition,
           groupBy(groupingExprs: _*)(aggregateExprs: _*).asInstanceOf[Aggregate])
       }
 
