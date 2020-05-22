f4f8fbec028b7c44c4f3e93912fe526a9560b8fd
Kent Yao <yaooqinn@hotmail.com>
2020-05-22 19:18:26 +0800

    mv tests

--- sql/core/src/test/resources/sql-tests/inputs/misc-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/misc-functions.sql
@@ -8,6 +8,3 @@ select typeof(cast(1.0 as float)), typeof(1.0D), typeof(1.2);
 select typeof(date '1986-05-23'),  typeof(timestamp '1986-05-23'), typeof(interval '23 days');
 select typeof(x'ABCD'), typeof('SPARK');
 select typeof(array(1, 2)), typeof(map(1, 2)), typeof(named_struct('a', 1, 'b', 'spark'));
-
--- get current_catalog
-select current_catalog();
