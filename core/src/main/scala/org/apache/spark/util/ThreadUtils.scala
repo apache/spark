cc8d522098440ed92ff8b8cf2c9d78ef464b4966
Izek Greenfield <igreenfield@axiomsl.com>
2020-05-27 10:09:04 +0300

    remove unused method.

--- core/src/main/scala/org/apache/spark/util/ThreadUtils.scala
+++ core/src/main/scala/org/apache/spark/util/ThreadUtils.scala
@@ -229,14 +229,6 @@ private[spark] object ThreadUtils {
   }
 
   /**
-   * Wrapper over newCachedThreadPool. Thread names are formatted as prefix-ID, where ID is a
-   * unique, sequentially assigned integer.
-   */
-  def newCachedThreadPool(threadFactory: ThreadFactory): ThreadPoolExecutor = {
-    MDCAwareThreadPoolExecutor.newCachedThreadPool(threadFactory)
-  }
-
-  /**
    * Create a cached thread pool whose max number of threads is `maxThreadNumber`. Thread names
    * are formatted as prefix-ID, where ID is a unique, sequentially assigned integer.
    */
