fcb3e438dd0ee09143cace2a87acb65283e6bc78
Wenchen Fan <wenchen@databricks.com>
2020-05-27 15:22:12 +0800

    address comments

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DateTimeFormatterHelper.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DateTimeFormatterHelper.scala
@@ -218,9 +218,12 @@ private object DateTimeFormatterHelper {
   }
 
   final val bugInStandAloneForm = {
-    val formatter = DateTimeFormatter.ofPattern("LLL", Locale.US)
-    // JDK 8 has a bug for stand-alone form. See https://bugs.openjdk.java.net/browse/JDK-8114833
+    // Java 8 has a bug for stand-alone form. See https://bugs.openjdk.java.net/browse/JDK-8114833
+    // Note: we only check the US locale so that it's a static check. It can produce false-negative
+    // as some locales are not affected by the bug. Since `L` is rarely used, we choose to not
+    // complicate the check here.
     // TODO: remove it when we drop Java 8 support.
+    val formatter = DateTimeFormatter.ofPattern("LLL", Locale.US)
     formatter.format(LocalDate.of(2000, 1, 1)) == "1"
   }
   final val unsupportedLetters = Set('A', 'c', 'e', 'n', 'N', 'p')
@@ -251,9 +254,9 @@ private object DateTimeFormatterHelper {
             throw new IllegalArgumentException(s"Too many pattern letters: ${style.head}")
           }
           if (bugInStandAloneForm && (patternPart.contains("LLL") || patternPart.contains("qqq"))) {
-            throw new IllegalArgumentException("The current JDK has a bug to support stand-alone " +
+            throw new IllegalArgumentException("Java 8 has a bug to support stand-alone " +
               "form (3 or more 'L' or 'q' in the pattern string). Please use 'M' or 'Q' instead, " +
-              "or upgrade your JDK version. For more details, please read " +
+              "or upgrade your Java version. For more details, please read " +
               "https://bugs.openjdk.java.net/browse/JDK-8114833")
           }
           // The meaning of 'u' was day number of week in SimpleDateFormat, it was changed to year
