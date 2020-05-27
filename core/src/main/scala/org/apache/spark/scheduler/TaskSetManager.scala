c7426668bd4adaf26d44740db80a0cf65153f207
yi.wu <yi.wu@databricks.com>
2020-05-28 00:22:09 +0800

    address comment

--- core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSetManager.scala
@@ -1113,7 +1113,7 @@ private[spark] class TaskSetManager(
     localityWaits = myLocalityLevels.map(getLocalityWait)
     currentLocalityIndex = getLocalityIndex(previousLocalityLevel)
     if (currentLocalityIndex > previousLocalityIndex) {
-      // SPARK-31837: there's new higher level locality, so shift to
+      // SPARK-31837: there's new higher locality level, so shift to
       // the highest locality level in terms of better data locality
       currentLocalityIndex = 0
     }
