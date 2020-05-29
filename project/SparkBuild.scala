625abca9db7976065d0e0e8ecf96e378fbc35749
Dongjoon Hyun <dongjoon@apache.org>
2020-05-29 07:46:53 -0700

    [SPARK-31858][BUILD] Upgrade commons-io to 2.5 in Hadoop 3.2 profile
    ### What changes were proposed in this pull request?
    
    This PR aims to upgrade `commons-io` from 2.4 to 2.5 for Apache Spark 3.1.
    
    ### Why are the changes needed?
    
    Since Hadoop 3.1, `commons-io` 2.5 is used.
    - https://issues.apache.org/jira/browse/HADOOP-15261
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Pass the Jenkins with Hadoop-3.2 profile.
    
    Maven dependency is verified via `test-dependencies.sh` automatically. SBT dependency can be verified like the following manually.
    ```
    build/sbt -Phadoop-3.2 "core/dependencyTree" | grep commons-io:commons-io | head -n1
    [info]   | | +-commons-io:commons-io:2.5
    ```
    
    Closes #28665 from dongjoon-hyun/SPARK-31858.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -623,7 +623,6 @@ object KubernetesIntegrationTests {
 object DependencyOverrides {
   lazy val settings = Seq(
     dependencyOverrides += "com.google.guava" % "guava" % "14.0.1",
-    dependencyOverrides += "commons-io" % "commons-io" % "2.4",
     dependencyOverrides += "xerces" % "xercesImpl" % "2.12.0",
     dependencyOverrides += "jline" % "jline" % "2.14.6",
     dependencyOverrides += "org.apache.avro" % "avro" % "1.8.2")
