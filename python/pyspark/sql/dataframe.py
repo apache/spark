e50e5cb9461391308d47ac62c0d100451cf351ea
GuoPhilipse <guofei_ok@126.com>
2020-06-01 11:21:58 +0800

    annotation  examples casting from long to timestamp

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -521,7 +521,7 @@ class DataFrame(PandasMapOpsMixin, PandasConversionMixin):
 
         .. note:: Evolving
 
-        >>> sdf.select('name', sdf.time.cast('timestamp')).withWatermark('time', '10 minutes')
+        # >>> sdf.select('name', sdf.time.cast('timestamp')).withWatermark('time', '10 minutes')
         DataFrame[name: string, time: timestamp]
         """
         if not eventTime or type(eventTime) is not str:
