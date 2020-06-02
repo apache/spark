7e32341b33f565460b0bd159297dc973802dda93
Holden Karau <hkarau@apple.com>
2020-06-01 17:38:02 -0700

    Return faster with shuffle blocks since we don't need the rest of the logic in update block :)

--- core/src/main/scala/org/apache/spark/storage/BlockManagerMasterEndpoint.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManagerMasterEndpoint.scala
@@ -499,11 +499,14 @@ class BlockManagerMasterEndpoint(
         case ShuffleIndexBlockId(shuffleId, mapId, _) =>
           // Don't update the map output on just the index block
           logDebug("Received shuffle index block update for ${shuffleId} ${mapId}")
+          return true
         case ShuffleDataBlockId(shuffleId: Int, mapId: Long, reduceId: Int) =>
           logInfo("Received shuffle data block update for ${shuffleId} ${mapId}, performing update")
           mapOutputTracker.updateMapOutput(shuffleId, mapId, blockManagerId)
+          return true
         case _ =>
           logDebug(s"Unexpected shuffle block type ${blockId}")
+          return false
       }
     }
 
