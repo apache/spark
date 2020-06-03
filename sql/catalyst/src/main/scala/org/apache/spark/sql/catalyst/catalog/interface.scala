d5e999fe734f363ce7daf82705f42ae55dfce598
Karuppayya Rajendran <karuppayya1990@gmail.com>
2020-06-02 19:03:57 -0700

    Fix: Address review comments - use Option instead of Some

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/interface.scala
@@ -654,7 +654,7 @@ case class HiveTableRelation(
     tableMeta: CatalogTable,
     dataCols: Seq[AttributeReference],
     partitionCols: Seq[AttributeReference],
-    tableStats: Option[Statistics] = Some(Statistics(sizeInBytes = SQLConf.get.defaultSizeInBytes)),
+    tableStats: Option[Statistics] = Option(Statistics(sizeInBytes = SQLConf.get.defaultSizeInBytes)),
     @transient prunedPartitions: Option[Seq[CatalogTablePartition]] = None)
   extends LeafNode with MultiInstanceRelation {
   assert(tableMeta.identifier.database.isDefined)
