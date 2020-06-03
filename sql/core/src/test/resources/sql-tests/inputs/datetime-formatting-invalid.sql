11c96530a853d73a24e0c7a5a4c0fe0fe77a13f2
Kent Yao <yaooqinn@hotmail.com>
2020-06-04 01:21:40 +0800

    address comments

--- sql/core/src/test/resources/sql-tests/inputs/datetime-formatting-invalid.sql
+++ sql/core/src/test/resources/sql-tests/inputs/datetime-formatting-invalid.sql
@@ -2,42 +2,35 @@
 
 -- separating this from datetime-formatting.sql ,because the text form
 -- for patterns with 5 letters in SimpleDateFormat varies from different JDKs
-create temporary view v as select col from values
- (timestamp '1582-06-01 11:33:33.123UTC+080000'),
- (timestamp '1970-01-01 00:00:00.000Europe/Paris'),
- (timestamp '1970-12-31 23:59:59.999Asia/Srednekolymsk'),
- (timestamp '1996-04-01 00:33:33.123Australia/Darwin'),
- (timestamp '2018-11-17 13:33:33.123Z'),
- (timestamp '2020-01-01 01:33:33.123Asia/Shanghai'),
- (timestamp '2100-01-01 01:33:33.123America/Los_Angeles') t(col);
-
-select col, date_format(col, 'GGGGG') from v;
-select col, date_format(col, 'yyyyyyyyyyy') from v; -- pattern letter count can not be greater than 10
-select col, date_format(col, 'YYYYYYYYYYY') from v;
+select date_format('2018-11-17 13:33:33.333', 'GGGGG');
+-- pattern letter count can not be greater than 10
+select date_format('2018-11-17 13:33:33.333', 'yyyyyyyyyyy');
+select date_format('2018-11-17 13:33:33.333', 'YYYYYYYYYYY');
 -- q/L in JDK 8 will fail when the count is more than 2
-select col, date_format(col, 'qqqqq') from v;
-select col, date_format(col, 'QQQQQ') from v;
-select col, date_format(col, 'MMMMM') from v;
-select col, date_format(col, 'LLLLL') from v;
-select col, date_format(col, 'www') from v;
-select col, date_format(col, 'WW') from v;
-select col, date_format(col, 'uuuuu') from v;
-select col, date_format(col, 'EEEEE') from v;
-select col, date_format(col, 'FF') from v;
-select col, date_format(col, 'ddd') from v;
-select col, date_format(col, 'DD') from v;
-select col, date_format(col, 'DDDD') from v;
-select col, date_format(col, 'HHH') from v;
-select col, date_format(col, 'hhh') from v;
-select col, date_format(col, 'kkk') from v;
-select col, date_format(col, 'KKK') from v;
-select col, date_format(col, 'mmm') from v;
-select col, date_format(col, 'sss') from v;
-select col, date_format(col, 'SSSSSSSSSS') from v;
-select col, date_format(col, 'aa') from v;
-select col, date_format(col, 'V') from v;
-select col, date_format(col, 'zzzzz') from v;
-select col, date_format(col, 'XXXXXX') from v;
-select col, date_format(col, 'ZZZZZZ') from v;
-select col, date_format(col, 'OO') from v;
-select col, date_format(col, 'xxxxxx') from v;
+select date_format('2018-11-17 13:33:33.333', 'qqqqq');
+select date_format('2018-11-17 13:33:33.333', 'QQQQQ');
+select date_format('2018-11-17 13:33:33.333', 'MMMMM');
+select date_format('2018-11-17 13:33:33.333', 'LLLLL');
+select date_format('2018-11-17 13:33:33.333', 'www');
+select date_format('2018-11-17 13:33:33.333', 'WW');
+select date_format('2018-11-17 13:33:33.333', 'uuuuu');
+select date_format('2018-11-17 13:33:33.333', 'EEEEE');
+select date_format('2018-11-17 13:33:33.333', 'FF');
+select date_format('2018-11-17 13:33:33.333', 'ddd');
+-- DD is invalid in 8, but valid in 11 for the JDKs that PR builder uses
+-- select date_format('2018-11-17 13:33:33.333', 'DD');
+select date_format('2018-11-17 13:33:33.333', 'DDDD');
+select date_format('2018-11-17 13:33:33.333', 'HHH');
+select date_format('2018-11-17 13:33:33.333', 'hhh');
+select date_format('2018-11-17 13:33:33.333', 'kkk');
+select date_format('2018-11-17 13:33:33.333', 'KKK');
+select date_format('2018-11-17 13:33:33.333', 'mmm');
+select date_format('2018-11-17 13:33:33.333', 'sss');
+select date_format('2018-11-17 13:33:33.333', 'SSSSSSSSSS');
+select date_format('2018-11-17 13:33:33.333', 'aa');
+select date_format('2018-11-17 13:33:33.333', 'V');
+select date_format('2018-11-17 13:33:33.333', 'zzzzz');
+select date_format('2018-11-17 13:33:33.333', 'XXXXXX');
+select date_format('2018-11-17 13:33:33.333', 'ZZZZZZ');
+select date_format('2018-11-17 13:33:33.333', 'OO');
+select date_format('2018-11-17 13:33:33.333', 'xxxxxx');
