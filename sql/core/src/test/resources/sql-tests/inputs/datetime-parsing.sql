6b8dec4b8905fac295ac978a344a5a15eb9fb90a
Kent Yao <yaooqinn@hotmail.com>
2020-06-10 17:25:21 +0800

    nit

--- sql/core/src/test/resources/sql-tests/inputs/datetime-parsing.sql
+++ sql/core/src/test/resources/sql-tests/inputs/datetime-parsing.sql
@@ -12,5 +12,5 @@ select to_timestamp('12-365', 'MM-DDD');
 select to_timestamp('2020-365', 'yyyy-DDD');
 select to_timestamp('12-31-365', 'MM-dd-DDD');
 select to_timestamp('2020-30-365', 'yyyy-dd-DDD');
-select to_timestamp('2020-11-350', 'yyyy-MM-DDD');
+select to_timestamp('2020-12-350', 'yyyy-MM-DDD');
 select to_timestamp('2020-12-31-366', 'yyyy-MM-dd-DDD');
