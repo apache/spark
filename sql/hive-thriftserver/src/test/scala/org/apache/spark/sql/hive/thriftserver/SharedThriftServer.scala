d055d60aab356401f0f7b00b83b76dc76df0c30c
Kent Yao <yaooqinn@hotmail.com>
2020-06-10 22:09:51 +0800

    [SPARK-31957][SQL] Cleanup hive scratch dir for the developer api startWithContext

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/SharedThriftServer.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/SharedThriftServer.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.sql.hive.thriftserver
 
+import java.io.File
 import java.sql.{DriverManager, Statement}
 
 import scala.collection.JavaConverters._
@@ -27,11 +28,19 @@ import org.apache.hadoop.hive.conf.HiveConf.ConfVars
 import org.apache.hive.service.cli.thrift.ThriftCLIService
 
 import org.apache.spark.sql.test.SharedSparkSession
+import org.apache.spark.util.Utils
 
 trait SharedThriftServer extends SharedSparkSession {
 
   private var hiveServer2: HiveThriftServer2 = _
   private var serverPort: Int = 0
+
+  protected val tempScratchDir: File = {
+    val dir = Utils.createTempDir()
+    dir.setWritable(true, false)
+    Utils.createTempDir(dir.getAbsolutePath)
+    dir
+  }
 
   def mode: ServerMode.Value
 
@@ -85,6 +94,9 @@ trait SharedThriftServer extends SharedSparkSession {
     sqlContext.setConf(ConfVars.HIVE_SERVER2_THRIFT_PORT.varname, "0")
     sqlContext.setConf(ConfVars.HIVE_SERVER2_THRIFT_HTTP_PORT.varname, "0")
     sqlContext.setConf(ConfVars.HIVE_SERVER2_TRANSPORT_MODE.varname, mode.toString)
+    sqlContext.setConf(ConfVars.SCRATCHDIR.varname, tempScratchDir.getAbsolutePath)
+    sqlContext.setConf(ConfVars.HIVE_START_CLEANUP_SCRATCHDIR.varname, "true")
+    assert(tempScratchDir.exists())
 
     try {
       hiveServer2 = HiveThriftServer2.startWithContext(sqlContext)
@@ -94,6 +106,9 @@ trait SharedThriftServer extends SharedSparkSession {
           logInfo(s"Started HiveThriftServer2: port=$serverPort, attempt=$attempt")
         case _ =>
       }
+      // the scratch dir will be recreated after the probe sql `SELECT 1` executed, so we
+      // check it here first.
+      assert(!tempScratchDir.exists())
 
       // Wait for thrift server to be ready to serve the query, via executing simple query
       // till the query succeeds. See SPARK-30345 for more details.
