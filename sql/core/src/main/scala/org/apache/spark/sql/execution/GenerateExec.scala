a269c80a97b943b49734265d660c2f0ac329206b
Karuppayya Rajendran <karuppayya1990@gmail.com>
2020-06-11 09:26:24 -0700

    Fix: Handle review comments

--- sql/core/src/main/scala/org/apache/spark/sql/execution/GenerateExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/GenerateExec.scala
@@ -124,7 +124,7 @@ case class GenerateExec(
     }
   }
 
-  override def supportCodegen: Boolean = true
+  override def supportCodegen: Boolean = super.supportCodegen && generator.supportCodegen
 
   override def inputRDDs(): Seq[RDD[InternalRow]] = {
     child.asInstanceOf[CodegenSupport].inputRDDs()
