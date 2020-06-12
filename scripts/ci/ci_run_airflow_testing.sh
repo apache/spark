apache/spark

commit da748d02517d7903e92785e9da15458db35d20de

Remove remnant kubernetes stuff from breeze scripts (#9138)

scripts/ci/ci_run_airflow_testing.sh
@@ -17,25 +17,8 @@
 # under the License.
 export VERBOSE=${VERBOSE:="false"}
 
-function run_airflow_testing_in_docker_with_kubernetes() {
-    export KUBERNETES_MODE=${KUBERNETES_MODE:="image"}
-    export KUBERNETES_VERSION=${KUBERNETES_VERSION:="v1.15.3"}
-
-    # shellcheck disable=SC2016
-    docker-compose --log-level INFO \
-      -f "${MY_DIR}/docker-compose/base.yml" \
-      -f "${MY_DIR}/docker-compose/backend-${BACKEND}.yml" \
-      -f "${MY_DIR}/docker-compose/runtime-kubernetes.yml" \
-      "${INTEGRATIONS[@]}" \
-      "${DOCKER_COMPOSE_LOCAL[@]}" \
-         run airflow \
-           '/opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"' \
-           /opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"
-         # Note the command is there twice (!) because it is passed via bash -c
-         # and bash -c starts passing parameters from $0. TODO: fixme
-}
-
 function run_airflow_testing_in_docker() {
+    set +u
     # shellcheck disable=SC2016
     docker-compose --log-level INFO \
       -f "${MY_DIR}/docker-compose/base.yml" \
@@ -47,8 +30,8 @@ function run_airflow_testing_in_docker() {
            /opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"
          # Note the command is there twice (!) because it is passed via bash -c
          # and bash -c starts passing parameters from $0. TODO: fixme
+    set -u
 }
-
 
 # shellcheck source=scripts/ci/_script_init.sh
 . "$( dirname "${BASH_SOURCE[0]}" )/_script_init.sh"
@@ -129,11 +112,7 @@ MAX_RETRIES=4
 while [[ ${MAX_RETRIES} -ge "0" ]]
 do
     set +e
-    if [[ ${RUNTIME:=} == "kubernetes" ]]; then
-        run_airflow_testing_in_docker_with_kubernetes "${@}"
-    else
-        run_airflow_testing_in_docker "${@}"
-    fi
+    run_airflow_testing_in_docker "${@}"
     EXIT_CODE=$?
     set -e
     if [[ ${EXIT_CODE} == 254 ]] ; then
@@ -148,26 +127,7 @@ do
     fi
     exit ${EXIT_CODE}
 done
-set -u
 
-#TODO FIXME
-if [[ "${INTEGRATIONS[*]}" == *"kubernetes"* ]]; then
-    export KUBERNETES_MODE=${KUBERNETES_MODE:="image"}
-    export KUBERNETES_VERSION=${KUBERNETES_VERSION:="v1.15.3"}
-    export KIND_CLUSTER_NAME="airflow-python-${PYTHON_MAJOR_MINOR_VERSION}-${KUBERNETES_VERSION}"
-
-    if [[ ${ENABLE_KIND_CLUSTER} == "true" ]]; then
-        install_kind_in_host
-        "${MY_DIR}/setup_kind_cluster_in_host.sh"
-    fi
-
-    # adding trap to exiting trap
-    HANDLERS="$( trap -p EXIT | cut -f2 -d \' )"
-    # shellcheck disable=SC2064
-    trap "${HANDLERS}${HANDLERS:+;}send_kubernetes_logs_to_file_io" EXIT
-fi
-
-set +u
 # shellcheck disable=SC2016
 docker-compose --log-level INFO \
   -f "${MY_DIR}/docker-compose/base.yml" \

breeze
@@ -378,17 +378,6 @@ EOF
 
 EOF
         fi
-
-        if [[ ${INTEGRATIONS[*]} == *"kubernetes"* ]]; then
-            cat <<EOF
-
-                               Kubernetes mode:     ${KUBERNETES_MODE}
-                               Kubernetes version:  ${KUBERNETES_VERSION}
-
-                               Enable KinD cluster: ${ENABLE_KIND_CLUSTER}
-                               Cluster operation:   ${KIND_CLUSTER_OPERATION}
-EOF
-        fi
     else
         if [[ ${PRODUCTION_IMAGE} == "true" ]]; then
             cat <<EOF
@@ -422,18 +411,6 @@ EOF
 
 EOF
         fi
-        set +u
-        if [[ ${INTEGRATIONS[*]} == *"kubernetes"* ]]; then
-            cat <<EOF
-
-   Kubernetes mode:     ${KUBERNETES_MODE}
-   Kubernetes version:  ${KUBERNETES_VERSION}
-
-   Enable KinD cluster: ${ENABLE_KIND_CLUSTER}
-   Cluster operation:   ${KIND_CLUSTER_OPERATION}
-EOF
-        fi
-        set -u
     fi
 }
 
@@ -458,9 +435,6 @@ export DOCKERHUB_REPO=${DOCKERHUB_REPO}
 export COMPOSE_FILE="${COMPOSE_FILE}"
 export PYTHON_MAJOR_MINOR_VERSION="${PYTHON_MAJOR_MINOR_VERSION}"
 export BACKEND="${BACKEND}"
-export ENABLE_KIND_CLUSTER="${ENABLE_KIND_CLUSTER}"
-export KUBERNETES_MODE="${KUBERNETES_MODE}"
-export KUBERNETES_VERSION="${KUBERNETES_VERSION}"
 export AIRFLOW_VERSION="${AIRFLOW_VERSION}"
 export INSTALL_AIRFLOW_VERSION="${INSTALL_AIRFLOW_VERSION}"
 export RUN_TESTS="${TESTS}"

scripts/ci/in_container/entrypoint_ci.sh
@@ -26,9 +26,6 @@ AIRFLOW_SOURCES=$(cd "${MY_DIR}/../../.." || exit 1; pwd)
 
 PYTHON_MAJOR_MINOR_VERSION=${PYTHON_MAJOR_MINOR_VERSION:=3.6}
 BACKEND=${BACKEND:=sqlite}
-KUBERNETES_MODE=${KUBERNETES_MODE:=""}
-KUBERNETES_VERSION=${KUBERNETES_VERSION:=""}
-ENABLE_KIND_CLUSTER=${ENABLE_KIND_CLUSTER:="false"}
 
 export AIRFLOW_HOME=${AIRFLOW_HOME:=${HOME}}
 
@@ -164,8 +161,6 @@ while [[ $(ssh-keyscan -H localhost 2>/dev/null | wc -l) != "3" ]] ; do
 done
 
 ssh-keyscan -H localhost >> ~/.ssh/known_hosts 2>/dev/null
-
-export KUBERNETES_VERSION=${KUBERNETES_VERSION:=""}
 
 # shellcheck source=scripts/ci/in_container/configure_environment.sh
 . "${MY_DIR}/configure_environment.sh"

tests/models/test_dag.py
@@ -58,8 +58,6 @@ class TestDag(unittest.TestCase):
 
     @staticmethod
     def _clean_up(dag_id: str):
-        if os.environ.get('KUBERNETES_VERSION') is not None:
-            return
         with create_session() as session:
             session.query(DagRun).filter(
                 DagRun.dag_id == dag_id).delete(

commit 2540d9cd0a446ff74b4e4d50c20d8a82303329fc

CI: Propogate Exit Code Correctly (#9247)
This was unfortunately broken since #9138

Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>

scripts/ci/ci_run_airflow_testing.sh
@@ -106,37 +106,4 @@ done
 
 RUN_INTEGRATION_TESTS=${RUN_INTEGRATION_TESTS:=""}
 
-# Repeat tests in case initialization failed
-set +u
-MAX_RETRIES=4
-while [[ ${MAX_RETRIES} -ge "0" ]]
-do
-    set +e
-    run_airflow_testing_in_docker "${@}"
-    EXIT_CODE=$?
-    set -e
-    if [[ ${EXIT_CODE} == 254 ]] ; then
-        echo
-        echo "Retrying on initialization failure. Remaining ${MAX_RETRIES} retries left"
-        echo
-        MAX_RETRIES=$(( MAX_RETRIES - 1 ))
-        # Cleanup docker containers and images to make sure everything is retried from scratch
-        docker-compose -f "${MY_DIR}/docker-compose/base.yml" down --remove-orphans --timeout 20
-        docker system prune --force --volumes
-        continue
-    fi
-    exit ${EXIT_CODE}
-done
-
-# shellcheck disable=SC2016
-docker-compose --log-level INFO \
-  -f "${MY_DIR}/docker-compose/base.yml" \
-  -f "${MY_DIR}/docker-compose/backend-${BACKEND}.yml" \
-  "${INTEGRATIONS[@]}" \
-  "${DOCKER_COMPOSE_LOCAL[@]}" \
-     run airflow \
-       '/opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"' \
-       /opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"
-     # Note the command is there twice (!) because it is passed via bash -c
-     # and bash -c starts passing parameters from $0. TODO: fixme
-set -u
+run_airflow_testing_in_docker "${@}"