e4449430fecd029606c4d48a0d8c82c887704649
ulysses <youxiduo@weidian.com>
2020-06-18 10:39:33 +0800

    fix children

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/v2Commands.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/v2Commands.scala
@@ -520,4 +520,6 @@ case class CommentOnTable(child: LogicalPlan, comment: String) extends Command {
 /**
  * The logical plan of the REFRESH FUNCTION command that works for v2 catalogs.
  */
-case class RefreshFunction(child: LogicalPlan) extends Command
+case class RefreshFunction(child: LogicalPlan) extends Command {
+  override def children: Seq[LogicalPlan] = child :: Nil
+}
