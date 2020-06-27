9c134b57bff5b7e7f9c85aeed2e9539117a5b57d
Dongjoon Hyun <dongjoon@apache.org>
2020-06-26 19:43:29 -0700

    [SPARK-32058][BUILD] Use Apache Hadoop 3.2.0 dependency by default
    ### What changes were proposed in this pull request?
    
    According to the dev mailing list discussion, this PR aims to switch the default Apache Hadoop dependency from 2.7.4 to 3.2.0 for Apache Spark 3.1.0 on December 2020.
    
    | Item | Default Hadoop Dependency |
    |------|-----------------------------|
    | Apache Spark Website | 3.2.0 |
    | Apache Download Site | 3.2.0 |
    | Apache Snapshot | 3.2.0 |
    | Maven Central | 3.2.0 |
    | PyPI | 2.7.4 (We will switch later) |
    | CRAN | 2.7.4 (We will switch later) |
    | Homebrew | 3.2.0 (already) |
    
    In Apache Spark 3.0.0 release, we focused on the other features. This PR targets for [Apache Spark 3.1.0 scheduled on December 2020](https://spark.apache.org/versioning-policy.html).
    
    ### Why are the changes needed?
    
    Apache Hadoop 3.2 has many fixes and new cloud-friendly features.
    
    **Reference**
    - 2017-08-04: https://hadoop.apache.org/release/2.7.4.html
    - 2019-01-16: https://hadoop.apache.org/release/3.2.0.html
    
    ### Does this PR introduce _any_ user-facing change?
    
    Since the default Hadoop dependency changes, the users will get a better support in a cloud environment.
    
    ### How was this patch tested?
    
    Pass the Jenkins.
    
    Closes #28897 from dongjoon-hyun/SPARK-32058.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -574,7 +574,7 @@ def main():
         # if we're on the Amplab Jenkins build servers setup variables
         # to reflect the environment settings
         build_tool = os.environ.get("AMPLAB_JENKINS_BUILD_TOOL", "sbt")
-        hadoop_version = os.environ.get("AMPLAB_JENKINS_BUILD_PROFILE", "hadoop2.7")
+        hadoop_version = os.environ.get("AMPLAB_JENKINS_BUILD_PROFILE", "hadoop3.2")
         hive_version = os.environ.get("AMPLAB_JENKINS_BUILD_HIVE_PROFILE", "hive2.3")
         test_env = "amplab_jenkins"
         # add path for Python3 in Jenkins if we're calling from a Jenkins machine
