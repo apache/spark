bc9cf626c2e293d88a57fa5b2bfca3b4aa063f5d
angerszhu <angers.zhu@gmail.com>
2020-06-29 15:13:29 +0800

    save

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveStrategies.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveStrategies.scala
@@ -22,7 +22,7 @@ import java.util.Locale
 
 import org.apache.hadoop.fs.{FileSystem, Path}
 
-import org.apache.spark.sql.{Strategy, _}
+import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.catalog._
 import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.planning._
