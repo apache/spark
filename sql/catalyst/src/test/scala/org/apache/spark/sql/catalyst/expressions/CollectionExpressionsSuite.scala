9f910d7586b337041753a344b0163f5209cecaf3
TJX2014 <xiaoxingstack@gmail.com>
2020-07-01 07:28:36 +0800

    move test case to Sequence of dates

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CollectionExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/CollectionExpressionsSuite.scala
@@ -933,6 +933,13 @@ class CollectionExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper
           Literal(negateExact(stringToInterval("interval 1 month")))),
         EmptyRow,
         s"sequence boundaries: 0 to 2678400000000 by -${28 * MICROS_PER_DAY}")
+
+      // SPARK-32133: Sequence step must be a day interval if start and end values are dates
+      checkExceptionInExpression[IllegalArgumentException](Sequence(
+        Cast(Literal("2011-03-01"), DateType),
+        Cast(Literal("2011-04-01"), DateType),
+        Option(Literal(stringToInterval("interval 1 hour")))), null,
+        "sequence step must be a day interval if start and end values are dates")
     }
   }
 
@@ -1853,19 +1860,5 @@ class CollectionExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper
       Literal(Date.valueOf("2018-01-01")),
       Literal(stringToInterval("interval 1 year"))),
       Seq(Date.valueOf("2018-01-01")))
-  }
-
-  test("SPARK-32133: Sequence step must be a day interval " +
-    "if start and end values are dates") {
-    checkExceptionInExpression[IllegalArgumentException](Sequence(
-      Cast(Literal("2011-03-01"), DateType),
-      Cast(Literal("2011-04-01"), DateType),
-      Option(Literal(stringToInterval("interval 1 hour")))), null,
-      "sequence step must be a day interval if start and end values are dates")
-    checkEvaluation(Sequence(
-      Cast(Literal("2011-03-01"), DateType),
-      Cast(Literal("2011-03-02"), DateType),
-      Option(Literal(stringToInterval("interval 1 day")))),
-      Seq(Date.valueOf("2011-03-01"), Date.valueOf("2011-03-02")))
   }
 }
