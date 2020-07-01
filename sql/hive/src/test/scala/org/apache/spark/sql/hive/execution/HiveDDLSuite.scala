e31ab70d100b7c3dfc99649d2f1a9e2571ab39d9
ulysses <youxiduo@weidian.com>
2020-07-01 19:11:44 +0800

    rewrite test

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
@@ -2733,19 +2733,33 @@ class HiveDDLSuite
 
   test("SPARK-32144: Retain EXTERNAL property in hive table properties") {
     val catalog = spark.sessionState.catalog
-    withTable("t1") {
-      sql("create table t1(c int)")
-      val table = catalog.getTableMetadata(TableIdentifier("t1"))
-      assert(table.properties.get("EXTERNAL").isEmpty)
-      val alter1 = table.copy(properties = table.properties ++ Map("EXTERNAL" -> "true"))
-      // spark cann't modify EXTERNAL property so we have to use hive client
-      hiveClient.alterTable(alter1)
-      val table1 = catalog.getTableMetadata(TableIdentifier("t1"))
-      assert(table1.properties("EXTERNAL") == "true")
-      val alter2 = table.copy(properties = table.properties ++ Map("EXTERNAL" -> "false"))
-      hiveClient.alterTable(alter2)
-      val table2 = catalog.getTableMetadata(TableIdentifier("t1"))
-      assert(table2.properties("EXTERNAL").isEmpty)
+    withTable("t0", "t1") {
+      Seq(true, false).zipWithIndex.foreach { case (isExternalTable, i) =>
+        val tbl = s"t$i"
+        val external = if (isExternalTable) {
+          "external"
+        } else {
+          ""
+        }
+        sql(s"create $external table $tbl(c int)")
+        val table = catalog.getTableMetadata(TableIdentifier(tbl))
+        val alter1 = table.copy(properties = table.properties ++ Map("EXTERNAL" -> "true"))
+        // spark cann't modify EXTERNAL property so we have to use hive client
+        hiveClient.alterTable(alter1)
+        val table1 = catalog.getTableMetadata(TableIdentifier(tbl))
+        val alter2 = table.copy(properties = table.properties ++ Map("EXTERNAL" -> "false"))
+        hiveClient.alterTable(alter2)
+        val table2 = catalog.getTableMetadata(TableIdentifier(tbl))
+
+        assert(table.properties.get("EXTERNAL").isEmpty)
+        if (isExternalTable) {
+          assert(table1.properties("EXTERNAL") == "false")
+          assert(table2.properties("EXTERNAL").isEmpty)
+        } else {
+          assert(table1.properties("EXTERNAL") == "true")
+          assert(table2.properties("EXTERNAL").isEmpty)
+        }
+      }
     }
   }
 }
