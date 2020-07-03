be6ed86ccd6e5921563be39877d93fb91713bbb9
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-07-03 22:28:43 +0200

    Fixed failing Kubernetes tests after deny_all for experimental API (#9647)
    The tests were broken by #9611

--- scripts/ci/libraries/_kind.sh
+++ scripts/ci/libraries/_kind.sh
@@ -294,7 +294,8 @@ function deploy_airflow_with_helm() {
         --set "defaultAirflowRepository=${DOCKERHUB_USER}/${DOCKERHUB_REPO}" \
         --set "images.airflow.repository=${DOCKERHUB_USER}/${DOCKERHUB_REPO}" \
         --set "images.airflow.tag=${AIRFLOW_PROD_BASE_TAG}" -v 1 \
-        --set "defaultAirflowTag=${AIRFLOW_PROD_BASE_TAG}" -v 1
+        --set "defaultAirflowTag=${AIRFLOW_PROD_BASE_TAG}" -v 1 \
+        --set "api.authBackend=airflow.api.auth.backend.default"
     echo
     popd || exit 1
 }
