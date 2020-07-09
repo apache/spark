86efa456d8e11a2b7e10bce70d4ead20c75acbe1
Takuya UESHIN <ueshin@databricks.com>
2020-07-09 15:51:56 +0900

    [SPARK-32160][CORE][PYSPARK] Disallow to create SparkContext in executors
    ### What changes were proposed in this pull request?
    
    This PR proposes to disallow to create `SparkContext` in executors, e.g., in UDFs.
    
    ### Why are the changes needed?
    
    Currently executors can create SparkContext, but shouldn't be able to create it.
    
    ```scala
    sc.range(0, 1).foreach { _ =>
      new SparkContext(new SparkConf().setAppName("test").setMaster("local"))
    }
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, users won't be able to create `SparkContext` in executors.
    
    ### How was this patch tested?
    
    Addes tests.
    
    Closes #28986 from ueshin/issues/SPARK-32160/disallow_spark_context_in_executors.
    
    Authored-by: Takuya UESHIN <ueshin@databricks.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit cfecc2030d8b4774c1f4754fe81f57fbc61c9c75)
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/tests/test_context.py
+++ python/pyspark/tests/test_context.py
@@ -267,6 +267,14 @@ class ContextTests(unittest.TestCase):
             resources = sc.resources
             self.assertEqual(len(resources), 0)
 
+    def test_disallow_to_create_spark_context_in_executors(self):
+        # SPARK-32160: SparkContext should not be created in executors.
+        with SparkContext("local-cluster[3, 1, 1024]") as sc:
+            with self.assertRaises(Exception) as context:
+                sc.range(2).foreach(lambda _: SparkContext())
+            self.assertIn("SparkContext should only be created and accessed on the driver.",
+                          str(context.exception))
+
 
 class ContextTestsWithResources(unittest.TestCase):
 
