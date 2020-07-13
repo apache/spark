d31e8a3250885061a75d74a08e74b0b70cc493c4
Tobiasz Kędzierski <tobiasz.kedzierski@polidea.com>
2020-07-13 19:50:03 +0200

    Add DAG Source endpoint (#9322)

--- airflow/api_connexion/endpoints/dag_source_endpoint.py
+++ airflow/api_connexion/endpoints/dag_source_endpoint.py
@@ -14,13 +14,34 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import logging
 
-# TODO(mik-laj): We have to implement it.
-#     Do you want to help? Please look at: https://github.com/apache/airflow/issues/8137
+from flask import Response, current_app, request
+from itsdangerous import BadSignature, URLSafeSerializer
+
+from airflow.api_connexion.exceptions import NotFound
+from airflow.api_connexion.schemas.dag_source_schema import dag_source_schema
+from airflow.models.dagcode import DagCode
+
+log = logging.getLogger(__name__)
 
 
-def get_dag_source():
+def get_dag_source(file_token: str):
     """
     Get source code using file token
     """
-    raise NotImplementedError("Not implemented yet.")
+    secret_key = current_app.config["SECRET_KEY"]
+    auth_s = URLSafeSerializer(secret_key)
+    try:
+        path = auth_s.loads(file_token)
+        dag_source = DagCode.code(path)
+    except (BadSignature, FileNotFoundError):
+        raise NotFound("Dag source not found")
+
+    return_type = request.accept_mimetypes.best_match(['text/plain', 'application/json'])
+    if return_type == 'text/plain':
+        return Response(dag_source, headers={'Content-Type': return_type})
+    if return_type == 'application/json':
+        content = dag_source_schema.dumps(dict(content=dag_source))
+        return Response(content, headers={'Content-Type': return_type})
+    return Response("Not Allowed Accept Header", status=406)
--- tests/api_connexion/endpoints/test_dag_source_endpoint.py
+++ tests/api_connexion/endpoints/test_dag_source_endpoint.py
@@ -14,21 +14,22 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import ast
+import os
 import unittest
+from unittest import mock
 
-import pytest
+from itsdangerous import URLSafeSerializer
+from parameterized import parameterized
 
+from airflow import DAG
+from airflow.configuration import conf
+from airflow.models import DagBag
 from airflow.www import app
+from tests.test_utils.db import clear_db_dag_code, clear_db_dags, clear_db_serialized_dags
 
-
-class TestDagSourceEndpoint(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        super().setUpClass()
-        cls.app = app.create_app(testing=True)  # type:ignore
-
-    def setUp(self) -> None:
-        self.client = self.app.test_client()  # type:ignore
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+EXAMPLE_DAG_FILE = os.path.join("airflow", "example_dags", "example_bash_operator.py")
 
 
 class TestGetSource(unittest.TestCase):
@@ -39,8 +40,94 @@ class TestGetSource(unittest.TestCase):
 
     def setUp(self) -> None:
         self.client = self.app.test_client()  # type:ignore
+        self.clear_db()
 
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_should_response_200(self):
-        response = self.client.get("/api/v1/health")
-        assert response.status_code == 200
+    def tearDown(self) -> None:
+        self.clear_db()
+
+    @staticmethod
+    def clear_db():
+        clear_db_dags()
+        clear_db_serialized_dags()
+        clear_db_dag_code()
+
+    @staticmethod
+    def _get_dag_file_docstring(fileloc: str) -> str:
+        with open(fileloc) as f:
+            file_contents = f.read()
+        module = ast.parse(file_contents)
+        docstring = ast.get_docstring(module)
+        return docstring
+
+    @parameterized.expand([(True,), (False,)])
+    def test_should_response_200_text(self, store_dag_code):
+        serializer = URLSafeSerializer(conf.get('webserver', 'SECRET_KEY'))
+        with mock.patch(
+            "airflow.models.dag.settings.STORE_DAG_CODE", store_dag_code
+        ), mock.patch("airflow.models.dagcode.STORE_DAG_CODE", store_dag_code):
+            dagbag = DagBag(dag_folder=EXAMPLE_DAG_FILE)
+            dagbag.sync_to_db()
+            first_dag: DAG = next(iter(dagbag.dags.values()))
+            dag_docstring = self._get_dag_file_docstring(first_dag.fileloc)
+
+            url = f"/api/v1/dagSources/{serializer.dumps(first_dag.fileloc)}"
+            response = self.client.get(url, headers={
+                "Accept": "text/plain"
+            })
+
+            self.assertEqual(200, response.status_code)
+            self.assertIn(dag_docstring, response.data.decode())
+            self.assertEqual('text/plain', response.headers['Content-Type'])
+
+    @parameterized.expand([(True,), (False,)])
+    def test_should_response_200_json(self, store_dag_code):
+        serializer = URLSafeSerializer(conf.get('webserver', 'SECRET_KEY'))
+        with mock.patch(
+            "airflow.models.dag.settings.STORE_DAG_CODE", store_dag_code
+        ), mock.patch("airflow.models.dagcode.STORE_DAG_CODE", store_dag_code):
+            dagbag = DagBag(dag_folder=EXAMPLE_DAG_FILE)
+            dagbag.sync_to_db()
+            first_dag: DAG = next(iter(dagbag.dags.values()))
+            dag_docstring = self._get_dag_file_docstring(first_dag.fileloc)
+
+            url = f"/api/v1/dagSources/{serializer.dumps(first_dag.fileloc)}"
+            response = self.client.get(url, headers={
+                "Accept": 'application/json'
+            })
+
+            self.assertEqual(200, response.status_code)
+            self.assertIn(
+                dag_docstring,
+                response.json['content']
+            )
+            self.assertEqual('application/json', response.headers['Content-Type'])
+
+    @parameterized.expand([(True,), (False,)])
+    def test_should_response_406(self, store_dag_code):
+        serializer = URLSafeSerializer(conf.get('webserver', 'SECRET_KEY'))
+        with mock.patch(
+            "airflow.models.dag.settings.STORE_DAG_CODE", store_dag_code
+        ), mock.patch("airflow.models.dagcode.STORE_DAG_CODE", store_dag_code):
+            dagbag = DagBag(dag_folder=EXAMPLE_DAG_FILE)
+            dagbag.sync_to_db()
+            first_dag: DAG = next(iter(dagbag.dags.values()))
+
+            url = f"/api/v1/dagSources/{serializer.dumps(first_dag.fileloc)}"
+            response = self.client.get(url, headers={
+                "Accept": 'image/webp'
+            })
+
+            self.assertEqual(406, response.status_code)
+
+    @parameterized.expand([(True,), (False,)])
+    def test_should_response_404(self, store_dag_code):
+        with mock.patch(
+            "airflow.models.dag.settings.STORE_DAG_CODE", store_dag_code
+        ), mock.patch("airflow.models.dagcode.STORE_DAG_CODE", store_dag_code):
+            wrong_fileloc = "abcd1234"
+            url = f"/api/v1/dagSources/{wrong_fileloc}"
+            response = self.client.get(url, headers={
+                "Accept": 'application/json'
+            })
+
+            self.assertEqual(404, response.status_code)
