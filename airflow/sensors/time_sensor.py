b34ba874452809495354f3012e0b1dcbf4209e09
zikun <33176974+zikun@users.noreply.github.com>
2020-07-19 01:36:28 +0800

    TimeSensor should respect the default_timezone config (#9699)

--- airflow/sensors/time_sensor.py
+++ airflow/sensors/time_sensor.py
@@ -36,4 +36,4 @@ class TimeSensor(BaseSensorOperator):
 
     def poke(self, context):
         self.log.info('Checking if the time (%s) has come', self.target_time)
-        return timezone.utcnow().time() > self.target_time
+        return timezone.make_naive(timezone.utcnow()).time() > self.target_time
