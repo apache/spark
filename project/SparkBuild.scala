21b92929dadb6c191a2c9710561710c67470648e
Yuanjian Li <xyliyuanjian@gmail.com>
2020-07-22 16:32:16 +0800

    Revert "For A/B testing"
    This reverts commit 38e5ff423084005424461daa8c5085ff32a41a06.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -469,7 +469,6 @@ object SparkParallelTestGrouping {
     "org.apache.spark.sql.catalyst.expressions.MathExpressionsSuite",
     "org.apache.spark.sql.hive.HiveExternalCatalogSuite",
     "org.apache.spark.sql.hive.StatisticsSuite",
-    "org.apache.spark.sql.hive.execution.HiveCompatibilitySuite",
     "org.apache.spark.sql.hive.client.VersionsSuite",
     "org.apache.spark.sql.hive.client.HiveClientVersions",
     "org.apache.spark.sql.hive.HiveExternalCatalogVersionsSuite",
@@ -490,7 +489,7 @@ object SparkParallelTestGrouping {
 
   private def testNameToTestGroup(name: String): String = name match {
     case _ if testsWhichShouldRunInTheirOwnDedicatedJvm.contains(name) => name
-    case _ if name.contains("org.apache.spark.sql.hive.execution") => DEFAULT_TEST_GROUP
+    case _ if name.contains("org.apache.spark.sql.hive.execution") => HIVE_EXECUTION_TEST_GROUP
     case _ => DEFAULT_TEST_GROUP
   }
 
