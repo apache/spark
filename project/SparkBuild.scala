fccf9d2940142c04d5a3d24aeb7b0b2bc0c56d2d
HyukjinKwon <gurwls223@apache.org>
2020-07-24 10:23:36 +0900

    Show Java tests not being ran

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -1040,7 +1040,7 @@ object TestSettings {
       }.getOrElse(Nil): _*),
     testOptions in Test += Tests.Argument(TestFrameworks.JUnit, "-v", "-a"),
     // Required to detect Junit tests for each project, see also https://github.com/sbt/junit-interface/issues/35
-    crossPaths := false,
+    crossPaths := true,
     // Enable Junit testing.
     libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test",
     // `parallelExecutionInTest` controls whether test suites belonging to the same SBT project
