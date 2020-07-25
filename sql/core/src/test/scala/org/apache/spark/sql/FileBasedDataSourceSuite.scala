e0c09201382ffa005b25e189d4a3fe0d0d699bf0
Max Gekk <max.gekk@gmail.com>
2020-07-25 11:58:36 +0300

    Add a test to FileBasedDataSourceSuite

--- sql/core/src/test/scala/org/apache/spark/sql/FileBasedDataSourceSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/FileBasedDataSourceSuite.scala
@@ -902,6 +902,40 @@ class FileBasedDataSourceSuite extends QueryTest
       }
     }
   }
+
+  test("SPARK-32431: consistent error for nested and top-level duplicate columns") {
+    val caseInsensitiveSchema = new StructType()
+      .add("LowerCase", LongType)
+      .add("camelcase", LongType)
+      .add("CamelCase", LongType)
+    Seq(
+      "id AS lowercase", "id + 1 AS camelCase",
+      "NAMED_STRUCT('lowercase', id, 'camelCase', id + 1) AS StructColumn").foreach { selectExpr =>
+      withSQLConf(SQLConf.CASE_SENSITIVE.key -> "false") {
+        Seq("parquet", "orc", "json").map { format =>
+          withTempPath { dir =>
+            val path = dir.getCanonicalPath
+            spark
+              .range(1L)
+              .selectExpr(selectExpr)
+              .write.mode("overwrite")
+              .format(format)
+              .save(path)
+            val e = intercept[AnalysisException] {
+              spark
+                .read
+                .schema(caseInsensitiveSchema)
+                .format(format)
+                .load(path)
+                .show
+            }
+            assert(e.getMessage.contains(
+              "Found duplicate column(s) in the data schema: `camelcase`"))
+          }
+        }
+      }
+    }
+  }
 }
 
 object TestingUDT {
