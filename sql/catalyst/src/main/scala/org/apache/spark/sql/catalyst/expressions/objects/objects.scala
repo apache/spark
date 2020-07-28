73e798e80d7a6dd20337aaae3d6407511feede2a
yi.wu <yi.wu@databricks.com>
2020-07-28 11:48:05 +0800

    codegen

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/objects/objects.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/objects/objects.scala
@@ -920,18 +920,15 @@ case class MapObjects private(
           // Scala WrappedArray
           val getBuilder = s"${cls.getName}$$.MODULE$$.newBuilder()"
           val builder = ctx.freshName("collectionBuilder")
-          val toWrappedArray = (arrayBuffer: ArrayBuffer[_]) =>
-            WrappedArray.make(arrayBuffer.toArray)
-          val toWrappedArrayFunc = ctx.addReferenceObj(
-            "toWrappedArray", toWrappedArray,
-            classOf[ArrayBuffer[_] => WrappedArray[_]].getName)
           (
             s"""
                ${classOf[Builder[_, _]].getName} $builder = $getBuilder;
                $builder.sizeHint($dataLength);
              """,
             (genValue: String) => s"$builder.$$plus$$eq($genValue);",
-            s"(${cls.getName}) $toWrappedArrayFunc.apply($builder.result());"
+            s"(${cls.getName}) ${classOf[WrappedArray[_]].getName}$$." +
+              s"MODULE$$.make(((${classOf[ArrayBuffer[_]].getName})$builder" +
+              s".result()).toArray(scala.reflect.ClassTag$$.MODULE$$.Object()));"
           )
 
         case Some(cls) if classOf[Seq[_]].isAssignableFrom(cls) ||
