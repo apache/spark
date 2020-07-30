78b58337b6b12a0f024cf8ae5ebe4c8be9d90036
HyukjinKwon <gurwls223@apache.org>
2020-07-30 19:28:34 +0900

    [SPARK-32491][INFRA] Do not install SparkR in test-only mode in testing script
    ### What changes were proposed in this pull request?
    
    This PR proposes to skip SparkR installation that is to run R linters (see SPARK-8505) in the test-only mode at `dev/run-tests.py` script.
    
    As of SPARK-32292, the test-only mode in `dev/run-tests.py` was introduced, for example:
    
    ```
    dev/run-tests.py --modules sql,core
    ```
    
    which only runs the relevant tests and does not run other tests such as linters. Therefore, we don't need to install SparkR when `--modules` are specified.
    
    ### Why are the changes needed?
    
    GitHub Actions build is currently failed as below:
    
    ```
    ERROR: this R is version 3.4.4, package 'SparkR' requires R >= 3.5
    [error] running /home/runner/work/spark/spark/R/install-dev.sh ; received return code 1
    ##[error]Process completed with exit code 10.
    ```
    
    For some reasons, looks GitHub Actions started to have R 3.4.4 installed by default; however, R 3.4 was dropped as of SPARK-32073.  When SparkR tests are not needed, GitHub Actions still builds SparkR with a low R version and it causes the test failure.
    
    This PR partially fixes it by avoid the installation of SparkR.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, dev-only.
    
    ### How was this patch tested?
    
    GitHub Actions tests should run to confirm this fix is correct.
    
    Closes #29300 from HyukjinKwon/install-r.
    
    Authored-by: HyukjinKwon <gurwls223@apache.org>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -608,11 +608,14 @@ def main():
               " install one and retry.")
         sys.exit(2)
 
-    # install SparkR
-    if which("R"):
-        run_cmd([os.path.join(SPARK_HOME, "R", "install-dev.sh")])
-    else:
-        print("Cannot install SparkR as R was not found in PATH")
+    # Install SparkR
+    should_only_test_modules = opts.modules is not None
+    if not should_only_test_modules:
+        # If tests modules are specified, we will not run R linter.
+        if which("R"):
+            run_cmd([os.path.join(SPARK_HOME, "R", "install-dev.sh")])
+        else:
+            print("Cannot install SparkR as R was not found in PATH")
 
     if os.environ.get("AMPLAB_JENKINS"):
         # if we're on the Amplab Jenkins build servers setup variables
@@ -642,7 +645,6 @@ def main():
     changed_modules = None
     test_modules = None
     changed_files = None
-    should_only_test_modules = opts.modules is not None
     included_tags = []
     excluded_tags = []
     if should_only_test_modules:
