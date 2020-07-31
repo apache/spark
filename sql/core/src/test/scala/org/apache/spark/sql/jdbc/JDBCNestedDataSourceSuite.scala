5d1dcaf37713b500f85f594bc7d6314a9270a740
Max Gekk <max.gekk@gmail.com>
2020-07-31 19:23:04 +0300

    Renaming: Jdbc -> JDBC

--- sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCNestedDataSourceSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCNestedDataSourceSuite.scala
@@ -21,7 +21,7 @@ import org.apache.spark.sql.NestedDataSourceSuiteBase
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.util.Utils
 
-class JdbcNestedDataSourceSuite extends NestedDataSourceSuiteBase {
+class JDBCNestedDataSourceSuite extends NestedDataSourceSuiteBase {
   override val nestedDataSources: Seq[String] = Seq("jdbc")
   private val tempDir = Utils.createTempDir()
   private val url = s"jdbc:h2:${tempDir.getCanonicalPath};user=testUser;password=testPass"
