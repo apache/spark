dfc73873f6342774a8f96782c2e4853ede86a190
Huaxin Gao <huaxing@us.ibm.com>
2020-08-04 23:24:49 -0700

    remove private[sql]

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
@@ -179,7 +179,7 @@ object JdbcUtils extends Logging {
     }
   }
 
-  private[sql] def getJdbcType(dt: DataType, dialect: JdbcDialect): JdbcType = {
+  def getJdbcType(dt: DataType, dialect: JdbcDialect): JdbcType = {
     dialect.getJDBCType(dt).orElse(getCommonJDBCType(dt)).getOrElse(
       throw new IllegalArgumentException(s"Can't get JDBC type for ${dt.catalogString}"))
   }
