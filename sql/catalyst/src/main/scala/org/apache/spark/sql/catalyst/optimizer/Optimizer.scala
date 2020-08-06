a71e96bf3c22d979d15b8b373c262bdf6c6e4cf9
Karl-WangSK <wskqing@qq.com>
2020-08-06 11:56:00 +0800

    update

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
@@ -1825,7 +1825,6 @@ object OptimizeLimitZero extends Rule[LogicalPlan] {
   }
 }
 
-
 /**
  * Split [[Expand]] into several splitExpand if the projection size of Expand is larger
  * than default projection size.
