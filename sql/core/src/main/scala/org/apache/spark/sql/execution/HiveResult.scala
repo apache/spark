d34b08961a2da012cd01ddfa01febc84115b66cf
Karl-WangSK <wskqing@qq.com>
2020-08-07 16:14:22 +0800

    update

--- sql/core/src/main/scala/org/apache/spark/sql/execution/HiveResult.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/HiveResult.scala
@@ -107,7 +107,8 @@ object HiveResult {
     case (s: String, StringType) => if (nested) "\"" + s + "\"" else s
     case (interval: CalendarInterval, CalendarIntervalType) => interval.toString
     case (seq: Seq[_], ArrayType(typ, _)) =>
-      seq.map(v => (v, typ)).map(e => toHiveString(e, true, formatters)).sorted.mkString("[", ",", "]")
+      seq.map(v => (v, typ)).map(e => toHiveString(e, true, formatters)).sorted
+        .mkString("[", ",", "]")
     case (m: Map[_, _], MapType(kType, vType, _)) =>
       m.map { case (key, value) =>
         toHiveString((key, kType), true, formatters) + ":" +
