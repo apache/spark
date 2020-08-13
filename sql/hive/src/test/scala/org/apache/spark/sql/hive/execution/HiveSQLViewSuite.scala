d20a82fbf43e2a3c5f114c5c7906d8872abf19dc
ulysses <youxiduo@weidian.com>
2020-08-13 19:27:22 +0800

    fix

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveSQLViewSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveSQLViewSuite.scala
@@ -138,8 +138,9 @@ class HiveSQLViewSuite extends SQLViewSuite with TestHiveSingleton {
     }
   }
 
-  test("Add HiveVoidType to compatible with Hive void type") {
+  test("SPARK-20680: Add HiveVoidType to compatible with Hive void type") {
     withView("v1") {
+      sql("drop view if exists v1")
       sql("create view v1 as select null as c")
       val df = sql("select * from v1")
       assert(df.schema.fields.head.dataType == NullType)
