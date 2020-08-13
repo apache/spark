5c2e630fd83a9b32af4e471401d7bf47823750a5
Karl-WangSK <53078090+Karl-WangSK@users.noreply.github.com>
2020-08-13 19:44:33 +0800

    Update SQLConf.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2700,7 +2700,7 @@ object SQLConf {
       "can reduce the shuffle pressure and improve performance in multidimensional analysis " +
       "when data is huge.")
     .intConf
-    .createWithDefault(8)
+    .createWithDefault(4)
 
   val LEGACY_COMPLEX_TYPES_TO_STRING =
     buildConf("spark.sql.legacy.castComplexTypesToString.enabled")
