8b119f1663dc65c96f20606ac8405169d8f9d31d
Wenchen Fan <wenchen@databricks.com>
2020-08-20 13:26:39 -0700

    [SPARK-32640][SQL] Downgrade Janino to fix a correctness bug
    ### What changes were proposed in this pull request?
    
    This PR reverts https://github.com/apache/spark/pull/27860 to downgrade Janino, as the new version has a bug.
    
    ### Why are the changes needed?
    
    The symptom is about NaN comparison. For code below
    ```
    if (double_value <= 0.0) {
      ...
    } else {
      ...
    }
    ```
    
    If `double_value` is NaN, `NaN <= 0.0` is false and we should go to the else branch. However, current Spark goes to the if branch and causes correctness issues like SPARK-32640.
    
    One way to fix it is:
    ```
    boolean cond = double_value <= 0.0;
    if (cond) {
      ...
    } else {
      ...
    }
    ```
    
    I'm not familiar with Janino so I don't know what's going on there.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, fix correctness bugs.
    
    ### How was this patch tested?
    
    a new test
    
    Closes #29495 from cloud-fan/revert.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala
@@ -27,9 +27,8 @@ import scala.util.control.NonFatal
 
 import com.google.common.cache.{CacheBuilder, CacheLoader}
 import com.google.common.util.concurrent.{ExecutionError, UncheckedExecutionException}
-import org.codehaus.commons.compiler.{CompileException, InternalCompilerException}
-import org.codehaus.commons.compiler.util.reflect.ByteArrayClassLoader
-import org.codehaus.janino.{ClassBodyEvaluator, SimpleCompiler}
+import org.codehaus.commons.compiler.CompileException
+import org.codehaus.janino.{ByteArrayClassLoader, ClassBodyEvaluator, InternalCompilerException, SimpleCompiler}
 import org.codehaus.janino.util.ClassFile
 
 import org.apache.spark.{TaskContext, TaskKilledException}
@@ -1421,10 +1420,9 @@ object CodeGenerator extends Logging {
   private def updateAndGetCompilationStats(evaluator: ClassBodyEvaluator): ByteCodeStats = {
     // First retrieve the generated classes.
     val classes = {
-      val scField = classOf[ClassBodyEvaluator].getDeclaredField("sc")
-      scField.setAccessible(true)
-      val compiler = scField.get(evaluator).asInstanceOf[SimpleCompiler]
-      val loader = compiler.getClassLoader.asInstanceOf[ByteArrayClassLoader]
+      val resultField = classOf[SimpleCompiler].getDeclaredField("result")
+      resultField.setAccessible(true)
+      val loader = resultField.get(evaluator).asInstanceOf[ByteArrayClassLoader]
       val classesField = loader.getClass.getDeclaredField("classes")
       classesField.setAccessible(true)
       classesField.get(loader).asInstanceOf[JavaMap[String, Array[Byte]]].asScala
