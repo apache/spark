5c6e4e2b218ac9aad333a59279f2c50ee1db2864
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-08-23 17:10:27 +0200

    Make Stats pylint compatible (#10496)

--- airflow/stats.py
+++ airflow/stats.py
@@ -32,36 +32,40 @@ log = logging.getLogger(__name__)
 
 class StatsLogger(Protocol):
     """This class is only used for TypeChecking (for IDEs, mypy, pylint, etc)"""
+    @classmethod
     def incr(cls, stat: str, count: int = 1, rate: int = 1) -> None:
-        ...
+        """Increment stat"""
 
+    @classmethod
     def decr(cls, stat: str, count: int = 1, rate: int = 1) -> None:
-        ...
+        """Decrement stat"""
 
+    @classmethod
     def gauge(cls, stat: str, value: float, rate: int = 1, delta: bool = False) -> None:
-        ...
+        """Gauge stat"""
 
+    @classmethod
     def timing(cls, stat: str, dt) -> None:
-        ...
+        """Stats timing"""
 
 
 class DummyStatsLogger:
     """If no StatsLogger is configured, DummyStatsLogger is used as a fallback"""
     @classmethod
     def incr(cls, stat, count=1, rate=1):
-        pass
+        """Increment stat"""
 
     @classmethod
     def decr(cls, stat, count=1, rate=1):
-        pass
+        """Decrement stat"""
 
     @classmethod
     def gauge(cls, stat, value, rate=1, delta=False):
-        pass
+        """Gauge stat"""
 
     @classmethod
     def timing(cls, stat, dt):
-        pass
+        """Stats timing"""
 
 
 # Only characters in the character set are considered valid
@@ -108,7 +112,7 @@ def validate_stat(fn: T) -> T:
             return fn(_self, stat_name, *args, **kwargs)
         except InvalidStatsNameException:
             log.error('Invalid stat name: %s.', stat, exc_info=True)
-            return
+            return None
 
     return cast(T, wrapper)
 
@@ -123,6 +127,7 @@ class AllowListValidator:
             self.allow_list = None
 
     def test(self, stat):
+        """ Test if stat is in the Allow List """
         if self.allow_list is not None:
             return stat.strip().lower().startswith(self.allow_list)
         else:
@@ -138,23 +143,31 @@ class SafeStatsdLogger:
 
     @validate_stat
     def incr(self, stat, count=1, rate=1):
+        """Increment stat"""
         if self.allow_list_validator.test(stat):
             return self.statsd.incr(stat, count, rate)
+        return None
 
     @validate_stat
     def decr(self, stat, count=1, rate=1):
+        """Decrement stat"""
         if self.allow_list_validator.test(stat):
             return self.statsd.decr(stat, count, rate)
+        return None
 
     @validate_stat
     def gauge(self, stat, value, rate=1, delta=False):
+        """Gauge stat"""
         if self.allow_list_validator.test(stat):
             return self.statsd.gauge(stat, value, rate, delta)
+        return None
 
     @validate_stat
     def timing(self, stat, dt):
+        """Stats timing"""
         if self.allow_list_validator.test(stat):
             return self.statsd.timing(stat, dt)
+        return None
 
 
 class SafeDogStatsdLogger:
@@ -166,27 +179,35 @@ class SafeDogStatsdLogger:
 
     @validate_stat
     def incr(self, stat, count=1, rate=1, tags=None):
+        """Increment stat"""
         if self.allow_list_validator.test(stat):
             tags = tags or []
             return self.dogstatsd.increment(metric=stat, value=count, tags=tags, sample_rate=rate)
+        return None
 
     @validate_stat
     def decr(self, stat, count=1, rate=1, tags=None):
+        """Decrement stat"""
         if self.allow_list_validator.test(stat):
             tags = tags or []
             return self.dogstatsd.decrement(metric=stat, value=count, tags=tags, sample_rate=rate)
+        return None
 
     @validate_stat
-    def gauge(self, stat, value, rate=1, delta=False, tags=None):
+    def gauge(self, stat, value, rate=1, delta=False, tags=None):  # pylint: disable=unused-argument
+        """Gauge stat"""
         if self.allow_list_validator.test(stat):
             tags = tags or []
             return self.dogstatsd.gauge(metric=stat, value=value, tags=tags, sample_rate=rate)
+        return None
 
     @validate_stat
     def timing(self, stat, dt, tags=None):
+        """Stats timing"""
         if self.allow_list_validator.test(stat):
             tags = tags or []
             return self.dogstatsd.timing(metric=stat, value=dt, tags=tags)
+        return None
 
 
 class _Stats(type):
@@ -195,38 +216,41 @@ class _Stats(type):
     def __getattr__(cls, name):
         return getattr(cls.instance, name)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(self)
-        if self.__class__.instance is None:
+    def __init__(cls, *args, **kwargs):
+        super().__init__(cls)
+        if cls.__class__.instance is None:
             try:
                 is_datadog_enabled_defined = conf.has_option('scheduler', 'statsd_datadog_enabled')
                 if is_datadog_enabled_defined and conf.getboolean('scheduler', 'statsd_datadog_enabled'):
-                    self.__class__.instance = self.get_dogstatsd_logger()
+                    cls.__class__.instance = cls.get_dogstatsd_logger()
                 elif conf.getboolean('scheduler', 'statsd_on'):
-                    self.__class__.instance = self.get_statsd_logger()
+                    cls.__class__.instance = cls.get_statsd_logger()
                 else:
-                    self.__class__.instance = DummyStatsLogger()
+                    cls.__class__.instance = DummyStatsLogger()
             except (socket.gaierror, ImportError) as e:
                 log.error("Could not configure StatsClient: %s, using DummyStatsLogger instead.", e)
-                self.__class__.instance = DummyStatsLogger()
+                cls.__class__.instance = DummyStatsLogger()
 
-    def get_statsd_logger(self):
-        if conf.getboolean('scheduler', 'statsd_on'):
-            from statsd import StatsClient
+    @classmethod
+    def get_statsd_logger(cls):
+        """Returns logger for statsd"""
+        # no need to check for the scheduler/statsd_on -> this method is only called when it is set
+        # and previously it would crash with None is callable if it was called without it.
+        from statsd import StatsClient
 
-            if conf.has_option('scheduler', 'statsd_custom_client_path'):
-                stats_class = conf.getimport('scheduler', 'statsd_custom_client_path')
+        if conf.has_option('scheduler', 'statsd_custom_client_path'):
+            stats_class = conf.getimport('scheduler', 'statsd_custom_client_path')
 
-                if not issubclass(stats_class, StatsClient):
-                    raise AirflowConfigException(
-                        "Your custom Statsd client must extend the statsd.StatsClient in order to ensure "
-                        "backwards compatibility."
-                    )
-                else:
-                    log.info("Successfully loaded custom Statsd client")
-
+            if not issubclass(stats_class, StatsClient):
+                raise AirflowConfigException(
+                    "Your custom Statsd client must extend the statsd.StatsClient in order to ensure "
+                    "backwards compatibility."
+                )
             else:
-                stats_class = StatsClient
+                log.info("Successfully loaded custom Statsd client")
+
+        else:
+            stats_class = StatsClient
 
         statsd = stats_class(
             host=conf.get('scheduler', 'statsd_host'),
@@ -235,18 +259,22 @@ class _Stats(type):
         allow_list_validator = AllowListValidator(conf.get('scheduler', 'statsd_allow_list', fallback=None))
         return SafeStatsdLogger(statsd, allow_list_validator)
 
-    def get_dogstatsd_logger(self):
+    @classmethod
+    def get_dogstatsd_logger(cls):
+        """Get DataDog statsd logger"""
         from datadog import DogStatsd
         dogstatsd = DogStatsd(
             host=conf.get('scheduler', 'statsd_host'),
             port=conf.getint('scheduler', 'statsd_port'),
             namespace=conf.get('scheduler', 'statsd_prefix'),
-            constant_tags=self.get_constant_tags())
+            constant_tags=cls.get_constant_tags())
         dogstatsd_allow_list = conf.get('scheduler', 'statsd_allow_list', fallback=None)
         allow_list_validator = AllowListValidator(dogstatsd_allow_list)
         return SafeDogStatsdLogger(dogstatsd, allow_list_validator)
 
-    def get_constant_tags(self):
+    @classmethod
+    def get_constant_tags(cls):
+        """Get constanst DataDog tags to add to all stats"""
         tags = []
         tags_in_string = conf.get('scheduler', 'statsd_datadog_tags', fallback=None)
         if tags_in_string is None or tags_in_string == '':
@@ -261,4 +289,4 @@ if TYPE_CHECKING:
     Stats: StatsLogger
 else:
     class Stats(metaclass=_Stats):  # noqa: D101
-        pass
+        """Empty class for Stats - we use metaclass to inject the right one"""
