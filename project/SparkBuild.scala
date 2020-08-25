c5ba1e6248dd9aa085b4ff79909bae0ebbc8c50e
Denis Pyshev <git@gemelen.net>
2020-08-25 03:50:12 +0300

    Reuse avro auto-generated classes in sql/core module tests

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -671,6 +671,9 @@ object Catalyst {
 }
 
 object SQL {
+
+  import sbtavro.SbtAvro.autoImport._
+
   lazy val settings = Seq(
     initialCommands in console :=
       """
@@ -692,8 +695,10 @@ object SQL {
         |import sqlContext.implicits._
         |import sqlContext._
       """.stripMargin,
-    cleanupCommands in console := "sc.stop()"
+    cleanupCommands in console := "sc.stop()",
+    Test / avroGenerate := (Compile / avroGenerate).value
   )
+
 }
 
 object Hive {
