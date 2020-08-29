acc6646e7e83fa9e3f082b1aaa8c6227e3d8a7cf
Peter Toth <peter.toth@gmail.com>
2020-08-29 10:30:41 +0200

    add UT

--- sql/core/src/test/scala/org/apache/spark/sql/JoinSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/JoinSuite.scala
@@ -749,6 +749,15 @@ class JoinSuite extends QueryTest with SharedSparkSession with AdaptiveSparkPlan
         )
       }
 
+      // LEFT SEMI JOIN without bound condition does not use [[ExternalAppendOnlyUnsafeRowArray]]
+      // so should not cause any spill
+      assertNotSpilled(sparkContext, "left semi join") {
+        checkAnswer(
+          sql("SELECT * FROM testData LEFT SEMI JOIN testData2 ON key = a WHERE key = 2"),
+          Row(2, "2") :: Nil
+        )
+      }
+
       val expected = new ListBuffer[Row]()
       expected.append(
         Row(1, "1", 1, 1), Row(1, "1", 1, 2),
