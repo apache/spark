065f17386d1851d732b4c1badf1ce2e14d0de338
Udbhav30 <u.agrawal30@gmail.com>
2020-08-30 10:25:32 -0700

    [SPARK-32481][CORE][SQL] Support truncate table to move data to trash
    ### What changes were proposed in this pull request?
    Instead of deleting the data, we can move the data to trash.
    Based on the configuration provided by the user it will be deleted permanently from the trash.
    
    ### Why are the changes needed?
    Instead of directly deleting the data, we can provide flexibility to move data to the trash and then delete it permanently.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes, After truncate table the data is not permanently deleted now.
    It is first moved to the trash and then after the given time deleted permanently;
    
    ### How was this patch tested?
    new UTs added
    
    Closes #29552 from Udbhav30/truncate.
    
    Authored-by: Udbhav30 <u.agrawal30@gmail.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -2732,6 +2732,18 @@ object SQLConf {
       .booleanConf
       .createWithDefault(false)
 
+  val TRUNCATE_TRASH_ENABLED =
+    buildConf("spark.sql.truncate.trash.enabled")
+      .doc("This configuration decides when truncating table, whether data files will be moved " +
+        "to trash directory or deleted permanently. The trash retention time is controlled by " +
+        "'fs.trash.interval', and in default, the server side configuration value takes " +
+        "precedence over the client-side one. Note that if 'fs.trash.interval' is non-positive, " +
+        "this will be a no-op and log a warning message. If the data fails to be moved to "  +
+        "trash, Spark will turn to delete it permanently.")
+      .version("3.1.0")
+      .booleanConf
+      .createWithDefault(false)
+
   /**
    * Holds information about keys that have been deprecated.
    *
@@ -3349,6 +3361,8 @@ class SQLConf extends Serializable with Logging {
     getConf(SQLConf.OPTIMIZE_NULL_AWARE_ANTI_JOIN)
 
   def legacyPathOptionBehavior: Boolean = getConf(SQLConf.LEGACY_PATH_OPTION_BEHAVIOR)
+
+  def truncateTrashEnabled: Boolean = getConf(SQLConf.TRUNCATE_TRASH_ENABLED)
 
   /** ********************** SQLConf functionality methods ************ */
 
