f76f4a980e708a5bfd97fd6c0509f7efef337efd
zero323 <mszymkiewicz@gmail.com>
2020-08-31 18:50:03 +0200

    Ignore internal member imports from pyspark.sql.types

--- python/pyspark/sql/tests/test_types.py
+++ python/pyspark/sql/tests/test_types.py
@@ -30,8 +30,10 @@ from pyspark.sql.udf import UserDefinedFunction
 from pyspark.sql.types import ByteType, ShortType, IntegerType, FloatType, DateType, \
     TimestampType, MapType, StringType, StructType, StructField, ArrayType, DoubleType, LongType, \
     DecimalType, BinaryType, BooleanType, NullType
-from pyspark.sql.types import _array_signed_int_typecode_ctype_mappings, _array_type_mappings, \
+from pyspark.sql.types import (  # type: ignore
+    _array_signed_int_typecode_ctype_mappings, _array_type_mappings,
     _array_unsigned_int_typecode_ctype_mappings, _infer_type, _make_type_verifier, _merge_type
+)
 from pyspark.testing.sqlutils import ReusedSQLTestCase, ExamplePointUDT, PythonOnlyUDT, \
     ExamplePoint, PythonOnlyPoint, MyObject
 
