84df7359e7c253220d115756ab144740ee2915cd
yi.wu <yi.wu@databricks.com>
2020-09-03 17:36:50 +0800

    fix style

--- core/src/main/scala/org/apache/spark/scheduler/ExecutorLossReason.scala
+++ core/src/main/scala/org/apache/spark/scheduler/ExecutorLossReason.scala
@@ -53,7 +53,8 @@ private [spark] object LossReasonPending extends ExecutorLossReason("Pending los
 
 /**
  * @param _message human readable loss reason
- * @param workerHost it's defined when the host is confirmed lost too (i.e. including shuffle service)
+ * @param workerHost it's defined when the host is confirmed lost too (i.e. including
+ *                   shuffle service)
  * @param causedByApp whether the loss of the executor is the fault of the running app.
  *                    (assumed true by default unless known explicitly otherwise)
  */
