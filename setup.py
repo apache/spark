3f7831c73e006ee55dc950787669a3f1ea21f0e9
Kaxil Naik <kaxilnaik@gmail.com>
2020-09-04 14:37:40 +0100

    Relax requirement to allow latest version of flask-login (#9990)
    The current latest available version is 0.5.0 (https://pypi.org/project/Flask-Login/0.5.0/)
    
    Version 0.5.0 drops support for Python 2.6, 3.3, 3.4 and we don't use those versions in Airflow Master

--- setup.py
+++ setup.py
@@ -698,7 +698,7 @@ INSTALL_REQUIREMENTS = [
     'flask>=1.1.0, <2.0',
     'flask-appbuilder>2.3.4,~=3.0',
     'flask-caching>=1.3.3, <2.0.0',
-    'flask-login>=0.3, <0.5',
+    'flask-login>=0.3, <1.0',
     'flask-swagger==0.2.13',
     'flask-wtf>=0.14.2, <0.15',
     'funcsigs>=1.0.0, <2.0.0',
