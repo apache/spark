0c7616b9b5c5f2d69b0410b988bf5e3955e34a6c
Yuming Wang <yumwang@ebay.com>
2020-09-05 23:49:17 +0800

    [SPARK-32791][SQL] Non-partitioned table metric should not have dynamic partition pruning time
    ### What changes were proposed in this pull request?
    
    This pr make non-partitioned table metric should not have dynamic partition pruning time.
    
    ### Why are the changes needed?
    
    It is useless for non-partitioned table.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Manual test
    
    Before this pr:
    ![image](https://user-images.githubusercontent.com/5399861/92141803-87fed380-ee45-11ea-9784-09625b246fea.png)
    After this pr:
    ![image](https://user-images.githubusercontent.com/5399861/92141774-7c131180-ee45-11ea-8a9e-6775c592f496.png)
    
    Closes #29641 from wangyum/SPARK-32791.
    
    Authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Yuming Wang <yumwang@ebay.com>
    (cherry picked from commit 0b3bb45b893005d73afb15e4904ac6440c87811c)
    Signed-off-by: Yuming Wang <yumwang@ebay.com>

--- sql/core/src/test/scala/org/apache/spark/sql/DynamicPartitionPruningSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DynamicPartitionPruningSuite.scala
@@ -345,6 +345,7 @@ abstract class DynamicPartitionPruningSuiteBase
         val allFilesNum = scan1.metrics("numFiles").value
         val allFilesSize = scan1.metrics("filesSize").value
         assert(scan1.metrics("numPartitions").value === numPartitions)
+        assert(scan1.metrics("pruningTime").value === -1)
 
         // No dynamic partition pruning, so no static metrics
         // Only files from fid = 5 partition are scanned
@@ -358,6 +359,7 @@ abstract class DynamicPartitionPruningSuiteBase
         assert(0 < partFilesNum && partFilesNum < allFilesNum)
         assert(0 < partFilesSize && partFilesSize < allFilesSize)
         assert(scan2.metrics("numPartitions").value === 1)
+        assert(scan2.metrics("pruningTime").value === -1)
 
         // Dynamic partition pruning is used
         // Static metrics are as-if reading the whole fact table
@@ -370,6 +372,7 @@ abstract class DynamicPartitionPruningSuiteBase
         assert(scan3.metrics("numFiles").value == partFilesNum)
         assert(scan3.metrics("filesSize").value == partFilesSize)
         assert(scan3.metrics("numPartitions").value === 1)
+        assert(scan3.metrics("pruningTime").value !== -1)
       }
     }
   }
