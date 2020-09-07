de44e9cfa07e32d293d68355916ac0dbd31d5c54
Kent Yao <yaooqinn@hotmail.com>
2020-09-07 05:11:30 +0000

    [SPARK-32785][SQL] Interval with dangling parts should not results null
    ### What changes were proposed in this pull request?
    
    bugfix for incomplete interval values, e.g. interval '1', interval '1 day 2', currently these cases will result null, but actually we should fail them with IllegalArgumentsException
    
    ### Why are the changes needed?
    
    correctness
    
    ### Does this PR introduce _any_ user-facing change?
    
    yes, incomplete intervals will throw exception now
    
    #### before
    ```
    bin/spark-sql -S -e "select interval '1', interval '+', interval '1 day -'"
    
    NULL NULL NULL
    ```
    #### after
    
    ```
    -- !query
    select interval '1'
    -- !query schema
    struct<>
    -- !query output
    org.apache.spark.sql.catalyst.parser.ParseException
    
    Cannot parse the INTERVAL value: 1(line 1, pos 7)
    
    == SQL ==
    select interval '1'
    ```
    
    ### How was this patch tested?
    
    unit tests added
    
    Closes #29635 from yaooqinn/SPARK-32785.
    
    Authored-by: Kent Yao <yaooqinn@hotmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/IntervalUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/IntervalUtils.scala
@@ -736,7 +736,10 @@ object IntervalUtils {
     val result = state match {
       case UNIT_SUFFIX | UNIT_END | TRIM_BEFORE_SIGN =>
         new CalendarInterval(months, days, microseconds)
-      case _ => null
+      case TRIM_BEFORE_VALUE => throwIAE(s"expect a number after '$currentWord' but hit EOL")
+      case VALUE | VALUE_FRACTIONAL_PART =>
+        throwIAE(s"expect a unit name after '$currentWord' but hit EOL")
+      case _ => throwIAE(s"unknown error when parsing '$currentWord'")
     }
 
     result
