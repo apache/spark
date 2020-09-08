55d38a479b3d9d2607e1c564f07bf617fca8a6c2
Zhenhua Wang <wzh_zju@163.com>
2020-09-08 20:20:16 +0900

    [SPARK-32748][SQL] Revert "Support local property propagation in SubqueryBroadcastExec"
    ### What changes were proposed in this pull request?
    
    This reverts commit 04f7f6dac0b9177e11482cca4e7ebf7b7564e45f due to the discussion in [comment](https://github.com/apache/spark/pull/29589#discussion_r484657207).
    
    ### Why are the changes needed?
    
    Based on  the discussion in [comment](https://github.com/apache/spark/pull/29589#discussion_r484657207), propagation for thread local properties in `SubqueryBroadcastExec` is not necessary, since they will be propagated by broadcast exchange threads anyway.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Also revert the added test.
    
    Closes #29674 from wzhfy/revert_dpp_thread_local.
    
    Authored-by: Zhenhua Wang <wzh_zju@163.com>
    Signed-off-by: Takeshi Yamamuro <yamamuro@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SubqueryBroadcastExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SubqueryBroadcastExec.scala
@@ -17,9 +17,7 @@
 
 package org.apache.spark.sql.execution
 
-import java.util.concurrent.{Future => JFuture}
-
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.Duration
 
 import org.apache.spark.rdd.RDD
@@ -28,7 +26,6 @@ import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.QueryPlan
 import org.apache.spark.sql.execution.joins.{HashedRelation, HashJoin, LongHashedRelation}
 import org.apache.spark.sql.execution.metric.SQLMetrics
-import org.apache.spark.sql.internal.{SQLConf, StaticSQLConf}
 import org.apache.spark.util.ThreadUtils
 
 /**
@@ -63,12 +60,10 @@ case class SubqueryBroadcastExec(
   }
 
   @transient
-  private lazy val relationFuture: JFuture[Array[InternalRow]] = {
+  private lazy val relationFuture: Future[Array[InternalRow]] = {
     // relationFuture is used in "doExecute". Therefore we can get the execution id correctly here.
     val executionId = sparkContext.getLocalProperty(SQLExecution.EXECUTION_ID_KEY)
-    SQLExecution.withThreadLocalCaptured[Array[InternalRow]](
-      sqlContext.sparkSession,
-      SubqueryBroadcastExec.executionContext) {
+    Future {
       // This will run in another thread. Set the execution id so that we can connect these jobs
       // with the correct execution.
       SQLExecution.withExecutionId(sqlContext.sparkSession, executionId) {
@@ -94,7 +89,7 @@ case class SubqueryBroadcastExec(
 
         rows
       }
-    }
+    }(SubqueryBroadcastExec.executionContext)
   }
 
   protected override def doPrepare(): Unit = {
@@ -115,6 +110,5 @@ case class SubqueryBroadcastExec(
 
 object SubqueryBroadcastExec {
   private[execution] val executionContext = ExecutionContext.fromExecutorService(
-    ThreadUtils.newDaemonCachedThreadPool("dynamic-pruning",
-      SQLConf.get.getConf(StaticSQLConf.BROADCAST_EXCHANGE_MAX_THREAD_THRESHOLD)))
+    ThreadUtils.newDaemonCachedThreadPool("dynamicpruning", 16))
 }
