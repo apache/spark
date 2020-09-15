d43bb75367c7907dde48118d2757cfeb4df04d0b
Kaxil Naik <kaxilnaik@gmail.com>
2020-09-15 14:19:29 +0100

    Remove test dependency from TestApiKerberos (#10950)
    TestApiKerberos::test_trigger_dag previously was dependent that the `example_bash_operator` exist in the Database.
    
    If one of the other tests didn't write it to the DB or if one of the other tests cleared it from the DB, this test failed.

--- tests/www/api/experimental/test_kerberos_endpoints.py
+++ tests/www/api/experimental/test_kerberos_endpoints.py
@@ -26,20 +26,32 @@ from unittest import mock
 import pytest
 
 from airflow.api.auth.backend.kerberos_auth import CLIENT_AUTH
+from airflow.models import DagBag
 from airflow.www import app as application
 from tests.test_utils.config import conf_vars
+from tests.test_utils.db import clear_db_dags
 
 KRB5_KTNAME = os.environ.get("KRB5_KTNAME")
 
 
 @pytest.mark.integration("kerberos")
 class TestApiKerberos(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        dagbag = DagBag(include_examples=True)
+        for dag in dagbag.dags.values():
+            dag.sync_to_db()
+
     @conf_vars({
         ("api", "auth_backend"): "airflow.api.auth.backend.kerberos_auth",
         ("kerberos", "keytab"): KRB5_KTNAME,
     })
     def setUp(self):
         self.app = application.create_app(testing=True)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        clear_db_dags()
 
     def test_trigger_dag(self):
         with self.app.test_client() as client:
