apache/spark

commit c9f006b5409b77085cd140e0954501352feb096f

added environment configuration for using --start-airflow (#10971)

scripts/in_container/check_environment.sh
@@ -100,6 +100,8 @@ function resetdb_if_requested() {
 function startairflow_if_requested() {
     if [[ ${START_AIRFLOW:="false"} == "true" ]]; then
 
+	. "$( dirname "${BASH_SOURCE[0]}" )/configure_environment.sh"
+
         # initialize db and create the admin user if it's a new run
         airflow db init
         airflow users create -u admin -p admin -f Thor -l Adminstra -r Admin -e dummy@dummy.email

commit 1ed621ea869c6e02023e299afa50c875d049ddaf

Fix static error (tabs) introduced in #10971 (#10973)

scripts/in_container/check_environment.sh
@@ -100,7 +100,7 @@ function resetdb_if_requested() {
 function startairflow_if_requested() {
     if [[ ${START_AIRFLOW:="false"} == "true" ]]; then
 
-	. "$( dirname "${BASH_SOURCE[0]}" )/configure_environment.sh"
+        . "$( dirname "${BASH_SOURCE[0]}" )/configure_environment.sh"
 
         # initialize db and create the admin user if it's a new run
         airflow db init