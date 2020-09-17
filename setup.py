f934e2f3ef5c5ebf50a14ec4cae12d313fbea3b0
Kaxil Naik <kaxilnaik@gmail.com>
2020-09-17 15:20:31 +0100

    Unpin 'tornado' dep pulled in by flower (#10993)
    'tornado' version was pinned in https://github.com/apache/airflow/pull/4815
    
    The underlying issue is fixed for Py 3.5.2 so that is not a problem. Also since Airflow Master is already Py 3.6+ this does not apply to us.

--- setup.py
+++ setup.py
@@ -202,7 +202,6 @@ cassandra = [
 celery = [
     'celery~=4.4.2',
     'flower>=0.7.3, <1.0',
-    'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
     'vine~=1.3',  # https://stackoverflow.com/questions/32757259/celery-no-module-named-five
 ]
 cgroups = [
