a82fb7f6e2d936f30a0d30d97a480be24d2c07d4
Liang-Chi Hsieh <viirya@gmail.com>
2020-09-20 22:52:57 -0700

    Add json optimization rule.

---
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/JsonSuite.scala
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.optimizer
+
+import org.apache.spark.sql.catalyst.dsl.expressions._
+import org.apache.spark.sql.catalyst.dsl.plans._
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.catalyst.plans.PlanTest
+import org.apache.spark.sql.catalyst.plans.logical.{LocalRelation, LogicalPlan}
+import org.apache.spark.sql.catalyst.rules.RuleExecutor
+import org.apache.spark.sql.types._
+
+class JsonSuite extends PlanTest with ExpressionEvalHelper {
+
+  object Optimizer extends RuleExecutor[LogicalPlan] {
+    val batches = Batch("Json optimization", FixedPoint(10), OptimizeJsonExprs) :: Nil
+  }
+
+  val schema = StructType.fromDDL("a int, b int")
+
+  private val jsonAtt = ('json).string.notNull
+  private val structAtt = ('struct).struct(schema).notNull
+
+  private val testRelation = LocalRelation(jsonAtt, structAtt)
+
+  test("SPARK-32948: optimize to_json + from_json") {
+    val options = Map.empty[String, String]
+
+    val query = testRelation
+      .select(StructsToJson(options, JsonToStructs(schema, options, 'json)).as("json"))
+    val optimized = Optimizer.execute(query.analyze)
+
+    val expected = testRelation.select('json.as("json")).analyze
+    comparePlans(optimized, expected)
+  }
+
+  test("SPARK-32948: optimize from_json + to_json") {
+    val options = Map.empty[String, String]
+    val schema = StructType.fromDDL("a int, b int")
+
+    val query = testRelation
+      .select(JsonToStructs(schema, options, StructsToJson(options, 'struct)).as("struct"))
+    val optimized = Optimizer.execute(query.analyze)
+
+    val expected = testRelation.select('struct.as("struct")).analyze
+    comparePlans(optimized, expected)
+  }
+
+}
