bf32ac8efa9818be551fe720a71eaba50d5d41ad
nssalian <neelesh.salian@stitchfix.com>
2020-09-25 12:05:40 -0700

    [SPARK-27872][K8S][2.4] Fix executor service account inconsistency
    ### What changes were proposed in this pull request?
    
    Similar patch to https://github.com/apache/spark/pull/24748 but applied to the branch-2.4.
    Backporting the fix to releases 2.4.x.
    Please let me know if I missed some step; I haven't contributed to spark in a long time.
    
    Closes #29844 from nssalian/patch-SPARK-27872.
    
    Authored-by: nssalian <neelesh.salian@stitchfix.com>
    Signed-off-by: Erik Erlandson <eerlands@redhat.com>

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
@@ -264,6 +264,10 @@ private[spark] class KubernetesSuite extends SparkFunSuite
       === baseMemory)
   }
 
+  protected def doExecutorServiceAccountCheck(executorPod: Pod, account: String): Unit = {
+    doBasicExecutorPodCheck(executorPod)
+    assert(executorPod.getSpec.getServiceAccount == kubernetesTestComponents.serviceAccountName)
+  }
 
   protected def doBasicDriverPyPodCheck(driverPod: Pod): Unit = {
     assert(driverPod.getMetadata.getName === driverPodName)
