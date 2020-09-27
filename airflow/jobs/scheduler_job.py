e2dc706b08209be9c078918bb4df465724e27335
Tomek Urbaszek <turbaszek@gmail.com>
2020-09-28 00:24:58 +0200

    Make kill log in DagFileProcessorProcess more informative (#11124)

--- airflow/jobs/scheduler_job.py
+++ airflow/jobs/scheduler_job.py
@@ -243,7 +243,7 @@ class DagFileProcessorProcess(AbstractDagFileProcessorProcess, LoggingMixin, Mul
             raise AirflowException("Tried to kill process before starting!")
 
         if self._process.is_alive() and self._process.pid:
-            self.log.warning("Killing PID %s", self._process.pid)
+            self.log.warning("Killing DAGFileProcessorProcess (PID=%d)", self._process.pid)
             os.kill(self._process.pid, signal.SIGKILL)
 
     @property
