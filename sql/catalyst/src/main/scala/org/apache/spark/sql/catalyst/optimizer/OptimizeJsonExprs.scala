43a4620f676a232787eadbe7b8d1679d8fab7d10
Liang-Chi Hsieh <viirya@gmail.com>
2020-09-28 18:54:22 -0700

    Update sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/OptimizeJsonExprs.scala
    Co-authored-by: Hyukjin Kwon <gurwls223@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/OptimizeJsonExprs.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/OptimizeJsonExprs.scala
@@ -27,10 +27,10 @@ import org.apache.spark.sql.catalyst.rules.Rule
 object OptimizeJsonExprs extends Rule[LogicalPlan] {
   override def apply(plan: LogicalPlan): LogicalPlan = plan transform {
     case p => p.transformExpressions {
-      case JsonToStructs(schema, options1,
+      case jsonToStructs @ JsonToStructs(schema, options1,
         StructsToJson(options2, child, timeZoneId2), timeZoneId1)
           if options1.isEmpty && options2.isEmpty && timeZoneId1 == timeZoneId2 &&
-            schema == child.dataType =>
+            jsonToStructs.dataType == child.dataType =>
         child
     }
   }
