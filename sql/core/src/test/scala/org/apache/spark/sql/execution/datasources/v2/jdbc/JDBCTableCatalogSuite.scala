7a32df6fa28eda99aae2af71dcc591531662cabe
Max Gekk <max.gekk@gmail.com>
2020-10-05 12:30:55 +0300

    Update column type in not existing table and namespace

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/v2/jdbc/JDBCTableCatalogSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/v2/jdbc/JDBCTableCatalogSuite.scala
@@ -226,6 +226,12 @@ class JDBCTableCatalogSuite extends QueryTest with SharedSparkSession {
       val expectedSchema = new StructType().add("ID", DoubleType)
       assert(t.schema === expectedSchema)
     }
+    // Update column type in not existing table and namespace
+    Seq("h2.test.not_existing_table", "h2.bad_test.not_existing_table").foreach { table =>
+      intercept[AnalysisException] {
+        sql(s"ALTER TABLE $table ALTER COLUMN id TYPE DOUBLE")
+      }
+    }
   }
 
   test("alter table ... update column nullability") {
