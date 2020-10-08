f5b7bbcb929d6b7cec6b2b3868fbff2db60797de
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-10-08 21:08:11 +0200

    Better diagnostics when there are problems with Kerberos (#11353)

--- scripts/in_container/_in_container_utils.sh
+++ scripts/in_container/_in_container_utils.sh
@@ -261,6 +261,9 @@ function setup_kerberos() {
     RES_3=$?
 
     if [[ ${RES_1} != 0 || ${RES_2} != 0 || ${RES_3} != 0 ]]; then
+        echo
+        echo "Error when setting up Kerberos: ${RES_1} ${RES_2} ${RES_3}}!"
+        echo
         exit 1
     else
         echo
