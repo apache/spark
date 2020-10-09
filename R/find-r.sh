0bf7cd3b31c9783008a2a697479ca2002f6afb2e
AparnaRoy23 <45351282+AparnaRoy23@users.noreply.github.com>
2020-10-09 22:46:06 +0530

    Update find-r.sh

--- R/find-r.sh
+++ R/find-r.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#
+#doing this for tshirt
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
