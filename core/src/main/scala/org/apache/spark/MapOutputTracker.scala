836d43ff1ad2e1683db1c55b58b304ebb9c5db5e
Bo Yang <boy@uber.com>
2020-10-10 22:51:14 -0700

    Update import with new lines

--- core/src/main/scala/org/apache/spark/MapOutputTracker.scala
+++ core/src/main/scala/org/apache/spark/MapOutputTracker.scala
@@ -27,7 +27,9 @@ import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration.Duration
 import scala.reflect.ClassTag
 import scala.util.control.NonFatal
+
 import org.apache.commons.io.output.{ByteArrayOutputStream => ApacheByteArrayOutputStream}
+
 import org.apache.spark.broadcast.{Broadcast, BroadcastManager}
 import org.apache.spark.internal.Logging
 import org.apache.spark.internal.config._
