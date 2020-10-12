8f9f2cda8b3cc6a0b3bfb357417b0690be34f0e0
Denis Pyshev <git@gemelen.net>
2020-10-12 19:34:13 +0300

    Add arguments to exception's message

--- sql/catalyst/src/test/scala/org/apache/spark/sql/connector/InMemoryTable.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/connector/InMemoryTable.scala
@@ -107,7 +107,8 @@ class InMemoryTable(
           case (micros: Long, TimestampType) =>
             val localDate = DateTimeUtils.microsToInstant(micros).atZone(UTC).toLocalDate
             ChronoUnit.YEARS.between(EPOCH_LOCAL_DATE, localDate)
-          case _ => throw new IllegalArgumentException("Match: unsupported argument(s) type")
+          case (v, t) =>
+            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
         }
       case MonthsTransform(ref) =>
         extractor(ref.fieldNames, schema, row) match {
@@ -116,7 +117,8 @@ class InMemoryTable(
           case (micros: Long, TimestampType) =>
             val localDate = DateTimeUtils.microsToInstant(micros).atZone(UTC).toLocalDate
             ChronoUnit.MONTHS.between(EPOCH_LOCAL_DATE, localDate)
-          case _ => throw new IllegalArgumentException("Match: unsupported argument(s) type")
+          case (v, t) =>
+            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
         }
       case DaysTransform(ref) =>
         extractor(ref.fieldNames, schema, row) match {
@@ -124,13 +126,15 @@ class InMemoryTable(
             days
           case (micros: Long, TimestampType) =>
             ChronoUnit.DAYS.between(Instant.EPOCH, DateTimeUtils.microsToInstant(micros))
-          case _ => throw new IllegalArgumentException("Match: unsupported argument(s) type")
+          case (v, t) =>
+            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
         }
       case HoursTransform(ref) =>
         extractor(ref.fieldNames, schema, row) match {
           case (micros: Long, TimestampType) =>
             ChronoUnit.HOURS.between(Instant.EPOCH, DateTimeUtils.microsToInstant(micros))
-          case _ => throw new IllegalArgumentException("Match: unsupported argument(s) type")
+          case (v, t) =>
+            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
         }
       case BucketTransform(numBuckets, ref) =>
         (extractor(ref.fieldNames, schema, row).hashCode() & Integer.MAX_VALUE) % numBuckets
