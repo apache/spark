89d329c11baa61a81a4361d3e870e1e2c0a67203
Jungtaek Lim (HeartSaVioR) <kabhwan.opensource@gmail.com>
2020-10-14 07:14:06 +0900

    [SPARK-33136][SQL] Fix mistakenly swapped parameter in V2WriteCommand.outputResolved

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/v2Commands.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/v2Commands.scala
@@ -45,7 +45,7 @@ trait V2WriteCommand extends Command {
           case (inAttr, outAttr) =>
             // names and types must match, nullability must be compatible
             inAttr.name == outAttr.name &&
-              DataType.equalsIgnoreCompatibleNullability(outAttr.dataType, inAttr.dataType) &&
+              DataType.equalsIgnoreCompatibleNullability(inAttr.dataType, outAttr.dataType) &&
               (outAttr.nullable || !inAttr.nullable)
         }
     }
