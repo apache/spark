a9582e11f3ac5fba1dd02a55b3bdd7aa5ef25e34
Denis Pyshev <git@gemelen.net>
2020-10-13 18:03:05 +0300

    Add missing string interpolation to messages

--- sql/catalyst/src/test/scala/org/apache/spark/sql/connector/InMemoryTable.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/connector/InMemoryTable.scala
@@ -108,7 +108,7 @@ class InMemoryTable(
             val localDate = DateTimeUtils.microsToInstant(micros).atZone(UTC).toLocalDate
             ChronoUnit.YEARS.between(EPOCH_LOCAL_DATE, localDate)
           case (v, t) =>
-            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
+            throw new IllegalArgumentException(s"Match: unsupported argument(s) type - ($v, $t)")
         }
       case MonthsTransform(ref) =>
         extractor(ref.fieldNames, schema, row) match {
@@ -118,7 +118,7 @@ class InMemoryTable(
             val localDate = DateTimeUtils.microsToInstant(micros).atZone(UTC).toLocalDate
             ChronoUnit.MONTHS.between(EPOCH_LOCAL_DATE, localDate)
           case (v, t) =>
-            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
+            throw new IllegalArgumentException(s"Match: unsupported argument(s) type - ($v, $t)")
         }
       case DaysTransform(ref) =>
         extractor(ref.fieldNames, schema, row) match {
@@ -127,14 +127,14 @@ class InMemoryTable(
           case (micros: Long, TimestampType) =>
             ChronoUnit.DAYS.between(Instant.EPOCH, DateTimeUtils.microsToInstant(micros))
           case (v, t) =>
-            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
+            throw new IllegalArgumentException(s"Match: unsupported argument(s) type - ($v, $t)")
         }
       case HoursTransform(ref) =>
         extractor(ref.fieldNames, schema, row) match {
           case (micros: Long, TimestampType) =>
             ChronoUnit.HOURS.between(Instant.EPOCH, DateTimeUtils.microsToInstant(micros))
           case (v, t) =>
-            throw new IllegalArgumentException("Match: unsupported argument(s) type - ($v, $t)")
+            throw new IllegalArgumentException(s"Match: unsupported argument(s) type - ($v, $t)")
         }
       case BucketTransform(numBuckets, ref) =>
         (extractor(ref.fieldNames, schema, row).hashCode() & Integer.MAX_VALUE) % numBuckets
