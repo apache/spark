3a0af1011a1bf18d6445fd6426b047cffd865d72
Jungtaek Lim (HeartSaVioR) <kabhwan.opensource@gmail.com>
2020-10-15 15:28:52 +0900

    Revert "Revert "[SPARK-33146][CORE] Check for non-fatal errors when loading new applications in SHS""
    This reverts commit e40c147a5d194adbba13f12590959dc68347ec14.

--- core/src/main/scala/org/apache/spark/deploy/history/FsHistoryProvider.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/FsHistoryProvider.scala
@@ -527,6 +527,9 @@ private[history] class FsHistoryProvider(conf: SparkConf, clock: Clock)
                 reader.fileSizeForLastIndex > 0
               } catch {
                 case _: FileNotFoundException => false
+                case NonFatal(e) =>
+                  logWarning(s"Error while reading new log ${reader.rootPath}", e)
+                  false
               }
 
             case NonFatal(e) =>
