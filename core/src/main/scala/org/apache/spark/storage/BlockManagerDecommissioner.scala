d39e7b2cde6307454b7798380132265010f31807
Holden Karau <hkarau@apple.com>
2020-10-15 11:13:59 -0700

    code review feedback. PY3 is the default now. Also explain why loadblocks might fail.

--- core/src/main/scala/org/apache/spark/storage/BlockManagerDecommissioner.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManagerDecommissioner.scala
@@ -105,7 +105,10 @@ private[storage] class BlockManagerDecommissioner(
                 } catch {
                   case e: IOException =>
                     // If a block got deleted before netty opened the file handle, then trying to
-                    // load the blocks now will fail.
+                    // load the blocks now will fail. This is most likely to occur if we start
+                    // migrating blocks and then the shuffle TTL cleaner kicks in. However this
+                    // could also happen with manually managed shuffles or a GC event on the driver
+                    // a no longer referenced RDD with shuffle files.
                     if (bm.migratableResolver.getMigrationBlocks(shuffleBlockInfo).isEmpty) {
                       logWarning(s"Skipping block ${shuffleBlockInfo}, block deleted.")
                     } else {
