7aee3694ec1b1ccb619164714da37b32237a8214
turbofei <fwang12@ebay.com>
2020-10-15 20:51:46 +0800

    retest please

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/CliSuite.scala
@@ -583,6 +583,5 @@ class CliSuite extends SparkFunSuite with BeforeAndAfterAll with Logging {
     runCliWithin(1.minute)("/*$meta chars{^\\;}*/ SELECT 'test';" -> "test")
     runCliWithin(1.minute)("/*\nmulti-line\n*/ SELECT 'test';" -> "test")
     runCliWithin(1.minute)("/*/* multi-level bracketed*/*/ SELECT 'test';" -> "test")
-
   }
 }
