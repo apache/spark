e73acc50bf1ad5af4b591d7f8594d71ab5b7fac5
Liang-Chi Hsieh <viirya@gmail.com>
2020-10-26 12:37:23 -0700

    Revert "[SPARK-33228][SQL] Don't uncache data when replacing a view having the same logical plan
    This reverts commit a85d69049c5ad994b3fb6cde1b3d739f3da336d4.
    
    Closes #30152 from viirya/revert-33228.
    
    Authored-by: Liang-Chi Hsieh <viirya@gmail.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/views.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/views.scala
@@ -143,19 +143,17 @@ case class CreateViewCommand(
 
     val catalog = sparkSession.sessionState.catalog
     if (viewType == LocalTempView) {
-      if (replace && catalog.getTempView(name.table).isDefined &&
-          !catalog.getTempView(name.table).get.sameResult(child)) {
-        logInfo(s"Try to uncache ${name.quotedString} before replacing.")
+      if (replace && catalog.getTempView(name.table).isDefined) {
+        logDebug(s"Try to uncache ${name.quotedString} before replacing.")
         CommandUtils.uncacheTableOrView(sparkSession, name.quotedString)
       }
       val aliasedPlan = aliasPlan(sparkSession, analyzedPlan)
       catalog.createTempView(name.table, aliasedPlan, overrideIfExists = replace)
     } else if (viewType == GlobalTempView) {
-      if (replace && catalog.getGlobalTempView(name.table).isDefined &&
-          !catalog.getGlobalTempView(name.table).get.sameResult(child)) {
+      if (replace && catalog.getGlobalTempView(name.table).isDefined) {
         val db = sparkSession.sessionState.conf.getConf(StaticSQLConf.GLOBAL_TEMP_DATABASE)
         val globalTempView = TableIdentifier(name.table, Option(db))
-        logInfo(s"Try to uncache ${globalTempView.quotedString} before replacing.")
+        logDebug(s"Try to uncache ${globalTempView.quotedString} before replacing.")
         CommandUtils.uncacheTableOrView(sparkSession, globalTempView.quotedString)
       }
       val aliasedPlan = aliasPlan(sparkSession, analyzedPlan)
