b104516e5fe5e9168a7df3c0ab58d0ed3dae75d7
Ash Berlin-Taylor <ash_github@firemirror.com>
2020-10-29 23:22:34 +0000

    Update flask-caching dep to version that works with Werkzeug 1 (#11955)
    Werkzeug 1.0 removed the werkzeug.contrib.caching module, so we need at
    least 1.5.0 of flask-caching to deal with this -- see
    
    https://github.com/sh4nks/flask-caching/blob/v1.9.0/CHANGES#L97-L107

--- setup.py
+++ setup.py
@@ -700,7 +700,7 @@ INSTALL_REQUIREMENTS = [
     'dill>=0.2.2, <0.4',
     'flask>=1.1.0, <2.0',
     'flask-appbuilder~=3.1.1',
-    'flask-caching>=1.3.3, <2.0.0',
+    'flask-caching>=1.5.0, <2.0.0',
     'flask-login>=0.3, <0.5',
     'flask-swagger==0.2.13',
     'flask-wtf>=0.14.3, <0.15',
