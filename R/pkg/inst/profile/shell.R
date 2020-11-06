762b86e40892ea5ecbd8368f6d59fdd41f211d6f
neko <echohlne@gmail.com>
2020-11-06 18:20:00 +0800

    Update shell.R

--- R/pkg/inst/profile/shell.R
+++ R/pkg/inst/profile/shell.R
@@ -43,8 +43,8 @@
   cat("      /_/", "\n")
   cat("\n")
 
-  cat("\nSpark context Web UI available at", SparkR::sparkR.uiWebUrl(), "\n")
+  cat("\nSpark context Web UI available at", SparkR::sparkR.uiWebUrl())
   cat("\nSpark context available as 'sc' (master = ", unlist(SparkR::sparkR.conf("spark.master")),
-    ", app id = ",unlist(SparkR::sparkR.conf("spark.app.id")), ").\n", sep="")
+    ", app id = ",unlist(SparkR::sparkR.conf("spark.app.id")), ").", sep="")
   cat("\nSparkSession available as 'spark'.\n")
 }
