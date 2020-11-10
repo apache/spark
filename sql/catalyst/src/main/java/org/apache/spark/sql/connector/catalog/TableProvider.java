a1f84d8714cd1bd6cc6e2da6eb97fb9f58f3ee8f
Gengliang Wang <gengliang.wang@databricks.com>
2020-11-10 04:43:32 +0000

    [SPARK-33369][SQL] DSV2: Skip schema inference in write if table provider supports external metadata
    ### What changes were proposed in this pull request?
    
    When TableProvider.supportsExternalMetadata() is true, Spark will use the input Dataframe's schema in `DataframeWriter.save()`/`DataStreamWriter.start()` and skip schema/partitioning inference.
    
    ### Why are the changes needed?
    
    For all the v2 data sources which are not FileDataSourceV2, Spark always infers the table schema/partitioning on `DataframeWriter.save()`/`DataStreamWriter.start()`.
    The inference of table schema/partitioning can be expensive. However, there is no such trait or flag for indicating a V2 source can use the input DataFrame's schema on `DataframeWriter.save()`/`DataStreamWriter.start()`. We can resolve the problem by adding a new expected behavior for the method `TableProvider.supportsExternalMetadata()`.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, a new behavior for the data source v2 API `TableProvider.supportsExternalMetadata()` when it returns true.
    
    ### How was this patch tested?
    
    Unit test
    
    Closes #30273 from gengliangwang/supportsExternalMetadata.
    
    Authored-by: Gengliang Wang <gengliang.wang@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/java/org/apache/spark/sql/connector/catalog/TableProvider.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/connector/catalog/TableProvider.java
@@ -77,8 +77,11 @@ public interface TableProvider {
 
   /**
    * Returns true if the source has the ability of accepting external table metadata when getting
-   * tables. The external table metadata includes user-specified schema from
-   * `DataFrameReader`/`DataStreamReader` and schema/partitioning stored in Spark catalog.
+   * tables. The external table metadata includes:
+   *   1. For table reader: user-specified schema from `DataFrameReader`/`DataStreamReader` and
+   *      schema/partitioning stored in Spark catalog.
+   *   2. For table writer: the schema of the input `Dataframe` of
+   *      `DataframeWriter`/`DataStreamWriter`.
    * <p>
    * By default this method returns false, which means the schema and partitioning passed to
    * `getTable` are from the infer methods. Please override it if this source has expensive
