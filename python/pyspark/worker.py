4a1c143f1a042a9a23d00929670eadbdb1afca11
lrz <lrz@lrzdeMacBook-Pro.local>
2020-11-10 19:39:18 +0900

    [SPARK-33339][PYTHON] Pyspark application will hang due to non Exception error
    ### What changes were proposed in this pull request?
    
    When a system.exit exception occurs during the process, the python worker exits abnormally, and then the executor task is still waiting for the worker for reading from socket, causing it to hang.
    The system.exit exception may be caused by the user's error code, but spark should at least throw an error to remind the user, not get stuck
    we can run a simple test to reproduce this case:
    
    ```
    from pyspark.sql import SparkSession
    def err(line):
      raise SystemExit
    spark = SparkSession.builder.appName("test").getOrCreate()
    spark.sparkContext.parallelize(range(1,2), 2).map(err).collect()
    spark.stop()
    ```
    
    ### Why are the changes needed?
    
    to make sure pyspark application won't hang if there's non-Exception error in python worker
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    added a new test and also manually tested the case above
    
    Closes #30248 from li36909/pyspark.
    
    Lead-authored-by: lrz <lrz@lrzdeMacBook-Pro.local>
    Co-authored-by: Hyukjin Kwon <gurwls223@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit 27bb40b6297361985e3590687f0332a72b71bc85)
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/worker.py
+++ python/pyspark/worker.py
@@ -608,7 +608,7 @@ def main(infile, outfile):
         # reuse.
         TaskContext._setTaskContext(None)
         BarrierTaskContext._setTaskContext(None)
-    except Exception:
+    except BaseException:
         try:
             exc_info = traceback.format_exc()
             if isinstance(exc_info, bytes):
@@ -622,7 +622,7 @@ def main(infile, outfile):
         except IOError:
             # JVM close the socket
             pass
-        except Exception:
+        except BaseException:
             # Write the error to stderr if it happened while serializing
             print("PySpark worker failed with exception:", file=sys.stderr)
             print(traceback.format_exc(), file=sys.stderr)
