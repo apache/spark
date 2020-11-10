1521965bef003d9dc868345111ac602c6977e1c5
Ash Berlin-Taylor <ash_github@firemirror.com>
2020-11-10 12:48:54 +0000

    Release 2.0.0b2 (#12243)

--- airflow/version.py
+++ airflow/version.py
@@ -17,4 +17,4 @@
 # under the License.
 #
 
-version = '2.0.0b1'
+version = '2.0.0b2'
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ from setuptools import Command, find_namespace_packages, find_packages, setup
 logger = logging.getLogger(__name__)
 
 # This is automatically maintained in sync via pre-commit from airflow/version.py
-version = '2.0.0b1'
+version = '2.0.0b2'
 
 PY3 = sys.version_info[0] == 3
 
