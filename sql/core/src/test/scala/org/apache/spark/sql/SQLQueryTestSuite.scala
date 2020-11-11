f851a4ceae8eaca7a1460f48f0266b3fd42519af
gengjiaan <gengjiaan@360.cn>
2020-11-11 19:26:41 +0800

    Add test case.

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestSuite.scala
@@ -563,7 +563,8 @@ class SQLQueryTestSuite extends QueryTest with SharedSparkSession with SQLHelper
     // Filter out test files with invalid extensions such as temp files created
     // by vi (.swp), Mac (.DS_Store) etc.
     val filteredFiles = files.filter(_.getName.endsWith(validFileExtensions))
-    filteredFiles ++ dirs.flatMap(listFilesRecursively)
+    (filteredFiles ++ dirs.flatMap(listFilesRecursively))
+      .filter(_.getName.equals("window.sql"))
   }
 
   /** Load built-in test tables into the SparkSession. */
