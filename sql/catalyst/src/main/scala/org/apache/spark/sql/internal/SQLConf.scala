8e2a0bdce706128891ebc4c00345a25d7dd41371
Bryan Cutler <cutlerb@gmail.com>
2020-11-18 21:18:19 +0900

    [SPARK-24554][PYTHON][SQL] Add MapType support for PySpark with Arrow
    ### What changes were proposed in this pull request?
    
    This change adds MapType support for PySpark with Arrow, if using pyarrow >= 2.0.0.
    
    ### Why are the changes needed?
    
    MapType was previous unsupported with Arrow.
    
    ### Does this PR introduce _any_ user-facing change?
    
    User can now enable MapType for `createDataFrame()`, `toPandas()` with Arrow optimization, and with Pandas UDFs.
    
    ### How was this patch tested?
    
    Added new PySpark tests for createDataFrame(), toPandas() and Scalar Pandas UDFs.
    
    Closes #30393 from BryanCutler/arrow-add-MapType-SPARK-24554.
    
    Authored-by: Bryan Cutler <cutlerb@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1903,7 +1903,7 @@ object SQLConf {
         "1. pyspark.sql.DataFrame.toPandas " +
         "2. pyspark.sql.SparkSession.createDataFrame when its input is a Pandas DataFrame " +
         "The following data types are unsupported: " +
-        "MapType, ArrayType of TimestampType, and nested StructType.")
+        "ArrayType of TimestampType, and nested StructType.")
       .version("3.0.0")
       .fallbackConf(ARROW_EXECUTION_ENABLED)
 
