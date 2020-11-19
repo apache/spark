13128f44ec46941cd64cd0ffe8316ea20c106e9e
John Bampton <jbampton@users.noreply.github.com>
2020-11-20 02:29:57 +1000

    Fix Python docstring parameter (#12483)

--- airflow/utils/sqlalchemy.py
+++ airflow/utils/sqlalchemy.py
@@ -193,7 +193,7 @@ def with_row_locks(query, **kwargs):
     Apply with_for_update to an SQLAlchemy query, if row level locking is in use.
 
     :param query: An SQLAlchemy Query object
-    :param **kwargs: Extra kwargs to pass to with_for_update (of, nowait, skip_locked, etc)
+    :param kwargs: Extra kwargs to pass to with_for_update (of, nowait, skip_locked, etc)
     :return: updated query
     """
     if USE_ROW_LEVEL_LOCKING:
