cd4a184f067a0bd262049c05e842d0583ff635c1
neko <echohlne@gmail.com>
2020-11-21 23:17:10 +0800

    Update EventLoggingListenerSuite.scala

--- core/src/test/scala/org/apache/spark/scheduler/EventLoggingListenerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/EventLoggingListenerSuite.scala
@@ -101,17 +101,17 @@ class EventLoggingListenerSuite extends SparkFunSuite with LocalSparkContext wit
     val (secretKey, secretPassword) = ("spark.executorEnv.HADOOP_CREDSTORE_PASSWORD",
       "secret_password")
     val (customKey, customValue) = ("parse_token", "secret_password")
-    
+
     val conf = getLoggingConf(testDirPath, None).set(secretKey, secretPassword)
-    
+
     val properties = new Properties()
     properties.setProperty(secretKey, secretPassword)
     properties.setProperty(customKey, customValue)
-    
+
     val logName = "properties-reaction-test"
     val eventLogger = new EventLoggingListener(logName, None, testDirPath.toUri(), conf)
     val listenerBus = new LiveListenerBus(conf)
-    
+
     val stageId = 1
     val jobId = 1
     val stageInfo = new StageInfo(stageId, 0, stageId.toString, 0,
@@ -127,7 +127,7 @@ class EventLoggingListenerSuite extends SparkFunSuite with LocalSparkContext wit
     events.foreach(event => listenerBus.post(event))
     listenerBus.stop()
     eventLogger.stop()
-    
+
     val logData = EventLogFileReader.openEventLog(new Path(eventLogger.logWriter.logPath),
       fileSystem)
     try {
@@ -135,17 +135,17 @@ class EventLoggingListenerSuite extends SparkFunSuite with LocalSparkContext wit
       assert(lines.size === 2)
       assert(lines(0).contains("SparkListenerStageSubmitted"))
       assert(lines(1).contains("SparkListenerJobStart"))
-      
+
       lines.foreach{
         line => JsonProtocol.sparkEventFromJson(parse(line)) match {
           case stageSubmittedEvent: SparkListenerStageSubmitted =>
             assert(stageSubmittedEvent.properties.getProperty(secretKey) == "*********(redacted)")
             assert(stageSubmittedEvent.properties.getProperty(customKey) ==  customValue)
-            
+
           case jobStartEvent : SparkListenerJobStart =>
             assert(jobStartEvent.properties.getProperty(secretKey) == "*********(redacted)")
             assert(jobStartEvent.properties.getProperty(customKey) ==  customValue)
-            
+
           case _ => assert(false)
         }
       }
