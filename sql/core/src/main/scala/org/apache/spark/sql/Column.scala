a617f9430064f897a85e6373702b5c45bb7250b6
yi.wu <yi.wu@databricks.com>
2020-11-24 22:20:24 +0800

    fix

--- sql/core/src/main/scala/org/apache/spark/sql/Column.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Column.scala
@@ -1164,7 +1164,7 @@ class Column(val expr: Expression) extends Logging {
    * @since 2.0.0
    */
   def name(alias: String): Column = withExpr {
-    Alias(normalizedExpr(), alias)()
+    Alias(expr, alias)()
   }
 
   /**
