01321bc0fec54a1610d0873c17fa7354137d3a6b
zero323 <mszymkiewicz@gmail.com>
2020-11-25 10:24:41 +0900

    [SPARK-33252][PYTHON][DOCS] Migration to NumPy documentation style in MLlib (pyspark.mllib.*)
    ### What changes were proposed in this pull request?
    
    This PR proposes migration of `pyspark.mllib` to NumPy documentation style.
    
    ### Why are the changes needed?
    
    To improve documentation style.
    
    Before:
    
    ![old](https://user-images.githubusercontent.com/1554276/100097941-90234980-2e5d-11eb-8b4d-c25d98d85191.png)
    
    After:
    
    ![new](https://user-images.githubusercontent.com/1554276/100097966-987b8480-2e5d-11eb-9e02-07b18c327624.png)
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, this changes both rendered HTML docs and console representation (SPARK-33243).
    
    ### How was this patch tested?
    
    `dev/lint-python` and manual inspection.
    
    Closes #30413 from zero323/SPARK-33252.
    
    Authored-by: zero323 <mszymkiewicz@gmail.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- python/pyspark/mllib/recommendation.py
+++ python/pyspark/mllib/recommendation.py
@@ -32,13 +32,15 @@ class Rating(namedtuple("Rating", ["user", "product", "rating"])):
     """
     Represents a (user, product, rating) tuple.
 
+    .. versionadded:: 1.2.0
+
+    Examples
+    --------
     >>> r = Rating(1, 2, 5.0)
     >>> (r.user, r.product, r.rating)
     (1, 2, 5.0)
     >>> (r[0], r[1], r[2])
     (1, 2, 5.0)
-
-    .. versionadded:: 1.2.0
     """
 
     def __reduce__(self):
@@ -51,6 +53,10 @@ class MatrixFactorizationModel(JavaModelWrapper, JavaSaveable, JavaLoader):
     """A matrix factorisation model trained by regularized alternating
     least-squares.
 
+    .. versionadded:: 0.9.0
+
+    Examples
+    --------
     >>> r1 = (1, 1, 1.0)
     >>> r2 = (1, 2, 2.0)
     >>> r3 = (2, 1, 2.0)
@@ -126,8 +132,6 @@ class MatrixFactorizationModel(JavaModelWrapper, JavaSaveable, JavaLoader):
     ...     rmtree(path)
     ... except OSError:
     ...     pass
-
-    .. versionadded:: 0.9.0
     """
     @since("0.9.0")
     def predict(self, user, product):
@@ -237,7 +241,6 @@ class ALS(object):
         return ratings
 
     @classmethod
-    @since("0.9.0")
     def train(cls, ratings, rank, iterations=5, lambda_=0.01, blocks=-1, nonnegative=False,
               seed=None):
         """
@@ -247,35 +250,38 @@ class ALS(object):
         features). To solve for these features, ALS is run iteratively with
         a configurable level of parallelism.
 
-        :param ratings:
-          RDD of `Rating` or (userID, productID, rating) tuple.
-        :param rank:
-          Number of features to use (also referred to as the number of latent factors).
-        :param iterations:
-          Number of iterations of ALS.
-          (default: 5)
-        :param lambda_:
-          Regularization parameter.
-          (default: 0.01)
-        :param blocks:
-          Number of blocks used to parallelize the computation. A value
-          of -1 will use an auto-configured number of blocks.
-          (default: -1)
-        :param nonnegative:
-          A value of True will solve least-squares with nonnegativity
-          constraints.
-          (default: False)
-        :param seed:
-          Random seed for initial matrix factorization model. A value
-          of None will use system time as the seed.
-          (default: None)
+        .. versionadded:: 0.9.0
+
+        Parameters
+        ----------
+        ratings : :py:class:`pyspark.RDD`
+            RDD of `Rating` or (userID, productID, rating) tuple.
+        rank : int
+            Number of features to use (also referred to as the number of latent factors).
+        iterations : int, optional
+            Number of iterations of ALS.
+            (default: 5)
+        lambda\\_ : float, optional
+            Regularization parameter.
+            (default: 0.01)
+        blocks : int, optional
+            Number of blocks used to parallelize the computation. A value
+            of -1 will use an auto-configured number of blocks.
+            (default: -1)
+        nonnegative : bool, optional
+            A value of True will solve least-squares with nonnegativity
+            constraints.
+            (default: False)
+        seed : bool, optional
+            Random seed for initial matrix factorization model. A value
+            of None will use system time as the seed.
+            (default: None)
         """
         model = callMLlibFunc("trainALSModel", cls._prepare(ratings), rank, iterations,
                               lambda_, blocks, nonnegative, seed)
         return MatrixFactorizationModel(model)
 
     @classmethod
-    @since("0.9.0")
     def trainImplicit(cls, ratings, rank, iterations=5, lambda_=0.01, blocks=-1, alpha=0.01,
                       nonnegative=False, seed=None):
         """
@@ -285,31 +291,35 @@ class ALS(object):
         given rank (number of features). To solve for these features, ALS
         is run iteratively with a configurable level of parallelism.
 
-        :param ratings:
-          RDD of `Rating` or (userID, productID, rating) tuple.
-        :param rank:
-          Number of features to use (also referred to as the number of latent factors).
-        :param iterations:
-          Number of iterations of ALS.
-          (default: 5)
-        :param lambda_:
-          Regularization parameter.
-          (default: 0.01)
-        :param blocks:
-          Number of blocks used to parallelize the computation. A value
-          of -1 will use an auto-configured number of blocks.
-          (default: -1)
-        :param alpha:
-          A constant used in computing confidence.
-          (default: 0.01)
-        :param nonnegative:
-          A value of True will solve least-squares with nonnegativity
-          constraints.
-          (default: False)
-        :param seed:
-          Random seed for initial matrix factorization model. A value
-          of None will use system time as the seed.
-          (default: None)
+        .. versionadded:: 0.9.0
+
+        Parameters
+        ----------
+        ratings : :py:class:`pyspark.RDD`
+            RDD of `Rating` or (userID, productID, rating) tuple.
+        rank : int
+            Number of features to use (also referred to as the number of latent factors).
+        iterations : int, optional
+            Number of iterations of ALS.
+            (default: 5)
+        lambda\\_ : float, optional
+            Regularization parameter.
+            (default: 0.01)
+        blocks : int, optional
+            Number of blocks used to parallelize the computation. A value
+            of -1 will use an auto-configured number of blocks.
+            (default: -1)
+        alpha : float, optional
+            A constant used in computing confidence.
+            (default: 0.01)
+        nonnegative : bool, optional
+            A value of True will solve least-squares with nonnegativity
+            constraints.
+            (default: False)
+        seed : int, optional
+            Random seed for initial matrix factorization model. A value
+            of None will use system time as the seed.
+            (default: None)
         """
         model = callMLlibFunc("trainImplicitALSModel", cls._prepare(ratings), rank,
                               iterations, lambda_, blocks, alpha, nonnegative, seed)
