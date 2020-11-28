ee1c976f2002ce004a132322722251ce7345b55d
Kousuke Saruta <sarutak@oss.nttdata.com>
2020-11-28 13:26:56 +0900

    Fix a typo in the comment.

--- external/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/MariaDBKrbIntegrationSuite.scala
+++ external/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/MariaDBKrbIntegrationSuite.scala
@@ -29,7 +29,7 @@ import org.apache.spark.tags.DockerTest
  * {{{
  *   MARIADB_DOCKER_IMAGE_NAME=mariadb:10.5.8
  *     ./build/sbt -Pdocker-integration-tests
- *     "testOnly org.apache.spark.sql.jdbc.MariaDBKrb2IntegrationSuite"
+ *     "testOnly org.apache.spark.sql.jdbc.MariaDBKrbIntegrationSuite"
  * }}}
  */
 @DockerTest
