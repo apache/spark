ab96eb701109ef20b46caaa13740a2d662011bd9
zky.zhoukeyong <zky.zhoukeyong@alibaba-inc.com>
2020-11-30 15:26:09 +0800

    refine code
    Change-Id: I9452ce09f37b1ab227cca66fea4ede05dfc48f81

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/DateExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/DateExpressionsSuite.scala
@@ -1023,8 +1023,6 @@ class DateExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper {
     checkEvaluation(MakeDate(Literal(2019), Literal(7), Literal(32)), null)
   }
 
-  // Since MakeDate is NullIntolerant, expect null output for any null input
-  // Expect Exception for invalid parameter
   test("ANSI mode: creating values of DateType via make_date") {
     withSQLConf(SQLConf.ANSI_ENABLED.key -> "true") {
       checkEvaluation(MakeDate(Literal(2013), Literal(7), Literal(15)), Date.valueOf("2013-7-15"))
@@ -1076,8 +1074,6 @@ class DateExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper {
     checkEvaluation(makeTimestampExpr, Timestamp.valueOf("2019-08-12 00:00:58.000001"))
   }
 
-  // Since MakeTimestamp is NullIntolerant, expect null output for any null input
-  // Expect Exception for invalid parameter
   test("ANSI mode: creating values of TimestampType via make_timestamp") {
     withSQLConf(SQLConf.ANSI_ENABLED.key -> "true") {
       var makeTimestampExpr = MakeTimestamp(
