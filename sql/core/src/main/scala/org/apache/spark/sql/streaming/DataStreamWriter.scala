49a8c8793bd81dc3e183847c32f90148e205b685
Yuanjian Li <yuanjian.li@databricks.com>
2020-12-03 11:48:04 +0800

    leave comment and todo for v2 table support

--- sql/core/src/main/scala/org/apache/spark/sql/streaming/DataStreamWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/streaming/DataStreamWriter.scala
@@ -305,6 +305,9 @@ final class DataStreamWriter[T] private[sql](ds: Dataset[T]) {
    * table as new data arrives. A new table will be created if the table not exists. The returned
    * [[StreamingQuery]] object can be used to interact with the stream.
    *
+   * Note, currently the new table creation by this API doesn't fully cover the V2 table.
+   * TODO (SPARK-33638): Full support of v2 table creation
+   *
    * @since 3.1.0
    */
   @throws[TimeoutException]
