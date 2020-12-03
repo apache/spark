6b339c70c45a2bad0e1e2c3f6638f4c59475569e
Xiaodong DENG <xd.deng.r@gmail.com>
2020-12-03 22:36:25 +0100

    Avoid log spam & have more meaningful log when pull image in DockerOperator (#12763)
    Fixing issue reported in https://github.com/apache/airflow/issues/12576
    
    This change actually also makes the log more meaningful
    
    Co-authored-by: Tomek Urbaszek <turbaszek@gmail.com>

--- airflow/providers/docker/operators/docker.py
+++ airflow/providers/docker/operators/docker.py
@@ -282,13 +282,24 @@ class DockerOperator(BaseOperator):
             raise Exception("The 'cli' should be initialized before!")
 
         # Pull the docker image if `force_pull` is set or image does not exist locally
+        # pylint: disable=too-many-nested-blocks
         if self.force_pull or not self.cli.images(name=self.image):
             self.log.info('Pulling docker image %s', self.image)
+            latest_status = {}
             for output in self.cli.pull(self.image, stream=True, decode=True):
                 if isinstance(output, str):
                     self.log.info("%s", output)
+                    continue
                 if isinstance(output, dict) and 'status' in output:
-                    self.log.info("%s", output['status'])
+                    output_status = output["status"]
+                    if 'id' not in output:
+                        self.log.info("%s", output_status)
+                        continue
+
+                    output_id = output["id"]
+                    if latest_status.get(output_id) != output_status:
+                        self.log.info("%s: %s", output_id, output_status)
+                        latest_status[output_id] = output_status
 
         self.environment['AIRFLOW_TMP_DIR'] = self.tmp_dir
         return self._run_image()
