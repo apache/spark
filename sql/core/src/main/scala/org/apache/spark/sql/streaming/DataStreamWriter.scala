37c4cd8f05316227465ff9cccbba063779827660
Ammar Al-Batool <ammar.albatool@gmail.com>
2020-12-19 14:53:40 -0600

    [MINOR][DOCS] Fix typos in ScalaDocs for DataStreamWriter#foreachBatch
    The title is pretty self-explanatory.
    
    ### What changes were proposed in this pull request?
    
    Fixing typos in the docs for `foreachBatch` functions.
    
    ### Why are the changes needed?
    
    To fix typos in JavaDoc/ScalaDoc.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Yes.
    
    Closes #30782 from ammar1x/patch-1.
    
    Lead-authored-by: Ammar Al-Batool <ammar.albatool@gmail.com>
    Co-authored-by: Ammar Al-Batool <ammar.al-batool@disneystreaming.com>
    Signed-off-by: Sean Owen <srowen@gmail.com>

--- sql/core/src/main/scala/org/apache/spark/sql/streaming/DataStreamWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/streaming/DataStreamWriter.scala
@@ -494,12 +494,13 @@ final class DataStreamWriter[T] private[sql](ds: Dataset[T]) {
    * :: Experimental ::
    *
    * (Scala-specific) Sets the output of the streaming query to be processed using the provided
-   * function. This is supported only the in the micro-batch execution modes (that is, when the
+   * function. This is supported only in the micro-batch execution modes (that is, when the
    * trigger is not continuous). In every micro-batch, the provided function will be called in
    * every micro-batch with (i) the output rows as a Dataset and (ii) the batch identifier.
-   * The batchId can be used deduplicate and transactionally write the output
+   * The batchId can be used to deduplicate and transactionally write the output
    * (that is, the provided Dataset) to external systems. The output Dataset is guaranteed
-   * to exactly same for the same batchId (assuming all operations are deterministic in the query).
+   * to be exactly the same for the same batchId (assuming all operations are deterministic
+   * in the query).
    *
    * @since 2.4.0
    */
@@ -515,12 +516,13 @@ final class DataStreamWriter[T] private[sql](ds: Dataset[T]) {
    * :: Experimental ::
    *
    * (Java-specific) Sets the output of the streaming query to be processed using the provided
-   * function. This is supported only the in the micro-batch execution modes (that is, when the
+   * function. This is supported only in the micro-batch execution modes (that is, when the
    * trigger is not continuous). In every micro-batch, the provided function will be called in
    * every micro-batch with (i) the output rows as a Dataset and (ii) the batch identifier.
-   * The batchId can be used deduplicate and transactionally write the output
+   * The batchId can be used to deduplicate and transactionally write the output
    * (that is, the provided Dataset) to external systems. The output Dataset is guaranteed
-   * to exactly same for the same batchId (assuming all operations are deterministic in the query).
+   * to be exactly the same for the same batchId (assuming all operations are deterministic
+   * in the query).
    *
    * @since 2.4.0
    */
