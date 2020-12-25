de367cc9137a7710bc0883a5a36e107f3acff45f
Max Gekk <max.gekk@gmail.com>
2020-12-25 14:11:58 +0300

    Add comments for CommandSuiteBase

--- sql/core/src/test/scala/org/apache/spark/sql/execution/command/v1/CommandSuiteBase.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/command/v1/CommandSuiteBase.scala
@@ -20,8 +20,14 @@ package org.apache.spark.sql.execution.command.v1
 import org.apache.spark.sql.connector.catalog.CatalogManager
 import org.apache.spark.sql.test.SharedSparkSession
 
+/**
+ * The trait contains settings and utility functions. It can be mixed to the test suites for
+ * datasource v1 In-Memory catalog. This trait complements the common trait
+ * [[org.apache.spark.sql.execution.command.DDLCommandTestUtils]] with utility functions and
+ * settings for all unified datasource V1 and V2 test suites.
+ */
 trait CommandSuiteBase extends SharedSparkSession {
-  def version: String = "V1"
+  def version: String = "V1" // The prefix is added to test names
   def catalog: String = CatalogManager.SESSION_CATALOG_NAME
-  def defaultUsing: String = "USING parquet"
+  def defaultUsing: String = "USING parquet" // The clause is used in creating tables under testing
 }
