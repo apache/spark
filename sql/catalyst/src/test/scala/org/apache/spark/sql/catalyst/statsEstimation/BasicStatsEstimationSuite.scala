3c8e165b1278cf37f495373e3bba2cd788e6adea
Yuming Wang <yumwang@ebay.com>
2021-01-01 16:36:26 +0800

    Some operator missing rowCount when enable CBO

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/statsEstimation/BasicStatsEstimationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/statsEstimation/BasicStatsEstimationSuite.scala
@@ -128,6 +128,10 @@ class BasicStatsEstimationSuite extends PlanTest with StatsEstimationTestBase {
       expectedStatsCboOff = Statistics.DUMMY)
   }
 
+  test("SPARK-33954: Some operator missing rowCount when enable CBO") {
+    checkStats(plan.repartition(10), Statistics(sizeInBytes = 120, rowCount = Some(10)))
+  }
+
   /** Check estimated stats when cbo is turned on/off. */
   private def checkStats(
       plan: LogicalPlan,
