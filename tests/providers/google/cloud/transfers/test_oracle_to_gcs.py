6e1a6ff3c8a4f8f9bcf8b7601362359bfb2be6bf
Jarek Potiuk <jarek.potiuk@polidea.com>
2021-01-02 11:32:42 +0100

    Fixed failing pylint errors introduced in #13403 (#13429)
    This fixes a failing pylint error introduced in #13403. This error
    also trigger another pylint problem involved with c-extension

--- tests/providers/google/cloud/transfers/test_oracle_to_gcs.py
+++ tests/providers/google/cloud/transfers/test_oracle_to_gcs.py
@@ -15,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
+# pylint: disable=c-extension-no-member
 import unittest
 from unittest import mock
 
