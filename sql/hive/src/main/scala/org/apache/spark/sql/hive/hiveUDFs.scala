bf9d0cb7f8d1be4143e77cfffb1d4d28169bc166
ulysses-you <ulyssesyou18@gmail.com>
2021-01-04 10:11:43 +0800

    review

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/hiveUDFs.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/hiveUDFs.scala
@@ -210,14 +210,14 @@ private[hive] case class HiveGenericUDTF(
   }
 
   @transient
-  protected lazy val inputInspectors = {
+  protected lazy val inputInspector = {
     val inspectors = children.map(toInspector)
     val fields = inspectors.indices.map(index => s"_col$index").asJava
     ObjectInspectorFactory.getStandardStructObjectInspector(fields, inspectors.asJava)
   }
 
   @transient
-  protected lazy val outputInspector = function.initialize(inputInspectors)
+  protected lazy val outputInspector = function.initialize(inputInspector)
 
   @transient
   protected lazy val udtInput = new Array[AnyRef](children.length)
