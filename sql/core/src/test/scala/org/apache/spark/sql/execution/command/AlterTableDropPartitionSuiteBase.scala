21c0c8e4c346dd7327c0bb18a14e0f920a984790
Max Gekk <max.gekk@gmail.com>
2021-01-04 14:12:24 +0300

    Use withNamespaceAndTable() in the test

--- sql/core/src/test/scala/org/apache/spark/sql/execution/command/AlterTableDropPartitionSuiteBase.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/command/AlterTableDropPartitionSuiteBase.scala
@@ -146,17 +146,17 @@ trait AlterTableDropPartitionSuiteBase extends QueryTest with DDLCommandTestUtil
   }
 
   test("SPARK-33950: refresh cache after partition dropping") {
-    withTable("t") {
-      sql(s"CREATE TABLE t (id int, part int) $defaultUsing PARTITIONED BY (part)")
-      sql("INSERT INTO t PARTITION (part=0) SELECT 0")
-      sql("INSERT INTO t PARTITION (part=1) SELECT 1")
-      assert(!spark.catalog.isCached("t"))
-      sql("CACHE TABLE t")
-      assert(spark.catalog.isCached("t"))
-      QueryTest.checkAnswer(sql("SELECT * FROM t"), Seq(Row(0, 0), Row(1, 1)))
-      sql("ALTER TABLE t DROP PARTITION (part=0)")
-      assert(spark.catalog.isCached("t"))
-      QueryTest.checkAnswer(sql("SELECT * FROM t"), Seq(Row(1, 1)))
+    withNamespaceAndTable("ns", "tbl") { t =>
+      sql(s"CREATE TABLE $t (id int, part int) $defaultUsing PARTITIONED BY (part)")
+      sql(s"INSERT INTO $t PARTITION (part=0) SELECT 0")
+      sql(s"INSERT INTO $t PARTITION (part=1) SELECT 1")
+      assert(!spark.catalog.isCached(t))
+      sql(s"CACHE TABLE $t")
+      assert(spark.catalog.isCached(t))
+      QueryTest.checkAnswer(sql(s"SELECT * FROM $t"), Seq(Row(0, 0), Row(1, 1)))
+      sql(s"ALTER TABLE $t DROP PARTITION (part=0)")
+      assert(spark.catalog.isCached(t))
+      QueryTest.checkAnswer(sql(s"SELECT * FROM $t"), Seq(Row(1, 1)))
     }
   }
 }
