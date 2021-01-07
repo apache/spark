983307ed005cb15304d37736456ea9741914633a
Tom.Howland <Tom.Howland@target.com>
2021-01-07 11:08:41 -0800

    making the test easier to read

--- R/pkg/tests/fulltests/test_daemon_initialization.R
+++ R/pkg/tests/fulltests/test_daemon_initialization.R
@@ -14,43 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 library(testthat)
-
 context("Daemon Initialization")
-
 sparkR.stop()
-sparkR.session(
-  master = "local[*]",
-  spark.r.daemonInit = 'message("Initting the Daemon ..."); testInit <- "wow"'
-)
-
-df <- createDataFrame (list(list(1L, 1, "1", 0.1), list(1L, 2, "1", 0.2), list(3L, 3, "3", 0.3)),
-                       c("a", "b", "c", "d"))
-
-eek <- function(...) {
-  warning(...)
-  quit(status = 1, save = "no")
+sparkR.session(master = "local[*]",
+               spark.r.daemonInit =
+                 'message("Initting the Daemon ..."); testInit <- "wow"')
+data <-
+  list(list(1L, 1, "1", 0.1), list(1L, 2, "1", 0.2), list(3L, 3, "3", 0.3))
+inputColumnNames <- c("a", "b", "c", "d")
+outputColumnNames <- c("a", "c")
+workerFunction <- function(key, x) {
+  if (!exists("testInit")) {
+    warning("daemon did not initialize")
+    quit(status = 1, save = "no")
+  }
+  data.frame(key, mean(x$b), stringsAsFactors = FALSE)
 }
-
 schema <-
   structType(
     structField("a", "integer"),
     structField("c", "string"),
-    structField("avg", "double"))
-
-result <- gapply(df,
-                 c("a", "c"),
-                 function(key, x) {
-                   if(!exists("testInit"))
-                     eek("daemon did not initialize")
-                   data.frame(key, mean(x$b), stringsAsFactors = FALSE)
-                 }, schema)
-
+    structField("avg", "double")
+  )
 test_that("Daemon Initialization",
           {
-            result <- collect(result)
+            df <- createDataFrame (data, inputColumnNames)
+            resultDF <-
+              gapply(df, outputColumnNames, workerFunction, schema)
+            result <- collect(resultDF)
             expect_equal(max(result$avg), 3)
           })
-
 sparkR.stop()
