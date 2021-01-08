89c15724d5af9b423a14f512c8764bd26eee6c9f
Max Gekk <max.gekk@gmail.com>
2021-01-08 11:03:06 +0300

    Fix for scala 2.13

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/ExternalCatalogUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/ExternalCatalogUtils.scala
@@ -180,7 +180,7 @@ object ExternalCatalogUtils {
   }
 
   def convertNullPartitionValues(spec: TablePartitionSpec): TablePartitionSpec = {
-    spec.mapValues(v => if (v == null) DEFAULT_PARTITION_NAME else v)
+    spec.mapValues(v => if (v == null) DEFAULT_PARTITION_NAME else v).toMap
   }
 }
 
