1c08d3725393b6086db1926d9131d61c83d02c54
Gengliang Wang <gengliang.wang@databricks.com>
2021-01-12 13:25:18 +0800

    address comment

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/Count.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/Count.scala
@@ -52,8 +52,7 @@ case class Count(children: Seq[Expression]) extends DeclarativeAggregate {
   override def dataType: DataType = LongType
 
   override def checkInputDataTypes(): TypeCheckResult = {
-    val conf = SQLConf.get
-    if (children.isEmpty && !conf.getConf(SQLConf.ALLOW_PARAMETERLESS_COUNT)) {
+    if (children.isEmpty && !SQLConf.get.getConf(SQLConf.ALLOW_PARAMETERLESS_COUNT)) {
       TypeCheckResult.TypeCheckFailure(s"$prettyName requires at least one argument. " +
         s"If you have to call the function $prettyName without arguments, set the legacy " +
         s"configuration `${SQLConf.ALLOW_PARAMETERLESS_COUNT.key}` as true")
