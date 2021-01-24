a39c8846ec28347592a9ead40f4226c4b50cfefc
Yu Zhong <zhongyu8@gmail.com>
2021-01-24 20:42:46 +0800

    Materialize BroadcastQueryStage first to try to avoid broadcast timeout in AQE

--- sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/QueryStageExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/QueryStageExec.scala
@@ -76,6 +76,7 @@ abstract class QueryStageExec extends LeafExecNode {
    * stage is ready.
    */
   final def materialize(): Future[Any] = executeQuery {
+    logDebug(s"Materialize query stage ${this.getClass.getSimpleName}: $id")
     doMaterialize()
   }
 
