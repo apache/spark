4cc5a20e1221ad958edaff00768382ec224ceb99
Jiaan Geng <beliefer@163.com>
2021-01-25 21:14:37 +0800

    Update code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
@@ -28,7 +28,7 @@ import org.codehaus.janino.InternalCompilerException
 import org.apache.spark.SparkException
 import org.apache.spark.sql.catalyst.analysis.UnresolvedGenerator
 import org.apache.spark.sql.catalyst.catalog.CatalogDatabase
-import org.apache.spark.sql.catalyst.expressions.{Expression, SortOrder, UnevaluableAggregate}
+import org.apache.spark.sql.catalyst.expressions.{Expression, UnevaluableAggregate}
 import org.apache.spark.sql.catalyst.plans.logical.{LeafNode, LogicalPlan}
 import org.apache.spark.sql.types.{DataType, Decimal}
 import org.apache.spark.unsafe.array.ByteArrayMethods
