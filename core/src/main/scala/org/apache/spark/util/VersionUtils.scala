43367c51cc85c2893d663bc248ee95d4d15cdbde
Chao Sun <sunchao@apple.com>
2021-01-26 13:30:15 -0800

    fix comment

--- core/src/main/scala/org/apache/spark/util/VersionUtils.scala
+++ core/src/main/scala/org/apache/spark/util/VersionUtils.scala
@@ -75,7 +75,7 @@ private[spark] object VersionUtils {
    *  - 2.4   (extracts to (2, 4, 0))
    *  - 3.2.2   (extracts to (3, 2, 2))
    *  - 3.2.2.4   (extracts to 3, 2, 2))
-   *  - 3.3.1-SNAPSHOT   (extracts to (3, 2, 2))
+   *  - 3.3.1-SNAPSHOT   (extracts to (3, 3, 1))
    *  - 3.2.2.4SNAPSHOT   (extracts to (3, 2, 2), only the first 3 components)
    *
    * Examples of invalid version:
