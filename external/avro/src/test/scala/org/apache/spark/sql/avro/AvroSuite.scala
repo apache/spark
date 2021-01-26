9e154eeca083eeeff921a3162040e80c0be9d69e
Dongjoon Hyun <dhyun@apple.com>
2021-01-26 14:23:54 +0900

    [SPARK-34229][SQL] Avro should read decimal values with the file schema
    ### What changes were proposed in this pull request?
    
    This PR aims to fix Avro data source to use the decimal precision and scale of file schema.
    
    ### Why are the changes needed?
    
    The decimal value should be interpreted with its original precision and scale. Otherwise, it returns incorrect result like the following. The schema mismatch happens when we use `userSpecifiedSchema` or there are multiple files with inconsistent schema or HiveMetastore schema is updated by the user.
    ```scala
    scala> sql("SELECT 3.14 a").write.format("avro").save("/tmp/avro")
    scala> spark.read.schema("a DECIMAL(4, 3)").format("avro").load("/tmp/avro").show
    +-----+
    |    a|
    +-----+
    |0.314|
    +-----+
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, this will return correct result.
    
    ### How was this patch tested?
    
    Pass the CI with the newly added test case.
    
    Closes #31329 from dongjoon-hyun/SPARK-34229.
    
    Authored-by: Dongjoon Hyun <dhyun@apple.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>
    (cherry picked from commit 7d09eac1ccb6a14a36fce30ae7cda575c29e1974)
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- external/avro/src/test/scala/org/apache/spark/sql/avro/AvroSuite.scala
+++ external/avro/src/test/scala/org/apache/spark/sql/avro/AvroSuite.scala
@@ -589,6 +589,14 @@ abstract class AvroSuite extends QueryTest with SharedSparkSession {
     }
   }
 
+  test("SPARK-34229: Avro should read decimal values with the file schema") {
+    withTempPath { path =>
+      sql("SELECT 3.14 a").write.format("avro").save(path.toString)
+      val data = spark.read.schema("a DECIMAL(4, 3)").format("avro").load(path.toString).collect()
+      assert(data.map(_ (0)).contains(new java.math.BigDecimal("3.140")))
+    }
+  }
+
   test("converting some specific sparkSQL types to avro") {
     withTempPath { tempDir =>
       val testSchema = StructType(Seq(
