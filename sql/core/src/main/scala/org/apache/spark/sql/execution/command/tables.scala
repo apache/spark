4a60a312fe22468f7c54c83ed8e83427028caa15
Angerszhuuuu <angers.zhu@gmail.com>
2021-02-01 23:12:06 +0800

    keep same as 2.x hive

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
@@ -911,7 +911,7 @@ case class ShowTablePropertiesCommand(
           val propValue = catalogTable
             .properties
             .getOrElse(p, s"Table ${catalogTable.qualifiedName} does not have property: $p")
-          Seq(Row(p, propValue))
+          Seq(Row(propValue))
         case None =>
           catalogTable.properties.map(p => Row(p._1, p._2)).toSeq
       }
