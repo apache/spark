fd8ac9f4ff2b7bfcb297b2072c4749eee9dbcfd0
Phillip Henry <PhillHenry@gmail.com>
2021-02-09 11:27:56 +0000

    Code style.

--- mllib/src/test/scala/org/apache/spark/ml/tuning/ParamRandomBuilderSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/tuning/ParamRandomBuilderSuite.scala
@@ -1,11 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.ml.tuning
 
 import org.apache.spark.SparkFunSuite
 import org.apache.spark.ml.param.{Param, ParamMap, ParamPair, TestParams}
+
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class ParamRandomBuilderSuite extends SparkFunSuite with ScalaCheckDrivenPropertyChecks with Matchers {
+class ParamRandomBuilderSuite extends SparkFunSuite with ScalaCheckDrivenPropertyChecks
+  with Matchers {
 
   val solver = new TestParams() {
     val randomCol: Param[Double] = new Param[Double](this, "randomVal", "randomly generated value")
@@ -14,12 +33,12 @@ class ParamRandomBuilderSuite extends SparkFunSuite with ScalaCheckDrivenPropert
 
   test("random params mixed with fixed values") {
     import RandomRanges._
-    val maxIterations                   = 10
-    val basedOn:    Array[ParamPair[_]] = Array(maxIter -> maxIterations)
-    val inputCols:  Array[String]       = Array("input0", "input1")
-    val limit:      Limits[Double]      = Limits(0d, 100d)
-    val nRandoms                        = 5
-    val paramMap:   Array[ParamMap]     = new ParamRandomBuilder()
+    val maxIterations: Int = 10
+    val basedOn: Array[ParamPair[_]] = Array(maxIter -> maxIterations)
+    val inputCols: Array[String] = Array("input0", "input1")
+    val limit: Limits[Double] = Limits(0d, 100d)
+    val nRandoms: Int = 5
+    val paramMap: Array[ParamMap] = new ParamRandomBuilder()
       .baseOn(basedOn: _*)
       .addGrid(inputCol, inputCols)
       .addRandom(randomCol, limit, nRandoms)
--- mllib/src/test/scala/org/apache/spark/ml/tuning/RandomRangesSuite.scala
+++ mllib/src/test/scala/org/apache/spark/ml/tuning/RandomRangesSuite.scala
@@ -1,13 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.ml.tuning
 
-import scala.reflect.runtime.universe.TypeTag
 import org.apache.spark.SparkFunSuite
+
 import org.scalacheck.Arbitrary._
-import org.scalacheck.{Arbitrary, Gen}
 import org.scalacheck.Gen.Choose
-import org.scalatest.{Assertion, Succeeded}
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.matchers.must.Matchers
+import org.scalatest.{Assertion, Succeeded}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+import scala.reflect.runtime.universe.TypeTag
 
 class RandomRangesSuite extends SparkFunSuite with ScalaCheckDrivenPropertyChecks with Matchers {
 
@@ -101,18 +120,17 @@ class RandomRangesSuite extends SparkFunSuite with ScalaCheckDrivenPropertyCheck
     }
   }
 
-  def checkRange[T: Numeric: Generator: Choose: TypeTag: Arbitrary](rand: RandomFn[T]): Assertion = {
+  def checkRange[T: Numeric: Generator: Choose: TypeTag: Arbitrary](rand: RandomFn[T]): Assertion =
     forAll { (x: T, y: T) =>
       if (rand.appropriate(x, y)) {
-        val ops: Numeric[T]     = implicitly[Numeric[T]]
-        val limit:  Limits[T]   = Limits(x, y)
-        val gen:    RandomT[T]  = RandomRanges(limit)
-        val result: T           = rand(gen)
-        val ordered             = lowerUpper(x, y)
+        val ops: Numeric[T] = implicitly[Numeric[T]]
+        val limit: Limits[T] = Limits(x, y)
+        val gen: RandomT[T] = RandomRanges(limit)
+        val result: T = rand(gen)
+        val ordered: (T, T) = lowerUpper(x, y)
         assert(ops.gteq(result, ordered._1) && ops.lteq(result, ordered._2))
       } else Succeeded
     }
-  }
 
   def checkDistributionOf[T: Numeric: Generator: Choose](range: T): Unit = {
     val ops: Numeric[T] = implicitly[Numeric[T]]
@@ -127,32 +145,32 @@ class RandomRangesSuite extends SparkFunSuite with ScalaCheckDrivenPropertyCheck
   }
 
   def meanAndStandardDeviation[T: Numeric](xs: Seq[T]): (Double, Double) = {
-    val ops:          Numeric[T]  = implicitly[Numeric[T]]
-    val n:            Int         = xs.length
-    val mean:         Double      = ops.toDouble(xs.sum) / n
-    val squaredDiff:  Seq[Double] = xs.map { x: T => math.pow(ops.toDouble(x) - mean, 2) }
-    val stdDev:       Double      = math.pow(squaredDiff.sum / n - 1, 0.5)
+    val ops: Numeric[T] = implicitly[Numeric[T]]
+    val n: Int = xs.length
+    val mean: Double = ops.toDouble(xs.sum) / n
+    val squaredDiff: Seq[Double] = xs.map { x: T => math.pow(ops.toDouble(x) - mean, 2) }
+    val stdDev: Double = math.pow(squaredDiff.sum / n - 1, 0.5)
     (mean, stdDev)
   }
 
   def lowerUpper[T: Numeric](x: T, y: T): (T, T) = {
-    val ops:          Numeric[T]  = implicitly[Numeric[T]]
+    val ops: Numeric[T] = implicitly[Numeric[T]]
     (ops.min(x, y), ops.max(x, y))
   }
 
   def midPointOf[T: Numeric : Generator](lim: Limits[T]): Double = {
-    val ordered:  (T, T)      = lowerUpper(lim.x, lim.y)
-    val ops:      Numeric[T]  = implicitly[Numeric[T]]
-    val range:    T           = ops.minus(ordered._2, ordered._1)
+    val ordered: (T, T) = lowerUpper(lim.x, lim.y)
+    val ops: Numeric[T] = implicitly[Numeric[T]]
+    val range: T = ops.minus(ordered._2, ordered._1)
     (ops.toDouble(range) / 2) + ops.toDouble(ordered._1)
   }
 
   def assertEvenDistribution[T: Numeric: Generator](n: Int, lim: Limits[T]): Assertion = {
-    val gen:          RandomT[T]  = RandomRanges(lim)
-    val xs:           Seq[T]      = (0 to n).map { _: Int => gen.randomT() }
-    val (mean, stdDev)            = meanAndStandardDeviation(xs)
-    val tolerance:    Double      = 4 * stdDev
-    val halfWay:      Double      = midPointOf(lim)
+    val gen: RandomT[T] = RandomRanges(lim)
+    val xs: Seq[T] = (0 to n).map { _: Int => gen.randomT() }
+    val (mean, stdDev) = meanAndStandardDeviation(xs)
+    val tolerance: Double = 4 * stdDev
+    val halfWay: Double = midPointOf(lim)
     assert(mean > halfWay - tolerance && mean < halfWay + tolerance)
   }
 
