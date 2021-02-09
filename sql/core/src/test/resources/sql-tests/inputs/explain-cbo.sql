6afa78bf5586aaf2ba64ddc94aedbe6d2fa80dfa
Angerszhuuuu <angers.zhu@gmail.com>
2021-02-09 14:32:25 +0800

    update

--- sql/core/src/test/resources/sql-tests/inputs/explain-cbo.sql
+++ sql/core/src/test/resources/sql-tests/inputs/explain-cbo.sql
@@ -1,24 +1,27 @@
 --SET spark.sql.cbo.enabled=true
 --SET spark.sql.maxMetadataStringLength = 500
 
-CREATE TABLE t1(a INT, b INT) USING PARQUET;
-CREATE TABLE t2(c INT, d INT) USING PARQUET;
+CREATE TABLE explain_temp1(a INT, b INT) USING PARQUET;
+CREATE TABLE explain_temp2(c INT, d INT) USING PARQUET;
 
-ANALYZE TABLE t1 COMPUTE STATISTICS FOR ALL COLUMNS;
-ANALYZE TABLE t2 COMPUTE STATISTICS FOR ALL COLUMNS;
+ANALYZE TABLE explain_temp1 COMPUTE STATISTICS FOR ALL COLUMNS;
+ANALYZE TABLE explain_temp2 COMPUTE STATISTICS FOR ALL COLUMNS;
 
 EXPLAIN COST WITH max_store_sales AS
 (
   SELECT max(csales) tpcds_cmax
   FROM (
     SELECT sum(b) csales
-    FROM t1 WHERE a < 100
+    FROM explain_temp1 WHERE a < 100
   ) x
 ),
 best_ss_customer AS
 (
   SELECT c
-  FROM t2
+  FROM explain_temp2
   WHERE d > (SELECT * FROM max_store_sales)
 )
 SELECT c FROM best_ss_customer;
+
+DROP TABLE explain_temp1;
+DROP TABLE explain_temp2;
