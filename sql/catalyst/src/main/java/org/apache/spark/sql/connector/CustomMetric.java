b8c762a776d535a1083c424a61922dc8d1401ccd
Liang-Chi Hsieh <viirya@gmail.com>
2021-02-13 10:58:10 -0800

    Enrich metric comment.

--- sql/catalyst/src/main/java/org/apache/spark/sql/connector/CustomMetric.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/connector/CustomMetric.java
@@ -18,9 +18,19 @@
 package org.apache.spark.sql.connector;
 
 import org.apache.spark.annotation.Evolving;
+import org.apache.spark.sql.connector.read.PartitionReader;
+import org.apache.spark.sql.connector.read.Scan;
 
 /**
- * A custom metric.
+ * A custom metric. This is a logical representation of a metric reported by data sources during
+ * read path. Data sources can report supported metric list by {@link Scan} to Spark in query
+ * planning. During query execution, Spark will collect the metrics per partition by
+ * {@link PartitionReader} and combine metrics from partitions to the final result. How Spark
+ * combines metrics depends on the metric type. For streaming query, Spark will collect and combine
+ * metrics for a final result per micro batch.
+ *
+ * The metrics will be gathered during query execution back to the driver and then combined. The
+ * final result will be shown up in the physical operator in Spark UI.
  *
  * @since 3.2.0
  */
@@ -35,4 +45,17 @@ public interface CustomMetric {
      * Returns the description of custom metric.
      */
     String description();
+
+    /**
+     * Supported metric type. The metric types must be supported by Spark SQL internal metrics.
+     * SUM: Spark sums up metrics from partitions as the final result.
+     */
+    enum MetricType {
+      SUM
+    }
+
+    /**
+     * Returns the type of custom metric.
+     */
+    MetricType type();
 }
