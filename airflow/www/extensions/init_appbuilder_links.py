213c22c32a3371ad241a7270ca17c21d3797d6a4
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-02-19 16:30:11 -0700

    Allow viewers to see all docs links (#14197)
    Currently, Viewers can only see the "Documentation" link in the docs menu. This PR allows Viewers (and above) to see all of the links.

--- airflow/www/extensions/init_appbuilder_links.py
+++ airflow/www/extensions/init_appbuilder_links.py
@@ -21,10 +21,19 @@ from airflow.utils.docs import get_docs_url
 def init_appbuilder_links(app):
     """Add links to Docs menu in navbar"""
     appbuilder = app.appbuilder
-    appbuilder.add_link("Documentation", href=get_docs_url(), category="Docs")
-    appbuilder.add_link("Airflow Website", href='https://airflow.apache.org', category="Docs")
-    appbuilder.add_link("GitHub Repo", href='https://github.com/apache/airflow', category="Docs")
+    appbuilder.add_link(name="docs_links", label="Documentation", href=get_docs_url(), category="Docs")
     appbuilder.add_link(
-        "REST API Reference (Swagger UI)", href='/api/v1./api/v1_swagger_ui_index', category="Docs"
+        name="docs_links", label="Airflow Website", href='https://airflow.apache.org', category="Docs"
     )
-    appbuilder.add_link("REST API Reference (Redoc)", href="RedocView.redoc", category='Docs')
+    appbuilder.add_link(
+        name="docs_links", label="GitHub Repo", href='https://github.com/apache/airflow', category="Docs"
+    )
+    appbuilder.add_link(
+        name="docs_links",
+        label="REST API Reference (Swagger UI)",
+        href='/api/v1./api/v1_swagger_ui_index',
+        category="Docs",
+    )
+    appbuilder.add_link(
+        name="docs_links", label="REST API Reference (Redoc)", href="RedocView.redoc", category='Docs'
+    )
