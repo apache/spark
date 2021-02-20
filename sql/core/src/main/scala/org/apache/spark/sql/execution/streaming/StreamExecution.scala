52a6999d3c602f4968a3f1e2d137e1cb5d25cd4e
yi.wu <yi.wu@databricks.com>
2021-02-20 16:32:50 +0800

    fix

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/StreamExecution.scala
@@ -323,7 +323,7 @@ abstract class StreamExecution(
       startLatch.countDown()
 
       // While active, repeatedly attempt to run batches.
-      SparkSession.setActiveSession(sparkSession)
+      SparkSession.setActiveSession(sparkSessionForStream)
 
       updateStatusMessage("Initializing sources")
       // force initialization of the logical plan so that the sources can be created
