afde2e46116311fcb5b05762bb33e4e2db1f99bd
Max Gekk <max.gekk@gmail.com>
2021-02-22 20:00:27 +0300

    Fix comments in DayTimeIntervalType

--- sql/catalyst/src/main/scala/org/apache/spark/sql/types/DayTimeIntervalType.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/types/DayTimeIntervalType.scala
@@ -25,10 +25,10 @@ import org.apache.spark.annotation.Unstable
 /**
  * The type represents day-time intervals of the SQL standard. A day-time interval is made up
  * of a contiguous subset of the following fields:
- *   - SECOND, seconds within minutes and possibly fractions of a second (0-59.999999),
- *   - MINUTE, minutes within hours (0-59),
- *   - HOUR, hours within days (0-23),
- *   - DAY, days in the range (0-106751991).
+ *   - SECOND, seconds within minutes and possibly fractions of a second [0..59.999999],
+ *   - MINUTE, minutes within hours [0..59],
+ *   - HOUR, hours within days [0..23],
+ *   - DAY, days in the range [0..106751991].
  *
  * `DayTimeIntervalType` represents positive as well as negative day-time intervals.
  *
@@ -39,7 +39,7 @@ import org.apache.spark.annotation.Unstable
 @Unstable
 class DayTimeIntervalType private() extends AtomicType {
   /**
-   * Internally, values of day-time intervals are stored in long values as amount of time in terms
+   * Internally, values of day-time intervals are stored in `Long` values as amount of time in terms
    * of microseconds that are calculated by the formula:
    *   -/+ (24*60*60 * DAY + 60*60 * HOUR + 60 * MINUTE + SECOND) * 1000000
    */
@@ -50,7 +50,8 @@ class DayTimeIntervalType private() extends AtomicType {
   private[sql] val ordering = implicitly[Ordering[InternalType]]
 
   /**
-   * The day-time interval type has constant precision, and it always occupies 8 bytes.
+   * The day-time interval type has constant precision. A value of the type always occupies 8 bytes.
+   * The DAY field is constrained by the upper bound 106751991 to fit to `Long`.
    */
   override def defaultSize: Int = 8
 
