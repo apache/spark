4a3200b08ac3e7733b5a3dc7271d35e6872c5967
Yuming Wang <yumwang@apache.org>
2021-02-25 18:07:39 +0800

    [SPARK-34436][SQL] DPP support LIKE ANY/ALL expression
    ### What changes were proposed in this pull request?
    
    This pr make DPP support LIKE ANY/ALL expression:
    ```sql
    SELECT date_id, product_id FROM fact_sk f
    JOIN dim_store s
    ON f.store_id = s.store_id WHERE s.country LIKE ANY ('%D%E%', '%A%B%')
    ```
    
    ### Why are the changes needed?
    
    Improve query performance.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Unit test.
    
    Closes #31563 from wangyum/SPARK-34436.
    
    Lead-authored-by: Yuming Wang <yumwang@apache.org>
    Co-authored-by: Yuming Wang <yumwang@ebay.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/dynamicpruning/PartitionPruning.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/dynamicpruning/PartitionPruning.scala
@@ -164,6 +164,7 @@ object PartitionPruning extends Rule[LogicalPlan] with PredicateHelper {
     case _: BinaryComparison => true
     case _: In | _: InSet => true
     case _: StringPredicate => true
+    case _: MultiLikeBase => true
     case _ => false
   }
 
