apache/spark

commit 62a5b2dfa41b2928c35be954d13b5ed6e2b5b339

Fix return type in prev-date context variables (#12910)

airflow/models/taskinstance.py
@@ -759,7 +759,7 @@ class TaskInstance(Base, LoggingMixin):  # pylint: disable=R0902,R0904
         """
         self.log.debug("previous_execution_date was called")
         prev_ti = self.get_previous_ti(state=state, session=session)
-        return prev_ti and prev_ti.execution_date
+        return prev_ti and pendulum.instance(prev_ti.execution_date)
 
     @provide_session
     def get_previous_start_date(
@@ -773,7 +773,7 @@ class TaskInstance(Base, LoggingMixin):  # pylint: disable=R0902,R0904
         """
         self.log.debug("previous_start_date was called")
         prev_ti = self.get_previous_ti(state=state, session=session)
-        return prev_ti and prev_ti.start_date
+        return prev_ti and pendulum.instance(prev_ti.start_date)
 
     @property
     def previous_start_date_success(self) -> Optional[pendulum.DateTime]:

tests/operators/test_python.py
@@ -1150,9 +1150,22 @@ class TestPythonVirtualenvOperator(unittest.TestCase):
             default_args={'owner': 'airflow', 'start_date': DEFAULT_DATE},
             schedule_interval=INTERVAL,
         )
+        self.dag.create_dagrun(
+            run_type=DagRunType.MANUAL,
+            start_date=timezone.utcnow(),
+            execution_date=DEFAULT_DATE,
+            state=State.RUNNING,
+        )
         self.addCleanup(self.dag.clear)
 
+    def tearDown(self):
+        super().tearDown()
+        with create_session() as session:
+            session.query(DagRun).delete()
+            session.query(TI).delete()
+
     def _run_as_operator(self, fn, python_version=sys.version_info[0], **kwargs):
+
         task = PythonVirtualenvOperator(
             python_callable=fn, python_version=python_version, task_id='task', dag=self.dag, **kwargs
         )

commit 21f297425ae85ce89e21477d55b51d5560f47bf8

Fix crash when user clicks on  "Task Instance Details" caused by start_date being None (#14416)
This is to fix the following error that happens when a user clicks on 'Task Instance Details' for a TaskInstance that has previous TaskInstance not yet run. E.g.

The previous TaskInstance has not yet run because its dependencies are not yet met
The previous TaskInstance has not yet run because scheduler is busy,
the previous TaskInstance was marked success without running.
This bug was caused by #12910. It affects Airflow 2.0.0 and 2.0.1.

airflow/models/taskinstance.py
@@ -823,7 +823,8 @@ class TaskInstance(Base, LoggingMixin):  # pylint: disable=R0902,R0904
         """
         self.log.debug("previous_start_date was called")
         prev_ti = self.get_previous_ti(state=state, session=session)
-        return prev_ti and pendulum.instance(prev_ti.start_date)
+        # prev_ti may not exist and prev_ti.start_date may be None.
+        return prev_ti and prev_ti.start_date and pendulum.instance(prev_ti.start_date)
 
     @property
     def previous_start_date_success(self) -> Optional[pendulum.DateTime]:

tests/models/test_taskinstance.py
@@ -1488,6 +1488,39 @@ class TestTaskInstance(unittest.TestCase):
         assert ti_list[3].get_previous_start_date(state=State.SUCCESS) == ti_list[1].start_date
         assert ti_list[3].get_previous_start_date(state=State.SUCCESS) != ti_list[2].start_date
 
+    def test_get_previous_start_date_none(self):
+        """
+        Test that get_previous_start_date() can handle TaskInstance with no start_date.
+        """
+        with DAG("test_get_previous_start_date_none", start_date=DEFAULT_DATE, schedule_interval=None) as dag:
+            task = DummyOperator(task_id="op")
+
+        day_1 = DEFAULT_DATE
+        day_2 = DEFAULT_DATE + datetime.timedelta(days=1)
+
+        # Create a DagRun for day_1 and day_2. Calling ti_2.get_previous_start_date()
+        # should return the start_date of ti_1 (which is None because ti_1 was not run).
+        # It should not raise an error.
+        dagrun_1 = dag.create_dagrun(
+            execution_date=day_1,
+            state=State.RUNNING,
+            run_type=DagRunType.MANUAL,
+        )
+
+        dagrun_2 = dag.create_dagrun(
+            execution_date=day_2,
+            state=State.RUNNING,
+            run_type=DagRunType.MANUAL,
+        )
+
+        ti_1 = dagrun_1.get_task_instance(task.task_id)
+        ti_2 = dagrun_2.get_task_instance(task.task_id)
+        ti_1.task = task
+        ti_2.task = task
+
+        assert ti_2.get_previous_start_date() == ti_1.start_date
+        assert ti_1.start_date is None
+
     def test_pendulum_template_dates(self):
         dag = models.DAG(
             dag_id='test_pendulum_template_dates',