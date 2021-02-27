7d181508ef5383d36eae584ceedb9845b7467776
Kamil Breguła <mik-laj@users.noreply.github.com>
2021-02-28 00:32:40 +0100

    Enable LDAP auth in docker-compose.yaml (#14516)
    Co-authored-by: Kamil Breguła <kamilbregula@apache.org>

--- docs/apache-airflow/start/airflow.sh
+++ docs/apache-airflow/start/airflow.sh
@@ -24,5 +24,5 @@ PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 set -euo pipefail
 
-export COMPOSE_FILE=${PROJECT_DIR}/docker-compose.yaml
+export COMPOSE_FILE="${PROJECT_DIR}/docker-compose.yaml"
 exec docker-compose run airflow-worker "${@}"
