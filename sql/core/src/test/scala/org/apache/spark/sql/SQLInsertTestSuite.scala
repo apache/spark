20ae7c1d22cbafd397863b4c57efdcfd0915af5c
Shixiong Zhu <zsxwing@gmail.com>
2021-02-27 10:27:10 -0800

    skip a part of the test for Hive

--- sql/core/src/test/scala/org/apache/spark/sql/SQLInsertTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLInsertTestSuite.scala
@@ -218,11 +218,15 @@ trait SQLInsertTestSuite extends QueryTest with SQLTestUtils {
       }
       assert(e.getMessage.contains("Found duplicate keys 'c'"))
     }
-    withSQLConf(SQLConf.CASE_SENSITIVE.key -> "true") {
-      withTable("t") {
-        sql(s"CREATE TABLE t(i int, c string, C string) USING PARQUET PARTITIONED BY (c, C)")
-        sql("INSERT OVERWRITE t PARTITION (c='2', C='3') VALUES (1)")
-        checkAnswer(spark.table("t"), Row(1, "2", "3"))
+    // The following code is skipped for Hive because columns stored in Hive Metastore is always
+    // case insensitive and we cannot create such table in Hive Metastore.
+    if (!format.startsWith("hive")) {
+      withSQLConf(SQLConf.CASE_SENSITIVE.key -> "true") {
+        withTable("t") {
+          sql(s"CREATE TABLE t(i int, c string, C string) USING PARQUET PARTITIONED BY (c, C)")
+          sql("INSERT OVERWRITE t PARTITION (c='2', C='3') VALUES (1)")
+          checkAnswer(spark.table("t"), Row(1, "2", "3"))
+        }
       }
     }
   }
