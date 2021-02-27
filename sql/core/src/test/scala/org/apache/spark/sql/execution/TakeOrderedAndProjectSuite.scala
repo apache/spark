05069ff4ce1bbacd88b0b8497c97d8a8ca23d5a7
Ruifeng Zheng <ruifengz@foxmail.com>
2021-02-27 16:48:20 +0900

    [SPARK-34353][SQL] CollectLimitExec avoid shuffle if input rdd has 0/1 partition
    ### What changes were proposed in this pull request?
    if child rdd has only one partition or zero partition, skip the shuffle
    
    ### Why are the changes needed?
    skip shuffle if possible
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    existing testsuites
    
    Closes #31468 from zhengruifeng/collect_limit_single_partition.
    
    Authored-by: Ruifeng Zheng <ruifengz@foxmail.com>
    Signed-off-by: Takeshi Yamamuro <yamamuro@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/TakeOrderedAndProjectSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/TakeOrderedAndProjectSuite.scala
@@ -37,12 +37,18 @@ class TakeOrderedAndProjectSuite extends SparkPlanTest with SharedSparkSession {
     rand = new Random(seed)
   }
 
-  private def generateRandomInputData(): DataFrame = {
+  private def generateRandomInputData(numRows: Int, numParts: Int): DataFrame = {
     val schema = new StructType()
       .add("a", IntegerType, nullable = false)
       .add("b", IntegerType, nullable = false)
-    val inputData = Seq.fill(10000)(Row(rand.nextInt(), rand.nextInt()))
-    spark.createDataFrame(sparkContext.parallelize(Random.shuffle(inputData), 10), schema)
+    val rdd = if (numParts > 0) {
+      val inputData = Seq.fill(numRows)(Row(rand.nextInt(), rand.nextInt()))
+      sparkContext.parallelize(Random.shuffle(inputData), numParts)
+    } else {
+      sparkContext.emptyRDD[Row]
+    }
+    assert(rdd.getNumPartitions == numParts)
+    spark.createDataFrame(rdd, schema)
   }
 
   /**
@@ -56,31 +62,35 @@ class TakeOrderedAndProjectSuite extends SparkPlanTest with SharedSparkSession {
 
   test("TakeOrderedAndProject.doExecute without project") {
     withClue(s"seed = $seed") {
-      checkThatPlansAgree(
-        generateRandomInputData(),
-        input =>
-          noOpFilter(TakeOrderedAndProjectExec(limit, sortOrder, input.output, input)),
-        input =>
-          GlobalLimitExec(limit,
-            LocalLimitExec(limit,
-              SortExec(sortOrder, true, input))),
-        sortAnswers = false)
+      Seq((0, 0), (10000, 1), (10000, 10)).foreach { case (n, m) =>
+        checkThatPlansAgree(
+          generateRandomInputData(n, m),
+          input =>
+            noOpFilter(TakeOrderedAndProjectExec(limit, sortOrder, input.output, input)),
+          input =>
+            GlobalLimitExec(limit,
+              LocalLimitExec(limit,
+                SortExec(sortOrder, true, input))),
+          sortAnswers = false)
+      }
     }
   }
 
   test("TakeOrderedAndProject.doExecute with project") {
     withClue(s"seed = $seed") {
-      checkThatPlansAgree(
-        generateRandomInputData(),
-        input =>
-          noOpFilter(
-            TakeOrderedAndProjectExec(limit, sortOrder, Seq(input.output.last), input)),
-        input =>
-          GlobalLimitExec(limit,
-            LocalLimitExec(limit,
-              ProjectExec(Seq(input.output.last),
-                SortExec(sortOrder, true, input)))),
-        sortAnswers = false)
+      Seq((0, 0), (10000, 1), (10000, 10)).foreach { case (n, m) =>
+        checkThatPlansAgree(
+          generateRandomInputData(n, m),
+          input =>
+            noOpFilter(
+              TakeOrderedAndProjectExec(limit, sortOrder, Seq(input.output.last), input)),
+          input =>
+            GlobalLimitExec(limit,
+              LocalLimitExec(limit,
+                ProjectExec(Seq(input.output.last),
+                  SortExec(sortOrder, true, input)))),
+          sortAnswers = false)
+      }
     }
   }
 }
