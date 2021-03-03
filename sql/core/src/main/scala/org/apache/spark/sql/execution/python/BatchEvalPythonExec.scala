bb312316f9de7739997d74d03dddcdb61bf74ec1
Peter Toth <peter.toth@gmail.com>
2021-03-03 21:27:06 +0100

    clarify cache by reference

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/BatchEvalPythonExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/BatchEvalPythonExec.scala
@@ -55,6 +55,7 @@ case class BatchEvalPythonExec(udfs: Seq[PythonUDF], resultAttrs: Seq[Attribute]
     // `GenericRowWithSchema(Array(1, 1),
     //    StructType(Seq(StructField("_1", IntegerType), StructField("_2", IntegerType))))`
     // to be `equal()` and so we need to disable this feature explicitly (`valueCompare=false`).
+    // Please note that cache by reference is still enabled depending on `needConversion`.
     val pickle = new Pickler(needConversion, false)
     // Input iterator to Python: input rows are grouped so we send them in batches to Python.
     // For each row, add it to the queue.
