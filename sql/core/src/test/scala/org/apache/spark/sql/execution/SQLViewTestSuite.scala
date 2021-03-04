271a2fb7ed093f9725eb89dcecf89cbaf83d34c5
ulysses-you <ulyssesyou18@gmail.com>
2021-03-04 20:46:26 +0800

    value

--- sql/core/src/test/scala/org/apache/spark/sql/execution/SQLViewTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/SQLViewTestSuite.scala
@@ -283,12 +283,13 @@ abstract class SQLViewTestSuite extends QueryTest with SQLTestUtils {
   test("SPARK-34613: Fix view does not capture disable hint config") {
     withSQLConf(DISABLE_HINTS.key -> "true") {
       withView("v1") {
-        createView("v1", "SELECT /*+ repartition(1) */ 1")
+        val viewName = createView("v1", "SELECT /*+ repartition(1) */ 1")
         assert(
           sql("SELECT * FROM v1").queryExecution.analyzed.collect {
             case e: Repartition => e
           }.isEmpty
         )
+        checkViewOutput(viewName, Seq(Row(1)))
       }
     }
   }
