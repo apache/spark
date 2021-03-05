d134803a8b12b3d2ebc9f9ea9ba9e9e1731332c4
Liang-Chi Hsieh <viirya@gmail.com>
2021-03-05 09:22:20 -0800

    update

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/StreamingSessionWindowStateManager.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/StreamingSessionWindowStateManager.scala
@@ -18,9 +18,10 @@
 package org.apache.spark.sql.execution.streaming.state
 
 import org.apache.spark.internal.Logging
-import org.apache.spark.sql.catalyst.expressions.{Attribute, UnsafeRow}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference, JoinedRow, Literal, UnsafeRow}
 import org.apache.spark.sql.catalyst.expressions.codegen.{GenerateUnsafeProjection, GenerateUnsafeRowJoiner}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{StructField, StructType, TimestampType}
 
 /**
  * Base trait for state manager purposed to be used from streaming session window aggregation.
@@ -30,23 +31,20 @@ sealed trait StreamingSessionWindowStateManager extends Serializable {
   /** Extract columns consisting key from input row, and return the new row for key columns. */
   def getKey(row: UnsafeRow): UnsafeRow
 
-  /** Generate a key with session window start time from a non-null key and start time. */
-  def genKeyWithStartTime(row: UnsafeRow, startTime: Long): UnsafeRow
+  /** Returns session window start and end time for input row. */
+  def getSessionTimes(row: UnsafeRow): (Long, Long)
 
-  /** Calculate schema for the value of state. The schema is mainly passed to the StateStoreRDD. */
-  def getStateValueSchema: StructType
-
-  /** Get the list of session window start time for a non-null key from the target state store. */
-  def getSessionWindowStartTimes(store: ReadStateStore, key: UnsafeRow): UnsafeRow
-
-  /** Get the current value of a non-null key from the target state store. */
-  def get(store: ReadStateStore, keyWithStartTime: UnsafeRow): UnsafeRow
+  /**
+   * Returns a list of states for the key. These states are candidates for session window
+   * merging.
+   */
+  def getCandidateStates(key: UnsafeRow, startTime: Long, endTime: Long): Seq[UnsafeRow]
 
   /** Put a new value for a non-null key to the target state store. */
-  def put(store: StateStore, keyWithStartTime: UnsafeRow, value: UnsafeRow): Unit
+  def putSessionWindowStartTimes(key: UnsafeRow, startTimes: UnsafeRow): Unit
 
   /** Put a new value for a non-null key to the target state store. */
-  def putSessionWindowStartTimes(store: StateStore, key: UnsafeRow, startTimes: UnsafeRow): Unit
+  def put(keyWithStartTime: UnsafeRow, value: UnsafeRow): Unit
 
   /**
    * Commit all the updates that have been made to the target state store, and return the
@@ -58,7 +56,21 @@ sealed trait StreamingSessionWindowStateManager extends Serializable {
   def remove(keyWithStartTime: UnsafeRow): Unit
 
   /** Remove a single non-null key from the target state store. */
-  def removeSessionWindowStartTimes(store: StateStore, key: UnsafeRow): Unit
+  def removeSessionWindowStartTimes(key: UnsafeRow): Unit
+}
+
+object StreamingSessionWindowStateManager extends Logging {
+  val supportedVersions = Seq(1)
+
+  def createStateManager(
+      keyExpressions: Seq[Attribute],
+      inputRowAttributes: Seq[Attribute],
+      stateFormatVersion: Int): StreamingSessionWindowStateManager = {
+    stateFormatVersion match {
+      case 1 => new StreamingSessionWindowStateManagerImplV1(keyExpressions, inputRowAttributes)
+      case _ => throw new IllegalArgumentException(s"Version $stateFormatVersion is invalid")
+    }
+  }
 }
 
 abstract class StreamingSessionWindowStateManagerBaseImpl(
@@ -68,9 +80,21 @@ abstract class StreamingSessionWindowStateManagerBaseImpl(
   @transient protected lazy val keyProjector =
     GenerateUnsafeProjection.generate(keyExpressions, inputRowAttributes)
 
+  @transient protected lazy val keyWithStartTimeProjector = {
+    val startTimeAttr = AttributeReference("_startTime", TimestampType)()
+    GenerateUnsafeProjection.generate(keyExpressions ++ Seq(startTimeAttr),
+      inputRowAttributes ++ Seq(startTimeAttr))
+  }
+
   override def getKey(row: UnsafeRow): UnsafeRow = keyProjector(row)
 
   override def genKeyWithStartTime(row: UnsafeRow, startTime: Long): UnsafeRow = {
+    val rightRow = InternalRow(startTime)
+    val joinedRow = new JoinedRow(row, rightRow)
+    keyWithStartTimeProjector(joinedRow)
+  }
+
+  override def getSessionWindowStartTimes(store: ReadStateStore, key: UnsafeRow): Seq[Long] = {
 
   }
 }
@@ -80,4 +104,9 @@ class StreamingSessionWindowStateManagerImplV1(
     inputRowAttributes: Seq[Attribute])
   extends StreamingSessionWindowStateManagerBaseImpl(keyExpressions, inputRowAttributes) {
 
+
+  private val keySchema = StructType.fromAttributes(keyExpressions)
+
+  private val keyToStartTimes = new KeyToStartTimesStore()
+  private val keyWithStartTimeToValue = new KeyWithStartTimeToValueStore()
 }
