126adb89c136df6a90fa9b7a926e2f8fddbdfec7
Swinky <mannswinky@gmail.com>
2021-03-08 00:03:08 +0530

    add UT

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/subquery.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/subquery.scala
@@ -93,64 +93,61 @@ object RewritePredicateSubquery extends Rule[LogicalPlan] with PredicateHelper {
   }
 
   def apply(plan: LogicalPlan): LogicalPlan = plan transform {
-    case Filter(condition, child) =>
+    case Filter(condition, child)
+      if SubqueryExpression.hasInOrCorrelatedExistsSubquery(condition) =>
       val (withSubquery, withoutSubquery) =
         splitConjunctivePredicates(condition)
           .partition(SubqueryExpression.hasInOrCorrelatedExistsSubquery)
 
-      withSubquery match {
-        case withSubquery =>
-          // Construct the pruned filter condition.
-          val newFilter: LogicalPlan = withoutSubquery match {
-            case Nil => child
-            case conditions => Filter(conditions.reduce(And), child)
-          }
+      // Construct the pruned filter condition.
+      val newFilter: LogicalPlan = withoutSubquery match {
+        case Nil => child
+        case conditions => Filter(conditions.reduce(And), child)
+      }
 
-          // Filter the plan by applying left semi and left anti joins.
-          withSubquery.foldLeft(newFilter) {
-            case (p, Exists(sub, conditions, _)) =>
-              val (joinCond, outerPlan) = rewriteExistentialExpr(conditions, p)
-              buildJoin(outerPlan, sub, LeftSemi, joinCond)
-            case (p, Not(Exists(sub, conditions, _))) =>
-              val (joinCond, outerPlan) = rewriteExistentialExpr(conditions, p)
-              buildJoin(outerPlan, sub, LeftAnti, joinCond)
-            case (p, InSubquery(values, ListQuery(sub, conditions, _, _))) =>
-              // Deduplicate conflicting attributes if any.
-              val newSub = dedupSubqueryOnSelfJoin(p, sub, Some(values))
-              val inConditions = values.zip(newSub.output).map(EqualTo.tupled)
-              val (joinCond, outerPlan) = rewriteExistentialExpr(inConditions ++ conditions, p)
-              Join(outerPlan, newSub, LeftSemi, joinCond, JoinHint.NONE)
-            case (p, Not(InSubquery(values, ListQuery(sub, conditions, _, _)))) =>
-              // This is a NULL-aware (left) anti join (NAAJ) e.g. col NOT IN expr
-              // Construct the condition. A NULL in one of the conditions is regarded as a positive
-              // result; such a row will be filtered out by the Anti-Join operator.
+      // Filter the plan by applying left semi and left anti joins.
+      withSubquery.foldLeft(newFilter) {
+        case (p, Exists(sub, conditions, _)) =>
+          val (joinCond, outerPlan) = rewriteExistentialExpr(conditions, p)
+          buildJoin(outerPlan, sub, LeftSemi, joinCond)
+        case (p, Not(Exists(sub, conditions, _))) =>
+          val (joinCond, outerPlan) = rewriteExistentialExpr(conditions, p)
+          buildJoin(outerPlan, sub, LeftAnti, joinCond)
+        case (p, InSubquery(values, ListQuery(sub, conditions, _, _))) =>
+          // Deduplicate conflicting attributes if any.
+          val newSub = dedupSubqueryOnSelfJoin(p, sub, Some(values))
+          val inConditions = values.zip(newSub.output).map(EqualTo.tupled)
+          val (joinCond, outerPlan) = rewriteExistentialExpr(inConditions ++ conditions, p)
+          Join(outerPlan, newSub, LeftSemi, joinCond, JoinHint.NONE)
+        case (p, Not(InSubquery(values, ListQuery(sub, conditions, _, _)))) =>
+          // This is a NULL-aware (left) anti join (NAAJ) e.g. col NOT IN expr
+          // Construct the condition. A NULL in one of the conditions is regarded as a positive
+          // result; such a row will be filtered out by the Anti-Join operator.
 
-              // Note that will almost certainly be planned as a Broadcast Nested Loop join.
-              // Use EXISTS if performance matters to you.
+          // Note that will almost certainly be planned as a Broadcast Nested Loop join.
+          // Use EXISTS if performance matters to you.
 
-              // Deduplicate conflicting attributes if any.
-              val newSub = dedupSubqueryOnSelfJoin(p, sub, Some(values))
-              val inConditions = values.zip(newSub.output).map(EqualTo.tupled)
-              val (joinCond, outerPlan) = rewriteExistentialExpr(inConditions, p)
-              // Expand the NOT IN expression with the NULL-aware semantic
-              // to its full form. That is from:
-              //   (a1,a2,...) = (b1,b2,...)
-              // to
-              //   (a1=b1 OR isnull(a1=b1)) AND (a2=b2 OR isnull(a2=b2)) AND ...
-              val baseJoinConds = splitConjunctivePredicates(joinCond.get)
-              val nullAwareJoinConds = baseJoinConds.map(c => Or(c, IsNull(c)))
-              // After that, add back the correlated join predicate(s) in the subquery
-              // Example:
-              // SELECT ... FROM A
-              // WHERE A.A1 NOT IN (SELECT B.B1 FROM B WHERE B.B2 = A.A2 AND B.B3 > 1)
-              // will have the final conditions in the LEFT ANTI as
-              // (A.A1 = B.B1 OR ISNULL(A.A1 = B.B1)) AND (B.B2 = A.A2) AND B.B3 > 1
-              val finalJoinCond = (nullAwareJoinConds ++ conditions).reduceLeft(And)
-              Join(outerPlan, newSub, LeftAnti, Option(finalJoinCond), JoinHint.NONE)
-            case (p, predicate) =>
-              val (newCond, inputPlan) = rewriteExistentialExpr(Seq(predicate), p)
-              Project(p.output, Filter(newCond.get, inputPlan))
-          }
+          // Deduplicate conflicting attributes if any.
+          val newSub = dedupSubqueryOnSelfJoin(p, sub, Some(values))
+          val inConditions = values.zip(newSub.output).map(EqualTo.tupled)
+          val (joinCond, outerPlan) = rewriteExistentialExpr(inConditions, p)
+          // Expand the NOT IN expression with the NULL-aware semantic
+          // to its full form. That is from:
+          //   (a1,a2,...) = (b1,b2,...)
+          // to
+          //   (a1=b1 OR isnull(a1=b1)) AND (a2=b2 OR isnull(a2=b2)) AND ...
+          val baseJoinConds = splitConjunctivePredicates(joinCond.get)
+          val nullAwareJoinConds = baseJoinConds.map(c => Or(c, IsNull(c)))
+          // After that, add back the correlated join predicate(s) in the subquery
+          // Example:
+          // SELECT ... FROM A WHERE A.A1 NOT IN (SELECT B.B1 FROM B WHERE B.B2 = A.A2 AND B.B3 > 1)
+          // will have the final conditions in the LEFT ANTI as
+          // (A.A1 = B.B1 OR ISNULL(A.A1 = B.B1)) AND (B.B2 = A.A2) AND B.B3 > 1
+          val finalJoinCond = (nullAwareJoinConds ++ conditions).reduceLeft(And)
+          Join(outerPlan, newSub, LeftAnti, Option(finalJoinCond), JoinHint.NONE)
+        case (p, predicate) =>
+          val (newCond, inputPlan) = rewriteExistentialExpr(Seq(predicate), p)
+          Project(p.output, Filter(newCond.get, inputPlan))
       }
   }
 
