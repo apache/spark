7499ac6d137dbe5f78fc8de0feea145a7b66de95
Kousuke Saruta <sarutak@oss.nttdata.com>
2021-03-09 01:28:15 +0900

    Revert an unnecessary change.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/unresolved.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/unresolved.scala
@@ -140,7 +140,8 @@ case class UnresolvedTableValuedFunction(
  */
 case class UnresolvedAttribute(nameParts: Seq[String]) extends Attribute with Unevaluable {
 
-  def name: String = nameParts.map(n => if (n.contains(".")) s"`$n`" else n).mkString(".")
+  def name: String =
+    nameParts.map(n => if (n.contains(".")) s"`$n`" else n).mkString(".")
 
   override def exprId: ExprId = throw new UnresolvedException("exprId")
   override def dataType: DataType = throw new UnresolvedException("dataType")
