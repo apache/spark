0ab8a2b3fc9ee922ad58b68eab7d1dfccad2a9b5
Bo Yang <14280154+hiboyang@users.noreply.github.com>
2021-03-25 15:01:39 -0700

    Revert "Add unit test for spark.shuffle.markFileLostOnExecutorLost"
    This reverts commit 1418e8f9507375f4fec025d8df357a86cb99ee45.

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -2120,11 +2120,9 @@ package object config {
 
   private[spark] val MARK_FILE_LOST_ON_EXECUTOR_LOST =
     ConfigBuilder("spark.shuffle.markFileLostOnExecutorLost")
-      .doc("Mark shuffle files as lost when an executor is lost. If you set " +
-       "spark.shuffle.manager with a customized class to use a different shuffle " +
-       "implementation like storing shuffle files on remote storage/server or using third party " +
-       "remote shuffle service, and you are sure the shuffle files are not stored on the " +
-       "executor, consider setting this to false.")
+      .doc("Mark shuffle files as lost when an executor is lost. If you are using a remote " +
+       "shuffle service such that shuffle files are not stored on the executor, consider " +
+       "setting this to false.")
       .version("3.2.0")
       .booleanConf
       .createWithDefault(true)
--- core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/DAGSchedulerSuite.scala
@@ -2592,24 +2592,6 @@ class DAGSchedulerSuite extends SparkFunSuite with TempLocalSparkContext with Ti
     assertDataStructuresEmpty()
   }
 
-  test("shuffle files should not be lost when setting spark.shuffle.markFileLostOnExecutorLost " +
-    "to false") {
-    conf.set(config.MARK_FILE_LOST_ON_EXECUTOR_LOST.key, "false")
-    assert(sc.env.blockManager.markFileLostOnExecutorLost == false)
-
-    val shuffleMapRdd = new MyRDD(sc, 2, Nil)
-    val shuffleDep = new ShuffleDependency(shuffleMapRdd, new HashPartitioner(1))
-    val shuffleId = shuffleDep.shuffleId
-    val reduceRdd = new MyRDD(sc, 1, List(shuffleDep), tracker = mapOutputTracker)
-    submit(reduceRdd, Array(0))
-    completeShuffleMapStageSuccessfully(0, 0, 1)
-    runEvent(ExecutorLost("hostA-exec", ExecutorKilled))
-    verify(blockManagerMaster, times(1)).removeExecutor("hostA-exec")
-    verify(mapOutputTracker, times(0)).removeOutputsOnExecutor("hostA-exec")
-    assert(mapOutputTracker.getMapSizesByExecutorId(shuffleId, 0).map(_._1).toSet ===
-      HashSet(makeBlockManagerId("hostA"), makeBlockManagerId("hostB")))
-  }
-
   /**
    * Checks the DAGScheduler's internal logic for traversing an RDD DAG by making sure that
    * getShuffleDependenciesAndResourceProfiles correctly returns the direct shuffle dependencies
