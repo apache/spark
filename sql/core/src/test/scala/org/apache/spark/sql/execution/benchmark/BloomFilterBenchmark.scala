e1c6594d83565e18d4f1eac5a1f21bf677d44ae9
yangjie01 <yangjie01@baidu.com>
2021-03-31 00:01:22 +0900

    [SPARK-34900][TEST][3.0] Make sure benchmarks can run using spark-submit cmd described in the guide
    ### What changes were proposed in this pull request?
    Some `spark-submit`  commands used to run benchmarks in the user's guide is wrong, we can't use these commands to run benchmarks successful.
    
    So the major changes of this pr is correct these wrong commands, for example, run a benchmark which inherits from `SqlBasedBenchmark`, we must specify `--jars <spark core test jar>,<spark catalyst test jar>` because `SqlBasedBenchmark` based benchmark extends `BenchmarkBase(defined in spark core test jar)` and `SQLHelper(defined in spark catalyst test jar)`.
    
    Another change of this pr is removed the `scalatest Assertions` dependency of Benchmarks because `scalatest-*.jar` are not in the distribution package, it will be troublesome to use.
    
    ### Why are the changes needed?
    Make sure benchmarks can run using spark-submit cmd described in the guide
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Use the corrected `spark-submit` commands to run benchmarks successfully.
    
    Closes #32003 from LuciferYang/SPARK-34900-30.
    
    Authored-by: yangjie01 <yangjie01@baidu.com>
    Signed-off-by: HyukjinKwon <gurwls223@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/BloomFilterBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/BloomFilterBenchmark.scala
@@ -29,7 +29,8 @@ import org.apache.spark.benchmark.Benchmark
  *
  * To run this benchmark:
  * {{{
- *   1. without sbt: bin/spark-submit --class <this class> <spark sql test jar>
+ *   1. without sbt: bin/spark-submit --class <this class>
+ *     --jars <spark core test jar>,<spark catalyst test jar> <spark sql test jar>
  *   2. build/sbt "sql/test:runMain <this class>"
  *   3. generate result: SPARK_GENERATE_BENCHMARK_FILES=1 build/sbt "sql/test:runMain <this class>"
  *      Results will be written to "benchmarks/BloomFilterBenchmark-results.txt".
