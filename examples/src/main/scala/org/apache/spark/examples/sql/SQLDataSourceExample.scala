f71f12c56b71b5529dea190ddd08950e86388936
twoentartian <twoentartian@hotmail.com>
2021-04-03 01:53:56 +0200

    Remove unused Scala imports.

--- examples/src/main/scala/org/apache/spark/examples/sql/SQLDataSourceExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/sql/SQLDataSourceExample.scala
@@ -249,11 +249,6 @@ object SQLDataSourceExample {
   }
 
   private def runCsvDatasetExample(spark: SparkSession): Unit = {
-    // $example on:csv_dataset$
-    // Primitive types (Int, String, etc) and Product types (case classes) encoders are
-    // supported by importing this when creating a Dataset.
-    import spark.implicits._
-
     // A CSV dataset is pointed to by path.
     // The path can be either a single CSV file or a directory of CSV files
     val path = "examples/src/main/resources/people.csv"
