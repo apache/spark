5463920e284eca5972df61bafb0d53f9f08e57ba
Angerszhuuuu <angers.zhu@gmail.com>
2021-04-07 15:20:42 +0800

    Update grouping.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/grouping.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/grouping.scala
@@ -32,8 +32,6 @@ trait GroupingSet extends Expression with CodegenFallback {
   def selectedGroupByExprs: Seq[Seq[Expression]]
 
   def groupByExprs: Seq[Expression] = {
-    assert(children.forall(_.resolved),
-      "Cannot call GroupingSet.groupByExprs before the children expressions are all resolved.")
     children.foldLeft(Seq.empty[Expression]) { (result, currentExpr) =>
       // Only unique expressions are included in the group by expressions and is determined
       // based on their semantic equality. Example. grouping sets ((a * b), (b * a)) results
