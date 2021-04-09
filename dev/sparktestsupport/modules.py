83a9a6a50462100b7d739269506c0403cf0c6f7c
Xinrong Meng <xinrong.meng@databricks.com>
2021-04-09 14:04:14 -0700

    Add to pyspark-pandas module

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -612,6 +612,9 @@ pyspark_pandas = Module(
         "pyspark.pandas.typedef.typehints",
         # unittests
         "pyspark.pandas.tests.test_dataframe",
+        "pyspark.pandas.tests.test_dataframe_conversion",
+        "pyspark.pandas.tests.test_dataframe_spark_io",
+        "pyspark.pandas.tests.test_frame_spark",
     ],
     excluded_python_implementations=[
         "PyPy"  # Skip these tests under PyPy since they require numpy, pandas, and pyarrow and
