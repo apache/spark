0a3d717723f03752ed106b7837455773b987a543
HyukjinKwon <gurwls223@apache.org>
2021-04-09 10:08:15 +0900

    Set SPARK_LOCAL_IP

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -2358,7 +2358,6 @@ private[spark] object Utils extends Logging {
             logWarning(s"Service$serviceString could not bind on port $tryPort. " +
               s"Attempting port ${tryPort + 1}.")
           }
-          if (sys.env.contains("GITHUB_ACTIONS")) Thread.sleep(1)
       }
     }
     // Should never happen
