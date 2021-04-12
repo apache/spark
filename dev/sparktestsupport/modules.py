be499d5f1ad6caf36784d373b98d487bb6931485
Xinrong Meng <xinrong.meng@databricks.com>
2021-04-12 14:45:57 -0700

    Add test_ops_on_diff_frames to module

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -612,6 +612,7 @@ pyspark_pandas = Module(
         "pyspark.pandas.typedef.typehints",
         # unittests
         "pyspark.pandas.tests.test_dataframe",
+        "pyspark.pandas.tests.test_ops_on_diff_frames",
         "pyspark.pandas.tests.test_ops_on_diff_frames_groupby_rolling",
         "pyspark.pandas.tests.test_ops_on_diff_frames_groupby_expanding",
         "pyspark.pandas.tests.test_ops_on_diff_frames_groupby",
