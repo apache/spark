34b8273141020a5c617e9419dc9263f514809f73
Xinrong Meng <xinrong.meng@databricks.com>
2021-04-12 09:27:39 -0700

    Add to pyspark_pandas module

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -612,6 +612,9 @@ pyspark_pandas = Module(
         "pyspark.pandas.typedef.typehints",
         # unittests
         "pyspark.pandas.tests.test_dataframe",
+        "pyspark.pandas.tests.test_ops_on_diff_frames_groupby_rolling",
+        "pyspark.pandas.tests.test_ops_on_diff_frames_groupby_expanding",
+        "pyspark.pandas.tests.test_ops_on_diff_frames_groupby",
     ],
     excluded_python_implementations=[
         "PyPy"  # Skip these tests under PyPy since they require numpy, pandas, and pyarrow and
