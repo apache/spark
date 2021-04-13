6f5b0ee509e1e3134274eee2db1e88fced38c788
gengjiaan <gengjiaan@360.cn>
2021-04-13 10:07:56 +0800

    Update code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
@@ -40,7 +40,6 @@ import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{DataType, Decimal, StructType}
 import org.apache.spark.unsafe.array.ByteArrayMethods
 import org.apache.spark.unsafe.types.UTF8String
-import org.apache.spark.util.AccumulatorV2
 
 /**
  * Object for grouping error messages from (most) exceptions thrown during query execution.
