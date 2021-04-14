f4a82665691f399bc192a48891c14de11bb5f977
Yingyi Bu <yingyi.bu@databricks.com>
2021-04-13 22:26:25 -0700

    Fix the requirement in BitSet.union

--- core/src/main/scala/org/apache/spark/util/collection/BitSet.scala
+++ core/src/main/scala/org/apache/spark/util/collection/BitSet.scala
@@ -240,7 +240,7 @@ class BitSet(numBits: Int) extends Serializable {
    * Compute bit-wise union with another BitSet and overwrite bits in this BitSet with the result.
    */
   def union(other: BitSet): Unit = {
-    require(this.numWords <= other.numWords)
+    require(this.numWords >= other.numWords)
     var ind = 0
     while( ind < this.numWords ) {
       this.words(ind) = this.words(ind) | other.words(ind)
