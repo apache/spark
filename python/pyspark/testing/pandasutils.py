2eb6c87886ce126e2973ec7fe0554fe7fb1a3b1c
Xinrong Meng <xinrong.meng@databricks.com>
2021-04-15 11:05:22 -0700

    Format

--- python/pyspark/testing/pandasutils.py
+++ python/pyspark/testing/pandasutils.py
@@ -6,7 +6,7 @@
 # (the "License"); you may not use this file except in compliance with
 # the License.  You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -344,7 +344,7 @@ def assert_produces_warning(
                 saw_warning = True
 
                 if check_stacklevel and issubclass(
-                        actual_warning.category, (FutureWarning, DeprecationWarning)
+                    actual_warning.category, (FutureWarning, DeprecationWarning)
                 ):
                     from inspect import getframeinfo, stack
 
