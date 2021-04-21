43ad939a7e96744038b293b17682b2d86be2bcb9
Gengliang Wang <ltnwgl@gmail.com>
2021-04-21 16:08:20 +0800

    [SPARK-35152][SQL] ANSI mode: IntegralDivide throws exception on overflow
    ### What changes were proposed in this pull request?
    
    IntegralDivide should throw an exception on overflow in ANSI mode.
    There is only one case that can cause that:
    ```
    Long.MinValue div -1
    ```
    
    ### Why are the changes needed?
    
    ANSI compliance
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, IntegralDivide throws an exception on overflow in ANSI mode
    
    ### How was this patch tested?
    
    Unit test
    
    Closes #32260 from gengliangwang/integralDiv.
    
    Authored-by: Gengliang Wang <ltnwgl@gmail.com>
    Signed-off-by: Gengliang Wang <ltnwgl@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/arithmetic.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/arithmetic.scala
@@ -404,6 +404,9 @@ trait DivModLike extends BinaryArithmetic {
 
   protected def decimalToDataTypeCodeGen(decimalResult: String): String = decimalResult
 
+  // Whether we should check overflow or not in ANSI mode.
+  protected def checkDivideOverflow: Boolean = false
+
   override def nullable: Boolean = true
 
   private lazy val isZero: Any => Boolean = right.dataType match {
@@ -424,6 +427,9 @@ trait DivModLike extends BinaryArithmetic {
         if (isZero(input2)) {
           // when we reach here, failOnError must bet true.
           throw QueryExecutionErrors.divideByZeroError
+        }
+        if (checkDivideOverflow && input1 == Long.MinValue && input2 == -1) {
+          throw QueryExecutionErrors.overflowInIntegralDivideError()
         }
         evalOperation(input1, input2)
       }
@@ -450,6 +456,15 @@ trait DivModLike extends BinaryArithmetic {
     } else {
       s"($javaType)(${eval1.value} $symbol ${eval2.value})"
     }
+    val checkIntegralDivideOverflow = if (checkDivideOverflow) {
+      s"""
+        |if (${eval1.value} == ${Long.MinValue}L && ${eval2.value} == -1)
+        |  throw QueryExecutionErrors.overflowInIntegralDivideError();
+        |""".stripMargin
+    } else {
+      ""
+    }
+
     // evaluate right first as we have a chance to skip left if right is 0
     if (!left.nullable && !right.nullable) {
       val divByZero = if (failOnError) {
@@ -465,6 +480,7 @@ trait DivModLike extends BinaryArithmetic {
           $divByZero
         } else {
           ${eval1.code}
+          $checkIntegralDivideOverflow
           ${ev.value} = $operation;
         }""")
     } else {
@@ -486,6 +502,7 @@ trait DivModLike extends BinaryArithmetic {
             ${ev.isNull} = true;
           } else {
             $failOnErrorBranch
+            $checkIntegralDivideOverflow
             ${ev.value} = $operation;
           }
         }""")
@@ -545,6 +562,11 @@ case class IntegralDivide(
     failOnError: Boolean = SQLConf.get.ansiEnabled) extends DivModLike {
 
   def this(left: Expression, right: Expression) = this(left, right, SQLConf.get.ansiEnabled)
+
+  override def checkDivideOverflow: Boolean = left.dataType match {
+    case LongType if failOnError => true
+    case _ => false
+  }
 
   override def inputType: AbstractDataType = TypeCollection(LongType, DecimalType)
 
