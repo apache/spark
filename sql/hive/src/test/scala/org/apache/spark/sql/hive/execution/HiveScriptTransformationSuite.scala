6f782efb044403ab3ca79662fdeca7f1f906e1bf
Angerszhuuuu <angers.zhu@gmail.com>
2021-04-26 11:26:32 +0900

    [SPARK-35220][SQL] DayTimeIntervalType/YearMonthIntervalType show different between Hive SerDe and row format delimited
    ### What changes were proposed in this pull request?
    DayTimeIntervalType/YearMonthIntervalString show different between Hive SerDe and row format delimited.
    Create this pr to add a test and  have disscuss.
    
    For this problem I think we have two direction:
    
    1. leave it as current and add a item t explain this  in migration guide docs.
    2. Since we should not change hive serde's behavior, so we can cast spark row format delimited's behavior to use cast  DayTimeIntervalType/YearMonthIntervalType as HIVE_STYLE
    
    ### Why are the changes needed?
    Add UT
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    added ut
    
    Closes #32335 from AngersZhuuuu/SPARK-35220.
    
    Authored-by: Angerszhuuuu <angers.zhu@gmail.com>
    Signed-off-by: hyukjinkwon <gurwls223@apache.org>

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveScriptTransformationSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveScriptTransformationSuite.scala
@@ -39,6 +39,8 @@ class HiveScriptTransformationSuite extends BaseScriptTransformationSuite with T
 
   import ScriptTransformationIOSchema._
 
+  override protected def defaultSerDe(): String = "hive-serde"
+
   override def createScriptTransformationExec(
       script: String,
       output: Seq[Attribute],
