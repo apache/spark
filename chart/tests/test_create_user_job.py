18531f81848dbd8d8a0d25b9f26988500a27e2a7
Ephraim Anierobi <splendidzigy24@gmail.com>
2021-04-30 20:37:43 +0100

    Add test for the create user job in helm chart (#15614)
    This change adds test for the create user job and ensures changing of the uid is detected
    
    Co-authored-by: Kaxil Naik <kaxilnaik@gmail.com>

---
+++ chart/tests/test_create_user_job.py
@@ -0,0 +1,29 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import unittest
+
+import jmespath
+
+from chart.tests.helm_template_generator import render_chart
+
+
+class CreateUserJobTest(unittest.TestCase):
+    def test_should_run_by_default(self):
+        docs = render_chart(show_only=["templates/create-user-job.yaml"])
+        assert "create-user" == jmespath.search("spec.template.spec.containers[0].name", docs[0])
+        assert 50000 == jmespath.search("spec.template.spec.securityContext.runAsUser", docs[0])
