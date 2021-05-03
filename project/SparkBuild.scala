68b5425311e0c645ee7144f82c14185c9f779b20
Ludovic Henry <git@ludovic.dev>
2021-05-03 13:40:21 +0200

    Bump to dev.ludovic.netlib:2.0.0

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -272,8 +272,6 @@ object SparkBuild extends PomBuild {
       // See https://storage-download.googleapis.com/maven-central/index.html for more info.
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
       DefaultMavenRepository,
-      // /!\ DO NOT MERGE !!! /!\
-      "sonatype-oss-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
       Resolver.mavenLocal,
       Resolver.file("ivyLocal", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     ),
