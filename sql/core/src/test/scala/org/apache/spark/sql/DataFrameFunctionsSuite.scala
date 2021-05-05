6df4ec09a17077c2a0b114a7bf5736711ba268e4
dsolow <dsolow@sayari.com>
2021-05-05 12:46:13 +0900

    [SPARK-34794][SQL] Fix lambda variable name issues in nested DataFrame functions
    ### What changes were proposed in this pull request?
    
    To fix lambda variable name issues in nested DataFrame functions, this PR modifies code to use a global counter for `LambdaVariables` names created by higher order functions.
    
    This is the rework of #31887. Closes #31887.
    
    ### Why are the changes needed?
    
     This moves away from the current hard-coded variable names which break on nested function calls. There is currently a bug where nested transforms in particular fail (the inner variable shadows the outer variable)
    
    For this query:
    ```
    val df = Seq(
        (Seq(1,2,3), Seq("a", "b", "c"))
    ).toDF("numbers", "letters")
    
    df.select(
        f.flatten(
            f.transform(
                $"numbers",
                (number: Column) => { f.transform(
                    $"letters",
                    (letter: Column) => { f.struct(
                        number.as("number"),
                        letter.as("letter")
                    ) }
                ) }
            )
        ).as("zipped")
    ).show(10, false)
    ```
    This is the current (incorrect) output:
    ```
    +------------------------------------------------------------------------+
    |zipped                                                                  |
    +------------------------------------------------------------------------+
    |[{a, a}, {b, b}, {c, c}, {a, a}, {b, b}, {c, c}, {a, a}, {b, b}, {c, c}]|
    +------------------------------------------------------------------------+
    ```
    And this is the correct output after fix:
    ```
    +------------------------------------------------------------------------+
    |zipped                                                                  |
    +------------------------------------------------------------------------+
    |[{1, a}, {1, b}, {1, c}, {2, a}, {2, b}, {2, c}, {3, a}, {3, b}, {3, c}]|
    +------------------------------------------------------------------------+
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Added the new test in `DataFrameFunctionsSuite`.
    
    Closes #32424 from maropu/pr31887.
    
    Lead-authored-by: dsolow <dsolow@sayari.com>
    Co-authored-by: Takeshi Yamamuro <yamamuro@apache.org>
    Co-authored-by: dmsolow <dsolow@sayarianalytics.com>
    Signed-off-by: Takeshi Yamamuro <yamamuro@apache.org>
    (cherry picked from commit f550e03b96638de93381734c4eada2ace02d9a4f)
    Signed-off-by: Takeshi Yamamuro <yamamuro@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameFunctionsSuite.scala
@@ -3629,6 +3629,29 @@ class DataFrameFunctionsSuite extends QueryTest with SharedSparkSession {
       df.select(map(map_entries($"m"), lit(1))),
       Row(Map(Seq(Row(1, "a")) -> 1)))
   }
+
+  test("SPARK-34794: lambda variable name issues in nested functions") {
+    val df1 = Seq((Seq(1, 2), Seq("a", "b"))).toDF("numbers", "letters")
+
+    checkAnswer(df1.select(flatten(transform($"numbers", (number: Column) =>
+      transform($"letters", (letter: Column) =>
+        struct(number, letter))))),
+      Seq(Row(Seq(Row(1, "a"), Row(1, "b"), Row(2, "a"), Row(2, "b"))))
+    )
+    checkAnswer(df1.select(flatten(transform($"numbers", (number: Column, i: Column) =>
+      transform($"letters", (letter: Column, j: Column) =>
+        struct(number + j, concat(letter, i)))))),
+      Seq(Row(Seq(Row(1, "a0"), Row(2, "b0"), Row(2, "a1"), Row(3, "b1"))))
+    )
+
+    val df2 = Seq((Map("a" -> 1, "b" -> 2), Map("a" -> 2, "b" -> 3))).toDF("m1", "m2")
+
+    checkAnswer(df2.select(map_zip_with($"m1", $"m2", (k1: Column, ov1: Column, ov2: Column) =>
+      map_zip_with($"m1", $"m2", (k2: Column, iv1: Column, iv2: Column) =>
+        ov1 + iv1 + ov2 + iv2))),
+      Seq(Row(Map("a" -> Map("a" -> 6, "b" -> 8), "b" -> Map("a" -> 8, "b" -> 10))))
+    )
+  }
 }
 
 object DataFrameFunctionsSuite {
