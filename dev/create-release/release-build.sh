2d275891147341ef233ac2082e973a0e98660832
Liang-Chi Hsieh <viirya@gmail.com>
2021-05-09 14:29:46 -0700

    Set jvm memory for release build.

--- dev/create-release/release-build.sh
+++ dev/create-release/release-build.sh
@@ -210,6 +210,8 @@ if [[ "$1" == "package" ]]; then
     PYSPARK_VERSION=`echo "$SPARK_VERSION" |  sed -e "s/-/./" -e "s/SNAPSHOT/dev0/" -e "s/preview/dev/"`
     echo "__version__='$PYSPARK_VERSION'" > python/pyspark/version.py
 
+    export MAVEN_OPTS="-Xmx12000m"
+
     # Get maven home set by MVN
     MVN_HOME=`$MVN -version 2>&1 | grep 'Maven home' | awk '{print $NF}'`
 
@@ -365,6 +367,9 @@ if [[ "$1" == "publish-snapshot" ]]; then
     echo "ERROR: You gave version '$SPARK_VERSION'"
     exit 1
   fi
+
+  export MAVEN_OPTS="-Xmx12000m"
+
   # Coerce the requested version
   $MVN versions:set -DnewVersion=$SPARK_VERSION
   tmp_settings="tmp-settings.xml"
@@ -390,6 +395,9 @@ if [[ "$1" == "publish-release" ]]; then
   # Publish Spark to Maven release repo
   echo "Publishing Spark checkout at '$GIT_REF' ($git_hash)"
   echo "Publish version is $SPARK_VERSION"
+
+  export MAVEN_OPTS="-Xmx12000m"
+
   # Coerce the requested version
   $MVN versions:set -DnewVersion=$SPARK_VERSION
 
