45b7fd4abfe0bacf3db02ec1c905d0f763a5093c
Erik Krogen <xkrogen@apache.org>
2021-05-11 12:41:26 -0700

    fix test failure in YarnShuffleServiceMetricsSuite

--- resource-managers/yarn/src/test/scala/org/apache/spark/network/yarn/YarnShuffleServiceMetricsSuite.scala
+++ resource-managers/yarn/src/test/scala/org/apache/spark/network/yarn/YarnShuffleServiceMetricsSuite.scala
@@ -85,7 +85,8 @@ class YarnShuffleServiceMetricsSuite extends SparkFunSuite with Matchers {
             Seq("rate1", "rate5", "rate15", "rateMean", "nanos_mean", "nanos_stdDev",
               "nanos_1stPercentile", "nanos_5thPercentile", "nanos_25thPercentile",
               "nanos_50thPercentile", "nanos_75thPercentile", "nanos_95thPercentile",
-              "nanos_99thPercentile").map(suffix => s"${testname}_$suffix")
+              "nanos_98thPercentile", "nanos_99thPercentile", "nanos_99.9thPercentile")
+                .map(suffix => s"${testname}_$suffix")
         )
       }
       assert(gaugeLongNames.sorted === expectLong.sorted)
