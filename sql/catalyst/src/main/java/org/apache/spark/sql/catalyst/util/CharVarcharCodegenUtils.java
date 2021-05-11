2168c3cbb4c6b5c2e5256571ebf67969d28bc226
fhygh <283452027@qq.com>
2021-05-11 16:46:12 +0800

    [SPARK-35359][SQL]Insert data with char/varchar datatype will fail when data length exceed length limitation

--- sql/catalyst/src/main/java/org/apache/spark/sql/catalyst/util/CharVarcharCodegenUtils.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/catalyst/util/CharVarcharCodegenUtils.java
@@ -17,6 +17,7 @@
 
 package org.apache.spark.sql.catalyst.util;
 
+import org.apache.spark.sql.internal.SQLConf;
 import org.apache.spark.unsafe.types.UTF8String;
 
 public class CharVarcharCodegenUtils {
@@ -26,7 +27,7 @@ public class CharVarcharCodegenUtils {
       UTF8String inputStr, int numChars, int limit) {
     int numTailSpacesToTrim = numChars - limit;
     UTF8String trimmed = inputStr.trimTrailingSpaces(numTailSpacesToTrim);
-    if (trimmed.numChars() > limit) {
+    if (trimmed.numChars() > limit && !SQLConf.get().charVarcharAsString()) {
       throw new RuntimeException("Exceeds char/varchar type length limitation: " + limit);
     } else {
       return trimmed;
