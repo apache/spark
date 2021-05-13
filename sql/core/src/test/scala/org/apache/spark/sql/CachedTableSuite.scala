2e8492baf2c9b6dba3bd635f2b3a25d288e7f3ab
ulysses-you <ulyssesyou18@gmail.com>
2021-05-13 17:04:00 +0800

    comment

--- sql/core/src/test/scala/org/apache/spark/sql/CachedTableSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CachedTableSuite.scala
@@ -1564,19 +1564,19 @@ class CachedTableSuite extends QueryTest with SQLTestUtils
         withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "false") {
           sql("CACHE TABLE t1 as SELECT /*+ REPARTITION */ * FROM values(1) as t(c)")
           assert(spark.table("t1").rdd.partitions.length == 2)
+        }
 
-          withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "true") {
-            assert(spark.table("t1").rdd.partitions.length == 2)
-            sql("CACHE TABLE t2 as SELECT /*+ REPARTITION */ * FROM values(2) as t(c)")
-            assert(spark.table("t2").rdd.partitions.length == 1)
+        withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "true") {
+          assert(spark.table("t1").rdd.partitions.length == 2)
+          sql("CACHE TABLE t2 as SELECT /*+ REPARTITION */ * FROM values(2) as t(c)")
+          assert(spark.table("t2").rdd.partitions.length == 1)
+        }
 
-            withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "false") {
-              assert(spark.table("t1").rdd.partitions.length == 2)
-              assert(spark.table("t2").rdd.partitions.length == 1)
-              sql("CACHE TABLE t3 as SELECT /*+ REPARTITION */ * FROM values(3) as t(c)")
-              assert(spark.table("t3").rdd.partitions.length == 2)
-            }
-          }
+        withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "false") {
+          assert(spark.table("t1").rdd.partitions.length == 2)
+          assert(spark.table("t2").rdd.partitions.length == 1)
+          sql("CACHE TABLE t3 as SELECT /*+ REPARTITION */ * FROM values(3) as t(c)")
+          assert(spark.table("t3").rdd.partitions.length == 2)
         }
       }
     }
@@ -1603,13 +1603,13 @@ class CachedTableSuite extends QueryTest with SQLTestUtils
             assert(spark.table("t1").rdd.partitions.length == 2)
             sql("CACHE TABLE t2")
             assert(spark.table("t2").rdd.partitions.length == 1)
+          }
 
-            withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "false") {
-              assert(spark.table("t1").rdd.partitions.length == 2)
-              assert(spark.table("t2").rdd.partitions.length == 1)
-              sql("CACHE TABLE t3")
-              assert(spark.table("t3").rdd.partitions.length == 2)
-            }
+          withSQLConf(SQLConf.CAN_CHANGE_CACHED_PLAN_OUTPUT_PARTITIONING.key -> "false") {
+            assert(spark.table("t1").rdd.partitions.length == 2)
+            assert(spark.table("t2").rdd.partitions.length == 1)
+            sql("CACHE TABLE t3")
+            assert(spark.table("t3").rdd.partitions.length == 2)
           }
         }
       }
