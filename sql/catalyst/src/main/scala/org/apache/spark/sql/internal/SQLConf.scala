648cc58ef9eb0ee3a2f2d6e2939d726e94be4bac
ulysses-you <ulyssesyou18@gmail.com>
2021-05-18 09:20:18 +0800

    docs

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -602,8 +602,8 @@ object SQLConf {
     buildConf("spark.sql.adaptive.shuffledHashJoinLocalMapThreshold")
       .doc("Configures the maximum size in bytes for per partition that can be allowed to build " +
         "local hash map. If all the partition size not larger than this threshold, join " +
-        "selection may use shuffled hash join instead of sort merge join when " +
-        s"${PREFER_SORTMERGEJOIN.key} is false.")
+        "selection may use shuffled hash join instead of sort merge join regardless of the " +
+        s"value of ${PREFER_SORTMERGEJOIN.key}.")
       .version("3.2.0")
       .bytesConf(ByteUnit.BYTE)
       .createWithDefaultString("-1")
