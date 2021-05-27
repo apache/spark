5fbc86f0364fbc4f7c2ba10cae4a13449709730c
Isaacwhyuenac <13378284+Isaacwhyuenac@users.noreply.github.com>
2021-05-27 18:28:47 +0900

    Remove the `not-allow-trailing-slash` rule on S3_hook (#15609)

--- airflow/providers/amazon/aws/hooks/s3.py
+++ airflow/providers/amazon/aws/hooks/s3.py
@@ -143,7 +143,7 @@ class S3Hook(AwsBaseHook):
             raise AirflowException(f'Please provide a bucket_name instead of "{s3url}"')
 
         bucket_name = parsed_url.netloc
-        key = parsed_url.path.strip('/')
+        key = parsed_url.path.lstrip('/')
 
         return bucket_name, key
 
--- tests/providers/amazon/aws/hooks/test_s3.py
+++ tests/providers/amazon/aws/hooks/test_s3.py
@@ -69,6 +69,10 @@ class TestAwsS3Hook:
         parsed = S3Hook.parse_s3_url("s3://test/this/is/not/a-real-key.txt")
         assert parsed == ("test", "this/is/not/a-real-key.txt"), "Incorrect parsing of the s3 url"
 
+    def test_parse_s3_object_directory(self):
+        parsed = S3Hook.parse_s3_url("s3://test/this/is/not/a-real-s3-directory/")
+        assert parsed == ("test", "this/is/not/a-real-s3-directory/"), "Incorrect parsing of the s3 url"
+
     def test_check_for_bucket(self, s3_bucket):
         hook = S3Hook()
         assert hook.check_for_bucket(s3_bucket) is True
