4ea78f89eff4d607c4d6b2512852009b8205ba57
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-05-27 16:35:54 -0600

    Chart: Add extra ini config to ``pgbouncer`` (#16120)
    This allows users to add extra ini config to pgbouncer, for example to
    adjust how frequently pgbouncer stats are output or reserve_pool size.

--- chart/tests/test_pgbouncer.py
+++ chart/tests/test_pgbouncer.py
@@ -227,7 +227,13 @@ class PgbouncerConfigTest(unittest.TestCase):
 
     def test_databases_override(self):
         values = {
-            "pgbouncer": {"enabled": True, "metadataPoolSize": 12, "resultBackendPoolSize": 7},
+            "pgbouncer": {
+                "enabled": True,
+                "metadataPoolSize": 12,
+                "resultBackendPoolSize": 7,
+                "extraIniMetadata": "reserve_pool = 5",
+                "extraIniResultBackend": "reserve_pool = 3",
+            },
             "data": {
                 "metadataConnection": {"host": "meta_host", "db": "meta_db", "port": 1111},
                 "resultBackendConnection": {
@@ -243,8 +249,14 @@ class PgbouncerConfigTest(unittest.TestCase):
         }
         ini = self._get_pgbouncer_ini(values)
 
-        assert "RELEASE-NAME-metadata = host=meta_host dbname=meta_db port=1111 pool_size=12" in ini
-        assert "RELEASE-NAME-result-backend = host=rb_host dbname=rb_db port=2222 pool_size=7" in ini
+        assert (
+            "RELEASE-NAME-metadata = host=meta_host dbname=meta_db port=1111 pool_size=12 reserve_pool = 5"
+            in ini
+        )
+        assert (
+            "RELEASE-NAME-result-backend = host=rb_host dbname=rb_db port=2222 pool_size=7 reserve_pool = 3"
+            in ini
+        )
 
     def test_config_defaults(self):
         ini = self._get_pgbouncer_ini({"pgbouncer": {"enabled": True}})
@@ -314,6 +326,13 @@ class PgbouncerConfigTest(unittest.TestCase):
             encoded = jmespath.search(f'data."{key}"', docs[0])
             value = base64.b64decode(encoded).decode()
             assert expected == value
+
+    def test_extra_ini_configs(self):
+        values = {"pgbouncer": {"enabled": True, "extraIni": "server_round_robin = 1\nstats_period = 30"}}
+        ini = self._get_pgbouncer_ini(values)
+
+        assert "server_round_robin = 1" in ini
+        assert "stats_period = 30" in ini
 
 
 class PgbouncerExporterTest(unittest.TestCase):
