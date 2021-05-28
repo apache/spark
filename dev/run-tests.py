2de19e460bb3b305d4d155709fb88ad97efd6142
Kousuke Saruta <sarutak@oss.nttdata.com>
2021-05-28 16:54:47 +0900

    [SPARK-35483][INFRA] Add docker-integration-tests to run-tests.py and GA
    ### What changes were proposed in this pull request?
    
    This PR proposes to add `docker-integratin-tests` to `run-tests.py` and GA.
    Once #32631 was merged but there was a lack of consideration.
    
    Diff between this change and https://github.com/apache/spark/pull/32631/commits/692d95d1458993cbb9cbd47014202e84cd6aa328 merged in #32631 is as follows.
    
    ```
           if: github.repository != 'apache/spark'
           id: sync-branch
           run: |
    +        apache_spark_ref=`git rev-parse HEAD`
             git fetch https://github.com/$GITHUB_REPOSITORY.git ${GITHUB_REF#refs/heads/}
             git -c user.name='Apache Spark Test Account' -c user.email='sparktestaccgmail.com' merge --no-commit --progress --squash FETCH_HEAD
             git -c user.name='Apache Spark Test Account' -c user.email='sparktestaccgmail.com' commit -m "Merged commit"
    +        echo "::set-output name=APACHE_SPARK_REF::$apache_spark_ref"
         - name: Cache Scala, SBT and Maven
           uses: actions/cachev2
           with:
    ```
    
    ### Why are the changes needed?
    
    CI for `docker-integration-tests` is absent for now.
    
    ### Does this PR introduce _any_ user-facing change?
    
    GA.
    
    ### How was this patch tested?
    
    Closes #32691 from sarutak/docker-integration-test-ga-take2.
    
    Authored-by: Kousuke Saruta <sarutak@oss.nttdata.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -122,19 +122,21 @@ def determine_modules_to_test(changed_modules, deduplicated=True):
     ['graphx', 'examples']
     >>> [x.name for x in determine_modules_to_test([modules.sql])]
     ... # doctest: +NORMALIZE_WHITESPACE
-    ['sql', 'avro', 'hive', 'mllib', 'sql-kafka-0-10', 'examples', 'hive-thriftserver',
-     'pyspark-sql', 'repl', 'sparkr', 'pyspark-mllib', 'pyspark-pandas', 'pyspark-ml']
+    ['sql', 'avro', 'hive', 'mllib', 'sql-kafka-0-10', 'examples',
+     'hive-thriftserver', 'pyspark-sql', 'repl', 'sparkr',
+     'pyspark-mllib', 'pyspark-pandas', 'pyspark-ml']
     >>> sorted([x.name for x in determine_modules_to_test(
     ...     [modules.sparkr, modules.sql], deduplicated=False)])
     ... # doctest: +NORMALIZE_WHITESPACE
-    ['avro', 'examples', 'hive', 'hive-thriftserver', 'mllib', 'pyspark-ml',
-     'pyspark-mllib', 'pyspark-pandas', 'pyspark-sql', 'repl', 'sparkr', 'sql', 'sql-kafka-0-10']
+    ['avro', 'examples', 'hive', 'hive-thriftserver', 'mllib',
+     'pyspark-ml', 'pyspark-mllib', 'pyspark-pandas', 'pyspark-sql',
+     'repl', 'sparkr', 'sql', 'sql-kafka-0-10']
     >>> sorted([x.name for x in determine_modules_to_test(
     ...     [modules.sql, modules.core], deduplicated=False)])
     ... # doctest: +NORMALIZE_WHITESPACE
-    ['avro', 'catalyst', 'core', 'examples', 'graphx', 'hive', 'hive-thriftserver',
-     'mllib', 'mllib-local', 'pyspark-core', 'pyspark-ml', 'pyspark-mllib', 'pyspark-pandas',
-     'pyspark-resource', 'pyspark-sql', 'pyspark-streaming', 'repl', 'root',
+    ['avro', 'catalyst', 'core', 'examples', 'graphx', 'hive',
+     'hive-thriftserver', 'mllib', 'mllib-local', 'pyspark-core', 'pyspark-ml', 'pyspark-mllib',
+     'pyspark-pandas', 'pyspark-resource', 'pyspark-sql', 'pyspark-streaming', 'repl', 'root',
      'sparkr', 'sql', 'sql-kafka-0-10', 'streaming', 'streaming-kafka-0-10',
      'streaming-kinesis-asl']
     """
