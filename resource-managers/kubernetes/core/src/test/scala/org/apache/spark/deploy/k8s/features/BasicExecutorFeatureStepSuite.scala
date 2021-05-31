8fbc385ec56134846f1dc16c2bd4d03987bcb6f7
Binjie Yang <2213335496@qq.com>
2021-05-31 16:42:23 +0800

    add unit test case to test blockmanager port not be 0

--- resource-managers/kubernetes/core/src/test/scala/org/apache/spark/deploy/k8s/features/BasicExecutorFeatureStepSuite.scala
+++ resource-managers/kubernetes/core/src/test/scala/org/apache/spark/deploy/k8s/features/BasicExecutorFeatureStepSuite.scala
@@ -370,13 +370,22 @@ class BasicExecutorFeatureStepSuite extends SparkFunSuite with BeforeAndAfter {
     }
   }
 
-  test("SPARK-35572 test enable hostNetwork") {
+  test("SPARK-35572: Configure executor pods with host network enable") {
     baseConf.set(KUBERNETES_EXECUTOR_HOSTNETWORK, true)
     baseConf.set(BLOCK_MANAGER_PORT, 0)
     val baseDriverPod = SparkPod.initialPod()
     val basicExecutorPod = new BasicExecutorFeatureStep(
       newExecutorConf(), new SecurityManager(baseConf), defaultProfile).configurePod(baseDriverPod)
-    assert(basicExecutorPod.pod.getSpec.getHostNetwork == true)
+    assert(basicExecutorPod.pod.getSpec.getHostNetwork)
+  }
+
+  test("SPARK-35572: Configure executor pods with host network and BLOCK_MANAGER_PORT not be 0") {
+    baseConf.set(KUBERNETES_EXECUTOR_HOSTNETWORK, true)
+    val baseDriverPod = SparkPod.initialPod()
+    assertThrows[IllegalArgumentException] {
+      new BasicExecutorFeatureStep(newExecutorConf(),
+        new SecurityManager(baseConf), defaultProfile).configurePod(baseDriverPod)
+    }
   }
 
   // There is always exactly one controller reference, and it points to the driver pod.
