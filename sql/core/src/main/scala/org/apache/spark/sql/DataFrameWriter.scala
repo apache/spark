73d4f67145dd7fbad282a9608ac2ac0f31c4b385
itholic <haejoon.lee@databricks.com>
2021-06-01 10:58:49 +0900

    [SPARK-35433][DOCS] Move CSV data source options from Python and Scala into a single page
    ### What changes were proposed in this pull request?
    
    This PR proposes move CSV data source options from Python, Scala and Java into a single page.
    
    ### Why are the changes needed?
    
    So far, the documentation for CSV data source options is separated into different pages for each language API documents. However, this makes managing many options inconvenient, so it is efficient to manage all options in a single page and provide a link to that page in the API of each language.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, the documents will be shown below after this change:
    
    - "CSV Files" page
    <img width="970" alt="Screen Shot 2021-05-27 at 12 35 36 PM" src="https://user-images.githubusercontent.com/44108233/119762269-586a8c80-bee8-11eb-8443-ae5b3c7a685c.png">
    
    - Python
    <img width="785" alt="Screen Shot 2021-05-25 at 4 12 10 PM" src="https://user-images.githubusercontent.com/44108233/119455390-83cc6a80-bd74-11eb-9156-65785ae27db0.png">
    
    - Scala
    <img width="718" alt="Screen Shot 2021-05-25 at 4 12 39 PM" src="https://user-images.githubusercontent.com/44108233/119455414-89c24b80-bd74-11eb-9775-aeda549d081e.png">
    
    - Java
    <img width="667" alt="Screen Shot 2021-05-25 at 4 13 09 PM" src="https://user-images.githubusercontent.com/44108233/119455422-8d55d280-bd74-11eb-97e8-86c1eabeadc2.png">
    
    ### How was this patch tested?
    
    Manually build docs and confirm the page.
    
    Closes #32658 from itholic/SPARK-35433.
    
    Authored-by: itholic <haejoon.lee@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/DataFrameWriter.scala
@@ -850,48 +850,9 @@ final class DataFrameWriter[T] private[sql](ds: Dataset[T]) {
    *   format("csv").save(path)
    * }}}
    *
-   * You can set the following CSV-specific option(s) for writing CSV files:
-   * <ul>
-   * <li>`sep` (default `,`): sets a single character as a separator for each
-   * field and value.</li>
-   * <li>`quote` (default `"`): sets a single character used for escaping quoted values where
-   * the separator can be part of the value. If an empty string is set, it uses `u0000`
-   * (null character).</li>
-   * <li>`escape` (default `\`): sets a single character used for escaping quotes inside
-   * an already quoted value.</li>
-   * <li>`charToEscapeQuoteEscaping` (default `escape` or `\0`): sets a single character used for
-   * escaping the escape for the quote character. The default value is escape character when escape
-   * and quote characters are different, `\0` otherwise.</li>
-   * <li>`escapeQuotes` (default `true`): a flag indicating whether values containing
-   * quotes should always be enclosed in quotes. Default is to escape all values containing
-   * a quote character.</li>
-   * <li>`quoteAll` (default `false`): a flag indicating whether all values should always be
-   * enclosed in quotes. Default is to only escape values containing a quote character.</li>
-   * <li>`header` (default `false`): writes the names of columns as the first line.</li>
-   * <li>`nullValue` (default empty string): sets the string representation of a null value.</li>
-   * <li>`emptyValue` (default `""`): sets the string representation of an empty value.</li>
-   * <li>`encoding` (by default it is not set): specifies encoding (charset) of saved csv
-   * files. If it is not set, the UTF-8 charset will be used.</li>
-   * <li>`compression` (default `null`): compression codec to use when saving to file. This can be
-   * one of the known case-insensitive shorten names (`none`, `bzip2`, `gzip`, `lz4`,
-   * `snappy` and `deflate`). </li>
-   * <li>`dateFormat` (default `yyyy-MM-dd`): sets the string that indicates a date format.
-   * Custom date formats follow the formats at
-   * <a href="https://spark.apache.org/docs/latest/sql-ref-datetime-pattern.html">
-   *   Datetime Patterns</a>.
-   * This applies to date type.</li>
-   * <li>`timestampFormat` (default `yyyy-MM-dd'T'HH:mm:ss[.SSS][XXX]`): sets the string that
-   * indicates a timestamp format. Custom date formats follow the formats at
-   * <a href="https://spark.apache.org/docs/latest/sql-ref-datetime-pattern.html">
-   *   Datetime Patterns</a>.
-   * This applies to timestamp type.</li>
-   * <li>`ignoreLeadingWhiteSpace` (default `true`): a flag indicating whether or not leading
-   * whitespaces from values being written should be skipped.</li>
-   * <li>`ignoreTrailingWhiteSpace` (default `true`): a flag indicating defines whether or not
-   * trailing whitespaces from values being written should be skipped.</li>
-   * <li>`lineSep` (default `\n`): defines the line separator that should be used for writing.
-   * Maximum length is 1 character.</li>
-   * </ul>
+   * You can find the CSV-specific options for writing CSV files in
+   * <a href="https://spark.apache.org/docs/latest/sql-data-sources-csv.html#data-source-option">
+   *   Data Source Option</a> in the version you use.
    *
    * @since 2.0.0
    */
