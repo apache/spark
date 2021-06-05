363477fe0e375e8581c0976616be943eb56a09bd
Kamil Breguła <mik-laj@users.noreply.github.com>
2021-06-05 13:35:55 +0200

    Don't run migration for adhoc command (#16255)

--- docs/apache-airflow/start/airflow.sh
+++ docs/apache-airflow/start/airflow.sh
@@ -25,4 +25,8 @@ PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 set -euo pipefail
 
 export COMPOSE_FILE="${PROJECT_DIR}/docker-compose.yaml"
-exec docker-compose run --rm -e CONNECTION_CHECK_MAX_COUNT=0 airflow-worker "${@}"
+if [ $# -gt 0 ]; then
+    exec docker-compose run --rm airflow-cli "${@}"
+else
+    exec docker-compose run --rm airflow-cli
+fi
