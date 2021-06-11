57ba5c9c2187a2cde4eec1ef6eadb40ba4c73208
Tanel Kiis <tanel.kiis@gmail.com>
2021-06-11 07:43:35 +0300

    Collect metrics from adaptive execution

--- sql/core/src/main/scala/org/apache/spark/sql/execution/CollectMetricsExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/CollectMetricsExec.scala
@@ -22,6 +22,7 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.{CatalystTypeConverters, InternalRow}
 import org.apache.spark.sql.catalyst.expressions.{Attribute, NamedExpression, SortOrder}
 import org.apache.spark.sql.catalyst.plans.physical.Partitioning
+import org.apache.spark.sql.execution.adaptive.{AdaptiveSparkPlanExec, QueryStageExec}
 import org.apache.spark.sql.execution.columnar.InMemoryTableScanExec
 import org.apache.spark.sql.types.StructType
 
@@ -97,6 +98,10 @@ object CollectMetricsExec {
       case collector: CollectMetricsExec => Map(collector.name -> collector.collectedMetrics)
       case tableScan: InMemoryTableScanExec =>
         CollectMetricsExec.collect(tableScan.relation.cachedPlan)
+      case adaptivePlan: AdaptiveSparkPlanExec =>
+        CollectMetricsExec.collect(adaptivePlan.executedPlan)
+      case queryStageExec: QueryStageExec =>
+        CollectMetricsExec.collect(queryStageExec.plan)
     }
     metrics.reduceOption(_ ++ _).getOrElse(Map.empty)
   }
