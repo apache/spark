93fe5bdea2dcaf554138815c6966ac2f7b2a9b55
Cheng Su <chengsu@fb.com>
2021-06-14 12:19:29 -0700

    Add back HadoopMapReduceCommitProtocol.getFilename and mark it deprecated as PathOutputCommitProtocol depends on it

--- core/src/main/scala/org/apache/spark/internal/io/HadoopMapReduceCommitProtocol.scala
+++ core/src/main/scala/org/apache/spark/internal/io/HadoopMapReduceCommitProtocol.scala
@@ -128,6 +128,15 @@ class HadoopMapReduceCommitProtocol(
     throw new UnsupportedOperationException
   }
 
+  @deprecated("use newTaskFile", "3.2.0")
+  protected def getFilename(taskContext: TaskAttemptContext, ext: String): String = {
+    // The file name looks like part-00000-2dd664f9-d2c4-4ffe-878f-c6c70c1fb0cb_00003-c000.parquet
+    // Note that %05d does not truncate the split number, so if we have more than 100000 tasks,
+    // the file name is fine and won't overflow.
+    val split = taskContext.getTaskAttemptID.getTaskID.getId
+    f"part-$split%05d-$jobId$ext"
+  }
+
   override def newTaskFile(
       taskContext: TaskAttemptContext, stagingPath: String, finalPath: Option[String]): Unit = {
     finalPath match {
