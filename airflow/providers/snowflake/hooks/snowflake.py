643e46ca7ad0b86ddcdae37ffe5b77d31c46b52f
sfc-gh-madkins <82121043+sfc-gh-madkins@users.noreply.github.com>
2021-06-15 03:14:09 -0500

    Added ability for Snowflake to attribute usage to Airflow by adding an application parameter (#16420)

--- airflow/providers/snowflake/hooks/snowflake.py
+++ airflow/providers/snowflake/hooks/snowflake.py
@@ -179,6 +179,8 @@ class SnowflakeHook(DbApiHook):
             "role": self.role or role,
             "authenticator": self.authenticator or authenticator,
             "session_parameters": self.session_parameters or session_parameters,
+            # application is used to track origin of the requests
+            "application": os.environ.get("AIRFLOW_SNOWFLAKE_PARTNER", "AIRFLOW")
         }
 
         # If private_key_file is specified in the extra json, load the contents of the file as a private
