82611c083f79d6862aaf9f06ba151af6a71d37c8
kudhru <gargdhruv36@gmail.com>
2021-06-16 19:13:44 +0530

    fix entry for BloomFilter in MimaExcludes

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -61,7 +61,7 @@ object MimaExcludes {
     ProblemFilters.exclude[IncompatibleTemplateDefProblem]("org.apache.spark.util.collection.WritablePartitionedIterator"),
 
     // [SPARK-35757][CORE] Add bitwise AND operation and functionality for intersecting bloom filters
-    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.util.sketch.BloomFilter")
+    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.util.sketch.BloomFilter.intersectInPlace")
   )
 
   // Exclude rules for 3.1.x
