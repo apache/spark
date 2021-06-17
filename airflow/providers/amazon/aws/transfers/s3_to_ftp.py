1c82b4d015a1785a881bb916ffa0265249c2cde7
JavierLopezT <javierlopeztomas@gmail.com>
2021-06-17 13:35:24 +0200

    Fix S3ToFTPOperator (#13796)

--- airflow/providers/amazon/aws/transfers/s3_to_ftp.py
+++ airflow/providers/amazon/aws/transfers/s3_to_ftp.py
@@ -27,21 +27,23 @@ class S3ToFTPOperator(BaseOperator):
     """
     This operator enables the transferring of files from S3 to a FTP server.
 
-    :param ftp_conn_id: The ftp connection id. The name or identifier for
-        establishing a connection to the FTP server.
-    :type ftp_conn_id: str
-    :param ftp_path: The ftp remote path. This is the specified file path for
-        uploading file to the FTP server.
-    :type ftp_path: str
     :param s3_bucket: The targeted s3 bucket. This is the S3 bucket from
         where the file is downloaded.
     :type s3_bucket: str
     :param s3_key: The targeted s3 key. This is the specified file path for
         downloading the file from S3.
     :type s3_key: str
+    :param ftp_path: The ftp remote path. This is the specified file path for
+        uploading file to the FTP server.
+    :type ftp_path: str
+    :param aws_conn_id: reference to a specific AWS connection
+    :type aws_conn_id: str
+    :param ftp_conn_id: The ftp connection id. The name or identifier for
+        establishing a connection to the FTP server.
+    :type ftp_conn_id: str
     """
 
-    template_fields = ('s3_bucket', 's3_key')
+    template_fields = ('s3_bucket', 's3_key', 'ftp_path')
 
     def __init__(
         self,
@@ -67,5 +69,8 @@ class S3ToFTPOperator(BaseOperator):
         s3_obj = s3_hook.get_key(self.s3_key, self.s3_bucket)
 
         with NamedTemporaryFile() as local_tmp_file:
+            self.log.info('Downloading file from %s', self.s3_key)
             s3_obj.download_fileobj(local_tmp_file)
+            local_tmp_file.seek(0)
             ftp_hook.store_file(self.ftp_path, local_tmp_file.name)
+            self.log.info('File stored in %s', {self.ftp_path})
