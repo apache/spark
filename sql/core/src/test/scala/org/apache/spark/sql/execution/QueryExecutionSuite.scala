0400221e311813185c235301af2050ce8640c872
gengjiaan <gengjiaan@360.cn>
2021-06-17 14:32:07 +0800

    Update code

--- sql/core/src/test/scala/org/apache/spark/sql/execution/QueryExecutionSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/QueryExecutionSuite.scala
@@ -21,7 +21,7 @@ import scala.io.Source
 import org.apache.spark.sql.{AnalysisException, FastOperator}
 import org.apache.spark.sql.catalyst.analysis.UnresolvedNamespace
 import org.apache.spark.sql.catalyst.plans.QueryPlan
-import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.plans.logical.{CommandResult, LogicalPlan, OneRowRelation, Project, ShowTables, SubqueryAlias}
 import org.apache.spark.sql.catalyst.trees.TreeNodeTag
 import org.apache.spark.sql.execution.command.{ExecutedCommandExec, ShowTablesCommand}
 import org.apache.spark.sql.internal.SQLConf
