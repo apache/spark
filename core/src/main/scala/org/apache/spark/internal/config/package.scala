70cb3362c947861df1670ba29c334cb579c40360
Dongjoon Hyun <dongjoon@apache.org>
2021-06-27 10:30:15 -0700

    Disable bufferPool

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -1734,7 +1734,7 @@ package object config {
       .doc("If true, enable buffer pool of ZSTD JNI library.")
       .version("3.2.0")
       .booleanConf
-      .createWithDefault(true)
+      .createWithDefault(false)
 
   private[spark] val IO_COMPRESSION_ZSTD_LEVEL =
     ConfigBuilder("spark.io.compression.zstd.level")
