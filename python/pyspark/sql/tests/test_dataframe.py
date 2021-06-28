03e6de2abe6b767e55de2761499ae937db588508
itholic <haejoon.lee@databricks.com>
2021-06-28 11:47:09 +0900

    [SPARK-35605][PYTHON] Move to_pandas_on_spark to the Spark DataFrame
    ### What changes were proposed in this pull request?
    
    This PR proposes move `to_pandas_on_spark` function from `pyspark.pandas.frame` to `pyspark.sql.dataframe`, and added the related tests to the PySpark DataFrame tests.
    
    ### Why are the changes needed?
    
    Because now the Koalas is ported into PySpark, so we don't need to Spark auto-patch anymore.
    And also `to_pandas_on_spark` is belongs to the pandas-on-Spark DataFrame doesn't look make sense.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, it's kinda internal refactoring stuff.
    
    ### How was this patch tested?
    
    Added the related tests and manually check they're passed.
    
    Closes #33054 from itholic/SPARK-35605.
    
    Authored-by: itholic <haejoon.lee@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/tests/test_dataframe.py
+++ python/pyspark/sql/tests/test_dataframe.py
@@ -855,6 +855,22 @@ class DataFrameTests(ReusedSQLTestCase):
         with self.assertRaisesRegex(TypeError, "Parameter 'truncate=foo'"):
             df.show(truncate='foo')
 
+    @unittest.skipIf(
+        not have_pandas or not have_pyarrow,
+        pandas_requirement_message or pyarrow_requirement_message)  # type: ignore
+    def test_to_pandas_on_spark(self):
+        import pandas as pd
+        from pandas.testing import assert_frame_equal
+
+        sdf = self.spark.createDataFrame([("a", 1), ("b", 2), ("c",  3)], ["Col1", "Col2"])
+        psdf_from_sdf = sdf.to_pandas_on_spark()
+        psdf_from_sdf_with_index = sdf.to_pandas_on_spark(index_col="Col1")
+        pdf = pd.DataFrame({"Col1": ["a", "b", "c"], "Col2": [1, 2, 3]})
+        pdf_with_index = pdf.set_index("Col1")
+
+        assert_frame_equal(pdf, psdf_from_sdf.to_pandas())
+        assert_frame_equal(pdf_with_index, psdf_from_sdf_with_index.to_pandas())
+
 
 class QueryExecutionListenerTests(unittest.TestCase, SQLTestUtils):
     # These tests are separate because it uses 'spark.sql.queryExecutionListeners' which is
