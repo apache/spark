a98c8ae57d2daeedf46c3be8eafa9170db9e4805
Takuya UESHIN <ueshin@databricks.com>
2021-07-01 09:40:07 +0900

    [SPARK-35944][PYTHON] Introduce Name and Label type aliases
    ### What changes were proposed in this pull request?
    
    Introduce `Name` and `Label` type aliases to distinguish what is expected instead of `Any` or `Union[Any, Tuple]`.
    
    - `Label`: `Tuple[Any, ...]`
      Internal expression for name-like metadata, like `index_names`, `column_labels`, and `column_label_names` in `InternalFrame`, and similar internal structures.
    - `Name`: `Union[Any, Label]`
      External expression for user-facing names, which can be scalar values or tuples.
    
    ### Why are the changes needed?
    
    Currently `Any` or `Union[Any, Tuple]` is used for name-like types, but type aliases should be used to distinguish what is expected clearly.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Existing tests.
    
    Closes #33159 from ueshin/issues/SPARK-35944/name_and_label.
    
    Authored-by: Takuya UESHIN <ueshin@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/pandas/series.py
+++ python/pyspark/pandas/series.py
@@ -67,7 +67,7 @@ from pyspark.sql.types import (
 from pyspark.sql.window import Window
 
 from pyspark import pandas as ps  # For running doctests and reference resolution in PyCharm.
-from pyspark.pandas._typing import Axis, Dtype, Scalar, T
+from pyspark.pandas._typing import Axis, Dtype, Label, Name, Scalar, T
 from pyspark.pandas.accessors import PandasOnSparkSeriesMethods
 from pyspark.pandas.categorical import CategoricalAccessor
 from pyspark.pandas.config import get_option
@@ -410,7 +410,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
             assert not fastpath
 
             self._anchor = data  # type: DataFrame
-            self._col_label = index  # type: Tuple
+            self._col_label = index  # type: Label
         else:
             if isinstance(data, pd.Series):
                 assert index is None
@@ -441,7 +441,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         return self._psdf._internal.select_column(self._column_label)
 
     @property
-    def _column_label(self) -> Optional[Tuple]:
+    def _column_label(self) -> Optional[Label]:
         return self._col_label
 
     def _update_anchor(self, psdf: DataFrame) -> None:
@@ -1042,7 +1042,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         return (len(self),)
 
     @property
-    def name(self) -> Union[Any, Tuple]:
+    def name(self) -> Name:
         """Return name of the Series."""
         name = self._column_label
         if name is not None and len(name) == 1:
@@ -1051,12 +1051,12 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
             return name
 
     @name.setter
-    def name(self, name: Union[Any, Tuple]) -> None:
+    def name(self, name: Name) -> None:
         self.rename(name, inplace=True)
 
     # TODO: Functionality and documentation should be matched. Currently, changing index labels
     # taking dictionary and function to change index are not supported.
-    def rename(self, index: Optional[Union[Any, Tuple]] = None, **kwargs: Any) -> "Series":
+    def rename(self, index: Optional[Name] = None, **kwargs: Any) -> "Series":
         """
         Alter Series name.
 
@@ -1227,9 +1227,9 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
 
     def reset_index(
         self,
-        level: Optional[Union[int, Any, Tuple, Sequence[Union[int, Any, Tuple]]]] = None,
+        level: Optional[Union[int, Name, Sequence[Union[int, Name]]]] = None,
         drop: bool = False,
-        name: Optional[Union[Any, Tuple]] = None,
+        name: Optional[Name] = None,
         inplace: bool = False,
     ) -> Optional[Union["Series", DataFrame]]:
         """
@@ -1324,7 +1324,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         else:
             return psdf
 
-    def to_frame(self, name: Optional[Union[Any, Tuple]] = None) -> DataFrame:
+    def to_frame(self, name: Optional[Name] = None) -> DataFrame:
         """
         Convert Series to DataFrame.
 
@@ -1491,13 +1491,13 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
     def to_latex(
         self,
         buf: Optional[IO[str]] = None,
-        columns: Optional[List[Union[Any, Tuple]]] = None,
+        columns: Optional[List[Name]] = None,
         col_space: Optional[int] = None,
         header: bool = True,
         index: bool = True,
         na_rep: str = "NaN",
         formatters: Optional[
-            Union[List[Callable[[Any], str]], Dict[Union[Any, Tuple], Callable[[Any], str]]]
+            Union[List[Callable[[Any], str]], Dict[Name, Callable[[Any], str]]]
         ] = None,
         float_format: Optional[Callable[[float], str]] = None,
         sparsify: Optional[bool] = None,
@@ -2062,8 +2062,8 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
 
     def drop(
         self,
-        labels: Optional[Union[Any, Tuple, List[Union[Any, Tuple]]]] = None,
-        index: Optional[Union[Any, Tuple, List[Union[Any, Tuple]]]] = None,
+        labels: Optional[Union[Name, List[Name]]] = None,
+        index: Optional[Union[Name, List[Name]]] = None,
         level: Optional[int] = None,
     ) -> "Series":
         """
@@ -2177,8 +2177,8 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
 
     def _drop(
         self,
-        labels: Optional[Union[Any, Tuple, List[Union[Any, Tuple]]]] = None,
-        index: Optional[Union[Any, Tuple, List[Union[Any, Tuple]]]] = None,
+        labels: Optional[Union[Name, List[Name]]] = None,
+        index: Optional[Union[Name, List[Name]]] = None,
         level: Optional[int] = None,
     ) -> DataFrame:
         if labels is not None:
@@ -2193,7 +2193,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
                 raise ValueError("'level' should be less than the number of indexes")
 
             if is_name_like_tuple(index):  # type: ignore
-                index_list = [cast(Tuple, index)]
+                index_list = [cast(Label, index)]
             elif is_name_like_value(index):
                 index_list = [(index,)]
             elif all(is_name_like_value(idxes, allow_tuple=False) for idxes in index):
@@ -2205,7 +2205,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
                     "that contain index names"
                 )
             else:
-                index_list = cast(List[Tuple], index)
+                index_list = cast(List[Label], index)
 
             drop_index_scols = []
             for idxes in index_list:
@@ -2602,7 +2602,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
             return first_series(psdf)
 
     def swaplevel(
-        self, i: Union[int, Any, Tuple] = -2, j: Union[int, Any, Tuple] = -1, copy: bool = True
+        self, i: Union[int, Name] = -2, j: Union[int, Name] = -1, copy: bool = True
     ) -> "Series":
         """
         Swap levels i and j in a MultiIndex.
@@ -3903,7 +3903,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         else:
             return tuple(values)
 
-    def pop(self, item: Union[Any, Tuple]) -> Union["Series", Scalar]:
+    def pop(self, item: Name) -> Union["Series", Scalar]:
         """
         Return item and drop from series.
 
@@ -4671,7 +4671,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         """
         return self.where(cast(Series, ~cond), other)
 
-    def xs(self, key: Union[Any, Tuple], level: Optional[int] = None) -> "Series":
+    def xs(self, key: Name, level: Optional[int] = None) -> "Series":
         """
         Return cross-section from the Series.
 
@@ -5278,7 +5278,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         """
         return self.head(2)._to_internal_pandas().item()
 
-    def iteritems(self) -> Iterable[Tuple[Union[Any, Tuple], Any]]:
+    def iteritems(self) -> Iterable[Tuple[Name, Any]]:
         """
         Lazily iterate over (index, value) tuples.
 
@@ -5311,7 +5311,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         internal_index_columns = self._internal.index_spark_column_names
         internal_data_column = self._internal.data_spark_column_names[0]
 
-        def extract_kv_from_spark_row(row: Row) -> Tuple[Union[Any, Tuple], Any]:
+        def extract_kv_from_spark_row(row: Row) -> Tuple[Name, Any]:
             k = (
                 row[internal_index_columns[0]]
                 if len(internal_index_columns) == 1
@@ -5325,11 +5325,11 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         ):
             yield k, v
 
-    def items(self) -> Iterable[Tuple[Union[Any, Tuple], Any]]:
+    def items(self) -> Iterable[Tuple[Name, Any]]:
         """This is an alias of ``iteritems``."""
         return self.iteritems()
 
-    def droplevel(self, level: Union[int, Any, Tuple, List[Union[int, Any, Tuple]]]) -> "Series":
+    def droplevel(self, level: Union[int, Name, List[Union[int, Name]]]) -> "Series":
         """
         Return Series with requested index level(s) removed.
 
@@ -6213,7 +6213,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
     # Override the `groupby` to specify the actual return type annotation.
     def groupby(
         self,
-        by: Union[Any, Tuple, "Series", List[Union[Any, Tuple, "Series"]]],
+        by: Union[Name, "Series", List[Union[Name, "Series"]]],
         axis: Axis = 0,
         as_index: bool = True,
         dropna: bool = True,
@@ -6225,7 +6225,7 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
     groupby.__doc__ = Frame.groupby.__doc__
 
     def _build_groupby(
-        self, by: List[Union["Series", Tuple]], as_index: bool, dropna: bool
+        self, by: List[Union["Series", Label]], as_index: bool, dropna: bool
     ) -> "SeriesGroupBy":
         from pyspark.pandas.groupby import SeriesGroupBy
 
