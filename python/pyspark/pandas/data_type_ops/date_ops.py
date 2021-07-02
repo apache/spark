de101165c07244b7bb45c414f4f34bfd7008d6fa
Xinrong Meng <xinrong.meng@databricks.com>
2021-07-02 14:04:21 -0700

    date_ops

--- python/pyspark/pandas/data_type_ops/date_ops.py
+++ python/pyspark/pandas/data_type_ops/date_ops.py
@@ -78,6 +78,15 @@ class DateOps(DataTypeOps):
         else:
             raise TypeError("date subtraction can only be applied to date series.")
 
+    def neg(self, operand: IndexOpsLike) -> IndexOpsLike:
+        raise TypeError("Unary - can not be applied to %s." % self.pretty_name)
+
+    def invert(self, operand: IndexOpsLike) -> IndexOpsLike:
+        raise TypeError("Unary ~ can not be applied to %s." % self.pretty_name)
+
+    def abs(self, operand: IndexOpsLike) -> IndexOpsLike:
+        raise TypeError("abs() can not be applied to %s." % self.pretty_name)
+
     def astype(self, index_ops: IndexOpsLike, dtype: Union[str, type, Dtype]) -> IndexOpsLike:
         dtype, spark_type = pandas_on_spark_type(dtype)
 
