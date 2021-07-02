ed1203d11ad86494f34367d5f019a720f756d0bd
Xinrong Meng <xinrong.meng@databricks.com>
2021-07-02 14:42:34 -0700

    null_ops

--- python/pyspark/pandas/data_type_ops/null_ops.py
+++ python/pyspark/pandas/data_type_ops/null_ops.py
@@ -40,6 +40,15 @@ class NullOps(DataTypeOps):
     def pretty_name(self) -> str:
         return "nulls"
 
+    def neg(self, operand: IndexOpsLike) -> IndexOpsLike:
+        raise TypeError("Unary - can not be applied to %s." % self.pretty_name)
+
+    def invert(self, operand: IndexOpsLike) -> IndexOpsLike:
+        raise TypeError("Unary ~ can not be applied to %s." % self.pretty_name)
+
+    def abs(self, operand: IndexOpsLike) -> IndexOpsLike:
+        raise TypeError("abs() can not be applied to %s." % self.pretty_name)
+
     def astype(self, index_ops: IndexOpsLike, dtype: Union[str, type, Dtype]) -> IndexOpsLike:
         dtype, spark_type = pandas_on_spark_type(dtype)
 
