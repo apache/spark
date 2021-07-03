a2dc01b34590fc7830bdb76fea653e1a0ebecbd3
freget <freget@googlemail.com>
2021-07-03 16:53:28 +0200

    SSHHook: Using correct hostname for host_key when using non-default ssh port (#15964)

--- airflow/providers/ssh/hooks/ssh.py
+++ airflow/providers/ssh/hooks/ssh.py
@@ -241,7 +241,12 @@ class SSHHook(BaseHook):
         else:
             if self.host_key is not None:
                 client_host_keys = client.get_host_keys()
-                client_host_keys.add(self.remote_host, self.host_key.get_name(), self.host_key)
+                if self.port == SSH_PORT:
+                    client_host_keys.add(self.remote_host, self.host_key.get_name(), self.host_key)
+                else:
+                    client_host_keys.add(
+                        f"[{self.remote_host}]:{self.port}", self.host_key.get_name(), self.host_key
+                    )
             else:
                 pass  # will fallback to system host keys if none explicitly specified in conn extra
 
