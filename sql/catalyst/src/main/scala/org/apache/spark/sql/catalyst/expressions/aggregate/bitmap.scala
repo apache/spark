f918c0205d43d6065e9e27deac5f2d56a54bfabf
ReachInfi <87301083+ReachInfi@users.noreply.github.com>
2021-07-13 15:14:48 +0800

    Update bitmap.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/bitmap.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/bitmap.scala
@@ -1,21 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.apache.spark.sql.catalyst.expressions.aggregate
 
 import java.io.ByteArrayInputStream
 import java.io.DataInputStream
-import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
-import org.apache.spark.sql.catalyst.expressions.Expression
-import org.apache.spark.sql.catalyst.expressions.ExpressionDescription
-import org.apache.spark.sql.types._
-import org.roaringbitmap.RoaringBitmap
-import org.apache.spark.sql.catalyst.expressions.codegen.CodegenFallback
-import org.apache.spark.sql.catalyst.expressions.ExpectsInputTypes
-import org.apache.spark.sql.catalyst.expressions.UnaryExpression
 import java.io.ByteArrayOutputStream
 import java.io.DataOutputStream
-
-
+import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.catalyst.analysis.TypeCheckResult
+import org.apache.spark.sql.catalyst.InternalRow
+import org.roaringbitmap.RoaringBitmap
 
 @ExpressionDescription(
  usage = "_FUNC_(expr) - Returns the bitmap from values of a group of the offset.",
