d9221df74e4163a626bc05fc45d5e063a8c51d63
Shraman Basyal <49772843+BShraman@users.noreply.github.com>
2021-07-16 00:53:19 -0500

    Updated clean-logs.sh (#16978)

--- scripts/in_container/prod/clean-logs.sh
+++ scripts/in_container/prod/clean-logs.sh
@@ -19,12 +19,12 @@
 
 set -euo pipefail
 
-DIRECTORY="${AIRFLOW_HOME:-/usr/local/airflow}"
-RETENTION="${AIRFLOW__LOG_RETENTION_DAYS:-15}"
+readonly DIRECTORY="${AIRFLOW_HOME:-/usr/local/airflow}"
+readonly RETENTION="${AIRFLOW__LOG_RETENTION_DAYS:-15}"
 
 trap "exit" INT TERM
 
-EVERY=$((15*60))
+readonly EVERY=$((15*60))
 
 echo "Cleaning logs every $EVERY seconds"
 
@@ -33,5 +33,5 @@ while true; do
   find "${DIRECTORY}"/logs -mtime +"${RETENTION}" -name '*.log' -delete
 
   seconds=$(( $(date -u +%s) % EVERY))
-  [[ $seconds -lt 1 ]] || sleep $((EVERY - seconds))
+  (( seconds < 1 )) || sleep $((EVERY - seconds))
 done
