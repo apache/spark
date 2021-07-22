f83a9ec2fd71b6e808b6a70c9fd5d627cfd6ecfe
Takuya UESHIN <ueshin@databricks.com>
2021-07-21 22:34:04 -0700

    [SPARK-36214][PYTHON] Add add_categories to CategoricalAccessor and CategoricalIndex
    ### What changes were proposed in this pull request?
    
    Add `add_categories` to `CategoricalAccessor` and `CategoricalIndex`.
    
    ### Why are the changes needed?
    
    We should implement `add_categories` in `CategoricalAccessor` and `CategoricalIndex`.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, users will be able to use `add_categories`.
    
    ### How was this patch tested?
    
    Added some tests.
    
    Closes #33470 from ueshin/issues/SPARK-36214/add_categories.
    
    Authored-by: Takuya UESHIN <ueshin@databricks.com>
    Signed-off-by: Takuya UESHIN <ueshin@databricks.com>
    (cherry picked from commit dcc0aaa3efb2d441b2dfadb0c64dbc28ee197de5)
    Signed-off-by: Takuya UESHIN <ueshin@databricks.com>

--- python/pyspark/pandas/tests/test_categorical.py
+++ python/pyspark/pandas/tests/test_categorical.py
@@ -79,6 +79,24 @@ class CategoricalTest(PandasOnSparkTestCase, TestUtils):
         with self.assertRaises(ValueError):
             psser.cat.categories = [1, 2, 3, 4]
 
+    def test_add_categories(self):
+        pdf, psdf = self.df_pair
+
+        pser = pdf.a
+        psser = psdf.a
+
+        self.assert_eq(pser.cat.add_categories(4), psser.cat.add_categories(4))
+        self.assert_eq(pser.cat.add_categories([4, 5]), psser.cat.add_categories([4, 5]))
+        self.assert_eq(pser.cat.add_categories([]), psser.cat.add_categories([]))
+
+        pser.cat.add_categories(4, inplace=True)
+        psser.cat.add_categories(4, inplace=True)
+        self.assert_eq(pser, psser)
+        self.assert_eq(pdf, psdf)
+
+        self.assertRaises(ValueError, lambda: psser.cat.add_categories(4))
+        self.assertRaises(ValueError, lambda: psser.cat.add_categories([5, 5]))
+
     def test_as_ordered_unordered(self):
         pdf, psdf = self.df_pair
 
