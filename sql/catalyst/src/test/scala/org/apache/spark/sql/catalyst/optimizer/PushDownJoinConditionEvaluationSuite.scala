c16d606c159727d458c1c6d68da1849d7b70592c
Yuming Wang <yumwang@ebay.com>
2021-07-28 22:21:59 +0800

    Fix test

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/PushDownJoinConditionEvaluationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/PushDownJoinConditionEvaluationSuite.scala
@@ -23,6 +23,7 @@ import org.apache.spark.sql.catalyst.expressions.{Alias, Coalesce, Substring, Up
 import org.apache.spark.sql.catalyst.plans._
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.rules._
+import org.apache.spark.sql.internal.SQLConf
 
 class PushDownJoinConditionEvaluationSuite extends PlanTest {
 
@@ -39,41 +40,63 @@ class PushDownJoinConditionEvaluationSuite extends PlanTest {
   private val y = testRelation1.subquery('y)
 
   test("Push down join condition evaluation(String expressions)") {
-    val joinType = Inner
-    Seq(Upper("y.d".attr), Substring("y.d".attr, 1, 5)).foreach { udf =>
-      val originalQuery = x.join(y, joinType, Option("x.a".attr === udf))
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
+      val joinType = Inner
+      Seq(Upper("y.d".attr), Substring("y.d".attr, 1, 5)).foreach { udf =>
+        val originalQuery = x.join(y, joinType, Option("x.a".attr === udf))
+          .select("x.a".attr, "y.e".attr)
+        val correctAnswer = x.select("x.a".attr, "x.b".attr, "x.c".attr)
+          .join(y.select("y.d".attr, "y.e".attr, Alias(udf, udf.sql)()),
+            joinType, Option("x.a".attr === s"`${udf.sql}`".attr)).select("x.a".attr, "y.e".attr)
+
+        comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+      }
+    }
+  }
+
+  test("Push down join condition evaluation(null expressions)") {
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
+      val joinType = Inner
+      val udf = Coalesce(Seq("x.b".attr, "x.c".attr))
+      val originalQuery = x.join(y, joinType, Option(udf === "y.e".attr))
+        .select("x.a".attr, "y.e".attr)
       val correctAnswer =
-        x.join(y.select("y.d".attr, "y.e".attr, Alias(udf, udf.sql)()),
-          joinType, Option("x.a".attr === s"`${udf.sql}`".attr))
+        x.select("x.a".attr, "x.b".attr, "x.c".attr, Alias(udf, udf.sql)()).join(
+          y.select("y.d".attr, "y.e".attr),
+          joinType, Option(s"`${udf.sql}`".attr === "y.e".attr))
+          .select("x.a".attr, "y.e".attr)
 
       comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
     }
   }
 
-  test("Push down join condition evaluation(null expressions)") {
-    val joinType = Inner
-    val udf = Coalesce(Seq("x.b".attr, "x.c".attr))
-      val originalQuery = x.join(y, joinType, Option(udf === "y.e".attr))
-      val correctAnswer =
-        x.select("x.a".attr, "x.b".attr, "x.c".attr, Alias(udf, udf.sql)()).join(y,
-          joinType, Option(s"`${udf.sql}`".attr === "y.e".attr))
+  test("Negative case: Non broadcast hash join") {
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "10000000") {
+      val joinType = Inner
+      val udf = Upper("y.d".attr)
+      val originalQuery = x.join(y, joinType, Option("x.a".attr === udf))
 
-      comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+      comparePlans(Optimize.execute(originalQuery.analyze), originalQuery.analyze)
+    }
   }
 
   test("Negative case: all children are Attributes") {
-    val condition = Option("x.a".attr === "y.d".attr)
-    val originalQuery = x.join(y, Inner, condition)
-    val correctAnswer = x.join(y, Inner, condition)
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
+      val condition = Option("x.a".attr === "y.d".attr)
+      val originalQuery = x.join(y, Inner, condition)
+      val correctAnswer = x.join(y, Inner, condition)
 
-    comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+      comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+    }
   }
 
   test("Negative case: contains Literal") {
-    val condition = Option("x.a".attr === "string")
-    val originalQuery = x.join(y, Inner, condition)
-    val correctAnswer = x.join(y, Inner, condition)
+    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
+      val condition = Option("x.a".attr === "string")
+      val originalQuery = x.join(y, Inner, condition)
+      val correctAnswer = x.join(y, Inner, condition)
 
-    comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+      comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+    }
   }
 }
