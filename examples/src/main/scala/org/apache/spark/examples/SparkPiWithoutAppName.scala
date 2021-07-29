e47cd501da71d55c6bd6274057593121fa1782e6
ulysses-you <ulyssesyou18@gmail.com>
2021-07-29 10:57:12 +0800

    test

--- examples/src/main/scala/org/apache/spark/examples/SparkPiWithoutAppName.scala
+++ examples/src/main/scala/org/apache/spark/examples/SparkPiWithoutAppName.scala
@@ -15,33 +15,15 @@
  * limitations under the License.
  */
 
-// scalastyle:off println
 package org.apache.spark.examples
-
-import scala.math.random
 
 import org.apache.spark.sql.SparkSession
 
-/** Computes an approximation to pi */
-object SparkPi {
+/** Computes an approximation to pi but with no app name specified */
+object SparkPiWithoutAppName {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-      .builder
-      .appName("Spark Pi")
-      .getOrCreate()
-    run(args, spark)
+    val spark = SparkSession.builder().getOrCreate()
+    SparkPi.run(args, spark)
     spark.stop()
   }
-
-  def run(args: Array[String], spark: SparkSession): Unit = {
-    val slices = if (args.length > 0) args(0).toInt else 2
-    val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
-    val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
-      val x = random * 2 - 1
-      val y = random * 2 - 1
-      if (x*x + y*y <= 1) 1 else 0
-    }.reduce(_ + _)
-    println(s"Pi is roughly ${4.0 * count / (n - 1)}")
-  }
 }
-// scalastyle:on println
