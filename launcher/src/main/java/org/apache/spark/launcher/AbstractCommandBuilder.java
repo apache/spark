3edd0530ee3b4ee05813b81e3312900f7fa1cc5d
yangjie01 <yangjie01@baidu.com>
2021-07-29 19:43:36 +0800

    remove unused import

--- launcher/src/main/java/org/apache/spark/launcher/AbstractCommandBuilder.java
+++ launcher/src/main/java/org/apache/spark/launcher/AbstractCommandBuilder.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-import java.util.function.BiConsumer;
 import java.util.regex.Pattern;
 
 import static org.apache.spark.launcher.CommandBuilderUtils.*;
