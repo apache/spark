b151d8f2323a18b9fedabd56f7d560da80dbcc3c
Holden Karau <hkarau@netflix.com>
2021-07-29 11:04:02 -0700

    We do the deletion of the pods inside of the executorpodsallocator now because the method of doing the delete can change, change the tests to validate the stop is called and then validate the stop entry in EPAS

--- resource-managers/kubernetes/core/src/test/scala/org/apache/spark/scheduler/cluster/k8s/KubernetesClusterSchedulerBackendSuite.scala
+++ resource-managers/kubernetes/core/src/test/scala/org/apache/spark/scheduler/cluster/k8s/KubernetesClusterSchedulerBackendSuite.scala
@@ -150,7 +150,7 @@ class KubernetesClusterSchedulerBackendSuite extends SparkFunSuite with BeforeAn
     verify(eventQueue).stop()
     verify(watchEvents).stop()
     verify(pollEvents).stop()
-    verify(labeledPods).delete()
+    verify(podAllocator).stop(TEST_SPARK_APP_ID)
     verify(labledConfigMaps).delete()
     verify(kubernetesClient).close()
   }
