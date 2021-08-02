b82bbe83ddb0d3c589ce5037eff526b7bedf7307
josh-fell <48934154+josh-fell@users.noreply.github.com>
2021-08-02 16:10:40 -0400

    Example DAG-related updates for Apache Drill (#17384)

--- airflow/providers/apache/drill/example_dags/__init__.py
+++ airflow/providers/apache/drill/example_dags/__init__.py
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,26 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-"""
-Example Airflow DAG to execute SQL in an Apache Drill environment using the `DrillOperator`.
-"""
-from airflow.models import DAG
-from airflow.providers.apache.drill.operators.drill import DrillOperator
-from airflow.utils.dates import days_ago
-
-with DAG(
-    dag_id='example_drill_dag',
-    schedule_interval=None,
-    start_date=days_ago(2),
-    tags=['example'],
-) as dag:
-    # [START howto_operator_drill]
-    sql_task = DrillOperator(
-        task_id='json_to_parquet_table',
-        sql='''
-        drop table if exists dfs.tmp.employee;
-        create table dfs.tmp.employee as select * from cp.`employee.json`;
-        ''',
-    )
-    # [END howto_operator_drill]
