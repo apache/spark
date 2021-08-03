7c586842d71064169aa77baf666a8566d9ed785e
Wenchen Fan <wenchen@databricks.com>
2021-08-03 10:43:00 +0300

    [SPARK-36380][SQL] Simplify the logical plan names for ALTER TABLE ... COLUMN
    ### What changes were proposed in this pull request?
    
    This a followup of the recent work such as https://github.com/apache/spark/pull/33200
    
    For `ALTER TABLE` commands, the logical plans do not have the common `AlterTable` prefix in the name and just use names like `SetTableLocation`. This PR proposes to follow the same naming rule in `ALTER TABE ... COLUMN` commands.
    
    This PR also moves these AlterTable commands to a individual file and give them a base trait.
    
    ### Why are the changes needed?
    
    name simplification
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    existing test
    
    Closes #33609 from cloud-fan/dsv2.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>
    (cherry picked from commit 7cb9c1c2415a0984515e4d4733f816673e4ae3c8)
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/CheckAnalysis.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/CheckAnalysis.scala
@@ -442,8 +442,8 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog {
           case write: V2WriteCommand if write.resolved =>
             write.query.schema.foreach(f => TypeUtils.failWithIntervalType(f.dataType))
 
-          case alter: AlterTableColumnCommand if alter.table.resolved =>
-            checkAlterTableColumnCommand(alter)
+          case alter: AlterTableCommand =>
+            checkAlterTableCommand(alter)
 
           case _ => // Falls back to the following checks
         }
@@ -939,7 +939,7 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog {
   /**
    * Validates the options used for alter table commands after table and columns are resolved.
    */
-  private def checkAlterTableColumnCommand(alter: AlterTableColumnCommand): Unit = {
+  private def checkAlterTableCommand(alter: AlterTableCommand): Unit = {
     def checkColumnNotExists(op: String, fieldNames: Seq[String], struct: StructType): Unit = {
       if (struct.findNestedField(fieldNames, includeCollections = true).isDefined) {
         alter.failAnalysis(s"Cannot $op column, because ${fieldNames.quoted} " +
@@ -948,7 +948,7 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog {
     }
 
     alter match {
-      case AlterTableAddColumns(table: ResolvedTable, colsToAdd) =>
+      case AddColumns(table: ResolvedTable, colsToAdd) =>
         colsToAdd.foreach { colToAdd =>
           checkColumnNotExists("add", colToAdd.name, table.schema)
         }
@@ -957,10 +957,10 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog {
           "in the user specified columns",
           alter.conf.resolver)
 
-      case AlterTableRenameColumn(table: ResolvedTable, col: ResolvedFieldName, newName) =>
+      case RenameColumn(table: ResolvedTable, col: ResolvedFieldName, newName) =>
         checkColumnNotExists("rename", col.path :+ newName, table.schema)
 
-      case a @ AlterTableAlterColumn(table: ResolvedTable, col: ResolvedFieldName, _, _, _, _) =>
+      case a @ AlterColumn(table: ResolvedTable, col: ResolvedFieldName, _, _, _, _) =>
         val fieldName = col.name.quoted
         if (a.dataType.isDefined) {
           val field = CharVarcharUtils.getRawType(col.field.metadata)
