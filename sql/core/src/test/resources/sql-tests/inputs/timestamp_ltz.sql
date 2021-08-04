a5b74fd87eea8fddfd3e0077daa2d16c8de16bb0
Gengliang Wang <gengliang@apache.org>
2021-08-04 20:36:29 +0800

    fix

--- sql/core/src/test/resources/sql-tests/inputs/timestamp_ltz.sql
+++ sql/core/src/test/resources/sql-tests/inputs/timestamp_ltz.sql
@@ -3,10 +3,7 @@
 --CONFIG_DIM1 spark.sql.timestampType=TIMESTAMP_NTZ
 
 select timestamp_ltz'2019-10-06 10:11:12.345678';
-select timestamp_ltz'-1969-12-31 16:00:00';
 select timestamp_ltz'0015-03-18 16:00:00';
-select timestamp_ltz'-000001';
-select timestamp_ltz'99999-03-18T12:03:17';
 
 select to_timestamp_ltz(null), to_timestamp_ltz('2016-12-31 00:12:00'), to_timestamp_ltz('2016-12-31', 'yyyy-MM-dd');
 select to_timestamp_ltz(to_date(null)), to_timestamp_ltz(to_date('2016-12-31')), to_timestamp_ltz(to_date('2016-12-31', 'yyyy-MM-dd'));
