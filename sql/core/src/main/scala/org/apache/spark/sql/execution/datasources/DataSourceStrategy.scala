e1d70620952ae4804c91c92a88aaaf35ad2ea703
Angerszhuuuu <angers.zhu@gmail.com>
2021-08-06 17:08:09 +0800

    Update DataSourceStrategy.scala

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
@@ -473,25 +473,6 @@ object DataSourceStrategy
     }
   }
 
-  protected[sql] def normalizeExprsAttrNameMap(
-      output: Seq[AttributeReference],
-      projects: Seq[NamedExpression],
-      filters: Seq[Expression]): Map[String, String] = {
-    var attrNameMap: Map[String, String] = Map.empty
-    val normalizedAttrNameMap = output.map(att => (att.exprId, att.name)).toMap
-    projects.map(_.transform {
-      case attr: AttributeReference if normalizedAttrNameMap.contains(attr.exprId) =>
-        attrNameMap += attr.name -> normalizedAttrNameMap(attr.exprId)
-        attr
-    })
-    filters.map(_.transform {
-      case attr: AttributeReference if normalizedAttrNameMap.contains(attr.exprId) =>
-        attrNameMap += attr.name -> normalizedAttrNameMap(attr.exprId)
-        attr
-    })
-    attrNameMap
-  }
-
   def getPushedDownFilters(
       partitionColumns: Seq[Expression],
       normalizedFilters: Seq[Expression]): ExpressionSet = {
