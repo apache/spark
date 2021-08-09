a5ecf2a490727fec97790b149f59bdc498b445be
Angerszhuuuu <angers.zhu@gmail.com>
2021-08-09 16:47:56 +0800

    [SPARK-36352][SQL] Spark should check result plan's output schema name
    ### What changes were proposed in this pull request?
    Spark should check result plan's output schema name
    
    ### Why are the changes needed?
    In current code, some optimizer rule may change plan's output schema, since in the code we always use semantic equal to check output, but it may change the plan's output schema.
    For example, for SchemaPruning, if we have a plan
    ```
    Project[a, B]
    |--Scan[A, b, c]
    ```
    the origin output schema is `a, B`, after SchemaPruning. it become
    ```
    Project[A, b]
    |--Scan[A, b]
    ```
    It change the plan's schema. when we use CTAS, the schema is same as query plan's output.
    Then since we change the schema, it not consistent with origin SQL. So we need to check final result plan's schema with origin plan's schema
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    existed UT
    
    Closes #33583 from AngersZhuuuu/SPARK-36352.
    
    Authored-by: Angerszhuuuu <angers.zhu@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit e051a540a10cdda42dc86a6195c0357aea8900e4)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/AQEOptimizer.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/adaptive/AQEOptimizer.scala
@@ -22,6 +22,7 @@ import org.apache.spark.sql.catalyst.optimizer.ConvertToLocalRelation
 import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, LogicalPlanIntegrity, PlanHelper}
 import org.apache.spark.sql.catalyst.rules.RuleExecutor
 import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.types.DataType
 import org.apache.spark.util.Utils
 
 /**
@@ -64,9 +65,12 @@ class AQEOptimizer(conf: SQLConf) extends RuleExecutor[LogicalPlan] {
     }
   }
 
-  override protected def isPlanIntegral(plan: LogicalPlan): Boolean = {
-    !Utils.isTesting || (plan.resolved &&
-      plan.find(PlanHelper.specialExpressionsInUnsupportedOperator(_).nonEmpty).isEmpty &&
-      LogicalPlanIntegrity.checkIfExprIdsAreGloballyUnique(plan))
+  override protected def isPlanIntegral(
+      previousPlan: LogicalPlan,
+      currentPlan: LogicalPlan): Boolean = {
+    !Utils.isTesting || (currentPlan.resolved &&
+      currentPlan.find(PlanHelper.specialExpressionsInUnsupportedOperator(_).nonEmpty).isEmpty &&
+      LogicalPlanIntegrity.checkIfExprIdsAreGloballyUnique(currentPlan) &&
+      DataType.equalsIgnoreNullability(previousPlan.schema, currentPlan.schema))
   }
 }
