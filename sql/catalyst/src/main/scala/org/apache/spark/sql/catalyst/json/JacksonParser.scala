445140dd34c03b49c215d8a2daec5c095c1b1ddb
gengjiaan <gengjiaan@360.cn>
2021-08-10 16:29:37 +0800

    Update code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JacksonParser.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JacksonParser.scala
@@ -331,11 +331,9 @@ class JacksonParser(
       makeConverter(udt.sqlType)
 
     case _: NullType =>
-      (parser: JsonParser) =>
-        // Here, we pass empty `PartialFunction` so that this case can be
-        // handled as a failed conversion. It will throw an exception as
-        // long as the value is not null.
-        parseJsonToken[AnyRef](parser, dataType)(PartialFunction.empty[JsonToken, AnyRef])
+      (parser: JsonParser) => parseJsonToken[java.lang.Long](parser, dataType) {
+        case _ => null
+      }
 
     // We don't actually hit this exception though, we keep it for understandability
     case _ => throw QueryExecutionErrors.unsupportedTypeError(dataType)
