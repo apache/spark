b6c20a8f5dadd14e45ecec05b8434f9a76fb4e27
N <13322818+noelmcloughlin@users.noreply.github.com>
2021-08-19 01:16:01 +0100

    docs(dagowner): describe dag owner more carefully (#17699)
    Describe dag `owner` more carefully
    
    Co-authored-by: Ephraim Anierobi <splendidzigy24@gmail.com>

--- airflow/models/baseoperator.py
+++ airflow/models/baseoperator.py
@@ -221,7 +221,8 @@ class BaseOperator(Operator, LoggingMixin, TaskMixin, metaclass=BaseOperatorMeta
 
     :param task_id: a unique, meaningful id for the task
     :type task_id: str
-    :param owner: the owner of the task, using the unix username is recommended
+    :param owner: the owner of the task. Using a meaningful description
+        (e.g. user/person/team/role name) to clarify ownership is recommended.
     :type owner: str
     :param email: the 'to' email address(es) used in email alerts. This can be a
         single email or multiple ones. Multiple addresses can be specified as a
