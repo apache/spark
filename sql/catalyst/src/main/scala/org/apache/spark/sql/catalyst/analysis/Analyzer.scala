4f8341778b01e73234d8392c97d3f26671566d77
Angerszhuuuu <angers.zhu@gmail.com>
2021-08-24 23:33:00 +0800

    [SPARK-35876][SQL][3.1] ArraysZip should retain field names to avoid being re-written by analyzer/optimizer
    #### What changes were proposed in this pull request?
    This PR fixes an issue that field names of structs generated by arrays_zip function could be unexpectedly re-written by analyzer/optimizer.
    Here is an example.
    ```
    val df = sc.parallelize(Seq((Array(1, 2), Array(3, 4)))).toDF("a1", "b1").selectExpr("arrays_zip(a1, b1) as zipped")
    df.printSchema
    root
     |-- zipped: array (nullable = true)
     |    |-- element: struct (containsNull = false)
     |    |    |-- a1: integer (nullable = true)                                      // OK. a1 is expected name
     |    |    |-- b1: integer (nullable = true)                                      // OK. b1 is expected name
    
    df.explain
    == Physical Plan ==
    *(1) Project [arrays_zip(_1#3, _2#4) AS zipped#12]               // Not OK. field names are re-written as _1 and _2 respectively
    
    df.write.parquet("/tmp/test.parquet")
    val df2 = spark.read.parquet("/tmp/test.parquet")
    
    df2.printSchema
    root
     |-- zipped: array (nullable = true)
     |    |-- element: struct (containsNull = true)
     |    |    |-- _1: integer (nullable = true)                                      // Not OK. a1 is expected but got _1
     |    |    |-- _2: integer (nullable = true)                                      // Not OK. b1 is expected but got _2
    ```
    This issue happens when aliases are eliminated by AliasHelper.replaceAliasButKeepName or AliasHelper.trimNonTopLevelAliases called via analyzer/optimizer
    spark/sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
    
    Line 883 in b89cd8d
    
     upper.map(replaceAliasButKeepName(_, aliases))
    
    spark/sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
    
    Line 3759 in b89cd8d
    
     val cleanedProjectList = projectList.map(trimNonTopLevelAliases)
    
    I investigated functions which can be affected this issue but I found only arrays_zip so far.
    To fix this issue, this PR changes the definition of ArraysZip to retain field names to avoid being re-written by analyzer/optimizer.
    
    ### Why are the changes needed?
    This is apparently a bug.
    
    ### Does this PR introduce any user-facing change?
    No. After this change, the field names are no longer re-written but it should be expected behavior for users.
    
    #### How was this patch tested?
    New tests.
    
    Closes #33810 from AngersZhuuuu/SPARK-35876-3.1.
    
    Lead-authored-by: Angerszhuuuu <angers.zhu@gmail.com>
    Co-authored-by: Kousuke Saruta <sarutak@oss.nttdata.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit 644cad26f489b3c1e8210245f1d958d05c208e53)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -213,7 +213,7 @@ class Analyzer(
       ResolveRelations ::
       ResolveTables ::
       ResolveReferences ::
-      ResolveCreateNamedStruct ::
+      ResolveExpressionsWithNamePlaceholders ::
       ResolveDeserializer ::
       ResolveNewInstance ::
       ResolveUpCast ::
@@ -3508,10 +3508,18 @@ object TimeWindowing extends Rule[LogicalPlan] {
 }
 
 /**
- * Resolve a [[CreateNamedStruct]] if it contains [[NamePlaceholder]]s.
+ * Resolve expressions if they contains [[NamePlaceholder]]s.
  */
-object ResolveCreateNamedStruct extends Rule[LogicalPlan] {
+object ResolveExpressionsWithNamePlaceholders extends Rule[LogicalPlan] {
   override def apply(plan: LogicalPlan): LogicalPlan = plan.resolveExpressions {
+    case e: ArraysZip if !e.resolved =>
+      val names = e.children.zip(e.names).map {
+        case (e: NamedExpression, NamePlaceholder) if e.resolved =>
+          Literal(e.name)
+        case (_, other) => other
+      }
+      ArraysZip(e.children, names)
+
     case e: CreateNamedStruct if !e.resolved =>
       val children = e.children.grouped(2).flatMap {
         case Seq(NamePlaceholder, e: NamedExpression) if e.resolved =>
