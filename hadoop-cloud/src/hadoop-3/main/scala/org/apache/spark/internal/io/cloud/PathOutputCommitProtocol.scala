44d1d8f8bae102ce647bf2ecb885d33c218b0e04
Angerszhuuuu <angers.zhu@gmail.com>
2021-08-24 18:00:52 +0800

    Update PathOutputCommitProtocol.scala

--- hadoop-cloud/src/hadoop-3/main/scala/org/apache/spark/internal/io/cloud/PathOutputCommitProtocol.scala
+++ hadoop-cloud/src/hadoop-3/main/scala/org/apache/spark/internal/io/cloud/PathOutputCommitProtocol.scala
@@ -60,7 +60,7 @@ class PathOutputCommitProtocol(
   }
 
   /** The committer created. */
-  @transient private var committer: PathOutputCommitter = _
+  @transient protected var committer: PathOutputCommitter = _
 
   require(dest != null, "Null destination specified")
 
