617fed9af6631c4725bc5a7abc9fc66d21f57b15
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-09-01 15:35:09 -0600

    Chart: Make cleanup cronjob cmd/args configurable (#17970)

--- chart/tests/test_cleanup_pods.py
+++ chart/tests/test_cleanup_pods.py
@@ -18,6 +18,7 @@
 import unittest
 
 import jmespath
+from parameterized import parameterized
 
 from tests.helm_template_generator import render_chart
 
@@ -104,3 +105,49 @@ class CleanupPodsTest(unittest.TestCase):
             "spec.jobTemplate.spec.template.spec.tolerations[0].key",
             docs[0],
         )
+
+    def test_default_command_and_args(self):
+        docs = render_chart(
+            values={"cleanup": {"enabled": True}}, show_only=["templates/cleanup/cleanup-cronjob.yaml"]
+        )
+
+        assert jmespath.search("spec.jobTemplate.spec.template.spec.containers[0].command", docs[0]) is None
+        assert ["bash", "-c", "exec airflow kubernetes cleanup-pods --namespace=default"] == jmespath.search(
+            "spec.jobTemplate.spec.template.spec.containers[0].args", docs[0]
+        )
+
+    @parameterized.expand(
+        [
+            (None, None),
+            (None, ["custom", "args"]),
+            (["custom", "command"], None),
+            (["custom", "command"], ["custom", "args"]),
+        ]
+    )
+    def test_command_and_args_overrides(self, command, args):
+        docs = render_chart(
+            values={"cleanup": {"enabled": True, "command": command, "args": args}},
+            show_only=["templates/cleanup/cleanup-cronjob.yaml"],
+        )
+
+        assert command == jmespath.search(
+            "spec.jobTemplate.spec.template.spec.containers[0].command", docs[0]
+        )
+        assert args == jmespath.search("spec.jobTemplate.spec.template.spec.containers[0].args", docs[0])
+
+    def test_log_groomer_command_and_args_overrides_are_templated(self):
+        docs = render_chart(
+            values={
+                "cleanup": {
+                    "enabled": True,
+                    "command": ["{{ .Release.Name }}"],
+                    "args": ["{{ .Release.Service }}"],
+                }
+            },
+            show_only=["templates/cleanup/cleanup-cronjob.yaml"],
+        )
+
+        assert ["RELEASE-NAME"] == jmespath.search(
+            "spec.jobTemplate.spec.template.spec.containers[0].command", docs[0]
+        )
+        assert ["Helm"] == jmespath.search("spec.jobTemplate.spec.template.spec.containers[0].args", docs[0])
