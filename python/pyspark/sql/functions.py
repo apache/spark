ee170d6162125c566fca88b9628aab681378ed17
Yuto Akutsu <yuto.akutsu@nttdata.com>
2021-09-03 14:57:36 +0900

    add cotangent support on PySpark

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -377,6 +377,23 @@ def cosh(col):
     return _invoke_function_over_column("cosh", col)
 
 
+def cot(col):
+    """
+    .. versionadded:: 3.2.0
+
+    Parameters
+    ----------
+    col : :class:`~pyspark.sql.Column` or str
+        angle in radians
+
+    Returns
+    -------
+    :class:`~pyspark.sql.Column`
+        cotangent of the angle.
+    """
+    return _invoke_function_over_column("cot", col)
+
+
 @since(1.4)
 def exp(col):
     """
