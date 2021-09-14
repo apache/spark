f27c4e12530e7d98eefb49bd9631f5d19785d9c2
Angerszhuuuu <angers.zhu@gmail.com>
2021-09-14 12:06:59 +0800

    Update SQLOpenHashSet.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/util/SQLOpenHashSet.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/util/SQLOpenHashSet.scala
@@ -23,10 +23,6 @@ import org.apache.spark.annotation.Private
 import org.apache.spark.sql.types.{DataType, DoubleType, FloatType}
 import org.apache.spark.util.collection.OpenHashSet
 
-/**
- * A wrap of [[OpenHashSet]] that can handle null, Double.NaN and Float.NaN
- * w.r.t. the SQL semantic.
- */
 @Private
 class SQLOpenHashSet[@specialized(Long, Int, Double, Float) T: ClassTag](
     initialCapacity: Int,
