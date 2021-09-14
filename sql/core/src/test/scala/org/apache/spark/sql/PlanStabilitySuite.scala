e0fd9ab6d91a2d55aaa7cb2ca43d870acdb8b0db
asif shahid <asif.shahid@workday.com>
2021-09-14 12:31:38 -0700

    SPARK-33152. fixed scala style error

--- sql/core/src/test/scala/org/apache/spark/sql/PlanStabilitySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/PlanStabilitySuite.scala
@@ -99,7 +99,6 @@ trait PlanStabilitySuite extends DisableAdaptiveExecutionSuite {
     } else baseFileName
     val file = new File(dir, goldenFileName)
     val expectedSimplified = FileUtils.readFileToString(file, StandardCharsets.UTF_8)
-
     val baseExplainFileName = "explain.txt"
     val goldenExplainFileName = if (SQLConf.get.useOptimizedConstraintPropagation) {
       if (new File(dir, prefix + baseExplainFileName).exists()) {
@@ -108,7 +107,6 @@ trait PlanStabilitySuite extends DisableAdaptiveExecutionSuite {
     } else baseExplainFileName
     val explainFile = new File(dir, goldenExplainFileName)
     val expectedExplain = FileUtils.readFileToString(explainFile, StandardCharsets.UTF_8)
-    
     expectedSimplified == actualSimplifiedPlan && expectedExplain == actualExplain
   }
 
