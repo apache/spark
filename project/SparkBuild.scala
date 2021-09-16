ac7e61dde9cc7e19fdbeb1443e5efafb8295b3f3
Dongjoon Hyun <dongjoon@apache.org>
2021-09-15 22:22:53 -0700

    Use official release

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -270,7 +270,6 @@ object SparkBuild extends PomBuild {
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
       DefaultMavenRepository,
       Resolver.mavenLocal,
-      "Apache Staging Repository" at "https://repository.apache.org/content/repositories/orgapacheorc-1050",
       Resolver.file("ivyLocal", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     ),
     externalResolvers := resolvers.value,
