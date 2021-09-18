36ce9cce55619160db6104da10b9b53141c10105
dgd-contributor <dgd_contributor@viettel.com.vn>
2021-09-17 17:48:15 -0700

    [SPARK-36762][PYTHON] Fix Series.isin when Series has NaN values
    ### What changes were proposed in this pull request?
    Fix Series.isin when Series has NaN values
    
    ### Why are the changes needed?
    Fix Series.isin when Series has NaN values
    ``` python
    >>> pser = pd.Series([None, 5, None, 3, 2, 1, None, 0, 0])
    >>> psser = ps.from_pandas(pser)
    >>> pser.isin([1, 3, 5, None])
    0    False
    1     True
    2    False
    3     True
    4    False
    5     True
    6    False
    7    False
    8    False
    dtype: bool
    >>> psser.isin([1, 3, 5, None])
    0    None
    1    True
    2    None
    3    True
    4    None
    5    True
    6    None
    7    None
    8    None
    dtype: object
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    After this PR
    ``` python
    >>> pser = pd.Series([None, 5, None, 3, 2, 1, None, 0, 0])
    >>> psser = ps.from_pandas(pser)
    >>> psser.isin([1, 3, 5, None])
    0    False
    1     True
    2    False
    3     True
    4    False
    5     True
    6    False
    7    False
    8    False
    dtype: bool
    
    ```
    
    ### How was this patch tested?
    unit tests
    
    Closes #34005 from dgd-contributor/SPARK-36762_fix_series.isin_when_values_have_NaN.
    
    Authored-by: dgd-contributor <dgd_contributor@viettel.com.vn>
    Signed-off-by: Takuya UESHIN <ueshin@databricks.com>
    (cherry picked from commit 32b8512912c211f7f12e717e7029e89645da9d3b)
    Signed-off-by: Takuya UESHIN <ueshin@databricks.com>

--- python/pyspark/pandas/tests/test_series.py
+++ python/pyspark/pandas/tests/test_series.py
@@ -394,6 +394,23 @@ class SeriesTest(PandasOnSparkTestCase, SQLTestUtils):
         with self.assertRaisesRegex(TypeError, msg):
             psser.isin(1)
 
+        # when Series have NaN
+        pser = pd.Series(["lama", "cow", None, "lama", "beetle", "lama", "hippo", None], name="a")
+        psser = ps.from_pandas(pser)
+
+        self.assert_eq(psser.isin(["cow", "lama"]), pser.isin(["cow", "lama"]))
+
+        pser = pd.Series([None, 5, None, 3, 2, 1, None, 0, 0], name="a")
+        psser = ps.from_pandas(pser)
+
+        if LooseVersion(pd.__version__) >= LooseVersion("1.2"):
+            self.assert_eq(psser.isin([1, 5, 0, None]), pser.isin([1, 5, 0, None]))
+        else:
+            expected = pd.Series(
+                [False, True, False, False, False, True, False, True, True], name="a"
+            )
+            self.assert_eq(psser.isin([1, 5, 0, None]), expected)
+
     def test_drop_duplicates(self):
         pdf = pd.DataFrame({"animal": ["lama", "cow", "lama", "beetle", "lama", "hippo"]})
         psdf = ps.from_pandas(pdf)
