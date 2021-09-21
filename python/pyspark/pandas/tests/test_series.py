198302662be1c7001c192a07a9f47f7f818df92b
Xinrong Meng <xinrong.meng@databricks.com>
2021-09-21 10:27:28 -0700

    reduce version checks

--- python/pyspark/pandas/tests/test_series.py
+++ python/pyspark/pandas/tests/test_series.py
@@ -1663,7 +1663,7 @@ class SeriesTest(PandasOnSparkTestCase, SQLTestUtils):
             self.assert_eq(psser, pser)
         else:
             # Before pandas 1.3.0, `pop` modifies the dtype of categorical series wrongly.
-            self.assert_eq(psser.pop(0), pser.pop(0))
+            self.assert_eq(psser.pop(0), "a")
             self.assert_eq(
                 psser,
                 pd.Series(
@@ -1671,20 +1671,11 @@ class SeriesTest(PandasOnSparkTestCase, SQLTestUtils):
                 ),
             )
 
-            if LooseVersion(pd.__version__) >= LooseVersion("1.1.0"):
-                self.assert_eq(psser.pop(3), pser.pop(3))
-                self.assert_eq(
-                    psser,
-                    pd.Series(pd.Categorical(["b", "c"], categories=["a", "b", "c"]), index=[1, 2]),
-                )
-            else:
-                # Before pandas 1.1.5, a categorical series can only apply `pop` once.
-                # Otherwise, error 'numpy.ndarray' object has no attribute '_internal_get_values'.
-                psser.pop(3)
-                self.assert_eq(
-                    psser,
-                    pd.Series(pd.Categorical(["b", "c"], categories=["a", "b", "c"]), index=[1, 2]),
-                )
+            self.assert_eq(psser.pop(3), "a")
+            self.assert_eq(
+                psser,
+                pd.Series(pd.Categorical(["b", "c"], categories=["a", "b", "c"]), index=[1, 2]),
+            )
 
     def test_replace(self):
         pser = pd.Series([10, 20, 15, 30, np.nan], name="x")
