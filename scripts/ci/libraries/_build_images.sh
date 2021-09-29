d2bdf05cf04414ec2c973cdec0f970bf96c3a0e2
Jarek Potiuk <jarek@potiuk.com>
2021-09-29 19:57:53 +0200

    Check docker-compose version in breeze (#18536)
    We started to use some newer features in docker-compose of breeze.
    They are new enough (May 2020) that older versions of
    docker-compose do not work with them.
    
    This PR checks if the docker-compose is >= 1.29

--- scripts/ci/libraries/_build_images.sh
+++ scripts/ci/libraries/_build_images.sh
@@ -282,7 +282,7 @@ function build_images::get_local_build_cache_hash() {
         return
 
     fi
-    docker_v create --name "local-airflow-ci-container" "${AIRFLOW_CI_IMAGE}" 2>/dev/null
+    docker_v create --name "local-airflow-ci-container" "${AIRFLOW_CI_IMAGE}" 2>/dev/null >/dev/null
     docker_v cp "local-airflow-ci-container:/build-cache-hash" \
         "${local_image_build_cache_file}" 2>/dev/null ||
         touch "${local_image_build_cache_file}"
