99a2978645da135de68bc5be6a0ae9efa263d26f
zero323 <mszymkiewicz@gmail.com>
2021-10-02 11:25:19 +0200

    Separate date / time types

--- python/pyspark/sql/_typing.pyi
+++ python/pyspark/sql/_typing.pyi
@@ -42,7 +42,7 @@ AtomicDataTypeOrString = Union[pyspark.sql.types.AtomicType, str]
 DataTypeOrString = Union[pyspark.sql.types.DataType, str]
 OptionalPrimitiveType = Optional[PrimitiveType]
 
-AtomicValue = TypeVar("AtomicValue", DateTimeLiteral, DecimalLiteral, bool, str, int, float)
+AtomicValue = TypeVar("AtomicValue", datetime.datetime, datetime.date, decimal.Decimal, bool, str, int, float)
 
 RowLike = TypeVar("RowLike", List[Any], Tuple[Any, ...], pyspark.sql.types.Row)
 
