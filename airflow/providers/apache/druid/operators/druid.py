f8c31b5bf787de662f73583ee6b65dad6addf453
Josh Fell <48934154+josh-fell@users.noreply.github.com>
2021-10-03 19:22:55 -0400

    Add max_ingestion_time to DruidOperator docstring (#18693)

--- airflow/providers/apache/druid/operators/druid.py
+++ airflow/providers/apache/druid/operators/druid.py
@@ -31,6 +31,8 @@ class DruidOperator(BaseOperator):
     :param druid_ingest_conn_id: The connection id of the Druid overlord which
         accepts index jobs
     :type druid_ingest_conn_id: str
+    :param max_ingestion_time: The maximum ingestion time before assuming the job failed
+    :type max_ingestion_time: int
     """
 
     template_fields = ('json_index_file',)
