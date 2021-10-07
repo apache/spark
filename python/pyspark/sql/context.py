97529b336e4ff3ab6683fa8e1bd6eedbd0f5573b
dch nguyen <dgd_contributor@viettel.com.vn>
2021-10-07 08:40:42 +0700

    add a todo note

--- python/pyspark/sql/context.py
+++ python/pyspark/sql/context.py
@@ -59,6 +59,7 @@ if TYPE_CHECKING:
 __all__ = ["SQLContext", "HiveContext"]
 
 
+# TODO: ignore[attr-defined] will be removed, once SparkContext is inlined
 class SQLContext(object):
     """The entry point for working with structured data (rows and columns) in Spark, in Spark 1.x.
 
@@ -695,6 +696,7 @@ class SQLContext(object):
         return StreamingQueryManager(self._ssql_ctx.streams())
 
 
+# TODO: ignore[attr-defined] will be removed, once SparkContext is inlined
 class HiveContext(SQLContext):
     """A variant of Spark SQL that integrates with data stored in Hive.
 
