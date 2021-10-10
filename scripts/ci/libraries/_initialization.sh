34e09cc023b77091efdfac6875237bcd138697c2
Jarek Potiuk <jarek@potiuk.com>
2021-10-10 15:07:04 +0200

    Strips suffixes from docker version (#18871)
    This version will strip any + alpha postfixes from docker version.
    Seems that CodeQL uses azure-specific docker version and the
    version suffix is added folowing `+`. This change will strip suffix
    but also will not fail docker version check in case of similar
    error, it will continue running with a warning if version cannot
    be retrieved.

--- scripts/ci/libraries/_initialization.sh
+++ scripts/ci/libraries/_initialization.sh
@@ -907,7 +907,14 @@ function initialization::ver() {
 
 function initialization::check_docker_version() {
     local docker_version
-    docker_version=$(docker version --format '{{.Client.Version}}')
+    # In GitHub Code QL, the version of docker has +azure suffix which we should remove
+    docker_version=$(docker version --format '{{.Client.Version}}' | sed 's/\+.*$//' || true)
+    if [ "${docker_version}" == "" ]; then
+        echo
+        echo "${COLOR_YELLOW}Your version of docker is unknown. If the scripts faill, please make sure to install docker at least: ${min_docker_version} version.${COLOR_RESET}"
+        echo
+        return
+    fi
     local comparable_docker_version
     comparable_docker_version=$(initialization::ver "${docker_version}")
     local min_docker_version="20.10.0"
