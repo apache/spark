b795b2312ef3ca82d9928eb5633147da0909274b
Leona Yoda <yodal@oss.nttdata.com>
2021-10-11 19:01:54 +0900

    Remove backslash on docstrings

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -228,7 +228,7 @@ def max_by(col: "ColumnOrName", ord: "ColumnOrName") -> Column:
     ...     ("Java", 2013, 30000)], \\
     ...     schema=("course", "year", "earnings")) \\
     ...     .groupby("course").agg(max_by("year", "earnings")).collect()
-        [Row(course='Java', max_by(year, earnings)=2013), \\
+        [Row(course='Java', max_by(year, earnings)=2013), \
         Row(course='dotNET', max_by(year, earnings)=2013)]
     """
     return _invoke_function("max_by", _to_java_column(col), _to_java_column(ord))
@@ -263,7 +263,7 @@ def min_by(col: "ColumnOrName", ord: "ColumnOrName") -> Column:
     ...     ("Java", 2013, 30000)], \\
     ...     schema=("course", "year", "earnings")) \\
     ...     .groupby("course").agg(min_by("year", "earnings")).collect()
-        [Row(course='Java', min_by(year, earnings)=2012), \\
+        [Row(course='Java', min_by(year, earnings)=2012), \
         Row(course='dotNET', min_by(year, earnings)=2012)]
     """
     return _invoke_function("min_by", _to_java_column(col), _to_java_column(ord))
