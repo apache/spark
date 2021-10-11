2925783a222e47d1ea3fa499dd711e2ce3fb46aa
yangjie01 <yangjie01@baidu.com>
2021-10-11 21:57:43 +0800

    refactor

--- core/src/main/scala/org/apache/spark/util/JavaModuleUtils.scala
+++ core/src/main/scala/org/apache/spark/util/JavaModuleUtils.scala
@@ -17,8 +17,6 @@
 
 package org.apache.spark.util
 
-import scala.collection.mutable
-
 import org.apache.commons.lang3.{JavaVersion, SystemUtils}
 
 import org.apache.spark.SparkConf
@@ -35,8 +33,8 @@ object JavaModuleUtils {
   def supplementJavaModuleOptions(conf: SparkConf): Unit = {
 
     def supplementModuleOpts(
-        targetEntry: OptionalConfigEntry[String],
-        sourceEntry: ConfigEntry[String]): Unit = {
+        sourceEntry: ConfigEntry[String],
+        targetEntry: OptionalConfigEntry[String]): Unit = {
       val v = conf.get(targetEntry) match {
         case Some(opts) => conf.get(sourceEntry) + opts
         case None => conf.get(sourceEntry)
@@ -44,26 +42,7 @@ object JavaModuleUtils {
       conf.set(targetEntry.key, v)
     }
 
-    supplementModuleOpts(DRIVER_JAVA_OPTIONS, DRIVER_JAVA_MODULE_OPTIONS)
-    supplementModuleOpts(EXECUTOR_JAVA_OPTIONS, EXECUTOR_JAVA_MODULE_OPTIONS)
-  }
-
-  def supplementJava17ModuleOptsIfNeeded(args: Seq[String]): Seq[String] = {
-
-    def supplementModuleOpts(buffer: mutable.Buffer[String], key: String): Unit = {
-      val index = buffer.indexWhere(_.startsWith(s"$key="))
-      if (index != -1) {
-        buffer.update(index, buffer(index) + JavaModuleUtils.defaultModuleOptions())
-      } else {
-        buffer.prependAll(Seq("--conf", s"$key=${JavaModuleUtils.defaultModuleOptions()}"))
-      }
-    }
-
-    if (Utils.isTesting && isJavaVersionAtLeast17) {
-      val buffer = args.toBuffer
-      supplementModuleOpts(buffer, DRIVER_JAVA_OPTIONS.key)
-      supplementModuleOpts(buffer, EXECUTOR_JAVA_OPTIONS.key)
-      buffer.toSeq
-    } else args
+    supplementModuleOpts(DRIVER_JAVA_MODULE_OPTIONS, DRIVER_JAVA_OPTIONS)
+    supplementModuleOpts(EXECUTOR_JAVA_MODULE_OPTIONS, EXECUTOR_JAVA_OPTIONS)
   }
 }
