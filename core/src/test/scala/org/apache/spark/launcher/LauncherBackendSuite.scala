1abe44e79f676e40b65866d9528d9447ff9fc04a
yangjie01 <yangjie01@baidu.com>
2021-10-11 21:43:35 +0800

    revert change of LauncherBackendSuite

--- core/src/test/scala/org/apache/spark/launcher/LauncherBackendSuite.scala
+++ core/src/test/scala/org/apache/spark/launcher/LauncherBackendSuite.scala
@@ -27,7 +27,7 @@ import org.scalatest.matchers.should.Matchers._
 
 import org.apache.spark._
 import org.apache.spark.internal.config.UI.UI_ENABLED
-import org.apache.spark.util.{JavaModuleUtils, Utils}
+import org.apache.spark.util.Utils
 
 class LauncherBackendSuite extends SparkFunSuite with Matchers {
 
@@ -50,8 +50,7 @@ class LauncherBackendSuite extends SparkFunSuite with Matchers {
       .setSparkHome(sys.props("spark.test.home"))
       .setConf(SparkLauncher.DRIVER_EXTRA_CLASSPATH, System.getProperty("java.class.path"))
       .setConf(UI_ENABLED.key, "false")
-      .setConf(SparkLauncher.DRIVER_EXTRA_JAVA_OPTIONS,
-        s"-Dtest.appender=console ${JavaModuleUtils.defaultModuleOptions()}")
+      .setConf(SparkLauncher.DRIVER_EXTRA_JAVA_OPTIONS, s"-Dtest.appender=console")
       .setMaster(master)
       .setAppResource(SparkLauncher.NO_RESOURCE)
       .setMainClass(TestApp.getClass.getName().stripSuffix("$"))
