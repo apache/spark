dc1db950adb9a210acfe4a0a77988955a5f35e5e
Wenchen Fan <wenchen@databricks.com>
2021-10-12 22:47:31 +0800

    [SPARK-36867][SQL] Fix error message with GROUP BY alias
    ### What changes were proposed in this pull request?
    
    When checking unresolved attributes, we should check `Aggregate.aggregateExpressions` before `Aggregate.groupingExpressions`, because the latter may rely on the former, due to the GROUP BY alias feature.
    
    ### Why are the changes needed?
    
    improve error message
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    new test
    
    Closes #34244 from cloud-fan/bug.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/test/resources/sql-tests/inputs/group-by.sql
+++ sql/core/src/test/resources/sql-tests/inputs/group-by.sql
@@ -45,6 +45,9 @@ SELECT COUNT(DISTINCT b), COUNT(DISTINCT b, c) FROM (SELECT 1 AS a, 2 AS b, 3 AS
 SELECT a AS k, COUNT(b) FROM testData GROUP BY k;
 SELECT a AS k, COUNT(b) FROM testData GROUP BY k HAVING k > 1;
 
+-- GROUP BY alias with invalid col in SELECT list
+SELECT a AS k, COUNT(non_existing) FROM testData GROUP BY k;
+
 -- Aggregate functions cannot be used in GROUP BY
 SELECT COUNT(b) AS k FROM testData GROUP BY k;
 
