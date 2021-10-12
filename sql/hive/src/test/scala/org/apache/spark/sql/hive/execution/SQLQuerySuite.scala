35be0eecfc92f1c27f52013d2c1fc99cd9576cc1
Linhong Liu <linhong.liu@databricks.com>
2021-10-12 17:55:56 +0800

    fix test and address comments

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/SQLQuerySuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/SQLQuerySuite.scala
@@ -2634,18 +2634,13 @@ abstract class SQLQuerySuiteBase extends QueryTest with SQLTestUtils with TestHi
   }
 
   test("SPARK-36905: read hive views without without explicit column names") {
-    withTempDir { dir =>
-      val t1Loc = s"file:///$dir/t1"
       withTable("t1") {
         withView("test_view") {
-          hiveClient.runSqlHive(
-            s"create table t1(id int) stored as avro location '$t1Loc'")
-          hiveClient.runSqlHive("insert into t1 select 2")
+          hiveClient.runSqlHive("create table t1 stored as avro as select 2 as id")
           hiveClient.runSqlHive("create view test_view as select 1, id + 1 from t1")
           checkAnswer(sql("select * from test_view"), Seq(Row(1, 3)))
         }
       }
-    }
   }
 }
 
