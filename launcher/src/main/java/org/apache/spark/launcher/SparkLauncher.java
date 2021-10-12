2e87bdeff7500bbf50d18efa25b02205b48a0dff
yangjie01 <yangjie01@baidu.com>
2021-10-12 13:57:09 +0800

    remove new conf

--- launcher/src/main/java/org/apache/spark/launcher/SparkLauncher.java
+++ launcher/src/main/java/org/apache/spark/launcher/SparkLauncher.java
@@ -55,8 +55,6 @@ public class SparkLauncher extends AbstractLauncher<SparkLauncher> {
   public static final String DRIVER_EXTRA_CLASSPATH = "spark.driver.extraClassPath";
   /** Configuration key for the default driver VM options. */
   public static final String DRIVER_DEFAULT_JAVA_OPTIONS = "spark.driver.defaultJavaOptions";
-  /** Configuration key for the driver VM `--add-opens` options. */
-  public static final String DRIVER_JAVA_MODULE_OPTIONS = "spark.driver.javaModuleOptions";
   /** Configuration key for the driver VM options. */
   public static final String DRIVER_EXTRA_JAVA_OPTIONS = "spark.driver.extraJavaOptions";
   /** Configuration key for the driver native library path. */
@@ -68,8 +66,6 @@ public class SparkLauncher extends AbstractLauncher<SparkLauncher> {
   public static final String EXECUTOR_EXTRA_CLASSPATH = "spark.executor.extraClassPath";
   /** Configuration key for the default executor VM options. */
   public static final String EXECUTOR_DEFAULT_JAVA_OPTIONS = "spark.executor.defaultJavaOptions";
-  /** Configuration key for the executor VM `--add-opens` options. */
-  public static final String EXECUTOR_JAVA_MODULE_OPTIONS = "spark.executor.javaModuleOptions";
   /** Configuration key for the executor VM options. */
   public static final String EXECUTOR_EXTRA_JAVA_OPTIONS = "spark.executor.extraJavaOptions";
   /** Configuration key for the executor native library path. */
