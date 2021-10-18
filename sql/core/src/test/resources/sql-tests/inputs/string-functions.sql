7a52529805a63a597b8ebc0117ca9f9761d6f6c6
Menelaos Karavelas <menelaos.karavelas@databricks.com>
2021-10-18 11:33:05 -0700

    Add test cases where we omit the padding pattern.

--- sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
@@ -63,6 +63,9 @@ SELECT lpad('hi', 'invalid_length');
 SELECT rpad('hi', 'invalid_length');
 
 -- lpad for BINARY inputs
+SELECT hex(lpad(unhex(''), 5));
+SELECT hex(lpad(unhex('aabb'), 5));
+SELECT hex(lpad(unhex('aabbcc'), 2));
 SELECT hex(lpad(unhex(''), 5, unhex('1f')));
 SELECT hex(lpad(unhex('aa'), 5, unhex('1f')));
 SELECT hex(lpad(unhex('aa'), 6, unhex('1f')));
@@ -74,6 +77,9 @@ SELECT hex(lpad(unhex('aabbcc'), 6, unhex('')));
 SELECT hex(lpad(unhex('aabbcc'), 2, unhex('ff')));
 
 -- rpad for BINARY inputs
+SELECT hex(rpad(unhex(''), 5));
+SELECT hex(rpad(unhex('aabb'), 5));
+SELECT hex(rpad(unhex('aabbcc'), 2));
 SELECT hex(rpad(unhex(''), 5, unhex('1f')));
 SELECT hex(rpad(unhex('aa'), 5, unhex('1f')));
 SELECT hex(rpad(unhex('aa'), 6, unhex('1f')));
