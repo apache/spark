8b03630d96c9a482cb264dca4ba01140f028ad07
Weiwei Yang <wyang@cloudera.com>
2021-10-19 09:38:45 -0700

    address review comments

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/scheduler/cluster/k8s/ExecutorPodsAllocator.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/scheduler/cluster/k8s/ExecutorPodsAllocator.scala
@@ -460,7 +460,7 @@ class ExecutorPodsAllocator(
       currentTime - startTime > executorIdleTimeout
     } catch {
       case e: Exception =>
-        logError(s"Cannot get the createTime of the pod $state.pod", e)
+        logError(s"Cannot get the creationTimestamp of the pod ${state.pod}", e)
         true
     }
   }
