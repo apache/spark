adaaf4c904ad4e4354c56c01fd056d04900c9611
Abhishek Somani <abhishek.somani@databricks.com>
2021-10-20 16:41:52 +0800

    [SPARK-37046][SQL] Alter view does not preserve column case
    Column case was lost when we did an alter view because we would not save schema information in table properties for datasource tables when doing an alter view.
    The fix proposed is to save the required properties in table props in the alter codepath
    
    This is a bug as schema case information is lost. Repro
    
    ```
    scala> sql("create view v as select 1 as A, 1 as B")
    res2: org.apache.spark.sql.DataFrame = []
    
    scala> sql("describe v").collect.foreach(println)
    [A,int,null]
    [B,int,null]
    
    scala> sql("alter view v as select 1 as C, 1 as D")
    res4: org.apache.spark.sql.DataFrame = []
    
    scala> sql("describe v").collect.foreach(println)
    [c,int,null]
    [d,int,null]
    ```
    
    Yes. Now on an alter view, user will see column case preserved.
    
    Added unit tests.
    
    Closes #34317 from somani/alter-oss-3.
    
    Authored-by: Abhishek Somani <abhishek.somani@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit ec785223c94e0e94157c6691f233e9dc056af805)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveDDLSuite.scala
@@ -2999,4 +2999,43 @@ class HiveDDLSuite
       assert(spark.sparkContext.listJars().exists(_.contains(jarName)))
     }
   }
+
+  test("SPARK-37046: Alter view should preserve column case with view definition change") {
+    withView("v") {
+      // Changing view definition should preserve column case
+      spark.sql("CREATE VIEW v AS SELECT 1 AS A, 1 AS B")
+      val df = spark.table("v")
+      assert(df.schema.names.toSeq == Seq("A", "B"))
+
+      spark.sql("ALTER VIEW v AS SELECT 1 AS C, 1 AS D")
+      val df1 = spark.table("v")
+      assert(df1.schema.names.toSeq == Seq("C", "D"))
+    }
+  }
+
+  test("SPARK-37046: Alter view should preserve column case with view name change") {
+    withView("v") {
+      // Renaming view should preserve column case
+      spark.sql("CREATE VIEW v AS SELECT 1 AS A, 1 AS B")
+      val df = spark.table("v")
+      assert(df.schema.names.toSeq == Seq("A", "B"))
+
+      sql("ALTER VIEW v RENAME TO vRenamed")
+      val df1 = spark.table("vRenamed")
+      assert(df1.schema.names.toSeq == Seq("A", "B"))
+    }
+  }
+
+  test("SPARK-37046: Alter view should preserve column case with tbl properties change") {
+    withView("v") {
+      // Setting table properties should preserve column case
+      spark.sql("CREATE VIEW v AS SELECT 1 AS A, 1 AS B")
+      val df = spark.table("v")
+      assert(df.schema.names.toSeq == Seq("A", "B"))
+
+      sql("ALTER VIEW v SET TBLPROPERTIES('testkey' = 'testval')")
+      val df1 = spark.table("v")
+      assert(df1.schema.names.toSeq == Seq("A", "B"))
+    }
+  }
 }
