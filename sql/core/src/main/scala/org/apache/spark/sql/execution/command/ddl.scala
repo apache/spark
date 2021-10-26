05aea976873e3ca55251f8e6c7e38fa5ae7e8c91
Kent Yao <yao@apache.org>
2021-10-26 11:10:10 +0800

    Revert "[SPARK-37098][SQL] Alter table properties should invalidate cache"
    This reverts commit 286a37663213ef3abfbf9effb8cd5723ec6382ff.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/ddl.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/ddl.scala
@@ -277,7 +277,6 @@ case class AlterTableSetPropertiesCommand(
       properties = table.properties ++ properties,
       comment = properties.get(TableCatalog.PROP_COMMENT).orElse(table.comment))
     catalog.alterTable(newTable)
-    catalog.invalidateCachedTable(tableName)
     Seq.empty[Row]
   }
 
@@ -316,7 +315,6 @@ case class AlterTableUnsetPropertiesCommand(
     val newProperties = table.properties.filter { case (k, _) => !propKeys.contains(k) }
     val newTable = table.copy(properties = newProperties, comment = tableComment)
     catalog.alterTable(newTable)
-    catalog.invalidateCachedTable(tableName)
     Seq.empty[Row]
   }
 
