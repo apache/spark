9e6188c45e4d86da4a5ce296fd7df03f26c5c077
Wenchen Fan <cloud0fan@gmail.com>
2021-10-28 00:38:09 +0800

    Update ExpressionParserSuite.scala

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/parser/ExpressionParserSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/parser/ExpressionParserSuite.scala
@@ -937,7 +937,9 @@ class ExpressionParserSuite extends AnalysisTest {
       assertEqual("current_date", UnresolvedAttribute.quoted("current_date"))
       assertEqual("current_timestamp", UnresolvedAttribute.quoted("current_timestamp"))
     }
-    withSQLConf(SQLConf.ANSI_ENABLED.key -> "false") {
+    withSQLConf(
+      SQLConf.ANSI_ENABLED.key -> "false",
+      SQLConf.ENFORCE_RESERVED_KEYWORDS.key -> "true") {
       testNonAnsiBehavior()
     }
     withSQLConf(
