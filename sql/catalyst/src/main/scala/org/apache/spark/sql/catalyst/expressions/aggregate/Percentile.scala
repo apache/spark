11f6e75bbb9b3fed06bf4f7c83193b88d6f32ca2
Angerszhuuuu <angers.zhu@gmail.com>
2021-10-30 15:17:16 +0800

    update

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/Percentile.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/Percentile.scala
@@ -62,11 +62,9 @@ import org.apache.spark.util.collection.OpenHashMap
        3.0
       > SELECT _FUNC_(col, array(0.25, 0.75)) FROM VALUES (0), (10) AS tab(col);
        [2.5,7.5]
-      > SELECT _FUNC_(col, 0.5) FROM VALUES
-          (INTERVAL '0' MONTH), (INTERVAL '10' MONTH) AS tab(col);
+      > SELECT _FUNC_(col, 0.5) FROM VALUES (INTERVAL '0' MONTH), (INTERVAL '10' MONTH) AS tab(col);
        5.0
-      > SELECT _FUNC_(col, array(0.2, 0.5)) FROM VALUES
-          (INTERVAL '0' SECOND), (INTERVAL '10' SECOND) AS tab(col);
+      > SELECT _FUNC_(col, array(0.2, 0.5)) FROM VALUES (INTERVAL '0' SECOND), (INTERVAL '10' SECOND) AS tab(col);
        [2000000.0,5000000.0]
   """,
   group = "agg_funcs",
