e2820222db7fb6261cc1d5b43c3bbbbffd8855dd
Huaxin Gao <huaxin_gao@apple.com>
2021-11-04 12:39:26 -0700

    nit

--- sql/catalyst/src/main/java/org/apache/spark/sql/connector/catalog/index/SupportsIndex.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/connector/catalog/index/SupportsIndex.java
@@ -40,7 +40,7 @@ public interface SupportsIndex extends Table {
    * @param columns the columns on which index to be created
    * @param columnsProperties the properties of the columns on which index to be created
    * @param properties the properties of the index to be created. {@code indexType} is a special
-   *                   properties that is used to specify the type of the index to be created.
+   *                   property that is used to specify the type of the index to be created.
    * @throws IndexAlreadyExistsException If the index already exists.
    */
   void createIndex(String indexName,
