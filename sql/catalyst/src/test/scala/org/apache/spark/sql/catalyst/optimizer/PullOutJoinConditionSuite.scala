c6fca9aad5b3e1d819d35441dd30c6e12de04da2
Yuming Wang <yumwang@ebay.com>
2021-11-05 23:19:01 +0800

    Address comments

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/PullOutJoinConditionSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/PullOutJoinConditionSuite.scala
@@ -23,7 +23,6 @@ import org.apache.spark.sql.catalyst.expressions.{Alias, Coalesce, IsNull, Liter
 import org.apache.spark.sql.catalyst.plans._
 import org.apache.spark.sql.catalyst.plans.logical._
 import org.apache.spark.sql.catalyst.rules._
-import org.apache.spark.sql.internal.SQLConf
 
 class PullOutJoinConditionSuite extends PlanTest {
 
@@ -40,94 +39,59 @@ class PullOutJoinConditionSuite extends PlanTest {
   private val y = testRelation1.subquery('y)
 
   test("Pull out join keys evaluation(String expressions)") {
-    val joinType = Inner
     Seq(Upper("y.d".attr), Substring("y.d".attr, 1, 5)).foreach { udf =>
-      val originalQuery = x.join(y, joinType, Option("x.a".attr === udf))
-        .select("x.a".attr, "y.e".attr)
-      val correctAnswer = x.select("x.a".attr, "x.b".attr, "x.c".attr)
-        .join(y.select("y.d".attr, "y.e".attr, Alias(udf, udf.sql)()),
-          joinType, Option("x.a".attr === s"`${udf.sql}`".attr)).select("x.a".attr, "y.e".attr)
+      val originalQuery = x.join(y, condition = Option('a === udf)).select('a, 'e)
+      val correctAnswer = x.select('a, 'b, 'c)
+        .join(y.select('d, 'e, Alias(udf, udf.sql)()),
+          condition = Option('a === s"`${udf.sql}`".attr)).select('a, 'e)
 
       comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
     }
   }
 
-  test("Pull out join keys evaluation(null expressions)") {
-    val joinType = Inner
-    val udf = Coalesce(Seq("x.b".attr, "x.c".attr))
-    val originalQuery = x.join(y, joinType, Option(udf === "y.e".attr))
-      .select("x.a".attr, "y.e".attr)
-    val correctAnswer =
-      x.select("x.a".attr, "x.b".attr, "x.c".attr, Alias(udf, udf.sql)()).join(
-        y.select("y.d".attr, "y.e".attr),
-        joinType, Option(s"`${udf.sql}`".attr === "y.e".attr))
-        .select("x.a".attr, "y.e".attr)
-
-    comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
-  }
-
   test("Pull out join condition contains other predicates") {
     val udf = Substring("y.d".attr, 1, 5)
-    val joinType = Inner
-    val originalQuery = x.join(y, joinType, Option("x.a".attr === udf && "x.b".attr > "y.e".attr))
-      .select("x.a".attr, "y.e".attr)
-    val correctAnswer =
-      x.select("x.a".attr, "x.b".attr, "x.c".attr).join(
-        y.select("y.d".attr, "y.e".attr, Alias(udf, udf.sql)()),
-        joinType, Option("x.a".attr === s"`${udf.sql}`".attr && "x.b".attr > "y.e".attr))
-        .select("x.a".attr, "y.e".attr)
+    val originalQuery = x.join(y, condition = Option('a === udf && 'b > 'e)).select('a, 'e)
+    val correctAnswer = x.select('a, 'b, 'c)
+      .join(y.select('d, 'e, Alias(udf, udf.sql)()),
+        condition = Option('a === s"`${udf.sql}`".attr && 'b > 'e)).select('a, 'e)
 
       comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
   }
 
   test("Pull out EqualNullSafe join condition") {
-    withSQLConf(SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
-      val joinType = Inner
-      val udf = "x.b".attr + 1
-      val coalesce1 = Coalesce(Seq(udf, Literal(0)))
-      val coalesce2 = Coalesce(Seq("y.e".attr, Literal(0)))
-      val isNull1 = IsNull(udf)
-      val isNull2 = IsNull("y.e".attr)
+    val joinType = Inner
+    val udf = "x.b".attr + 1
+    val coalesce1 = Coalesce(Seq(udf, Literal(0)))
+    val coalesce2 = Coalesce(Seq("y.e".attr, Literal(0)))
+    val isNull1 = IsNull(udf)
+    val isNull2 = IsNull("y.e".attr)
 
-      val originalQuery = x.join(y, joinType, Option(udf <=> "y.e".attr))
-        .select("x.a".attr, "y.e".attr)
-      val correctAnswer =
-        x.select("x.a".attr, "x.b".attr, "x.c".attr,
-          Alias(coalesce1, coalesce1.sql)(),
-          Alias(isNull1, isNull1.sql)()).join(
-          y.select("y.d".attr, "y.e".attr,
-            Alias(coalesce2, coalesce2.sql)(),
-            Alias(isNull2, isNull2.sql)()),
-          joinType, Option(s"`${coalesce1.sql}`".attr === s"`${coalesce2.sql}`".attr &&
-            s"`${isNull1.sql}`".attr === s"`${isNull2.sql}`".attr))
-          .select("x.a".attr, "y.e".attr)
+    val originalQuery = x.join(y, joinType, Option(udf <=> 'e)).select('a, 'e)
+    val correctAnswer =
+      x.select('a, 'b, 'c, Alias(coalesce1, coalesce1.sql)(), Alias(isNull1, isNull1.sql)())
+        .join(y.select('d, 'e, Alias(coalesce2, coalesce2.sql)(), Alias(isNull2, isNull2.sql)()),
+          condition = Option(s"`${coalesce1.sql}`".attr === s"`${coalesce2.sql}`".attr &&
+            s"`${isNull1.sql}`".attr === s"`${isNull2.sql}`".attr)).select('a, 'e)
 
-      comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
-    }
+    comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
   }
 
   test("Negative case: non-equality join keys") {
-    val joinType = Inner
-    val udf = "x.b".attr + 1
-    val originalQuery = x.join(y, joinType, Option(udf > "y.e".attr))
-      .select("x.a".attr, "y.e".attr)
+    val originalQuery = x.join(y, condition = Option("x.b".attr + 1 > 'e)).select('a, 'e)
 
     comparePlans(Optimize.execute(originalQuery.analyze), originalQuery.analyze)
   }
 
   test("Negative case: all children are Attributes") {
-    val condition = Option("x.a".attr === "y.d".attr)
-    val originalQuery = x.join(y, Inner, condition)
-    val correctAnswer = x.join(y, Inner, condition)
+    val originalQuery = x.join(y, condition = Option('a === 'd))
 
-    comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+    comparePlans(Optimize.execute(originalQuery.analyze), originalQuery.analyze)
   }
 
   test("Negative case: contains Literal") {
-    val condition = Option("x.a".attr === "string")
-    val originalQuery = x.join(y, Inner, condition)
-    val correctAnswer = x.join(y, Inner, condition)
+    val originalQuery = x.join(y, condition = Option('a === "string"))
 
-    comparePlans(Optimize.execute(originalQuery.analyze), correctAnswer.analyze)
+    comparePlans(Optimize.execute(originalQuery.analyze), originalQuery.analyze)
   }
 }
