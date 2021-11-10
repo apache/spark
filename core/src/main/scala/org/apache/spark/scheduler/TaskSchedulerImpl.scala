16e26049afc8ba92b06bdc58c47b211ea87e0d2b
yi.wu <yi.wu@databricks.com>
2021-11-10 15:18:05 +0800

    Revert "[SPARK-36575][CORE] Should ignore task finished event if its task set is gone in TaskSchedulerImpl.handleSuccessfulTask"
    This reverts commit bc80c844fcb37d8d699d46bb34edadb98ed0d9f7.

--- core/src/main/scala/org/apache/spark/scheduler/TaskSchedulerImpl.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskSchedulerImpl.scala
@@ -871,13 +871,7 @@ private[spark] class TaskSchedulerImpl(
       taskSetManager: TaskSetManager,
       tid: Long,
       taskResult: DirectTaskResult[_]): Unit = synchronized {
-    if (taskIdToTaskSetManager.contains(tid)) {
-      taskSetManager.handleSuccessfulTask(tid, taskResult)
-    } else {
-      logInfo(s"Ignoring update with state finished for task (TID $tid) because its task set " +
-        "is gone (this is likely the result of receiving duplicate task finished status updates)" +
-        " or its executor has been marked as failed.")
-    }
+    taskSetManager.handleSuccessfulTask(tid, taskResult)
   }
 
   def handleFailedTask(
