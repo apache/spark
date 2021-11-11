d9aa266c67b2b9d07da69f0e4d8c9c8e1b78c527
Yuming Wang <yumwang@ebay.com>
2021-11-11 22:06:55 +0800

    fix

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/basicLogicalOperators.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/basicLogicalOperators.scala
@@ -1454,17 +1454,15 @@ object RepartitionByExpression {
  */
 case class RebalancePartitions(
     partitionExpressions: Seq[Expression],
-    child: LogicalPlan) extends RepartitionOperation {
-
-  override def numPartitions: Int = conf.numShufflePartitions
+    child: LogicalPlan) extends UnaryNode {
+  override def maxRows: Option[Long] = child.maxRows
+  override def output: Seq[Attribute] = child.output
 
   def partitioning: Partitioning = if (partitionExpressions.isEmpty) {
     RoundRobinPartitioning(conf.numShufflePartitions)
   } else {
     HashPartitioning(partitionExpressions, conf.numShufflePartitions)
   }
-
-  override def shuffle: Boolean = true
 
   override protected def withNewChildInternal(newChild: LogicalPlan): RebalancePartitions =
     copy(child = newChild)
