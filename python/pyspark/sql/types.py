cd28f60009c1f22089f451db55df794c689f7317
Hyukjin Kwon <gurwls223@apache.org>
2021-11-12 12:58:15 +0900

    Remove the reference of typeName as DDL format in documentation

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -911,9 +911,7 @@ def _parse_datatype_string(s: str) -> DataType:
     """
     Parses the given data type string to a :class:`DataType`. The data type string format equals
     :class:`DataType.simpleString`, except that the top level struct type can omit
-    the ``struct<>`` and atomic types use ``typeName()`` as their format, e.g. use ``byte`` instead
-    of ``tinyint`` for :class:`ByteType`. We can also use ``int`` as a short name
-    for :class:`IntegerType`. Since Spark 2.3, this also supports a schema in a DDL-formatted
+    the ``struct<>``. Since Spark 2.3, this also supports a schema in a DDL-formatted
     string and case-insensitive strings.
 
     Examples
