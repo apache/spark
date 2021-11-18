f8dd8030b5c51fe75a609819cf4d608683532254
Takuya UESHIN <ueshin@databricks.com>
2021-11-18 15:47:33 -0800

    Fix.

--- python/pyspark/tests/test_statcounter.py
+++ python/pyspark/tests/test_statcounter.py
@@ -14,79 +14,85 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import unittest
-
 from pyspark.statcounter import StatCounter
 from pyspark.testing.utils import ReusedPySparkTestCase
 
 
 class StatCounterTests(ReusedPySparkTestCase):
-    def test_as_dict(self):
+    def test_base(self):
         stats = self.sc.parallelize([1.0, 2.0, 3.0, 4.0]).stats()
-        self.assertDictEqual(
-            stats.asDict(),
-            {
-                "count": 4,
-                "max": 4.0,
-                "mean": 2.5,
-                "min": 1.0,
-                "stdev": 1.2909944487358056,
-                "sum": 10.0,
-                "variance": 1.6666666666666667,
-            },
-        )
+        self.assertEqual(stats.count(), 4)
+        self.assertEqual(stats.max(), 4.0)
+        self.assertEqual(stats.mean(), 2.5)
+        self.assertEqual(stats.min(), 1.0)
+        self.assertAlmostEqual(stats.stdev(), 1.118033988749895)
+        self.assertAlmostEqual(stats.sampleStdev(), 1.2909944487358056)
+        self.assertEqual(stats.sum(), 10.0)
+        self.assertAlmostEqual(stats.variance(), 1.25)
+        self.assertAlmostEqual(stats.sampleVariance(), 1.6666666666666667)
+
+    def test_as_dict(self):
+        stats = self.sc.parallelize([1.0, 2.0, 3.0, 4.0]).stats().asDict()
+        self.assertEqual(stats["count"], 4)
+        self.assertEqual(stats["max"], 4.0)
+        self.assertEqual(stats["mean"], 2.5)
+        self.assertEqual(stats["min"], 1.0)
+        self.assertAlmostEqual(stats["stdev"], 1.2909944487358056)
+        self.assertEqual(stats["sum"], 10.0)
+        self.assertAlmostEqual(stats["variance"], 1.6666666666666667)
+
+        stats = self.sc.parallelize([1.0, 2.0, 3.0, 4.0]).stats().asDict(sample=True)
+        self.assertEqual(stats["count"], 4)
+        self.assertEqual(stats["max"], 4.0)
+        self.assertEqual(stats["mean"], 2.5)
+        self.assertEqual(stats["min"], 1.0)
+        self.assertAlmostEqual(stats["stdev"], 1.118033988749895)
+        self.assertEqual(stats["sum"], 10.0)
+        self.assertAlmostEqual(stats["variance"], 1.25)
 
     def test_merge(self):
         stats = StatCounter([1.0, 2.0, 3.0, 4.0])
         stats.merge(5.0)
-        self.assertDictEqual(
-            stats.asDict(),
-            {
-                "count": 5,
-                "max": 5.0,
-                "mean": 3.0,
-                "min": 1.0,
-                "stdev": 1.5811388300841898,
-                "sum": 15.0,
-                "variance": 2.5,
-            },
-        )
+        self.assertEqual(stats.count(), 5)
+        self.assertEqual(stats.max(), 5.0)
+        self.assertEqual(stats.mean(), 3.0)
+        self.assertEqual(stats.min(), 1.0)
+        self.assertAlmostEqual(stats.stdev(), 1.414213562373095)
+        self.assertAlmostEqual(stats.sampleStdev(), 1.5811388300841898)
+        self.assertEqual(stats.sum(), 15.0)
+        self.assertAlmostEqual(stats.variance(), 2.0)
+        self.assertAlmostEqual(stats.sampleVariance(), 2.5)
 
     def test_merge_stats(self):
         stats1 = StatCounter([1.0, 2.0, 3.0, 4.0])
         stats2 = StatCounter([1.0, 2.0, 3.0, 4.0])
-        merged = stats1.mergeStats(stats2)
-        self.assertDictEqual(
-            merged.asDict(),
-            {
-                "count": 8,
-                "max": 4.0,
-                "mean": 2.5,
-                "min": 1.0,
-                "stdev": 1.1952286093343936,
-                "sum": 20.0,
-                "variance": 1.4285714285714286,
-            },
-        )
+        stats = stats1.mergeStats(stats2)
+        self.assertEqual(stats.count(), 8)
+        self.assertEqual(stats.max(), 4.0)
+        self.assertEqual(stats.mean(), 2.5)
+        self.assertEqual(stats.min(), 1.0)
+        self.assertAlmostEqual(stats.stdev(), 1.118033988749895)
+        self.assertAlmostEqual(stats.sampleStdev(), 1.1952286093343936)
+        self.assertEqual(stats.sum(), 20.0)
+        self.assertAlmostEqual(stats.variance(), 1.25)
+        self.assertAlmostEqual(stats.sampleVariance(), 1.4285714285714286)
 
     def test_merge_stats_with_self(self):
         stats = StatCounter([1.0, 2.0, 3.0, 4.0])
         stats.mergeStats(stats)
-        self.assertDictEqual(
-            stats.asDict(),
-            {
-                "count": 8,
-                "max": 4.0,
-                "mean": 2.5,
-                "min": 1.0,
-                "stdev": 1.1952286093343936,
-                "sum": 20.0,
-                "variance": 1.4285714285714286,
-            },
-        )
+        self.assertEqual(stats.count(), 8)
+        self.assertEqual(stats.max(), 4.0)
+        self.assertEqual(stats.mean(), 2.5)
+        self.assertEqual(stats.min(), 1.0)
+        self.assertAlmostEqual(stats.stdev(), 1.118033988749895)
+        self.assertAlmostEqual(stats.sampleStdev(), 1.1952286093343936)
+        self.assertEqual(stats.sum(), 20.0)
+        self.assertAlmostEqual(stats.variance(), 1.25)
+        self.assertAlmostEqual(stats.sampleVariance(), 1.4285714285714286)
 
 
 if __name__ == "__main__":
+    import unittest
     from pyspark.tests.test_statcounter import *  # noqa: F401
 
     try:
