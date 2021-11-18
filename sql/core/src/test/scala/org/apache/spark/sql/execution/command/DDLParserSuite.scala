870d8396dfbd92895fa84e0c7d4f7058747799dd
Hyukjin Kwon <gurwls223@apache.org>
2021-11-18 11:40:31 +0900

    Update sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLParserSuite.scala

--- sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLParserSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLParserSuite.scala
@@ -360,7 +360,7 @@ class DDLParserSuite extends AnalysisTest with SharedSparkSession {
       LocalTempView)
     comparePlans(parsed2, expected2)
 
-    // TODO(SPARK-37367): Reeable exception test in DDLParserSuite.create view -- basic
+    // TODO(SPARK-37367): Reenable exception test in DDLParserSuite.create view -- basic
     // val v3 = "CREATE TEMPORARY VIEW a.b AS SELECT 1"
     // intercept(v3, "It is not allowed to add database prefix")
   }
