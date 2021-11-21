c6c662798b3cdee88a760cfeccd1be83ec7922f2
Khalid Mammadov <xmamedov@hotmail.com>
2021-11-21 09:04:08 +0000

    Remove duplicate line call in CI (#19728)

--- scripts/ci/testing/ci_run_airflow_testing.sh
+++ scripts/ci/testing/ci_run_airflow_testing.sh
@@ -40,7 +40,6 @@ function run_test_types_in_parallel() {
     do
         export TEST_TYPE
         mkdir -p "${PARALLEL_MONITORED_DIR}/${SEMAPHORE_NAME}/${TEST_TYPE}"
-        mkdir -p "${PARALLEL_MONITORED_DIR}/${SEMAPHORE_NAME}/${TEST_TYPE}"
         export JOB_LOG="${PARALLEL_MONITORED_DIR}/${SEMAPHORE_NAME}/${TEST_TYPE}/stdout"
         export PARALLEL_JOB_STATUS="${PARALLEL_MONITORED_DIR}/${SEMAPHORE_NAME}/${TEST_TYPE}/status"
         # Each test job will get SIGTERM followed by SIGTERM 200ms later and SIGKILL 200ms later after 45 mins
