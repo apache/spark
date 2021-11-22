0b6765150798799a418d39209b4e5f6d4a16276e
Hyukjin Kwon <gurwls223@apache.org>
2021-11-22 15:25:38 +0900

    Uses Python's standard string formatter for SQL API in pandas API on Spark

--- python/pyspark/pandas/sql_processor.py
+++ python/pyspark/pandas/sql_processor.py
@@ -43,142 +43,6 @@ def sql(
     locals: Optional[Dict[str, Any]] = None,
     **kwargs: Any,
 ) -> DataFrame:
-    """
-    Execute a SQL query and return the result as a pandas-on-Spark DataFrame.
-
-    This function also supports embedding Python variables (locals, globals, and parameters)
-    in the SQL statement by wrapping them in curly braces. See examples section for details.
-
-    In addition to the locals, globals and parameters, the function will also attempt
-    to determine if the program currently runs in an IPython (or Jupyter) environment
-    and to import the variables from this environment. The variables have the same
-    precedence as globals.
-
-    The following variable types are supported:
-
-        * string
-        * int
-        * float
-        * list, tuple, range of above types
-        * pandas-on-Spark DataFrame
-        * pandas-on-Spark Series
-        * pandas DataFrame
-
-    Parameters
-    ----------
-    query : str
-        the SQL query
-    index_col : str or list of str, optional
-        Column names to be used in Spark to represent pandas-on-Spark's index. The index name
-        in pandas-on-Spark is ignored. By default, the index is always lost.
-
-        .. note:: If you want to preserve the index, explicitly use :func:`DataFrame.reset_index`,
-            and pass it to the sql statement with `index_col` parameter.
-
-            For example,
-
-            >>> psdf = ps.DataFrame({"A": [1, 2, 3], "B":[4, 5, 6]}, index=['a', 'b', 'c'])
-            >>> psdf_reset_index = psdf.reset_index()
-            >>> ps.sql("SELECT * FROM {psdf_reset_index}", index_col="index")
-            ... # doctest: +NORMALIZE_WHITESPACE
-                   A  B
-            index
-            a      1  4
-            b      2  5
-            c      3  6
-
-            For MultiIndex,
-
-            >>> psdf = ps.DataFrame(
-            ...     {"A": [1, 2, 3], "B": [4, 5, 6]},
-            ...     index=pd.MultiIndex.from_tuples(
-            ...         [("a", "b"), ("c", "d"), ("e", "f")], names=["index1", "index2"]
-            ...     ),
-            ... )
-            >>> psdf_reset_index = psdf.reset_index()
-            >>> ps.sql("SELECT * FROM {psdf_reset_index}", index_col=["index1", "index2"])
-            ... # doctest: +NORMALIZE_WHITESPACE
-                           A  B
-            index1 index2
-            a      b       1  4
-            c      d       2  5
-            e      f       3  6
-
-            Also note that the index name(s) should be matched to the existing name.
-
-    globals : dict, optional
-        the dictionary of global variables, if explicitly set by the user
-    locals : dict, optional
-        the dictionary of local variables, if explicitly set by the user
-    kwargs
-        other variables that the user may want to set manually that can be referenced in the query
-
-    Returns
-    -------
-    pandas-on-Spark DataFrame
-
-    Examples
-    --------
-
-    Calling a built-in SQL function.
-
-    >>> ps.sql("select * from range(10) where id > 7")
-       id
-    0   8
-    1   9
-
-    A query can also reference a local variable or parameter by wrapping them in curly braces:
-
-    >>> bound1 = 7
-    >>> ps.sql("select * from range(10) where id > {bound1} and id < {bound2}", bound2=9)
-       id
-    0   8
-
-    You can also wrap a DataFrame with curly braces to query it directly. Note that when you do
-    that, the indexes, if any, automatically become top level columns.
-
-    >>> mydf = ps.range(10)
-    >>> x = range(4)
-    >>> ps.sql("SELECT * from {mydf} WHERE id IN {x}")
-       id
-    0   0
-    1   1
-    2   2
-    3   3
-
-    Queries can also be arbitrarily nested in functions:
-
-    >>> def statement():
-    ...     mydf2 = ps.DataFrame({"x": range(2)})
-    ...     return ps.sql("SELECT * from {mydf2}")
-    >>> statement()
-       x
-    0  0
-    1  1
-
-    Mixing pandas-on-Spark and pandas DataFrames in a join operation. Note that the index is
-    dropped.
-
-    >>> ps.sql('''
-    ...   SELECT m1.a, m2.b
-    ...   FROM {table1} m1 INNER JOIN {table2} m2
-    ...   ON m1.key = m2.key
-    ...   ORDER BY m1.a, m2.b''',
-    ...   table1=ps.DataFrame({"a": [1,2], "key": ["a", "b"]}),
-    ...   table2=pd.DataFrame({"b": [3,4,5], "key": ["a", "b", "b"]}))
-       a  b
-    0  1  3
-    1  2  4
-    2  2  5
-
-    Also, it is possible to query using Series.
-
-    >>> myser = ps.Series({'a': [1.0, 2.0, 3.0], 'b': [15.0, 30.0, 45.0]})
-    >>> ps.sql("SELECT * from {myser}")
-                        0
-    0     [1.0, 2.0, 3.0]
-    1  [15.0, 30.0, 45.0]
-    """
     if globals is None:
         globals = _get_ipython_scope()
     _globals = builtin_globals() if globals is None else dict(globals)
@@ -195,7 +59,7 @@ def sql(
     return SQLProcessor(_dict, query, default_session()).execute(index_col)
 
 
-_CAPTURE_SCOPES = 2
+_CAPTURE_SCOPES = 3
 
 
 def _get_local_scope() -> Dict[str, Any]:
@@ -268,26 +132,6 @@ class SQLProcessor(object):
         self._session = session
 
     def execute(self, index_col: Optional[Union[str, List[str]]]) -> DataFrame:
-        """
-        Returns a DataFrame for which the SQL statement has been executed by
-        the underlying SQL engine.
-
-        >>> str0 = 'abc'
-        >>> ps.sql("select {str0}")
-           abc
-        0  abc
-
-        >>> str1 = 'abc"abc'
-        >>> str2 = "abc'abc"
-        >>> ps.sql("select {str0}, {str1}, {str2}")
-           abc  abc"abc  abc'abc
-        0  abc  abc"abc  abc'abc
-
-        >>> strs = ['a', 'b']
-        >>> ps.sql("select 'a' in {strs} as cond1, 'c' in {strs} as cond2")
-           cond1  cond2
-        0   True  False
-        """
         blocks = _string.formatter_parser(self._statement)
         # TODO: use a string builder
         res = ""
@@ -354,33 +198,3 @@ class SQLProcessor(object):
         if isinstance(var, (tuple, range)):
             return self._convert_var(list(var))
         raise ValueError("Unsupported variable type {}: {}".format(type(var).__name__, str(var)))
-
-
-def _test() -> None:
-    import os
-    import doctest
-    import sys
-    from pyspark.sql import SparkSession
-    import pyspark.pandas.sql_processor
-
-    os.chdir(os.environ["SPARK_HOME"])
-
-    globs = pyspark.pandas.sql_processor.__dict__.copy()
-    globs["ps"] = pyspark.pandas
-    spark = (
-        SparkSession.builder.master("local[4]")
-        .appName("pyspark.pandas.sql_processor tests")
-        .getOrCreate()
-    )
-    (failure_count, test_count) = doctest.testmod(
-        pyspark.pandas.sql_processor,
-        globs=globs,
-        optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
-    )
-    spark.stop()
-    if failure_count:
-        sys.exit(-1)
-
-
-if __name__ == "__main__":
-    _test()
