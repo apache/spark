bdedc32843da4ae3a110aafbca37ce40a1219433
Angerszhuuuu <angers.zhu@gmail.com>
2021-11-23 16:12:02 +0800

    Update HiveClientImpl.scala

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -207,7 +207,7 @@ private[hive] class HiveClientImpl(
     } catch {
       // SPARK-37069: not all Hive versions have the above method (e.g., Hive 2.3.9 has it but
       // 2.3.8 don't), therefore here we fallback when encountering the exception.
-      case _: NoSuchMethodError =>
+      case _: NoSuchMethodException =>
         Hive.get(conf)
     }
   }
