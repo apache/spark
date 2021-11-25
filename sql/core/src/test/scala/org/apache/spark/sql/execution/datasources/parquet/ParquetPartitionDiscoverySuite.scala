2c69f14dbc755b6dd5d74c83b6e75fae46104293
Max Gekk <max.gekk@gmail.com>
2021-11-25 15:55:02 +0800

    [SPARK-36861][SQL] Use `yyyy-MM-dd` as the date pattern in partition discovery
    ### What changes were proposed in this pull request?
    In the PR, I propose to explicitly set the date pattern to `yyyy-MM-dd` while inferring types of partition values.
    
    ### Why are the changes needed?
    The existing date partition parser is much more tolerant to its input, and can skip some parts of date strings. For example, see SPARK-36861. As a consequence, it can loose some user's info (pieces of partition values).
    
    ### Does this PR introduce _any_ user-facing change?
    No. New behaviour introduced by https://github.com/apache/spark/pull/33709 hasn't released yet.
    
    ### How was this patch tested?
    By running the modified test suite:
    ```
    $ build/sbt "test:testOnly *ParquetV2PartitionDiscoverySuite"
    $ build/sbt "test:testOnly *ImageFileFormatSuite"
    ```
    
    Closes #34700 from MaxGekk/fix-infer-of-date-part.
    
    Authored-by: Max Gekk <max.gekk@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetPartitionDiscoverySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetPartitionDiscoverySuite.scala
@@ -1065,14 +1065,15 @@ abstract class ParquetPartitionDiscoverySuite
     }
   }
 
-  test("SPARK-23436: invalid Dates should be inferred as String in partition inference") {
+  test(
+    "SPARK-23436, SPARK-36861: invalid Dates should be inferred as String in partition inference") {
     withTempPath { path =>
-      val data = Seq(("1", "2018-41", "2018-01-01-04", "test"))
-        .toDF("id", "date_month", "date_hour", "data")
+      val data = Seq(("1", "2018-41", "2018-01-01-04", "2021-01-01T00", "test"))
+        .toDF("id", "date_month", "date_hour", "date_t_hour", "data")
 
-      data.write.partitionBy("date_month", "date_hour").parquet(path.getAbsolutePath)
+      data.write.partitionBy("date_month", "date_hour", "date_t_hour").parquet(path.getAbsolutePath)
       val input = spark.read.parquet(path.getAbsolutePath).select("id",
-        "date_month", "date_hour", "data")
+        "date_month", "date_hour", "date_t_hour", "data")
 
       assert(input.schema.sameType(input.schema))
       checkAnswer(input, data)
