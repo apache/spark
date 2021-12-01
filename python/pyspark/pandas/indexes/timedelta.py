ae3cd3be00da0512e0a3f0b202dbc0f340123f07
Xinrong Meng <xinrong.meng@databricks.com>
2021-12-01 13:53:10 +0800

    TODO SPARK-37512
    create from series/index

--- python/pyspark/pandas/indexes/timedelta.py
+++ python/pyspark/pandas/indexes/timedelta.py
@@ -56,6 +56,18 @@ class TimedeltaIndex(Index):
     >>> from datetime import timedelta
     >>> ps.TimedeltaIndex([timedelta(1), timedelta(microseconds=2)])
     TimedeltaIndex(['1 days 00:00:00', '0 days 00:00:00.000002'], dtype='timedelta64[ns]', freq=None)
+
+    From an Series:
+
+    >>> s = ps.Series([timedelta(1), timedelta(microseconds=2)], index=[10, 20])
+    >>> ps.TimedeltaIndex(s)
+    TimedeltaIndex(['1 days 00:00:00', '0 days 00:00:00.000002'], dtype='timedelta64[ns]', freq=None)
+
+    From an Index:
+
+    >>> idx = ps.TimedeltaIndex([timedelta(1), timedelta(microseconds=2)])
+    >>> ps.TimedeltaIndex(idx)
+    TimedeltaIndex(['1 days 00:00:00', '0 days 00:00:00.000002'], dtype='timedelta64[ns]', freq=None)
     """
 
     @no_type_check
