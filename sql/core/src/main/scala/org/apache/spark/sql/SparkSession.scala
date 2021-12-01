a7f4a51273bd1d5749f5c9c7a8322442ff8349e3
Angerszhuuuu <angers.zhu@gmail.com>
2021-12-02 07:59:52 +0800

    Update SparkSession.scala

--- sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
@@ -1060,7 +1060,7 @@ object SparkSession extends Logging {
 
   /**
    * Apply modifiable settings to an existed SparkSession. This method are used
-   * both in scala and pyspark, so put this under SparkSession object.
+   * both in scala and Pyspark, so put this under SparkSession object.
    */
   private[sql] def applyModifiableSettings(
       session: SparkSession,
