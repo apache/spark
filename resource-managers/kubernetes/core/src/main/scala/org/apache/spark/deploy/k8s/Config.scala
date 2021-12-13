fba219c50dd47103e0a5309069a4cc8862e38170
Dongjoon Hyun <dhyun@apple.com>
2021-12-13 01:25:37 -0800

    [SPARK-37622][K8S] Support K8s executor rolling policy
    ### What changes were proposed in this pull request?
    
    This PR aims to support K8s executor rolling policies via `spark.kubernetes.executor.rollPolicy`.
    
    ### Why are the changes needed?
    
    Users can choose one of the following policy in order to decommission one executor at every `spark.kubernetes.executor.rollInterval`.
    - ID: An executor with the smallest executor ID. This is most human-readable.
    - ADD_TIME: An executor with the smallest add-time. This is the most long-live executor.
    - TOTAL_GC_TIME: An executor with the biggest GC time. This could have GC issue for some reason.
    - TOTAL_DURATION: An executor with the biggest total task time. This executor may have busy neighbor or CPU thresholding by K8s controller.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, but this is a new feature.
    
    ### How was this patch tested?
    
    Pass the CIs.
    
    Closes #34874 from dongjoon-hyun/SPARK-37622.
    
    Lead-authored-by: Dongjoon Hyun <dhyun@apple.com>
    Co-authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/Config.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/Config.scala
@@ -16,6 +16,7 @@
  */
 package org.apache.spark.deploy.k8s
 
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 import org.apache.spark.deploy.k8s.Constants._
@@ -144,6 +145,25 @@ private[spark] object Config extends Logging {
       .timeConf(TimeUnit.SECONDS)
       .checkValue(_ >= 0, "Interval should be non-negative")
       .createWithDefault(0)
+
+  object ExecutorRollPolicy extends Enumeration {
+    val ID, ADD_TIME, TOTAL_GC_TIME, TOTAL_DURATION = Value
+  }
+
+  val EXECUTOR_ROLL_POLICY =
+    ConfigBuilder("spark.kubernetes.executor.rollPolicy")
+      .doc("Executor roll policy: Valid values are ID, ADD_TIME, TOTAL_GC_TIME (default), " +
+        "and TOTAL_DURATION. When executor roll happens, Spark uses this policy to choose " +
+        "an executor and decomission it. The built-in policies are based on executor summary." +
+        "ID policy chooses an executor with the smallest executor ID. " +
+        "ADD_TIME policy chooses an executor with the smallest add-time. " +
+        "TOTAL_GC_TIME policy chooses an executor with the biggest total task GC time. " +
+        "TOTAL_DURATION policy chooses an executor with the biggest total task time. ")
+      .version("3.3.0")
+      .stringConf
+      .transform(_.toUpperCase(Locale.ROOT))
+      .checkValues(ExecutorRollPolicy.values.map(_.toString))
+      .createWithDefault(ExecutorRollPolicy.TOTAL_GC_TIME.toString)
 
   val KUBERNETES_AUTH_DRIVER_CONF_PREFIX = "spark.kubernetes.authenticate.driver"
   val KUBERNETES_AUTH_EXECUTOR_CONF_PREFIX = "spark.kubernetes.authenticate.executor"
