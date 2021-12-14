e0c3eab254eff844e9a9793208dac3816993ba05
manuzhang <tianlzhang@ebay.com>
2021-12-14 23:14:11 +0800

    non-ansi cast

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/UnwrapCastInBinaryComparison.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/UnwrapCastInBinaryComparison.scala
@@ -306,12 +306,7 @@ object UnwrapCastInBinaryComparison extends Rule[LogicalPlan] {
     // decimal type), or that the literal `value` is within range `(min, max)`. For these, we
     // optimize by moving the cast to the literal side.
 
-    val newValue = try {
-      Cast(Literal(value), fromType).eval()
-    } catch {
-      case _: ArithmeticException => null
-      case ex: Throwable => throw ex
-    }
+    val newValue = Cast(Literal(value), fromType, ansiEnabled = false).eval()
     if (newValue == null) {
       // This means the cast failed, for instance, due to the value is not representable in the
       // narrower type. In this case we simply return the original expression.
