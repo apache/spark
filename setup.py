48b872ea95454ea16165297d7ea51ed5be61b306
Jarek Potiuk <jarek@potiuk.com>
2021-12-15 23:52:15 +0100

    Adds missing mypy types (#20324)
    This PR adds a few missing type stub packages that we have but so
    far MyPy did not complain about lack of those.
    
    Added by `mypy --install-types` command.
    
    Part of #19891

--- setup.py
+++ setup.py
@@ -521,6 +521,8 @@ zendesk = [
 # mypyd which does not support installing the types dynamically with --install-types
 mypy_dependencies = [
     'mypy==0.910',
+    'types-boto',
+    'types-certifi',
     'types-croniter',
     'types-docutils',
     'types-freezegun',
@@ -534,6 +536,7 @@ mypy_dependencies = [
     'types-setuptools',
     'types-termcolor',
     'types-tabulate',
+    'types-toml',
     'types-Markdown',
     'types-PyMySQL',
     'types-PyYAML',
