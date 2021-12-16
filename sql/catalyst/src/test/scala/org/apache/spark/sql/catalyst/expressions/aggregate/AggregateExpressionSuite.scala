4e026beab53dd8da8542b91a22ae258a13f45089
Jiaan Geng <beliefer@163.com>
2021-12-16 09:24:08 +0800

    Update code

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/aggregate/AggregateExpressionSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/aggregate/AggregateExpressionSuite.scala
@@ -18,9 +18,8 @@
 package org.apache.spark.sql.catalyst.expressions.aggregate
 
 import org.apache.spark.SparkFunSuite
-import org.apache.spark.sql.catalyst.analysis.{TypeCheckResult, UnresolvedAttribute}
-import org.apache.spark.sql.catalyst.expressions.{Add, AttributeSet, Literal}
-import org.apache.spark.sql.types.Decimal
+import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
+import org.apache.spark.sql.catalyst.expressions.{Add, AttributeSet}
 
 class AggregateExpressionSuite extends SparkFunSuite {
 
@@ -30,25 +29,6 @@ class AggregateExpressionSuite extends SparkFunSuite {
     val actual = AggregateExpression(Sum(Add(x, y)), mode = Complete, isDistinct = false).references
     val expected = AttributeSet(x :: y :: Nil)
     assert(expected == actual, s"Expected: $expected. Actual: $actual")
-  }
-
-  test("test regr_count input types") {
-    val checkResult1 = RegrCount(Literal("a"), Literal(1)).checkInputDataTypes()
-    assert(checkResult1.isInstanceOf[TypeCheckResult.TypeCheckFailure])
-    assert(checkResult1.asInstanceOf[TypeCheckResult.TypeCheckFailure].message
-      .contains("argument 1 requires numeric type, however, ''a'' is of string type"))
-    val checkResult2 = RegrCount(Literal(3.0D), Literal('b')).checkInputDataTypes()
-    assert(checkResult2.isInstanceOf[TypeCheckResult.TypeCheckFailure])
-    assert(checkResult2.asInstanceOf[TypeCheckResult.TypeCheckFailure].message
-      .contains("argument 2 requires numeric type, however, ''b'' is of string type"))
-    val checkResult3 = RegrCount(Literal(3.0D), Literal(Array(0))).checkInputDataTypes()
-    assert(checkResult3.isInstanceOf[TypeCheckResult.TypeCheckFailure])
-    assert(checkResult3.asInstanceOf[TypeCheckResult.TypeCheckFailure].message
-      .contains("argument 2 requires numeric type, however, '[0]' is of array<int> type"))
-    assert(RegrCount(Literal(3.0D), Literal(1)).checkInputDataTypes() ===
-      TypeCheckResult.TypeCheckSuccess)
-    assert(RegrCount(Literal(3.0D), Literal(Decimal(1))).checkInputDataTypes() ===
-      TypeCheckResult.TypeCheckSuccess)
   }
 
 }
