47b07002416a0db303bdff27d38933d8a0589930
zero323 <mszymkiewicz@gmail.com>
2021-12-17 13:44:10 +0100

    Ignore access to internal _values and dtype type mismatch

--- python/pyspark/pandas/indexes/base.py
+++ python/pyspark/pandas/indexes/base.py
@@ -545,7 +545,7 @@ class Index(IndexOpsMixin):
             "`to_numpy` loads all data into the driver's memory. "
             "It should only be used if the resulting NumPy ndarray is expected to be small."
         )
-        result = np.asarray(self._to_internal_pandas()._values, dtype=dtype)
+        result = np.asarray(self._to_internal_pandas()._values, dtype=dtype)  # type: ignore[arg-type,attr-defined]
         if copy:
             result = result.copy()
         return result
