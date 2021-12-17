8fefa0379fb270184a134946416564983a2ae602
yangjie01 <yangjie01@baidu.com>
2021-12-18 00:55:10 +0800

    refactor test case

--- core/src/test/scala/org/apache/spark/storage/DiskBlockObjectWriterSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/DiskBlockObjectWriterSuite.scala
@@ -188,15 +188,16 @@ class DiskBlockObjectWriterSuite extends SparkFunSuite with BeforeAndAfterEach {
   test("calling closeAndDelete() on a partial write file") {
     val (writer, file, writeMetrics) = createWriter()
 
-    writer.write(Long.box(20), Long.box(30))
-    writer.write(Long.box(10), Long.box(50))
+    for (i <- 1 to 1000) {
+      writer.write(i, i)
+    }
     val firstSegment = writer.commitAndGet()
     assert(firstSegment.length === file.length())
     assert(writeMetrics.bytesWritten === file.length())
 
-    writer.write(Long.box(40), Long.box(50))
-    writer.write(Long.box(30), Long.box(30))
-
+    for (i <- 1 to 500) {
+      writer.write(i, i)
+    }
     writer.closeAndDelete()
     assert(!file.exists())
     assert(writeMetrics.bytesWritten == 0)
