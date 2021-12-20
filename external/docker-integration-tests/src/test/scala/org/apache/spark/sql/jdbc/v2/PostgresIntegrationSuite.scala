a54c9b555f3d2289626cb36daa2ab89a858a7041
Jiaan Geng <beliefer@163.com>
2021-12-20 19:09:52 +0800

    Update code

--- external/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/PostgresIntegrationSuite.scala
+++ external/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/PostgresIntegrationSuite.scala
@@ -20,7 +20,8 @@ package org.apache.spark.sql.jdbc.v2
 import java.sql.Connection
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.AnalysisException
+import org.apache.spark.sql.{AnalysisException, DataFrame}
+import org.apache.spark.sql.catalyst.plans.logical.Aggregate
 import org.apache.spark.sql.execution.datasources.v2.jdbc.JDBCTableCatalog
 import org.apache.spark.sql.jdbc.{DatabaseOnDocker, DockerJDBCIntegrationSuite}
 import org.apache.spark.sql.types._
@@ -52,7 +53,26 @@ class PostgresIntegrationSuite extends DockerJDBCIntegrationSuite with V2JDBCTes
     .set("spark.sql.catalog.postgresql.pushDownTableSample", "true")
     .set("spark.sql.catalog.postgresql.pushDownLimit", "true")
 
-  override def dataPreparation(conn: Connection): Unit = {}
+  override def dataPreparation(conn: Connection): Unit = {
+    conn.prepareStatement("CREATE SCHEMA \"test\"").executeUpdate()
+    conn.prepareStatement("CREATE TYPE position_type AS (x NUMERIC(20, 2), y NUMERIC(20, 2))")
+      .executeUpdate()
+    conn.prepareStatement(
+      "CREATE TABLE \"test\".\"employee\" (dept INTEGER, name VARCHAR(32), salary NUMERIC(20, 2)," +
+        " bonus double precision, position position_type)")
+      .executeUpdate()
+    conn.prepareStatement(
+      "INSERT INTO \"test\".\"employee\" VALUES (1, 'amy', 10000, 1000, '(1, 2)')").executeUpdate()
+    conn.prepareStatement(
+      "INSERT INTO \"test\".\"employee\" VALUES (2, 'alex', 12000, 1200, '(1, 2)')").executeUpdate()
+    conn.prepareStatement(
+      "INSERT INTO \"test\".\"employee\" VALUES (1, 'cathy', 9000, 1200, '(2, 3)')").executeUpdate()
+    conn.prepareStatement(
+"INSERT INTO \"test\".\"employee\" VALUES (2, 'david', 10000, 1300, '(2, 4)')")
+      .executeUpdate()
+    conn.prepareStatement(
+      "INSERT INTO \"test\".\"employee\" VALUES (6, 'jen', 12000, 1200, '(3, 4))").executeUpdate()
+  }
 
   override def testUpdateColumnType(tbl: String): Unit = {
     sql(s"CREATE TABLE $tbl (ID INTEGER)")
@@ -84,4 +104,30 @@ class PostgresIntegrationSuite extends DockerJDBCIntegrationSuite with V2JDBCTes
   override def supportsIndex: Boolean = true
 
   override def indexOptions: String = "FILLFACTOR=70"
+
+  test("scan with aggregate push-down: group by with nested column") {
+    val df = sql("select position.x, MAX(employee.SALARY) FROM postgresql.test.employee" +
+      " where dept > 0 group by position.x")
+    checkAggregateRemoved(df)
+    val row = df.collect()
+    assert(row.length === 3)
+    assert(row(0).length === 2)
+    assert(row(0).getDouble(0) === 10000d)
+    assert(row(0).getDouble(1) === 20000d)
+    assert(row(1).getDouble(0) === 2500d)
+    assert(row(1).getDouble(1) === 5000d)
+    assert(row(2).getDouble(0) === 0d)
+    assert(row(2).isNullAt(1))
+  }
+
+  private def checkAggregateRemoved(df: DataFrame, removed: Boolean = true): Unit = {
+    val aggregates = df.queryExecution.optimizedPlan.collect {
+      case agg: Aggregate => agg
+    }
+    if (removed) {
+      assert(aggregates.isEmpty)
+    } else {
+      assert(aggregates.nonEmpty)
+    }
+  }
 }
