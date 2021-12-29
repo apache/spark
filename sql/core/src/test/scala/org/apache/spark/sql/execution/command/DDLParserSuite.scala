960e33ee01743f6cc44cc77908a52856c31a8d41
Angerszhuuuu <angers.zhu@gmail.com>
2021-12-29 10:18:20 +0800

    Update DDLParserSuite.scala

--- sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLParserSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/command/DDLParserSuite.scala
@@ -356,7 +356,7 @@ class DDLParserSuite extends AnalysisTest with SharedSparkSession {
 
     // TODO(SPARK-37367): Reenable exception test in DDLParserSuite.create view -- basic
     val v3 = "CREATE TEMPORARY VIEW a.b AS SELECT 1"
-    //    intercept(v3, "It is not allowed to add database prefix")
+    intercept(v3, "It is not allowed to add database prefix")
     val e = intercept[ParseException] {
       val plan = parser.parsePlan(v3)
       plan.explain(true)
