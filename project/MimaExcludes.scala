54a3ff67a3262b1344080eeaf61a7d475407c6e4
Angerszhuuuu <angers.zhu@gmail.com>
2021-12-30 17:18:02 +0800

    Update MimaExcludes.scala

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -43,7 +43,9 @@ object MimaExcludes {
     ProblemFilters.exclude[MissingTypesProblem]("org.apache.spark.executor.CoarseGrainedExecutorBackend$Arguments$"),
 
     // [SPARK-37391][SQL] JdbcConnectionProvider tells if it modifies security context
-    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.sql.jdbc.JdbcConnectionProvider.modifiesSecurityContext")
+    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.sql.jdbc.JdbcConnectionProvider.modifiesSecurityContext"),
+    // [SPARK-37786][SQL] [SPARK-37786][SQL] StreamingQueryListener support use SQLConf.get to get corresponding SessionState's SQLConf
+    ProblemFilters.exclude[DirectMissingMethodProblem]("org.apache.spark.sql.streaming.StreamingQueryManager.this")
   )
 
   // Exclude rules for 3.2.x from 3.1.1
