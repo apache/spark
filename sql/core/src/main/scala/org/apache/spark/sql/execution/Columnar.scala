94a69ffe0d26369004dcca8d9cb30057d2dcbf96
Dongjoon Hyun <dongjoon@apache.org>
2022-01-04 23:59:16 -0800

    Revert "[SPARK-37779][SQL] Make ColumnarToRowExec plan canonicalizable after (de)serialization"
    This reverts commit e17ab6e995a213e442e91df168e87fb724672613.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/Columnar.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/Columnar.scala
@@ -64,8 +64,7 @@ trait ColumnarToRowTransition extends UnaryExecNode
  * [[MapPartitionsInRWithArrowExec]]. Eventually this should replace those implementations.
  */
 case class ColumnarToRowExec(child: SparkPlan) extends ColumnarToRowTransition with CodegenSupport {
-  // supportsColumnar requires to be only called on driver side, see also SPARK-37779.
-  assert(TaskContext.get != null || child.supportsColumnar)
+  assert(child.supportsColumnar)
 
   override def output: Seq[Attribute] = child.output
 
