2096b316b02fb4baee41d9794799ae72aee7d88f
Zhenhua Wang <wzh_zju@163.com>
2022-01-08 00:23:51 +0800

    get sleep interval in task

--- core/src/test/scala/org/apache/spark/storage/BlockManagerDecommissionIntegrationSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/BlockManagerDecommissionIntegrationSuite.scala
@@ -141,25 +141,25 @@ class BlockManagerDecommissionIntegrationSuite extends SparkFunSuite with LocalS
     val input = sc.parallelize(1 to numParts, numParts)
     val accum = sc.longAccumulator("mapperRunAccumulator")
 
-    val sleepIntervalMs = whenToDecom match {
-      // Increase the window of time b/w task started and ended so that we can decom within that.
-      case TaskStarted => 10000
-      // Make one task take a really short time so that we can decommission right after it is
-      // done but before its peers are done.
-      case TaskEnded =>
-        if (TaskContext.getPartitionId() == 0) {
-          100
-        } else {
-          1000
-        }
-      // No sleep otherwise
-      case _ => 0
-    }
-
     // Create a new RDD where we have sleep in each partition, we are also increasing
     // the value of accumulator in each partition
     val baseRdd = input.mapPartitions { x =>
       accum.add(1)
+
+      val sleepIntervalMs = whenToDecom match {
+        // Increase the window of time b/w task started and ended so that we can decom within that.
+        case "TASK_STARTED" => 10000
+        // Make one task take a really short time so that we can decommission right after it is
+        // done but before its peers are done.
+        case "TASK_ENDED" =>
+          if (TaskContext.getPartitionId() == 0) {
+            100
+          } else {
+            1000
+          }
+        // No sleep otherwise
+        case _ => 0
+      }
       if (sleepIntervalMs > 0) {
         Thread.sleep(sleepIntervalMs)
       }
