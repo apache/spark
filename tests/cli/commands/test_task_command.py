a20d577ee70a61ca65550b84292b7da186a9e493
Khalid Mammadov <xmamedov@hotmail.com>
2022-01-14 07:59:04 +0000

    Fixing flaky task that fails in first check ocasionally (#19132)

--- tests/cli/commands/test_task_command.py
+++ tests/cli/commands/test_task_command.py
@@ -110,11 +110,14 @@ class TestCliTasks(unittest.TestCase):
 
         args = self.parser.parse_args(["tasks", "test", self.dag_id, task_id, DEFAULT_DATE.isoformat()])
 
-        with redirect_stdout(io.StringIO()) as stdout:
+        with self.assertLogs('airflow.models', level='INFO') as cm:
             task_command.task_test(args)
-
-        # Check that prints, and log messages, are shown
-        assert f"Marking task as SUCCESS. dag_id={self.dag_id}, task_id={task_id}" in stdout.getvalue()
+            assert any(
+                [
+                    f"Marking task as SUCCESS. dag_id={self.dag_id}, task_id={task_id}" in log
+                    for log in cm.output
+                ]
+            )
 
     @mock.patch("airflow.cli.commands.task_command.LocalTaskJob")
     def test_run_with_existing_dag_run_id(self, mock_local_job):
