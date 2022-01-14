9f577280ed4edc1258646676ec6e094c5fbe7dc0
ulysses-you <ulyssesyou18@gmail.com>
2022-01-14 14:56:15 +0800

    Improve RebalancePartitions in rules of Optimizer

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/PropagateEmptyRelationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/PropagateEmptyRelationSuite.scala
@@ -294,4 +294,20 @@ class PropagateEmptyRelationSuite extends PlanTest {
     val expected = LocalRelation.fromExternalRows(Seq('a.int, 'b.int, 'c.int), Nil)
     comparePlans(optimized, expected)
   }
+
+  test("SPARK-37904: Improve rebalance in PropagateEmptyRelation") {
+    val emptyRelation = LocalRelation($"a".int)
+
+    // test root node
+    val plan1 = emptyRelation.rebalance($"a").analyze
+    val optimized1 = Optimize.execute(plan1)
+    val expected1 = emptyRelation.analyze
+    comparePlans(optimized1, expected1)
+
+    // test non-root node
+    val plan2 = emptyRelation.rebalance($"a").where($"a" > 0).select($"a").analyze
+    val optimized2 = Optimize.execute(plan2)
+    val expected2 = emptyRelation.analyze
+    comparePlans(optimized2, expected2)
+  }
 }
