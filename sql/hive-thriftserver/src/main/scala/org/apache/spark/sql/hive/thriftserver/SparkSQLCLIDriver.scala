6e1a4283ba0e607a8d3f6aa85887fcd81109d3d8
Angerszhuuuu <angers.zhu@gmail.com>
2022-01-17 23:34:36 +0800

    Update SparkSQLCLIDriver.scala

--- sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver.scala
+++ sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIDriver.scala
@@ -616,12 +616,13 @@ private[hive] class SparkSQLCLIDriver extends CliDriver with Logging {
     // Check the last char is end of nested bracketed comment.
     val endOfBracketedComment = leavingBracketedComment && bracketedCommentLevel == 1
     // Spark SQL support simple comment and nested bracketed comment in query body.
-    // But if Spark SQL only received a comment, it will throw mismatched input exception.
+    // But if Spark SQL only received a comment, it will throw parser exception.
     // In Spark SQL CLI, if there is a completed comment in the end of whole query,
     // since Spark SQL CLL use `;` to split the query, CLI will pass the comment
     // to the backend engine and throw exception. CLI should ignore this comment,
     // If there is an uncompleted statement or an uncompleted bracketed comment in the end,
-    // CLI should also pass this part to the backend engine.
+    // CLI should also pass this part to the backend engine, which may throw an exception
+    // with clear error message.
     if (!endOfBracketedComment && (isStatement || insideBracketedComment)) {
       ret.add(line.substring(beginIndex))
     }
