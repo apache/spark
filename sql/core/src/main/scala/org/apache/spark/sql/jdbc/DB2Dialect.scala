7e5c3b216431b6a5e9a0786bf7cded694228cdee
Ivan Karol <reexar1@gmail.com>
2022-01-25 19:14:24 -0800

    [SPARK-30062][SQL] Add the IMMEDIATE statement to the DB2 dialect truncate implementation
    ### What changes were proposed in this pull request?
    I've added a DB2 specific truncate implementation that adds an IMMEDIATE statement at the end of the query.
    
    ### Why are the changes needed?
    I've encountered this issue myself while working with DB2 and trying to use truncate functionality.
    A quick google search shows that some people have also encountered this issue before:
    https://stackoverflow.com/questions/70027567/overwrite-mode-does-not-work-in-spark-sql-while-adding-data-in-db2
    https://issues.apache.org/jira/browse/SPARK-30062
    
    By looking into DB2 docs it becomes apparent that the IMMEDIATE statement is only optional if the table is column organized(though I'm not sure if it applies to all DB2 versions). So for the cases(such as mine) where the table is not column organized adding an IMMEDIATE statement becomes essential for the query to work.
    https://www.ibm.com/support/knowledgecenter/en/SSEPGG_11.5.0/com.ibm.db2.luw.sql.ref.doc/doc/r0053474.html
    
    Also, that might not be the best example, but I've found that DbVisualizer does add an IMMEDIATE statement at the end of the truncate command. Though, does it only for versions that are >=9.7
    https://fossies.org/linux/dbvis/resources/profiles/db2.xml (please look at line number 473)
    
    ### Does this PR introduce _any_ user-facing change?
    It should not, as even though the docs mention that if the TRUNCATE statement is executed in conjunction with IMMEDIATE, it has to be the first statement in the transaction, the JDBC connection that is established to execute the TRUNCATE statement has the auto-commit mode turned on. This means that there won't be any other query/statement executed prior within the same transaction.
    https://www.ibm.com/docs/en/db2/11.5?topic=statements-truncate (see the description for IMMEDIATE)
    https://github.com/apache/spark/blob/master/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcRelationProvider.scala#L49
    https://github.com/apache/spark/blob/master/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcRelationProvider.scala#L57
    https://github.com/apache/spark/blob/master/sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala#L108
    
    ### How was this patch tested?
    Existing test case with slightly adjusted logic.
    
    Closes #35283 from ikarol/SPARK-30062.
    
    Authored-by: Ivan Karol <reexar1@gmail.com>
    Signed-off-by: huaxingao <huaxin.gao11@gmail.com>

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/DB2Dialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/DB2Dialect.scala
@@ -66,6 +66,15 @@ private object DB2Dialect extends JdbcDialect {
   override def isCascadingTruncateTable(): Option[Boolean] = Some(false)
 
   // scalastyle:off line.size.limit
+  // See https://www.ibm.com/support/knowledgecenter/en/SSEPGG_11.5.0/com.ibm.db2.luw.sql.ref.doc/doc/r0053474.html
+  // scalastyle:on line.size.limit
+  override def getTruncateQuery(
+      table: String,
+      cascade: Option[Boolean] = isCascadingTruncateTable): String = {
+    s"TRUNCATE TABLE $table IMMEDIATE"
+  }
+
+  // scalastyle:off line.size.limit
   // See https://www.ibm.com/support/knowledgecenter/en/SSEPGG_11.5.0/com.ibm.db2.luw.sql.ref.doc/doc/r0000980.html
   // scalastyle:on line.size.limit
   override def renameTable(oldTable: String, newTable: String): String = {
