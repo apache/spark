e4b1984dc502e8be7a9832cce52b9501be4d7015
Jiaan Geng <beliefer@163.com>
2022-01-26 13:29:23 +0800

    [SPARK-37929][SQL][FOLLOWUP] Support cascade mode for JDBC V2
    ### What changes were proposed in this pull request?
    https://github.com/apache/spark/pull/35246 support `cascade` mode for dropNamespace API.
    This PR followup https://github.com/apache/spark/pull/35246 to make JDBC V2 respect `cascade`.
    
    ### Why are the changes needed?
    Let JDBC V2 respect `cascade`.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes.
    Users could manipulate `drop namespace` with `cascade` on JDBC V2.
    
    ### How was this patch tested?
    New tests.
    
    Closes #35271 from beliefer/SPARK-37929-followup.
    
    Authored-by: Jiaan Geng <beliefer@163.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- external/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/V2JDBCNamespaceTest.scala
+++ external/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/V2JDBCNamespaceTest.scala
@@ -17,20 +17,30 @@
 
 package org.apache.spark.sql.jdbc.v2
 
+import java.util
+import java.util.Collections
+
 import scala.collection.JavaConverters._
 
 import org.apache.logging.log4j.Level
 
 import org.apache.spark.sql.AnalysisException
-import org.apache.spark.sql.connector.catalog.NamespaceChange
+import org.apache.spark.sql.catalyst.analysis.NonEmptyNamespaceException
+import org.apache.spark.sql.connector.catalog.{Identifier, NamespaceChange}
 import org.apache.spark.sql.execution.datasources.v2.jdbc.JDBCTableCatalog
 import org.apache.spark.sql.jdbc.DockerIntegrationFunSuite
 import org.apache.spark.sql.test.SharedSparkSession
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 import org.apache.spark.tags.DockerTest
 
 @DockerTest
 private[v2] trait V2JDBCNamespaceTest extends SharedSparkSession with DockerIntegrationFunSuite {
   val catalog = new JDBCTableCatalog()
+
+  private val emptyProps: util.Map[String, String] = Collections.emptyMap[String, String]
+  private val schema: StructType = new StructType()
+    .add("id", IntegerType)
+    .add("data", StringType)
 
   def builtinNamespaces: Array[Array[String]]
 
@@ -59,5 +69,27 @@ private[v2] trait V2JDBCNamespaceTest extends SharedSparkSession with DockerInte
       catalog.listNamespaces(Array("foo"))
     }.getMessage
     assert(msg.contains("Namespace 'foo' not found"))
+  }
+
+  test("Drop namespace") {
+    val ident1 = Identifier.of(Array("foo"), "tab")
+    // Drop empty namespace without cascade
+    catalog.createNamespace(Array("foo"), Map("comment" -> "test comment").asJava)
+    assert(catalog.namespaceExists(Array("foo")) === true)
+    catalog.dropNamespace(Array("foo"), cascade = false)
+    assert(catalog.namespaceExists(Array("foo")) === false)
+
+    // Drop non empty namespace without cascade
+    catalog.createNamespace(Array("foo"), Map("comment" -> "test comment").asJava)
+    assert(catalog.namespaceExists(Array("foo")) === true)
+    catalog.createTable(ident1, schema, Array.empty, emptyProps)
+    intercept[NonEmptyNamespaceException] {
+      catalog.dropNamespace(Array("foo"), cascade = false)
+    }
+
+    // Drop non empty namespace with cascade
+    assert(catalog.namespaceExists(Array("foo")) === true)
+    catalog.dropNamespace(Array("foo"), cascade = true)
+    assert(catalog.namespaceExists(Array("foo")) === false)
   }
 }
