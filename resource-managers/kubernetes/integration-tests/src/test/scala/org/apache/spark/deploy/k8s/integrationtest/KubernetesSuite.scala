f05f2c2e7f3a5939156f12576f0a8097cdec9772
Dongjoon Hyun <dongjoon@apache.org>
2022-02-01 08:30:39 -0800

    [SPARK-38081][K8S][TESTS] Support `cloud`-backend in K8s IT with SBT
    This PR aims to
    - Support `cloud` backend in K8s IT with SBT (Image building/pushing/testing)
    - Add a new K8s test tag, `local`, and apply it to a test case using local HTTP server.
    
    To run K8s IT in the cloud environment more easily.
    
    No
    
    Manually test like the following.
    ```
    $ build/sbt -Psparkr -Pkubernetes -Pkubernetes-integration-tests -Dtest.exclude.tags=minikube,local -Dspark.kubernetes.test.deployMode=cloud -Dspark.kubernetes.test.master=k8s://....eks.amazonaws.com -Dspark.kubernetes.test.namespace=spark-cloud-test -Dspark.kubernetes.test.imageRepo=... -Dspark.kubernetes.test.imageTag=2022-02-01 "kubernetes-integration-tests/test"
    
    ...
    [info] KubernetesSuite:
    [info] - Run SparkPi with no resources (26 seconds, 678 milliseconds)
    [info] - Run SparkPi with no resources & statefulset allocation (18 seconds, 617 milliseconds)
    [info] - Run SparkPi with a very long application name. (17 seconds, 205 milliseconds)
    [info] - Use SparkLauncher.NO_RESOURCE (17 seconds, 555 milliseconds)
    [info] - Run SparkPi with a master URL without a scheme. (17 seconds, 478 milliseconds)
    [info] - Run SparkPi with an argument. (17 seconds, 518 milliseconds)
    [info] - Run SparkPi with custom labels, annotations, and environment variables. (17 seconds, 648 milliseconds)
    [info] - All pods have the same service account by default (17 seconds, 800 milliseconds)
    [info] - Run extraJVMOptions check on driver (11 seconds, 141 milliseconds)
    [info] - Verify logging configuration is picked from the provided SPARK_CONF_DIR/log4j2.properties (25 seconds, 608 milliseconds)
    [info] - Run SparkPi with env and mount secrets. (27 seconds, 114 milliseconds)
    [info] - Run PySpark on simple pi.py example (42 seconds, 929 milliseconds)
    [info] - Run PySpark to test a pyfiles example (19 seconds, 914 milliseconds)
    [info] - Run PySpark with memory customization (16 seconds, 985 milliseconds)
    [info] - Run in client mode. (10 seconds, 42 milliseconds)
    [info] - Start pod creation from template (16 seconds, 207 milliseconds)
    [info] - Test basic decommissioning (49 seconds, 519 milliseconds)
    [info] - Test basic decommissioning with shuffle cleanup (49 seconds, 472 milliseconds)
    [info] - Test decommissioning with dynamic allocation & shuffle cleanups (2 minutes, 49 seconds)
    [info] - Test decommissioning timeouts (50 seconds, 423 milliseconds)
    [info] - SPARK-37576: Rolling decommissioning (1 minute, 13 seconds)
    [info] - Run SparkR on simple dataframe.R example (51 seconds, 712 milliseconds)
    [info] Run completed in 15 minutes, 50 seconds.
    [info] Total number of tests run: 22
    [info] Suites: completed 1, aborted 0
    [info] Tests: succeeded 22, failed 0, canceled 0, ignored 0, pending 0
    [info] All tests passed.
    [success] Total time: 1920 s (32:00), completed Jan 31, 2022 11:56:38 PM
    ```
    
    Closes #35376 from dongjoon-hyun/SPARK-38081.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit c37c6c393dc0302abd41b0c4c41002710ba52270)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesSuite.scala
@@ -567,6 +567,7 @@ class KubernetesSuite extends SparkFunSuite
 private[spark] object KubernetesSuite {
   val k8sTestTag = Tag("k8s")
   val pvTestTag = Tag("persistentVolume")
+  val localTestTag = Tag("local")
   val rTestTag = Tag("r")
   val MinikubeTag = Tag("minikube")
   val SPARK_PI_MAIN_CLASS: String = "org.apache.spark.examples.SparkPi"
