112660248979c8c7fe2bfe81d4a1473ed263aea4
Yikun Jiang <yikunkero@gmail.com>
2022-02-07 20:51:47 +0800

    update config doc and remove redundant deps

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/Config.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/Config.scala
@@ -20,6 +20,7 @@ import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 import org.apache.spark.deploy.k8s.Constants._
+import org.apache.spark.deploy.k8s.features.scheduler.VolcanoFeatureStep
 import org.apache.spark.internal.Logging
 import org.apache.spark.internal.config.{PYSPARK_DRIVER_PYTHON, PYSPARK_PYTHON}
 import org.apache.spark.internal.config.ConfigBuilder
@@ -678,14 +679,15 @@ private[spark] object Config extends Logging {
 
   val KUBERNETES_JOB_MIN_MEMORY = ConfigBuilder("spark.kubernetes.job.min.memory")
     .doc("The minimum memory for running the job, in MiB unless otherwise specified. This only " +
-      "applicable when you enable `VolcanoFeatureStep` feature step in" +
+      s"applicable when you enable `${classOf[VolcanoFeatureStep].getName}` feature step in" +
       " `spark.kubernetes.driver.pod.featureSteps`.")
     .version("3.3.0")
     .bytesConf(ByteUnit.MiB)
     .createWithDefaultString("3g")
   val KUBERNETES_JOB_MIN_CPU = ConfigBuilder("spark.kubernetes.job.min.cpu")
-    .doc("The minimum CPU for running the job. This only applicable when you enable " +
-      "`VolcanoFeatureStep` feature step in `spark.kubernetes.driver.pod.featureSteps`.")
+    .doc(s"The minimum CPU for running the job. This only applicable when you enable " +
+      s"`${classOf[VolcanoFeatureStep].getName}` feature step in " +
+      "`spark.kubernetes.driver.pod.featureSteps`.")
     .version("3.3.0")
     .doubleConf
     .createWithDefault(2.0)
