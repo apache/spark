d946bb1aa278bd8b05bc3f220f48a580fa6143a6
pralabhkumar <pralabhkumar@gmail.com>
2022-02-07 19:40:38 +0530

    Implemented the suggested changes

--- python/pyspark/pandas/series.py
+++ python/pyspark/pandas/series.py
@@ -5254,127 +5254,32 @@ class Series(Frame, IndexOpsMixin, Generic[T]):
         index_scol = self._internal.index_spark_columns[0]
         index_type = self._internal.spark_type_for(index_scol)
 
-        # e.g where = [10, 20]
-        # In the comments below , will explain how the dataframe will look after transformations.
-        # e.g pd.Series([2, 1, np.nan, 4], index=[10, 20, 30, 40], name="Koalas")
-
-        column_prefix_constant = "col_"
         cond = [
-            F.when(
-                index_scol <= SF.lit(index).cast(index_type),
-                F.struct(
-                    F.lit(column_prefix_constant + str(index) + "_" + str(idx)).alias("identifier"),
-                    self.spark.column.alias("col_value"),
-                ),
-            ).alias(column_prefix_constant + str(index) + "_" + str(idx))
-            for idx, index in enumerate(where)
+            F.last(
+                F.when(index_scol <= SF.lit(index).cast(index_type), self.spark.column),
+                ignorenulls=True,
+            )
+            for index in where
         ]
-        cond.append(index_scol)
-
-        #
-        # +---------------+---------------+-----------------+
-        # |       col_10_0|       col_20_1|__index_level_0__|
-        # +---------------+---------------+-----------------+
-        # |{col_10_0, 2.0}|{col_20_1, 2.0}|               10|
-        # |           null|{col_20_1, 1.0}|               20|
-        # |           null|           null|               30|
-        # |           null|           null|               40|
-        # +---------------+---------------+-----------------+
-
         sdf = self._internal.spark_frame.select(cond)
-        columns = [cols for cols in sdf.columns if cols.startswith("col")]
-
-        # +-----------------+----------------------------------+
-        # |__index_level_0__|combined_cols                     |
-        # +-----------------+----------------------------------+
-        # |10               |[{col_10_0, 2.0}, {col_20_1, 2.0}]|
-        # |20               |[null, {col_20_1, 1.0}]           |
-        # |30               |[null, null]                      |
-        # |40               |[null, null]                      |
-        # +-----------------+----------------------------------+
-
-        sdf = sdf.withColumn("combined_cols", F.array(columns)).drop(*columns)
-        # After explode
-        # +-----------------+---------------+
-        # |__index_level_0__|         values|
-        # +-----------------+---------------+
-        # |               10|{col_10_0, 2.0}|
-        # |               10|{col_20_1, 2.0}|
-        # |               20|           null|
-        # |               20|{col_20_1, 1.0}|
-        # |               30|           null|
-        # |               30|           null|
-        # |               40|           null|
-        # |               40|           null|
-        # +-----------------+---------------+
-        #  Final sdf after below transformations
-        # +-----------------+----------+-----+
-        # |__index_level_0__|identifier|value|
-        # +-----------------+----------+-----+
-        # |10               |col_10_0  |2.0  |
-        # |10               |col_20_1  |2.0  |
-        # |20               |col_20_1  |1.0  |
-        # +-----------------+----------+-----+
-
-        sdf = (
-            sdf.select(index_scol, F.explode("combined_cols").alias("values"))
-            .withColumn("identifier", F.col("values.identifier"))
-            .withColumn("value", F.col("values.col_value"))
-            .drop("values")
-            .dropna(subset="value")
-        )
-        window_specification = Window.partitionBy("identifier").orderBy(index_scol.desc())
-
-        # After row number and window
-        # +-----------------+----------+-----+----------+
-        # |__index_level_0__|identifier|value|row_number|
-        # +-----------------+----------+-----+----------+
-        # |               10|  col_10_0|  2.0|         1|
-        # |               20|  col_20_1|  1.0|         1|
-        # |               10|  col_20_1|  2.0|         2|
-        # +-----------------+----------+-----+----------+
-        #  Final sdf value after below transformation
-        # +----------+-----+
-        # |identifier|value|
-        # +----------+-----+
-        # |col_10_0  |2.0  |
-        # |col_20_1  |1.0  |
-        # +----------+-----+
-
-        sdf = (
-            sdf.withColumn("row_number", F.row_number().over(window_specification))
-            .filter("row_number == 1")
-            .drop(index_scol)
-            .drop("row_number")
-        )
-
-        # Number of rows would be less, since only one row per index.  so collect can be done
-        calculated_values = sdf.collect()
-
-        if len(calculated_values) == 0 and not should_return_series:
-            return np.nan
-
         if not should_return_series:
             with sql_conf({SPARK_CONF_ARROW_ENABLED: False}):
                 # Disable Arrow to keep row ordering.
-                result = cast(pd.DataFrame, sdf.limit(1).toPandas()).iloc[0, 1]
+                result = cast(pd.DataFrame, sdf.limit(1).toPandas()).iloc[0, 0]
             return result if result is not None else np.nan
 
-        calculated_values_map = {data.identifier: data.value for data in calculated_values}
-
-        complete_values = []
-        if len(calculated_values) == 0:
-            complete_values = [np.nan] * len(where)
-        else:
-            for idx, data in enumerate(where):
-                key = column_prefix_constant + str(data) + "_" + str(idx)
-                if key in calculated_values_map:
-                    complete_values.append(calculated_values_map[key])
-                else:
-                    complete_values.append(None)
-
-        df = pd.DataFrame(data=complete_values, columns=[self.name], index=where)
-        return df[df.columns[0]]
+        # The data is expected to be small so it's fine to transpose/use default index.
+        with ps.option_context("compute.default_index_type", "distributed", "compute.max_rows", 1):
+            if len(where) == len(set(where)):
+                psdf: DataFrame = DataFrame(sdf)
+                psdf.columns = pd.Index(where)
+                return first_series(psdf.transpose()).rename(self.name)
+            else:
+                # If `where` has duplicate items, leverage the pandas directly
+                # since pandas API on Spark doesn't support the duplicate column name.
+                pdf: pd.DataFrame = sdf.limit(1).toPandas()
+                pdf.columns = pd.Index(where)
+                return first_series(DataFrame(pdf.transpose())).rename(self.name)
 
     def mad(self) -> float:
         """
