7887b43494e0049712b2c3da2727aa4b01c87b30
Xinrong Meng <xinrong.meng@databricks.com>
2022-02-09 12:23:58 +0800

    SQLQueryTest input

--- sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
@@ -135,3 +135,10 @@ select to_number('-454', '-000');
 select to_number('-454', 'S000');
 select to_number('12,454.8-', '00,000.9-');
 select to_number('00,454.8-', '00,000.9-');
+
+-- to_binary
+select to_binary('abc');
+select to_binary('abc', 'utf-8');
+select to_binary('abc', 'base64');
+select to_binary('abc', 'base2');
+select to_binary('abc', 'hex');
