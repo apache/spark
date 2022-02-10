c22ac3c4ab7b566d97c5767cce99ca04190243b3
Xinrong Meng <xinrong.meng@databricks.com>
2022-02-10 14:50:33 +0800

    foldable/case-insensitive format test

--- sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
+++ sql/core/src/test/resources/sql-tests/inputs/string-functions.sql
@@ -142,3 +142,7 @@ select to_binary('abc', 'utf-8');
 select to_binary('abc', 'base64');
 select to_binary('abc', 'base2');
 select to_binary('abc', 'hex');
+select to_binary('abc', concat('utf', '-8'));
+select to_binary('abc', concat('base', '64'));
+select to_binary('abc', 'Hex');
+select to_binary('abc', 'UTF-8');
