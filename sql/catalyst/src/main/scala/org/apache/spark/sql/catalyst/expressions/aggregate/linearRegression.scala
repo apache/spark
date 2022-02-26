4ddd4c21122b66b58927ab2ef6ab3f9c619ed92c
Jiaan Geng <beliefer@163.com>
2022-02-26 09:19:04 +0800

    Update code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/linearRegression.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/aggregate/linearRegression.scala
@@ -20,7 +20,7 @@ package org.apache.spark.sql.catalyst.expressions.aggregate
 import org.apache.spark.sql.catalyst.dsl.expressions._
 import org.apache.spark.sql.catalyst.expressions.{And, AttributeReference, EqualTo, Expression, ExpressionDescription, If, ImplicitCastInputTypes, IsNotNull, IsNull, Literal, Or, RuntimeReplaceableAggregate}
 import org.apache.spark.sql.catalyst.trees.BinaryLike
-import org.apache.spark.sql.types.{AbstractDataType, DataType, DoubleType, NumericType}
+import org.apache.spark.sql.types.{AbstractDataType, DataType, DoubleType, LongType, NumericType}
 
 // scalastyle:off line.size.limit
 @ExpressionDescription(
@@ -238,31 +238,31 @@ case class RegrSYY(
     this.copy(left = newLeft, right = newRight)
 }
 
-abstract class Regression(val left: Expression, val right: Expression)
+abstract class Regression
   extends DeclarativeAggregate with ImplicitCastInputTypes with BinaryLike[Expression] {
 
   override def nullable: Boolean = true
   override def dataType: DataType = DoubleType
   override def inputTypes: Seq[AbstractDataType] = Seq(DoubleType, DoubleType)
 
-  protected val count = AttributeReference("count", DoubleType, nullable = false)()
-  protected val meanX = AttributeReference("meanX", DoubleType, nullable = false)()
-  protected val meanY = AttributeReference("meanY", DoubleType, nullable = false)()
-  protected val c2 = AttributeReference("c2", DoubleType, nullable = false)()
-  protected val m2X = AttributeReference("m2X", DoubleType, nullable = false)()
+  protected val count = AttributeReference("count", LongType)()
+  protected val meanX = AttributeReference("meanX", DoubleType)()
+  protected val meanY = AttributeReference("meanY", DoubleType)()
+  protected val c2 = AttributeReference("c2", DoubleType)()
+  protected val m2X = AttributeReference("m2X", DoubleType)()
 
   override val aggBufferAttributes: Seq[AttributeReference] = Seq(count, meanX, meanY, c2, m2X)
 
-  override val initialValues: Seq[Expression] = Array.fill(5)(Literal(0.0))
+  override val initialValues: Seq[Expression] = Literal(0L) +: Array.fill(4)(Literal(0.0))
 
   override lazy val updateExpressions: Seq[Expression] = {
-    lazy val newCount = count + 1.0
-    lazy val oldMeanX = meanX
-    lazy val newMeanX = oldMeanX + (right - oldMeanX) / newCount
-    lazy val oldMeanY = meanY
-    lazy val newMeanY = oldMeanY + (left - oldMeanY) / newCount
-    lazy val newC2 = c2 + (right - oldMeanX) * (left - newMeanY)
-    lazy val newM2X = m2X + (right - oldMeanX) * (right - newMeanX)
+    val newCount = count + 1L
+    val oldMeanX = meanX
+    val newMeanX = oldMeanX + (right - oldMeanX) / newCount
+    val oldMeanY = meanY
+    val newMeanY = oldMeanY + (left - oldMeanY) / newCount
+    val newC2 = c2 + (right - oldMeanX) * (left - newMeanY)
+    val newM2X = m2X + (right - oldMeanX) * (right - newMeanX)
 
     val isNull = left.isNull || right.isNull
     Seq(
@@ -275,24 +275,20 @@ abstract class Regression(val left: Expression, val right: Expression)
   }
 
   override val mergeExpressions: Seq[Expression] = {
-    lazy val count1 = count.left
-    lazy val count2 = count.right
-    lazy val newCount = count1 + count2
-    lazy val newM2X =
-      m2X.left + m2X.right + count1 * count2 * Pow(meanX.left - meanX.right, 2) / newCount
-    lazy val deltaX = meanX.right - meanX.left
-    lazy val deltaY = meanY.right - meanY.left
-    lazy val newC2 = c2.left + c2.right + deltaX * deltaY * count1 * count2 / newCount
-    lazy val newMeanX = meanX.left + deltaX * count2 / newCount
-    lazy val newMeanY = meanY.left + deltaY * count2 / newCount
+    val count1 = count.left
+    val count2 = count.right
+    val newCount = count1 + count2
+    val multiplyCount = count1.cast(dataType) * count2
+    val newM2X =
+      m2X.left + m2X.right + multiplyCount * Pow(meanX.left - meanX.right, 2) / newCount
+    val deltaX = meanX.right - meanX.left
+    val deltaY = meanY.right - meanY.left
+    val newC2 = c2.left + c2.right + deltaX * deltaY * multiplyCount / newCount
+    val newMeanX = meanX.left + deltaX * count2 / newCount
+    val newMeanY = meanY.left + deltaY * count2 / newCount
 
     Seq(newCount, newMeanX, newMeanY, newC2, newM2X)
   }
-
-  override lazy val evaluateExpression: Expression =
-    If(EqualTo(count, Literal(0.0)), Literal(null, dataType), doEvaluateExpression)
-
-  protected def doEvaluateExpression: Expression
 }
 
 // scalastyle:off line.size.limit
@@ -306,17 +302,15 @@ abstract class Regression(val left: Expression, val right: Expression)
        NULL
       > SELECT _FUNC_(y, x) FROM VALUES (null, 1) AS tab(y, x);
        NULL
-  """,
+   """,
   group = "agg_funcs",
   since = "3.3.0")
 // scalastyle:on line.size.limit
-case class RegrSlope(
-    override val left: Expression,
-    override val right: Expression) extends Regression(left, right) {
+case class RegrSlope(left: Expression, right: Expression) extends Regression {
+  override val evaluateExpression = If(count == 0L, Literal(null, dataType), c2 / m2X)
   override def prettyName: String = "regr_slope"
-  override val doEvaluateExpression = c2 / m2X
   override protected def withNewChildrenInternal(
-                                                  newLeft: Expression, newRight: Expression): RegrSlope =
+      newLeft: Expression, newRight: Expression): RegrSlope =
     copy(left = newLeft, right = newRight)
 }
 
@@ -331,19 +325,17 @@ case class RegrSlope(
        NULL
       > SELECT _FUNC_(y, x) FROM VALUES (null, 1) AS tab(y, x);
        NULL
-  """,
+   """,
   group = "agg_funcs",
   since = "3.3.0")
 // scalastyle:on line.size.limit
-case class RegrIntercept(
-    override val left: Expression,
-    override val right: Expression) extends Regression(left, right) {
-  override def prettyName: String = "regr_intercept"
-  override val doEvaluateExpression = {
-    val slope = c2 / m2X
-    meanY - slope * meanX
+case class RegrIntercept(left: Expression, right: Expression) extends Regression {
+  override val evaluateExpression = {
+    lazy val slope = c2 / m2X
+    If(count == 0L, Literal(null, dataType), meanY - slope * meanX)
   }
+  override def prettyName: String = "regr_intercept"
   override protected def withNewChildrenInternal(
-                                                  newLeft: Expression, newRight: Expression): RegrIntercept =
+      newLeft: Expression, newRight: Expression): RegrIntercept =
     copy(left = newLeft, right = newRight)
 }
