09c898a0027f6a9aac7cb92e81daa630b1c13c46
Max Gekk <max.gekk@gmail.com>
2022-03-01 15:06:25 +0300

    Re-gen timestamp-ntz.sql.out

--- sql/core/src/test/resources/sql-tests/inputs/timestamp-ntz.sql
+++ sql/core/src/test/resources/sql-tests/inputs/timestamp-ntz.sql
@@ -20,6 +20,6 @@ SELECT convert_timezone('Europe/Moscow', 'America/Los_Angeles', timestamp_ntz'20
 
 -- Get the difference between timestamps w/o time zone in the specified units
 select timestampdiff('QUARTER', timestamp_ntz'2022-01-01 01:02:03', timestamp_ntz'2022-05-02 05:06:07');
-select timestampdiff(HOUR, timestamp_ntz'2022-02-14 01:02:03', timestamp'2022-02-14 02:03:04');
+select timestampdiff(HOUR, timestamp_ntz'2022-02-14 01:02:03', timestamp_ltz'2022-02-14 02:03:04');
 select timestampdiff(YEAR, date'2022-02-15', timestamp_ntz'2023-02-15 10:11:12');
 select timestampdiff('MILLISECOND', timestamp_ntz'2022-02-14 23:59:59.123', date'2022-02-15');
