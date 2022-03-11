09963047d7e8368bd85db1824555b51cad02c936
Jiawei Li <jiawei.li@kyligence.io>
2022-03-11 14:46:43 +0800

    KE-31638 add parameter in request head to proxy spark ui (#424)
    * KE-31638 add parameter in head to proxy spark ui

--- core/src/main/scala/org/apache/spark/ui/UIUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/UIUtils.scala
@@ -204,6 +204,13 @@ private[spark] object UIUtils extends Logging {
 
   // Yarn has to go through a proxy so the base uri is provided and has to be on all links
   def uiRoot(request: HttpServletRequest): String = {
+
+    // for KE proxy spark ui
+    val kylinProxyPath = request.getHeader("X-Kylin-Proxy-Path")
+    if (kylinProxyPath != null) {
+      return kylinProxyPath
+    }
+
     // Knox uses X-Forwarded-Context to notify the application the base path
     val knoxBasePath = Option(request.getHeader("X-Forwarded-Context"))
     // SPARK-11484 - Use the proxyBase set by the AM, if not found then use env.
@@ -211,6 +218,7 @@ private[spark] object UIUtils extends Logging {
       .orElse(sys.env.get("APPLICATION_WEB_PROXY_BASE"))
       .orElse(knoxBasePath)
       .getOrElse("")
+
   }
 
   def prependBaseUri(
