5796f66f764f4ad7c4daf2f28122d00ad4763dca
Bruce Robbins <bersprockets@gmail.com>
2022-03-11 16:25:29 -0800

    Update test name

--- sql/core/src/test/scala/org/apache/spark/sql/GeneratorFunctionSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/GeneratorFunctionSuite.scala
@@ -358,7 +358,7 @@ class GeneratorFunctionSuite extends QueryTest with SharedSparkSession {
     checkAnswer(df.select(explode(array(min($"v"), max($"v")))), Row(1) :: Row(3) :: Nil)
   }
 
-  test("generator in stream of aggregate expressions") {
+  test("SPARK-38528: generator in stream of aggregate expressions") {
     val df = Seq(1, 2, 3).toDF("v")
     checkAnswer(
       df.select(Stream(explode(array(min($"v"), max($"v"))), sum($"v")): _*),
