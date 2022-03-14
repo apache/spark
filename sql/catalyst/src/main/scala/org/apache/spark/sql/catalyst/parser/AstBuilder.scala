35536a1478020fbd844703517b34d655f89906b2
Yuto Akutsu <yuto.akutsu@oss.nttdata.com>
2022-03-14 09:11:58 +0300

    [SPARK-38103][SQL] Migrate parsing errors of transform into the new error framework
    ### What changes were proposed in this pull request?
    
    In this PR, I migrated parsing errors of transform listed below into the new error framework.
    
    - transformNotSupportQuantifierError
    - transformWithSerdeUnsupportedError
    - tooManyArgumentsForTransformError
    - notEnoughArgumentsForTransformError
    - invalidTransformArgumentError
    
    ### Why are the changes needed?
    
    Porting the parsing errors of transform into the new error framework should improve user experience with Spark SQL.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    `$ build/sbt "test:testOnly *QueryParsingErrorsSuite"`
    
    Closes #35759 from yutoacts/SPARK-38103.
    
    Authored-by: Yuto Akutsu <yuto.akutsu@oss.nttdata.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilder.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AstBuilder.scala
@@ -3024,7 +3024,7 @@ class AstBuilder extends SqlBaseParserBaseVisitor[AnyRef] with SQLConfHelper wit
       if (arguments.size > 1) {
         throw QueryParsingErrors.tooManyArgumentsForTransformError(name, ctx)
       } else if (arguments.isEmpty) {
-        throw QueryParsingErrors.notEnoughArgumentsForTransformError(name, ctx)
+        throw new IllegalStateException(s"Not enough arguments for transform $name")
       } else {
         getFieldReference(ctx, arguments.head)
       }
@@ -3085,7 +3085,7 @@ class AstBuilder extends SqlBaseParserBaseVisitor[AnyRef] with SQLConfHelper wit
           .map(typedVisit[Literal])
           .map(lit => LiteralValue(lit.value, lit.dataType))
       reference.orElse(literal)
-          .getOrElse(throw QueryParsingErrors.invalidTransformArgumentError(ctx))
+          .getOrElse(throw new IllegalStateException("Invalid transform argument"))
     }
   }
 
