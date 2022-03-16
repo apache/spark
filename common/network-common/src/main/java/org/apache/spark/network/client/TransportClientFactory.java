6d3e8eba055bb2809f17d74aa3442b18bf7beb16
weixiuli <weixiuli@jd.com>
2022-03-16 17:01:33 -0500

    [SPARK-38555][NETWORK][SHUFFLE] Avoid contention and get or create clientPools quickly in the TransportClientFactory
    ### What changes were proposed in this pull request?
    Avoid contention and get or create clientPools quickly in the TransportClientFactory.
    ### Why are the changes needed?
    
    Avoid contention for getting or creating clientPools, and clean up the code.
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Existing unittests.
    
    Closes #35860 from weixiuli/SPARK-38555-NETWORK.
    
    Authored-by: weixiuli <weixiuli@jd.com>
    Signed-off-by: Mridul Muralidharan <mridul<at>gmail.com>

--- common/network-common/src/main/java/org/apache/spark/network/client/TransportClientFactory.java
+++ common/network-common/src/main/java/org/apache/spark/network/client/TransportClientFactory.java
@@ -155,12 +155,8 @@ public class TransportClientFactory implements Closeable {
       InetSocketAddress.createUnresolved(remoteHost, remotePort);
 
     // Create the ClientPool if we don't have it yet.
-    ClientPool clientPool = connectionPool.get(unresolvedAddress);
-    if (clientPool == null) {
-      connectionPool.putIfAbsent(unresolvedAddress, new ClientPool(numConnectionsPerPeer));
-      clientPool = connectionPool.get(unresolvedAddress);
-    }
-
+    ClientPool clientPool = connectionPool.computeIfAbsent(unresolvedAddress,
+        key -> new ClientPool(numConnectionsPerPeer));
     int clientIndex = rand.nextInt(numConnectionsPerPeer);
     TransportClient cachedClient = clientPool.clients[clientIndex];
 
