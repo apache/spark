74053cfed4f21c73a353368338f640445f4d73af
mcdull-zhang <work4dong@163.com>
2022-03-24 11:03:03 +0800

    fix

--- sql/core/src/main/scala/org/apache/spark/sql/execution/dynamicpruning/PartitionPruning.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/dynamicpruning/PartitionPruning.scala
@@ -59,8 +59,6 @@ object PartitionPruning extends Rule[LogicalPlan] with PredicateHelper {
    */
   def getFilterableTableScan(a: Expression, plan: LogicalPlan): Option[LogicalPlan] = {
     val srcInfo: Option[(Expression, LogicalPlan)] = findExpressionAndTrackLineageDown(a, plan)
-      // filter out literal
-      .filter(_._1.references.nonEmpty)
     srcInfo.flatMap {
       case (resExp, l: LogicalRelation) =>
         l.relation match {
