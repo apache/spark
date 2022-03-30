9f6aad407724997dc04a7689bb870d1d3ddd5526
Gengliang Wang <gengliang@apache.org>
2022-03-30 17:17:54 +0800

    [SPARK-38676][SQL] Provide SQL query context in runtime error message of Add/Subtract/Multiply
    ### What changes were proposed in this pull request?
    
    Provide SQL query context in runtime error of Add/Subtract/Multiply if the data type is Int or Long. This is the first PR for improving the runtime error message. There are more to be done after we decide what the error message should be like.
    
    Before changes:
    ```
    > SELECT i.f1 - 100, i.f1 * smallint('2') AS x FROM INT4_TBL i
    java.lang.ArithmeticException
    integer overflow. If necessary set spark.sql.ansi.enabled to false (except for ANSI interval type) to bypass this error.
    ```
    After changes:
    ```
    > SELECT i.f1 - 100, i.f1 * smallint('2') AS x FROM INT4_TBL i
    java.lang.ArithmeticException
    integer overflow. If necessary set spark.sql.ansi.enabled to false (except for ANSI interval type) to bypass this error.
    
    == SQL(line 1, position 25) ==
    SELECT '' AS five, i.f1, i.f1 * smallint('2') AS x FROM INT4_TBL i
                             ^^^^^^^^^^^^^^^^^^^^
    ```
    
    ### Why are the changes needed?
    
    Provide SQL query context of runtime errors to users, so that they can understand it better.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, improve the runtime error message of Add/Subtract/Multiply
    
    ### How was this patch tested?
    
    UT
    
    Closes #35992 from gengliangwang/runtimeError.
    
    Authored-by: Gengliang Wang <gengliang@apache.org>
    Signed-off-by: Gengliang Wang <gengliang@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
@@ -437,10 +437,14 @@ object QueryExecutionErrors {
       s"to false to bypass this error.")
   }
 
-  def arithmeticOverflowError(message: String, hint: String = ""): ArithmeticException = {
+  def arithmeticOverflowError(
+      message: String,
+      hint: String = "",
+      errorContext: String = ""): ArithmeticException = {
     val alternative = if (hint.nonEmpty) s" To return NULL instead, use '$hint'." else ""
     new ArithmeticException(s"$message.$alternative If necessary set " +
-      s"${SQLConf.ANSI_ENABLED.key} to false (except for ANSI interval type) to bypass this error.")
+      s"${SQLConf.ANSI_ENABLED.key} to false (except for ANSI interval type) to bypass this " +
+      "error." + errorContext)
   }
 
   def unaryMinusCauseOverflowError(originValue: AnyVal): ArithmeticException = {
