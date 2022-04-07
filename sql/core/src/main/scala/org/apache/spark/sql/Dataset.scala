beb64fa053a16464819efb1076e0a52de9688872
Hyukjin Kwon <gurwls223@apache.org>
2022-04-07 09:45:02 +0900

    Revert "[MINOR][SQL][SS][DOCS] Add varargs to Dataset.observe(String, ..) with a documentation fix"
    This reverts commit 952d476786ff7e2f5216094b272d46a253891358.

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -1928,7 +1928,6 @@ class Dataset[T] private[sql](
   * {{{
   *   // Monitor the metrics using a listener.
   *   spark.streams.addListener(new StreamingQueryListener() {
-  *     override def onQueryStarted(event: QueryStartedEvent): Unit = {}
   *     override def onQueryProgress(event: QueryProgressEvent): Unit = {
   *       event.progress.observedMetrics.asScala.get("my_event").foreach { row =>
   *         // Trigger if the number of errors exceeds 5 percent
@@ -1940,7 +1939,8 @@ class Dataset[T] private[sql](
   *         }
   *       }
   *     }
-  *     override def onQueryTerminated(event: QueryTerminatedEvent): Unit = {}
+  *     def onQueryStarted(event: QueryStartedEvent): Unit = {}
+  *     def onQueryTerminated(event: QueryTerminatedEvent): Unit = {}
   *   })
   *   // Observe row count (rc) and error row count (erc) in the streaming Dataset
   *   val observed_ds = ds.observe("my_event", count(lit(1)).as("rc"), count($"error").as("erc"))
@@ -1950,7 +1950,6 @@ class Dataset[T] private[sql](
   * @group typedrel
   * @since 3.0.0
   */
-  @varargs
   def observe(name: String, expr: Column, exprs: Column*): Dataset[T] = withTypedPlan {
     CollectMetrics(name, (expr +: exprs).map(_.named), logicalPlan)
   }
