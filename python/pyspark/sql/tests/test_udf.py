27ac62767ed3a1e75b7bd2d2c726a7634936c81e
Max Gekk <max.gekk@gmail.com>
2022-04-19 21:49:34 +0300

    Reformat test_udf.py

--- python/pyspark/sql/tests/test_udf.py
+++ python/pyspark/sql/tests/test_udf.py
@@ -258,7 +258,8 @@ class UDFTests(ReusedSQLTestCase):
         def runWithJoinType(join_type, type_string):
             with self.assertRaisesRegex(
                 AnalysisException,
-                """Using PythonUDF in join condition of join type "%s" is not supported""" % type_string,
+                """Using PythonUDF in join condition of join type "%s" is not supported"""
+                % type_string,
             ):
                 left.join(right, [f("a", "b"), left.a1 == right.b1], join_type).collect()
 
