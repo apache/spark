e848f7d16fba8e02462ec72f557aca976e0e3851
sychen <sychen@ctrip.com>
2022-04-21 00:02:52 +0800

    use class name

--- sql/core/src/main/scala/org/apache/spark/sql/execution/BaseScriptTransformationExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/BaseScriptTransformationExec.scala
@@ -262,8 +262,7 @@ trait BaseScriptTransformationExec extends UnaryExecNode {
       }
 }
 
-abstract class BaseScriptTransformationWriterThread
-  extends Thread("Thread-ScriptTransformation-Feed") with Logging {
+abstract class BaseScriptTransformationWriterThread extends Thread with Logging {
 
   def iter: Iterator[InternalRow]
   def inputSchema: Seq[DataType]
@@ -274,6 +273,7 @@ abstract class BaseScriptTransformationWriterThread
   def taskContext: TaskContext
   def conf: Configuration
 
+  setName(s"Thread-${this.getClass.getSimpleName}-Feed")
   setDaemon(true)
 
   @volatile protected var _exception: Throwable = null
@@ -329,7 +329,7 @@ abstract class BaseScriptTransformationWriterThread
         // Javadoc this call will not throw an exception:
         _exception = t
         proc.destroy()
-        logError("Thread-ScriptTransformation-Feed exit cause by: ", t)
+        logError(s"Thread-${this.getClass.getSimpleName}-Feed exit cause by: ", t)
     } finally {
       try {
         Utils.tryLogNonFatalError(outputStream.close())
