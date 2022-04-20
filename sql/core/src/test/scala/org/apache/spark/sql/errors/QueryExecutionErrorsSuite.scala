ef00aa1434ae5c5ecfec5e9b4ffaa2ed0f0e45d4
William Hyun <william@apache.org>
2022-04-20 11:03:55 +0900

    [SPARK-38956][TESTS] Fix FAILED_EXECUTE_UDF test case on Java 17
    ### What changes were proposed in this pull request?
    This PR aims to fix FAILED_EXECUTE_UDF test case on Java 17.
    
    ### Why are the changes needed?
    **BEFORE (Java17)**
    ```
    [info] QueryExecutionErrorsSuite:
    16:04:22.234 WARN org.apache.hadoop.util.NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
    16:04:24.377 ERROR org.apache.spark.executor.Executor: Exception in task 0.0 in stage 0.0 (TID 0)
    org.apache.spark.SparkException: Failed to execute user defined function (QueryExecutionErrorsSuite$$Lambda$1582/0x0000000801653be8: (string, int) => string)
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Manually test on Java 17.
    
    Closes #36270 from williamhyun/w.
    
    Authored-by: William Hyun <william@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/errors/QueryExecutionErrorsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/errors/QueryExecutionErrorsSuite.scala
@@ -432,6 +432,6 @@ class QueryExecutionErrorsSuite extends QueryTest
     val e2 = e1.getCause.asInstanceOf[SparkException]
     assert(e2.getErrorClass === "FAILED_EXECUTE_UDF")
     assert(e2.getMessage.matches("Failed to execute user defined function " +
-      "\\(QueryExecutionErrorsSuite\\$\\$Lambda\\$\\d+/\\d+: \\(string, int\\) => string\\)"))
+      "\\(QueryExecutionErrorsSuite\\$\\$Lambda\\$\\d+/\\w+: \\(string, int\\) => string\\)"))
   }
 }
