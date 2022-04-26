c3ad2fa8b66c64bca897f4ee986238a49a7f60a5
Serge Rielau <srielau@users.noreply.github.com>
2022-04-26 08:24:55 -0700

    Update python/pyspark/sql/tests/test_udf.py
    Co-authored-by: Wenchen Fan <cloud0fan@gmail.com>

--- python/pyspark/sql/tests/test_udf.py
+++ python/pyspark/sql/tests/test_udf.py
@@ -258,7 +258,7 @@ class UDFTests(ReusedSQLTestCase):
         def runWithJoinType(join_type, type_string):
             with self.assertRaisesRegex(
                 AnalysisException,
-                """Python UDF in the ON clause of a "%s" JOIN.""" % type_string,
+                """Python UDF in the ON clause of a %s JOIN.""" % type_string,
             ):
                 left.join(right, [f("a", "b"), left.a1 == right.b1], join_type).collect()
 
