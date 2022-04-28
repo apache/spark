fba68e6f20b88779dc5ca78742958952b3d7acf0
yangjie01 <yangjie01@baidu.com>
2022-04-28 18:51:28 -0500

    [SPARK-38896][CORE][SQL] Use `tryWithResource` release `LevelDB/RocksDBIterator` resources earlier
    ### What changes were proposed in this pull request?
    Similar to SPARK-38847, this pr aims to release the `LevelDB/RocksDBIterator` resources earlier by using `tryWithResource`. The main change of this pr as follows:
    
    1. Use Java `tryWithResource` and Spark `Utils.tryWithResource` to recycling `KVStoreIterator` opened by `RocksDB.view(Class<T> type).iterator` and `RocksDB.view(Class<T> type).iterator`
    2. Introduce 4 new function for KVUtils(`count|foreach|mapToSeq|size`),  these function will close `KVStoreIterator` in time.
    
    ### Why are the changes needed?
    Release the `LevelDB/RocksDBIterator` resources earlier
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Pass GA
    
    Closes #36237 from LuciferYang/Manual-Close-KVStoreIterator.
    
    Authored-by: yangjie01 <yangjie01@baidu.com>
    Signed-off-by: Sean Owen <srowen@gmail.com>

--- core/src/main/scala/org/apache/spark/deploy/history/FsHistoryProvider.scala
+++ core/src/main/scala/org/apache/spark/deploy/history/FsHistoryProvider.scala
@@ -325,12 +325,8 @@ private[history] class FsHistoryProvider(conf: SparkConf, clock: Clock)
 
   override def getListing(): Iterator[ApplicationInfo] = {
     // Return the listing in end time descending order.
-    listing.view(classOf[ApplicationInfoWrapper])
-      .index("endTime")
-      .reverse()
-      .iterator()
-      .asScala
-      .map(_.toApplicationInfo())
+    KVUtils.mapToSeq(listing.view(classOf[ApplicationInfoWrapper])
+      .index("endTime").reverse())(_.toApplicationInfo()).iterator
   }
 
   override def getApplicationInfo(appId: String): Option[ApplicationInfo] = {
@@ -982,14 +978,11 @@ private[history] class FsHistoryProvider(conf: SparkConf, clock: Clock)
 
     // If the number of files is bigger than MAX_LOG_NUM,
     // clean up all completed attempts per application one by one.
-    val num = listing.view(classOf[LogInfo]).index("lastProcessed").asScala.size
+    val num = KVUtils.size(listing.view(classOf[LogInfo]).index("lastProcessed"))
     var count = num - maxNum
     if (count > 0) {
       logInfo(s"Try to delete $count old event logs to keep $maxNum logs in total.")
-      val oldAttempts = listing.view(classOf[ApplicationInfoWrapper])
-        .index("oldestAttempt")
-        .asScala
-      oldAttempts.foreach { app =>
+      KVUtils.foreach(listing.view(classOf[ApplicationInfoWrapper]).index("oldestAttempt")) { app =>
         if (count > 0) {
           // Applications may have multiple attempts, some of which may not be completed yet.
           val (toDelete, remaining) = app.attempts.partition(_.info.completed)
