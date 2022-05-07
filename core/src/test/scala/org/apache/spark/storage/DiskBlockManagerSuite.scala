2ee4bb1c288a3a6e4b3d12503171aeddf40ccfde
Mridul Muralidharan <mridulatgmail.com>
2022-05-06 23:33:54 -0500

    Address review comment

--- core/src/test/scala/org/apache/spark/storage/DiskBlockManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/DiskBlockManagerSuite.scala
@@ -156,33 +156,31 @@ class DiskBlockManagerSuite extends SparkFunSuite with BeforeAndAfterEach with B
     conf.set("spark.shuffle.service.removeShuffle", "false")
     val posix = POSIXFactory.getPOSIX
 
-    if (posix.isNative) {
-      val oldUmask = getAndSetUmask(posix, "077")
-      try {
-        val diskBlockManager = new DiskBlockManager(conf, deleteFilesOnStop = true,
-          isDriver = false)
-        val blockId = new TestBlockId("test")
-        val newFile = diskBlockManager.getFile(blockId)
-        val parentDir = newFile.getParentFile()
-        assert(parentDir.exists && parentDir.isDirectory)
-        val permission = Files.getPosixFilePermissions(parentDir.toPath)
-        assert(!permission.contains(PosixFilePermission.GROUP_WRITE))
+    assume(posix.isNative, "Skipping test for SPARK-37618, native posix support not found")
 
-        assert(parentDir.delete())
+    val oldUmask = getAndSetUmask(posix, "077")
+    try {
+      val diskBlockManager = new DiskBlockManager(conf, deleteFilesOnStop = true,
+        isDriver = false)
+      val blockId = new TestBlockId("test")
+      val newFile = diskBlockManager.getFile(blockId)
+      val parentDir = newFile.getParentFile()
+      assert(parentDir.exists && parentDir.isDirectory)
+      val permission = Files.getPosixFilePermissions(parentDir.toPath)
+      assert(!permission.contains(PosixFilePermission.GROUP_WRITE))
 
-        conf.set("spark.shuffle.service.removeShuffle", "true")
-        val diskBlockManager2 = new DiskBlockManager(conf, deleteFilesOnStop = true,
-          isDriver = false)
-        val newFile2 = diskBlockManager2.getFile(blockId)
-        val parentDir2 = newFile2.getParentFile()
-        assert(parentDir2.exists && parentDir2.isDirectory)
-        val permission2 = Files.getPosixFilePermissions(parentDir2.toPath)
-        assert(permission2.contains(PosixFilePermission.GROUP_WRITE))
-      } finally {
-        getAndSetUmask(posix, oldUmask)
-      }
-    } else {
-      logInfo("Skipping test for SPARK-37618, native posix support not found")
+      assert(parentDir.delete())
+
+      conf.set("spark.shuffle.service.removeShuffle", "true")
+      val diskBlockManager2 = new DiskBlockManager(conf, deleteFilesOnStop = true,
+        isDriver = false)
+      val newFile2 = diskBlockManager2.getFile(blockId)
+      val parentDir2 = newFile2.getParentFile()
+      assert(parentDir2.exists && parentDir2.isDirectory)
+      val permission2 = Files.getPosixFilePermissions(parentDir2.toPath)
+      assert(permission2.contains(PosixFilePermission.GROUP_WRITE))
+    } finally {
+      getAndSetUmask(posix, oldUmask)
     }
   }
 
