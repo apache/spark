1ebf3e500243003b584d21ff024fba2c11ac7dac
Angerszhuuuu <angers.zhu@gmail.com>
2022-05-08 08:13:03 -0500

    [SPARK-39110][WEBUI] Add metrics properties to environment tab
    ### What changes were proposed in this pull request?
    There are three ways to define metrics properties:
    
    1. Default metrics properties
    2. metrics.properties file under $SPARK_CONF_DIR
    3. spark.metrics.conf  to include a metrics properties file
    4. spark.metrics.conf.xx.xx
    
    Many new users always confused when test with metrics system, i think we can add final metrics properties in the environment tab, to let user can directly know which metrics are working.
    
    <img width="1757" alt="截屏2022-05-06 上午11 23 04" src="https://user-images.githubusercontent.com/46485123/167062876-c0c98a69-13c7-4a25-bb31-74f1ada88153.png">
    
    <img width="1786" alt="截屏2022-05-06 上午11 33 00" src="https://user-images.githubusercontent.com/46485123/167062893-f297eeda-b08f-4c9d-a2a2-a74add97493f.png">
    
    ### Why are the changes needed?
    Make user clear about which metrics properties are working
    
    ### Does this PR introduce _any_ user-facing change?
    user can see working metrics properties in UI  environment tag
    
    ### How was this patch tested?
    MT
    
    Closes #36462 from AngersZhuuuu/SPARK-39110.
    
    Authored-by: Angerszhuuuu <angers.zhu@gmail.com>
    Signed-off-by: Sean Owen <srowen@gmail.com>

--- core/src/main/scala/org/apache/spark/ui/env/EnvironmentPage.scala
+++ core/src/main/scala/org/apache/spark/ui/env/EnvironmentPage.scala
@@ -83,6 +83,9 @@ private[ui] class EnvironmentPage(
     val systemPropertiesTable = UIUtils.listingTable(propertyHeader, propertyRow,
       Utils.redact(conf, appEnv.systemProperties.sorted), fixedWidth = true,
       headerClasses = headerClasses)
+    val metricsPropertiesTable = UIUtils.listingTable(propertyHeader, propertyRow,
+      Utils.redact(conf, appEnv.metricsProperties.sorted), fixedWidth = true,
+      headerClasses = headerClasses)
     val classpathEntriesTable = UIUtils.listingTable(
       classPathHeader, classPathRow, appEnv.classpathEntries.sorted, fixedWidth = true,
       headerClasses = headerClasses)
@@ -142,6 +145,17 @@ private[ui] class EnvironmentPage(
         </span>
         <div class="aggregated-systemProperties collapsible-table collapsed">
           {systemPropertiesTable}
+        </div>
+        <span class="collapse-aggregated-metricsProperties collapse-table"
+              onClick="collapseTable('collapse-aggregated-metricsProperties',
+            'aggregated-metricsProperties')">
+          <h4>
+            <span class="collapse-table-arrow arrow-closed"></span>
+            <a>Metrics Properties</a>
+          </h4>
+        </span>
+        <div class="aggregated-metricsProperties collapsible-table collapsed">
+          {metricsPropertiesTable}
         </div>
         <span class="collapse-aggregated-classpathEntries collapse-table"
             onClick="collapseTable('collapse-aggregated-classpathEntries',
