18ec585b8bd23aa2ec56faca881d528f5f80a7a1
Anywalker <97273184+AnywalkerGiser@users.noreply.github.com>
2022-05-13 15:10:02 +0800

    Create types.py
    Fix problems with pyspark in Windowns:
    1. Fixed datetime conversion to timestamp before 1970;
    2. Fixed datetime conversion when timestamp is negative;

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -191,14 +191,25 @@ class TimestampType(AtomicType):
 
     def toInternal(self, dt):
         if dt is not None:
-            seconds = (calendar.timegm(dt.utctimetuple()) if dt.tzinfo
-                       else time.mktime(dt.timetuple()))
+            seconds = 0.0
+            try:
+                seconds = (calendar.timegm(dt.utctimetuple()) if dt.tzinfo
+                           else time.mktime(dt.timetuple()))
+            except:
+                # On Windows, the current value is converted to a timestamp when the current value is less than 1970
+                seconds = (dt - datetime.datetime.fromtimestamp(int(time.localtime(0).tm_sec) / 1000)).total_seconds()
+
             return int(seconds) * 1000000 + dt.microsecond
 
     def fromInternal(self, ts):
         if ts is not None:
-            # using int to avoid precision loss in float
-            return datetime.datetime.fromtimestamp(ts // 1000000).replace(microsecond=ts % 1000000)
+            try:
+                # using int to avoid precision loss in float
+                return datetime.datetime.fromtimestamp(ts // 1000000).replace(microsecond=ts % 1000000)
+            except:
+                # On Windows, resolve when the timestamp is negative
+                return datetime.datetime.fromtimestamp(int(time.localtime(0).tm_sec) / 1000) + \
+                       datetime.timedelta(microseconds=ts)
 
 
 class DecimalType(FractionalType):
@@ -1635,8 +1646,14 @@ class DatetimeConverter(object):
 
     def convert(self, obj, gateway_client):
         Timestamp = JavaClass("java.sql.Timestamp", gateway_client)
-        seconds = (calendar.timegm(obj.utctimetuple()) if obj.tzinfo
-                   else time.mktime(obj.timetuple()))
+        seconds = 0.0
+        try:
+            seconds = (calendar.timegm(obj.utctimetuple()) if obj.tzinfo
+                       else time.mktime(obj.timetuple()))
+        except:
+            # On Windows, the current value is converted to a timestamp when the current value is less than 1970
+            seconds = (obj - datetime.datetime.fromtimestamp(int(time.localtime(0).tm_sec) / 1000)).total_seconds()
+
         t = Timestamp(int(seconds) * 1000)
         t.setNanos(obj.microsecond * 1000)
         return t
