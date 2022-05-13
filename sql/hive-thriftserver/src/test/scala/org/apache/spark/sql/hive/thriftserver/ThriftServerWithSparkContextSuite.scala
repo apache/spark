aabe3a75454462c9ce665a3d947a7edb91f37d13
Kent Yao <yao@apache.org>
2022-05-14 00:03:23 +0800

    [SPARK-39138][SQL] Add ANSI general value specification and function - user
    ### What changes were proposed in this pull request?
    
    Add ANSI general value specification and function - user
    
    ### Why are the changes needed?
    
    According to ANSI SQL,
    ```
    CURRENT_USER and USER are semantically the same
    ```
    
    USER is also supported by other systems like MySQL, PG, hive, etc.
    
    ### Does this PR introduce _any_ user-facing change?
    
    new function added
    
    for ansi mode, and if enforceReservedKeywords, USER always reserved
    
    otherwise, it will be resolved as a literal if no attribute matches.
    
    ### How was this patch tested?
    
    new tests
    
    Closes #36497 from yaooqinn/SPARK-39138.
    
    Authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Kent Yao <yao@apache.org>

--- sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/ThriftServerWithSparkContextSuite.scala
+++ sql/hive-thriftserver/src/test/scala/org/apache/spark/sql/hive/thriftserver/ThriftServerWithSparkContextSuite.scala
@@ -126,20 +126,27 @@ trait ThriftServerWithSparkContextSuite extends SharedThriftServer {
 
       exec(s"set ${SQLConf.ANSI_ENABLED.key}=false")
 
-      val opHandle1 = exec("select current_user(), current_user")
-      val rowSet1 = client.fetchResults(opHandle1)
-      rowSet1.getColumns.forEach { col =>
-        assert(col.getStringVal.getValues.get(0) === clientUser)
+      val userFuncs = Seq("user", "current_user")
+      userFuncs.foreach { func =>
+        val opHandle1 = exec(s"select $func(), $func")
+        val rowSet1 = client.fetchResults(opHandle1)
+        rowSet1.getColumns.forEach { col =>
+          assert(col.getStringVal.getValues.get(0) === clientUser)
+        }
       }
 
       exec(s"set ${SQLConf.ANSI_ENABLED.key}=true")
       exec(s"set ${SQLConf.ENFORCE_RESERVED_KEYWORDS.key}=true")
-      val opHandle2 = exec("select current_user")
-      assert(client.fetchResults(opHandle2).getColumns.get(0)
-        .getStringVal.getValues.get(0) === clientUser)
+      userFuncs.foreach { func =>
+        val opHandle2 = exec(s"select $func")
+        assert(client.fetchResults(opHandle2)
+          .getColumns.get(0).getStringVal.getValues.get(0) === clientUser)
+      }
 
-      val e = intercept[HiveSQLException](exec("select current_user()"))
-      assert(e.getMessage.contains("current_user"))
+      userFuncs.foreach { func =>
+        val e = intercept[HiveSQLException](exec(s"select $func()"))
+        assert(e.getMessage.contains(func))
+      }
     }
   }
 }
