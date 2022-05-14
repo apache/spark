1b37f19876298e995596a30edc322c856ea1bbb4
Kent Yao <yao@apache.org>
2022-05-14 19:45:24 +0900

    [SPARK-39174][SQL] Catalogs loading swallows missing classname for ClassNotFoundException
    ### What changes were proposed in this pull request?
    
    this PR captures the actual missing classname when catalog loading meets ClassNotFoundException
    
    ### Why are the changes needed?
    
    ClassNotFoundException can occur when missing dependencies, we shall not always report the catalog class is missing
    
    ### Does this PR introduce _any_ user-facing change?
    
    yes, when loading catalogs and ClassNotFoundException occurs, it shows the correct missing class.
    
    ### How was this patch tested?
    
    new test added
    
    Closes #36534 from yaooqinn/SPARK-39174.
    
    Authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
@@ -1541,8 +1541,9 @@ object QueryExecutionErrors extends QueryErrorsBase {
 
   def catalogPluginClassNotFoundForCatalogError(
       name: String,
-      pluginClassName: String): Throwable = {
-    new SparkException(s"Cannot find catalog plugin class for catalog '$name': $pluginClassName")
+      pluginClassName: String,
+      e: Exception): Throwable = {
+    new SparkException(s"Cannot find catalog plugin class for catalog '$name': $pluginClassName", e)
   }
 
   def catalogFailToFindPublicNoArgConstructorError(
