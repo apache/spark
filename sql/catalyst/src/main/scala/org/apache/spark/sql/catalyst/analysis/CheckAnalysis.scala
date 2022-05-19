24a3fa95a384159004d45cf2c01a699f1c2e55f7
Gengliang Wang <gengliang@apache.org>
2022-05-19 20:34:50 +0800

    [SPARK-39233][SQL] Remove the check for TimestampNTZ output in Analyzer
    ### What changes were proposed in this pull request?
    
    In [#36094](https://github.com/apache/spark/pull/36094), a check for failing TimestampNTZ type output(since we are disabling TimestampNTZ in 3.3) is added:
    ```
          case operator: LogicalPlan
            if !Utils.isTesting && operator.output.exists(attr =>
              attr.resolved && attr.dataType.isInstanceOf[TimestampNTZType]) =>
            operator.failAnalysis("TimestampNTZ type is not supported in Spark 3.3.")
    ```
    
    However, the check can cause misleading error messages.
    
    In 3.3:
    ```
    > sql( "select date '2018-11-17' > 1").show()
    org.apache.spark.sql.AnalysisException: Invalid call to toAttribute on unresolved object;
    'Project [unresolvedalias((2018-11-17 > 1), None)]
    +- OneRowRelation
    
      at org.apache.spark.sql.catalyst.analysis.UnresolvedAlias.toAttribute(unresolved.scala:510)
      at org.apache.spark.sql.catalyst.plans.logical.Project.$anonfun$output$1(basicLogicalOperators.scala:70)
    ```
    In master or 3.2
    ```
    > sql( "select date '2018-11-17' > 1").show()
    org.apache.spark.sql.AnalysisException: cannot resolve '(DATE '2018-11-17' > 1)' due to data type mismatch: differing types in '(DATE '2018-11-17' > 1)' (date and int).; line 1 pos 7;
    'Project [unresolvedalias((2018-11-17 > 1), None)]
    +- OneRowRelation
    
      at org.apache.spark.sql.catalyst.analysis.package$AnalysisErrorAt.failAnalysis(package.scala:42)
    ```
    
    We should just remove the check to avoid such regression. It's not necessary for disabling TimestampNTZ anyway.
    
    ### Why are the changes needed?
    
    Fix regression in the error output of analysis check.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, it is not released yet.
    
    ### How was this patch tested?
    
    Build and try on `spark-shell`
    
    Closes #36609 from gengliangwang/fixRegression.
    
    Authored-by: Gengliang Wang <gengliang@apache.org>
    Signed-off-by: Gengliang Wang <gengliang@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/CheckAnalysis.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/CheckAnalysis.scala
@@ -32,7 +32,6 @@ import org.apache.spark.sql.errors.QueryCompilationErrors
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.util.SchemaUtils
-import org.apache.spark.util.Utils
 
 /**
  * Throws user facing errors when passed invalid queries that fail to analyze.
@@ -159,11 +158,6 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog {
       // `ShowTableExtended` should have been converted to the v1 command if the table is v1.
       case _: ShowTableExtended =>
         throw QueryCompilationErrors.commandUnsupportedInV2TableError("SHOW TABLE EXTENDED")
-
-      case operator: LogicalPlan
-        if !Utils.isTesting && operator.output.exists(attr =>
-          attr.resolved && attr.dataType.isInstanceOf[TimestampNTZType]) =>
-        operator.failAnalysis("TimestampNTZ type is not supported in Spark 3.3.")
 
       case operator: LogicalPlan =>
         // Check argument data types of higher-order functions downwards first.
