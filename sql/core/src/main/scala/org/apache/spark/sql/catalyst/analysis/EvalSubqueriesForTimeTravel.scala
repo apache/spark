81bd0d9586d928e31b2dbb0b44dc9c75ebeea16f
Wenchen Fan <cloud0fan@gmail.com>
2022-05-31 10:35:07 +0800

    Update EvalSubqueriesForTimeTravel.scala

--- sql/core/src/main/scala/org/apache/spark/sql/catalyst/analysis/EvalSubqueriesForTimeTravel.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/catalyst/analysis/EvalSubqueriesForTimeTravel.scala
@@ -33,7 +33,7 @@ class EvalSubqueriesForTimeTravel extends Rule[LogicalPlan] {
         case s: ScalarSubquery =>
           // `RelationTimeTravel` is a leaf node. Subqueries in it cannot resolve any
           // outer references and should not be correlated.
-          assert(!s.isCorrelated, "Correlated subquery should not appear in " + 
+          assert(!s.isCorrelated, "Correlated subquery should not appear in " +
             classOf[EvalSubqueriesForTimeTravel].getSimpleName)
           SimpleAnalyzer.checkSubqueryExpression(r, s)
           val executedPlan = QueryExecution.prepareExecutedPlan(SparkSession.active, s.plan)
