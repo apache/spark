e32dc13c14ea407784029b789421907a071e3b00
Daniel Tenedorio <daniel.tenedorio@databricks.com>
2022-06-01 17:40:54 -0700

    fix tests

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/ResolveDefaultColumnsUtil.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/ResolveDefaultColumnsUtil.scala
@@ -234,14 +234,16 @@ object ResolveDefaultColumns {
   }
 
   def checkDataSourceSupportsDefaultColumns(table: CatalogTable): Unit = {
-    table.provider.getOrElse("").toLowerCase() match {
-      case "csv" | "json" | "parquet" | "orc"
-        if table.schema.fields.map(_.metadata).exists { m =>
-          m.contains(CURRENT_DEFAULT_COLUMN_METADATA_KEY) ||
-          m.contains(EXISTS_DEFAULT_COLUMN_METADATA_KEY)
-        } =>
-      case _ => throw QueryCompilationErrors.defaultReferencesNotAllowedInDataSource(
-        table.identifier.toString)
+    if (table.schema.fields.map(_.metadata).exists { m =>
+      m.contains(CURRENT_DEFAULT_COLUMN_METADATA_KEY) ||
+        m.contains(EXISTS_DEFAULT_COLUMN_METADATA_KEY)
+    }) {
+      table.provider.getOrElse("").toLowerCase() match {
+        case "csv" | "json" | "parquet" | "orc" =>
+        case _ =>
+          throw QueryCompilationErrors.defaultReferencesNotAllowedInDataSource(
+            table.provider.getOrElse(""))
+      }
     }
   }
 }
