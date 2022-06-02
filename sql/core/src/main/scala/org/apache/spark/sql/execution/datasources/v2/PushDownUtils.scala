8e196fadd2bc5e763074e84cebd7fd9ef974e62e
Zhixiong Chen <chen@apache.org>
2022-06-02 09:42:59 +0800

    [SPARK-38768][SQL] Remove Limit from plan if complete push down limit to data source (#474)
    * [SPARK-38768][SQL] Remove `Limit` from plan if complete push down limit to data source
    
    Currently, Spark supports push down limit to data source.
    If limit could pushed down and Data source only have one partition, DS V2 still do limit again.
    This PR want remove `Limit` from plan if complete push down limit to data source.
    
    Improve performance.
    
    'No'.
    New feature.
    
    Tests updated.
    
    Closes #36043 from beliefer/SPARK-38768.
    
    Authored-by: Jiaan Geng <beliefer@163.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    
    * [SPARK-38391][SPARK-38768][SQL][FOLLOWUP] Add comments for `pushLimit` and `pushTopN` of `PushDownUtils`
    
    `pushLimit` and `pushTopN` of `PushDownUtils` returns tuple of boolean. It will be good to explain what the boolean value represents.
    
    Make DS V2 API more friendly to developers.
    
    'No'.
    Just update comments.
    
    N/A
    
    Closes #36092 from beliefer/SPARK-38391_SPARK-38768_followup.
    
    Authored-by: Jiaan Geng <beliefer@163.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    
    * [SPARK-37960][SQL][FOLLOWUP] Make the testing CASE WHEN query more reasonable
    
    Some testing CASE WHEN queries are not carefully written and do not make sense. In the future, the optimizer may get smarter and get rid of the CASE WHEN completely, and then we loose test coverage.
    
    This PR updates some CASE WHEN queries to make them more reasonable.
    
    future-proof test coverage.
    
    'No'.
    
    N/A
    
    Closes #36125 from beliefer/SPARK-37960_followup3.
    
    Authored-by: Jiaan Geng <beliefer@163.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    
    * update spark version
    
    Co-authored-by: Jiaan Geng <beliefer@163.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/PushDownUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/PushDownUtils.scala
@@ -122,11 +122,11 @@ object PushDownUtils extends PredicateHelper {
    *         the second Boolean value represents whether to push down partially, which means
    *         Spark will keep the Limit and do it again.
    */
-  def pushLimit(scanBuilder: ScanBuilder, limit: Int): Boolean = {
+  def pushLimit(scanBuilder: ScanBuilder, limit: Int): (Boolean, Boolean) = {
     scanBuilder match {
-      case s: SupportsPushDownLimit =>
-        s.pushLimit(limit)
-      case _ => false
+      case s: SupportsPushDownLimit if s.pushLimit(limit) =>
+        (true, s.isPartiallyPushed)
+      case _ => (false, false)
     }
   }
 
