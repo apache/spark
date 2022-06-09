c38689bc312f7b0f8e28874bfa67f8697f321210
Li Xian <lxian2shell@gmail.com>
2022-06-09 17:04:17 +0800

    convert inner join to semi join

---
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/ConvertInnerJoinToSemiJoin.scala
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.optimizer
+
+import org.apache.spark.sql.catalyst.expressions.{Alias, Expression, PredicateHelper}
+import org.apache.spark.sql.catalyst.planning.ExtractEquiJoinKeys
+import org.apache.spark.sql.catalyst.plans.{Inner, LeftSemi}
+import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, Join, LogicalPlan, Project}
+import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.catalyst.trees.TreePattern.{JOIN, PROJECT}
+
+object ConvertInnerJoinToSemiJoin extends Rule[LogicalPlan] with PredicateHelper {
+  override def apply(plan: LogicalPlan): LogicalPlan = plan.transformWithPruning(
+    _.containsAllPatterns(PROJECT, JOIN)) {
+    case p @ Project(_, j @ ExtractEquiJoinKeys(_, _, _, nonEquiCond, _, _, agg: Aggregate, _))
+        if innerEqualJoin(j, nonEquiCond) & canTransform(agg, j, p) =>
+      // eliminate the agg, replace it with a project with all its expressions
+      p.copy(child = j.copy(
+        joinType = LeftSemi, right = Project(agg.aggregateExpressions, agg.child)))
+    case p @ Project(_, j @ ExtractEquiJoinKeys(_, _, _, nonEquiCond, _, agg: Aggregate, _, _))
+      if innerEqualJoin(j, nonEquiCond) & canTransform(agg, j, p) =>
+      // eliminate the agg, replace it with a project with all its expressions
+      // it will swap the join order from inner join(left, right) to semi join(right, left)
+      p.copy(child = j.copy(
+        joinType = LeftSemi, left = j.right, right = Project(agg.aggregateExpressions, agg.child)))
+  }
+
+  def innerEqualJoin(join: Join, nonEquiCond: Option[Expression]): Boolean = {
+    join.joinType == Inner && join.condition.nonEmpty && nonEquiCond.isEmpty
+  }
+
+  def canTransform(agg: Aggregate, join: Join, project: Project): Boolean = {
+    // 1. grouping only - the agg output and only output all the grouping cols
+    // 2. all grouping cols should be in join condition
+    // 3. all grouping cols should not appear in the project above
+    exactGroupingOnly(agg) &&
+      agg.aggregateExpressions.forall(join.condition.get.references.contains(_)) &&
+      agg.aggregateExpressions.forall(!project.references.contains(_))
+  }
+
+  // test if the aggregate exprs and grouping exprs are exactly the same
+  def exactGroupingOnly(agg: Aggregate): Boolean = {
+    if (agg.aggregateExpressions.size != agg.groupingExpressions.size) {
+      return false
+    }
+
+    val foundInGroupings = agg.aggregateExpressions.map {
+      case Alias(child, _) =>
+        agg.groupingExpressions.indexWhere(_.semanticEquals(child))
+      case other =>
+        agg.groupingExpressions.indexWhere(_.semanticEquals(other))
+    }.toSet
+    // all agg expr should be found in grouping expr
+    // and the number should match
+    !foundInGroupings.contains(-1) && foundInGroupings.size == agg.groupingExpressions.size
+  }
+}
---
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/ConvertInnerJoinToSemiJoinSuite.scala
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.optimizer
+
+import org.apache.spark.sql.catalyst.dsl.expressions._
+import org.apache.spark.sql.catalyst.dsl.plans._
+import org.apache.spark.sql.catalyst.plans.{Inner, LeftOuter, LeftSemi, PlanTest}
+import org.apache.spark.sql.catalyst.plans.logical.{Aggregate, Intersect, Join, JoinHint, LocalRelation, LogicalPlan, Project}
+import org.apache.spark.sql.catalyst.rules.RuleExecutor
+
+class ConvertInnerJoinToSemiJoinSuite extends PlanTest {
+
+  object Optimize extends RuleExecutor[LogicalPlan] {
+    val batches =
+      Batch("Convert inner join to semi", Once, ConvertInnerJoinToSemiJoin) :: Nil
+  }
+
+  test("Convert inner join to semi") {
+    val table1 = LocalRelation('a.int, 'b.int)
+    val table2 = LocalRelation('c.int, 'd.int, 'e.int)
+    val agg = Aggregate(Seq('c, 'e), Seq('c, 'e), table2)
+    val join = Join(table1, agg, Inner, Option('a <=> 'c && 'b <=> 'e), JoinHint.NONE)
+    val project = Project(Seq('a, 'b), join)
+
+    val optimized = Optimize.execute(project.analyze)
+
+    val correctAnswer =
+      Project(Seq('a, 'b),
+        Join(table1,
+          Project(Seq('c, 'e), table2),
+          LeftSemi, Option('a <=> 'c && 'b <=> 'e), JoinHint.NONE)).analyze
+
+    comparePlans(optimized, correctAnswer)
+  }
+
+  test("Convert inner join to semi - aggregate on the left side") {
+    val table1 = LocalRelation('a.int, 'b.int)
+    val table2 = LocalRelation('c.int, 'd.int, 'e.int)
+    val agg = Aggregate(Seq('c, 'e), Seq('c, 'e), table2)
+    val join = Join(agg, table1, Inner, Option('a <=> 'c && 'b <=> 'e), JoinHint.NONE)
+    val project = Project(Seq('a, 'b), join)
+
+    val optimized = Optimize.execute(project.analyze)
+
+    val correctAnswer =
+      Project(Seq('a, 'b),
+        Join(table1,
+          Project(Seq('c, 'e), table2),
+          LeftSemi, Option('a <=> 'c && 'b <=> 'e), JoinHint.NONE)).analyze
+
+    comparePlans(optimized, correctAnswer)
+  }
+
+  test("Convert inner join to semi - join cond miss some columns") {
+    val table1 = LocalRelation('a.int, 'b.int)
+    val table2 = LocalRelation('c.int, 'd.int)
+    val agg = Aggregate(Seq('c, 'd), Seq('c, 'd), table2)
+    val join = Join(table1, agg, Inner, Option('a <=> 'c), JoinHint.NONE)
+    val project = Project(Seq('a, 'b), join)
+
+    val optimized = Optimize.execute(project.analyze)
+    comparePlans(optimized, project.analyze)
+  }
+
+  test("Convert inner join to semi - doesn't produce all grouping columns") {
+      val table1 = LocalRelation('a.int, 'b.int)
+      val table2 = LocalRelation('c.int, 'd.int)
+      val agg = Aggregate(Seq('c, 'd), Seq('c), table2)
+      val join = Join(table1, agg, Inner, Option('a <=> 'c), JoinHint.NONE)
+      val project = Project(Seq('a, 'b), join)
+
+      val optimized = Optimize.execute(project.analyze)
+      comparePlans(optimized, project.analyze)
+  }
+
+  test("Convert inner join to semi - not an inner join") {
+    val table1 = LocalRelation('a.int, 'b.int)
+    val table2 = LocalRelation('c.int, 'd.int, 'e.int)
+    val agg = Aggregate(Seq('c, 'e), Seq('c, 'e), table2)
+    val join = Join(table1, agg, LeftOuter, Option('a <=> 'c && 'b <=> 'e), JoinHint.NONE)
+    val project = Project(Seq('a, 'b), join)
+
+    val optimized = Optimize.execute(project.analyze)
+    comparePlans(optimized, project.analyze)
+  }
+}
