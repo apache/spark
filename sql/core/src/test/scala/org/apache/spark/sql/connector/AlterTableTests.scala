47882196f7c28888278ccd94a8ec21af56c299fa
Daniel Tenedorio <daniel.tenedorio@databricks.com>
2022-06-16 11:26:07 -0400

    remove duplicate test

--- sql/core/src/test/scala/org/apache/spark/sql/connector/AlterTableTests.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/connector/AlterTableTests.scala
@@ -311,26 +311,6 @@ trait AlterTableTests extends SharedSparkSession {
     }
   }
 
-  test("SPARK-39383 DEFAULT columns on V2 data sources with ALTER TABLE ADD COLUMN") {
-    withSQLConf(SQLConf.DEFAULT_COLUMN_ALLOWED_PROVIDERS.key -> s"inmemory,$v2Format") {
-      val t = s"${catalogAndNamespace}table_name"
-      withTable("t") {
-        sql(s"create table $t (a string) using $v2Format")
-        sql(s"alter table $t add column (b int default 2 + 3)")
-
-        val tableName = fullTableName(t)
-        val table = getTableMetadata(tableName)
-
-        assert(table.name === tableName)
-        assert(table.schema === new StructType()
-          .add("a", StringType)
-          .add(StructField("b", IntegerType)
-            .withCurrentDefaultValue("2 + 3")
-            .withExistenceDefaultValue("5")))
-      }
-    }
-  }
-
   test("SPARK-39383 DEFAULT columns on V2 data sources with ALTER TABLE ALTER COLUMN") {
     withSQLConf(SQLConf.DEFAULT_COLUMN_ALLOWED_PROVIDERS.key -> s"inmemory,$v2Format") {
       val t = s"${catalogAndNamespace}table_name"
