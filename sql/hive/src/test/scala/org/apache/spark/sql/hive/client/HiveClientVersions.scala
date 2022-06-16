538c7ba49425c83be3c0671b19d107e1251373c0
Dongjoon Hyun <dongjoon@apache.org>
2022-06-16 16:26:59 -0700

    Avoid Java 11 'strip' method

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/client/HiveClientVersions.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/client/HiveClientVersions.scala
@@ -24,7 +24,7 @@ import org.apache.commons.lang3.{JavaVersion, SystemUtils}
 private[client] trait HiveClientVersions {
   private val testVersions = sys.env.get("SPARK_TEST_HIVE_CLIENT_VERSIONS")
   protected val versions = if (testVersions.nonEmpty) {
-    testVersions.get.split(",").map(_.strip).filter(_.nonEmpty).toIndexedSeq
+    testVersions.get.split(",").map(_.trim).filter(_.nonEmpty).toIndexedSeq
   } else if (SystemUtils.isJavaVersionAtLeast(JavaVersion.JAVA_9)) {
     IndexedSeq("2.0", "2.1", "2.2", "2.3", "3.0", "3.1")
   } else {
