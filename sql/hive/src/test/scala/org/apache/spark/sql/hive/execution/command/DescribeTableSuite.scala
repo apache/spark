42d33e133634e0d216cfcf914e6596e64412df07
Max Gekk <max.gekk@gmail.com>
2022-06-21 10:45:27 +0300

    [SPARK-37888][SQL][TESTS] Unify v1 and v2 `DESCRIBE TABLE` tests
    ### What changes were proposed in this pull request?
    1. Move `DESCRIBE TABLE` parsing tests to `DescribeRelationParserSuite`.
    2. Put common `DESCRIBE TABLE` tests into one trait `org.apache.spark.sql.execution.command.DescribeTableSuiteBase`, and put datasource specific tests to the `v1.DescribeTableSuite` and `v2.DescribeTableSuite`.
    
    The changes follow the approach of #30287.
    
    Closes #36671.
    
    ### Why are the changes needed?
    1. The unification will allow to run common `DESCRIBE TABLE` tests for both DSv1/Hive DSv1 and DSv2
    2. We can detect missing features and differences between DSv1 and DSv2 implementations.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    By running the modified test suites:
    ```
    $ build/sbt "testOnly *DDLParserSuite"
    $ build/sbt -Phive-2.3 -Phive-thriftserver "test:testOnly *HiveDDLSuite"
    ```
    and new test suites:
    ```
    $ build/sbt "sql/test:testOnly *DescribeTableParserSuite"
    $ build/sbt -Phive-2.3 -Phive-thriftserver "test:testOnly *DescribeTableSuite"
    ```
    
    Closes #36912 from MaxGekk/unify-describe-table-tests-4.
    
    Authored-by: Max Gekk <max.gekk@gmail.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

---
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/command/DescribeTableSuite.scala
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.hive.execution.command
+
+import org.apache.spark.sql.{QueryTest, Row}
+import org.apache.spark.sql.connector.catalog.TableCatalog
+import org.apache.spark.sql.execution.command.v1
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.util.Utils
+
+/**
+ * The class contains tests for the `DESCRIBE TABLE` command to check V1 Hive external
+ * table catalog.
+ */
+class DescribeTableSuite extends v1.DescribeTableSuiteBase with CommandSuiteBase {
+  override def commandVersion: String = super[DescribeTableSuiteBase].commandVersion
+
+  test("Table Ownership") {
+    withNamespaceAndTable("ns", "tbl") { t =>
+      sql(s"CREATE TABLE $t (c int) $defaultUsing")
+      checkHiveClientCalls(expected = 6) {
+        checkAnswer(
+          sql(s"DESCRIBE TABLE EXTENDED $t")
+            .where("col_name='Owner'")
+            .select("col_name", "data_type"),
+          Row("Owner", Utils.getCurrentUserName()))
+      }
+    }
+  }
+
+
+  test("DESCRIBE TABLE EXTENDED of a partitioned table") {
+    withNamespaceAndTable("ns", "table") { tbl =>
+      spark.sql(s"CREATE TABLE $tbl (id bigint, data string) $defaultUsing" +
+        " PARTITIONED BY (id)" +
+        " COMMENT 'this is a test table'" +
+        " LOCATION 'file:/tmp/testcat/table_name'")
+      val descriptionDf = spark.sql(s"DESCRIBE TABLE EXTENDED $tbl")
+      assert(descriptionDf.schema.map(field => (field.name, field.dataType)) === Seq(
+        ("col_name", StringType),
+        ("data_type", StringType),
+        ("comment", StringType)))
+      QueryTest.checkAnswer(
+        // Filter out 'Table Properties' to don't check `transient_lastDdlTime`
+        descriptionDf.filter("col_name != 'Created Time' and col_name != 'Table Properties'"),
+        Seq(
+          Row("data", "string", null),
+          Row("id", "bigint", null),
+          Row("# Partition Information", "", ""),
+          Row("# col_name", "data_type", "comment"),
+          Row("id", "bigint", null),
+          Row("", "", ""),
+          Row("# Detailed Table Information", "", ""),
+          Row("Database", "ns", ""),
+          Row("Table", "table", ""),
+          Row(TableCatalog.PROP_OWNER.capitalize, Utils.getCurrentUserName(), ""),
+          Row("Last Access", "UNKNOWN", ""),
+          Row("Created By", "Spark 3.4.0-SNAPSHOT", ""),
+          Row("Type", "EXTERNAL", ""),
+          Row("Provider", "hive", ""),
+          Row("Comment", "this is a test table", ""),
+          Row("Location", "file:/tmp/testcat/table_name", ""),
+          Row("Serde Library", "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe", ""),
+          Row("InputFormat", "org.apache.hadoop.mapred.TextInputFormat", ""),
+          Row("OutputFormat", "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat", ""),
+          Row("Storage Properties", "[serialization.format=1]", ""),
+          Row("Partition Provider", "Catalog", "")))
+    }
+  }
+}
