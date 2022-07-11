a056db38dad5c337f5443526220d63e0f96e0961
Yuming Wang <yumwang@ebay.com>
2022-07-11 22:32:00 +0800

    fix

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/statsEstimation/BasicStatsEstimationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/statsEstimation/BasicStatsEstimationSuite.scala
@@ -25,7 +25,6 @@ import org.apache.spark.sql.catalyst.dsl.plans._
 import org.apache.spark.sql.catalyst.expressions.{Ascending, Attribute, AttributeMap, AttributeReference, Literal, SortOrder}
 import org.apache.spark.sql.catalyst.plans.PlanTest
 import org.apache.spark.sql.catalyst.plans.logical._
-import org.apache.spark.sql.catalyst.plans.logical.statsEstimation.EstimationUtils
 import org.apache.spark.sql.connector.catalog.SupportsNamespaces
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{BooleanType, ByteType, IntegerType, LongType}
@@ -180,9 +179,7 @@ class BasicStatsEstimationSuite extends PlanTest with StatsEstimationTestBase {
   test("windows") {
     val windows = Window(Seq(min(attribute).as("sum_attr")), Seq(attribute), Nil, plan)
     val expectedDefaultStats = Statistics(sizeInBytes = plan.size.get * (4 + 4 + 8) / (4 + 8))
-    val expectedCboStats = Statistics(
-      sizeInBytes = plan.stats.sizeInBytes +
-        EstimationUtils.getSizePerRow(windows.windowOutputSet.toSeq) * 10,
+    val expectedCboStats = expectedDefaultStats.copy(
       rowCount = Some(10),
       attributeStats = AttributeMap(Seq(attribute -> colStat)))
     checkStats(
