dc9ca8dbb011de6d28f2fd78571269810ab5324a
Jungtaek Lim <kabhwan.opensource@gmail.com>
2022-07-13 21:29:45 +0900

    [SPARK-39748][SQL][SS][FOLLOWUP] Fix a bug on column stat in LogicalRDD on mismatching exprIDs

--- sql/core/src/main/scala/org/apache/spark/sql/execution/ExistingRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/ExistingRDD.scala
@@ -116,10 +116,20 @@ case class LogicalRDD(
       case e: Attribute => rewrite.getOrElse(e, e)
     }.asInstanceOf[SortOrder])
 
+    val rewrittenOriginLogicalPlan = originLogicalPlan.map { plan =>
+      val newPlan = plan.transformAllExpressions {
+        case e: Attribute => rewrite.getOrElse(e, e)
+      }
+      // It's not feasible to transform column stats directly for all logical nodes. Instead, we
+      // invalidate the cache so that the column stats are recalculated with new expression IDs.
+      newPlan.invalidateStatsCache()
+      newPlan
+    }
+
     LogicalRDD(
       output.map(rewrite),
       rdd,
-      originLogicalPlan,
+      rewrittenOriginLogicalPlan,
       rewrittenPartitioning,
       rewrittenOrdering,
       isStreaming
