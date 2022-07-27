ab328c6f32075c30201678306d4eed02727e0415
ulysses-you <ulyssesyou18@gmail.com>
2022-07-27 15:42:27 +0800

    address comment

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/basicLogicalOperators.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/basicLogicalOperators.scala
@@ -1430,11 +1430,12 @@ object Limit {
 /**
  * A global (coordinated) limit. This operator can emit at most `limitExpr` number in total.
  *
- * We can not make it inherit [[OrderPreservingUnaryNode]] due to the different strategy of
- * physical plan. The output ordering of child will be broken if a shuffle exchange comes in
- * between the child and global limit.
- *
  * See [[Limit]] for more information.
+ *
+ * Note that, we can not make it inherit [[OrderPreservingUnaryNode]] due to the different strategy
+ * of physical plan. The output ordering of child will be broken if a shuffle exchange comes in
+ * between the child and global limit, due to the fact that shuffle reader fetches blocks in random
+ * order.
  */
 case class GlobalLimit(limitExpr: Expression, child: LogicalPlan) extends UnaryNode {
   override def output: Seq[Attribute] = child.output
