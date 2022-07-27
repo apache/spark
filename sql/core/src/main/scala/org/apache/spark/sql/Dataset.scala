91b950568066830ecd7a4581ab5bf4dbdbbeb474
Ruifeng Zheng <ruifengz@apache.org>
2022-07-27 08:11:18 +0800

    [SPARK-39823][SQL][PYTHON] Rename Dataset.as as Dataset.to and add DataFrame.to in PySpark
    ### What changes were proposed in this pull request?
    
    1, rename `Dataset.as(StructType)` to `Dataset.to(StructType)`, since `as` is a keyword in python, we dont want to use a different name;
    2, Add `DataFrame.to(StructType)` in Python
    
    ### Why are the changes needed?
    for function parity
    
    ### Does this PR introduce _any_ user-facing change?
    yes, new api
    
    ### How was this patch tested?
    added UT
    
    Closes #37233 from zhengruifeng/py_ds_as.
    
    Authored-by: Ruifeng Zheng <ruifengz@apache.org>
    Signed-off-by: Ruifeng Zheng <ruifengz@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -476,14 +476,14 @@ class Dataset[T] private[sql](
    *   int.</li>
    *   <li>Carry over the metadata from the specified schema, while the columns and/or inner fields
    *   still keep their own metadata if not overwritten by the specified schema.</li>
-   *   <li>Fail if the nullability are not compatible. For example, the column and/or inner field is
+   *   <li>Fail if the nullability is not compatible. For example, the column and/or inner field is
    *   nullable but the specified schema requires them to be not nullable.</li>
    * </ul>
    *
    * @group basic
    * @since 3.4.0
    */
-  def as(schema: StructType): DataFrame = withPlan {
+  def to(schema: StructType): DataFrame = withPlan {
     Project.matchSchema(logicalPlan, schema, sparkSession.sessionState.conf)
   }
 
