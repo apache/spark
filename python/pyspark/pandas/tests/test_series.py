3021eb197f488bc5342549a720c6348e65d160ee
Ruifeng Zheng <ruifengz@apache.org>
2022-08-01 10:47:34 +0900

    [SPARK-39907][PS] Implement axis and skipna of Series.argmin
    ### What changes were proposed in this pull request?
    
    1, Implement axis and skipna of Series.argmin
    2, compute the argmin on single pass, like `argmax`
    
    ### Why are the changes needed?
    
    to add missing parameter
    after this change, the underlying implements of argmax and argmin are almost the same
    
    ### Does this PR introduce _any_ user-facing change?
    yes, new parameter
    
    ### How was this patch tested?
    added tests
    
    Closes #37328 from zhengruifeng/ps_update_argmin.
    
    Authored-by: Ruifeng Zheng <ruifengz@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/pandas/tests/test_series.py
+++ python/pyspark/pandas/tests/test_series.py
@@ -3012,8 +3012,12 @@ class SeriesTest(PandasOnSparkTestCase, SQLTestUtils):
         psser = ps.from_pandas(pser)
         self.assert_eq(pser.argmin(), psser.argmin())
         self.assert_eq(pser.argmax(), psser.argmax())
+        self.assert_eq(pser.argmin(skipna=False), psser.argmin(skipna=False))
+        self.assert_eq(pser.argmax(skipna=False), psser.argmax(skipna=False))
         self.assert_eq(pser.argmax(skipna=False), psser.argmax(skipna=False))
         self.assert_eq((pser + 1).argmax(skipna=False), (psser + 1).argmax(skipna=False))
+        self.assert_eq(pser.argmin(skipna=False), psser.argmin(skipna=False))
+        self.assert_eq((pser + 1).argmin(skipna=False), (psser + 1).argmin(skipna=False))
 
         # MultiIndex
         pser.index = pd.MultiIndex.from_tuples(
@@ -3023,6 +3027,13 @@ class SeriesTest(PandasOnSparkTestCase, SQLTestUtils):
         self.assert_eq(pser.argmin(), psser.argmin())
         self.assert_eq(pser.argmax(), psser.argmax())
         self.assert_eq(pser.argmax(skipna=False), psser.argmax(skipna=False))
+
+        pser2 = pd.Series([np.NaN, 1.0, 2.0, np.NaN])
+        psser2 = ps.from_pandas(pser2)
+        self.assert_eq(pser2.argmin(), psser2.argmin())
+        self.assert_eq(pser2.argmax(), psser2.argmax())
+        self.assert_eq(pser2.argmin(skipna=False), psser2.argmin(skipna=False))
+        self.assert_eq(pser2.argmax(skipna=False), psser2.argmax(skipna=False))
 
         # Null Series
         self.assert_eq(pd.Series([np.nan]).argmin(), ps.Series([np.nan]).argmin())
@@ -3037,6 +3048,8 @@ class SeriesTest(PandasOnSparkTestCase, SQLTestUtils):
             ps.Series([]).argmax()
         with self.assertRaisesRegex(ValueError, "axis can only be 0 or 'index'"):
             psser.argmax(axis=1)
+        with self.assertRaisesRegex(ValueError, "axis can only be 0 or 'index'"):
+            psser.argmin(axis=1)
 
     def test_backfill(self):
         pdf = pd.DataFrame({"x": [np.nan, 2, 3, 4, np.nan, 6]})
