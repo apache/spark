0d8e472dea4d48f78734af31f7ffcc05e88775a4
xzhou <15210830305@163.com>
2022-08-04 11:20:51 +0800

    modify UT description

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/TransposeWindowSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/TransposeWindowSuite.scala
@@ -143,7 +143,8 @@ class TransposeWindowSuite extends PlanTest {
     comparePlans(optimized, analyzed)
   }
 
-  test("transpose two adjacent windows with compatible partitions which is not a prefix") {
+  test("SPARK-38034: transpose two adjacent windows with compatible partitions " +
+    "which is not a prefix") {
     val query = testRelation
       .window(Seq(sum(c).as('sum_a_2)), partitionSpec4, orderSpec2)
       .window(Seq(sum(c).as('sum_a_1)), partitionSpec3, orderSpec1)
