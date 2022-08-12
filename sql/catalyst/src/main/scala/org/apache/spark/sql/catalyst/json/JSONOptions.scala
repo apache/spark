e57fa76ffd22538bf930eb4c6699fc178fb1f8c6
Daniel Tenedorio <daniel.tenedorio@databricks.com>
2022-08-12 14:01:59 -0700

    Update JSONOptions.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JSONOptions.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/json/JSONOptions.scala
@@ -88,7 +88,7 @@ private[sql] class JSONOptions(
   // values, never skip writing the NULL values to storage, overriding 'ignoreNullFields' above.
   // This can be useful to enforce that inserted NULL values are present in storage to differentiate
   // from missing data.
-  val defaultColumnsForceNullFields = SQLConf.get.defaultColumnJsonGeneratorForceNullFields
+  val defaultColumnsForceNullFields = SQLConf.get.defaultColumnJsonWriteNullIfWithDefaultValue
 
   // A language tag in IETF BCP 47 format
   val locale: Locale = parameters.get("locale").map(Locale.forLanguageTag).getOrElse(Locale.US)
