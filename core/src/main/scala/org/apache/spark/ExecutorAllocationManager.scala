75efb3ad3b6ced740875067846f9dae0de80826e
Warren Zhu <warren.zhu25@gmail.com>
2022-08-12 10:11:25 -0700

    Add numberDecommissioningExecutors metric

--- core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
+++ core/src/main/scala/org/apache/spark/ExecutorAllocationManager.scala
@@ -1005,6 +1005,8 @@ private[spark] class ExecutorAllocationManagerSource(
   registerGauge("numberMaxNeededExecutors",
     executorAllocationManager.numExecutorsTargetPerResourceProfileId.keys
       .map(executorAllocationManager.maxNumExecutorsNeededPerResourceProfile(_)).sum, 0)
+  registerGauge("numberDecommissioningExecutors",
+    executorAllocationManager.executorMonitor.decommissioningCount, 0)
 }
 
 private object ExecutorAllocationManager {
