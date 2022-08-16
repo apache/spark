2a8ac2522a716814b7f06e2c80e756015392d0d3
Qian.Sun <qian.sun2020@gmail.com>
2022-08-16 15:55:50 +0800

    Update python/pyspark/sql/streaming/query.py
    Co-authored-by: Hyukjin Kwon <gurwls223@gmail.com>

--- python/pyspark/sql/streaming/query.py
+++ python/pyspark/sql/streaming/query.py
@@ -483,6 +483,7 @@ class StreamingQueryManager:
         >>> test_listener = TestListener()
 
         Register streaming query listener
+
         >>> spark.streams.addListener(test_listener)
         """
         from pyspark import SparkContext
