eef7596d6156a14543656b4c44df3c3f4eceb159
Hyukjin Kwon <gurwls223@apache.org>
2022-08-17 07:47:19 -0700

    [SPARK-40114][R] Arrow 9.0.0 support with SparkR
    ### What changes were proposed in this pull request?
    
    This PR switches the deprecated API `write_arrow(..., raw())` to use `write_to_raw`. This is exactly the same behaviour/replacement.
    
    See also:
    - https://arrow.apache.org/docs/r/news/index.html#reading-and-writing-9-0-0 for `write_arrow`
    - https://arrow.apache.org/docs/r/news/index.html#feather-v2-0-17-0 for `write_to_raw`.
    - https://github.com/apache/arrow/blob/apache-arrow-8.0.0/r/R/deprecated.R#L31-L40 for deprecated implementation of `write_arrow`.
    
    ### Why are the changes needed?
    
    `write_arrow` is removed out from Arrow 9.0.0 in R Array library.
    
    Note that `write_to_raw` has existed from Arrow 0.17.0 so I expect this is a safe change.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, users can use R Arrow 9.0.0.
    
    ### How was this patch tested?
    
    Ci in this PR should test it out.
    
    Closes #37553 from HyukjinKwon/SPARK-40114.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- R/pkg/R/serialize.R
+++ R/pkg/R/serialize.R
@@ -231,7 +231,7 @@ writeSerializeInArrow <- function(conn, df) {
     # There looks no way to send each batch in streaming format via socket
     # connection. See ARROW-4512.
     # So, it writes the whole Arrow streaming-formatted binary at once for now.
-    writeRaw(conn, arrow::write_arrow(df, raw()))
+    writeRaw(conn, arrow::write_to_raw(df))
   } else {
     stop("'arrow' package should be installed.")
   }
