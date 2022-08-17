9601be96a86eced683e6aa2b772c726eeb231de8
Peter Toth <ptoth@cloudera.com>
2022-08-17 14:57:35 +0800

    [SPARK-39887][SQL][FOLLOW-UP] Do not exclude Union's first child attributes when traversing other children in RemoveRedundantAliases
    ### What changes were proposed in this pull request?
    Do not exclude `Union`'s first child attributes when traversing other children in `RemoveRedundantAliases`.
    
    ### Why are the changes needed?
    We don't need to exclude those attributes that `Union` inherits from its first child. See discussion here: https://github.com/apache/spark/pull/37496#discussion_r944509115
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Existing UTs.
    
    Closes #37534 from peter-toth/SPARK-39887-keep-attributes-of-unions-first-child-follow-up.
    
    Authored-by: Peter Toth <ptoth@cloudera.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit e732232dac420826af269d8cf5efacb52933f59a)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
@@ -544,7 +544,7 @@ object RemoveRedundantAliases extends Rule[LogicalPlan] {
         })
         Join(newLeft, newRight, joinType, newCondition, hint)
 
-      case _: Union =>
+      case u: Union =>
         var first = true
         plan.mapChildren { child =>
           if (first) {
@@ -555,7 +555,8 @@ object RemoveRedundantAliases extends Rule[LogicalPlan] {
             // output attributes could return incorrect result.
             removeRedundantAliases(child, excluded ++ child.outputSet)
           } else {
-            removeRedundantAliases(child, excluded)
+            // We don't need to exclude those attributes that `Union` inherits from its first child.
+            removeRedundantAliases(child, excluded -- u.children.head.outputSet)
           }
         }
 
