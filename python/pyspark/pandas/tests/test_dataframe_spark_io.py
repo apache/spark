9b7f7dcb59611afd60c376d7a5b0de16f55f901d
itholic <haejoon.lee@databricks.com>
2022-08-26 16:56:19 +0900

    [SPARK-40229][PS][TEST] Re-enable excel I/O test for pandas API on Spark.

--- python/pyspark/pandas/tests/test_dataframe_spark_io.py
+++ python/pyspark/pandas/tests/test_dataframe_spark_io.py
@@ -247,7 +247,6 @@ class DataFrameSparkIOTest(PandasOnSparkTestCase, TestUtils):
                 expected_idx.sort_values(by="f").to_spark().toPandas(),
             )
 
-    @unittest.skip("openpyxl")
     def test_read_excel(self):
         with self.temp_dir() as tmp:
 
