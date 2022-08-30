c1c4c9bcfaa3017eca331712d87366de9a1a666e
Ziqi Liu <ziqi.liu@databricks.com>
2022-08-30 09:19:11 -0700

    fix test

--- core/src/test/scala/org/apache/spark/scheduler/TaskResultGetterSuite.scala
+++ core/src/test/scala/org/apache/spark/scheduler/TaskResultGetterSuite.scala
@@ -307,7 +307,7 @@ class TaskResultGetterSuite extends SparkFunSuite with BeforeAndAfter with Local
     // value itself is pretty small(empty arrays)
     // Even setting MAX_RESULT_SIZE to a small value(1M here), it should not throw exception
     // because the actual result is small
-    assert(rdd.filter(_ < 0).collect() === 0)
+    assert(rdd.filter(_ < 0).collect().isEmpty)
   }
 
 }
