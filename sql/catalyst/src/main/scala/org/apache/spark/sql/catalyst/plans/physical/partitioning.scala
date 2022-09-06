0e43c1962b4ca20fb26be00e688909d4151ccc76
aokolnychyi <aokolnychyi@apple.com>
2022-09-06 11:03:05 -0700

    Improve the check

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/physical/partitioning.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/physical/partitioning.scala
@@ -17,6 +17,7 @@
 
 package org.apache.spark.sql.catalyst.plans.physical
 
+import scala.annotation.tailrec
 import scala.collection.mutable
 
 import org.apache.spark.sql.catalyst.InternalRow
@@ -367,9 +368,16 @@ object KeyGroupedPartitioning {
       transform.children.size == 1 && transform.children.head.isInstanceOf[AttributeReference]
     }
 
+    @tailrec
+    def isReference(e: Expression): Boolean = e match {
+      case _: Attribute => true
+      case g: GetStructField => isReference(g.child)
+      case _ => false
+    }
+
     expressions.forall {
-      case _: NamedExpression => true
       case t: TransformExpression if isSupportedTransform(t) => true
+      case e: Expression if isReference(e) => true
       case _ => false
     }
   }
