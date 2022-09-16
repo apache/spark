a2f25e3d17a768f3e225db27a2fd1b050369469f
Jungtaek Lim <kabhwan.opensource@gmail.com>
2022-09-16 15:03:55 +0900

    unused import

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/PandasGroupUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/PandasGroupUtils.scala
@@ -24,7 +24,7 @@ import org.apache.spark.TaskContext
 import org.apache.spark.api.python.BasePythonRunner
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{Attribute, UnsafeProjection}
-import org.apache.spark.sql.execution.{GroupedIterator, SparkPlan}
+import org.apache.spark.sql.execution.GroupedIterator
 import org.apache.spark.sql.vectorized.{ArrowColumnVector, ColumnarBatch}
 
 /**
