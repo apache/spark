95a1400284572e841822353c9113b788fa359600
Jungtaek Lim <kabhwan.opensource@gmail.com>
2022-09-20 21:10:59 +0900

    further doc

--- sql/core/src/main/scala/org/apache/spark/sql/RelationalGroupedDataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/RelationalGroupedDataset.scala
@@ -622,6 +622,20 @@ class RelationalGroupedDataset protected[sql](
     Dataset.ofRows(df.sparkSession, plan)
   }
 
+  /**
+   * Applies a grouped vectorized python user-defined function to each group of data.
+   * The user-defined function defines a transformation: iterator of `pandas.DataFrame` ->
+   * iterator of `pandas.DataFrame`.
+   * For each group, all elements in the group are passed as an iterator of `pandas.DataFrame`
+   * along with corresponding state, and the results for all groups are combined into a new
+   * [[DataFrame]].
+   *
+   * This function does not support partial aggregation, and requires shuffling all the data in
+   * the [[DataFrame]].
+   *
+   * This function uses Apache Arrow as serialization format between Java executors and Python
+   * workers.
+   */
   private[sql] def applyInPandasWithState(
       func: PythonUDF,
       outputStructType: StructType,
