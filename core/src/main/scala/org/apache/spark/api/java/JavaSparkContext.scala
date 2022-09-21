c64428b96a4ed728e3aa0878bde0e35efed2badc
Jiaan Geng <beliefer@163.com>
2022-09-21 10:23:28 +0800

    Update JavaSparkContext.scala

--- core/src/main/scala/org/apache/spark/api/java/JavaSparkContext.scala
+++ core/src/main/scala/org/apache/spark/api/java/JavaSparkContext.scala
@@ -182,6 +182,8 @@ class JavaSparkContext(val sc: SparkContext) extends Closeable {
   def textFile(path: String, minPartitions: Int): JavaRDD[String] =
     sc.textFile(path, minPartitions)
 
+
+
   /**
    * Read a directory of text files from HDFS, a local file system (available on all nodes), or any
    * Hadoop-supported file system URI. Each file is read as a single record and returned in a
