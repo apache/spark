6e794fbda55043bd82993182178cc3344c473cd7
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2022-09-24 19:46:06 +0300

    fix

--- dev/change-scala-version.sh
+++ dev/change-scala-version.sh
@@ -89,7 +89,7 @@ else
 fi
 sed_i 's/scala\-'$FROM_VERSION'/scala\-'$TO_VERSION'/' "$BASEDIR/docs/_plugins/copy_api_dirs.rb"
 
-
+echo "$BASEDIR/mllib/src/main/scala/org/apache/spark/mllib/feature/SkipGram.scala"
 sed_i 's/^\([[:space:]]*\/\* \(#if scala-'$TO_VERSION'\)\)/\/\/ \2/' "$BASEDIR/mllib/src/main/scala/org/apache/spark/mllib/feature/SkipGram.scala"
 sed_i 's/^\([[:space:]]*#endif scala-'$TO_VERSION'\) \(\*\/\)/\/\/ \1/' "$BASEDIR/mllib/src/main/scala/org/apache/spark/mllib/feature/SkipGram.scala"
 
