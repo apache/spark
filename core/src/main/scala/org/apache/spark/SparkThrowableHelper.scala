7230f59868808ad7544f035247d776db1088a74d
lvshaokang <lvshaokang1@gmail.com>
2022-09-26 19:26:12 +0300

    [SPARK-40357][SQL] Migrate window type check failures onto error classes
    ### What changes were proposed in this pull request?
    
    In the PR, I propose to use error classes in the case of type check failure in window expressions.
    
    ### Why are the changes needed?
    
    Migration onto error classes unifies Spark SQL error messages.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes. The PR changes user-facing error messages.
    
    ### How was this patch tested?
    
    ```
    build/sbt "sql/testOnly *SQLQueryTestSuite"
    build/sbt "test:testOnly *Window*Suite"
    ```
    
    Closes #37986 from lvshaokang/SPARK-40357.
    
    Authored-by: lvshaokang <lvshaokang1@gmail.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- core/src/main/scala/org/apache/spark/SparkThrowableHelper.scala
+++ core/src/main/scala/org/apache/spark/SparkThrowableHelper.scala
@@ -102,7 +102,8 @@ private[spark] object SparkThrowableHelper {
             messageParameters.asScala
               .toMap // To remove duplicates
               .toSeq.sortBy(_._1)
-              .foreach { case (name, value) => g.writeStringField(name, value) }
+              .foreach { case (name, value) =>
+                g.writeStringField(name, value.replaceAll("#\\d+", "#x")) }
             g.writeEndObject()
           }
           val queryContext = e.getQueryContext
