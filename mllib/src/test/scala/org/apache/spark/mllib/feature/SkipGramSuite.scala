f5917f915d3f9b3fd4cdd6aedd48ddd706f67e8f
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2022-10-04 18:53:21 +0300

    fix

---
+++ mllib/src/test/scala/org/apache/spark/mllib/feature/SkipGramSuite.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.mllib.feature
+
+import org.apache.spark.SparkFunSuite
+import org.apache.spark.internal.config.Kryo._
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.util.MLlibTestSparkContext
+import org.apache.spark.sql.internal.SQLConf._
+import org.apache.spark.util.Utils
+
+class SkipGramSuite extends SparkFunSuite with MLlibTestSparkContext {
+
+  // TODO: add more tests
+
+  test("SkipGram") {
+    val sentence = "a b " * 100 + "a c " * 10
+    val localDoc = Seq(sentence, sentence)
+    val doc = sc.parallelize(localDoc)
+      .map(line => line.split(" ").toSeq)
+    val model = new Word2Vec().setVectorSize(10).setSeed(42L).fit(doc)
+    val syms = model.findSynonyms("a", 2)
+    assert(syms.length == 2)
+    assert(syms(0)._1 == "b")
+    assert(syms(1)._1 == "c")
+
+    // Test that model built using Word2Vec, i.e wordVectors and wordIndec
+    // and a Word2VecMap give the same values.
+    val word2VecMap = model.getVectors
+    val newModel = new Word2VecModel(word2VecMap)
+    assert(newModel.getVectors.mapValues(_.toSeq).toMap ===
+      word2VecMap.mapValues(_.toSeq).toMap)
+  }
+}
