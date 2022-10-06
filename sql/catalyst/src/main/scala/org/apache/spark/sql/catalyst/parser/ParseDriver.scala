ee1c624fa03d15e6b71b0ecef044ee3c9167a812
Gengliang Wang <gengliang@apache.org>
2022-10-06 10:55:49 -0700

    [SPARK-40585][SQL][FOLLOWUP] Simplify string literal implementation in SQL parser
    ### What changes were proposed in this pull request?
    
    * Simplify string literal implementation in SQL parser by using `stringLit` as many as possible in SqlBaseParser.g4
    * rename `double_quoted_identifiers` as `doubleQuotedIdentifiers` for keeping naming style consistent.
    ### Why are the changes needed?
    
    Simplify string literal implementation in SQL parser.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Existing UT
    
    Closes #38111 from gengliangwang/refactor.
    
    Authored-by: Gengliang Wang <gengliang@apache.org>
    Signed-off-by: Gengliang Wang <gengliang@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/ParseDriver.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/ParseDriver.scala
@@ -118,7 +118,7 @@ abstract class AbstractSqlParser extends ParserInterface with SQLConfHelper with
     parser.legacy_setops_precedence_enabled = conf.setOpsPrecedenceEnforced
     parser.legacy_exponent_literal_as_decimal_enabled = conf.exponentLiteralAsDecimalEnabled
     parser.SQL_standard_keyword_behavior = conf.enforceReservedKeywords
-    parser.double_quoted_identifiers = conf.double_quoted_identifiers
+    parser.double_quoted_identifiers = conf.doubleQuotedIdentifiers
 
     try {
       try {
