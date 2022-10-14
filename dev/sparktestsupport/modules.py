25975fbf85bbed45a3039e08675a35c314f5fdff
Hyukjin Kwon <gurwls223@gmail.com>
2022-10-14 09:29:54 +0900

    Update dev/sparktestsupport/modules.py

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -465,7 +465,7 @@ pyspark_sql = Module(
         "pyspark.sql.tests.pandas.test_pandas_grouped_map",
         "pyspark.sql.tests.pandas.test_pandas_grouped_map_with_state",
         "pyspark.sql.tests.pandas.test_pandas_map",
-        "pyspark.sql.tests.pandas.test_arrow_map",
+        "pyspark.sql.tests.test_arrow_map",
         "pyspark.sql.tests.pandas.test_pandas_udf",
         "pyspark.sql.tests.pandas.test_pandas_udf_grouped_agg",
         "pyspark.sql.tests.pandas.test_pandas_udf_scalar",
