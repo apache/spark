1023eb2be42c939401fe5bc4e99807bb4e07c748
Ruifeng Zheng <ruifengz@apache.org>
2022-10-14 11:17:26 +0800

    add to ci

--- connector/connect/dev/generate_protos.sh
+++ connector/connect/dev/generate_protos.sh
@@ -18,8 +18,23 @@
 #
 set -ex
 
+if [[ $# -gt 1 ]]; then
+  echo "Illegal number of parameters."
+  echo "Usage: ./connector/connect/dev/generate_protos.sh [path]"
+  exit -1
+fi
+
+
 SPARK_HOME="$(cd "`dirname $0`"/../../..; pwd)"
 cd "$SPARK_HOME"
+
+
+OUTPUT_PATH=${SPARK_HOME}/python/pyspark/sql/connect/proto/
+if [[ $# -eq 1 ]]; then
+  rm -Rf $1
+  mkdir -p $1
+  OUTPUT_PATH=$1
+fi
 
 pushd connector/connect/src/main
 
@@ -77,7 +92,7 @@ black --config $SPARK_HOME/dev/pyproject.toml gen/proto/python
 
 # Last step copy the result files to the destination module.
 for f in `find gen/proto/python -name "*.py*"`; do
-  cp $f $SPARK_HOME/python/pyspark/sql/connect/proto
+  cp $f $OUTPUT_PATH
 done
 
 # Clean up everything.
