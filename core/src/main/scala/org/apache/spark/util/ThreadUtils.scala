d373c00d5ca3e3692a7865deb692eaef891231b7
Jerry Peng <jerry.peng@databricks.com>
2022-10-19 16:39:01 -0700

    [SPARK-40849][SS] Async log purge

--- core/src/main/scala/org/apache/spark/util/ThreadUtils.scala
+++ core/src/main/scala/org/apache/spark/util/ThreadUtils.scala
@@ -162,9 +162,9 @@ private[spark] object ThreadUtils {
   /**
    * Wrapper over newSingleThreadExecutor.
    */
-  def newDaemonSingleThreadExecutor(threadName: String): ExecutorService = {
+  def newDaemonSingleThreadExecutor(threadName: String): ThreadPoolExecutor = {
     val threadFactory = new ThreadFactoryBuilder().setDaemon(true).setNameFormat(threadName).build()
-    Executors.newSingleThreadExecutor(threadFactory)
+    Executors.newSingleThreadExecutor(threadFactory).asInstanceOf[ThreadPoolExecutor]
   }
 
   /**
