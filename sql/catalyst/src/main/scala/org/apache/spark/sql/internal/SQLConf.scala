91407958b49bc04b4d1aa8609a9064056a25d2df
ulysses-you <ulyssesyou18@gmail.com>
2022-10-24 15:20:54 +0800

    [SPARK-40798][SQL] Alter partition should verify value follow storeAssignmentPolicy
    ### What changes were proposed in this pull request?
    
    extract the check insertion field cast methold so that we can do validate patition value at PartitioningUtils.normalizePartitionSpec
    
    ### Why are the changes needed?
    
    Insertion follow the behavior of config `spark.sql.storeAssignmentPolicy`, which will fail if the value can not cast to target data type by default. Alter partition should also follow it. For example:
    ```SQL
    CREATE TABLE t (c int) USING PARQUET PARTITIONED BY(p int);
    
    -- This DDL should fail but worked:
    ALTER TABLE t ADD PARTITION(p='aaa');
    
    -- FAILED which follows spark.sql.storeAssignmentPolicy
    INSERT INTO t PARTITION(p='aaa') SELECT 1
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    yes,  the added partition value will follow the behavior of  `storeAssignmentPolicy`.
    
    To restore the previous behavior, set spark.sql.legacy.skipPartitionSpecTypeValidation = true;
    
    ### How was this patch tested?
    
    add test
    
    Closes #38257 from ulysses-you/verify-partition.
    
    Authored-by: ulysses-you <ulyssesyou18@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -3004,6 +3004,16 @@ object SQLConf {
       .booleanConf
       .createWithDefault(false)
 
+  val SKIP_TYPE_VALIDATION_ON_ALTER_PARTITION =
+    buildConf("spark.sql.legacy.skipTypeValidationOnAlterPartition")
+      .internal()
+      .doc("When true, skip validation for partition spec in ALTER PARTITION. E.g., " +
+        "`ALTER TABLE .. ADD PARTITION(p='a')` would work even the partition type is int. " +
+        s"When false, the behavior follows ${STORE_ASSIGNMENT_POLICY.key}")
+      .version("3.4.0")
+      .booleanConf
+      .createWithDefault(false)
+
   val SORT_BEFORE_REPARTITION =
     buildConf("spark.sql.execution.sortBeforeRepartition")
       .internal()
