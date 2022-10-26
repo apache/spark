e7970311cefa755197c97e36aaea659baf4a8d51
itholic <haejoon.lee@databricks.com>
2022-10-26 13:52:38 +0900

    revert fix test

--- sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/sources/RateStreamProviderSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/sources/RateStreamProviderSuite.scala
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
-import org.apache.spark.{SparkArithmeticException, SparkRuntimeException}
+import org.apache.spark.{SparkRuntimeException}
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.connector.read.streaming.{Offset, SparkDataStream}
@@ -310,7 +310,7 @@ class RateStreamProviderSuite extends StreamTest {
       .distinct()
     testStream(input)(
       AdvanceRateManualClock(2),
-      ExpectFailure[SparkArithmeticException](t => {
+      ExpectFailure[SparkRuntimeException](t => {
         Seq("INCORRECT_END_OFFSET", "rowsPerSecond").foreach { msg =>
           assert(t.getMessage.contains(msg))
         }
