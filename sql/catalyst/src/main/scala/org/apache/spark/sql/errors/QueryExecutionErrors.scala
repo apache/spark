b357a2ee06f9b3c72fc4e7738d659a03e90aeb7d
itholic <haejoon.lee@databricks.com>
2022-10-28 15:32:37 +0900

    CatalogNotFoundException already extends SparkException

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
@@ -45,7 +45,7 @@ import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.plans.logical.statsEstimation.ValueInterval
 import org.apache.spark.sql.catalyst.trees.{SQLQueryContext, TreeNode}
 import org.apache.spark.sql.catalyst.util.{sideBySide, BadRecordException, DateTimeUtils, FailFastMode}
-import org.apache.spark.sql.connector.catalog.{Identifier, Table, TableProvider}
+import org.apache.spark.sql.connector.catalog.{CatalogNotFoundException, Identifier, Table, TableProvider}
 import org.apache.spark.sql.connector.catalog.CatalogV2Implicits._
 import org.apache.spark.sql.connector.expressions.Transform
 import org.apache.spark.sql.execution.QueryExecutionException
@@ -2090,11 +2090,8 @@ private[sql] object QueryExecutionErrors extends QueryErrorsBase {
   }
 
   def catalogPluginClassNotFoundError(name: String): Throwable = {
-    new SparkException(
-      errorClass = "_LEGACY_ERROR_TEMP_2213",
-      messageParameters = Map(
-        "name" -> name),
-      cause = null)
+    new CatalogNotFoundException(
+      s"Catalog '$name' plugin class not found: spark.sql.catalog.$name is not defined")
   }
 
   def catalogPluginClassNotImplementedError(name: String, pluginClassName: String): Throwable = {
