3cccb9e4c7074d58f4f221a9b11af46c0b79889a
yangjie01 <yangjie01@baidu.com>
2022-11-02 00:13:06 +0800

    fix test

--- sql/catalyst/src/test/scala/org/apache/spark/sql/RowTest.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/RowTest.scala
@@ -97,11 +97,12 @@ class RowTest extends AnyFunSpec with Matchers {
     }
 
     it("json should convert a mutable array to JSON") {
+      import org.json4s.jackson.JsonMethods.compact
       val schema = new StructType().add(StructField("list", ArrayType(StringType)))
       val values = ArraySeq("1", "2", "3")
       val row = new GenericRowWithSchema(Array(values), schema)
       val expectedList = JArray(JString("1") :: JString("2") :: JString("3") :: Nil)
-      row.jsonValue shouldBe new JObject(("list", expectedList) :: Nil)
+      assert(row.json === compact(JObject(("list", expectedList) :: Nil)))
     }
   }
 
