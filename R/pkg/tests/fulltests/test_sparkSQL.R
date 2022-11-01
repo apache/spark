0726347e059b81106ff12cc5e8510f7d4caefe61
itholic <haejoon.lee@databricks.com>
2022-11-01 19:42:06 +0900

    fix

--- R/pkg/tests/fulltests/test_sparkSQL.R
+++ R/pkg/tests/fulltests/test_sparkSQL.R
@@ -3991,20 +3991,15 @@ test_that("Call DataFrameWriter.load() API in Java without path and check argume
                paste("Error in load : analysis error - Unable to infer schema for JSON.",
                      "It must be specified manually"))
   expect_error(read.df("arbitrary_path"),
-               paste("Error in load : analysis error - [PATH_NOT_FOUND] Path does not exist:  ",
-                     "file:/__w/spark/spark/arbitrary_path."))
+               "Error in load : analysis error - [PATH_NOT_FOUND] Path does not exist:")
   expect_error(read.json("arbitrary_path"),
-               paste("Error in json : analysis error - [PATH_NOT_FOUND] Path does not exist:  ",
-                     "file:/__w/spark/spark/arbitrary_path."))
+               "Error in json : analysis error - [PATH_NOT_FOUND] Path does not exist:")
   expect_error(read.text("arbitrary_path"),
-               paste("Error in text : analysis error - [PATH_NOT_FOUND] Path does not exist:  ",
-                     "file:/__w/spark/spark/arbitrary_path."))
+               "Error in text : analysis error - [PATH_NOT_FOUND] Path does not exist:")
   expect_error(read.orc("arbitrary_path"),
-               paste("Error in orc : analysis error - [PATH_NOT_FOUND] Path does not exist:  ",
-                     "file:/__w/spark/spark/arbitrary_path."))
+               "Error in orc : analysis error - [PATH_NOT_FOUND] Path does not exist:")
   expect_error(read.parquet("arbitrary_path"),
-               paste("Error in parquet : analysis error - [PATH_NOT_FOUND] Path does not exist:  ",
-                     "file:/__w/spark/spark/arbitrary_path."))
+               "Error in parquet : analysis error - [PATH_NOT_FOUND] Path does not exist:")
 
   # Arguments checking in R side.
   expect_error(read.df(path = c(3)),
