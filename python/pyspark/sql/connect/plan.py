83730081bb160e58fa748729be6f62cf45869d37
Rui Wang <rui.wang@databricks.com>
2022-11-10 16:57:01 -0800

    update

--- python/pyspark/sql/connect/plan.py
+++ python/pyspark/sql/connect/plan.py
@@ -712,7 +712,8 @@ class SubqueryAlias(LogicalPlan):
 
     def plan(self, session: "RemoteSparkSession") -> proto.Relation:
         rel = proto.Relation()
-        rel.subquery_alias.input.CopyFrom(self._child.plan(session))
+        if self._child is not None:
+            rel.subquery_alias.input.CopyFrom(self._child.plan(session))
         rel.subquery_alias.alias = self._alias
         return rel
 
