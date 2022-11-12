7b108497824f941af020e2816ed4506d08a00ce9
Rui Wang <rui.wang@databricks.com>
2022-11-12 01:07:54 -0800

    update

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -1158,7 +1158,7 @@ class DatasetSuite extends QueryTest
           dataset.createTempView("test_db.tempView"))
         checkError(e,
           errorClass = "TEMP_VIEW_DOES_NOT_BELONG_TO_A_DATABASE",
-          parameters = Map("dbName" -> "test_db"))
+          parameters = Map("database" -> "test_db"))
       }
 
       withSQLConf(SQLConf.ALLOW_TEMP_VIEW_CREATION_WITH_DATABASE_NAME.key -> "true") {
