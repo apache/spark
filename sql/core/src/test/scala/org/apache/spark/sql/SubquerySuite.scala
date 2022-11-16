0ba23c5b4b37109f410f2fa86de4393a30b5e636
itholic <haejoon.lee@databricks.com>
2022-11-16 15:07:27 +0900

    fix test

--- sql/core/src/test/scala/org/apache/spark/sql/SubquerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SubquerySuite.scala
@@ -964,17 +964,14 @@ class SubquerySuite extends QueryTest
             |               WHERE t1.c1 = t2.c1)
           """.stripMargin)
       }
-      checkErrorMatchPVals(
+      checkError(
         exception1,
         errorClass = "UNSUPPORTED_SUBQUERY_EXPRESSION_CATEGORY.CORRELATED_REFERENCE",
-        parameters = Map("treeNode" -> "(?s).*"),
-        sqlState = None,
+        parameters = Map("sqlExprs" -> "explode(outer(t2.arr_c2)),c2"),
         context = ExpectedContext(
           fragment = "LATERAL VIEW explode(t2.arr_c2) q AS c2",
           start = 68,
           stop = 106))
-      assert(exception1.getMessage.contains(
-        "Expressions referencing the outer query are not supported outside of WHERE/HAVING"))
     }
   }
 
