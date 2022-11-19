4066162d8c242f999c89e7a82f616044267db311
Weichen Xu <weichen.xu@databricks.com>
2022-11-19 12:51:41 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -542,8 +542,12 @@ class SparkContext(config: SparkConf) extends Logging {
     executorEnvs("SPARK_USER") = sparkUser
 
     if (_conf.getOption("spark.executorEnv.OMP_NUM_THREADS").isEmpty) {
+      // if OMP_NUM_THREADS is not explicitly set, override it with the value of "spark.task.cpus"
       // SPARK-41188: limit the thread number for OpenBLAS routine to the number of cores assigned
       // to this executor because some spark ML algorithms calls OpenBlAS via netlib-java
+      // SPARK-28843: limit the OpenMP thread pool to the number of cores assigned to this executor
+      // this avoids high memory consumption with pandas/numpy because of a large OpenMP thread pool
+      // see https://github.com/numpy/numpy/issues/10455
       executorEnvs.put("OMP_NUM_THREADS", _conf.get("spark.task.cpus", "1"))
     }
 
