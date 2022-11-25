50f6153d5ee70e095f4341f2f38cf81d6cbd146e
Martin Grund <grundprinzip@gmail.com>
2022-11-25 16:12:04 +0100

    This and other thigns

--- python/pyspark/sql/connect/session.py
+++ python/pyspark/sql/connect/session.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-
 from threading import RLock
 from typing import Optional, Any, Union, Dict, cast, overload
 
