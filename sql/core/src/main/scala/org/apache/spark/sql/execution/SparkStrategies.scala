7c8acb5d07a49df5adf7c8ff33aea2fe2da03651
Jiaan Geng <beliefer@163.com>
2022-11-30 21:28:53 +0800

    Update code

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkStrategies.scala
@@ -670,7 +670,7 @@ abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
 
       plan match {
         case filter @ Filter(condition,
-          logical.Window(windowExpressions, partitionSpec, orderSpec, child))
+          window @ logical.Window(windowExpressions, partitionSpec, orderSpec, child))
           if !child.isInstanceOf[logical.Window] &&
             supports(windowExpressions) && orderSpec.nonEmpty =>
           val limits = windowExpressions.collect {
@@ -686,7 +686,7 @@ abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
           minLimit match {
             case Some((limit, rankLikeFunction)) if limit <= conf.windowGroupLimitThreshold =>
               if (limit > 0) {
-                // TODO: add a physical rule to remove the partialLimitExec node,
+                // TODO: [SPARK-41337] add a physical rule to remove the partialLimitExec node,
                 // if there is no shuffle between the two nodes (partialLimitExec's
                 // outputPartitioning satisfies the finalLimitExec's requiredChildDistribution)
                 val partialLimitExec = execution.window.WindowGroupLimitExec(partitionSpec,
@@ -695,6 +695,7 @@ abstract class SparkStrategies extends QueryPlanner[SparkPlan] {
                   orderSpec, rankLikeFunction, limit, execution.window.Final, partialLimitExec)
                 val windowExec = execution.window.WindowExec(
                   windowExpressions, partitionSpec, orderSpec, finalLimitExec)
+                windowExec.setLogicalLink(window)
                 val filterExec = execution.FilterExec(condition, windowExec)
                 filterExec :: Nil
               } else {
