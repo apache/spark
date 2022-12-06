4fa9abb8d0aab8364f7242769bca062edeb4949d
yangjie01 <yangjie01@baidu.com>
2022-12-06 11:40:55 +0800

    add comments

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -86,6 +86,7 @@ object BuildCommons {
   val javaVersion = settingKey[String]("source and target JVM version for javac and scalac")
 
   // Google Protobuf version used for generating the protobuf.
+  // SPARK-41247: needs to be consistent with `protobuf.version` in `pom.xml`.
   val protoVersion = "3.21.9"
   // GRPC version used for Spark Connect.
   val gprcVersion = "1.47.0"
