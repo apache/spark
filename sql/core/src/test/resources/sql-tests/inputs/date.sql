e8b70cd51892e4eeaf12ec6dd23e10f801b95d0b
yangjie01 <yangjie01@baidu.com>
2022-12-06 23:43:43 +0800

    change case

--- sql/core/src/test/resources/sql-tests/inputs/date.sql
+++ sql/core/src/test/resources/sql-tests/inputs/date.sql
@@ -19,7 +19,7 @@ select date'2021-4294967297-11';
 select current_date = current_date;
 -- under ANSI mode, `current_date` can't be a function name.
 select current_date() = current_date();
-select curdate();
+select curdate() = curdate();
 select curdate(1);
 
 -- conversions between date and unix_date (number of days from epoch)
