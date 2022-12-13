d34eeb2a1e61dc033537146c69badfb08bac20cb
Gengliang Wang <gengliang@apache.org>
2022-12-13 00:17:29 -0800

    support TaskDataWrapper

--- core/src/main/scala/org/apache/spark/status/protobuf/Utils.scala
+++ core/src/main/scala/org/apache/spark/status/protobuf/Utils.scala
@@ -17,21 +17,10 @@
 
 package org.apache.spark.status.protobuf
 
-import org.apache.spark.status.{JobDataWrapper, TaskDataWrapper}
-import org.apache.spark.status.KVUtils.KVStoreScalaSerializer
-
-private[spark] class KVStoreProtobufSerializer extends KVStoreScalaSerializer {
-  override def serialize(o: Object): Array[Byte] = o match {
-    case j: JobDataWrapper => JobDataWrapperSerializer.serialize(j)
-    case t: TaskDataWrapper => TaskDataWrapperSerializer.serialize(t)
-    case other => super.serialize(other)
-  }
-
-  override def deserialize[T](data: Array[Byte], klass: Class[T]): T = klass match {
-    case _ if classOf[JobDataWrapper].isAssignableFrom(klass) =>
-      JobDataWrapperSerializer.deserialize(data).asInstanceOf[T]
-    case _ if classOf[TaskDataWrapper].isAssignableFrom(klass) =>
-      TaskDataWrapperSerializer.deserialize(data).asInstanceOf[T]
-    case other => super.deserialize(data, klass)
+object Utils {
+  def getOptional[T](condition: Boolean, result: () => T): Option[T] = if (condition) {
+    Some(result())
+  } else {
+    None
   }
 }
