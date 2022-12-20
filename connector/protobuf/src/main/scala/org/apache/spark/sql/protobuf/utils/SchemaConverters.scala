231f0a8ae189202fb8b29d710f02eb63630f403a
SandishKumarHN <sanysandish@gmail.com>
2022-12-20 12:01:21 -0800

    review changes, comment for existingRecordNames

--- connector/protobuf/src/main/scala/org/apache/spark/sql/protobuf/utils/SchemaConverters.scala
+++ connector/protobuf/src/main/scala/org/apache/spark/sql/protobuf/utils/SchemaConverters.scala
@@ -57,6 +57,9 @@ object SchemaConverters {
       nullable = true)
   }
 
+  // existingRecordNames: Map[String, Int] used to track the depth of recursive fields and to
+  // ensure that the conversion of the protobuf message to a Spark SQL StructType object does not
+  // exceed the maximum recursive depth specified by the recursiveFieldMaxDepth option.
   def structFieldFor(
       fd: FieldDescriptor,
       existingRecordNames: Map[String, Int],
