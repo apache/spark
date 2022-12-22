743cd10c17817a9a0b0be7bef9a88163a76a0a7d
Sandeep Singh <sandeep@techaddict.me>
2022-12-21 16:50:33 -0800

    [SPARK-41432][UI][SQL] Protobuf serializer for SparkPlanGraphWrapper

---
+++ sql/core/src/test/scala/org/apache/spark/status/protobuf/sql/KVStoreProtobufSerializerSuite.scala
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.status.protobuf.sql
+
+import org.apache.spark.SparkFunSuite
+import org.apache.spark.sql.execution.ui._
+import org.apache.spark.status.protobuf.KVStoreProtobufSerializer
+
+class KVStoreProtobufSerializerSuite extends SparkFunSuite {
+  private val serializer = new KVStoreProtobufSerializer()
+
+  test("Spark Plan Graph") {
+    val input = new SparkPlanGraphWrapper(
+      executionId = 1,
+      nodes = Seq(new SparkPlanGraphNodeWrapper(
+        node = new SparkPlanGraphNode(
+          id = 2,
+          name = "name_1",
+          desc = "desc_1",
+          metrics = Seq(
+            SQLPlanMetric(
+              name = "name_2",
+              accumulatorId = 3,
+              metricType = "metric_1"
+            ),
+            SQLPlanMetric(
+              name = "name_3",
+              accumulatorId = 4,
+              metricType = "metric_2"
+            )
+          )
+        ),
+        cluster = new SparkPlanGraphClusterWrapper(
+          id = 5,
+          name = "name_5",
+          desc = "desc_5",
+          nodes = Seq(new SparkPlanGraphNodeWrapper(
+            node = new SparkPlanGraphNode(
+              id = 12,
+              name = "name_12",
+              desc = "desc_12",
+              metrics = Seq(
+                SQLPlanMetric(
+                  name = "name_13",
+                  accumulatorId = 13,
+                  metricType = "metric_13"
+                ),
+                SQLPlanMetric(
+                  name = "name_14",
+                  accumulatorId = 14,
+                  metricType = "metric_14"
+                )
+              )
+            ),
+            cluster = new SparkPlanGraphClusterWrapper(
+              id = 15,
+              name = "name_15",
+              desc = "desc_15",
+              nodes = Seq(),
+              metrics = Seq(
+                SQLPlanMetric(
+                  name = "name_16",
+                  accumulatorId = 16,
+                  metricType = "metric_16"
+                ),
+                SQLPlanMetric(
+                  name = "name_17",
+                  accumulatorId = 17,
+                  metricType = "metric_17"
+                )
+              )
+            )
+          )),
+          metrics = Seq(
+            SQLPlanMetric(
+              name = "name_6",
+              accumulatorId = 6,
+              metricType = "metric_6"
+            ),
+            SQLPlanMetric(
+              name = "name_7 d",
+              accumulatorId = 7,
+              metricType = "metric_7"
+            )
+          )
+        )
+      )),
+      edges = Seq(
+        SparkPlanGraphEdge(8, 9),
+        SparkPlanGraphEdge(10, 11)
+      )
+    )
+
+    val bytes = serializer.serialize(input)
+    val result = serializer.deserialize(bytes, classOf[SparkPlanGraphWrapper])
+    assert(result.executionId == input.executionId)
+    assert(result.nodes.size == input.nodes.size)
+
+    assert(result.edges.size == input.edges.size)
+  }
+
+}
