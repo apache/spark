39d36c49fa5052c11705cc9448aaaa1bde3d3f1d
Hyukjin Kwon <gurwls223@apache.org>
2022-12-27 16:57:17 +0900

    [SPARK-41721][CONNECT][TESTS] Enable doctests in pyspark.sql.connect.catalog
    ### What changes were proposed in this pull request?
    
    This PR proposes to enable doctests in `pyspark.sql.connect.catalog` that is virtually the same as `pyspark.sql.catalog`.
    
    ### Why are the changes needed?
    
    To make sure on the PySpark compatibility and test coverage.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, test-only.
    
    ### How was this patch tested?
    
    CI in this PR should test this out.
    
    Closes #39224 from HyukjinKwon/SPARK-41721.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -504,7 +504,7 @@ pyspark_connect = Module(
     source_file_regexes=["python/pyspark/sql/connect"],
     python_test_goals=[
         # doctests
-        # No doctests yet.
+        "pyspark.sql.connect.catalog",
         # unittests
         "pyspark.sql.tests.connect.test_connect_column_expressions",
         "pyspark.sql.tests.connect.test_connect_plan_only",
