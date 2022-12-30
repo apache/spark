480149f7a8cc304e8602a9028457df04c26281f7
Ankit Prakash Gupta <info.ankitp@gmail.com>
2022-12-30 15:57:38 +0530

    [SPARK-41232] Fixing SQL tests for array_append

--- sql/core/src/test/resources/sql-tests/inputs/array.sql
+++ sql/core/src/test/resources/sql-tests/inputs/array.sql
@@ -141,6 +141,7 @@ select array_compact(array(null));
 -- function array_append
 select array_append(array(1, 2, 3), 4);
 select array_append(array('a', 'b', 'c'), 'd');
-select array_append(array(1, 2, 3), null);
+select array_append(array(1, 2, 3, NULL), NULL);
+select array_append(array('a', 'b', 'c', NULL), NULL);
 select array_append(CAST(null AS ARRAY<String>), 'a');
 select array_append(CAST(null AS ARRAY<String>), CAST(null as String));
