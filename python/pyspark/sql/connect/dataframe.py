8b23d46dc648e45597ae385ae3c96963a210e3ed
Sandeep Singh <sandeep@techaddict.me>
2023-01-02 09:51:18 -0800

    add JIRAs

--- python/pyspark/sql/connect/dataframe.py
+++ python/pyspark/sql/connect/dataframe.py
@@ -1394,34 +1394,56 @@ def _test() -> None:
             sc, options={"spark.app.name": "sql.connect.dataframe tests"}
         )
 
+        # TODO(SPARK-41819): Implement RDD.getNumPartitions
         del pyspark.sql.connect.dataframe.DataFrame.coalesce.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.createGlobalTempView.__doc__
+        del pyspark.sql.connect.dataframe.DataFrame.repartition.__doc__
+
+        # TODO(SPARK-41820): Fix SparkConnectException: requirement failed
         del pyspark.sql.connect.dataframe.DataFrame.createOrReplaceGlobalTempView.__doc__
         del pyspark.sql.connect.dataframe.DataFrame.createOrReplaceTempView.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.createTempView.__doc__
+
+        # TODO(SPARK-41821): Fix DataFrame.describe
         del pyspark.sql.connect.dataframe.DataFrame.describe.__doc__
+
+        # TODO(SPARK-41823): ambiguous column names
         del pyspark.sql.connect.dataframe.DataFrame.drop.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.explain.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.fillna.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.groupBy.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.hint.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.intersect.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.isEmpty.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.isStreaming.__doc__
         del pyspark.sql.connect.dataframe.DataFrame.join.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.na.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.repartition.__doc__
+
+        # TODO(SPARK-41824): DataFrame.explain format is different
+        del pyspark.sql.connect.dataframe.DataFrame.explain.__doc__
+        del pyspark.sql.connect.dataframe.DataFrame.hint.__doc__
+
+        # TODO(SPARK-41825): Dataframe.show formatting int as double
+        del pyspark.sql.connect.dataframe.DataFrame.fillna.__doc__
+        del pyspark.sql.connect.dataframe.DataFrameNaFunctions.replace.__doc__
+        del pyspark.sql.connect.dataframe.DataFrameNaFunctions.fill.__doc__
         del pyspark.sql.connect.dataframe.DataFrame.replace.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.sample.__doc__
+        del pyspark.sql.connect.dataframe.DataFrame.intersect.__doc__
+
+        # TODO(SPARK-41826): Implement Dataframe.readStream
+        del pyspark.sql.connect.dataframe.DataFrame.isStreaming.__doc__
+
+        # TODO(SPARK-41827): groupBy requires all cols be Column or str
+        del pyspark.sql.connect.dataframe.DataFrame.groupBy.__doc__
+
+        # TODO(SPARK-41828): Implement creating empty DataFrame
+        del pyspark.sql.connect.dataframe.DataFrame.isEmpty.__doc__
+
+        # TODO(SPARK-41829): Add Dataframe sort ordering
         del pyspark.sql.connect.dataframe.DataFrame.sort.__doc__
         del pyspark.sql.connect.dataframe.DataFrame.sortWithinPartitions.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.sparkSession.__doc__
-        del pyspark.sql.connect.dataframe.DataFrame.stat.__doc__
+
+        # TODO(SPARK-41830): fix sample parameters
+        del pyspark.sql.connect.dataframe.DataFrame.sample.__doc__
+
+        # TODO(SPARK-41831): fix transform to accept ColumnReference
         del pyspark.sql.connect.dataframe.DataFrame.transform.__doc__
+
+        # TODO(SPARK-41832): fix unionByName
         del pyspark.sql.connect.dataframe.DataFrame.unionByName.__doc__
+
+        # TODO(SPARK-41818): Support saveAsTable
         del pyspark.sql.connect.dataframe.DataFrame.write.__doc__
-        del pyspark.sql.connect.dataframe.DataFrameNaFunctions.fill.__doc__
-        del pyspark.sql.connect.dataframe.DataFrameNaFunctions.replace.__doc__
 
         # Creates a remote Spark session.
         os.environ["SPARK_REMOTE"] = "sc://localhost"
