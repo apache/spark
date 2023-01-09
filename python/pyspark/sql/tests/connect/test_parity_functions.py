b381b1e97f78e2cb6b49af7792e7c42947cb8527
Jiaan Geng <beliefer@163.com>
2023-01-09 17:08:07 +0800

    Update code

--- python/pyspark/sql/tests/connect/test_parity_functions.py
+++ python/pyspark/sql/tests/connect/test_parity_functions.py
@@ -90,9 +90,6 @@ class FunctionsParityTests(FunctionsTestsMixin, ReusedConnectTestCase):
     def test_np_scalar_input(self):
         super().test_np_scalar_input()
 
-    def test_nth_value(self):
-        super().test_nth_value()
-
     # TODO(SPARK-41901): Parity in String representation of Column
     @unittest.skip("Fails in Spark Connect, should enable.")
     def test_overlay(self):
