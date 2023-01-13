c809aec7c3c0320c4297711ac254c754c154da3d
Serge Rielau <serge@rielau.com>
2023-01-13 09:34:57 -0800

    Fix README.md

--- core/src/test/scala/org/apache/spark/SparkThrowableSuite.scala
+++ core/src/test/scala/org/apache/spark/SparkThrowableSuite.scala
@@ -110,7 +110,7 @@ class SparkThrowableSuite extends SparkFunSuite {
     val sqlTableRows = sqlTable.split("\n").filter(_.startsWith("|")).drop(2)
     val validSqlStates = sqlTableRows.map(_.slice(1, 6)).toSet
     // Sanity check
-    assert(Set("22012", "22003", "XX000").subsetOf(validSqlStates))
+    assert(Set("22012", "22003", "42601").subsetOf(validSqlStates))
     assert(validSqlStates.forall(_.length == 5), validSqlStates)
     checkCondition(sqlStates, s => validSqlStates.contains(s))
   }
