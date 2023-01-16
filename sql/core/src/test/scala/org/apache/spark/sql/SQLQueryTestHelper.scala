e225e9180d7f91ee2add70f34a5defb096eed395
yangjie01 <yangjie01@baidu.com>
2023-01-16 21:41:11 +0800

    change regex and override toString

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestHelper.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestHelper.scala
@@ -41,13 +41,12 @@ trait SQLQueryTestHelper {
       .replaceAll(
         s"Location.*$clsName/",
         s"Location $notIncludedMsg/{warehouse_dir}/")
-      .replaceAll(s"file:.*$clsName", s"Location $notIncludedMsg/{warehouse_dir}")
+      .replaceAll(s"file:.*?$clsName", s"Location $notIncludedMsg/{warehouse_dir}")
       .replaceAll("Created By.*", s"Created By $notIncludedMsg")
       .replaceAll("Created Time.*", s"Created Time $notIncludedMsg")
       .replaceAll("Last Access.*", s"Last Access $notIncludedMsg")
       .replaceAll("Partition Statistics\t\\d+", s"Partition Statistics\t$notIncludedMsg")
       .replaceAll("\\*\\(\\d+\\) ", "*") // remove the WholeStageCodegen codegenStageIds
-      .replaceAll("@[0-9a-z]+,", ",") // remove hashCode
   }
 
 
