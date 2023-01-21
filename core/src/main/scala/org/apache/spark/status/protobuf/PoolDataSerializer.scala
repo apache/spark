8806efa9a3d2268af028a0520222f489e0bc1353
Gengliang Wang <gengliang@apache.org>
2023-01-21 13:28:57 -0800

    [SPARK-42142][UI] Handle null string values in CachedQuantile/ExecutorSummary/PoolData
    ### What changes were proposed in this pull request?
    
    Similar to https://github.com/apache/spark/pull/39666, this PR handles null string values in CachedQuantile/ExecutorSummary/PoolData
    ### Why are the changes needed?
    
    Properly handles null string values in the protobuf serializer.
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    New UTs
    
    Closes #39685 from gengliangwang/fixMoreNull.
    
    Authored-by: Gengliang Wang <gengliang@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- core/src/main/scala/org/apache/spark/status/protobuf/PoolDataSerializer.scala
+++ core/src/main/scala/org/apache/spark/status/protobuf/PoolDataSerializer.scala
@@ -20,12 +20,13 @@ package org.apache.spark.status.protobuf
 import scala.collection.JavaConverters._
 
 import org.apache.spark.status.PoolData
+import org.apache.spark.status.protobuf.Utils.{getStringField, setStringField}
 
 class PoolDataSerializer extends ProtobufSerDe[PoolData] {
 
   override def serialize(input: PoolData): Array[Byte] = {
     val builder = StoreTypes.PoolData.newBuilder()
-    builder.setName(input.name)
+    setStringField(input.name, builder.setName)
     input.stageIds.foreach(id => builder.addStageIds(id.toLong))
     builder.build().toByteArray
   }
@@ -33,7 +34,7 @@ class PoolDataSerializer extends ProtobufSerDe[PoolData] {
   override def deserialize(bytes: Array[Byte]): PoolData = {
     val poolData = StoreTypes.PoolData.parseFrom(bytes)
     new PoolData(
-      name = poolData.getName,
+      name = getStringField(poolData.hasName, poolData.getName),
       stageIds = poolData.getStageIdsList.asScala.map(_.toInt).toSet
     )
   }
