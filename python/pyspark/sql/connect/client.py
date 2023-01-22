1aefb69068881ca07c60f399175c314f574ceed1
Martin Grund <martin.grund@databricks.com>
2023-01-22 10:10:27 +0100

    removing imports

--- python/pyspark/sql/connect/client.py
+++ python/pyspark/sql/connect/client.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import contextlib
-import functools
 import logging
 import os
 import random
