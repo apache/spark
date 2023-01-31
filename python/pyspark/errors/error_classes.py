a1362b19094fc8e8da3409feb0e20576c0515c7f
itholic <haejoon.lee@databricks.com>
2023-01-31 09:06:27 +0900

    [SPARK-42192][PYTHON] Migrate the `TypeError` from `pyspark/sql/dataframe.py` into `PySparkTypeError`
    ### What changes were proposed in this pull request?
    
    This PR proposes to migrate the `TypeError` from `pyspark/sql/dataframe.py` into `PySparkTypeError`.
    
    ### Why are the changes needed?
    
    To integrate the errors generated by PySpark into single path.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Added & updated UTs.
    
    Closes #39785 from itholic/error_dataframe.
    
    Authored-by: itholic <haejoon.lee@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/errors/error_classes.py
+++ python/pyspark/errors/error_classes.py
@@ -19,14 +19,34 @@ import json
 
 ERROR_CLASSES_JSON = """
 {
+  "ARGUMENT_REQUIRED": {
+    "message": [
+      "Argument `<arg_name>` is required when <condition>."
+    ]
+  },
   "COLUMN_IN_LIST": {
     "message": [
       "<func_name> does not allow a Column in a list."
     ]
   },
+  "DISALLOWED_TYPE_FOR_CONTAINER" : {
+    "message" : [
+      "Argument `<arg_name>`(type: <arg_type>) should only contain a type in [<allowed_types>], got <return_type>"
+    ]
+  },
   "HIGHER_ORDER_FUNCTION_SHOULD_RETURN_COLUMN" : {
     "message" : [
-      "Function `<func_name>` should return Column, got <return_type>"
+      "Function `<func_name>` should return Column, got <return_type>."
+    ]
+  },
+  "NOT_AN_INTEGER" : {
+    "message" : [
+      "Argument `<arg_name>` should be a integer, got <arg_type>."
+    ]
+  },
+  "NOT_A_BOOLEAN" : {
+    "message" : [
+      "Argument `<arg_name>` should be a boolean, got <arg_type>."
     ]
   },
   "NOT_A_COLUMN" : {
@@ -39,14 +59,49 @@ ERROR_CLASSES_JSON = """
       "Argument `<arg_name>` must be a DataFrame, got <arg_type>."
     ]
   },
+  "NOT_A_DATAFRAME" : {
+    "message" : [
+      "Argument `<arg_name>` should be a DataFrame, got <arg_type>."
+    ]
+  },
+  "NOT_A_DICT" : {
+    "message" : [
+      "Argument `<arg_name>` should be a dict, got <arg_type>."
+    ]
+  },
   "NOT_A_STRING" : {
     "message" : [
       "Argument `<arg_name>` should be a str, got <arg_type>."
     ]
   },
+  "NOT_BOOL_OR_DICT_OR_FLOAT_OR_INTEGER_OR_LIST_OR_STRING_OR_TUPLE" : {
+    "message" : [
+      "Argument `<arg_name>` should be a boolean, dict, float, integer, string or tuple, got <arg_type>."
+    ]
+  },
+  "NOT_BOOL_OR_DICT_OR_FLOAT_OR_INTEGER_OR_STRING" : {
+    "message" : [
+      "Argument `<arg_name>` should be a boolean, dict, float, integer or string, got <arg_type>."
+    ]
+  },
+  "NOT_BOOL_OR_LIST" : {
+    "message" : [
+      "Argument `<arg_name>` should be a boolean or list, got <arg_type>."
+    ]
+  },
+  "NOT_BOOL_OR_STRING" : {
+    "message" : [
+      "Argument `<arg_name>` should be a boolean or string, got <arg_type>."
+    ]
+  },
   "NOT_COLUMN_OR_DATATYPE_OR_STRING" : {
     "message" : [
       "Argument `<arg_name>` should be a Column or str or DataType, but got <arg_type>."
+    ]
+  },
+  "NOT_COLUMN_OR_FLOAT_OR_INTEGER_OR_LIST_OR_STRING" : {
+    "message" : [
+      "Argument `<arg_name>` should be a column, float, integer, list or string, got <arg_type>."
     ]
   },
   "NOT_COLUMN_OR_INTEGER" : {
@@ -62,6 +117,26 @@ ERROR_CLASSES_JSON = """
   "NOT_COLUMN_OR_STRING" : {
     "message" : [
       "Argument `<arg_name>` should be a Column or str, got <arg_type>."
+    ]
+  },
+  "NOT_FLOAT_OR_INTEGER" : {
+    "message" : [
+      "Argument `<arg_name>` should be a float or integer, got <arg_type>."
+    ]
+  },
+  "NOT_FLOAT_OR_INTEGER_OR_LIST_OR_STRING" : {
+    "message" : [
+      "Argument `<arg_name>` should be a float, integer, list or string, got <arg_type>."
+    ]
+  },
+  "NOT_LIST_OR_STRING_OR_TUPLE" : {
+    "message" : [
+      "Argument `<arg_name>` should be a list, string or tuple, got <arg_type>."
+    ]
+  },
+  "NOT_LIST_OR_TUPLE" : {
+    "message" : [
+      "Argument `<arg_name>` should be a list or tuple, got <arg_type>."
     ]
   },
   "UNSUPPORTED_NUMPY_ARRAY_SCALAR" : {
