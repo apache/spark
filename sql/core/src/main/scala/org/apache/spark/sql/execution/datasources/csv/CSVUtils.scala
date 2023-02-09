a4da66da85e6da94c04c04479ef405d46b1167ba
ted-jenks <tedcj@palantir.com>
2023-02-09 08:49:05 +0000

    update CSV Utils

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/CSVUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/csv/CSVUtils.scala
@@ -48,11 +48,17 @@ object CSVUtils {
    */
   def skipUnwantedLines(lines: Dataset[String], options: CSVOptions): Dataset[String] = {
     import lines.sqlContext.implicits._
-    val filteredLines = filterCommentAndEmpty(lines, options)
-    filteredLines.rdd.zipWithIndex().toDF("value", "order")
+    val aliased = lines.toDF("value")
+    val nonEmptyLines = aliased.filter(length(trim($"value")) > 0).as[String]
+    val skippedLines = nonEmptyLines.rdd.zipWithIndex().toDF("value", "order")
       .filter($"order" >= options.skipLines)
       .drop("order")
-      .as[String]
+      .as[String].toDF("value")
+    if (options.isCommentSet) {
+      skippedLines.filter(!$"value".startsWith(options.comment.toString)).as[String]
+    } else {
+      skippedLines.as[String]
+    }
   }
 
   /**
