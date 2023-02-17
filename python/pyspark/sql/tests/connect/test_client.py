e4c65f54b3e0fc4987be732279582eda47ae792c
Niranjan Jayakar <nija@databricks.com>
2023-02-17 13:08:16 +0100

    PR feedback

--- python/pyspark/sql/tests/connect/test_client.py
+++ python/pyspark/sql/tests/connect/test_client.py
@@ -1,3 +1,20 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import unittest
 
 from pyspark.sql.connect.client import SparkConnectClient
@@ -29,10 +46,9 @@ class SparkConnectClientTestCase(unittest.TestCase):
 
 
 class MockService:
-    """
-    Simplest mock of the SparkConnectService.
-    If this needs more complex logic, it needs to be replaced with Python mocking.
-    """
+    # Simplest mock of the SparkConnectService.
+    # If this needs more complex logic, it needs to be replaced with Python mocking.
+
     req: proto.ExecutePlanRequest | None
 
     def __init__(self, session_id: str):
