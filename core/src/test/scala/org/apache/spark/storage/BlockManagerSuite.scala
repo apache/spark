2abe3d9d84e61d2bd4a291c04d230249a06b397b
Tengfei Huang <tengfei.h@gmail.com>
2023-02-19 21:41:23 +0800

    add uts

--- core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
@@ -2308,21 +2308,66 @@ class BlockManagerSuite extends SparkFunSuite with Matchers with PrivateMethodTe
     verify(master, times(2)).updateRDDBlockTaskInfo(blockId, 1)
   }
 
-  test("clean up visibility status once a rdd block is removed") {
+  test("add block rdd visibility status") {
     val store = makeBlockManager(8000, "executor1")
     val blockId = RDDBlockId(rddId = 1, splitIndex = 1)
     val data = Seq(1, 2, 3)
     store.putIterator(blockId, data.iterator, StorageLevel.MEMORY_ONLY, tellMaster = true)
-    store.blockInfoManager.tryAddVisibleBlock(blockId)
 
+    // Add rdd block visibility status.
+    store.blockInfoManager.tryAddVisibleBlock(blockId)
     assert(store.getStatus(blockId).nonEmpty && store.blockInfoManager.isRDDBlockVisible(blockId))
 
+    // Remove rdd block.
     store.removeBlock(blockId)
     assert(!store.blockInfoManager.isRDDBlockVisible(blockId))
+    assert(!store.blockInfoManager.visibleRDDBlocks.contains(blockId))
+
+    // Visibility status should not be added once rdd is removed.
+    store.blockInfoManager.tryAddVisibleBlock(blockId)
     assert(!store.blockInfoManager.visibleRDDBlocks.contains(blockId))
   }
 
   test("master & manager interaction about rdd block visibility information") {
+    val store1 = makeBlockManager(8000, "executor1")
+    val store2 = makeBlockManager(8000, "executor2")
+    val store3 = makeBlockManager(8000, "executor3")
+
+    val taskId = 0L
+    val blockId = RDDBlockId(rddId = 1, splitIndex = 1)
+    val data = Seq(1, 2, 3)
+
+    store1.getOrElseUpdateRDDBlock(
+      taskId, blockId, StorageLevel.MEMORY_ONLY, classTag[Int], () => data.iterator)
+    // Block information is reported and block is not visible.
+    assert(master.getLocations(blockId).nonEmpty)
+    assert(!master.isRDDBlockVisible(blockId))
+    assert(!store1.blockInfoManager.visibleRDDBlocks.contains(blockId))
+
+    // A copy is reported, visibility status not changed.
+    store2.putIterator(blockId, data.iterator, StorageLevel.MEMORY_ONLY)
+    assert(master.getLocations(blockId).length === 2)
+    assert(!master.isRDDBlockVisible(blockId))
+    assert(!store2.blockInfoManager.visibleRDDBlocks.contains(blockId))
+
+    // Report rdd block visibility as true, driver should ask block managers to mark the block
+    // as visible.
+    master.updateRDDBlockVisibility(taskId, visible = true)
+    eventually(timeout(5.seconds)) {
+      assert(master.isRDDBlockVisible(blockId))
+      assert(store1.blockInfoManager.isRDDBlockVisible(blockId))
+      assert(store2.blockInfoManager.isRDDBlockVisible(blockId))
+    }
+
+    // Visibility status should be updated right after block reported since it's already visible.
+    assert(!store3.blockInfoManager.isRDDBlockVisible(blockId))
+    store3.putIterator(blockId, data.iterator, StorageLevel.MEMORY_ONLY)
+    eventually(timeout(5.seconds)) {
+      assert(store3.blockInfoManager.isRDDBlockVisible(blockId))
+    }
+  }
+
+  test("rdd block's visibility status should be cached once got from driver") {
     val store = makeBlockManager(8000, "executor1")
     val taskId = 0L
     val blockId = RDDBlockId(rddId = 1, splitIndex = 1)
@@ -2335,12 +2380,11 @@ class BlockManagerSuite extends SparkFunSuite with Matchers with PrivateMethodTe
     assert(!master.isRDDBlockVisible(blockId))
     assert(!store.blockInfoManager.visibleRDDBlocks.contains(blockId))
 
-    // Report rdd block visibility as true.
-    master.updateRDDBlockVisibility(taskId, visible = true)
-    assert(master.isRDDBlockVisible(blockId))
-    eventually(timeout(5.seconds)) {
-      assert(store.blockInfoManager.isRDDBlockVisible(blockId))
-    }
+    doAnswer(_ => true).when(master).isRDDBlockVisible(mc.any())
+    // Visibility status should be cached.
+    assert(store.isRDDBlockVisible(blockId))
+    assert(store.blockInfoManager.visibleRDDBlocks.contains(blockId))
+    assert(store.blockInfoManager.isRDDBlockVisible(blockId))
   }
 
   test("getOrElseUpdateRDDBlock should make sure accumulators updated when block already" +
