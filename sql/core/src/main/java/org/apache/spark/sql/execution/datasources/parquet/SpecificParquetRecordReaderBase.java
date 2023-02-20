4560d4c4f758abb87631195f60d9c293e3d5b2a1
Cheng Pan <chengpan@apache.org>
2023-02-20 09:40:44 -0800

    [SPARK-41952][SQL] Fix Parquet zstd off-heap memory leak as a workaround for PARQUET-2160
    ### What changes were proposed in this pull request?
    
    SPARK-41952 was raised for a while, but unfortunately, the Parquet community does not publish the patched version yet, as a workaround, we can fix the issue on the Spark side first.
    
    We encountered this memory issue when migrating data from parquet/snappy to parquet/zstd, Spark executors always occupy unreasonable off-heap memory and have a high risk of being killed by NM.
    
    See more discussions at https://github.com/apache/parquet-mr/pull/982 and https://github.com/apache/iceberg/pull/5681
    
    ### Why are the changes needed?
    
    The issue is fixed in the parquet community [PARQUET-2160](https://issues.apache.org/jira/browse/PARQUET-2160), but the patched version is not available yet.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, it's bug fix.
    
    ### How was this patch tested?
    
    The existing UT should cover the correctness check, I also verified this patch by scanning a large parquet/zstd table.
    
    ```
    spark-shell --executor-cores 4 --executor-memory 6g --conf spark.executor.memoryOverhead=2g
    ```
    
    ```
    spark.sql("select sum(hash(*)) from parquet_zstd_table ").show(false)
    ```
    
    - before this patch
    
    All executors get killed by NM quickly.
    ```
    ERROR YarnScheduler: Lost executor 1 on hadoop-xxxx.****.org: Container killed by YARN for exceeding physical memory limits. 8.2 GB of 8 GB physical memory used. Consider boosting spark.executor.memoryOverhead.
    ```
    <img width="1872" alt="image" src="https://user-images.githubusercontent.com/26535726/220031678-e9060244-5586-4f0c-8fe7-55bb4e20a580.png">
    
    - after this patch
    
    Query runs well, no executor gets killed.
    
    <img width="1881" alt="image" src="https://user-images.githubusercontent.com/26535726/220031917-4fe38c07-b38f-49c6-a982-2091a6c2a8ed.png">
    
    Closes #40091 from pan3793/SPARK-41952.
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Chao Sun <sunchao@apple.com>

--- sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/SpecificParquetRecordReaderBase.java
+++ sql/core/src/main/java/org/apache/spark/sql/execution/datasources/parquet/SpecificParquetRecordReaderBase.java
@@ -96,6 +96,7 @@ public abstract class SpecificParquetRecordReaderBase<T> extends RecordReader<Vo
     ParquetReadOptions options = HadoopReadOptions
       .builder(configuration, file)
       .withRange(split.getStart(), split.getStart() + split.getLength())
+      .withCodecFactory(new ParquetCodecFactory(configuration, 0))
       .build();
     ParquetFileReader fileReader = new ParquetFileReader(
         HadoopInputFile.fromPath(file, configuration), options);
@@ -159,6 +160,7 @@ public abstract class SpecificParquetRecordReaderBase<T> extends RecordReader<Vo
     ParquetReadOptions options = HadoopReadOptions
       .builder(config, file)
       .withRange(0, length)
+      .withCodecFactory(new ParquetCodecFactory(config, 0))
       .build();
     ParquetFileReader fileReader = ParquetFileReader.open(
       HadoopInputFile.fromPath(file, config), options);
