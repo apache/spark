6080da56f15c1ce435b6dfb44c967b65843692ad
Ruifeng Zheng <ruifengz@apache.org>
2023-02-20 15:42:34 +0800

    init module
    init
    
    init
    
    init
    
    add test
    
    add test
    
    nit
    
    try to disable mima for now
    
    fix tests
    
    add to CI
    
    update CI
    
    update CI
    
    update CI
    
    add some mima exclude
    
    make mllib-common depends on catalyst instead of sql
    
    fix dep
    
    fix dep
    
    fix dep
    
    try mima
    
    fix mima
    
    fix style
    
    fix mima
    
    fix
    
    address comments
    
    address comments
    
    address comments
    
    address comments
    
    add tests
    
    add tests
    
    init
    
    init
    
    init
    
    init
    
    init

---
+++ connector/connect/client/jvm/src/main/scala/org/apache/spark/ml/feature/LabeledPoint.scala
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.feature
+
+import org.apache.spark.annotation.Since
+import org.apache.spark.ml.linalg.Vector
+
+/**
+ * Class that represents the features and label of a data point.
+ *
+ * @param label
+ *   Label for this data point.
+ * @param features
+ *   List of features for this data point.
+ */
+@Since("3.5.0")
+case class LabeledPoint(label: Double, features: Vector) {
+
+  def getLabel: Double = label
+
+  def getFeatures: Vector = features
+
+  override def toString: String = {
+    s"($label,$features)"
+  }
+}
---
+++ mllib/core/src/test/scala/org/apache/spark/ml/linalg/MatrixUDTSuite.scala
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.ml.linalg
+
+import org.apache.spark.SparkFunSuite
+import org.apache.spark.sql.types._
+
+class MatrixUDTSuite extends SparkFunSuite {
+
+  test("preloaded MatrixUDT") {
+    val dm1 = new DenseMatrix(2, 2, Array(0.9, 1.2, 2.3, 9.8))
+    val dm2 = new DenseMatrix(3, 2, Array(0.0, 1.21, 2.3, 9.8, 9.0, 0.0))
+    val dm3 = new DenseMatrix(0, 0, Array())
+    val sm1 = dm1.toSparse
+    val sm2 = dm2.toSparse
+    val sm3 = dm3.toSparse
+
+    for (m <- Seq(dm1, dm2, dm3, sm1, sm2, sm3)) {
+      val udt = UDTRegistration.getUDTFor(m.getClass.getName).get.getConstructor().newInstance()
+        .asInstanceOf[MatrixUDT]
+      assert(m === udt.deserialize(udt.serialize(m)))
+      assert(udt.typeName == "matrix")
+      assert(udt.simpleString == "matrix")
+    }
+  }
+}
