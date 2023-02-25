b99435328f76febd36dadd93649d1b3c10fe03da
Takuya UESHIN <ueshin@databricks.com>
2023-02-25 10:38:53 +0900

    [SPARK-42568][CONNECT] Fix SparkConnectStreamHandler to handle configs properly while planning
    ### What changes were proposed in this pull request?
    
    Fixes `SparkConnectStreamHandler` to handle configs properly while planning.
    
    The whole process should be done in `session.withActive` to take the proper `SQLConf` into account.
    
    ### Why are the changes needed?
    
    Some components for planning need to check configs in `SQLConf.get` while building the plan, but currently it's unavailable.
    
    For example, `spark.sql.legacy.allowNegativeScaleOfDecimal` needs to check when construct `DecimalType` but it's not set while planning, thus it causes an error when trying to cast to `DecimalType(1, -1)` with the config set to `"true"`:
    
    ```
    [INTERNAL_ERROR] Negative scale is not allowed: -1. Set the config "spark.sql.legacy.allowNegativeScaleOfDecimal" to "true" to allow it.
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    The configs will take effect while planning.
    
    ### How was this patch tested?
    
    Enabled a related test.
    
    Closes #40165 from ueshin/issues/SPARK-42568/withActive.
    
    Authored-by: Takuya UESHIN <ueshin@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/connect/types.py
+++ python/pyspark/sql/connect/types.py
@@ -87,7 +87,8 @@ def pyspark_types_to_proto_types(data_type: DataType) -> pb2.DataType:
     elif isinstance(data_type, DoubleType):
         ret.double.CopyFrom(pb2.DataType.Double())
     elif isinstance(data_type, DecimalType):
-        ret.decimal.CopyFrom(pb2.DataType.Decimal())
+        ret.decimal.scale = data_type.scale
+        ret.decimal.precision = data_type.precision
     elif isinstance(data_type, DateType):
         ret.date.CopyFrom(pb2.DataType.Date())
     elif isinstance(data_type, TimestampType):
