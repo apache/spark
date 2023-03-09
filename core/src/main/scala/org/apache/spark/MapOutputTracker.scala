ed4bd8bec82d2129ae146349073fc088106c2b95
roryqi <roryqi@tencent.com>
2023-03-09 10:30:24 +0800

    address comments

--- core/src/main/scala/org/apache/spark/MapOutputTracker.scala
+++ core/src/main/scala/org/apache/spark/MapOutputTracker.scala
@@ -1113,7 +1113,7 @@ private[spark] class MapOutputTrackerMaster(
       endMapIndex: Int): Seq[String] =
   {
     val shuffleStatus = shuffleStatuses.get(dep.shuffleId).orNull
-    if (shuffleStatus != null && shuffleLocalityEnabled) {
+    if (shuffleLocalityEnabled && shuffleStatus != null) {
       shuffleStatus.withMapStatuses { statuses =>
         if (startMapIndex < endMapIndex &&
           (startMapIndex >= 0 && endMapIndex <= statuses.length)) {
