6bfcefc58e92dd07b94018d250f863a2d8a5d40d
Jiaan Geng <beliefer@163.com>
2023-03-12 18:58:42 +0800

    Update code

--- connector/connect/server/src/main/scala/org/apache/spark/sql/connect/SimpleSparkConnectService.scala
+++ connector/connect/server/src/main/scala/org/apache/spark/sql/connect/SimpleSparkConnectService.scala
@@ -17,6 +17,8 @@
 
 package org.apache.spark.sql.connect
 
+import java.sql.DriverManager
+import java.util.Properties
 import java.util.concurrent.TimeUnit
 
 import scala.io.StdIn
@@ -25,8 +27,7 @@ import scala.sys.exit
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.connect.service.SparkConnectService
-import org.apache.spark.sql.execution.datasources.jdbc.{JDBCOptions, JdbcUtils}
-import org.apache.spark.sql.jdbc.JdbcDialects
+import org.apache.spark.util.Utils
 
 /**
  * A simple main class method to start the spark connect server as a service for client tests
@@ -40,15 +41,17 @@ private[sql] object SimpleSparkConnectService {
   private val stopCommand = "q"
 
   private def createJDBCTestSchema(): Unit = {
-    val url = "jdbc:h2:mem:testdb2"
-    val dialect = JdbcDialects.get(url)
-    val options = new JDBCOptions(Map("url" -> url, "driver" -> "org.h2.Driver"))
-    val db = "TEST"
-    JdbcUtils.withConnection(options) { conn =>
-      JdbcUtils.classifyException(s"Failed create name space: $db", dialect) {
-        JdbcUtils.createSchema(conn, options, db, "")
-      }
-    }
+    Utils.classForName("org.h2.Driver")
+    // Extra properties that will be specified for our database. We need these to test
+    // usage of parameters from OPTIONS clause in queries.
+    val properties = new Properties()
+    properties.setProperty("user", "testUser")
+    properties.setProperty("password", "testPass")
+
+    val conn = DriverManager.getConnection("jdbc:h2:mem:testdb0", properties)
+    conn.prepareStatement("create schema test").executeUpdate()
+    conn.commit()
+    conn.close()
   }
 
   def main(args: Array[String]): Unit = {
