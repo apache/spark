f6b5fd933ed41b09c733ae339118133508dd0870
Zhen Li <zhenlineo@users.noreply.github.com>
2023-03-14 18:31:27 +0900

    [SPARK-42733][CONNECT][FOLLOWUP] Write without path or table
    Fixes `DataFrameWriter.save` to work without path or table parameter.
    Added support of jdbc method in the writer as it is one of the impl that does not contains a path or table.
    
    DataFrameWriter.save should work without path parameter because some data sources, such as jdbc, noop, works without those parameters.
    The follow up fix for scala client of https://github.com/apache/spark/pull/40356
    
    No
    
    Unit and E2E test
    
    Closes #40358 from zhenlineo/write-without-path-table.
    
    Authored-by: Zhen Li <zhenlineo@users.noreply.github.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>
    (cherry picked from commit 93334e295483a0ba66e22d8398512ad970a3ea80)
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/ClientE2ETestSuite.scala
+++ connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/ClientE2ETestSuite.scala
@@ -22,6 +22,7 @@ import java.nio.file.Files
 import scala.collection.JavaConverters._
 
 import io.grpc.StatusRuntimeException
+import java.util.Properties
 import org.apache.commons.io.FileUtils
 import org.apache.commons.io.output.TeeOutputStream
 import org.scalactic.TolerantNumerics
@@ -172,6 +173,26 @@ class ClientE2ETestSuite extends RemoteSparkSession with SQLHelper {
       assert(result(2).getLong(0) == 1)
       assert(result(3).getLong(0) == 1)
       assert(result(4).getLong(0) == 2)
+    }
+  }
+
+  test("write without table or path") {
+    // Should receive no error to write noop
+    spark.range(10).write.format("noop").mode("append").save()
+  }
+
+  test("write jdbc") {
+    val url = "jdbc:derby:memory:1234"
+    val table = "t1"
+    try {
+      spark.range(10).write.jdbc(url = s"$url;create=true", table, new Properties())
+      val result = spark.read.jdbc(url = url, table, new Properties()).collect()
+      assert(result.length == 10)
+    } finally {
+      // clean up
+      assertThrows[StatusRuntimeException] {
+        spark.read.jdbc(url = s"$url;drop=true", table, new Properties()).collect()
+      }
     }
   }
 
