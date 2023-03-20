a6bb3d7ed7ce7973171796aa03cf7f8c86f8c126
yangjie01 <yangjie01@baidu.com>
2023-03-20 16:50:07 +0800

    [SPARK-42827][CONNECT] Support `functions#array_prepend` for Scala connect client
    ### What changes were proposed in this pull request?
    This pr aims support  `functions#array_prepend` for Scala connect client.
    
    ### Why are the changes needed?
    Add Spark connect jvm client api coverage.
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Add new test
    
    Closes #40481 from LuciferYang/SPARK-42827.
    
    Lead-authored-by: yangjie01 <yangjie01@baidu.com>
    Co-authored-by: YangJie <yangjie01@baidu.com>
    Signed-off-by: Ruifeng Zheng <ruifengz@apache.org>

--- connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/connect/client/CheckConnectJvmClientCompatibility.scala
+++ connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/connect/client/CheckConnectJvmClientCompatibility.scala
@@ -177,7 +177,6 @@ object CheckConnectJvmClientCompatibility {
       ProblemFilters.exclude[Problem]("org.apache.spark.sql.functions.broadcast"),
       ProblemFilters.exclude[Problem]("org.apache.spark.sql.functions.typedlit"),
       ProblemFilters.exclude[Problem]("org.apache.spark.sql.functions.typedLit"),
-      ProblemFilters.exclude[Problem]("org.apache.spark.sql.functions.array_prepend"),
 
       // RelationalGroupedDataset
       ProblemFilters.exclude[Problem]("org.apache.spark.sql.RelationalGroupedDataset.apply"),
