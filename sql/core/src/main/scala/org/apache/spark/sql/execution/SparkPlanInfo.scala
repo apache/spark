ce2bbffae8ffbbb6c37d7acf913b3515e865a6a2
yliou <yliou@berkeley.edu>
2023-03-19 21:14:00 -0700

    SPARK-42890 add repeat identifier on SQL UI

--- sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlanInfo.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/SparkPlanInfo.scala
@@ -17,12 +17,18 @@
 
 package org.apache.spark.sql.execution
 
+import java.util.{IdentityHashMap => JavaIdentityMap}
+
 import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.execution.adaptive.{AdaptiveSparkPlanExec, QueryStageExec}
-import org.apache.spark.sql.execution.columnar.InMemoryTableScanExec
+import org.apache.spark.sql.execution.columnar.{CachedRDDBuilder, InMemoryTableScanExec}
 import org.apache.spark.sql.execution.exchange.ReusedExchangeExec
 import org.apache.spark.sql.execution.metric.SQLMetricInfo
 import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.util.IdGenerator
 
 /**
  * :: DeveloperApi ::
@@ -51,15 +57,27 @@ class SparkPlanInfo(
 
 private[execution] object SparkPlanInfo {
 
-  def fromSparkPlan(plan: SparkPlan): SparkPlanInfo = {
-    val children = plan match {
-      case ReusedExchangeExec(_, child) => child :: Nil
-      case ReusedSubqueryExec(child) => child :: Nil
-      case a: AdaptiveSparkPlanExec => a.executedPlan :: Nil
-      case stage: QueryStageExec => stage.plan :: Nil
-      case inMemTab: InMemoryTableScanExec => inMemTab.relation.cachedPlan :: Nil
-      case _ => plan.children ++ plan.subqueries
+  def fromSparkPlan(plan: SparkPlan, inMemoryRelationEncountered:
+  JavaIdentityMap[CachedRDDBuilder, Integer] = new JavaIdentityMap[CachedRDDBuilder, Integer](),
+  idGenerator: IdGenerator = new IdGenerator()): SparkPlanInfo = {
+    val (children, decoratedPlanForStringGeneration) = plan match {
+      case ReusedExchangeExec(_, child) => (child :: Nil) -> plan
+      case ReusedSubqueryExec(child) => (child :: Nil) -> plan
+      case a: AdaptiveSparkPlanExec => (a.executedPlan :: Nil) -> plan
+      case stage: QueryStageExec => (stage.plan :: Nil) -> plan
+      case inMemTab: InMemoryTableScanExec =>
+        if (inMemoryRelationEncountered.containsKey(inMemTab.relation.cacheBuilder)) {
+          // get the id from the map
+          val id = inMemoryRelationEncountered.get(inMemTab.relation.cacheBuilder)
+          Seq.empty -> WrapperInMemoryTableScanExec(id, inMemTab)
+        } else {
+          val id = idGenerator.next
+          inMemoryRelationEncountered.put(inMemTab.relation.cacheBuilder, id)
+          (inMemTab.relation.cachedPlan :: Nil) -> WrapperInMemoryTableScanExec(id, inMemTab)
+        }
+      case _ => (plan.children ++ plan.subqueries) -> plan
     }
+
     val metrics = plan.metrics.toSeq.map { case (key, metric) =>
       new SQLMetricInfo(metric.name.getOrElse(key), metric.id, metric.metricType)
     }
@@ -69,11 +87,23 @@ private[execution] object SparkPlanInfo {
       case fileScan: FileSourceScanExec => fileScan.metadata
       case _ => Map[String, String]()
     }
-    new SparkPlanInfo(
-      plan.nodeName,
-      plan.simpleString(SQLConf.get.maxToStringFields),
-      children.map(fromSparkPlan),
-      metadata,
-      metrics)
+
+    val simpleString = decoratedPlanForStringGeneration.simpleString(SQLConf.get.maxToStringFields)
+
+    new SparkPlanInfo(decoratedPlanForStringGeneration.nodeName,
+      simpleString, children.map(child => fromSparkPlan(child,
+        inMemoryRelationEncountered, idGenerator)), metadata, metrics)
+  }
+
+  private case class WrapperInMemoryTableScanExec(repeatId: Int, imr: InMemoryTableScanExec)
+    extends LeafExecNode {
+    private val prefix = s"InMemoryTableScan(Repeat Identifier: $repeatId) "
+
+    override protected def doExecute(): RDD[InternalRow] = throw new UnsupportedOperationException
+
+    override def output: Seq[Attribute] = imr.output
+    override def simpleString(maxFields: Int): String = s"$prefix ${imr.simpleString(maxFields)}"
+
+    override def nodeName: String = s"${imr.nodeName}(Repeat Identifier: $repeatId)"
   }
 }
--- sql/core/src/test/scala/org/apache/spark/sql/execution/ui/SparkPlanInfoSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/ui/SparkPlanInfoSuite.scala
@@ -17,6 +17,8 @@
 
 package org.apache.spark.sql.execution.ui
 
+import scala.collection.mutable
+
 import org.apache.spark.sql.execution.SparkPlanInfo
 import org.apache.spark.sql.test.SharedSparkSession
 
@@ -24,12 +26,31 @@ class SparkPlanInfoSuite extends SharedSparkSession {
 
   import testImplicits._
 
-  def validateSparkPlanInfo(sparkPlanInfo: SparkPlanInfo): Unit = {
+  val imrExtractionPattern =
+    """InMemoryTableScan\(Repeat Identifier: [0-9]+\)""".r
+
+  def validateSparkPlanInfo(sparkPlanInfo: SparkPlanInfo,
+  repeatMapWithChildrenCount: mutable.HashMap[String, Int] = new mutable.HashMap[String, Int]()):
+  Unit = {
     sparkPlanInfo.nodeName match {
+      case s if sparkPlanInfo.simpleString.startsWith("InMemoryTableScan(Repeat Identifier") =>
+        val currentChildrenCount = repeatMapWithChildrenCount.getOrElse(s, 0)
+        repeatMapWithChildrenCount.put(sparkPlanInfo.simpleString,
+          currentChildrenCount + sparkPlanInfo.children.length)
       case "InMemoryTableScan" => assert(sparkPlanInfo.children.length == 1)
-      case _ => sparkPlanInfo.children.foreach(validateSparkPlanInfo)
+      case _ => sparkPlanInfo.children.foreach(validateSparkPlanInfo(_, repeatMapWithChildrenCount))
+    }
+    repeatMapWithChildrenCount.foreach {
+      case (_, childrenCount) => assert(childrenCount == 1 || childrenCount == 0)
     }
   }
+
+  def extractIMRWithRepeatIdentifier(sparkPlanInfo: SparkPlanInfo): Seq[String] =
+    sparkPlanInfo.nodeName match {
+      case s if imrExtractionPattern.unapplySeq(s).isDefined =>
+        s :: Nil
+      case _ => sparkPlanInfo.children.flatMap(extractIMRWithRepeatIdentifier(_))
+    }
 
   test("SparkPlanInfo creation from SparkPlan with InMemoryTableScan node") {
     val dfWithCache = Seq(
@@ -40,5 +61,51 @@ class SparkPlanInfoSuite extends SharedSparkSession {
     val planInfoResult = SparkPlanInfo.fromSparkPlan(dfWithCache.queryExecution.executedPlan)
 
     validateSparkPlanInfo(planInfoResult)
+  }
+
+  test("SparkPlanInfo containing wrapper plan for InMemoryRelation") {
+    val dfWithCache = Seq(
+      (1, 1),
+      (2, 2)
+    ).toDF().filter("_1 > 1").cache()
+
+    val planInfoResult = SparkPlanInfo.fromSparkPlan(dfWithCache.queryExecution.executedPlan)
+    validateSparkPlanInfo(planInfoResult)
+  }
+
+  test("Repeat IMR is skipped") {
+    val dfWithCache = Seq(
+      (1, 1),
+      (2, 2)
+    ).toDF().filter("_1 > 1").cache()
+    val joinedDf = dfWithCache.join(dfWithCache)
+
+    val planInfoResult = SparkPlanInfo.fromSparkPlan(joinedDf.queryExecution.executedPlan)
+    validateSparkPlanInfo(planInfoResult)
+    // ensure that same IMRs are not generating different Ids
+    // id should be incremented by 1 only
+    val imrs = extractIMRWithRepeatIdentifier(planInfoResult)
+    assert(imrs.toSet.size == 1)
+  }
+
+  test("distinct IMR are not skipped ") {
+    val dfWithCache1 = Seq(
+      (1, 1),
+      (2, 2)
+    ).toDF("a", "b").filter("a > 1").cache()
+
+    val dfWithCache2 = Seq(
+      (1, 1),
+      (2, 2),
+      (3, 2)
+    ).toDF("x", "y").filter("y > 1").cache()
+
+    val joinedDf = dfWithCache1.join(dfWithCache2)
+    val planInfoResult = SparkPlanInfo.fromSparkPlan(joinedDf.queryExecution.executedPlan)
+    validateSparkPlanInfo(planInfoResult)
+    // ensure that same IMRs are  generating different Ids
+    // id should be incremented by 2 only
+    val imrs = extractIMRWithRepeatIdentifier(planInfoResult)
+    assert(imrs.toSet.size == 2)
   }
 }
