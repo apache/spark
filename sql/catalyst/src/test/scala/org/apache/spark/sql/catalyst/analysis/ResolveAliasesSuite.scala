c1edd1aeab538d9d763312faf9be813fd5ec89dc
Max Gekk <max.gekk@gmail.com>
2023-03-27 22:49:02 +0300

    Fix ResolveAliasesSuite

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/ResolveAliasesSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/ResolveAliasesSuite.scala
@@ -107,20 +107,20 @@ class ResolveAliasesSuite extends AnalysisTest {
         "interval 3 year 1 month" -> "INTERVAL3YEAR1MONTH",
         "x'00'" -> "x'00'",
         // Preserve case
-        "CAST(1 as tinyint)" -> "CAST(1AStinyint)",
+        "CAST(1 as tinyint)" -> "CAST(1ASTINYINT)",
         // Brackets
         "getbit(11L, 2 + 1)" -> "getbit(11L,2+1)",
         "string(int(shiftleft(int(-1), 31))+1)" -> "string(int(shiftleft(int(-1),31))+1)",
         "map(1, 'a') [ 5 ]" -> "map(1,'a')[5]",
         // Preserve type
-        "CAST('123.a' AS long)" -> "CAST('123.a'ASlong)",
+        "CAST('123.a' AS long)" -> "CAST('123.a'ASLONG)",
         // Spaces
         "'1' = 1" -> "'1'=1",
         "upper('a') = upper('A')" -> "upper('a')=upper('A')",
         "FLOOR(5, 0)" -> "FLOOR(5,0)",
         "-1" -> "-1",
         "1 in (1.0)" -> "1IN(1.0)",
-        "CAST(null AS ARRAY<String>)" -> "CAST(NULLASARRAY<String>)",
+        "CAST(null AS ARRAY<String>)" -> "CAST(NULLASARRAY<STRING>)",
         """(
           |  WITH t AS (SELECT 1)
           |  SELECT * FROM t
