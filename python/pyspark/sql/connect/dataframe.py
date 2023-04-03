c7b54670c38b7261cfd8e5fbfbb3d65df9b83755
itholic <haejoon.lee@databricks.com>
2023-04-03 14:15:54 +0900

    Fix test

--- python/pyspark/sql/connect/dataframe.py
+++ python/pyspark/sql/connect/dataframe.py
@@ -563,9 +563,7 @@ class DataFrame:
             or is_withReplacement_omitted_kwargs
             or is_withReplacement_omitted_args
         ):
-            argtypes = [
-                str(type(arg)) for arg in [withReplacement, fraction, seed] if arg is not None
-            ]
+            argtypes = [type(arg).__name__ for arg in [withReplacement, fraction, seed]]
             raise PySparkTypeError(
                 error_class="NOT_BOOL_OR_FLOAT_OR_INT",
                 message_parameters={
