27d6238567d1b2339e39ca1d4d870f3bada3ebfc
Takuya UESHIN <ueshin@databricks.com>
2023-04-03 11:35:45 -0700

    Fix DataFrameTests.test_cache_dataframe.

--- python/pyspark/sql/tests/test_dataframe.py
+++ python/pyspark/sql/tests/test_dataframe.py
@@ -1036,7 +1036,7 @@ class DataFrameTestsMixin:
             self.assertEqual(df.storageLevel, StorageLevel.NONE)
 
             df.cache()
-            self.assertEqual(df.storageLevel, StorageLevel.MEMORY_AND_DISK)
+            self.assertEqual(df.storageLevel, StorageLevel.MEMORY_AND_DISK_DESER)
 
             df.unpersist()
             self.assertEqual(df.storageLevel, StorageLevel.NONE)
