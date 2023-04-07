b8bb1a1a76ed9b11930c1315bd9fccfada6701fc
Rui Wang <rui.wang@databricks.com>
2023-04-06 19:47:14 -0700

    [SPARK-43058] Move Numeric to PhysicalDataType.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/types/DecimalType.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/types/DecimalType.scala
@@ -61,6 +61,7 @@ case class DecimalType(precision: Int, scale: Int) extends FractionalType {
 
   private[sql] type InternalType = Decimal
   @transient private[sql] lazy val tag = typeTag[InternalType]
+
   private[sql] val asIntegral = Decimal.DecimalAsIfIntegral
 
   override def typeName: String = s"decimal($precision,$scale)"
