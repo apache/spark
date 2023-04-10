38b6e6f1a347cb9f235ba7620dece99edc56df8e
tirumaleshn2458 <tirumaleshn2000@gmail.com>
2023-04-10 00:41:46 -0500

    updated the paths in test cases

--- python/pyspark/pandas/tests/test_transform.py
+++ python/pyspark/pandas/tests/test_transform.py
@@ -10,7 +10,9 @@ def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
     return [ filename for filename in filenames if filename.endswith( suffix ) ]
 
-files_list=find_csv_filenames('/Users/tirumaleshn2000/Downloads/seaborn-data-master/')
+
+path=''
+files_list=find_csv_filenames(path)
 
 
 def encoding(test_data):
@@ -28,17 +30,17 @@ def test_case(file):
     print('------------------------------------------------------------------------------------------------')
     print('File name:',file)
     print('-------------------------------------')
-    test_data=pd.read_csv('/Users/tirumaleshn2000/Downloads/seaborn-data-master/'+file)
+    test_data=pd.read_csv(path+file)
     test1=encoding(test_data)
-    test1.to_csv('/Users/tirumaleshn2000/IllinoisTech/Big data/Project/test_case_files/'+'encoding'+'_'+file)
+    test1.to_csv('encoding'+'_'+file)
     del test1
     print('test 1 success')
     test2=null_replace(test_data)
-    test2.to_csv('/Users/tirumaleshn2000/IllinoisTech/Big data/Project/test_case_files/'+'null_replace'+'_'+file)
+    test2.to_csv('null_replace'+'_'+file)
     del test2
     print('test 2 success')
     test3=mani(test_data)
-    test3.to_csv('/Users/tirumaleshn2000/IllinoisTech/Big data/Project/test_case_files/'+'mani_comb'+'_'+file)
+    test3.to_csv('mani_comb'+'_'+file)
     del test3
     print('test 3 success')
 
