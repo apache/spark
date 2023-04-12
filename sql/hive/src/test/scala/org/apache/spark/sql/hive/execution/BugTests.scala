31c72974ffaa1351547c43b450fed8ee2757c47e
ashahid <ashahid@cloudera.com>
2023-04-12 16:37:16 -0700

    SPARK-43112. added bugtest

---
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/BugTests.scala
@@ -0,0 +1,173 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql.hive.execution
+
+import java.io._
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hive.conf.HiveConf.ConfVars
+import org.apache.hadoop.hive.metastore.MetaStoreEventListener
+import org.apache.hadoop.hive.metastore.events.{AddPartitionEvent, CreateTableEvent, DropTableEvent}
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{QueryTest, SaveMode, SparkSession}
+import org.apache.spark.sql.hive.test.TestHiveContext
+import org.apache.spark.sql.test.SQLTestUtils
+import org.apache.spark.util.Utils
+
+
+case class BugTestData(key: Int, value: String)
+
+case class BugTestDataBothInt(key: Int, value: Int)
+
+class BugTests extends QueryTest with SQLTestUtils {
+
+  protected var spark: SparkSession = _
+  private var scratchDir: String = _
+  val resultFile = "result"
+  private var path: File = null
+
+  import testImplicits._
+
+  lazy private val testDataFrame = spark.sparkContext.parallelize(
+    (1 to 100).map(i => BugTestData(i, i.toString))).toDF()
+
+  lazy private val testDataFrameBothInt = spark.sparkContext.parallelize(
+    (1 to 100).map(i => BugTestDataBothInt(1, i))).toDF()
+
+  override protected def beforeAll(): Unit = {
+    // stop any existing context
+    SparkContext.getActive.foreach(_.stop())
+
+    val sparkContext = new SparkContext(
+      new SparkConf()
+        .set("spark.master", "local[*]")
+        .set("hive.exec.dynamic.partition.mode", "nonstrict")
+        .set("hive.exec.dynamic.partition", "true")
+        .set("spark.app.name", "test")
+        .set("hive.metastore.dml.events", "true")
+        .set(InsertEventListener.keyForResultFileName, resultFile)
+        .set("hive.metastore.event.listeners", classOf[InsertEventListener].getName)
+        .set("spark.ui.enabled", "false"))
+
+    val hiveContext = new TestHiveContext(sparkContext)
+    spark = hiveContext.sparkSession
+    // scalastyle:off hadoopconfiguration
+    scratchDir = spark.sparkContext.hadoopConfiguration.get(
+      ConfVars.SCRATCHDIR.varname).substring("file:".length)
+    // scalastyle:on hadoopconfiguration
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    spark.stop()
+    super.afterAll()
+  }
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    path = Utils.createTempDir()
+    path.delete()
+  }
+
+  test("SPARK-43112 insert using dataframe api on partitioned table") {
+    withTable("createAndInsertTest") {
+      sql(s"CREATE TABLE createAndInsertTest (key int, value string) using" +
+        s" hive OPTIONS(fileFormat='parquet') partitioned by (key)")
+      val tableSchema = spark.table("createAndInsertTest").schema
+      // Add some data.
+      val dataFrameSchema = testDataFrame.schema
+    //  assert(tableSchema == dataFrameSchema)
+      testDataFrame.write.mode(SaveMode.Append).
+        insertInto("createAndInsertTest")
+    }
+  }
+
+  test("SPARK-43112 wrong partitions being created") {
+    withTable("createAndInsertTest") {
+      sql(s"CREATE TABLE createAndInsertTest (key int, value int) using" +
+        s" hive OPTIONS(fileFormat='parquet') partitioned by (key)")
+      val tableSchema = spark.table("createAndInsertTest").schema
+      // use dataframe with key as 1 and values ranging from 1 to 100
+      testDataFrameBothInt.write.mode(SaveMode.Append).insertInto("createAndInsertTest")
+      // count the number of partitions created.
+      // it should be 1 and not 100
+      assertFromResultsFile(1)
+
+    }
+  }
+
+  test("SPARK-43112 check schema fields order correctness") {
+    withTable("createAndInsertTest") {
+      sql(s"CREATE TABLE createAndInsertTest (key int, value string) using" +
+        s" hive OPTIONS(fileFormat='parquet') partitioned by (key)")
+      val tableSchema = spark.table("createAndInsertTest").schema
+      assert(tableSchema.fieldNames(0) == "key")
+      assert(tableSchema.fieldNames(1) == "value")
+    }
+  }
+
+  private def assertFromResultsFile(expectedPartitionCount: Int): Unit = {
+    val reader = new BufferedReader(new InputStreamReader(new FileInputStream(
+      new File(scratchDir, resultFile))))
+    val actualCount = Integer.parseInt(reader.readLine().trim())
+    assert(actualCount === expectedPartitionCount)
+  }
+}
+
+class InsertEventListener(config: Configuration) extends MetaStoreEventListener(config) {
+
+  lazy val resultFile = {
+    val scratchDir = new File(config.get(ConfVars.SCRATCHDIR.varname).substring("file:".length))
+    scratchDir.mkdir()
+    val resultFile = config.get(InsertEventListener.keyForResultFileName)
+    new java.io.File(scratchDir, resultFile)
+  }
+
+  override def onCreateTable(tableEvent: CreateTableEvent): Unit = {
+    val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(resultFile)))
+    writer.write("")
+    writer.close()
+  }
+
+
+  override def onAddPartition(partitionEvent: AddPartitionEvent): Unit = {
+    val reader = new BufferedReader(new InputStreamReader(new FileInputStream(resultFile)))
+    val previousPartsStr = reader.readLine()
+    val previousParts = if ((previousPartsStr eq null) || previousPartsStr.isEmpty) {
+      0
+    } else {
+      previousPartsStr.toInt
+    }
+    import scala.collection.JavaConverters._
+    val newParts = partitionEvent.getPartitionIterator.asScala.length
+    val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(resultFile)))
+
+    writer.write(String.valueOf(newParts + previousParts))
+    writer.close()
+  }
+
+  override def onDropTable(tableEvent: DropTableEvent): Unit = {
+    val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(resultFile)))
+    writer.write("")
+    writer.close()
+  }
+}
+
+object InsertEventListener {
+  val keyForResultFileName = "hive.metastore.dml.events.resultfile"
+}
