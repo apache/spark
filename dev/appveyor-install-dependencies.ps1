c012ad9cdc00bafc4a4755a501d46db33be124e7
yangjie01 <yangjie01@baidu.com>
2023-04-12 15:46:57 +0800

    force change

--- dev/appveyor-install-dependencies.ps1
+++ dev/appveyor-install-dependencies.ps1
@@ -81,7 +81,7 @@ if (!(Test-Path $tools)) {
 # ========================== Maven
 # Push-Location $tools
 #
-# $mavenVer = "3.8.7"
+# $mavenVer = "3.9.1"
 # Start-FileDownload "https://archive.apache.org/dist/maven/maven-3/$mavenVer/binaries/apache-maven-$mavenVer-bin.zip" "maven.zip"
 #
 # # extract
@@ -90,7 +90,7 @@ if (!(Test-Path $tools)) {
 # # add maven to environment variables
 # $env:PATH = "$tools\apache-maven-$mavenVer\bin;" + $env:PATH
 # $env:M2_HOME = "$tools\apache-maven-$mavenVer"
-# $env:MAVEN_OPTS = "-Xmx2g -XX:ReservedCodeCacheSize=1g"
+# $env:MAVEN_OPTS = "-Xmx2g -XX:ReservedCodeCacheSize=1g -Dmaven.resolver.transport=wagon"
 #
 # Pop-Location
 
