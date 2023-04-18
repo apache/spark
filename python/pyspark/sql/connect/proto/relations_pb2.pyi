cbe94a172ca2e361fba38318298cb349389eb8a2
pengzhon-db <peng.zhong@databricks.com>
2023-04-18 09:21:41 +0900

    [SPARK-43084][SS] Add applyInPandasWithState support for spark connect
    ### What changes were proposed in this pull request?
    
    This change adds applyInPandasWithState support for Spark connect.
    Example (try with local mode `./bin/pyspark --remote "local[*]"`):
    
    ```
    >>> from pyspark.sql.streaming.state import GroupStateTimeout, GroupState
    >>> from pyspark.sql.types import (
    ...     LongType,
    ...     StringType,
    ...     StructType,
    ...     StructField,
    ...     Row,
    ... )
    >>> import pandas as pd
    >>> output_type = StructType(
    ...     [StructField("key", StringType()), StructField("countAsString", StringType())]
    ... )
    >>> state_type = StructType([StructField("c", LongType())])
    >>> def func(key, pdf_iter, state):
    ...     total_len = 0
    ...     for pdf in pdf_iter:
    ...         total_len += len(pdf)
    ...     state.update((total_len,))
    ...     yield pd.DataFrame({"key": [key[0]], "countAsString": [str(total_len)]})
    ...
    >>>
    >>> input_path = "/Users/peng.zhong/tmp/applyInPandasWithState"
    >>> df = spark.readStream.format("text").load(input_path)
    >>> q = (
    ...       df.groupBy(df["value"])
    ...       .applyInPandasWithState(
    ...           func, output_type, state_type, "Update", GroupStateTimeout.NoTimeout
    ...       )
    ...       .writeStream.queryName("this_query")
    ...       .format("memory")
    ...       .outputMode("update")
    ...       .start()
    ...   )
    >>>
    >>> q.status
    {'message': 'Processing new data', 'isDataAvailable': True, 'isTriggerActive': True}
    >>>
    >>> spark.sql("select * from this_query").show()
    +-----+-------------+
    |  key|countAsString|
    +-----+-------------+
    |hello|            1|
    | this|            1|
    +-----+-------------+
    ```
    
    ### Why are the changes needed?
    
    This change adds an API support for spark connect.
    
    ### Does this PR introduce _any_ user-facing change?
    
    This change adds an API support for spark connect.
    
    ### How was this patch tested?
    
    Manually tested.
    
    Closes #40736 from pengzhon-db/connect_applyInPandasWithState.
    
    Authored-by: pengzhon-db <peng.zhong@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/connect/proto/relations_pb2.pyi
+++ python/pyspark/sql/connect/proto/relations_pb2.pyi
@@ -95,6 +95,7 @@ class Relation(google.protobuf.message.Message):
     GROUP_MAP_FIELD_NUMBER: builtins.int
     CO_GROUP_MAP_FIELD_NUMBER: builtins.int
     WITH_WATERMARK_FIELD_NUMBER: builtins.int
+    APPLY_IN_PANDAS_WITH_STATE_FIELD_NUMBER: builtins.int
     FILL_NA_FIELD_NUMBER: builtins.int
     DROP_NA_FIELD_NUMBER: builtins.int
     REPLACE_FIELD_NUMBER: builtins.int
@@ -176,6 +177,8 @@ class Relation(google.protobuf.message.Message):
     @property
     def with_watermark(self) -> global___WithWatermark: ...
     @property
+    def apply_in_pandas_with_state(self) -> global___ApplyInPandasWithState: ...
+    @property
     def fill_na(self) -> global___NAFill:
         """NA functions"""
     @property
@@ -245,6 +248,7 @@ class Relation(google.protobuf.message.Message):
         group_map: global___GroupMap | None = ...,
         co_group_map: global___CoGroupMap | None = ...,
         with_watermark: global___WithWatermark | None = ...,
+        apply_in_pandas_with_state: global___ApplyInPandasWithState | None = ...,
         fill_na: global___NAFill | None = ...,
         drop_na: global___NADrop | None = ...,
         replace: global___NAReplace | None = ...,
@@ -265,6 +269,8 @@ class Relation(google.protobuf.message.Message):
         field_name: typing_extensions.Literal[
             "aggregate",
             b"aggregate",
+            "apply_in_pandas_with_state",
+            b"apply_in_pandas_with_state",
             "approx_quantile",
             b"approx_quantile",
             "catalog",
@@ -366,6 +372,8 @@ class Relation(google.protobuf.message.Message):
         field_name: typing_extensions.Literal[
             "aggregate",
             b"aggregate",
+            "apply_in_pandas_with_state",
+            b"apply_in_pandas_with_state",
             "approx_quantile",
             b"approx_quantile",
             "catalog",
@@ -497,6 +505,7 @@ class Relation(google.protobuf.message.Message):
         "group_map",
         "co_group_map",
         "with_watermark",
+        "apply_in_pandas_with_state",
         "fill_na",
         "drop_na",
         "replace",
@@ -2979,6 +2988,77 @@ class CoGroupMap(google.protobuf.message.Message):
     ) -> None: ...
 
 global___CoGroupMap = CoGroupMap
+
+class ApplyInPandasWithState(google.protobuf.message.Message):
+    DESCRIPTOR: google.protobuf.descriptor.Descriptor
+
+    INPUT_FIELD_NUMBER: builtins.int
+    GROUPING_EXPRESSIONS_FIELD_NUMBER: builtins.int
+    FUNC_FIELD_NUMBER: builtins.int
+    OUTPUT_SCHEMA_FIELD_NUMBER: builtins.int
+    STATE_SCHEMA_FIELD_NUMBER: builtins.int
+    OUTPUT_MODE_FIELD_NUMBER: builtins.int
+    TIMEOUT_CONF_FIELD_NUMBER: builtins.int
+    @property
+    def input(self) -> global___Relation:
+        """(Required) Input relation for applyInPandasWithState."""
+    @property
+    def grouping_expressions(
+        self,
+    ) -> google.protobuf.internal.containers.RepeatedCompositeFieldContainer[
+        pyspark.sql.connect.proto.expressions_pb2.Expression
+    ]:
+        """(Required) Expressions for grouping keys."""
+    @property
+    def func(self) -> pyspark.sql.connect.proto.expressions_pb2.CommonInlineUserDefinedFunction:
+        """(Required) Input user-defined function."""
+    output_schema: builtins.str
+    """(Required) Schema for the output DataFrame."""
+    state_schema: builtins.str
+    """(Required) Schema for the state."""
+    output_mode: builtins.str
+    """(Required) The output mode of the function."""
+    timeout_conf: builtins.str
+    """(Required) Timeout configuration for groups that do not receive data for a while."""
+    def __init__(
+        self,
+        *,
+        input: global___Relation | None = ...,
+        grouping_expressions: collections.abc.Iterable[
+            pyspark.sql.connect.proto.expressions_pb2.Expression
+        ]
+        | None = ...,
+        func: pyspark.sql.connect.proto.expressions_pb2.CommonInlineUserDefinedFunction
+        | None = ...,
+        output_schema: builtins.str = ...,
+        state_schema: builtins.str = ...,
+        output_mode: builtins.str = ...,
+        timeout_conf: builtins.str = ...,
+    ) -> None: ...
+    def HasField(
+        self, field_name: typing_extensions.Literal["func", b"func", "input", b"input"]
+    ) -> builtins.bool: ...
+    def ClearField(
+        self,
+        field_name: typing_extensions.Literal[
+            "func",
+            b"func",
+            "grouping_expressions",
+            b"grouping_expressions",
+            "input",
+            b"input",
+            "output_mode",
+            b"output_mode",
+            "output_schema",
+            b"output_schema",
+            "state_schema",
+            b"state_schema",
+            "timeout_conf",
+            b"timeout_conf",
+        ],
+    ) -> None: ...
+
+global___ApplyInPandasWithState = ApplyInPandasWithState
 
 class CollectMetrics(google.protobuf.message.Message):
     """Collect arbitrary (named) metrics from a dataset."""
