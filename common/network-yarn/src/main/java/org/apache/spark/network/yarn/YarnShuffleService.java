a8b0d26c2627c3ec350ef9bc2ff62372a64f5cbc
Chandni Singh <singh.chandni@gmail.com>
2023-04-19 09:49:34 -0700

    Removed the unused import

--- common/network-yarn/src/main/java/org/apache/spark/network/yarn/YarnShuffleService.java
+++ common/network-yarn/src/main/java/org/apache/spark/network/yarn/YarnShuffleService.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.ByteBuffer;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
