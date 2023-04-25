44c1e4d1401bdc43ddfd131bef6beceecdbeae3e
Ruifeng Zheng <ruifengz@apache.org>
2023-04-25 18:11:29 +0800

    set -ex

--- dev/protobuf-breaking-changes-check.sh
+++ dev/protobuf-breaking-changes-check.sh
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+set -ex
 
 if [[ $# -gt 1 ]]; then
   echo "Illegal number of parameters."
