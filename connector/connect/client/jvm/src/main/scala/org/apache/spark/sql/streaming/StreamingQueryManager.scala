331dd872d434b6a5388ed697d0ad4d444bdc139e
Wei Liu <wei.liu@databricks.com>
2023-05-04 14:50:05 -0700

    fmt

--- connector/connect/client/jvm/src/main/scala/org/apache/spark/sql/streaming/StreamingQueryManager.scala
+++ connector/connect/client/jvm/src/main/scala/org/apache/spark/sql/streaming/StreamingQueryManager.scala
@@ -41,8 +41,8 @@ class StreamingQueryManager private[sql] (sparkSession: SparkSession) {
    * @since 3.5.0
    */
   def active: Array[StreamingQuery] = {
-    executeManagerCmd(_.setActive(true)).getActive.getActiveQueriesList.asScala.map {
-      q => RemoteStreamingQuery.fromStreamingQueryInstanceResponse(sparkSession, q)
+    executeManagerCmd(_.setActive(true)).getActive.getActiveQueriesList.asScala.map { q =>
+      RemoteStreamingQuery.fromStreamingQueryInstanceResponse(sparkSession, q)
     }.toArray
   }
 
@@ -68,18 +68,18 @@ class StreamingQueryManager private[sql] (sparkSession: SparkSession) {
   }
 
   /**
-   * Wait until any of the queries on the associated SQLContext has terminated since the
-   * creation of the context, or since `resetTerminated()` was called. If any query was terminated
-   * with an exception, then the exception will be thrown.
+   * Wait until any of the queries on the associated SQLContext has terminated since the creation
+   * of the context, or since `resetTerminated()` was called. If any query was terminated with an
+   * exception, then the exception will be thrown.
    *
    * If a query has terminated, then subsequent calls to `awaitAnyTermination()` will either
-   * return immediately (if the query was terminated by `query.stop()`),
-   * or throw the exception immediately (if the query was terminated with exception). Use
-   * `resetTerminated()` to clear past terminations and wait for new terminations.
+   * return immediately (if the query was terminated by `query.stop()`), or throw the exception
+   * immediately (if the query was terminated with exception). Use `resetTerminated()` to clear
+   * past terminations and wait for new terminations.
    *
-  *  For correctly documenting exceptions across multiple queries,
-   * users need to stop all of them after any of them terminates with exception, and then check the
-   * `query.exception()` for each query.
+   * For correctly documenting exceptions across multiple queries, users need to stop all of them
+   * after any of them terminates with exception, and then check the `query.exception()` for each
+   * query.
    *
    * @since 3.5.0
    */
@@ -90,19 +90,19 @@ class StreamingQueryManager private[sql] (sparkSession: SparkSession) {
   }
 
   /**
-   * Wait until any of the queries on the associated SQLContext has terminated since the
-   * creation of the context, or since `resetTerminated()` was called. Returns whether any query
-   * has terminated or not (multiple may have terminated). If any query has terminated with an
+   * Wait until any of the queries on the associated SQLContext has terminated since the creation
+   * of the context, or since `resetTerminated()` was called. Returns whether any query has
+   * terminated or not (multiple may have terminated). If any query has terminated with an
    * exception, then the exception will be thrown.
    *
    * If a query has terminated, then subsequent calls to `awaitAnyTermination()` will either
-   * return `true` immediately (if the query was terminated by `query.stop()`),
-   * or throw the exception immediately (if the query was terminated with exception). Use
-   * `resetTerminated()` to clear past terminations and wait for new terminations.
+   * return `true` immediately (if the query was terminated by `query.stop()`), or throw the
+   * exception immediately (if the query was terminated with exception). Use `resetTerminated()`
+   * to clear past terminations and wait for new terminations.
    *
-   * For correctly documenting exceptions across multiple queries,
-   * users need to stop all of them after any of them terminates with exception, and then check the
-   * `query.exception()` for each query.
+   * For correctly documenting exceptions across multiple queries, users need to stop all of them
+   * after any of them terminates with exception, and then check the `query.exception()` for each
+   * query.
    *
    * @since 3.5.0
    */
@@ -111,7 +111,8 @@ class StreamingQueryManager private[sql] (sparkSession: SparkSession) {
   def awaitAnyTermination(timeoutMs: Long): Boolean = {
     require(timeoutMs > 0, "Timeout has to be positive")
     executeManagerCmd(
-      _.getAwaitAnyTerminationBuilder.setTimeoutMs(timeoutMs)).getAwaitAnyTermination.getTerminated
+      _.getAwaitAnyTerminationBuilder.setTimeoutMs(
+        timeoutMs)).getAwaitAnyTermination.getTerminated
   }
 
   /**
@@ -125,7 +126,7 @@ class StreamingQueryManager private[sql] (sparkSession: SparkSession) {
   }
 
   private def executeManagerCmd(
-    setCmdFn: StreamingQueryManagerCommand.Builder => Unit // Sets the command field, like stop().
+      setCmdFn: StreamingQueryManagerCommand.Builder => Unit // Sets the command field, like stop().
   ): StreamingQueryManagerCommandResult = {
 
     val cmdBuilder = Command.newBuilder()
@@ -137,7 +138,8 @@ class StreamingQueryManager private[sql] (sparkSession: SparkSession) {
     val resp = sparkSession.execute(cmdBuilder.build()).head
 
     if (!resp.hasStreamingQueryManagerCommandResult) {
-      throw new RuntimeException("Unexpected missing response for streaming query manager command")
+      throw new RuntimeException(
+        "Unexpected missing response for streaming query manager command")
     }
 
     resp.getStreamingQueryManagerCommandResult
