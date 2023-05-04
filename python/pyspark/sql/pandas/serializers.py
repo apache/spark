e694b92c1104278e33a64ca01e462db5b1d49a8f
Takuya UESHIN <ueshin@databricks.com>
2023-05-03 21:33:56 -0700

    Fix.

--- python/pyspark/sql/pandas/serializers.py
+++ python/pyspark/sql/pandas/serializers.py
@@ -207,6 +207,7 @@ class ArrowStreamPandasSerializer(ArrowStreamSerializer):
             _check_series_convert_timestamps_internal,
             _convert_dict_to_map_items,
         )
+        from pandas.api.types import is_categorical_dtype
 
         # Make input conform to [(series1, type1), (series2, type2), ...]
         if not isinstance(series, (list, tuple)) or (
@@ -225,6 +226,8 @@ class ArrowStreamPandasSerializer(ArrowStreamSerializer):
                 s = _check_series_convert_timestamps_internal(s, self._timezone)
             elif t is not None and pa.types.is_map(t):
                 s = _convert_dict_to_map_items(s)
+            elif t is None and is_categorical_dtype(s.dtype):
+                s = s.astype(s.dtypes.categories.dtype)
             try:
                 array = pa.Array.from_pandas(s, mask=mask, type=t, safe=self._safecheck)
             except TypeError as e:
