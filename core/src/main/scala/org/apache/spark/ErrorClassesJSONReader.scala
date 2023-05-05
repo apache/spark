3819959cb797af2615fd8562a5717f11d06c409a
ClownXC <chen3494269@163.com>
2023-05-05 09:12:06 +0800

    error-classes add field "isTransient"

--- core/src/main/scala/org/apache/spark/ErrorClassesJSONReader.scala
+++ core/src/main/scala/org/apache/spark/ErrorClassesJSONReader.scala
@@ -65,6 +65,8 @@ class ErrorClassesJsonReader(jsonFileURLs: Seq[URL]) {
     val errorInfo = errorInfoMap.getOrElse(
       mainErrorClass,
       throw SparkException.internalError(s"Cannot find main error class '$errorClass'"))
+    assert(errorInfo.subClass.isDefined == subErrorClass.isDefined)
+
     if (subErrorClass.isEmpty) {
       errorInfo.messageTemplate
     } else {
