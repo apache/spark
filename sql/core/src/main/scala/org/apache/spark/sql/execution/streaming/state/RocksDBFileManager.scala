8a27088ece623559f209067cd3ee30f627340b69
Anish Shrigondekar <anish.shrigondekar@databricks.com>
2023-05-08 09:32:22 -0700

    Address Jungtaek's comments

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/RocksDBFileManager.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/RocksDBFileManager.scala
@@ -371,7 +371,7 @@ class RocksDBFileManager(
     // Get the immutable files used in previous versions, as some of those uploaded files can be
     // reused for this version
     logInfo(s"Saving RocksDB files to DFS for $version")
-    val prevFilesToSizes = versionToRocksDBFiles.asScala.filterKeys(_ != version)
+    val prevFilesToSizes = versionToRocksDBFiles.asScala.filterKeys(_ < version)
       .values.flatten.map { f =>
       f.localFileName -> f
     }.toMap
