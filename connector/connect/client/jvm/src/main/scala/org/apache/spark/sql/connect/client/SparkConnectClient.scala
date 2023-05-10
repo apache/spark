5750bdc553be6f78ceb81a16d124ecc11481db8d
Max Gekk <max.gekk@gmail.com>
2023-05-10 14:29:17 +0300

    [SPARK-42585][CONNECT][FOLLOWUP] Store cached local relations as proto
    ### What changes were proposed in this pull request?
    In the PR, I propose to store the cached local relations in the proto format, the same as `LocalRelation`. Also I reverted `transformLocalRelation()` to the state before the commit https://github.com/apache/spark/commit/0d7618a2ca847cf9577659e50409dd5a383d66d3.
    
    ### Why are the changes needed?
    To explicitly specify the format of cached local relations in the proto API.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes but the feature of cached local relations haven't been released yet.
    
    ### How was this patch tested?
    By running the existing tests:
    ```
    $ build/sbt "test:testOnly *.ArtifactManagerSuite"
    $ build/sbt "test:testOnly *.ClientE2ETestSuite"
    $ build/sbt "test:testOnly *.ArtifactStatusesHandlerSuite"
    ```
    
    Closes #41107 from MaxGekk/cached-blob-in-proto.
    
    Authored-by: Max Gekk <max.gekk@gmail.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- connector/connect/client/jvm/src/main/scala/org/apache/spark/sql/connect/client/SparkConnectClient.scala
+++ connector/connect/client/jvm/src/main/scala/org/apache/spark/sql/connect/client/SparkConnectClient.scala
@@ -18,8 +18,6 @@
 package org.apache.spark.sql.connect.client
 
 import java.net.URI
-import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
 import java.util.UUID
 import java.util.concurrent.Executor
 
@@ -237,14 +235,14 @@ private[sql] class SparkConnectClient(
   /**
    * Cache the given local relation at the server, and return its key in the remote cache.
    */
-  def cacheLocalRelation(size: Int, data: ByteString, schema: String): String = {
-    val schemaBytes = schema.getBytes(StandardCharsets.UTF_8)
-    val locRelData = data.toByteArray
-    val locRel = ByteBuffer.allocate(4 + locRelData.length + schemaBytes.length)
-    locRel.putInt(size)
-    locRel.put(locRelData)
-    locRel.put(schemaBytes)
-    artifactManager.cacheArtifact(locRel.array())
+  def cacheLocalRelation(data: ByteString, schema: String): String = {
+    val localRelation = proto.Relation
+      .newBuilder()
+      .getLocalRelationBuilder
+      .setSchema(schema)
+      .setData(data)
+      .build()
+    artifactManager.cacheArtifact(localRelation.toByteArray)
   }
 }
 
