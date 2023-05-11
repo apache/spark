64b7a11f134ba3b62846c4555e6d97157dc6d57e
Ruifeng Zheng <ruifengz@apache.org>
2023-05-11 10:31:43 +0800

    init
    nit

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -852,6 +852,22 @@ pyspark_pandas_connect = Module(
         "pyspark.pandas.tests.connect.test_parity_utils",
         "pyspark.pandas.tests.connect.test_parity_window",
         "pyspark.pandas.tests.connect.indexes.test_parity_base",
+    ],
+    excluded_python_implementations=[
+        "PyPy"  # Skip these tests under PyPy since they require numpy, pandas, and pyarrow and
+        # they aren't available there
+    ],
+)
+
+
+pyspark_pandas_slow_connect = Module(
+    name="pyspark-pandas-slow-connect",
+    dependencies=[pyspark_connect, pyspark_pandas],
+    source_file_regexes=[
+        "python/pyspark/pandas",
+    ],
+    python_test_goals=[
+        # pandas-on-Spark unittests
         "pyspark.pandas.tests.connect.indexes.test_parity_datetime",
         "pyspark.pandas.tests.connect.test_parity_dataframe",
         "pyspark.pandas.tests.connect.test_parity_dataframe_slow",
