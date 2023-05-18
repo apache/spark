54646ff0b0050da1c2716dde49b00865b77ec5d8
l00609879 <liukuijian1@huawei.com>
2023-05-18 10:08:58 +0800

    [SPARK-43491][SQL] In expression should act as same as EqualTo when elements in IN expression have same DataType.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/predicates.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/predicates.scala
@@ -517,7 +517,9 @@ case class In(value: Expression, list: Seq[Expression]) extends Predicate {
            |  $tmpResult = $HAS_NULL; // ${ev.isNull} = true;
            |}
          """.stripMargin
-      val codeElseIf =
+      val codeElseIf = {
+        // When x.isNull is a variable name or false, the else if branch needs to keep,
+        // and only when isNull is set to true the branch just no need to generate.
         if (!java.lang.Boolean.parseBoolean(x.isNull.toString)) {
           s"""
              | else if (${ctx.genEqual(value.dataType, valueArg, x.value)}) {
@@ -526,6 +528,7 @@ case class In(value: Expression, list: Seq[Expression]) extends Predicate {
              |}
            """.stripMargin
         } else ""
+      }
       codeIf + codeElseIf
     }
 
