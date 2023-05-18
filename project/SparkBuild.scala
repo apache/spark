c7b24280dedfcbfe938dc3056bf5013e516da62f
Cheng Pan <chengpan@apache.org>
2023-05-18 20:10:59 +0800

    remove staging repo

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -308,9 +308,7 @@ object SparkBuild extends PomBuild {
       DefaultMavenRepository,
       Resolver.mavenLocal,
       Resolver.file("ivyLocal", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
-    ) ++ Seq(
-      "staging-releases-mirror" at "https://repository.apache.org/content/repositories/staging/",
-      Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)),
+    ),
     externalResolvers := resolvers.value,
     otherResolvers := SbtPomKeys.mvnLocalRepository(dotM2 => Seq(Resolver.file("dotM2", dotM2))).value,
     (MavenCompile / publishLocalConfiguration) := PublishConfiguration()
