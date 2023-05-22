40dd5235373891bdcc536e25082597aca24e6507
Jia Fan <fanjiaeminem@qq.com>
2023-05-22 10:51:25 -0700

    [SPARK-41532][CONNECT][FOLLOWUP] add error class `SESSION_NOT_SAME` into error_classes.py
    ### What changes were proposed in this pull request?
    This is a follow up PR for #40684 . Add error class `SESSION_NOT_SAME` define into `error_classes.py` with a template error message.
    
    ### Why are the changes needed?
    Unified error message
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    add new test.
    
    Closes #41259 from Hisoka-X/follow_up_session_not_same.
    
    Authored-by: Jia Fan <fanjiaeminem@qq.com>
    Signed-off-by: Xinrong Meng <xinrong@apache.org>

--- python/pyspark/sql/connect/dataframe.py
+++ python/pyspark/sql/connect/dataframe.py
@@ -265,7 +265,10 @@ class DataFrame:
 
     def checkSameSparkSession(self, other: "DataFrame") -> None:
         if self._session.session_id != other._session.session_id:
-            raise SessionNotSameException("Both Datasets must belong to the same SparkSession")
+            raise SessionNotSameException(
+                error_class="SESSION_NOT_SAME",
+                message_parameters={},
+            )
 
     def coalesce(self, numPartitions: int) -> "DataFrame":
         if not numPartitions > 0:
