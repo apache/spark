efae5362cbeaf1594b18edf594e83b2cf72afce6
Wanqiang Ji <jiwq@apache.org>
2023-05-29 10:47:13 +0800

    [SPARK-42421][CORE] Use the utils to get the switch for dynamic allocation used in local checkpoint
    ### What changes were proposed in this pull request?
    
    Use the utils to get the switch for dynamic allocation used in local checkpoint
    
    ### Why are the changes needed?
    
    In RDD's local checkpoint, only through retrieve the value from configuration, but not adjudge the local master and testing for dynamic allocation which unified in Utils.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Used the existing UTs
    
    Closes #39998 from jiwq/SPARK-42421.
    
    Authored-by: Wanqiang Ji <jiwq@apache.org>
    Signed-off-by: Kent Yao <yao@apache.org>
    (cherry picked from commit c4b880f8d3936c1b7c8ddd9621ab392c70ace1e7)
    Signed-off-by: Kent Yao <yao@apache.org>

--- core/src/main/scala/org/apache/spark/rdd/RDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/RDD.scala
@@ -1652,8 +1652,8 @@ abstract class RDD[T: ClassTag](
    * The checkpoint directory set through `SparkContext#setCheckpointDir` is not used.
    */
   def localCheckpoint(): this.type = RDDCheckpointData.synchronized {
-    if (conf.get(DYN_ALLOCATION_ENABLED) &&
-        conf.contains(DYN_ALLOCATION_CACHED_EXECUTOR_IDLE_TIMEOUT)) {
+    if (Utils.isDynamicAllocationEnabled(conf) &&
+      conf.contains(DYN_ALLOCATION_CACHED_EXECUTOR_IDLE_TIMEOUT)) {
       logWarning("Local checkpointing is NOT safe to use with dynamic allocation, " +
         "which removes executors along with their cached blocks. If you must use both " +
         "features, you are advised to set `spark.dynamicAllocation.cachedExecutorIdleTimeout` " +
