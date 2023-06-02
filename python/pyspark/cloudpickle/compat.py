fd6397d1542ba74ed4919158b96d42b323f4056c
Hyukjin Kwon <gurwls223@apache.org>
2023-06-02 19:38:13 +0900

    [SPARK-43949][PYTHON] Upgrade cloudpickle to 2.2.1
    This PR proposes to upgrade Cloudpickle from 2.2.0 to 2.2.1.
    
    Cloudpickle 2.2.1 has a fix (https://github.com/cloudpipe/cloudpickle/pull/495) for namedtuple issue (https://github.com/cloudpipe/cloudpickle/issues/460). PySpark relies on namedtuple heavily especially for RDD. We should upgrade and fix it.
    
    Yes, see https://github.com/cloudpipe/cloudpickle/issues/460.
    
    Relies on cloudpickle's unittests. Existing test cases should pass too.
    
    Closes #41433 from HyukjinKwon/cloudpickle-upgrade.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>
    (cherry picked from commit 085dfeb2bed61f6d43d9b99b299373e797ac8f17)
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/cloudpickle/compat.py
+++ python/pyspark/cloudpickle/compat.py
@@ -7,7 +7,12 @@ if sys.version_info < (3, 8):
         from pickle5 import Pickler  # noqa: F401
     except ImportError:
         import pickle  # noqa: F401
+
+        # Use the Python pickler for old CPython versions
         from pickle import _Pickler as Pickler  # noqa: F401
 else:
     import pickle  # noqa: F401
-    from _pickle import Pickler  # noqa: F401
+
+    # Pickler will the C implementation in CPython and the Python
+    # implementation in PyPy
+    from pickle import Pickler  # noqa: F401
