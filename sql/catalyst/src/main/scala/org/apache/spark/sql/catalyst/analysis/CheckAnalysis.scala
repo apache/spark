3be1d5f358db2d96c69a9c53bef15a8db4511565
Jiaan Geng <beliefer@163.com>
2023-06-03 14:51:43 +0800

    Update code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/CheckAnalysis.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/CheckAnalysis.scala
@@ -474,9 +474,11 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog with QueryErrorsB
                 // This is just a sanity check, our analysis rule PullOutNondeterministic should
                 // already pull out those nondeterministic expressions and evaluate them in
                 // a Project node.
-                expr.failAnalysis(
-                  errorClass = "GROUP_EXPRESSION_IS_NON_DETERMINISTIC",
-                  messageParameters = Map("sqlExpr" -> toSQLExpr(expr)))
+                throw SparkException.internalError(
+                  msg = s"Non-deterministic expression '${toSQLExpr(expr)}' should not appear in " +
+                    "grouping expression.",
+                  context = expr.origin.getQueryContext,
+                  summary = expr.origin.context.summary)
               }
             }
 
@@ -546,7 +548,7 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog with QueryErrorsB
               if (!RowOrdering.isOrderable(order.dataType)) {
                 order.failAnalysis(
                   errorClass = "EXPRESSION_TYPE_IS_NOT_ORDERABLE",
-                  messageParameters = Map("type" -> toSQLType(order.dataType)))
+                  messageParameters = Map("exprType" -> toSQLType(order.dataType)))
               }
             }
 
@@ -624,7 +626,7 @@ trait CheckAnalysis extends PredicateHelper with LookupCatalog with QueryErrorsB
 
             if (badReferences.nonEmpty) {
               create.failAnalysis(
-                errorClass = "PARTITION_WITH_NESTED_COLUMN_IS_UNSUPPORTED",
+                errorClass = "UNSUPPORTED_FEATURE.PARTITION_WITH_NESTED_COLUMN_IS_UNSUPPORTED",
                 messageParameters = Map("cols" -> badReferences.map(r => s"`$r`").mkString(", ")))
             }
 
