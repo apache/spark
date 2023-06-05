8a942a00c111ca0f3aaeafbacf5b4665a235e539
Jia Fan <fanjiaeminem@qq.com>
2023-06-05 13:30:23 +0800

    Update sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
    Co-authored-by: Wenchen Fan <cloud0fan@gmail.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSourceStrategy.scala
@@ -275,7 +275,7 @@ class FindDataSourceTable(sparkSession: SparkSession) extends Rule[LogicalPlan] 
 
   override def apply(plan: LogicalPlan): LogicalPlan = plan resolveOperators {
     case i @ InsertIntoStatement(UnresolvedCatalogRelation(tableMeta, options, false),
-    _, _, _, _, _, _) if DDLUtils.isDatasourceTable(tableMeta) =>
+        _, _, _, _, _, _) if DDLUtils.isDatasourceTable(tableMeta) =>
       i.copy(table = readDataSourceTable(tableMeta, options))
 
     case i@InsertIntoStatement(UnresolvedCatalogRelation(tableMeta, _, false),
