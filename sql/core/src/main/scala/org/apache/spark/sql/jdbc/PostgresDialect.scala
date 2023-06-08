ba196a4f2b6682cbef10fbe352de38c741ce8b56
Enrico Minack <github@enrico.minack.dev>
2023-06-08 15:39:20 +0200

    Implement upsert for MsSqlServer

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/PostgresDialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/PostgresDialect.scala
@@ -172,6 +172,7 @@ private case class PostgresDialect()
   override def getUpsertStatement(
       tableName: String,
       columns: Array[String],
+      types: Array[DataType],
       isCaseSensitive: Boolean,
       options: JDBCOptions): String = {
     val insertColumns = columns.mkString(", ")
