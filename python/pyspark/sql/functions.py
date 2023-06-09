7a6c3ebad994ceac9fbef4b4f94f700302e3236d
panbingkun <pbk1982@gmail.com>
2023-06-09 17:39:31 +0800

    [SPARK-43938][CONNECT][PYTHON] Add to_* functions to Scala and Python

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -6406,16 +6406,16 @@ def to_timestamp_ltz(
 
     Examples
     --------
-    >>> spark.conf.set("spark.sql.session.timeZone", "America/Los_Angeles")
-    >>> df = spark.createDataFrame([("2016-04-08",)], ["e"])
+    >>> spark.conf.set("spark.sql.session.timeZone", "UTC")
+    >>> df = spark.createDataFrame([("2016-12-31",)], ["e"])
     >>> df.select(to_timestamp_ltz(df.e, lit("yyyy-MM-dd")).alias('r')).collect()
-    [Row(r=datetime.datetime(2016, 4, 8, 15, 0))]
+    [Row(r=datetime.datetime(2016, 12, 31, 8, 0))]
     >>> spark.conf.unset("spark.sql.session.timeZone")
 
-    >>> spark.conf.set("spark.sql.session.timeZone", "America/Los_Angeles")
-    >>> df = spark.createDataFrame([("2016-04-08",)], ["e"])
+    >>> spark.conf.set("spark.sql.session.timeZone", "UTC")
+    >>> df = spark.createDataFrame([("2016-12-31",)], ["e"])
     >>> df.select(to_timestamp_ltz(df.e).alias('r')).collect()
-    [Row(r=datetime.datetime(2016, 4, 8, 15, 0))]
+    [Row(r=datetime.datetime(2016, 12, 31, 8, 0))]
     >>> spark.conf.unset("spark.sql.session.timeZone")
     """
     if format is not None:
