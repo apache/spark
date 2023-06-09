57e23f1cb03848271737a338e3bc025e57a360e0
YangJie <yangjie01@baidu.com>
2023-06-09 14:11:36 +0800

    Update connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/SparkSessionE2ESuite.scala
    Co-authored-by: Hyukjin Kwon <gurwls223@gmail.com>

--- connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/SparkSessionE2ESuite.scala
+++ connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/SparkSessionE2ESuite.scala
@@ -26,9 +26,9 @@ import org.apache.spark.sql.connect.client.util.RemoteSparkSession
 import org.apache.spark.util.ThreadUtils
 
 /**
- * Warning(SPARK-43648): Please don't import classes that only exist in
+ * NOTE: Do not import classes that only exist in
  * `spark-connect-client-jvm.jar` into the this class, whether explicit or implicit, as it will
- * trigger udf deserialization error during Maven testing.
+ * trigger a UDF deserialization error during Maven build/test.
  */
 class SparkSessionE2ESuite extends RemoteSparkSession {
 
