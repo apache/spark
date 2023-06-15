33d896138b87b2069d4452a78305db7289f3a596
Zhen Li <zhenlineo@users.noreply.github.com>
2023-06-15 11:07:28 -0700

    Reproduce the error with classNotFound with SparkResult

--- connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/SparkSessionE2ESuite.scala
+++ connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/SparkSessionE2ESuite.scala
@@ -25,6 +25,7 @@ import org.scalatest.concurrent.Eventually._
 
 import org.apache.spark.SparkException
 import org.apache.spark.sql.connect.client.util.RemoteSparkSession
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.util.ThreadUtils
 
 /**
@@ -246,5 +247,20 @@ class SparkSessionE2ESuite extends RemoteSparkSession {
       result.toArray
     }
     assert(e.getMessage contains "OPERATION_CANCELED")
+  }
+
+  test("spark result schema") {
+    val df = spark.sql("select val from (values ('Hello'), ('World')) as t(val)")
+//    val result = df.collectResult()
+//    try {
+//      val schema = result.schema
+//      assert(schema == StructType(StructField("val", StringType, nullable = false) :: Nil))
+//    } finally {
+//      result.close()
+//    }
+    df.withResult { result =>
+      val schema = result.schema
+      assert(schema == StructType(StructField("val", StringType, nullable = false) :: Nil))
+    }
   }
 }
