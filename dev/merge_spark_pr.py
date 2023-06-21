5a55061df25a9f9c1c35c272b1563705d957eb84
Hyukjin Kwon <gurwls223@apache.org>
2023-06-21 11:11:57 +0900

    Revert "[SPARK-44129][INFRA] Use "3.5.0" for "master" branch until creating `branch-3.5`"
    This reverts commit 6cc63cbccfca67d13b2e4166382ccd4f2bd49681.

--- dev/merge_spark_pr.py
+++ dev/merge_spark_pr.py
@@ -240,7 +240,7 @@ def cherry_pick(pr_num, merge_hash, default_branch):
 def fix_version_from_branch(branch, versions):
     # Note: Assumes this is a sorted (newest->oldest) list of un-released versions
     if branch == "master":
-        return "3.5.0" # TODO(SPARK-44130) Revert SPARK-44129 after creating branch-3.5
+        return versions[0]
     else:
         branch_ver = branch.replace("branch-", "")
         return list(filter(lambda x: x.name.startswith(branch_ver), versions))[-1]
