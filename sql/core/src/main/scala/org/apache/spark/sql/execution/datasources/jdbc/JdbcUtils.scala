d2e6dcad448501994098dbfd47beef1d497353b4
Enrico Minack <github@enrico.minack.dev>
2023-06-23 15:29:35 +0200

    Add dialect to upsert not supported error

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
@@ -1122,7 +1122,8 @@ object JdbcUtils extends Logging with SQLConfHelper {
     val isolationLevel = options.isolationLevel
 
     if (!dialect.isInstanceOf[MergeByTempTable]) {
-      throw QueryCompilationErrors.tableDoesNotSupportUpsertError(options.table)
+      throw QueryCompilationErrors.tableDoesNotSupportUpsertError(
+        options.table, dialect.getClass.getSimpleName)
     }
     val dialectWithMerge = dialect.asInstanceOf[JdbcDialect with MergeByTempTable]
 
@@ -1154,7 +1155,8 @@ object JdbcUtils extends Logging with SQLConfHelper {
       options: JdbcOptionsInWrite): (StructType, Option[JdbcDialect with MergeByTempTable]) = {
     if (options.isUpsert) {
       if (!dialect.isInstanceOf[MergeByTempTable]) {
-        throw QueryCompilationErrors.tableDoesNotSupportUpsertError(options.table)
+        throw QueryCompilationErrors.tableDoesNotSupportUpsertError(
+          options.table, dialect.getClass.getSimpleName)
       }
       val mergeDialect = dialect.asInstanceOf[JdbcDialect with MergeByTempTable]
 
