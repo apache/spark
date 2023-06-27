ab41d4821948e530a1a18cc5906efefa459a2000
Nihal Potdar <nihal.potdar@databricks.com>
2023-06-27 16:23:29 -0700

    fix merge conflicts

--- connector/kafka-0-10-sql/src/test/scala/org/apache/spark/sql/kafka010/KafkaRelationSuite.scala
+++ connector/kafka-0-10-sql/src/test/scala/org/apache/spark/sql/kafka010/KafkaRelationSuite.scala
@@ -617,31 +617,6 @@ abstract class KafkaRelationSuiteBase extends QueryTest with SharedSparkSession 
     assert(df.rdd.collectPartitions().flatMap(_.map(_.getString(0))).toSet
       === (0 to 30).map(_.toString).toSet)
   }
-
-  test("test MSK IAM auth") {
-    val topic = newTopic()
-    testUtils.createTopic(topic, partitions = 3)
-    testUtils.sendMessages(topic, (0 to 9).map(_.toString).toArray, Some(0))
-    testUtils.sendMessages(topic, (10 to 19).map(_.toString).toArray, Some(1))
-    testUtils.sendMessages(topic, Array("20"), Some(2))
-
-    // Implicit offset values, should default to earliest and latest
-    spark.readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "kafka.privatelink." +
-        "oregon.private.staging.cloud.databricks.com:9090")
-      .option("subscribe", "nihal-msk-iam-test")
-      .option("startingOffsets", "earliest")
-      .option("kafka.sasl.mechanism", "AWS_MSK_IAM")
-      .option("kafka.sasl.jaas.config", "software.amazon.msk.auth.iam.IAMLoginModule required;")
-      .option("kafka.security.protocol", "SASL_SSL")
-      .option("kafka.sasl.client.callback.handler.class", "software." +
-        "amazon.msk.auth.iam.IAMClientCallbackHandler")
-      .load()
-      .writeStream
-      .format("console")
-      .start()
-  }
 }
 
 class KafkaRelationSuiteWithAdminV1 extends KafkaRelationSuiteV1 {
