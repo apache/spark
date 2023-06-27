bb2ad33136625d6264432b6e0ee52ddc01cbcd74
Raghu Angadi <raghu.angadi@databricks.com>
2023-06-27 15:27:03 -0700

    SessionHodler tests

--- connector/connect/server/src/test/scala/org/apache/spark/sql/connect/service/SparkConnectSessionHodlerSuite.scala
+++ connector/connect/server/src/test/scala/org/apache/spark/sql/connect/service/SparkConnectSessionHodlerSuite.scala
@@ -20,69 +20,63 @@ package org.apache.spark.sql.connect.service
 import org.apache.spark.sql.connect.common.InvalidPlanInput
 import org.apache.spark.sql.test.SharedSparkSession
 
-class SparkConnectCachedDataFrameManagerSuite extends SharedSparkSession {
+class SparkConnectSessionHolderSuite extends SharedSparkSession {
 
-  test("Successful put and get") {
-    val spark = this.spark
-    import spark.implicits._
+  test("DataFrame cache: Successful put and get") {
+    val sessionHolder = SessionHolder.forTesting(spark)
+    import sessionHolder.session.implicits._
 
-    val cachedDataFrameManager = new SparkConnectCachedDataFrameManager()
-
-    val key1 = "key_1"
     val data1 = Seq(("k1", "v1"), ("k2", "v2"), ("k3", "v3"))
     val df1 = data1.toDF()
-    cachedDataFrameManager.put(spark, key1, df1)
+    val id1 = "df_id_1"
+    sessionHolder.cacheDataFrameById(id1, df1)
 
-    val expectedDf1 = cachedDataFrameManager.get(spark, key1)
+    val expectedDf1 = sessionHolder.getDataFrameOrThrow(id1)
     assert(expectedDf1 == df1)
 
-    val key2 = "key_2"
     val data2 = Seq(("k4", "v4"), ("k5", "v5"))
     val df2 = data2.toDF()
-    cachedDataFrameManager.put(spark, key2, df2)
+    val id2 = "df_id_2"
+    sessionHolder.cacheDataFrameById(id2, df2)
 
-    val expectedDf2 = cachedDataFrameManager.get(spark, key2)
+    val expectedDf2 = sessionHolder.getDataFrameOrThrow(id2)
     assert(expectedDf2 == df2)
   }
 
-  test("Get cache that does not exist should fail") {
-    val spark = this.spark
-    import spark.implicits._
-
-    val cachedDataFrameManager = new SparkConnectCachedDataFrameManager()
+  test("DataFrame cache: should throw when dataframe is not found") {
+    val sessionHolder = SessionHolder.forTesting(spark)
+    import sessionHolder.session.implicits._
 
     val key1 = "key_1"
 
     assertThrows[InvalidPlanInput] {
-      cachedDataFrameManager.get(spark, key1)
+      sessionHolder.getDataFrameOrThrow(key1)
     }
 
     val data1 = Seq(("k1", "v1"), ("k2", "v2"), ("k3", "v3"))
     val df1 = data1.toDF()
-    cachedDataFrameManager.put(spark, key1, df1)
-    cachedDataFrameManager.get(spark, key1)
+    sessionHolder.cacheDataFrameById(key1, df1)
+    sessionHolder.getDataFrameOrThrow(key1)
 
     val key2 = "key_2"
     assertThrows[InvalidPlanInput] {
-      cachedDataFrameManager.get(spark, key2)
+      sessionHolder.getDataFrameOrThrow(key2)
     }
   }
 
-  test("Remove cache and then get should fail") {
-    val spark = this.spark
-    import spark.implicits._
-
-    val cachedDataFrameManager = new SparkConnectCachedDataFrameManager()
+  test("DataFrame cache: Remove cache and then get should fail") {
+    val sessionHolder = SessionHolder.forTesting(spark)
+    import sessionHolder.session.implicits._
 
     val key1 = "key_1"
     val data1 = Seq(("k1", "v1"), ("k2", "v2"), ("k3", "v3"))
     val df1 = data1.toDF()
-    cachedDataFrameManager.put(spark, key1, df1)
-    cachedDataFrameManager.get(spark, key1)
+    sessionHolder.cacheDataFrameById(key1, df1)
+    sessionHolder.getDataFrameOrThrow(key1)
 
-    cachedDataFrameManager.remove(spark)
+    sessionHolder.removeCachedDataFrame(key1)
     assertThrows[InvalidPlanInput] {
-      cachedDataFrameManager.get(spark, key1)
+      sessionHolder.getDataFrameOrThrow(key1)
     }
   }
 }
