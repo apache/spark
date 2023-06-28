c3b949a4d4ae5052382b22bb860e24a26c668b0b
Amanda Liu <amanda.liu@databricks.com>
2023-06-28 09:47:38 -0700

    Add prettytable error message formatting

--- python/pyspark/testing/utils.py
+++ python/pyspark/testing/utils.py
@@ -35,7 +35,9 @@ from pyspark.find_spark_home import _find_spark_home
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame as PySparkDataFrame
 from pyspark.sql.types import StructType, AtomicType
+from prettytable import PrettyTable
 from functools import reduce
+from itertools import zip_longest
 
 import pandas as pd
 
@@ -58,6 +60,36 @@ except ImportError:
 
 
 SPARK_HOME = _find_spark_home()
+
+
+class bcolors:
+    NC = "\033[0m"  # No Color, reset all
+
+    Bold = "\033[1m"
+    Underlined = "\033[4m"
+    Blink = "\033[5m"
+    Inverted = "\033[7m"
+    Hidden = "\033[8m"
+
+    Black = "\033[30m"
+    Red = "\033[31m"
+    Green = "\033[32m"
+    Yellow = "\033[33m"
+    Blue = "\033[34m"
+    Purple = "\033[35m"
+    Cyan = "\033[36m"
+    LightGray = "\033[37m"
+    DarkGray = "\033[30m"
+    LightRed = "\033[31m"
+    LightGreen = "\033[32m"
+    LightYellow = "\033[93m"
+    LightBlue = "\033[34m"
+    LightPurple = "\033[35m"
+    LightCyan = "\033[36m"
+    White = "\033[97m"
+
+def blue(s: str) -> str:
+    return bcolors.LightBlue + str(s) + bcolors.LightRed
 
 
 def read_int(b):
@@ -236,7 +268,15 @@ def assertSparkDFEquality(
 ):
     def assert_rows_equality(rows1, rows2):
         if rows1 != rows2:
-            msg = "Dataframes are different"
+            t = PrettyTable(["df1", "df2"])
+            zipped = list(zip_longest(rows1, rows2))
+
+            for r1, r2 in zipped:
+                if r1 == r2:
+                    t.add_row([blue(r1), blue(r2)])
+                else:
+                    t.add_row([r1, r2])
+            msg = f"Dataframes are different" + "\n" + t.get_string()
             raise AssertionError(msg)
 
     left = left.sort(left.columns)
