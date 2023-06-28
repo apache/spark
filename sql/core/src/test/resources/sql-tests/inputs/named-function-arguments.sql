345b96d790e1ce86c9cc202522bd98019125ecbd
Richard Yu <richard.yu@databricks.com>
2023-06-28 15:18:13 -0700

    Remove unnecessary line

--- sql/core/src/test/resources/sql-tests/inputs/named-function-arguments.sql
+++ sql/core/src/test/resources/sql-tests/inputs/named-function-arguments.sql
@@ -1,4 +1,3 @@
-SELECT mask('abcd-EFGH-8765-4321', 'A', 'abc');
 SELECT mask('AbCD123-@$#', lowerChar => 'q', upperChar => 'Q', otherChar => 'o', digitChar => 'd');
 SELECT mask(lowerChar => 'q', upperChar => 'Q', otherChar => 'o', digitChar => 'd', str => 'AbCD123-@$#');
 SELECT mask('AbCD123-@$#', lowerChar => 'q', upperChar => 'Q', digitChar => 'd');
