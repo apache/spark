642b4d96fb4e97333a6cd23f287b6bc5cccddb6f
Amanda Liu <amanda.liu@databricks.com>
2023-06-28 09:51:02 -0700

    clean up bcolors

--- python/pyspark/testing/utils.py
+++ python/pyspark/testing/utils.py
@@ -63,30 +63,8 @@ SPARK_HOME = _find_spark_home()
 
 
 class bcolors:
-    NC = "\033[0m"  # No Color, reset all
-
-    Bold = "\033[1m"
-    Underlined = "\033[4m"
-    Blink = "\033[5m"
-    Inverted = "\033[7m"
-    Hidden = "\033[8m"
-
-    Black = "\033[30m"
-    Red = "\033[31m"
-    Green = "\033[32m"
-    Yellow = "\033[33m"
-    Blue = "\033[34m"
-    Purple = "\033[35m"
-    Cyan = "\033[36m"
-    LightGray = "\033[37m"
-    DarkGray = "\033[30m"
     LightRed = "\033[31m"
-    LightGreen = "\033[32m"
-    LightYellow = "\033[93m"
     LightBlue = "\033[34m"
-    LightPurple = "\033[35m"
-    LightCyan = "\033[36m"
-    White = "\033[97m"
 
 def blue(s: str) -> str:
     return bcolors.LightBlue + str(s) + bcolors.LightRed
