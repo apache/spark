a9129defc0ebbe68f20ec888352c30a90925d7ea
panbingkun <pbk1982@gmail.com>
2023-06-29 17:31:03 +0300

    [SPARK-44208][CORE][SQL] Assign clear error class names for some logic that directly uses exceptions
    ### What changes were proposed in this pull request?
    The pr aims to assign clear error class names for some logic that directly uses exceptions, include:
    - ALL_PARTITION_COLUMNS_NOT_ALLOWED
    - INVALID_HIVE_COLUMN_NAME
    - SPECIFY_BUCKETING_IS_NOT_ALLOWED
    - SPECIFY_PARTITION_IS_NOT_ALLOWED
    - UNSUPPORTED_ADD_FILE.DIRECTORY
    - UNSUPPORTED_ADD_FILE.LOCAL_DIRECTORY
    
    ### Why are the changes needed?
    The changes improve the error framework.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    - Update UT.
    - Pass GA.
    
    Closes #41740 from panbingkun/assign_new_name.
    
    Lead-authored-by: panbingkun <pbk1982@gmail.com>
    Co-authored-by: panbingkun <84731559@qq.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- core/src/main/scala/org/apache/spark/errors/SparkCoreErrors.scala
+++ core/src/main/scala/org/apache/spark/errors/SparkCoreErrors.scala
@@ -467,6 +467,20 @@ private[spark] object SparkCoreErrors {
         "receivedBytes" -> receivedBytes.toString).asJava)
   }
 
+  def addLocalDirectoryError(path: Path): Throwable = {
+    new SparkException(
+      errorClass = "UNSUPPORTED_ADD_FILE.LOCAL_DIRECTORY",
+       messageParameters = Map("path" -> path.toString),
+      cause = null)
+  }
+
+  def addDirectoryError(path: Path): Throwable = {
+    new SparkException(
+      errorClass = "UNSUPPORTED_ADD_FILE.DIRECTORY",
+      messageParameters = Map("path" -> path.toString),
+      cause = null)
+  }
+
   private def quoteByDefault(elem: String): String = {
     "\"" + elem + "\""
   }
