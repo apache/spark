13d9d633345b3b50c7ef2c538577d55d63e012d8
Mathew Jacob <mathew.jacob@databricks.com>
2023-07-05 11:44:08 -0700

    Removed all ssh environment setup for the OSS.

--- python/pyspark/ml/torch/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def write_to_location(location: str, content: str) -> None:
-    os.makedirs(os.path.dirname(location), exist_ok=True)
-    with open(location, "a") as f:
-        f.write(content)
-
