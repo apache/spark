d2b8adf8d47b60cf7df002e1e352c5f730ee976c
maheshbehera <maheshbehera@microsoft.com>
2023-07-06 13:50:11 +0530

    SPARK-44307 : [SQL] Add Bloom filter for left outer join even if the left side table is smaller than broadcast threshold.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/InjectRuntimeFilter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/InjectRuntimeFilter.scala
@@ -171,12 +171,12 @@ object InjectRuntimeFilter extends Rule[LogicalPlan] with PredicateHelper with J
   }
 
   private def isProbablyShuffleJoin(left: LogicalPlan,
-                                    right: LogicalPlan,
-                                    hint: JoinHint,
-                                    joinType: JoinType): Boolean = {
+      right: LogicalPlan,
+      hint: JoinHint,
+      joinType: JoinType): Boolean = {
     // If any of the side can be broadcast, then it is not a shuffle join.
-    !canLeftSideBeBroadcast(left, conf, hint, joinType) &&
-      !canRightSideBeBroadcast(right, conf, hint, joinType)
+    !(canLeftSideBeBroadcast(left, conf, hint, joinType) ||
+      canRightSideBeBroadcast(right, conf, hint, joinType))
   }
 
   private def probablyHasShuffle(plan: LogicalPlan): Boolean = {
