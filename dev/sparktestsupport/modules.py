01918bb90170c13abd6c0f0f5c47f5d9bcc02adc
Weichen Xu <weichen.xu@databricks.com>
2023-07-11 07:00:19 +0800

    [SPARK-43983][PYTHON][ML][CONNECT] Implement cross validator estimator
    ### What changes were proposed in this pull request?
    
    Implement cross validator estimator for spark connect.
    
    ### Why are the changes needed?
    
    Distributed ML on spark connect project.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes.
    
    New class `pyspark.ml.connect.tuning.CrossValidator` and `pyspark.ml.connect.tuning.CrossValidatorModel` are added.
    
    ### How was this patch tested?
    
    Unit tests.
    
    Closes #41881 from WeichenXu123/SPARK-43983-cross-val.
    
    Authored-by: Weichen Xu <weichen.xu@databricks.com>
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -620,6 +620,7 @@ pyspark_ml = Module(
         "pyspark.ml.tests.connect.test_legacy_mode_feature",
         "pyspark.ml.tests.connect.test_legacy_mode_classification",
         "pyspark.ml.tests.connect.test_legacy_mode_pipeline",
+        "pyspark.ml.tests.connect.test_legacy_mode_tuning",
     ],
     excluded_python_implementations=[
         "PyPy"  # Skip these tests under PyPy since they require numpy and it isn't available there
@@ -866,6 +867,7 @@ pyspark_connect = Module(
         "pyspark.ml.tests.connect.test_connect_feature",
         "pyspark.ml.tests.connect.test_connect_classification",
         "pyspark.ml.tests.connect.test_connect_pipeline",
+        "pyspark.ml.tests.connect.test_connect_tuning",
     ],
     excluded_python_implementations=[
         "PyPy"  # Skip these tests under PyPy since they require numpy, pandas, and pyarrow and
