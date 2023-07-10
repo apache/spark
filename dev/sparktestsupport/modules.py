c291fa8d4ec28f408138b730b09c2ffad9291dd4
Amanda Liu <amanda.liu@databricks.com>
2023-07-10 10:43:59 -0700

    remove pyspark.testing.utils from pyspark-sql

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -468,7 +468,6 @@ pyspark_sql = Module(
         "pyspark.sql.pandas.typehints",
         "pyspark.sql.pandas.utils",
         "pyspark.sql.observation",
-        "pyspark.testing.utils",
         # unittests
         "pyspark.sql.tests.test_arrow",
         "pyspark.sql.tests.test_arrow_python_udf",
