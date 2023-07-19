d7d7e985ef1fb19c7818b9d1572f2b6f1108e304
Rui Wang <rui.wang@databricks.com>
2023-07-18 21:15:20 -0700

    update

--- sql/core/src/test/scala/org/apache/spark/sql/connector/AlterTableTests.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/connector/AlterTableTests.scala
@@ -811,7 +811,7 @@ trait AlterTableTests extends SharedSparkSession with QueryErrorsBase {
           .add("z", IntegerType))
         .add("b", IntegerType))
 
-      val e1 = intercept[AnalysisException](
+      val e1 = intercept[SparkException](
         sql(s"ALTER TABLE $t ALTER COLUMN b AFTER non_exist"))
       assert(e1.getMessage.contains("Missing field non_exist"))
 
@@ -833,12 +833,12 @@ trait AlterTableTests extends SharedSparkSession with QueryErrorsBase {
           .add("y", IntegerType))
         .add("b", IntegerType))
 
-      val e2 = intercept[AnalysisException](
+      val e2 = intercept[SparkException](
         sql(s"ALTER TABLE $t ALTER COLUMN point.y AFTER non_exist"))
       assert(e2.getMessage.contains("Missing field point.non_exist"))
 
       // `AlterTable.resolved` checks column existence.
-      intercept[AnalysisException](
+      intercept[SparkException](
         sql(s"ALTER TABLE $t ALTER COLUMN a.y AFTER x"))
     }
   }
