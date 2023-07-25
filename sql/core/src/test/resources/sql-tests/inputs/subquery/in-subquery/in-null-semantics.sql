6c934de6339354cef093b0439d13ac69ea294b53
Jack Chen <jack.chen@databricks.com>
2023-07-24 23:47:44 -0400

    null-in-empty-exec

--- sql/core/src/test/resources/sql-tests/inputs/subquery/in-subquery/in-null-semantics.sql
+++ sql/core/src/test/resources/sql-tests/inputs/subquery/in-subquery/in-null-semantics.sql
@@ -29,7 +29,6 @@ select null not in (select e from v_empty);
 
 -- IN subquery which is not rewritten to join - here we use IN in the ON condition because that is a case that doesn't get rewritten to join in RewritePredicateSubquery, so we can observe the execution behavior of InSubquery directly
 -- Correct results: column t2.d should be NULL because the ON condition is always false
--- This will be fixed by the execution fixes.
 select * from t left join t2 on (t.c in (select e from t_empty)) is null;
 select * from t left join t2 on (t.c not in (select e from t_empty)) is null;
 
