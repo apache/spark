874661e3f86b02362e0b9d922e24920cd79675d6
Mathew Jacob <mathew.jacob@databricks.com>
2023-07-26 10:23:13 -0700

    resolved rebase issue; please continue

--- python/pyspark/ml/deepspeed/deepspeed_distributor.py
+++ python/pyspark/ml/deepspeed/deepspeed_distributor.py
@@ -143,17 +143,16 @@ class DeepspeedTorchDistributor(TorchDistributor):
     @staticmethod
     def _run_training_on_pytorch_file(
         input_params: Dict[str, Any], train_path: str, *args: Any, **kwargs: Any
-    ) -> Optional[List[Any]]:
+    ) -> None:
         if kwargs:
             raise ValueError(
                 "DeepspeedTorchDistributor with pytorch file doesn't support keyword arguments"
             )
+
         log_streaming_client = input_params.get("log_streaming_client", None)
         training_command = DeepspeedTorchDistributor._create_torchrun_command(
             input_params, train_path, *args
         )
-        # Spark CI doesn't have GPUs (primary use case for deepspeed); 
-        # Just make sure everything else would work
         DeepspeedTorchDistributor._execute_command(
             training_command, log_streaming_client=log_streaming_client
         )
