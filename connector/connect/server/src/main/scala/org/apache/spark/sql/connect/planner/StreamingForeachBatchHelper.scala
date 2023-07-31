00b9cfc92cb883f3d8302d8b30bb9efa36b4fe55
Wei Liu <wei.liu@databricks.com>
2023-07-31 14:36:54 -0700

    [SPARK-42941][SS][CONNECT] Python StreamingQueryListener
    ### What changes were proposed in this pull request?
    
    Implement the python streaming query listener and the `addListener` method and `removeListener` method, follow up filed in: SPARK-44516 to actually terminate the query listener process when `removeListener` is called.  SPARK-44516 depends on SPARK-44433.
    
    ### Why are the changes needed?
    
    SS Connect development
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes now they can use connect listener
    
    ### How was this patch tested?
    
    Manual test and added unit test
    
    #### addListener:
    ```
    # Client side:
    >>> from pyspark.sql.streaming.listener import StreamingQueryListener;from pyspark.sql.streaming.listener import (QueryStartedEvent, QueryProgressEvent, QueryTerminatedEvent, QueryIdleEvent)
    >>> class MyListener(StreamingQueryListener):
    ...     def onQueryStarted(self, event: QueryStartedEvent) -> None: print("hi, event query id is: " +  str(event.id)); df=self.spark.createDataFrame(["10","11","13"], "string").toDF("age"); df.write.saveAsTable("tbllistener1")
    ...     def onQueryProgress(self, event: QueryProgressEvent) -> None: pass
    ...     def onQueryIdle(self, event: QueryIdleEvent) -> None: pass
    ...     def onQueryTerminated(self, event: QueryTerminatedEvent) -> None: pass
    ...
    >>> spark.streams.addListener(MyListener())
    >>> q = spark.readStream.format("rate").load().writeStream.format("console").start()
    >>> q.stop()
    >>> spark.read.table("tbllistener1").collect()
    [Row(age='13'), Row(age='10'), Row(age='11’)]
    
    # Server side:
    ##### event_type received from python process is 0
    hi, event query id is: dd7ba1c4-6c8f-4369-9c3c-5dede22b8a2f
    ```
    
    #### removeListener:
    ```
    # Client side:
    >>> listener = MyListener(); spark.streams.addListener(listener)
    >>> spark.streams.removeListener(listener)
    
    # Server side:
    # nothing to print actually, the listener is removed from server side StreamingQueryManager and cache in sessionHolder, but the process still hangs there. Follow up SPARK-44516 filed to stop this process
    ```
    
    Closes #42116 from WweiL/listener-poc-newest.
    
    Lead-authored-by: Wei Liu <wei.liu@databricks.com>
    Co-authored-by: pengzhon-db <peng.zhong@databricks.com>
    Signed-off-by: Takuya UESHIN <ueshin@databricks.com>

--- connector/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/StreamingForeachBatchHelper.scala
+++ connector/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/StreamingForeachBatchHelper.scala
@@ -18,9 +18,7 @@ package org.apache.spark.sql.connect.planner
 
 import java.util.UUID
 
-import org.apache.spark.api.python.PythonRDD
-import org.apache.spark.api.python.SimplePythonFunction
-import org.apache.spark.api.python.StreamingPythonRunner
+import org.apache.spark.api.python.{PythonRDD, SimplePythonFunction, StreamingPythonRunner}
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.connect.service.SessionHolder
@@ -90,7 +88,10 @@ object StreamingForeachBatchHelper extends Logging {
     val port = SparkConnectService.localPort
     val connectUrl = s"sc://localhost:$port/;user_id=${sessionHolder.userId}"
     val runner = StreamingPythonRunner(pythonFn, connectUrl)
-    val (dataOut, dataIn) = runner.init(sessionHolder.sessionId)
+    val (dataOut, dataIn) =
+      runner.init(
+        sessionHolder.sessionId,
+        "pyspark.sql.connect.streaming.worker.foreachBatch_worker")
 
     val foreachBatchRunnerFn: FnArgsWithId => Unit = (args: FnArgsWithId) => {
 
