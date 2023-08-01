c858851fc6c23da80bece921e9cf345a148cc4ae
Kent Yao <yao@apache.org>
2023-08-01 09:45:06 +0800

    [SPARK-44579][SQL] Support Interrupt On Cancel in SQLExecution
    ### What changes were proposed in this pull request?
    
    This PR adds spark.sql.execution.interruptOnCancel that defaults to true to allow interrupting task threads in SQL layer
    
    ### Why are the changes needed?
    
    Currently, we support interrupting task threads for users by 1) APIs of the spark core module, 2) a thrift config for the SQL module.  Other Spark SQL Apps are limited to using this functionality. Specifically,  the built-in spark-sql-shell lacks a user-controlled knob for interrupting task threads.
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    tested locally
    
    Closes #42199 from yaooqinn/SPARK-44579.
    
    Authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Xiduo You <ulyssesyou@apache.org>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1374,6 +1374,12 @@ object SQLConf {
     .timeConf(TimeUnit.SECONDS)
     .createWithDefaultString(s"${5 * 60}")
 
+  val INTERRUPT_ON_CANCEL = buildConf("spark.sql.execution.interruptOnCancel")
+    .doc("When true, all running tasks will be interrupted if one cancels a query.")
+    .version("4.0.0")
+    .booleanConf
+    .createWithDefault(true)
+
   // This is only used for the thriftserver
   val THRIFTSERVER_POOL = buildConf("spark.sql.thriftserver.scheduler.pool")
     .doc("Set a Fair Scheduler pool for a JDBC client session.")
@@ -1394,8 +1400,7 @@ object SQLConf {
       .doc("When true, all running tasks will be interrupted if one cancels a query. " +
         "When false, all running tasks will remain until finished.")
       .version("3.2.0")
-      .booleanConf
-      .createWithDefault(true)
+      .fallbackConf(INTERRUPT_ON_CANCEL)
 
   val THRIFTSERVER_QUERY_TIMEOUT =
     buildConf("spark.sql.thriftServer.queryTimeout")
