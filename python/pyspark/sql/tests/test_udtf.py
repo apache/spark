14b61c1ddb3e83d093cb5a0e4a3cb891f75b7256
allisonwang-db <allison.wang@databricks.com>
2023-08-03 21:36:53 -0700

    fix tests

--- python/pyspark/sql/tests/test_udtf.py
+++ python/pyspark/sql/tests/test_udtf.py
@@ -1663,13 +1663,16 @@ class UDTFArrowTestsMixin(BaseUDTFTestsMixin):
             def eval(self):
                 yield 1,
 
+        # We do not use `self.sql_conf` here to test the SQL SET command
+        # instead of using PySpark's `spark.conf.set`.
+        old_value = self.spark.conf.get("spark.sql.execution.pythonUDTF.arrow.enabled")
         self.spark.sql("SET spark.sql.execution.pythonUDTF.arrow.enabled=False")
         self.assertEqual(udtf(TestUDTF, returnType="x: int").evalType, PythonEvalType.SQL_TABLE_UDF)
         self.spark.sql("SET spark.sql.execution.pythonUDTF.arrow.enabled=True")
         self.assertEqual(
             udtf(TestUDTF, returnType="x: int").evalType, PythonEvalType.SQL_ARROW_TABLE_UDF
         )
-        self.spark.sql("RESET spark.sql.execution.pythonUDTF.arrow.enabled")
+        self.spark.conf.set("spark.sql.execution.pythonUDTF.arrow.enabled", old_value)
 
     def test_udtf_eval_returning_non_tuple(self):
         class TestUDTF:
