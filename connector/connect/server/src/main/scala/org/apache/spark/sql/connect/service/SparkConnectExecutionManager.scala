7436099f312654bcae4c46459b98b148c24e9b70
Juliusz Sompolski <julek@databricks.com>
2023-08-11 13:30:59 +0200

    fix access

--- connector/connect/server/src/main/scala/org/apache/spark/sql/connect/service/SparkConnectExecutionManager.scala
+++ connector/connect/server/src/main/scala/org/apache/spark/sql/connect/service/SparkConnectExecutionManager.scala
@@ -61,7 +61,7 @@ private[connect] class SparkConnectExecutionManager() extends Logging {
   /**
    * Create a new ExecuteHolder and register it with this global manager and with its session.
    */
-  private[service] def createExecuteHolder(request: proto.ExecutePlanRequest): ExecuteHolder = {
+  private[connect] def createExecuteHolder(request: proto.ExecutePlanRequest): ExecuteHolder = {
     val sessionHolder = SparkConnectService
       .getOrCreateIsolatedSession(request.getUserContext.getUserId, request.getSessionId)
     val executeHolder = new ExecuteHolder(request, sessionHolder)
@@ -110,7 +110,7 @@ private[connect] class SparkConnectExecutionManager() extends Logging {
     executeHolder.foreach(_.close())
   }
 
-  private[service] def getExecuteHolder(key: ExecuteKey): Option[ExecuteHolder] = {
+  private[connect] def getExecuteHolder(key: ExecuteKey): Option[ExecuteHolder] = {
     executionsLock.synchronized {
       executions.get(key)
     }
