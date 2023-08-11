e0769b5144075bd70f2bebfcff76dc9a7dcb6ae9
Herman van Hovell <herman@databricks.com>
2023-08-11 14:36:22 +0200

    [SPARK-44761][CONNECT] Support DataStreamWriter.foreachBatch(VoidFunction2)
    ### What changes were proposed in this pull request?
    This PR adds the `DatastreamWriter.foreachBatch(VoidFunction2).
    
    ### Why are the changes needed?
    To increase binary compatibility with the APIs in `sql/core`.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes. It adds a new method to DatastreamWriter.
    
    ### How was this patch tested?
    I modified an existing code path.
    
    Closes #42430 from hvanhovell/SPARK-44761.
    
    Lead-authored-by: Herman van Hovell <herman@databricks.com>
    Co-authored-by: Hyukjin Kwon <gurwls223@gmail.com>
    Signed-off-by: Herman van Hovell <herman@databricks.com>

--- connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/connect/client/CheckConnectJvmClientCompatibility.scala
+++ connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/connect/client/CheckConnectJvmClientCompatibility.scala
@@ -234,9 +234,6 @@ object CheckConnectJvmClientCompatibility {
       ProblemFilters.exclude[MissingClassProblem](
         "org.apache.spark.sql.streaming.DataStreamWriter$"),
       ProblemFilters.exclude[Problem](
-        "org.apache.spark.sql.streaming.DataStreamWriter.foreachBatch" // TODO(SPARK-42944)
-      ),
-      ProblemFilters.exclude[Problem](
         "org.apache.spark.sql.streaming.DataStreamWriter.SOURCE*" // These are constant vals.
       ),
 
