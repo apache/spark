481546385a605143d3a6103fa2aaf9936612465d
Herman van Hovell <herman@databricks.com>
2023-08-14 05:39:15 +0200

    Revert "Add test"
    This reverts commit 2c465f681ada8be8cb53edfff3ddbd273b89bc72.

--- connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/application/ReplE2ESuite.scala
+++ connector/connect/client/jvm/src/test/scala/org/apache/spark/sql/application/ReplE2ESuite.scala
@@ -276,13 +276,4 @@ class ReplE2ESuite extends RemoteSparkSession with BeforeAndAfterEach {
     val output = runCommandsInShell(input)
     assertContains("Array[MyTestClass] = Array(MyTestClass(1), MyTestClass(3))", output)
   }
-
-  test("REPL class in UDF") {
-    val input = """
-        |case class MyTestClass(value: Int)
-        |spark.range(2).map(i => MyTestClass(i.toInt)).collect()
-      """.stripMargin
-    val output = runCommandsInShell(input)
-    assertContains("Array[MyTestClass] = Array(MyTestClass(0), MyTestClass(1))", output)
-  }
 }
