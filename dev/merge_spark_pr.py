305ef394bc7c5969fa15ca517884ad12b5487ad3
Dongjoon Hyun <dongjoon@apache.org>
2023-08-16 02:35:12 -0700

    Update dev/merge_spark_pr.py
    Co-authored-by: Kent Yao <yao@apache.org>

--- dev/merge_spark_pr.py
+++ dev/merge_spark_pr.py
@@ -496,8 +496,8 @@ def main():
     # Check this up front to avoid failing the JIRA update at the very end
     if not JIRA_ACCESS_TOKEN and (not JIRA_USERNAME or not JIRA_PASSWORD):
         msg = (
-            "The env-vars JIRA_ACCESS_TOKEN, JIRA_USERNAME and/or JIRA_PASSWORD are not set. "
-            + "Continue?"
+            "The env-vars JIRA_ACCESS_TOKEN or JIRA_USERNAME/JIRA_PA
+SSWORD are not set. Continue?"
         )
         continue_maybe(msg)
 
