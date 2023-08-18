f77ddd4110959b89a08348e072af8247988d15ab
Daniel Tenedorio <daniel.tenedorio@databricks.com>
2023-08-18 10:05:11 -0700

    commit

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/BatchEvalPythonUDTFExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/BatchEvalPythonUDTFExec.scala
@@ -134,6 +134,7 @@ object PythonUDTFRunner {
             dataOut.writeBoolean(false)
         }
     }
+    /*
     udtf.pythonUDTFPartitionColumnIndexes match {
       case Some(partitionColumnIndexes) =>
         dataOut.writeInt(partitionColumnIndexes.partitionChildIndexes.length)
@@ -142,6 +143,7 @@ object PythonUDTFRunner {
       case None =>
         dataOut.writeInt(0)
     }
+    */
     dataOut.writeInt(udtf.func.command.length)
     dataOut.write(udtf.func.command.toArray)
     PythonWorkerUtils.writeUTF(udtf.elementSchema.json, dataOut)
