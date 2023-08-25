f0f058b9bac27123faf9e36534b1aadb204557a7
Hasnain Lakhani <hasnain.lakhani@databricks.com>
2023-08-25 13:52:52 -0700

    typo

--- common/network-common/src/main/java/org/apache/spark/network/ssl/SSLFactory.java
+++ common/network-common/src/main/java/org/apache/spark/network/ssl/SSLFactory.java
@@ -462,9 +462,9 @@ public class SSLFactory {
       "TLS_AES_128_GCM_SHA256",
       "TLS_AES_256_GCM_SHA384",
       // Next we have the TLS1.2 ciphers for intermediate compatibility (since JDK8 does not
-      // support TLS1.2)
+      // support TLS1.3)
       "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-      "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+      "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
       "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
       "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
       "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
