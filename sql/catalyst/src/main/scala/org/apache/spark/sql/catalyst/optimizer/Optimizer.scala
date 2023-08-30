31c23f60b430fd397b4049d1c65338dd32fab50c
Mingming Ge <7mming7@gmail.com>
2023-08-30 14:08:49 +0800

    KE-42353 [SQL] Optimize join if maximum number of rows on one side is 1 (#669)

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/Optimizer.scala
@@ -239,6 +239,7 @@ abstract class Optimizer(catalogManager: CatalogManager)
       CollapseProject,
       RemoveRedundantAliases,
       RemoveNoopOperators) :+
+    Batch("OptimizeOneRowJoin", Once, OptimizeOneRowJoin) :+
     // This batch must be executed after the `RewriteSubquery` batch, which creates joins.
     Batch("NormalizeFloatingNumbers", Once, NormalizeFloatingNumbers) :+
     Batch("ReplaceUpdateFieldsExpression", Once, ReplaceUpdateFieldsExpression)
