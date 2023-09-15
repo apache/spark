6ad274cd85a4990cc7d5ee78a64f0ef5ec36a078
Haejoon Lee <haejoon.lee@databricks.com>
2023-09-15 19:44:54 +0900

    remove duplication

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -1106,15 +1106,6 @@ pyspark_errors = Module(
         # in PySpark, since the Python Packaging Tests is only enabled within this module.
         # This module is the smallest Python test module, it contains only 1 test file
         # and normally takes < 2 seconds, so the additional cost is small.
-
-pyspark_errors = Module(
-    name="pyspark-errors",
-    dependencies=[],
-    source_file_regexes=[
-        # SPARK-44544: Force the execution of pyspark_errors when there are any changes
-        # in PySpark, since the Python Packaging Tests is only enabled within this module.
-        # This module is the smallest Python test module, it contains only 1 test file
-        # and normally takes < 2 seconds, so the additional cost is small.
         "python/",
         "python/pyspark/errors",
     ],
