5f74c9930b2c498273086cb6aea5162b69bb531f
Jiaan Geng <beliefer@163.com>
2023-10-08 19:59:58 +0800

    Fix Java codestyle.

--- common/utils/src/main/java/org/apache/spark/storage/StorageLevelMapper.java
+++ common/utils/src/main/java/org/apache/spark/storage/StorageLevelMapper.java
@@ -21,27 +21,27 @@ package org.apache.spark.storage;
  * A mapper class easy to obtain storage levels based on their names.
  */
 public enum StorageLevelMapper {
-    NONE(StorageLevel.NONE()),
-    DISK_ONLY(StorageLevel.DISK_ONLY()),
-    DISK_ONLY_2(StorageLevel.DISK_ONLY_2()),
-    DISK_ONLY_3(StorageLevel.DISK_ONLY_3()),
-    MEMORY_ONLY(StorageLevel.MEMORY_ONLY()),
-    MEMORY_ONLY_2(StorageLevel.MEMORY_ONLY_2()),
-    MEMORY_ONLY_SER(StorageLevel.MEMORY_ONLY_SER()),
-    MEMORY_ONLY_SER_2(StorageLevel.MEMORY_ONLY_SER_2()),
-    MEMORY_AND_DISK(StorageLevel.MEMORY_AND_DISK()),
-    MEMORY_AND_DISK_2(StorageLevel.MEMORY_AND_DISK_2()),
-    MEMORY_AND_DISK_SER(StorageLevel.MEMORY_AND_DISK_SER()),
-    MEMORY_AND_DISK_SER_2(StorageLevel.MEMORY_AND_DISK_SER_2()),
-    OFF_HEAP(StorageLevel.OFF_HEAP());
+  NONE(StorageLevel.NONE()),
+  DISK_ONLY(StorageLevel.DISK_ONLY()),
+  DISK_ONLY_2(StorageLevel.DISK_ONLY_2()),
+  DISK_ONLY_3(StorageLevel.DISK_ONLY_3()),
+  MEMORY_ONLY(StorageLevel.MEMORY_ONLY()),
+  MEMORY_ONLY_2(StorageLevel.MEMORY_ONLY_2()),
+  MEMORY_ONLY_SER(StorageLevel.MEMORY_ONLY_SER()),
+  MEMORY_ONLY_SER_2(StorageLevel.MEMORY_ONLY_SER_2()),
+  MEMORY_AND_DISK(StorageLevel.MEMORY_AND_DISK()),
+  MEMORY_AND_DISK_2(StorageLevel.MEMORY_AND_DISK_2()),
+  MEMORY_AND_DISK_SER(StorageLevel.MEMORY_AND_DISK_SER()),
+  MEMORY_AND_DISK_SER_2(StorageLevel.MEMORY_AND_DISK_SER_2()),
+  OFF_HEAP(StorageLevel.OFF_HEAP());
 
-    private final StorageLevel storageLevel;
+  private final StorageLevel storageLevel;
 
-    StorageLevelMapper(StorageLevel storageLevel) {
-      this.storageLevel = storageLevel;
-    }
+  StorageLevelMapper(StorageLevel storageLevel) {
+    this.storageLevel = storageLevel;
+  }
 
-    public static StorageLevel fromString(String s) throws IllegalArgumentException {
-      return StorageLevelMapper.valueOf(s).storageLevel;
-    }
+  public static StorageLevel fromString(String s) throws IllegalArgumentException {
+    return StorageLevelMapper.valueOf(s).storageLevel;
+  }
 }
