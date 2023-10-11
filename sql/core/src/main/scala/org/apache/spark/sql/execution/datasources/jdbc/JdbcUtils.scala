04e6b713ab5e4a607254f331a0ea1e331ae6c857
Hyukjin Kwon <gurwls223@apache.org>
2023-10-11 14:15:40 +0900

    [SPARK-45475][SQL][3.5] Uses DataFrame.foreachPartition instead of RDD.foreachPartition in JdbcUtils
    This PR cherry-picks https://github.com/apache/spark/pull/43304 to branch-3.5
    
    ---
    
    ### What changes were proposed in this pull request?
    
    This PR is kind of a followup for https://github.com/apache/spark/pull/39976 that addresses https://github.com/apache/spark/pull/39976#issuecomment-1752930380 comment.
    
    ### Why are the changes needed?
    
    In order to probably assign the SQL execution ID so `df.observe` works with this.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes. `df.observe` will work with JDBC connectors.
    
    ### How was this patch tested?
    
    Manually tested.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    Unit test was added.
    
    Closes #43322 from HyukjinKwon/SPARK-45475-3.5.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
@@ -898,7 +898,7 @@ object JdbcUtils extends Logging with SQLConfHelper {
       case Some(n) if n < df.rdd.getNumPartitions => df.coalesce(n)
       case _ => df
     }
-    repartitionedDF.rdd.foreachPartition { iterator => savePartition(
+    repartitionedDF.foreachPartition { iterator: Iterator[Row] => savePartition(
       table, iterator, rddSchema, insertStmt, batchSize, dialect, isolationLevel, options)
     }
   }
