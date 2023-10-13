280f6b33110d707ebee6fec6e5bafa45b45213ae
allisonwang-db <allison.wang@databricks.com>
2023-10-12 17:02:41 -0700

    [SPARK-45505][PYTHON] Refactor analyzeInPython to make it reusable
    ### What changes were proposed in this pull request?
    
    Currently, the `analyzeInPython` method in UserDefinedPythonTableFunction object can starts a Python process in driver and run a Python function in the Python process. This PR aims to refactor this logic into a reusable runner class.
    
    ### Why are the changes needed?
    
    To make the code more reusable.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    Existing tests
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #43340 from allisonwang-db/spark-45505-refactor-analyze-in-py.
    
    Authored-by: allisonwang-db <allison.wang@databricks.com>
    Signed-off-by: Takuya UESHIN <ueshin@databricks.com>

--- python/pyspark/sql/worker/analyze_udtf.py
+++ python/pyspark/sql/worker/analyze_udtf.py
@@ -98,14 +98,14 @@ def main(infile: IO, outfile: IO) -> None:
     """
     Runs the Python UDTF's `analyze` static method.
 
-    This process will be invoked from `UserDefinedPythonTableFunction.analyzeInPython` in JVM
-    and receive the Python UDTF and its arguments for the `analyze` static method,
+    This process will be invoked from `UserDefinedPythonTableFunctionAnalyzeRunner.runInPython`
+    in JVM and receive the Python UDTF and its arguments for the `analyze` static method,
     and call the `analyze` static method, and send back a AnalyzeResult as a result of the method.
     """
     try:
         check_python_version(infile)
 
-        memory_limit_mb = int(os.environ.get("PYSPARK_UDTF_ANALYZER_MEMORY_MB", "-1"))
+        memory_limit_mb = int(os.environ.get("PYSPARK_PLANNER_MEMORY_MB", "-1"))
         setup_memory_limits(memory_limit_mb)
 
         setup_spark_files(infile)
