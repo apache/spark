f12d2441a1b94118f759f4ed95ca7fbd4b5f0f74
Takuya UESHIN <ueshin@databricks.com>
2023-10-24 08:30:34 +0900

    [SPARK-45619][CONNECT][PYTHON] Apply the observed metrics to Observation object
    ### What changes were proposed in this pull request?
    
    Apply the observed metrics to `Observation` object.
    
    ### Why are the changes needed?
    
    When using `Observation`, the observed metrics should be applied to the object.
    
    For example, in vanilla PySpark:
    
    ```py
    >>> df = spark.createDataFrame([["Alice", 2], ["Bob", 5]], ["name", "age"])
    >>> observation = Observation("my metrics")
    >>> observed_df = df.observe(observation, count(lit(1)).alias("count"), max(col("age")))
    >>> observed_df.count()
    2
    >>>
    >>> observation.get
    {'count': 2, 'max(age)': 5}
    ```
    
    whereas in Spark Connect, currently it fails with `PySparkNotImplementedError`:
    
    ```py
    >>> observation.get
    Traceback (most recent call last):
    ...
        raise PySparkNotImplementedError(
    pyspark.errors.exceptions.base.PySparkNotImplementedError: [NOT_IMPLEMENTED] Observation support for Spark Connect is not implemented.
    ```
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, the `Observation` will see the changes.
    
    ### How was this patch tested?
    
    Added/modified the related tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #43469 from ueshin/issues/SPARK-45619/observation.
    
    Authored-by: Takuya UESHIN <ueshin@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/tests/connect/test_connect_basic.py
+++ python/pyspark/sql/tests/connect/test_connect_basic.py
@@ -1789,14 +1789,16 @@ class SparkConnectBasicTests(SparkConnectSQLTestCase):
             .toPandas(),
         )
 
+        from pyspark.sql.connect.observation import Observation as ConnectObservation
         from pyspark.sql.observation import Observation
 
+        cobservation = ConnectObservation(observation_name)
         observation = Observation(observation_name)
 
         cdf = (
             self.connect.read.table(self.tbl_name)
             .filter("id > 3")
-            .observe(observation, CF.min("id"), CF.max("id"), CF.sum("id"))
+            .observe(cobservation, CF.min("id"), CF.max("id"), CF.sum("id"))
             .toPandas()
         )
         df = (
@@ -1807,6 +1809,8 @@ class SparkConnectBasicTests(SparkConnectSQLTestCase):
         )
 
         self.assert_eq(cdf, df)
+
+        self.assert_eq(cobservation.get, observation.get)
 
         observed_metrics = cdf.attrs["observed_metrics"]
         self.assert_eq(len(observed_metrics), 1)
