63cf0fec05e6caf53450dc53828ed6e95190664d
Dongjoon Hyun <dhyun@apple.com>
2023-11-01 08:17:33 -0700

    [SPARK-45753][CORE] Support `spark.deploy.driverIdPattern`
    ### What changes were proposed in this pull request?
    
    This PR aims to support `spark.deploy.driverIdPattern` for Apache Spark 4.0.0.
    
    ### Why are the changes needed?
    
    This allows the users to be able to control driver ID pattern.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Pass the CIs with the newly added test case.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #43615 from dongjoon-hyun/SPARK-45753.
    
    Authored-by: Dongjoon Hyun <dhyun@apple.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- core/src/main/scala/org/apache/spark/deploy/master/Master.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/Master.scala
@@ -53,6 +53,8 @@ private[deploy] class Master(
   private val forwardMessageThread =
     ThreadUtils.newDaemonSingleThreadScheduledExecutor("master-forward-message-thread")
 
+  private val driverIdPattern = conf.get(DRIVER_ID_PATTERN)
+
   // For application IDs
   private def createDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US)
 
@@ -1175,7 +1177,7 @@ private[deploy] class Master(
   }
 
   private def newDriverId(submitDate: Date): String = {
-    val appId = "driver-%s-%04d".format(createDateFormat.format(submitDate), nextDriverNumber)
+    val appId = driverIdPattern.format(createDateFormat.format(submitDate), nextDriverNumber)
     nextDriverNumber += 1
     appId
   }
