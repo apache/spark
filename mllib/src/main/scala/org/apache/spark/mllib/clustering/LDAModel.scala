070e1b12fe787b059df78844651c008d871bb083
tangjiafu <jiafu.tang@qq.com>
2023-11-07 13:34:48 +0800

    wip

--- mllib/src/main/scala/org/apache/spark/mllib/clustering/LDAModel.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/clustering/LDAModel.scala
@@ -20,7 +20,7 @@ package org.apache.spark.mllib.clustering
 import breeze.linalg.{argmax, argtopk, normalize, sum, DenseMatrix => BDM, DenseVector => BDV}
 import breeze.numerics.{exp, lgamma}
 import org.apache.hadoop.fs.Path
-import org.json4s.DefaultFormats
+import org.json4s.{DefaultFormats, Formats}
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
