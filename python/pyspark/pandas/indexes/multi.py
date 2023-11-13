52d164aaa01e50da01b3a4325bf8c2433cbf266d
Ruifeng Zheng <ruifengz@apache.org>
2023-11-13 17:33:30 +0800

    simplify

--- python/pyspark/pandas/indexes/multi.py
+++ python/pyspark/pandas/indexes/multi.py
@@ -810,20 +810,17 @@ class MultiIndex(Index):
         sdf_self = self._psdf._internal.spark_frame.select(self._internal.index_spark_columns)
         sdf_other = other._psdf._internal.spark_frame.select(other._internal.index_spark_columns)
 
-        tmp_tag_column_name = verify_temp_column_name(sdf_self, "__multi_index_tag__")
-        tmp_max_column_name = verify_temp_column_name(sdf_self, "__multi_index_max_tag__")
-        tmp_min_column_name = verify_temp_column_name(sdf_self, "__multi_index_min_tag__")
+        tmp_tag_col = verify_temp_column_name(sdf_self, "__multi_index_tag__")
+        tmp_max_col = verify_temp_column_name(sdf_self, "__multi_index_max_tag__")
+        tmp_min_col = verify_temp_column_name(sdf_self, "__multi_index_min_tag__")
 
         sdf_symdiff = (
-            sdf_self.withColumn(tmp_tag_column_name, F.lit(0))
-            .union(sdf_other.withColumn(tmp_tag_column_name, F.lit(1)))
+            sdf_self.withColumn(tmp_tag_col, F.lit(0))
+            .union(sdf_other.withColumn(tmp_tag_col, F.lit(1)))
             .groupBy(*self._internal.index_spark_column_names)
-            .agg(
-                F.min(tmp_tag_column_name).alias(tmp_min_column_name),
-                F.max(tmp_tag_column_name).alias(tmp_max_column_name),
-            )
-            .where(F.col(tmp_min_column_name) == F.col(tmp_max_column_name))
-            .drop(tmp_min_column_name, tmp_max_column_name)
+            .agg(F.min(tmp_tag_col).alias(tmp_min_col), F.max(tmp_tag_col).alias(tmp_max_col))
+            .where(F.col(tmp_min_col) == F.col(tmp_max_col))
+            .select(*self._internal.index_spark_column_names)
         )
 
         if sort:
