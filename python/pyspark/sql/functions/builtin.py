92f496ec56be590ee620c5a516204b15eeec06e2
Hyukjin Kwon <gurwls223@apache.org>
2023-11-18 16:17:11 +0900

    Completely ignore

--- python/pyspark/sql/functions/builtin.py
+++ python/pyspark/sql/functions/builtin.py
@@ -72,7 +72,7 @@ if TYPE_CHECKING:
         UserDefinedFunctionLike,
     )
 
-if _has_numpy:  # type: ignore[has-type]
+if _has_numpy:  # type: ignore
     import numpy as np
 
 # Note to developers: all of PySpark functions here take string as column names whenever possible.
