400db88d00e50750513d733be697b6b2dd9043d3
Haejoon Lee <haejoon.lee@databricks.com>
2023-11-27 08:49:18 +0900

    [SPARK-46103][PYTHON][INFRA][BUILD][DOCS] Enhancing PySpark documentation
    ### What changes were proposed in this pull request?
    
    This PR proposes to enhance the PySpark documentation by leveraging modern Sphinx features and functionalities. The primary objective is to improve the overall user experience and readability of the documentation. To achieve this, the PR includes an upgrade of `Sphinx` and `Jinja2` to their newer/latest versions, enabling us to use the latest `pydata_sphinx_theme` features such as light/dark mode toggling.
    
    ### Why are the changes needed?
    
    Currently, the PySpark documentation is unable to utilize many of the advanced features available in recent `Sphinx` versions due to older package versions. This limitation hinders the documentation's visual appeal and usability, particularly when compared to other projects like Pandas which have already adopted these enhancements. For example:
    
    ## Pandas API reference (better layout / switching light & dark mode available)
    
    ### Dark mode
    <img width="1409" alt="Screenshot 2023-11-26 at 5 43 29 AM" src="https://github.com/apache/spark/assets/44108233/0f97ce4a-c1ec-47fb-9295-445c2d557393">
    
    ### Light mode
    <img width="1403" alt="Screenshot 2023-11-26 at 5 45 01 AM" src="https://github.com/apache/spark/assets/44108233/715f74a8-9e49-4c05-80ef-5531d2e68220">
    
    ## PySpark API reference (less readable compare to pandas / no light & dark mode)
    <img width="1312" alt="Screenshot 2023-11-26 at 5 43 48 AM" src="https://github.com/apache/spark/assets/44108233/722d2b61-e231-4387-a5ab-dcd447045d94">
    
    By updating the `Sphinx` and `Jinja2` versions, we can significantly improve the documentation's layout, design, and interactive features, thereby enhancing the end-user experience.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No API changes, but users will notice a more modern and user-friendly interface in the PySpark documentation. New features like light/dark mode and improved page layouts will be available as below:
    
    ## Before
    <img width="1312" alt="Screenshot 2023-11-26 at 5 43 48 AM" src="https://github.com/apache/spark/assets/44108233/722d2b61-e231-4387-a5ab-dcd447045d94">
    
    ## After
    ### Dark mode
    <img width="1388" alt="Screenshot 2023-11-26 at 6 17 13 AM" src="https://github.com/apache/spark/assets/44108233/b5ed6cfd-9a65-4c03-a067-b40e89cc8c48">
    
    ### Light mode
    <img width="1392" alt="Screenshot 2023-11-26 at 6 16 47 AM" src="https://github.com/apache/spark/assets/44108233/24b723a7-5b00-4565-81d9-9c87154c115f">
    
    ### How was this patch tested?
    
    Manually built docs from local environment, and also tested combinations between various `Jinja2`, `Sphinx` and `pydata_sphinx_theme` versions for best document rendering.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #44012 from itholic/upgrade_sphinx.
    
    Authored-by: Haejoon Lee <haejoon.lee@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/docs/source/conf.py
+++ python/docs/source/conf.py
@@ -194,7 +194,11 @@ html_context = {
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "navbar_end": ["version-switcher"]
+    "navbar_end": ["version-switcher", "theme-switcher"],
+    "logo": {
+        "image_light": "_static/spark-logo-light.png",
+        "image_dark": "_static/spark-logo-dark.png",
+    }
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
