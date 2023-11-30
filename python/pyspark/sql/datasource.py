2fad7b7ba28ef29e06b2f6a664ec54fe561e3b41
allisonwang-db <allison.wang@databricks.com>
2023-12-01 08:41:13 +0900

    [SPARK-45940][PYTHON] Add InputPartition to DataSourceReader interface
    ### What changes were proposed in this pull request?
    
    This PR introduces a new Python class `InputPartition` that represents the partition value returned by the `partitions` method in DataSourceReader.
    
    ### Why are the changes needed?
    
    Before this PR, the `partitions` method can return anything, and it can be confusing to infer what is the `partition` argument in the `read(self, partition)` method.
    
    Adding `InputPartition` can make the Python data source API more intuitive and user-friendly.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    New unit tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #44085 from allisonwang-db/spark-45940-input-partition.
    
    Authored-by: allisonwang-db <allison.wang@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/datasource.py
+++ python/pyspark/sql/datasource.py
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 from abc import ABC, abstractmethod
-from typing import final, Any, Dict, Iterator, List, Tuple, Type, Union, TYPE_CHECKING
+from typing import final, Any, Dict, Iterator, List, Sequence, Tuple, Type, Union, TYPE_CHECKING
 
 from pyspark.sql import Row
 from pyspark.sql.types import StructType
@@ -145,6 +145,44 @@ class DataSource(ABC):
         raise NotImplementedError
 
 
+class InputPartition:
+    """
+    A base class representing an input partition returned by the `partitions()`
+    method of `DataSourceReader`.
+
+    .. versionadded: 4.0.0
+
+    Notes
+    -----
+    This class must be picklable.
+
+    Examples
+    --------
+    Use the default input partition implementation:
+
+    >>> def partitions(self):
+    ...     return [InputPartition(1)]
+
+    Subclass the input partition class:
+
+    >>> from dataclasses import dataclass
+    >>> @dataclass
+    ... class RangeInputPartition(InputPartition):
+    ...     start: int
+    ...     end: int
+
+    >>> def partitions(self):
+    ...     return [RangeInputPartition(1, 3), RangeInputPartition(4, 6)]
+    """
+
+    def __init__(self, value: Any) -> None:
+        self.value = value
+
+    def __repr__(self) -> str:
+        attributes = ", ".join([f"{k}={v!r}" for k, v in self.__dict__.items()])
+        return f"{self.__class__.__name__}({attributes})"
+
+
 class DataSourceReader(ABC):
     """
     A base class for data source readers. Data source readers are responsible for
@@ -153,7 +191,7 @@ class DataSourceReader(ABC):
     .. versionadded: 4.0.0
     """
 
-    def partitions(self) -> Iterator[Any]:
+    def partitions(self) -> Sequence[InputPartition]:
         """
         Returns an iterator of partitions for this data source.
 
@@ -171,40 +209,40 @@ class DataSourceReader(ABC):
 
         Returns
         -------
-        Iterator[Any]
-            An iterator of partitions for this data source. The partition value can be
-            any serializable objects.
+        Sequence[InputPartition]
+            A sequence of partitions for this data source. Each partition value
+            must be an instance of `InputPartition` or a subclass of it.
 
         Notes
         -----
-        This method should not return any un-picklable objects.
+        All partition values must be picklable objects.
 
         Examples
         --------
         Returns a list of integers:
 
         >>> def partitions(self):
-        ...     return [1, 2, 3]
+        ...     return [InputPartition(1), InputPartition(2), InputPartition(3)]
 
         Returns a list of string:
 
         >>> def partitions(self):
-        ...     return ["a", "b", "c"]
+        ...     return [InputPartition("a"), InputPartition("b"), InputPartition("c")]
 
-        Returns a list of tuples:
+        Returns a list of ranges:
+
+        >>> class RangeInputPartition(InputPartition):
+        ...    def __init__(self, start, end):
+        ...        self.start = start
+        ...        self.end = end
 
         >>> def partitions(self):
-        ...     return [("a", 1), ("b", 2), ("c", 3)]
-
-        Returns a list of dictionaries:
-
-        >>> def partitions(self):
-        ...     return [{"a": 1}, {"b": 2}, {"c": 3}]
+        ...     return [RangeInputPartition(1, 3), RangeInputPartition(5, 10)]
         """
-        yield None
+        raise NotImplementedError
 
     @abstractmethod
-    def read(self, partition: Any) -> Iterator[Union[Tuple, Row]]:
+    def read(self, partition: InputPartition) -> Iterator[Union[Tuple, Row]]:
         """
         Generates data for a given partition and returns an iterator of tuples or rows.
 
@@ -229,15 +267,15 @@ class DataSourceReader(ABC):
         --------
         Yields a list of tuples:
 
-        >>> def read(self, partition):
-        ...     yield (partition, 0)
-        ...     yield (partition, 1)
+        >>> def read(self, partition: InputPartition):
+        ...     yield (partition.value, 0)
+        ...     yield (partition.value, 1)
 
         Yields a list of rows:
 
-        >>> def read(self, partition):
-        ...     yield Row(partition=partition, value=0)
-        ...     yield Row(partition=partition, value=1)
+        >>> def read(self, partition: InputPartition):
+        ...     yield Row(partition=partition.value, value=0)
+        ...     yield Row(partition=partition.value, value=1)
         """
         ...
 
@@ -313,6 +351,10 @@ class WriterCommitMessage:
     sent back to the driver side as input parameter of ``commit`` or ``abort`` method.
 
     .. versionadded: 4.0.0
+
+    Notes
+    -----
+    This class must be picklable.
     """
 
     ...
