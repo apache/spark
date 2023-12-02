374bb833187024a43d95a9fc776e951bf36e1751
asmita <asmita@utexas.edu>
2023-12-01 18:17:40 -0600

    Changing RDD suite

--- core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala
+++ core/src/test/scala/org/apache/spark/rdd/RDDSuite.scala
@@ -806,7 +806,7 @@ class RDDSuite extends SparkFunSuite with SharedSparkContext with Eventually {
   }
 
   test("runJob on an invalid partition") {
-    intercept[SparkException] {
+    intercept[SparkIllegalArgumentException] {
       sc.runJob(sc.parallelize(1 to 10, 2), {iter: Iterator[Int] => iter.size}, Seq(0, 1, 2))
     }
   }
