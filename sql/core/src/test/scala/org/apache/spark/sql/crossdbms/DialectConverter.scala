fcbfb0b6674e32cb15dc1f7af4fe86ff3a908dae
Andy Lam <andy.lam@databricks.com>
2023-12-04 17:50:28 -0800

    Initial commit

--- sql/core/src/test/scala/org/apache/spark/sql/crossdbms/DialectConverter.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/crossdbms/DialectConverter.scala
@@ -15,18 +15,9 @@
  * limitations under the License.
  */
 
-package org.apache.spark.sql.crossdbms
+sackage org.apache.spark.sql.crossdbms
 
 trait DialectConverter {
-
-  final val NOOP = ""
-  final val SET_COMMAND_PREFIX = "set"
-  final val COMMAND_PREFIXES_TO_IGNORE = Seq(SET_COMMAND_PREFIX)
-
-  final val SPARK_SET_COMMAND_PATTERN = """set (\w+) = (true|false);""".r
-
-  protected def checkIgnore(query: String): Boolean =
-    COMMAND_PREFIXES_TO_IGNORE.count(prefix => query.startsWith(prefix)) > 0
 
   /**
    * Transform the query string, which is compatible with Spark, such that it is compatible with
@@ -34,21 +25,6 @@ trait DialectConverter {
    * may not always produce a compatible query string.
    */
   def preprocessQuery(query: String): String
-
-  /**
-   * Transform the query SET, which is compatible with Spark, such that it is compatible with
-   * dialect.
-   * For example:
-   * {{{
-   *   1. create temporary view t1 as values (..);
-   *   2. set spark.sql... = true;
-   *   3. select * from ...
-   *   4. set spark.sql... = false;
-   * }}}
-   * We can remove everything between lines 2 and 4 because if setting configs is not applicable to
-   * other DBMS systems, and the tests between such config setting is not relevant.
-   */
-  def preprocessQuerySet(queries: Seq[String]): Seq[String]
 }
 
 /**
@@ -56,11 +32,77 @@ trait DialectConverter {
  */
 object PostgresDialectConverter extends DialectConverter {
 
-  def preprocessQuery(query: String): String = {
-    query
+  private def dataTypeConverter(query: String): String = {
+    // Convert {some decimal number}D to CAST({decimal number} AS DOUBLE PRECISION)
+    val doublePrecisionPattern = """(\d+\.\d+)D""".r
+    val doublePrecisionResult = doublePrecisionPattern.replaceAllIn(query,
+      m => s"CAST(${m.group(1)} AS DOUBLE PRECISION)")
+
+    // Convert {number}S to CAST({number} AS SMALLINT)
+    val smallIntPattern = """(\d+)S""".r
+    val smallIntResult = smallIntPattern.replaceAllIn(doublePrecisionResult, m =>
+      s"CAST(${m.group(1)} AS SMALLINT)")
+
+    // Convert float({some decimal number}) to CAST({decimal number} AS REAL)
+    val floatPattern = """float\((\d+\.\d+)\)""".r
+    val floatResult = floatPattern.replaceAllIn(smallIntResult, m => s"CAST(${m.group(1)} AS REAL)")
+
+    // Convert {{some number}E{some number}}BD to CAST({number}E{number} AS DECIMAL)
+    val decimalPattern = """(\d+E\d+)BD""".r
+    decimalPattern.replaceAllIn(floatResult, m => s"CAST(${m.group(1)} AS DECIMAL)")
   }
 
-  override def preprocessQuerySet(queries: Seq[String]): Seq[String] = {
-    queries
+  // Replace double quotes with single quotes
+  private def replaceDoubleQuotes(query: String): String = query.replaceAll("\"", "'")
+
+  private def convertCreateViewSyntax(query: String): String = {
+    // (?i):
+    //    This is a flag for case-insensitive matching.
+    // CREATE\s+:
+    //    Matches the literal string "CREATE" followed by one or more whitespace characters.
+    // ((?:TEMP(?:ORARY)?)?\s+)?:
+    //    This is a capturing group for the optional "TEMP" or "TEMPORARY" part. The (?: ... ) is a
+    //    non-capturing group, and the ? after it makes the entire group optional.
+    // VIEW\s+:
+    //    Matches the literal string "VIEW" followed by one or more whitespace characters.
+    // (IF NOT EXISTS\s+)?:
+    //    This is a capturing group for the optional "IF NOT EXISTS" part followed by one or more
+    //    whitespace characters. The ? after it makes the entire group optional.
+    // (\w+):
+    //    This is a capturing group that matches one or more word characters (letters, digits, or
+    //    underscores), capturing the view name.
+    // \s*:
+    //    Matches zero or more whitespace characters.
+    // (?:\(([\w, ]+)\))?:
+    //    This is a non-capturing group that matches an optional group containing a list of word
+    //    characters, commas, and spaces within parentheses. The \w allows word characters, allows
+    //    for multiple column names separated by commas, and the space allows for spaces within the
+    //    parentheses. The ?: makes it a non-capturing group, and the entire group is optional.
+    // \s+:
+    //    Matches one or more whitespace characters.
+    //  AS:
+    //    Matches the literal string "AS".
+    // \s+:
+    //    Matches one or more whitespace characters.
+    // (.+):
+    //    This is a capturing group that matches one or more of any character (except for a
+    //    newline), capturing the view query.
+
+    val createViewPattern =
+      """(?i)CREATE\s+((?:TEMP(?:ORARY)?)?\s+)?VIEW\s+(IF NOT EXISTS\s+)?(\w+)\s*(?:\(([\w, ]+)\))? AS\s+(.+)""".r
+
+    query match {
+      case createViewPattern(temp, ifNotExists, viewName, columnList, viewQuery) =>
+        s"""
+          | CREATE $temp VIEW $ifNotExists $viewName($columnList) AS ($viewQuery)
+          |""".stripMargin
+      case _ => query
+    }
+  }
+
+  def preprocessQuery(query: String): String = {
+    val singleQuotedQuery = replaceDoubleQuotes(query)
+    val queryWithReplacedDataTypes = dataTypeConverter(singleQuotedQuery)
+    convertCreateViewSyntax(queryWithReplacedDataTypes)
   }
 }
