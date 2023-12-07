dee4d33e975950a3240211fe1bbecec8bc3f729d
Ruifeng Zheng <ruifengz@apache.org>
2023-12-07 14:06:19 +0800

    init

--- python/pyspark/pandas/tests/series/test_as_type.py
+++ python/pyspark/pandas/tests/series/test_as_type.py
@@ -30,14 +30,6 @@ from pyspark.pandas.typedef.typehints import (
 
 
 class SeriesAsTypeMixin:
-    @property
-    def pser(self):
-        return pd.Series([1, 2, 3, 4, 5, 6, 7], name="x")
-
-    @property
-    def psser(self):
-        return ps.from_pandas(self.pser)
-
     def test_astype(self):
         psers = [pd.Series([10, 20, 15, 30, 45], name="x")]
 
--- python/pyspark/pandas/tests/series/test_index.py
+++ python/pyspark/pandas/tests/series/test_index.py
@@ -25,14 +25,6 @@ from pyspark.testing.sqlutils import SQLTestUtils
 
 
 class SeriesIndexMixin:
-    @property
-    def pser(self):
-        return pd.Series([1, 2, 3, 4, 5, 6, 7], name="x")
-
-    @property
-    def psser(self):
-        return ps.from_pandas(self.pser)
-
     def test_align(self):
         pdf = pd.DataFrame({"a": [1, 2, 3], "b": ["a", "b", "c"]})
         psdf = ps.from_pandas(pdf)
