ba9c4ad586af5f2f5ae1e7cf7e6394ff62eaf4a0
Raghu Angadi <raghu.angadi@databricks.com>
2023-12-08 16:40:27 +0900

    [SPARK-46275] Protobuf: Return null in permissive mode when deserialization fails
    This updates the the behavior of `from_protobuf()` built function when underlying record fails to deserialize.
    
      * **Current behvior**:
        * By default, this would throw an error and the query fails. [This part is not changed in the PR]
        * When `mode` is set to 'PERMISSIVE' it returns a non-null struct with each of the inner fields set to null e.g. `{ "field_a": null, "field_b": null }`  etc.
           * This is not very convenient to the users. They don't know if this was due to malformed record or if the input itself has null. It is very hard to check for each field for null in SQL query (imagine a sql query with a struct that has 10 fields).
    
      * **New behavior**
        * When `mode` is set to 'PERMISSIVE' it simply returns `null`.
    
    This makes it easier for users to detect and handle malformed records.
    
    Yes, but this does not change the contract. In fact, it clarifies it.
    
     - Unit tests are updated.
    
    No.
    
    Closes #44214 from rangadi/protobuf-null.
    
    Authored-by: Raghu Angadi <raghu.angadi@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- connector/protobuf/src/test/scala/org/apache/spark/sql/protobuf/ProtobufCatalystDataConversionSuite.scala
+++ connector/protobuf/src/test/scala/org/apache/spark/sql/protobuf/ProtobufCatalystDataConversionSuite.scala
@@ -78,20 +78,9 @@ class ProtobufCatalystDataConversionSuite
         .eval()
     }
 
-    val expected = {
-      val expectedSchema = ProtobufUtils.buildDescriptor(descFilePath, badSchema)
-      SchemaConverters.toSqlType(expectedSchema).dataType match {
-        case st: StructType =>
-          Row.fromSeq((0 until st.length).map { _ =>
-            null
-          })
-        case _ => null
-      }
-    }
-
     checkEvaluation(
       ProtobufDataToCatalyst(binary, badSchema, Some(descFilePath), Map("mode" -> "PERMISSIVE")),
-      expected)
+      expected = null)
   }
 
   protected def prepareExpectedResult(expected: Any): Any = expected match {
