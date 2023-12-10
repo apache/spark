50844630d2370d1888a82c981d65cf5ed24c813b
Ruifeng Zheng <ruifengz@apache.org>
2023-12-10 09:35:14 +0800

    [SPARK-46341][PS][TESTS] Reorganize `SeriesInterpolateTests`
    ### What changes were proposed in this pull request?
    1, Move `SeriesInterpolateTests` to `pyspark.pandas.tests.series.*`
    2, also optimize the test by control the combinations
    
    ### Why are the changes needed?
    move it to the right place
    
    ### Does this PR introduce _any_ user-facing change?
    no, test-only
    
    ### How was this patch tested?
    ci
    
    ### Was this patch authored or co-authored using generative AI tooling?
    no
    
    Closes #44274 from zhengruifeng/ps_test_series_interpolate.
    
    Authored-by: Ruifeng Zheng <ruifengz@apache.org>
    Signed-off-by: Ruifeng Zheng <ruifengz@apache.org>

--- python/pyspark/pandas/tests/connect/series/test_parity_interpolate.py
+++ python/pyspark/pandas/tests/connect/series/test_parity_interpolate.py
@@ -16,19 +16,21 @@
 #
 import unittest
 
-from pyspark.pandas.tests.test_series_interpolate import SeriesInterpolateTestsMixin
+from pyspark.pandas.tests.series.test_interpolate import SeriesInterpolateMixin
 from pyspark.testing.connectutils import ReusedConnectTestCase
-from pyspark.testing.pandasutils import PandasOnSparkTestUtils, TestUtils
+from pyspark.testing.pandasutils import PandasOnSparkTestUtils
 
 
-class SeriesInterpolateParityTests(
-    SeriesInterpolateTestsMixin, TestUtils, PandasOnSparkTestUtils, ReusedConnectTestCase
+class SeriesParityInterpolateTests(
+    SeriesInterpolateMixin,
+    PandasOnSparkTestUtils,
+    ReusedConnectTestCase,
 ):
     pass
 
 
 if __name__ == "__main__":
-    from pyspark.pandas.tests.connect.test_parity_series_interpolate import *  # noqa: F401
+    from pyspark.pandas.tests.connect.series.test_parity_interpolate import *  # noqa: F401
 
     try:
         import xmlrunner  # type: ignore[import]
