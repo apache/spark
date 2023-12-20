57c88ee99978554951ec602dbdd9a84c00a1c32f
Aleksandar Tomic <aleksandar.tomic@databricks.com>
2023-12-20 10:30:30 +0100

    Updating expression documentation

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Between.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/Between.scala
@@ -19,7 +19,7 @@ package org.apache.spark.sql.catalyst.expressions
 
 // scalastyle:off line.size.limit
 @ExpressionDescription(
-  usage = "BETWEEN(projection, lower, upper) - Returns true if `projection` > `lower` and `projection` <  `upper`.",
+  usage = "expr1 [NOT] BETWEEN expr2 AND expr3 - Returns true if `projection` > `lower` and `projection` <  `upper`.",
   examples = """
     Examples:
       > SELECT 0.5 _FUNC_ 0.1 AND 1.0;
