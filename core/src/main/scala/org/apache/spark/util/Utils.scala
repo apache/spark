640ea6ee24237be2418c2c15e7e8b89d2233bdb7
yangjie01 <yangjie01@baidu.com>
2023-12-22 17:17:52 +0800

    import

--- core/src/main/scala/org/apache/spark/util/Utils.scala
+++ core/src/main/scala/org/apache/spark/util/Utils.scala
@@ -67,7 +67,7 @@ import org.slf4j.Logger
 
 import org.apache.spark._
 import org.apache.spark.deploy.SparkHadoopUtil
-import org.apache.spark.internal.{config, Logging}
+import org.apache.spark.internal.Logging
 import org.apache.spark.internal.config._
 import org.apache.spark.internal.config.Streaming._
 import org.apache.spark.internal.config.Tests.IS_TESTING
