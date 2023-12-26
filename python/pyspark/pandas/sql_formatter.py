c6c880ce27af2f7c8953bd44f4b27bc08cfe6235
Nicholas Chammas <nicholas.chammas@gmail.com>
2023-12-25 20:35:11 -0500

    fix rst link

--- python/pyspark/pandas/sql_formatter.py
+++ python/pyspark/pandas/sql_formatter.py
@@ -109,13 +109,13 @@ def sql(
     args : dict or list
         A dictionary of parameter names to Python objects or a list of Python objects
         that can be converted to SQL literal expressions. See
-        <a href="https://spark.apache.org/docs/latest/sql-ref-datatypes.html">
-        Supported Data Types</a> for supported value types in Python.
+        `Supported Data Types`_ for supported value types in Python.
         For example, dictionary keys: "rank", "name", "birthdate";
         dictionary values: 1, "Steven", datetime.date(2023, 4, 2).
         A value can be also a `Column` of a literal or collection constructor functions such
         as `map()`, `array()`, `struct()`, in that case it is taken as is.
 
+        .. _Supported Data Types: https://spark.apache.org/docs/latest/sql-ref-datatypes.html
 
         .. versionadded:: 3.4.0
 
@@ -176,7 +176,7 @@ def sql(
     1  2
     2  3
 
-    And substitude named parameters with the `:` prefix by SQL literals.
+    And substitute named parameters with the `:` prefix by SQL literals.
 
     >>> ps.sql("SELECT * FROM range(10) WHERE id > :bound1", args={"bound1":7})
        id
