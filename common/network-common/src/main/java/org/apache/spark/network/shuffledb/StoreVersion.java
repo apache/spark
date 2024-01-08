ce0feb9df8870142b2016d425d70e7d6f60619df
yangjie01 <yangjie01@baidu.com>
2024-01-08 18:23:36 +0800

    init

--- common/network-common/src/main/java/org/apache/spark/network/shuffledb/StoreVersion.java
+++ common/network-common/src/main/java/org/apache/spark/network/shuffledb/StoreVersion.java
@@ -54,4 +54,9 @@ public class StoreVersion {
         result = 31 * result + minor;
         return result;
     }
+
+    @Override
+    public String toString() {
+      return "StoreVersion[" + major + "." + minor + ']';
+    }
 }
