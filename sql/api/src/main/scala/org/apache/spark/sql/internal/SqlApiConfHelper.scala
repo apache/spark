7ae3ac2baa3fa889c688741331d60b960612ffab
Rui Wang <rui.wang@databricks.com>
2024-01-08 14:33:19 -0800

    update

--- sql/api/src/main/scala/org/apache/spark/sql/internal/SqlApiConfHelper.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/internal/SqlApiConfHelper.scala
@@ -18,6 +18,13 @@ package org.apache.spark.sql.internal
 
 import java.util.concurrent.atomic.AtomicReference
 
+/**
+ * SqlApiConfHelper is created to avoid a deadlock during a concurrent access to SQLConf and
+ * SqlApiConf, which is because SQLConf and SqlApiConf tries to load each other upon
+ * initializations. SqlApiConfHelper is private to sql package and is not supposed to be
+ * accessed by end users. Variables and methods within SqlApiConfHelper are defined to
+ * be used by SQLConf and SqlApiConf only.
+ */
 private[sql] object SqlApiConfHelper {
   // Shared keys.
   val ANSI_ENABLED_KEY: String = "spark.sql.ansi.enabled"
