257bb571762016f2efba581fe16199cfbb7a2f6a
Andrey Gubichev <andrey.gubichev@databricks.com>
2024-01-09 13:21:35 -0800

    tests

--- sql/core/src/test/resources/sql-tests/inputs/subquery/exists-subquery/exists-orderby-limit.sql
+++ sql/core/src/test/resources/sql-tests/inputs/subquery/exists-subquery/exists-orderby-limit.sql
@@ -203,13 +203,12 @@ WHERE  EXISTS (SELECT max(dept.dept_id)
                GROUP  BY state
                LIMIT  1);
 
+-- SPARK-46526: LIMIT over correlated predicate that references only the outer table.
 SELECT *
 FROM   emp
 WHERE  EXISTS (SELECT max(dept.dept_id)
                FROM   dept
-               WHERE  dept.dept_id <> emp.dept_id
-                 AND emp.salary > 200
-               GROUP  BY state
+               WHERE  emp.salary > 200
                LIMIT  1);
 
 -- limit and offset in the not exists subquery block.
