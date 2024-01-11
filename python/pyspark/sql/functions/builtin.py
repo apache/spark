bb6eec5fd7f97af2f8c5043931e9dde253b0dee1
panbingkun <panbingkun@baidu.com>
2024-01-11 13:45:13 +0800

    [SPARK-46654][SQL] Make to_csv can correctly display complex types data

--- python/pyspark/sql/functions/builtin.py
+++ python/pyspark/sql/functions/builtin.py
@@ -15054,11 +15054,11 @@ def to_csv(col: "ColumnOrName", options: Optional[Dict[str, str]] = None) -> Col
     >>> data = [(1, Row(age=2, name='Alice', scores=[100, 200, 300]))]
     >>> df = spark.createDataFrame(data, ("key", "value"))
     >>> df.select(sf.to_csv(df.value)).show(truncate=False)
-    +-----------------------+
-    |to_csv(value)          |
-    +-----------------------+
-    |2,Alice,"[100,200,300]"|
-    +-----------------------+
+    +-------------------------+
+    |to_csv(value)            |
+    +-------------------------+
+    |2,Alice,"[100, 200, 300]"|
+    +-------------------------+
 
     Example 3: Converting a StructType with null values to a CSV string
 
