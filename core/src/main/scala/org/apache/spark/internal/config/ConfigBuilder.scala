e3ee67a6df3a6b5516bec4f7da53d7a1816dcbeb
Nicholas Chammas <nicholas.chammas@gmail.com>
2024-01-12 12:42:26 -0500

    add documentation groups

--- core/src/main/scala/org/apache/spark/internal/config/ConfigBuilder.scala
+++ core/src/main/scala/org/apache/spark/internal/config/ConfigBuilder.scala
@@ -131,7 +131,7 @@ private[spark] class TypedConfigBuilder[T](
   def createOptional: OptionalConfigEntry[T] = {
     val entry = new OptionalConfigEntry[T](parent.key, parent._prependedKey,
       parent._prependSeparator, parent._alternatives, converter, stringConverter, parent._doc,
-      parent._public, parent._version)
+      parent._public, parent._documentationGroups, parent._version)
     parent._onCreate.foreach(_(entry))
     entry
   }
@@ -146,7 +146,8 @@ private[spark] class TypedConfigBuilder[T](
         val transformedDefault = converter(stringConverter(default))
         val entry = new ConfigEntryWithDefault[T](parent.key, parent._prependedKey,
           parent._prependSeparator, parent._alternatives, transformedDefault, converter,
-          stringConverter, parent._doc, parent._public, parent._version)
+          stringConverter, parent._doc, parent._public, parent._documentationGroups,
+          parent._version)
         parent._onCreate.foreach(_ (entry))
         entry
     }
@@ -156,7 +157,7 @@ private[spark] class TypedConfigBuilder[T](
   def createWithDefaultFunction(defaultFunc: () => T): ConfigEntry[T] = {
     val entry = new ConfigEntryWithDefaultFunction[T](parent.key, parent._prependedKey,
       parent._prependSeparator, parent._alternatives, defaultFunc, converter, stringConverter,
-      parent._doc, parent._public, parent._version)
+      parent._doc, parent._public, parent._documentationGroups, parent._version)
     parent._onCreate.foreach(_ (entry))
     entry
   }
@@ -168,7 +169,7 @@ private[spark] class TypedConfigBuilder[T](
   def createWithDefaultString(default: String): ConfigEntry[T] = {
     val entry = new ConfigEntryWithDefaultString[T](parent.key, parent._prependedKey,
       parent._prependSeparator, parent._alternatives, default, converter, stringConverter,
-      parent._doc, parent._public, parent._version)
+      parent._doc, parent._public, parent._documentationGroups, parent._version)
     parent._onCreate.foreach(_(entry))
     entry
   }
@@ -187,6 +188,7 @@ private[spark] case class ConfigBuilder(key: String) {
   private[config] var _prependedKey: Option[String] = None
   private[config] var _prependSeparator: String = ""
   private[config] var _public = true
+  private[config] var _documentationGroups = Set.empty[String]
   private[config] var _doc = ""
   private[config] var _version = ""
   private[config] var _onCreate: Option[ConfigEntry[_] => Unit] = None
@@ -194,6 +196,12 @@ private[spark] case class ConfigBuilder(key: String) {
 
   def internal(): ConfigBuilder = {
     _public = false
+    this
+  }
+
+  def withDocumentationGroup(group: String): ConfigBuilder = {
+    require(group.matches("[a-z0-9-]+"))
+    _documentationGroups = _documentationGroups + group
     this
   }
 
@@ -263,7 +271,7 @@ private[spark] case class ConfigBuilder(key: String) {
 
   def fallbackConf[T](fallback: ConfigEntry[T]): ConfigEntry[T] = {
     val entry = new FallbackConfigEntry(key, _prependedKey, _prependSeparator, _alternatives, _doc,
-      _public, _version, fallback)
+      _public, _documentationGroups, _version, fallback)
     _onCreate.foreach(_(entry))
     entry
   }
