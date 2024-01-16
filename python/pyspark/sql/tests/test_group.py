d1ae8a2913a9adfcb90c563b92ca826cfc55df61
Dongjoon Hyun <dhyun@apple.com>
2024-01-16 11:33:13 -0800

    pyarrow too

--- python/pyspark/sql/tests/test_group.py
+++ python/pyspark/sql/tests/test_group.py
@@ -18,12 +18,19 @@ import unittest
 
 from pyspark.sql import Row
 from pyspark.sql import functions as sf
-from pyspark.testing.sqlutils import ReusedSQLTestCase, have_pandas, pandas_requirement_message
+from pyspark.testing.sqlutils import (
+    ReusedSQLTestCase,
+    have_pandas,
+    have_pyarrow,
+    pandas_requirement_message,
+    pyarrow_requirement_message
+)
 from pyspark.testing import assertDataFrameEqual, assertSchemaEqual
 
 
 class GroupTestsMixin:
     @unittest.skipIf(not have_pandas, pandas_requirement_message)  # type: ignore
+    @unittest.skipIf(not have_pyarrow, pyarrow_requirement_message)  # type: ignore
     def test_agg_func(self):
         data = [Row(key=1, value=10), Row(key=1, value=20), Row(key=1, value=30)]
         df = self.spark.createDataFrame(data)
@@ -63,6 +70,7 @@ class GroupTestsMixin:
         self.assertEqual(100, g.agg(functions.countDistinct(df.value)).first()[0])
 
     @unittest.skipIf(not have_pandas, pandas_requirement_message)  # type: ignore
+    @unittest.skipIf(not have_pyarrow, pyarrow_requirement_message)  # type: ignore
     def test_group_by_ordinal(self):
         spark = self.spark
         df = spark.createDataFrame(
@@ -123,6 +131,7 @@ class GroupTestsMixin:
                 df.groupBy(10).agg(sf.sum("b"))
 
     @unittest.skipIf(not have_pandas, pandas_requirement_message)  # type: ignore
+    @unittest.skipIf(not have_pyarrow, pyarrow_requirement_message)  # type: ignore
     def test_order_by_ordinal(self):
         spark = self.spark
         df = spark.createDataFrame(
