116d3c61cf228fd1ad5462f276c28de4723e3552
Enrico Minack <github@enrico.minack.dev>
2024-01-17 14:38:17 +0100

    Add scope attributes to ScopedExpression children

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/namedExpressions.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/namedExpressions.scala
@@ -418,15 +418,20 @@ case class PrettyAttribute(
 /**
  * An expression that has to be resolved against a scope of resolved attributes.
  */
-case class ScopedExpression(child: Expression, scope: Seq[Attribute])
-  extends UnaryExpression with Unevaluable {
-  override def dataType: DataType = child.dataType
-  override def nullable: Boolean = child.nullable
+case class ScopedExpression(expr: Expression, scope: Seq[Attribute])
+  extends Expression with Unevaluable {
+  override def children: Seq[Expression] = expr +: scope
+  override def dataType: DataType = expr.dataType
+  override def nullable: Boolean = expr.nullable
   override def prettyName: String = "scoped"
+  override def sql: String = s"$prettyName(${expr.sql}, $scope)"
+  override lazy val resolved: Boolean = expr.resolved
 
-  override def sql: String = s"$prettyName(${child.sql}, $scope)"
-
-  override def withNewChildInternal(newChild: Expression): ScopedExpression = copy(child = newChild)
+  override protected def withNewChildrenInternal(children: IndexedSeq[Expression]): Expression = {
+    val scope = children.tail
+    assert(scope.forall(_.isInstanceOf[Attribute]), "Scope children have to be attributes")
+    copy(expr = children.head, scope = scope.map(_.asInstanceOf[Attribute]))
+  }
 }
 
 /**
