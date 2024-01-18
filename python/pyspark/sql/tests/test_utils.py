9bfb1ef7ea76824226ae8f5c42c05529bc3e9673
panbingkun <panbingkun@baidu.com>
2024-01-18 13:59:21 +0800

    add test_utils

--- python/pyspark/sql/tests/test_utils.py
+++ python/pyspark/sql/tests/test_utils.py
@@ -45,9 +45,14 @@ from pyspark.sql.types import (
     IntegerType,
     BooleanType,
 )
-from pyspark.testing.sqlutils import have_pandas
+from pyspark.testing.sqlutils import (
+    have_pandas,
+    have_pyarrow,
+    pyarrow_requirement_message,
+)
 
 
+@unittest.skipIf(not have_pyarrow, pyarrow_requirement_message)
 class UtilsTestsMixin:
     def test_assert_equal_inttype(self):
         df1 = self.spark.createDataFrame(
