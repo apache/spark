689ab0ee8ca87286db6167f672348e73116b9186
cashmand <david.cashman@databricks.com>
2024-01-20 10:45:51 +0800

    [SPARK-45827] Disallow partitioning on Variant column
    ### What changes were proposed in this pull request?
    
    Follow-up to https://github.com/apache/spark/pull/43984: we should not allow partitioning on VariantType. Even though it is is an atomic type, it represents a nested semi-structured value, so not partitioning is consistent with our decision to not allow partitioning on nested types. Also, for now at least, it is not even comparable, so attempting to partition fails with a confusing codegen error about a method named `compare` not being declared.
    
    ### Why are the changes needed?
    
    Improves error message when attempting to partition on Variant, and explicitly forbids a case that we do not intend to support.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Improved error message if a user tries to partition on Variant.
    
    ### How was this patch tested?
    
    Added unit test, which fails without the code change.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #44742 from cashmand/SPARK-45827-no-partitioning.
    
    Authored-by: cashmand <david.cashman@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/rules.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/rules.scala
@@ -33,7 +33,7 @@ import org.apache.spark.sql.execution.command.DDLUtils
 import org.apache.spark.sql.execution.datasources.{CreateTable => CreateTableV1}
 import org.apache.spark.sql.execution.datasources.v2.FileDataSourceV2
 import org.apache.spark.sql.sources.InsertableRelation
-import org.apache.spark.sql.types.{AtomicType, StructType}
+import org.apache.spark.sql.types.{AtomicType, StructType, VariantType}
 import org.apache.spark.sql.util.PartitioningUtils.normalizePartitionSpec
 import org.apache.spark.sql.util.SchemaUtils
 import org.apache.spark.util.ArrayImplicits._
@@ -330,7 +330,8 @@ case class PreprocessTableCreation(catalog: SessionCatalog) extends Rule[Logical
     }
 
     schema.filter(f => normalizedPartitionCols.contains(f.name)).map(_.dataType).foreach {
-      case _: AtomicType => // OK
+      // VariantType values are not comparable, so can't be used as partition columns.
+      case a: AtomicType if !a.isInstanceOf[VariantType] => // OK
       case other => failAnalysis(s"Cannot use ${other.catalogString} for partition column")
     }
 
