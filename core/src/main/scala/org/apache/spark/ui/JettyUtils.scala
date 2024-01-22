1ba384c0a7fb7187566e16f9c1802a4444825e2f
HiuFung Kwok <hiufkwok@gmail.com>
2024-01-23 00:18:21 +0800

    Remove connector.setStopTimeout as Jetty rely on graceful shutdown instead

--- core/src/main/scala/org/apache/spark/ui/JettyUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/JettyUtils.scala
@@ -300,7 +300,6 @@ private[spark] object JettyUtils extends Logging {
         connector.setReuseAddress(!Utils.isWindows)
          // spark-45248: set the idle timeout to prevent slow DoS
         connector.setIdleTimeout(8000)
-        connector.setStopTimeout(stopTimeout)
 
         // Currently we only use "SelectChannelConnector"
         // Limit the max acceptor number to 8 so that we don't waste a lot of threads
