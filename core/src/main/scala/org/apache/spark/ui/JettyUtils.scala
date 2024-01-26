744314987ec5fa6d8caba21e19e1cd3fac8a5e19
HiuFung Kwok <hiufkwok@gmail.com>
2024-01-26 16:06:33 +0800

    Update code comment

--- core/src/main/scala/org/apache/spark/ui/JettyUtils.scala
+++ core/src/main/scala/org/apache/spark/ui/JettyUtils.scala
@@ -325,7 +325,8 @@ private[spark] object JettyUtils extends Logging {
       // If SSL is configured, create the secure connector first.
       val securePort = sslOptions.createJettySslContextFactoryServer().map { factory =>
 
-        // spark-45522: SniHostCheck defaulted to true since Jetty 10, this will affect the standalone deployment.
+        // spark-45522: SniHostCheck defaulted to true since Jetty 10,
+        // this will affect the standalone deployment.
         val src = new SecureRequestCustomizer()
         src.setSniHostCheck(false)
         httpConfig.addCustomizer(src)
