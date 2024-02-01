7dd36caeb615e8ba714812250e706df7e6f8b25c
Herman van Hovell <herman@databricks.com>
2024-02-01 16:37:37 -0400

    Wrong version

--- connector/connect/client/jvm/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ connector/connect/client/jvm/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -242,7 +242,7 @@ class Dataset[T] private[sql] (
    * Returns the schema of this Dataset.
    *
    * @group basic
-   * @since 4.0.0
+   * @since 3.4.0
    */
   def schema: StructType = cachedSchema
 
