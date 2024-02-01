02a3f78fe7594af7cb1460818d3a38f07f60f9a2
Serge Rielau <srielau@users.noreply.github.com>
2024-01-31 22:07:02 -0800

    Update sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -1943,7 +1943,7 @@ class Analyzer(override val catalogManager: CatalogManager) extends RuleExecutor
             case s: Star => expand(s, child)
             case o => o :: Nil
           })
-          // count(*) has been replaced by count(1)
+        // count(*) has been replaced by count(1)
         case o if containsStar(o.children) =>
           throw QueryCompilationErrors.invalidStarUsageError(s"expression `${o.prettyName}`",
             extractStar(o.children))
