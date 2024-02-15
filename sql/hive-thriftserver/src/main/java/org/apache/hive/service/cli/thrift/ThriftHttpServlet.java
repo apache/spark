882ee8762ae9ba26a22ea506b412212a0c825b63
HiuFung Kwok <hiufkwok@gmail.com>
2024-02-15 17:53:06 +0800

    Downgrade libThrift

--- sql/hive-thriftserver/src/main/java/org/apache/hive/service/cli/thrift/ThriftHttpServlet.java
+++ sql/hive-thriftserver/src/main/java/org/apache/hive/service/cli/thrift/ThriftHttpServlet.java
@@ -47,7 +47,7 @@ import org.apache.hive.service.auth.HttpAuthenticationException;
 import org.apache.hive.service.auth.PasswdAuthenticationProvider;
 import org.apache.hive.service.cli.HiveSQLException;
 import org.apache.hive.service.cli.session.SessionManager;
-import org.apache.hive.service.cli.TServlet;
+import org.apache.hive.service.cli.thrift.TServlet;
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.ietf.jgss.GSSContext;
