561fb2968c188e002d5333469a9a5c02c08b8ab2
yangjie01 <yangjie01@baidu.com>
2024-02-19 13:38:18 -0800

    [SPARK-37434][TESTS] Disable unsupported `ExtendedLevelDBTest` on `MacOS/aarch64`

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -1515,7 +1515,11 @@ object TestSettings {
   import BuildCommons._
   private val defaultExcludedTags = Seq("org.apache.spark.tags.ChromeUITest",
     "org.apache.spark.deploy.k8s.integrationtest.YuniKornTag",
-    "org.apache.spark.internal.io.cloud.IntegrationTestSuite")
+    "org.apache.spark.internal.io.cloud.IntegrationTestSuite") ++
+    (if (System.getProperty("os.name").startsWith("Mac OS X") &&
+        System.getProperty("os.arch").equals("aarch64")) {
+      Seq("org.apache.spark.tags.ExtendedLevelDBTest")
+    } else Seq.empty)
 
   lazy val settings = Seq (
     // Fork new JVMs for tests and set Java options for those
