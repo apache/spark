d63d187fe348c07c17d0e278cca8504fafcc797f
doki <tunyum94@gmail.com>
2024-02-21 21:45:50 +0800

    revert fmt

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/csv/CSVExprUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/csv/CSVExprUtils.scala
@@ -24,8 +24,8 @@ import org.apache.spark.SparkIllegalArgumentException
 object CSVExprUtils {
 
   /**
-   * Filter ignorable rows for CSV iterator (lines empty and starting with `comment`). This is
-   * currently being used in CSV reading path and CSV schema inference.
+   * Filter ignorable rows for CSV iterator (lines empty and starting with `comment`).
+   * This is currently being used in CSV reading path and CSV schema inference.
    */
   def filterCommentAndEmpty(iter: Iterator[String], options: CSVOptions): Iterator[String] = {
     @inline
@@ -67,8 +67,8 @@ object CSVExprUtils {
   }
 
   /**
-   * Helper method that converts string representation of a character to actual character. It
-   * handles some Java escaped strings and throws exception if given string is longer than one
+   * Helper method that converts string representation of a character to actual character.
+   * It handles some Java escaped strings and throws exception if given string is longer than one
    * character.
    */
   @throws[SparkIllegalArgumentException]
@@ -96,19 +96,25 @@ object CSVExprUtils {
   }
 
   /**
-   * Helper method that converts string representation of a character sequence to actual delimiter
-   * characters. The input is processed in "chunks", and each chunk is converted by calling
-   * [[CSVExprUtils.toChar()]]. A chunk is either: <ul> <li>a backslash followed by another
-   * character</li> <li>a non-backslash character by itself</li> </ul> , in that order of
-   * precedence. The result of the converting all chunks is returned as a [[String]].
+   * Helper method that converts string representation of a character sequence to actual
+   * delimiter characters. The input is processed in "chunks", and each chunk is converted
+   * by calling [[CSVExprUtils.toChar()]].  A chunk is either:
+   * <ul>
+   *   <li>a backslash followed by another character</li>
+   *   <li>a non-backslash character by itself</li>
+   * </ul>
+   * , in that order of precedence. The result of the converting all chunks is returned as
+   * a [[String]].
    *
-   * <br/><br/>Examples: <ul><li>`\t` will result in a single tab character as the separator (same
-   * as before) </li><li>`|||` will result in a sequence of three pipe characters as the separator
+   * <br/><br/>Examples:
+   * <ul><li>`\t` will result in a single tab character as the separator (same as before)
+   * </li><li>`|||` will result in a sequence of three pipe characters as the separator
    * </li><li>`\\` will result in a single backslash as the separator (same as before)
    * </li><li>`\.` will result in an error (since a dot is not a character that needs escaped)
    * </li><li>`\\.` will result in a backslash, then dot, as the separator character sequence
    * </li><li>`.\t.` will result in a dot, then tab, then dot as the separator character sequence
-   * </li> </ul>
+   * </li>
+   * </ul>
    *
    * @param str the string representing the sequence of separator characters
    * @return a [[String]] representing the multi-character delimiter
