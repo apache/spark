6185e5cad7bea662365e15e667cc7b2548ce8de0
Albert Ziegler <55346556+wunderalbert@users.noreply.github.com>
2024-02-22 11:14:29 -0800

    [SPARK-47132][DOCS][PYTHON] Correct docstring for pyspark's dataframe.head
    ### What changes were proposed in this pull request?
    
    Change description of `returns` in docstring for pyspark's dataframe's `head` to match actual behaviour.
    
    ### Why are the changes needed?
    
    The docstring claimed that `head(n)` would return a `Row` (rather than a list of rows) iff n == 1, but that's incorrect.
    
    Type hints, example, and implementation show that the difference between row or list of rows lies in whether n is supplied at all -- if it isn't, `head()` returns a `Row`, if it is, even if it is 1, `head(n)` returns a list.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No, only a Python docstring is changed.
    
    ### How was this patch tested?
    
    No tests were added since only docs changed.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #45197 from wunderalbert/patch-1.
    
    Authored-by: Albert Ziegler <55346556+wunderalbert@users.noreply.github.com>
    Signed-off-by: Xinrong Meng <xinrong@apache.org>

--- python/pyspark/sql/dataframe.py
+++ python/pyspark/sql/dataframe.py
@@ -3526,8 +3526,9 @@ class DataFrame(PandasMapOpsMixin, PandasConversionMixin):
 
         Returns
         -------
-        If n is greater than 1, return a list of :class:`Row`.
-        If n is 1, return a single Row.
+        If n is supplied, return a list of :class:`Row` of length n
+        or less if the DataFrame has fewer elements.
+        If n is missing, return a single Row.
 
         Examples
         --------
@@ -3537,6 +3538,8 @@ class DataFrame(PandasMapOpsMixin, PandasConversionMixin):
         Row(age=2, name='Alice')
         >>> df.head(1)
         [Row(age=2, name='Alice')]
+        >>> df.head(0)
+        []
         """
         if n is None:
             rs = self.head(1)
