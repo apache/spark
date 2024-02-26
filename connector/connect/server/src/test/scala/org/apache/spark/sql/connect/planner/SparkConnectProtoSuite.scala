790701ab732b65d7c499bf94b3203c741087d9ce
Nikola Mandic <nikola.mandic@databricks.com>
2024-02-26 17:36:15 +0100

    Fix

--- connector/connect/server/src/test/scala/org/apache/spark/sql/connect/planner/SparkConnectProtoSuite.scala
+++ connector/connect/server/src/test/scala/org/apache/spark/sql/connect/planner/SparkConnectProtoSuite.scala
@@ -1048,7 +1048,7 @@ class SparkConnectProtoSuite extends PlanTest with SparkConnectPlanTest {
     Seq("UCS_BASIC", "UCS_BASIC_LCASE", "UNICODE", "UNICODE_CI").map(collationName =>
       Seq(
         s"select 'abc' collate '$collationName'",
-        s"select collated('abc' collate '$collationName')"
+        s"select collation('abc' collate '$collationName')"
       ).map(query => comparePlans(connect.sql(query), spark.sql(query)))
     )
   }
