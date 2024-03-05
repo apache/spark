7f03ef47d86cf465031d0c7cd13625f6edb7a295
Hyukjin Kwon <gurwls223@apache.org>
2024-03-05 13:49:15 +0900

    fix up

--- core/src/test/scala/org/apache/spark/deploy/master/ResourceProfilesSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/master/ResourceProfilesSuite.scala
@@ -32,9 +32,9 @@ class ResourceProfilesSuite extends MasterSuiteBase {
   }
 
   test("resource description with multiple resource profiles") {
-    val appInfo = makeAppInfo(1024, Some(4), None, Map(GPU -> 2))
+    val appInfo = makeAppInfo(128, Some(4), None, Map(GPU -> 2))
     val rp1 = DeployTestUtils.createResourceProfile(None, Map(FPGA -> 2), None)
-    val rp2 = DeployTestUtils.createResourceProfile(Some(2048), Map(GPU -> 3, FPGA -> 3), Some(2))
+    val rp2 = DeployTestUtils.createResourceProfile(Some(256), Map(GPU -> 3, FPGA -> 3), Some(2))
 
     val resourceProfileToTotalExecs = Map(
       appInfo.desc.defaultProfile -> 1,
@@ -45,18 +45,18 @@ class ResourceProfilesSuite extends MasterSuiteBase {
 
     // Default resource profile take it's own resource request.
     var resourceDesc = appInfo.getResourceDescriptionForRpId(DEFAULT_RESOURCE_PROFILE_ID)
-    assert(resourceDesc.memoryMbPerExecutor === 1024)
+    assert(resourceDesc.memoryMbPerExecutor === 128)
     assert(resourceDesc.coresPerExecutor === Some(4))
     assert(resourceDesc.customResourcesPerExecutor === Seq(ResourceRequirement(GPU, 2)))
 
     // Non-default resource profiles take cores and memory from default profile if not specified.
     resourceDesc = appInfo.getResourceDescriptionForRpId(rp1.id)
-    assert(resourceDesc.memoryMbPerExecutor === 1024)
+    assert(resourceDesc.memoryMbPerExecutor === 128)
     assert(resourceDesc.coresPerExecutor === Some(4))
     assert(resourceDesc.customResourcesPerExecutor === Seq(ResourceRequirement(FPGA, 2)))
 
     resourceDesc = appInfo.getResourceDescriptionForRpId(rp2.id)
-    assert(resourceDesc.memoryMbPerExecutor === 2048)
+    assert(resourceDesc.memoryMbPerExecutor === 256)
     assert(resourceDesc.coresPerExecutor === Some(2))
     assert(resourceDesc.customResourcesPerExecutor ===
       Seq(ResourceRequirement(FPGA, 3), ResourceRequirement(GPU, 3)))
