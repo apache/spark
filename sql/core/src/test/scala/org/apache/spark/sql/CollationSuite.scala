8ec9efde0040da93254092b1f200168f489df01f
Stefan Kandic <stefan.kandic@databricks.com>
2024-03-08 21:10:56 +0100

    improve test for collator concurrency

--- sql/core/src/test/scala/org/apache/spark/sql/CollationSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationSuite.scala
@@ -18,6 +18,7 @@
 package org.apache.spark.sql
 
 import scala.collection.immutable.Seq
+import scala.collection.parallel.CollectionConverters.ImmutableIterableIsParallelizable
 import scala.jdk.CollectionConverters.MapHasAsJava
 
 import org.apache.spark.SparkException
@@ -438,37 +439,17 @@ class CollationSuite extends DatasourceV2SQLBase with AdaptiveSparkPlanHelper {
     }
   }
 
-  test("test concurrently running aggregates") {
+  test("test concurrently generating collation keys") {
     // generating ICU sort keys is not thread-safe by default so this should fail
     // if we don't handle the concurrency properly on Collator level
 
-    for (_ <- 1 to 100) {
-      Seq(
-        ("ucs_basic", Seq("AAA", "aaa"), Seq(Row(1, "AAA"), Row(1, "aaa"))),
-        ("ucs_basic", Seq("aaa", "aaa"), Seq(Row(2, "aaa"))),
-        ("ucs_basic", Seq("aaa", "bbb"), Seq(Row(1, "aaa"), Row(1, "bbb"))),
-        ("ucs_basic_lcase", Seq("aaa", "aaa"), Seq(Row(2, "aaa"))),
-        ("ucs_basic_lcase", Seq("AAA", "aaa"), Seq(Row(2, "AAA"))),
-        ("ucs_basic_lcase", Seq("aaa", "bbb"), Seq(Row(1, "aaa"), Row(1, "bbb"))),
-        ("unicode", Seq("AAA", "aaa"), Seq(Row(1, "AAA"), Row(1, "aaa"))),
-        ("unicode", Seq("aaa", "aaa"), Seq(Row(2, "aaa"))),
-        ("unicode", Seq("aaa", "bbb"), Seq(Row(1, "aaa"), Row(1, "bbb"))),
-        ("unicode_CI", Seq("aaa", "aaa"), Seq(Row(2, "aaa"))),
-        ("unicode_CI", Seq("AAA", "aaa"), Seq(Row(2, "AAA"))),
-        ("unicode_CI", Seq("aaa", "bbb"), Seq(Row(1, "aaa"), Row(1, "bbb")))
-      ).foreach {
-        case (collationName: String, input: Seq[String], expected: Seq[Row]) =>
-          checkAnswer(sql(
-            s"""
-            with t as (
-            select collate(col1, '$collationName') as c
-            from
-            values ${input.map(s => s"('$s')").mkString(", ")}
-          )
-          SELECT COUNT(*), c FROM t GROUP BY c
-          """), expected)
+    (0 to 10).foreach(_ => {
+      val collator = CollationFactory.fetchCollation("UNICODE").collator
+
+      (0 to 100).par.foreach { _ =>
+        collator.getCollationKey("aaa")
       }
-    }
+    })
   }
 
   test("text writing to parquet with collation enclosed with backticks") {
