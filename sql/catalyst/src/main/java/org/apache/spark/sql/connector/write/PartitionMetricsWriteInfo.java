9f46308121841774a7973962ba27fc013b341d28
Steve Vaughan Jr <s_vaughan@apple.com>
2024-03-14 09:59:44 -0400

    Switch PartitionMetrics to scala

--- sql/catalyst/src/main/java/org/apache/spark/sql/connector/write/PartitionMetricsWriteInfo.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/connector/write/PartitionMetricsWriteInfo.java
@@ -9,12 +9,17 @@ public class PartitionMetricsWriteInfo {
 
   public void merge (PartitionMetricsWriteInfo otherAccumulator) {
     otherAccumulator.metrics.forEach((p, m) ->
-        metrics.computeIfAbsent(p, key -> new PartitionMetrics())
+        metrics.computeIfAbsent(p, key -> new PartitionMetrics(0L, 0L, 0))
             .merge(m));
   }
 
+  public void update(String partitionPath, long bytes, long records, int files) {
+    metrics.computeIfAbsent(partitionPath, key -> new PartitionMetrics(0L, 0L, 0))
+        .merge(new PartitionMetrics(bytes, records, files));
+  }
+
   public void updateFile(String partitionPath, long bytes, long records) {
-    metrics.computeIfAbsent(partitionPath, key -> new PartitionMetrics())
+    metrics.computeIfAbsent(partitionPath, key -> new PartitionMetrics(0L, 0L, 0))
         .updateFile(bytes, records);
   }
 
