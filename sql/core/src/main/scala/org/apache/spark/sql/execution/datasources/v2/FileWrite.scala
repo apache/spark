7a9a1e0692e06cf53821c0e746c7cebe5715f42a
Steve Vaughan Jr <s_vaughan@apple.com>
2024-03-15 17:57:18 -0400

    Fix import

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/FileWrite.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/FileWrite.scala
@@ -18,8 +18,6 @@ package org.apache.spark.sql.execution.datasources.v2
 
 import java.util.UUID
 
-import scala.collection.JavaConverters._
-import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 
 import org.apache.hadoop.conf.Configuration
