c08ab6c027f3eaf4e6240d92c6d946909d0e570c
Stevo Mitric <stevo.mitric@databricks.com>
2024-03-19 09:35:21 +0100

    Updated 'select.show' to give more info in map_sort desc

--- python/pyspark/sql/functions/builtin.py
+++ python/pyspark/sql/functions/builtin.py
@@ -16878,7 +16878,7 @@ def map_sort(col: "ColumnOrName", asc: bool = True) -> Column:
 
     >>> import pyspark.sql.functions as sf
     >>> df = spark.sql("SELECT map(3, 'c', 1, 'a', 2, 'b') as data")
-    >>> df.select(sf.map_sort(df.data, False)).show()
+    >>> df.select(sf.map_sort(df.data, False)).show(truncate=False)
     +---------------------+
     |map_sort(data, false)|
     +---------------------+
