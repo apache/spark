8d4003a75398682a8ddd80d8f8e68b8e4fc7f0d3
jiale_tan <jiale_tan@apple.com>
2024-03-20 00:42:21 -0700

    re-trigger test after enabling action in forked repo

--- core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
+++ core/src/test/scala/org/apache/spark/deploy/SparkSubmitSuite.scala
@@ -512,7 +512,7 @@ class SparkSubmitSuite
       "--master", "k8s://host:port",
       "--executor-memory", "1g",
       "--class", "org.SomeClass",
-      "--driver-memory", "4g",
+      "--driver-memory", "2g",
       "--conf", "spark.kubernetes.submitInDriver=true",
       "--jars", "src/test/resources/TestUDTF.jar",
       "/home/jarToIgnore.jar",
