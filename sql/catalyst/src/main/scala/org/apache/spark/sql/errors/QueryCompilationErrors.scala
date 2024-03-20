00d72924c1c9010100db7f806460fe5c3f8f15d7
huangxiaoping <1754789345@qq.com>
2024-03-20 14:52:00 +0800

    [SPARK-47479][SQL] Optimize cannot write data to relations with multiple paths error log

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryCompilationErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryCompilationErrors.scala
@@ -1709,10 +1709,11 @@ private[sql] object QueryCompilationErrors extends QueryErrorsBase with Compilat
         "partColumns" -> targetPartitionSchema.fields.map(_.name).mkString("[", ",", "]")))
   }
 
-  def cannotWriteDataToRelationsWithMultiplePathsError(): Throwable = {
+  def cannotWriteDataToRelationsWithMultiplePathsError(paths: Seq[Path]): Throwable = {
     new AnalysisException(
       errorClass = "_LEGACY_ERROR_TEMP_1148",
-      messageParameters = Map.empty)
+      messageParameters = Map(
+        "paths" -> paths.mkString("[", ",", "]")))
   }
 
   def failedToRebuildExpressionError(filter: Filter): Throwable = {
