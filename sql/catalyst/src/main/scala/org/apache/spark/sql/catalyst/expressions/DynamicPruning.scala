e39d07fc056ce5a8e0499a3580f24f687044a245
Jack Chen <jack.chen@databricks.com>
2024-03-22 15:46:35 -0400

    add a comment

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/DynamicPruning.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/DynamicPruning.scala
@@ -61,6 +61,8 @@ case class DynamicPruningSubquery(
   override def withNewPlan(plan: LogicalPlan): DynamicPruningSubquery = copy(buildQuery = plan)
 
   override def withNewOuterAttrs(outerAttrs: Seq[Expression]): DynamicPruningSubquery = {
+    // Updating outer attrs of DynamicPruningSubquery is unsupported; assert that they match
+    // pruningKey and return a copy without any changes.
     assert(outerAttrs.size == 1 && outerAttrs.head.semanticEquals(pruningKey))
     copy()
   }
