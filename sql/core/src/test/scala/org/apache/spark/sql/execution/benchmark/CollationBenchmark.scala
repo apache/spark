cfb44f839be75ba8bd9a1b5bfc86bcaa9020d404
GideonPotok <g.potok4@gmail.com>
2024-03-24 14:51:45 -0400

    withSuggestions

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/CollationBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/CollationBenchmark.scala
@@ -36,13 +36,29 @@ import org.apache.spark.unsafe.types.UTF8String
  */
 
 object CollationBenchmark extends SqlBasedBenchmark {
-  private val collationTypes = Seq("UTF8_BINARY", "UTF8_BINARY_LCASE", "UNICODE", "UNICODE_CI")
+  private val collationTypes = Seq("UTF8_BINARY_LCASE", "UNICODE", "UTF8_BINARY", "UNICODE_CI")
 
-  def generateUTF8Strings(n: Int): Seq[UTF8String] = {
-    // Generate n UTF8Strings
-    Seq("ABC", "aBC", "abc", "DEF", "def", "GHI", "ghi", "JKL", "jkl",
-      "MNO", "mno", "PQR", "pqr", "STU", "stu", "VWX", "vwx", "YZ").map(UTF8String.fromString) ++
-    (18 to n).map(i => UTF8String.fromString(i.toOctalString))
+  def generateSeqInput(n: Long): Seq[UTF8String] = {
+    val input = Seq("ABC", "ABC", "aBC", "aBC", "abc", "abc", "DEF", "DEF", "def",
+      "def", "GHI", "ghi",
+      "JKL", "jkl", "MNO", "mno", "PQR", "pqr", "STU", "stu", "VWX", "vwx", "YZ",
+      "ABC", "ABC", "aBC", "aBC", "abc", "abc", "DEF", "DEF", "def", "def", "GHI", "ghi",
+      "JKL", "jkl", "MNO", "mno", "PQR", "pqr", "STU", "stu", "VWX", "vwx", "YZ")
+      .map(UTF8String.fromString)
+    val inputLong: Seq[UTF8String] = (0L until n).map(i => input(i.toInt % input.size))
+    inputLong
+  }
+
+  private def getDataFrame(strings: Seq[String]): DataFrame = {
+    val asPairs = strings.sliding(2, 1).toSeq.map {
+      case Seq(s1, s2) => (s1, s2)
+    }
+    val d = spark.createDataFrame(asPairs).toDF("s1", "s2")
+    d
+  }
+
+  private def generateDataframeInput(l: Long): DataFrame = {
+    getDataFrame(generateSeqInput(l).map(_.toString))
   }
 
   def benchmarkUTFString(collationTypes: Seq[String], utf8Strings: Seq[UTF8String]): Unit = {
@@ -77,36 +93,26 @@ object CollationBenchmark extends SqlBasedBenchmark {
     benchmark.run()
   }
 
-  def df1: DataFrame = {
-    val d = spark.createDataFrame(Seq(
-      ("ABC", "ABC"), ("aBC", "abc"), ("abc", "ABC"), ("DEF", "DEF"), ("def", "DEF"),
-      ("GHI", "GHI"), ("ghi", "IG"), ("JKL", "NOP"), ("jkl", "LKJ"), ("mnO", "MNO"),
-      ("hello", "hola"))
-    ).toDF("s1", "s2")
-    d
-  }
-
-  def collationBenchmarkFilterEqual(collationTypes: Seq[String]): Unit = {
+  def benchmarkFilterEqual(collationTypes: Seq[String],
+                           dfUncollated: DataFrame): Unit = {
     val benchmark = collationTypes.foldLeft(
-      new Benchmark(s"filter df column with collation", 11, output = output)) {
+      new Benchmark(s"filter df column with collation", dfUncollated.count(), output = output)) {
       (b, collationType) =>
-        b.addCase(s"filter df column with collation - $collationType") { _ =>
-          val df = df1.selectExpr(
+          val dfCollated = dfUncollated.selectExpr(
             s"collate(s2, '$collationType') as k2_$collationType",
             s"collate(s1, '$collationType') as k1_$collationType")
-
-          df.where(col(s"k1_$collationType") === col(s"k2_$collationType"))
+        b.addCase(s"filter df column with collation - $collationType") { _ =>
+          dfCollated.where(col(s"k1_$collationType") === col(s"k2_$collationType"))
                 .queryExecution.executedPlan.executeCollect()
-
         }
         b
     }
     benchmark.run()
   }
 
-
   override def runBenchmarkSuite(mainArgs: Array[String]): Unit = {
-    collationBenchmarkFilterEqual(collationTypes.reverse)
-    benchmarkUTFString(collationTypes, generateUTF8Strings(200))
+    benchmarkFilterEqual(collationTypes, generateDataframeInput(10000L))
+    benchmarkUTFString(collationTypes, generateSeqInput(10000L))
   }
+
 }
