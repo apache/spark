4a1f2419e243272064fde96529149316fe53bc10
Chenhao Li <chenhao.li@databricks.com>
2024-03-25 14:58:01 +0800

    [SPARK-47451][SQL] Support to_json(variant).
    ### What changes were proposed in this pull request?
    
    This PR adds the functionality to format a variant value as a JSON string. It is exposed in the `to_json` expression by allowing the variant type (or a nested type containing the variant type) as its input.
    
    ### How was this patch tested?
    
    Unit tests that validate the `to_json` result. The input includes both `parse_json` results and manually constructed bytes.
    Negative cases with malformed inputs are also covered.
    
    Some tests disabled in https://github.com/apache/spark/pull/45479 are re-enabled.
    
    Closes #45575 from chenhao-db/variant_to_json.
    
    Authored-by: Chenhao Li <chenhao.li@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- common/variant/src/main/java/org/apache/spark/types/variant/Variant.java
+++ common/variant/src/main/java/org/apache/spark/types/variant/Variant.java
@@ -17,6 +17,14 @@
 
 package org.apache.spark.types.variant;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import java.io.CharArrayWriter;
+import java.io.IOException;
+
+import static org.apache.spark.types.variant.VariantUtil.*;
+
 /**
  * This class is structurally equivalent to {@link org.apache.spark.unsafe.types.VariantVal}. We
  * define a new class to avoid depending on or modifying Spark.
@@ -28,6 +36,15 @@ public final class Variant {
   public Variant(byte[] value, byte[] metadata) {
     this.value = value;
     this.metadata = metadata;
+    // There is currently only one allowed version.
+    if (metadata.length < 1 || (metadata[0] & VERSION_MASK) != VERSION) {
+      throw malformedVariant();
+    }
+    // Don't attempt to use a Variant larger than 16 MiB. We'll never produce one, and it risks
+    // memory instability.
+    if (metadata.length > SIZE_LIMIT || value.length > SIZE_LIMIT) {
+      throw variantConstructorSizeLimit();
+    }
   }
 
   public byte[] getValue() {
@@ -36,5 +53,79 @@ public final class Variant {
 
   public byte[] getMetadata() {
     return metadata;
+  }
+
+  // Stringify the variant in JSON format.
+  // Throw `MALFORMED_VARIANT` if the variant is malformed.
+  public String toJson() {
+    StringBuilder sb = new StringBuilder();
+    toJsonImpl(value, metadata, 0, sb);
+    return sb.toString();
+  }
+
+  // Escape a string so that it can be pasted into JSON structure.
+  // For example, if `str` only contains a new-line character, then the result content is "\n"
+  // (4 characters).
+  static String escapeJson(String str) {
+    try (CharArrayWriter writer = new CharArrayWriter();
+         JsonGenerator gen = new JsonFactory().createGenerator(writer)) {
+      gen.writeString(str);
+      gen.flush();
+      return writer.toString();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static void toJsonImpl(byte[] value, byte[] metadata, int pos, StringBuilder sb) {
+    switch (VariantUtil.getType(value, pos)) {
+      case OBJECT:
+        handleObject(value, pos, (size, idSize, offsetSize, idStart, offsetStart, dataStart) -> {
+          sb.append('{');
+          for (int i = 0; i < size; ++i) {
+            int id = readUnsigned(value, idStart + idSize * i, idSize);
+            int offset = readUnsigned(value, offsetStart + offsetSize * i, offsetSize);
+            int elementPos = dataStart + offset;
+            if (i != 0) sb.append(',');
+            sb.append(escapeJson(getMetadataKey(metadata, id)));
+            sb.append(':');
+            toJsonImpl(value, metadata, elementPos, sb);
+          }
+          sb.append('}');
+          return null;
+        });
+        break;
+      case ARRAY:
+        handleArray(value, pos, (size, offsetSize, offsetStart, dataStart) -> {
+          sb.append('[');
+          for (int i = 0; i < size; ++i) {
+            int offset = readUnsigned(value, offsetStart + offsetSize * i, offsetSize);
+            int elementPos = dataStart + offset;
+            if (i != 0) sb.append(',');
+            toJsonImpl(value, metadata, elementPos, sb);
+          }
+          sb.append(']');
+          return null;
+        });
+        break;
+      case NULL:
+        sb.append("null");
+        break;
+      case BOOLEAN:
+        sb.append(VariantUtil.getBoolean(value, pos));
+        break;
+      case LONG:
+        sb.append(VariantUtil.getLong(value, pos));
+        break;
+      case STRING:
+        sb.append(escapeJson(VariantUtil.getString(value, pos)));
+        break;
+      case DOUBLE:
+        sb.append(VariantUtil.getDouble(value, pos));
+        break;
+      case DECIMAL:
+        sb.append(VariantUtil.getDecimal(value, pos).toPlainString());
+        break;
+    }
   }
 }
