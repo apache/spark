dae2aeed56354ae81f54b7db804c2515ba69a080
Wenchen Fan <wenchen@databricks.com>
2024-03-26 20:10:19 +0800

    always throw FAILED_READ_FILE error when fail to read files

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/binaryfile/BinaryFileFormatSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/binaryfile/BinaryFileFormatSuite.scala
@@ -368,7 +368,7 @@ class BinaryFileFormatSuite extends QueryTest with SharedSparkSession {
           checkAnswer(readContent(), expected)
         }
       }
-      assert(caught.getErrorClass == "FAILED_READ_FILE")
+      assert(caught.getErrorClass.startsWith("FAILED_READ_FILE"))
       assert(caught.getCause.getMessage.contains("exceeds the max length allowed"))
     }
   }
