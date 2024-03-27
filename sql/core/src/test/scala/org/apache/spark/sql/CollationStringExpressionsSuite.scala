d4677004d9630753e7f323b5da948df1dd2e8b64
GideonPotok <g.potok4@gmail.com>
2024-03-27 10:29:37 -0400

    init commit

--- sql/core/src/test/scala/org/apache/spark/sql/CollationStringExpressionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationStringExpressionsSuite.scala
@@ -18,13 +18,13 @@
 package org.apache.spark.sql
 
 import scala.collection.immutable.Seq
-
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.catalyst.ExtendedAnalysisException
+import org.apache.spark.sql.catalyst.dsl.expressions.DslExpression
 import org.apache.spark.sql.catalyst.expressions.{Collation, ExpressionEvalHelper, Literal, StringRepeat}
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.SharedSparkSession
-import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.{IntegerType, StringType}
 
 class CollationStringExpressionsSuite extends QueryTest
   with SharedSparkSession with ExpressionEvalHelper {
@@ -85,7 +85,51 @@ class CollationStringExpressionsSuite extends QueryTest
     testRepeat("UNICODE", 2, "abc", 2)
     testRepeat("UNICODE_CI", 3, "abc", 2)
   }
+/*
+Enable collation support for the Substring built-in string function in Spark (including Right and Left functions). First confirm what is the expected behaviour for these functions when given collated strings, then move on to the implementation that would enable handling strings of all collation types. Implement the corresponding unit tests (CollationStringExpressionsSuite) and E2E tests (CollationSuite) to reflect how this function should be used with collation in SparkSQL, and feel free to use your chosen Spark SQL Editor to experiment with the existing functions to learn more about how they work. In addition, look into the possible use-cases and implementation of similar functions within other other open-source DBMS, such as PostgreSQL.
 
+The goal for this Jira ticket is to implement the Substring, Right, and Left functions so that they support all collation types currently supported in Spark. To understand what changes were introduced in order to enable full collation support for other existing functions in Spark, take a look at the Spark PRs and Jira tickets for completed tasks in this parent (for example: Contains, StartsWith, EndsWith).
+todo ===== POSSIBLE THREAD LEAK IN SUITE o.a.s.sql.CollationStringExpressionsSuite, threads: rpc-boss-3-1 (daemon=true), shuffle-boss-6-1 (daemon=true) =====
+
+Read more about ICU Collation Concepts and Collator class. Also, refer to the Unicode Technical Standard for collation.
+ */
+  test("substring check output type on explicitly collated string") {
+    val checks = Seq(
+      CollationTestCase("Spark", "2", "UTF8_BINARY", "park")
+    )
+    checks.foreach(ct => {
+      checkAnswer(sql(s"SELECT substr(collate('${ct.s1}', '${ct.collation}'), 2)"),
+        Row(ct.expectedResult))
+    })
+    def testSubstring(
+        expected: String,
+        collationId: Int,
+        input: String,
+        pos: Int,
+        len: Int): Unit = {
+      val s = Literal.create(input, StringType(collationId))
+      val l = Literal.create(pos, IntegerType)
+
+      checkEvaluation(Collation(s.substring(s, l)).replacement, expected)
+    }
+
+    testSubstring("UTF8_BINARY", 0, "abc", 1, 2)
+
+    def testSubstringValue(
+        expected: String,
+        collationId: Int,
+        input: String,
+        pos: Int,
+        len: Int): Unit = {
+      val s = Literal.create(input, StringType(collationId))
+      val l = Literal.create(pos, IntegerType)
+//      checkResult(
+//        s.substring(s, l).,
+//        expected)
+
+    }
+    testSubstringValue("bc", 0, "abc", 2, 2)
+  }
   // TODO: Add more tests for other string expressions
 
 }
