b19b0ebc68dc48ec435bdf5bdd89f3d7a408a493
Mihailo Milosevic <mihailo.milosevic@databricks.com>
2024-03-29 09:08:13 +0100

    Fix collated tests

--- sql/core/src/test/scala/org/apache/spark/sql/CollationStringExpressionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationStringExpressionsSuite.scala
@@ -68,7 +68,7 @@ class CollationStringExpressionsSuite
           messageParameters = Map(
             "paramIndex" -> "first",
             "requiredType" -> """"STRING"""",
-            "inputSql" -> """" """",
+            "inputSql" -> """"' '"""",
             "inputType" -> s""""STRING COLLATE ${ct.collation}""""
           )
         )
