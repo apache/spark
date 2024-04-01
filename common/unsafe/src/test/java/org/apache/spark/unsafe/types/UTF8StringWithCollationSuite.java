1bfb0276b5b646cf15665d5141cab4509adfb2e7
Milan Dankovic <milan.dankovic@databricks.com>
2024-04-01 12:30:59 +0200

    Fix Java linter error

--- common/unsafe/src/test/java/org/apache/spark/unsafe/types/UTF8StringWithCollationSuite.java
+++ common/unsafe/src/test/java/org/apache/spark/unsafe/types/UTF8StringWithCollationSuite.java
@@ -19,8 +19,6 @@ package org.apache.spark.unsafe.types;
 import org.apache.spark.SparkException;
 import org.apache.spark.sql.catalyst.util.CollationFactory;
 import org.junit.jupiter.api.Test;
-
-import static org.apache.spark.unsafe.types.UTF8String.fromString;
 import static org.junit.jupiter.api.Assertions.*;
 
 
