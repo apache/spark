43cedc0b3deabc388896ff71ae0312b34b638f4b
Bobby Wang <wbo4958@gmail.com>
2024-04-02 09:11:23 +0900

    [SPARK-46812][CONNECT][PYTHON] Make mapInPandas / mapInArrow support ResourceProfile
    ### What changes were proposed in this pull request?
    
    Support stage-level scheduling for PySpark connect DataFrame APIs (mapInPandas and mapInArrow).
    
    ### Why are the changes needed?
    
    https://github.com/apache/spark/pull/44852 has supported ResourceProfile in mapInPandas/mapInArrow for SQL, So it's the right time to enable it for connect.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, Users can pass ResourceProfile to mapInPandas/mapInArrow through the connect pyspark client.
    
    ### How was this patch tested?
    
    Pass the CIs and manual tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No
    
    Closes #45232 from wbo4958/connect-rp.
    
    Authored-by: Bobby Wang <wbo4958@gmail.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -570,6 +570,7 @@ pyspark_resource = Module(
         "pyspark.resource.profile",
         # unittests
         "pyspark.resource.tests.test_resources",
+        "pyspark.resource.tests.test_connect_resources",
     ],
 )
 
@@ -1058,6 +1059,7 @@ pyspark_connect = Module(
         "pyspark.sql.tests.connect.test_parity_pandas_udf_scalar",
         "pyspark.sql.tests.connect.test_parity_pandas_udf_grouped_agg",
         "pyspark.sql.tests.connect.test_parity_pandas_udf_window",
+        "pyspark.sql.tests.connect.test_resources",
     ],
     excluded_python_implementations=[
         "PyPy"  # Skip these tests under PyPy since they require numpy, pandas, and pyarrow and
