15dcb5d129e362aa376082809dea2bffe9ed73fd
Hyukjin Kwon <gurwls223@apache.org>
2024-04-04 12:13:50 +0900

    Set up the CI for pyspark-connect package

--- python/pyspark/sql/connect/group.py
+++ python/pyspark/sql/connect/group.py
@@ -448,6 +448,7 @@ PandasCogroupedOps.__doc__ = PySparkPandasCogroupedOps.__doc__
 
 
 def _test() -> None:
+    import os
     import sys
     import doctest
     from pyspark.sql import SparkSession as PySparkSession
@@ -456,7 +457,9 @@ def _test() -> None:
     globs = pyspark.sql.connect.group.__dict__.copy()
 
     globs["spark"] = (
-        PySparkSession.builder.appName("sql.connect.group tests").remote("local[4]").getOrCreate()
+        PySparkSession.builder.appName("sql.connect.group tests")
+        .remote(os.environ.get("SPARK_CONNECT_TESTING_REMOTE", "local[4]"))
+        .getOrCreate()
     )
 
     (failure_count, test_count) = doctest.testmod(
