2972bb4ccf2159f5663fa397e7d3b6d3b1af599e
Mihailo Milosevic <mihailo.milosevic@databricks.com>
2024-04-08 08:34:07 +0200

    Refactor code

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/StringTypeCollated.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/StringTypeCollated.scala
@@ -24,13 +24,13 @@ import org.apache.spark.sql.types.{AbstractDataType, DataType, StringType}
  */
 abstract class StringTypeCollated extends AbstractDataType {
   override private[sql] def defaultConcreteType: DataType = StringType
+  override private[sql] def simpleString: String = "string"
 }
 
 /**
  * Use StringTypeBinary for expressions supporting only binary collation.
  */
 case object StringTypeBinary extends StringTypeCollated {
-  override private[sql] def simpleString: String = "string"
   override private[sql] def acceptsType(other: DataType): Boolean =
     other.isInstanceOf[StringType] && other.asInstanceOf[StringType].supportsBinaryEquality
 }
@@ -39,7 +39,6 @@ case object StringTypeBinary extends StringTypeCollated {
  * Use StringTypeBinaryLcase for expressions supporting only binary and lowercase collation.
  */
 case object StringTypeBinaryLcase extends StringTypeCollated {
-  override private[sql] def simpleString: String = "string"
   override private[sql] def acceptsType(other: DataType): Boolean =
     other.isInstanceOf[StringType] && (other.asInstanceOf[StringType].supportsBinaryEquality ||
       other.asInstanceOf[StringType].isUTF8BinaryLcaseCollation)
@@ -49,6 +48,5 @@ case object StringTypeBinaryLcase extends StringTypeCollated {
  * Use StringTypeAnyCollation for expressions supporting all possible collation types.
  */
 case object StringTypeAnyCollation extends StringTypeCollated {
-  override private[sql] def simpleString: String = "string"
   override private[sql] def acceptsType(other: DataType): Boolean = other.isInstanceOf[StringType]
 }
