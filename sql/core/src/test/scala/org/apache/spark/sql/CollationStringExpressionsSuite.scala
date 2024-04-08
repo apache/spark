369dc7925a2dd0ff040243a2a43b4f0ccc6d3141
GideonPotok <g.potok4@gmail.com>
2024-04-08 07:10:33 +0300

    changes

--- sql/core/src/test/scala/org/apache/spark/sql/CollationStringExpressionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationStringExpressionsSuite.scala
@@ -22,19 +22,15 @@ import org.apache.spark.sql.catalyst.analysis.TypeCheckResult.DataTypeMismatch
 import org.apache.spark.sql.catalyst.dsl.expressions.DslExpression
 import org.apache.spark.sql.catalyst.expressions.{Collation, ConcatWs, ExpressionEvalHelper, Literal, StringRepeat}
 import org.apache.spark.sql.catalyst.util.CollationFactory
-import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.internal.{SqlApiConf, SQLConf}
 import org.apache.spark.sql.test.SharedSparkSession
-import org.apache.spark.sql.types.{IntegerType, StringType}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField}
 
 class CollationStringExpressionsSuite
   extends QueryTest
   with SharedSparkSession
   with ExpressionEvalHelper {
 
-  case class UnaryCollationTestCase[R](
-      expression1: String,
-      expectedResult: R,
-      expectedCollation: String)
   case class CollationTestCase[R](s1: String, s2: String, collation: String, expectedResult: R)
   case class CollationTestFail[R](s1: String, s2: String, collation: String)
 
@@ -115,20 +111,127 @@ class CollationStringExpressionsSuite
     testSubstring("UTF8_BINARY", 0, "abc", 1)
   }
 
-  test("left/right check output type on explicitly collated string") {
-    val checks = Seq(
-      UnaryCollationTestCase("left(collate('Spark', 'UTF8_BINARY'), 2)", "Sp", "UTF8_BINARY"),
-      UnaryCollationTestCase("right(collate('Spark', 'UTF8_BINARY'), 2)", "rk", "UTF8_BINARY"),
-      UnaryCollationTestCase(
-        "right(collate('Spark', 'UTF8_BINARY_LCASE'), 2)",
-        "rk",
-        "UTF8_BINARY_LCASE")
-    )
-    checks.foreach(ct => {
-      checkAnswer(sql(s"SELECT ${ct.expression1}"),
-        Row(ct.expectedResult))
-    })
+  test("left/right/substr on collated proper string returns proper value") {
+    Seq("utf8_binary_lcase", "utf8_binary", "unicode", "unicode_ci").foreach { collationName =>
+      checkAnswerAndType(sql(s"select left(collate('Spark', $collationName), 2))"), Row("Sp"),
+        StringType(CollationFactory.collationNameToId(collationName)))
+      checkAnswer(sql(s"select right(collate('Spark', $collationName), 2))"), Row("rk"))
+      checkAnswer(sql(s"select right('Def' collate $collationName, 1)"), Row("f"))
+      checkAnswer(sql(s"select substr('abc' collate $collationName, 2)"), Row("bc"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 0, 2)"), Row("ex"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 1, 2)"), Row("ex"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 0, 7)"), Row("example"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 1, 7)"), Row("example"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 0, 100)"), Row("example"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 1, 100)"), Row("example"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 2, 2)"), Row("xa"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 1, 6)"), Row("exampl"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 2, 100)"), Row("xample"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 0, 0)"), Row(""))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 100, 4)"), Row(""))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 0, 100)"), Row("example"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 1, 100)"), Row("example"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, 2, 100)"), Row("xample"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, -3, 2)"), Row("pl"))
+      checkAnswer(sql(s"select substr('example' collate $collationName, -100, 4)"), Row(""))
+      checkAnswer(sql(s"select substr('example' collate $collationName, -2147483648, 6)"), Row(""))
+      checkAnswer(sql(s"select substr(' a世a ' collate $collationName, 2, 3)"), Row("a世a")) // scalastyle:ignore
+      checkAnswer(sql(s"select left(' a世a ' collate $collationName, 3)"), Row(" a世")) // scalastyle:ignore
+      checkAnswer(sql(s"select right(' a世a ' collate $collationName, 3)"), Row("世a ")) // scalastyle:ignore
+      checkAnswer(sql(s"select substr('AaAaAaAa000000' collate $collationName, 2, 3)"), Row("aAa"))
+      checkAnswer(sql(s"select left('AaAaAaAa000000' collate $collationName, 3)"), Row("AaA"))
+      checkAnswer(sql(s"select right('AaAaAaAa000000' collate $collationName, 3)"), Row("000"))
+      checkAnswer(sql(s"select substr('' collate $collationName, 1, 1)"), Row(""))
+      checkAnswer(sql(s"select left('' collate $collationName, 1)"), Row(""))
+      checkAnswer(sql(s"select right('' collate $collationName, 1)"), Row(""))
+      checkAnswer(sql(s"select left('ghi' collate $collationName, 1)"), Row("g"))
+    }
   }
+
+  private def checkAnswerAndType(
+      frame: DataFrame,
+      row: Row,
+      stringType: StringType) = {
+    checkAnswer(frame, row)
+    assert(frame.schema(0).dataType == stringType)
+  }
+
+  test("left/right/substr on collated improper string returns proper value") {
+    Seq("utf8_binary_lcase", "utf8_binary", "unicode", "unicode_ci").foreach { collationName =>
+      checkAnswer(sql(s"select right(null collate $collationName, 1)"), Row(null))
+      checkAnswer(sql(s"select substr(null collate $collationName, 1)"), Row(null))
+      checkAnswer(sql(s"select left(null collate $collationName, 1)"), Row(null))
+    }
+  }
+
+  test("left/right/substr on collated improper length and position returns proper value") {
+    Seq("utf8_binary_lcase", "utf8_binary", "unicode", "unicode_ci").foreach { collationName =>
+      checkAnswer(sql(s"select left(' a世a ' collate $collationName, '3')"), Row(" a世")) // scalastyle:ignore
+      checkAnswer(sql(s"select right(' a世a ' collate $collationName, '3')"), Row("世a ")) // scalastyle:ignore
+      checkAnswer(sql(s"select right('' collate $collationName, null)"), Row(null))
+      checkAnswer(sql(s"select substr('' collate $collationName, null)"), Row(null))
+      checkAnswer(sql(s"select substr('' collate $collationName, null, null)"), Row(null))
+      checkAnswer(sql(s"select left('' collate $collationName, null)"), Row(null))
+    }
+  }
+
+  test("left/right/substr on session-collated string returns proper type") {
+    Seq("utf8_binary_lcase", "utf8_binary", "unicode", "unicode_ci").foreach { collationName =>
+      val collationId = CollationFactory.collationNameToId(collationName)
+      withSQLConf(SqlApiConf.DEFAULT_COLLATION -> collationName) {
+        assert(sql("select left('abc', 1)").schema(0).dataType == StringType(collationId))
+        assert(sql("select right('def', 1)").schema(0).dataType == StringType(collationId))
+        assert(sql("select substr('ghi', 1)").schema(0).dataType == StringType(collationId))
+      }
+    }
+  }
+
+  test("left/right/substr on struct fields that are collated strings") {
+    Seq(None, Some("utf8_binary_lcase"), Some("utf8_binary"), Some("unicode"), Some("unicode_ci"))
+      .foreach { collationNameMaybe =>
+        withTable("t") {
+          sql("CREATE TABLE t(i STRING, s" +
+            s" struct<a: string${collationNameMaybe.map(cn => " collate " + cn).getOrElse("")}>)" +
+            s" USING parquet")
+          (1 to 5).map(n => "a" + " " * n).foreach { v =>
+            sql(s"INSERT OVERWRITE t VALUES ('1', named_struct('a', '$v'))")
+          }
+          assert(sql(s"SELECT i, left(s.a, 1) FROM t").schema(1).dataType ==
+            collationNameMaybe.map(cn =>
+              StringType(CollationFactory.collationNameToId(cn))).getOrElse(StringType))
+          assert(sql(s"SELECT i, right(s.a, 1) FROM t").schema(1).dataType ==
+            collationNameMaybe.map(cn =>
+              StringType(CollationFactory.collationNameToId(cn))).getOrElse(StringType))
+          assert(sql(s"SELECT i, substr(s.a, 1, 0) FROM t").schema(1).dataType ==
+            collationNameMaybe.map(cn =>
+              StringType(CollationFactory.collationNameToId(cn))).getOrElse(StringType))
+        }
+      }
+  }
+
+  test("left/right/substr on collated improper string returns proper type") {
+    Seq("utf8_binary_lcase", "utf8_binary", "unicode", "unicode_ci").foreach { collationName =>
+      val collationId = CollationFactory.collationNameToId(collationName)
+      assert(sql(s"select left(null collate $collationName, 1)").schema(0).dataType
+        == StringType(collationId))
+      assert(sql(s"select right(null collate $collationName, 1)").schema(0).dataType
+        == StringType(collationId))
+      assert(sql(s"select substr(null collate $collationName, 1)").schema(0).dataType
+        == StringType(collationId))
+    }
+  }
+  test("left/right/substr on collated proper string returns proper type") {
+    Seq("utf8_binary_lcase", "utf8_binary", "unicode", "unicode_ci").foreach { collationName =>
+      val collationId = CollationFactory.collationNameToId(collationName)
+      assert(sql(s"select left('hij' collate $collationName, 1)").schema(0).dataType
+        == StringType(collationId))
+      assert(sql(s"select right('klm' collate $collationName, 1)").schema(0).dataType
+        == StringType(collationId))
+      assert(sql(s"select substr('nop' collate $collationName, 1)").schema(0).dataType
+        == StringType(collationId))
+    }
+  }
+
   // TODO: Add more tests for other string expressions
 
 }
