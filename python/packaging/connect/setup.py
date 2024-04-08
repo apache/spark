04bf981781ba79d4b2d5a493ea32935eaa177709
Haejoon Lee <haejoon.lee@databricks.com>
2024-04-08 09:44:49 -0700

    [SPARK-47737][PYTHON] Bump PyArrow to 10.0.0
    ### What changes were proposed in this pull request?
    
    This PR proposes to bump PyArrow version up to 10.0.0
    
    ### Why are the changes needed?
    
    To leverage the new features from the latest version.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No API changes, but the PyArrow version from user-facing documentation will be changed.
    
    ### How was this patch tested?
    
    The existing CI should pass.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #45892 from itholic/bump_arrow_10.
    
    Authored-by: Haejoon Lee <haejoon.lee@databricks.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>

--- python/packaging/connect/setup.py
+++ python/packaging/connect/setup.py
@@ -91,7 +91,7 @@ try:
     # python/packaging/classic/setup.py
     _minimum_pandas_version = "1.4.4"
     _minimum_numpy_version = "1.21"
-    _minimum_pyarrow_version = "4.0.0"
+    _minimum_pyarrow_version = "10.0.0"
     _minimum_grpc_version = "1.59.3"
     _minimum_googleapis_common_protos_version = "1.56.4"
 
