aeb5a7cfb764a6243f616b4b6f44a5bec944da1e
Haejoon Lee <haejoon.lee@databricks.com>
2024-04-09 09:14:23 +0900

    revert test changes

--- common/utils/src/main/scala/org/apache/spark/internal/LogKey.scala
+++ common/utils/src/main/scala/org/apache/spark/internal/LogKey.scala
@@ -99,7 +99,6 @@ object LogKey extends Enumeration {
   val SHUFFLE_MERGE_ID = Value
   val SIZE = Value
   val SLEEP_TIME = Value
-  val SPARK_VERSION = Value
   val STAGE_ID = Value
   val STATEMENT_ID = Value
   val SUBMISSION_ID = Value
