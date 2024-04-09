b57c259c2cffcd91d12b96272e7a70f02d9de793
Gene Pang <gene.pang@databricks.com>
2024-04-08 19:03:19 -0700

    fix docs

--- python/pyspark/sql/__init__.py
+++ python/pyspark/sql/__init__.py
@@ -39,7 +39,7 @@ Important classes of Spark SQL and DataFrames:
     - :class:`pyspark.sql.Window`
       For working with window functions.
 """
-from pyspark.sql.types import Row
+from pyspark.sql.types import Row, VariantVal
 from pyspark.sql.context import SQLContext, HiveContext, UDFRegistration, UDTFRegistration
 from pyspark.sql.session import SparkSession
 from pyspark.sql.column import Column
