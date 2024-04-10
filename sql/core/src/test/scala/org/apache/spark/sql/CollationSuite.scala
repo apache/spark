b577e1e9ce429d653e22ab574089d2b583aa3e5d
GideonPotok <g.potok4@gmail.com>
2024-04-10 19:31:24 +0300

    hello

--- sql/core/src/test/scala/org/apache/spark/sql/CollationSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationSuite.scala
@@ -43,20 +43,20 @@ class CollationSuite extends DatasourceV2SQLBase with AdaptiveSparkPlanHelper {
   test("left/right/substr on struct fields that are collated strings") {
     Seq(None, Some("utf8_binary_lcase"), Some("utf8_binary"), Some("unicode"), Some("unicode_ci"))
       .foreach { collationNameMaybe =>
-        withTable("t") {
-          sql("CREATE TABLE t(i STRING, s" +
+        withTable("t1234") {
+          sql("CREATE TABLE t1234(i STRING, s" +
             s" struct<a: string${collationNameMaybe.map(cn => " collate " + cn).getOrElse("")}>)" +
             s" USING parquet")
           (1 to 5).map(n => "a" + " " * n).foreach { v =>
-            sql(s"INSERT OVERWRITE t VALUES ('1', named_struct('a', '$v'))")
+            sql(s"INSERT OVERWRITE t1234 VALUES ('1', named_struct('a', '$v'))")
           }
-          assert(sql(s"SELECT i, left(s.a, 1) FROM t").schema(1).dataType ==
+          assert(sql(s"SELECT i, left(s.a, 1) FROM t1234").schema(1).dataType ==
             collationNameMaybe.map(cn =>
               StringType(CollationFactory.collationNameToId(cn))).getOrElse(StringType))
-          assert(sql(s"SELECT i, right(s.a, 1) FROM t").schema(1).dataType ==
+          assert(sql(s"SELECT i, right(s.a, 1) FROM t1234").schema(1).dataType ==
             collationNameMaybe.map(cn =>
               StringType(CollationFactory.collationNameToId(cn))).getOrElse(StringType))
-          assert(sql(s"SELECT i, substr(s.a, 1, 0) FROM t").schema(1).dataType ==
+          assert(sql(s"SELECT i, substr(s.a, 1, 0) FROM t1234").schema(1).dataType ==
             collationNameMaybe.map(cn =>
               StringType(CollationFactory.collationNameToId(cn))).getOrElse(StringType))
         }
