21703733b83ad0094a45694cfee1d20058f68f9e
milastdbx <milan.stefanovic@databricks.com>
2024-04-11 14:41:14 +0200

    typo issue

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AbstractSqlParser.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/AbstractSqlParser.scala
@@ -16,7 +16,7 @@
  */
 package org.apache.spark.sql.catalyst.parser
 
-import org.antlr.v4.runtime.ParserRulCeontext
+import org.antlr.v4.runtime.ParserRuleContext
 
 import org.apache.spark.sql.catalyst.{FunctionIdentifier, TableIdentifier}
 import org.apache.spark.sql.catalyst.expressions.Expression
