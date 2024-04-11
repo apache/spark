dedddf510af925ca1b1e0e0dc92e196bae3187b4
Wenchen Fan <wenchen@databricks.com>
2024-04-11 19:18:54 +0800

    [SPARK-47809][SQL] `checkExceptionInExpression` should check error for each codegen mode
    ### What changes were proposed in this pull request?
    
    There is a bug in the test util `checkExceptionInExpression`. It may fail to catch bugs when codegen and non-codegen have different error behaviors. This PR fixes it by checking the error for each codegen mode.
    
    This PR also fixes a small error wrapping issue in `SubExprEvaluationRuntime` (used by interpreted common subexpression elimination).
    
    ### Why are the changes needed?
    
    bug fix for test utils
    
    ### Does this PR introduce _any_ user-facing change?
    
    no
    
    ### How was this patch tested?
    
    N/A
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #45997 from cloud-fan/test-bug.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Kent Yao <yao@apache.org>

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/ExpressionEvalHelper.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/ExpressionEvalHelper.scala
@@ -170,18 +170,15 @@ trait ExpressionEvalHelper extends ScalaCheckDrivenPropertyChecks with PlanTestB
     def checkException(eval: => Unit, testMode: String): Unit = {
       val modes = Seq(CodegenObjectFactoryMode.CODEGEN_ONLY, CodegenObjectFactoryMode.NO_CODEGEN)
       withClue(s"($testMode)") {
-        val e = intercept[T] {
-          for (fallbackMode <- modes) {
-            withSQLConf(SQLConf.CODEGEN_FACTORY_MODE.key -> fallbackMode.toString) {
-              eval
-            }
+        for (fallbackMode <- modes) {
+          withSQLConf(SQLConf.CODEGEN_FACTORY_MODE.key -> fallbackMode.toString) {
+            checkError(
+              exception = intercept[T](eval),
+              errorClass = errorClass,
+              parameters = parameters
+            )
           }
         }
-        checkError(
-          exception = e,
-          errorClass = errorClass,
-          parameters = parameters
-        )
       }
     }
 
@@ -209,23 +206,21 @@ trait ExpressionEvalHelper extends ScalaCheckDrivenPropertyChecks with PlanTestB
     def checkException(eval: => Unit, testMode: String): Unit = {
       val modes = Seq(CodegenObjectFactoryMode.CODEGEN_ONLY, CodegenObjectFactoryMode.NO_CODEGEN)
       withClue(s"($testMode)") {
-        val errMsg = intercept[T] {
-          for (fallbackMode <- modes) {
-            withSQLConf(SQLConf.CODEGEN_FACTORY_MODE.key -> fallbackMode.toString) {
-              eval
+        for (fallbackMode <- modes) {
+          withSQLConf(SQLConf.CODEGEN_FACTORY_MODE.key -> fallbackMode.toString) {
+            val errMsg = intercept[T](eval).getMessage
+            if (errMsg == null) {
+              if (expectedErrMsg != null) {
+                fail(s"Expected `$expectedErrMsg` but null error message found")
+              }
+            } else if (expectedErrMsg == null) {
+              if (errMsg != null) {
+                fail(s"Expected null error message, but `$errMsg` found")
+              }
+            } else if (!errMsg.contains(expectedErrMsg)) {
+              fail(s"Expected error message is `$expectedErrMsg`, but `$errMsg` found")
             }
           }
-        }.getMessage
-        if (errMsg == null) {
-          if (expectedErrMsg != null) {
-            fail(s"Expected `$expectedErrMsg` but null error message found")
-          }
-        } else if (expectedErrMsg == null) {
-          if (errMsg != null) {
-            fail(s"Expected null error message, but `$errMsg` found")
-          }
-        } else if (!errMsg.contains(expectedErrMsg)) {
-          fail(s"Expected error message is `$expectedErrMsg`, but `$errMsg` found")
         }
       }
     }
