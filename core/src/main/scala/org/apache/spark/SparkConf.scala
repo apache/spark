6eda3cb22511520f17c9120917ec8926db0aedc2
Cheng Pan <chengpan@apache.org>
2024-04-15 11:16:37 +0800

    [SPARK-47847][CORE] Deprecate spark.network.remoteReadNioBufferConversion

--- core/src/main/scala/org/apache/spark/SparkConf.scala
+++ core/src/main/scala/org/apache/spark/SparkConf.scala
@@ -640,7 +640,8 @@ private[spark] object SparkConf extends Logging {
       DeprecatedConfig("spark.blacklist.killBlacklistedExecutors", "3.1.0",
         "Please use spark.excludeOnFailure.killExcludedExecutors"),
       DeprecatedConfig("spark.yarn.blacklist.executor.launch.blacklisting.enabled", "3.1.0",
-        "Please use spark.yarn.executor.launch.excludeOnFailure.enabled")
+        "Please use spark.yarn.executor.launch.excludeOnFailure.enabled"),
+      DeprecatedConfig("spark.network.remoteReadNioBufferConversion", "3.5.2", "")
     )
 
     Map(configs.map { cfg => (cfg.key -> cfg) } : _*)
