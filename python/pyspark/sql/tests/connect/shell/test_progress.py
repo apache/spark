5fc58fff3be4e000040265d0442a54cd14b79659
Martin Grund <martin.grund@databricks.com>
2024-04-16 05:42:44 +0200

    fix

--- python/pyspark/sql/tests/connect/shell/test_progress.py
+++ python/pyspark/sql/tests/connect/shell/test_progress.py
@@ -126,17 +126,15 @@ class SparkConnectProgressHandlerE2E(SparkConnectSQLTestCase):
             self.connect.clearProgressHandlers()
 
     def test_progress_properly_recorded(self):
-        state = {"counter": 0, "tasks": 0}
+        state = {"counter": 0}
 
         def handler(stages, inflight_tasks, operation_id, done):
             state["counter"] += 1
-            state["tasks"] = sum([x.num_tasks for x in stages])
 
         try:
             self.connect.registerProgressHandler(handler)
             self.connect.range(10000).repartition(20).count()
             self.assertGreaterEqual(state["counter"], 1, "Handler should be called at least once.")
-            self.assertGreaterEqual(state["tasks"], 1, "Total tasks should be grater than 1")
         finally:
             self.connect.clearProgressHandlers()
 
