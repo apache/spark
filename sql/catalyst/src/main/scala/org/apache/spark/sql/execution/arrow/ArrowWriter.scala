ae93be7793244993dbaf0b0e67f92a9a05c74333
Harsh Motwani <harsh.motwani@databricks.com>
2024-04-16 09:26:55 -0700

    Removed unnecessary changes

--- sql/catalyst/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala
@@ -385,15 +385,6 @@ private[arrow] class StructWriter(
     valueVector.setNull(count)
   }
 
-    var i = 0
-    while (i < children.length) {
-      children(i).setNull()
-      children(i).count += 1
-      i += 1
-    }
-    valueVector.setNull(count)
-  }
-
   override def setValue(input: SpecializedGetters, ordinal: Int): Unit = {
     if (isVariant) {
       valueVector.setIndexDefined(count)
