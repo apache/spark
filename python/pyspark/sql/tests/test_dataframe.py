a5eabf3603db5302712e640957fd09d274422c80
Haejoon Lee <haejoon.lee@databricks.com>
2024-04-17 11:26:29 +0900

    clean testing

--- python/pyspark/sql/tests/test_dataframe.py
+++ python/pyspark/sql/tests/test_dataframe.py
@@ -1289,9 +1289,6 @@ class DataFrameTestsMixin:
 
 
 class DataFrameTests(DataFrameTestsMixin, ReusedSQLTestCase):
-    def test_dataframe_error_context(self):
-        super().test_dataframe_error_context()
-
     pass
 
 
