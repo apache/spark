74c8476b2e2c110b9724054a9f142a55042701ea
Harsh Motwani <harsh.motwani@databricks.com>
2024-04-18 13:39:32 -0700

    Removed unnecessary changes

--- sql/core/src/test/scala/org/apache/spark/sql/VariantEndToEndSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/VariantEndToEndSuite.scala
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.spark.sql
 
 import org.apache.spark.sql.catalyst.expressions.{CreateArray, CreateNamedStruct, Literal, StructsToJson}
