50af3fac58bafa2ae35062030b3391406a46ead8
Vladimir Golubev <vladimir.golubev@databricks.com>
2024-04-24 09:31:04 +0200

    Add tests for parameterized describe and explain

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -4715,6 +4715,145 @@ class SQLQuerySuite extends QueryTest with SharedSparkSession with AdaptiveSpark
     val df6 = df3.join(df2, col("df3.zaak_id") === col("df2.customer_id"), "outer")
     df5.crossJoin(df6)
   }
+
+  test("SPARK-47939: Describe should work with parameterized queries") {
+    checkAnswer(
+      spark.sql("describe select ?", Array(1)),
+      Array(
+        Row("1", "int", null)
+      )
+    )
+    checkAnswer(
+      spark.sql("describe select :first", Map("first" -> 1)),
+      Array(
+        Row("1", "int", null)
+      )
+    )
+
+    checkAnswer(
+      spark.sql("describe select * from values (?, ?) t(x, y)", Array(1, "a")),
+      Array(
+        Row("x", "int", null),
+        Row("y", "string", null)
+      )
+    )
+    checkAnswer(
+      spark.sql(
+        "describe select * from values (:first, :second) t(x, y)",
+        Map("first" -> 1, "second" -> "a")
+      ),
+      Array(
+        Row("x", "int", null),
+        Row("y", "string", null)
+      )
+    )
+  }
+
+  test("SPARK-47939: Explain should work with parameterized queries") {
+    def checkQueryPlan(df: DataFrame, plan: String): Unit = assert(
+      df.collect()
+        .map(_.getString(0))
+        .map(_.replaceAll("#[0-9]+", "#N"))
+        .sameElements(Array(plan))
+    )
+
+    checkQueryPlan(
+      spark.sql("explain select ?", Array(1)),
+      """== Physical Plan ==
+*(1) Project [1 AS 1#N]
++- *(1) Scan OneRowRelation[]
+
+"""
+    )
+    checkQueryPlan(
+      spark.sql("explain select :first", Map("first" -> 1)),
+      """== Physical Plan ==
+*(1) Project [1 AS 1#N]
++- *(1) Scan OneRowRelation[]
+
+"""
+    )
+
+    checkQueryPlan(
+      spark.sql("explain explain explain select ?", Array(1)),
+      """== Physical Plan ==
+Execute ExplainCommand
+   +- ExplainCommand ExplainCommand 'PosParameterizedQuery [1], SimpleMode, SimpleMode
+
+"""
+    )
+    checkQueryPlan(
+      spark.sql("explain explain explain select :first", Map("first" -> 1)),
+      """== Physical Plan ==
+Execute ExplainCommand
+   +- ExplainCommand ExplainCommand 'NameParameterizedQuery [first], [1], SimpleMode, SimpleMode
+
+"""
+    )
+
+    checkQueryPlan(
+      spark.sql("explain describe select ?", Array(1)),
+      """== Physical Plan ==
+Execute DescribeQueryCommand
+   +- DescribeQueryCommand select ?
+
+"""
+    )
+    checkQueryPlan(
+      spark.sql("explain describe select :first", Map("first" -> 1)),
+      """== Physical Plan ==
+Execute DescribeQueryCommand
+   +- DescribeQueryCommand select :first
+
+"""
+    )
+
+    checkQueryPlan(
+      spark.sql("explain extended select * from values (?, ?) t(x, y)", Array(1, "a")),
+      """== Parsed Logical Plan ==
+'PosParameterizedQuery [1, a]
++- 'Project [*]
+   +- 'SubqueryAlias t
+      +- 'UnresolvedInlineTable [x, y], [[posparameter(39), posparameter(42)]]
+
+== Analyzed Logical Plan ==
+x: int, y: string
+Project [x#N, y#N]
++- SubqueryAlias t
+   +- LocalRelation [x#N, y#N]
+
+== Optimized Logical Plan ==
+LocalRelation [x#N, y#N]
+
+== Physical Plan ==
+LocalTableScan [x#N, y#N]
+"""
+    )
+    checkQueryPlan(
+      spark.sql(
+        "explain extended select * from values (:first, :second) t(x, y)",
+        Map("first" -> 1, "second" -> "a")
+      ),
+      """== Parsed Logical Plan ==
+'NameParameterizedQuery [first, second], [1, a]
++- 'Project [*]
+   +- 'SubqueryAlias t
+      +- 'UnresolvedInlineTable [x, y], [[namedparameter(first), namedparameter(second)]]
+
+== Analyzed Logical Plan ==
+x: int, y: string
+Project [x#N, y#N]
++- SubqueryAlias t
+   +- LocalRelation [x#N, y#N]
+
+== Optimized Logical Plan ==
+LocalRelation [x#N, y#N]
+
+== Physical Plan ==
+LocalTableScan [x#N, y#N]
+"""
+    )
+  }
 }
 
 case class Foo(bar: Option[String])
