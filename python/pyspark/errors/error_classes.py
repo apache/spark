c0cb503d8ae2cb18a7ec5aadd1dacae5959712e6
Nicholas Chammas <nicholas.chammas@gmail.com>
2024-04-30 11:14:40 -0400

    migrate away from deprecated importlib method

--- python/pyspark/errors/error_classes.py
+++ python/pyspark/errors/error_classes.py
@@ -22,8 +22,10 @@ import importlib.resources
 #   hence why the name of the JSON file different.
 #   For more information, please see: https://issues.apache.org/jira/browse/SPARK-46810
 #   This discrepancy will be resolved as part of: https://issues.apache.org/jira/browse/SPARK-47429
-# Note: When we drop support for Python 3.8, we should migrate from importlib.resources.read_text()
-#   to importlib.resources.files().joinpath().read_text().
-#   See: https://docs.python.org/3/library/importlib.resources.html#importlib.resources.open_text
-ERROR_CLASSES_JSON = importlib.resources.read_text("pyspark.errors", "error-conditions.json")
+ERROR_CLASSES_JSON = (
+    importlib.resources
+    .files("pyspark.errors")
+    .joinpath("error-conditions.json")
+    .read_text()
+)
 ERROR_CLASSES_MAP = json.loads(ERROR_CLASSES_JSON)
