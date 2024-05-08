07ad804350fa0479ba63f9c8c89759bcbd1a44a0
Kent Yao <yao@apache.org>
2024-05-08 19:06:57 +0800

    Revert "[SPARK-48185][SQL] Fix 'symbolic reference class is not accessible: class sun.util.calendar.ZoneInfo'"
    This reverts commit 6b5a1aebcfc2fe2f5495cb222be6116130ebc7d7.

--- sql/api/src/main/scala/org/apache/spark/sql/catalyst/util/SparkDateTimeUtils.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/catalyst/util/SparkDateTimeUtils.scala
@@ -197,8 +197,8 @@ trait SparkDateTimeUtils {
     rebaseJulianToGregorianDays(julianDays)
   }
 
-  private lazy val zoneInfoClassName = "sun.util.calendar.ZoneInfo"
-  private lazy val getOffsetsByWallHandle = {
+  private val zoneInfoClassName = "sun.util.calendar.ZoneInfo"
+  private val getOffsetsByWallHandle = {
     val lookup = MethodHandles.lookup()
     val classType = SparkClassUtils.classForName(zoneInfoClassName)
     val methodName = "getOffsetsByWall"
