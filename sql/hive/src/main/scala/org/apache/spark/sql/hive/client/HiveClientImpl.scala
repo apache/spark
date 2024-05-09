88bcd90f33e0eb0b82c9cee642d5589d79141a1f
Dongjoon Hyun <dongjoon@apache.org>
2024-05-09 10:42:27 -0700

    Update HiveClientImpl.scala

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/client/HiveClientImpl.scala
@@ -1358,7 +1358,7 @@ private[hive] object HiveClientImpl extends Logging {
     try {
       Hive.getWithoutRegisterFns(hiveConf)
     } catch {
-      // SPARK-37069: not all Hive versions have the above method (e.g., Hive 2.3.10 has it but
+      // SPARK-37069: not all Hive versions have the above method (e.g., Hive 2.3.9 has it but
       // 2.3.8 don't), therefore here we fallback when encountering the exception.
       case _: NoSuchMethodError =>
         Hive.get(hiveConf)
