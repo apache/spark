e4da7b687b2d46076d8743b95948a791a7915f3b
Nicholas Chammas <nicholas.chammas@gmail.com>
2024-05-13 13:52:14 -0400

    Revert "Mypy says this ignore is unused"
    This reverts commit 3ccde0637a7daad63a45fadfeac3772f5524742b.

--- python/pyspark/sql/pandas/conversion.py
+++ python/pyspark/sql/pandas/conversion.py
@@ -487,7 +487,7 @@ class SparkConversionMixin:
                                 else:
                                     return (
                                         pd.Timestamp(value)
-                                        .tz_localize(timezone, ambiguous=False)
+                                        .tz_localize(timezone, ambiguous=False)  # type: ignore
                                         .tz_convert(_get_local_timezone())
                                         .tz_localize(None)
                                         .to_pydatetime()
