192f2f7afa1fa0f011510e5e10eb7de442cbaf26
subham611 <subhamsinghal@sharechat.co>
2024-05-13 11:28:35 +0530

    Fix UT

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -3832,6 +3832,28 @@ class Dataset[T] private[sql](
    * @group typedrel
    * @since 1.6.0
    */
+  def coalesce(numPartitions: Int):
+  Dataset[T] = withTypedPlan {
+    Repartition(numPartitions, shuffle = false, logicalPlan, None)
+  }
+
+  /**
+   * Returns a new Dataset that has exactly `numPartitions` partitions, when the fewer partitions
+   * are requested. If a larger number of partitions is requested, it will stay at the current
+   * number of partitions. Similar to coalesce defined on an `RDD`, this operation results in
+   * a narrow dependency, e.g. if you go from 1000 partitions to 100 partitions, there will not
+   * be a shuffle, instead each of the 100 new partitions will claim 10 of the current partitions.
+   *
+   * However, if you're doing a drastic coalesce, e.g. to numPartitions = 1,
+   * this may result in your computation taking place on fewer nodes than
+   * you like (e.g. one node in the case of numPartitions = 1). To avoid this,
+   * you can call repartition. This will add a shuffle step, but means the
+   * current upstream partitions will be executed in parallel (per whatever
+   * the current partitioning is).
+   *
+   * @group typedrel
+   * @since 1.6.0
+   */
   def coalesce(numPartitions: Int,
                userDefinedCoalescer: Option[PartitionCoalescer] = None):
   Dataset[T] = withTypedPlan {
