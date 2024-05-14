24c46511a41ad93f3e92cf636c13a821f3d991b7
panbingkun <panbingkun@baidu.com>
2024-05-14 14:49:19 +0800

    fix

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -275,7 +275,7 @@ object SparkBuild extends PomBuild {
       // See https://storage-download.googleapis.com/maven-central/index.html for more info.
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
       DefaultMavenRepository) ++
-      { if skipLocalM2 Nil else Seq(Resolver.mavenLocal) } :+
+      { if (skipLocalM2) Nil else Seq(Resolver.mavenLocal) } :+
       Resolver.file("ivyLocal", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns),
     externalResolvers := resolvers.value,
     otherResolvers := SbtPomKeys.mvnLocalRepository(dotM2 => Seq(Resolver.file("dotM2", dotM2))).value,
