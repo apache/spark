0e7156d2d80171876c7a5e674349c53ee013be38
Mihailo Milosevic <mihailo.milosevic@databricks.com>
2024-05-15 22:15:52 +0800

    [SPARK-48172][SQL] Fix escaping issues in JDBCDialects
    This PR is a fix of https://github.com/apache/spark/pull/46437. The previous PR was reverted as `LONGTEXT` is not supported by all dialects.
    
    Special case escaping for MySQL and fix issues with redundant escaping for ' character.
    New changes introduced in the fix include change `LONGTEXT` -> `VARCHAR(50)`, as well as fix for table naming in the tests.
    
    When pushing down startsWith, endsWith and contains they are converted to LIKE. This requires addition of escape characters for these expressions. Unfortunately, MySQL uses ESCAPE '\' syntax instead of ESCAPE '' which would cause errors when trying to push down.
    
    Yes
    
    Tests for each existing dialect.
    
    No.
    
    Closes #46588 from mihailom-db/SPARK-48172.
    
    Authored-by: Mihailo Milosevic <mihailo.milosevic@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit 9e386b472981979e368a5921c58da5bfefe3acfe)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- connector/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/OracleIntegrationSuite.scala
+++ connector/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/OracleIntegrationSuite.scala
@@ -83,6 +83,12 @@ class OracleIntegrationSuite extends DockerJDBCIntegrationV2Suite with V2JDBCTes
     connection.prepareStatement(
       "CREATE TABLE employee (dept NUMBER(32), name VARCHAR2(32), salary NUMBER(20, 2)," +
         " bonus BINARY_DOUBLE)").executeUpdate()
+    connection.prepareStatement(
+      s"""CREATE TABLE pattern_testing_table (
+         |pattern_testing_col VARCHAR(50)
+         |)
+                   """.stripMargin
+    ).executeUpdate()
   }
 
   override def testUpdateColumnType(tbl: String): Unit = {
