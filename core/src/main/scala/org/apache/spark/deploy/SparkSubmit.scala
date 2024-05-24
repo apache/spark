1bba82fb33aae6770a393be220691dff40a68f92
Gengliang Wang <gengliang@apache.org>
2024-05-24 10:00:25 -0700

    revert changes in SparkSubmit.scala

--- core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
+++ core/src/main/scala/org/apache/spark/deploy/SparkSubmit.scala
@@ -135,13 +135,13 @@ private[spark] class SparkSubmit extends Logging {
 
   /** Print version information to the log. */
   private def printVersion(): Unit = {
-    logInfo(log"""Welcome to
+    logInfo("""Welcome to
       ____              __
      / __/__  ___ _____/ /__
     _\ \/ _ \/ _ `/ __/  '_/
-   /___/ .__/\_,_/_/ /_/\_\   version ${MDC(LogKeys.SPARK_VERSION, SPARK_VERSION)}
+   /___/ .__/\_,_/_/ /_/\_\   version %s
       /_/
-      """)
+                        """.format(SPARK_VERSION))
     logInfo(log"Using Scala ${MDC(LogKeys.SCALA_VERSION, Properties.versionString)}," +
       log" ${MDC(LogKeys.JAVA_VM_NAME, Properties.javaVmName)}," +
       log" ${MDC(LogKeys.JAVA_VERSION, Properties.javaVersion)}")
