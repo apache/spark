3e70e6d559e53db918f9c37365b7b2f15c12e5ce
Uros Bojanic <157381213+uros-db@users.noreply.github.com>
2024-05-24 09:05:59 +0200

    Undo unnecessary changes

--- common/unsafe/src/main/java/org/apache/spark/sql/catalyst/util/CollationSupport.java
+++ common/unsafe/src/main/java/org/apache/spark/sql/catalyst/util/CollationSupport.java
@@ -280,6 +280,7 @@ public final class CollationSupport {
         return execICU(v, collationId);
       }
     }
+
     public static String genCode(final String v, final int collationId) {
       CollationFactory.Collation collation = CollationFactory.fetchCollation(collationId);
       String expr = "CollationSupport.InitCap.exec";
@@ -289,9 +290,11 @@ public final class CollationSupport {
         return String.format(expr + "ICU(%s, %d)", v, collationId);
       }
     }
+
     public static UTF8String execUTF8(final UTF8String v) {
       return v.toLowerCase().toTitleCase();
     }
+
     public static UTF8String execICU(final UTF8String v, final int collationId) {
       return UTF8String.fromString(
               CollationAwareUTF8String.toTitleCase(
