54ea0a774d8dab51e02073f47ae46ed4e7398304
Mihailo Milosevic <mihailo.milosevic@databricks.com>
2024-05-30 11:15:09 +0200

    Update DockerJDBCIntegrationV2Suite.scala

--- connector/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/DockerJDBCIntegrationV2Suite.scala
+++ connector/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/v2/DockerJDBCIntegrationV2Suite.scala
@@ -39,17 +39,23 @@ abstract class DockerJDBCIntegrationV2Suite extends DockerJDBCIntegrationSuite {
     connection.prepareStatement("INSERT INTO employee VALUES (6, 'jen', 12000, 1200)")
       .executeUpdate()
 
-    connection.prepareStatement("INSERT INTO pattern_testing_table VALUES ('special_character_quote''_present')")
+    connection.prepareStatement("INSERT INTO pattern_testing_table "
+                                + "VALUES ('special_character_quote''_present')")
       .executeUpdate()
-    connection.prepareStatement("INSERT INTO pattern_testing_table VALUES ('special_character_quote_not_present')")
+    connection.prepareStatement("INSERT INTO pattern_testing_table "
+                                + "VALUES ('special_character_quote_not_present')")
       .executeUpdate()
-    connection.prepareStatement("INSERT INTO pattern_testing_table VALUES ('special_character_percent%_present')")
+    connection.prepareStatement("INSERT INTO pattern_testing_table "
+                                + "VALUES ('special_character_percent%_present')")
       .executeUpdate()
-    connection.prepareStatement("INSERT INTO pattern_testing_table VALUES ('special_character_percent_not_present')")
+    connection.prepareStatement("INSERT INTO pattern_testing_table "
+                                + "VALUES ('special_character_percent_not_present')")
       .executeUpdate()
-    connection.prepareStatement("INSERT INTO pattern_testing_table VALUES ('special_character_underscore_present')")
+    connection.prepareStatement("INSERT INTO pattern_testing_table "
+                                + "VALUES ('special_character_underscore_present')")
       .executeUpdate()
-    connection.prepareStatement("INSERT INTO pattern_testing_table VALUES ('special_character_underscorenot_present')")
+    connection.prepareStatement("INSERT INTO pattern_testing_table "
+                                + "VALUES ('special_character_underscorenot_present')")
       .executeUpdate()
   }
 
