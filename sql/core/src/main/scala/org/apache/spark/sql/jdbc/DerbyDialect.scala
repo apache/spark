69afd4be9c93cb31a840b969ed1984c0b6b92f8e
Kent Yao <yao@apache.org>
2024-05-30 17:31:28 +0800

    [SPARK-47361][SQL] Derby: Calculate suitable precision and scale for DECIMAL type
    ### What changes were proposed in this pull request?
    
    When storing `decimal(p, s)` to derby, if `p > 31`, `s` is wrongly hardcoded to `5` which is the assumed default scale of derby decimal. Actually, 0 is the default scale, 5 is the default precision https://db.apache.org/derby/docs/10.13/ref/rrefsqlj15260.html
    
    This PR calculates a suitable scale to make room for precision.
    
    ### Why are the changes needed?
    
    avoid precision loss
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, but derby is rare in production environments, and the new mapping are compatible for most usecases
    
    ### How was this patch tested?
    
    new tests
    
    ### Was this patch authored or co-authored using generative AI tooling?
    no
    
    Closes #46776 from yaooqinn/SPARK-48439.
    
    Authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: yangjie01 <yangjie01@baidu.com>

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/DerbyDialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/DerbyDialect.scala
@@ -48,9 +48,15 @@ private case class DerbyDialect() extends JdbcDialect {
     case ByteType => Option(JdbcType("SMALLINT", java.sql.Types.SMALLINT))
     case ShortType => Option(JdbcType("SMALLINT", java.sql.Types.SMALLINT))
     case BooleanType => Option(JdbcType("BOOLEAN", java.sql.Types.BOOLEAN))
-    // 31 is the maximum precision and 5 is the default scale for a Derby DECIMAL
-    case t: DecimalType if t.precision > 31 =>
-      Option(JdbcType("DECIMAL(31,5)", java.sql.Types.DECIMAL))
+    // 31 is the maximum precision
+    // https://db.apache.org/derby/docs/10.13/ref/rrefsqlj15260.html
+    case t: DecimalType =>
+      val (p, s) = if (t.precision > 31) {
+        (31, math.max(t.scale - (t.precision - 31), 0))
+      } else {
+        (t.precision, t.scale)
+      }
+      Option(JdbcType(s"DECIMAL($p,$s)", java.sql.Types.DECIMAL))
     case _ => None
   }
 
