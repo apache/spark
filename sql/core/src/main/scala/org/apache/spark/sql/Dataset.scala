3cb37f2722483fafd34145ce7f6575358c7501f7
Xinrong Meng <xinrong@apache.org>
2024-06-06 17:31:52 -0700

    scala api

--- sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/Dataset.scala
@@ -2175,6 +2175,17 @@ class Dataset[T] private[sql](
       valueColumnName: String): DataFrame =
     unpivot(ids.toArray, variableColumnName, valueColumnName)
 
+
+  def transpose(
+      firstColumnValues: Seq[Column],
+      valueType: DataType): DataFrame = withPlan {
+    Transpose(
+      firstColumnValues.map(_.expr),
+      valueType,
+      logicalPlan,
+    )
+  }
+
   /**
    * Unpivot a DataFrame from wide format to long format, optionally leaving identifier columns set.
    * This is the reverse to `groupBy(...).pivot(...).agg(...)`, except for the aggregation,
