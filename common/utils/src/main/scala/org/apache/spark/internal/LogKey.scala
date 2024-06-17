945d0be9e2bd7e92176f6af10364f66116d0d5b4
Amanda Liu <amanda.liu@databricks.com>
2024-06-17 14:07:44 -0700

    keep OPERATION_HANDLE_IDENTIFIER

--- common/utils/src/main/scala/org/apache/spark/internal/LogKey.scala
+++ common/utils/src/main/scala/org/apache/spark/internal/LogKey.scala
@@ -541,7 +541,7 @@ private[spark] object LogKeys {
   case object OLD_VALUE extends LogKey
   case object OPEN_COST_IN_BYTES extends LogKey
   case object OPERATION_HANDLE extends LogKey
-  case object OPERATION_ID extends LogKey
+  case object OPERATION_HANDLE_IDENTIFIER extends LogKey
   case object OPTIMIZED_PLAN_COLUMNS extends LogKey
   case object OPTIMIZER_CLASS_NAME extends LogKey
   case object OPTIONS extends LogKey
