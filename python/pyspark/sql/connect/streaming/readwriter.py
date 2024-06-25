9d4abaf19fa4d508aca35ac00528b2ce9f4e8805
Martin Grund <martin.grund@databricks.com>
2024-06-26 08:35:32 +0900

    [SPARK-48638][CONNECT] Add ExecutionInfo support for DataFrame
    ### What changes were proposed in this pull request?
    
    One of the interesting shortcomings in Spark Connect is that the query execution metrics are not easily accessible directly. In Spark Classic, the query execution is only accessible via the `_jdf` private variable and this is not available in Spark Connect.
    
    However, since the first release of Spark Connect, the response messages were already containing the metrics from the executed plan.
    
    This patch makes them accessible directly and provides a way to visualize them.
    
    ```python
    df = spark.range(100)
    df.collect()
    metrics = df.executionInfo.metrics
    metrics.toDot()
    ```
    
    The `toDot()` method returns an instance of the `graphviz.Digraph` object that can be either directly displayed in a notebook or further manipulated.
    
    <img width="909" alt="image" src="https://github.com/apache/spark/assets/3421/e1710350-54d2-4e6d-9b80-0aaf1b8583e3">
    
    <img width="871" alt="image" src="https://github.com/apache/spark/assets/3421/6a972119-76b6-4e36-bc81-8d01110fa31c">
    
    The purpose of the `executionInfo` property and the associated `ExecutionInfo` class is not to provide equivalence to the `QueryExecution` class used internally by Spark (and, for example, access to the analyzed, optimized, and executed plan) but rather provide a convenient way of accessing execution related information.
    
    ### Why are the changes needed?
    User Experience
    
    ### Does this PR introduce _any_ user-facing change?
    Adding a new API for accessing the query execution of a Spark SQL execution.
    
    ### How was this patch tested?
    Added new UT
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No
    
    Closes #46996 from grundprinzip/SPARK-48638.
    
    Lead-authored-by: Martin Grund <martin.grund@databricks.com>
    Co-authored-by: Martin Grund <grundprinzip@gmail.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/connect/streaming/readwriter.py
+++ python/pyspark/sql/connect/streaming/readwriter.py
@@ -601,7 +601,7 @@ class DataStreamWriter:
             self._write_proto.table_name = tableName
 
         cmd = self._write_stream.command(self._session.client)
-        (_, properties) = self._session.client.execute_command(cmd)
+        (_, properties, _) = self._session.client.execute_command(cmd)
 
         start_result = cast(
             pb2.WriteStreamOperationStartResult, properties["write_stream_operation_start_result"]
---
+++ python/pyspark/sql/tests/connect/test_df_debug.py
@@ -0,0 +1,86 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import unittest
+
+from pyspark.testing.connectutils import (
+    should_test_connect,
+    have_graphviz,
+    graphviz_requirement_message,
+)
+from pyspark.sql.tests.connect.test_connect_basic import SparkConnectSQLTestCase
+
+if should_test_connect:
+    from pyspark.sql.connect.dataframe import DataFrame
+
+
+class SparkConnectDataFrameDebug(SparkConnectSQLTestCase):
+    def test_df_debug_basics(self):
+        df: DataFrame = self.connect.range(100).repartition(10).groupBy("id").count()
+        x = df.collect()  # noqa: F841
+        ei = df.executionInfo
+
+        root, graph = ei.metrics.extract_graph()
+        self.assertIn(root, graph, "The root must be rooted in the graph")
+
+    def test_df_quey_execution_empty_before_execution(self):
+        df: DataFrame = self.connect.range(100).repartition(10).groupBy("id").count()
+        ei = df.executionInfo
+        self.assertIsNone(ei, "The query execution must be None before the action is executed")
+
+    def test_df_query_execution_with_writes(self):
+        df: DataFrame = self.connect.range(100).repartition(10).groupBy("id").count()
+        df.write.save("/tmp/test_df_query_execution_with_writes", format="json", mode="overwrite")
+        ei = df.executionInfo
+        self.assertIsNotNone(
+            ei, "The query execution must be None after the write action is executed"
+        )
+
+    def test_query_execution_text_format(self):
+        df: DataFrame = self.connect.range(100).repartition(10).groupBy("id").count()
+        df.collect()
+        self.assertIn("HashAggregate", df.executionInfo.metrics.toText())
+
+        # Different execution mode.
+        df: DataFrame = self.connect.range(100).repartition(10).groupBy("id").count()
+        df.toPandas()
+        self.assertIn("HashAggregate", df.executionInfo.metrics.toText())
+
+    @unittest.skipIf(not have_graphviz, graphviz_requirement_message)
+    def test_df_query_execution_metrics_to_dot(self):
+        df: DataFrame = self.connect.range(100).repartition(10).groupBy("id").count()
+        x = df.collect()  # noqa: F841
+        ei = df.executionInfo
+
+        dot = ei.metrics.toDot()
+        source = dot.source
+        self.assertIsNotNone(dot, "The dot representation must not be None")
+        self.assertGreater(len(source), 0, "The dot representation must not be empty")
+        self.assertIn("digraph", source, "The dot representation must contain the digraph keyword")
+        self.assertIn("Metrics", source, "The dot representation must contain the Metrics keyword")
+
+
+if __name__ == "__main__":
+    from pyspark.sql.tests.connect.test_df_debug import *  # noqa: F401
+
+    try:
+        import xmlrunner  # type: ignore
+
+        testRunner = xmlrunner.XMLTestRunner(output="target/test-reports", verbosity=2)
+    except ImportError:
+        testRunner = None
+    unittest.main(testRunner=testRunner, verbosity=2)
