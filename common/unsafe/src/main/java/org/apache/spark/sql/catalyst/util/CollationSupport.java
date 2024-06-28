6304484a14a8a356ee5cc739d91a3a51e9754e93
Uros Bojanic <157381213+uros-db@users.noreply.github.com>
2024-06-28 09:06:07 +0800

    [SPARK-48282][SQL] Alter string search logic for UTF8_BINARY_LCASE collation (StringReplace, FindInSet)
    ### What changes were proposed in this pull request?
    String searching in UTF8_BINARY_LCASE now works on character-level, rather than on byte-level. For example: `replace("İ", "i")`; now returns "İ", because there exists no `start, len` such that `lowercase(substring("İ", start, len)) == "i"`.
    
    ### Why are the changes needed?
    Fix functions that give unusable results due to one-to-many case mapping when performing string search under UTF8_BINARY_LCASE (see example above).
    
    ### Does this PR introduce _any_ user-facing change?
    Yes, behaviour of `replace` expression is changed for edge cases with one-to-many case mapping.
    
    ### How was this patch tested?
    New unit tests for StringReplace and FindInSet in `CollationSupportSuite`.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #46682 from uros-db/alter-lcase-vol3.
    
    Authored-by: Uros Bojanic <157381213+uros-db@users.noreply.github.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- common/unsafe/src/main/java/org/apache/spark/sql/catalyst/util/CollationSupport.java
+++ common/unsafe/src/main/java/org/apache/spark/sql/catalyst/util/CollationSupport.java
@@ -322,10 +322,8 @@ public final class CollationSupport {
       CollationFactory.Collation collation = CollationFactory.fetchCollation(collationId);
       if (collation.supportsBinaryEquality) {
         return execBinary(word, set);
-      } else if (collation.supportsLowercaseEquality) {
-        return execLowercase(word, set);
       } else {
-        return execICU(word, set, collationId);
+        return execCollationAware(word, set, collationId);
       }
     }
     public static String genCode(final String word, final String set, final int collationId) {
@@ -333,20 +331,15 @@ public final class CollationSupport {
       String expr = "CollationSupport.FindInSet.exec";
       if (collation.supportsBinaryEquality) {
         return String.format(expr + "Binary(%s, %s)", word, set);
-      } else if (collation.supportsLowercaseEquality) {
-        return String.format(expr + "Lowercase(%s, %s)", word, set);
       } else {
-        return String.format(expr + "ICU(%s, %s, %d)", word, set, collationId);
+        return String.format(expr + "execCollationAware(%s, %s, %d)", word, set, collationId);
       }
     }
     public static int execBinary(final UTF8String word, final UTF8String set) {
       return set.findInSet(word);
     }
-    public static int execLowercase(final UTF8String word, final UTF8String set) {
-      return set.toLowerCase().findInSet(word.toLowerCase());
-    }
-    public static int execICU(final UTF8String word, final UTF8String set,
-                                  final int collationId) {
+    public static int execCollationAware(final UTF8String word, final UTF8String set,
+        final int collationId) {
       return CollationAwareUTF8String.findInSet(word, set, collationId);
     }
   }
