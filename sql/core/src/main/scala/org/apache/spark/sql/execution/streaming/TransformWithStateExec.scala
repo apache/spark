bb478d9f8d92ffd616fea3c470150dc624b0a312
Eric Marnadi <eric.marnadi@databricks.com>
2024-07-01 17:09:54 -0700

    Feedback

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/TransformWithStateExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/TransformWithStateExec.scala
@@ -434,7 +434,7 @@ case class TransformWithStateExec(
 
     val operatorPropertiesJson: JValue =
       ("timeMode" -> JString(timeMode.toString)) ~
-        ("outputMode" -> JString(outputMode.toString))
+      ("outputMode" -> JString(outputMode.toString))
 
     val json = compact(render(operatorPropertiesJson))
     OperatorStateMetadataV2(operatorInfo, stateStoreInfo, json)
