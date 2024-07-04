e2e52c2fd1fd42912ec409ce685b2de890c039a5
Eric Marnadi <eric.marnadi@databricks.com>
2024-07-03 18:44:14 -0700

    feedback

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/IncrementalExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/IncrementalExecution.scala
@@ -85,6 +85,7 @@ class IncrementalExecution(
     .map(SQLConf.SHUFFLE_PARTITIONS.valueConverter)
     .getOrElse(sparkSession.sessionState.conf.numShufflePartitions)
 
+  private val STATE_SCHEMA_DEFAULT_VERSION: Int = 2
   /**
    * This value dictates which schema format version the state schema should be written in
    * for all operators other than TransformWithState.
