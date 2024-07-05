d640b944aa2300f2c662b484b130b90f3b22fec4
Amanda Liu <amanda.liu@databricks.com>
2024-07-05 13:50:48 -0700

    improve error message

--- dev/structured-logging.py
+++ dev/structured-logging.py
@@ -25,37 +25,42 @@ import glob
 from sparktestsupport import SPARK_HOME
 
 def main():
-    nonmigrated_pattern = r'log(Info|Warning|Error)\((?:".*"\.format\(.*\)|s".*(?:\$|\+\s*[^\s"]).*"\))'
-    whitelist_file = 'dev/structured-logging-whitelist.txt'
+    nonmigrated_pattern = r'log(?:Info|Warning|Error)\((?:".*"\.format\(.*\)|s".*(?:\$|\+\s*[^\s"]).*"\))'
+    excluded_file_patterns = [
+        '[Tt]est',
+        './sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/codegen/CodeGenerator.scala'
+        './sql/hive-thriftserver/src/main/scala/org/apache/spark/sql/hive/thriftserver/SparkSQLCLIService.scala'
+    ]
 
-    nonmigrated_files = []
-    whitelist_files = set()
-
-    with open(whitelist_file, 'r') as wf:
-        whitelist_patterns = [line.strip() for line in wf if line.strip()]
+    nonmigrated_files = {}
+    excluded_files = set()
 
     scala_files = glob.glob(os.path.join(SPARK_HOME, '**', '*.scala'), recursive=True)
 
     for file in scala_files:
-        for whitelist_pattern in whitelist_patterns:
-            if re.search(whitelist_pattern, file):
-                whitelist_files.add(file)
+        for exclude_pattern in excluded_file_patterns:
+            if re.search(exclude_pattern, file):
+                excluded_files.add(file)
                 break
 
     for file in scala_files:
-        if file not in whitelist_files:
+        if file not in excluded_files:
             with open(file, 'r') as f:
-                content = f.read().replace('\t', '').strip()
-                if re.search(nonmigrated_pattern, content):
-                    nonmigrated_files.append(file)
+                lines = f.readlines()
+                for line_number, line in enumerate(lines, start=1):
+                    if re.search(nonmigrated_pattern, line):
+                        if file not in nonmigrated_files:
+                            nonmigrated_files[file] = []
+                        nonmigrated_files[file].append((line_number, line))
 
     if not nonmigrated_files:
         print("Structured logging style check passed.")
         sys.exit(0)
     else:
-        print("Structured logging style check failed for the following:")
-        for file in nonmigrated_files:
-            print(file)
+        for file_path, issues in nonmigrated_files.items():
+            print(f"[error] Structured logging style check failed for the file '{file}' at:")
+            for line_number, code in issues:
+                print(f"Line: {line_number} code: {code}")
 
         sys.exit(-1)
 
