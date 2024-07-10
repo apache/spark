e987a8f5706319b039afb2394adf87da66b7fb10
Milan Dankovic <milan.dankovic@databricks.com>
2024-07-10 17:11:36 +0200

    Add parameter doc for label

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/SqlScriptingLogicalOperators.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/SqlScriptingLogicalOperators.scala
@@ -51,6 +51,8 @@ case class SingleStatement(parsedPlan: LogicalPlan)
 /**
  * Logical operator for a compound body. Contains all statements within the compound body.
  * @param collection Collection of statements within the compound body.
+ * @param label Label set to CompoundBody by user or UUID otherwise.
+ *              It can be None in case of CompoundBody is inside loop or conditional block.
  */
 case class CompoundBody(
     collection: Seq[CompoundPlanStatement],
