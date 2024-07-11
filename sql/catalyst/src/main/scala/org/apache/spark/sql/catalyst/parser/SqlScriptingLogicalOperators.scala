66c71f0e3878d99d1da23f03fefb8660e67be196
David Milicevic <david.milicevic@databricks.com>
2024-07-11 11:12:19 +0800

    [SPARK-48529][SQL] Introduction of Labels in SQL Scripting
    ### What changes were proposed in this pull request?
    Previous [PR1](https://github.com/apache/spark/pull/46665) and [PR2](https://github.com/apache/spark/pull/46665) introduced parser and interpreter changes for SQL Scripting. This PR is a follow-up to introduce the concept of labels for SQL Scripting language and proposes the following changes:
    
    - Changes grammar to support labels at start and end of the compound statements.
    - Updates visitor functions for compound nodes in the syntax tree in AstBuilder to check if labels are present and valid.
    
    More details can be found in [Jira item](https://issues.apache.org/jira/browse/SPARK-48529) for this task and its parent (where the design doc is uploaded as well).
    
    ### Why are the changes needed?
    The intent is to add support for various SQL scripting concepts like loops, leave & iterate statements.
    
    ### Does this PR introduce any user-facing change?
    No.
    This PR is among first PRs in series of PRs that will introduce changes to sql() API to add support for SQL scripting, but for now, the API remains unchanged.
    In the future, the API will remain the same as well, but it will have new possibility to execute SQL scripts.
    
    ### How was this patch tested?
    There are tests for newly introduced parser changes:
    
    SqlScriptingParserSuite - unit tests for execution nodes.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #47146 from miland-db/sql_batch_labels.
    
    Lead-authored-by: David Milicevic <david.milicevic@databricks.com>
    Co-authored-by: Milan Dankovic <milan.dankovic@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/SqlScriptingLogicalOperators.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/parser/SqlScriptingLogicalOperators.scala
@@ -51,5 +51,10 @@ case class SingleStatement(parsedPlan: LogicalPlan)
 /**
  * Logical operator for a compound body. Contains all statements within the compound body.
  * @param collection Collection of statements within the compound body.
+ * @param label Label set to CompoundBody by user or UUID otherwise.
+ *              It can be None in case when CompoundBody is not part of BeginEndCompoundBlock
+ *              for example when CompoundBody is inside loop or conditional block.
  */
-case class CompoundBody(collection: Seq[CompoundPlanStatement]) extends CompoundPlanStatement
+case class CompoundBody(
+    collection: Seq[CompoundPlanStatement],
+    label: Option[String]) extends CompoundPlanStatement
