fdc86697f3347ea6842b394efae11c461d838fdd
jackylee-ch <lijunqing@baidu.com>
2024-07-12 18:15:56 +0800

    [SPARK-48845][SQL] GenericUDF catch exceptions from children
    ### What changes were proposed in this pull request?
    This pr is trying to fix the syntax issues with GenericUDF since 3.5.0. The problem arose from DeferredObject currently passing a value instead of a function, which prevented users from catching exceptions in GenericUDF, resulting in semantic differences.
    
    Here is an example case we encountered. Originally, the semantics were that udf_exception would throw an exception, while udf_catch_exception could catch the exception and return a null value. However, currently, any exception encountered by udf_exception will cause the program to fail.
    ```
    select udf_catch_exception(udf_exception(col1)) from table
    ```
    
    ### Why are the changes needed?
    For before Spark 3.5, we directly made the GenericUDF's DeferredObject lazy and evaluated the children in `function.evaluate(deferredObjects)`.
    Now, we would run the children's code first. If an exception is thrown, we would make it lazy to GenericUDF's DeferredObject.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Newly added UT.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #47268 from jackylee-ch/generic_udf_catch_exception_from_child_func.
    
    Lead-authored-by: jackylee-ch <lijunqing@baidu.com>
    Co-authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Kent Yao <yao@apache.org>
    (cherry picked from commit 236d95738b6e50bc9ec54955e86d01b6dcf11c0e)
    Signed-off-by: Kent Yao <yao@apache.org>

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveUDFSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/execution/HiveUDFSuite.scala
@@ -35,6 +35,7 @@ import org.apache.hadoop.io.{LongWritable, Writable}
 
 import org.apache.spark.{SparkException, SparkFiles, TestUtils}
 import org.apache.spark.sql.{AnalysisException, QueryTest, Row}
+import org.apache.spark.sql.catalyst.expressions.CodegenObjectFactoryMode
 import org.apache.spark.sql.catalyst.plans.logical.Project
 import org.apache.spark.sql.execution.WholeStageCodegenExec
 import org.apache.spark.sql.functions.{call_function, max}
@@ -788,6 +789,28 @@ class HiveUDFSuite extends QueryTest with TestHiveSingleton with SQLTestUtils {
             "result" -> "boolean"
           )
         )
+      }
+    }
+  }
+
+  test("SPARK-48845: GenericUDF catch exceptions from child UDFs") {
+    withTable("test_catch_exception") {
+      withUserDefinedFunction("udf_throw_exception" -> true, "udf_catch_exception" -> true) {
+        Seq("9", "9-1").toDF("a").write.saveAsTable("test_catch_exception")
+        sql("CREATE TEMPORARY FUNCTION udf_throw_exception AS " +
+          s"'${classOf[UDFThrowException].getName}'")
+        sql("CREATE TEMPORARY FUNCTION udf_catch_exception AS " +
+          s"'${classOf[UDFCatchException].getName}'")
+        Seq(
+          CodegenObjectFactoryMode.FALLBACK.toString,
+          CodegenObjectFactoryMode.NO_CODEGEN.toString
+        ).foreach { codegenMode =>
+          withSQLConf(SQLConf.CODEGEN_FACTORY_MODE.key -> codegenMode) {
+            val df = sql(
+              "SELECT udf_catch_exception(udf_throw_exception(a)) FROM test_catch_exception")
+            checkAnswer(df, Seq(Row("9"), Row(null)))
+          }
+        }
       }
     }
   }
