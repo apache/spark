2a8ec4d7df852c400736cffd93ca394bf51b8658
Hyukjin Kwon <gurwls223@apache.org>
2024-07-12 14:05:07 +0900

    python specific

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/planning/patterns.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/planning/patterns.scala
@@ -67,7 +67,7 @@ trait OperationHelper extends AliasHelper with PredicateHelper {
           empty
         }
 
-      case Filter(condition, child) if !condition.exists(_.isInstanceOf[Unevaluable]) =>
+      case Filter(condition, child) if !condition.exists(_.isInstanceOf[PythonUDF]) =>
         val (fields, filters, other, aliases) = collectProjectsAndFilters(child, alwaysInline)
         // When collecting projects and filters, we effectively push down filters through
         // projects. We need to meet the following conditions to do so:
@@ -115,8 +115,8 @@ object PhysicalOperation extends OperationHelper {
     val (fields, filters, child, _) = collectProjectsAndFilters(plan, alwaysInline)
     // If more than 2 filters are collected, they must all be deterministic.
     if (filters.length > 1) assert(filters.forall(_.deterministic))
-    // Unevaluable expressions should not be pushed
-    assert(filters.forall(!_.exists(_.isInstanceOf[Unevaluable])))
+    // PythonUDFs should not be pushed
+    assert(filters.forall(!_.exists(_.isInstanceOf[PythonUDF])))
     Some((
       fields.getOrElse(child.output),
       filters.flatMap(splitConjunctivePredicates),
