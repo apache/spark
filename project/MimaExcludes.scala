f2332b08b7b2323a8f23cd0c58f21ef3f7017386
Mingkang Li <mingkang.li@databricks.com>
2024-07-19 11:36:39 -0700

    fix syntax error in MimaExcludes

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -100,7 +100,7 @@ object MimaExcludes {
     ProblemFilters.exclude[DirectMissingMethodProblem]("org.apache.spark.sql.SQLContext#implicits._sqlContext"),
     ProblemFilters.exclude[DirectMissingMethodProblem]("org.apache.spark.sql.SQLImplicits._sqlContext"),
     ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.sql.SQLImplicits.session"),
-    ProblemFilters.exclude[DirectMissingMethodProblem]("org.apache.spark.sql.SparkSession#implicits._sqlContext")
+    ProblemFilters.exclude[DirectMissingMethodProblem]("org.apache.spark.sql.SparkSession#implicits._sqlContext"),
     // SPARK-48900: Add `reason` string to all job / stage / job group cancellation calls
     ProblemFilters.exclude[ReversedMissingMethodProblem]("org.apache.spark.scheduler.JobWaiter#cancel")
   )
