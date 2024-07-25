510e4c0ed7a6f6627872ee8d9b8dec62fedc5540
Kent Yao <yao@apache.org>
2024-07-25 13:51:38 +0800

    [SPARK-48844][FOLLOWUP][TESTS] Cleanup duplicated data resource files in hive-thriftserver test

--- sql/core/src/test/resources/sql-tests/inputs/sql-on-files.sql
+++ sql/core/src/test/resources/sql-tests/inputs/sql-on-files.sql
@@ -2,26 +2,26 @@
 CREATE TABLE TEST_PARQUET USING PARQUET AS SELECT 1;
 SELECT * FROM parquet.``;
 SELECT * FROM parquet.`/file/not/found`;
-SELECT * FROM parquet.`${spark.sql.warehouse.dir}/TEST_PARQUET`;
+SELECT * FROM parquet.`${spark.sql.warehouse.dir}/test_parquet`;
 DROP TABLE TEST_PARQUET;
 
 -- ORC
 CREATE TABLE TEST_ORC USING ORC AS SELECT 1;
 SELECT * FROM orc.``;
 SELECT * FROM orc.`/file/not/found`;
-SELECT * FROM orc.`${spark.sql.warehouse.dir}/TEST_ORC`;
+SELECT * FROM orc.`${spark.sql.warehouse.dir}/test_orc`;
 DROP TABLE TEST_ORC;
 
 -- CSV
 CREATE TABLE TEST_CSV USING CSV AS SELECT 1;
 SELECT * FROM csv.``;
 SELECT * FROM csv.`/file/not/found`;
-SELECT * FROM csv.`${spark.sql.warehouse.dir}/TEST_CSV`;
+SELECT * FROM csv.`${spark.sql.warehouse.dir}/test_csv`;
 DROP TABLE TEST_CSV;
 
 -- JSON
 CREATE TABLE TEST_JSON USING JSON AS SELECT 1;
 SELECT * FROM json.``;
 SELECT * FROM json.`/file/not/found`;
-SELECT * FROM json.`${spark.sql.warehouse.dir}/TEST_JSON`;
+SELECT * FROM json.`${spark.sql.warehouse.dir}/test_json`;
 DROP TABLE TEST_JSON;
