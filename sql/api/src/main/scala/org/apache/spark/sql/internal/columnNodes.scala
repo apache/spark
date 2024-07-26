2d30e57bdd918ab502ff5ff46cada19388ca1fa8
Herman van Hovell <herman@databricks.com>
2024-07-25 23:16:39 -0400

    Move to internal

--- sql/api/src/main/scala/org/apache/spark/sql/internal/columnNodes.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/internal/columnNodes.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.spark.sql.column
+package org.apache.spark.sql.internal
 
 import org.apache.spark.sql.catalyst.trees.{CurrentOrigin, Origin}
 import org.apache.spark.sql.types.{DataType, Metadata}
