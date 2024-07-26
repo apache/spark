2f54c99ac71cfda18b9c47b43fd75b3b2749ecde
Wei Guo <guow93@gmail.com>
2024-07-26 20:57:53 +0800

    update

--- sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/StringFunctionsSuite.scala
@@ -426,11 +426,13 @@ class StringFunctionsSuite extends QueryTest with SharedSparkSession {
     )
 
     val testTable = "test_substring_index"
-    withTable(testTable) {
-      sql(s"CREATE TABLE ${testTable} (num int) USING parquet")
-      sql(s"INSERT INTO ${testTable} VALUES (1), (2), (3), (NULL)")
-      val query = s"SELECT num, SUBSTRING_INDEX('a_a_a', '_', num) as sub_str FROM ${testTable}"
-      checkAnswer(sql(query), Seq(Row(1, "a"), Row(2, "a_a"), Row(3, "a_a_a"), Row(null, null)))
+    withTempPath { path =>
+      withTable(testTable) {
+        sql(s"CREATE TABLE $testTable (num int) USING parquet LOCATION '${path.toURI.toString}'")
+        sql(s"INSERT INTO $testTable VALUES (1), (2), (3), (NULL)")
+        val query = s"SELECT num, SUBSTRING_INDEX('a_a_a', '_', num) as sub_str FROM $testTable"
+        checkAnswer(sql(query), Seq(Row(1, "a"), Row(2, "a_a"), Row(3, "a_a_a"), Row(null, null)))
+      }
     }
   }
 
