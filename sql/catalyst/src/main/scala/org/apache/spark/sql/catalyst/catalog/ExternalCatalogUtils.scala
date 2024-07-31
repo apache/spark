ee5ae14730a9800d4333a01039510e3a5648bbd0
Kent Yao <yao@apache.org>
2024-07-31 17:54:20 +0800

    address comments

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/ExternalCatalogUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/catalog/ExternalCatalogUtils.scala
@@ -19,6 +19,7 @@ package org.apache.spark.sql.catalyst.catalog
 
 import java.net.URI
 
+import org.apache.commons.lang3.StringUtils
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.util.Shell
@@ -280,7 +281,6 @@ object CatalogUtils {
    * @return the URI of the path
    */
   def stringToURI(str: String): URI = {
-    import org.apache.commons.lang3.StringUtils
     if (StringUtils.isEmpty(str)) {
       throw QueryExecutionErrors.invalidLocationError(str, "INVALID_EMPTY_LOCATION")
     }
