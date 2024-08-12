097cf81c1e81278b738475e60423d6938fc9786c
Uros Bojanic <157381213+uros-db@users.noreply.github.com>
2024-08-12 21:04:56 +0200

    Scalastyle fix

--- common/unsafe/src/main/java/org/apache/spark/sql/catalyst/util/CollationAwareUTF8String.java
+++ common/unsafe/src/main/java/org/apache/spark/sql/catalyst/util/CollationAwareUTF8String.java
@@ -38,7 +38,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 /**
  * Utility class for collation-aware UTF8String operations.
