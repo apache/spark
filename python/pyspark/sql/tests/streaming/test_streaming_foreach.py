975f2a79c9e8641dbc1580a10e07b942f5ee92e4
jingz-db <jing.zhan@databricks.com>
2024-08-21 10:26:38 -0700

    typo

--- python/pyspark/sql/tests/streaming/test_streaming_foreach.py
+++ python/pyspark/sql/tests/streaming/test_streaming_foreach.py
@@ -223,7 +223,7 @@ class StreamingTestsForeachMixin:
         except StreamingQueryException as e:
             err_msg = str(e)
             self.assertTrue("test error" in err_msg)
-            self.assertTrue("FOREACH_BATCH_USER_FUNCTION_ERROR" in err_msg)
+            self.assertTrue("FOREACH_USER_FUNCTION_ERROR" in err_msg)
 
 
         self.assertEqual(len(tester.process_events()), 0)  # no row was processed
