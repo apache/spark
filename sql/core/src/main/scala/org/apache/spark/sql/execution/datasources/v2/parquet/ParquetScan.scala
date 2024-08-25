d0a43f2f6a774168204cb9d7f557a119fdc5160c
MichaelMoldavsky <michael.moldavsky@veeva.com>
2024-08-25 22:15:03 +0300

    split V2.parquet case classes ito an abstract class and a case class

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/parquet/ParquetScan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/parquet/ParquetScan.scala
@@ -16,121 +16,38 @@
  */
 package org.apache.spark.sql.execution.datasources.v2.parquet
 
-import scala.jdk.CollectionConverters._
-
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
-import org.apache.parquet.hadoop.ParquetInputFormat
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.connector.expressions.aggregate.Aggregation
 import org.apache.spark.sql.connector.read.PartitionReaderFactory
-import org.apache.spark.sql.execution.datasources.{AggregatePushDownUtils, PartitioningAwareFileIndex}
-import org.apache.spark.sql.execution.datasources.parquet.{ParquetOptions, ParquetReadSupport, ParquetWriteSupport}
-import org.apache.spark.sql.execution.datasources.v2.FileScan
-import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.execution.datasources.PartitioningAwareFileIndex
 import org.apache.spark.sql.sources.Filter
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
-import org.apache.spark.util.ArrayImplicits._
-import org.apache.spark.util.SerializableConfiguration
 
 case class ParquetScan(
-    sparkSession: SparkSession,
-    hadoopConf: Configuration,
-    fileIndex: PartitioningAwareFileIndex,
-    dataSchema: StructType,
-    readDataSchema: StructType,
-    readPartitionSchema: StructType,
-    pushedFilters: Array[Filter],
-    options: CaseInsensitiveStringMap,
-    pushedAggregate: Option[Aggregation] = None,
-    partitionFilters: Seq[Expression] = Seq.empty,
-    dataFilters: Seq[Expression] = Seq.empty) extends FileScan {
-  override def isSplitable(path: Path): Boolean = {
-    // If aggregate is pushed down, only the file footer will be read once,
-    // so file should not be split across multiple tasks.
-    pushedAggregate.isEmpty
-  }
+                        sparkSession: SparkSession,
+                        hadoopConf: Configuration,
+                        fileIndex: PartitioningAwareFileIndex,
+                        dataSchema: StructType,
+                        readDataSchema: StructType,
+                        readPartitionSchema: StructType,
+                        pushedFilters: Array[Filter],
+                        options: CaseInsensitiveStringMap,
+                        pushedAggregate: Option[Aggregation] = None,
+                        partitionFilters: Seq[Expression] = Seq.empty,
+                        dataFilters: Seq[Expression] = Seq.empty
+                      ) extends ParquetScanBase(
+  sparkSession, hadoopConf, fileIndex, dataSchema, readDataSchema, readPartitionSchema,
+  partitionFilters, dataFilters
+) {
+  override def equals(obj: Any): Boolean =
+    super.equals(obj)
+  override def hashCode(): Int =
+    super.hashCode()
 
-  override def readSchema(): StructType = {
-    // If aggregate is pushed down, schema has already been pruned in `ParquetScanBuilder`
-    // and no need to call super.readSchema()
-    if (pushedAggregate.nonEmpty) readDataSchema else super.readSchema()
-  }
-
-  override def createReaderFactory(): PartitionReaderFactory = {
-    val readDataSchemaAsJson = readDataSchema.json
-    hadoopConf.set(ParquetInputFormat.READ_SUPPORT_CLASS, classOf[ParquetReadSupport].getName)
-    hadoopConf.set(
-      ParquetReadSupport.SPARK_ROW_REQUESTED_SCHEMA,
-      readDataSchemaAsJson)
-    hadoopConf.set(
-      ParquetWriteSupport.SPARK_ROW_SCHEMA,
-      readDataSchemaAsJson)
-    hadoopConf.set(
-      SQLConf.SESSION_LOCAL_TIMEZONE.key,
-      sparkSession.sessionState.conf.sessionLocalTimeZone)
-    hadoopConf.setBoolean(
-      SQLConf.NESTED_SCHEMA_PRUNING_ENABLED.key,
-      sparkSession.sessionState.conf.nestedSchemaPruningEnabled)
-    hadoopConf.setBoolean(
-      SQLConf.CASE_SENSITIVE.key,
-      sparkSession.sessionState.conf.caseSensitiveAnalysis)
-
-    // Sets flags for `ParquetToSparkSchemaConverter`
-    hadoopConf.setBoolean(
-      SQLConf.PARQUET_BINARY_AS_STRING.key,
-      sparkSession.sessionState.conf.isParquetBinaryAsString)
-    hadoopConf.setBoolean(
-      SQLConf.PARQUET_INT96_AS_TIMESTAMP.key,
-      sparkSession.sessionState.conf.isParquetINT96AsTimestamp)
-    hadoopConf.setBoolean(
-      SQLConf.PARQUET_INFER_TIMESTAMP_NTZ_ENABLED.key,
-      sparkSession.sessionState.conf.parquetInferTimestampNTZEnabled)
-    hadoopConf.setBoolean(
-      SQLConf.LEGACY_PARQUET_NANOS_AS_LONG.key,
-      sparkSession.sessionState.conf.legacyParquetNanosAsLong)
-
-    val broadcastedConf = sparkSession.sparkContext.broadcast(
-      new SerializableConfiguration(hadoopConf))
-    val sqlConf = sparkSession.sessionState.conf
-    ParquetPartitionReaderFactory(
-      sqlConf,
-      broadcastedConf,
-      dataSchema,
-      readDataSchema,
-      readPartitionSchema,
-      pushedFilters,
-      pushedAggregate,
-      new ParquetOptions(options.asCaseSensitiveMap.asScala.toMap, sqlConf))
-  }
-
-  override def equals(obj: Any): Boolean = obj match {
-    case p: ParquetScan =>
-      val pushedDownAggEqual = if (pushedAggregate.nonEmpty && p.pushedAggregate.nonEmpty) {
-        AggregatePushDownUtils.equivalentAggregations(pushedAggregate.get, p.pushedAggregate.get)
-      } else {
-        pushedAggregate.isEmpty && p.pushedAggregate.isEmpty
-      }
-      super.equals(p) && dataSchema == p.dataSchema && options == p.options &&
-        equivalentFilters(pushedFilters, p.pushedFilters) && pushedDownAggEqual
-    case _ => false
-  }
-
-  override def hashCode(): Int = getClass.hashCode()
-
-  lazy private val (pushedAggregationsStr, pushedGroupByStr) = if (pushedAggregate.nonEmpty) {
-    (seqToString(pushedAggregate.get.aggregateExpressions.toImmutableArraySeq),
-      seqToString(pushedAggregate.get.groupByExpressions.toImmutableArraySeq))
-  } else {
-    ("[]", "[]")
-  }
-
-  override def getMetaData(): Map[String, String] = {
-    super.getMetaData() ++ Map("PushedFilters" -> seqToString(pushedFilters.toImmutableArraySeq)) ++
-      Map("PushedAggregation" -> pushedAggregationsStr) ++
-      Map("PushedGroupBy" -> pushedGroupByStr)
-  }
+  override def createReaderFactory(): PartitionReaderFactory =
+    (ParquetPartitionReaderFactory.apply _).tupled(createReaderFactoryArgs())
 }
