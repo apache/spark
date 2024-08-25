4be26aeac48e37e3a55c24c5463fbfb18a4d39cc
Bruce Robbins <bersprockets@gmail.com>
2024-08-25 16:15:00 -0700

    Add test

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameAggregateSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameAggregateSuite.scala
@@ -2490,6 +2490,27 @@ class DataFrameAggregateSuite extends QueryTest
       })
     }
   }
+
+  test("plunk") {
+    val data = Seq((1, 1.001d, 2), (2, 3.001d, 4), (2, 3.001, 4)).toDF("a", "b", "c")
+    withTempView("v1") {
+      data.createOrReplaceTempView("v1")
+      val df =
+        sql("""select
+              |  round(sum(b), 6) as sum1,
+              |  count(distinct a) as count1,
+              |  count(distinct c) as count2
+              |  from (
+              |    select
+              |      6 as gb,
+              |      *
+              |    from v1
+              |  )
+              |group by a, gb
+              |""".stripMargin)
+      checkAnswer(df, Row(1.001d, 1, 1) :: Row(6.002d, 1, 1) :: Nil)
+    }
+  }
 }
 
 case class B(c: Option[Double])
