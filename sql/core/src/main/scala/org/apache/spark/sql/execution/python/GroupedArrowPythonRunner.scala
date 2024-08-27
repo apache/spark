6eb86c61ffd2649c72cc251bca9b31dfac437ab8
Adam Binford <adamq43@gmail.com>
2024-08-27 07:25:22 -0400

    Start building toward passing batches as input

---
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/GroupedArrowPythonRunner.scala
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.python
+
+import java.io.DataOutputStream
+
+import org.apache.arrow.vector.VectorSchemaRoot
+import org.apache.arrow.vector.ipc.ArrowStreamWriter
+
+import org.apache.spark.api.python.ChainedPythonFunctions
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.execution.metric.SQLMetric
+import org.apache.spark.sql.types.StructType
+
+
+/**
+ * Python UDF Runner for grouped udfs.
+ */
+class GroupedArrowPythonRunner(
+    funcs: Seq[(ChainedPythonFunctions, Long)],
+    evalType: Int,
+    argOffsets: Array[Array[Int]],
+    schema: StructType,
+    timeZoneId: String,
+    largeVarTypes: Boolean,
+    arrowMaxRecordsPerBatch: Int,
+    conf: Map[String, String],
+    pythonMetrics: Map[String, SQLMetric],
+    jobArtifactUUID: Option[String],
+    profiler: Option[String])
+  extends ArrowPythonRunner(
+    funcs, evalType, argOffsets, schema, timeZoneId, largeVarTypes, conf, pythonMetrics,
+    jobArtifactUUID, profiler
+  ) {
+
+  override protected def writeNextInputToArrowStream(
+      root: VectorSchemaRoot,
+      writer: ArrowStreamWriter,
+      dataOut: DataOutputStream,
+      inputIterator: Iterator[Iterator[InternalRow]]): Boolean = {
+    val batchWriter = new BaseStreamingArrowWriter(root, writer, arrowMaxRecordsPerBatch)
+
+    if (inputIterator.hasNext) {
+      val startData = dataOut.size()
+      val nextBatch = inputIterator.next()
+
+      while (nextBatch.hasNext) {
+        val dataRow = nextBatch.next()
+        batchWriter.writeRow(dataRow)
+      }
+      batchWriter.finalizeCurrentArrowBatch()
+      val deltaData = dataOut.size() - startData
+      pythonMetrics("pythonDataSent") += deltaData
+      true
+    } else {
+      close()
+      false
+    }
+  }
+}
+
