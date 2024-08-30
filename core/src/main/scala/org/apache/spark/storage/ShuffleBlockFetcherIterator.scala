cec1252b0a2234cf64759a27758a9234de091828
zhangliang <zhangliang@trip.com>
2024-08-30 11:10:56 +0800

    [SPARK-43242][CORE][3.4] Fix throw 'Unexpected type of BlockId' in shuffle corruption diagnose
    ### What changes were proposed in this pull request?
    port to 3.4 for [SPARK-43242][CORE] Fix throw 'Unexpected type of BlockId' in shuffle corruption diagnose
    
    ### Why are the changes needed?
    3.4 conflict with master, see #40921
    
    ### Does this PR introduce any user-facing change?
    No
    
    ### How was this patch tested?
    Existing tests
    
    Closes #47909 from CavemanIV/port3.4-SPARK-43242.
    
    Authored-by: zhangliang <zhangliang@trip.com>
    Signed-off-by: Yi Wu <yi.wu@databricks.com>

--- core/src/main/scala/org/apache/spark/storage/ShuffleBlockFetcherIterator.scala
+++ core/src/main/scala/org/apache/spark/storage/ShuffleBlockFetcherIterator.scala
@@ -1142,6 +1142,12 @@ final class ShuffleBlockFetcherIterator(
           s"diagnosis is skipped due to lack of shuffle checksum support for push-based shuffle."
         logWarning(diagnosisResponse)
         diagnosisResponse
+      case shuffleBlockBatch: ShuffleBlockBatchId =>
+          val diagnosisResponse = s"BlockBatch $shuffleBlockBatch is corrupted " +
+          s"but corruption diagnosis is skipped due to lack of shuffle checksum support for " +
+          s"ShuffleBlockBatchId"
+        logWarning(diagnosisResponse)
+        diagnosisResponse
       case unexpected: BlockId =>
         throw new IllegalArgumentException(s"Unexpected type of BlockId, $unexpected")
     }
