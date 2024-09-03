e5c02204250d9f7d4fbd4ee4dc2664a1a1accea3
bogao007 <bo.gao@databricks.com>
2024-09-03 13:02:18 -0700

    fix

--- python/pyspark/sql/streaming/StateMessage_pb2.py
+++ python/pyspark/sql/streaming/StateMessage_pb2.py
@@ -20,13 +20,9 @@
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
 
-_runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC, 5, 27, 3, "", "StateMessage.proto"
-)
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
