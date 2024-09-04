c5293ecb017b55ff661ea05353e4463a08d0073c
Herman van Hovell <herman@databricks.com>
2024-09-04 08:05:56 -0400

    [SPARK-49426][CONNECT][SQL] Create a shared interface for DataFrameWriterV2
    ### What changes were proposed in this pull request?
    This PR creates a shared interface for DataFrameWriterV2.
    
    ### Why are the changes needed?
    We are creating a shared Scala Spark SQL interface for Classic and Connect.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Existing tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #47962 from hvanhovell/SPARK-49426.
    
    Authored-by: Herman van Hovell <herman@databricks.com>
    Signed-off-by: Herman van Hovell <herman@databricks.com>

--- project/MimaExcludes.scala
+++ project/MimaExcludes.scala
@@ -126,7 +126,12 @@ object MimaExcludes {
     ProblemFilters.exclude[MissingClassProblem]("org.apache.spark.sql.Observation$"),
 
     // SPARK-49425: Create a shared DataFrameWriter interface.
-    ProblemFilters.exclude[MissingClassProblem]("org.apache.spark.sql.DataFrameWriter")
+    ProblemFilters.exclude[MissingClassProblem]("org.apache.spark.sql.DataFrameWriter"),
+
+    // SPARK-49426: Shared DataFrameWriterV2
+    ProblemFilters.exclude[MissingClassProblem]("org.apache.spark.sql.CreateTableWriter"),
+    ProblemFilters.exclude[MissingClassProblem]("org.apache.spark.sql.DataFrameWriterV2"),
+    ProblemFilters.exclude[MissingClassProblem]("org.apache.spark.sql.WriteConfigMethods"),
   )
 
   // Default exclude rules
