4e7dbb83e99138310321ff777781b3aece21a959
jingz-db <jing.zhan@databricks.com>
2024-09-04 23:08:26 -0700

    add docstrings

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/state/utils/SchemaUtil.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/state/utils/SchemaUtil.scala
@@ -86,6 +86,112 @@ object SchemaUtil {
     row
   }
 
+  /**
+   * For map state variables, state rows are stored as composite key.
+   * To return grouping key -> Map{user key -> value} as one state reader row to
+   * the users, we need to perform grouping on state rows by their grouping key,
+   * and construct a map for that grouping key.
+   *
+   * We traverse the iterator returned from state store,
+   * and will only return a row for `next()` only if the grouping key in the next row
+   * from state store is different (or there are no more rows)
+   *
+   * Note that as state rows in RocksDB are ordered, all state rows with the
+   * same grouping key will appear consecutively during the iterator traversal.
+   */
+  def unifyMapStateRowPair(
+      stateRows: Iterator[UnsafeRowPair],
+      compositeKeySchema: StructType,
+      partitionId: Int): Iterator[InternalRow] = {
+    val groupingKeySchema = SchemaUtil.getSchemaAsDataType(
+      compositeKeySchema, "key"
+    ).asInstanceOf[StructType]
+    val userKeySchema = SchemaUtil.getSchemaAsDataType(
+      compositeKeySchema, "userKey"
+    ).asInstanceOf[StructType]
+
+    def appendKVPairToMap(
+        curMap: mutable.Map[Any, Any],
+        stateRowPair: UnsafeRowPair): Unit = {
+      curMap += (
+        stateRowPair.key.get(1, userKeySchema)
+          .asInstanceOf[UnsafeRow].copy() ->
+          stateRowPair.value.copy()
+        )
+    }
+
+    def updateDataRow(
+        groupingKey: Any,
+        curMap: mutable.Map[Any, Any]): GenericInternalRow = {
+      val row = new GenericInternalRow(3)
+      val mapData = new ArrayBasedMapData(
+        ArrayData.toArrayData(curMap.keys.toArray),
+        ArrayData.toArrayData(curMap.values.toArray)
+      )
+      row.update(0, groupingKey)
+      row.update(1, mapData)
+      row.update(2, partitionId)
+      row
+    }
+
+    // state rows are sorted in rocksDB. So all of the rows with same
+    // grouping key were grouped together consecutively
+    new Iterator[InternalRow] {
+      var curGroupingKey: UnsafeRow = _
+      var curStateRowPair: UnsafeRowPair = _
+      val curMap = mutable.Map.empty[Any, Any]
+
+      override def hasNext: Boolean =
+        stateRows.hasNext || !curMap.isEmpty
+
+      override def next(): InternalRow = {
+        var keepTraverse = true
+        while (stateRows.hasNext && keepTraverse) {
+          curStateRowPair = stateRows.next()
+          if (curGroupingKey == null) {
+            // First time in the iterator
+            // Need to make a copy because we need to keep the
+            // value across function calls
+            curGroupingKey = curStateRowPair.key
+              .get(0, groupingKeySchema).asInstanceOf[UnsafeRow].copy()
+            appendKVPairToMap(curMap, curStateRowPair)
+          } else {
+            val curPairGroupingKey =
+              curStateRowPair.key.get(0, groupingKeySchema)
+            if (curPairGroupingKey == curGroupingKey) {
+              appendKVPairToMap(curMap, curStateRowPair)
+            } else {
+              // find a different grouping key, exit loop and return a row
+              keepTraverse = false
+            }
+          }
+        }
+        if (!keepTraverse) {
+          // found a different grouping key
+          val row = updateDataRow(curGroupingKey, curMap)
+          // update vars
+          curGroupingKey =
+            curStateRowPair.key.get(0, groupingKeySchema)
+              .asInstanceOf[UnsafeRow].copy()
+          // empty the map, append current row
+          curMap.clear()
+          appendKVPairToMap(curMap, curStateRowPair)
+          // return map value of previous grouping key
+          row
+        } else {
+          // reach the end of the state rows
+          if (curMap.isEmpty) null.asInstanceOf[InternalRow]
+          else {
+            val row = updateDataRow(curGroupingKey, curMap)
+            // clear the map to end the iterator
+            curMap.clear()
+            row
+          }
+        }
+      }
+    }
+  }
+
   def isValidSchema(
       sourceOptions: StateSourceOptions,
       schema: StructType,
@@ -178,8 +284,27 @@ object SchemaUtil {
     }
   }
 
-  private def getUserKeySchema(schema: StructType): Option[StructType] = {
-    try {
+  /** Helper functions for map state data source reader
+   * Map state variables are stored in RocksDB state store in format of composite key,
+   * it has the schema of `TransformWithStateKeyValueRowSchemaUtils.getCompositeKeySchema()`;
+   * But for state store reader, we need to return in format of:
+   * "key": groupingKey, "map_value": Map(userKey -> value)
+   *
+   * The following helper functions help to translate the schema between composite key and
+   * the schema we want to return to the users.
+  */
+  def isMapStateVariable(
+      stateVariableInfoOpt: Option[TransformWithStateVariableInfo]): Boolean = {
+    stateVariableInfoOpt.isDefined &&
+      stateVariableInfoOpt.get.stateVariableType == MapState
+  }
+
+  /** Given key-value schema generated from `generateSchemaForStateVar()`,
+   *  returns the compositeKey schema that key is stored in the state store */
+  def getCompositeKeySchema(schema: StructType): StructType = {
+    val groupingKeySchema = SchemaUtil.getSchemaAsDataType(
+      schema, "key").asInstanceOf[StructType]
+    val userKeySchema = try {
       Option(
         SchemaUtil.getSchemaAsDataType(schema, "map_value").asInstanceOf[MapType]
           .keyType.asInstanceOf[StructType])
@@ -187,118 +312,15 @@ object SchemaUtil {
       case _: Exception =>
         None
     }
-  }
-
-  def getCompositeKeySchema(schema: StructType): StructType = {
-    val groupingKeySchema = SchemaUtil.getSchemaAsDataType(
-      schema, "key").asInstanceOf[StructType]
-    val userKeySchema = getUserKeySchema(schema)
     new StructType()
       .add("key", groupingKeySchema)
       .add("userKey", userKeySchema.get)
   }
 
+  /** Given key-value schema generated from `generateSchemaForStateVar()`,
+   * returns the value schema that it is stored in the state store */
   def getValueSchema(schema: StructType): StructType = {
     SchemaUtil.getSchemaAsDataType(schema, "map_value").asInstanceOf[MapType]
       .valueType.asInstanceOf[StructType]
-  }
-
-  def unifyMapStateRowPair(
-      stateRows: Iterator[UnsafeRowPair],
-      compositeKeySchema: StructType,
-      partitionId: Int): Iterator[InternalRow] = {
-    val groupingKeySchema = SchemaUtil.getSchemaAsDataType(
-      compositeKeySchema, "key"
-    ).asInstanceOf[StructType]
-    val userKeySchema = SchemaUtil.getSchemaAsDataType(
-      compositeKeySchema, "userKey"
-    ).asInstanceOf[StructType]
-
-    def appendKVPairToMap(
-        curMap: mutable.Map[Any, Any],
-        stateRowPair: UnsafeRowPair): Unit = {
-      curMap += (
-        stateRowPair.key.get(1, userKeySchema)
-          .asInstanceOf[UnsafeRow].copy() ->
-          stateRowPair.value.copy()
-        )
-    }
-
-    def updateDataRow(
-        groupingKey: Any,
-        curMap: mutable.Map[Any, Any]): GenericInternalRow = {
-      val row = new GenericInternalRow(3)
-      val mapData = new ArrayBasedMapData(
-        ArrayData.toArrayData(curMap.keys.toArray),
-        ArrayData.toArrayData(curMap.values.toArray)
-      )
-      row.update(0, groupingKey)
-      row.update(1, mapData)
-      row.update(2, partitionId)
-      row
-    }
-
-    // state rows are sorted in rocksDB. So all of the rows with same
-    // grouping key were grouped together consecutively
-    new Iterator[InternalRow] {
-      var curGroupingKey: UnsafeRow = _
-      var curStateRowPair: UnsafeRowPair = _
-      val curMap = mutable.Map.empty[Any, Any]
-
-      override def hasNext: Boolean =
-        stateRows.hasNext || !curMap.isEmpty
-
-      override def next(): InternalRow = {
-        var keepGoing = true
-        while (stateRows.hasNext && keepGoing) {
-          curStateRowPair = stateRows.next()
-          if (curGroupingKey == null) {
-            // First time in the iterator
-            // Need to make a copy because we need to keep the
-            // value across function calls
-            curGroupingKey = curStateRowPair.key
-              .get(0, groupingKeySchema).asInstanceOf[UnsafeRow].copy()
-            appendKVPairToMap(curMap, curStateRowPair)
-          } else {
-            val curPairGroupingKey =
-              curStateRowPair.key.get(0, groupingKeySchema)
-            if (curPairGroupingKey == curGroupingKey) {
-              appendKVPairToMap(curMap, curStateRowPair)
-            } else {
-              // find a different grouping key, exit loop and return a row
-              keepGoing = false
-            }
-          }
-        }
-        if (!keepGoing) {
-          // found a different grouping key
-          val row = updateDataRow(curGroupingKey, curMap)
-          // update vars
-          curGroupingKey =
-            curStateRowPair.key.get(0, groupingKeySchema)
-              .asInstanceOf[UnsafeRow].copy()
-          // empty the map, append current row
-          curMap.clear()
-          appendKVPairToMap(curMap, curStateRowPair)
-          // return map value of previous grouping key
-          row
-        } else {
-          // reach the end of the state rows
-          if (curMap.isEmpty) null.asInstanceOf[InternalRow]
-          else {
-            val row = updateDataRow(curGroupingKey, curMap)
-            // clear the map to end the iterator
-            curMap.clear()
-            row
-          }
-        }
-      }
-    }
-  }
-
-  def isMapStateVariable(
-      stateVariableInfoOpt: Option[TransformWithStateVariableInfo]): Boolean = {
-    stateVariableInfoOpt.isDefined &&
-      stateVariableInfoOpt.get.stateVariableType == MapState
   }
 }
