2c51f7fa15a85149bab55b36fbe3a8bef0d69f44
Xinrong Meng <xinrong@apache.org>
2024-09-06 15:16:59 +0800

    avoid dataset

--- python/pyspark/pandas/config.py
+++ python/pyspark/pandas/config.py
@@ -288,7 +288,7 @@ _options: List[Option] = [
             "'plotting.sample_ratio' sets the proportion of data that will be plotted for sample-"
             "based plots such as `plot.line` and `plot.area`. "
             "If not set, it is derived from 'plotting.max_rows', by calculating the ratio of "
-            "'plotting.max_rows' to the total dataset size."
+            "'plotting.max_rows' to the total data size."
         ),
         default=None,
         types=(float, type(None)),
