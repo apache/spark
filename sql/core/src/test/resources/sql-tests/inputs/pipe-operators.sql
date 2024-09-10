0cd4f2ab01b86fc3a2141463fc75c1c64a895e6f
Daniel Tenedorio <daniel.tenedorio@databricks.com>
2024-09-10 11:56:35 -0700

    add more testing

--- sql/core/src/test/resources/sql-tests/inputs/pipe-operators.sql
+++ sql/core/src/test/resources/sql-tests/inputs/pipe-operators.sql
@@ -1,24 +1,84 @@
+-- Prepare some test data.
+--------------------------
+drop table if exists t;
 create table t(x int, y string) using csv;
 insert into t values (0, 'abc'), (1, 'def');
 
--- Selection operators.
+drop table if exists other;
+create table other(a int, b int) using json;
+insert into other values (1, 1), (1, 2), (2, 4);
+
+drop table if exists st;
+create table st(x int, col struct<i1:int, i2:int>) using parquet;
+insert into st values (1, (2, 3));
+
+-- Selection operators: positive tests.
+---------------------------------------
+
+-- Selecting a constant.
 table t
 |> select 1 as x;
 
+-- Selecting attributes.
 table t
 |> select x, y;
 
+-- Chained pipe SELECT operators.
 table t
 |> select x, y
 |> select x + length(y) as z;
 
+-- Using the VALUES list as the source relation.
 values (0), (1) tab(col)
 |> select col * 2 as result;
 
+-- Using a table subquery as the source relation.
 (select * from t union all select * from t)
 |> select x + length(y) as result;
 
+-- Enclosing the result of a pipe SELECT operation in a table subquery.
+(table t
+ |> select x, y
+ |> select x)
+union all
+select x from t where x < 1;
+
+-- Selecting struct fields.
+(select col from st)
+|> select col.i1;
+
+table st
+|> select st.col.i1;
+
+-- Expression subqueries in the pipe operator SELECT list.
+table t
+|> select (select a from other where x = a limit 1);
+
+-- Aggregations are allowed within expression subqueries in the pipe operator SELECT list as long as
+-- no aggregate functions exist in the top-level select list.
+table t
+|> select (select any_value(a) from other where x = a limit 1);
+
+-- Lateral column aliases in the pipe operator SELECT list.
+table t
+|> select x + length(x) as z, z + 1 as plus_one;
+
+-- Window functions are allowed in the pipe operator SELECT list.
+table t
+|> select first_value(x) over (partition by y);
+
+-- Selection operators: negative tests.
+---------------------------------------
+
+-- Aggregate functions are not allowed in the pipe operator SELECT list.
 table t
 |> select sum(x) as result;
 
+table t
+|> select y, length(y) + sum(x) as result;
+
+-- Cleanup.
+-----------
 drop table t;
+drop table other;
+drop table st;
