17a1df9c8facf72d69a12e4aae4f089b0cce0019
panbingkun <panbingkun@baidu.com>
2024-09-11 19:16:02 +0800

    update

--- sql/catalyst/src/main/java/org/apache/spark/sql/catalyst/expressions/ArrayExpressionUtils.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/catalyst/expressions/ArrayExpressionUtils.java
@@ -24,8 +24,8 @@ import org.apache.spark.sql.catalyst.util.SQLOrderingUtil;
 public class ArrayExpressionUtils {
 
   // comparator
-  // boolean nullable comparator
-  private static final Comparator<Object> booleanComp = (o1, o2) -> {
+  // Boolean nullable comparator
+  private static final Comparator<Boolean> booleanComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -33,12 +33,12 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    boolean c1 = (Boolean) o1, c2 = (Boolean) o2;
+    boolean c1 = o1, c2 = o2;
     return c1 == c2 ? 0 : (c1 ? 1 : -1);
   };
 
-  // byte nullable comparator
-  private static final Comparator<Object> byteComp = (o1, o2) -> {
+  // Byte nullable comparator
+  private static final Comparator<Byte> byteComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -46,12 +46,12 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    byte c1 = (Byte) o1, c2 = (Byte) o2;
+    byte c1 = o1, c2 = o2;
     return Byte.compare(c1, c2);
   };
 
-  // short nullable comparator
-  private static final Comparator<Object> shortComp = (o1, o2) -> {
+  // Short nullable comparator
+  private static final Comparator<Short> shortComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -59,12 +59,12 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    short c1 = (Short) o1, c2 = (Short) o2;
+    short c1 = o1, c2 = o2;
     return Short.compare(c1, c2);
   };
 
-  // integer nullable comparator
-  private static final Comparator<Object> integerComp = (o1, o2) -> {
+  // Integer nullable comparator
+  private static final Comparator<Integer> integerComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -72,12 +72,12 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    int c1 = (Integer) o1, c2 = (Integer) o2;
+    int c1 = o1, c2 = o2;
     return Integer.compare(c1, c2);
   };
 
-  // long nullable comparator
-  private static final Comparator<Object> longComp = (o1, o2) -> {
+  // Long nullable comparator
+  private static final Comparator<Long> longComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -85,12 +85,12 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    long c1 = (Long) o1, c2 = (Long) o2;
+    long c1 = o1, c2 = o2;
     return Long.compare(c1, c2);
   };
 
-  // float nullable comparator
-  private static final Comparator<Object> floatComp = (o1, o2) -> {
+  // Float nullable comparator
+  private static final Comparator<Float> floatComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -98,12 +98,12 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    float c1 = (Float) o1, c2 = (Float) o2;
+    float c1 = o1, c2 = o2;
     return SQLOrderingUtil.compareFloats(c1, c2);
   };
 
-  // double nullable comparator
-  private static final Comparator<Object> doubleComp = (o1, o2) -> {
+  // Double nullable comparator
+  private static final Comparator<Double> doubleComp = (o1, o2) -> {
     if (o1 == null && o2 == null) {
       return 0;
     } else if (o1 == null) {
@@ -111,11 +111,11 @@ public class ArrayExpressionUtils {
     } else if (o2 == null) {
       return 1;
     }
-    double c1 = (Double) o1, c2 = (Double) o2;
+    double c1 = o1, c2 = o2;
     return SQLOrderingUtil.compareDoubles(c1, c2);
   };
 
-  // boolean nullable comparator
+  // Boolean nullable comparator
   public static int binarySearch(Boolean[] data, Boolean value) {
     return Arrays.binarySearch(data, value, booleanComp);
   }
@@ -126,7 +126,7 @@ public class ArrayExpressionUtils {
     return Arrays.binarySearch(data, value);
   }
 
-  // byte nullable
+  // Byte nullable
   public static int binarySearch(Byte[] data, Byte value) {
     return Arrays.binarySearch(data, value, byteComp);
   }
@@ -137,7 +137,7 @@ public class ArrayExpressionUtils {
     return Arrays.binarySearch(data, value);
   }
 
-  // short nullable
+  // Short nullable
   public static int binarySearch(Short[] data, Short value) {
     return Arrays.binarySearch(data, value, shortComp);
   }
@@ -148,7 +148,7 @@ public class ArrayExpressionUtils {
     return Arrays.binarySearch(data, value);
   }
 
-  // int nullable
+  // Integer nullable
   public static int binarySearch(Integer[] data, Integer value) {
     return Arrays.binarySearch(data, value, integerComp);
   }
@@ -159,7 +159,7 @@ public class ArrayExpressionUtils {
     return Arrays.binarySearch(data, value);
   }
 
-  // long nullable
+  // Long nullable
   public static int binarySearch(Long[] data, Long value) {
     return Arrays.binarySearch(data, value, longComp);
   }
@@ -170,7 +170,7 @@ public class ArrayExpressionUtils {
     return Arrays.binarySearch(data, value);
   }
 
-  // float nullable
+  // Float nullable
   public static int binarySearch(Float[] data, Float value) {
     return Arrays.binarySearch(data, value, floatComp);
   }
@@ -181,12 +181,12 @@ public class ArrayExpressionUtils {
     return Arrays.binarySearch(data, value);
   }
 
-  // double nullable
+  // Double nullable
   public static int binarySearch(Double[] data, Double value) {
     return Arrays.binarySearch(data, value, doubleComp);
   }
 
-  // object
+  // Object
   public static int binarySearch(Object[] data, Object value, Comparator<Object> comp) {
     return Arrays.binarySearch(data, value, comp);
   }
