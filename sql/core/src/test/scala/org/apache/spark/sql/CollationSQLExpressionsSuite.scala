f2d0503d8e38bed1db5914911b48f1056c9ad389
Gideon P <g.potok4@gmail.com>
2024-09-13 10:43:14 -0400

    formatting

--- sql/core/src/test/scala/org/apache/spark/sql/CollationSQLExpressionsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationSQLExpressionsSuite.scala
@@ -1739,9 +1739,9 @@ class CollationSQLExpressionsSuite
 
   test("Support Mode.eval(buffer) with complex types") {
     case class UTF8StringModeTestCase[R](
-                                          collationId: String,
-                                          bufferValues: Map[InternalRow, Long],
-                                          result: R)
+      collationId: String,
+      bufferValues: Map[InternalRow, Long],
+      result: R)
 
     val bufferValuesUTF8String: Map[Any, Long] = Map(
       UTF8String.fromString("a") -> 5L,
