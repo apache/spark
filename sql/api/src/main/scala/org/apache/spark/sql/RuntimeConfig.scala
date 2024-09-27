38b489e6dfb0b829ad444ba4d08919b5a44a9b68
Xi Lyu <xi.lyu@databricks.com>
2024-09-27 14:14:59 +0200

    Reformat

--- sql/api/src/main/scala/org/apache/spark/sql/RuntimeConfig.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/RuntimeConfig.scala
@@ -65,9 +65,9 @@ abstract class RuntimeConfig {
   def get(key: String): String
 
   /**
-   * Returns the value of Spark runtime configuration property for the given key. If the key is not
-   * set yet, return the given `default`. This is useful when `defaultValue` of its [[ConfigEntry]]
-   * is not the desired one.
+   * Returns the value of Spark runtime configuration property for the given key. If the key is
+   * not set yet, return the given `default`. This is useful when `defaultValue` of its
+   * [[ConfigEntry]] is not the desired one.
    *
    * @since 2.0.0
    */
@@ -81,9 +81,9 @@ abstract class RuntimeConfig {
   def getAll: Map[String, String]
 
   /**
-   * Returns the value of Spark runtime configuration property for the given key. If the key is not
-   * set yet, return `defaultValue` of its [[ConfigEntry]]. If there is no `defaultValue`, return
-   * `None`.
+   * Returns the value of Spark runtime configuration property for the given key. If the key is
+   * not set yet, return `defaultValue` of its [[ConfigEntry]]. If there is no `defaultValue`,
+   * return `None`.
    *
    * @since 2.0.0
    */
