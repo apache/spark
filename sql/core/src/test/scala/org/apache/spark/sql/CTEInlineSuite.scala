d9467b54349f7db7eadba1ca9efbffcbc707534a
Wenchen Fan <wenchen@databricks.com>
2024-09-30 23:34:57 -0700

    [SPARK-49816][SQL][3.5] Should only update out-going-ref-count for referenced outer CTE relation
    backport https://github.com/apache/spark/pull/48284 to 3.5
    
    ### What changes were proposed in this pull request?
    
    This PR fixes a long-standing reference counting bug in the rule `InlineCTE`. Let's look at the minimal repro:
    ```
          sql(
            """
              |WITH
              |t1 AS (SELECT 1 col),
              |t2 AS (SELECT * FROM t1)
              |SELECT * FROM t2
              |""".stripMargin).createTempView("v")
          // r1 is un-referenced, but it should not decrease the ref count of t2 inside view v.
          val df = sql(
            """
              |WITH
              |r1 AS (SELECT * FROM v),
              |r2 AS (SELECT * FROM v)
              |SELECT * FROM r2
              |""".stripMargin)
    ```
    The logical plan is something like below
    ```
    WithCTE
      CTEDef r1
        View v
          WithCTE
            CTEDef t1
              OneRowRelation
            CTEDef t2
              CTERef t1
            CTERef t2    // main query of the inner WithCTE
      CTEDef r2
        View v   // exactly the same as the view v above
          WithCTE
            CTEDef t1
              OneRowRelation
            CTEDef t2
              CTERef t1
            CTERef t2
      CTERef r2    // main query of the outer WithCTE
    ```
    Ideally, the ref count of `t1`, `t2` and `r2` should be all `1`. They will be inlined and the final plan is the `OneRowRelation`. However, in `InlineCTE#buildCTEMap`, when we traverse into `CTEDef r1` and hit `CTERef t2`, we mistakenly update the out-going-ref-count of `r1`, which means that `r1` references `t2` and this is totally wrong. Later on, in `InlineCTE#cleanCTEMap`, we find that `r1` is not referenced at all, so we decrease the ref count of its out-going-ref, which is `t2`, and the ref count of `t2` becomes `0`. Finally, in `InlineCTE#inlineCTE`, we leave the plan of `t2` unchanged because its ref count is `0`, and the plan of `t2` contains `CTERef t1`. `t2` is still inlined so we end up with `CTERef t1` as the final plan without the `WithCTE` node.
    
    ### Why are the changes needed?
    
    bug fix
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes, the query failed before and now can work
    
    ### How was this patch tested?
    
    new test
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    no
    
    Closes #48301 from cloud-fan/cte.
    
    Authored-by: Wenchen Fan <wenchen@databricks.com>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/core/src/test/scala/org/apache/spark/sql/CTEInlineSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CTEInlineSuite.scala
@@ -678,6 +678,27 @@ abstract class CTEInlineSuiteBase
       }.isDefined, "CTE columns should not be pruned.")
     }
   }
+
+  test("SPARK-49816: should only update out-going-ref-count for referenced outer CTE relation") {
+    withView("v") {
+      sql(
+        """
+          |WITH
+          |t1 AS (SELECT 1 col),
+          |t2 AS (SELECT * FROM t1)
+          |SELECT * FROM t2
+          |""".stripMargin).createTempView("v")
+      // r1 is un-referenced, but it should not decrease the ref count of t2 inside view v.
+      val df = sql(
+        """
+          |WITH
+          |r1 AS (SELECT * FROM v),
+          |r2 AS (SELECT * FROM v)
+          |SELECT * FROM r2
+          |""".stripMargin)
+      checkAnswer(df, Row(1))
+    }
+  }
 }
 
 class CTEInlineSuiteAEOff extends CTEInlineSuiteBase with DisableAdaptiveExecutionSuite
