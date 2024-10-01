1b46c2263c70850bb864f46ac3966ac1d493ea14
ashahid <ashahid@cloudera.com>
2024-09-30 21:59:37 -0700

    SPARK-46679 adding code which got missed in previous checkin

--- sql/api/src/main/scala/org/apache/spark/sql/catalyst/JavaTypeInference.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/catalyst/JavaTypeInference.scala
@@ -139,12 +139,15 @@ object JavaTypeInference {
 
     case pt: ParameterizedType =>
       val newTvs = JavaTypeUtils.getTypeArguments(pt).asScala.toMap
-      val updatedTvs = newTvs.map {
-        case (k, v: TypeVariable[_]) if typeVariables.contains(v) =>
-          k -> typeVariables(v)
-        case (k, v) =>
-          k -> v
-      }.asInstanceOf[Iterable[(TypeVariable[_], Type)]].toMap
+      val updatedTvs = newTvs
+        .map {
+          case (k, v: TypeVariable[_]) if typeVariables.contains(v) =>
+            k -> typeVariables(v)
+
+          case pair => pair
+        }
+        .asInstanceOf[Iterable[(TypeVariable[_], Type)]]
+        .toMap
       encoderFor(pt.getRawType, seenTypeSet, updatedTvs)
 
     case c: Class[_] =>
