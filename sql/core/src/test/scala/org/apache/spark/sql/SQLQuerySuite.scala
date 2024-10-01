75860a3cc50f366403d396a27eaa726f6860519a
Nikhil Sheoran <125331115+nikhilsheoran-db@users.noreply.github.com>
2024-10-01 09:49:00 +0800

    [SPARK-49743][SQL] OptimizeCsvJsonExpr should not change schema fields when pruning GetArrayStructFields
    ### What changes were proposed in this pull request?
    - Cherry-pick of the original PR - https://github.com/apache/spark/pull/48190 to Spark 3.5
    
    - When pruning the schema of the struct in `GetArrayStructFields`, rely on the existing `StructType` to obtain the pruned schema instead of using the accessed field.
    
    ### Why are the changes needed?
    
    - Fixes a bug in `OptimizeCsvJsonExprs` rule that would have otherwise changed the schema fields of the underlying struct to be extracted.
    - This would show up as a correctness issue where for a field instead of picking the right values we would have ended up giving null output.
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes. The query output would change for the queries of the following type:
    ```
    SELECT
      from_json('[{"a": '||id||', "b": '|| (2*id) ||'}]', 'array<struct<a: INT, b: INT>>').a,
      from_json('[{"a": '||id||', "b": '|| (2*id) ||'}]', 'array<struct<a: INT, b: INT>>').A
    FROM
      range(3) as t
    ```
    
    Earlier, the result would had been:
    ```
    Array([ArraySeq(0),ArraySeq(null)], [ArraySeq(1),ArraySeq(null)], [ArraySeq(2),ArraySeq(null)])
    ```
    vs the new result is (verified through spark-shell):
    ```
    Array([ArraySeq(0),ArraySeq(0)], [ArraySeq(1),ArraySeq(1)], [ArraySeq(2),ArraySeq(2)])
    ```
    
    ### How was this patch tested?
    - Added unit tests.
    - Without this change, the added test would fail as we would have modified the schema from `a` to `A`:
    ```
    - SPARK-49743: prune unnecessary columns from GetArrayStructFields does not change schema *** FAILED ***
      == FAIL: Plans do not match ===
      !Project [from_json(ArrayType(StructType(StructField(A,IntegerType,true)),true), json#0, Some(America/Los_Angeles)).A AS a#0]   Project [from_json(ArrayType(StructType(S
    tructField(a,IntegerType,true)),true), json#0, Some(America/Los_Angeles)).A AS a#0]
       +- LocalRelation <empty>, [json#0]                                                                                             +- LocalRelation <empty>, [json#0] (PlanT
    est.scala:179)
    ```
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #48308 from nikhilsheoran-db/SPARK-49743-3.5.
    
    Authored-by: Nikhil Sheoran <125331115+nikhilsheoran-db@users.noreply.github.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQuerySuite.scala
@@ -4711,6 +4711,19 @@ class SQLQuerySuite extends QueryTest with SharedSparkSession with AdaptiveSpark
     val df6 = df3.join(df2, col("df3.zaak_id") === col("df2.customer_id"), "outer")
     df5.crossJoin(df6)
   }
+
+  test("SPARK-49743: OptimizeCsvJsonExpr does not change schema when pruning struct") {
+    val df = sql("""
+        | SELECT
+        |    from_json('[{"a": '||id||', "b": '|| (2*id) ||'}]', 'array<struct<a: INT, b: INT>>').a,
+        |    from_json('[{"a": '||id||', "b": '|| (2*id) ||'}]', 'array<struct<a: INT, b: INT>>').A
+        | FROM
+        |    range(3) as t
+        |""".stripMargin)
+    val expectedAnswer = Seq(
+      Row(Array(0), Array(0)), Row(Array(1), Array(1)), Row(Array(2), Array(2)))
+    checkAnswer(df, expectedAnswer)
+  }
 }
 
 case class Foo(bar: Option[String])
