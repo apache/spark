6ea79ef61db5e40476605766c21e6da14392ca80
Mihailo Milosevic <mihailo.milosevic@databricks.com>
2024-10-03 11:55:30 +0200

    Incorporate changes

--- sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/errors/QueryExecutionErrors.scala
@@ -610,27 +610,31 @@ private[sql] object QueryExecutionErrors extends QueryErrorsBase with ExecutionE
   }
 
   def binaryArithmeticCauseOverflowError(
-      eval1: Short, symbol: String, eval2: Short, hint: String): SparkArithmeticException = {
-    if (hint == "") {
-      new SparkArithmeticException(
-        errorClass = "BINARY_ARITHMETIC_OVERFLOW.WITHOUT_SUGGESTION",
-        messageParameters = Map(
-          "value1" -> toSQLValue(eval1, ShortType),
-          "symbol" -> symbol,
-          "value2" -> toSQLValue(eval2, ShortType)),
-        context = Array.empty,
-        summary = "")
-    } else {
-      new SparkArithmeticException(
-        errorClass = "BINARY_ARITHMETIC_OVERFLOW.WITH_SUGGESTION",
-        messageParameters = Map(
-          "value1" -> toSQLValue(eval1, ShortType),
-          "symbol" -> symbol,
-          "value2" -> toSQLValue(eval2, ShortType),
-          "hint" -> hint),
-        context = Array.empty,
-        summary = "")
-    }
+      eval1: Short, symbol: String, eval2: Short): SparkArithmeticException = {
+    new SparkArithmeticException(
+      errorClass = "BINARY_ARITHMETIC_OVERFLOW.WITHOUT_SUGGESTION",
+      messageParameters = Map(
+        "value1" -> toSQLValue(eval1, ShortType),
+        "symbol" -> symbol,
+        "value2" -> toSQLValue(eval2, ShortType)),
+      context = Array.empty,
+      summary = "")
+  }
+
+  def binaryArithmeticCauseOverflowError(
+      eval1: Short,
+      symbol: String,
+      eval2: Short,
+      functionName: String): SparkArithmeticException = {
+    new SparkArithmeticException(
+      errorClass = "BINARY_ARITHMETIC_OVERFLOW.WITH_SUGGESTION",
+      messageParameters = Map(
+        "value1" -> toSQLValue(eval1, ShortType),
+        "symbol" -> symbol,
+        "value2" -> toSQLValue(eval2, ShortType),
+        "functionName" -> toSQLId(functionName)),
+      context = Array.empty,
+      summary = "")
   }
 
   def intervalArithmeticOverflowError(
