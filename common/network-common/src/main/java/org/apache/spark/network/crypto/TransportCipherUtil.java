93f77183c6af1e879cc2060d6bdb3103b267f462
yangjie01 <yangjie01@baidu.com>
2024-10-05 00:59:47 +0800

    init

--- common/network-common/src/main/java/org/apache/spark/network/crypto/TransportCipherUtil.java
+++ common/network-common/src/main/java/org/apache/spark/network/crypto/TransportCipherUtil.java
@@ -17,12 +17,25 @@
 
 package org.apache.spark.network.crypto;
 
-import io.netty.channel.Channel;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.crypto.tink.subtle.Hex;
+import com.google.crypto.tink.subtle.Hkdf;
 
-import java.io.IOException;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 
-interface TransportCipher {
-    String getKeyId() throws GeneralSecurityException;
-    void addToChannel(Channel channel) throws IOException, GeneralSecurityException;
+class TransportCipherUtil {
+    /*
+     * This method is used for testing to verify key derivation.
+     */
+    @VisibleForTesting
+    static String getKeyId(SecretKeySpec key) throws GeneralSecurityException {
+        byte[] keyIdBytes = Hkdf.computeHkdf("HmacSha256",
+                key.getEncoded(),
+                null,
+                "keyID".getBytes(StandardCharsets.UTF_8),
+                32);
+        return Hex.encode(keyIdBytes);
+    }
 }
