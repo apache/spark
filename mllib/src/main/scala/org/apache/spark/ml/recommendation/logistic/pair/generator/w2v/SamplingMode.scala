beccb4070201f7de31cd81fc564bd4cbb4ea5a7d
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2024-10-05 12:58:53 +0300

    fix

--- mllib/src/main/scala/org/apache/spark/ml/recommendation/logistic/pair/generator/w2v/SamplingMode.scala
+++ mllib/src/main/scala/org/apache/spark/ml/recommendation/logistic/pair/generator/w2v/SamplingMode.scala
@@ -18,5 +18,5 @@ package org.apache.spark.ml.recommendation.logistic.pair.generator.w2v
 
 private[ml] object SamplingMode extends Enumeration {
   type SamplingMode = Value
-  val WINDOW, ITEM2VEC, ITEM2VEC_POS2NEG = Value
+  val WINDOW, ITEM2VEC = Value
 }
