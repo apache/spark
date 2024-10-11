2af465a606a270df1f4c348c55e29b481d3b4d55
Ruifeng Zheng <ruifengz@apache.org>
2024-10-11 21:43:10 +0800

    nit

--- python/pyspark/sql/connect/expressions.py
+++ python/pyspark/sql/connect/expressions.py
@@ -323,10 +323,8 @@ class LiteralExpression(Expression):
             return StringType()
         elif isinstance(value, decimal.Decimal):
             return DecimalType()
-        elif isinstance(value, datetime.datetime) and is_timestamp_ntz_preferred():
-            return TimestampNTZType()
         elif isinstance(value, datetime.datetime):
-            return TimestampType()
+            return TimestampNTZType() if is_timestamp_ntz_preferred() else TimestampType()
         elif isinstance(value, datetime.date):
             return DateType()
         elif isinstance(value, datetime.timedelta):
