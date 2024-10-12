108a498e8b605181acd83edb10869d89cfe0a778
yangjie01 <yangjie01@baidu.com>
2024-10-12 13:25:12 +0800

    revert asf staging

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -276,7 +276,6 @@ object SparkBuild extends PomBuild {
     resolvers := Seq(
       // Google Mirror of Maven Central, placed first so that it's used instead of flaky Maven Central.
       // See https://storage-download.googleapis.com/maven-central/index.html for more info.
-      "asf-staging" at "https://repository.apache.org/content/groups/staging/",
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
       DefaultMavenRepository,
       Resolver.mavenLocal,
