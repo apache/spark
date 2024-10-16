361a5fd23ba35b91f39b4c109a31151549acf30b
Harsh Motwani <harsh.motwani@databricks.com>
2024-10-16 15:11:49 -0700

    improvements recommended by @MaxGekk

--- sql/core/src/test/scala/org/apache/spark/sql/CollationSQLRegexpSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/CollationSQLRegexpSuite.scala
@@ -113,10 +113,15 @@ class CollationSQLRegexpSuite
           val exception = intercept[SparkRuntimeException] {
             df.collect()
           }
-          assert(exception.getCondition == "INVALID_REGEXP_REPLACE")
-          assert(exception.getMessage.contains("""Could not perform regexp_replace for """ +
-            """`source = "first last"`, `pattern = "(?<first>[a-zA-Z]+) (?<last>[a-zA-Z]+)"`, """ +
-            """`replacement = "$3 $1"` and `position = 1`."""))
+          checkError(
+            exception = exception,
+            condition = "INVALID_REGEXP_REPLACE",
+            parameters = Map(
+              "source" -> "first last",
+              "pattern" -> "(?<first>[a-zA-Z]+) (?<last>[a-zA-Z]+)",
+              "replacement" -> "$3 $1",
+              "position" -> "1")
+          )
           assert(exception.getCause.getMessage.contains("No group 3"))
         }
       }
