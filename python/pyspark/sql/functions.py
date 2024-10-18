6b9b3c04f9a78a903b05d81209b41836e3061523
panbingkun <panbingkun@baidu.com>
2024-10-18 10:46:06 +0200

    Revert "[SPARK-49909][SQL][3.5] Fix the pretty name of some expressions"
    ### What changes were proposed in this pull request?
    The pr aims to revert https://github.com/apache/spark/pull/48393.
    This reverts commit 4472fb26ec4af3398389cd4f96bc9d94663895a9.
    
    ### Why are the changes needed?
    Only revert.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Pass GA
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #48531 from panbingkun/branch-3.5_SPARK-49909_revert.
    
    Authored-by: panbingkun <panbingkun@baidu.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- python/pyspark/sql/functions.py
+++ python/pyspark/sql/functions.py
@@ -6710,31 +6710,31 @@ def dateadd(start: "ColumnOrName", days: Union["ColumnOrName", int]) -> Column:
     >>> spark.createDataFrame(
     ...     [('2015-04-08', 2,)], ['dt', 'add']
     ... ).select(sf.dateadd("dt", 1)).show()
-    +--------------+
-    |dateadd(dt, 1)|
-    +--------------+
-    |    2015-04-09|
-    +--------------+
+    +---------------+
+    |date_add(dt, 1)|
+    +---------------+
+    |     2015-04-09|
+    +---------------+
 
     >>> import pyspark.sql.functions as sf
     >>> spark.createDataFrame(
     ...     [('2015-04-08', 2,)], ['dt', 'add']
     ... ).select(sf.dateadd("dt", sf.lit(2))).show()
-    +--------------+
-    |dateadd(dt, 2)|
-    +--------------+
-    |    2015-04-10|
-    +--------------+
+    +---------------+
+    |date_add(dt, 2)|
+    +---------------+
+    |     2015-04-10|
+    +---------------+
 
     >>> import pyspark.sql.functions as sf
     >>> spark.createDataFrame(
     ...     [('2015-04-08', 2,)], ['dt', 'add']
     ... ).select(sf.dateadd("dt", -1)).show()
-    +---------------+
-    |dateadd(dt, -1)|
-    +---------------+
-    |     2015-04-07|
-    +---------------+
+    +----------------+
+    |date_add(dt, -1)|
+    +----------------+
+    |      2015-04-07|
+    +----------------+
     """
     days = lit(days) if isinstance(days, int) else days
     return _invoke_function_over_columns("dateadd", start, days)
