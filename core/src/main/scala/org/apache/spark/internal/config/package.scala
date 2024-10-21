e2446e1a86ab7ddeec0439b6f34d4ab7a6c956cc
Mingkang Li <mingkang.li@databricks.com>
2024-10-21 11:58:13 -0700

    update docs

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -2457,9 +2457,8 @@ package object config {
   private[spark] val KILL_ON_FATAL_DEPTH =
     ConfigBuilder("spark.executor.killOnFatalError.depth")
       .doc("The max depth of the exception chain in a failed task Spark will search for a fatal " +
-        "error to check whether it should kill an executor/worker/master. 0 means not" +
-        " checking any fatal error, 1 means checking only the exception but not the cause," +
-        " and so on.")
+        "error to check whether it should kill the JVM process. 0 means not checking any fatal" +
+        " error, 1 means checking only the exception but not the cause, and so on.")
       .internal()
       .version("3.1.0")
       .intConf
