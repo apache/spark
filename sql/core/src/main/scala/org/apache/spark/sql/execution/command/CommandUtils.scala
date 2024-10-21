6b703cdb3da357be988d41b09d8873aa56d5a658
Stevo Mitric <stevo.mitric@databricks.com>
2024-10-21 18:47:20 +0200

    analyze fix

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/CommandUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/CommandUtils.scala
@@ -411,7 +411,7 @@ object CommandUtils extends Logging {
       case DoubleType | FloatType => fixedLenTypeStruct
       case BooleanType => fixedLenTypeStruct
       case _: DatetimeType => fixedLenTypeStruct
-      case BinaryType | StringType =>
+      case BinaryType | _: StringType =>
         // For string and binary type, we don't compute min, max or histogram
         val nullLit = Literal(null, col.dataType)
         struct(
