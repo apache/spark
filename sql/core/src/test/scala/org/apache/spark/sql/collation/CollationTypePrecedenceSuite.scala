aa99dbdcded0e0d2fd800a700e489c5ff0481d87
Stefan Kandic <stefan.kandic@databricks.com>
2024-10-25 22:02:47 +0200

    more tests

--- sql/core/src/test/scala/org/apache/spark/sql/collation/CollationTypePrecedenceSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/collation/CollationTypePrecedenceSuite.scala
@@ -33,15 +33,158 @@ class CollationTypePrecedenceSuite extends DatasourceV2SQLBase with AdaptiveSpar
     assert(exception.getCondition === errorClass)
   }
 
-  test("asdfffff") {
-    // TODO: add tests for variable/subq etc
-    sql(s"DECLARE a = 'a'")
-    sql(s"DECLARE b = 'a' collate unicode")
+  private def assertCollation(df: => DataFrame, collation: String): Unit = {
+    checkAnswer(df, Seq(Row(collation)))
+  }
 
-//    sql(s"SELECT a = 'a'")
-    sql(s"SELECT a = CAST('5' as STRING)")
-    sql(s"SELECT a = (SELECT b COLLATE UNICODE)")
-//    sql(s"SELECT a = b")
+  private def assertExplicitMismatch(df: => DataFrame): Unit =
+    assertThrowsError(df, "COLLATION_MISMATCH.EXPLICIT")
+
+  private def assertImplicitMismatch(df: => DataFrame): Unit =
+    assertThrowsError(df, "COLLATION_MISMATCH.IMPLICIT")
+
+  test("explicit collation propagates up") {
+    checkAnswer(
+      sql(s"SELECT COLLATION('a' collate unicode)"),
+      Row("UNICODE"))
+
+    checkAnswer(
+      sql(s"SELECT COLLATION('a' collate unicode || 'b')"),
+      Row("UNICODE"))
+
+    checkAnswer(
+      sql(s"SELECT COLLATION(SUBSTRING('a' collate unicode, 0, 1))"),
+      Row("UNICODE"))
+
+    checkAnswer(
+      sql(s"SELECT COLLATION(SUBSTRING('a' collate unicode, 0, 1) || 'b')"),
+      Row("UNICODE"))
+
+    assertExplicitMismatch(
+      sql(s"SELECT COLLATION('a' collate unicode || 'b' collate utf8_lcase)"))
+
+    assertExplicitMismatch(
+      sql(s"""
+           |SELECT COLLATION(
+           |  SUBSTRING('a' collate unicode, 0, 1) ||
+           |  SUBSTRING('b' collate utf8_lcase, 0, 1))
+           |""".stripMargin))
+  }
+
+  test("implicit collation in columns") {
+    val tableName = "implicit_coll_tbl"
+    withTable(tableName) {
+      sql(s"""
+           |CREATE TABLE $tableName (
+           |  c1 STRING COLLATE UNICODE,
+           |  c2 STRING COLLATE UNICODE_CI,
+           |  c3 STRUCT<col1: STRING COLLATE UTF8_LCASE>)
+           |""".stripMargin)
+      sql(s"INSERT INTO $tableName VALUES ('a', 'b', named_struct('col1', 'c'))")
+
+      checkAnswer(
+        sql(s"SELECT COLLATION(c1 || 'a') FROM $tableName"),
+        Seq(Row("UNICODE")))
+
+      checkAnswer(
+        sql(s"SELECT COLLATION(SUBSTRING(c1, 0, 1) || 'a') FROM $tableName"),
+        Seq(Row("UNICODE")))
+
+      assertImplicitMismatch(sql(s"SELECT COLLATION(c1 || c2) FROM $tableName"))
+      assertImplicitMismatch(sql(s"SELECT COLLATION(c1 || c3.col1) FROM $tableName"))
+      assertImplicitMismatch(
+        sql(s"SELECT COLLATION(SUBSTRING(c1, 0, 1) || c2) FROM $tableName"))
+    }
+  }
+
+  test("variables have implicit collation") {
+    sql(s"DECLARE v1 = 'a'")
+    sql(s"DECLARE v2 = 'b' collate utf8_lcase")
+
+    checkAnswer(
+      sql(s"SELECT COLLATION(v1 || 'a')"),
+      Row("UTF8_BINARY"))
+
+    checkAnswer(
+      sql(s"SELECT COLLATION(v2 || 'a')"),
+      Row("UTF8_LCASE"))
+
+    checkAnswer(
+      sql(s"SELECT COLLATION(v2 || 'a' COLLATE UTF8_BINARY)"),
+      Row("UTF8_BINARY"))
+
+    checkAnswer(
+      sql(s"SELECT COLLATION(SUBSTRING(v2, 0, 1) || 'a')"),
+      Row("UTF8_LCASE"))
+
+    assertImplicitMismatch(sql(s"SELECT COLLATION(v1 || v2)"))
+    assertImplicitMismatch(sql(s"SELECT COLLATION(SUBSTRING(v1, 0, 1) || v2)"))
+  }
+
+  test("subqueries have implicit collation strength") {
+    // TODO:
+  }
+
+  test("literals collation strength") {
+
+  }
+
+  test("struct test") {
+    val tableName = "struct_tblll"
+    withTable(tableName) {
+      sql(s"""
+           |CREATE TABLE $tableName (
+           |  c1 STRUCT<col1: STRING COLLATE UNICODE_CI, col2: STRING COLLATE UNICODE>,
+           |  c2 STRUCT<col1: STRUCT<col1: STRING COLLATE UNICODE_CI>>)
+           |USING $dataSource
+           |""".stripMargin)
+      sql(s"INSERT INTO $tableName VALUES (named_struct('col1', 'a', 'col2', 'b'), named_struct('col1', named_struct('col1', 'c')))")
+//      sql(s"INSERT INTO $tableName VALUES (named_struct('col1', 'a', 'col2', 'b'))")
+
+      checkAnswer(
+        sql(s"SELECT COLLATION(c2.col1.col1 || 'a') FROM $tableName"),
+        Seq(Row("UNICODE_CI")))
+
+      checkAnswer(
+        sql(s"SELECT COLLATION(c1.col1 || 'a') FROM $tableName"),
+        Seq(Row("UNICODE_CI")))
+    }
+  }
+
+  test("array test") {
+    val tableName = "array_tbl"
+    withTable(tableName) {
+      sql(s"""
+           |CREATE TABLE $tableName (
+           |  c1 STRING COLLATE UNICODE,
+           |  c2 ARRAY<STRING COLLATE UNICODE_CI>)
+           |USING $dataSource
+           |""".stripMargin)
+
+      sql(s"INSERT INTO $tableName VALUES ('a', array('b', 'c'))")
+
+      checkAnswer(
+        sql(s"SELECT collation(element_at(array('a', 'b' collate utf8_lcase), 1))"),
+        Seq(Row("UTF8_LCASE")))
+
+      assertExplicitMismatch(
+        sql(s"SELECT collation(element_at(array('a' collate unicode, 'b' collate utf8_lcase), 1))")
+      )
+
+      checkAnswer(
+        sql(s"SELECT collation(element_at(array('a', 'b' collate utf8_lcase), 1) || c1) from $tableName"),
+        Seq(Row("UTF8_LCASE")))
+
+      checkAnswer(
+        sql(s"SELECT collation(element_at(array_append(c2, 'd'), 1)) FROM $tableName"),
+        Seq(Row("UNICODE_CI"))
+      )
+
+      checkAnswer(
+        sql(s"SELECT collation(element_at(array_append(c2, 'd' collate utf8_lcase), 1)) FROM $tableName"),
+        Seq(Row("UTF8_LCASE"))
+      )
+    }
   }
 
   test("user defined cast") {
