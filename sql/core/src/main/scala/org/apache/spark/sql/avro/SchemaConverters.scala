545de9aed022f95261998929170c9b2f97da6b4c
Wenchen Fan <cloud0fan@gmail.com>
2024-11-01 02:42:20 +0800

    Update sql/core/src/main/scala/org/apache/spark/sql/avro/SchemaConverters.scala

--- sql/core/src/main/scala/org/apache/spark/sql/avro/SchemaConverters.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/avro/SchemaConverters.scala
@@ -27,7 +27,7 @@ import org.apache.avro.LogicalTypes.{Decimal, _}
 import org.apache.avro.Schema.Type._
 
 import org.apache.spark.annotation.DeveloperApi
-import org.apache.spark.internal.{Logging, MDC }
+import org.apache.spark.internal.{Logging, MDC}
 import org.apache.spark.internal.LogKeys.{FIELD_NAME, FIELD_TYPE, RECURSIVE_DEPTH}
 import org.apache.spark.sql.avro.AvroOptions.RECURSIVE_FIELD_MAX_DEPTH_LIMIT
 import org.apache.spark.sql.catalyst.parser.CatalystSqlParser
