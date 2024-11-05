43385b6f13995f79b26e160d71293c717d146c95
Dongjoon Hyun <dongjoon@apache.org>
2024-11-04 16:53:28 -0800

    [SPARK-50222][CORE] Support `spark.submit.appName`

--- sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/SparkSession.scala
@@ -889,6 +889,9 @@ object SparkSession extends api.BaseSparkSessionCompanion with Logging {
 
         // No active nor global default session. Create a new one.
         val sparkContext = userSuppliedContext.getOrElse {
+          // Override appName with the submitted appName
+          sparkConf.getOption("spark.submit.appName")
+            .map(sparkConf.setAppName)
           // set a random app name if not given.
           if (!sparkConf.contains("spark.app.name")) {
             sparkConf.setAppName(java.util.UUID.randomUUID().toString)
