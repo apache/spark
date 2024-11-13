85f8303935177563da06c5aa88bc67a41d499179
Herman van Hovell <herman@databricks.com>
2024-11-12 21:00:10 -0400

    More pyspark fixes

--- python/pyspark/java_gateway.py
+++ python/pyspark/java_gateway.py
@@ -161,6 +161,8 @@ def launch_gateway(conf=None, popen_kwargs=None):
     java_import(gateway.jvm, "org.apache.spark.mllib.api.python.*")
     java_import(gateway.jvm, "org.apache.spark.resource.*")
     # TODO(davies): move into sql
+    java_import(gateway.jvm, "org.apache.spark.sql.Encoders")
+    java_import(gateway.jvm, "org.apache.spark.sql.functions")
     java_import(gateway.jvm, "org.apache.spark.sql.classic.*")
     java_import(gateway.jvm, "org.apache.spark.sql.api.python.*")
     java_import(gateway.jvm, "org.apache.spark.sql.hive.*")
