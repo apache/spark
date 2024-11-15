b393797fc9c28094b86261a61fdf76ead647ae97
Haejoon Lee <haejoon.lee@databricks.com>
2024-11-15 16:00:01 +0900

    Reuse COLUMN_NOT_DEFINED_IN_TABLE

--- sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCWriteSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/jdbc/JDBCWriteSuite.scala
@@ -191,11 +191,13 @@ class JDBCWriteSuite extends SharedSparkSession with BeforeAndAfter {
         exception = intercept[AnalysisException] {
           df2.write.mode(SaveMode.Append).jdbc(url, "TEST.APPENDTEST", new Properties())
         },
-        condition = "COLUMN_NOT_FOUND_IN_SCHEMA",
+        condition = "COLUMN_NOT_DEFINED_IN_TABLE",
         parameters = Map(
+          "colType" -> "\"STRING\"",
           "colName" -> "`NAME`",
-          "tableSchema" ->
-            "Some(StructType(StructField(name,StringType,true),StructField(id,IntegerType,true)))"))
+          "tableName" -> "`TEST`.`APPENDTEST`",
+          "tableCols" -> "`NAME`, `ID`")
+      )
     }
 
     withSQLConf(SQLConf.CASE_SENSITIVE.key -> "false") {
@@ -224,11 +226,13 @@ class JDBCWriteSuite extends SharedSparkSession with BeforeAndAfter {
           df3.write.mode(SaveMode.Overwrite).option("truncate", true)
             .jdbc(url1, "TEST.TRUNCATETEST", properties)
         },
-        condition = "COLUMN_NOT_FOUND_IN_SCHEMA",
+        condition = "COLUMN_NOT_DEFINED_IN_TABLE",
         parameters = Map(
+          "colType" -> "\"INT\"",
           "colName" -> "`seq`",
-          "tableSchema" ->
-            "Some(StructType(StructField(name,StringType,true),StructField(id,IntegerType,true)))"))
+          "tableName" -> "`TEST`.`TRUNCATETEST`",
+          "tableCols" -> "`name`, `id`, `seq`")
+      )
     } finally {
       JdbcDialects.unregisterDialect(testH2Dialect)
       JdbcDialects.registerDialect(H2Dialect())
@@ -256,11 +260,13 @@ class JDBCWriteSuite extends SharedSparkSession with BeforeAndAfter {
       exception = intercept[AnalysisException] {
         df2.write.mode(SaveMode.Append).jdbc(url, "TEST.INCOMPATIBLETEST", new Properties())
       },
-      condition = "COLUMN_NOT_FOUND_IN_SCHEMA",
+      condition = "COLUMN_NOT_DEFINED_IN_TABLE",
       parameters = Map(
+        "colType" -> "\"INT\"",
         "colName" -> "`seq`",
-        "tableSchema" ->
-          "Some(StructType(StructField(name,StringType,true),StructField(id,IntegerType,true)))"))
+        "tableName" -> "`TEST`.`INCOMPATIBLETEST`",
+        "tableCols" -> "`name`, `id`, `seq`")
+    )
   }
 
   test("INSERT to JDBC Datasource") {
