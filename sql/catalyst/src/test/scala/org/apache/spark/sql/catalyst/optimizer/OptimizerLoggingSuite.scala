cd08241a5f3818f1c8e4f72c78ec5734281815c2
Haoyu Qi <avery.qi@databricks.com>
2024-11-21 22:11:10 -0800

    Update OptimizerLoggingSuite.scala

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/OptimizerLoggingSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/OptimizerLoggingSuite.scala
@@ -160,7 +160,7 @@ class OptimizerLoggingSuite extends PlanTest {
     val query = input.select($"a", $"b").where(inSetPredicate)
     val analyzed = query.analyze
 
-    assert(query.toString.contains("'a INSET 1, 2"))
+    assert(query.toString.contains("'a INSET (1, 2)"))
     assert(analyzed.toString.contains("INSET 1, 2"))
   }
 }
