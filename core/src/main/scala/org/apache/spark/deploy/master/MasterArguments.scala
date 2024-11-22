586e980cb20421541be48f5458dc0cb2f003e92b
Dongjoon Hyun <dongjoon@apache.org>
2024-11-22 12:19:39 -0800

    [SPARK-50397][CORE] Remove deprecated `--ip` and `-i` arguments from Master/Worker

--- core/src/main/scala/org/apache/spark/deploy/master/MasterArguments.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/MasterArguments.scala
@@ -63,11 +63,6 @@ private[master] class MasterArguments(args: Array[String], conf: SparkConf) exte
 
   @tailrec
   private def parse(args: List[String]): Unit = args match {
-    case ("--ip" | "-i") :: value :: tail =>
-      Utils.checkHost(value)
-      host = value
-      parse(tail)
-
     case ("--host" | "-h") :: value :: tail =>
       Utils.checkHost(value)
       host = value
@@ -103,7 +98,6 @@ private[master] class MasterArguments(args: Array[String], conf: SparkConf) exte
       "Usage: Master [options]\n" +
       "\n" +
       "Options:\n" +
-      "  -i HOST, --ip HOST     Hostname to listen on (deprecated, please use --host or -h) \n" +
       "  -h HOST, --host HOST   Hostname to listen on\n" +
       "  -p PORT, --port PORT   Port to listen on (default: 7077)\n" +
       "  --webui-port PORT      Port for web UI (default: 8080)\n" +
