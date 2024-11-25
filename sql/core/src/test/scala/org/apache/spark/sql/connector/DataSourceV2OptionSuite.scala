f6a2ab9d545fbfea967da4d5a84b59138b2b3e84
Stefan Kandic <stefan.kandic@databricks.com>
2024-11-25 15:47:02 +0100

    fix compilation issues from new master commit

--- sql/core/src/test/scala/org/apache/spark/sql/connector/DataSourceV2OptionSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/connector/DataSourceV2OptionSuite.scala
@@ -102,7 +102,7 @@ class DataSourceV2OptionSuite extends DatasourceV2SQLBase {
       val df = sql(s"INSERT INTO $t1 WITH (`write.split-size` = 10) VALUES (1, 'a'), (2, 'b')")
 
       var collected = df.queryExecution.optimizedPlan.collect {
-        case CommandResult(_, AppendData(relation: DataSourceV2Relation, _, _, _, _, _), _, _) =>
+        case CommandResult(_, AppendData(relation: DataSourceV2Relation, _, _, _, _, _, _), _, _) =>
           assert(relation.options.get("write.split-size") == "10")
       }
       assert (collected.size == 1)
@@ -135,7 +135,7 @@ class DataSourceV2OptionSuite extends DatasourceV2SQLBase {
       assert(captured.size === 1)
       val qe = captured.head
       var collected = qe.optimizedPlan.collect {
-        case AppendData(_: DataSourceV2Relation, _, writeOptions, _, _, _) =>
+        case AppendData(_: DataSourceV2Relation, _, writeOptions, _, _, _, _) =>
           assert(writeOptions("write.split-size") == "10")
       }
       assert (collected.size == 1)
@@ -162,7 +162,7 @@ class DataSourceV2OptionSuite extends DatasourceV2SQLBase {
       assert(captured.size === 1)
       val qe = captured.head
       var collected = qe.optimizedPlan.collect {
-        case AppendData(_: DataSourceV2Relation, _, writeOptions, _, _, _) =>
+        case AppendData(_: DataSourceV2Relation, _, writeOptions, _, _, _, _) =>
           assert(writeOptions("write.split-size") == "10")
       }
       assert (collected.size == 1)
