2020a4c2168fb277c899a42936dd72073d8a177c
Stefan Kandic <stefan.kandic@databricks.com>
2024-11-27 18:17:10 +0100

    bring back the if/else

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveDefaultStringTypes.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveDefaultStringTypes.scala
@@ -40,10 +40,10 @@ class ResolveDefaultStringTypes(replaceWithTempType: Boolean) extends Rule[Logic
 
   def apply(plan: LogicalPlan): LogicalPlan = {
 
-    val newPlan = plan match {
-      case _ if isDDLCommand(plan) => transformDDL(plan)
-      case _ if isDefaultSessionCollationUsed => plan
-      case _ => transformPlan(plan, stringTypeForDMLCommand)
+    val newPlan = if (isDDLCommand(plan)) {
+      transformDDL(plan)
+    } else {
+      transformPlan(plan, stringTypeForDMLCommand)
     }
 
     if (!replaceWithTempType || newPlan.fastEquals(plan)) {
