aaf859053c7a6200527e0ae1010a7bbc10727ccf
panbingkun <panbingkun@apache.org>
2024-11-29 09:15:37 +0800

    [SPARK-50448][SQL][TESTS] Extract postgres image common conf as PostgresDatabaseOnDocker
    ### What changes were proposed in this pull request?
    The  pr aims to extract `postgres` image `common` conf as `PostgresDatabaseOnDocker`.
    
    ### Why are the changes needed?
    Reduce the cost of the next `postgres` image upgrade.
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Pass existed UT & GA.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #49004 from panbingkun/SPARK-50448.
    
    Authored-by: panbingkun <panbingkun@apache.org>
    Signed-off-by: panbingkun <panbingkun@apache.org>

--- connector/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/querytest/PostgresSQLQueryTestSuite.scala
+++ connector/docker-integration-tests/src/test/scala/org/apache/spark/sql/jdbc/querytest/PostgresSQLQueryTestSuite.scala
@@ -38,23 +38,13 @@ import org.apache.spark.tags.DockerTest
  * }}}
  */
 @DockerTest
-class PostgreSQLQueryTestSuite extends CrossDbmsQueryTestSuite {
+class PostgresSQLQueryTestSuite extends CrossDbmsQueryTestSuite {
 
   val DATABASE_NAME = CrossDbmsQueryTestSuite.POSTGRES
   // Scope to only subquery directory for now.
   protected val customInputFilePath: String = new File(inputFilePath, "subquery").getAbsolutePath
 
-  override val db = new DatabaseOnDocker {
-    override val imageName = sys.env.getOrElse("POSTGRES_DOCKER_IMAGE_NAME", "postgres:17.2-alpine")
-    override val env = Map(
-      "POSTGRES_PASSWORD" -> "rootpass"
-    )
-    override val usesIpc = false
-    override val jdbcPort = 5432
-
-    override def getJdbcUrl(ip: String, port: Int): String =
-      s"jdbc:postgresql://$ip:$port/postgres?user=postgres&password=rootpass"
-  }
+  override val db = new PostgresDatabaseOnDocker
 
   override def dataPreparation(conn: Connection): Unit = {
     conn.prepareStatement(
