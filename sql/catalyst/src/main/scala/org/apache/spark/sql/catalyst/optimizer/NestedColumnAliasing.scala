7ddf36d03c74232bd65cd5d024a07d0c7aa69653
Thomas Rohwer <thomas.rohwer@deutschebahn.com>
2024-12-05 12:49:27 +0000

    fix code format

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/NestedColumnAliasing.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/NestedColumnAliasing.scala
@@ -185,16 +185,16 @@ object NestedColumnAliasing {
       plan: LogicalPlan,
       nestedFieldToAlias: Map[Expression, Alias],
       attrToAliases: AttributeMap[Seq[Alias]]): LogicalPlan = {
-    // withNewChildren is dangerous for Generate; one has to adjust unrequiredChildIndex accordingly in this case
-    val withNewChildren= plan match {
-      case g: Generate => {
+    // withNewChildren is dangerous for Generate;
+    // one has to adjust unrequiredChildIndex accordingly in this case
+    val withNewChildren = plan match {
+      case g: Generate =>
         val unrequiredSet = g.unrequiredChildIndex.toSet
-        val flagRes= g.child.output.zipWithIndex.flatMap( t =>
+        val flagRes = g.child.output.zipWithIndex.flatMap( t =>
           attrToAliases.getOrElse(t._1, Seq(t._1)).map( e => ( e, unrequiredSet.contains(t._2) ) )
         )
-        val unrequiredChildIndex= flagRes.map(_._2).zipWithIndex.filter(t => t._1).map(_._2)
-        g.copy(child=Project(flagRes.map(_._1), g.child), unrequiredChildIndex=unrequiredChildIndex)
-      }
+        val unrequiredChildIndex = flagRes.map(_._2).zipWithIndex.filter(t => t._1).map(_._2)
+        g.copy(child = Project(flagRes.map(_._1), g.child), unrequiredChildIndex = unrequiredChildIndex)
       case _ => plan.withNewChildren(plan.children.map { plan =>
         Project(plan.output.flatMap(a => attrToAliases.getOrElse(a, Seq(a))), plan)})
     }
