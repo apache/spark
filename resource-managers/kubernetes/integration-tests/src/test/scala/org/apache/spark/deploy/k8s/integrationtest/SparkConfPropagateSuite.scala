dd2b72c87d179d4e0900e59dccff98dde31744fe
panbingkun <panbingkun@apache.org>
2024-12-10 11:07:40 +0800

    fix style

--- resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/SparkConfPropagateSuite.scala
+++ resource-managers/kubernetes/integration-tests/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/SparkConfPropagateSuite.scala
@@ -25,8 +25,8 @@ import scala.io.Source
 private[spark] trait SparkConfPropagateSuite { k8sSuite: KubernetesSuite =>
   import KubernetesSuite.{k8sTestTag, SPARK_PI_MAIN_CLASS}
 
-  ignore("Verify logging configuration is picked from the provided SPARK_CONF_DIR/log4j2.properties",
-    k8sTestTag) {
+  ignore("Verify logging configuration is picked from " +
+    "the provided SPARK_CONF_DIR/log4j2.properties", k8sTestTag) {
     val loggingConfigFileName = "log-config-test-log4j.properties"
     val loggingConfURL: URL = this.getClass.getClassLoader.getResource(loggingConfigFileName)
     assert(loggingConfURL != null, "Logging configuration file not available.")
