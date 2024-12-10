bbb7277596c466298f6ad62f92e6f74f03cf02eb
Haejoon Lee <haejoon.lee@databricks.com>
2024-12-10 11:03:24 +0900

    fix mypy

--- python/pyspark/errors/exceptions/connect.py
+++ python/pyspark/errors/exceptions/connect.py
@@ -54,18 +54,14 @@ def convert_exception(
     resp: Optional[pb2.FetchErrorDetailsResponse],
     display_server_stacktrace: bool = False,
 ) -> SparkConnectException:
-    classes = info.metadata.get("classes")
-    if classes:
-        classes = json.loads(classes)
-    else:
-        classes = []
+    raw_classes = info.metadata.get("classes")
+    classes: List[str] = json.loads(raw_classes) if raw_classes else []
     sql_state = info.metadata.get("sqlState")
     error_class = info.metadata.get("errorClass")
-    message_parameters = info.metadata.get("messageParameters")
-    if message_parameters:
-        message_parameters = json.loads(message_parameters)
-    else:
-        message_parameters = {}
+    raw_message_parameters = info.metadata.get("messageParameters")
+    message_parameters: Dict[str, str] = (
+        json.loads(raw_message_parameters) if raw_message_parameters else {}
+    )
     stacktrace: Optional[str] = None
 
     if resp is not None and resp.HasField("root_error_idx"):
