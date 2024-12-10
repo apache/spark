f7db0612daaef9db25eafe9e65cfccb2f9596107
Xinrong Meng <xinrong@apache.org>
2024-12-10 15:40:01 +0800

    tests

--- python/pyspark/sql/tests/connect/test_parity_udtf.py
+++ python/pyspark/sql/tests/connect/test_parity_udtf.py
@@ -76,6 +76,10 @@ class UDTFParityTests(BaseUDTFTestsMixin, ReusedConnectTestCase):
     def test_udtf_access_spark_session(self):
         super().test_udtf_access_spark_session()
 
+    @unittest.skip("TODO(SPARK-50393): support df.asTable() in Spark Connect")
+    def test_df_asTable(self):
+        super().test_df_asTable()
+
     def _add_pyfile(self, path):
         self.spark.addArtifacts(path, pyfile=True)
 
