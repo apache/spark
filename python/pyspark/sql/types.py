1f6cb60e3c78dc8592c0b76435f22d0f36194ed7
Ruifeng Zheng <ruifengz@apache.org>
2024-12-12 10:24:09 +0800

    [SPARK-50544][PYTHON][CONNECT] Implement `StructType.toDDL`
    ### What changes were proposed in this pull request?
    Implement `StructType.toDDL`
    
    ### Why are the changes needed?
    we has `fromDDL` in python side, but the `toDDL` is still missing
    
    ### Does this PR introduce _any_ user-facing change?
    new feature
    
    ```
    In [1]: spark.range(100).schema.toDDL()
    Out[1]: 'id BIGINT NOT NULL'
    ```
    
    ### How was this patch tested?
    added tests
    
    ### Was this patch authored or co-authored using generative AI tooling?
    no
    
    Closes #49142 from zhengruifeng/struct_type_to_ddl.
    
    Authored-by: Ruifeng Zheng <ruifengz@apache.org>
    Signed-off-by: Ruifeng Zheng <ruifengz@apache.org>

--- python/pyspark/sql/types.py
+++ python/pyspark/sql/types.py
@@ -1555,6 +1555,25 @@ class StructType(DataType):
             field._build_formatted_string(prefix, stringConcat, depth)
         return stringConcat.toString()
 
+    def toDDL(self) -> str:
+        from pyspark.sql.utils import is_remote
+
+        if is_remote():
+            from pyspark.sql.connect.session import SparkSession
+
+            session = SparkSession.getActiveSession()
+            assert session is not None
+            return session._client._analyze(  # type: ignore[return-value]
+                method="json_to_ddl", json_string=self.json()
+            ).ddl_string
+
+        else:
+            from py4j.java_gateway import JVMView
+
+            sc = get_active_spark_context()
+            assert sc._jvm is not None
+            return cast(JVMView, sc._jvm).PythonSQLUtils.jsonToDDL(self.json())
+
 
 class VariantType(AtomicType):
     """
