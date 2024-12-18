2b1369b4abed643d986ee6a22ec86371eadb831e
Sebastian Hillig <101127480+sebastianhillig-db@users.noreply.github.com>
2024-12-19 08:47:21 +0900

    [SPARK-50499][PYTHON] Expose metrics from BasePythonRunner
    ### What changes were proposed in this pull request?
    
    Expose additional metrics from BasePythonRunner for all Python UDF executions: bootTime, initTime, and totalTime.
    
    ### Why are the changes needed?
    
    Metrics are currently only exposed as logs, but can be helpful to debug slow queries.
    
    ### Does this PR introduce _any_ user-facing change?
    
    ### How was this patch tested?
    
    Extended existing testcases.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #49076 from sebastianhillig-db/add-basepythonrunner-metrics.
    
    Lead-authored-by: Sebastian Hillig <101127480+sebastianhillig-db@users.noreply.github.com>
    Co-authored-by: Sebastian Hillig <sebastian.hillig@databricks.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/PythonUDFRunner.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/PythonUDFRunner.scala
@@ -36,7 +36,7 @@ abstract class BasePythonUDFRunner(
     pythonMetrics: Map[String, SQLMetric],
     jobArtifactUUID: Option[String])
   extends BasePythonRunner[Array[Byte], Array[Byte]](
-    funcs.map(_._1), evalType, argOffsets, jobArtifactUUID) {
+    funcs.map(_._1), evalType, argOffsets, jobArtifactUUID, pythonMetrics) {
 
   override val pythonExec: String =
     SQLConf.get.pysparkWorkerPythonExecutable.getOrElse(
