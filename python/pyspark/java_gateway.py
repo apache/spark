13dde7fcff89f05a136272836baa501f82c33a35
Arik <noam@zouz.io>
2024-12-25 11:18:30 +0200

    fix formatting

--- python/pyspark/java_gateway.py
+++ python/pyspark/java_gateway.py
@@ -71,7 +71,7 @@ def launch_gateway(conf=None, popen_kwargs=None):
                 # On windows escape & with triple carets to avoid it being removed by cmd.exe
                 if on_windows:
                     v = v.replace("&", "^^^&")
-                
+
                 command += ["--conf", "%s=%s" % (k, v)]
         submit_args = os.environ.get("PYSPARK_SUBMIT_ARGS", "pyspark-shell")
         if os.environ.get("SPARK_TESTING"):
