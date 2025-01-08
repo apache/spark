21929cc06c574cd8793eead4ade4868072d69624
Stefan Kandic <stefan.kandic@databricks.com>
2025-01-08 11:32:16 +0100

    remove runtime error from jsonValue method

--- sql/api/src/main/scala/org/apache/spark/sql/types/StringType.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/types/StringType.scala
@@ -19,7 +19,6 @@ package org.apache.spark.sql.types
 
 import org.json4s.JsonAST.{JString, JValue}
 
-import org.apache.spark.SparkRuntimeException
 import org.apache.spark.annotation.Stable
 import org.apache.spark.sql.catalyst.util.CollationFactory
 
@@ -132,12 +131,8 @@ case object StringType
  * String type that was the result of coercing two different non-explicit collations.
  */
 private[spark] case object IndeterminateStringType
-    extends StringType(CollationFactory.INDETERMINATE_COLLATION_ID) {
+  extends StringType(CollationFactory.INDETERMINATE_COLLATION_ID)
 
-  override def jsonValue: JValue = throw new SparkRuntimeException(
-    errorClass = "INDETERMINATE_COLLATION_NOT_SERIALIZABLE",
-    messageParameters = Map.empty)
-}
 
 sealed trait StringConstraint
 
