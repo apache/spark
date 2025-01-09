c60c0e41195922106df15d45a0e35cafb686c788
Haejoon Lee <haejoon.lee@databricks.com>
2025-01-09 16:01:08 +0900

    [SPARK-50719][PYTHON] Support interruptOperation for PySpark

--- python/pyspark/sql/session.py
+++ python/pyspark/sql/session.py
@@ -2253,12 +2253,14 @@ class SparkSession(SparkConversionMixin):
 
         return python_list
 
-    @remote_only
     def interruptOperation(self, op_id: str) -> List[str]:
         """
         Interrupt an operation of this session with the given operationId.
 
         .. versionadded:: 3.5.0
+
+        .. versionchanged:: 4.0.0
+            Supports Spark Classic.
 
         Returns
         -------
@@ -2269,10 +2271,15 @@ class SparkSession(SparkConversionMixin):
         -----
         There is still a possibility of operation finishing just as it is interrupted.
         """
-        raise PySparkRuntimeError(
-            errorClass="ONLY_SUPPORTED_WITH_SPARK_CONNECT",
-            messageParameters={"feature": "SparkSession.interruptOperation"},
-        )
+        java_list = self._jsparkSession.interruptOperation(op_id)
+        python_list = list()
+
+        # Use iterator to manually iterate through Java list
+        java_iterator = java_list.iterator()
+        while java_iterator.hasNext():
+            python_list.append(str(java_iterator.next()))
+
+        return python_list
 
     def addTag(self, tag: str) -> None:
         """
