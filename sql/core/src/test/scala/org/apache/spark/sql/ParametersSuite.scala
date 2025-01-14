cd8516473f9d09038cbd152d334929543b9ef389
Max Gekk <max.gekk@gmail.com>
2025-01-14 15:25:18 +0300

    Add one more test case

--- sql/core/src/test/scala/org/apache/spark/sql/ParametersSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/ParametersSuite.scala
@@ -786,6 +786,8 @@ class ParametersSuite extends QueryTest with SharedSparkSession with PlanTest {
     checkAnswer(spark.sql(
       "execute immediate 'select :param1, :param2' using :param2 as param1, 43 as param2",
       Map("param2" -> 42)), Row(42, 43))
+    checkAnswer(spark.sql("execute immediate 'select :param' using 0 as param",
+      Map("param" -> 42)), Row(0))
     checkError(
       exception = intercept[AnalysisException] {
         spark.sql("execute immediate 'select :param1, :param2' using 1 as param1",
