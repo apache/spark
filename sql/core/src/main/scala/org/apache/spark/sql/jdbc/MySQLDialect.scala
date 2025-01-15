368f937411fda7388b5d47ef78fe4b03d6a8fc34
Xiaoguang Sun <sunxiaoguang@gmail.com>
2025-01-15 17:54:07 +0800

    Throw exception with not supported types
    Signed-off-by: Xiaoguang Sun <sunxiaoguang@gmail.com>

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/MySQLDialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/MySQLDialect.scala
@@ -121,6 +121,7 @@ private case class MySQLDialect() extends JdbcDialect with SQLConfHelper with No
         // To avoid breaking code relying on ResultSet metadata, we support BIGINT only at
         // this time.
         case LongType => "SIGNED INTEGER"
+        case ShortType | IntegerType => QueryExecutionErrors.notSupportTypeError(dataType)
         // MySQL uses BINARY in the cast function for the type BLOB
         case BinaryType => "BINARY"
         case _ => getJDBCType(dataType).map(_.databaseTypeDefinition).getOrElse(dataType.typeName)
