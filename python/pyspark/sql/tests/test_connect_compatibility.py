48acded7f1b906fc7b29f26d4706c7495a67e256
Haejoon Lee <haejoon.lee@databricks.com>
2025-01-21 15:21:24 +0900

    [SPARK-50718][PYTHON] Support `addArtifact(s)` for PySpark
    ### What changes were proposed in this pull request?
    
    This PR proposes to support `addArtifact(s)` for PySpark
    
    ### Why are the changes needed?
    
    For feature parity with Spark Connect
    
    ### Does this PR introduce _any_ user-facing change?
    
    No API changes, but adding new API `addArtifact(s)`
    
    ### How was this patch tested?
    
    Added corresponding UTs with Spark Connect
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #49572 from itholic/add_artifacts.
    
    Authored-by: Haejoon Lee <haejoon.lee@databricks.com>
    Signed-off-by: Haejoon Lee <haejoon.lee@databricks.com>

--- python/pyspark/sql/tests/test_connect_compatibility.py
+++ python/pyspark/sql/tests/test_connect_compatibility.py
@@ -262,8 +262,6 @@ class ConnectCompatibilityTestsMixin:
         expected_missing_connect_properties = {"sparkContext"}
         expected_missing_classic_properties = {"is_stopped", "session_id"}
         expected_missing_connect_methods = {
-            "addArtifact",
-            "addArtifacts",
             "clearProgressHandlers",
             "copyFromLocalToFs",
             "newSession",
