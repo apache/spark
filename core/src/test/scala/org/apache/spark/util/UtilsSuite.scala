d07fd7a5420f195d32a44adbda07895fd6b2d6a8
Dongjoon Hyun <dongjoon@apache.org>
2025-01-22 09:47:04 -0800

    [SPARK-50946][CORE][TESTS][3.5] Add version check for Java 17.0.14 to make `UtilsSuite` test pass

--- core/src/test/scala/org/apache/spark/util/UtilsSuite.scala
+++ core/src/test/scala/org/apache/spark/util/UtilsSuite.scala
@@ -525,13 +525,12 @@ class UtilsSuite extends SparkFunSuite with ResetSystemProperties {
 
     // The following 3 scenarios are only for the method: createDirectory(File)
     // 6. Symbolic link
-    // JAVA_RUNTIME_VERSION is like "17.0.14+7-LTS"
-    lazy val javaVersion = Runtime.Version.parse(SystemUtils.JAVA_RUNTIME_VERSION)
+    lazy val javaVersion = System.getProperty("java.version").split("[+.\\-]+", 3).map(_.toInt)
     val scenario6 = java.nio.file.Files.createSymbolicLink(new File(testDir, "scenario6")
       .toPath, scenario1.toPath).toFile
     if (Utils.isJavaVersionAtLeast21) {
       assert(Utils.createDirectory(scenario6))
-    } else if (javaVersion.feature() == 17 && javaVersion.update() >= 14) {
+    } else if (javaVersion(0) == 17 && javaVersion(2) >= 14) {
       // SPARK-50946: Java 17.0.14 includes JDK-8294193, so scenario6 can succeed.
       assert(Utils.createDirectory(scenario6))
     } else {
