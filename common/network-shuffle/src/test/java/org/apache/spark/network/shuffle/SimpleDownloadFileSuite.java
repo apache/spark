9849062dfd91d020a37f7ee289373a8b17bf1c85
Michael Chen <mike.chen@workday.com>
2025-01-22 14:53:07 -0800

    Fixing checkstyle

--- common/network-shuffle/src/test/java/org/apache/spark/network/shuffle/SimpleDownloadFileSuite.java
+++ common/network-shuffle/src/test/java/org/apache/spark/network/shuffle/SimpleDownloadFileSuite.java
@@ -27,22 +27,22 @@ import java.io.IOException;
 import org.junit.jupiter.api.Assertions;
 
 public class SimpleDownloadFileSuite {
-    @Test
-    public void testChanenlIsClosedAfterCloseAndRead() throws IOException {
-        File tempFile = File.createTempFile("testChannelIsClosed", ".tmp");
-        tempFile.deleteOnExit();
-        TransportConf conf = new TransportConf("test", MapConfigProvider.EMPTY);
+  @Test
+  public void testChanenlIsClosedAfterCloseAndRead() throws IOException {
+    File tempFile = File.createTempFile("testChannelIsClosed", ".tmp");
+    tempFile.deleteOnExit();
+    TransportConf conf = new TransportConf("test", MapConfigProvider.EMPTY);
 
-        DownloadFile downloadFile = null;
-        try {
-            downloadFile = new SimpleDownloadFile(tempFile, conf);
-            DownloadFileWritableChannel channel = downloadFile.openForWriting();
-            channel.closeAndRead();
-            Assertions.assertFalse(channel.isOpen(), "Channel should be closed after closeAndRead.");
-        } finally {
-            if (downloadFile != null) {
-                downloadFile.delete();
-            }
-        }
+    DownloadFile downloadFile = null;
+    try {
+      downloadFile = new SimpleDownloadFile(tempFile, conf);
+      DownloadFileWritableChannel channel = downloadFile.openForWriting();
+      channel.closeAndRead();
+      Assertions.assertFalse(channel.isOpen(), "Channel should be closed after closeAndRead.");
+    } finally {
+      if (downloadFile != null) {
+        downloadFile.delete();
+      }
     }
+  }
 }
