1a3fcda884a48b03c13def2fc19351fc123cdb0b
Max Gekk <max.gekk@gmail.com>
2025-01-23 10:32:20 +0200

    Fix a  test in DataFrameSuite

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameSuite.scala
@@ -2058,7 +2058,7 @@ class DataFrameSuite extends QueryTest
   test("SPARK-29442 Set `default` mode should override the existing mode") {
     val df = Seq(Tuple1(1)).toDF()
     val writer = df.write.mode("overwrite").mode("default")
-    val modeField = classOf[DataFrameWriter[_]].getDeclaredField("mode")
+    val modeField = classOf[DataFrameWriter[_]].getDeclaredField("curmode")
     modeField.setAccessible(true)
     assert(SaveMode.ErrorIfExists === modeField.get(writer).asInstanceOf[SaveMode])
   }
