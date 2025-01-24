52dc80262cc73c8cfa46b7821523ddcca86b3de4
Avery <averyqi115@gmail.com>
2025-01-24 14:17:09 -0800

    restore unrelated comments

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -359,7 +359,7 @@ class Analyzer(override val catalogManager: CatalogManager) extends RuleExecutor
       // Please do not insert any other rules in between. See the TODO comments in rule
       // ResolveLateralColumnAliasReference for more details.
       ResolveLateralColumnAliasReference ::
-      ResolveExpressionsWithNamePlaceholders ::
+       ::
       ResolveDeserializer ::
       ResolveNewInstance ::
       ResolveUpCast ::
@@ -4109,25 +4109,25 @@ object ResolveExpressionsWithNamePlaceholders extends Rule[LogicalPlan] {
  *    +- Filter exists#245 [min(b#227)#249]
  *       :  +- Project [1 AS 1#247]
  *       :     +- Filter (d#238 < min(outer(b#227)))       <-----
- *       :- SubqueryAlias r
- *       :   - Project [_1#234 AS c#237, _2#235 AS d#238]
- *       :      - LocalRelation [_1#234, _2#235]
+ *       :        +- SubqueryAlias r
+ *       :           +- Project [_1#234 AS c#237, _2#235 AS d#238]
+ *       :              +- LocalRelation [_1#234, _2#235]
  *       +- Aggregate [a#226], [a#226, min(b#227) AS min(b#227)#249]
- *  - SubqueryAlias l
- *     - Project [_1#223 AS a#226, _2#224 AS b#227]
- *        - LocalRelation [_1#223, _2#224]
+ *          +- SubqueryAlias l
+ *             +- Project [_1#223 AS a#226, _2#224 AS b#227]
+ *                +- LocalRelation [_1#223, _2#224]
  * Plan after the rule.
  *    Project [a#226]
  *    +- Filter exists#245 [min(b#227)#249]
  *       :  +- Project [1 AS 1#247]
  *       :     +- Filter (d#238 < outer(min(b#227)#249))   <-----
- *       :- SubqueryAlias r
- *       :   - Project [_1#234 AS c#237, _2#235 AS d#238]
- *       :      - LocalRelation [_1#234, _2#235]
+ *       :        +- SubqueryAlias r
+ *       :           +- Project [_1#234 AS c#237, _2#235 AS d#238]
+ *       :              +- LocalRelation [_1#234, _2#235]
  *       +- Aggregate [a#226], [a#226, min(b#227) AS min(b#227)#249]
- *  - SubqueryAlias l
- *     - Project [_1#223 AS a#226, _2#224 AS b#227]
- *        - LocalRelation [_1#223, _2#224]
+ *          +- SubqueryAlias l
+ *             +- Project [_1#223 AS a#226, _2#224 AS b#227]
+ *                +- LocalRelation [_1#223, _2#224]
  */
 object UpdateOuterReferences extends Rule[LogicalPlan] {
   private def stripAlias(expr: Expression): Expression = expr match { case a: Alias => a.child }
