84f3b230a53a10508668f47a9fa0f2b350323427
Ruifeng Zheng <ruifengz@apache.org>
2025-01-27 21:06:25 +0800

    fix
    fix

--- sql/connect/server/src/main/scala/org/apache/spark/sql/connect/ml/MLUtils.scala
+++ sql/connect/server/src/main/scala/org/apache/spark/sql/connect/ml/MLUtils.scala
@@ -639,6 +639,7 @@ private[ml] object MLUtils {
     (classOf[MaxAbsScalerModel], Set("maxAbs")),
     (classOf[MinMaxScalerModel], Set("originalMax", "originalMin")),
     (classOf[RobustScalerModel], Set("range", "median")),
+    (classOf[VectorIndexerModel], Set("numFeatures")),
     (classOf[ChiSqSelectorModel], Set("selectedFeatures")),
     (classOf[UnivariateFeatureSelectorModel], Set("selectedFeatures")),
     (classOf[VarianceThresholdSelectorModel], Set("selectedFeatures")),
