3f56d0f41f1acf44444fc4447af62d174f81d0bd
Chris Twiner <chris.twiner@gmail.com>
2025-01-28 13:40:12 +0100

    [SPARK-49961] lock down input and output types, correct auto-imports reorg

--- sql/connect/common/src/main/scala/org/apache/spark/sql/connect/Dataset.scala
+++ sql/connect/common/src/main/scala/org/apache/spark/sql/connect/Dataset.scala
@@ -17,11 +17,13 @@
 package org.apache.spark.sql.connect
 
 import java.util
+
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 import scala.reflect.ClassTag
 import scala.reflect.runtime.universe.TypeTag
 import scala.util.control.NonFatal
+
 import org.apache.spark.SparkException
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.api.java.JavaRDD
@@ -29,7 +31,7 @@ import org.apache.spark.api.java.function._
 import org.apache.spark.connect.proto
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql
-import org.apache.spark.sql.{AnalysisException, Column, Encoder, Observation, Row, TypedColumn, functions}
+import org.apache.spark.sql.{functions, AnalysisException, Column, Encoder, Observation, Row, TypedColumn}
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.catalyst.encoders.AgnosticEncoder
 import org.apache.spark.sql.catalyst.encoders.AgnosticEncoders._
@@ -1541,6 +1543,7 @@ class Dataset[T] private[sql] (
       metadata: Seq[Metadata]): DataFrame =
     super.withColumns(colNames, cols, metadata)
 
+  /** @inheritdoc */
   override private[spark] def withColumn(
       colName: String,
       col: Column,
