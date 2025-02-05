23e2bec84c273b1c4f33cdd344f94add2d1c42e9
Ruifeng Zheng <ruifengz@apache.org>
2025-02-05 11:53:41 +0800

    simplify

--- python/pyspark/ml/feature.py
+++ python/pyspark/ml/feature.py
@@ -4830,19 +4830,13 @@ class StringIndexerModel(
         requires an active SparkContext.
         """
         if is_remote():
-            uid = StringIndexerModel().uid
+            model = StringIndexerModel()
             helper = JavaWrapper(java_obj=ML_CONNECT_HELPER_ID)
-            model = StringIndexerModel(
-                helper._call_java(
-                    "stringIndexerModelFromLabels",
-                    uid,
-                    (list(labels), ArrayType(StringType(), False)),
-                )
+            model._java_obj = helper._call_java(
+                "stringIndexerModelFromLabels",
+                model.uid,
+                (list(labels), ArrayType(StringType(), False)),
             )
-            model._resetUid(uid)
-            # reset uid won't automatically update the default value of outputCol
-            # should set it manually
-            model._setDefault(outputCol=model.uid + "__output")
 
         else:
             from pyspark.core.context import SparkContext
@@ -4876,22 +4870,16 @@ class StringIndexerModel(
         requires an active SparkContext.
         """
         if is_remote():
-            uid = StringIndexerModel().uid
+            model = StringIndexerModel()
             helper = JavaWrapper(java_obj=ML_CONNECT_HELPER_ID)
-            model = StringIndexerModel(
-                helper._call_java(
-                    "stringIndexerModelFromLabelsArray",
-                    uid,
-                    (
-                        [list(labels) for labels in arrayOfLabels],
-                        ArrayType(ArrayType(StringType(), False)),
-                    ),
-                )
+            model._java_obj = helper._call_java(
+                "stringIndexerModelFromLabelsArray",
+                model.uid,
+                (
+                    [list(labels) for labels in arrayOfLabels],
+                    ArrayType(ArrayType(StringType(), False)),
+                ),
             )
-            model._resetUid(uid)
-            # reset uid won't automatically update the default value of outputCol
-            # should set it manually
-            model._setDefault(outputCol=model.uid + "__output")
 
         else:
             from pyspark.core.context import SparkContext
