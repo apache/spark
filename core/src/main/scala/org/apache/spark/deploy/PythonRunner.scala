fab02b4233d019509246b13c25c64df5d059aced
Hyukjin Kwon <gurwls223@apache.org>
2025-02-05 14:12:43 +0900

    fixup

--- core/src/main/scala/org/apache/spark/deploy/PythonRunner.scala
+++ core/src/main/scala/org/apache/spark/deploy/PythonRunner.scala
@@ -20,6 +20,7 @@ package org.apache.spark.deploy
 import java.io.File
 import java.net.URI
 import java.nio.file.Files
+import java.util.Locale
 
 import scala.collection.mutable.ArrayBuffer
 import scala.jdk.CollectionConverters._
@@ -52,7 +53,7 @@ object PythonRunner {
     // Format python file paths before adding them to the PYTHONPATH
     val formattedPythonFile = formatPath(pythonFile)
     val formattedPyFiles = resolvePyFiles(formatPaths(pyFiles))
-    lazy val apiMode = sparkConf.get(SPARK_API_MODE)
+    lazy val apiMode = sparkConf.get(SPARK_API_MODE).toLowerCase(Locale.ROOT)
     lazy val isAPIModeClassic = apiMode == "classic"
     lazy val isAPIModeConnect = apiMode == "connect"
 
