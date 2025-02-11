76045edbcd205ab9c3101fbc10efc9f2fd50fe31
fusheng <fusheng9399@gmail.com>
2025-02-11 21:22:01 +0800

    add more comprehensive examples

--- python/pyspark/sql/functions/builtin.py
+++ python/pyspark/sql/functions/builtin.py
@@ -20115,11 +20115,23 @@ def get_json_object(col: "ColumnOrName", path: str) -> Column:
 
     Examples
     --------
+    Example1: Get json object from json object
+
     >>> data = [("1", '''{"f1": "value1", "f2": "value2"}'''), ("2", '''{"f1": "value12"}''')]
     >>> df = spark.createDataFrame(data, ("key", "jstring"))
     >>> df.select(df.key, get_json_object(df.jstring, '$.f1').alias("c0"), \\
     ...                   get_json_object(df.jstring, '$.f2').alias("c1") ).collect()
     [Row(key='1', c0='value1', c1='value2'), Row(key='2', c0='value12', c1=None)]
+
+    Example2: Get json object from json array object
+    data = [("1", '''[{"f1": "value1", "f2": "value2"},{"f1": "value3", "f2": "value4"}]'''), ("2", '''[{"f1": "value12"},{"f1": "value13"}]''')]
+    df = spark.createDataFrame(data, ("key", "jarray"))
+    df.select(df.key, get_json_object(df.jarray, '$[0].f1').alias("c0"), \\
+    ...                   get_json_object(df.jarray, '$[0].f2').alias("c1") ).collect()
+    [Row(key='1', c0='value1', c1='value2'), Row(key='2', c0='value12', c1=None)]
+    df.select(df.key, get_json_object(df.jarray, '$[*].f1').alias("c0"), \\
+    ...                   get_json_object(df.jarray, '$[*].f2').alias("c1") ).collect()
+    [Row(key='1', c0='["value1","value3"]', c1='["value2","value4"]'), Row(key='2', c0='["value12","value13"]', c1=None)]
     """
     from pyspark.sql.classic.column import _to_java_column
 
