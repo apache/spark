3b663dd26377807c8064a9716b1f57b312680f97
fusheng <fusheng9399@gmail.com>
2025-02-12 17:40:03 +0800

    fix the indentation

--- python/pyspark/sql/functions/builtin.py
+++ python/pyspark/sql/functions/builtin.py
@@ -20126,7 +20126,7 @@ def get_json_object(col: "ColumnOrName", path: str) -> Column:
     Example 2: Extracts a json object from json array
 
     >>> data = [("1", '''[{"f1": "value1", "f2": "value2"},{"f1": "value3", "f2": "value4"}]'''), \\
-    ...                   ("2", '''[{"f1": "value12"},{"f1": "value13"}]''')]
+    ...                   ("2", '''[{"f1": "value12"}]''')]
     >>> df = spark.createDataFrame(data, ("key", "jarray"))
     >>> df.select(df.key, get_json_object(df.jarray, '$[0].f1').alias("c0"), \\
     ...                   get_json_object(df.jarray, '$[0].f2').alias("c1") ).collect()
@@ -20134,8 +20134,7 @@ def get_json_object(col: "ColumnOrName", path: str) -> Column:
 
     >>> df.select(df.key, get_json_object(df.jarray, '$[*].f1').alias("c0"), \\
     ...                   get_json_object(df.jarray, '$[*].f2').alias("c1") ).collect()
-    [Row(key='1', c0='["value1","value3"]', c1='["value2","value4"]'), \\
-    ...                   Row(key='2', c0='["value12","value13"]', c1=None)]
+    [Row(key='1', c0='["value1","value3"]', c1='["value2","value4"]'), Row(key='2', c0='"value12"', c1=None)]
     """
     from pyspark.sql.classic.column import _to_java_column
 
