c8b2a5293d2b532bb79378b4ef7c1fbd373ba235
pavle-martinovic_data <pavle.martinovic@databricks.com>
2025-02-14 09:50:11 +0100

    Apply Milan's already existing changes

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -4516,6 +4516,14 @@ object SQLConf {
     .checkValues(LegacyBehaviorPolicy.values.map(_.toString))
     .createWithDefault(LegacyBehaviorPolicy.CORRECTED.toString)
 
+  val CTE_RECURSION_LEVEL_LIMIT = buildConf("spark.sql.cteRecursionLevelLimit")
+    .doc("Maximum level of recursion that is allowed wile executing a recursive CTE definition." +
+      "If a query does not get exhausted before reaching this limit it fails. Use -1 for " +
+      "unlimited.")
+    .version("4.0.0")
+    .intConf
+    .createWithDefault(100)
+
   val LEGACY_INLINE_CTE_IN_COMMANDS = buildConf("spark.sql.legacy.inlineCTEInCommands")
     .internal()
     .doc("If true, always inline the CTE relations for the queries in commands. This is the " +
