01edac40ec858a95c7c51419aa3b12f28a9595de
Dima <dimanowq@gmail.com>
2025-02-18 20:46:08 +0800

    [SPARK-51219][SQL] Fix `ShowTablesExec.isTempView` to work with non-`V2SessionCatalog` catalogs
    ### What changes were proposed in this pull request?
    
    When non buildin catalog is configured (for example, `org.apache.spark.sql.delta.catalog.DeltaCatalog`) and there is a temp table in catalog, running catalog listTable will fail:
    ```scala
    spark.conf.set("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
    
    spark.range(0,2).createOrReplaceTempView("abc")
    spark.catalog.listTables().show()
    
    // org.apache.spark.sql.catalyst.parser.ParseException:
    // [PARSE_EMPTY_STATEMENT] Syntax error, unexpected empty statement. SQLSTATE: 42617 (line 1, pos 0)
    //
    // == SQL ==
    //
    // ^^^
    ```
    
    If default `V2SessionCatalog` catalog is in use, or there are no temp tables, the same command run without issues.
    
    This behavior is due to `ShowTablesExec. isTempView ` method, where only for `V2SessionCatalog` catalogs dedicated `isTempView` is executed. This PR fixes that, by using `session.sessionState.catalog.isTempView` instead.
    
    ### Why are the changes needed?
    
    To avoid unnecessary fails when a non-buildin v2 catalog is in use.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    New unit tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #49959 from ostronaut/features/fix-ShowTablesExec-isTempView.
    
    Authored-by: Dima <dimanowq@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit aa37f89811878d9993962c75b73025bcadcbb6f5)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/ShowTablesExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/ShowTablesExec.scala
@@ -22,9 +22,10 @@ import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.util.StringUtils
-import org.apache.spark.sql.connector.catalog.{Identifier, TableCatalog}
+import org.apache.spark.sql.connector.catalog.{CatalogV2Util, Identifier, TableCatalog}
 import org.apache.spark.sql.connector.catalog.CatalogV2Implicits.NamespaceHelper
 import org.apache.spark.sql.execution.LeafExecNode
+import org.apache.spark.util.ArrayImplicits._
 
 /**
  * Physical plan node for showing tables.
@@ -40,17 +41,17 @@ case class ShowTablesExec(
     val tables = catalog.listTables(namespace.toArray)
     tables.map { table =>
       if (pattern.map(StringUtils.filterPattern(Seq(table.name()), _).nonEmpty).getOrElse(true)) {
-        rows += toCatalystRow(table.namespace().quoted, table.name(), isTempView(table))
+        rows += toCatalystRow(table.namespace().quoted, table.name(), isTempView(table, catalog))
       }
     }
 
     rows.toSeq
   }
 
-  private def isTempView(ident: Identifier): Boolean = {
-    catalog match {
-      case s: V2SessionCatalog => s.isTempView(ident)
-      case _ => false
-    }
+  private def isTempView(ident: Identifier, catalog: TableCatalog): Boolean = {
+    if (CatalogV2Util.isSessionCatalog(catalog)) {
+      session.sessionState.catalog
+        .isTempView((ident.namespace() :+ ident.name()).toImmutableArraySeq)
+    } else false
   }
 }
