8c0466bc51735732eb188f6a227b01cf7f8c48e6
Mihailo Milosevic <mihailo.milosevic@databricks.com>
2025-02-25 11:02:40 +0100

    Fix problem

--- sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DatasetSuite.scala
@@ -2778,6 +2778,17 @@ class DatasetSuite extends QueryTest
     }
   }
 
+  test("SPARK-51312: createDataFrame should work with both Date and LocalDate") {
+    for (confVal <- Seq("true", "false")) {
+      withSQLConf(SQLConf.DATETIME_JAVA8API_ENABLED.key -> confVal) {
+        val schema = new org.apache.spark.sql.types.StructType().add("a", "date")
+        val rdd = spark.sparkContext.parallelize(Row(java.time.LocalDate.of(2020, 5, 13)) ::
+          Row(java.sql.Date.valueOf("2020-05-13")) :: Nil)
+        spark.createDataFrame(rdd, schema).collect()
+      }
+    }
+  }
+
   test("SPARK-46791: Dataset with set field") {
     val ds = Seq(WithSet(0, HashSet("foo", "bar")), WithSet(1, HashSet("bar", "zoo"))).toDS()
     checkDataset(ds.map(t => t),
