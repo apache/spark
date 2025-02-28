a3f4aec00e255f3a77e4daf90d7008258a296d78
Hyukjin Kwon <gurwls223@apache.org>
2025-02-28 20:23:13 +0900

    [SPARK-51316][PYTHON][FOLLOW-UP] Revert unrelated changes and mark mapInPandas/mapInArrow batched in byte size
    This PR is a followup of https://github.com/apache/spark/pull/50096 that reverts unrelated changes and mark mapInPandas/mapInArrow batched in byte size
    
    To make the original change self-contained, and mark mapInPandas/mapInArrow batched in byte size to be consistent.
    
    No, the main change has not been released out yet.
    
    Manually.
    
    No.
    
    Closes #50111 from HyukjinKwon/SPARK-51316-followup.
    
    Authored-by: Hyukjin Kwon <gurwls223@apache.org>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>
    (cherry picked from commit 5b45671654003d4292bd97dab3e6ff9ff9a8e651)
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>
    (cherry picked from commit 1df6fc6bf52a9e898f65f9a644943942c05a6a95)

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/PythonArrowInput.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/PythonArrowInput.scala
@@ -150,8 +150,10 @@ private[python] trait BasicPythonArrowInput extends PythonArrowInput[Iterator[In
 
 private[python] trait BatchedPythonArrowInput extends BasicPythonArrowInput {
   self: BasePythonRunner[Iterator[InternalRow], _] =>
-
-  private val arrowMaxRecordsPerBatch = SQLConf.get.arrowMaxRecordsPerBatch
+  private val arrowMaxRecordsPerBatch = {
+    val v = SQLConf.get.arrowMaxRecordsPerBatch
+    if (v > 0) v else Int.MaxValue
+  }
   private val maxBytesPerBatch = SQLConf.get.arrowMaxBytesPerBatch
 
   // Marker inside the input iterator to indicate the start of the next batch.
@@ -176,7 +178,7 @@ private[python] trait BatchedPythonArrowInput extends BasicPythonArrowInput {
         maxBytesPerBatch == Int.MaxValue || arrowWriter.sizeInBytes() < maxBytesPerBatch
 
       while (nextBatchStart.hasNext &&
-          (arrowMaxRecordsPerBatch <= 0 || numRowsInBatch < arrowMaxRecordsPerBatch) &&
+          numRowsInBatch < arrowMaxRecordsPerBatch &&
           underBatchSizeLimit) {
         arrowWriter.write(nextBatchStart.next())
         numRowsInBatch += 1
