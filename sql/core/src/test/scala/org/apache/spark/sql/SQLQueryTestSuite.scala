fdb5c33dcd9276bceefcac03dbf643f9d40d2ebc
yangjie01 <yangjie01@baidu.com>
2025-03-06 12:29:54 +0800

    revert some change

--- sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/SQLQueryTestSuite.scala
@@ -599,11 +599,6 @@ class SQLQueryTestSuite extends QueryTest with SharedSparkSession with SQLHelper
     session.sql("DROP TABLE IF EXISTS tenk1")
   }
 
-  protected override def beforeEach(): Unit = {
-    super.beforeEach()
-    System.gc()
-  }
-
   override def beforeAll(): Unit = {
     super.beforeAll()
     createTestTables(spark)
