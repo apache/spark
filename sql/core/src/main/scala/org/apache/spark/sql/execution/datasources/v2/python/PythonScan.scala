2c584a15105456aa59549bef0ba2f66a358990e8
Haoyu Weng <wenghy02@gmail.com>
2025-03-19 09:42:57 -0700

    fix

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/python/PythonScan.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/python/PythonScan.scala
@@ -16,10 +16,7 @@
  */
 package org.apache.spark.sql.execution.datasources.v2.python
 
-import org.apache.commons.lang3.StringUtils
-
 import org.apache.spark.JobArtifactSet
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.connector.metric.CustomMetric
 import org.apache.spark.sql.connector.read._
 import org.apache.spark.sql.connector.read.streaming.MicroBatchStream
@@ -27,7 +24,6 @@ import org.apache.spark.sql.internal.connector.SupportsMetadata
 import org.apache.spark.sql.sources.Filter
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
-import org.apache.spark.util.Utils
 
 class PythonScan(
     ds: PythonDataSourceV2,
@@ -36,8 +32,6 @@ class PythonScan(
     options: CaseInsensitiveStringMap,
     supportedFilters: Array[Filter]
 ) extends Scan with SupportsMetadata {
-  private lazy val sparkSession = SparkSession.active
-
   override def toBatch: Batch = new PythonBatch(ds, shortName, outputSchema, options)
 
   override def toMicroBatchStream(checkpointLocation: String): MicroBatchStream =
