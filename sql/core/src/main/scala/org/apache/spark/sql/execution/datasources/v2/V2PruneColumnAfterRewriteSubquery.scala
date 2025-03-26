8ec67f52695d9f57c2e1fb327413e41719cb07e4
Akeron Zhu <luckilyaf@gmail.com>
2025-03-26 13:09:12 +0800

    Prune column for DSV2 after RewriteSubquery

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/V2PruneColumnAfterRewriteSubquery.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/V2PruneColumnAfterRewriteSubquery.scala
@@ -1,7 +1,7 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
- * this work for additiosnal information regarding copyright ownership.
+ * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
@@ -27,6 +27,7 @@ import org.apache.spark.sql.util.SchemaUtils.restoreOriginalOutputNames
 
 object V2PruneColumnAfterRewriteSubquery extends Rule[LogicalPlan] with Logging {
   import DataSourceV2Implicits._
+
   override def apply(plan: LogicalPlan): LogicalPlan = {
     if (plan.isInstanceOf[V2WriteCommand]) return plan
     plan.transform {
