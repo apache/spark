f70e0689e1caf3006791902a65286b4f2679ec04
yangjie01 <yangjie01@baidu.com>
2025-04-01 11:58:37 +0800

    one test

--- sql/connect/client/integration-tests/src/test/scala/org/apache/spark/sql/application/ReplE2ESuite.scala
+++ sql/connect/client/integration-tests/src/test/scala/org/apache/spark/sql/application/ReplE2ESuite.scala
@@ -173,7 +173,7 @@ class ReplE2ESuite extends ConnectFunSuite with RemoteSparkSession with BeforeAn
     // scalastyle:off classforname line.size.limit
     val sparkHome = IntegrationTestUtils.sparkHome
     val testJar = Paths
-      .get(s"$sparkHome/sql/connect/client/jvm/src/test/resources/TestHelloV2_$scalaVersion.jar")
+      .get(s"$sparkHome/sql/connect/client/integration-tests/src/test/resources/TestHelloV2_$scalaVersion.jar")
       .toFile
 
     assume(testJar.exists(), "Missing TestHelloV2 jar!")
