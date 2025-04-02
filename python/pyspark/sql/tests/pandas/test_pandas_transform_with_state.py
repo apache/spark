5583e747b6ae9249cc8e37f7b06cf82ec61c826d
bogao007 <bo.gao@databricks.com>
2025-04-02 15:39:53 -0700

    fix

--- python/pyspark/sql/tests/pandas/test_pandas_transform_with_state.py
+++ python/pyspark/sql/tests/pandas/test_pandas_transform_with_state.py
@@ -266,10 +266,7 @@ class TransformWithStateInPandasTestsMixin:
                 assert set(batch_df.sort("id").collect()) == {
                     Row(id="0", countAsString="2"),
                     Row(id="1", countAsString="2"),
-            }
-            else:
-                for q in self.spark.streams.active:
-                    q.stop()
+                }
 
         self._test_transform_with_state_in_pandas_basic(
             ListStateProcessor(), check_results, True, "processingTime"
