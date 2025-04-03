3f6fc1b7c1c7787906efeb92f77f5c43eade6d9d
Hyukjin Kwon <gurwls223@apache.org>
2025-04-03 19:02:00 +0900

    fixup

--- core/src/test/scala/org/apache/spark/api/python/PythonRDDSuite.scala
+++ core/src/test/scala/org/apache/spark/api/python/PythonRDDSuite.scala
@@ -79,7 +79,7 @@ class PythonRDDSuite extends SparkFunSuite with LocalSparkContext {
 
   test("python server error handling") {
     val conf = new SparkConf()
-    conf.set(PYTHON_UNIX_DOMAIN_SOCKET_ENABLED.key, false.toString)
+    assume(!conf.get(PYTHON_UNIX_DOMAIN_SOCKET_ENABLED))
     val authHelper = new SocketAuthHelper(conf)
     val errorServer = new ExceptionPythonServer(authHelper)
     val serverSocketChannel = ServerSocketChannel.open()
