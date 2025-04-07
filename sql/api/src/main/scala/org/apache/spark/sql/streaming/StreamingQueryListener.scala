45c19b24aa5f3b30e63264544b3029552a2b8dbb
jcowell <jcowell@atlassian.com>
2025-04-06 23:00:03 -0400

    NoIssue: lint

--- sql/api/src/main/scala/org/apache/spark/sql/streaming/StreamingQueryListener.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/streaming/StreamingQueryListener.scala
@@ -98,7 +98,7 @@ private[spark] trait PythonStreamingQueryListener {
 
   def onQueryTerminated(event: QueryTerminatedEvent): Unit
 
-  def onQueryTriggerStart(event: QueryTriggerStartEvent): Unit = {}
+  def onQueryTriggerStart(event: QueryTriggerStartEvent): Unit
 }
 
 private[spark] class PythonStreamingQueryListenerWrapper(listener: PythonStreamingQueryListener)
@@ -113,7 +113,8 @@ private[spark] class PythonStreamingQueryListenerWrapper(listener: PythonStreami
 
   def onQueryTerminated(event: QueryTerminatedEvent): Unit = listener.onQueryTerminated(event)
 
-  override def onQueryTriggerStart(event: QueryTriggerStartEvent): Unit = listener.onQueryTriggerStart(event)
+  override def onQueryTriggerStart(event: QueryTriggerStartEvent): Unit =
+    listener.onQueryTriggerStart(event)
 }
 
 /**
