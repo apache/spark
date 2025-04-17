05129d9d3bb30876f9c566ccbdba6d2c2926998c
Ruifeng Zheng <ruifengz@apache.org>
2025-04-17 14:30:41 +0800

    Revert "Revert "[SPARK-51824][ML][CONNECT][TESTS] Force to clean up the ML cache after each test""
    This reverts commit e9b504269f983ef2b48fc8184ba87c0d98b0ec21.

--- python/pyspark/testing/connectutils.py
+++ python/pyspark/testing/connectutils.py
@@ -16,7 +16,6 @@
 #
 import shutil
 import tempfile
-import typing
 import os
 import functools
 import unittest
@@ -187,6 +186,10 @@ class ReusedConnectTestCase(unittest.TestCase, SQLTestUtils, PySparkErrorTestUti
     def tearDownClass(cls):
         shutil.rmtree(cls.tempdir.name, ignore_errors=True)
         cls.spark.stop()
+
+    def tearDown(self) -> None:
+        # force to clean up the ML cache after each test
+        self.spark.client._cleanup_ml()
 
     def test_assert_remote_mode(self):
         from pyspark.sql import is_remote
