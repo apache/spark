1e8fecfeb777c0bd3682c159dac9109031ef8469
beliefer <beliefer@163.com>
2025-04-18 15:16:33 +0800

    [SPARK-49488][SQL][FOLLOWUP] Do not push down extract expression if extracted field is second

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/MySQLDialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/MySQLDialect.scala
@@ -58,10 +58,11 @@ private case class MySQLDialect() extends JdbcDialect with SQLConfHelper with No
         case "DAY_OF_YEAR" => s"DAYOFYEAR(${build(extract.source())})"
         case "WEEK" => s"WEEKOFYEAR(${build(extract.source())})"
         case "YEAR_OF_WEEK" => visitUnexpectedExpr(extract)
+        case "SECOND" => visitUnexpectedExpr(extract)
         // WEEKDAY uses Monday = 0, Tuesday = 1, ... and ISO standard is Monday = 1, ...,
         // so we use the formula (WEEKDAY + 1) to follow the ISO standard.
         case "DAY_OF_WEEK" => s"(WEEKDAY(${build(extract.source())}) + 1)"
-        // SECOND, MINUTE, HOUR, DAY, MONTH, QUARTER, YEAR are identical on MySQL and Spark for
+        // MINUTE, HOUR, DAY, MONTH, QUARTER, YEAR are identical on MySQL and Spark for
         // both datetime and interval types.
         case _ => super.visitExtract(field, build(extract.source()))
       }
