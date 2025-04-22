f4febd0776319e1b51513ee1760c736928fb6643
ilicmarkodb <marko.ilic@databricks.com>
2025-04-22 23:51:32 +0800

    [SPARK-51849][SQL] Refactoring `ResolveDDLCommandStringTypes`
    ### What changes were proposed in this pull request?
    `ResolveDDLCommandStringTypes` renamed to `ApplyDefaultCollationToStringType`.
    
    ### Why are the changes needed?
    This is needed because this rule applies also to non-DDL plans (when querying View).
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #50609 from ilicmarkodb/split_resolve_ddl_and_view.
    
    Authored-by: ilicmarkodb <marko.ilic@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>
    (cherry picked from commit c86f617b2f1a2813c099f5a98eb5732b9c3f86b2)
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -396,7 +396,7 @@ class Analyzer(override val catalogManager: CatalogManager) extends RuleExecutor
       ResolveAliases ::
       ResolveSubquery ::
       ResolveSubqueryColumnAliases ::
-      ResolveDDLCommandStringTypes ::
+      ApplyDefaultCollationToStringType ::
       ResolveWindowOrder ::
       ResolveWindowFrame ::
       ResolveNaturalAndUsingJoin ::
