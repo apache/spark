927bdc8237c49a9b71053c461f294be81fb834f1
Mihailo Timotic <mihailo.timotic@databricks.com>
2025-04-25 09:22:25 +0200

    fix

--- sql/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/SparkConnectPlanner.scala
+++ sql/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/SparkConnectPlanner.scala
@@ -4108,7 +4108,8 @@ class SparkConnectPlanner(
    */
   private def replaceIntegerLiteralWithOrdinal(groupingExpression: Expression) =
     groupingExpression match {
-      case Literal(value: Int, IntegerType) => UnresolvedOrdinal(value)
+      case literal @ Literal(value: Int, IntegerType) =>
+        CurrentOrigin.withOrigin(literal.origin) { UnresolvedOrdinal(value) }
       case other => other
     }
 
