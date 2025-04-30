9a0b3fbad693c30deac3a89d68aa129d66445bb9
Ruifeng Zheng <ruifengz@apache.org>
2025-04-30 10:32:45 +0800

    fix test

--- mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
+++ mllib/src/main/scala/org/apache/spark/ml/classification/LogisticRegression.scala
@@ -1318,7 +1318,7 @@ class LogisticRegressionModel private[spark] (
 
 @Since("1.6.0")
 object LogisticRegressionModel extends MLReadable[LogisticRegressionModel] {
-  case class Data(
+  private[spark] case class Data(
     numClasses: Int,
     numFeatures: Int,
     interceptVector: Vector,
