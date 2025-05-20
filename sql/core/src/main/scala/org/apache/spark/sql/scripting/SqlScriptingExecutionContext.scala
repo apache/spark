031ce81245916b8dd76731ff0ee09669360a194b
David Milicevic <david.milicevic@databricks.com>
2025-05-20 13:15:45 +0800

    [SPARK-52221][SQL] Refactor SqlScriptingLocalVariableManager into more generic context manager
    ### What changes were proposed in this pull request?
    
    Replacing `SqlScriptingLocalVariableManager` Thread Local variable with more generic `SqlScriptingContextManager`.
    
    Newly introduced `SqlScriptingContextManager` encapsulates local variable manager, but also includes `SqlScriptingExecutionContext` information. These information will be required in the future, for example, to implement Stored Procedure calls - since the access for call stack related operations will be needed.
    
    ### Why are the changes needed?
    
    These are refactor changes. The refactor is needed to better support future implementations, as explained in JIRA ticket.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    This is a refactor PR. Already existing test cases ensure that nothing is broken.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #50938 from davidm-db/scripting_context_manager.
    
    Authored-by: David Milicevic <david.milicevic@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/scripting/SqlScriptingExecutionContext.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/scripting/SqlScriptingExecutionContext.scala
@@ -23,13 +23,13 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 import org.apache.spark.SparkException
-import org.apache.spark.sql.catalyst.catalog.VariableDefinition
+import org.apache.spark.sql.catalyst.catalog.{SqlScriptingExecutionContextExtension, VariableDefinition}
 import org.apache.spark.sql.scripting.SqlScriptingFrameType.SqlScriptingFrameType
 
 /**
  * SQL scripting execution context - keeps track of the current execution state.
  */
-class SqlScriptingExecutionContext {
+class SqlScriptingExecutionContext extends SqlScriptingExecutionContextExtension {
   // List of frames that are currently active.
   private[scripting] val frames: ListBuffer[SqlScriptingExecutionFrame] = ListBuffer.empty
   private[scripting] var firstHandlerScopeLabel: Option[String] = None
