11090d554b1177744fc8f3122f3901f7dfd3b00f
Jon Mio <jon.mio@databricks.com>
2025-05-21 10:10:04 -0700

    [SPARK-52166][SDP] Add support for PipelineEvents
    ### What changes were proposed in this pull request?
     The execution of an SDP is a complex, multi-stage computation. To observe progress and monitor state transitions of a pipeline execution, we will maintain a stream of pipeline events (e.g. flow started, flow completed, pipeline run started, pipeline run completed). This PR introduces the data model for PipelineEvents.
    
    Additionally since this is the first PR to add tests to the pipelines module, we update the GA CI job to run tests in the pipeline module.
    
    ### Why are the changes needed?
    See description above
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    Added unit tests for event construction as well as unit tests for the helper functions used in `ConstructPipelineEvent`.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No
    
    Closes #50906 from jonmio/include_event_classes.
    
    Authored-by: Jon Mio <jon.mio@databricks.com>
    Signed-off-by: Gengliang Wang <gengliang@apache.org>

---
+++ sql/pipelines/src/main/scala/org/apache/spark/sql/pipelines/logging/PipelineEvent.scala
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.sql.pipelines.logging
+
+import org.apache.spark.sql.pipelines.common.FlowStatus
+
+/**
+ * An internal event that is emitted during the run of a pipeline.
+ * @param id A globally unique id
+ * @param timestamp The time of the event
+ * @param origin Where the event originated from
+ * @param message A user friendly description of the event
+ * @param details The details of the event
+ * @param error An error that occurred during the event
+ */
+case class PipelineEvent(
+    id: String,
+    timestamp: String,
+    origin: PipelineEventOrigin,
+    message: String,
+    details: EventDetails,
+    error: Option[ErrorDetail]
+)
+
+/**
+ * Describes where the event originated from
+ * @param datasetName The name of the dataset
+ * @param flowName The name of the flow
+ * @param sourceCodeLocation The location of the source code
+ */
+case class PipelineEventOrigin(
+    datasetName: Option[String],
+    flowName: Option[String],
+    sourceCodeLocation: Option[SourceCodeLocation]
+)
+
+/**
+ * Describes the location of the source code
+ * @param path The path to the source code
+ * @param lineNumber The line number of the source code
+ * @param columnNumber The column number of the source code
+ * @param endingLineNumber The ending line number of the source code
+ * @param endingColumnNumber The ending column number of the source code
+ */
+case class SourceCodeLocation(
+    path: Option[String],
+    lineNumber: Option[Int],
+    columnNumber: Option[Int],
+    endingLineNumber: Option[Int],
+    endingColumnNumber: Option[Int]
+)
+
+// Additional details about the PipelineEvent
+trait EventDetails
+
+// An event indicating that a flow has made progress and transitioned to a different state
+case class FlowProgress(status: FlowStatus) extends EventDetails
+
+// Additional details about the error that occurred during the event
+case class ErrorDetail(exceptions: Seq[SerializedException])
+
+// An exception that was thrown during a pipeline run
+case class SerializedException(className: String, message: String, stack: Seq[StackFrame])
+
+// A stack frame of an exception
+case class StackFrame(declaringClass: String, methodName: String)
