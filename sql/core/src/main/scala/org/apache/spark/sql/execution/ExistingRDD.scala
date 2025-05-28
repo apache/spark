9f5527e20af4ea6d1fdb415baa83829005661e1f
Wenchen Fan <cloud0fan@gmail.com>
2025-05-28 10:05:30 +0800

    Update sql/core/src/main/scala/org/apache/spark/sql/execution/ExistingRDD.scala

--- sql/core/src/main/scala/org/apache/spark/sql/execution/ExistingRDD.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/ExistingRDD.scala
@@ -337,7 +337,7 @@ case class OneRowRelationExec() extends LeafExecNode
     val numOutputRows = longMetric("numOutputRows")
     session
       .sparkContext
-      .parallelize(Seq[Int](), 1)
+      .parallelize(Seq(""), 1)
       .mapPartitionsInternal { _ =>
         val proj = UnsafeProjection.create(Seq.empty[Expression])
         Iterator(proj.apply(InternalRow.empty)).map { r =>
