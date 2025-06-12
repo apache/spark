348636605ff238ca6fe4f0e910b145fca8433aba
pavle-martinovic_data <pavle.martinovic@databricks.com>
2025-06-11 17:24:18 -0700

    [SPARK-52430][SQL] Resolve bug with nullability of Union in rCTEs
    ### What changes were proposed in this pull request?
    
    Change the output of UnionLoopRef to be nullable by default and use the correct output in Local Optimization optimization for small result sets.
    
    ### Why are the changes needed?
    
    Currently, we mark the nullability of UnionLoopRef with the nullability of the anchor. However, the NULL may occur because of the recursive step, leading to unexpected behavior (treating NULLs as 0s).
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    New golden file test with failing example.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #51134 from Pajaraja/pavle-martinovic_data/FixUnionLoopRefNullability.
    
    Authored-by: pavle-martinovic_data <pavle.martinovic@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveWithCTE.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveWithCTE.scala
@@ -306,7 +306,10 @@ object ResolveWithCTE extends Rule[LogicalPlan] {
       columnNames: Option[Seq[String]]) = {
     recursion.transformUpWithSubqueriesAndPruning(_.containsPattern(CTE)) {
       case r: CTERelationRef if r.recursive && r.cteId == cteDefId =>
-        val ref = UnionLoopRef(r.cteId, anchor.output.map(_.newInstance()), false)
+        // We mark the output of UnionLoopRef as nullable as it may become NULL in the following
+        // iterations.
+        val ref =
+          UnionLoopRef(r.cteId, anchor.output.map(_.newInstance().withNullability(true)), false)
         columnNames.map(UnresolvedSubqueryColumnAliases(_, ref)).getOrElse(ref)
     }
   }
