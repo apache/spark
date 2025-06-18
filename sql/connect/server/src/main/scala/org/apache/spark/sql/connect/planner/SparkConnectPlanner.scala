daa77db3ae6907328b2a6f82c3c69616e271d05f
Ruifeng Zheng <ruifengz@apache.org>
2025-06-18 11:57:23 +0800

    another method

--- sql/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/SparkConnectPlanner.scala
+++ sql/connect/server/src/main/scala/org/apache/spark/sql/connect/planner/SparkConnectPlanner.scala
@@ -2420,11 +2420,7 @@ class SparkConnectPlanner(
   private def transformDrop(rel: proto.Drop): LogicalPlan = {
     var output = Dataset.ofRows(session, transformRelation(rel.getInput))
     if (rel.getColumnsCount > 0) {
-      val cols = rel.getColumnsList.asScala.toSeq.map { expr =>
-        val e = transformExpression(expr)
-        e.setTagValue(LogicalPlan.ALLOW_NON_EXISTENT_COL, ())
-        Column(e)
-      }
+      val cols = rel.getColumnsList.asScala.toSeq.map(expr => Column(transformExpression(expr)))
       output = output.drop(cols.head, cols.tail: _*)
     }
     if (rel.getColumnNamesCount > 0) {
