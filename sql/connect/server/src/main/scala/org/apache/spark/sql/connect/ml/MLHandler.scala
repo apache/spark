1d3f6cd068629d59be9010c955be7bdd029268a9
Weichen Xu <weichen.xu@databricks.com>
2025-06-19 20:07:19 +0800

    c3
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- sql/connect/server/src/main/scala/org/apache/spark/sql/connect/ml/MLHandler.scala
+++ sql/connect/server/src/main/scala/org/apache/spark/sql/connect/ml/MLHandler.scala
@@ -42,7 +42,7 @@ private case class Method(
 
 /**
  * Helper function to get the attribute from an object by reflection
- * c2
+ * c3
  */
 private class AttributeHelper(
     val sessionHolder: SessionHolder,
