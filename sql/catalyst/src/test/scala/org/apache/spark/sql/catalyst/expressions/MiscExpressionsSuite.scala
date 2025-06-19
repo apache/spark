e67769ac554b91cd70db789f8644ee30cd4ab81c
Xi Lyu <xi.lyu@databricks.com>
2025-06-19 12:34:13 +0000

    Fix compile

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/MiscExpressionsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/expressions/MiscExpressionsSuite.scala
@@ -141,14 +141,14 @@ class MiscExpressionsSuite extends SparkFunSuite with ExpressionEvalHelper {
     // Invalid input
     checkErrorInExpression[SparkRuntimeException](
       ZstdDecompress(Literal("invalid input".getBytes("UTF-8"))),
-      errorClass = "INVALID_PARAMETER_VALUE.ZSTD_DECOMPRESS_INPUT",
+      condition = "INVALID_PARAMETER_VALUE.ZSTD_DECOMPRESS_INPUT",
       parameters = Map("parameter" -> "`input`", "functionName" -> "`zstd_decompress`")
     )
 
     // Invalid input: empty byte array
     checkErrorInExpression[SparkRuntimeException](
       ZstdDecompress(Literal("".getBytes("UTF-8"))),
-      errorClass = "INVALID_PARAMETER_VALUE.ZSTD_DECOMPRESS_INPUT",
+      condition = "INVALID_PARAMETER_VALUE.ZSTD_DECOMPRESS_INPUT",
       parameters = Map("parameter" -> "`input`", "functionName" -> "`zstd_decompress`")
     )
   }
