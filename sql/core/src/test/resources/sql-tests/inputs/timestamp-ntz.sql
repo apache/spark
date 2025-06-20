2e9a2fd4a9958df87d5bad5deacb4ab1e5abbc92
Max Gekk <max.gekk@gmail.com>
2025-06-20 20:09:13 +0300

    Fix golden files

--- sql/core/src/test/resources/sql-tests/inputs/timestamp-ntz.sql
+++ sql/core/src/test/resources/sql-tests/inputs/timestamp-ntz.sql
@@ -19,7 +19,7 @@ SELECT make_timestamp_ntz(make_date(2021, 07, 11), make_time(6, 30, 45.678));
 SELECT make_timestamp_ntz(NULL, TIME'00:00:00');
 SELECT make_timestamp_ntz(DATE'1970-01-01', NULL);
 SELECT make_timestamp_ntz(timestamp_ntz'2018-11-17 13:33:33', TIME'0:0:0');
-SELECT make_timestamp_ntz(DATE'2025-06-20', TIMESTAMP'0:0:0');
+SELECT make_timestamp_ntz(DATE'2025-06-20', '0:0:0');
 
 SELECT convert_timezone('Europe/Moscow', 'America/Los_Angeles', timestamp_ntz'2022-01-01 00:00:00');
 SELECT convert_timezone('Europe/Brussels', timestamp_ntz'2022-03-23 00:00:00');
