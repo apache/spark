5e1e4d31d7d9fb2f93ce8c72d32774b560bcd81f
Max Gekk <max.gekk@gmail.com>
2025-06-23 09:33:40 +0200

    [SPARK-52540][SQL] Make TIMESTAMP_NTZ from DATE and TIME
    ### What changes were proposed in this pull request?
    In the PR, I propose to extend the `make_timestamp_ntz` function, and accept a date and time fields.
    
    #### Syntax
    ```sql
    make_timestamp_ntz(date[, time])
    ```
    
    #### Arguments
    
    - date: A date expression
    - time: A time expression
    
    #### Returns
    A TIMESTAMP_NTZ.
    
    #### Examples
    
    ```sql
    > SELECT make_timestamp_ntz(DATE'2014-12-28', TIME'6:30:45.887');
     2014-12-28 06:30:45.887
    ```
    
    ### Why are the changes needed?
    Users will be able to create a timestamp without time zone by combining a time and a date.
    
    ### Does this PR introduce _any_ user-facing change?
    No, it just extends the existing API.
    
    ### How was this patch tested?
    By running the affected test suites:
    ```
    $ build/sbt "test:testOnly *ExpressionInfoSuite"
    $ build/sbt "sql/testOnly org.apache.spark.sql.SQLQueryTestSuite -- -z timestamp-ntz.sql"
    ```
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #51179
    
    Closes #51232 from MaxGekk/make_timestamp_ntz.
    
    Lead-authored-by: Max Gekk <max.gekk@gmail.com>
    Co-authored-by: Maxim Gekk <max.gekk@gmail.com>
    Signed-off-by: Max Gekk <max.gekk@gmail.com>

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/util/DateTimeUtilsSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/util/DateTimeUtilsSuite.scala
@@ -1216,4 +1216,18 @@ class DateTimeUtilsSuite extends SparkFunSuite with Matchers with SQLHelper {
           s"Invalid value for SecondOfMinute (valid values 0 - 59): ${invalidSecond.toLong}"))
     }
   }
+
+  test("makeTimestampNTZ") {
+    assert(makeTimestampNTZ(0, 0) == 0)
+    assert(makeTimestampNTZ(0, localTime(23, 59, 59)) * NANOS_PER_MICROS == localTime(23, 59, 59))
+    assert(makeTimestampNTZ(-1, 0) == -1 * MICROS_PER_DAY)
+    assert(makeTimestampNTZ(-1, localTime(23, 59, 59, 999999)) == -1)
+    assert(makeTimestampNTZ(days(9999, 12, 31), localTime(23, 59, 59, 999999)) ==
+      date(9999, 12, 31, 23, 59, 59, 999999))
+    assert(makeTimestampNTZ(days(1, 1, 1), localTime(0, 0, 0)) == date(1, 1, 1, 0, 0, 0))
+    val msg = intercept[DateTimeException] {
+      makeTimestampNTZ(0, -1)
+    }.getMessage
+    assert(msg.contains("Invalid value"))
+  }
 }
