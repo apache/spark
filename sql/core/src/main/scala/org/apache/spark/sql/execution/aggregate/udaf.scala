a56879e67a11967b54812a8365d5bedcfc3d1861
Dongjoon Hyun <dongjoon@apache.org>
2025-07-01 08:12:19 -0700

    Revert "[SPARK-52023][SQL] Fix data corruption/segfault returning Option[Product] from udaf"
    This reverts commit 3f2a3ba440d9a5e6d719f739f7e026f57a8e1c32.

--- sql/core/src/main/scala/org/apache/spark/sql/execution/aggregate/udaf.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/aggregate/udaf.scala
@@ -530,7 +530,7 @@ case class ScalaAggregator[IN, BUF, OUT](
 
   def eval(buffer: BUF): Any = {
     val row = outputSerializer(agg.finish(buffer))
-    if (outputEncoder.isSerializedAsStructForTopLevel) row else row.get(0, dataType)
+    if (outputEncoder.isSerializedAsStruct) row else row.get(0, dataType)
   }
 
   private[this] lazy val bufferRow = new UnsafeRow(bufferEncoder.namedExpressions.length)
