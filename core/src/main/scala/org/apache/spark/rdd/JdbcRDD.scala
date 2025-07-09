9f5690f956513ff4b30a07ad1bf28607bdf92ef2
Uros Stankovic <uros.stankovic@databricks.com>
2025-07-09 10:21:03 +0200

    Align annotation name with the class name

--- core/src/main/scala/org/apache/spark/rdd/JdbcRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/JdbcRDD.scala
@@ -55,7 +55,7 @@ private[spark] class JdbcPartition(idx: Int, val lower: Long, val upper: Long) e
  *   This should only call getInt, getString, etc; the RDD takes care of calling next.
  *   The default maps a ResultSet to an array of Object.
  */
-@deprecated("JDBC RDD is deprecated, consider using JDBC data source instead.")
+@deprecated("Jdbc RDD is deprecated, consider using JDBC data source instead.")
 class JdbcRDD[T: ClassTag](
     sc: SparkContext,
     getConnection: () => Connection,
