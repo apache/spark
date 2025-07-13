3a2c2f1056d8fcb956f157ced661a95d5332c18d
yhuang-db <itisyuchuan@gmail.com>
2025-07-12 17:30:37 -0700

    add grid test for all types

--- sql/core/src/test/scala/org/apache/spark/sql/ApproxTopKSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/ApproxTopKSuite.scala
@@ -28,6 +28,64 @@ class ApproxTopKSuite extends QueryTest with SharedSparkSession {
 
   import testImplicits._
 
+  val itemsWithTopK: Seq[(String, Seq[Row])] = Seq(
+    ("0, 0, 1, 1, 2, 3, 4, 4",
+      Seq(Row(0, 2), Row(4, 2), Row(1, 2), Row(2, 1), Row(3, 1))), // Int
+    ("'a', 'b', 'c', 'c', 'c', 'c', 'd', 'd'",
+      Seq(Row("c", 4), Row("d", 2), Row("a", 1), Row("b", 1))), // String
+    ("(true), (true), (false), (true), (true), (false), (false)",
+      Seq(Row(true, 4), Row(false, 3))), // Boolean
+    ("cast(0 AS BYTE), cast(0 AS BYTE), cast(0 AS BYTE), cast(0 AS BYTE), " +
+      "cast(1 AS BYTE), cast(1 AS BYTE), cast(1 AS BYTE), cast(2 AS BYTE)",
+      Seq(Row(0, 4), Row(1, 3), Row(2, 1))), // Byte
+    ("cast(0 AS SHORT), cast(0 AS SHORT), cast(0 AS SHORT), cast(0 AS SHORT), " +
+      "cast(1 AS SHORT), cast(1 AS SHORT), cast(1 AS SHORT), cast(2 AS SHORT)",
+      Seq(Row(0, 4), Row(1, 3), Row(2, 1))), // Short
+    ("cast(0 AS LONG), cast(0 AS LONG), cast(0 AS LONG), cast(0 AS LONG), " +
+      "cast(1 AS LONG), cast(1 AS LONG), cast(1 AS LONG), cast(2 AS LONG)",
+      Seq(Row(0, 4), Row(1, 3), Row(2, 1))), // Long
+    ("cast(0.0 AS FLOAT), cast(0.0 AS FLOAT), cast(0.0 AS FLOAT), cast(0.0 AS FLOAT), " +
+      "cast(1.0 AS FLOAT), cast(1.0 AS FLOAT), cast(1.0 AS FLOAT), cast(2.0 AS FLOAT)",
+      Seq(Row(0.0, 4), Row(1.0, 3), Row(2.0, 1))), // Float
+    ("cast(0.0 AS DOUBLE), cast(0.0 AS DOUBLE), cast(0.0 AS DOUBLE), cast(0.0 AS DOUBLE), " +
+      "cast(1.0 AS DOUBLE), cast(1.0 AS DOUBLE), cast(1.0 AS DOUBLE), cast(2.0 AS DOUBLE)",
+      Seq(Row(0.0, 4), Row(1.0, 3), Row(2.0, 1))), // Double
+    ("DATE'2025-01-01', DATE'2025-01-01', DATE'2025-01-01', DATE'2025-01-01', " +
+      "DATE'2025-01-02', DATE'2025-01-02', DATE'2025-01-02', DATE'2025-01-03'",
+      Seq(Row(Date.valueOf("2025-01-01"), 4), Row(Date.valueOf("2025-01-02"), 3),
+        Row(Date.valueOf("2025-01-03"), 1))), // Date
+    ("TIMESTAMP'2025-01-01 00:00:00', TIMESTAMP'2025-01-01 00:00:00', " +
+      "TIMESTAMP'2025-01-01 00:00:00', TIMESTAMP'2025-01-02 00:00:00'",
+      Seq(Row(Timestamp.valueOf("2025-01-01 00:00:00"), 3),
+        Row(Timestamp.valueOf("2025-01-02 00:00:00"), 1))), // Timestamp
+    ("TIMESTAMP_NTZ'2025-01-01 00:00:00', TIMESTAMP_NTZ'2025-01-01 00:00:00', " +
+      "TIMESTAMP_NTZ'2025-01-01 00:00:00', TIMESTAMP_NTZ'2025-01-02 00:00:00'",
+      Seq(Row(LocalDateTime.of(2025, 1, 1, 0, 0), 3),
+        Row(LocalDateTime.of(2025, 1, 2, 0, 0), 1))), // Timestamp_ntz
+    ("CAST(0.0 AS DECIMAL(4, 1)), CAST(0.0 AS DECIMAL(4, 1)), " +
+      "CAST(0.0 AS DECIMAL(4, 1)), CAST(1.0 AS DECIMAL(4, 1)), " +
+      "CAST(1.0 AS DECIMAL(4, 1)), CAST(2.0 AS DECIMAL(4, 1))",
+      Seq(Row(new java.math.BigDecimal("0.0"), 3),
+        Row(new java.math.BigDecimal("1.0"), 2),
+        Row(new java.math.BigDecimal("2.0"), 1))), // Decimal(4, 1)
+    ("CAST(0.0 AS DECIMAL(10, 2)), CAST(0.0 AS DECIMAL(10, 2)), " +
+      "CAST(0.0 AS DECIMAL(10, 2)), CAST(1.0 AS DECIMAL(10, 2)), " +
+      "CAST(1.0 AS DECIMAL(10, 2)), CAST(2.0 AS DECIMAL(10, 2))",
+      Seq(Row(new java.math.BigDecimal("0.00"), 3),
+        Row(new java.math.BigDecimal("1.00"), 2),
+        Row(new java.math.BigDecimal("2.00"), 1))), // Decimal(10, 2)
+    ("CAST(0.0 AS DECIMAL(20, 3)), CAST(0.0 AS DECIMAL(20, 3)), " +
+      "CAST(0.0 AS DECIMAL(20, 3)), CAST(1.0 AS DECIMAL(20, 3)), " +
+      "CAST(1.0 AS DECIMAL(20, 3)), CAST(2.0 AS DECIMAL(20, 3))",
+      Seq(Row(new java.math.BigDecimal("0.000"), 3),
+        Row(new java.math.BigDecimal("1.000"), 2),
+        Row(new java.math.BigDecimal("2.000"), 1))), // Decimal(20, 3)
+    ("(0.0), (0.0), (0.0), (0.0), (1.0), (1.0), (1.0), (2.0)",
+      Seq(Row(new java.math.BigDecimal("0.0"), 4),
+        Row(new java.math.BigDecimal("1.0"), 3),
+        Row(new java.math.BigDecimal("2.0"), 1))) // Decimal default
+  )
+
   /////////////////////////////////
   // approx_top_k tests
   /////////////////////////////////
@@ -51,6 +109,12 @@ class ApproxTopKSuite extends QueryTest with SharedSparkSession {
       "SELECT approx_top_k(expr, 10, 100) FROM VALUES (0), (1), (1), (2), (2), (2) AS tab(expr);"
     )
     checkAnswer(res, Row(Seq(Row(2, 3), Row(1, 2), Row(0, 1))))
+  }
+
+  gridTest("SPARK-52515: test of different types")(itemsWithTopK) {
+    case (input, expected) =>
+      val res = sql(s"SELECT approx_top_k(expr) FROM VALUES $input AS tab(expr);")
+      checkAnswer(res, Row(expected))
   }
 
   test("SPARK-52515: test of Integer type") {
@@ -330,6 +394,13 @@ class ApproxTopKSuite extends QueryTest with SharedSparkSession {
     checkAnswer(res, Row(Seq(
       Row(new java.math.BigDecimal("0.000"), 3),
       Row(new java.math.BigDecimal("1.000"), 2))))
+  }
+
+  gridTest("SPARK-52588: accumulate and estimate of different types")(itemsWithTopK) {
+    case (input, expected) =>
+      val res = sql(s"SELECT approx_top_k_estimate(approx_top_k_accumulate(expr)) " +
+        s"FROM VALUES $input AS tab(expr);")
+      checkAnswer(res, Row(expected))
   }
 
   test("SPARK-52588: invalid accumulate if maxItemsTracked is null") {
