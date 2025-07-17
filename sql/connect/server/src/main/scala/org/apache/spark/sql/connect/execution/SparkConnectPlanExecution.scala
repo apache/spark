61272bce2136226962bf3c6f964fa397c28734a0
Karuppayya Rajendran <karuppayya.rajendran@salesforce.com>
2025-07-16 18:25:07 -0700

    Fix build: Remove unused import

--- sql/connect/server/src/main/scala/org/apache/spark/sql/connect/execution/SparkConnectPlanExecution.scala
+++ sql/connect/server/src/main/scala/org/apache/spark/sql/connect/execution/SparkConnectPlanExecution.scala
@@ -41,7 +41,6 @@ import org.apache.spark.sql.execution.{DoNotCleanup, LocalTableScanExec, QueryEx
 import org.apache.spark.sql.execution.{LocalTableScanExec, QueryExecution, SQLExecution}
 >>>>>>> af1b66ffe8f (SPARK-52777: Enable shuffle cleanup mode configuration in Spark SQL)
 import org.apache.spark.sql.execution.arrow.ArrowConverters
-import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.util.ThreadUtils
 
