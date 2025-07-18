f3ae40dcfa7acdac793ab83426ecd6c029cdc043
Dongjoon Hyun <dongjoon@apache.org>
2025-07-18 15:49:57 -0700

    [SPARK-52876][CORE] Fix a typo `buffer` to `body` in `ChunkFetchSuccess.toString`

--- common/network-common/src/main/java/org/apache/spark/network/protocol/ChunkFetchSuccess.java
+++ common/network-common/src/main/java/org/apache/spark/network/protocol/ChunkFetchSuccess.java
@@ -85,7 +85,7 @@ public final class ChunkFetchSuccess extends AbstractResponseMessage {
   public String toString() {
     return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
       .append("streamChunkId", streamChunkId)
-      .append("buffer", body())
+      .append("body", body())
       .toString();
   }
 }
