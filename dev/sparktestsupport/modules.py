e90dbce11d7632b6a1bef9178ed558964f7feaff
Jacky Wang <jacky.wang@databricks.com>
2025-07-21 16:20:23 -0700

    register new python test file

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -1517,6 +1517,7 @@ pyspark_pipelines = Module(
     source_file_regexes=["python/pyspark/pipelines"],
     python_test_goals=[
         "pyspark.pipelines.tests.test_block_connect_access",
+        "pyspark.pipelines.tests.test_block_imperative_construct.py",
         "pyspark.pipelines.tests.test_cli",
         "pyspark.pipelines.tests.test_decorators",
         "pyspark.pipelines.tests.test_graph_element_registry",
