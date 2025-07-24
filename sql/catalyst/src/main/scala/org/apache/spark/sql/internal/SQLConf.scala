d8ba43137f5dfd19640a1446d18f0a54b5fd652f
DenineLu <deninelu@163.com>
2025-07-24 13:36:21 +0800

    update conf description

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -6161,8 +6161,14 @@ object SQLConf {
   val LEGACY_TRUNCATE_FOR_EMPTY_REGEX_SPLIT =
     buildConf("spark.sql.legacy.truncateForEmptyRegexSplit")
       .internal()
-      .doc("When set to true, using empty regex to split will discard the trailing string, which " +
-        "is consistent with the behavior of Spark 3.4 - Spark 4.0. See SPARK-49968 for detail.")
+      .doc("When set to true, splitting a string of length n using an empty regex with a " +
+        "positive limit discards the last n - limit characters." +
+        "For example: SELECT split('abcd', '', 2) returns ['a', 'b']." +
+        "When set to false, the last element of the resulting array contains all input beyond " +
+        "the last matched regex." +
+        "For example: SELECT split('abcd', '', 2) returns ['a', 'bcd']." +
+        "According to the description of the split function, this should be set to false by " +
+        "default. See SPARK-49968 for details.")
       .version("4.1.0")
       .booleanConf
       .createWithDefault(false)
