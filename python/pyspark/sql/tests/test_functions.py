a6c664afded929ca42ca58bf6c0a4facc10f69aa
Uros Bojanic <uros.bojanic@databricks.com>
2025-07-27 19:58:40 +0200

    Remove TODO

--- python/pyspark/sql/tests/test_functions.py
+++ python/pyspark/sql/tests/test_functions.py
@@ -82,11 +82,10 @@ class FunctionsTestsMixin:
 
         # Functions that we expect to be missing in python until they are added to pyspark
         expected_missing_in_py = set(
-            # TODO(SPARK-52888): Implement the make_time function in Python
             # TODO(SPARK-52889): Implement the current_time function in Python
             # TODO(SPARK-52890): Implement the to_time function in Python
             # TODO(SPARK-52891): Implement the try_to_time function in Python
-            ["current_time", "make_time", "to_time", "try_to_time"]
+            ["current_time", "to_time", "try_to_time"]
         )
 
         self.assertEqual(
