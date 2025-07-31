706075540246da5c1fa49fc75168dbce5ac4ff19
Ruifeng Zheng <ruifengz@apache.org>
2025-07-31 11:01:37 +0800

    test

--- python/pyspark/sql/pandas/functions.py
+++ python/pyspark/sql/pandas/functions.py
@@ -388,21 +388,26 @@ def pandas_udf(f=None, returnType=None, functionType=None):
     `pandas.DataFrame` as below:
 
     >>> @pandas_udf("col1 string, col2 long")
-    >>> def func(s1: pd.Series, s2: pd.Series, s3: pd.DataFrame) -> pd.DataFrame:
+    ... def func(s1: pd.Series, s2: pd.Series, s3: pd.DataFrame) -> pd.DataFrame:
     ...     s3['col2'] = s1 + s2.str.len()
     ...     return s3
-    ...
-    >>> # Create a Spark DataFrame that has three columns including a struct column.
-    ... df = spark.createDataFrame(
+
+
+    Create a Spark DataFrame that has three columns including a struct column.
+
+    >>> df = spark.createDataFrame(
     ...     [[1, "a string", ("a nested string",)]],
     ...     "long_col long, string_col string, struct_col struct<col1:string>")
+
     >>> df.printSchema()
     root
-    |-- long_column: long (nullable = true)
-    |-- string_column: string (nullable = true)
-    |-- struct_column: struct (nullable = true)
+    |-- long_col: long (nullable = true)
+    |-- string_col: string (nullable = true)
+    |-- struct_col: struct (nullable = true)
     |    |-- col1: string (nullable = true)
+
     >>> df.select(func("long_col", "string_col", "struct_col")).printSchema()
+    root
     |-- func(long_col, string_col, struct_col): struct (nullable = true)
     |    |-- col1: string (nullable = true)
     |    |-- col2: long (nullable = true)
