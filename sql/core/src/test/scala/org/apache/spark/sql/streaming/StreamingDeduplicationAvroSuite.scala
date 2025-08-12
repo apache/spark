e3cc25426fead113b4bb31bb7c00ad3f59c7785a
zeruibao <zerui.bao@databricks.com>
2025-08-12 15:21:50 -0700

    save

---
+++ sql/core/src/test/scala/org/apache/spark/sql/streaming/StreamingDeduplicationAvroSuite.scala
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.streaming
+
+import org.scalactic.source.Position
+import org.scalatest.Tag
+
+import org.apache.spark.sql.internal.SQLConf
+
+class StreamingDeduplicationAvroSuite extends StreamingDeduplicationSuite {
+
+  import testImplicits._
+
+  override protected def test(testName: String, testTags: Tag*)(testBody: => Any)
+                             (implicit pos: Position): Unit = {
+    super.test(s"$testName (encoding = Avro)", testTags: _*) {
+      withSQLConf(SQLConf.STREAMING_STATE_STORE_ENCODING_FORMAT.key -> "avro") {
+        testBody
+      }
+    }
+  }
+
+  test("deduplicate with add fields schema evolution") {
+
+  }
+
+  test("deduplicate with remove fields schema evolution") {
+
+  }
+
+  test("deduplicate with reorder fields schema evolution") {
+
+  }
+
+  test("deduplicate with upcast fields schema evolution") {
+
+  }
+
+  test("deduplicate with downcast fields would fail") {
+
+  }
+}
