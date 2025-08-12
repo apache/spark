a3f49ec2b291bbbfb268e64bc51b19b558ede9c1
Szehon Ho <szehon.apache@gmail.com>
2025-08-12 02:19:17 -0700

    do the update star and insert star trimming regardless of schema evolution

--- sql/core/src/test/scala/org/apache/spark/sql/connector/DeltaBasedMergeIntoTableSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/connector/DeltaBasedMergeIntoTableSuite.scala
@@ -18,7 +18,7 @@
 package org.apache.spark.sql.connector
 
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{ArrayType, BooleanType, IntegerType, MapType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.StructType
 
 class DeltaBasedMergeIntoTableSuite extends DeltaBasedMergeIntoTableSuiteBase {
 
@@ -75,68 +75,6 @@ class DeltaBasedMergeIntoTableSuite extends DeltaBasedMergeIntoTableSuiteBase {
         updateWriteLogEntry(id = 4, metadata = Row("hr", null), data = Row(4, 401, "hr")),
         updateWriteLogEntry(id = 5, metadata = Row("hr", null), data = Row(5, 501, "hr")),
         insertWriteLogEntry(data = Row(6, 0, "new")))
-    }
-  }
-
-  test("merge into schema evolution add column with nested field and set explicit columns" +
-    "2") {
-    Seq(true, false).foreach { withSchemaEvolution =>
-      withTempView("source") {
-        createAndInitTable(
-          s"""pk INT NOT NULL,
-             |s STRUCT<c1: INT, c2: STRUCT<a: ARRAY<INT>, m: MAP<STRING, STRING>>>,
-             |dep STRING""".stripMargin,
-          """{ "pk": 1, "s": { "c1": 2, "c2": { "a": [1,2], "m": { "a": "b" } } }, "dep": "hr" }""")
-
-        val sourceTableSchema = StructType(Seq(
-          StructField("pk", IntegerType, nullable = false),
-          StructField("s", StructType(Seq(
-            StructField("c1", IntegerType),
-            StructField("c2", StructType(Seq(
-              StructField("a", ArrayType(IntegerType)),
-              StructField("m", MapType(StringType, StringType)),
-              StructField("c3", BooleanType) // new column
-            )))
-          ))),
-          StructField("dep", StringType)
-        ))
-        val data = Seq(
-          Row(1, Row(10, Row(Array(3, 4), Map("c" -> "d"), false)), "sales"),
-          Row(2, Row(20, Row(Array(4, 5), Map("e" -> "f"), true)), "engineering")
-        )
-        spark.createDataFrame(spark.sparkContext.parallelize(data), sourceTableSchema)
-          .createOrReplaceTempView("source")
-
-        val schemaEvolutionClause = if (withSchemaEvolution) "WITH SCHEMA EVOLUTION" else ""
-        val mergeStmt =
-          s"""MERGE $schemaEvolutionClause
-             |INTO $tableNameAsString t
-             |USING source src
-             |ON t.pk = src.pk
-             |WHEN MATCHED THEN
-             | UPDATE SET s.c1 = -1, s.c2.m = map('k', 'v'), s.c2.a = array(-1),
-             | s.c2.c3 = src.s.c2.c3
-             |WHEN NOT MATCHED THEN
-             | INSERT (pk, s, dep) VALUES (src.pk,
-             |   named_struct('c1', src.s.c1,
-             |     'c2', named_struct('a', src.s.c2.a, 'm', map('g', 'h'), 'c3', true)), src.dep)
-             |""".stripMargin
-
-        if (withSchemaEvolution) {
-          sql(mergeStmt)
-          checkAnswer(
-            sql(s"SELECT * FROM $tableNameAsString"),
-            Seq(Row(1, Row(-1, Row(Seq(-1), Map("k" -> "v"), false)), "hr"),
-              Row(2, Row(20, Row(Seq(4, 5), Map("g" -> "h"), true)), "engineering")))
-        } else {
-          val exception = intercept[org.apache.spark.sql.AnalysisException] {
-            sql(mergeStmt)
-          }
-          assert(exception.errorClass.get == "FIELD_NOT_FOUND")
-          assert(exception.getMessage.contains("No such struct field `c3` in `a`, `m`. "))
-        }
-      }
-      sql(s"DROP TABLE IF EXISTS $tableNameAsString")
     }
   }
 }
