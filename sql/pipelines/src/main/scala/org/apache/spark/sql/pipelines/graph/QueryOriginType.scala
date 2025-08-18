b34b95088f2bcb5fc4bd87667ef5b4fcf307713a
yangjie01 <yangjie01@baidu.com>
2025-08-18 21:22:31 +0800

    [SPARK-53306][SQL][CONNECT][YARN][TESTS] Fix wrong package statements
    ### What changes were proposed in this pull request?
    This pr aims to correct the incorrect package name to ensure it matches the path where the file is located.
    
    Specifically, the current PR involves renaming two package object filenames:
    
    - `sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/resolver.scala` is renamed to `sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/resolver/package.scala`
    - `resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/config.scala` is renamed to `resource-managers/yarn/src/main/scala/org/apache/spark/deploy/yarn/config/package.scala`
    
    These changes correct the file paths and names to align with what they should be for `package object` and will not affect compatibility.
    
    ### Why are the changes needed?
    Fix wrong package statements
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    - Pass GitHub Actions
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No
    
    Closes #52059 from LuciferYang/SPARK-53306.
    
    Authored-by: yangjie01 <yangjie01@baidu.com>
    Signed-off-by: yangjie01 <yangjie01@baidu.com>

--- sql/pipelines/src/main/scala/org/apache/spark/sql/pipelines/graph/QueryOriginType.scala
+++ sql/pipelines/src/main/scala/org/apache/spark/sql/pipelines/graph/QueryOriginType.scala
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.spark.sql.pipelines
+package org.apache.spark.sql.pipelines.graph
 
 object QueryOriginType extends Enumeration {
   type QueryOriginType = Value
