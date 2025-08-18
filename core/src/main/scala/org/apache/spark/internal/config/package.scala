7976a47c89a315de25c389ab8b847195d48749c5
Cheng Pan <chengpan@apache.org>
2025-08-18 09:24:07 +0800

    Test Hadoop 3.4.2 RC2

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -2748,7 +2748,7 @@ package object config {
       .stringConf
       .toSequence
       .createWithDefault(
-        Seq("https://repository.apache.org/content/repositories/orgapachehadoop-1437/"))
+        Seq("https://repository.apache.org/content/repositories/orgapachehadoop-1441/"))
 
   private[spark] val APP_ATTEMPT_ID =
     ConfigBuilder("spark.app.attempt.id")
