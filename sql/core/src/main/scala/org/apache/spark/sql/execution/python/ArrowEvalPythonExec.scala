b8c786e5991276efd3f83a40270696c649b59721
Ben Hurdelhey <ben.hurdelhey@databricks.com>
2025-08-19 21:30:46 +0000

    try recursive equality check

--- sql/core/src/main/scala/org/apache/spark/sql/execution/python/ArrowEvalPythonExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/python/ArrowEvalPythonExec.scala
@@ -27,7 +27,7 @@ import org.apache.spark.sql.errors.QueryExecutionErrors
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.execution.metric.SQLMetric
 import org.apache.spark.sql.execution.python.EvalPythonExec.ArgumentMetadata
-import org.apache.spark.sql.types.{DataType, DayTimeIntervalType, StructType, UserDefinedType}
+import org.apache.spark.sql.types.{DataType, DayTimeIntervalType, StructType, UserDefinedType, ArrayType, MapType}
 import org.apache.spark.sql.types.DataType.equalsIgnoreCompatibleCollation
 import org.apache.spark.sql.vectorized.{ArrowColumnVector, ColumnarBatch}
 
@@ -129,13 +129,34 @@ class ArrowEvalPythonEvaluatorFactory(
       expectedTypes: Seq[DataType],
       actualTypes: Seq[DataType]): Boolean = {
     expectedTypes.length == actualTypes.length &&
-    expectedTypes.zip(actualTypes).forall {
+    expectedTypes.zip(actualTypes).forall { case (expected, actual) =>
+      typesEqualRecursive(expected, actual)
+    }
+  }
+
+  private def typesEqualRecursive(expected: DataType, actual: DataType): Boolean = {
+    (expected, actual) match {
       case (expected: DayTimeIntervalType, actual: DayTimeIntervalType) =>
         // Use lenient type checking that treats DayTimeIntervalType variants as compatible
         // Arrow always returns the broadest range, so as long as we're not losing information,
         // we can consider the types equal.
         actual.startField <= expected.startField && expected.endField <= actual.endField
-      case (expected, actual) => equalsIgnoreCompatibleCollation(expected, actual)
+      
+      case (expected: ArrayType, actual: ArrayType) =>
+        typesEqualRecursive(expected.elementType, actual.elementType)
+      
+      case (expected: MapType, actual: MapType) =>
+        typesEqualRecursive(expected.keyType, actual.keyType) &&
+        typesEqualRecursive(expected.valueType, actual.valueType)
+      
+      case (expected: StructType, actual: StructType) =>
+        expected.fields.length == actual.fields.length &&
+        expected.fields.zip(actual.fields).forall { case (expectedField, actualField) =>
+          expectedField.name == actualField.name &&
+          typesEqualRecursive(expectedField.dataType, actualField.dataType)
+        }
+      
+      case _ => equalsIgnoreCompatibleCollation(expected, actual)
     }
   }
 
