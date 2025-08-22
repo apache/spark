3cf81ab224808005330ff4c761b3990238679322
Yi Wu <yi.wu@databricks.com>
2025-08-22 16:51:48 +0800

    fmt

--- common/network-shuffle/src/main/java/org/apache/spark/network/shuffle/BlockIdUtils.java
+++ common/network-shuffle/src/main/java/org/apache/spark/network/shuffle/BlockIdUtils.java
@@ -20,71 +20,68 @@ package org.apache.spark.network.shuffle;
 import org.apache.spark.network.shuffle.checksum.ShuffleChecksumHelper;
 
 public class BlockIdUtils {
-    public static final String SHUFFLE_BLOCK_PREFIX = "shuffle_";
+  public static final String SHUFFLE_BLOCK_PREFIX = "shuffle_";
+  public static final String SHUFFLE_CHUNK_PREFIX = "shuffleChunk_";
+  public static final String RDD_BLOCK_PREFIX = "rdd_";
+  private static final String NOOP_REDUCE_ID = "0";
 
-    public static final String SHUFFLE_CHUNK_PREFIX = "shuffleChunk_";
+  public static Boolean isRddBlock(String blockId) {
+    if (blockId.startsWith(RDD_BLOCK_PREFIX)) return true;
+    return false;
+  }
 
-    public static final String RDD_BLOCK_PREFIX = "rdd_";
+  public static Boolean isShuffleBlock(String blockId) {
+    if (blockId.startsWith(SHUFFLE_BLOCK_PREFIX)) return true;
+    return false;
+  }
 
-    private static final String NOOP_REDUCE_ID = "0";
+  public static Boolean isShuffleChunk(String blockId) {
+    // Shuffle chunk block is not supported by Aether, and we don't need to check for wrapped
+    // Aether blocks.
+    return blockId.startsWith(SHUFFLE_CHUNK_PREFIX);
+  }
 
-    public static Boolean isRddBlock(String blockId) {
-        if (blockId.startsWith(RDD_BLOCK_PREFIX)) return true;
-        return false;
+  public static int getShuffleId(String blockId) {
+    String[] blockIdParts = blockId.split("_");
+    if (blockIdParts.length <  4) {
+      throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
+    }
+    return Integer.parseInt(blockIdParts[1]);
+  }
+
+  public static long getMapId(String blockId) {
+    String[] blockIdParts = blockId.split("_");
+    if (blockIdParts.length <  4) {
+      throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
+    }
+    return Long.parseLong(blockIdParts[2]);
+  }
+
+  public static int getReduceId(String blockId) {
+    String[] blockIdParts = blockId.split("_");
+    if (blockIdParts.length <  4) {
+      throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
+    }
+    return Integer.parseInt(blockIdParts[3]);
+  }
+
+  /**
+   * Returns the name of the ShuffleChecksumBlockId. This should be in sync with
+   *  IndexShuffleBlockResolver.getChecksumFile
+   */
+  static public String getChecksumFileName(String blockId, String algorithm) {
+    String[] blockIdParts = blockId.split("_");
+    if (blockIdParts.length <  4) {
+      throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
+    }
+    // Replace the reduceId with NOOP_REDUCE_ID to get the checksum file name
+    if (blockIdParts.length == 4 && isShuffleBlock(blockId)) {
+      blockIdParts[3] = NOOP_REDUCE_ID;
+    } else {
+      throw new IllegalArgumentException("Unexpected block id format");
     }
 
-    public static Boolean isShuffleBlock(String blockId) {
-        if (blockId.startsWith(SHUFFLE_BLOCK_PREFIX)) return true;
-        return false;
-    }
-
-    public static Boolean isShuffleChunk(String blockId) {
-        // Shuffle chunk block is not supported by Aether, and we don't need to check for wrapped
-        // Aether blocks.
-        return blockId.startsWith(SHUFFLE_CHUNK_PREFIX);
-    }
-
-    public static int getShuffleId(String blockId) {
-        String[] blockIdParts = blockId.split("_");
-        if (blockIdParts.length <  4) {
-          throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
-        }
-        return Integer.parseInt(blockIdParts[1]);
-    }
-
-    public static long getMapId(String blockId) {
-        String[] blockIdParts = blockId.split("_");
-        if (blockIdParts.length <  4) {
-          throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
-        }
-        return Long.parseLong(blockIdParts[2]);
-    }
-
-    public static int getReduceId(String blockId) {
-        String[] blockIdParts = blockId.split("_");
-        if (blockIdParts.length <  4) {
-          throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
-        }
-        return Integer.parseInt(blockIdParts[3]);
-    }
-
-    /**
-     * Returns the name of the ShuffleChecksumBlockId. This should be in sync with
-     *  IndexShuffleBlockResolver.getChecksumFile
-     */
-    static public String getChecksumFileName(String blockId, String algorithm) {
-        String[] blockIdParts = blockId.split("_");
-        if (blockIdParts.length <  4) {
-          throw new IllegalArgumentException("Unexpected shuffle block id format: " + blockId);
-        }
-        // Replace the reduceId with NOOP_REDUCE_ID to get the checksum file name
-        if (blockIdParts.length == 4 && isShuffleBlock(blockId)) {
-          blockIdParts[3] = NOOP_REDUCE_ID;
-        } else {
-          throw new IllegalArgumentException("Unexpected block id format");
-        }
-
-        return ShuffleChecksumHelper.getChecksumFileName(
-         String.join("_", blockIdParts) + ".checksum", algorithm);
-    }
+    return ShuffleChecksumHelper.getChecksumFileName(
+     String.join("_", blockIdParts) + ".checksum", algorithm);
+  }
 }
