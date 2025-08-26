d2a254d36f14cc87e37a48c4c15559db720213c2
Ben Hurdelhey <ben.hurdelhey@databricks.com>
2025-08-26 14:01:03 +0000

    format

--- python/pyspark/sql/tests/arrow/test_arrow_python_udf.py
+++ python/pyspark/sql/tests/arrow/test_arrow_python_udf.py
@@ -206,7 +206,9 @@ class ArrowPythonUDFTestsMixin(BaseUDFTestsMixin):
             with self.sql_conf(
                 {"spark.sql.execution.pythonUDF.pandas.intToDecimalCoercionEnabled": False}
             ):
-                with self.assertRaisesRegex(PythonException, "An exception was thrown from the Python worker"):
+                with self.assertRaisesRegex(
+                    PythonException, "An exception was thrown from the Python worker"
+                ):
                     df.select(int_to_decimal_udf("id").alias("decimal_val")).collect()
 
             @udf(returnType="decimal(25,1)", useArrow=True)
@@ -228,7 +230,9 @@ class ArrowPythonUDFTestsMixin(BaseUDFTestsMixin):
             with self.sql_conf(
                 {"spark.sql.execution.pythonUDF.pandas.intToDecimalCoercionEnabled": False}
             ):
-                with self.assertRaisesRegex(PythonException, "An exception was thrown from the Python worker"):
+                with self.assertRaisesRegex(
+                    PythonException, "An exception was thrown from the Python worker"
+                ):
                     df.select(high_precision_udf("id").alias("decimal_val")).collect()
 
     def test_err_return_type(self):
