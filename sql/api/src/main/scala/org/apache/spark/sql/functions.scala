9c5015603acc80fe10da3be941d7ad0de293afb8
Uros Bojanic <uros.bojanic@databricks.com>
2025-09-01 11:35:11 +0800

    [SPARK-53108][SQL] Implement the time_diff function in Scala
    ### What changes were proposed in this pull request?
    Implement the `time_diff` function in Scala API.
    
    ### Why are the changes needed?
    Expand API support for the `TimeDiff` expression.
    
    ### Does this PR introduce _any_ user-facing change?
    Yes, the new function is now available in Scala API.
    
    ### How was this patch tested?
    Added appropriate Scala function tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #51826 from uros-db/scala-time_diff.
    
    Authored-by: Uros Bojanic <uros.bojanic@databricks.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/api/src/main/scala/org/apache/spark/sql/functions.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/functions.scala
@@ -6293,6 +6293,28 @@ object functions {
     Column.internalFn("timestampadd", lit(unit), quantity, ts)
 
   /**
+   * Returns the difference between two times, measured in specified units. Throws a
+   * SparkIllegalArgumentException, in case the specified unit is not supported.
+   *
+   * @param unit
+   *   A STRING representing the unit of the time difference. Supported units are: "HOUR",
+   *   "MINUTE", "SECOND", "MILLISECOND", and "MICROSECOND". The unit is case-insensitive.
+   * @param start
+   *   A starting TIME.
+   * @param end
+   *   An ending TIME.
+   * @return
+   *   The difference between `end` and `start` times, measured in specified units.
+   * @note
+   *   If any of the inputs is `NULL`, the result is `NULL`.
+   * @group datetime_funcs
+   * @since 4.1.0
+   */
+  def time_diff(unit: Column, start: Column, end: Column): Column = {
+    Column.fn("time_diff", unit, start, end)
+  }
+
+  /**
    * Returns `time` truncated to the `unit`.
    *
    * @param unit
