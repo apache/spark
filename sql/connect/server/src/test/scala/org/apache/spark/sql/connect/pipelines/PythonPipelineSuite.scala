7f5db0c1c55742027b7d8417b524b768e7ab8e18
Jacky Wang <jacky.wang@databricks.com>
2025-09-02 22:29:43 -0700

    [SPARK-52346][SDP] Propagate partition columns from destination for BatchTableWrite
    ### What changes were proposed in this pull request?
    
    Propagate partition columns specified in the destination table into during flow execution for batch table write.
    
    Fix below exception during flow execution:
    ```
    org.apache.spark.sql.AnalysisException: Specified partitioning does not match that of the existing table spark_catalog.default.mv.
    Specified partition columns: [].
    Existing partition columns: [id_mod].
    ```
    
    ### Why are the changes needed?
    
    In "append" mode with saveAsTable, partition columns must be specified in query because the format and options of the existing table is used, and the table could have been created with partition columns.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    New unit tests
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #52119 from JiaqiWang18/flow-exe-propagate-partition-columns.
    
    Authored-by: Jacky Wang <jacky.wang@databricks.com>
    Signed-off-by: Gengliang Wang <gengliang@apache.org>

--- sql/connect/server/src/test/scala/org/apache/spark/sql/connect/pipelines/PythonPipelineSuite.scala
+++ sql/connect/server/src/test/scala/org/apache/spark/sql/connect/pipelines/PythonPipelineSuite.scala
@@ -30,6 +30,7 @@ import org.apache.spark.api.python.PythonUtils
 import org.apache.spark.sql.AnalysisException
 import org.apache.spark.sql.catalyst.TableIdentifier
 import org.apache.spark.sql.connect.service.SparkConnectService
+import org.apache.spark.sql.connector.catalog.{Identifier, TableCatalog}
 import org.apache.spark.sql.pipelines.graph.{DataflowGraph, PipelineUpdateContextImpl}
 import org.apache.spark.sql.pipelines.utils.{EventVerificationTestHelpers, TestPipelineUpdateContextMixin}
 
@@ -491,6 +492,39 @@ class PythonPipelineSuite
       assert(df.count() == 3 + 1) // 3 ids + 1 total
       val totalRow = df.filter("id IS NULL").collect().head
       assert(totalRow.getLong(1) == 3L && totalRow.getLong(2) == 3L)
+    }
+  }
+
+  test("MV/ST with partition columns works") {
+    withTable("mv", "st") {
+      val graph = buildGraph("""
+             |from pyspark.sql.functions import col
+             |
+             |@dp.materialized_view(partition_cols = ["id_mod"])
+             |def mv():
+             |  return spark.range(5).withColumn("id_mod", col("id") % 2)
+             |
+             |@dp.table(partition_cols = ["id_mod"])
+             |def st():
+             |  return spark.readStream.table("mv")
+             |""".stripMargin)
+
+      val updateContext = new PipelineUpdateContextImpl(graph, eventCallback = _ => ())
+      updateContext.pipelineExecution.runPipeline()
+      updateContext.pipelineExecution.awaitCompletion()
+
+      // check table is created with correct partitioning
+      val catalog = spark.sessionState.catalogManager.currentCatalog.asInstanceOf[TableCatalog]
+
+      Seq("mv", "st").foreach { tableName =>
+        val table = catalog.loadTable(Identifier.of(Array("default"), tableName))
+        assert(
+          table.partitioning().map(_.references().head.fieldNames().head) === Array("id_mod"))
+
+        val rows = spark.table(tableName).collect().map(r => (r.getLong(0), r.getLong(1))).toSet
+        val expected = (0 until 5).map(id => (id.toLong, (id % 2).toLong)).toSet
+        assert(rows == expected)
+      }
     }
   }
 
