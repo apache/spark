b01747341918b4ac4e13ec35c9e816fa9239754b
Shujing Yang <shujing.yang@databricks.com>
2025-09-03 23:13:12 +0800

    [SPARK-53401][SQL] Enable Direct Passthrough Partitioning in the DataFrame API
    ### What changes were proposed in this pull request?
    
     Currently, Spark's DataFrame repartition() API only supports hash-based and range-based partitioning strategies. Users who need precise control over which partition each row goes to (similar to RDD's partitionBy with custom partitioners) have no direct way to achieve this at the DataFrame level.
    
    This PR introduces a new DataFrame API, `repartitionById(col, numPartitions)`, an API that allows users to directly specify target partition IDs in DataFrame repartitioning operations:
    
    ```
    // Partition rows based on a computed partition ID
    val df = spark.range(100).withColumn("partition_id", col("id") % 10)
    val repartitioned = df.repartitionById($"partition_id", 10)
    ```
    
    ### Why are the changes needed?
    
    Enable precise control over which partition each row goes to (similar to RDD's partitionBy with custom partitioners) at the DataFrame level
    
    ### Does this PR introduce _any_ user-facing change?
    
    Yes.
    ```
    // Partition rows based on a computed partition ID
    val df = spark.range(100).withColumn("partition_id", col("id") % 10)
    val repartitioned = df.repartitionById($"partition_id", 10)
    ```
    
    ### How was this patch tested?
    
    New Unit Tests
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #52153 from shujingyang-db/direct-partitionId-pass-through.
    
    Lead-authored-by: Shujing Yang <shujing.yang@databricks.com>
    Co-authored-by: Shujing Yang <135740748+shujingyang-db@users.noreply.github.com>
    Co-authored-by: Wenchen Fan <cloud0fan@gmail.com>
    Signed-off-by: Wenchen Fan <wenchen@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/classic/Dataset.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/classic/Dataset.scala
@@ -1544,6 +1544,21 @@ class Dataset[T] private[sql](
     }
   }
 
+  /**
+   * Repartitions the Dataset into the given number of partitions using the specified
+   * partition ID expression.
+   *
+   * @param numPartitions the number of partitions to use.
+   * @param partitionIdExpr the expression to be used as the partition ID. Must be an integer type.
+   *
+   * @group typedrel
+   * @since 4.1.0
+   */
+  def repartitionById(numPartitions: Int, partitionIdExpr: Column): Dataset[T] = {
+    val directShufflePartitionIdCol = Column(DirectShufflePartitionID(partitionIdExpr.expr))
+    repartitionByExpression(Some(numPartitions), Seq(directShufflePartitionIdCol))
+  }
+
   protected def repartitionByRange(
       numPartitions: Option[Int],
       partitionExprs: Seq[Column]): Dataset[T] = {
