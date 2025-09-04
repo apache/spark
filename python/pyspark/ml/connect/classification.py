b57351c636359d670e98c8bae161fd27f41a73a8
Ruifeng Zheng <ruifengz@apache.org>
2025-09-04 10:48:52 +0800

    test

--- python/pyspark/ml/connect/classification.py
+++ python/pyspark/ml/connect/classification.py
@@ -381,7 +381,7 @@ class LogisticRegressionModel(
     def _load_core_model(self, path: str) -> None:
         import torch
 
-        lor_torch_model = torch.load(path, weights_only=False)
+        lor_torch_model = torch.load(path, weights_only=True)
         self.torch_model = lor_torch_model[0]
 
     def _get_extra_metadata(self) -> Dict[str, Any]:
