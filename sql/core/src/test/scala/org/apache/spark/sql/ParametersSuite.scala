d458986b0a0b9444e963651e69b688d5b3442ffc
pavle-martinovic_data <pavle.martinovic@databricks.com>
2025-09-11 09:12:57 +0200

    undo test change

--- sql/core/src/test/scala/org/apache/spark/sql/ParametersSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/ParametersSuite.scala
@@ -789,12 +789,11 @@ class ParametersSuite extends QueryTest with SharedSparkSession {
     spark.conf.set("spark.sql.cteRecursionRowLimit", "50")
     def query(p: String): String = {
       s"""
-         |WITH RECURSIVE t MAX RECURSION LEVEL 100 AS (
-         |    SELECT 1 AS n
-         |    UNION ALL
-         |    SELECT n + 1 FROM t WHERE n < 60
-         |    )
-         |   SELECT * FROM t LIMIT ALL""".stripMargin
+         |WITH RECURSIVE t1(n) AS (
+         |  SELECT 1
+         |  UNION ALL
+         |  SELECT n+1 FROM IDENTIFIER($p) WHERE n < 5)
+         |SELECT * FROM t1""".stripMargin
     }
 
     checkAnswer(spark.sql(query(":cte"), args = Map("cte" -> "t1")),
