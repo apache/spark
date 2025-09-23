cd6966ee657a81f26332ad61b2b61374f6fd059f
Ruifeng Zheng <ruifengz@apache.org>
2025-09-23 16:12:19 +0800

    test

--- python/pyspark/sql/tests/udf_type_tests/test_udf_return_types.py
+++ python/pyspark/sql/tests/udf_type_tests/test_udf_return_types.py
@@ -43,31 +43,24 @@ from pyspark.sql.types import (
     StructType,
     TimestampType,
 )
-from pyspark.loose_version import LooseVersion
 from pyspark.testing.utils import (
     have_pyarrow,
     have_pandas,
-    have_numpy,
     pyarrow_requirement_message,
     pandas_requirement_message,
-    numpy_requirement_message,
 )
 from pyspark.testing.sqlutils import ReusedSQLTestCase
 from .type_table_utils import generate_table_diff, format_type_table
-
-if have_numpy:
-    import numpy as np
 
 
 @unittest.skipIf(
     not have_pandas
     or not have_pyarrow
-    or not have_numpy
-    or LooseVersion(np.__version__) <= LooseVersion("2.0.0"),
+    or os.environ.get("PYTHON_MINIMUM", "false") == "true"
+    or os.environ.get("PYTHON_PS_MINIMUM", "false") == "true",
     pandas_requirement_message
     or pyarrow_requirement_message
-    or numpy_requirement_message
-    or "Test depends on newer versions",
+    or "Skip in Python with old dependency",
 )
 class UDFReturnTypeTests(ReusedSQLTestCase):
     @classmethod
