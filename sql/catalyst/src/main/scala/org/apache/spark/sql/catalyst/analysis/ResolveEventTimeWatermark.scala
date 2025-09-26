a16100761048b42e05d8696ad0490e6e6f8bbadd
Jungtaek Lim <kabhwan.opensource@gmail.com>
2025-09-26 09:32:21 +0900

    Move the logic of resolving EventTimeWatermark to separate analysis rule

---
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveEventTimeWatermark.scala
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.analysis
+
+import org.apache.spark.sql.AnalysisException
+import org.apache.spark.sql.catalyst.expressions.{Expression, NamedExpression}
+import org.apache.spark.sql.catalyst.plans.logical.{EventTimeWatermark, LogicalPlan, Project}
+import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.catalyst.trees.TreePattern
+
+/**
+ * Resolve [[UnresolvedEventTimeWatermark]] to [[EventTimeWatermark]].
+ */
+object ResolveEventTimeWatermark extends Rule[LogicalPlan] {
+  override def apply(plan: LogicalPlan): LogicalPlan = plan.resolveOperatorsUpWithPruning(
+    _.containsPattern(TreePattern.UNRESOLVED_EVENT_TIME_WATERMARK), ruleId) {
+
+    case u: UnresolvedEventTimeWatermark if u.childrenResolved =>
+      val uuid = java.util.UUID.randomUUID()
+
+      if (u.eventTimeColExpr.isInstanceOf[MultiAlias]) {
+        throw new AnalysisException(
+          errorClass = "CANNOT_USE_MULTI_ALIASES_IN_WATERMARK_CLAUSE",
+          messageParameters = Map()
+        )
+      }
+
+      val namedExpression = u.eventTimeColExpr match {
+        case e: NamedExpression => e
+        case e: Expression => UnresolvedAlias(e)
+      }
+
+      val exprInChildOutput = u.child.output.exists(_.name == namedExpression.name)
+      if (exprInChildOutput) {
+        // We don't need to have projection since the expression will be available.
+        EventTimeWatermark(uuid, namedExpression.toAttribute, u.getDelay, u.child)
+      } else {
+        // We need to inject projection as we can't find the matching column directly in the
+        // child output.
+        val proj = Project(Seq(namedExpression, UnresolvedStar(None)), u.child)
+        val attrRef = proj.projectList.head.toAttribute
+        EventTimeWatermark(uuid, attrRef, u.getDelay, proj)
+      }
+    }
+}
