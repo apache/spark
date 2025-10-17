dc8ecd8ab7eedc551e66afa07c3b2e170d1409d0
Cheng Pan <chengpan@apache.org>
2025-10-17 12:55:34 +0800

    remove unused import

--- sql/catalyst/src/main/java/org/apache/spark/sql/catalyst/expressions/ExpressionImplUtils.java
+++ sql/catalyst/src/main/java/org/apache/spark/sql/catalyst/expressions/ExpressionImplUtils.java
@@ -35,7 +35,6 @@ import org.apache.spark.sql.catalyst.util.ArrayData;
 import org.apache.spark.sql.catalyst.util.GenericArrayData;
 import org.apache.spark.sql.errors.QueryExecutionErrors;
 import org.apache.spark.unsafe.types.UTF8String;
-import org.apache.spark.util.VersionUtils;
 import org.apache.spark.util.random.XORShiftRandom;
 
 /**
