7cfb54e11a3e69dbcd3118a6a8a2b84c5ae9f1d3
Kousuke Saruta <sarutak@amazon.co.jp>
2025-10-20 20:28:53 +0900

    Override the version of slf4j-api instead of using excludeDependencies

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -1101,7 +1101,8 @@ object DependencyOverrides {
   lazy val settings = Seq(
     dependencyOverrides += "com.google.guava" % "guava" % guavaVersion,
     dependencyOverrides += "jline" % "jline" % "2.14.6",
-    dependencyOverrides += "org.apache.avro" % "avro" % "1.12.0")
+    dependencyOverrides += "org.apache.avro" % "avro" % "1.12.0",
+    dependencyOverrides += "org.slf4j" % "slf4j-api" % "2.0.17")
 }
 
 /**
