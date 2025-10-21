ueshin/apache-spark

Introduce python_worker_logs system session view.

sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/pythonLogicalOperators.scala
@@ -18,11 +18,15 @@
 package org.apache.spark.sql.catalyst.plans.logical
 
 import org.apache.spark.resource.ResourceProfile
-import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeSet, Expression, PythonUDF, PythonUDTF}
+import org.apache.spark.sql.catalyst.SQLConfHelper
+import org.apache.spark.sql.catalyst.analysis.{MultiInstanceRelation, UnresolvedAttribute, UnresolvedStar}
+import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, AttributeSet, Expression, JsonToStructs, PythonUDF, PythonUDTF}
 import org.apache.spark.sql.catalyst.trees.TreePattern._
+import org.apache.spark.sql.catalyst.types.DataTypeUtils
 import org.apache.spark.sql.catalyst.util.truncatedString
 import org.apache.spark.sql.streaming.{GroupStateTimeout, OutputMode, TimeMode}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.util.LogUtils
 
 /**
  * FlatMap groups using a udf: pandas.Dataframe -> pandas.DataFrame.
@@ -379,5 +383,43 @@ case class AttachDistributedSequence(
     val truncatedOutputString = truncatedString(output, "[", ", ", "]", maxFields)
     val indexColumn = s"Index: $sequenceAttr"
     s"$nodeName$truncatedOutputString $indexColumn"
+  }
+}
+
+case class PythonWorkerLogs(jsonAttr: Attribute)
+  extends LeafNode with MultiInstanceRelation with SQLConfHelper {
+
+  override def output: Seq[Attribute] = Seq(jsonAttr)
+
+  override def newInstance(): PythonWorkerLogs =
+    copy(jsonAttr = jsonAttr.newInstance())
+
+  override protected def stringArgs: Iterator[Any] = Iterator(output)
+
+  override def computeStats(): Statistics = Statistics(
+    // TODO: Instead of returning a default value here, find a way to return a meaningful size
+    // estimate for RDDs. See PR 1238 for more discussions.
+    sizeInBytes = BigInt(conf.defaultSizeInBytes)
+  )
+}
+
+object PythonWorkerLogs {
+  val ViewName = "python_worker_logs"
+
+  def apply(): LogicalPlan = {
+    PythonWorkerLogs(DataTypeUtils.toAttribute(StructField("json", StringType)))
+  }
+
+  def viewDefinition(): LogicalPlan = {
+    Project(
+      Seq(UnresolvedStar(Some(Seq("from_json")))),
+      Project(
+        Seq(Alias(
+          JsonToStructs(
+            schema = StructType.fromDDL(LogUtils.SPARK_LOG_SCHEMA),
+            options = Map.empty,
+            child = UnresolvedAttribute("json")),
+          "from_json")()),
+        PythonWorkerLogs()))
   }
 }

sql/core/src/main/scala/org/apache/spark/sql/execution/python/PythonWorkerLogsExec.scala
@@ -1,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.python
+
+import org.apache.spark.SparkEnv
+import org.apache.spark.rdd.{BlockRDD, RDD}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.encoders.encoderFor
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.execution.LeafExecNode
+import org.apache.spark.sql.execution.metric.{SQLMetric, SQLMetrics}
+import org.apache.spark.storage.{BlockId, PythonWorkerLogBlockId, PythonWorkerLogLine}
+
+case class PythonWorkerLogsExec(jsonAttr: Attribute)
+  extends LeafExecNode {
+
+  override def output: Seq[Attribute] = Seq(jsonAttr)
+
+  override lazy val metrics: Map[String, SQLMetric] = Map(
+    "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"))
+
+  override protected def doExecute(): RDD[InternalRow] = {
+    import session.implicits._
+
+    val blockIds = getBlockIds(session.sessionUUID)
+    val rdd = new BlockRDD[PythonWorkerLogLine](session.sparkContext, blockIds.toArray)
+
+    val encoder = encoderFor[String]
+    val toRow = encoder.createSerializer()
+
+    val numOutputRows = longMetric("numOutputRows")
+    rdd.mapPartitionsInternal { iter =>
+      iter.map { value =>
+        numOutputRows += 1
+        toRow(value.json).copy()
+      }
+    }
+  }
+
+  private def getBlockIds(sessionId: String): Seq[BlockId] = {
+    val blockManager = SparkEnv.get.blockManager.master
+    blockManager.getMatchingBlockIds(
+      id => id.isInstanceOf[PythonWorkerLogBlockId] &&
+        id.asInstanceOf[PythonWorkerLogBlockId].sessionId == sessionId,
+      askStorageEndpoints = true
+    ).distinct
+  }
+}

apache/spark

sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/pythonLogicalOperators.scala
@@ -18,11 +18,15 @@
 package org.apache.spark.sql.catalyst.plans.logical
 
 import org.apache.spark.resource.ResourceProfile
-import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeSet, Expression, PythonUDF, PythonUDTF}
+import org.apache.spark.sql.catalyst.SQLConfHelper
+import org.apache.spark.sql.catalyst.analysis.{MultiInstanceRelation, UnresolvedAttribute, UnresolvedStar}
+import org.apache.spark.sql.catalyst.expressions.{Alias, Attribute, AttributeSet, Expression, JsonToStructs, PythonUDF, PythonUDTF}
 import org.apache.spark.sql.catalyst.trees.TreePattern._
+import org.apache.spark.sql.catalyst.types.DataTypeUtils
 import org.apache.spark.sql.catalyst.util.truncatedString
 import org.apache.spark.sql.streaming.{GroupStateTimeout, OutputMode, TimeMode}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.util.LogUtils
 
 /**
  * FlatMap groups using a udf: pandas.Dataframe -> pandas.DataFrame.
@@ -379,5 +383,43 @@ case class AttachDistributedSequence(
     val truncatedOutputString = truncatedString(output, "[", ", ", "]", maxFields)
     val indexColumn = s"Index: $sequenceAttr"
     s"$nodeName$truncatedOutputString $indexColumn"
+  }
+}
+
+case class PythonWorkerLogs(jsonAttr: Attribute)
+  extends LeafNode with MultiInstanceRelation with SQLConfHelper {
+
+  override def output: Seq[Attribute] = Seq(jsonAttr)
+
+  override def newInstance(): PythonWorkerLogs =
+    copy(jsonAttr = jsonAttr.newInstance())
+
+  override protected def stringArgs: Iterator[Any] = Iterator(output)
+
+  override def computeStats(): Statistics = Statistics(
+    // TODO: Instead of returning a default value here, find a way to return a meaningful size
+    // estimate for RDDs. See PR 1238 for more discussions.
+    sizeInBytes = BigInt(conf.defaultSizeInBytes)
+  )
+}
+
+object PythonWorkerLogs {
+  val ViewName = "python_worker_logs"
+
+  def apply(): LogicalPlan = {
+    PythonWorkerLogs(DataTypeUtils.toAttribute(StructField("message", StringType)))
+  }
+
+  def viewDefinition(): LogicalPlan = {
+    Project(
+      Seq(UnresolvedStar(Some(Seq("from_json")))),
+      Project(
+        Seq(Alias(
+          JsonToStructs(
+            schema = StructType.fromDDL(LogUtils.SPARK_LOG_SCHEMA),
+            options = Map.empty,
+            child = UnresolvedAttribute("message")),
+          "from_json")()),
+        PythonWorkerLogs()))
   }
 }

sql/core/src/main/scala/org/apache/spark/sql/execution/python/PythonWorkerLogsExec.scala
@@ -1,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.python
+
+import org.apache.spark.SparkEnv
+import org.apache.spark.rdd.{BlockRDD, RDD}
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.encoders.encoderFor
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.execution.LeafExecNode
+import org.apache.spark.sql.execution.metric.{SQLMetric, SQLMetrics}
+import org.apache.spark.storage.{BlockId, PythonWorkerLogBlockId, PythonWorkerLogLine}
+
+case class PythonWorkerLogsExec(jsonAttr: Attribute)
+  extends LeafExecNode {
+
+  override def output: Seq[Attribute] = Seq(jsonAttr)
+
+  override lazy val metrics: Map[String, SQLMetric] = Map(
+    "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"))
+
+  override protected def doExecute(): RDD[InternalRow] = {
+    import session.implicits._
+
+    val blockIds = getBlockIds(session.sessionUUID)
+    val rdd = new BlockRDD[PythonWorkerLogLine](session.sparkContext, blockIds.toArray)
+
+    val encoder = encoderFor[String]
+    val toRow = encoder.createSerializer()
+
+    val numOutputRows = longMetric("numOutputRows")
+    rdd.mapPartitionsInternal { iter =>
+      iter.map { value =>
+        numOutputRows += 1
+        toRow(value.message).copy()
+      }
+    }
+  }
+
+  private def getBlockIds(sessionId: String): Seq[BlockId] = {
+    val blockManager = SparkEnv.get.blockManager.master
+    blockManager.getMatchingBlockIds(
+      id => id.isInstanceOf[PythonWorkerLogBlockId] &&
+        id.asInstanceOf[PythonWorkerLogBlockId].sessionId == sessionId,
+      askStorageEndpoints = true
+    ).distinct
+  }
+}