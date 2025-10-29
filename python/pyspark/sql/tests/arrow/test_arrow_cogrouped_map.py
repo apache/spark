9988a01674e10a247d17778bf9b7ba822894d522
Ruifeng Zheng <ruifengz@apache.org>
2025-10-29 09:04:19 +0800

    test

--- python/pyspark/sql/tests/arrow/test_arrow_cogrouped_map.py
+++ python/pyspark/sql/tests/arrow/test_arrow_cogrouped_map.py
@@ -362,6 +362,11 @@ class CogroupedMapInArrowTestsMixin:
 
                     self.assertEqual(expected, result)
 
+    def test_negative_and_zero_batch_size(self):
+        for batch_size in [0, -1]:
+            with self.sql_conf({"spark.sql.execution.arrow.maxRecordsPerBatch": batch_size}):
+                CogroupedMapInArrowTestsMixin.test_apply_in_arrow(self)
+
 
 class CogroupedMapInArrowTests(CogroupedMapInArrowTestsMixin, ReusedSQLTestCase):
     @classmethod
