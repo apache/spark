4968d93620d081e72a8429c552d5c942d917c17a
Chiran Ravani <chiran54321@gmail.com>
2025-10-30 15:06:00 +0100

    Update sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
    Co-authored-by: Wenchen Fan <cloud0fan@gmail.com>

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/HiveExternalCatalog.scala
@@ -603,7 +603,7 @@ private[spark] class HiveExternalCatalog(conf: SparkConf, hadoopConf: Configurat
         // Spark-created views do not have to be Hive compatible. If the data type is not
         // Hive compatible, we can set schema to empty so that Spark can still read this
         // view as the schema is also encoded in the table properties.
-        case schema if tableDefinition.tableType == CatalogTableType.VIEW &&
+        case schema if
           schema.exists(f => !isHiveCompatibleDataType(f.dataType)) =>
           EMPTY_DATA_SCHEMA
         case other => other
