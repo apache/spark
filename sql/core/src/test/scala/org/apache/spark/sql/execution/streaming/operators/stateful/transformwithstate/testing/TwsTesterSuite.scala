7a2e668c6426a786245f7f6e8d0d5a2f3dba9feb
Dmytro Fedoriaka <dmytro.fedoriaka@databricks.com>
2025-11-01 03:20:30 +0000

    Add license

--- sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/operators/stateful/transformwithstate/testing/TwsTesterSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/operators/stateful/transformwithstate/testing/TwsTesterSuite.scala
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.spark.sql.execution.streaming.operators.stateful.transformwithstate.testing
 
 import java.time.Clock
