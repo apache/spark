e55333c9506b78e9746ed4b34e066838f81088e2
Cheng Pan <chengpan@apache.org>
2025-11-04 09:18:19 -0800

    [SPARK-54177][BUILD] Upgrade gRPC to 1.76 and protobuf to 6.33
    ### What changes were proposed in this pull request?
    
    Bump gRPC from 1.67 to 1.76, with additional Python package upgrades for consistency:
    
    - `googleapis-common-protos==1.71.0`
    - `protobuf==6.33.0`
    
    And `buf v33.0`
    
    Fix the shading leaks of the `spark-connect` jar
    
    before
    ```
    $ jar tf spark-connect_2.13-4.1.0-preview3.jar | grep '.class$' | grep -v 'org/apache/spark' | grep -v 'org/sparkproject' | grep -v 'META-INF'
    javax/annotation/Generated.class
    ...
    javax/ejb/EJB.class
    ...
    javax/persistence/PersistenceContext.class
    ...
    javax/xml/ws/WebServiceRef.class
    ...
    com/google/shopping/type/Price$Builder.class
    ...
    com/google/apps/card/v1/Widget$DataCase.class
    ...
    ```
    
    after
    
    ```
    $ jar tf spark-connect_2.13-4.2.0-SNAPSHOT.jar | grep '.class$' | grep -v 'org/apache/spark' | grep -v 'org/sparkproject' | grep -v 'META-INF'
    <no-output>
    ```
    
    ### Why are the changes needed?
    
    For Python:
    
    - [grpcio v1.75.1](https://github.com/grpc/grpc/releases/tag/v1.75.1) addes official Python 3.14 support
    - googleapis-common-proto v1.71.0 addes official Python 3.14 support, see https://github.com/googleapis/google-cloud-python/pull/14699
    
    For Java:
    
    - v1.74 removes dependency on Tomcat's annotation API, see https://github.com/grpc/grpc-java/issues/9179
    
    Check full release notes at: https://github.com/grpc/grpc/releases
    
    ### Does this PR introduce _any_ user-facing change?
    
    Maybe, reduce the potential conflict risks between Spark and user classes.
    
    ### How was this patch tested?
    
    Pass GHA, plus manual checks (see above sections).
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #52874 from pan3793/SPARK-54177.
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- python/packaging/connect/setup.py
+++ python/packaging/connect/setup.py
@@ -89,8 +89,8 @@ try:
     _minimum_pandas_version = "2.0.0"
     _minimum_numpy_version = "1.21"
     _minimum_pyarrow_version = "11.0.0"
-    _minimum_grpc_version = "1.67.0"
-    _minimum_googleapis_common_protos_version = "1.65.0"
+    _minimum_grpc_version = "1.76.0"
+    _minimum_googleapis_common_protos_version = "1.71.0"
     _minimum_pyyaml_version = "3.11"
 
     with open("README.md") as f:
