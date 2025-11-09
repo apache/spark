9d3e28f1945888540078d37fe0501805660a5633
Cheng Pan <chengpan@apache.org>
2025-11-08 19:47:51 -0800

    [SPARK-53944][K8S][CORE][FOLLOWUP] Override DRIVER_HOST_ADDRESS in SparkContext instead of SparkEnv when useDriverPodIP is true
    ### What changes were proposed in this pull request?
    
    This is an alternative to #52923.
    
    In addition, this ensures `spark.kubernetes.executor.useDriverPodIP` only takes effect when `spark.master` startsWith `k8s`
    
    ### Why are the changes needed?
    
    This is a simpler way than https://github.com/apache/spark/pull/52923, see more details at the discussion https://github.com/apache/spark/pull/52923#discussion_r2501500911
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Pass the K8s IT case provided by #52923
    ```
    [info] KubernetesSuite:
    [info] - SPARK-53944: Run SparkPi without driver service (11 seconds, 914 milliseconds)
    [info] YuniKornSuite:
    [info] Run completed in 21 seconds, 737 milliseconds.
    [info] Total number of tests run: 1
    [info] Suites: completed 2, aborted 0
    [info] Tests: succeeded 1, failed 0, canceled 0, ignored 0, pending 0
    [info] All tests passed.
    [success] Total time: 44 s, completed Nov 9, 2025, 1:19:07 AM
    ```
    
    Also manually tested in the internal cluster.
    
    checklist:
    
    - `spark.kubernetes.executor.useDriverPodIP` only takes effect on K8s mode
    - when `s.k.e.useDriverPodIP` is true, check UI `Executors` and `Environment` tabs, driver address displays correctly in IP, not svc endpoint
      <img width="574" height="189" alt="image" src="https://github.com/user-attachments/assets/9e538448-b66f-482b-b97a-ae1ee00601b3" />
      <img width="969" height="125" alt="image" src="https://github.com/user-attachments/assets/27f77899-1e04-40de-9f23-a66e5e6e3ce3" />
    - run some queries, shuffle works as expected. (we use Apache Celeborn as Remote Shuffle Service)
    - all built-in functionalities that we used work with the driver svc disabled.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #52954 from pan3793/SPARK-53944-refactor.
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>
    (cherry picked from commit cc57743a229a70e2060230e3c9da49cbe24ad257)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- core/src/main/scala/org/apache/spark/SparkContext.scala
+++ core/src/main/scala/org/apache/spark/SparkContext.scala
@@ -454,7 +454,14 @@ class SparkContext(config: SparkConf) extends Logging {
 
     // Set Spark driver host and port system properties. This explicitly sets the configuration
     // instead of relying on the default value of the config constant.
-    _conf.set(DRIVER_HOST_ADDRESS, _conf.get(DRIVER_HOST_ADDRESS))
+    if (master.startsWith("k8s") &&
+        _conf.getBoolean("spark.kubernetes.executor.useDriverPodIP", false)) {
+      logInfo("Use DRIVER_BIND_ADDRESS instead of DRIVER_HOST_ADDRESS as driver address " +
+        "because spark.kubernetes.executor.useDriverPodIP is true in K8s mode.")
+      _conf.set(DRIVER_HOST_ADDRESS, _conf.get(DRIVER_BIND_ADDRESS))
+    } else {
+      _conf.set(DRIVER_HOST_ADDRESS, _conf.get(DRIVER_HOST_ADDRESS))
+    }
     _conf.setIfMissing(DRIVER_PORT, 0)
 
     _conf.set(EXECUTOR_ID, SparkContext.DRIVER_IDENTIFIER)
