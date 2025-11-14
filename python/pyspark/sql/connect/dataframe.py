eb26242fd42fe83ff94d15db68355c92e793fd24
Ruifeng Zheng <ruifengz@apache.org>
2025-11-14 13:47:35 +0800

    test

--- python/pyspark/sql/connect/dataframe.py
+++ python/pyspark/sql/connect/dataframe.py
@@ -1736,9 +1736,8 @@ class DataFrame(ParentDataFrame):
                 errorClass="JVM_ATTRIBUTE_NOT_SUPPORTED", messageParameters={"attr_name": name}
             )
 
-        if (
-            os.environ.get("PYSPARK_VALIDATE_COLUMN_NAME_LEGACY") == "1"
-            and name not in self.columns
+        if os.environ.get("PYSPARK_VALIDATE_COLUMN_NAME_LEGACY", "1") == "1" and not any(
+            field.name == name for field in self._schema
         ):
             raise PySparkAttributeError(
                 errorClass="ATTRIBUTE_NOT_SUPPORTED", messageParameters={"attr_name": name}
