60f7a3fb3004bc14816901bfb7982d9fdecf6a98
micheal-o <micheal.okutubo@gmail.com>
2025-11-14 16:37:43 -0800

    lint

--- sql/api/src/main/scala/org/apache/spark/sql/streaming/StreamingCheckpointManager.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/streaming/StreamingCheckpointManager.scala
@@ -24,24 +24,27 @@ private[spark] abstract class StreamingCheckpointManager {
 
   /**
    * Repartition the stateful streaming operators state in the streaming checkpoint to have
-   * `numPartitions` partitions. The streaming query MUST not be running.
-   * If `numPartitions` is the same as the current number of partitions, this is a no-op,
-   * and an exception will be thrown.
+   * `numPartitions` partitions. The streaming query MUST not be running. If `numPartitions` is
+   * the same as the current number of partitions, this is a no-op, and an exception will be
+   * thrown.
    *
    * This produces a new microbatch in the checkpoint that contains the repartitioned state i.e.
-   * if the last streaming batch was batch `N`, this will create batch `N+1` with the repartitioned
-   * state. Note that this new batch doesn't read input data from sources, it only represents the
-   * repartition operation. The next time the streaming query is started, it will pick up from
-   * this new batch.
+   * if the last streaming batch was batch `N`, this will create batch `N+1` with the
+   * repartitioned state. Note that this new batch doesn't read input data from sources, it only
+   * represents the repartition operation. The next time the streaming query is started, it will
+   * pick up from this new batch.
    *
    * This will return only when the repartitioning is complete or fails.
    *
-   * @note This operation should only be performed after the streaming query has been stopped.
-   * @param checkpointLocation The checkpoint location of the streaming query, should be the
-   *                           `checkpointLocation` option on the DataStreamWriter.
-   * @param numPartitions the target number of state partitions.
-   * @param enforceExactlyOnceSink if we shouldn't allow skipping failed batches,
-   *                               to avoid duplicates in exactly once sinks.
+   * @note
+   *   This operation should only be performed after the streaming query has been stopped.
+   * @param checkpointLocation
+   *   The checkpoint location of the streaming query, should be the `checkpointLocation` option
+   *   on the DataStreamWriter.
+   * @param numPartitions
+   *   the target number of state partitions.
+   * @param enforceExactlyOnceSink
+   *   if we shouldn't allow skipping failed batches, to avoid duplicates in exactly once sinks.
    */
   private[spark] def repartition(
       checkpointLocation: String,
