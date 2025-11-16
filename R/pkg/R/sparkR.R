c125aea395b37ec1fa3b4e8b5a3e9bee270203c2
Dongjoon Hyun <dongjoon@apache.org>
2025-11-16 09:19:38 -0800

    Preparing Spark release v4.1.0-preview4-rc1

--- R/pkg/R/sparkR.R
+++ R/pkg/R/sparkR.R
@@ -455,8 +455,8 @@ sparkR.session <- function(
 
   # Check if version number of SparkSession matches version number of SparkR package
   jvmVersion <- callJMethod(sparkSession, "version")
-  # Remove -SNAPSHOT from jvm versions
-  jvmVersionStrip <- gsub("-SNAPSHOT", "", jvmVersion, fixed = TRUE)
+  # Remove -preview4 from jvm versions
+  jvmVersionStrip <- gsub("-preview4", "", jvmVersion, fixed = TRUE)
   rPackageVersion <- paste0(packageVersion("SparkR"))
 
   if (jvmVersionStrip != rPackageVersion) {
