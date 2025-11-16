3f573f5481388c6946525fd5101c838e5bd75386
Angerszhuuuu <angers.zhu@gmail.com>
2025-11-16 15:16:47 +0800

    Update BlockManagerMaster.scala

--- core/src/main/scala/org/apache/spark/storage/BlockManagerMaster.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManagerMaster.scala
@@ -41,9 +41,7 @@ class BlockManagerMaster(
   val timeout = RpcUtils.askRpcTimeout(conf)
 
   private val waitBlockRemovalTimeout =
-    RpcTimeout(conf,
-      CLEANER_REFERENCE_TRACKING_BLOCKING_TIMEOUT.key,
-      CLEANER_REFERENCE_TRACKING_BLOCKING_TIMEOUT.defaultValueString)
+    RpcTimeout(conf, CLEANER_REFERENCE_TRACKING_BLOCKING_TIMEOUT.key)
 
   /** Remove a dead executor from the driver endpoint. This is only called on the driver side. */
   def removeExecutor(execId: String): Unit = {
