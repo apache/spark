6227fbab03082a19ad04ab300b89a721334eb9bb
antban <dmitry.sorokin@gmail.com>
2025-11-18 13:52:13 -0800

    [SPARK-54349][PYTHON] Refactor code a bit to simplify faulthandler integration extension
    ### What changes were proposed in this pull request?
    There are many places where pyspark is trying to integrate with faulthandler and use the same functionality to dump stack traces/record thread dumps.
    In order to reduce the complexity of the integration and ease the extension of integration it makes sense to technically refactor the code to use the same code in all the places.
    
    ### Why are the changes needed?
    Improves developer experience.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    
    ### How was this patch tested?
    
    By existing unit tests
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    Closes #53016 from antban/simplify-faulthandler-integration.
    
    Authored-by: antban <dmitry.sorokin@gmail.com>
    Signed-off-by: Takuya Ueshin <ueshin@databricks.com>

--- python/pyspark/worker.py
+++ python/pyspark/worker.py
@@ -27,7 +27,6 @@ import inspect
 import itertools
 import json
 from typing import Any, Callable, Iterable, Iterator, Optional, Tuple
-import faulthandler
 
 from pyspark.accumulators import (
     SpecialAccumulatorIds,
@@ -84,7 +83,12 @@ from pyspark.sql.types import (
     _create_row,
     _parse_datatype_json_string,
 )
-from pyspark.util import fail_on_stopiteration, handle_worker_exception
+from pyspark.util import (
+    fail_on_stopiteration,
+    handle_worker_exception,
+    with_faulthandler,
+    start_faulthandler_periodic_traceback,
+)
 from pyspark import shuffle
 from pyspark.errors import PySparkRuntimeError, PySparkTypeError, PySparkValueError
 from pyspark.worker_util import (
@@ -3290,23 +3294,14 @@ def read_udfs(pickleSer, infile, eval_type):
     return func, None, ser, ser
 
 
+@with_faulthandler
 def main(infile, outfile):
-    faulthandler_log_path = os.environ.get("PYTHON_FAULTHANDLER_DIR", None)
-    tracebackDumpIntervalSeconds = os.environ.get("PYTHON_TRACEBACK_DUMP_INTERVAL_SECONDS", None)
     try:
-        if faulthandler_log_path:
-            faulthandler_log_path = os.path.join(faulthandler_log_path, str(os.getpid()))
-            faulthandler_log_file = open(faulthandler_log_path, "w")
-            faulthandler.enable(file=faulthandler_log_file)
-
         boot_time = time.time()
         split_index = read_int(infile)
         if split_index == -1:  # for unit tests
             sys.exit(-1)
-
-        if tracebackDumpIntervalSeconds is not None and int(tracebackDumpIntervalSeconds) > 0:
-            faulthandler.dump_traceback_later(int(tracebackDumpIntervalSeconds), repeat=True)
-
+        start_faulthandler_periodic_traceback()
         check_python_version(infile)
 
         # read inputs only for a barrier task
@@ -3397,11 +3392,6 @@ def main(infile, outfile):
     except BaseException as e:
         handle_worker_exception(e, outfile)
         sys.exit(-1)
-    finally:
-        if faulthandler_log_path:
-            faulthandler.disable()
-            faulthandler_log_file.close()
-            os.remove(faulthandler_log_path)
     finish_time = time.time()
     report_times(outfile, boot_time, init_time, finish_time)
     write_long(shuffle.MemoryBytesSpilled, outfile)
@@ -3418,9 +3408,6 @@ def main(infile, outfile):
         # write a different value to tell JVM to not reuse this worker
         write_int(SpecialLengths.END_OF_DATA_SECTION, outfile)
         sys.exit(-1)
-
-    # Force to cancel dump_traceback_later
-    faulthandler.cancel_dump_traceback_later()
 
 
 if __name__ == "__main__":
