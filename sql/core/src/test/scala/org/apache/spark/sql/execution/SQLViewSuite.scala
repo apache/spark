67ec248bc8457168d1518386f2e784dc76fe07a4
Cheng Pan <chengpan@apache.org>
2025-11-19 18:03:10 +0800

    rm duplicated ut

--- sql/core/src/test/scala/org/apache/spark/sql/execution/SQLViewSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/SQLViewSuite.scala
@@ -154,20 +154,6 @@ abstract class SQLViewSuite extends QueryTest with SQLTestUtils {
     }
   }
 
-  test("CREATE TEMPORARY VIEW IF NOT EXISTS") {
-    withTable("tab1", "tab2") {
-      withTempView("temp_v1") {
-        sql("CREATE TABLE tab1 (id int) USING parquet")
-        sql("INSERT INTO tab1 VALUES (1)")
-        sql("CREATE TABLE tab2 (name STRING) USING parquet")
-        sql("INSERT INTO tab2 VALUES ('one'), ('two')")
-        sql("CREATE TEMPORARY VIEW temp_v1 AS SELECT * FROM tab1")
-        sql("CREATE TEMPORARY VIEW IF NOT EXISTS temp_v1 AS SELECT * FROM tab2")
-        checkAnswer(sql("SELECT * FROM temp_v1"), Row(1))
-      }
-    }
-  }
-
   test("Issue exceptions for ALTER VIEW on the temporary view") {
     val viewName = "testView"
     withTempView(viewName) {
