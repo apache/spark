ed5668324c24a346ccb66baef0254e81fced2d45
Dmytro Fedoriaka <dmytro.fedoriaka@databricks.com>
2025-11-25 23:04:11 +0000

    fix annotations

--- python/pyspark/sql/streaming/tws_tester.py
+++ python/pyspark/sql/streaming/tws_tester.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from __future__ import annotations
 
 from itertools import groupby
 from typing import Any, Iterator, Optional, Union
