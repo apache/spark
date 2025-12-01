54e4b832b678cb4393cf8e472bdb3caaaa772f78
Jungtaek Lim <kabhwan.opensource@gmail.com>
2025-12-01 17:00:28 +0900

    [SPARK-54478][SPARK-54479][SPARK-54480][SPARK-54484] Re-enable streaming tests for connect compat test CI
    ### What changes were proposed in this pull request?
    
    This PR proposes to re-enable streaming tests for connect compatibility test CI.
    
    ### Why are the changes needed?
    
    They were disabled due to failure, but I can't reproduce these failures in both local and CI after installing zstandard.
    
    Code change to trigger compatibility test CI against test branch + install zstandard:
    https://github.com/apache/spark/compare/master...HeartSaVioR:spark:WIP-investigate-ss-spark-connect-compat-test-failures-master-and-4.0
    
    Code change to re-enable these tests during reproducing:
    https://github.com/apache/spark/compare/branch-4.0...HeartSaVioR:spark:branch-4.0-SC-213385
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    GA run with the above reproducer setup:
    https://github.com/HeartSaVioR/spark/actions/runs/19807973545/job/56745231698
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #53266 from HeartSaVioR/reenable-streaming-connect-tests.
    
    Authored-by: Jungtaek Lim <kabhwan.opensource@gmail.com>
    Signed-off-by: Jungtaek Lim <kabhwan.opensource@gmail.com>
    (cherry picked from commit aecd932c34d03234dc2cdc80468d95dddb121d46)
    Signed-off-by: Jungtaek Lim <kabhwan.opensource@gmail.com>

--- python/pyspark/sql/tests/test_python_streaming_datasource.py
+++ python/pyspark/sql/tests/test_python_streaming_datasource.py
@@ -139,9 +139,6 @@ class BasePythonStreamingDataSourceTestsMixin:
 
         return TestDataSource
 
-    @unittest.skipIf(
-        os.environ.get("SPARK_SKIP_CONNECT_COMPAT_TESTS") == "1", "SPARK-54484: To be reenabled"
-    )
     def test_stream_reader(self):
         self.spark.dataSource.register(self._get_test_data_source())
         df = self.spark.readStream.format("TestDataSource").load()
@@ -216,7 +213,6 @@ class BasePythonStreamingDataSourceTestsMixin:
 
         assertDataFrameEqual(df, expected_data)
 
-    @unittest.skipIf(os.environ.get("SPARK_SKIP_CONNECT_COMPAT_TESTS") == "1", "To be reenabled")
     def test_simple_stream_reader(self):
         class SimpleStreamReader(SimpleDataSourceStreamReader):
             def initialOffset(self):
