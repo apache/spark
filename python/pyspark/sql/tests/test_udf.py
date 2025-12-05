e270433e50a1b50c1a0a4751d7cd473a1d980c65
Holden Karau <holden.karau@snowflake.com>
2025-12-05 11:41:10 -0800

    Use assert in to make sure we grab the core of the lambda.
    Co-authored-by: Holden Karau <holden@pigscanfly.ca>

--- python/pyspark/sql/tests/test_udf.py
+++ python/pyspark/sql/tests/test_udf.py
@@ -72,10 +72,7 @@ class BaseUDFTestsMixin(object):
         pudf = UserDefinedFunction(call, LongType())
         res = data.select(pudf(data["number"]).alias("plus_four"))
         self.assertEqual(res.agg({"plus_four": "sum"}).collect()[0][0], 85)
-        # Currently does not work because of inspect limitations.
-        # Make sure we sent the extra bits over for transpilation.
-        self.assertEqual(pudf._judf.src(), "")
-        self.assertEqual(pudf._judf.ast(), "")
+        self.assertIn("col + 4", pudf._judf.src())
 
     def test_udf_with_partial_function(self):
         data = self.spark.createDataFrame([(i, i**2) for i in range(10)], ["number", "squared"])
