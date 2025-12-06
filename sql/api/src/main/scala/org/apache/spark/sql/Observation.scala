653023d39ae2da597eb84c52ba22773f1f28a4b7
Liang-Chi Hsieh <viirya@gmail.com>
2025-12-05 18:24:29 -0800

    update doc

--- sql/api/src/main/scala/org/apache/spark/sql/Observation.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/Observation.scala
@@ -70,6 +70,9 @@ class Observation(val name: String) {
    * first action. Only the result of the first action is available. Subsequent actions do not
    * modify the result.
    *
+   * Note that if no metrics were recorded, an empty map is probably returned. It possibly happens
+   * when the operators used for observation are optimized away.
+   *
    * @return
    *   the observed metrics as a `Map[String, Any]`
    * @throws InterruptedException
@@ -89,6 +92,9 @@ class Observation(val name: String) {
    * (Java-specific) Get the observed metrics. This waits for the observed dataset to finish its
    * first action. Only the result of the first action is available. Subsequent actions do not
    * modify the result.
+   *
+   * Note that if no metrics were recorded, an empty map is probably returned. It possibly happens
+   * when the operators used for observation are optimized away.
    *
    * @return
    *   the observed metrics as a `java.util.Map[String, Object]`
