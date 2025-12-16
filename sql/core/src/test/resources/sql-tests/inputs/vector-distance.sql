a23dacc454f965263075e749f207f216fd106253
zhidongqu-db <zhidong.qu@databricks.com>
2025-12-16 22:43:26 +0000

    comments

--- sql/core/src/test/resources/sql-tests/inputs/vector-distance.sql
+++ sql/core/src/test/resources/sql-tests/inputs/vector-distance.sql
@@ -97,6 +97,29 @@ SELECT vector_l2_distance(array(1, 2, 3), array(4, 5, 6));
 -- Mixed type errors (first arg correct, second wrong)
 SELECT vector_cosine_similarity(array(1.0F, 2.0F, 3.0F), array(1.0D, 2.0D, 3.0D));
 
+-- Non-array argument errors (argument is not an array at all)
+
+-- vector_cosine_similarity: first argument is not an array
+SELECT vector_cosine_similarity('not an array', array(1.0F, 2.0F, 3.0F));
+
+-- vector_cosine_similarity: second argument is not an array
+SELECT vector_cosine_similarity(array(1.0F, 2.0F, 3.0F), 'not an array');
+
+-- vector_cosine_similarity: both arguments are not arrays
+SELECT vector_cosine_similarity(123, 456);
+
+-- vector_inner_product: first argument is not an array
+SELECT vector_inner_product('not an array', array(1.0F, 2.0F, 3.0F));
+
+-- vector_inner_product: second argument is not an array
+SELECT vector_inner_product(array(1.0F, 2.0F, 3.0F), 'not an array');
+
+-- vector_l2_distance: first argument is not an array
+SELECT vector_l2_distance('not an array', array(1.0F, 2.0F, 3.0F));
+
+-- vector_l2_distance: second argument is not an array
+SELECT vector_l2_distance(array(1.0F, 2.0F, 3.0F), 'not an array');
+
 -- Large vectors (test SIMD unroll path with 16 elements)
 SELECT vector_inner_product(
     array(1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 9.0F, 10.0F, 11.0F, 12.0F, 13.0F, 14.0F, 15.0F, 16.0F),
