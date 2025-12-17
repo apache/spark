a484efdd29f3fc52fb18ab4a36b3612d57e4585c
Tian Gao <gaogaotiantian@hotmail.com>
2025-12-17 11:52:27 -0800

    Change the default executable to sys.executable

--- python/run-tests.py
+++ python/run-tests.py
@@ -212,15 +212,8 @@ def run_individual_python_test(target_dir, test_name, pyspark_python, keep_test_
 
 
 def get_default_python_executables():
-    python_execs = [x for x in ["python3.11", "pypy3"] if which(x)]
+    python_execs = [x for x in [sys.executable, "pypy3"] if which(x)]
 
-    if "python3.11" not in python_execs:
-        p = which("python3")
-        if not p:
-            LOGGER.error("No python3 executable found.  Exiting!")
-            os._exit(1)
-        else:
-            python_execs.insert(0, p)
     return python_execs
 
 
