20da081cd84ed30fb43e51deac5ea864f6a01d05
Enrico Minack <github@enrico.minack.dev>
2025-12-17 08:58:17 +0100

    Revert testing recovery from missing decommissionig

--- core/src/main/scala/org/apache/spark/storage/BlockManagerDecommissioner.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManagerDecommissioner.scala
@@ -118,8 +118,7 @@ private[storage] class BlockManagerDecommissioner(
             try {
               val startTime = System.currentTimeMillis()
               if (fallbackStorage.isDefined && peer == FallbackStorage.FALLBACK_BLOCK_MANAGER_ID) {
-                logInfo("not copying to fallback storage to test recovery")
-                // fallbackStorage.foreach(_.copy(shuffleBlockInfo, bm))
+                fallbackStorage.foreach(_.copy(shuffleBlockInfo, bm))
               } else {
                 blocks.foreach { case (blockId, buffer) =>
                   logDebug(s"Migrating sub-block ${blockId}")
@@ -152,8 +151,7 @@ private[storage] class BlockManagerDecommissioner(
                     // Confirm peer is not the fallback BM ID because fallbackStorage would already
                     // have been used in the try-block above so there's no point trying again
                     && peer != FallbackStorage.FALLBACK_BLOCK_MANAGER_ID) {
-                  logInfo("not falling back to copying to fallback storage to test recovery")
-                  // fallbackStorage.foreach(_.copy(shuffleBlockInfo, bm))
+                  fallbackStorage.foreach(_.copy(shuffleBlockInfo, bm))
                 } else if (e.getCause != null && e.getCause.getMessage != null
                   && e.getCause.getMessage
                   .contains(blockSavedOnDecommissionedBlockManagerException)) {
