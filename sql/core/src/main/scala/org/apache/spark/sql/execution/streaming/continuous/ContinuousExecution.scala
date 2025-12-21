3bedf39036cd4591d37b1b914e93f71dfc2d0c18
jcowell <jcowell@atlassian.com>
2025-12-21 18:23:34 -0500

    refactor: add trigger start reporting in ContinuousExecution and MicroBatchExecution

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/continuous/ContinuousExecution.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/continuous/ContinuousExecution.scala
@@ -281,6 +281,7 @@ class ContinuousExecution(
             } else if (isActive) {
               execCtx.batchId = epochEndpoint.askSync[Long](IncrementAndGetEpoch)
               logInfo(log"New epoch ${MDC(BATCH_ID, execCtx.batchId)} is starting.")
+              execCtx.reportTriggerStart()
               true
             } else {
               false
