7c2f1d78f0febc3626a174100648db97ed611a4b
jcowell <jcowell@atlassian.com>
2025-12-21 23:42:26 -0500

    gen-protos for connect

--- python/pyspark/sql/connect/proto/commands_pb2.pyi
+++ python/pyspark/sql/connect/proto/commands_pb2.pyi
@@ -68,7 +68,7 @@ class _StreamingQueryEventTypeEnumTypeWrapper(
     QUERY_PROGRESS_EVENT: _StreamingQueryEventType.ValueType  # 1
     QUERY_TERMINATED_EVENT: _StreamingQueryEventType.ValueType  # 2
     QUERY_IDLE_EVENT: _StreamingQueryEventType.ValueType  # 3
-    QUERY_TRIGGER_START_EVENT: _StreamingQueryEventType.ValueType # 4
+    QUERY_EXECUTION_START_EVENT: _StreamingQueryEventType.ValueType  # 4
 
 class StreamingQueryEventType(
     _StreamingQueryEventType, metaclass=_StreamingQueryEventTypeEnumTypeWrapper
@@ -82,7 +82,7 @@ QUERY_PROGRESS_UNSPECIFIED: StreamingQueryEventType.ValueType  # 0
 QUERY_PROGRESS_EVENT: StreamingQueryEventType.ValueType  # 1
 QUERY_TERMINATED_EVENT: StreamingQueryEventType.ValueType  # 2
 QUERY_IDLE_EVENT: StreamingQueryEventType.ValueType  # 3
-QUERY_TRIGGER_START_EVENT:StreamingQueryEventType.ValueType # 4
+QUERY_EXECUTION_START_EVENT: StreamingQueryEventType.ValueType  # 4
 global___StreamingQueryEventType = StreamingQueryEventType
 
 class Command(google.protobuf.message.Message):
