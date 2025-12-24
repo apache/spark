4068f653d6cd64c5a86672da5999482480f96c5f
Szehon Ho <szehon.apache@gmail.com>
2025-12-23 17:37:29 -0800

    Missing file

---
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/UsesCachedData.scala
@@ -0,0 +1,28 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.plans.logical
+
+/**
+ * A marker trait for [[Command]] operators that should use cached results from CacheManager.
+ *
+ * By default, all commands skip the cache in CacheManager. Commands that extend this
+ * trait will opt-in to using cached data for their child query plans (and are eagerly-evaluated
+ * on df.cache()).
+ */
+trait UsesCachedData extends Command
+
