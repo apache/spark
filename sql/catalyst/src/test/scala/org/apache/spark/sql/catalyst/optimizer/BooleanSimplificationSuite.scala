ead1da5ac62458c104993f66560a7cb3c4d73fde
Asif Hussain Shahid <asif.shahid@gmail.com>
2026-01-06 12:48:42 -0800

    SPARK-54881. refactored test

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/BooleanSimplificationSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/optimizer/BooleanSimplificationSuite.scala
@@ -291,25 +291,26 @@ class BooleanSimplificationSuite extends PlanTest with ExpressionEvalHelper {
     checkCondition(Not(IsNull($"b")), IsNotNull($"b"))
   }
 
-  test("SPARK-54881: simplify Not(A <= B OR A >= B) to (a > b AND a < b) in single pass ") {
-    val initialExpr = Not(($"a" <= $"b") || ($"a" >= $"b"))
-    val expectedOptimizedExpr = $"a" > $"b" && $"a" < $"b"
-    val planAfterRuleApp = BooleanSimplification.apply(testRelation.where(initialExpr).analyze)
-    val expectedOptPlan = testRelation.where(expectedOptimizedExpr).analyze
-    comparePlans(expectedOptPlan, planAfterRuleApp)
-  }
-
-  test("SPARK-54881: simplify Not((expr1 OR expr2) AND (expr3 OR expr4)) in single pass") {
-    val initialExpr = Not(($"a" <= $"b" || $"c" > $"a" + 4) || ($"a" >= $"b" && $"c" < $"a"))
-
-    val expectedOptimizedExpr = And(
-      And($"a" > $"b", $"c" <= $"a" + 4),
-      Or($"a" < $"b", $"c" >= $"a")
+  test("SPARK-54881: simplify Not(Expr) in single pass") {
+    def executeRuleOnce(exprToTest: Expression, optimizedExprExpected: Expression): Unit = {
+      val planAfterRuleApp = BooleanSimplification.apply(testRelation.where(exprToTest).analyze)
+      val expectedOptPlan = testRelation.where(optimizedExprExpected).analyze
+      comparePlans(expectedOptPlan, planAfterRuleApp)
+    }
+    // check simplify Not(A <= B OR A >= B) to (a > b AND a < b) in single pass
+    executeRuleOnce(
+      Not(($"a" <= $"b") || ($"a" >= $"b")),
+      $"a" > $"b" && $"a" < $"b"
     )
 
-    val planAfterRuleApp = BooleanSimplification.apply(testRelation.where(initialExpr).analyze)
-    val expectedOptPlan = testRelation.where(expectedOptimizedExpr).analyze
-    comparePlans(expectedOptPlan, planAfterRuleApp)
+    // check simplify Not((expr1 OR expr2) AND (expr3 OR expr4)) in single pass
+    executeRuleOnce(
+      Not(($"a" <= $"b" || $"c" > $"a" + 4) || ($"a" >= $"b" && $"c" < $"a")),
+      And(
+        And($"a" > $"b", $"c" <= $"a" + 4),
+        Or($"a" < $"b", $"c" >= $"a")
+      )
+    )
   }
 
   protected def assertEquivalent(e1: Expression, e2: Expression): Unit = {
