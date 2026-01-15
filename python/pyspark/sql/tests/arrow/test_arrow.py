b4b7a4b4f356b5b2e22951ef9381d6ab74bc40fe
Yicong-Huang <17627829+Yicong-Huang@users.noreply.github.com>
2026-01-15 13:52:01 -0800

    fix: format

--- python/pyspark/sql/tests/arrow/test_arrow.py
+++ python/pyspark/sql/tests/arrow/test_arrow.py
@@ -1853,8 +1853,6 @@ class ArrowTestsMixin:
                     self.assertEqual(t.num_rows, 10000)
                     self.assertEqual(t.column_names, ["id", "str_col", "mod_col"])
 
-
-
     def test_toPandas_double_nested_array_empty_outer(self):
         schema = StructType([StructField("data", ArrayType(ArrayType(StringType())))])
         df = self.spark.createDataFrame([Row(data=[])], schema=schema)
@@ -1882,11 +1880,14 @@ class ArrowTestsMixin:
         self.assertEqual(len(pdf["data"][0]), 0)
 
     def test_toPandas_nested_array_with_map_empty_outer(self):
-        schema = StructType([StructField("data", ArrayType(ArrayType(MapType(StringType(), StringType()))))])
+        schema = StructType(
+            [StructField("data", ArrayType(ArrayType(MapType(StringType(), StringType()))))]
+        )
         df = self.spark.createDataFrame([Row(data=[])], schema=schema)
         pdf = df.toPandas()
         self.assertEqual(len(pdf), 1)
         self.assertEqual(len(pdf["data"][0]), 0)
+
 
 @unittest.skipIf(
     not have_pandas or not have_pyarrow,
