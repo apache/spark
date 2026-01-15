e693a3086d977aa858dead57fe17ef2eb3bca812
Cheng Pan <chengpan@apache.org>
2026-01-15 20:57:43 +0800

    [WIP][SPARK-54276][BUILD] Bump Hadoop 3.4.3 RC0

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -304,6 +304,7 @@ object SparkBuild extends PomBuild {
       // Google Mirror of Maven Central, placed first so that it's used instead of flaky Maven Central.
       // See https://storage-download.googleapis.com/maven-central/index.html for more info.
       "gcs-maven-central-mirror" at "https://maven-central.storage-download.googleapis.com/maven2/",
+      "hadoop-staging" at "https://repository.apache.org/content/repositories/orgapachehadoop-1461/",
       "jitpack" at "https://jitpack.io",
       DefaultMavenRepository,
       Resolver.mavenLocal,
