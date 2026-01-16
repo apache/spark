813f7576aef225095393bce86df5379409bcd40b
zifeif2 <zifeifeng11@gmail.com>
2026-01-15 17:38:16 -0800

    [SPARK-54590][SS] Support Checkpoint V2 for State Rewriter and Repartitioning
    ### What changes were proposed in this pull request?
    
    Support checkpointV2 for repartition writer and StateRewriter by returning the checkpoint Id to caller function after write is done.
    Changes include
    - RocksDB loadWithCheckpointId supports loadEmpty
    - StatePartitionAllColumnFamiliesWriter return StateStoreCheckpointInfo
    - StateRewriter also propagate StateStoreCheckpointInfo back to the RepartitionRunner
    - RepartitionRunner stores the checkpointIds in commitLog
    
    ### Why are the changes needed?
    
    This is required in PrPr for repartition project
    
    ### Does this PR introduce _any_ user-facing change?
    
    No
    ### How was this patch tested?
    
    See added unit tests on moth operator with single state store and multiple state stores
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    Yes. Sonnet 4.5
    
    Closes #53720 from zifeif2/repartition-cp-v2.
    
    Lead-authored-by: zifeif2 <zifeifeng11@gmail.com>
    Co-authored-by: Zifei Feng <zifeifeng11@gmail.com>
    Signed-off-by: Anish Shrigondekar <anish.shrigondekar@databricks.com>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/state/RocksDBSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/streaming/state/RocksDBSuite.scala
@@ -3943,48 +3943,68 @@ class RocksDBSuite extends AlsoTestWithRocksDBFeatures with SharedSparkSession
     }}
   }
 
-  test("SPARK-54420: load with createEmpty creates empty store") {
-    val remoteDir = Utils.createTempDir().toString
-    new File(remoteDir).delete()
+  testWithStateStoreCheckpointIds(
+    "SPARK-54420: load with createEmpty creates empty store") { enableCkptId =>
+      val remoteDir = Utils.createTempDir().toString
+      new File(remoteDir).delete()
+      var lastVersion = 0L
+      var lastCheckpointInfo: Option[StateStoreCheckpointInfo] = None
 
-    withDB(remoteDir) { db =>
-      // loading batch 0 with loadEmpty = true
-      db.load(0, None, loadEmpty = true)
-      assert(iterator(db).isEmpty)
-      db.put("a", "1")
-      val (version1, _) = db.commit()
-      assert(toStr(db.get("a")) === "1")
+      withDB(remoteDir, enableStateStoreCheckpointIds = enableCkptId) { db =>
+        // loading batch 0 with loadEmpty = true
+        db.load(0, None, loadEmpty = true)
+        assert(iterator(db).isEmpty)
+        db.put("a", "1")
+        val (version1, checkpointInfoV1) = db.commit()
+        assert(toStr(db.get("a")) === "1")
 
-      // check we can load store normally even the previous one loadEmpty = true
-      db.load(version1)
-      db.put("b", "2")
-      val (version2, _) = db.commit()
-      assert(version2 === version1 + 1)
-      assert(toStr(db.get("b")) === "2")
-      assert(toStr(db.get("a")) === "1")
+        // check we can load store normally even the previous one loadEmpty = true
+        db.load(version1, checkpointInfoV1.stateStoreCkptId)
+        db.put("b", "2")
+        val (version2, _) = db.commit()
+        assert(version2 === version1 + 1)
+        assert(toStr(db.get("b")) === "2")
+        assert(toStr(db.get("a")) === "1")
 
-      // load an empty store
-      db.load(version2, loadEmpty = true)
-      db.put("c", "3")
-      val (version3, _) = db.commit()
-      assert(db.get("b") === null)
-      assert(db.get("a") === null)
-      assert(toStr(db.get("c")) === "3")
-      assert(version3 === version2 + 1)
+        // load an empty store
+        db.load(version2, loadEmpty = true)
+        db.put("c", "3")
+        val (version3, _) = db.commit()
+        assert(db.get("b") === null)
+        assert(db.get("a") === null)
+        assert(toStr(db.get("c")) === "3")
+        assert(version3 === version2 + 1)
 
-      // load 2 empty store in a row
-      db.load(version3, loadEmpty = true)
-      db.put("d", "4")
-      val (version4, _) = db.commit()
-      assert(db.get("c") === null)
-      assert(toStr(db.get("d")) === "4")
-      assert(version4 === version3 + 1)
+        // load 2 empty store in a row
+        db.load(version3, loadEmpty = true)
+        db.put("d", "4")
 
-      db.load(version4)
-      db.put("e", "5")
-      db.commit()
-      assert(db.iterator().map(toStr).toSet === Set(("d", "4"), ("e", "5")))
-    }
+        val (version4, checkpointV4) = db.commit()
+        assert(db.get("c") === null)
+        assert(toStr(db.get("d")) === "4")
+        lastVersion = version4
+        lastCheckpointInfo = Option(checkpointV4)
+        assert(lastVersion === version3 + 1)
+      }
+
+      withDB(remoteDir, enableStateStoreCheckpointIds = enableCkptId) { db =>
+        db.load(lastVersion, lastCheckpointInfo.map(_.stateStoreCkptId).orNull)
+        db.put("e", "5")
+        db.commit()
+        assert(db.iterator().map(toStr).toSet === Set(("d", "4"), ("e", "5")))
+      }
+
+      if (enableCkptId) {
+        withDB(remoteDir, enableStateStoreCheckpointIds = enableCkptId) { db =>
+          val ex = intercept[IllegalArgumentException] {
+            db.load(
+              lastVersion,
+              lastCheckpointInfo.map(_.stateStoreCkptId).orNull,
+              loadEmpty = true)
+          }
+          assert(ex.getMessage.contains("stateStoreCkptId should be empty when loadEmpty is true"))
+        }
+      }
   }
 
   test("SPARK-44639: Use Java tmp dir instead of configured local dirs on Yarn") {
@@ -4027,13 +4047,13 @@ class RocksDBSuite extends AlsoTestWithRocksDBFeatures with SharedSparkSession
         version: Long,
         ckptId: Option[String] = None,
         readOnly: Boolean = false,
-        createEmpty: Boolean = false): RocksDB = {
+        loadEmpty: Boolean = false): RocksDB = {
       // When a ckptId is defined, it means the test is explicitly using v2 semantic
       // When it is not, it is possible that implicitly uses it.
       // So still do a versionToUniqueId.get
       ckptId match {
-        case Some(_) => super.load(version, ckptId, readOnly)
-        case None => super.load(version, versionToUniqueId.get(version), readOnly)
+        case Some(_) => super.load(version, ckptId, readOnly, loadEmpty)
+        case None => super.load(version, versionToUniqueId.get(version), readOnly, loadEmpty)
       }
     }
 
