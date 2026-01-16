0497b386ec4e939a7229cfb5bc6f534f8aa79f5d
zeruibao <zerui.bao@databricks.com>
2026-01-16 12:57:26 -0800

    [SPARK-54824] Add support for multiGet and deleteRange for Rocksdb State Store
    ### What changes were proposed in this pull request?
    Add support for multiGet and deleteRange for Rocksdb State Store
    
    ### Why are the changes needed?
    For some streaming operators, using multiGet and deleteRange can improve the read/write performance.
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    UT
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No
    
    Closes #53583 from zeruibao/zeruibao/SPARK-54824-add-support-for-multi-get-and-rangeDelete.
    
    Authored-by: zeruibao <zerui.bao@databricks.com>
    Signed-off-by: Anish Shrigondekar <anish.shrigondekar@databricks.com>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/RocksDBStateEncoder.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/streaming/state/RocksDBStateEncoder.scala
@@ -44,6 +44,7 @@ import org.apache.spark.unsafe.Platform
 
 sealed trait RocksDBKeyStateEncoder {
   def supportPrefixKeyScan: Boolean
+  def supportsDeleteRange: Boolean
   def encodePrefixKey(prefixKey: UnsafeRow): Array[Byte]
   def encodeKey(row: UnsafeRow): Array[Byte]
   def decodeKey(keyBytes: Array[Byte]): UnsafeRow
@@ -1472,6 +1473,8 @@ class PrefixKeyScanStateEncoder(
   }
 
   override def supportPrefixKeyScan: Boolean = true
+
+  override def supportsDeleteRange: Boolean = false
 }
 
 /**
@@ -1669,6 +1672,8 @@ class RangeKeyScanStateEncoder(
   }
 
   override def supportPrefixKeyScan: Boolean = true
+
+  override def supportsDeleteRange: Boolean = true
 }
 
 /**
@@ -1698,6 +1703,8 @@ class NoPrefixKeyStateEncoder(
   }
 
   override def supportPrefixKeyScan: Boolean = false
+
+  override def supportsDeleteRange: Boolean = false
 
   override def encodePrefixKey(prefixKey: UnsafeRow): Array[Byte] = {
     throw new IllegalStateException("This encoder doesn't support prefix key!")
