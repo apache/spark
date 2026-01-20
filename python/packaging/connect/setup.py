4d3b45db9195b20f312aaccf8f5fbac9b6ee2f5c
Ruifeng Zheng <ruifengz@apache.org>
2026-01-20 15:54:46 +0800

    [SPARK-54849][PYTHON][FOLLOW-UP] Update minimum version of pyarrow in `setup.py` and `pyspark_upgrade.rst`
    ### What changes were proposed in this pull request?
    Update minimum version of pyarrow in `setup.py` and `pyspark_upgrade.rst`
    
    ### Why are the changes needed?
    the two parts were missing in previous pr
    
    ### Does this PR introduce _any_ user-facing change?
    no
    
    ### How was this patch tested?
    ci
    
    ### Was this patch authored or co-authored using generative AI tooling?
    no
    
    Closes #53863 from zhengruifeng/py_setup_doc.
    
    Authored-by: Ruifeng Zheng <ruifengz@apache.org>
    Signed-off-by: Ruifeng Zheng <ruifengz@apache.org>

--- python/packaging/connect/setup.py
+++ python/packaging/connect/setup.py
@@ -88,7 +88,7 @@ try:
     # python/packaging/classic/setup.py, and python/packaging/client/setup.py
     _minimum_pandas_version = "2.0.0"
     _minimum_numpy_version = "1.21"
-    _minimum_pyarrow_version = "11.0.0"
+    _minimum_pyarrow_version = "18.0.0"
     _minimum_grpc_version = "1.76.0"
     _minimum_googleapis_common_protos_version = "1.71.0"
     _minimum_pyyaml_version = "3.11"
