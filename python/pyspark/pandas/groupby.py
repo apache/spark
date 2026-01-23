018fc2060ba63bd9a363031aa09ab05e9ce01a52
Tian Gao <gaogaotiantian@hotmail.com>
2026-01-22 23:33:14 -0800

    Only need to silence one of the imports

--- python/pyspark/pandas/groupby.py
+++ python/pyspark/pandas/groupby.py
@@ -1956,7 +1956,7 @@ class GroupBy(Generic[FrameLike], metaclass=ABCMeta):
         )
 
         if LooseVersion(pd.__version__) < "3.0.0":
-            from pandas.core.common import is_builtin_func
+            from pandas.core.common import is_builtin_func  # type: ignore[import-untyped]
 
             f = is_builtin_func(func)
         else:
