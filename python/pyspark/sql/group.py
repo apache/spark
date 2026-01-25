6226f2fb2f03b7a29bb99b78d5e7d5c7cbd0cb1f
Hyukjin Kwon <gurwls223@apache.org>
2026-01-26 08:24:22 +0900

    Support keyword arguments for df.groupby(...).agg(...)

--- python/pyspark/sql/group.py
+++ python/pyspark/sql/group.py
@@ -19,6 +19,7 @@ import sys
 
 from typing import Callable, List, Optional, TYPE_CHECKING, overload, Dict, Union, cast, Tuple
 
+from pyspark.errors import PySparkValueError
 from pyspark.sql.column import Column
 from pyspark.sql.session import SparkSession
 from pyspark.sql.dataframe import DataFrame
@@ -87,7 +88,11 @@ class GroupedData(PandasGroupedOpsMixin):
     def agg(self, __exprs: Dict[str, str]) -> "DataFrame":
         ...
 
-    def agg(self, *exprs: Union[Column, Dict[str, str]]) -> "DataFrame":
+    @overload
+    def agg(self, **kwargs: str) -> "DataFrame":
+        ...
+
+    def agg(self, *exprs: Union[Column, Dict[str, str]], **kwargs: str) -> "DataFrame":
         """Compute aggregates and returns the result as a :class:`DataFrame`.
 
         The available aggregate functions can be:
@@ -118,6 +123,10 @@ class GroupedData(PandasGroupedOpsMixin):
         exprs : dict
             a dict mapping from column name (string) to aggregate functions (string),
             or a list of :class:`Column`.
+        **kwargs : str
+            keyword arguments mapping column names to aggregate function names.
+
+            .. versionadded:: 4.2.0
 
         Notes
         -----
@@ -153,6 +162,16 @@ class GroupedData(PandasGroupedOpsMixin):
         |  Bob|       2|
         +-----+--------+
 
+        Group-by name us`ing keyword arguments.
+
+        >>> df.groupBy(df.name).agg(age="min").sort("name").show()
+        +-----+--------+
+        | name|min(age)|
+        +-----+--------+
+        |Alice|       2|
+        |  Bob|       5|
+        +-----+--------+`
+
         Group-by name, and calculate the minimum age.
 
         >>> df.groupBy(df.name).agg(sf.min(df.age)).sort("name").show()
@@ -179,6 +198,13 @@ class GroupedData(PandasGroupedOpsMixin):
         +-----+------------+
         """
         from pyspark.sql.classic.column import _to_seq
+
+        if kwargs:
+            if exprs:
+                raise PySparkValueError(
+                    "Cannot use both positional arguments and keyword arguments"
+                )
+            exprs = (kwargs,)
 
         assert exprs, "exprs should not be empty"
         if len(exprs) == 1 and isinstance(exprs[0], dict):
