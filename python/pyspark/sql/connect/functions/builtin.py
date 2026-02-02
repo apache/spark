abf0214c3a0ba12d26391d62d93e92350ef07227
Uros Bojanic <uros.bojanic@databricks.com>
2026-02-02 10:46:34 +0100

    Fix srid

--- python/pyspark/sql/connect/functions/builtin.py
+++ python/pyspark/sql/connect/functions/builtin.py
@@ -5062,9 +5062,9 @@ def st_geomfromwkb(
     if srid is None:
         return _invoke_function_over_columns("st_geomfromwkb", wkb)
     else:
+        srid = _enum_to_value(srid)
         srid = lit(srid) if isinstance(srid, int) else srid
         return _invoke_function_over_columns("st_geomfromwkb", wkb, srid)
-
 
 st_geomfromwkb.__doc__ = pysparkfuncs.st_geomfromwkb.__doc__
 
