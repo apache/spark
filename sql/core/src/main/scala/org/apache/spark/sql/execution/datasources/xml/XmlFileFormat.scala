f22b214482b176420d5807f177d9f2ce16de41ea
Uros Bojanic <uros.bojanic@databricks.com>
2026-02-03 15:34:35 +0100

    Initial commit

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/xml/XmlFileFormat.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/xml/XmlFileFormat.scala
@@ -131,9 +131,6 @@ case class XmlFileFormat() extends TextBasedFileFormat with DataSourceRegister {
 
   override def supportDataType(dataType: DataType): Boolean = dataType match {
     case _: VariantType => true
-
-    case _: GeometryType | _: GeographyType => false
-
     case _: AtomicType => true
 
     case st: StructType => st.forall { f => supportDataType(f.dataType) }
