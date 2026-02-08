d54498861119989b041399b68545972ce851e133
Tian Gao <gaogaotiantian@hotmail.com>
2026-02-09 07:19:50 +0900

    [SPARK-55406][PYTHON][CONNECT] Reimplement the thread pool for ExecutePlanResponseReattachableIterator
    ### What changes were proposed in this pull request?
    
    Reimplemented the thread pool mechanism for ExecutePlanResponseReattachableIterator.
    * Number of `ExecutePlanResponseReattachableIterator` instances is counted with `WeakSet` so we know when to release the thread pool
    * Instead of relying on thread pool to finish, we collect the futures for each release task and wait for them in the client
    
    ### Why are the changes needed?
    
    The current implementation is fundamentally wrong. It tries to shutdown a thread pool for a class from client instances. We could have multiple client instances in a process and flipping the state of a global class would just cause racing issues.
    
    For example, the old code can return a thread pool in a thread which could be shutdown before it is used.
    
    The original reason that we "shutdown" the thread pool is because we need the client to wait for the release requests to finish. That can't justify waiting for the whole thread pool (or shut it down) because it can be shared with other clients. We actually have a deadlock caused by it.
    
    Now we collect futures explicitly for each `ExecutePlanResponseReattachableIterator` call and when we need to close the client, just wait for those future to be done.
    
    Notice that we will not keep these futures longer than they should be. We used `WeakSet` which means if the future has no reference, it will still be garbage collected properly. We won't use more memory because we keep track of these. In normal cases the set will probably be empty because all the release tasks are done.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Simple local test passed. Waiting for CI.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #54189 from gaogaotiantian/fix-connect-shutdown.
    
    Authored-by: Tian Gao <gaogaotiantian@hotmail.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/connect/client/reattach.py
+++ python/pyspark/sql/connect/client/reattach.py
@@ -22,9 +22,10 @@ check_dependencies(__name__)
 from threading import RLock
 import uuid
 from collections.abc import Generator
-from typing import Optional, Any, Iterator, Iterable, Tuple, Callable, cast, Type, ClassVar
-from concurrent.futures import ThreadPoolExecutor
+from typing import Optional, Any, Iterator, Iterable, Tuple, Callable, cast, ClassVar
+from concurrent.futures import Future, ThreadPoolExecutor
 import os
+import weakref
 
 import grpc
 from grpc_status import rpc_status
@@ -56,34 +57,16 @@ class ExecutePlanResponseReattachableIterator(Generator):
     ReleaseExecute RPCs that instruct the server to release responses that it already processed.
     """
 
-    # Lock to manage the pool
     _lock: ClassVar[RLock] = RLock()
     _release_thread_pool_instance: Optional[ThreadPoolExecutor] = None
+    _instances: ClassVar[
+        weakref.WeakSet["ExecutePlanResponseReattachableIterator"]
+    ] = weakref.WeakSet()
 
-    @classmethod
-    def _get_or_create_release_thread_pool(cls) -> ThreadPoolExecutor:
-        # Perform a first check outside the critical path.
-        if cls._release_thread_pool_instance is not None:
-            return cls._release_thread_pool_instance
-        with cls._lock:
-            if cls._release_thread_pool_instance is None:
-                max_workers = os.cpu_count() or 8
-                cls._release_thread_pool_instance = ThreadPoolExecutor(max_workers=max_workers)
-            return cls._release_thread_pool_instance
-
-    @classmethod
-    def shutdown(cls: Type["ExecutePlanResponseReattachableIterator"]) -> None:
-        """
-        When the channel is closed, this method will be called before, to make sure all
-        outstanding calls are closed.
-        """
-        with cls._lock:
-            if cls._release_thread_pool_instance is not None:
-                thread_pool = cls._release_thread_pool_instance
-                cls._release_thread_pool_instance = None
-                # This method could be called within the thread pool so don't wait for the
-                # shutdown to complete. Otherwise it could deadlock.
-                thread_pool.shutdown(wait=False)
+    def __new__(cls, *args: Any, **kwargs: Any) -> "ExecutePlanResponseReattachableIterator":
+        instance = super().__new__(cls)
+        cls._instances.add(instance)
+        return instance
 
     def __init__(
         self,
@@ -92,9 +75,9 @@ class ExecutePlanResponseReattachableIterator(Generator):
         retrying: Callable[[], Retrying],
         metadata: Iterable[Tuple[str, str]],
     ):
-        self._release_thread_pool  # Trigger initialization
         self._request = request
         self._retrying = retrying
+        self._release_futures: weakref.WeakSet[Future] = weakref.WeakSet()
         if request.operation_id:
             self._operation_id = request.operation_id
         else:
@@ -133,8 +116,29 @@ class ExecutePlanResponseReattachableIterator(Generator):
         self._current: Optional[pb2.ExecutePlanResponse] = None
 
     @property
+    def release_futures(self) -> weakref.WeakSet[Future]:
+        return self._release_futures
+
+    @property
     def _release_thread_pool(self) -> ThreadPoolExecutor:
-        return self._get_or_create_release_thread_pool()
+        if self._release_thread_pool_instance is not None:
+            # It's impossible for the thread pool to shutdown when the iterator is still alive
+            # We can safely return the thread pool instance here as long as it is not used
+            # by external objects.
+            return self._release_thread_pool_instance
+        with self._lock:
+            if self._release_thread_pool_instance is None:
+                self._release_thread_pool_instance = ThreadPoolExecutor(
+                    max_workers=os.cpu_count() or 8
+                )
+            return self._release_thread_pool_instance
+
+    @classmethod
+    def shutdown_threadpool_if_idle(cls) -> None:
+        with cls._lock:
+            if not cls._instances and cls._release_thread_pool_instance is not None:
+                cls._release_thread_pool_instance.shutdown(wait=False)
+                cls._release_thread_pool_instance = None
 
     def send(self, value: Any) -> pb2.ExecutePlanResponse:
         # will trigger reattach in case the stream completed without result_complete
@@ -214,11 +218,7 @@ class ExecutePlanResponseReattachableIterator(Generator):
             except Exception as e:
                 logger.warning(f"ReleaseExecute failed with exception: {e}.")
 
-        with self._lock:
-            if self._release_thread_pool_instance is not None:
-                thread_pool = self._release_thread_pool
-                if not thread_pool._shutdown:
-                    thread_pool.submit(target)
+        self._release_futures.add(self._release_thread_pool.submit(target))
 
     def _release_all(self) -> None:
         """
@@ -241,11 +241,7 @@ class ExecutePlanResponseReattachableIterator(Generator):
             except Exception as e:
                 logger.warning(f"ReleaseExecute failed with exception: {e}.")
 
-        with self._lock:
-            if self._release_thread_pool_instance is not None:
-                thread_pool = self._release_thread_pool
-                if not thread_pool._shutdown:
-                    thread_pool.submit(target)
+        self._release_futures.add(self._release_thread_pool.submit(target))
         self._result_complete = True
 
     def _call_iter(self, iter_fun: Callable) -> Any:
