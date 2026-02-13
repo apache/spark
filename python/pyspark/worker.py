1ad6cfd6d60146cdd498f0238f8b8b2c5a21193f
Allison Wang <allison.wang@databricks.com>
2026-02-13 13:34:46 -0800

    fix

--- python/pyspark/worker.py
+++ python/pyspark/worker.py
@@ -2343,7 +2343,7 @@ def read_udtf(pickleSer, infile, eval_type, runner_conf):
 
                 def raise_conversion_error(original_exception):
                     raise PySparkRuntimeError(
-                        errorClass="UDTF_ARROW_TYPE_CONVERSION_ERROR",
+                        errorClass="UDTF_ARROW_DATA_CONVERSION_ERROR",
                         messageParameters={
                             "data": str(data),
                             "schema": return_type.simpleString(),
