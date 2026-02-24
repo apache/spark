c3a97cf3d4c534af427c9ec75bf05e825deabf11
Takuya Ueshin <ueshin@databricks.com>
2026-02-23 17:17:27 -0800

    [SPARK-55624][PS][TESTS][FOLLOW-UP] Fix `_ignore_arrow_dtypes` util for DataFrame
    ### What changes were proposed in this pull request?
    
    This is a follow-up of apache/spark#54412.
    
    Fix `_ignore_arrow_dtypes` util for DataFrame.
    
    ### Why are the changes needed?
    
    There is a bug in `_ignore_arrow_dtypes` for DataFrame case.
    
    `obj.columns` will return a list of strings, instead of a list of `Series`.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    The existing tests should pass.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #54430 from ueshin/issues/SPARK-55624/fix.
    
    Authored-by: Takuya Ueshin <ueshin@databricks.com>
    Signed-off-by: Takuya Ueshin <ueshin@databricks.com>

--- python/pyspark/testing/pandasutils.py
+++ python/pyspark/testing/pandasutils.py
@@ -498,13 +498,13 @@ class PandasOnSparkTestUtils:
         else:
             if isinstance(obj, pd.DataFrame):
                 arrow_boolean_columns = [
-                    col
-                    for col in obj.columns
+                    name
+                    for name, col in obj.items()
                     if isinstance(col.dtype, pd.ArrowDtype)
                     and col.dtype.pyarrow_dtype == pa.bool_()
                 ]
                 if arrow_boolean_columns:
-                    return obj.astype({col: "boolean" for col in arrow_boolean_columns})
+                    return obj.astype({name: "boolean" for name in arrow_boolean_columns})
             elif isinstance(obj, (pd.Series, pd.Index)):
                 if isinstance(obj.dtype, pd.ArrowDtype) and obj.dtype.pyarrow_dtype == pa.bool_():
                     return obj.astype("boolean")
