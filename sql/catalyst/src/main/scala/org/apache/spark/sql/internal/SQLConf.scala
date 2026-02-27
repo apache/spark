fcf8636105c060b0cc05712d503d4f6fe5a5dff5
Jungtaek Lim <kabhwan.opensource@gmail.com>
2026-02-27 13:58:53 +0900

    [SPARK-55144][SS] Introduce new state format version for performant stream-stream join
    ### What changes were proposed in this pull request?
    
    This PR proposes to implement the new state format for stream-stream join, based on the new state key encoding w.r.t. event time awareness.
    
    The new state format is focused to eliminate the necessity of full scan during eviction & populating unmatched rows. The overhead of eviction should have bound to the actual number of state rows to be evicted (indirectly impacted by the amount of watermark advancement), but we have been doing the full scan with the existing state format, which could take more than 2 seconds in 1,000,000 rows even if there is zero row to be evicted. The overhead of eviction with the new state format would be bound to the actual number of state rows to be evicted, taking around 30ms or even less in 1,000,000 rows when there is zero row to be evicted.
    
    To achieve the above, we make a drastic change of data structure to move out from the logical array, and introduce a secondary index in addition to the main data.
    
    Each side of the join will use two (virtual) column families (total 4 column families), which are following:
    
    * KeyWithTsToValuesStore
      * Primary data store
      * (key, event time) -> values
      * each element in values consists of (value, matched)
    * TsWithKeyTypeStore
      * Secondary index for efficient eviction
      * (event time, key) -> empty value (configured as multi-values)
      * numValues is calculated by the number of elements in the value side; new element is added when a new value is added into values in primary data store
        * This is to track the number of deleted rows accurately. It's optional but the metric has been useful so we want to keep it as it is.
    
    As the format of key part implies, KeyWithTsToValuesStore will use `TimestampAsPostfixKeyStateEncoderSpec`, and TsWithKeyTypeStore will use `TimestampAsPrefixKeyStateEncoderSpec`.
    
    The granularity of the timestamp for event time is 1 millisecond, which is in line with the granularity for watermark advancement. This can be a kind of knob controlling the number of the keys vs the number of the values in the key, trading off the granularity of eviction based on watermark advancement vs the size of key space (may impact performance).
    
    There are several follow-ups with this state format implementation, which can be addressed on top of this:
    
    * further optimizations with RocksDB offering: WriteBatch (for batched writes), MGET, etc.
    * retrieving matched rows with the "scope" of timestamps (in time-interval join)
      * while the format is ready to support ordered scan of timestamp, this needs another state store API to define the range of keys to scan, which needs some effort
    * Do not update the matched flag for non-outer join side.
    
    ### Why are the changes needed?
    
    The cost of eviction based on full scan is severe to make the stream-stream join to be lower latency. Also, the logic of maintaining logical array is complicated enough to maintain and the performance characteristic is less predictable given the behavior of deleting the element in random index (placing the value of the last index to the deleted index).
    
    ### Does this PR introduce _any_ user-facing change?
    
    No. At this point, this state format is not integrated with the actual stream-stream join operator, and we need to do follow-up work for integration to finally introduce the change to user-facing.
    
    ### How was this patch tested?
    
    New UT suites, refactoring the existing suite to test with both time window and time interval cases.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #53930 from HeartSaVioR/SPARK-55144-on-top-of-SPARK-55129.
    
    Authored-by: Jungtaek Lim <kabhwan.opensource@gmail.com>
    Signed-off-by: Jungtaek Lim <kabhwan.opensource@gmail.com>

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -3087,9 +3087,12 @@ object SQLConf {
       .doc("State format version used by streaming join operations in a streaming query. " +
         "State between versions are tend to be incompatible, so state format version shouldn't " +
         "be modified after running. Version 3 uses a single state store with virtual column " +
-        "families instead of four stores and is only supported with RocksDB.")
+        "families instead of four stores and is only supported with RocksDB. NOTE: version " +
+        "1 is DEPRECATED and should not be explicitly set by users.")
       .version("3.0.0")
       .intConf
+      // TODO: [SPARK-55628] Add version 4 once we integrate the state format version 4 into
+      //  stream-stream join operator.
       .checkValue(v => Set(1, 2, 3).contains(v), "Valid versions are 1, 2, and 3")
       .createWithDefault(2)
 
