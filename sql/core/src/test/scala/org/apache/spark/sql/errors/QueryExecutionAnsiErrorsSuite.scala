f468100d30a3d81b22b84169b586702eea2c076c
s.repnikov <srpnkv.dev@gmail.com>
2026-03-05 09:27:53 +0300

    Fix QueryExecutionAnsiErrorsSuite tests

--- sql/core/src/test/scala/org/apache/spark/sql/errors/QueryExecutionAnsiErrorsSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/errors/QueryExecutionAnsiErrorsSuite.scala
@@ -154,8 +154,7 @@ class QueryExecutionAnsiErrorsSuite extends SharedSparkSession {
       parameters = Map(
         "value" -> "66666666666666.666",
         "precision" -> "8",
-        "scale" -> "1",
-        "config" -> ansiConf),
+        "scale" -> "1"),
       context = ExpectedContext(
         fragment = "CAST('66666666666666.666' AS DECIMAL(8, 1))",
         start = 7,
@@ -170,8 +169,7 @@ class QueryExecutionAnsiErrorsSuite extends SharedSparkSession {
       parameters = Map(
         "value" -> "66666666666666.666",
         "precision" -> "8",
-        "scale" -> "1",
-        "config" -> ansiConf),
+        "scale" -> "1"),
       context = ExpectedContext(
         fragment = "cast",
         callSitePattern = getCurrentClassCallSitePattern))
