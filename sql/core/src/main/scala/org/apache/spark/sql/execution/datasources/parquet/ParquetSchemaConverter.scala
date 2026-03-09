0d4131ecdf6961d2042eaf68e334acec52ce77b2
Vandana Sah <vandanasah140@ibm.com>
2026-03-09 14:11:27 +0530

    orc parquet typecoercion

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetSchemaConverter.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetSchemaConverter.scala
@@ -523,7 +523,7 @@ class SparkToParquetSchemaConverter(
       case IntegerType | _: YearMonthIntervalType =>
         Types.primitive(INT32, repetition).named(field.name)
 
-      case LongType | _: DayTimeIntervalType =>
+      case LongType | _: DayTimeIntervalType | TimeType =>
         Types.primitive(INT64, repetition).named(field.name)
 
       case FloatType =>
