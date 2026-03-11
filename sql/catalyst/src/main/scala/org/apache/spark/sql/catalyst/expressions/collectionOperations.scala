f965a0cfc701e3528b43f06e2e7da2b539b840ac
yangjie01 <yangjie01@baidu.com>
2026-03-11 11:04:13 +0800

    revert collectionOperations.scala

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/collectionOperations.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/collectionOperations.scala
@@ -2741,9 +2741,7 @@ case class ElementAt(
         }
       }
     case _: MapType =>
-      (value, ordinal) => {
-        getValueEval(value, ordinal, mapKeyType, ordering)
-      }
+      (value, ordinal) => getValueEval(value, ordinal, mapKeyType, ordering)
   }
 
   override def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
