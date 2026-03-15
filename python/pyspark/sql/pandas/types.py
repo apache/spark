8d2e0fe2c2de25a5c27d86713630b9f40ff2262e
yangjie01 <yangjie01@baidu.com>
2026-03-16 01:33:28 +0800

    fix lint

--- python/pyspark/sql/pandas/types.py
+++ python/pyspark/sql/pandas/types.py
@@ -787,11 +787,9 @@ def _check_series_convert_timestamps_localize(
         return s.dt.tz_convert(to_tz).dt.tz_localize(None)
     elif is_datetime64_dtype(s.dtype) and from_tz != to_tz:
         # `s.dt.tz_localize('tzlocal()')` doesn't work properly when including NaT.
-        return s.apply(
+        return cast(Any, s).apply(
             lambda ts: (
-                ts.tz_localize(from_tz, ambiguous=False)  # type: ignore[arg-type, return-value]
-                .tz_convert(to_tz)
-                .tz_localize(None)
+                ts.tz_localize(from_tz, ambiguous=False).tz_convert(to_tz).tz_localize(None)
                 if ts is not pd.NaT
                 else pd.NaT
             )
