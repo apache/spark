3ef0dff5b82116390643e4fc45629f11404aef68
Ziya Mukhtarov <ziya5muxtarov@gmail.com>
2026-03-18 16:44:39 +0000

    Delete config binding policy

--- sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -1624,7 +1624,6 @@ object SQLConf {
         "inference and infers NullType. When disabled, ignores the UNKNOWN annotation " +
         "and uses the physical type instead.")
       .version("4.1.2")
-      .withBindingPolicy(ConfigBindingPolicy.SESSION)
       .booleanConf
       .createWithDefault(false)
 
