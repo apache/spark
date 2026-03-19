811c7b780f33b6285f975496d1ca70a4538106a4
Tian Gao <gaogaotiantian@hotmail.com>
2026-03-19 12:30:44 +0900

    [SPARK-56062][PYTHON] Isolate memory_profiler to improve import time
    ### What changes were proposed in this pull request?
    
    * Moved `memory_profiler` related code to another file
    * Make that module lazy so importing it won't trigger `import memory_profiler`
    * Updated the code that uses `memory_profiler` related code to use the new module
    
    ### Why are the changes needed?
    
    We should make `import pyspark` fast. Bootstrap time is important to user experience, and our CI speed. Also, we should make worker spawn faster so simple worker can be faster to boot. We have a pending SPIP about using simpler worker for UDF work and import time is critical.
    
    `import pyspark` takes **220 ms** on master and will be **90 ms** after this PR. Similar acceleration will be achieved for `pyspark.worker`.
    
    This would probably make a lot of our tests faster as a side effect. Including the newly added python data source simple worker test which timeout a lot in coverage run - https://github.com/apache/spark/actions/runs/23046212656/job/66936106272 .
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    I ran some profiler tests locally and they passed. CI is needed.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #54895 from gaogaotiantian/isolate-memory-profiler.
    
    Authored-by: Tian Gao <gaogaotiantian@hotmail.com>
    Signed-off-by: Hyukjin Kwon <gurwls223@apache.org>

--- python/pyspark/sql/profiler.py
+++ python/pyspark/sql/profiler.py
@@ -35,6 +35,7 @@ from typing import (
 )
 import warnings
 
+import pyspark.memory_profiler_ext
 from pyspark.accumulators import (
     Accumulator,
     AccumulatorParam,
@@ -47,7 +48,6 @@ from pyspark.profiler import (
     MemoryProfiler,
     MemUsageParam,
     PStatsParam,
-    has_memory_profiler,
 )
 
 if TYPE_CHECKING:
@@ -138,7 +138,7 @@ class WorkerMemoryProfiler:
         result_key: Union[int, str],
         func_or_code: Union[Callable, CodeType],
     ) -> None:
-        from pyspark.profiler import UDFLineProfilerV2
+        from pyspark.memory_profiler_ext import UDFLineProfilerV2
 
         self._accumulator = accumulator
         self._profiler = UDFLineProfilerV2()
@@ -245,7 +245,7 @@ class ProfilerCollector(ABC):
         with self._lock:
             code_map = self._memory_profile_results
 
-        if not has_memory_profiler and not code_map:
+        if not pyspark.memory_profiler_ext.has_memory_profiler and not code_map:
             warnings.warn(
                 "Install the 'memory_profiler' library in the cluster to enable memory profiling",
                 UserWarning,
@@ -331,7 +331,7 @@ class ProfilerCollector(ABC):
         with self._lock:
             code_map = self._memory_profile_results
 
-        if not has_memory_profiler and not code_map:
+        if not pyspark.memory_profiler_ext.has_memory_profiler and not code_map:
             warnings.warn(
                 "Install the 'memory_profiler' library in the cluster to enable memory profiling",
                 UserWarning,
