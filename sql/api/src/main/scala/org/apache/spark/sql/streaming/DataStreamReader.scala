f90f8730c72a91bab10eb38c7793ac158474d2c2
Gengliang Wang <gengliang@apache.org>
2026-03-19 16:05:41 -0700

    fix @param

--- sql/api/src/main/scala/org/apache/spark/sql/streaming/DataStreamReader.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/streaming/DataStreamReader.scala
@@ -131,7 +131,8 @@ abstract class DataStreamReader {
    *     .changes("my_table")
    * }}}
    *
-   * @param tableName is either a qualified or unqualified name that designates a table.
+   * @param tableName
+   *   a qualified or unqualified name that designates a table.
    * @since 4.2.0
    */
   def changes(tableName: String): DataFrame
