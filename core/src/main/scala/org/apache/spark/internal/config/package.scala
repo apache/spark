bf493ce2893b5af05861b67898aa7420d0f17aad
Cheng Pan <chengpan@apache.org>
2026-03-25 15:37:04 +0800

    rc2

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -2786,7 +2786,7 @@ package object config {
       .stringConf
       .toSequence
       .createWithDefault(
-        Seq("https://repository.apache.org/content/repositories/orgapachehadoop-1469/"))
+        Seq("https://repository.apache.org/content/repositories/orgapachehadoop-1470/"))
 
   private[spark] val APP_ATTEMPT_ID =
     ConfigBuilder("spark.app.attempt.id")
