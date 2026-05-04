d57f1de44c1e7a6c722af579fb90d6427487a3e0
Kousuke Saruta <sarutak@amazon.co.jp>
2026-05-04 15:19:59 +0900

    [SPARK-56675][BUILD] Make sbt publishLocal work
    ### What changes were proposed in this pull request?
    This PR fixes an issue that `sbt publishLocal` doesn't work.
    The reason is that `packageDoc/publishArtifact` doesn't work (so `sbt doc` also doesn't work).
    In the Spark community, API documents are generated using `unidoc` and I feel having API documents in `~/.m2` or `~/.ivy2` is not so useful.
    So, the solution is publishing API documents by `packageDoc / publishArtifact := false`.
    In addition, this PR allows developers to configure whether to publish source JARs by an environment variable `PUBLISH_PACKAGE_SRC`. The default value is `false`, which is the same behavior as `mvn install`.
    
    ### Why are the changes needed?
    Having artifacts in `~/.m2` or `~.ivy2` is useful for downstream developers
    
    ### Does this PR introduce _any_ user-facing change?
    No.
    
    ### How was this patch tested?
    Confirmed artifacts are in both `~/.m2` and `~/.ivy2` by the following command.
    ```
    $ build/sbt publishLocal
    ```
    
    Also confirmed `*-sources.jar` are in both `~/.m2` and `~/.ivy2` by the following command.
    ```
    $ PUBLISH_PACKAGE_SRC=1 build/sbt publishLocal
    ```
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #55624 from sarutak/publishLocal-work.
    
    Authored-by: Kousuke Saruta <sarutak@amazon.co.jp>
    Signed-off-by: Kousuke Saruta <sarutak@apache.org>
    (cherry picked from commit 4b20e52d63b740a360ec8bb068b99777e7b3358a)
    Signed-off-by: Kousuke Saruta <sarutak@apache.org>

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -299,6 +299,8 @@ object SparkBuild extends PomBuild {
       .orElse(sys.props.get("java.home"))
       .map(file),
     publishMavenStyle := true,
+    packageDoc / publishArtifact := false,
+    packageSrc / publishArtifact := (if (sys.env.contains("PUBLISH_PACKAGE_SRC")) true else false),
     unidocGenjavadocVersion := "0.19",
 
     // Override SBT's default resolvers:
