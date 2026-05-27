88eb1b38aa43b2598bb4882a2733194a02d1b971
Ruifeng Zheng <ruifengz@apache.org>
2026-05-27 06:31:08 +0000

    [SQL][PYTHON][DOCS] Fix versionadded/@since for kll_merge_agg_* (4.1.0 -> 4.1.2)
    kll_merge_agg_{bigint,float,double} were annotated as introduced in
    4.1.0 across Python, Scala, and the SQL ExpressionDescription, but the
    master commit that added them (SPARK-54785, fc15f726eab on 2026-01-12)
    landed after v4.1.0 was tagged (2025-12-11), and the corresponding
    branch-4.1 cherry-pick (a39c1b8e5e2) shipped in v4.1.2 (2026-05-16).
    No 4.1.0 or 4.1.1 release contains them.
    
    Update the annotation to 4.1.2 across:
      - python/pyspark/sql/functions/builtin.py (.. versionadded:: in 3
        docstrings)
      - sql/api/src/main/scala/org/apache/spark/sql/functions.scala
        (@since in 15 overload Scaladocs)
      - sql/catalyst/.../aggregate/kllAggregates.scala
        (since = in 3 @ExpressionDescription annotations)

--- python/pyspark/sql/functions/builtin.py
+++ python/pyspark/sql/functions/builtin.py
@@ -27606,7 +27606,7 @@ def kll_merge_agg_bigint(
     sketch (range 8-65535). If k is not specified, the merged sketch adopts the k value
     from the first input sketch.
 
-    .. versionadded:: 4.1.0
+    .. versionadded:: 4.1.2
 
     Parameters
     ----------
@@ -27650,7 +27650,7 @@ def kll_merge_agg_float(
     sketch (range 8-65535). If k is not specified, the merged sketch adopts the k value
     from the first input sketch.
 
-    .. versionadded:: 4.1.0
+    .. versionadded:: 4.1.2
 
     Parameters
     ----------
@@ -27694,7 +27694,7 @@ def kll_merge_agg_double(
     sketch (range 8-65535). If k is not specified, the merged sketch adopts the k value
     from the first input sketch.
 
-    .. versionadded:: 4.1.0
+    .. versionadded:: 4.1.2
 
     Parameters
     ----------
