442ff43a459cf2a7f4a0868643ec74a5c773561e
Matthis Gördel <matthis.goerdel@databricks.com>
2026-05-29 18:04:28 +0000

    Have SharedSparkSession as empty alias of classic.SparkSessionBinder

--- sql/core/src/test/scala/org/apache/spark/sql/classic/SparkSessionBinder.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/classic/SparkSessionBinder.scala
@@ -17,6 +17,8 @@
 
 package org.apache.spark.sql.classic
 
+import scala.concurrent.duration._
+
 import org.apache.spark.sql
 
 /**
@@ -24,4 +26,31 @@ import org.apache.spark.sql
  */
 trait SparkSessionBinder extends sql.SparkSessionBinder with QueryTest {
   override protected def spark: SparkSession = super.spark.asInstanceOf[SparkSession]
+
+  // Runs func (which must trigger exactly one SQL execution) and returns the SQL metrics of that
+  // execution as a map keyed by (planNodeId, planNodeName, metricName) -> metricValue.
+  def runAndFetchMetrics(func: => Unit): Map[(Long, String, String), String] = {
+    val statusStore = spark.sharedState.statusStore
+    val oldCount = statusStore.executionsList().size
+
+    func
+
+    // Wait until the new execution is started and being tracked.
+    eventually(timeout(10.seconds), interval(10.milliseconds)) {
+      assert(statusStore.executionsCount() >= oldCount)
+    }
+
+    // Wait for listener to finish computing the metrics for the execution.
+    eventually(timeout(10.seconds), interval(10.milliseconds)) {
+      assert(statusStore.executionsList().nonEmpty &&
+        statusStore.executionsList().last.metricValues != null)
+    }
+
+    val exec = statusStore.executionsList().last
+    val execId = exec.executionId
+    val sqlMetrics = statusStore.planGraph(execId).allNodes
+      .flatMap(n => n.metrics.map(m => (m.accumulatorId, (n.id, n.name, m.name))))
+      .toMap
+    statusStore.executionMetrics(execId).map { case (k, v) => sqlMetrics(k) -> v }
+  }
 }
