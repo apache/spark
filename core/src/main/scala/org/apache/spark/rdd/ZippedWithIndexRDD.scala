759baeca48cc286e552e77b185e86f491cfa960a
Wenchen Fan <cloud0fan@gmail.com>
2026-06-21 16:00:33 -0700

    Apply suggestion from @cloud-fan

--- core/src/main/scala/org/apache/spark/rdd/ZippedWithIndexRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/ZippedWithIndexRDD.scala
@@ -40,7 +40,7 @@ class ZippedWithIndexRDDPartition(val prev: Partition, val startIndex: Long)
 private[spark]
 class ZippedWithIndexRDD[T: ClassTag](prev: RDD[T]) extends RDD[(T, Long)](prev) {
 
-  @tailrec
+  @scala.annotation.tailrec
   private def getAncestorWithSamePartitionSizes(rdd: RDD[_]): RDD[_] = {
     rdd match {
       // A ZippedWithIndexRDD ancestor that has already computed its startIndices covers the same
