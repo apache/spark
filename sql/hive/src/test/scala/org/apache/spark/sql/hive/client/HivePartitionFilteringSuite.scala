38c71a76e8a4698b47b6a56430dcfd1146dc0145
Dongjoon Hyun <dongjoon@apache.org>
2026-06-27 19:32:44 -0700

    [SPARK-57730][SQL][TESTS] Mark `Hive(PartitionFiltering|ClientUserName)Suite` as SlowHiveTest

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/client/HivePartitionFilteringSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/client/HivePartitionFilteringSuite.scala
@@ -34,8 +34,10 @@ import org.apache.spark.sql.catalyst.expressions._
 import org.apache.spark.sql.catalyst.plans.SQLHelper
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{BooleanType, DateType, IntegerType, LongType, StringType, StructType, TimestampType}
+import org.apache.spark.tags.SlowHiveTest
 import org.apache.spark.util.Utils
 
+@SlowHiveTest
 class HivePartitionFilteringSuite(version: String)
     extends HiveVersionSuite(version) with SQLHelper {
 
