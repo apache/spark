91b508bce526e4f9d06a0e7969f17b31ba633a59
Vinod KC <vinod.kc.in@gmail.com>
2026-07-01 17:24:44 +0000

    Fix review comments

--- sql/core/src/test/scala/org/apache/spark/sql/TimeFunctionsSuiteBase.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/TimeFunctionsSuiteBase.scala
@@ -21,7 +21,7 @@ import java.time.LocalDate
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit
 
-import org.apache.spark.{SparkConf, SparkDateTimeException, SparkIllegalArgumentException}
+import org.apache.spark.{SparkConf, SparkDateTimeException, SparkIllegalArgumentException, SparkRuntimeException}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.SharedSparkSession
@@ -786,93 +786,88 @@ abstract class TimeFunctionsSuiteBase extends SharedSparkSession {
 
   test("time_format") {
     // Basic formats: 24-hour and 12-hour with AM/PM
+    val df1 = Seq((LocalTime.of(14, 30, 45), LocalTime.of(9, 15, 30)))
+      .toDF("afternoon", "morning")
     checkAnswer(
-      sql("""
-        SELECT
-          time_format(TIME'14:30:45', 'HH:mm:ss') as format_24hr,
-          time_format(TIME'14:30:45', 'hh:mm:ss a') as format_12hr_pm,
-          time_format(TIME'09:15:30', 'hh:mm:ss a') as format_12hr_am
-      """),
-      Row("14:30:45", "02:30:45 PM", "09:15:30 AM")
-    )
+      df1.select(
+        time_format($"afternoon", "HH:mm:ss"),
+        time_format($"afternoon", "hh:mm:ss a"),
+        time_format($"morning", "hh:mm:ss a")),
+      Row("14:30:45", "02:30:45 PM", "09:15:30 AM"))
 
     // Precision: milliseconds, microseconds, single-digit hour
+    val df2 = Seq((LocalTime.of(14, 30, 45, 123000000), LocalTime.of(14, 30, 45, 123456000),
+      LocalTime.of(9, 5, 0))).toDF("millis", "micros", "single_digit")
     checkAnswer(
-      sql("""
-        SELECT
-          time_format(TIME'14:30:45.123', 'HH:mm:ss.SSS') as millis,
-          time_format(TIME'14:30:45.123456', 'HH:mm:ss.SSSSSS') as micros,
-          time_format(TIME'09:05:00', 'H:mm a') as single_digit
-      """),
-      Row("14:30:45.123", "14:30:45.123456", "9:05 AM")
-    )
+      df2.select(
+        time_format($"millis", "HH:mm:ss.SSS"),
+        time_format($"micros", "HH:mm:ss.SSSSSS"),
+        time_format($"single_digit", "H:mm a")),
+      Row("14:30:45.123", "14:30:45.123456", "9:05 AM"))
 
     // Edge cases: midnight, noon, max time, different patterns
+    val df3 = Seq((LocalTime.of(0, 0, 0), LocalTime.of(12, 0, 0),
+      LocalTime.of(23, 59, 59, 999999000), LocalTime.of(14, 30, 45)))
+      .toDF("midnight", "noon", "max_time", "afternoon")
     checkAnswer(
-      sql("""
-        SELECT
-          time_format(TIME'00:00:00', 'HH:mm:ss') as midnight_24hr,
-          time_format(TIME'00:00:00', 'hh:mm:ss a') as midnight_12hr,
-          time_format(TIME'12:00:00', 'hh:mm:ss a') as noon,
-          time_format(TIME'23:59:59.999999', 'HH:mm:ss.SSSSSS') as max_time,
-          time_format(TIME'14:30:45', 'HH:mm') as hour_min,
-          time_format(TIME'14:30:45', 'HH') as hour_only
-      """),
-      Row("00:00:00", "12:00:00 AM", "12:00:00 PM", "23:59:59.999999", "14:30", "14")
-    )
+      df3.select(
+        time_format($"midnight", "HH:mm:ss"),
+        time_format($"midnight", "hh:mm:ss a"),
+        time_format($"noon", "hh:mm:ss a"),
+        time_format($"max_time", "HH:mm:ss.SSSSSS"),
+        time_format($"afternoon", "HH:mm"),
+        time_format($"afternoon", "HH")),
+      Row("00:00:00", "12:00:00 AM", "12:00:00 PM", "23:59:59.999999", "14:30", "14"))
 
     // Custom separators and text literals
+    val df4 = Seq(LocalTime.of(14, 30, 45)).toDF("time_col")
     checkAnswer(
-      sql("""
-        SELECT
-          time_format(TIME'14:30:45', 'hh-mm-ss a') as hyphen,
-          time_format(TIME'14:30:45', 'HH.mm.ss') as dot,
-          time_format(TIME'14:30:45', '''Time:'' HH:mm:ss') as with_text
-      """),
-      Row("02-30-45 PM", "14.30.45", "Time: 14:30:45")
-    )
+      df4.select(
+        time_format($"time_col", "hh-mm-ss a"),
+        time_format($"time_col", "HH.mm.ss"),
+        time_format($"time_col", "'Time:' HH:mm:ss")),
+      Row("02-30-45 PM", "14.30.45", "Time: 14:30:45"))
 
     // Null handling
+    val df5 = Seq[(LocalTime, LocalTime)]((null, LocalTime.of(14, 30, 45)))
+      .toDF("null_time", "time_col")
     checkAnswer(
-      sql("""
-        SELECT
-          time_format(NULL, 'HH:mm:ss') as null_time,
-          time_format(TIME'14:30:45', NULL) as null_format
-      """),
-      Row(null, null)
-    )
+      df5.select(
+        time_format($"null_time", "HH:mm:ss"),
+        time_format($"time_col", null)),
+      Row(null, null))
 
-    // DataFrame API with column expressions
-    val df = Seq(
+    // Column expressions over multiple rows
+    val labeledDf = Seq(
       ("morning", LocalTime.of(9, 30, 0)),
       ("afternoon", LocalTime.of(14, 30, 45)),
       ("evening", LocalTime.of(19, 45, 30))
     ).toDF("label", "time_col")
-
     checkAnswer(
-      df.select($"label", time_format($"time_col", "hh:mm a")).orderBy($"label"),
+      labeledDf.select($"label", time_format($"time_col", "hh:mm a")).orderBy($"label"),
       Seq(
         Row("afternoon", "02:30 PM"),
         Row("evening", "07:45 PM"),
-        Row("morning", "09:30 AM")
-      )
-    )
+        Row("morning", "09:30 AM")))
 
     // Verbose format with natural language
+    val df6 = Seq(LocalTime.of(2, 20, 30, 40000000)).toDF("time_col")
     checkAnswer(
-      sql("SELECT time_format(TIME'02:20:30.040', " +
-        "'H ''hours'' mm ''mins'' ss ''seconds'' SSS ''milliseconds''')"),
-      Row("2 hours 20 mins 30 seconds 040 milliseconds")
-    )
+      df6.select(
+        time_format($"time_col", "H 'hours' mm 'mins' ss 'seconds' SSS 'milliseconds'")),
+      Row("2 hours 20 mins 30 seconds 040 milliseconds"))
 
     // Invalid format pattern should throw
-    val ex = intercept[Exception] {
-      sql("SELECT time_format(TIME'14:30:45', 'invalid[[[')").collect()
-    }
-    assert(ex.getMessage.contains("Illegal pattern") ||
-           ex.getMessage.contains("Unknown pattern") ||
-           ex.getMessage.contains("Invalid") ||
-           ex.getMessage.contains("Unrecognized datetime pattern"))
+    checkError(
+      exception = intercept[SparkRuntimeException] {
+        df6.select(time_format($"time_col", "invalid[[[")).collect()
+      },
+      condition = "INVALID_DATETIME_PATTERN.WITH_SUGGESTION",
+      parameters = Map(
+        "pattern" -> "'invalid[[['",
+        "docroot" -> "https://spark.apache.org/docs/latest"
+      )
+    )
   }
 }
 
