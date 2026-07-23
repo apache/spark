2bbf825e36cf04599828ad053db6b5f48ca45a50
Chao Sun <chao@openai.com>
2026-07-22 18:05:48 -0700

    [SPARK-58272][SQL] Preserve runtime Bloom filters above cached leaves

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/InjectRuntimeFilter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/InjectRuntimeFilter.scala
@@ -198,6 +198,12 @@ object InjectRuntimeFilter extends Rule[LogicalPlan] with PredicateHelper with J
               hasSelectivePredicate = hasHitSelectiveFilter || leaf.hasSelectivePredicate,
               materializedRowCount = Some(rowCount))
           }
+        } else if (hasHitSelectiveFilter) {
+          Some(FilterCreationSide(
+            targetKey,
+            currentPlan,
+            useMaterializedThreshold = false,
+            hasSelectivePredicate = true))
         } else {
           None
         }
