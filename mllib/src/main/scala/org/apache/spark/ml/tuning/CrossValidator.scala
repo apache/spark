e0ace495560f37f1fbe2ccfb18aa497c044b1959
Ruifeng Zheng <ruifengz@apache.org>
2026-07-31 03:43:51 +0000

    [ML] Guard retained CrossValidator submodels in size estimation

--- mllib/src/main/scala/org/apache/spark/ml/tuning/CrossValidator.scala
+++ mllib/src/main/scala/org/apache/spark/ml/tuning/CrossValidator.scala
@@ -340,7 +340,13 @@ class CrossValidatorModel private[ml] (
     // Array[Double]
     size += SizeEstimator.estimate(avgMetrics)
     _subModels.foreach { models =>
-      size += models.iterator.flatMap(_.iterator).map(_.estimatedSize).sum
+      models.foreach { foldModels =>
+        foldModels.foreach { model =>
+          if (model != null) {
+            size += model.estimatedSize
+          }
+        }
+      }
     }
     size
   }
