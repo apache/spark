3fe23315edf2c675cab64a290a3cf050c0952985
SreeramaYeshwanthGowd <yeshwanthgowdsreerama@gmail.com>
2026-08-01 02:03:35 +0530

    [SPARK-51975][SQL] Add Connect functionTest and goldens for variant_from_arrays/entries

--- sql/connect/client/jvm/src/test/scala/org/apache/spark/sql/PlanGenerationTestSuite.scala
+++ sql/connect/client/jvm/src/test/scala/org/apache/spark/sql/PlanGenerationTestSuite.scala
@@ -2807,6 +2807,14 @@ class PlanGenerationTestSuite extends ConnectFunSuite with Logging {
     fn.schema_of_variant(fn.parse_json(fn.col("g")))
   }
 
+  functionTest("variant_from_arrays") {
+    fn.variant_from_arrays(fn.array(lit("a"), lit("b")), fn.array(lit(1), lit(2)))
+  }
+
+  functionTest("variant_from_entries") {
+    fn.variant_from_entries(fn.array(fn.struct(lit("a"), lit(1)), fn.struct(lit("b"), lit(2))))
+  }
+
   functionTest("schema_of_variant_agg") {
     fn.schema_of_variant_agg(fn.parse_json(fn.col("g")))
   }
