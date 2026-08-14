4fca9625342a2666d8a64355b7bfed433b24d235
Asif Hussain Shahid <asif.shahid@gmail.com>
2026-08-14 10:26:15 -0700

    made the test little compact

--- sql/core/src/test/scala/org/apache/spark/sql/execution/adaptive/TableCacheQueryStageExecSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/adaptive/TableCacheQueryStageExecSuite.scala
@@ -17,93 +17,63 @@
 
 package org.apache.spark.sql.execution.adaptive
 
+import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.execution.SparkPlan
 import org.apache.spark.sql.execution.exchange.ReusedExchangeExec
 import org.apache.spark.sql.functions.broadcast
 import org.apache.spark.sql.test.SharedSparkSession
 
-class TableCacheQueryStageExecSuite  extends SharedSparkSession with AdaptiveSparkPlanHelper {
+class TableCacheQueryStageExecSuite extends SharedSparkSession with AdaptiveSparkPlanHelper {
   import testImplicits._
 
   test("SPARK-58767: re-use of exchange involving cached plans ") {
-    withTable("t1", "t2", "t3") {
-      spark.sql(
-          """
-            |CREATE TABLE t1 USING PARQUET AS
-            |SELECT * FROM VALUES
-            |  (1, 2),
-            |  (1, 1),
-            |  (2, 2),
-            |  (2, 2),
-            |  (2, 1),
-            |  (3, 4),
-            |  (4, 3),
-            |  (3, 3),
-            |  (4, 4),
-            |  (5, 6),
-            |  (6, 5),
-            |  (6, 6),
-            |  (5, 5)
-            |AS data(c1_1, c2_1)
+    withTempPaths(3) { paths =>
+      // to be safe use 3 data paths so that the 3 tables are not in any situation canonicalized
+      // same.
+      val data = Seq((1, 1), (2, 2), (3, 3), (4, 4), (6, 6), (5, 5))
+      paths.zipWithIndex.foreach{case(path, indx) =>
+        val tableNumber = indx + 1
+        spark.createDataFrame(data).toDF(s"c1_$tableNumber", s"c2_$tableNumber").write.mode(
+          SaveMode.Overwrite).parquet(path.getAbsolutePath)}
+
+      withTable("t1", "t2", "t3") {
+        spark.sql(
+          s"""
+            |CREATE TABLE t1 USING PARQUET LOCATION '${paths(0).getAbsolutePath}'
             |""".stripMargin
         )
         spark.sql(
-          """
-            |CREATE TABLE t2 USING PARQUET AS
-            |SELECT * FROM VALUES
-            |  (1, 2),
-            |  (1, 1),
-            |  (2, 2),
-            |  (2, 2),
-            |  (2, 1),
-            |  (3, 4),
-            |  (4, 3),
-            |  (3, 3),
-            |  (4, 4),
-            |  (5, 6),
-            |  (6, 5),
-            |  (6, 6),
-            |  (5, 5)
-            |AS data(c1_2, c2_2)
-            |""".stripMargin
+          s"""
+             |CREATE TABLE t2 USING PARQUET LOCATION '${paths(1).getAbsolutePath}'
+             |""".stripMargin
+        )
+        spark.sql(
+          s"""
+             |CREATE TABLE t3 USING PARQUET LOCATION '${paths(2).getAbsolutePath}'
+             |""".stripMargin
         )
 
-        spark.sql(
-          """
-            |CREATE TABLE t3 USING PARQUET AS
-            |SELECT * FROM VALUES
-            | (1, 2),
-            |  (1, 1),
-            |  (2, 2),
-            |  (2, 2),
-            |  (2, 1),
-            |  (3, 4),
-            |  (4, 3),
-            |  (3, 3),
-            |  (4, 4),
-            |  (5, 6),
-            |  (6, 5),
-            |  (6, 6),
-            |  (5, 5)
-            |AS data(c1_3, c2_3)
-            |""".stripMargin
-        )
         spark.table("t1").where($"c1_1" > 0).createTempView("v1")
         spark.catalog.cacheTable("v1")
         val al1 = spark.table("v1").as("al1")
         val al2 = spark.table("v1").as("al2")
-        val df = spark.table("t2").where($"c2_2" > 0).join(
-            spark.table("t3"), spark.table("t2").col("c1_2") === spark.table("t3").col("c1_3")).
-          join(
-            broadcast(al1.where($"al1.c1_1" > 0)), $"al1.c1_1" === spark.table("t2").col("c1_2"))
-          .join(broadcast(al2.where($"al2.c1_1" > 0)), $"al2.c1_1" === spark.table("t3").col
-          ("c1_3"))
-        df.collect()
+        val t2 = spark.table("t2")
+        val t3 = spark.table("t3")
+        val j1 = t2.where($"c2_2" > 0).join(t3, t2.col("c1_2") === t3.col("c1_3"))
+        assert(j1.collect().nonEmpty)
+        val j2 = j1.join(
+          broadcast(al1.where($"al1.c1_1" > 0)), $"al1.c1_1" === t2.col("c1_2"))
+        assert(j2.collect().nonEmpty)
+        val df = j2
+          .join(broadcast(al2.where($"al2.c1_1" > 0)), $"al2.c1_1" === t3.col("c1_3"))
+        assert(df.collect().nonEmpty)
         val allBroadcastStages = collectAllBroadcastStageExec(df.queryExecution.executedPlan)
+        // There should be 1 re-use of exchange
         assert(allBroadcastStages.count(_.plan.isInstanceOf[ReusedExchangeExec]) == 1)
-
+      }
     }
   }
+
   def collectAllBroadcastStageExec(sp: SparkPlan): Seq[BroadcastQueryStageExec] = {
     sp match {
       case ap: AdaptiveSparkPlanExec => collectAllBroadcastStageExec(ap.finalPhysicalPlan)
@@ -112,6 +82,5 @@ class TableCacheQueryStageExecSuite  extends SharedSparkSession with AdaptiveSpa
 
       case other => other.children.flatMap(collectAllBroadcastStageExec)
     }
-
   }
 }
