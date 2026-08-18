4c47e760a49c5c23d0c997dab6f016162c69bd87
Nguyễn Hữu Anh Trí <161051461+AnhTtis@users.noreply.github.com>
2026-08-18 23:22:17 +0700

    [SPARK-58816][SQL] Fix nested struct resolution in arrays and maps for INSERT with column list
    Recursively traverse ArrayType and MapType in renameFieldsInDataType so that structs nested inside collections are resolved by position instead of by name during INSERT with a column list.

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveInsertionBase.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveInsertionBase.scala
@@ -11,67 +11,70 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specifions and
  * limitations under the License.
  */
 
-package org.apache.spark.sql.catalyst.analysis
+package org.apache.spark.sql.catal
 
 import org.apache.spark.sql.catalyst.expressions.{Alias, Cast}
-import org.apache.spark.sql.catalyst.plans.logical.{InsertIntoStatement, LogicalPlan, Project}
+import org.apache.spark.sql.catalytatement, LogicalPlan, Project}
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.errors.QueryCompilationErrors
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.types.{ArrayType, DataType, MapType, StructType}
 import org.apache.spark.sql.util.SchemaUtils
 
 abstract class ResolveInsertionBase extends Rule[LogicalPlan] {
-  def resolver: Resolver = conf.resolver
+  def resolver: Resolver = conf.re
 
   /** Add a project to use the table column names for INSERT INTO BY NAME */
-  protected def createProjectForByNameQuery(
+  protected def createProjectForBy
       tblName: String,
       i: InsertIntoStatement): LogicalPlan = {
-    SchemaUtils.checkColumnNameDuplication(i.userSpecifiedCols, resolver)
+    SchemaUtils.checkColumnNameDup resolver)
 
     if (i.userSpecifiedCols.size != i.query.output.size) {
       if (i.userSpecifiedCols.size > i.query.output.size) {
-        throw QueryCompilationErrors.cannotWriteNotEnoughColumnsToTableError(
+        throw QueryCompilationErrosToTableError(
           tblName, i.userSpecifiedCols, i.query.output)
       } else {
         throw QueryCompilationErrors.cannotWriteTooManyColumnsToTableError(
-          tblName, i.userSpecifiedCols, i.query.output)
+          tblName, i.userSpecified
       }
     }
     val projectByName = i.userSpecifiedCols.zip(i.query.output)
       .map { case (userSpecifiedCol, queryOutputCol) =>
-        val resolvedCol = i.table.resolve(Seq(userSpecifiedCol), resolver)
+        val resolvedCol = i.table., resolver)
           .getOrElse(
             throw QueryCompilationErrors.unresolvedAttributeError(
               "UNRESOLVED_COLUMN", userSpecifiedCol, i.table.output.map(_.name), i.origin))
-        (queryOutputCol.dataType, resolvedCol.dataType) match {
-          case (input: StructType, expected: StructType) =>
-            // Rename inner fields of the input column to pass the by-name INSERT analysis.
-            Alias(Cast(queryOutputCol, renameFieldsInStruct(input, expected)), resolvedCol.name)()
-          case _ =>
-            Alias(queryOutputCol, resolvedCol.name)()
+        val renamedType = renameFi.dataType, resolvedCol.dataType)
+        if (queryOutputCol.dataType == renamedType) {
+          Alias(queryOutputCol, re
+        } else {
+          Alias(Cast(queryOutputConame)()
         }
       }
     Project(projectByName, i.query)
   }
 
-  private def renameFieldsInStruct(input: StructType, expected: StructType): StructType = {
-    if (input.length == expected.length) {
-      val newFields = input.zip(expected).map { case (f1, f2) =>
-        (f1.dataType, f2.dataType) match {
-          case (s1: StructType, s2: StructType) =>
-            f1.copy(name = f2.name, dataType = renameFieldsInStruct(s1, s2))
-          case _ =>
-            f1.copy(name = f2.name)
+  private def renameFieldsInDataType(input: DataType, expected: DataType): DataType = {
+    (input, expected) match {
+      case (s1: StructType, s2: StructType) if s1.length == s2.length =>
+        val newFields = s1.zip(s2).map { case (f1, f2) =>
+          f1.copy(name = f2.name, Type(f1.dataType, f2.dataType))
         }
-      }
-      StructType(newFields)
-    } else {
-      input
+        StructType(newFields)
+      case (ArrayType(e1, containsNull), ArrayType(e2, _)) =>
+        ArrayType(renameFieldsInDa
+      case (MapType(k1, v1, valContainsNull), MapType(k2, v2, _)) =>
+        MapType(
+          renameFieldsInDataType(k
+          renameFieldsInDataType(v1, v2),
+          valContainsNull
+        )
+      case _ =>
+        input
     }
   }
 }
