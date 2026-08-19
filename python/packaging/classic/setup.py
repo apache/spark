33effd54b939224902513cf8909bd676e399abe7
Nicholas Chammas <nicholas.chammas@gmail.com>
2026-08-19 09:03:51 -0400

    [SPARK-58845][PYTHON] Collapse check-then-create-dir pattern into single call
    ### What changes were proposed in this pull request?
    
    Instead of checking whether a directory exists before calling `os.makedirs`, simply pass `exist_ok=True`.
    
    ### Why are the changes needed?
    
    Simpler, more idiomatic use of the Python standard library.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Existing CI tests.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #58032 from nchammas/makedirs-exist-ok.
    
    Authored-by: Nicholas Chammas <nicholas.chammas@gmail.com>
    Signed-off-by: Nicholas Chammas <nicholas.chammas@gmail.com>

--- python/packaging/classic/setup.py
+++ python/packaging/classic/setup.py
@@ -206,11 +206,7 @@ try:
     # We copy the shell script to be under pyspark/python/pyspark so that the launcher scripts
     # find it where expected. The rest of the files aren't copied because they are accessed
     # using Python imports instead which will be resolved correctly.
-    try:
-        os.makedirs("pyspark/python/pyspark")
-    except OSError:
-        # Don't worry if the directory already exists.
-        pass
+    os.makedirs("pyspark/python/pyspark", exist_ok=True)
     copyfile("pyspark/shell.py", "pyspark/python/pyspark/shell.py")
 
     if in_spark:
