a9c59d9dba1b893b35f41cab8d79444c87c63a2a
StormSpirit <jiangxt2@vip.qq.com>
2026-08-20 10:19:25 +0800

    Update sql/api/src/main/scala/org/apache/spark/sql/functions.scala
    Agreed
    
    Co-authored-by: Wenchen Fan <cloud0fan@gmail.com>

--- sql/api/src/main/scala/org/apache/spark/sql/functions.scala
+++ sql/api/src/main/scala/org/apache/spark/sql/functions.scala
@@ -7371,8 +7371,7 @@ object functions {
    * input column should be bitmaps created from bitmap_construct_agg().
    *
    * @param col
-   *   The input column should be bitmaps created from bitmap_construct_agg(). A column that
-   *   evaluates to a binary.
+   *   A column containing bitmaps created by bitmap_construct_agg() and evaluating to binary data.
    * @group agg_funcs
    * @since 4.4.0
    * @return
