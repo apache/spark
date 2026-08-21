4d8aafab60159b11a347a3302ee5b7390123cf76
Shinoaki14 <nhan100405@gmail.com>
2026-08-21 12:56:13 +0700

    [SPARK-58927][SQL] Support hash-based aggregation on non-binary collated grouping keys

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/RewriteCollationAggregate.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/RewriteCollationAggregate.scala
@@ -30,8 +30,8 @@ import org.apache.spark.sql.catalyst.util.UnsafeRowUtils
  * This rule rewrites Aggregate grouping expressions to ensure that non-binary collated strings
  * are converted to their binary-stable collation keys (via [[CollationKey]]).
  *
- * This allows hash-based aggregation (e.g., [[org.apache.spark.sql.execution.aggregate.ObjectHashAggregateExec]])
- * to work properly on data with non-binary collations, avoiding full sorting and spilling.
+ * This allows hash-based aggregation (e.g., ObjectHashAggregateExec) to work properly on data
+ * with non-binary collations, avoiding full sorting and spilling.
  *
  * Any original grouping expression referenced in the aggregate expressions (output) is preserved
  * by wrapping it in `First(expr, ignoreNulls = false)`, an arbitrary representative of each
@@ -44,7 +44,8 @@ object RewriteCollationAggregate extends Rule[LogicalPlan] {
     } else {
       plan.transformWithPruning(_.containsPattern(AGGREGATE)) {
         case a @ Aggregate(groupingExpressions, aggregateExpressions, child, _)
-            if a.resolved && groupingExpressions.exists(e => !UnsafeRowUtils.isBinaryStable(e.dataType)) =>
+            if a.resolved &&
+              groupingExpressions.exists(e => !UnsafeRowUtils.isBinaryStable(e.dataType)) =>
           val keyMapping = mutable.LinkedHashMap.empty[Expression, Expression]
           val newGroupingExpressions = groupingExpressions.map { ge =>
             val processed = CollationKey.injectCollationKey(ge)
@@ -73,7 +74,10 @@ object RewriteCollationAggregate extends Rule[LogicalPlan] {
               case a @ Alias(child, name) =>
                 val newChild = replaceGroupingKeyReferences(child)
                 if (!newChild.fastEquals(child)) {
-                  Alias(newChild, name)(exprId = a.exprId, qualifier = a.qualifier, explicitMetadata = a.explicitMetadata)
+                  Alias(newChild, name)(
+                    exprId = a.exprId,
+                    qualifier = a.qualifier,
+                    explicitMetadata = a.explicitMetadata)
                 } else {
                   a
                 }
