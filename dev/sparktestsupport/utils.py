9a75a1d04be4b82c0626f5ac00cb65b59cfdf37e
Nicholas Chammas <nicholas.chammas@gmail.com>
2026-08-21 10:46:20 -0400

    [SPARK-58885][BUILD][INFRA] Refactor handling of ignored patterns in module test detection
    ### What changes were proposed in this pull request?
    
    Refactor how we ignore file patterns when determining what modules to test. Use `pathlib.PurePath` glob-style patterns instead of regexes.
    
    Add some doctests to `modules.py` so we can test the new `is_ignored_file` function directly. Ensure these doctests can be run by themselves and are also run as part of `run-tests.py`.
    
    ### Why are the changes needed?
    
    #56312 captured ignored patterns as a `dev-tools` module that has special handling. The problem is that test module detection is additive, so a change to `python/README.md` triggers `dev-tools` _and_ `pyspark-install` and `pyspark-core`. You can see this in the [CI run associated with][1] https://github.com/apache/spark/commit/75ac74bafec1f489224765041a19bec6bad36a9d.
    
    [1]: https://github.com/apache/spark/actions/runs/32265494187/job/96146278679
    
    There is also a problem with using regexes for these ignore patterns because most of the patterns include extensions like `.sh` or `.md` without escaping the `.`. This leads to the unlikely but nonetheless surprising case of `README1md` matching the `README.md` pattern.
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    I ran the doctests on `modules.py` and `utils.py`.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    I wrote this with assistance from GitHub Copilot.
    
    Closes #58136 from nchammas/SPARK-58885-ignored-test-patterns.
    
    Authored-by: Nicholas Chammas <nicholas.chammas@gmail.com>
    Signed-off-by: Nicholas Chammas <nicholas.chammas@gmail.com>

--- dev/sparktestsupport/utils.py
+++ dev/sparktestsupport/utils.py
@@ -39,9 +39,13 @@ def determine_modules_for_files(filenames):
     ['pyspark-core', 'pyspark-install', 'sql']
     >>> [x.name for x in determine_modules_for_files(["file_not_matched_by_any_subproject"])]
     ['root']
+    >>> [x.name for x in determine_modules_for_files(["python/README.md"])]
+    []
     """
     changed_modules = set()
     for filename in filenames:
+        if modules.is_ignored_file(filename):
+            continue
         if ("GITHUB_ACTIONS" not in os.environ) and filename.startswith(".github"):
             continue
         matched_at_least_one_module = False
