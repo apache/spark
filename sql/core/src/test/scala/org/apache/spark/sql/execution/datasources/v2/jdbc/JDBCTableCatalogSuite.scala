2c7626a30adc855b75a11beb85b4b74f19ef67d9
subhramit <subhramit.bb@live.in>
2026-09-04 23:59:21 +0530

    [SPARK-58945][SQL] Remove obsolete imports in JDBCTableCatalogSuite
    Signed-off-by: subhramit <subhramit.bb@live.in>

--- sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/v2/jdbc/JDBCTableCatalogSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/datasources/v2/jdbc/JDBCTableCatalogSuite.scala
@@ -16,7 +16,7 @@
  */
 package org.apache.spark.sql.execution.datasources.v2.jdbc
 
-import java.sql.{Connection, DriverManager, SQLException}
+import java.sql.{Connection, DriverManager}
 import java.util.Properties
 
 import scala.jdk.CollectionConverters._
@@ -32,7 +32,6 @@ import org.apache.spark.sql.connector.catalog.{Identifier, TableSummary}
 import org.apache.spark.sql.errors.DataTypeErrors.{toSQLConf, toSQLStmt}
 import org.apache.spark.sql.execution.columnar.InMemoryTableScanExec
 import org.apache.spark.sql.internal.SQLConf
-import org.apache.spark.sql.jdbc.H2Dialect
 import org.apache.spark.sql.test.SharedSparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
