d511027ce40ea2534c827e61eaf0c0b0f80032eb
Jungtaek Lim <kabhwan.opensource@gmail.com>
2026-09-08 11:41:52 +0900

    [SPARK-57489][SS][FOLLOWUP] Model projected metadata in tests

--- sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/ResolveDeduplicateSuite.scala
+++ sql/catalyst/src/test/scala/org/apache/spark/sql/catalyst/analysis/ResolveDeduplicateSuite.scala
@@ -20,7 +20,7 @@ package org.apache.spark.sql.catalyst.analysis
 import org.apache.spark.sql.AnalysisException
 import org.apache.spark.sql.catalyst.dsl.expressions._
 import org.apache.spark.sql.catalyst.expressions.{Attribute, MetadataAttribute}
-import org.apache.spark.sql.catalyst.plans.logical.{Deduplicate, DeduplicateAllColumnsAsKey, DeduplicateKeyColumns, DeduplicateSpec, DeduplicateWithinWatermark, LocalRelation, LogicalPlan}
+import org.apache.spark.sql.catalyst.plans.logical.{Deduplicate, DeduplicateAllColumnsAsKey, DeduplicateKeyColumns, DeduplicateSpec, DeduplicateWithinWatermark, LocalRelation, LogicalPlan, Project}
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.StringType
 
@@ -134,8 +134,9 @@ class ResolveDeduplicateSuite extends AnalysisTest {
   test("streaming recomputation excludes metadata added after the deduplication boundary") {
     val evolvedDataColumn = $"evolved".int
     val metadataAddedAfterDeduplication = MetadataAttribute("_metadata", StringType)
-    val recomputationChild = LocalRelation(
+    val expandedRelation = LocalRelation(
       rel.output ++ Seq(evolvedDataColumn, metadataAddedAfterDeduplication))
+    val recomputationChild = Project(expandedRelation.output, expandedRelation)
     val spec = DeduplicateSpec(DeduplicateAllColumnsAsKey, viaSparkClassic = true)
 
     val recomputedKeys = ResolveDeduplicate.recomputeKeysPreservingMetadataBoundary(
@@ -147,7 +148,8 @@ class ResolveDeduplicateSuite extends AnalysisTest {
   test("streaming recomputation retains metadata visible at the deduplication boundary") {
     val metadataSelectedBeforeDeduplication = MetadataAttribute("_metadata", StringType)
     val originalKeys = rel.output :+ metadataSelectedBeforeDeduplication
-    val recomputationChild = LocalRelation(originalKeys)
+    val relation = LocalRelation(originalKeys)
+    val recomputationChild = Project(relation.output, relation)
     val spec = DeduplicateSpec(DeduplicateAllColumnsAsKey, viaSparkClassic = true)
 
     val recomputedKeys = ResolveDeduplicate.recomputeKeysPreservingMetadataBoundary(
