37d882a09fc9ff26f2405488f6adbec5d8ee63fa
Tian Gao <gaogaotiantian@hotmail.com>
2026-09-10 13:55:30 -0700

    Use pyspark periodic and add it to all scheduled CIs

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -914,8 +914,8 @@ pyspark_ml = Module(
     ],
 )
 
-pyspark_scheduled = Module(
-    name="pyspark-scheduled",
+pyspark_periodic = Module(
+    name="pyspark-periodic",
     dependencies=[],
     source_file_regexes=[
         # This module contains tests that are not sensitive to pyspark code changes.
