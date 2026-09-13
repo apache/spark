2d309efaf85b18d932acd837e81e60f83d6bad57
Nicholas Chammas <nicholas.chammas@gmail.com>
2026-09-13 23:40:16 +0300

    add sbin script to pyspark-connect test module

--- dev/sparktestsupport/modules.py
+++ dev/sparktestsupport/modules.py
@@ -1251,6 +1251,7 @@ pyspark_connect = Module(
     dependencies=[pyspark_sql, connect],
     source_file_regexes=[
         "python/pyspark/sql/connect",
+        "sbin/start-connect-server.sh",
     ],
     python_test_goals=[
         # sql doctests
