463c67e6d74c8df77f9f4852a44f1b5537781db7
srielau <serge@rielau.com>
2026-09-16 16:35:33 +0000

    fix: [SPARK-59274] preserve parser map ordering

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DuplicateMapKeyUtils.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/util/DuplicateMapKeyUtils.scala
@@ -20,6 +20,9 @@ package org.apache.spark.sql.catalyst.util
 import scala.collection.mutable
 
 import org.apache.spark.SparkRuntimeException
+import org.apache.spark.sql.errors.QueryExecutionErrors
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.types.{DataType, StringType}
 import org.apache.spark.unsafe.types.UTF8String
 import org.apache.spark.util.SparkErrorUtils
 
@@ -35,7 +38,38 @@ private[sql] object DuplicateMapKeyUtils {
   def unapply(exception: Throwable): Option[SparkRuntimeException] = cause(exception)
 
   def lastOccurrenceIndices(rawKeys: Array[UTF8String]): Seq[Int] = {
-    val seen = mutable.HashSet.empty[UTF8String]
-    rawKeys.indices.reverseIterator.filter(index => seen.add(rawKeys(index))).toSeq.reverse
+    val lastIndices = mutable.LinkedHashMap.empty[UTF8String, Int]
+    rawKeys.indices.foreach(index => lastIndices.update(rawKeys(index), index))
+    lastIndices.values.toSeq
+  }
+
+  def buildMapWithLastRawKeyWins(
+      rawKeys: Seq[UTF8String],
+      normalizedKeys: Seq[UTF8String],
+      values: Seq[Option[Any]],
+      keyType: DataType,
+      valueType: DataType): MapData = {
+    require(rawKeys.length == normalizedKeys.length && rawKeys.length == values.length)
+    val indices = lastOccurrenceIndices(rawKeys.toArray)
+    if (SQLConf.get.getConf(SQLConf.MAP_KEY_DEDUP_POLICY) ==
+        SQLConf.MapKeyDedupPolicy.EXCEPTION) {
+      val distinctKeys = keyType match {
+        case stringType: StringType if stringType.supportsBinaryEquality =>
+          new java.util.HashSet[Any]()
+        case _ =>
+          new java.util.TreeSet[Any](TypeUtils.getInterpretedOrdering(keyType))
+      }
+      indices.foreach { index =>
+        val key = normalizedKeys(index)
+        if (!distinctKeys.add(key)) {
+          throw QueryExecutionErrors.duplicateMapKeyFoundError(key)
+        }
+      }
+    }
+    val builder = new ArrayBasedMapBuilder(keyType, valueType)
+    indices.foreach { index =>
+      values(index).foreach(builder.put(normalizedKeys(index), _))
+    }
+    builder.build()
   }
 }
