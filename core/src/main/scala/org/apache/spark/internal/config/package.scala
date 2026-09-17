04f63271970e46dabcfc5bf145d642d0a161d043
Holden Karau <holden@pigscanfly.ca>
2026-09-17 03:34:51 -0700

    Have fable generate some more tests and also update the comment on env vars

--- core/src/main/scala/org/apache/spark/internal/config/package.scala
+++ core/src/main/scala/org/apache/spark/internal/config/package.scala
@@ -2835,7 +2835,8 @@ package object config {
   private[spark] val STANDALONE_SUBMIT_FILTER_ENVIRONMENT =
     ConfigBuilder("spark.standalone.submit.filterEnvironment")
       .doc("In standalone cluster mode, controls whether the client forwards only " +
-        "Spark-related environment variables (e.g. SPARK_*) to the driver, " +
+        "Spark-related environment variables (e.g. SPARK_* excluding " +
+        "SPARK_ENV_LOADED, SPARK_HOME, SPARK_CONF_DIR) to the driver, " +
         "matching the REST submission client. If set to false, the full environment is " +
         "forwarded to the driver. This does not impact REST submissions except if they fall back.")
       .version("4.3.0")
