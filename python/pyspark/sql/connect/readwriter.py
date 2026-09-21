42d0c965c639c35849bcee503050a23ab09fafab
Haoyan Geng <haoyan.geng@gmail.com>
2026-09-21 21:04:34 +0000

    [SPARK-59699][CONNECT] Make DataFrameReader options case-insensitive

--- python/pyspark/sql/connect/readwriter.py
+++ python/pyspark/sql/connect/readwriter.py
@@ -111,6 +111,10 @@ class DataFrameReader(OptionUtils):
     schema.__doc__ = PySparkDataFrameReader.schema.__doc__
 
     def option(self, key: str, value: "OptionalPrimitiveType") -> "DataFrameReader":
+        normalized_key = key.lower()
+        for existing_key in list(self._options):
+            if existing_key.lower() == normalized_key:
+                del self._options[existing_key]
         self._options[key] = cast(str, to_str(value))
         return self
 
