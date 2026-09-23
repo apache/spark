31e8ae915c462edeba33d7ccdd6875cd3593323a
Tian Gao <gaogaotiantian@hotmail.com>
2026-09-23 16:23:23 -0700

    [SPARK-59508][PYTHON] Harden client safety with restricted unpickle serializer
    * Add an optional argument `allowed_names` for `CloudPickleSerializer`. If specified, it will be the allowlist for unpickling.
    * Replace the unrestricted `pickleSer` with a restricted one which only allows unpickling `pstat.Stats`.
    
    The client unpickles arbitrary bytes from the server. If the server is compromised, this will behave as a RCE. We should only allow pickling on the data structure we actually use.
    
    No.
    
    A new test is added to test the restricted unpickler.
    
    No.
    
    Closes #58798 from gaogaotiantian/safe-pickle.
    
    Authored-by: Tian Gao <gaogaotiantian@hotmail.com>
    Signed-off-by: Tian Gao <gaogaotiantian@hotmail.com>
    (cherry picked from commit 6886811d1b38b2452e394486026e61be9281f3d9)
    Signed-off-by: Tian Gao <gaogaotiantian@hotmail.com>

--- python/pyspark/sql/connect/client/core.py
+++ python/pyspark/sql/connect/client/core.py
@@ -30,6 +30,7 @@ import logging
 import threading
 import os
 import copy
+import pickle
 import platform
 import urllib.parse
 import uuid
@@ -64,7 +65,7 @@ from google.protobuf import text_format, any_pb2
 from google.rpc import error_details_pb2
 
 from pyspark.util import is_remote_only, disable_gc
-from pyspark.accumulators import SpecialAccumulatorIds, pickleSer
+from pyspark.accumulators import SpecialAccumulatorIds, specialAccumulatorSer
 from pyspark.version import __version__
 from pyspark.traceback_utils import CallSite
 from pyspark.resource.information import ResourceInformation
@@ -1560,7 +1561,15 @@ class SparkConnectClient(object):
                     else:
                         if observed_metrics.name == "__python_accumulator__":
                             for metric in observed_metrics.metrics:
-                                aid, update = pickleSer.loads(LiteralExpression._to_value(metric))
+                                try:
+                                    aid, update = specialAccumulatorSer.loads(
+                                        LiteralExpression._to_value(metric)
+                                    )
+                                except pickle.UnpicklingError as e:
+                                    # We found unexpected class/function in the accumulator metric.
+                                    # We will ignore this metric and continue.
+                                    logger.warning(f"Error unpickling accumulator metric: {e}")
+                                    continue
                                 if aid == SpecialAccumulatorIds.SQL_UDF_PROFIER_V2:
                                     self._profiler_collector._update(update)
                         elif observed_metrics.name in observations:
